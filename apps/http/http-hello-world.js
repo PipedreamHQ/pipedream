01ee456ec164589aa845b599291432a4173687d8
Pravin <psavkar@yahoo.com>
2020-03-08 22:35:03 -0700

    Create http-hello-world.js

---
+++ apps/http/http-hello-world.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "http",
+  version: "0.0.1",
+  props: {
+    http: "$.interface.http"
+  },
+  events: {
+    async default(event) {
+      this.http.respond({
+        status: 200,
+        body: "Hello world!",
+      });
+      // Emit the whole event, which contains
+      // the HTTP payload, headers, and more
+      this.$emit(event);
+    }
+  }
+};
