58c4b161f89e7250e262b186666ff806ac019580
PravinS <psavkar@yahoo.com>
2020-04-06 16:17:08 -0700

    updates

---
+++ components/twitter/twitter-search.js
@@ -0,0 +1,44 @@
+const twitter = require('twitter')
+const _ = require('lodash')
+
+module.exports = {
+  name: "twitter-search",
+  version: "0.0.1",
+  props: {
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      intervalSeconds: 60,
+    },
+    twitter: {
+      type: "app",
+      app: "twitter",
+    },
+    searchTerm: "string",
+  },
+  events: {
+    async default(event) {
+      const since_id = this.db.get("since_id") || 0
+      const oauthSignerUri = this.twitter.$auth.oauth_signer_uri
+      const tweet_mode = 'extended'
+      const count = '100'
+
+      const response = this.twitter.search(this.searchTerm, since_id, tweet_mode, count)
+
+      let maxId = since_id
+
+      response.statuses.forEach(tweet => {
+        if (tweet.in_reply_to_status_id === null && tweet.retweet_count === 0) {
+          if (_.get(tweet,'retweeted_status','') === '') {
+            this.$emit(tweet)
+          }
+          if (tweet.id > maxId) {
+            maxId = tweet.id
+          }
+        }
+      })
+
+      this.db.set("since_id", maxId)
+    },
+  },
+}
---
+++ components/twitter/twitter.js
@@ -0,0 +1,56 @@
+const axios = require('axios')
+const querystring = require("querystring")
+
+module.exports = {
+  type: "app",
+  app: "twitter",
+  propDefinitions: {
+    // TODO
+  },
+  methods: {
+    async _getAuthorizationHeader({ data, method, url }) {
+      const requestData = {
+        data,
+        method,
+        url,
+      }
+      const token = {
+        key: this.$auth.oauth_access_token,
+        secret: this.$auth.oauth_refresh_token,
+      }
+      return (await axios({
+        method: 'POST',
+        url: this.$auth.oauth_signer_uri,
+        data: {
+          requestData,
+          token,
+        }
+      })).data
+    },
+    async _makeRequest(config) {
+      if (!config.headers) config.headers = {}
+      const authorization = await this._getAuthorizationHeader(config)
+      config.headers.authorization = authorization
+      try {
+        return await axios(config)
+      } catch (err) {
+        console.log(err) // TODO
+      }
+    },
+    async search(q, since_id, tweet_mode, count) {   
+      const query = querystring.stringify({
+        q,
+        since_id,
+        tweet_mode,
+        count,
+      })
+
+      return (await this._makeRequest({
+        url: `https://api.twitter.com/1.1/search/tweets.json?${query}`,
+      })).data
+    },
+    webhooks: {
+      // TODO
+    },
+  },
+}
