24fe62d0867ec54dfb5021010b77749ffc07221a
Tod Sacerdoti <todsac@gmail.com>
2020-04-07 13:07:35 -0700

    Update firehose.js

--- components/hacker-news/firehose.js
+++ components/hacker-news/firehose.js
@@ -18,19 +18,26 @@ module.exports = {
   },
   events: {
     async default() {
-      const lastSeenKey = this.db.get("lastSeenKey")
+      const seenKeys = this.db.get("seenKeys") || []
+      const seenKeysMap = seenKeys.reduce((acc, v) => {
+        acc[v] = true
+        return acc
+      }, {})
       // All elements of an item are optional, however at least one of title or description must be present.
       // should be listed from most recent to least recent
-      const feed = await parser.parseURL(`https://hnrss.org/newest`)
-      let lastSeenIdx = feed.items.findIndex(item => lastSeenKey === this.itemKey(item))
-      const emitStartIdx = lastSeenIdx < 0 ? feed.items.length - 1 : lastSeenIdx - 1
-      let lastEmittedItem
-      for (let idx = emitStartIdx; idx >= 0; idx--) {
-        lastEmittedItem = feed.items[idx]
-        this.$emit(lastEmittedItem)
+      const feed = await parser.parseURL(this.url)
+      for (let idx = feed.items.length - 1; idx >= 0; idx--) {
+        const item = feed.items[idx]
+        const key = this.itemKey(item)
+        // XXX throw if !key?
+        if (seenKeysMap[key]) continue
+        seenKeys.unshift(key)
+        seenKeysMap[key] = true // just in case of dupes
+        this.$emit(item)
       }
-      if (lastEmittedItem) {
-        this.db.set("lastSeenKey", this.itemKey(lastEmittedItem))
+      if (seenKeys.length) {
+        // XXX restrict by byte size instead of el size
+        this.db.set("seenKeys", seenKeys.slice(0, 1000))
       }
     },
   },
