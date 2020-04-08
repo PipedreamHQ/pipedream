e914568a10f8ccba3a2540141b71889e6cace4ea
PravinS <psavkar@yahoo.com>
2020-04-07 18:42:10 -0700

    Update twitter-search.js

--- components/twitter/twitter-search.js
+++ components/twitter/twitter-search.js
@@ -22,8 +22,6 @@ module.exports = {
 
     let maxId = since_id
 
-    console.log(response.statuses)
-
     response.statuses.forEach(tweet => {
       if (tweet.in_reply_to_status_id === null && tweet.retweet_count === 0) {
         if (_.get(tweet,'retweeted_status','') === '') {
