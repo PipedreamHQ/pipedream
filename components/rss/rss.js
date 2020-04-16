405f2b58f19713e414c4887ad59614d2df497ac7
psavkar <psavkar@yahoo.com>
2020-04-16 16:45:16 -0700

    update async run() and added emit summary

--- components/rss/rss.js
+++ components/rss/rss.js
@@ -23,29 +23,31 @@ module.exports = {
       return item.guid || item.id || item.title || item.description
     },
   },
-  events: {
-    async default() {
-      const seenKeys = this.db.get("seenKeys") || []
-      const seenKeysMap = seenKeys.reduce((acc, v) => {
-        acc[v] = true
-        return acc
-      }, {})
-      // All elements of an item are optional, however at least one of title or description must be present.
-      // should be listed from most recent to least recent
-      const feed = await parser.parseURL(this.url)
-      for (let idx = feed.items.length - 1; idx >= 0; idx--) {
-        const item = feed.items[idx]
-        const key = this.itemKey(item)
-        // XXX throw if !key?
-        if (seenKeysMap[key]) continue
-        seenKeys.unshift(key)
-        seenKeysMap[key] = true // just in case of dupes
-        this.$emit(item)
-      }
-      if (seenKeys.length) {
-        // XXX restrict by byte size instead of el size
-        this.db.set("seenKeys", seenKeys.slice(0, 1000))
-      }
-    },
+  async run() {
+    const seenKeys = this.db.get("seenKeys") || []
+    const seenKeysMap = seenKeys.reduce((acc, v) => {
+      acc[v] = true
+      return acc
+    }, {})
+    // All elements of an item are optional, however at least one of title or description must be present.
+    // should be listed from most recent to least recent
+    const feed = await parser.parseURL(this.url)
+    for (let idx = feed.items.length - 1; idx >= 0; idx--) {
+      const item = feed.items[idx]
+      const key = this.itemKey(item)
+      // XXX throw if !key?
+      if (seenKeysMap[key]) continue
+      seenKeys.unshift(key)
+      seenKeysMap[key] = true // just in case of dupes
+      this.$emit(item, {
+        summary: item.title, 
+        ts: (item.pubDate ? new Date(item.pubDate).getTime() : undefined), 
+        id: item.id,
+      })
+    }
+    if (seenKeys.length) {
+      // XXX restrict by byte size instead of el size
+      this.db.set("seenKeys", seenKeys.slice(0, 1000))
+    }
   },
 }
