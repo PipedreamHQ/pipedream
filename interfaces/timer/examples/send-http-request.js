701134398b697ab5321724a3c4b65489495b9464
Dylan J. Sather <dylan.sather@gmail.com>
2020-04-16 15:01:23 -0700

    Correcting cron examples to use default prop syntax

--- interfaces/timer/examples/send-http-request.js
+++ interfaces/timer/examples/send-http-request.js
@@ -1,20 +1,22 @@
 module.exports = {
-  name: "invoke-http-function",
-  version: "0.0.1",
+  name: 'invoke-http-function',
+  version: '0.0.1',
   props: {
     timer: {
-      type: "$.interface.timer",
-      cron: "0 0 * * *"
-    }
+      type: '$.interface.timer',
+      default: {
+        cron: '0 0 * * *',
+      },
+    },
   },
   async run() {
-    const axios = require("axios");
+    const axios = require('axios');
 
     const resp = await axios({
-      method: "GET",
-      url: `https://swapi.co/api/films/`
+      method: 'GET',
+      url: `https://swapi.co/api/films/`,
     });
 
     console.log(resp.data);
-  }
+  },
 };
