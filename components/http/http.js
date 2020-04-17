293c425051c0594ab761fbb1f8f29cf21dc55d87
psavkar <psavkar@yahoo.com>
2020-04-17 15:58:37 -0700

    updates to props and emit summary

--- components/http/http.js
+++ components/http/http.js
@@ -1,20 +1,44 @@
 // Core HTTP component
-// Returns a 200 OK response, emits the whole HTTP request as an event
 module.exports = {
   name: "http",
   version: "0.0.1",
   props: {
-    http: "$.interface.http"
+    http: "$.interface.http",
+    eventShape: {
+      type: "string", 
+      label: "Event Shape",
+      description: "Emit the full HTTP request (including header, body, and query) or just the HTTP body.",
+      optional: true,
+      options: ['Full HTTP Request', 'HTTP Body Only'],
+      default: 'Full HTTP Request',
+    },
+    httpStatusCode: {
+      type: "string", 
+      label: "HTTP Status Code",
+      description: "The status code to return in the HTTP response.",
+      optional: true,
+      default: '200',
+    },
+    httpResponseBody: {
+      type: "string", 
+      label: "HTTP Response Body",
+      description: "The body to return in the HTTP response.",
+      optional: true,
+      default: `{ success: true }`,
+    },
   },
   async run(event) {
+    const summary = `${event.method} ${event.path}`
+
     this.http.respond({
-      status: 200,
-      body: {
-        success: true
-      }
+      status: this.httpStatusCode,
+      body: this.httpResponseBody,
     });
-    // Emit the whole event, which contains
-    // the HTTP payload, headers, and more
-    this.$emit(event);
+
+    if(this.eventShape === 'HTTP Body Only') {
+      this.$emit(event.body, { summary })
+    } else {
+      this.$emit(event, { summary })
+    }
   }
-};
+}
