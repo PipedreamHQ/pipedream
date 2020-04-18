84258ff769f54372602b035f35d49396138a5144
psavkar <psavkar@yahoo.com>
2020-04-18 10:24:05 -0700

    update

--- components/twitter/twitter-search.js
+++ components/twitter/twitter-search.js
@@ -1,4 +1,77 @@
-const twitter = require('https://github.com/PipedreamHQ/pipedream/blob/add-twitter/components/twitter/twitter.app.js')
+const twitter = {
+  type: "app",
+  app: "twitter",
+  propDefinitions: {
+    q: {
+      type: "string",
+      label: 'Search Term',
+      description: "Search for keywords, screen names, or hashtags. You can also use Twitter's standard search operators (https://developer.twitter.com/en/docs/tweets/rules-and-filtering/overview/standard-operators).",
+    },
+    result_type: {
+      type: "string", 
+      label: "Result Type",
+      description: `Specifies the type of results you want to retrieve.`,
+      optional: true,
+      options: ['recent', 'popular', 'mixed'],
+      default: 'recent',
+    },
+    count: {
+      type: "string",
+      label: "Count",
+      description: "The maximum number of tweets to return (up to 100)",
+      optional: true,
+      default: "100",
+    },
+  },
+  methods: {
+    async _getAuthorizationHeader({ data, method, url }) {
+      const requestData = {
+        data,
+        method,
+        url,
+      }
+      const token = {
+        key: this.$auth.oauth_access_token,
+        secret: this.$auth.oauth_refresh_token,
+      }
+      return (await axios({
+        method: 'POST',
+        url: this.$auth.oauth_signer_uri,
+        data: {
+          requestData,
+          token,
+        }
+      })).data
+    },
+    async _makeRequest(config) {
+      if (!config.headers) config.headers = {}
+      const authorization = await this._getAuthorizationHeader(config)
+      config.headers.authorization = authorization
+      try {
+        return await axios(config)
+      } catch (err) {
+        console.log(err) // TODO
+      }
+    },
+    async search(q, since_id, tweet_mode, count, result_type) {   
+      const query = querystring.stringify({
+        q,
+        since_id,
+        tweet_mode,
+        count,
+        result_type,
+      })
+      console.log(query)
+      return (await this._makeRequest({
+        url: `https://api.twitter.com/1.1/search/tweets.json?${query}`,
+      })).data
+    },
+    webhooks: {
+      // TODO
+    },
+  },
+}
+
 const _ = require('lodash')
 const querystring = require("querystring")
 const axios = require('axios')
