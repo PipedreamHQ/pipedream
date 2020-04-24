e70f2e18152a6b5a558c794850ff93d73050a488
jason <jason@pipedream.com>
2020-04-24 11:13:45 -0700

    put hooks back

--- components/google-calendar/google-calendar-new-calendar.js
+++ components/google-calendar/google-calendar-new-calendar.js
@@ -125,7 +125,7 @@ const googleCalendar = {
 }
 
 module.exports = {
-  name: 'google-calendar-event-canclled',
+  name: 'google-calendar-new-calendar',
   version: '0.0.1',
   props: {
     db: '$.service.db',
@@ -136,6 +136,15 @@ module.exports = {
         cron: "0/5 * * * *",
       },
     },
+  },
+  hooks: {
+    activate() {
+      // TODO get current calendars so we don't emit everything?
+      const calListResp = await this.googleCalendar.calendarList()
+      const calendars = _.get(calListResp, "data.items")
+      const calendarIds = calendars.map( item => item.id )
+      this.db.set("calendarIds", calendarIds)
+    }
   },
   async run(event) {
     const previousCalendarIds = this.db.get('calendarIds') || []
