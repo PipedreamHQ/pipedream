def085273e008c785158a483e02a5cc37e249b16
jsendo <jason@pipedream.com>
2020-04-28 11:34:03 -0700

    Google calendar (#15)
    Adding Google Calendar sources.

---
+++ components/google-calendar/event-ended.js
@@ -0,0 +1,58 @@
+const _ = require("lodash")
+const googleCalendar = require('https://github.com/PipedreamHQ/pipedream/components/google-calendar/google-calendar.app.js')
+
+module.exports = {
+  name: 'google-calendar-event-ended',
+  version: '0.0.1',
+  props: {
+    googleCalendar,
+    calendarId: {
+      type: "string",
+      async options() {
+        const calListResp = await this.googleCalendar.calendarList()
+        const calendars = _.get(calListResp, "data.items")
+        if (calendars) {
+          const calendarIds = calendars.map(item => { return {value: item.id, label: item.summary} })
+          return calendarIds
+        }
+        return []
+      }
+    },
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 5 * 60,
+      },
+    },
+  },
+  async run(event) {
+    const intervalMs = 1000 * (event.interval_seconds || 300) // fall through to default for manual testing
+    const now = new Date()
+
+    const timeMin = new Date(now.getTime() - intervalMs).toISOString()
+    const timeMax = new Date(now.getTime()).toISOString()
+
+    const config = {
+      calendarId: this.calendarId,
+      timeMax,
+      timeMin,
+      singleEvents: true,
+      orderBy: this.orderBy,
+    }
+    const resp = await this.googleCalendar.getEvents(config)
+
+    const events = _.get(resp.data, "items")
+    if (Array.isArray(events)) {
+      for (const event of events) {
+        const eventEnd = _.get(event, "end.dateTime")
+        const end = new Date(eventEnd)
+        const msFromEnd = now.getTime() - end.getTime()
+        if (eventEnd && msFromEnd > 0 && msFromEnd < intervalMs) {
+          this.$emit(event)
+        }
+      }
+    } else {
+      console.log("nothing to emit")
+    }
+  },
+}
