d62793bbee3bbda1db3846bcc1f45d833e3ecc45
Tod Sacerdoti <todsac@gmail.com>
2020-05-01 11:10:25 -0700

    Update new-comments-by-keyword.js

--- components/hacker-news/new-comments-by-keyword.js
+++ components/hacker-news/new-comments-by-keyword.js
@@ -1,12 +1,17 @@
 const Parser = require("rss-parser")
 const parser = new Parser()
 
+const hacker_news = {
+  type: "app",
+  app: "hacker_news",
+}
+
 module.exports = {
   name: "hacker-news-new-comments-by-keyword",
   app: "hacker_news",
   version: "0.0.2",
   props: {
-    // XXX have auto: true prop in case there is something to configure (unless pass --no-auto)
+    hacker_news,
     timer: {
       type: "$.interface.timer",
       default: {
