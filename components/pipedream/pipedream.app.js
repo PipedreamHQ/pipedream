11eb805b61e6a99861e4767be1aa163001e93131
Dylan J. Sather <dylan.sather@gmail.com>
2020-05-03 11:45:49 -0700

    Initial commit

---
+++ components/pipedream/pipedream.app.js
@@ -0,0 +1,28 @@
+const PIPEDREAM_BASE_URL = "https://api.pipedream.com/v1";
+const PIPEDREAM_SQL_BASE_URL = "https://rt.pipedream.com/sql";
+
+// Pipedream app
+const pipedream = {
+  type: "app",
+  app: "pipedream",
+  methods: {
+    async runSQLQuery(query) {
+      try {
+        const { data } = await axios({
+          url: PIPEDREAM_SQL_BASE_URL,
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${this.$auth.api_key}`,
+          },
+          data: { query },
+        });
+        // TODO: massage query results to get header / results cleanly
+        // TODO: return a link to the CSV in the emit body
+        return data;
+      } catch (err) {
+        console.log(`Error in SQL query: ${err}`);
+        throw err;
+      }
+    },
+  },
+};
---
+++ components/pipedream/sql.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "pipedream-sql",
+  version: "0.0.1",
+  props: {
+    sqlQuery: {
+      type: "string",
+      label: "SQL Query",
+      description:
+        "The Zoom event you'd like to subscribe to (leave blank to subscribe to all events)",
+    },
+    emitEachRecordAsEvent: {
+      type: "boolean",
+      label: "Emit each record as its own event",
+      description:
+        "If `true`, each record in your results set is emitted as its own event. Defaults to emitting results as a single event (an array of records)",
+      optional: true,
+      default: false,
+    },
+    pipedream,
+  },
+  async run() {
+    const results = await this.pipedream.runSQLQuery(this.sqlQuery);
+    console.log(results);
+    // TODO: Check to confirm results is an array.
+    // TODO: handle emitEachRecordAsEvent
+  },
+};
