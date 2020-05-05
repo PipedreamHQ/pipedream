d2a0a37eb67cb2e9f3b6c89ad9b0abe38b672bc7
Dylan J. Sather <dylan.sather@gmail.com>
2020-05-04 22:09:14 -0700

    Correcting pipedream app require

--- components/pipedream/sql/sql.js
+++ components/pipedream/sql/sql.js
@@ -1,4 +1,4 @@
-const pipedream = require("https://github.com/PipedreamHQ/pipedream/blob/sql/components/pipedream/pipedream.app.js");
+const pipedream = require("https://github.com/PipedreamHQ/pipedream/components/pipedream/pipedream.app.js");
 
 module.exports = {
   name: "pipedream-sql",
