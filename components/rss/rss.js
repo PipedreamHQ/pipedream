cfa74f531cdba410f3fd59c90be740af6c40733e
psavkar <psavkar@yahoo.com>
2020-05-06 15:46:39 -0700

    added hashing to itemKey() and migrated to unique dedupe strategy

--- components/rss/rss.js
+++ components/rss/rss.js
@@ -5,8 +5,6 @@ module.exports = {
   name: "rss",
   version: "0.0.1",
   props: {
-    // XXX have auto: true prop in case there is something to configure (unless pass --no-auto)
-    db: "$.service.db",
     url:{
       type: "string",
       label: 'Feed URL',
@@ -22,21 +20,16 @@ module.exports = {
   methods: {
     // in theory if alternate setting title and description or aren't unique this won't work
     itemKey(item) {
-      return item.guid || item.id || item.title || item.description
+      return item.guid || item.id || require('crypto').createHash('sha1').update(item).digest('base64')
     },
   },
+  dedupe: "unique",
   async run() {
-    const seenKeys = this.db.get("seenKeys") || []
-    const seenKeysMap = seenKeys.reduce((acc, v) => {
-      acc[v] = true
-      return acc
-    }, {})
-
-    const res = await fetch('https://flipboard.com/@DailyMailUS.rss', {
+    const res = await fetch(this.url, {
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
       accept: 'text/html,application/xhtml+xml',
     })
-    if (res.status != 200) throw new Error('Bad status code')
+    if (res.status !== 200) throw new Error('Bad status code')
     const feedparser = new FeedParser({
       addmeta: false,
     })
@@ -64,22 +57,12 @@ module.exports = {
       res.body.pipe(feedparser)
     })
 
-    for (let idx = items.length - 1; idx >= 0; idx--) {
-      const item = items[idx]
-      const key = this.itemKey(item)
-      // XXX throw if !key?
-      if (seenKeysMap[key]) continue
-      seenKeys.unshift(key)
-      seenKeysMap[key] = true // just in case of dupes
+    items.forEach(item=>{
       this.$emit(item, {
+        id: this.itemKey(item),
         summary: item.title,
-        ts: item.pubdate && +new Date(item.pubdate),
-        id: key,
+        ts: item.pubdate && +new Date(item.pubdate), 
       })
-    }
-    if (seenKeys.length) {
-      // XXX restrict by byte size instead of el size
-      this.db.set("seenKeys", seenKeys.slice(0, 1000))
-    }
+    })
   },
 }
