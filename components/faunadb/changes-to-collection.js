94d85391be03a1507771519a7e4a2275a1699f1e
Dylan J. Sather <dylan.sather@gmail.com>
2020-05-05 17:35:54 -0700

    First stab at Fauna

---
+++ components/faunadb/changes-to-collection.js
@@ -0,0 +1,54 @@
+const faunadb = require("https://github.com/PipedreamHQ/pipedream/blog/fauna/components/faunadb/fauna.app.js");
+
+module.exports = {
+  name: "changes-to-collection",
+  version: "0.0.1",
+  db: "$.service.db",
+  dedupe: "unique", // Dedupe events based on the concatenation of event + document ref id
+  props: {
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        cron: "*/15 * * * *",
+      },
+    },
+    collection: { propDefinition: [twitter, "collection"] },
+    emitEventsInBatch: {
+      type: "boolean",
+      label: "Emit changes as a single event",
+      description:
+        "If `true`, all events are emitted as an array, within a single Pipedream event. Defaults to `false`, emitting each event in Fauna as its own event in Pipedream",
+      optional: true,
+      default: false,
+    },
+    faunadb,
+  },
+  async run() {
+    // As soon as the script runs, mark the start time so we can fetch changes
+    // since this time on the next run. Fauna expects epoch ms as its cursor.
+    const ts = +new Date() * 1000;
+
+    const cursor = this.db.get("cursor") || ts;
+
+    const events = this.faunadb.getEventsInCollectionAfterTs(
+      this.collection,
+      cursor
+    );
+
+    // Batched emits do not take advantage of the built-in deduper
+    if (this.emitEventsInBatch) {
+      this.$emit(events);
+      return;
+    }
+
+    for (const event of events) {
+      this.$emit(event, {
+        summary: `${event.action.toUpperCase()} - ${event.document["@ref"].id}`,
+        id: `${event.action}-${event.document["@ref"].id}`, // dedupes events based on this ID
+      });
+    }
+
+    // Finally, set cursor for the next run
+    this.db.set("cursor", ts);
+  },
+};
---
+++ components/faunadb/fauna.app.js
@@ -0,0 +1,53 @@
+// FaunaDB app file
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+module.exports = {
+  type: "app",
+  app: "faunadb",
+  propDefinitions: {
+    collection: {
+      type: "string",
+      label: "Collection",
+      description: "The collection you'd like to watch for changes",
+      optional: false,
+      async options() {
+        const collections = [];
+        const collectionsPaginator = await client.paginate(q.Collections());
+
+        await collectionsPaginator.each((page) => {
+          for (const collection of page) {
+            collections.push(collection);
+          }
+        });
+
+        return collections;
+      },
+    },
+  },
+  methods: {
+    // Fetches events (changes) for all documents in a collection
+    // made after a specific epoch timestamp
+    async getEventsInCollectionAfterTs(collection, ts) {
+      const client = new faunadb.Client({ secret: this.$auth.secret });
+
+      // Fetch pages of all changes (events) for a particular collection
+      // since the given ts cursor. See docs on Events / Pagination
+      // https://docs.fauna.com/fauna/current/api/fql/functions/events
+      // https://docs.fauna.com/fauna/current/api/fql/functions/paginate
+      const paginationHelper = await client.paginate(
+        q.Documents(q.Collection(collection)),
+        { after: ts, events: true }
+      );
+
+      const events = [];
+      await paginationHelper.each((page) => {
+        for (const event of page) {
+          this.events.push(event);
+        }
+      });
+
+      return events;
+    },
+  },
+};
