1ffc36e9645b8cceb1b32785cb8cdfe91ba05d17
Dylan J. Sather <dylan.sather@gmail.com>
2020-05-15 10:04:47 -0700

    Splitting out app file

--- components/aws/ses/ses_inbound_email_catchall.js
+++ components/aws/ses/ses_inbound_email_catchall.js
@@ -1,22 +1,4 @@
-const aws = {
-  type: "app",
-  app: "aws",
-  methods: {
-    sdk(region) {
-      process.env.AWS_ACCESS_KEY_ID = this.$auth.accessKeyId;
-      process.env.AWS_SECRET_ACCESS_KEY = this.$auth.secretAccessKey;
-      const AWS = require("aws-sdk");
-      AWS.config.update({ region });
-      return AWS;
-    },
-    async sesIdentities(region) {
-      const AWS = this.sdk(region);
-      const ses = new AWS.SES();
-      return (await ses.listIdentities().promise()).Identities;
-    },
-  },
-};
-
+const aws = require("https://github.com/PipedreamHQ/blob/aws-dylan-edits/pipedream/components/aws/aws.app.js");
 const axios = require("axios");
 
 module.exports = {
