c2f0f57ea1b263c0540772fa437971d281fac3f7
Tod Sacerdoti <todsac@gmail.com>
2020-06-26 14:23:46 -0700

    Update new-stories.js

--- components/dev/new-stories.js
+++ components/dev/new-stories.js
@@ -8,10 +8,10 @@ const FeedParser = require('feedparser')
 const hash = require('object-hash')
 
 module.exports = {
-  dev_to,
   name: "dev-to-new-stories",
   version: "0.0.1",
   props: {
+    dev_to,
     timer: {
       type: "$.interface.timer",
       default: {
