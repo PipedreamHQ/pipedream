45ca5471f114ea2651c2d8b7d4d231143a84bde0
Tod Sacerdoti <todsac@gmail.com>
2020-06-29 16:44:19 -0700

    Update new-stories.js

--- components/dev/new-stories.js
+++ components/dev/new-stories.js
@@ -3,68 +3,39 @@ const dev_to = {
   app: "dev_to",
 }
 
-const fetch = require('node-fetch')
-const FeedParser = require('feedparser')
-const hash = require('object-hash')
+const moment = require('moment')
 
+const axios = require('axios')
 module.exports = {
-  name: "dev-to-new-stories",
+  name: "fresh_stories",
   version: "0.0.1",
   props: {
-    dev_to,
+    db: "$.service.db",
     timer: {
       type: "$.interface.timer",
       default: {
         intervalSeconds: 60 * 15,
       },
     },
+    dev_to,
   },
-  methods: {
-    // in theory if alternate setting title and description or aren't unique this won't work
-    itemKey(item) {
-      return item.guid || item.id || hash(item)
-    },
-  },
-  dedupe: "unique",
-  async run() {
-    const res = await fetch("https://dev.to/feed/", {
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
-      accept: 'text/html,application/xhtml+xml',
-    })
-    if (res.status !== 200) throw new Error('Bad status code')
-    const feedparser = new FeedParser({
-      addmeta: false,
-    })
-    const items = []
-    await new Promise((resolve, reject) => {
-      feedparser.on('error', reject)
-      feedparser.on('end', resolve)
-      feedparser.on('readable', function() {
-        let item
-        while (item = this.read()) {
-          for (const k in item) {
-            if (item[`rss:${k}`]) {
-              delete item[`rss:${k}`]
-              continue
-            }
-            const o = item[k]
-            if (o == null || (typeof o === 'object' && !Object.keys(o).length) || Array.isArray(o) && !o.length) {
-              delete item[k]
-              continue
-            }
-          }
-          items.push(item)
-        }
-      })
-      res.body.pipe(feedparser)
-    })
+  dedupe: "greatest",
+  async run(event) {
+    const url = `https://dev.to/api/articles?state=fresh&per_page=1000`
+    const data = (await axios({
+      method: "get",
+      url,
+      headers: {
+        "api-key": `${this.dev_to.$auth.api_key}`,
+      },
+    })).data
 
-    items.forEach(item=>{
-      this.$emit(item, {
-        id: this.itemKey(item),
-        summary: item.title,
-        ts: item.pubdate && +new Date(item.pubdate), 
-      })
-    })
+   data.forEach(event=>{
+     this.$emit(event,{
+       id: event.id,
+       ts: moment(event.created_at).valueOf(),     
+       summary: event.title,
+     })
+   })
   },
 }
