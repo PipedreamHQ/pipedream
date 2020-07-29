399c230f490230a4ab4e1d347464dd9f69941e81
Dan Hsiung <danhsiung@gmail.com>
2020-07-29 12:14:57 -0700

    change to cache each entity id separately and refactor to dry it up

--- components/slack/new-message-in-conversation.js
+++ components/slack/new-message-in-conversation.js
@@ -1,52 +1,5 @@
 const slack = require('https://github.com/PipedreamHQ/pipedream/components/slack/slack.app.js')
 
-async function getUserName(nameCache, sdk, id) {
-  const { users } = nameCache
-  if (users[id] == null) {
-    const info = await sdk.users.info({ user: id })
-    if (info.ok) {
-      users[id] = info.user.name
-    } else {
-      throw info.error
-    }
-  }
-  return users[id]
-}
-
-async function getConversationName(nameCache, sdk, id, username) {
-  const { users, conversations } = nameCache
-  if (conversations[id] == null) {
-    const info = await sdk.conversations.info({ channel: id })
-    if (info.ok) {
-      if (info.channel.is_im) {
-        conversations[id] = `DM with ${await getUserName(nameCache, sdk, info.channel.user)}`
-      } else {
-        conversations[id] = info.channel.name
-      }
-    } else {
-      throw info.error
-    }
-  }
-  return conversations[id]
-}
-
-async function getTeamName(nameCache, sdk, id) {
-  const { teams } = nameCache
-  if (teams[id] == null) {
-    try {
-      const info = await sdk.team.info({ team: id })
-      if (info.ok) {
-        teams[id] = info.team.name
-      } else {
-        throw info.error
-      }
-    } catch(err) {
-      console.log("Error getting team name, probably need to re-connect the account at pipedream.com/apps", err)
-      teams[id] = id
-    }
-  }
-  return teams[id]
-}
 
 module.exports = {
   name: "Slack - New Message In Conversation(s)",
@@ -114,6 +67,49 @@ module.exports = {
     },
     nameCache: "$.service.db",
   },
+  methods: {
+    async getName(type, key, id, format, timeout = 360000) {
+      const cacheKey = `${type}:${id}`
+      let record = this.nameCache.get(cacheKey)
+      const time = Date.now()
+      if (record == null || time - record.ts > timeout) {
+        try {
+          const info = await this.slack.sdk()[type].info({ [key]: id })
+          if (!info.ok) {
+            throw info.error
+          }
+          record = { ts: time, val: await format(info) }
+        } catch (err) {
+          if (type == "team") {
+            record = { ts: time, val: id }
+          } else {
+            throw err
+          }
+        }
+        this.nameCache.set(cacheKey, record)
+      }
+      return record.val
+    },
+    async getUserName(id) {
+      return this.getName('users', 'user', id, async (info) => {
+        return info.user.name
+      })
+    },
+    async getConversationName(id) {
+      return this.getName('conversations', 'channel', id, async (info) => {
+        if (info.channel.is_im) {
+          return `DM with ${await this.getUserName(info.channel.user)}`
+        } else {
+          return info.channel.name
+        }
+      })
+    },
+    async getTeamName(id) {
+      return this.getName('team', 'team', id, async (info) => {
+        return info.team.name
+      })
+    },
+  },
   async run(event) {
     if (event.subtype != null) {
       // This source is designed to just emit an event for each new message received.
@@ -127,22 +123,12 @@ module.exports = {
       return
     }
     if (this.resolveNames) {
-      // invalidate old cache entries after an hour
-      const cacheTime = this.nameCache.get("cache_time")
-      if (cacheTime == null || Date.now() - cacheTime > 3600000) {
-        this.nameCache.set("slack_entity_names", { users: {}, conversations: {}, teams: {} })
-        this.nameCache.set("cache_time", Date.now())
-      }
-      const nameCache = this.nameCache.get("slack_entity_names")
-
       event.user_id = event.user
-      event.user = await getUserName(nameCache, this.slack.sdk(), event.user)
+      event.user = await this.getUserName(event.user)
       event.channel_id = event.channel
-      event.channel = await getConversationName(nameCache, this.slack.sdk(), event.channel)
+      event.channel = await this.getConversationName(event.channel)
       event.team_id = event.team
-      event.team = await getTeamName(nameCache, this.slack.sdk(), event.team)
-
-      this.nameCache.set("slack_entity_names", nameCache)
+      event.team = await this.getTeamName(event.team)
     }
 
     this.$emit(event, { id: event.client_msg_id })
