74d88a84e92f15bd2a22a63cd5177e8c05c05749
jason <jason@pipedream.com>
2020-07-31 13:39:43 -0700

    simple component with local require

---
+++ local/http/http.js
@@ -0,0 +1,22 @@
+const hello = require("./hello")
+// Core HTTP component
+console.log(hello)
+module.exports = {
+  name: "http",
+  version: "0.0.1",
+  props: {
+    http: "$.interface.http",
+  },
+  async run(event) {
+    const summary = `${event.method} ${event.path}`
+    const h = {...hello, a: 12}
+    this.http.respond({
+      status: 200,
+      body: h,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    this.$emit(event)
+  }
+}
