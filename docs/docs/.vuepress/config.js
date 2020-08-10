2000214fa30662c09b6d246a2152b29cc3cbf821
Dylan J. Sather <dylan.sather@gmail.com>
2020-08-09 23:12:42 -0700

    Ejected theme for basic modifications

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  title: "Docs",
+  title: "",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   description: "Pipedream Documentation - Integrate your apps, data and APIs",
   base: "/",
@@ -11,22 +11,10 @@ module.exports = {
       apiKey: "1e23962724b59d018bdedc0f5a214ce5",
       indexName: "pipedream",
     },
+    searchPlaceholder: "Search...",
     logo: "/pipedream.svg",
-    nav: [
-      {
-        text: "Slack",
-        link: "https://pipedream.com/community",
-      },
-      {
-        text: "Support",
-        link: "/support/",
-      },
-      {
-        text: "pipedream.com",
-        link: "https://pipedream.com/",
-      },
-    ],
-    repo: "PipedreamHQ/docs",
+    repo: "PipedreamHQ/pipedream",
+    docsDir: "docs",
 
     // Optional options for generating "Edit this page" link
 
