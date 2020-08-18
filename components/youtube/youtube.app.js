9de49a0b822cab7f1060a09b91f88543e334aa5c
michelle0927 <michelle0927@users.noreply.github.com>
2020-08-18 13:22:45 -0700

    saved with JS Prettier

--- components/youtube/youtube.app.js
+++ components/youtube/youtube.app.js
@@ -1,19 +1,22 @@
 module.exports = {
-  type: "app",
-  app: "youtube_data_api",
-  methods: {
-  	async getVideos(pageToken=null) {
-      return await axios.get('https://www.googleapis.com/youtube/v3/search', {
-        headers: {
-          'Authorization' : `Bearer ${this.$auth.oauth_access_token}`
-        },
-        params: {
-          part : 'snippet',
-          type : 'video',
-          forMine : true,
-          pageToken
-        }
-      });
-    },
-  }
-}
+	type: "app",
+	app: "youtube_data_api",
+	methods: {
+		async getVideos(pageToken = null) {
+			return await axios.get(
+				"https://www.googleapis.com/youtube/v3/search",
+				{
+					headers: {
+						Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+					},
+					params: {
+						part: "snippet",
+						type: "video",
+						forMine: true,
+						pageToken,
+					},
+				}
+			);
+		},
+	},
+};
