23bd950c0fcb8be1f3a61522d1c8c13d7c61b784
michelle0927 <michelle0927@users.noreply.github.com>
2020-08-19 11:34:32 -0700

    updated for use with other components

--- components/youtube/youtube.app.js
+++ components/youtube/youtube.app.js
@@ -2,19 +2,14 @@ module.exports = {
 	type: "app",
 	app: "youtube_data_api",
 	methods: {
-		async getVideos(pageToken = null) {
+		async getVideos(params) {
 			return await axios.get(
 				"https://www.googleapis.com/youtube/v3/search",
 				{
 					headers: {
 						Authorization: `Bearer ${this.$auth.oauth_access_token}`,
 					},
-					params: {
-						part: "snippet",
-						type: "video",
-						forMine: true,
-						pageToken,
-					},
+					params,
 				}
 			);
 		},
