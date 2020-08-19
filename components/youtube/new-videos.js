abd23512d9b6286b7c62a957c39a1df5957d4f68
michelle0927 <michelle0927@users.noreply.github.com>
2020-08-19 11:35:26 -0700

    Update new-videos.js

--- components/youtube/new-videos.js
+++ components/youtube/new-videos.js
@@ -2,44 +2,51 @@ const axios = require("axios");
 const youtube = require("https://github.com/PipedreamHQ/pipedream/components/youtube/youtube.app.js");
 
 module.exports = {
-  name: "Youtube - New Videos",
-  version: "0.0.1",
-  dedupe: "unique",
-  props: {
-    youtube,
-    db: "$.service.db",
-    timer: {
-      type: "$.interface.timer",
-      default: {
-        intervalSeconds: 60 * 15,
+   name: "Youtube - New Videos",
+   version: "0.0.1",
+   dedupe: "unique",
+   props: {
+      youtube,
+      db: "$.service.db",
+      timer: {
+         type: "$.interface.timer",
+         default: {
+            intervalSeconds: 60 * 15,
+         },
       },
-    },
-  },
-  methods: {},
+   },
+   methods: {},
 
-  async run(event) {
-    let videos = [];
-    let totalResults = 1;
-    let nextPageToken = null;
-    let count = 0;
-    let results;
+   async run(event) {
+      let videos = [];
+      let totalResults = 1;
+      let nextPageToken = null;
+      let count = 0;
+      let results;
+      let params = {
+         part: "snippet",
+         type: "video",
+         forMine: true,
+         pageToken: null,
+      };
 
-    while (count < totalResults) {
-      results = await this.youtube.getVideos(nextPageToken);
-      totalResults = results.data.pageInfo.totalResults;
-      nextPageToken = results.data.nextPageToken;
-      results.data.items.forEach(function (video) {
-        videos.push(video);
-        count++;
-      });
-    }
+      while (count < totalResults) {
+         params.pageToken = nextPageToken;
+         results = await this.youtube.getVideos(params);
+         totalResults = results.data.pageInfo.totalResults;
+         nextPageToken = results.data.nextPageToken;
+         results.data.items.forEach(function (video) {
+            videos.push(video);
+            count++;
+         });
+      }
 
-    for (const video of videos) {
-      this.$emit(video, {
-        id: video.id.videoId,
-        summary: video.snippet.title,
-        ts: Date.now(),
-      });
-    }
-  },
+      for (const video of videos) {
+         this.$emit(video, {
+            id: video.id.videoId,
+            summary: video.snippet.title,
+            ts: Date.now(),
+         });
+      }
+   },
 };
