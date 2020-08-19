63501df5a55725250d22004c15c1f4c1fac12964
michelle0927 <michelle0927@users.noreply.github.com>
2020-08-19 11:36:43 -0700

    Add files via upload

---
+++ components/youtube/new-videos-in-channel.js
@@ -0,0 +1,58 @@
+const axios = require("axios");
+const youtube = require("https://github.com/PipedreamHQ/pipedream/components/youtube/youtube.app.js");
+
+module.exports = {
+  name: "Youtube - New Videos in Channel",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    youtube,
+    channelId: {
+      type: "string",
+      label: "Channel ID",
+      description: "The ID of the channel to search for new videos in.",
+      default: "",
+    },
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15,
+      },
+    },
+  },
+  methods: {},
+
+  async run(event) {
+    let videos = [];
+    let totalResults = 1;
+    let nextPageToken = null;
+    let count = 0;
+    let results;
+    let params = {
+      part: "snippet",
+      type: "video",
+      channelId: this.channelId,
+      pageToken: null,
+    };
+
+    while (count < totalResults) {
+      params.pageToken = nextPageToken;
+      results = await this.youtube.getVideos(params);
+      totalResults = results.data.pageInfo.totalResults;
+      nextPageToken = results.data.nextPageToken;
+      results.data.items.forEach(function (video) {
+        videos.push(video);
+        count++;
+      });
+    }
+
+    for (const video of videos) {
+      this.$emit(video, {
+        id: video.id.videoId,
+        summary: video.snippet.title,
+        ts: Date.now(),
+      });
+    }
+  },
+};
---
+++ components/youtube/new-videos-in-playlist.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const youtube = require("https://github.com/PipedreamHQ/pipedream/components/youtube/youtube.app.js");
+
+module.exports = {
+  name: "Youtube - New Videos in Playlist",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    youtube,
+    playlistId: {
+      type: "string",
+      label: "Playlist ID",
+      description: "The ID of the playlist to search for new videos in.",
+      default: "",
+    },
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15,
+      },
+    },
+  },
+  methods: {},
+
+  async run(event) {
+    let videos = [];
+    let totalResults = 1;
+    let nextPageToken = null;
+    let count = 0;
+    let results;
+    let params = {
+      part: "contentDetails,id,snippet,status",
+      playlistId: this.playlistId,
+      pageToken: null,
+    };
+
+    while (count < totalResults) {
+      params.pageToken = nextPageToken;
+      results = await this.youtube.getVideos(params);
+      totalResults = results.data.pageInfo.totalResults;
+      nextPageToken = results.data.nextPageToken;
+      results.data.items.forEach(function (video) {
+        videos.push(video);
+        count++;
+      });
+    }
+
+    for (const video of videos) {
+      this.$emit(video, {
+        id: video.id.videoId,
+        summary: video.snippet.title,
+        ts: Date.now(),
+      });
+    }
+  },
+};
