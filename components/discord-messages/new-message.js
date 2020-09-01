e212d4400549006526a74cab7a5c7b7b604689e9
Dan Hsiung <danhsiung@gmail.com>
2020-09-01 01:06:51 -0700

    register apphook for instant discord-messages

--- components/discord-messages/new-message.js
+++ components/discord-messages/new-message.js
@@ -2,9 +2,16 @@ const discord = require("https://github.com/PipedreamHQ/pipedream/components/dis
 
 module.exports = {
   name: 'instant discord message',
-  version: '0.0.1',
+  version: '0.0.2',
   props: {
     discord,
+    discordApphook: {
+      type: "$.interface.apphook",
+      appProp: "discord",
+      async eventNames() {
+        return []
+      },
+    },
   },
   async run(event) {
     this.$emit(event)
