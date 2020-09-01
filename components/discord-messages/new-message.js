63689739a40ac7c30fcca1c4ce887efbd8829d9d
Dan Hsiung <danhsiung@gmail.com>
2020-08-31 22:10:47 -0700

    give the source a name

--- components/discord-messages/new-message.js
+++ components/discord-messages/new-message.js
@@ -1,8 +1,8 @@
 const discord = require("https://github.com/PipedreamHQ/pipedream/components/discord-messages/discord-messages.app.js")
 
 module.exports = {
-  name: '',
-  version: '',
+  name: 'instant discord messages',
+  version: '0.0.1',
   props: {
     discord,
   },
