03fd7aae9688a87614340f538024828681049060
Jay Vercellone <juanjov@gmail.com>
2020-09-09 19:39:28 -0700

    Gitlab app common methods
    * Add utility methods to the Gitlab app file to create and delete
      webhooks in projects/repositories.
    * Add other utility methods to make code clearer.

--- components/gitlab/gitlab.app.js
+++ components/gitlab/gitlab.app.js
@@ -1,5 +1,63 @@
+const axios = require("axios");
+const uuid = require("uuid");
+
 module.exports = {
   type: "app",
   app: "gitlab",
-  methods: {},
+  propDefinitions: {
+    repoId: {
+      type: "string",
+      label: "Repo ID",
+    },
+  },
+  methods: {
+    apiUrl() {
+      return "https://gitlab.com/api/v4";
+    },
+    gitlabAuthToken() {
+      return this.$auth.oauth_access_token
+    },
+    generateSecret: uuid.v4,
+    async createHook(opts) {
+      const { repoId, hookParams } = opts;
+      const baseUrl = this.apiUrl();
+      const url = `${baseUrl}/projects/${repoId}/hooks`;
+
+      const token = this.generateSecret();
+      const data = {
+        ...hookParams,
+        token,
+      };
+
+      const gitlabAuthToken = this.gitlabAuthToken();
+      const headers = {
+        "Authorization": `Bearer ${gitlabAuthToken}`,
+      };
+
+      const requestParams = {
+        headers,
+      };
+      const response = await axios.post(url, data, requestParams);
+      const hookId = response.data.id;
+      return {
+        hookId,
+        token,
+      };
+    },
+    deleteHook(opts) {
+      const { hookId, repoId } = opts;
+      const baseUrl = this.apiUrl();
+      const url = `${baseUrl}/projects/${repoId}/hooks/${hookId}`;
+
+      const gitlabAuthToken = this.gitlabAuthToken();
+      const headers = {
+        "Authorization": `Bearer ${gitlabAuthToken}`,
+      };
+
+      const requestParams = {
+        headers,
+      };
+      return axios.delete(url, requestParams);
+    },
+  },
 };
