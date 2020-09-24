3a21b1ef75e6a3cfe3d1f66427f6703b81c779d2
michelle0927 <michelle0927@users.noreply.github.com>
2020-09-24 15:28:02 -0700

    Update event-type.js

--- components/calendly/event-type.js
+++ components/calendly/event-type.js
@@ -1,40 +1,40 @@
 const calendly = require("https://github.com/PipedreamHQ/pipedream/components/calendly/calendly.app.js");
 
 module.exports = {
-	name: "New Event Type",
-	description: "Emits an event for each new event type",
-	version: "0.0.1",
-	dedupe: "unique",
-	props: {
-		calendly,
-		db: "$.service.db",
-		timer: {
-			type: "$.interface.timer",
-			default: {
-				intervalSeconds: 60 * 15,
-			},
-		},
-	},
+  name: "New Event Type",
+  description: "Emits an event for each new event type",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    calendly,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15,
+      },
+    },
+  },
 
-	async run(event) {
-		const now = new Date();
-		const monthAgo = new Date(now.getTime());
-		monthAgo.setMonth(monthAgo.getMonth() - 1);
-		let lastEvent = this.db.get("lastEvent") || monthAgo;
-		lastEvent = new Date(lastEvent);
+  async run(event) {
+    const now = new Date();
+    const monthAgo = new Date(now.getTime());
+    monthAgo.setMonth(monthAgo.getMonth() - 1);
+    let lastEvent = this.db.get("lastEvent") || monthAgo;
+    lastEvent = new Date(lastEvent);
 
-		const eventTypes = await this.calendly.getEventTypes();
-		for (const eventType of eventTypes) {
-			let created_at = new Date(eventType.attributes.created_at);
-			if (created_at.getTime() > lastEvent) {
-				this.$emit(eventType, {
-					id: eventType.id,
-					summary: eventType.attributes.name,
-					ts: Date.now(),
-				});
-			}
-		}
+    const eventTypes = await this.calendly.getEventTypes();
+    for (const eventType of eventTypes) {
+      let created_at = new Date(eventType.attributes.created_at);
+      if (created_at.getTime() > lastEvent) {
+        this.$emit(eventType, {
+          id: eventType.id,
+          summary: eventType.attributes.name,
+          ts: Date.now(),
+        });
+      }
+    }
 
-		this.db.set("lastEvent", Date.now());
-	},
+    this.db.set("lastEvent", Date.now());
+  },
 };
