39d2e8efb50e0c869063ead332a0bc8e46ebbb74
Jay Vercellone <juanjov@gmail.com>
2020-09-28 22:14:16 -0700

    Add event source logic for New Commit

--- components/bitbucket/bitbucket.app.js
+++ components/bitbucket/bitbucket.app.js
@@ -62,6 +62,35 @@ module.exports = {
         };
       },
     },
+    branchName: {
+      type: "string",
+      label: "Branch Name",
+      description: "The name of the branch",
+      async options(context) {
+        const { workspaceId, repositoryId } = context;
+        const url = this._repositoryBranchesEndpoint(workspaceId, repositoryId);
+        const params = {
+          sort: "name",
+          fields: [
+            "next",
+            "values.name",
+          ],
+        };
+
+        const data = await this._propDefinitionsOptions(url, params, context);
+        const options = data.values.map(branch => ({
+          label: branch.name,
+          value: branch.name,
+        }));
+        return {
+          options,
+          context: {
+            // https://developer.atlassian.com/bitbucket/api/2/reference/meta/pagination
+            nextPageUrl: data.next,
+          },
+        };
+      },
+    },
   },
   methods: {
     _apiUrl() {
@@ -74,6 +103,14 @@ module.exports = {
     _workspaceRepositoriesEndpoint(workspaceId) {
       const baseUrl = this._apiUrl();
       return `${baseUrl}/repositories/${workspaceId}`;
+    },
+    _repositoryBranchesEndpoint(workspaceId, repositoryId) {
+      const baseUrl = this._apiUrl();
+      return `${baseUrl}/repositories/${workspaceId}/${repositoryId}/refs`;
+    },
+    _branchCommitsEndpoint(workspaceId, repositoryId, branchName) {
+      const baseUrl = this._apiUrl();
+      return `${baseUrl}/repositories/${workspaceId}/${repositoryId}/commits/${branchName}`;
     },
     _hooksEndpointUrl(workspaceId, repositoryId) {
       const baseUrl = this._workspaceRepositoriesEndpoint(workspaceId);
@@ -104,6 +141,31 @@ module.exports = {
 
       const { data } = await axios.get(url, requestConfig);
       return data;
+    },
+    async *getCommits(opts) {
+      const {
+        workspaceId,
+        repositoryId,
+        branchName,
+        lastProcessedCommitHash,
+      } = opts;
+      const requestConfig = this._makeRequestConfig();
+
+      let url = this._branchCommitsEndpoint(workspaceId, repositoryId, branchName);
+      do {
+        const { data } = await axios.get(url, requestConfig);
+        const { values, next } = data;
+
+        // Yield the retrieved commits in a serial manner, until
+        // we exhaust the response from the BitBucket API, or we reach
+        // the last processed commit, whichever comes first.
+        for (const commit of values) {
+          if (commit.hash === lastProcessedCommitHash) return;
+          yield commit;
+        }
+
+        url = next;
+      } while (url);
     },
     _authToken() {
       return this.$auth.oauth_access_token
