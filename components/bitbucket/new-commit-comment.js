1ea575d64e8d22ae0a07092dcc7c7afb8636ed66
Jay Vercellone <juanjov@gmail.com>
2020-09-28 19:10:52 -0700

    Add event source logic for New Commit Comment

--- components/bitbucket/new-commit-comment.js
+++ components/bitbucket/new-commit-comment.js
@@ -1,6 +1,6 @@
+// const bitbucket = require("https://github.com/PipedreamHQ/pipedream/components/bitbucket/bitbucket.app.js");
 const axios = require("axios");
-
-module.exports = {
+const bitbucket = {
   type: "app",
   app: "bitbucket",
   propDefinitions: {
@@ -146,5 +146,93 @@ module.exports = {
       const expectedHookId = db.get("hookId");
       return hookId === expectedHookId;
     },
+  },
+};
+
+module.exports = {
+  name: "New Commit Comment (Instant)",
+  description: "Emits an event when a commit receives a comment",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    bitbucket,
+    db: "$.service.db",
+    http: "$.interface.http",
+    workspaceId: { propDefinition: [bitbucket, "workspaceId"] },
+    repositoryId: {
+      propDefinition: [
+        bitbucket,
+        "repositoryId",
+        c => ({ workspaceId: c.workspaceId }),
+      ],
+    },
+  },
+  hooks: {
+    async activate() {
+      const hookParams = {
+        description: "Pipedream - New Commit Comment",
+        url: this.http.endpoint,
+        active: true,
+        events: [
+          "repo:commit_comment_created"
+        ],
+      };
+      const opts = {
+        workspaceId: this.workspaceId,
+        repositoryId: this.repositoryId,
+        hookParams,
+      };
+      const { hookId } = await this.bitbucket.createHook(opts);
+      console.log(
+        `Created "repository commit comment created" webhook for repository "${this.workspaceId}/${this.repositoryId}".
+        (Hook ID: ${hookId}, endpoint: ${hookParams.url})`
+      );
+      this.db.set("hookId", hookId);
+    },
+    async deactivate() {
+      const hookId = this.db.get("hookId");
+      const opts = {
+        workspaceId: this.workspaceId,
+        repositoryId: this.repositoryId,
+        hookId,
+      };
+      await this.bitbucket.deleteHook(opts);
+      console.log(
+        `Deleted webhook for repository "${this.workspaceId}/${this.repositoryId}".
+        (Hook ID: ${hookId})`
+      );
+    },
+  },
+  methods: {
+    generateMeta(data) {
+      const { headers, body } = data;
+      const { comment, commit } = body;
+      const summary = `New comment on commit ${commit.hash}`;
+      const ts = +new Date(headers["x-event-time"]);
+      return {
+        id: comment.id,
+        summary,
+        ts,
+      };
+    },
+  },
+  async run(event) {
+    const { headers, body } = event;
+
+    // Reject any calls not made by the proper BitBucket webhook.
+    if (!this.bitbucket.isValidSource(headers, this.db)) {
+      this.http.respond({
+        status: 404,
+      });
+      return;
+    }
+
+    // Acknowledge the event back to BitBucket.
+    this.http.respond({
+      status: 200,
+    });
+
+    const meta = this.generateMeta(event);
+    this.$emit(body, meta);
   },
 };
