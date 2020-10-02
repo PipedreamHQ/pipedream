1283c74cdb1af6bf56472b5a73d91c2f631a52d5
Jay Vercellone <jverce@users.noreply.github.com>
2020-10-01 20:30:19 -0700

    Bitbucket new commit
    * Add event source logic for New Commit
    * Add pagination field in API response

--- components/bitbucket/bitbucket.app.js
+++ components/bitbucket/bitbucket.app.js
@@ -13,6 +13,7 @@ module.exports = {
         const params = {
           sort: "name",
           fields: [
+            "next",
             "values.name",
             "values.slug",
           ],
@@ -42,6 +43,7 @@ module.exports = {
         const params = {
           sort: "slug",
           fields: [
+            "next",
             "values.slug",
           ],
         };
@@ -50,6 +52,35 @@ module.exports = {
         const options = data.values.map(repository => ({
           label: repository.slug,
           value: repository.slug,
+        }));
+        return {
+          options,
+          context: {
+            // https://developer.atlassian.com/bitbucket/api/2/reference/meta/pagination
+            nextPageUrl: data.next,
+          },
+        };
+      },
+    },
+    branchName: {
+      type: "string",
+      label: "Branch Name",
+      description: "The name of the branch",
+      async options(context) {
+        const { workspaceId, repositoryId } = context;
+        const url = this._repositoryBranchesEndpoint(workspaceId, repositoryId);
+        const params = {
+          sort: "name",
+          fields: [
+            "next",
+            "values.name",
+          ],
+        };
+
+        const data = await this._propDefinitionsOptions(url, params, context);
+        const options = data.values.map(branch => ({
+          label: branch.name,
+          value: branch.name,
         }));
         return {
           options,
@@ -72,6 +103,14 @@ module.exports = {
     _workspaceRepositoriesEndpoint(workspaceId) {
       const baseUrl = this._apiUrl();
       return `${baseUrl}/repositories/${workspaceId}`;
+    },
+    _repositoryBranchesEndpoint(workspaceId, repositoryId) {
+      const baseUrl = this._apiUrl();
+      return `${baseUrl}/repositories/${workspaceId}/${repositoryId}/refs`;
+    },
+    _branchCommitsEndpoint(workspaceId, repositoryId, branchName) {
+      const baseUrl = this._apiUrl();
+      return `${baseUrl}/repositories/${workspaceId}/${repositoryId}/commits/${branchName}`;
     },
     _hooksEndpointUrl(workspaceId, repositoryId) {
       const baseUrl = this._workspaceRepositoriesEndpoint(workspaceId);
@@ -102,6 +141,31 @@ module.exports = {
 
       const { data } = await axios.get(url, requestConfig);
       return data;
+    },
+    async *getCommits(opts) {
+      const {
+        workspaceId,
+        repositoryId,
+        branchName,
+        lastProcessedCommitHash,
+      } = opts;
+      const requestConfig = this._makeRequestConfig();
+
+      let url = this._branchCommitsEndpoint(workspaceId, repositoryId, branchName);
+      do {
+        const { data } = await axios.get(url, requestConfig);
+        const { values, next } = data;
+
+        // Yield the retrieved commits in a serial manner, until
+        // we exhaust the response from the BitBucket API, or we reach
+        // the last processed commit, whichever comes first.
+        for (const commit of values) {
+          if (commit.hash === lastProcessedCommitHash) return;
+          yield commit;
+        }
+
+        url = next;
+      } while (url);
     },
     _authToken() {
       return this.$auth.oauth_access_token
