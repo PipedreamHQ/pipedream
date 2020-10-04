e1e3eadc9fde750ffa991965f74f7001752ef9af
Dylan J. Sather <dylan.sather@gmail.com>
2020-10-04 16:06:53 -0700

    Initial commit of Strava components

---
+++ components/strava/activity-updated.js
@@ -0,0 +1,35 @@
+const strava = require("https://github.com/PipedreamHQ/pipedream/components/strava/strava.app.js");
+
+module.exports = {
+  name: "Activity Updated",
+  description: "Emits an event when an activity is updated",
+  version: "0.0.1",
+  props: {
+    strava,
+    stravaApphook: {
+      type: "$.interface.apphook",
+      appProp: "strava",
+      eventNames: ["activity.update"],
+    },
+  },
+  async run(event) {
+    console.log(event);
+    let details;
+    try {
+      details = await this.strava.getActivity(event.object_id);
+    } catch (err) {
+      console.log(`Error fetching activity details: ${err}`);
+    }
+    let summary = "Activity updated";
+    if (details && details.name) {
+      summary += `: ${details.name}`;
+    }
+    this.$emit(
+      { event, details },
+      {
+        summary,
+        ts: event.event_time * 1000,
+      }
+    );
+  },
+};
