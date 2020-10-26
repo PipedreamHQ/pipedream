7b4a7627420d9ac3cc760909f95515c8f8b911fe
michelle0927 <michellelbergero@hotmail.com>
2020-10-26 16:23:31 -0700

    formatting

--- components/hubspot/sources/new-deal/new-deal.js
+++ components/hubspot/sources/new-deal/new-deal.js
@@ -3,8 +3,7 @@ const hubspot = require("../../hubspot.app.js");
 module.exports = {
   key: "hubspot-new-deal",
   name: "New Deals",
-  description:
-    "Emits an event for each new deal created.",
+  description: "Emits an event for each new deal created.",
   version: "0.0.1",
   dedupe: "unique",
   props: {
@@ -19,42 +18,42 @@ module.exports = {
   },
 
   async run(event) {
-  	const lastRun = this.db.get("createdAfter") || this.hubspot.monthAgo();
-  	const createdAfter = new Date(lastRun);
-  	const data = {
-  		limit: 10,
-  		sorts: [
-      	{
-        	"propertyName": "createdate",
-        	"direction": "DESCENDING"
-      	}
-    	]
-  	};
+    const lastRun = this.db.get("createdAfter") || this.hubspot.monthAgo();
+    const createdAfter = new Date(lastRun);
+    const data = {
+      limit: 10,
+      sorts: [
+        {
+          propertyName: "createdate",
+          direction: "DESCENDING",
+        },
+      ],
+    };
 
-  	let done = false;
-  	let count = 0;
-  	let total = 1;
+    let done = false;
+    let count = 0;
+    let total = 1;
 
-  	while (!done && count < total) {
-  		let deals = await this.hubspot.searchCRM(data, 'deals');
-  		total = deals.total;
-  		if (deals.paging)
-  			data.after = deals.paging.next.after;
-  		for (const deal of deals.results) {
-  			let createdAt = new Date(deal.createdAt);
-  			if (createdAt.getTime() > createdAfter.getTime()) {
-  				this.$emit(deal, {
-        		id: deal.id,
-        		summary: deal.properties.dealname,
-        		ts: createdAt.getTime(),
-      		});
-  			} else { // don't need to continue if we've gotten to deals already evaluated
-  				done = true;
-  			}
-  			count++;
-  		}
-  	}
+    while (!done && count < total) {
+      let deals = await this.hubspot.searchCRM(data, "deals");
+      total = deals.total;
+      if (deals.paging) data.after = deals.paging.next.after;
+      for (const deal of deals.results) {
+        let createdAt = new Date(deal.createdAt);
+        if (createdAt.getTime() > createdAfter.getTime()) {
+          this.$emit(deal, {
+            id: deal.id,
+            summary: deal.properties.dealname,
+            ts: createdAt.getTime(),
+          });
+        } else {
+          // don't need to continue if we've gotten to deals already evaluated
+          done = true;
+        }
+        count++;
+      }
+    }
 
-  	this.db.set("createdAfter", Date.now());
-  }
+    this.db.set("createdAfter", Date.now());
+  },
 };
