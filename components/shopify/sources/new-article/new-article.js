792a5a3156e83fedabb1afe52c26898bce4b5636
michelle0927 <michelle0927@users.noreply.github.com>
2020-11-11 13:35:44 -0800

    formatting

--- components/shopify/sources/new-article/new-article.js
+++ components/shopify/sources/new-article/new-article.js
@@ -1,43 +1,43 @@
 const shopify = require("../../shopify.app.js");
 
 module.exports = {
-	key: "shopify-new-article",
-	name: "New Article",
-	description: "Emits an event for each new article in a blog.",
-	version: "0.0.4",
-	props: {
-		db: "$.service.db",
-		timer: {
-			type: "$.interface.timer",
-			default: {
-				intervalSeconds: 60 * 15,
-			},
-		},
-		shopify,
-		blogIds: {
-			type: "string[]",
-			label: "Blogs",
-			async options() {
-				const blogs = await this.shopify.getBlogs();
-				return blogs.map((blog) => {
-					return { label: blog.title, value: blog.id };
-				});
-			},
-		},
-	},
-	async run() {
-		for (const blog_id of this.blogIds) {
-			let since_id = this.db.get(blog_id) || null;
-			let results = await this.shopify.getArticles(blog_id, since_id);
-			for (const article of results) {
-				this.$emit(article, {
-					id: article.id,
-					summary: article.title,
-					ts: Date.now(),
-				});
-			}
-			if (results[results.length - 1])
-				this.db.set(blog_id, results[results.length - 1].id);
-		}
-	},
+  key: "shopify-new-article",
+  name: "New Article",
+  description: "Emits an event for each new article in a blog.",
+  version: "0.0.4",
+  props: {
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15,
+      },
+    },
+    shopify,
+    blogIds: {
+      type: "string[]",
+      label: "Blogs",
+      async options() {
+        const blogs = await this.shopify.getBlogs();
+        return blogs.map((blog) => {
+          return { label: blog.title, value: blog.id };
+        });
+      },
+    },
+  },
+  async run() {
+    for (const blog_id of this.blogIds) {
+      let since_id = this.db.get(blog_id) || null;
+      let results = await this.shopify.getArticles(blog_id, since_id);
+      for (const article of results) {
+        this.$emit(article, {
+          id: article.id,
+          summary: article.title,
+          ts: Date.now(),
+      });
+    }
+    if (results[results.length - 1])
+      this.db.set(blog_id, results[results.length - 1].id);
+    }
+  },
 };
