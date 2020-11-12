91ce64b60f1696d4c6dd0909f551e40043bb276c
adolfo-pd <adolfo@pipedream.com>
2020-11-12 09:27:26 -0800

    Make sending as test event on activation optional

--- components/yotpo/sources/new-reviews/new-reviews.js
+++ components/yotpo/sources/new-reviews/new-reviews.js
@@ -1,5 +1,4 @@
 const yotpo = require('../../yotpo.app.js')
-
 module.exports = {
   name: 'yotpo-new-reviews',
   version: '0.0.1',
@@ -7,14 +6,22 @@ module.exports = {
     http: {
       type: "$.interface.http",
     },
+    sendTestEvent: {
+      type: "boolean",
+      label: "Send Test Event",
+      description: "Create a test review when this source is activated",
+      optional: true,
+      default: true,
+    },
     yotpo,
   },
   hooks: {
     async activate() {
       await this.yotpo.createWebhook("review_create", this.http.endpoint)
       await this.yotpo.createWebhook("review_updated", this.http.endpoint)
-      await this.yotpo.sendTestEvents()
-      
+      if (this.sendTestEvent) {
+        await this.yotpo.sendTestEvents()
+      }
     },
     async deactivate() {
       await this.yotpo.deleteWebhook("review_create", this.http.endpoint)
