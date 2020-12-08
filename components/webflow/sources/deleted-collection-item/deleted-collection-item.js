d03cedf0351c4dec05de6560ae399afceafe7b17
Dylan J. Sather <dylan.sather@gmail.com>
2020-12-08 14:01:40 -0800

    Removing extraneous 'z'

--- components/webflow/sources/deleted-collection-item/deleted-collection-item.js
+++ components/webflow/sources/deleted-collection-item/deleted-collection-item.js
@@ -1,19 +1,19 @@
-const common = require("../common");
+const common = require('../common');
 
 module.exports = {
   ...common,
-  key: "webflow-deleted-collection-item",
-  name: "Deleted Collection Item (Instant)",
-  description: "Emit an event when a collection item is deleted",
-  version: "0.0.1",
-  dedupe: "unique",
+  key: 'webflow-deleted-collection-item',
+  name: 'Deleted Collection Item (Instant)',
+  description: 'Emit an event when a collection item is deleted',
+  version: '0.0.1',
+  dedupe: 'unique',
   methods: {
     ...common.methods,
     getWebhookTriggerType() {
-      return "collection_item_deleted";
+      return 'collection_item_deleted';
     },
     generateMeta(data) {
-      const { itemId: id } = data;
+      const {itemId: id} = data;
       const summary = `Collection item deleted: ID ${id}`;
       const ts = Date.now();
       return {
@@ -23,4 +23,4 @@ module.exports = {
       };
     },
   },
-};z
+};
