ea21453f18fc5b4600eb4aa1850bf05bff0766fe
Tod Sacerdoti <todsac@gmail.com>
2020-12-10 16:46:45 -0800

    Update new-comments-by-keyword.js

--- components/hacker_news/sources/new-comments-by-keyword/new-comments-by-keyword.js
+++ components/hacker_news/sources/new-comments-by-keyword/new-comments-by-keyword.js
@@ -49,7 +49,11 @@ module.exports = {
       if (seenKeysMap[key]) continue
       seenKeys.unshift(key)
       seenKeysMap[key] = true // just in case of dupes
-      this.$emit(item)
+      this.$emit(item, {
+        id: this.itemKey(item),
+        summary: item.title,
+        ts: item.pubdate && +new Date(item.pubdate), 
+      })
     }
     if (seenKeys.length) {
       // XXX restrict by byte size instead of el size
