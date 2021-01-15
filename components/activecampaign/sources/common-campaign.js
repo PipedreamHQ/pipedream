949ed7cb281991e1af2f56af57db04ebef4d5e08
michelle0927 <michelle0927@users.noreply.github.com>
2021-01-15 15:22:44 -0800

    formatting common-campaign.js

--- components/activecampaign/sources/common-campaign.js
+++ components/activecampaign/sources/common-campaign.js
@@ -2,23 +2,23 @@ const activecampaign = require("../activecampaign.app.js");
 const common = require("./common-webhook.js");
 
 module.exports = {
-	...common,
-	props: {
-		...common.props,
-		campaigns: { propDefinition: [activecampaign, "campaigns"] },
-	},
-	methods: {
-		isRelevant(body) {
-			return (
-				this.campaigns.length === 0 ||
-				this.campaigns.includes(body["campaign[id]"])
-			);
-		},
-		getMeta(body) {
-			return {
-				id: `${body["campaign[id]"]}${body["contact[id]"]}`,
-				summary: `${body["contact[email]"]}, Campaign: ${body["campaign[name]"]}`,
-			};
-		},
-	},
+  ...common,
+  props: {
+    ...common.props,
+    campaigns: { propDefinition: [activecampaign, "campaigns"] },
+  },
+  methods: {
+    isRelevant(body) {
+      return (
+        this.campaigns.length === 0 ||
+        this.campaigns.includes(body["campaign[id]"])
+      );
+    },
+    getMeta(body) {
+      return {
+        id: `${body["campaign[id]"]}${body["contact[id]"]}`,
+        summary: `${body["contact[email]"]}, Campaign: ${body["campaign[name]"]}`,
+      };
+    },
+  },
 };
