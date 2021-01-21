4b63d70f803d9cf75508b17824119f84fa0b9fd3
michelle0927 <michellelbergero@hotmail.com>
2021-01-20 16:02:37 -0800

    fixed key

--- components/activecampaign/sources/campaign-opened/campaign-opened.js
+++ components/activecampaign/sources/campaign-opened/campaign-opened.js
@@ -4,7 +4,7 @@ const common = require("../common-campaign.js");
 module.exports = {
   ...common,
   name: "Campaign Opened (Instant)",
-  key: "activecampaign-campaign-link-clicked",
+  key: "activecampaign-campaign-opened",
   description:
     "Emits an event when a contact opens a campaign (will trigger once per contact per campaign).",
   version: "0.0.1",
