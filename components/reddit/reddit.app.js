730a6c4112437ba88406759913ce7f76c35f2737
Sergio Rodriguez 🏡 <sergio_eliot@hotmail.com>
2021-02-16 12:56:48 -0700

    PR for initial feedback

--- components/reddit/reddit.app.js
+++ components/reddit/reddit.app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const axios = require("axios");
 const get = require("lodash/get");
 const retry = require("async-retry");
@@ -252,3 +253,54 @@ module.exports = {
     },
   },
 };
+=======
+module.exports = {
+    type: "app", 	
+    app: "reddit",
+    methods: {
+      _accessToken() {
+        return this.$auth.access_token;
+      },
+      _apiUrl() {
+        return `https://oauth.reddit.com`;
+      },
+      async _makeRequest(opts) {
+        if (!opts.headers) opts.headers = {};
+        opts.headers.authorization = `Bearer ${this._accessToken()}`;
+        opts.headers["user-agent"] = "@PipedreamHQ/pipedream v0.1";
+        const { path } = opts;
+        delete opts.path;
+        opts.url = `${this._apiUrl()}${path[0] === "/" ? "" : "/"}${path}`;
+        return await require("@pipedreamhq/platform").axios(this, opts);
+      },
+      // https://www.reddit.com/dev/api#GET_api_v1_me
+      async getMeInfo() {
+  
+          const me = await this._makeRequest({
+            path: "/api/v1/me",
+            params: {
+              per_page: 100, // max allowed by API
+              cursor,
+              updated_after,
+            },
+          });
+  
+  
+        return me;
+      },
+      
+  	  async getNewSubredditLinks(after_link){
+ 
+        const newSubredditLinks = await this._makeRequest({
+            path: "/r/redditdev/new",
+		    params: {
+		    	after:after_link
+		    },
+          });
+         
+  		return newSubredditLinks;
+
+        },
+    },
+  };
+>>>>>>> f27aed4... PR for initial feedback
