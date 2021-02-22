25eccb4979c48bb216c2b93f4c987f1aeaaabd4d
Sergio Rodriguez 🏡 <sergio_eliot@hotmail.com>
2021-02-22 01:19:01 -0700

    Initial commit for source new ho post on subreddit. Refactor couple of routines into reddit.app file, adjusted accordingly in sources files.

--- components/reddit/sources/new-link-on-a-subreddit/new-link-on-a-subreddit.js
+++ components/reddit/sources/new-link-on-a-subreddit/new-link-on-a-subreddit.js
@@ -1,5 +1,4 @@
 const reddit = require("../../reddit.app.js");
-const get = require("lodash.get");
 
 module.exports = {
   key: "new-link-on-a-subreddit",
@@ -20,7 +19,7 @@ module.exports = {
       description: "Pipedream polls Reddit for new links on this schedule.",
       type: "$.interface.timer",
       default: {
-        intervalSeconds: 60 * 10, // by default, run every 10 minute.
+        intervalSeconds: 60 * 10, // by default, run every 10 minutes.
       },
     },
     db: "$.service.db",
@@ -36,13 +35,12 @@ module.exports = {
         10
       );
 
-      if (get(reddit_things, "data.children") !== undefined) {
-        if (reddit_things.data.children.length > 0) {
-          before = reddit_things.data.children[0].data.name;
-          reddit_things.data.children.forEach((reddit_link) => {
-            this.emitRedditEvent(reddit_link);
-          });
-        }
+      const links_pulled = this.reddit.wereLinksPulled(reddit_things);
+      if (links_pulled) {
+        before = reddit_things.data.children[0].data.name;
+        reddit_things.data.children.forEach((reddit_link) => {
+          this.emitRedditEvent(reddit_link);
+        });
       }
 
       this.db.set("before", before);
@@ -59,19 +57,13 @@ module.exports = {
   },
   async run() {
     const start_before = this.db.get("before");
-
     do {
       const reddit_things = await this.reddit.getNewSubredditLinks(
         start_before,
         this.subreddit
       );
 
-      const links_length = get(reddit_things, "data.children.length");
-      var links_pulled =
-        links_length !== null &&
-        links_length !== undefined &&
-        reddit_things.data.children.length > 0;
-
+      var links_pulled = this.reddit.wereLinksPulled(reddit_things);
       if (links_pulled) {
         const new_before = reddit_things.data.children[0].data.name;
         this.db.set("before", new_before);
