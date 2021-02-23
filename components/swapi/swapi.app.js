a4058e8eb3ad71e1193d736071f396c6241724f7
Pravin <psavkar@yahoo.com>
2021-02-23 09:38:35 -0800

    swapi

---
+++ components/swapi/swapi.app.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+module.exports = {
+  type: 'app',
+  app: 'swapi',
+  propDefinitions: {
+    film: {
+      type: "string",
+      async options() {
+        return (await axios({ url: 'https://swapi.dev/api/films' })).data.results.map(function(film, index) {
+          return {
+            label: film.title,
+            value: index + 1
+          }
+        })
+      }
+    }
+  }
+}
