0124f686d3559645105d239ec6d438b32571cf39
michelle0927 <michellelbergero@hotmail.com>
2021-02-26 14:07:01 -0800

    updates

--- components/spotify/spotify.app.js
+++ components/spotify/spotify.app.js
@@ -1,8 +1,29 @@
 const axios = require("axios");
+const { promisify } = require("util");
+const pause = promisify((delay, fn) => setTimeout(fn, delay));
 
 module.exports = {
   type: "app",
   app: "spotify",
+  propDefinitions: {
+    playlists: {
+      type: "string[]",
+      label: "Playlist",
+      description: "Search for new tracks added to the specified playlist(s).",
+      async options({ prevContext }) {
+        const limit = 20;
+        const offset = prevContext.offset ? prevContext.offset : 0;
+        const results = await this.getPlaylists({ limit, offset });
+        const options = results.items.map((playlist) => {
+          return { label: playlist.name, value: playlist.id };
+        });
+        return {
+          options,
+          context: { offset: offset+limit },
+        };
+      },
+    },
+  },
   methods: {
     async _getBaseUrl() {
       return "https://api.spotify.com/v1"
@@ -12,34 +33,41 @@ module.exports = {
         Authorization: `Bearer ${this.$auth.oauth_access_token}`,
       };
     },
-    async getPlaylistItems(playlist_id, params) {
-      return await axios.get(
-        `${await this._getBaseUrl()}/playlists/${playlist_id}/tracks`,
-        {
-          headers: await this._getHeaders(),
-          params,
-        }
-      );
+    async _makeRequest(method, endpoint, params) {
+      config = {
+        method,
+        url: `${await this._getBaseUrl()}/${endpoint}`,
+        headers: await this._getHeaders(),
+        params,
+      };
+      return await this.retry(config);
     },
-
-    async getPlaylists(params) {
-      return await axios.get(
-        `${await this._getBaseUrl()}/me/playlists`, 
-        {
-          headers: await this._getHeaders(),
-          params,
+    // Retry axios request if not successful
+    async retry(config, retries = 3) {
+      let response;
+      try {
+        response = await axios(config);
+        return response.data;
+      } catch(err) {
+        if (retries <= 1) {
+          throw new Error(err);
         }
-      );
+        // if rate limit is exceeded, Retry-After will contain the # of seconds to wait before retrying
+        delay = (response && response.status == 429) ? (response.headers['Retry-After']*1000) : 500;
+        await pause(delay);
+        return await this.retry(config, retries - 1);
+      }
+    },
+    async getPlaylistItems(params) {
+      const { playlistId } = params;
+      return await this._makeRequest("GET", `playlists/${playlistId}/tracks`, params);
+    },
+    async getPlaylists(params) {
+      return await this._makeRequest("GET", `me/playlists`, params);
     },
 
     async getTracks(params) {
-      return await axios.get(
-        `${await this._getBaseUrl()}/me/tracks`, 
-        {
-          headers: await this._getHeaders(),
-          params,
-        }
-      );
+      return await this._makeRequest("GET", `me/tracks`, params);
     },
   },
 };
