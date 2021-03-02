e67836672142fc751b4f2f1adb29e121744b6489
Pravin <psavkar@yahoo.com>
2021-03-02 00:05:51 -0800

    updates

--- components/airtable/airtable.app.js
+++ components/airtable/airtable.app.js
@@ -27,7 +27,7 @@ module.exports = {
     records: {
       type: "string",
       label: "Records",
-      description: 'Enter an array of objects. Each object should represent a row with the column name as the key and the corresponding value. You can include all, some, or none of the fields. The most common usage is pass a reference to an array of objects exported by a previous step (e.g., `{{steps.foo.$return_value}}`). You may also pass a string value that will `JSON.parse()` to an array of objects (e.g., `[{"foo":"bar","id":"{{steps.trigger.context.workflow_id}}"},{"foo":"baz","id":"value"}]`).'
+      description: 'Enter an array of objects. Each object should represent a row with the column name as the key and the corresponding value. You can include all, some, or none of the fields. The most common usage is pass a reference to an array of objects exported by a previous step (e.g., `{{steps.foo.$return_value}}`). You may also pass a string value that will `JSON.parse()` to an array of objects (e.g., `[{"foo":"bar","id":123},{"foo":"baz","id":456}]`).'
     },
     sortDirection: { 
       type: "string", 
