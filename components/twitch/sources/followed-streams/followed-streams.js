1b8f62b1213b74710bbd0a4bb52d11b3444166c1
michelle0927 <michellelbergero@hotmail.com>
2021-03-29 13:35:34 -0700

    poll for streams

--- components/twitch/sources/followed-streams/followed-streams.js
+++ components/twitch/sources/followed-streams/followed-streams.js
@@ -1,40 +1,49 @@
-const common = require("../common-webhook.js");
+const common = require("../common-polling.js");
 
 module.exports = {
   ...common,
-  name: "Followed Streams (Instant)",
+  name: "Followed Streams",
   key: "twitch-followed-streams",
   description: "Emits an event when a followed stream is live.",
-  version: "0.0.2",
+  version: "0.0.3",
   methods: {
     ...common.methods,
-    async getTopics() {
-      // get the authenticated user
-      const { data: authenticatedUserData } = await this.twitch.getUsers();
-      const params = {
-        from_id: authenticatedUserData[0].id,
-      };
-      const items = await this.paginate(
-        this.twitch.getUserFollows.bind(this),
-        params
-      );
-      const topics = [];
-      for await (const item of items) {
-        topics.push(this.getTopicString(item));
-      }
-      return topics;
-    },
-    getTopicString(followed) {
-      return `streams?user_id=${followed.to_id}`;
-    },
-    getMeta(item, headers) {
-      const { started_at: startedAt, title: summary } = item;
+    getMeta(item) {
+      const { id, started_at: startedAt, title: summary } = item;
       const ts = new Date(startedAt).getTime();
       return {
-        id: headers["twitch-notification-id"],
+        id,
         summary,
         ts,
       };
     },
+  },
+  hooks: {
+    async deploy() {
+      // get the authenticated user
+      const { data: authenticatedUserData } = await this.twitch.getUsers();
+      this.db.set("authenticatedUserId", authenticatedUserData[0].id);
+    },
+  },
+  async run() {
+    const params = {
+      from_id: this.db.get("authenticatedUserId"),
+    };
+    // get the user_ids of the streamers followed by the authenticated user
+    const follows = await this.paginate(
+      this.twitch.getUserFollows.bind(this),
+      params
+    );
+    const followedIds = [];
+    for await (const follow of follows) {
+      followedIds.push(follow.to_id);
+    }
+    // get and emit streams for the followed streamers
+    const streams = await this.paginate(this.twitch.getStreams.bind(this), {
+      user_id: followedIds,
+    });
+    for await (const stream of streams) {
+      this.$emit(stream, this.getMeta(stream));
+    }
   },
 };
