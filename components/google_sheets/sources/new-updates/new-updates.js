cf5101cdb3fc2d408bc9dc87ffbc7693bfad6aa7
Jay Vercellone <juanjov@gmail.com>
2021-04-01 17:38:10 -0700

    Bump versions

--- components/google_sheets/sources/new-updates/new-updates.js
+++ components/google_sheets/sources/new-updates/new-updates.js
@@ -7,7 +7,7 @@ module.exports = {
   name: "New Updates (Instant)",
   description:
     "Emits an event each time a row or cell is updated in a spreadsheet.",
-  version: "0.0.10",
+  version: "0.0.11",
   dedupe: "unique",
   props: {
     ...common.props,
