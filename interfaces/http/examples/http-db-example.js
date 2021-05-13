886a32dbe9643ccc3c78803f8d5483c6015f779e
Jay Vercellone <juanjov@gmail.com>
2021-05-12 17:31:07 -0700

    Linting setup
    * Install [ESLint](https://eslint.org/) package and plugins
    * Add linting and formatting NPM scripts
    * Apply linting rules and fixes in a couple of non-critical
      directories
    * Sort `package.json` with standard tool instead

--- interfaces/http/examples/http-db-example.js
+++ interfaces/http/examples/http-db-example.js
@@ -2,23 +2,25 @@
 // Each time this component receives an HTTP request,
 // it increments the requestCount key and emits it
 module.exports = {
-  name: 'http-db',
-  version: '0.0.2',
+  name: "http-db",
+  version: "0.0.2",
   props: {
     http: {
       type: "$.interface.http",
       customResponse: true,
     },
-    db: '$.service.db',
+    db: "$.service.db",
   },
   async run(event) {
     this.http.respond({
       status: 200,
       body: event,
     });
-    let requestCount = this.db.get('requestCount') || 0;
+    let requestCount = this.db.get("requestCount") || 0;
     requestCount += 1;
-    this.$emit({requestCount});
-    this.db.set('requestCount', requestCount);
+    this.$emit({
+      requestCount,
+    });
+    this.db.set("requestCount", requestCount);
   },
 };
--- interfaces/timer/examples/create-component/deploy-cron-job.js
+++ interfaces/timer/examples/create-component/deploy-cron-job.js
@@ -1,10 +1,10 @@
-const axios = require('axios');
-require('dotenv').config();
-const data = require('./api-payload.json');
+const axios = require("axios");
+require("dotenv").config();
+const data = require("./api-payload.json");
 
 axios({
-  method: 'POST',
-  url: 'https://api.pipedream.com/v1/sources',
+  method: "POST",
+  url: "https://api.pipedream.com/v1/sources",
   headers: {
     Authorization: `Bearer ${process.env.PIPEDREAM_API_KEY}`,
   },
