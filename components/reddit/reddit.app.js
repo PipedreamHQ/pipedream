7a671cfabea2fcfbabf56e3f0e551b2baea35018
Sergio Rodriguez 🏡 <sergio_eliot@hotmail.com>
2021-05-28 18:19:43 -0600

    025 Rebasing for Mailgun sources Sergio's origin/master from upstream/master

--- components/reddit/reddit.app.js
+++ components/reddit/reddit.app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const axios = require("axios");
 const get = require("lodash/get");
 const retry = require("async-retry");
@@ -253,68 +252,3 @@ module.exports = {
     },
   },
 };
-=======
-module.exports = {
-    type: "app",
-    app: "reddit",
-    methods: {
-      _accessToken() {
-        return this.$auth.access_token;
-      },
-      _apiUrl() {
-        return `https://oauth.reddit.com`;
-      },
-      async _makeRequest(opts) {
-        if (!opts.headers) opts.headers = {};
-        opts.headers.authorization = `Bearer ${this._accessToken()}`;
-        opts.headers["user-agent"] = "@PipedreamHQ/pipedream v0.1";
-        const { path } = opts;
-        delete opts.path;
-        opts.url = `${this._apiUrl()}${path[0] === "/" ? "" : "/"}${path}`;
-        return await require("@pipedreamhq/platform").axios(this, opts);
-      },
-      // https://www.reddit.com/dev/api#GET_api_v1_me
-      async getMeInfo() {
-
-          const me = await this._makeRequest({
-            path: "/api/v1/me",
-            params: {
-              per_page: 100, // max allowed by API
-              cursor,
-              updated_after,
-            },
-          });
-
-
-        return me;
-      },
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-  	  async getNewSubredditLinks(after_link, subreddit){
-
-        const newSubredditLinks = await this._makeRequest({
-            path: `/r/${subreddit}/new`,
-=======
-  	  async getNewSubredditLinks(after_link){
-
-        const newSubredditLinks = await this._makeRequest({
-            path: "/r/redditdev/new",
->>>>>>> 2876951... PR for initial feedback
-=======
-  	  async getNewSubredditLinks(after_link, subreddit){
-
-        const newSubredditLinks = await this._makeRequest({
-            path: `/r/${subreddit}/new`,
->>>>>>> 3b7c991... Added an input prop for reddit.
-		    params: {
-		    	after:after_link
-		    },
-          });
-
-  		return newSubredditLinks;
-
-        },
-    },
-  };
->>>>>>> f27aed4... PR for initial feedback
--- components/reddit/sources/new-link-on-a-subreddit/new-link-on-a-subreddit.js
+++ components/reddit/sources/new-link-on-a-subreddit/new-link-on-a-subreddit.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 const reddit = require("../../reddit.app.js");
+=======
+const uservoice = require("../../reddit.app.js");
+>>>>>>> f27aed4... PR for initial feedback
 
 module.exports = {
     key: "new-link-on-a-subreddit",
     name: "New Link on a subreddit",
     description: "Emmits a new link submitted to a subreddit",
+<<<<<<< HEAD
     version: "0.0.2",
+=======
+    version: "0.0.1",	
+>>>>>>> f27aed4... PR for initial feedback
     props: {
         reddit,
         timer: {
@@ -13,6 +21,7 @@ module.exports = {
             type: "$.interface.timer",
             default: {
             intervalSeconds: 60, // by default, run every 1 minute
+<<<<<<< HEAD
             },
         },
         subreddit: {
@@ -22,13 +31,26 @@ module.exports = {
         },
         db: "$.service.db"
      },
+=======
+        },
+    },
+    db: "$.service.db",
+    },
+>>>>>>> f27aed4... PR for initial feedback
     hooks: {
     async deploy() {
 
         // Emmits all existing events for the first time.
+<<<<<<< HEAD
 
         do{
             const reddit_things = await this.getNewSubredditLinks(null, this.subreddit);
+=======
+        this.emitMeInfo(me_info);
+
+        do{   
+            const reddit_things = await this.getNewSubredditLinks(null);
+>>>>>>> f27aed4... PR for initial feedback
             var after = reddit_things.data.data.after;
             if(after){
                 last = reddit_things.data.data.children[reddit_things.data.data.children.length-1].data.name;
@@ -37,13 +59,23 @@ module.exports = {
                 reddit_things.data.data.children.forEach(reddit_link => {
                     this.emitMeInfo(reddit_link);
                 });
+<<<<<<< HEAD
         }
         }while(after);
 			this.db.set("after",after);
+=======
+        }		    
+        }while(after);
+			this.db.set("after",response.data.data.after);		
+>>>>>>> f27aed4... PR for initial feedback
         },
     },
     methods: {
         emitMeInfo(reddit_link) {
+<<<<<<< HEAD
+=======
+                    
+>>>>>>> f27aed4... PR for initial feedback
             this.$emit(reddit_link, {
                 summary: reddit_link.data.name,
             });
@@ -52,16 +84,29 @@ module.exports = {
     },
     async run() {
 
+<<<<<<< HEAD
         let current_after = this.db.get("after");
         const reddit_things = await this.getNewSubredditLinks(current_after,this.subreddit);
         var new_after = reddit_things.data.data.after;
         if(new_after){
+=======
+        let after = this.db.get("after");
+        const reddit_things = await this.getNewSubredditLinks(after);
+        var after = reddit_things.data.data.after;
+        if(after){
+>>>>>>> f27aed4... PR for initial feedback
             if(reddit_things.data.data.children.length>0){
                 reddit_things.data.data.children.forEach(reddit_link => {
                     this.emitMeInfo(reddit_link);
                 });
+<<<<<<< HEAD
                 this.db.set("after",new_after);
             }
         }
+=======
+                this.db.set("after",after);
+            }                        
+        }                		
+>>>>>>> f27aed4... PR for initial feedback
     },
 };
