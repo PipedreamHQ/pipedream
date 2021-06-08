a186115af8f38716817b12c5289abb32af44cffe
michelle0927 <michellelbergero@hotmail.com>
2021-06-08 16:56:46 -0700

    formatting

--- components/docusign/sources/common.js
+++ components/docusign/sources/common.js
@@ -2,14 +2,14 @@ const docusign = require("../docusign.app.js");
 
 module.exports = {
   props: {
-  	docusign,
-  	account: {
+    docusign,
+    account: {
       propDefinition: [
         docusign,
         "account",
       ],
     },
-  	db: "$.service.db",
+    db: "$.service.db",
     timer: {
       type: "$.interface.timer",
       default: {
@@ -18,26 +18,26 @@ module.exports = {
     },
   },
   methods: {
-  	_getBaseUri() {
-  		return this.db.get("baseUri");
-  	},
-  	_setBaseUri(baseUri) {
-  		this.db.set("baseUri", baseUri);
-  	},
+    _getBaseUri() {
+      return this.db.get("baseUri");
+    },
+    _setBaseUri(baseUri) {
+      this.db.set("baseUri", baseUri);
+    },
     monthAgo() {
       const monthAgo = new Date();
       monthAgo.setMonth(monthAgo.getMonth() - 1);
       return monthAgo;
     },
-  	emitEvent(result, ts) {
-  		const meta = this.generateMeta(result, ts);
-  		this.$emit(result, meta);
-  	},
+    emitEvent(result, ts) {
+      const meta = this.generateMeta(result, ts);
+      this.$emit(result, meta);
+    },
   },
   hooks: {
-  	async deploy() {
-  		const baseUri = await this.docusign.getBaseUri(this.account);
-  		this._setBaseUri(baseUri);
-  	},
+    async deploy() {
+      const baseUri = await this.docusign.getBaseUri(this.account);
+      this._setBaseUri(baseUri);
+    },
   },
 };
--- components/docusign/sources/new-folder/new-folder.js
+++ components/docusign/sources/new-folder/new-folder.js
@@ -8,62 +8,61 @@ module.exports = {
   version: "0.0.1",
   dedupe: "unique",
   methods: {
-  	...common.methods,
-  	_getFolderIds() {
-  		return this.db.get("folderIds");
-  	},
-  	_setFolderIds(folderIds) {
-  		this.db.set("folderIds", folderIds);
-  	},
-  	processFolder(folderIds, folder, ts) {
+    ...common.methods,
+    _getFolderIds() {
+      return this.db.get("folderIds");
+    },
+    _setFolderIds(folderIds) {
+      this.db.set("folderIds", folderIds);
+    },
+    processFolder(folderIds, folder, ts) {
       if (this.isRelevant(folderIds, folder.folderId)) {
         this.emitEvent(folder, ts);
         folderIds.push(folder.folderId);
       }
       return folderIds;
-  	},
-  	isRelevant(folderIds, id) {
-  		return !folderIds.includes(id);
-  	},
-  	generateMeta({
+    },
+    isRelevant(folderIds, id) {
+      return !folderIds.includes(id);
+    },
+    generateMeta({
       folderId: id, name: summary,
     }, ts) {
-  		return {
-  			id,
-  			summary,
-  			ts,
-  		};
-  	},
+      return {
+        id,
+        summary,
+        ts,
+      };
+    },
   },
   async run(event) {
-  	const { timestamp: ts } = event;
-  	const baseUri = this._getBaseUri();
-  	let folderIds = this._getFolderIds() || [];
-  	let done = false;
-  	const params = {
-  		start_position: 0,
-  		include: "envelope_folders,template_folders,shared_template_folders",
-  		include_items: true,
-  	};
-  	do {
-  		const {
+    const { timestamp: ts } = event;
+    const baseUri = this._getBaseUri();
+    let folderIds = this._getFolderIds() || [];
+    let done = false;
+    const params = {
+      start_position: 0,
+      include: "envelope_folders,template_folders,shared_template_folders",
+      include_items: true,
+    };
+    do {
+      const {
         folders = [],
         nextUri,
         endPosition,
-      } = await this.docusign.listFolders(baseUri,
-        params);
-  		if (nextUri) params.start_position += endPosition + 1;
-  		else done = true;
+      } = await this.docusign.listFolders(baseUri, params);
+      if (nextUri) params.start_position += endPosition + 1;
+      else done = true;
 
-  		for (const folder of folders) {
+      for (const folder of folders) {
         if (folder.hasSubFolders == "true") {
           for (const subfolder of folder.folders) {
             folderIds = this.processFolder(folderIds, subfolder, ts);
           }
         }
-  			folderIds = this.processFolder(folderIds, folder, ts);
-  		}
-  	} while (!done);
-  	this._setFolderIds(folderIds);
+        folderIds = this.processFolder(folderIds, folder, ts);
+      }
+    } while (!done);
+    this._setFolderIds(folderIds);
   },
 };
