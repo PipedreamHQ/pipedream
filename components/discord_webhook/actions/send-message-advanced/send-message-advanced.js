904106ef659be04a458faee5217fd3021bf64991
Dylan J. Sather <dylan.sather@gmail.com>
2021-06-22 16:59:54 -0700

    Correcting description

--- components/discord_webhook/actions/send-message-advanced/send-message-advanced.js
+++ components/discord_webhook/actions/send-message-advanced/send-message-advanced.js
@@ -5,7 +5,7 @@ module.exports = {
   key: "discord_webhook-send-message-advanced",
   name: "Send Message (Advanced)",
   description: "Send a simple or structured message (using embeds) to a Discord channel.",
-  version: "0.0.1",
+  version: "0.0.3",
   type: "action",
   props: {
     discordWebhook,
