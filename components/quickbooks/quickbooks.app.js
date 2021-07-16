e5d06a2080596eaac4bfcbfde86b5a0ced52ef63
Celeste Bancos <celestebancos@gmail.com>
2021-07-15 21:11:24 -0400

    Add propDefinition for operations_to_emit

--- components/quickbooks/quickbooks.app.js
+++ components/quickbooks/quickbooks.app.js
@@ -4,6 +4,13 @@ module.exports = {
   type: "app",
   app: "quickbooks",
   propDefinitions: {
+    operations_to_emit: {
+      type: 'string[]',
+      label: 'Operations',
+      description: 'Select which operations to emit. If you want to emit them all you can just leave this field blank.',
+      options: ['Create', 'Update', 'Merge', 'Delete', 'Void', 'Emailed'],
+      optional: true,
+    },
   },
   methods: {
   },
