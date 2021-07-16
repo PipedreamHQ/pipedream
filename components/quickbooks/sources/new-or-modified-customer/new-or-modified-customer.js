91228bd06c236e8f0238173ab4d072e6817d0636
Celeste Bancos <celestebancos@gmail.com>
2021-07-16 07:41:20 -0400

    Rename variables and change source names.

--- components/quickbooks/sources/new-or-modified-customer/new-or-modified-customer.js
+++ components/quickbooks/sources/new-or-modified-customer/new-or-modified-customer.js
@@ -1,39 +1,42 @@
 const quickbooks = require('../../quickbooks.app');
 const common = require('../common')
 
-const entity_name = 'Customer'
+const source_entity = {
+	name: 'Customer',
+	display_name: 'Customer',
+}
 
-const supported_operations = common.methods.getSupportedOperations(entity_name)
+const supported_operations = common.methods.getSupportedOperations(source_entity.name)
 const supported_operations_description = common.methods.describeOperations(supported_operations)
 
 module.exports = {
   ...common,
-  key: `quickbooks-new-or-modified-${entity_name.toLowerCase()}`,
-  name: `New or Modified ${entity_name}`,
-  description: `Emits an event when a ${entity_name} is ${supported_operations_description}.`,
+  key: `quickbooks-new-or-modified-${source_entity.name.toLowerCase()}`,
+  name: `${source_entity.display_name} Created, Updated, Merged, Deleted, Voided, or Emailed`,
+  description: `Emits an event when a ${source_entity.display_name} is ${supported_operations_description}.`,
   version: '0.0.1',
   props: {
     ...common.props,
     operations_to_emit: {
       propDefinition: [quickbooks, 'operations_to_emit'],
-      //list only the options supported by this entity instead of offering all the default options from the propDefinition
-      options: common.methods.getSupportedOperations(entity_name),
+      //list only the options supported by this source's entity instead of offering all the default options from the propDefinition
+      options: supported_operations,
     },
   },
   methods: {
   	...common.methods,
   },
   async run(event) {
-    const entity = this.getEntity(event)
-    this.sendHttpResponse(event, entity)
+    const webhook_entity = this.getEntity(event)
+    this.sendHttpResponse(event, webhook_entity)
 
-    //reject any events that don't match the entity name or operation (if those options have been selected)
-    if(entity.name !== entity_name){
-      console.log(`${entity.name} webhook received and ignored, since it is not a ${entity_name}`)
-    } else if(this.operations_to_emit.length > 0 && !this.operations_to_emit.includes(entity.operation)){
-      console.log(`Operation '${entity.operation}' not found in list of selected Operations`)
+    //reject any events that don't match the source entity name or operation (if those options have been selected)
+    if(webhook_entity.name !== source_entity.name){
+      console.log(`${webhook_entity.name} webhook received and ignored, since it is not a ${source_entity.name}`)
+    } else if(this.operations_to_emit.length > 0 && !this.operations_to_emit.includes(webhook_entity.operation)){
+      console.log(`Operation '${webhook_entity.operation}' not found in list of selected Operations`)
     } else {
-      this.emitEvent(event, entity)
+      this.emitEvent(event, webhook_entity)
     }
   },
 };
