c6ed1cdc2e73cc9cd11d619d23a6e8cb9dd2436a
Celeste Bancos <celestebancos@gmail.com>
2021-07-16 21:49:31 -0400

    Hard-code entity name into descriptive text to increase code readability

--- components/quickbooks/sources/new-or-modified-customer/new-or-modified-customer.js
+++ components/quickbooks/sources/new-or-modified-customer/new-or-modified-customer.js
@@ -1,19 +1,16 @@
 const quickbooks = require('../../quickbooks.app');
 const common = require('../common')
 
-const source_entity = {
-	name: 'Customer',
-	display_name: 'Customer',
-}
+const source_entity = 'Customer'
 
-const supported_operations = common.methods.getSupportedOperations(source_entity.name)
+const supported_operations = common.methods.getSupportedOperations(source_entity)
 const supported_operations_description = common.methods.describeOperations(supported_operations)
 
 module.exports = {
   ...common,
-  key: `quickbooks-new-or-modified-${source_entity.name.toLowerCase()}`,
-  name: `New or Modified ${source_entity.display_name} (${supported_operations.join(', ')})`,
-  description: `Emits an event when a ${source_entity.display_name} is ${supported_operations_description}. Visit the documentation page to learn how to configure webhooks for your QuickBooks company: https://developer.intuit.com/app/developer/qbo/docs/develop/webhooks`,
+  key: `quickbooks-new-or-modified-customer`,
+  name: `New or Modified Customer (${supported_operations.join(', ')})`,
+  description: `Emits an event when a Customer is ${supported_operations_description}. Visit the documentation page to learn how to configure webhooks for your QuickBooks company: https://developer.intuit.com/app/developer/qbo/docs/develop/webhooks`,
   version: '0.0.1',
   props: {
     ...common.props,
@@ -30,9 +27,9 @@ module.exports = {
     const webhook_entity = this.getEntity(event)
     this.sendHttpResponse(event, webhook_entity)
 
-    //reject any events that don't match the source entity name or operation (if those options have been selected)
-    if(webhook_entity.name !== source_entity.name){
-      console.log(`${webhook_entity.name} webhook received and ignored, since it is not a ${source_entity.name}`)
+    //reject any events that don't match the specified entity name or operation
+    if(webhook_entity.name !== source_entity){
+      console.log(`${webhook_entity.name} webhook received and ignored, since it is not a Customer`)
     } else if(this.operations_to_emit.length > 0 && !this.operations_to_emit.includes(webhook_entity.operation)){
       console.log(`Operation '${webhook_entity.operation}' not found in list of selected Operations`)
     } else {
