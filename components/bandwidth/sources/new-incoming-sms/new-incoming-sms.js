bde23baf9507c87f1f9aa3766ca554771ed7fc03
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-07-29 10:54:02 -0400

    Fix linter errors

--- components/bandwidth/sources/new-incoming-sms/new-incoming-sms.js
+++ components/bandwidth/sources/new-incoming-sms/new-incoming-sms.js
@@ -3,21 +3,21 @@ module.exports = {
   description: "Emits an event each time a `message-received` event is received at the source url",
   props: {
     http: {
-        type: "$.interface.http",
-        customResponse: true
-      }
+      type: "$.interface.http",
+      customResponse: true,
     },
+  },
 
   async run(event) {
-    let messageBody = event.body[0]
+    let messageBody = event.body[0];
     this.http.respond({
-      status: 204
-      });
+      status: 204,
+    });
 
     if (messageBody.message.direction == "in") {
       this.$emit(messageBody, {
-        summary: "Message Received"
-      })
+        summary: "Message Received",
+      });
     }
-  }
+  },
 };
