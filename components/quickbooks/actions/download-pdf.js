c730c06e9e9a49a81cf929ef5e2b420566d6792b
Celeste Bancos <celestebancos@gmail.com>
2021-08-15 21:44:22 -0400

    Begin drafting Download PDF action

---
+++ components/quickbooks/actions/download-pdf.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'Download PDF',
+	description: 'Download an invoice, bill, purchase order, etc. as a PDF',
+	key: 'download_pdf',
+	version: '0.0.1',
+	type: 'action',
+	props: {
+	},
+	methods: {},
+	async run(){
+
+	}
+}
