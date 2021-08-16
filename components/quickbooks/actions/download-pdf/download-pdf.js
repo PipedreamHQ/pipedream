de37b8a9997cfef5852cff14523e40dbfcd1f102
Celeste Bancos <celestebancos@gmail.com>
2021-08-15 21:47:26 -0400

    Move download-pdf to folder

--- components/quickbooks/actions/download-pdf/download-pdf.js
+++ components/quickbooks/actions/download-pdf/download-pdf.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'Download PDF',
-	description: 'Download an invoice, bill, purchase order, etc. as a PDF',
+	description: 'Download an invoice, bill, purchase order, etc. as a PDF and save it in the temporary file system for use in a later step.',
 	key: 'download_pdf',
 	version: '0.0.1',
 	type: 'action',
@@ -8,6 +8,6 @@ module.exports = {
 	},
 	methods: {},
 	async run(){
-
+		
 	}
 }
