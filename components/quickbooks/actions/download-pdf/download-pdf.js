20264e5f337536d042b3aeff9b4fd630c4dc669d
Celeste Bancos <celestebancos@gmail.com>
2021-08-16 20:32:38 -0400

    Add .pdf extension if it's not already included in the file name

--- components/quickbooks/actions/download-pdf/download-pdf.js
+++ components/quickbooks/actions/download-pdf/download-pdf.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'Download PDF',
 	description: 'Download an invoice, bill, purchase order, etc. as a PDF and save it in the temporary file system for use in a later step.',
 	key: 'download_pdf',
-	version: '0.1.1',
+	version: '0.1.3',
 	type: 'action',
 	props: {
 		quickbooks,
@@ -43,14 +43,18 @@ module.exports = {
 		    responseType: 'arraybuffer',
 		  })
 
-		  const file_path = '/tmp/' + (file_name || id)
+		  const file_path = '/tmp/' + file_name
 		  fs.writeFileSync(file_path, file)
 
 		  return file_path
 		}
 	},
 	async run({ $ }){
-		const file_path = await this.downloadPDF(this.entity, this.id, this.file_name)
+		const file_name = this.file_name || this.id
+		const file_name_with_extension = file_name.endsWith('.pdf') ? file_name : file_name + '.pdf' 
+
+		const file_path = await this.downloadPDF(this.entity, this.id, file_name_with_extension)
 		$.export('file_path', file_path)
+		$.export('file_name', file_name_with_extension)
 	}
 }
