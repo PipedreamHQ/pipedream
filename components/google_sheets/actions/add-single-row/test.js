0d1e385adee8757b6f54d3b7d4256a70af5192d6
Dylan J. Sather <dylan.sather@gmail.com>
2021-08-25 17:29:46 -0700

    Revving version after adding tests to core ESLint plugin, adding ESLint checks

---
+++ components/google_sheets/actions/add-single-row/test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  key: "test",
+  name: "Test",
+  description: "hello",
+  type: "foo",
+  version: "0.0.1",
+  props: {
+    test: {
+      type: "string",
+      label: "Test",
+      description: "test",
+    },
+  },
+};
