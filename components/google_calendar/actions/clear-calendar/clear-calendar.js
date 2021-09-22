a50d861205858decb7347864152a80240b0804ba
Moulik Aggarwal <qwertymoulik@gmail.com>
2021-09-22 15:53:46 +0530

    Clear Calendar | Clear a primary calendar. Deletes all events associated with the primary calendar of an account
    Signed-off-by: Moulik Aggarwal <qwertymoulik@gmail.com>

---
+++ components/google_calendar/actions/clear-calendar/clear-calendar.js
@@ -0,0 +1,26 @@
+const googleCalendar = require("../../google_calendar.app");
+
+module.exports = {
+  key: "google_calendar_clear_calendar",
+  name: "Delete all events from a calendar",
+  description: "Delete all events from a calendar.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    googleCalendar,
+    calendarId: {
+      propDefinition: [
+        googleCalendar,
+        "calendarId",
+      ],
+      optional: true,
+      default: "primary",
+    },
+  },
+  async run() {
+    const calendar = this.googleCalendar.calendar();
+    return (await calendar.calendars.clear({
+      calendarId: this.calendarId,
+    })).data;
+  },
+};
