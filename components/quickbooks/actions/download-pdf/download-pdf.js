fa90d153a656cc7ca836ca2a1f7ad3a0aacf1411
Celeste Bancos <celestebancos@gmail.com>
2021-09-26 14:07:56 -0400

    ES Lint Fixes for Download PDF action

--- components/quickbooks/actions/download-pdf/download-pdf.js
+++ components/quickbooks/actions/download-pdf/download-pdf.js
@@ -1,60 +1,64 @@
 const quickbooks = require("../../quickbooks.app");
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = {
-	name: 'Download PDF',
-	description: 'Download an invoice, bill, purchase order, etc. as a PDF and save it in the temporary file system for use in a later step.',
-	key: 'download_pdf',
-	version: '0.1.3',
-	type: 'action',
-	props: {
-		quickbooks,
-		entity: {
-			type: 'string',
-			label: 'Document Type',
-			options: [
-			  "CreditMemo",
-			  "Estimate",
-			  "Invoice",
-			  "Payment",
-			  "PurchaseOrder",
-			  "RefundReceipt",
-			  "SalesReceipt",
-			],
-		},
-		id: {
-			type: 'string',
-			label: 'Record Id',
-		},
-		file_name: {
-			type: 'string',
-			label: 'File Name (Optional)',
-			optional: true,
-		},
-	},
-	methods: {
-		async downloadPDF(entity, id, file_name){
-		  const file = await require("@pipedreamhq/platform").axios(this, {
-		    url: `https://quickbooks.api.intuit.com/v3/company/${this.quickbooks.$auth.company_id}/${entity.toLowerCase()}/${id}/pdf`,
-		    headers: {
-		      Authorization: `Bearer ${this.quickbooks.$auth.oauth_access_token}`,
-		      'accept': 'application/pdf',
-		    },
-		    responseType: 'arraybuffer',
-		  })
+  name: "Download PDF",
+  description: "Download an invoice, bill, purchase order, etc. as a PDF and save it in the temporary file system for use in a later step.",
+  key: "download_pdf",
+  version: "0.2.3",
+  type: "action",
+  props: {
+    quickbooks,
+    entity: {
+      type: "string",
+      label: "Document Type",
+      description: null,
+      options: [
+        "CreditMemo",
+        "Estimate",
+        "Invoice",
+        "Payment",
+        "PurchaseOrder",
+        "RefundReceipt",
+        "SalesReceipt",
+      ],
+    },
+    id: {
+      type: "string",
+      label: "Record ID",
+      description: null,
+    },
+    fileName: {
+      type: "string",
+      label: "File Name (Optional)",
+      description: "If no file name is provided, the PDF will be named using the record ID, e.g. '22743.pdf'",
+      optional: true,
+    },
+  },
+  methods: {
+    async downloadPDF(entity, id, fileName) {
+      const file = await require("@pipedreamhq/platform").axios(this, {
+        url: `https://quickbooks.api.intuit.com/v3/company/${this.quickbooks.$auth.company_id}/${entity.toLowerCase()}/${id}/pdf`,
+        headers: {
+          "Authorization": `Bearer ${this.quickbooks.$auth.oauth_access_token}`,
+          "accept": "application/pdf",
+        },
+        responseType: "arraybuffer",
+      });
 
-		  const file_path = '/tmp/' + file_name
-		  fs.writeFileSync(file_path, file)
+      const filePath = "/tmp/" + fileName;
+      fs.writeFileSync(filePath, file);
+      return filePath;
+    },
+  },
+  async run({ $ }) {
+    const fileName = this.fileName || this.id;
+    const fileNameWithExtension = fileName.endsWith(".pdf")
+      ? fileName
+      : fileName + ".pdf";
 
-		  return file_path
-		}
-	},
-	async run({ $ }){
-		const file_name = this.file_name || this.id
-		const file_name_with_extension = file_name.endsWith('.pdf') ? file_name : file_name + '.pdf' 
-
-		const file_path = await this.downloadPDF(this.entity, this.id, file_name_with_extension)
-		$.export('file_path', file_path)
-		$.export('file_name', file_name_with_extension)
-	}
-}
+    const filePath = await this.downloadPDF(this.entity, this.id, fileNameWithExtension);
+    $.export("file_path", filePath);
+    $.export("file_name", fileNameWithExtension);
+  },
+};
