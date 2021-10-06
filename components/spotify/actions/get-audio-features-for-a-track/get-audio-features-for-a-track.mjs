32fed37296c3e2a9cbf99467248a81877cb67252
Cassiano Vellames <cassiano.v@turing.com>
2021-10-06 16:16:23 -0300

    Get audio features for a track

---
+++ components/spotify/actions/get-audio-features-for-a-track/get-audio-features-for-a-track.mjs
@@ -0,0 +1,30 @@
+import lodash from "lodash";
+import spotify from "../../spotify.app.mjs";
+
+export default {
+  name: "Get Audio Features for a Track",
+  description: "Get audio feature information for a single track identified by its unique Spotify ID.",
+  key: "spotify-get-audio-features-for-a-track",
+  version: "0.0.2",
+  type: "action",
+  props: {
+    spotify,
+    id: {
+      type: "string",
+      label: "Id",
+      description: "The Spotify ID for the track. For example: `4iV5W9uYEdYUVa79Axb7Rh`.",
+    },
+  },
+  async run() {
+    const res = await this.spotify._makeRequest(
+      "GET",
+      `/audio-features/${this.id}`,
+      null,
+    );
+
+    return {
+      data: lodash.get(res, "data"),
+      headers: lodash.get(res, "headers"),
+    };
+  },
+};
