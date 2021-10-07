631abb3078a4787abadc7aaf7148045e653b1ce0
Cassiano Vellames <cassiano.v@turing.com>
2021-10-07 15:04:09 -0300

    Description

--- components/spotify/actions/save-track/save-track.mjs
+++ components/spotify/actions/save-track/save-track.mjs
@@ -3,7 +3,7 @@ import spotify from "../../spotify.app.mjs";
 
 export default {
   name: "Save tracks for user",
-  description: "Save one or more tracks to the current user’s ‘Your Music’ library.",
+  description: "Save one or more tracks to the current user’s ‘Your Music’ library. [See the docs here](https://developer.spotify.com/documentation/web-api/reference/#endpoint-save-tracks-user)",
   key: "spotify-save-track",
   version: "0.0.6",
   type: "action",
@@ -11,8 +11,8 @@ export default {
     spotify,
     ids: {
       type: "string[]",
-      label: "Ids",
-      description: "A comma-separated list of the Spotify IDs. For example: `ids=4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M`. Maximum: 50 IDs.",
+      label: "IDs",
+      description: "A comma-separated list of the Spotify IDs. For example: `4iV5W9uYEdYUVa79Axb7Rh,1301WleyT98MSxVHPZCA6M`. Maximum: 50 IDs.",
     },
   },
   async run() {
