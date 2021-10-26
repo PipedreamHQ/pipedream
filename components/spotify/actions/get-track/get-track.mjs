f1019901f7216627df7c582586e5d81be3d6cbbd
Danny Roosevelt <danny@pipedream.com>
2021-10-26 09:31:12 -0700

    Update get-track.mjs
    using res.name to surface track name in $summary

--- components/spotify/actions/get-track/get-track.mjs
+++ components/spotify/actions/get-track/get-track.mjs
@@ -38,7 +38,7 @@ export default {
       },
     }));
 
-    $.export("$summary", `🎉 Successfully fetched info for the track, "${get(trackId, "label", trackId)}"`);
+    $.export("$summary", `🎉 Successfully fetched info for the track, "${res.name}"`);
 
     return res;
   },
