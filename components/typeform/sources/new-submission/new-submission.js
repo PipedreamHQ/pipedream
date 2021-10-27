1802991ffb6ae5ab4bfb0f14d97b7a3d92ebd376
js07 <19861096+js07@users.noreply.github.com>
2021-10-27 13:07:56 -0400

    Revert "Merge branch 'typeform-actions' of github.com:PipedreamHQ/pipedream"
    This reverts commit c39ae674a219b99956ffb18b84242f1c122b7184, reversing
    changes made to 192e8d607b056af95a697861d6658ab2c557b079.

--- components/typeform/sources/new-submission/new-submission.js
+++ components/typeform/sources/new-submission/new-submission.js
@@ -1,93 +1,73 @@
-import typeform from "../../typeform.app.mjs";
-import { uuid } from "uuidv4";
-import { DateTime } from "luxon";
+const typeform = require('../../typeform.app.js');
+const {uuid} = require('uuidv4');
+const {DateTime} = require('luxon');
 
 function parseIsoDate(isoDate) {
   const dt = DateTime.fromISO(isoDate);
   return {
     isoDate,
-    date_time: dt.toFormat("yyyy-mm-dd hh:mm:ss a"),
-    date: dt.toFormat("yyyy-mm-dd"),
-    time: dt.toFormat("hh:mm:ss a"),
+    date_time: dt.toFormat('yyyy-mm-dd hh:mm:ss a'),
+    date: dt.toFormat('yyyy-mm-dd'),
+    time: dt.toFormat('hh:mm:ss a'),
     timezone: dt.zoneName,
     epoch: dt.toMillis(),
   };
 }
 
-export default {
-  key: "typeform-new-submission",
-  name: "New Submission",
-  version: "0.0.4",
-  type: "source",
-  description: "Emit new submission",
+module.exports = {
+  key: 'typeform-new-submission',
+  name: 'New Submission',
+  version: '0.0.4',
   props: {
     typeform,
-    formId: {
-      propDefinition: [
-        typeform,
-        "formId",
-      ],
-    },
-    http: "$.interface.http",
-    db: "$.service.db",
+    formId: {propDefinition: [typeform, 'formId']},
+    http: '$.interface.http',
+    db: '$.service.db',
   },
   methods: {
     generateSecret() {
-      return "" + Math.random();
+      return '' + Math.random();
     },
   },
   hooks: {
     async activate() {
       const secret = this.generateSecret();
-      this.db.set("secret", secret);
-      let tag = this.db.get("tag");
+      this.db.set('secret', secret);
+      let tag = this.db.get('tag');
       if (!tag) {
         tag = uuid();
-        this.db.set("tag", tag);
+        this.db.set('tag', tag);
       }
-      try {
-        return await this.typeform.createHook({
-          endpoint: this.http.endpoint,
-          formId: this.formId,
-          tag,
-          secret,
-        });
-
-      } catch (error) {
-        throw new Error(error);
-      }
+      return await this.typeform.createHook({
+        endpoint: this.http.endpoint,
+        formId: this.formId,
+        tag,
+        secret,
+      });
     },
     async deactivate() {
-      try {
-        return await this.typeform.deleteHook({
-          formId: this.formId,
-          tag: this.db.get("tag"),
-        });
-
-      } catch (error) {
-        throw new Error(error);
-      }
+      return await this.typeform.deleteHook({
+        formId: this.formId,
+        tag: this.db.get('tag'),
+      });
     },
   },
   async run(event) {
-    const {
-      body,
-      headers,
-    } = event;
+    const {body, headers} = event;
 
-    if (headers["Typeform-Signature"]) {
-      const crypto = require("crypto");
-      const algo = "sha256";
-      const hmac = crypto.createHmac(algo, this.db.get("secret"));
+    if (headers['Typeform-Signature']) {
+      const crypto = require('crypto');
+      const algo = 'sha256';
+      const hmac = crypto.createHmac(algo, this.db.get('secret'));
       hmac.update(body);
       if (
-        headers["Typeform-Signature"] !== `${algo}=${hmac.digest("base64")}`
+        headers['Typeform-Signature'] !== `${algo}=${hmac.digest('base64')}`
       ) {
-        throw new Error("signature mismatch");
+        throw new Error('signature mismatch');
       }
     }
 
-    let formResponseString = "";
+    let form_response_string = ``;
     const data = Object.assign({}, body.form_response);
     data.form_response_parsed = {};
     for (let i = 0; i < body.form_response.answers.length; i++) {
@@ -108,9 +88,9 @@ export default {
         parsedAnswer = value;
       }
       data.form_response_parsed[field.title] = parsedAnswer;
-      formResponseString += `### ${field.title}\n${parsedAnswer}\n`;
+      form_response_string += `### ${field.title}\n${parsedAnswer}\n`;
     }
-    data.form_response_string = formResponseString;
+    data.form_response_string = form_response_string;
     data.raw_webhook_event = body;
     if (data.landed_at) data.landed_at = parseIsoDate(data.landed_at);
     if (data.submitted_at) data.submitted_at = parseIsoDate(data.submitted_at);
