5129abff11dfe499009971fb6aea8c4d943a15d0
Cassiano Vellames <cassiano.v@turing.com>
2021-11-03 17:16:27 -0300

    Create meeting WIP

--- components/zoom_admin/zoom_admin.app.mjs
+++ components/zoom_admin/zoom_admin.app.mjs
@@ -1,10 +1,17 @@
-const axios = require("axios");
-const sortBy = require("lodash/sortBy");
+/* eslint-disable camelcase */
+import axios from "axios";
+import sortBy from "lodash/sortBy.js";
 
-module.exports = {
+export default {
   type: "app",
   app: "zoom_admin",
   propDefinitions: {
+    locationId: {
+      type: "string",
+      label: "LocationId",
+      description: "Location ID of the lowest level location in the (location hierarchy)[https://support.zoom.us/hc/en-us/articles/115000342983-Zoom-Rooms-Location-Hierarchy] where the Zoom Room is to be added. For instance if the structure of the location hierarchy is set up as “country, states, city, campus, building, floor”, a room can only be added under the floor level location.",
+      optional: true,
+    },
     webinars: {
       type: "string[]",
       label: "Webinars",
@@ -12,7 +19,10 @@ module.exports = {
       description:
         "Webinars you want to watch for new events. **Leave blank to watch all webinars**.",
       async options({ nextPageToken }) {
-        const { webinars, next_page_token } = await this.listWebinars({
+        const {
+          webinars,
+          next_page_token,
+        } = await this.listWebinars({
           nextPageToken,
         });
         if (!webinars.length) {
@@ -22,7 +32,9 @@ module.exports = {
           label: w.topic,
           value: w.id,
         }));
-        const options = sortBy(rawOptions, ["label"]);
+        const options = sortBy(rawOptions, [
+          "label",
+        ]);
 
         return {
           options,
@@ -35,25 +47,41 @@ module.exports = {
   },
   methods: {
     _apiUrl() {
-      return `https://api.zoom.us/v2`;
+      return "https://api.zoom.us/v2";
     },
-    _accessToken() {
-      return this.$auth.oauth_access_token;
+    _getHeaders() {
+      return {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        "user-agent": "@PipedreamHQ/pipedream v0.1",
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      return {
+        ...opts,
+        url: this._apiUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
     },
     async _makeRequest(opts) {
       if (!opts.headers) opts.headers = {};
       opts.headers["Accept"] = "application/json";
       opts.headers["Content-Type"] = "application/json";
-      opts.headers["Authorization"] = `Bearer ${this._accessToken()}`;
+      opts.headers["Authorization"] = `Bearer ${this.$auth.oauth_access_token}`;
       opts.headers["user-agent"] = "@PipedreamHQ/pipedream v0.1";
       const { path } = opts;
       delete opts.path;
+      // eslint-disable-next-line multiline-ternary
       opts.url = `${this._apiUrl()}${path[0] === "/" ? "" : "/"}${path}`;
       return await axios(opts);
     },
-    async listWebinars({ pageSize, nextPageToken }) {
+    async listWebinars({
+      pageSize,
+      nextPageToken,
+    }) {
       const { data } = await this._makeRequest({
-        path: `/users/me/webinars`,
+        path: "/users/me/webinars",
         params: {
           page_size: pageSize || 300,
           next_page_token: nextPageToken,
---
+++ components/zoom_admin/zoom_days_of_the_week.mjs
@@ -0,0 +1,30 @@
+export default [
+  {
+    label: "Sunday",
+    value: 1,
+  },
+  {
+    label: "Monday",
+    value: 2,
+  },
+  {
+    label: "Tuesday",
+    value: 3,
+  },
+  {
+    label: "Wednesday",
+    value: 4,
+  },
+  {
+    label: "Thursday",
+    value: 5,
+  },
+  {
+    label: "Friday",
+    value: 6,
+  },
+  {
+    label: "Saturday",
+    value: 7,
+  },
+];
