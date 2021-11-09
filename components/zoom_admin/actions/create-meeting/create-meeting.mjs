fe14fb1cfaefccd42d2140dac20da540caea61ae
Cassiano Vellames <cassiano.v@turing.com>
2021-11-09 15:26:42 -0300

    testing lint

--- components/zoom_admin/actions/create-meeting/create-meeting.mjs
+++ components/zoom_admin/actions/create-meeting/create-meeting.mjs
@@ -5,7 +5,7 @@ import isArray from "lodash/isArray.js";
 import { axios } from "@pipedream/platform";
 
 export default {
-  name: "Create a meeting",
+  name: 'Create a meeting',
   description: "Create a new room in zoom. [See the docs here](https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingcreate)",
   key: "zoom_admin-action-create-a-meeting",
   version: "0.0.22",
