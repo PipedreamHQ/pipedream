565f22a5ab9be5d49204f2092d0cd867d8f9ccb1
Cassiano Vellames <cassiano.v@turing.com>
2021-11-10 15:42:34 -0300

    add webinar panelist

---
+++ components/zoom_admin/actions/add-webinar-panelist/add-webinar-panelist.mjs
@@ -0,0 +1,51 @@
+import zoomAdmin from "../../zoom_admin.app.mjs";
+import get from "lodash/get.js";
+import { axios } from "@pipedream/platform";
+
+export default {
+  name: "Add webinar panelist",
+  description: "Register a panelist for a webinar. [See the docs here](https://marketplace.zoom.us/docs/api-reference/zoom-api/webinars/webinarpanelistcreate)",
+  key: "zoom_admin-action-add-webinar-panelist",
+  version: "0.0.2",
+  type: "action",
+  props: {
+    zoomAdmin,
+    webinar: {
+      propDefinition: [
+        zoomAdmin,
+        "webinars",
+      ],
+      type: "string",
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The panelist's full name.",
+      optional: false,
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Panelist's email",
+      optional: false,
+    },
+  },
+  async run ({ $ }) {
+    const res = await axios($, this.zoomAdmin._getAxiosParams({
+      method: "POST",
+      path: `/webinars/${get(this.webinar, "value", this.webinar)}/panelists`,
+      data: {
+        panelists: [
+          {
+            name: this.name,
+            email: this.email,
+          },
+        ],
+      },
+    }));
+
+    $.export("$summary", `"${this.name}" was successfully added as a panelist to "${get(this.webinar, "label", this.webinar)}" webinar.`);
+
+    return res;
+  },
+};
