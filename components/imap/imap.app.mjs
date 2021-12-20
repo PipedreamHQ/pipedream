11b7bf40117886526d86f2e5dfca9d1599b21725
js07 <19861096+js07@users.noreply.github.com>
2021-12-20 13:10:03 -0500

    Change IMAP New Email source to strictly polling
    Remove code for persistent connection and listening for new mail
    
    Refactor IMAP source
    
    * Refactor async code for consistency & maintainability
    
    * Move methods to IMAP app file
    
    Fix filtering selectable mailbox in IMAP source
    
    Add timer prop to IMAP source

--- components/imap/imap.app.mjs
+++ components/imap/imap.app.mjs
@@ -1,11 +1,160 @@
+import Imap from "imap";
+import { simpleParser } from "mailparser";
+import util from "util";
+import Stream from "stream";
+
 export default {
   type: "app",
   app: "imap",
-  propDefinitions: {},
+  propDefinitions: {
+    mailbox: {
+      type: "string",
+      label: "Mailbox",
+      description: "Name of the mailbox to watch for new emails",
+      async options({
+        user, password, host, port,
+      }) {
+        const connection = await this.getConnection({
+          user,
+          password,
+          host,
+          port,
+        });
+        try {
+          const mailboxes = await this.getMailboxes(connection);
+          return mailboxes;
+        } catch (err) {
+          throw new Error(err);
+        } finally {
+          await this.closeConnection(connection);
+        }
+      },
+    },
+  },
   methods: {
     // this.$auth contains connected account data
     authKeys() {
       console.log(Object.keys(this.$auth));
+    },
+    getConnection(opts) {
+      // const { host, user, password, port, } = this.$auth;
+      const {
+        user,
+        password,
+        host,
+        port,
+      } = opts;
+      return new Promise((resolve, reject) => {
+        const connection = new Imap({
+          user: user,
+          password: password,
+          host: host,
+          port: port,
+          tls: true,
+          tlsOptions: {
+            servername: host,
+          },
+        });
+        connection.on("error", reject);
+        connection.once("ready", () => {
+          resolve(connection);
+        });
+        connection.connect();
+      });
+    },
+    async closeConnection(connection) {
+      const connectionClosed = new Promise((resolve) => {
+        connection.once("end", resolve);
+      });
+      connection.end();
+      await connectionClosed;
+    },
+    async getMailboxes(connection) {
+      const getBoxes = util.promisify(connection.getBoxes.bind(connection));
+      const boxes = await getBoxes();
+      const getChildren = (boxName, box) => {
+        if (!box.children) {
+          return [];
+        }
+        return Object.keys(box.children).reduce((all, childName) => {
+          const childBox = box.children[childName];
+          const childBoxName = `${boxName}${box.delimiter}${childName}`;
+          const isSelectable = !childBox.attribs.find(
+            (attrib) => attrib.toLowerCase() === "\\noselect",
+          );
+          if (isSelectable) {
+            all.push(childBoxName);
+          }
+          if (childBox.children) {
+            all = all.concat(getChildren(childBoxName, childBox));
+          }
+          return all;
+        }, []);
+      };
+      return getChildren("", {
+        delimiter: "",
+        children: boxes,
+      });
+    },
+    async openMailbox(connection, mailbox) {
+      const openBox = util.promisify(connection.openBox.bind(connection));
+      return await openBox(mailbox, true);
+    },
+    fetchMessages(connection, {
+      startUid, startSeqno,
+    }) {
+      if (!startUid && !startSeqno) {
+        throw new Error("One of startUid, startSeqno is required to fetch messages");
+      }
+
+      const options = {
+        bodies: "",
+      };
+      let f;
+      if (startUid) {
+        f = connection.fetch(`${startUid}:*`, options);
+      } else {
+        f = connection.seq.fetch(`${startSeqno}:*`, options);
+      }
+
+      const messageStream = new Stream.Readable({
+        objectMode: true,
+        read() {},
+      });
+      let promises = [];
+
+      f.on("message", (msg) => {
+        const message = {};
+        let mailPromise;
+        msg.on("body", (stream) => {
+          mailPromise = simpleParser(stream);
+        });
+        msg.once("attributes", (attrs) => {
+          message.attributes = attrs;
+        });
+        msg.once("end", () => {
+          if (!mailPromise) {
+            return messageStream.push(message);
+          }
+          const messagePromise = mailPromise.then((mail) => {
+            message.mail = mail;
+          }).then(() => {
+            messageStream.push(message);
+          });
+          promises.push(messagePromise);
+        });
+      });
+      f.once("error", (err) => {
+        messageStream.destroy(err);
+      });
+      f.once("end", () => {
+        // Wait until all mail promises have been resolved before ending stream
+        Promise.all(promises).then(() => {
+          messageStream.push(null);
+        });
+      });
+
+      return messageStream;
     },
   },
 };
--- components/imap/sources/new-email/new-email.mjs
+++ components/imap/sources/new-email/new-email.mjs
@@ -1,26 +1,8 @@
-import Imap from "imap";
-import { simpleParser } from "mailparser";
-import util from "util";
-import { once  } from "events";
-import Stream from "stream";
-
-/**
- * The number of seconds the source will run before timing out.
- * @constant {number}
- * @default 30
- */
-const TIMEOUT = 30;
-/**
- * The number of seconds to create an IMAP connection and listen for events.
- * @constant {number}
- * @default 22
- */
-const LISTEN_FOR = TIMEOUT - 8;
+import imap from "../../imap.app.mjs";
 
 /**
  * Creates an IMAP server connection and emits emails added to a mailbox after the last emitted
- * email. Watches the mailbox for new emails for `LISTEN_FOR` seconds before closing the IMAP
- * connection.
+ * email.
  */
 export default {
   key: "imap-new-email",
@@ -30,7 +12,17 @@ export default {
   type: "source",
   dedupe: "unique",
   props: {
+    imap,
     db: "$.service.db",
+    timer: {
+      label: "Polling interval",
+      description:
+        "Pipedream will poll the IMAP server for new emails at this interval",
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15, // by default, run every 15 minutes
+      },
+    },
     host: {
       type: "string",
       label: "Host",
@@ -62,31 +54,18 @@ export default {
       default: 993,
     },
     mailbox: {
-      type: "string",
-      label: "Mailbox",
-      description: "Name of the mailbox to watch for new emails",
-      async options() {
-        return new Promise((resolve, reject) => {
-          let mailboxes = [];
-          const imap = this.imap();
-          imap.on("error", reject);
-          imap.on("end", () => {
-            resolve(mailboxes);
-          });
-          imap.connect();
-          once(imap, "ready")
-            .then(() => {
-              return this.getMailboxes(imap);
-            })
-            .then((result) => {
-              mailboxes = result;
-            })
-            .catch(reject)
-            .finally(() => {
-              return this.closeConnection(imap);
-            });
-        });
-      },
+      propDefinition: [
+        imap,
+        "mailbox",
+        ({
+          host, user, password, port,
+        }) => ({
+          host,
+          user,
+          password,
+          port,
+        }),
+      ],
     },
   },
   methods: {
@@ -105,54 +84,6 @@ export default {
     _handleUidValidityChange(uidValidity) {
       this._setUidValidity(uidValidity);
       this._setLastUid(null);
-    },
-    imap() {
-      const imap = new Imap({
-        user: this.user,
-        password: this.password,
-        host: this.host,
-        port: this.port,
-        tls: true,
-        tlsOptions: {
-          servername: this.host,
-        },
-      });
-      return imap;
-    },
-    async closeConnection(imap) {
-      const connectionClosed = new Promise((resolve) => {
-        imap.on("end", resolve);
-      });
-      imap.end();
-      await connectionClosed;
-    },
-    async getMailboxes(imap) {
-      const getBoxes = util.promisify(imap.getBoxes.bind(imap));
-      const boxes = await getBoxes();
-      const getChildren = (boxName, box) => {
-        if (!box.children) {
-          return [];
-        }
-        return Object.keys(box.children).reduce((all, childName) => {
-          const childBox = box.children[childName];
-          const childBoxName = `${boxName}${box.delimiter}${childName}`;
-          if (!childBox.attribs.includes("\\NOSELECT")) {
-            all.push(childBoxName);
-          }
-          if (childBox.children) {
-            all = all.concat(getChildren(childBoxName, childBox));
-          }
-          return all;
-        }, []);
-      };
-      return getChildren("", {
-        delimiter: "",
-        children: boxes,
-      });
-    },
-    async openMailbox(imap) {
-      const openBox = util.promisify(imap.openBox.bind(imap));
-      return await openBox(this.mailbox, true);
     },
     hasNewMessages(box) {
       const lastUid = this._getLastUid();
@@ -177,128 +108,47 @@ export default {
         this.processMessage(message);
       }
     },
-    fetchMessages(imap, {
-      startUid, startSeqno,
-    }) {
-      if (!startUid && !startSeqno) {
-        throw new Error("One of startUid, startSeqno is required to fetch messages");
-      }
-
-      const options = {
-        bodies: "",
-      };
-      let f;
-      if (startUid) {
-        f = imap.fetch(`${startUid}:*`, options);
-      } else {
-        f = imap.seq.fetch(`${startSeqno}:*`, options);
-      }
-
-      const readableStream = new Stream.Readable({
-        objectMode: true,
-        read() {},
-      });
-      let promises = [];
-
-      f.on("message", (msg) => {
-        const message = {};
-        let mailPromise;
-        msg.on("body", (stream) => {
-          mailPromise = simpleParser(stream);
-          promises.push(mailPromise);
-        });
-        msg.once("attributes", (attrs) => {
-          message.attributes = attrs;
-        });
-        msg.once("end", async () => {
-          message.mail = mailPromise && await mailPromise;
-          readableStream.push(message);
-        });
-      });
-      f.once("error", (err) => {
-        readableStream.destroy(err);
-      });
-      f.once("end", async () => {
-        // Wait until all mail promises have been resolved before ending stream
-        await Promise.all(promises);
-        readableStream.push(null);
-      });
-
-      return readableStream;
-    },
   },
   async run() {
-    await new Promise((resolve, reject) => {
-      (async () => {
-        // Configure imap connection
-        const imap = this.imap();
-
-        imap.once("error", reject);
-        imap.once("end", () => {
-          console.log("Connection ended");
-          resolve();
-        });
-
-        imap.on("uidvalidity", (uidvalidity) => {
-          this._handleUidValidityChange(uidvalidity);
-        });
-
-        // Close connection 8s before source TIMEOUT constant
-        setTimeout(() => {
-          this.closeConnection(imap);
-        }, 1000 * (LISTEN_FOR));
-
-        // Connect to imap server
-        imap.connect();
-
-        let box;
-        try {
-          await once(imap, "ready");
-          console.log("Connection started");
-
-          // Open mailbox
-          box = await this.openMailbox(imap);
-        } catch (err) {
-          reject(err);
-        }
-
-        const uidValidity = this._getUidValidity();
-        if (uidValidity !== box.uidvalidity) {
-          this._handleUidValidityChange(box.uidvalidity);
-        }
-
-        // Listen for new mail
-        imap.on("mail", (data) => {
-          // eslint-disable-next-line multiline-ternary
-          console.log(`Event: ${data} new message${data === 1 ? "" : "s"} in open mailbox`);
-
-          const lastUid = this._getLastUid();
-          // Fetch messages after lastUid if it exists, or most recent message otherwise
-          const f = this.fetchMessages(imap, {
-            startUid: lastUid && lastUid + 1,
-            startSeqno: box.messages.total,
-          });
-          this.processMessageStream(f);
-        });
-
-        if (!this.hasNewMessages(box)) {
-          console.log("No new messages since last run");
-          return;
-        }
-
-        const lastUid = this._getLastUid();
-
-        // Fetch messages after lastUid if it exists, or most recent message otherwise
-        const f = this.fetchMessages(imap, {
-          startUid: lastUid && lastUid + 1,
-          startSeqno: box.messages.total,
-        });
-        try {
-          await this.processMessageStream(f);
-        } catch (err) {
-          reject(err);
-        }
-      })();
+    const {
+      host,
+      user,
+      password,
+      port,
+      mailbox,
+    } = this;
+    const connection = await this.imap.getConnection({
+      host,
+      user,
+      password,
+      port,
     });
+
+    try {
+      const box = await this.imap.openMailbox(connection, mailbox);
+
+      const uidValidity = this._getUidValidity();
+      if (uidValidity !== box.uidvalidity) {
+        this._handleUidValidityChange(box.uidvalidity);
+      }
+
+      if (!this.hasNewMessages(box)) {
+        console.log("No new messages since last run");
+        return;
+      }
+
+      const lastUid = this._getLastUid();
+
+      // Fetch messages after lastUid if it exists, or most recent message otherwise
+      const f = this.imap.fetchMessages(connection, {
+        startUid: lastUid && lastUid + 1,
+        startSeqno: box.messages.total,
+      });
+      await this.processMessageStream(f);
+    } catch (err) {
+      throw new Error(err);
+    } finally {
+      await this.imap.closeConnection(connection);
+    }
   },
 };
