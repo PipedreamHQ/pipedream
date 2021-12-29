18f9263110e5d77dc5267505e86da565e3b3d84b
Danny Roosevelt <danny@pipedream.com>
2021-12-29 14:52:23 -0800

    Adding HubSpot doc

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -170,6 +170,7 @@ module.exports = {
         children: [
           "/apps/all-apps/",
           "/apps/discord/",
+          "/apps/hubspot/",
           "/apps/servicenow/",
           "/apps/slack/",
           "/apps/strava/",
