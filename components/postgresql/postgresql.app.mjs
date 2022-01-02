06ef676e470b75c1f8ae7d5529574bfc69e899f4
michelle0927 <michellelbergero@hotmail.com>
2022-01-02 18:14:57 -0500

    eslint updates

--- components/postgresql/postgresql.app.mjs
+++ components/postgresql/postgresql.app.mjs
@@ -1,4 +1,4 @@
-import pg from 'pg';
+import pg from "pg";
 
 export default {
   type: "app",
@@ -20,12 +20,12 @@ export default {
       });
       await client
         .connect()
-        .then(() => console.log('Successfully connected to database'))
-        .catch(err => console.error('Connection error', err.stack))
+        .then(() => console.log("Successfully connected to database"))
+        .catch((err) => console.error("Connection error", err.stack));
       return client;
     },
     async endClient(client) {
-      client.end()
+      client.end();
     },
     async getTables(client) {
       const { rows } = await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
--- components/postgresql/sources/new-table/new-table.mjs
+++ components/postgresql/sources/new-table/new-table.mjs
@@ -1,52 +1,55 @@
 import postgresql from "../../postgresql.app.mjs";
 
 export default {
-	name: "New Table",
-	key: "postgresql-new-table",
-	description: "Emits new event when you add a new table",
-	version: "0.0.1",
-	props: {
-		postgresql,
-		db: "$.service.db",
+  name: "New Table",
+  key: "postgresql-new-table",
+  description: "Emit new event when you add a new table",
+  version: "0.0.1",
+  type: "source",
+  props: {
+    postgresql,
+    db: "$.service.db",
     timer: {
       type: "$.interface.timer",
       default: {
         intervalSeconds: 60 * 15,
       },
+      label: "Polling Interval",
+      description: "Pipedream will poll the API on this schedule",
     },
-	},
-	methods: {
-		_getPreviousTables() {
-			return this.db.get('previousTables');
-		},
-		_setPreviousTables(previousTables) {
-			this.db.set('previousTables', previousTables);
-		},
-		generateMeta(table) {
-			return {
-				id: table,
-				summary: `New table: ${table}`,
-				ts: Date.now(),
-			}
-		},
-	},
-	async run() {
-		const client = await this.postgresql.getClient();
-		const previousTables = this._getPreviousTables() || [];
+  },
+  methods: {
+    _getPreviousTables() {
+      return this.db.get("previousTables");
+    },
+    _setPreviousTables(previousTables) {
+      this.db.set("previousTables", previousTables);
+    },
+    generateMeta(table) {
+      return {
+        id: table,
+        summary: `New table: ${table}`,
+        ts: Date.now(),
+      };
+    },
+  },
+  async run() {
+    const client = await this.postgresql.getClient();
+    const previousTables = this._getPreviousTables() || [];
 
-		const results = await this.postgresql.getTables(client);
-		const tables = [];
-		for (const result of results) {
-			tables.push(result.table_name);
-		}
-		
-		const newTables = tables.filter(x => !previousTables.includes(x));
-		for (const table of newTables) {
-			const meta = this.generateMeta(table);
-			this.$emit(table, meta);
-		}
+    const results = await this.postgresql.getTables(client);
+    const tables = [];
+    for (const result of results) {
+      tables.push(result.table_name);
+    }
 
-		this._setPreviousTables(tables);
-		await this.postgresql.endClient(client);
-	},
-}
+    const newTables = tables.filter((x) => !previousTables.includes(x));
+    for (const table of newTables) {
+      const meta = this.generateMeta(table);
+      this.$emit(table, meta);
+    }
+
+    this._setPreviousTables(tables);
+    await this.postgresql.endClient(client);
+  },
+};
