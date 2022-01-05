7b8d94ed0a5d644a5592318e70083a4361656d8e
Andrew Chuang <andrewjschuang@gmail.com>
2022-01-05 14:29:46 -0300

    add list docs action

--- components/coda/actions/list-docs/list-docs.mjs
+++ components/coda/actions/list-docs/list-docs.mjs
@@ -1,9 +1,105 @@
+import coda from "../../coda.app.mjs";
+
 export default {
   key: "coda_list-docs",
   name: "List Docs",
-  description: "Returns a list of Coda docs accessible by the user",
-  version: "0.0.1",
+  description: "Returns a list of Coda docs accessible by the user. These are returned in the same order as on the docs page: reverse chronological by the latest event relevant to the user (last viewed, edited, or shared).",
+  version: "0.0.13",
   type: "action",
-  props: {},
-  async run({ $ }) {},
+  props: {
+    coda,
+    isOwner: {
+      propDefinition: [
+        coda,
+        "isOwner",
+      ],
+      optional: true,
+    },
+    isPublished: {
+      propDefinition: [
+        coda,
+        "isPublished",
+      ],
+      optional: true,
+      default: false,
+    },
+    query: {
+      propDefinition: [
+        coda,
+        "query",
+      ],
+      optional: true,
+    },
+    sourceDoc: {
+      propDefinition: [
+        coda,
+        "sourceDoc",
+      ],
+      description: "Show only docs copied from the specified doc ID.",
+      optional: true,
+    },
+    isStarred: {
+      propDefinition: [
+        coda,
+        "isStarred",
+      ],
+      optional: true,
+      default: false,
+    },
+    inGallery: {
+      propDefinition: [
+        coda,
+        "inGallery",
+      ],
+      optional: true,
+      default: false,
+    },
+    workspaceId: {
+      propDefinition: [
+        coda,
+        "workspaceId",
+      ],
+      optional: true,
+    },
+    folderId: {
+      propDefinition: [
+        coda,
+        "folderId",
+      ],
+      description: "Show only docs belonging to the given folder.",
+      optional: true,
+    },
+    limit: {
+      propDefinition: [
+        coda,
+        "limit",
+      ],
+    },
+    pageToken: {
+      propDefinition: [
+        coda,
+        "pageToken",
+      ],
+      optional: true,
+    },
+  },
+  async run() {
+    var params = {
+      isOwner: this.isOwner,
+      isPublished: this.isPublished,
+      query: this.query,
+      sourceDoc: this.sourceDoc,
+      isStarred: this.isStarred,
+      inGallery: this.inGallery,
+      workspaceId: this.workspaceId,
+      folderId: this.folderId,
+      limit: this.limit,
+      pageToken: this.pageToken,
+    };
+    Object.keys(params).forEach((key) => (params[key] === null
+      || params[key] === undefined
+      || params[key] === "")
+      && delete params[key]);
+    return await this.coda.listDocs(params);
+  },
 };
--- components/coda/coda.app.mjs
+++ components/coda/coda.app.mjs
@@ -21,6 +21,49 @@ export default {
       label: "Source Doc ID",
       description: "A doc ID from which to create a copy.",
     },
+    isOwner: {
+      type: "boolean",
+      label: "Is Owner Docs",
+      description: "Show only docs owned by the user.",
+    },
+    isPublished: {
+      type: "boolean",
+      label: "Is Published Docs",
+      description: "Show only published docs.",
+    },
+    query: {
+      type: "string",
+      label: "Search Query",
+      description: "Search term used to filter down results.",
+    },
+    isStarred: {
+      type: "boolean",
+      label: "Is Starred Docs",
+      description: "If true, returns docs that are starred. If false, returns docs that are not starred.",
+    },
+    inGallery: {
+      type: "boolean",
+      label: "In Gallery Docs",
+      description: "Show only docs visible within the gallery.",
+    },
+    workspaceId: {
+      type: "string",
+      label: "Workspace ID",
+      description: "Show only docs belonging to the given workspace.",
+    },
+    limit: {
+      type: "integer",
+      label: "Limit",
+      description: "Maximum number of results to return in this query.",
+      default: 25,
+      min: 1,
+      max: 50,
+    },
+    pageToken: {
+      type: "string",
+      label: "Page Token",
+      description: "An opaque token used to fetch the next page of results.",
+    },
   },
   methods: {
     // this.$auth contains connected account data
@@ -51,6 +94,34 @@ export default {
         },
       };
       return (await axios(config)).data.id;
+    },
+    /**
+     * List Coda docs according to parameters
+     *
+     * @param {object} [params] - Optional Query Parameters
+     * @param {boolean} params.isOwner
+     * @param {boolean} params.isPublished
+     * @param {string} params.query
+     * @param {string} params.sourceDoc
+     * @param {boolean} params.isStarred
+     * @param {boolean} params.inGallery
+     * @param {string} params.workspaceId
+     * @param {string} params.folderId
+     * @param {int} params.limit
+     * @param {string} params.pageToken
+     *
+     * @returns {object[]} Array of listed Docs
+     */
+    async listDocs(params = {}) {
+      const config = {
+        method: "get",
+        url: "https://coda.io/apis/v1/docs",
+        headers: {
+          Authorization: `Bearer ${this.$auth.api_token}`,
+        },
+        params,
+      };
+      return (await axios(config)).data;
     },
   },
 };
