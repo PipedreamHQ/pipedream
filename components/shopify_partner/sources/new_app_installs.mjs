2f1b817b758b5e2305bc72951078c771cd604021
Dylan Pierce <me@dylanjpierce.com>
2022-01-11 14:49:20 -0500

    Shopify App Partner integration (#2022)
    * PoC Shopify App Partner integration
    
    * Adding pagination support and basic cursor tracking at the db level
    
    * Linting with ESLint
    
    * Adding summaries
    
    * Adding PR review comment changes
    
    * Adding PR review comment changes
    
    * Using propDefinitions
    
    * Moving up appId as well
    
    * Adding the generic events name instead of installs specifically
    
    * Adding example images to create an API key
    
    * Fixing spelling

---
+++ components/shopify_partner/sources/new_app_installs.mjs
@@ -0,0 +1,65 @@
+import common from "../common.mjs";
+import shopify from "../shopify_partner.app.mjs";
+import getAppInstalls from "../queries/getAppInstalls.mjs";
+
+export default {
+  key: "shopify_partner-new-app-installs",
+  name: "New App Installs",
+  type: "source",
+  version: "0.0.1",
+  description: "Emit new events when new shops install your app.",
+  ...common,
+  props: {
+    ...common.props,
+    db: "$.service.db",
+    appId: {
+      propDefinition: [
+        shopify,
+        "appId",
+      ],
+    },
+    occurredAtMin: {
+      propDefinition: [
+        shopify,
+        "occurredAtMin",
+      ],
+    },
+    occurredAtMax: {
+      propDefinition: [
+        shopify,
+        "occurredAtMax",
+      ],
+    },
+  },
+  async run() {
+    const {
+      appId,
+      occurredAtMin,
+      occurredAtMax,
+      db,
+    } = this;
+
+    const variables = {
+      appId,
+      ...(occurredAtMin || {}),
+      ...(occurredAtMax || {}),
+    };
+
+    await this.shopify.query({
+      db,
+      key: this.key,
+      query: getAppInstalls,
+      variables,
+      handleEmit: (data) => {
+        data.app.events.edges.map(({ node: { ...event } }) => {
+          this.$emit(event, {
+            id: event.occurredAt,
+            summary: `${event.shop.name} (${event.shop.myshopifyDomain}) installed ${event.app.name}`,
+          });
+        });
+      },
+      cursorPath: "app.events[0].edges[0].cursor",
+      hasNextPagePath: "app.events.pageInfo.hasNextPage",
+    });
+  },
+};
