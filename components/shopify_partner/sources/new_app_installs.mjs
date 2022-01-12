a1eb547c538290e6a061b1efe1b1c62ae3be680d
Dylan Pierce <me@dylanjpierce.com>
2022-01-12 10:03:42 -0500

    Incrementing semver

--- components/shopify_partner/sources/new_app_installs.mjs
+++ components/shopify_partner/sources/new_app_installs.mjs
@@ -6,7 +6,7 @@ export default {
   key: "shopify_partner-new-app-installs",
   name: "New App Installs",
   type: "source",
-  version: "0.0.1",
+  version: "0.0.2",
   description: "Emit new events when new shops install your app.",
   ...common,
   props: {
