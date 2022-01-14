c4fb78f6ac961a691e637860e5c87c1db88a8e24
Cassiano Vellames <cassiano.v@turing.com>
2022-01-14 15:55:36 -0300

    create document

--- components/mongodb/actions/create-new-document/create-new-document.mjs
+++ components/mongodb/actions/create-new-document/create-new-document.mjs
@@ -4,12 +4,49 @@ export default {
   key: "mongodb-create-new-document",
   name: "Create New Document",
   description: "Create a new document in a collection of your choice",
-  version: "0.0.9",
+  version: "0.0.65",
   type: "action",
   props: {
     mongodbApp,
+    collection: {
+      propDefinition: [
+        mongodbApp,
+        "collection",
+      ],
+    },
+    data: {
+      label: "Data",
+      type: "object",
+      description: "The object to be used in document creation",
+    },
+    parseNumbers: {
+      label: "Parse Numbers",
+      type: "boolean",
+      description: "If `true`. All number values represented by a string will be parsed to it respective type",
+      default: true,
+    },
+    parseBooleans: {
+      label: "Parse Booleans",
+      type: "boolean",
+      description: "If `true`. All boolean values represented by a string will be parsed to it respective type",
+      default: true,
+    },
+    parseDates: {
+      label: "Parse Dates",
+      type: "boolean",
+      description: "If `true`. All date values represented by a string will be parsed to it respective type",
+      default: true,
+    },
   },
-  async run() {
-    await this.mongodbApp.getConnection();
+  async run({ $ }) {
+    const document = await this.mongodbApp.createDocument(
+      this.data,
+      this.collection,
+      this.parseNumbers,
+      this.parseBooleans,
+      this.parseDates,
+    );
+    $.export("$summary", `Document ${document._id} successfully created`);
+    return document;
   },
 };
--- components/mongodb/mongodb.app.mjs
+++ components/mongodb/mongodb.app.mjs
@@ -3,20 +3,125 @@ import mongoose from "mongoose";
 export default {
   type: "app",
   app: "mongodb",
-  propDefinitions: {},
+  propDefinitions: {
+    collection: {
+      label: "Collection",
+      type: "string",
+      description: "Collection where the new document will be inserted.",
+      async options() {
+        return this.getCollections();
+      },
+    },
+  },
   methods: {
-    async getConnection() {
+    async connect() {
       const {
         username,
         password,
         database,
         hostname,
       } = this.$auth;
-      // mongodb+srv://root:root@cluster0.jcnyr.mongodb.net/test
-      const uri = `mongodb+srv://${username}:${password}@${hostname}/${database}`;
-      console.log(uri);
-      const res = await mongoose.connect(uri);
-      console.log(res);
+      const uri = `mongodb+srv://${username}:${password}@${hostname}/${database}?retryWrites=true&w=majority`;
+      await mongoose.connect(uri);
+    },
+    async createDocument(data, collection, parseNumbers, parseBooleans, parseDates) {
+      await this.connect();
+      const keys = Object.keys(data);
+      const schemaFields = {};
+      for (let i = 0; i < keys.length; i++) {
+        schemaFields[keys[i]] = {
+          type: mongoose.Schema.Types.Mixed,
+        };
+      }
+
+      const schema = new mongoose.Schema(schemaFields, {
+        strict: false,
+      });
+
+      const Model = mongoose.model(collection, schema);
+      const document = new Model(this.parseStrings(
+        data,
+        parseNumbers,
+        parseBooleans,
+        parseDates,
+      ));
+      try {
+        await document.save();
+        return document;
+      } finally {
+        mongoose.connection.close();
+      }
+    },
+    getCollections() {
+      return new Promise((resolve, reject) => {
+        this.connect()
+          .then(async () => {
+            try {
+              const collections = await mongoose.connection.db.listCollections().toArray();
+              resolve(collections.map((collection) => collection.name).sort());
+            } catch (err) {
+              reject(err);
+            } finally {
+              mongoose.connection.close();
+            }
+          })
+          .catch((err) => reject(err));
+      });
+    },
+    parseStrings(dataParam, parseNumbers, parseBooleans, parseDates) {
+      const data = {
+        ...dataParam,
+      };
+
+      const keys = Object.keys(data);
+      for (let i = 0; i < keys.length; i++) {
+        if (parseBooleans && this.isBooleanString(data[keys[i]])) {
+          data[keys[i]] = this.parseBoolean(data[keys[i]]);
+          continue;
+        }
+
+        if (parseDates && this.isDate(data[keys[i]])) {
+          data[keys[i]] = new Date(data[keys[i]]);
+          continue;
+        }
+
+        if (parseNumbers && !isNaN(data[keys[i]])) {
+          data[keys[i]] = parseFloat(data[keys[i]]);
+          continue;
+        }
+      }
+
+      return data;
+    },
+    parseBoolean(string) {
+      switch (string.toLowerCase().trim()) {
+      case "true":
+      case "1":
+      case 1:
+        return true;
+
+      case "false":
+      case "0":
+      case 0:
+        return false;
+      }
+    },
+    isBooleanString(string) {
+      switch (string.toLowerCase().trim()) {
+      case "true":
+      case "1":
+      case 1:
+      case "false":
+      case "0":
+      case 0:
+        return true;
+
+      default:
+        return false;
+      }
+    },
+    isDate(str) {
+      return !isNaN(new Date(str).getDate());
     },
   },
 };
