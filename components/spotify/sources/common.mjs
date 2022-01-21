4ead067978e1cf5bb05f1af2a22d97af0cf1949a
js07 <19861096+js07@users.noreply.github.com>
2022-01-20 21:05:08 -0500

    Fix linter errors in Spotify common module

--- components/spotify/sources/common.mjs
+++ components/spotify/sources/common.mjs
@@ -4,6 +4,8 @@ export default {
   props: {
     spotify,
     timer: {
+      label: "Polling interval",
+      description: "How often to poll the Spotify API for new event",
       type: "$.interface.timer",
       default: {
         intervalSeconds: 60 * 15,
