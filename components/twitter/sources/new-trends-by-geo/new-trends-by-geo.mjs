9b6a56324ec35df5ff97f0ec1f3d24313755a62f
michelle0927 <michellelbergero@hotmail.com>
2022-01-26 09:54:15 -0500

    updated version

--- components/twitter/sources/new-trends-by-geo/new-trends-by-geo.mjs
+++ components/twitter/sources/new-trends-by-geo/new-trends-by-geo.mjs
@@ -4,7 +4,7 @@ export default {
   key: "twitter-new-trends-by-geo",
   name: "New Trends by Geo",
   description: "Emit new event when a new topic is trending on Twitter in a specific geographic location",
-  version: "0.0.5",
+  version: "0.0.6",
   type: "source",
   props: {
     twitter,
