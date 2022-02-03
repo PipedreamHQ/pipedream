762d8ca14f530e84505742fdde82751cb0f83d2e
Danny Roosevelt <danny@pipedream.com>
2022-02-03 15:02:16 -0800

    Updating formatting based on linter errors

--- components/github/actions/create-issue/create-issue.js
+++ components/github/actions/create-issue/create-issue.js
@@ -1,5 +1,5 @@
-const github = require('../../github.app.js')
-const { Octokit } = require('@octokit/rest')
+const github = require("../../github.app.js");
+const { Octokit } = require("@octokit/rest");
 
 module.exports = {
   key: "github-create-issue",
@@ -9,24 +9,56 @@ module.exports = {
   type: "action",
   props: {
     github,
-    repoFullName: { propDefinition: [github, "repoFullName"] },
-    title: { propDefinition: [github, "issueTitle"] },
-    body: { propDefinition: [github, "issueBody"] },
-    labels: { 
-      propDefinition: [github, "labelNames", c => ({ repoFullName: c.repoFullName })],
+    repoFullName: {
+      propDefinition: [
+        github,
+        "repoFullName",
+      ],
+    },
+    title: {
+      propDefinition: [
+        github,
+        "issueTitle",
+      ],
+    },
+    body: {
+      propDefinition: [
+        github,
+        "issueBody",
+      ],
+    },
+    labels: {
+      propDefinition: [
+        github,
+        "labelNames",
+        (c) => ({
+          repoFullName: c.repoFullName,
+        }),
+      ],
       optional: true,
     },
-    milestone: { 
-      propDefinition: [github, "milestone", c => ({ repoFullName: c.repoFullName })],
-      optional: true 
+    milestone: {
+      propDefinition: [
+        github,
+        "milestone",
+        (c) => ({
+          repoFullName: c.repoFullName,
+        }),
+      ],
+      optional: true,
     },
-    assignees: { propDefinition: [github, "issueAssignees"] },
+    assignees: {
+      propDefinition: [
+        github,
+        "issueAssignees",
+      ],
+    },
   },
   async run() {
     const octokit = new Octokit({
-      auth: this.github.$auth.oauth_access_token
-    })
-    
+      auth: this.github.$auth.oauth_access_token,
+    });
+
     return (await octokit.issues.create({
       owner: this.repoFullName.split("/")[0],
       repo: this.repoFullName.split("/")[1],
@@ -35,6 +67,6 @@ module.exports = {
       labels: this.labels,
       assignees: this.assignees,
       milestone: this.milestone,
-    })).data
+    })).data;
   },
-}
+};
