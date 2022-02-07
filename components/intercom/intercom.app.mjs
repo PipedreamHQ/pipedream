17b5e10e3a93b5b85971089e5c2f9900df1d2148
michelle0927 <michellelbergero@hotmail.com>
2022-02-07 14:27:49 -0500

    actions

--- components/intercom/intercom.app.mjs
+++ components/intercom/intercom.app.mjs
@@ -23,6 +23,11 @@ export default {
         }));
       },
     },
+    body: {
+      type: "string",
+      label: "Body",
+      description: "The text of the note.",
+    },
   },
   methods: {
     monthAgo() {
@@ -30,9 +35,24 @@ export default {
       monthAgo.setMonth(monthAgo.getMonth() - 1);
       return monthAgo;
     },
-    async makeRequest({
-      method, url, endpoint, data, $,
-    }) {
+    /**
+    * Make a request to the Intercom API
+    * @params {Object} opts - An object representing the configuration options for this method
+    * @params {String} opts.method - The HTTP method
+    * @params {String} [opts.url] - A full URL to be used in the API request. If not present,
+    * endpoint is used.
+    * @params {String} [opts.endpoint] - Endpoint to be after the base URL in the API request.
+    * @params {Object} [opts.data] - The request body
+    * @returns {*} The response may vary depending on the specific API request.
+    */
+    async makeRequest(opts) {
+      const {
+        method,
+        url,
+        endpoint,
+        data,
+        $,
+      } = opts;
       const config = {
         method,
         url: url
@@ -46,42 +66,92 @@ export default {
       };
       return await axios($ || this, config);
     },
-    async getCompanies(lastCompanyCreatedAt) {
+    /**
+     * Paginate through a list of items and return the results
+     * @params {String} itemType - The type of item being retrieved
+     * (eg. "companies", "contacts", "conversation")
+     * @params {String} method - The HTTP method
+     * @params {Object} data - The request body
+     * @params {Boolean} [isSearch=false] - If set to true, adds the word "search" to the
+     * endpoint for search requests
+     * @params {Interger} [lastCreatedAt] - Timestamp of the last relevant item created.
+     * Used to retrieve only new results
+     * @returns {Array} The complete list of paginated items
+     */
+    async paginate(itemType, method, data, isSearch = false, lastCreatedAt) {
       let results = null;
       let done = false;
-      const companies = [];
-      while ((!results || results.pages.next) && !done) {
-        const startingAfter = results
-          ? results.pages.next.starting_after
-          : null;
+      let items = [];
+      while ((!results || results?.pages?.next) && !done) {
+        const startingAfter = results?.pages?.next?.starting_after || null;
+        const endpoint = `${itemType}/${isSearch
+          ? "search"
+          : ""}${startingAfter
+          ? "?starting_after=" + startingAfter
+          : ""}`;
         results = await this.makeRequest({
-          method: "GET",
-          endpoint: `companies${startingAfter
-            ? "?starting_after=" + startingAfter
-            : ""}`,
+          method,
+          endpoint,
+          data,
         });
-        for (const company of results.data) {
-          if (company.created_at > lastCompanyCreatedAt)
-            companies.push(company);
-          else
+        if (lastCreatedAt) {
+          for (const item of results.data) {
+            if (item.created_at > lastCreatedAt)
+              items.push(item);
+            else
+              done = true;
+          }
+        } else {
+          items = items.concat(results.data);
+          if (!startingAfter)
             done = true;
         }
       }
-      return companies;
+      return items;
     },
+    /**
+     * Get a list of companies
+     * @params {Interger} [lastCompanyCreatedAt] - Timestamp of the creation date of the last
+     * successfully retrieved company. If set, only companies created since this timestamp
+     * will be returned.
+     * @returns {Array} List of company objects
+     */
+    async getCompanies(lastCompanyCreatedAt) {
+      return await this.paginate("companies", "GET", null, false, lastCompanyCreatedAt);
+    },
+    /**
+     * Get the current admin
+     * @returns {Object} An admin object for the current authorized admin
+     */
+    async getAdmin($) {
+      return await this.makeRequest({
+        method: "GET",
+        endpoint: "me",
+        $,
+      });
+    },
+    /**
+     * Get a conversation by id
+     * @params {String} id - The identifier for the conversation as given by Intercom
+     * @returns {Object} A conversation object matching the given id
+     */
     async getConversation(id) {
       return await this.makeRequest({
         method: "GET",
         endpoint: `conversations/${id}`,
       });
     },
+    /**
+     * @params {String} userId - The id for the user who's events are being retrieved
+     * @params {String} [nextUrl=null] - URL of the next page of events for a user.
+     * Used to skip past results that have already been retrieved
+     * @returns {Object} Object containing an array of events and the value of nextUrl
+     */
     async getEvents(userId, nextUrl = null) {
       let results = null;
       let events = [];
       while (!results || results.pages.next) {
-        const url = results
-          ? results.pages.next
-          : null;
+        const url = results?.pages?.next;
         const endpoint = `events?type=user&intercom_user_id=${userId}`;
         results = await this.makeRequest({
           method: "GET",
@@ -98,43 +168,65 @@ export default {
         nextUrl,
       };
     },
-    async searchContacts(data) {
-      let results = null;
-      let contacts = [];
-      while (!results || results.pages.next) {
-        const startingAfter = results
-          ? results.pages.next.starting_after
-          : null;
-        const endpoint = `contacts/search${startingAfter
-          ? "?starting_after=" + startingAfter
-          : ""}`;
-        results = await this.makeRequest({
-          method: "POST",
-          endpoint,
-          data,
-        });
-        contacts = contacts.concat(results.data);
-      }
-      return contacts;
+    /**
+     * Get a contact by id
+     * @params {String} id - The unique identifier for the contact which is given by Intercom
+     * @returns {Object} A contact object matching the given id
+     */
+    async getContact(id, $) {
+      return this.makeRequest({
+        method: "GET",
+        endpoint: `contacts/${id}`,
+        $,
+      });
     },
+    /**
+     * Search for contacts
+     * @params {Ojbect} data - A query object used to search for contacts
+     * @returns {Array} List of contacts matching search query
+     */
+    async searchContacts(data) {
+      return this.paginate("contacts", "POST", data, true);
+    },
+    /**
+     * Search for conversations
+     * @params {Object} data - A query object used to search for conversations
+     * @returns {Array} List of conversations matching search query
+     */
     async searchConversations(data) {
-      let results = null;
-      let conversations = [];
-      while (!results || results.pages.next) {
-        const startingAfter = results
-          ? results.pages.next.starting_after
-          : null;
-        const endpoint = `conversations/search${startingAfter
-          ? "?starting_after=" + startingAfter
-          : ""}`;
-        results = await this.makeRequest({
-          method: "POST",
-          endpoint,
-          data,
-        });
-        conversations = conversations.concat(results.conversations);
-      }
-      return conversations;
+      return this.paginate("conversations", "POST", data, true);
+    },
+    /**
+     * Create a note for a specific user
+     * @params {String} userId - The unique identifier for the contact which is given by Intercom
+     * @params {String} adminId - The unique identifier for the admin which is given by Intercom
+     * @params {String} body - The text of the note.
+     * @returns {Ojbect} The note object created by the request
+     */
+    async createNote(userId, adminId, body, $) {
+      return this.makeRequest({
+        method: "POST",
+        endpoint: `contacts/${userId}/notes`,
+        data: {
+          body,
+          admin_id: adminId,
+        },
+        $,
+      });
+    },
+    /**
+     * Create an incoming message from a user
+     * @params {Object} data - The request body parameters including a `from` object and
+     * the message content
+     * @returns {Ojbect} The conversation object created by the request
+     */
+    async createConversation(data, $) {
+      return this.makeRequest({
+        method: "POST",
+        endpoint: "conversations",
+        data,
+        $,
+      });
     },
   },
 };
