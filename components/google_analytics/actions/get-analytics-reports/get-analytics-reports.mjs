e54358118aa71d426ec6a3a16b6c4b8cdbe54789
Andrew Chuang <andrewjschuang@gmail.com>
2022-02-08 13:20:27 -0300

    change to better usage of report request object

--- components/google_analytics/actions/get-analytics-reports/get-analytics-reports.mjs
+++ components/google_analytics/actions/get-analytics-reports/get-analytics-reports.mjs
@@ -3,7 +3,7 @@ import analytics from "../../google_analytics.app.mjs";
 export default {
   key: "google_analytics-get-analytics-reports",
   name: "Get Analytics Reports",
-  description: "Retrieves a list of analytics reports based on report requests",
+  description: "Retrieves an analytics report based on a request. [See docs](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#ReportRequest)",
   version: "0.0.1",
   type: "action",
   props: {
@@ -14,34 +14,194 @@ export default {
         "token",
       ],
     },
-    reportRequests: {
-      type: "string[]",
-      label: "Report Requests",
-      description: `Report Requests, each request will have a separate response
-        There can be a maximum of 5 requests. All requests should have the same dateRanges, viewId, segments, samplingLevel, and cohortGroup
-        More information at [Analytics Batch Get Method](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#ReportRequest)`,
+    accountId: {
+      propDefinition: [
+        analytics,
+        "accountId",
+        (c) => ({
+          token: c.token,
+        }),
+      ],
     },
-    useResourceQuotas: {
+    webPropertyId: {
+      propDefinition: [
+        analytics,
+        "webPropertyId",
+        (c) => ({
+          token: c.token,
+          accountId: c.accountId,
+        }),
+      ],
+    },
+    viewId: {
+      propDefinition: [
+        analytics,
+        "profileId",
+        (c) => ({
+          token: c.token,
+          accountId: c.accountId,
+          webPropertyId: c.webPropertyId,
+        }),
+      ],
+    },
+    startDate: {
+      type: "string",
+      label: "Start Date",
+      description: "The start date for the query in the format YYYY-MM-DD",
+      optional: true,
+    },
+    endDate: {
+      type: "string",
+      label: "End Date",
+      description: "The end date for the query in the format YYYY-MM-DD",
+      optional: true,
+    },
+    dimensions: {
+      type: "string[]",
+      label: "Dimension Names",
+      description: "Names of the dimensions to fetch. For more information [see docs](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#dimension)",
+      optional: true,
+    },
+    useMetrics: {
+      type: "boolean",
+      label: "Use Metrics",
+      description: "Whether to use Metrics. For attributes [see docs](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#metric)",
+      default: false,
+      reloadProps: true,
+    },
+    sort: {
+      type: "boolean",
+      label: "Sort",
+      description: "Specify sorting options. For sorting options [see docs](https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#orderby)",
+      default: false,
+      reloadProps: true,
+    },
+  },
+  async additionalProps() {
+    let props = {};
+
+    if (this.useMetrics) {
+      props.metricExpression = {
+        type: "string",
+        label: "Metric Expression",
+        description: "A metric expression in the request",
+      };
+      props.metricAlias = {
+        type: "string",
+        label: "Metric alias",
+        description: "An alternate name for the metric expression",
+        optional: true,
+      };
+      props.metricFormattingType = {
+        type: "string",
+        label: "Metric alias",
+        description: "Specifies how the metric expression should be formatted",
+        optional: true,
+        options: [
+          "INTEGER",
+          "FLOAT",
+          "CURRENCY",
+          "PERCENT",
+          "TIME",
+        ],
+      };
+    }
+
+    if (this.sort) {
+      props.sortFieldName = {
+        type: "string",
+        label: "Sort Field Name",
+        description: "The field which to sort by",
+      };
+      props.sortOrderType = {
+        type: "string",
+        label: "Sort Order Type",
+        description: "Controls how the sort order is being determined. Default is value",
+        optional: true,
+        options: [
+          "VALUE",
+          "DELTA",
+          "SMART",
+          "HISTOGRAM_BUCKET",
+          "DIMENSION_AS_INTEGER",
+        ],
+      };
+      props.sortOrder = {
+        type: "string",
+        label: "Sort Order",
+        description: "The sorting order of the sort. The default order is ascending",
+        optional: true,
+        options: [
+          "ASCENDING",
+          "DESCENDING",
+        ],
+      };
+    }
+
+    props.useResourceQuotas = {
       type: "boolean",
       label: "Use Resource Quotas",
       description: `Enables resource based quotas, (defaults to false)
         If this field is set to True the per view (profile) quotas are governed by the computational cost of the request`,
       optional: true,
-    },
+      default: false,
+    };
+
+    return props;
   },
   async run({ $ }) {
-    let params = {
-      requestBody: {
-        reportRequests: this.reportRequests.map((request) => JSON.parse(request)),
-        useResourceQuotas: this.useResourceQuotas,
-      },
+    let reportRequests = {
+      viewId: this.viewId,
     };
+
+    if (this.startDate || this.endDate) {
+      let dateRanges = {};
+      if (this.startDate) dateRanges.startDate = this.startDate;
+      if (this.endDate) dateRanges.endDate = this.endDate;
+      reportRequests.dateRanges = [
+        dateRanges,
+      ];
+    }
+
+    if (this.dimensions) {
+      reportRequests.dimensions = this.dimensions
+        .map((dimension) => ({
+          name: dimension,
+        }))
+        .filter((dimension) => dimension.name);
+    }
+
+    if (this.useMetrics) {
+      let metrics = {};
+      if (this.metricExpression) metrics.expression = this.metricExpression;
+      if (this.metricAlias) metrics.alias = this.metricAlias;
+      if (this.metricFormattingType) metrics.formattingType = this.metricFormattingType;
+      reportRequests.metrics = [
+        metrics,
+      ];
+    }
+
+    if (this.sort) {
+      let orderBys = {};
+      if (this.sortFieldName) orderBys.fieldName = this.sortFieldName;
+      if (this.orderType) orderBys.orderType = this.orderType;
+      if (this.sortOrder) orderBys.sortOrder = this.sortOrder;
+      reportRequests.orderBys = [
+        orderBys,
+      ];
+    }
+
     let response = await this.analytics.listReports(
       this.token,
-      params,
+      {
+        requestBody: {
+          reportRequests,
+        },
+        useResourceQuotas: this.useResourceQuotas,
+      },
     );
-    const viewId = params.requestBody.reportRequests[0].viewId;
-    $.export("$summary", `Found ${response.reports.length} reports(s) for view \`${viewId}\``);
+
+    $.export("$summary", `Found report for viewId \`${this.viewId}\``);
     return response;
   },
 };
