e60b32b8aa09b33c0e9499d6b34bf1c9f5474c80
Andrew Chuang <andrewjschuang@gmail.com>
2022-02-09 10:22:54 -0300

    add find page action

--- components/notion/notion.app.mjs
+++ components/notion/notion.app.mjs
@@ -1,11 +1,30 @@
+import notion from "@notionhq/client";
+
 export default {
   type: "app",
   app: "notion",
-  propDefinitions: {},
+  propDefinitions: {
+    title: {
+      type: "string",
+      label: "Page Title",
+      description: "The words contained in the page title to search for. Leave blank to list all pages",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getNotionClient() {
+      return new notion.Client({
+        auth: this.$auth.oauth_access_token,
+      });
+    },
+    async searchPage(title) {
+      return await this._getNotionClient().search({
+        query: title,
+        filter: {
+          property: "object",
+          value: "page",
+        },
+      });
     },
   },
 };
