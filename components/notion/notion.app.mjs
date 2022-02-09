9d23a9b11a84bc9efafa7a62b8d12e75f551ff3a
Andrew Chuang <andrewjschuang@gmail.com>
2022-02-09 19:59:12 -0300

    add retrieve page action

--- components/notion/notion.app.mjs
+++ components/notion/notion.app.mjs
@@ -4,6 +4,30 @@ export default {
   type: "app",
   app: "notion",
   propDefinitions: {
+    pageId: {
+      type: "string",
+      label: "Page ID",
+      description: "The identifier for a Notion page",
+      async options() {
+        return (await this.searchPage()).results
+          .map((page) => {
+            let title = Object.values(page.properties)
+              .map((property) => {
+                if (property.type === "title" && property.title.length > 0) {
+                  return property.title
+                    .map((title) => title.plain_text)
+                    .filter((title) => title.length > 0)
+                    .reduce((prev, next) => prev + next);
+                }
+              })
+              .filter((title) => title);
+            return {
+              label: title[0] ?? "Untitled",
+              value: page.id,
+            };
+          });
+      },
+    },
     title: {
       type: "string",
       label: "Page Title",
@@ -15,6 +39,11 @@ export default {
     _getNotionClient() {
       return new notion.Client({
         auth: this.$auth.oauth_access_token,
+      });
+    },
+    async retrievePage(pageId) {
+      return await this._getNotionClient().pages.retrieve({
+        page_id: pageId,
       });
     },
     async searchPage(title) {
