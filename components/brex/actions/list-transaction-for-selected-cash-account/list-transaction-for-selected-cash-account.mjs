a1901f4a6a89949bc5ca8a67045d562e06b09925
Cassiano Vellames <cassiano.v@turing.com>
2022-02-10 16:06:02 -0300

    fix bug

--- components/brex/actions/list-transaction-for-selected-cash-account/list-transaction-for-selected-cash-account.mjs
+++ components/brex/actions/list-transaction-for-selected-cash-account/list-transaction-for-selected-cash-account.mjs
@@ -2,6 +2,7 @@ import brexApp from "../../brex.app.mjs";
 import common from "./common.mjs";
 
 export default {
+  ...common,
   name: "List Transactions for Selected Cash Account",
   description: "Lists all transactions for the specified cash account. [See the docs here](https://developer.brex.com/openapi/transactions_api/#operation/listCashTransactions).",
   key: "brex-list-transactions-for-selected-cash-account",
