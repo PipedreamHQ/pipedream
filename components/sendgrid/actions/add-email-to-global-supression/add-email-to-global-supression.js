29a80ecdde6d9a5594a0fc9a1dd0344356f6e6ae
Sergio E. Rodríguez Wong <sergio_eliot@hotmail.com>
2022-02-12 16:00:07 -0700

    Update components/sendgrid/actions/add-email-to-global-supression/add-email-to-global-supression.js
    Added link to api endpoint docs
    
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

--- components/sendgrid/actions/add-email-to-global-supression/add-email-to-global-supression.js
+++ components/sendgrid/actions/add-email-to-global-supression/add-email-to-global-supression.js
@@ -5,7 +5,7 @@ module.exports = {
   key: "sendgrid-add-email-to-global-supression",
   name: "Add Email To Global Supression",
   description:
-    "Allows you to add one or more email addresses to the global suppressions group.",
+    "Allows you to add one or more email addresses to the global suppressions group. [See the docs here](https://sendgrid.api-docs.io/v3.0/suppressions-global-suppressions/add-recipient-addresses-to-the-global-suppression-group)",
   version: "0.0.1",
   type: "action",
   props: {
