79379ad161247bb2b596dc9fd08d99f8071c8cc3
js07 <19861096+js07@users.noreply.github.com>
2022-02-18 15:16:23 -0500

    Move app_placeholder actions to correct app folder and add app prop
    Moves `xero_accounting_api`-`make-an-api-call.mjs` & `zoho_crm`-
    `make-an-api-call.mjs`

--- components/xero_accounting_api/actions/make-an-api-call/make-an-api-call.mjs
+++ components/xero_accounting_api/actions/make-an-api-call/make-an-api-call.mjs
@@ -1,13 +1,17 @@
-// legacy_hash_id: a_8Ki0B8
+// legacy_hash_id: a_YEi2rp
 import { axios } from "@pipedream/platform";
 
 export default {
-  key: "app_placeholder-zoho-crm-make-an-api-call",
+  key: "xero_accounting_api-make-an-api-call",
   name: "Make API Call",
-  description: "Makes an aribitrary call to Zoho CRM API.",
+  description: "Makes an aribitrary call to Xero Accounting API.",
   version: "0.1.1",
   type: "action",
   props: {
+    xero_accounting_api: {
+      type: "app",
+      app: "xero_accounting_api",
+    },
     request_method: {
       type: "string",
       description: "Http method to use in the request.",
@@ -23,6 +27,11 @@ export default {
       type: "string",
       description: "A path relative to Xero Accounting API to send the request against.",
     },
+    query_string: {
+      type: "string",
+      description: "Query string of the request.",
+      optional: true,
+    },
     headers: {
       type: "object",
       description: "Headers to send in the request. Must include header `xero-tenant-id` with Id of the organization tenant to use on the Xero Accounting API. See [Get Tenant Connections](https://pipedream.com/@sergio/xero-accounting-api-get-tenant-connections-p_OKCzOgn/edit) for a workflow example on how to pull this data.",
@@ -34,15 +43,17 @@ export default {
     },
   },
   async run({ $ }) {
-  // See Quickbooks Rest Accounting API docs at: https://www.zoho.com/crm/developer/docs/api/v2/modules-api.html
+  // See Xero's Rest Accounting API docs at: https://developer.xero.com/documentation/api/api-overview
 
     if (!this.request_method || !this.relative_url) {
       throw new Error("Must provide request_method, and relative_url parameters.");
     }
 
+    this.query_string = this.query_string || "";
+
     return await axios($, {
       method: this.request_method,
-      url: `https://www.zohoapis.com/${this.relative_url}`,
+      url: `https://api.xero.com/${this.relative_url}${this.query_string}`,
       headers: this.headers,
       data: this.request_body,
     });
--- components/zoho_crm/actions/make-an-api-call/make-an-api-call.mjs
+++ components/zoho_crm/actions/make-an-api-call/make-an-api-call.mjs
@@ -1,13 +1,17 @@
-// legacy_hash_id: a_YEi2rp
+// legacy_hash_id: a_8Ki0B8
 import { axios } from "@pipedream/platform";
 
 export default {
-  key: "app_placeholder-xero-accounting-api-make-an-api-call",
+  key: "zoho_crm-make-an-api-call",
   name: "Make API Call",
-  description: "Makes an aribitrary call to Xero Accounting API.",
+  description: "Makes an aribitrary call to Zoho CRM API.",
   version: "0.1.1",
   type: "action",
   props: {
+    zoho_crm: {
+      type: "app",
+      app: "zoho_crm",
+    },
     request_method: {
       type: "string",
       description: "Http method to use in the request.",
@@ -23,11 +27,6 @@ export default {
       type: "string",
       description: "A path relative to Xero Accounting API to send the request against.",
     },
-    query_string: {
-      type: "string",
-      description: "Query string of the request.",
-      optional: true,
-    },
     headers: {
       type: "object",
       description: "Headers to send in the request. Must include header `xero-tenant-id` with Id of the organization tenant to use on the Xero Accounting API. See [Get Tenant Connections](https://pipedream.com/@sergio/xero-accounting-api-get-tenant-connections-p_OKCzOgn/edit) for a workflow example on how to pull this data.",
@@ -39,17 +38,15 @@ export default {
     },
   },
   async run({ $ }) {
-  // See Xero's Rest Accounting API docs at: https://developer.xero.com/documentation/api/api-overview
+  // See Quickbooks Rest Accounting API docs at: https://www.zoho.com/crm/developer/docs/api/v2/modules-api.html
 
     if (!this.request_method || !this.relative_url) {
       throw new Error("Must provide request_method, and relative_url parameters.");
     }
 
-    this.query_string = this.query_string || "";
-
     return await axios($, {
       method: this.request_method,
-      url: `https://api.xero.com/${this.relative_url}${this.query_string}`,
+      url: `https://www.zohoapis.com/${this.relative_url}`,
       headers: this.headers,
       data: this.request_body,
     });
