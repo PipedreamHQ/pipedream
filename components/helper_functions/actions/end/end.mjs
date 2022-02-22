090e524fe50bf119bf64c5f0cfa7b252e7e65af4
Danny Roosevelt <danny@pipedream.com>
2022-02-22 14:40:22 -0800

    Moving end action to Helper Functions app

--- components/helper_functions/actions/end/end.mjs
+++ components/helper_functions/actions/end/end.mjs
@@ -3,9 +3,13 @@ export default {
   key: "pipedream-end",
   name: "End Workflow",
   description: "End execution at this step. Later steps in the workflow will not run.",
-  version: "0.1.1",
+  version: "0.1.{{ts}}",
   type: "action",
   props: {
+    helper_functions: {
+      type: "app",
+      app: "helper_functions",
+    },
     reason: {
       type: "string",
       optional: true,
@@ -13,5 +17,6 @@ export default {
   },
   async run({ $ }) {
     $.flow.exit(this.reason);
+    $.export("$summary", `Workflow ended. Reason: \`${this.reason}\``);
   },
 };
