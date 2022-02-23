638e2a4ceb1e10069183566be5afbd15d3413a72
Dylan J. Sather <dylan.sather@gmail.com>
2022-02-22 19:43:17 -0800

    Closes #404

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -96,6 +96,7 @@ module.exports = {
               "/code/go/",
               "/code/bash/",
               "/destinations/",
+              "/scheduling-future-tasks/",
             ],
           },
           {
