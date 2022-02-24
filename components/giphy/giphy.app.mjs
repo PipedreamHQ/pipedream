97681af5d42cb35ebbd2c0e71c1403f4c4258fe1
Cassiano Vellames <cassiano.v@turing.com>
2022-02-24 14:35:44 -0300

    search gifs and stickers

--- components/giphy/giphy.app.mjs
+++ components/giphy/giphy.app.mjs
@@ -1,11 +1,67 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "giphy",
-  propDefinitions: {},
+  propDefinitions: {
+    max: {
+      type: "integer",
+      label: "Max",
+      description: "The maximum amount of registers to be fetched. Defaults to `50`",
+      min: 1,
+      default: 50,
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getAxiosParams(opts = {}) {
+      return {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        params: {
+          ...opts.params,
+          api_key: this.$auth.api_key,
+        },
+      };
+    },
+    _getBaseUrl() {
+      return "https://api.giphy.com";
+    },
+    async getGifsOrStickers(searchType, params, max, ctx = this) {
+      const TOTAL_LIMIT = max || 50;
+      const DEFAULT_LIMIT_PER_PAGE = 50;
+
+      let items = [];
+      let limit = Math.min(DEFAULT_LIMIT_PER_PAGE, parseInt(TOTAL_LIMIT));
+      let currentPage = 0;
+      do {
+        // Adjust the limit to avoid extra elements on the last page
+        if (items.length + limit > TOTAL_LIMIT) {
+          limit -= items.length + limit - TOTAL_LIMIT;
+        }
+        const res = await axios(ctx, this._getAxiosParams({
+          method: "GET",
+          path: searchType === "gifs"
+            ? "/v1/gifs/search"
+            : "/v1/stickers/search",
+          params: {
+            ...params,
+            limit,
+            offset: currentPage * DEFAULT_LIMIT_PER_PAGE,
+          },
+        }));
+
+        if (res.data?.length > 0) {
+          items = [
+            ...items,
+            ...res.data,
+          ];
+        } else {
+          break;
+        }
+      } while (items.length < TOTAL_LIMIT);
+
+      return items;
     },
   },
 };
