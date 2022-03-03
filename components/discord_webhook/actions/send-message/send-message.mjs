860bd878963ad814f8089e5f445c48e4be1bd0f3
js07 <19861096+js07@users.noreply.github.com>
2022-03-03 12:39:23 -0500

    Convert Slack & Discord Webhook components from CJS to ESM

---
+++ components/discord_webhook/actions/send-message/send-message.mjs
@@ -0,0 +1,38 @@
+import common from "../send-message-common.mjs";
+
+export default {
+  ...common,
+  key: "discord_webhook-send-message",
+  name: "Send Message",
+  description: "Send a simple message to a Discord channel",
+  version: "0.2.0",
+  type: "action",
+  props: {
+    ...common.props,
+  },
+  async run() {
+    const {
+      avatarURL,
+      threadID,
+      username,
+      includeSendMessageViaPipedreamFlag,
+    } = this;
+
+    let embeds;
+    if (includeSendMessageViaPipedreamFlag) {
+      embeds = [
+        ...(embeds ?? []),
+        this.makeSentViaPipedreamEmbed(),
+      ];
+    }
+
+    // No interesting data is returned from Discord
+    await this.discordWebhook.sendMessage({
+      avatarURL,
+      content: this.message,
+      threadID,
+      username,
+      embeds,
+    });
+  },
+};
