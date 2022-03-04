d5f98ef79b015118bde8223e9f1c90e576a9813b
js07 <19861096+js07@users.noreply.github.com>
2022-03-03 20:00:37 -0500

    Fix TypeError in Slack send-message-private-channel action

--- components/slack/actions/send-message-private-channel/send-message-private-channel.mjs
+++ components/slack/actions/send-message-private-channel/send-message-private-channel.mjs
@@ -1,10 +1,11 @@
 import common from "../send-message-common.mjs";
 
 export default {
+  ...common,
   key: "slack-send-message-private-channel",
   name: "Send Message to a Private Channel",
   description: "Send a message to a private channel and customize the name and avatar of the bot that posts the message",
-  version: "0.2.0",
+  version: "0.2.1",
   type: "action",
   props: {
     ...common.props,
