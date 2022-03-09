63291d93bfc300f69f67a1eb6bc84c1873808b2c
Andrew Chuang <andrewjschuang@gmail.com>
2022-03-09 17:03:20 -0300

    add new guild member trigger

--- components/discord_bot/sources/common.mjs
+++ components/discord_bot/sources/common.mjs
@@ -11,5 +11,11 @@ export default {
     _setLastMessageIDs(lastMessageIDs) {
       this.db.set(constants.LAST_MESSAGE_IDS, lastMessageIDs);
     },
+    _getGuildMemberIDs() {
+      return this.db.get(constants.GUILD_MEMBER_IDS) ?? {};
+    },
+    _setGuildMemberIDs(memberIDs) {
+      this.db.set(constants.GUILD_MEMBER_IDS, memberIDs);
+    },
   },
 };
