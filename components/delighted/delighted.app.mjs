a41a382545719aa800408d63b9868a7fb4393e66
Xiangxuan Liu <9570112+nauxliu@users.noreply.github.com>
2022-03-11 15:36:30 +0800

    Update delighted.app.mjs

--- components/delighted/delighted.app.mjs
+++ components/delighted/delighted.app.mjs
@@ -1,11 +1,17 @@
+import delightedLib from "delighted";
+
 export default {
   type: "app",
   app: "delighted",
+  label: "Delighted",
+  description: "The Delighted app that will be used to send the survey.",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    lib() {
+      return delightedLib(this.$auth.api_key);
+    },
+    async sendingToPeople(params) {
+      return this.lib().sendingToPeople(params);
     },
   },
 };
