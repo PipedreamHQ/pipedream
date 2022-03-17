0663fd36b12eb6c4f9f7b5c69eda8b4611d696ad
Marcelo Jabali <mqjabali@gmail.com>
2022-03-17 09:19:42 -0700

    Added Video Summary UI and Summary actions (#2442)
    * Added Post Video Summary UI action
    
    * Added Post Video Summary UI action
    
    * Added GET Summary action
    
    * Fixed eslint issues
    
    * Addressed PR review suggestions

--- components/symbl_ai/symbl_ai.app.mjs
+++ components/symbl_ai/symbl_ai.app.mjs
@@ -70,6 +70,18 @@ export default {
         path: `/job/${jobId}`,
       });
     },
+    async postVideoSummaryUI({
+      $,
+      conversationId,
+      data,
+    }) {
+      return this.makeRequest({
+        $,
+        method: "post",
+        path: `/conversations/${conversationId}/experiences`,
+        data,
+      });
+    },
     async getSpeechToText({
       $,
       conversationId,
@@ -129,5 +141,17 @@ export default {
         params,
       });
     },
+    async getSummary({
+      $,
+      conversationId,
+      params,
+    }) {
+      return this.makeRequest({
+        $,
+        path: `/conversations/${conversationId}/summary`,
+        params,
+      });
+    },
+
   },
 };
