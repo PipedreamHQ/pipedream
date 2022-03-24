392901dce38bd50795808b49188031e0dfc9401f
Danny Roosevelt <danny@pipedream.com>
2022-03-24 12:08:22 -0700

    Update new-email.mjs

--- components/email/sources/new-email/new-email.mjs
+++ components/email/sources/new-email/new-email.mjs
@@ -1,9 +1,13 @@
+import email from "../../email.app.mjs";
+
 export default {
   name: "New Email",
   version: "0.0.1",
   key: "email-new-email",
   description: "Get a unique address where you can send emails to trigger your workflow.",
-  props: {},
+  props: {
+    email,
+  },
   type: "source",
   methods: {},
   async run() {},
