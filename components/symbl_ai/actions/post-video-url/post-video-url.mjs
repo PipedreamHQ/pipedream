6df8d9b0e1fade35aaf7049367501d2e08f70c65
Marcelo Jabali <mqjabali@gmail.com>
2022-03-24 13:51:46 -0700

    Adjusted action version

--- components/symbl_ai/actions/post-video-url/post-video-url.mjs
+++ components/symbl_ai/actions/post-video-url/post-video-url.mjs
@@ -5,7 +5,7 @@ export default {
   key: "symbl_ai-post-video-url",
   name: "Submit Video URL",
   description: "Submit a Video file by providing the URL for processing. See the doc [here](https://docs.symbl.ai/docs/async-api/overview/video/post-video-url).",
-  version: "0.0.44",
+  version: "0.0.4",
   type: "action",
   props: {
     symblAIApp,
