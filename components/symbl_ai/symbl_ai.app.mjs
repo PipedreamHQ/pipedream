4a05164232b3b2cf44d7349ed602ac302cd612b0
Marcelo Jabali <mqjabali@gmail.com>
2022-03-27 08:05:00 -0700

    Refactored POST Tracker Analystics UI reused props

--- components/symbl_ai/symbl_ai.app.mjs
+++ components/symbl_ai/symbl_ai.app.mjs
@@ -24,6 +24,55 @@ export default {
         }));
       },
     },
+    logo: {
+      type: "string",
+      label: "Logo",
+      description: "URL of the custom logo to be used in the Summary UI.",
+      optional: true,
+    },
+    favicon: {
+      type: "string",
+      label: "Favicon",
+      description: "URL of the custom favicon to be used in the Summary UI.",
+      optional: true,
+    },
+    background: {
+      type: "string",
+      label: "Background Color",
+      description: "Background color to be used in the Summary UI. Hex Color Codes accepted.",
+      optional: true,
+    },
+    topicsFilter: {
+      type: "string",
+      label: "Topics Filter Element Color",
+      description: "Topics Filter Element color to be used in the Summary UI. Hex Color Codes accepted.",
+      optional: true,
+    },
+    insightsFilter: {
+      type: "string",
+      label: "Insights Element Color",
+      description: "Insights (Questions, Follow-ups, Action Items, etc) Filter Element color to be used in the Summary UI. Hex Color Codes accepted.",
+      optional: true,
+    },
+    font: {
+      type: "string",
+      label: "Font",
+      description: "The name of the font to be used in the Summary UI. All fonts available in the [Google Fonts](https://fonts.google.com/) are supported.",
+      optional: true,
+    },
+    summaryURLExpiresIn: {
+      type: "integer",
+      label: "Expiration Time of the Summary UI",
+      description: "Number of seconds set for expiration time of the Summary UI. Zero (0) will set the Summary UI to never expire. Default value is set to `2592000` (30 days).",
+      optional: true,
+      min: 0,
+    },
+    readOnly: {
+      type: "boolean",
+      label: "Read Only",
+      description: "Disable the editing capabilities of the Summary UI. Default value is `false`.",
+      optional: true,
+    },
   },
   methods: {
     getHeaders() {
