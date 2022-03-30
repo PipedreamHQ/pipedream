950596be7d0e0f750d763608b23e910ecf872a37
feyzo <feyzullah.metu@gmail.com>
2022-03-31 00:33:45 +0300

    fixed description fileds

--- components/data_stores/actions/add-update-multiple-records/add-update-multiple-records.mjs
+++ components/data_stores/actions/add-update-multiple-records/add-update-multiple-records.mjs
@@ -4,7 +4,7 @@ import data_stores from "../../data_stores.app.mjs";
 export default {
   key: "data_stores-add-update-multiple-records",
   name: "Add or update multiple records",
-  description: "Enter data you'd like to add as key-value pairs, or reference an object from a previous step using a custom expression (e.g., `{{steps.data.$return_value}}`).",
+  description: "Add or update multiple records to your [Pipedream Data Store](https://pipedream.com/data-stores/).",
   version: "0.0.3",
   type: "action",
   props: {
@@ -19,7 +19,7 @@ export default {
     data: {
       label: "Data",
       type: "object",
-      description: "Enter data you'd like to add as an object containing key-value pairs, or reference existing keys and update the values for those existing records. Refer to your existing keys [here](https://pipedream.com/data-stores/).",
+      description: "Enter data you'd like to add as key-value pairs, or reference an object from a previous step using a custom expression (e.g., `{{steps.data.$return_value}}`).",
     },
   },
   methods: {
