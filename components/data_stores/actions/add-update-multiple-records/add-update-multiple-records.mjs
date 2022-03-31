4cfa20f0053cc92a92c7a003f2be84a6daaf0f60
feyzo <feyzullah.metu@gmail.com>
2022-04-01 00:23:51 +0300

    fixed single non-recursive type value error, added return types, corrected descriptions

--- components/data_stores/actions/add-update-multiple-records/add-update-multiple-records.mjs
+++ components/data_stores/actions/add-update-multiple-records/add-update-multiple-records.mjs
@@ -4,7 +4,7 @@ import data_stores from "../../data_stores.app.mjs";
 export default {
   key: "data_stores-add-update-multiple-records",
   name: "Add or update multiple records",
-  description: "Enter data you'd like to add as key-value pairs, or reference an object from a previous step using a custom expression (e.g., `{{steps.data.$return_value}}`).",
+  description: "Add or update multiple records to your [Pipedream Data Store](https://pipedream.com/data-stores/).",
   version: "0.0.3",
   type: "action",
   props: {
@@ -19,17 +19,22 @@ export default {
     data: {
       label: "Data",
       type: "object",
-      description: "Enter data you'd like to add as an object containing key-value pairs, or reference existing keys and update the values for those existing records. Refer to your existing keys [here](https://pipedream.com/data-stores/).",
+      description: "Enter data you'd like to add as key-value pairs, or reference an object from a previous step using a custom expression (e.g., {{steps.data.$return_value}}). Note that any keys that are duplicated will get overwritten with the last value entered (so [{jerry: \"constanza\", jerry: \"seinfeld\"}] will get stored as [{jerry: \"seinfeld\"}]).",
     },
   },
   methods: {
-    //Because user might enter a JSON as JS object, this method converts a JS object string to a JSON string before parsing it, e.g. {a:"b", 'c':1} => {"a":"b", "c":1}
-    //We don't use eval here because of security reasons. Using eval may cause something undesired run in the script.
+    //Because user might enter a JSON as JS object;
+    //This method converts a JS object string to a JSON string before parsing it
+    //e.g. {a:"b", 'c':1} => {"a":"b", "c":1}
+    //We don't use eval here because of security reasons.
+    //Using eval may cause something undesired run in the script.
     _sanitizeJson(str) {
       return str.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, "\"$2\": ");
     },
-    //Parsing nested arrays recursively. e.g. [[{"a":"b"}],[[[{"c":"d"}, {"e":"f"}], {"g":"h"}]]]
-    //This allows to get key-value pairs in a nested array, so it will store the data even if there is no key provided for the array.
+    //Parsing nested arrays recursively.
+    //e.g. [[{"a":"b"}],[[[{"c":"d"}, {"e":"f"}], {"g":"h"}]]]
+    //This allows to get key-value pairs in a nested array;
+    //So it will store the data even if there is no key provided for the array.
     _parseDataRecursive(obj) {
       let result = {};
       if (Array.isArray(obj)) {
@@ -47,7 +52,8 @@ export default {
       } //else pass, because it is neither object nor array
       return result;
     },
-    //Handling all serialized and unserialized inputs to treat values as objects no matter how they entered.
+    //Handling all serialized and unserialized inputs
+    //to treat values as objects no matter how they entered.
     _parseData(obj) {
       if (typeof obj === "string") {
         const sanitizedValue = this._sanitizeJson(obj);
@@ -59,16 +65,22 @@ export default {
           key,
           value,
         ] of Object.entries(obj)) {
-          try {
-            const sanitizedValue = this._sanitizeJson(value);
-            const json = JSON.parse(sanitizedValue);
-            newObj[key] = this._parseDataRecursive(json);
-          } catch (err) {
-            if (typeof value === "object") {
-              newObj[key] = this._parseDataRecursive(value);
-            } else {
+          if (typeof value === "string") {
+            try {
+              const sanitizedValue = this._sanitizeJson(value);
+              const json = JSON.parse(sanitizedValue);
+              if (typeof json !== "object") {
+                newObj[key] = json;
+              } else {
+                newObj[key] = this._parseDataRecursive(json);
+              }
+            } catch (err) {
               newObj[key] = value;
             }
+          } else if (typeof value === "object") {
+            newObj[key] = this._parseDataRecursive(value);
+          } else {
+            newObj[key] = value;
           }
         }
         return newObj;
@@ -77,16 +89,15 @@ export default {
     },
   },
   async run({ $ }) {
-    let data;
-    if (typeof this.data !== "object") {
+    if (typeof this.data === "string") {
       try {
         const sanitizedValue = this._sanitizeJson(this.data);
-        data = JSON.parse(sanitizedValue);
+        JSON.parse(sanitizedValue);
       } catch (err) {
         throw new Error("please provide an object or object array!");
       }
     }
-    data = this._parseData(this.data);
+    let data = this._parseData(this.data);
     for (const [
       key,
       value,
@@ -95,9 +106,15 @@ export default {
     }
     let count = Object.keys(data).length;
     if (count == 1) {
+      let key = Object.keys(data)[0];
       $.export("$summary", "Successfully added or updated 1 record.");
+      return {
+        key,
+        value: data[key],
+      };
     } else {
       $.export("$summary", "Successfully added or updated " + count + " records.");
+      return data;
     }
   },
 };
