3d4f98e1d25d89876a207dc35c626442809a175f
Andrew Chuang <andrewjschuang@gmail.com>
2022-04-04 15:58:57 -0300

    change rules prop selection

--- components/shopify/actions/create-smart-collection/create-smart-collection.mjs
+++ components/shopify/actions/create-smart-collection/create-smart-collection.mjs
@@ -21,30 +21,54 @@ export default {
       ],
       description: "Title of the smart collection",
     },
-    columns: {
-      type: "string[]",
-      label: "Rule: Column",
-      description: "The property of a product being used to populate the smart collection",
-      options: rules.column,
-    },
-    relations: {
-      type: "string[]",
-      label: "Rule: Relation",
-      description: "The relationship between the column choice and the condition",
-      options: rules.relation,
-    },
-    conditions: {
-      type: "string[]",
-      label: "Rule: Condition",
-      description: "Select products for a smart collection using a condition. Values are either strings or numbers, depending on the relation value",
-    },
     disjunctive: {
       type: "boolean",
       label: "Disjunctive",
-      description: "If `false`, the product must match all the rules to be included in the smart collection. Otherwise, it only needs to match at lease one rule",
+      description: toSingleLineString(`
+        If \`false\`, the product must match all the rules to be included in the smart collection.
+        Otherwise, it only needs to match at least one rule
+      `),
       optional: true,
       default: false,
     },
+    rules: {
+      type: "integer",
+      label: "Number of rules",
+      description: "The number of rules to input",
+      default: 1,
+      min: 1,
+      reloadProps: true,
+    },
+  },
+  async additionalProps() {
+    const props = {};
+    for (let i = 1; i <= this.rules; i++) {
+      props[`column_${i}`] = {
+        type: "string",
+        label: `Rule ${i} - Column`,
+        description: toSingleLineString(`
+          The property of a product being used to populate the smart collection
+        `),
+        options: rules.column,
+      };
+      props[`relation_${i}`] = {
+        type: "string",
+        label: `Rule ${i} - Relation`,
+        description: toSingleLineString(`
+          The relationship between the **column** choice, and the **condition**
+        `),
+        options: rules.relation,
+      };
+      props[`condition_${i}`] = {
+        type: "string",
+        label: `Rule ${i} - Condition`,
+        description: toSingleLineString(`
+          Select products for a smart collection using a **condition**.
+          Values is either \`string\` or \`number\`, depending on the **relation** value
+        `),
+      };
+    }
+    return props;
   },
   async run({ $ }) {
     const params = {
@@ -52,20 +76,22 @@ export default {
       disjunctive: this.disjunctive,
       rules: [],
     };
-    const minLength = Math.min(
-      this.columns.length,
-      this.relations.length,
-      this.conditions.length,
-    );
-    for (let i = 0; i < minLength; i++) {
+
+    for (let i = 1; i <= this.rules; i++) {
+      const column = `column_${i}`;
+      const relation = `relation_${i}`;
+      const condition = `condition_${i}`;
+
       params.rules.push({
-        column: this.columns[i],
-        relation: this.relations[i],
-        condition: this.conditions[i],
+        column: this[column],
+        relation: this[relation],
+        condition: this[condition],
       });
     }
-    const response = (await this.shopify.createSmartCollection(params)).result;
-    $.export("$summary", `Created new smart collection \`${response.title}\` with id \`${response.id}\``);
-    return response;
+
+    const response = await this.shopify.createSmartCollection(params);
+    const { result } = response;
+    $.export("$summary", `Created new smart collection \`${result.title}\` with id \`${result.id}\``);
+    return result;
   },
 };
