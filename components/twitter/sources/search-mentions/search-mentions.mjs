687f50898976ee935a74058d5aba22b5a44d6ece
Danny Roosevelt <danny@pipedream.com>
2022-04-04 08:00:29 -0700

    Update search-mentions.mjs

--- components/twitter/sources/search-mentions/search-mentions.mjs
+++ components/twitter/sources/search-mentions/search-mentions.mjs
@@ -5,7 +5,7 @@ export default {
   key: "twitter-search-mentions",
   name: "Search Mentions",
   description: "Emit new Tweets that match your search criteria",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "source",
   props: {
     db: "$.service.db",
