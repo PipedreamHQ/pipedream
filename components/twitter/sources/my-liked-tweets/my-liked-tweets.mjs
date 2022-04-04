21547f3749b8118e622f784182eb18662cfd0b77
Danny Roosevelt <danny@pipedream.com>
2022-04-04 08:00:08 -0700

    Update my-liked-tweets.mjs

--- components/twitter/sources/my-liked-tweets/my-liked-tweets.mjs
+++ components/twitter/sources/my-liked-tweets/my-liked-tweets.mjs
@@ -4,7 +4,7 @@ export default {
   key: "twitter-my-liked-tweets",
   name: "My Liked Tweets",
   description: "Emit new Tweets you like on Twitter",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "source",
   props: {
     twitter,
