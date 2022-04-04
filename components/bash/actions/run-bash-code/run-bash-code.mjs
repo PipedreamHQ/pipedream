7c707d3386460f6b79ae0905c150026a1a178987
Danny Roosevelt <danny@pipedream.com>
2022-04-04 11:41:17 -0700

    Action placeholders for code apps (#2547)

---
+++ components/bash/actions/run-bash-code/run-bash-code.mjs
@@ -0,0 +1,12 @@
+export default {
+  name: "Run Bash Code",
+  version: "0.0.1",
+  key: "bash-run-bash-code",
+  description: "Run any Bash in a Pipedream step within your workflow. Refer to the [Pipedream Bash docs](https://pipedream.com/docs/code/bash) to learn more.",
+  props: {},
+  type: "action",
+  methods: {},
+  async run({ $ }) {
+    // Placeholder action for Bash code app
+  },
+};
