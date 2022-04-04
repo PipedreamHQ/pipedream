7f0d6411177965e81047c311033466968b53eafb
Danny Roosevelt <danny@pipedream.com>
2022-04-04 12:27:31 -0700

    Adding app props for code apps

--- components/bash/actions/run-bash-code/run-bash-code.mjs
+++ components/bash/actions/run-bash-code/run-bash-code.mjs
@@ -1,9 +1,13 @@
+import bash from "../../bash.app.mjs";
+
 export default {
   name: "Run Bash Code",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "bash-run-bash-code",
   description: "Run any Bash in a Pipedream step within your workflow. Refer to the [Pipedream Bash docs](https://pipedream.com/docs/code/bash) to learn more.",
-  props: {},
+  props: {
+    bash,
+  },
   type: "action",
   methods: {},
   async run({ $ }) {
--- components/python/actions/run-python-code/run-python-code.mjs
+++ components/python/actions/run-python-code/run-python-code.mjs
@@ -1,9 +1,13 @@
+import python from "../../python.app.mjs";
+
 export default {
   name: "Run Python Code",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "python-run-python-code",
   description: "Write Python and use any of the 350k+ PyPi packages available. Refer to the [Pipedream Python docs](https://pipedream.com/docs/code/python) to learn more.",
-  props: {},
+  props: {
+    python,
+  },
   type: "action",
   methods: {},
   async run({
