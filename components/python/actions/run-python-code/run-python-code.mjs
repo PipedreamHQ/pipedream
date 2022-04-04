206ce37109ee08df55a6eab6512232658148357c
Danny Roosevelt <danny@pipedream.com>
2022-04-04 11:38:04 -0700

    Action placeholders for code apps

---
+++ components/python/actions/run-python-code/run-python-code.mjs
@@ -0,0 +1,14 @@
+export default {
+  name: "Run Python Code",
+  version: "0.0.1",
+  key: "python-run-python-code",
+  description: "Write Python and use any of the 350k+ PyPi packages available. Refer to the [Pipedream Python docs](https://pipedream.com/docs/code/python) to learn more.",
+  props: {},
+  type: "action",
+  methods: {},
+  async run({
+    steps, $,
+  }) {
+    // Placeholder action for Python code app
+  },
+};
