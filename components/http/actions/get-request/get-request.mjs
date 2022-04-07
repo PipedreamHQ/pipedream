899280fedf105c5e4a6f9cf79cb0278cc0683adf
js07 <19861096+js07@users.noreply.github.com>
2022-04-07 16:17:40 -0400

    Update http actions to use http_request prop
    Updates actions in the http app folder and the Helper Functions Send
     Webhook action

--- components/http/actions/get-request/get-request.mjs
+++ components/http/actions/get-request/get-request.mjs
@@ -6,14 +6,15 @@ export default {
   name: "GET Request",
   description: "Make an HTTP GET request to any URL. Optionally configure query string parameters, headers and basic auth.",
   type: "action",
-  version: "0.1.3",
+  version: "1.0.0",
   props: {
     ...customRequest.props,
-    method: {
-      type: "string",
-      label: "Method",
-      description: "The HTTP method (for example, `GET` or `POST`)",
-      static: "GET",
+    /* eslint-disable-next-line pipedream/props-label,pipedream/props-description */
+    httpRequest: {
+      ...customRequest.props.httpRequest,
+      default: {
+        method: "GET",
+      },
     },
   },
 };
--- components/http/actions/put-request/put-request.mjs
+++ components/http/actions/put-request/put-request.mjs
@@ -6,14 +6,15 @@ export default {
   name: "PUT Request",
   description: "Make an HTTP PUT request to any URL. Optionally configure query string parameters, headers and basic auth.",
   type: "action",
-  version: "0.1.3",
+  version: "1.0.0",
   props: {
     ...customRequest.props,
-    method: {
-      type: "string",
-      label: "Method",
-      description: "The HTTP method (for example, `GET` or `POST`)",
-      static: "PUT",
+    /* eslint-disable-next-line pipedream/props-label,pipedream/props-description */
+    httpRequest: {
+      ...customRequest.props.httpRequest,
+      default: {
+        method: "PUT",
+      },
     },
   },
 };
