5f465794c99a6edf2075600a57e6656b5239e42b
Danny Roosevelt <danny@pipedream.com>
2022-04-08 13:41:53 -0700

    Initial scaffolding for Filter action

--- components/filter/filter.app.mjs
+++ components/filter/filter.app.mjs
@@ -1,7 +1,54 @@
 export default {
   type: "app",
   app: "filter",
-  propDefinitions: {},
+  propDefinitions: {
+    inputField: {
+      type: "any",
+      label: "Value to evaluate",
+      description: "Enter or reference a value here to evaluate (can be a string, number, or boolean).",
+    },
+    // eslint-disable-next-line pipedream/props-description
+    condition: {
+      type: "string",
+      label: "Condition for evaluation",
+      options: [
+        "Contains",
+        "Does not contain (text)",
+        "Matches exactly (text)",
+        "Does not exactly match (text)",
+        "In in (text)",
+        "Is not in (text)",
+        "Starts with (text)",
+        "Does not start with (text)",
+        "Ends with (text)",
+        "Does not end with (text)",
+        "Greater than (number)",
+        "Less than (number)",
+        "Is equal to (number)",
+        "After (date/time)",
+        "Before (date/time)",
+        "Equals (date/time)",
+        "Is True (boolean)",
+        "Is False (boolean)",
+        "Exists",
+        "Does not exist",
+      ],
+    },
+    // eslint-disable-next-line pipedream/props-description
+    valueToCompare: {
+      type: "any",
+      label: "Value to compare against",
+      description: "Enter or reference a value here to compare the initial value against (can be a string, number, or boolean).",
+    },
+    continue: {
+      type: "string",
+      label: "Continue if the condition is met?",
+      options: [
+        "Yes, continue the workflow",
+        "No, stop the workflow",
+      ],
+    },
+  },
   methods: {
     // this.$auth contains connected account data
     authKeys() {
