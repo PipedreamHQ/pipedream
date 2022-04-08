495cf4bb808f0dd31e06524a823cef8d3d060da6
michelle0927 <michellelbergero@hotmail.com>
2022-04-08 12:01:58 -0400

    actions

---
+++ components/vercel/actions/create-deployment/create-deployment.mjs
@@ -0,0 +1,69 @@
+import vercel from "../../vercel.app.mjs";
+
+export default {
+  key: "vercel-create-deployment",
+  name: "Create Deployment",
+  description: "Create a new deployment with all the required and intended data.. [See the docs](https://vercel.com/docs/rest-api#endpoints/deployments/create-a-new-deployment)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    vercel,
+    name: {
+      type: "string",
+      label: "Name",
+      description: "A string with the project name used in the deployment URL",
+    },
+    project: {
+      propDefinition: [
+        vercel,
+        "project",
+      ],
+      description: "The target project identifier in which the deployment will be created. When defined, this parameter overrides name",
+    },
+    repo: {
+      type: "string",
+      label: "Git Source Repository",
+      description: "Defines the Git Repository source to be deployed. E.g. {username}/{reponame}",
+    },
+    repoId: {
+      type: "string",
+      label: "Git Source Repository Id",
+      description: "Id of the source repository",
+    },
+    branch: {
+      type: "string",
+      label: "Branch",
+      description: "Branch of repository to deploy to",
+      default: "master",
+    },
+    team: {
+      propDefinition: [
+        vercel,
+        "team",
+      ],
+    },
+    public: {
+      type: "boolean",
+      label: "Public",
+      description: "Whether a deployment's source and logs are available publicly",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      name: this.name,
+      project: this.project,
+      teamId: this.team,
+      public: this.public,
+      gitSource: {
+        type: "github",
+        repoId: this.repoId,
+        repo: this.repo,
+        ref: this.branch,
+      },
+    };
+    const res = await this.vercel.createDeployment(data, $);
+    $.export("$summary", "Successfully created new deployment");
+    return res;
+  },
+};
--- components/vercel/vercel.app.mjs
+++ components/vercel/vercel.app.mjs
@@ -1,11 +1,152 @@
+import { axios } from "@pipedream/platform";
+import constants from "./constants.mjs";
+
 export default {
   type: "app",
   app: "vercel",
-  propDefinitions: {},
+  propDefinitions: {
+    project: {
+      type: "string",
+      label: "Project",
+      description: "Filter deployments from the given projectId",
+      optional: true,
+      async options() {
+        const projects = await this.listProjects();
+        if (!projects || projects.length === 0) {
+          return [];
+        }
+        return projects.map((project) => ({
+          label: project.name,
+          value: project.id,
+        }));
+      },
+    },
+    deployment: {
+      type: "string",
+      label: "Deployment",
+      description: "Select the deployment to cancel",
+      async options({ state }) {
+        const params = state
+          ? {
+            state,
+          }
+          : undefined;
+        const deployments = await this.listDeployments(params);
+        if (!deployments || deployments.length === 0) {
+          return [];
+        }
+        return deployments.map((deployment) => ({
+          label: deployment.name,
+          value: deployment.uid,
+        }));
+      },
+    },
+    team: {
+      type: "string",
+      label: "Team",
+      description: "The Team identifier or slug to perform the request on behalf of",
+      optional: true,
+      async options() {
+        try {
+          const teams = await this.listTeams();
+          if (!teams || teams.length === 0) {
+            return [];
+          }
+          return teams.map((team) => ({
+            label: team.slug,
+            value: team.id,
+          }));
+        } catch (e) {
+          throw new Error(e);
+        }
+      },
+    },
+    state: {
+      type: "string",
+      label: "State",
+      description: "Filter deployments based on their state",
+      optional: true,
+      options: constants.DEPLOYMENT_STATES,
+    },
+    max: {
+      type: "integer",
+      label: "Max",
+      description: "Maximum number of results to return",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    async makeRequest(config, $) {
+      config = {
+        ...config,
+        url: `https://api.vercel.com/${config.endpoint}`,
+        headers: {
+          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+          "User-Agent": "@PipedreamHQ/pipedream v0.1",
+        },
+      };
+      delete config.endpoint;
+      return axios($ ?? this, config);
+    },
+    async paginate(resource, config, max = constants.MAX_RESULTS, $) {
+      const allResults = [];
+      config.params = {
+        ...config.params,
+        limit: constants.PAGE_SIZE,
+      };
+      let results;
+      do {
+        results = await this.makeRequest(config, $);
+        config.params.from = results?.pagination?.next;
+        config.params.since = results?.pagination?.next;
+        allResults.push(...results[resource]);
+      } while (results?.pagination?.count == config.limit && allResults.length < max);
+      if (allResults.length > max) {
+        allResults.length = max;
+      }
+      return allResults;
+    },
+    async listProjects(max, $) {
+      const config = {
+        method: "GET",
+        endpoint: "v8/projects",
+      };
+      return this.paginate("projects", config, max, $);
+    },
+    async listDeployments(params, max, $) {
+      const config = {
+        method: "GET",
+        endpoint: "v6/deployments",
+        params,
+      };
+      return this.paginate("deployments", config, max, $);
+    },
+    async listTeams(max, $) {
+      const config = {
+        method: "GET",
+        endpoint: "v2/teams",
+      };
+      return this.paginate("teams", config, max, $);
+    },
+    async cancelDeployment(id, params, $) {
+      const config = {
+        method: "PATCH",
+        endpoint: `v12/deployments/${id}/cancel`,
+        params,
+      };
+      return this.makeRequest(config, $);
+    },
+    async createDeployment(data, $) {
+      const config = {
+        method: "POST",
+        endpoint: "v13/deployments",
+        data,
+      };
+      if (data.teamId) {
+        config.endpoint += `?teamId=${data.teamId}`;
+      }
+      delete data.teamId;
+      return this.makeRequest(config, $);
     },
   },
 };
