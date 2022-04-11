9bac9251ca0a9a8fca1a959bd6dfbb446c6684a4
Cassiano Vellames <cassiano.v@turing.com>
2022-04-11 15:02:07 -0300

    wip

--- components/google_tasks/actions/list-tasks/list-tasks.mjs
+++ components/google_tasks/actions/list-tasks/list-tasks.mjs
@@ -1,9 +1,9 @@
 import app from "../../google_tasks.app.mjs";
 
 export default {
-  key: "google_tasks-create-task",
-  name: "create task",
-  description: "create task [See the docs here]()",
+  key: "google_tasks-list-tasks",
+  name: "list tasks",
+  description: "list tasks [See the docs here]()",
   version: "0.0.1",
   type: "action",
   props: {
