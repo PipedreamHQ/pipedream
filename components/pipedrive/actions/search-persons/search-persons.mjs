eb7c34ab5c35a16899ba7d5847fc03b38e863855
Jorge Cortes <jacortesmahmud@gmail.com>
2022-04-11 16:32:28 -0500

    [ACTIONS/SOURCES] Pipedrive new actions and sources (#2306)
    * Refactor: centralized the use of axios in the app.mjs. Added consants and summary in responses
    
    * Refactored using pipedrive sdk and added 4 new sources
    
    * Removed unecessary try catch
    
    * End of line in package.json
    
    * Addressing comments, common properties and fixing links
    
    * Fixed some text and added async options with new function requests

--- components/pipedrive/actions/search-persons/search-persons.mjs
+++ components/pipedrive/actions/search-persons/search-persons.mjs
@@ -1,22 +1,14 @@
-// legacy_hash_id: a_4rixdN
-import { axios } from "@pipedream/platform";
+import pipedriveApp from "../../pipedrive.app.mjs";
+import constants from "../../common/constants.mjs";
 
 export default {
   key: "pipedrive-search-persons",
   name: "Search persons",
-  description: "Searches all Persons by name, email, phone, notes and/or custom fields. This endpoint is a wrapper of /v1/itemSearch with a narrower OAuth scope. Found Persons can be filtered by Organization ID.",
-  version: "0.1.1",
+  description: "Searches all Persons by `name`, `email`, `phone`, `notes` and/or custom fields. This endpoint is a wrapper of `/v1/itemSearch` with a narrower OAuth scope. Found Persons can be filtered by Organization ID. See the Pipedrive API docs [here](https://developers.pipedrive.com/docs/api/v1/Persons#searchPersons)",
+  version: "0.1.2",
   type: "action",
   props: {
-    pipedrive: {
-      type: "app",
-      app: "pipedrive",
-    },
-    companydomain: {
-      type: "string",
-      label: "Company domain",
-      description: "Your company name as registered in Pipedrive, which becomes part of Pipedrive API base url.",
-    },
+    pipedriveApp,
     term: {
       type: "string",
       label: "Search term",
@@ -27,67 +19,71 @@ export default {
       label: "Search fields",
       description: "A comma-separated string array. The fields to perform the search from. Defaults to all of them.",
       optional: true,
-      options: [
-        "custom_fields",
-        "email",
-        "notes",
-        "phone",
-        "name",
-      ],
+      options: constants.FIELD_OPTIONS,
     },
-    exact_match: {
+    exactMatch: {
       type: "boolean",
       label: "Exact match",
       description: "When enabled, only full exact matches against the given term are returned. It is not case sensitive.",
       optional: true,
     },
-    organization_id: {
-      type: "integer",
-      label: "Organization ID",
+    organizationId: {
+      propDefinition: [
+        pipedriveApp,
+        "organizationId",
+      ],
       description: "Will filter Deals by the provided Organization ID. The upper limit of found Deals associated with the Organization is 2000.",
-      optional: true,
     },
-    include_fields: {
+    includeFields: {
       type: "string",
-      label: "Optional fields",
+      label: "Include fields",
       description: "Supports including optional fields in the results which are not provided by default.",
       optional: true,
-      options: [
-        "person.picture",
-      ],
+      options: constants.INCLUDE_FIELDS_OPTIONS,
     },
     start: {
-      type: "integer",
-      label: "Pagination start",
-      description: "Pagination start. Note that the pagination is based on main results and does not include related items when using search_for_related_items parameter.",
-      optional: true,
+      propDefinition: [
+        pipedriveApp,
+        "start",
+      ],
     },
     limit: {
-      type: "integer",
-      label: "Limit",
-      description: "Items shown per page.",
-      optional: true,
+      propDefinition: [
+        pipedriveApp,
+        "limit",
+      ],
     },
   },
   async run({ $ }) {
-  //See the Pipedrive API docs here: https://developers.pipedrive.com/docs/api/v1/#!/Persons/get_persons_search
-    const config = {
-      method: "get",
-      url: `https://${this.companydomain}.pipedrive.com/v1/persons/search`,
-      params: {
-        term: this.term,
-        fields: this.fields,
-        exact_match: this.exact_match,
-        organization_id: this.organization_id,
-        include_fields: this.include_fields,
-        start: this.start,
-        limit: this.limit,
-      },
-      headers: {
-        Authorization: `Bearer ${this.pipedrive.$auth.oauth_access_token}`,
-      },
+    const {
+      term,
+      fields,
+      exactMatch,
+      organizationId,
+      includeFields,
+      start,
+      limit,
+    } = this;
 
-    };
-    return await axios($, config);
+    try {
+      const resp =
+        await this.pipedriveApp.searchPersons({
+          term,
+          fields,
+          exact_match: exactMatch,
+          org_id: organizationId,
+          include_fields: includeFields,
+          start,
+          limit,
+        });
+
+      $.export("$summary", `Successfully found ${resp.data?.items.length || 0} persons`);
+
+      return resp;
+
+    } catch (error) {
+      console.error(error.context?.body || error);
+      throw error.context?.body?.error || "Failed to search persons";
+    }
   },
 };
