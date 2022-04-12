2984df6e2cb41389ed3af7345aed4daacc7b5be3
Cassiano Vellames <cassiano.v@turing.com>
2022-04-12 14:49:42 -0300

    Create task list

--- components/google_tasks/google_tasks.app.mjs
+++ components/google_tasks/google_tasks.app.mjs
@@ -1,11 +1,38 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "google_tasks",
-  propDefinitions: {},
+  propDefinitions: {
+    title: {
+      type: "string",
+      label: "Title",
+      description: "The title of the task list.",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log((this.$auth));
+    _getBaseUrl() {
+      return "https://tasks.googleapis.com/tasks/v1";
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    _getRequestParams(opts = {}) {
+      return {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+    },
+    insertTaskList(ctx = this, data) {
+      return axios(ctx, this._getRequestParams({
+        path: "/users/@me/lists",
+        method: "POST",
+        data,
+      }));
     },
   },
 };
