42069ec1b80a2c1f836aec1ca15cfef238af6cc8
Cassiano Vellames <cassiano.v@turing.com>
2022-04-13 15:23:47 -0300

    wip

---
+++ components/google_tasks/actions/list-tasks-list/list-tasks-list.mjs
@@ -0,0 +1,29 @@
+import app from "../../google_tasks.app.mjs";
+
+export default {
+  key: "google_tasks-list-tasks-list",
+  name: "List Tasks List",
+  description: "Lists the authenticated user's task lists. [See the docs here](https://developers.google.com/tasks/reference/rest/v1/tasklists/list)",
+  version: "0.0.22",
+  type: "action",
+  props: {
+    app,
+    maxResults: {
+      propDefinition: [
+        app,
+        "maxResults",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const params = {
+      maxResults: this.maxResults,
+    };
+    const res = await this.app.paginate(
+      this.app.getTaskLists.bind(this),
+      params,
+    );
+    $.export("$summary", "Action successfully performed");
+    return res;
+  },
+};
--- components/google_tasks/google_tasks.app.mjs
+++ components/google_tasks/google_tasks.app.mjs
@@ -9,6 +9,31 @@ export default {
       label: "Title",
       description: "The title of the task list.",
     },
+    maxResults: {
+      type: "integer",
+      label: "Max Results",
+      description: "Maximum number of register to be fetched.",
+      default: 20,
+    },
+    taskListId: {
+      type: "string",
+      label: "Task List",
+      description: "The ID of the task list.",
+      async options({ prevContext }) {
+        const res = await this.getTaskLists(this, {
+          pageToken: prevContext.nextPageToken,
+        });
+        return {
+          context: {
+            nextPageToken: res.nextPageToken,
+          },
+          options: res.items.map((taskList) => ({
+            label: taskList.title,
+            value: taskList.id,
+          })),
+        };
+      },
+    },
   },
   methods: {
     _getBaseUrl() {
@@ -27,11 +52,55 @@ export default {
         headers: this._getHeaders(),
       };
     },
-    insertTaskList(ctx = this, data) {
+    async paginate(fn, params) {
+      let data = [];
+      let nextPageToken = null;
+      const TOTAL_MAX_RESULTS = Math.min(params.maxResults, 10000);
+      const ITEMS_PER_PAGE = Math.min(params.maxResults, 50);
+      do {
+        const pageResult = await fn(this, {
+          ...params,
+          maxResults: ITEMS_PER_PAGE,
+          pageToken: nextPageToken,
+        });
+        nextPageToken = pageResult.nextPageToken;
+        data = [
+          ...data,
+          ...pageResult.items,
+        ];
+
+        if (data.length >= TOTAL_MAX_RESULTS) {
+          data = data.slice(0, TOTAL_MAX_RESULTS);
+          break;
+        }
+      } while (nextPageToken);
+      return data;
+    },
+    async getTaskLists(ctx = this, params = {}) {
+      return axios(ctx, this._getRequestParams({
+        path: "/users/@me/lists",
+        method: "GET",
+        params,
+      }));
+    },
+    async insertTaskList(ctx = this, data) {
       return axios(ctx, this._getRequestParams({
         path: "/users/@me/lists",
         method: "POST",
         data,
+      }));
+    },
+    async updateTaskList(ctx = this, data) {
+      return axios(ctx, this._getRequestParams({
+        path: `/users/@me/lists/${data.id}`,
+        method: "PUT",
+        data,
+      }));
+    },
+    async deleteTaskList(ctx = this, id) {
+      return axios(ctx, this._getRequestParams({
+        path: `/users/@me/lists/${id}`,
+        method: "DELETE",
       }));
     },
   },
