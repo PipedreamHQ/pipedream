b11cfab81afb27238a8767aac81525b1ddc542e5
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-04-18 23:14:20 +0100

    Expand Bitlink completed

---
+++ components/bitly/actions/expand-bitlink/expand-bitlink.mjs
@@ -0,0 +1,31 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  key: "expand-bitlink",
+  name: "Expand a Bitlink",
+  description: "Retrieves information about Bitlink using ID.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    bitly: {
+      type: "app",
+      app: "bitly",
+    },
+    bitlink_id: {
+      type: "string",
+    },
+  },
+  async run({ $ }) {
+    return await axios($, {
+      method: "post",
+      url: "https://api-ssl.bitly.com/v4/expand",
+      headers: {
+        Authorization: `Bearer ${this.bitly.$auth.oauth_access_token}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        bitlink_id: this.bitlink_id,
+      },
+    });
+  },
+};
