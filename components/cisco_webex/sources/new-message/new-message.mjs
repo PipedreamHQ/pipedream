d18c2a3f606a0f12a1b23b38a35d3f89f8e9203b
Jorge Cortes <jacortesmahmud@gmail.com>
2022-04-18 09:28:55 -0500

    [ACTIONS/SOURCE] new actions and source with webhook (#2523)
    * Added new actions and source with webhook
    
    * Modified package.json
    
    * Addressed minor comments from CR

---
+++ components/cisco_webex/sources/new-message/new-message.mjs
@@ -0,0 +1,35 @@
+import constants from "../../common/constants.mjs";
+import common from "../common/webhook.mjs";
+
+export default {
+  ...common,
+  key: "cisco_webex-new-message",
+  name: "New Message (Instant)",
+  description: "Emit new event when a message is added. [See the docs here](https://developer.webex.com/docs/api/guides/webhooks#webex-webhooks)",
+  type: "source",
+  version: "0.0.1",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getWebhookName() {
+      return "New Message";
+    },
+    getResourceType() {
+      return constants.RESOURCE_TYPE.MESSAGES;
+    },
+    getEventType() {
+      return constants.EVENT_TYPE.CREATED;
+    },
+    getMetadata(body) {
+      const {
+        data: resource,
+        created,
+      } = body;
+      return {
+        id: resource.id,
+        summary: `New message created by ${resource.personEmail}`,
+        ts: Date.parse(created),
+      };
+    },
+  },
+};
