f1c2aafd556d60ab541bc65b5b48f27ab68eaa8c
Lucas Caresia <lucascarezia@gmail.com>
2022-04-18 21:52:46 -0300

    Create Tally actions and sources, and updated some .js files to .mjs

--- components/tally/tally.app.mjs
+++ components/tally/tally.app.mjs
@@ -1,11 +1,74 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "tally",
-  propDefinitions: {},
+  propDefinitions: {
+    forms: {
+      label: "Form",
+      description: "Select a form",
+      type: "string",
+      async options() {
+        const forms = await this.getForms();
+
+        return forms.map((form) => ({
+          label: form.name,
+          value: form.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _accessToken() {
+      return this.$auth.oauth_access_token;
+    },
+    _apiUrl() {
+      return "https://api.tally.so";
+    },
+    async _makeRequest(path, options = {}, $ = undefined) {
+      return await axios($ ?? this, {
+        url: `${this._apiUrl()}/${path}`,
+        headers: {
+          Authorization: `Bearer ${this._accessToken()}`,
+        },
+        ...options,
+      });
+    },
+    async _createWebhook(data) {
+      return await this._makeRequest("webhooks", {
+        method: "post",
+        data: {
+          ...data,
+          externalSubscriber: "PIPEDREAM",
+        },
+      });
+    },
+    async _removeWebhook(webhookId) {
+      return await this._makeRequest(`webhooks/${webhookId}`, {
+        method: "delete",
+      });
+    },
+    async getForms({ $ } = {}) {
+      return this._makeRequest("forms", {}, $);
+    },
+    async getResponses({
+      formId, page, $,
+    } = {}) {
+      return this._makeRequest(`forms/${formId}/responses`, {
+        params: {
+          page,
+        },
+      }, $);
+    },
+    async getFormFields({
+      formId, $,
+    } = {}) {
+      return this._makeRequest("integrations/output-fields", {
+        params: {
+          formId,
+          integrationType: "PIPEDREAM",
+        },
+      }, $);
     },
   },
 };
