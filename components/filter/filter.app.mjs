03ad0d230bfdf7139e19d54a14f95dbccfacc8c8
Andrew Chuang <andrewjschuang@gmail.com>
2022-04-20 10:14:46 -0300

    reduce number of methods and better error detection

--- components/filter/filter.app.mjs
+++ components/filter/filter.app.mjs
@@ -67,19 +67,19 @@ export default {
       case conditions.constants.IN:
         return this.checkIfIn(operand1, operand2);
       case conditions.constants.NOT_IN:
-        return this.checkIfNotIn(operand1, operand2);
+        return !this.checkIfIn(operand1, operand2);
       case conditions.constants.TEXT_EQUALS:
         return this.checkIfTextEquals(operand1, operand2);
       case conditions.constants.TEXT_NOT_EQUALS:
-        return this.checkIfTextNotEquals(operand1, operand2);
+        return !this.checkIfTextEquals(operand1, operand2);
       case conditions.constants.STARTS_WITH:
         return this.checkIfStartsWith(operand1, operand2);
       case conditions.constants.NOT_STARTS_WITH:
-        return this.checkIfNotStartsWith(operand1, operand2);
+        return !this.checkIfStartsWith(operand1, operand2);
       case conditions.constants.ENDS_WITH:
         return this.checkIfEndsWith(operand1, operand2);
       case conditions.constants.NOT_ENDS_WITH:
-        return this.checkIfNotEndsWith(operand1, operand2);
+        return !this.checkIfEndsWith(operand1, operand2);
       case conditions.constants.GREATER_THAN:
         return this.checkIfGreater(operand1, operand2);
       case conditions.constants.GREATER_THAN_EQUALS:
@@ -99,17 +99,17 @@ export default {
       case conditions.constants.TRUE:
         return this.checkIfTrue(operand1, operand2);
       case conditions.constants.FALSE:
-        return this.checkIfFalse(operand1, operand2);
+        return !this.checkIfTrue(operand1, operand2);
       case conditions.constants.IS_NULL:
         return this.checkIfIsNull(operand1);
       case conditions.constants.NOT_NULL:
-        return this.checkIfIsNotNull(operand1);
+        return !this.checkIfIsNull(operand1);
       case conditions.constants.KEY_EXISTS:
         return this.checkIfKeyExists(operand1, operand2);
       case conditions.constants.KEY_NOT_EXISTS:
-        return this.checkIfKeyNotExists(operand1, operand2);
+        return !this.checkIfKeyExists(operand1, operand2);
       default:
-        return false;
+        throw new Error("Condition operation not supported");
       }
     },
     checkIfIn(operand1, operand2) {
@@ -117,40 +117,20 @@ export default {
       operand2 = this.convertToString(operand2);
       return operand1.includes(operand2);
     },
-    checkIfNotIn(operand1, operand2) {
-      operand1 = this.convertToString(operand1);
-      operand2 = this.convertToString(operand2);
-      return !operand1.includes(operand2);
-    },
     checkIfTextEquals(operand1, operand2) {
       operand1 = this.convertToString(operand1);
       operand2 = this.convertToString(operand2);
       return operand1 === operand2;
-    },
-    checkIfTextNotEquals(operand1, operand2) {
-      operand1 = this.convertToString(operand1);
-      operand2 = this.convertToString(operand2);
-      return operand1 !== operand2;
     },
     checkIfStartsWith(operand1, operand2) {
       operand1 = this.convertToString(operand1);
       operand2 = this.convertToString(operand2);
       return operand1.startsWith(operand2);
     },
-    checkIfNotStartsWith(operand1, operand2) {
-      operand1 = this.convertToString(operand1);
-      operand2 = this.convertToString(operand2);
-      return !operand1.startsWith(operand2);
-    },
     checkIfEndsWith(operand1, operand2) {
       operand1 = this.convertToString(operand1);
       operand2 = this.convertToString(operand2);
       return operand1.endsWith(operand2);
-    },
-    checkIfNotEndsWith(operand1, operand2) {
-      operand1 = this.convertToString(operand1);
-      operand2 = this.convertToString(operand2);
-      return !operand1.endsWith(operand2);
     },
     checkIfGreater(operand1, operand2) {
       operand1 = this.convertToNumber(operand1);
@@ -178,45 +158,32 @@ export default {
       return operand1 === operand2;
     },
     checkIfAfter(operand1, operand2) {
-      operand1 = this.convertToDatetime(operand1);
-      operand2 = this.convertToDatetime(operand2);
+      operand1 = this.convertToDateTime(operand1);
+      operand2 = this.convertToDateTime(operand2);
       return operand1 > operand2;
     },
     checkIfBefore(operand1, operand2) {
-      operand1 = this.convertToDatetime(operand1);
-      operand2 = this.convertToDatetime(operand2);
+      operand1 = this.convertToDateTime(operand1);
+      operand2 = this.convertToDateTime(operand2);
       return operand1 < operand2;
     },
     checkIfDateEquals(operand1, operand2) {
-      operand1 = this.convertToDatetime(operand1);
-      operand2 = this.convertToDatetime(operand2);
+      operand1 = this.convertToDateTime(operand1);
+      operand2 = this.convertToDateTime(operand2);
       return operand1.getTime() === operand2.getTime();
     },
     checkIfTrue(operand1) {
       operand1 = this.convertToBoolean(operand1);
       return operand1;
     },
-    checkIfFalse(operand1) {
-      operand1 = this.convertToBoolean(operand1);
-      return !operand1;
-    },
     checkIfIsNull(operand1) {
       operand1 = this.convertToString(operand1);
       return (operand1 === "null" || operand1 === "undefined");
     },
-    checkIfIsNotNull(operand1) {
-      operand1 = this.convertToString(operand1);
-      return (operand1 !== "null" && operand1 !== "undefined");
-    },
     checkIfKeyExists(operand1, operand2) {
       operand1 = this.convertToObject(this.convertToString(operand1));
       operand2 = this.convertToString(operand2);
-      return operand2 in operand1 && this.checkIfIsNotNull(operand1[operand2]);
-    },
-    checkIfKeyNotExists(operand1, operand2) {
-      operand1 = this.convertToObject(this.convertToString(operand1));
-      operand2 = this.convertToString(operand2);
-      return !(operand2 in operand1) || this.checkIfIsNull(operand1[operand2]);
+      return operand2 in operand1 && !this.checkIfIsNull(operand1[operand2]);
     },
     convertToString(input) {
       return input.toString();
@@ -238,12 +205,20 @@ export default {
       }
       throw new Error("Input cannot be converted to a boolean");
     },
-    convertToDatetime(input) {
-      input = this.convertToNumber(input);
-      return new Date(input);
+    convertToDateTime(input) {
+      input = new Date(this.convertToNumber(input));
+      if (isNaN(input)) {
+        throw new Error("Input cannot be converted to datetime");
+      }
+      return input;
     },
     convertToObject(input) {
-      return JSON.parse(input);
+      try {
+        input = this.convertToString(input);
+        return JSON.parse(input);
+      } catch (e) {
+        throw new Error("Input cannot be converted to an object:", e);
+      }
     },
   },
 };
