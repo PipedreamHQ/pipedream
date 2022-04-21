3e05db7b00c3e17281e6c046db9f92a76f26ced0
Danny Roosevelt <danny@pipedream.com>
2022-04-21 08:50:43 -0700

    Update get-bucket.mjs

--- components/google_cloud/actions/get-bucket/get-bucket.mjs
+++ components/google_cloud/actions/get-bucket/get-bucket.mjs
@@ -1,11 +1,11 @@
 import googleCloud from "../../google_cloud.app.mjs";
 
 export default {
-  name: "Get Bucket Metadata [See the docs](https://googleapis.dev/nodejs/storage/latest/Bucket.html#getMetadata)",
+  name: "Get Bucket Metadata",
   version: "0.0.1",
   key: "google_cloud-get-bucket",
   type: "action",
-  description: "Gets Google Cloud Storage bucket metadata",
+  description: "Gets Google Cloud Storage bucket metadata. [See the docs](https://googleapis.dev/nodejs/storage/latest/Bucket.html#getMetadata).",
   props: {
     googleCloud,
     bucketName: {
