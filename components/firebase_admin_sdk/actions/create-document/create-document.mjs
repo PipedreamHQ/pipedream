146802e44c9aafd8bbba59a34b5f0c79ef2edef0
michelle0927 <michelle0927@users.noreply.github.com>
2022-04-21 14:10:16 -0400

    Firebase/Firestore new actions (#2311)
    * adding actions
    
    * adding actions
    
    * updates
    
    * updates
    
    * remove unecessary deleteApp method
    
    * update create-realtime-db-record

---
+++ components/firebase_admin_sdk/actions/create-document/create-document.mjs
@@ -0,0 +1,34 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "firebase_admin_sdk-create-document",
+  name: "Create Document",
+  description: "Creates a New Document. [See the docs here](https://googleapis.dev/nodejs/firestore/latest/CollectionReference.html#add)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    collection: {
+      propDefinition: [
+        common.props.firebase,
+        "collection",
+      ],
+    },
+    data: {
+      propDefinition: [
+        common.props.firebase,
+        "data",
+      ],
+    },
+  },
+  methods: {
+    ...common.methods,
+    async getResponse() {
+      return this.firebase.createDocument(this.collection, this.data);
+    },
+    emitSummary($, response) {
+      $.export("$summary", `Successfully added document ${response._path.segments[1]}`);
+    },
+  },
+};
