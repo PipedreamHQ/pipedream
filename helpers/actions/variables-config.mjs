7023fc39966ed7e04422e5b41f2af1a3ac40677d
Jorge Cortes <jacortesmahmud@gmail.com>
2022-04-22 12:59:38 -0500

    Added config action to setup variables for a workflow

---
+++ helpers/actions/variables-config.mjs
@@ -0,0 +1,18 @@
+export default {
+  name: "Variables",
+  description: "Configure variables for use in your workflow",
+  key: "helpers-variables-config",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    config: {
+      type: "object",
+      label: "Configuration",
+      description: "Enter key-value pairs that you'd like to reference throughout your workflow.",
+    },
+  },
+  run({ $ }) {
+    $.export("config", this.config);
+    return this.config;
+  },
+};
