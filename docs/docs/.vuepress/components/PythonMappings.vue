5425d36acd1224b0ee13462eb12e171bda3c656b
Dylan Pierce <me@dylanjpierce.com>
2022-04-25 11:51:31 -0400

    Adding form & basic submission support

---
+++ docs/docs/.vuepress/components/PythonMappings.vue
@@ -0,0 +1,151 @@
+<style lang="stylus"></style>
+
+<template>
+  <div class="md:w-3/4 m-auto">
+    <div
+      class="
+        bg-gray-100
+        py-8
+        px-4
+        w-full
+        border-l-8 border-green-500 border-solid
+      "
+    >
+      <h3 class="text-bold text-xl text-green-500 mb-2">
+        Don't see a package listed?
+      </h3>
+      Request to have a custom package mapping added:
+      <form
+        class="grid grid-cols-12"
+        action="https://eo7sttetfpbyysz.m.pipedream.net"
+        method="post"
+      >
+        <input
+          v-model="requested_package"
+          placeholder="PyPi package name"
+          class="
+            my-3
+            px-3
+            py-3
+            placeholder-slate-300
+            text-slate-600
+            relative
+            bg-white
+            rounded-l
+            text-sm
+            border-0
+            shadow
+            outline-none
+            focus:outline-none focus:ring
+            w-full
+            m-auto
+            col-span-10
+          "
+        />
+        <button
+          :disabled="!requested_package"
+          class="
+            bg-green-500
+            border-green-500
+            text-white
+            font-bold
+            border-4
+            py-3
+            my-3
+            col-span-2
+            rounded-r
+            disabled:opacity-50
+          "
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+    <div class="m-auto my-8">
+      <label class="text-sm font-semibold text-gray-400 my-4"
+        >Search for PyPi package</label
+      >
+      <input
+        v-model="query"
+        placeholder="PyPi package name"
+        class="
+          my-3
+          px-3
+          py-3
+          placeholder-slate-300
+          text-slate-600
+          relative
+          bg-white
+          rounded
+          text-sm
+          border-0
+          shadow
+          outline-none
+          focus:outline-none focus:ring
+          w-full
+          m-auto
+        "
+      />
+    </div>
+    <table class="m-auto table w-full">
+      <tr>
+        <th>PyPi Package Name</th>
+        <th>Import into Pipedream with</th>
+      </tr>
+      <tr v-for="mapping in mappings" :key="mapping[0]">
+        <td>
+          <span class="font-mono text-lg">{{ mapping[0] }}</span>
+          <p>
+            <a
+              target="_blank"
+              class="text-sm external"
+              :href="`https://pypi.org/project/${mapping[0]}`"
+              >PyPi Project
+
+              <OutboundLink />
+            </a>
+          </p>
+        </td>
+        <td>
+          <code>import {{ mapping[1] }}</code>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import orderBy from "lodash/orderBy";
+import rawMappings from "./python-mappings";
+
+export default {
+  name: "PythonMappings",
+  data() {
+    return {
+      mappings: [],
+      query: "",
+      requested_package: "",
+    };
+  },
+  watch: {
+    query(newQuery, oldQuery) {
+      this.search(newQuery);
+    },
+  },
+  methods: {
+    search(query) {
+      if (query === "") {
+        this.mappings = Object.entries(rawMappings);
+      }
+
+      this.mappings = this.mappings.filter((mapping) =>
+        mapping[0].startsWith(query)
+      );
+    },
+  },
+  mounted() {
+    this.mappings = Object.entries(rawMappings);
+  },
+};
+</script>
+
---
+++ docs/docs/code/python/import-mappings/README.md
@@ -0,0 +1,11 @@
+---
+content_class: max-w-max
+---
+
+# Use PyPi packages with differing import names
+
+Some Python PyPi package names do not match their import names. However, you can still import them into your Python steps with a specialized mapping.
+
+Search for the PyPi package you're intending to use, and use the shorthand `import module` shortcut to import them into a Python step.
+
+<PythonMappings />
