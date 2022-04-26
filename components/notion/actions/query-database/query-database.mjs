bfad94c2e735113c8336f6888690c821689b35a5
vunguyenhung <vunguyenhung@outlook.com>
2022-04-26 21:09:37 +0700

    Address comments

--- components/notion/actions/query-database/query-database.mjs
+++ components/notion/actions/query-database/query-database.mjs
@@ -16,19 +16,22 @@ export default {
     },
     filter: {
       type: "object",
-      description: "Filter Pages in the Database [See the docs](https://developers.notion.com/reference/post-database-query-filter)",
+      description: "A filter is a single condition used to specify and limit the entries returned from a database query. Database queries can be filtered by page property values. [See the docs](https://developers.notion.com/reference/post-database-query-filter)",
       optional: true
     },
     sorts: {
       type: "object",
-      description: "Sort Pages in the Database [See the docs](https://developers.notion.com/reference/post-database-query-sort)",
+      description: "A sort is a condition used to order the entries returned from a database query. [See the docs](https://developers.notion.com/reference/post-database-query-sort)",
       optional: true
     }
   },
   async run({ $ }) {
-    const params = { filter, sorts }
+    const params = { 
+      filter: this.filter,
+      sorts: this.sorts
+    }
     const response = await this.notion.queryDatabase(this.databaseId, params);
-    $.export("$summary", "Query Database successfully");
+    $.export("$summary", "Query Database successfully completed");
 
     return response;
   },
