bbddb4c1f3fd5411f1cd8239b01fcc586b3896c1
vunguyenhung <vunguyenhung@outlook.com>
2022-04-26 21:48:01 +0700

    Add Banner Beare create image action

---
+++ components/bannerbear/actions/create-image/create-image.mjs
@@ -0,0 +1,40 @@
+import { axios } from "@pipedream/platform";
+
+import bannerbear from '../../bannerbear.app.mjs'
+
+export default {
+  key: "bannerbear-create-image",
+  name: "Create an Image",
+  description: "Create an image. [See the docs](https://developers.bannerbear.com/#post-v2-images)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    bannerbear,
+    template: {
+      propDefinition: [
+        bannerbear,
+        "template",
+      ],
+    },
+    modifications: {
+      type: 'object',
+      description: 'A list of modifications you want to make. [See the docs](https://developers.bannerbear.com/#post-v2-images)'
+    }
+  },
+  async run({ $ }) {
+    const response = await axios($, {
+      method: 'POST',
+      url: 'https://sync.api.bannerbear.com/v2/images',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${bannerbear.getAuthKey()}`
+      },
+      data: {
+        template: this.template,
+        modifications: this.modifications
+      }
+    })
+
+    return response;
+  },
+};
