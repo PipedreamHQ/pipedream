d20f70707e741ec1eef13e49ed978790765d24f5
Andrew Chuang <andrewjschuang@gmail.com>
2022-04-27 13:21:09 -0300

    add team and project to sources

--- components/linear/sources/issue-created-instant/issue-created-instant.mjs
+++ components/linear/sources/issue-created-instant/issue-created-instant.mjs
@@ -1,11 +1,8 @@
-import linearApp from "../../linear.app.mjs";
+import common from "../common/webhook-props.mjs";
 import issueCreatedInstant from "../../../linear_app/sources/issue-created-instant/issue-created-instant.mjs";
 
-const {
-  // eslint-disable-next-line no-unused-vars
-  linearApp: app,
-  ...otherProps
-} = issueCreatedInstant.props;
+/* eslint-disable pipedream/required-properties-type */
+/* eslint-disable pipedream/required-properties-name */
 
 export default {
   ...issueCreatedInstant,
@@ -13,7 +10,6 @@ export default {
   description: "Emit new event when a new issue is created (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/webhooks)",
   version: "0.0.1",
   props: {
-    ...otherProps,
-    linearApp,
+    ...common.props,
   },
 };
--- components/linear/sources/issue-updated-instant/issue-updated-instant.mjs
+++ components/linear/sources/issue-updated-instant/issue-updated-instant.mjs
@@ -1,11 +1,8 @@
-import linearApp from "../../linear.app.mjs";
+import common from "../common/webhook-props.mjs";
 import issueUpdatedInstant from "../../../linear_app/sources/issue-updated-instant/issue-updated-instant.mjs";
 
-const {
-  // eslint-disable-next-line no-unused-vars
-  linearApp: app,
-  ...otherProps
-} = issueUpdatedInstant.props;
+/* eslint-disable pipedream/required-properties-type */
+/* eslint-disable pipedream/required-properties-name */
 
 export default {
   ...issueUpdatedInstant,
@@ -13,7 +10,6 @@ export default {
   description: "Emit new event when an issue is updated (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/webhooks)",
   version: "0.0.1",
   props: {
-    ...otherProps,
-    linearApp,
+    ...common.props,
   },
 };
