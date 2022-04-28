b22e9c8a048b811c97cfc0470117cb2e92aa3135
Andrew Chuang <andrewjschuang@gmail.com>
2022-04-28 13:46:10 -0300

    refactor create document action

--- components/google_docs/actions/create-document/create-document.mjs
+++ components/google_docs/actions/create-document/create-document.mjs
@@ -4,18 +4,15 @@ export default {
   key: "google_docs-create-document",
   name: "Create a New Document",
   description: "Create a new, empty document. To add content after creating the document, pass the document ID exported by this step to the Append Text action.",
-  version: "0.0.8",
+  version: "0.1.0",
   type: "action",
   props: {
     googleDocs,
     title: "string",
   },
-  async run() {
-    const docs = this.googleDocs.docs();
-    return (await docs.documents.create({
-      requestBody: {
-        title: this.title,
-      },
-    })).data;
+  async run({ $ }) {
+    const createdDoc = await this.googleDocs.createEmptyDoc(this.title);
+    $.export("summary", "Successfully created doc");
+    return createdDoc;
   },
 };
