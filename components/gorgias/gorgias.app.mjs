1914fe2d1667b3164c909302bc81a823fff98609
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-02 12:17:57 -0300

    add create ticket action

--- components/gorgias/gorgias.app.mjs
+++ components/gorgias/gorgias.app.mjs
@@ -1,4 +1,5 @@
 import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
 
 export default {
   type: "app",
@@ -26,6 +27,27 @@ export default {
         };
       },
     },
+    address: {
+      type: "string",
+      label: "Address",
+      description: "Actual address of the entry. Can be an email, facebook id, phone number, etc",
+    },
+    channel: {
+      type: "string",
+      label: "Channel",
+      description: "The channel used to send the message. Defaults to `email`",
+      optional: true,
+      default: "email",
+      options: constants.channels,
+    },
+    via: {
+      type: "string",
+      label: "Via",
+      description: "How the message has been received, or sent from Gorgias. Defaults to `help-center`",
+      optional: true,
+      default: "help-center",
+      options: constants.vias,
+    },
     limit: {
       type: "integer",
       label: "Limit",
@@ -41,22 +63,30 @@ export default {
       };
     },
     _defaultConfig({
-      path, method = "get", params = {},
+      path, method = "get", params = {}, data = null,
     }) {
-      return {
+      const config = {
         auth: this._auth(),
         url: `https://${this.$auth.domain}/api/${path}`,
+        headers: {
+          "Content-type": "application/json",
+        },
         method,
         params,
       };
+      if (data) {
+        config.data = data;
+      }
+      return config;
     },
     async _makeRequest({
-      path, method, params,
+      path, method, params, data,
     }) {
       const config = this._defaultConfig({
         path,
         method,
         params,
+        data,
       });
       return axios(this, config);
     },
@@ -89,6 +119,13 @@ export default {
       return this._makeRequest({
         path: "/customers",
         params,
+      });
+    },
+    async createTicket(data) {
+      return this._makeRequest({
+        path: "/tickets",
+        method: "post",
+        data,
       });
     },
     async listTickets(params) {
