d0b10d1b70d9dcb0262642ee06bfe76af8688183
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-02 19:09:16 -0300

    add trigger event with json action

--- components/ifttt/ifttt.app.mjs
+++ components/ifttt/ifttt.app.mjs
@@ -1,11 +1,74 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "ifttt",
-  propDefinitions: {},
+  propDefinitions: {
+    webhookKey: {
+      type: "string",
+      label: "Webhook Key",
+      description: "The webhook key obtained in [Webhook Settings](https://ifttt.com/maker_webhooks/settings)",
+    },
+    eventName: {
+      type: "string",
+      label: "Event Name",
+      description: "The event name associated with your applet",
+    },
+    data: {
+      type: "object",
+      label: "JSON Payload",
+      description: "A JSON payload to be sent",
+    },
+    method: {
+      type: "string",
+      label: "HTTP Method",
+      description: "The HTTP method to be used. Defaults to `post`",
+      options: [
+        "get",
+        "post",
+      ],
+      optional: true,
+      default: "post",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _defaultHeaders() {
+      return {
+        "IFTTT-Service-Key": this.$auth.ifttt_Service_Key,
+        "Content-Type": "application/json",
+      };
+    },
+    async _makeRequest({
+      $, opts,
+    }) {
+      return axios($ ?? this, {
+        headers: this._defaultHeaders(),
+        ...opts,
+      });
+    },
+    async _callWebhook({
+      $, url, method, data,
+    }) {
+      const opts = {
+        url,
+        method,
+        data,
+      };
+      return this._makeRequest({
+        $,
+        opts,
+      });
+    },
+    async callWebhookWithJSON({
+      $, webhookKey, eventName, method, data,
+    }) {
+      const url = encodeURI(`https://maker.ifttt.com/trigger/${eventName}/json/with/key/${webhookKey}`);
+      return this._callWebhook({
+        $,
+        url,
+        method,
+        data,
+      });
     },
   },
 };
