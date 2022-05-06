84da044e4f2f0feaadc91142b20793f64b0e3352
Lucas Caresia <lucascarezia@gmail.com>
2022-05-06 14:54:57 -0300

    Fixed action version

--- components/asana/sources/updated-task-in-project/updated-task-in-project.mjs
+++ components/asana/sources/updated-task-in-project/updated-task-in-project.mjs
@@ -7,7 +7,7 @@ export default {
   type: "source",
   name: "New Update To Task (Instant)",
   description: "Emit new event for each update to a task.",
-  version: "0.1.0",
+  version: "0.1.1",
   dedupe: "unique",
   props: {
     ...common.props,
