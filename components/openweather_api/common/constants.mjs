64f8de61335f235df48842cc6091817dd07176ca
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-05-06 10:18:49 +0100

    Created HTTP functions

--- components/openweather_api/common/constants.mjs
+++ components/openweather_api/common/constants.mjs
@@ -1,9 +1,27 @@
 const BASE_URL = "https://api.openweathermap.org";
-const DATA_PATH = "/data";
-const VERSION_PATH = "/2.5";
+
+const GEO_PATH = "/geo";
+const GEO_VERSION_PATH = "/1.0";
+
+const WEATHER_PATH = "/data";
+const WEATHER_VERSION_PATH = "/2.5";
+
+const FORCAST_PATH = "/data";
+const FORCAST_VERSION_PATH = "/2.5/forecast";
+
+const Category = {
+  GEO: 1,
+  WEATHER: 2,
+  FORCAST: 3,
+};
 
 export default {
   BASE_URL,
-  DATA_PATH,
-  VERSION_PATH,
+  GEO_PATH,
+  GEO_VERSION_PATH,
+  WEATHER_PATH,
+  WEATHER_VERSION_PATH,
+  FORCAST_PATH,
+  FORCAST_VERSION_PATH,
+  Category,
 };
--- components/openweather_api/openweather_api.app.mjs
+++ components/openweather_api/openweather_api.app.mjs
@@ -1,11 +1,91 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "openweather_api",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _authToken() {
+      return this.$auth.oauth_access_token;
+    },
+    getHeader() {
+      return {
+        "Content-Type": "application/json",
+      };
+    },
+    getLocationAPI() {
+      const {
+        GEO_PATH,
+        GEO_VERSION_PATH,
+      } = constants;
+      return GEO_PATH + GEO_VERSION_PATH;
+    },
+    getWeatherAPI() {
+      const {
+        WEATHER_PATH,
+        WEATHER_VERSION_PATH,
+      } = constants;
+      return WEATHER_PATH + WEATHER_VERSION_PATH;
+    },
+    getForcastAPI() {
+      const {
+        FORCAST_PATH,
+        FORCAST_VERSION_PATH,
+      } = constants;
+      return FORCAST_PATH + FORCAST_VERSION_PATH;
+    },
+    getAPIUrl(category, path) {
+      const {
+        BASE_URL,
+        Category,
+      } = constants;
+      const route = (category === Category.GEO && this.getLocationAPI() )
+      || (category === Category.WEATHER && this.getWeatherAPI() )
+      || (category === Category.FORCAST && this.getForcastAPI() );
+      return BASE_URL + route + path;
+    },
+    async makeRequest(args = {}) {
+      const {
+        $ = this,
+        method = "get",
+        path,
+        params,
+        data,
+        category,
+      } = args;
+      const config = {
+        method,
+        url: this.getUrl(category, path),
+        headers: this.getHeader(),
+        params,
+        data,
+      };
+      return axios($, config);
+    },
+    async getLocationCordinate(params) {
+      const {  Category } = constants;
+      return this.makeRequest({
+        path: "/direct",
+        params,
+        category: Category.GEO,
+      });
+    },
+    async getCurrentWeather(params) {
+      const {  Category } = constants;
+      return this.makeRequest({
+        path: "/weather",
+        params,
+        category: Category.WEATHER,
+      });
+    },
+    async getDailyWeatherForcast(params) {
+      const {  Category } = constants;
+      return this.makeRequest({
+        path: "/daily",
+        params,
+        category: Category.FORCAST,
+      });
     },
   },
 };
