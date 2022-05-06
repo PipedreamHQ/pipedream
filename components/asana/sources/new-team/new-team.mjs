3873ef9a2b99101cf105cfdc2a04798f59cb5d88
Lucas Caresia <lucascarezia@gmail.com>
2022-05-06 14:31:11 -0300

    [SOURCE] Asana: New Tags, New Webhook Events #1036 (#2487)
    * Asana actions refactored
    
    * Refactored the sources of Asana
    
    * Changed requested done! (source), some changes will be done in action branch
    
    * removed changes to actions
    
    * remove action delete-task
    
    * Requested changes done!
    
    * Refactored sources to add common.mjs file
    
    * Updating versions
    
    * Added await to emitEvent call method
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/asana/sources/new-team/new-team.mjs
@@ -0,0 +1,42 @@
+import asana from "../../asana.app.mjs";
+
+export default {
+  key: "asana-new-team",
+  type: "source",
+  name: "New Team",
+  description: "Emit new event for each task added to an organization.",
+  version: "0.1.0",
+  dedupe: "unique",
+  props: {
+    asana,
+    organization: {
+      label: "Organization",
+      description: "Gid of a organization.",
+      type: "string",
+      propDefinition: [
+        asana,
+        "organizations",
+      ],
+    },
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15,
+      },
+    },
+  },
+
+  async run() {
+    const teams = await this.asana.getTeams(this.organization);
+
+    for (let team of teams) {
+      team = await this.asana.getTeam(team.gid);
+
+      this.$emit(team, {
+        id: team.gid,
+        summary: team.name,
+        ts: Date.now(),
+      });
+    }
+  },
+};
