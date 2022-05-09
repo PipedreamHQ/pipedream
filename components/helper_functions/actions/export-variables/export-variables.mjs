14ec72224ac2430d4cbec39a968bb2e8fb09bead
Jorge Cortes <jacortesmahmud@gmail.com>
2022-05-09 18:34:24 -0500

    Added parse method in case of custom expression input and added package.json file

--- components/helper_functions/actions/export-variables/export-variables.mjs
+++ components/helper_functions/actions/export-variables/export-variables.mjs
@@ -1,4 +1,5 @@
-import helperFunctionsApp from "../../helper_functions.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+import helperFunctions from "../../helper_functions.app.mjs";
 
 export default {
   key: "helper_functions-export-variables",
@@ -7,14 +8,33 @@ export default {
   version: "0.0.1",
   type: "action",
   props: {
-    helperFunctionsApp,
+    helperFunctions,
     config: {
       type: "object",
       label: "Configuration",
       description: "Enter key-value pairs that you'd like to reference throughout your workflow.",
     },
   },
+  methods: {
+    emptyStrToUndefined(value) {
+      const trimmed = typeof(value) === "string" && value.trim();
+      return trimmed === ""
+        ? undefined
+        : value;
+    },
+    parse(value) {
+      const valueToParse = this.emptyStrToUndefined(value);
+      if (typeof(valueToParse) === "object" || valueToParse === undefined) {
+        return valueToParse;
+      }
+      try {
+        return JSON.parse(valueToParse);
+      } catch (e) {
+        throw new ConfigurationError("Make sure the custom expression contains a valid object");
+      }
+    },
+  },
   run({ $ }) {
-    $.export("config", this.config);
+    $.export("config", this.parse(this.config));
   },
 };
