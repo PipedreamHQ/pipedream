3c6139d5d454b63fec6b5d329e46367dc1fea86b
Dylan J. Sather <dylan.sather@gmail.com>
2022-05-08 23:00:07 -0400

    Adding dtslint

--- types/types.test.ts
+++ types/types.test.ts
@@ -1,3 +1,3 @@
-// Source methods the right `this`
-// Source run has the right `this`
+// Source methods have the right `this`
+// Source run have the right `this`
 // Source hooks have the right `this`
