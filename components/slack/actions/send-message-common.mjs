bc6a9bdf8f9309cac8c264d67d05e033304f39e8
Lucas Caresia <lucascarezia@gmail.com>
2022-05-10 11:55:25 -0300

    Resolved the problem with markdwn

--- components/slack/actions/send-message-common.mjs
+++ components/slack/actions/send-message-common.mjs
@@ -83,16 +83,30 @@ export default {
   async run() {
     let blocks = this.blocks;
 
+    // if (this.include_sent_via_pipedream_flag) {
+    // const sentViaPipedreamText = this._makeSentViaPipedreamBlock();
+
+    // if (!blocks) {
+    //   blocks = [
+    //     this._makeTextBlock(this.mrkdwn),
+    //   ];
+    // } else if (typeof blocks === "string") {
+    //   blocks = JSON.parse(blocks);
+    // }
+
+    // blocks.push(sentViaPipedreamText);
+    // }
+
+    if (!blocks) {
+      blocks = [
+        this._makeTextBlock(this.mrkdwn),
+      ];
+    } else if (typeof blocks === "string") {
+      blocks = JSON.parse(blocks);
+    }
+
     if (this.include_sent_via_pipedream_flag) {
       const sentViaPipedreamText = this._makeSentViaPipedreamBlock();
-
-      if (!blocks) {
-        blocks = [
-          this._makeTextBlock(this.mrkdwn),
-        ];
-      } else if (typeof blocks === "string") {
-        blocks = JSON.parse(blocks);
-      }
 
       blocks.push(sentViaPipedreamText);
     }
@@ -114,6 +128,24 @@ export default {
       reply_broadcast: this.reply_broadcast,
       thread_ts: this.thread_ts,
     };
+
+    console.log({
+      text: this.text,
+      channel: this.conversation ?? this.reply_channel,
+      attachments: this.attachments,
+      unfurl_links: this.unfurl_links,
+      unfurl_media: this.unfurl_media,
+      parse: this.parse,
+      as_user: this.as_user,
+      username: this.username,
+      icon_emoji: this.icon_emoji,
+      icon_url: this.icon_url,
+      mrkdwn: this.mrkdwn,
+      blocks,
+      link_names: this.link_names,
+      reply_broadcast: this.reply_broadcast,
+      thread_ts: this.thread_ts,
+    });
 
     if (this.post_at) {
       obj.post_at = this.post_at;
