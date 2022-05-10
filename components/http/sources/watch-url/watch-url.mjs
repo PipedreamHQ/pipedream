fa264c0bb13ef9eb44ca2f04921e6e1992dbc9fd
Alyson Freitas <alys.freitas@gmail.com>
2022-05-10 10:52:27 -0300

    using httpRequest to watch URL

--- components/http/sources/watch-url/watch-url.mjs
+++ components/http/sources/watch-url/watch-url.mjs
@@ -1,15 +1,15 @@
 import http from "../../http.app.mjs";
-import axios from "axios";
 import hash from "object-hash";
-import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   key: "http-watch-url",
-  name: "New item from Watching URL",
-  description: "Emit new item when the response of the watched URL changes.",
+  name: "New event when the content of an URL changes.",
+  description: "Emit new event when the content of an URL changes.",
   version: "0.0.1",
   type: "source",
+  dedupe: "unique",
   props: {
+    http,
     timer: {
       type: "$.interface.timer",
       label: "Watching timer",
@@ -18,56 +18,26 @@ export default {
         intervalSeconds: 60 * 15,
       },
     },
-    method: {
-      label: "Method",
-      description: "The configured HTTP Method for this request.",
-      type: "string",
-      default: "GET",
-      options: [
-        "GET",
-        "POST",
-        "PUT",
-        "DELETE",
-      ],
-    },
-    url: {
-      label: "URL",
-      description: "The watched URL.",
-      type: "string",
-    },
-    params: {
-      label: "Query params",
-      description: "Data to be sent on the `Request URL` as query parameters.",
-      type: "object",
-      optional: true,
-      default: {},
-    },
-    body: {
-      label: "Body",
-      description: "Data to be sent on the `Request Body`.",
-      type: "string",
-      optional: true,
-    },
-    headers: {
-      label: "Headers",
-      description: "Headers to be sent on the `Request Headers`.",
-      type: "object",
-      optional: true,
-      default: {},
-    },
-    emitAsArray: {
-      label: "Emit as array",
-      description: "Is expected that the provided URL should return a JSON Array, on this case this source will emit changes individually for each item of the array.",
-      type: "boolean",
-      optional: false,
+    httpRequest: {
+      type: "http_request",
+      label: "HTTP Request Configuration",
+      description: "HTTP Request Configuration",
+      default: {
+        method: "GET",
+      },
     },
     emitBodyOnly: {
       label: "Emit body only",
-      description: "If set as true the emitted item will contain only the response body, otherwise, it will be included the request status information.",
+      description: "If set as true the emitted item will contain only the response body, otherwise, it will include the request status code.",
       type: "boolean",
-      optional: false,
+      default: true,
     },
-    http,
+    emitAsArray: {
+      label: "Emit as array",
+      description: "If the request responds with an array, this source will emit changes individually for each item of the array.",
+      type: "boolean",
+      default: false,
+    },
   },
   methods: {
     getMeta(data, status) {
@@ -81,7 +51,7 @@ export default {
     },
     emitArray(event, response) {
       if (!Array.isArray(response.data)) {
-        throw new ConfigurationError("Response is not a JSON Array.");
+        return this.emitAny(event, response);
       }
       for (const item of response.data) {
         const meta = this.getMeta(item, response.status);
@@ -93,30 +63,16 @@ export default {
       this.emit(event, meta);
     },
     emit(event, meta) {
-      const timestamp = event.timestamp || Math.round(Date.now() / 1000);
-      const summary = `${timestamp} ${this.method} ${this.url} `;
+      const ts = event.timestamp || Math.round(Date.now() / 1000);
       this.$emit(meta, {
-        summary,
         id: hash(meta),
+        summary: `Requested at ${ts}`,
+        ts,
       });
     },
   },
   async run(event) {
-    const headers = {
-      "User-Agent": "pipedream/1",
-      ...this.headers,
-    };
-    const data = this.body || undefined;
-    const params = this.params || undefined;
-
-    const response = await axios({
-      method: this.method,
-      url: this.url,
-      params,
-      data,
-      headers,
-    });
-
+    const response = await this.httpRequest.execute();
     if (this.emitAsArray) {
       this.emitArray(event, response);
     } else {
