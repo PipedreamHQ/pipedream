afcbdf914ece8240597856c1febb856c5cf2a454
Dylan J. Sather <dylan.sather@gmail.com>
2022-05-09 18:01:42 -0700

    Adding export * for helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,14 +1,2 @@
-import sanitizedArray from "./sanitizedArray";
-import {
-  HTTPError, NoProtocolError, createHTTPError, generateHTTPErrorClasses,
-} from "./errors";
-
-export const helpers = {
-  sanitizedArray,
-  HTTPError,
-  NoProtocolError,
-  createHTTPError,
-  generateHTTPErrorClasses,
-};
-
-export default helpers;
+export * from "./sanitizedArray";
+export * from "./errors";
