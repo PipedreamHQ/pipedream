6931487456fe03ab2f3c955c7858210c1e05b58c
Luan Cazarine <luanhc@gmail.com>
2022-05-11 16:19:38 -0300

    Update components/survey_monkey/actions/find-survey/find-survey.mjs
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/survey_monkey/actions/find-survey/find-survey.mjs
+++ components/survey_monkey/actions/find-survey/find-survey.mjs
@@ -3,7 +3,7 @@ import surveyMonkey from "../../survey_monkey.app.mjs";
 export default {
   key: "survey_monkey-find-survey",
   name: "Find Survey",
-  description: "Find one of your surveys",
+  description: "Find one of your surveys. [See the docs here](https://developer.surveymonkey.com/api/v3/#api-endpoints-get-surveys-id-details)",
   version: "0.0.1",
   type: "action",
   props: {
