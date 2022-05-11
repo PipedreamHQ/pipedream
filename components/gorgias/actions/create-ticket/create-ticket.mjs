77456c254572033b30ada2c4d964379894febc98
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-11 19:11:13 -0300

    bump versions

--- components/gorgias/actions/create-ticket/create-ticket.mjs
+++ components/gorgias/actions/create-ticket/create-ticket.mjs
@@ -4,7 +4,7 @@ export default {
   key: "gorgias-create-ticket",
   name: "Create Ticket",
   description: "Create a new ticket. [See the docs](https://developers.gorgias.com/reference/post_api-tickets)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     gorgias,
