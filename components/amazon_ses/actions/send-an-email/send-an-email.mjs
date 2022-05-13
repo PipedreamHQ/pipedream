f89850ba4497335c54a379e7a7c41c65ce1d54fe
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-13 13:30:37 -0300

    send email action refactor

--- components/amazon_ses/actions/send-an-email/send-an-email.mjs
+++ components/amazon_ses/actions/send-an-email/send-an-email.mjs
@@ -1,21 +1,18 @@
-// legacy_hash_id: a_52idkj
-import AWS from "aws-sdk";
+import base from "../common/base.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   key: "amazon_ses-send-an-email",
   name: "Send an Email",
-  description: "Send an email using Amazon SES",
-  version: "0.8.2",
+  description: "Send an email using Amazon SES. Supports simple email messaging. [See the docs](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-sesv2/classes/sendemailcommand.html)",
+  version: "0.9.0",
   type: "action",
   props: {
-    amazon_ses: {
-      type: "app",
-      app: "amazon_ses",
-    },
-    region: {
-      type: "string",
-      label: "Region",
-      description: "The AWS region tied to your SES identity",
+    ...base.props,
+    ToAddresses: {
+      type: "string[]",
+      label: "To Addresses",
+      description: "An array of recipient email addresses you want to send to",
     },
     CcAddresses: {
       type: "string[]",
@@ -29,88 +26,64 @@ export default {
       description: "An array of email addresses you want to BCC",
       optional: true,
     },
-    ToAddresses: {
-      type: "string[]",
-      label: "To Addresses",
-      description: "An array of email addresses you want to send email to",
-    },
     ReplyToAddresses: {
       type: "string[]",
       label: "Reply To Addresses",
-      description: "An array of Reply-To addresses",
+      description: "An array of reply-to addresses",
       optional: true,
     },
-    html: {
+    Subject: {
+      type: "string",
+      label: "Subject",
+      description: "The subject line of the email",
+    },
+    Body: {
+      type: "string",
+      label: "Text",
+      description: "The plaintext email body of the message",
+      default: "",
+    },
+    Html: {
       type: "string",
       label: "HTML",
-      description: "HTML email body",
+      description: "The HTML email body of the message",
       optional: true,
     },
-    text: {
-      type: "string",
-      description: "Plaintext email body",
-    },
-    subject: {
-      type: "string",
-      description: "Email subject",
-    },
-    Source: {
+    FromEmailAddress: {
       type: "string",
       label: "From",
       description: "The email from which the email is addressed",
     },
   },
   async run({ $ }) {
-    const {
-      accessKeyId,
-      secretAccessKey,
-    } = this.amazon_ses.$auth;
-
-    const ses = new AWS.SES({
-      accessKeyId,
-      secretAccessKey,
-      region: this.region,
-    });
-
-    const {
-      CcAddresses,
-      BccAddresses,
-      ToAddresses,
-      ReplyToAddresses,
-      html,
-      text,
-      subject,
-      Source,
-    } = this;
-    const sesParams = {
-      Destination: {
-        CcAddresses,
-        BccAddresses,
-        ToAddresses,
-      },
-      Message: {
-        Body: {
-          Text: {
-            Charset: "UTF-8",
-            Data: text,
-          },
-        },
-        Subject: {
-          Charset: "UTF-8",
-          Data: subject,
-        },
-      },
-      ReplyToAddresses,
-      Source,
-    };
-
-    if (html) {
-      sesParams.Message.Body.Html = {
-        Charset: "UTF-8",
-        Data: html,
-      };
+    if (!(this.Body || this.Html)) {
+      throw new ConfigurationError("Email Text or HTML must be supplied");
     }
 
-    $.export("resp", await ses.sendEmail(sesParams).promise());
+    const params = {
+      Content: {
+        Simple: {
+          Subject: this.amazonSes.createCharsetContent(this.Subject),
+          Body: {
+            Text: this.amazonSes.createCharsetContent(this.Body),
+          },
+        },
+      },
+      Destination: {
+        ToAddresses: this.ToAddresses,
+        CcAddresses: this.CcAddresses,
+        BccAddresses: this.BccAddresses,
+      },
+      FromEmailAddress: this.FromEmailAddress,
+      ReplyToAddresses: this.ReplyToAddresses,
+    };
+
+    if (this.Html) {
+      params.Content.Simple.Body.Html = this.amazonSes.createCharsetContent(this.Html);
+    }
+
+    const response = await this.amazonSes.sendEmail(params);
+    $.export("$summary", "Sent email successfully");
+    return response;
   },
 };
--- components/amazon_ses/amazon_ses.app.mjs
+++ components/amazon_ses/amazon_ses.app.mjs
@@ -3,6 +3,7 @@ import {
   SESv2Client,
   SendEmailCommand,
 } from "@aws-sdk/client-sesv2";
+import constants from "./actions/common/constants.mjs";
 
 export default {
   type: "app",
@@ -14,6 +15,12 @@ export default {
     ...aws.methods,
     _client() {
       return this.getAWSClient(SESv2Client, this.region);
+    },
+    createCharsetContent(Data, Charset = constants.UTF_8) {
+      return {
+        Charset,
+        Data,
+      };
     },
     async sendEmail(params) {
       return this._client().send(new SendEmailCommand(params));
