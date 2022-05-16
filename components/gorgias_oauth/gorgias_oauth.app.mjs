efaacd2a47bc45702f0ad337aadf63eff8959805
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-16 11:10:51 -0300

    fix oauth authorization

--- components/gorgias_oauth/gorgias_oauth.app.mjs
+++ components/gorgias_oauth/gorgias_oauth.app.mjs
@@ -4,4 +4,22 @@ export default {
   ...app,
   type: "app",
   app: "gorgias_oauth",
+  methods: {
+    ...app.methods,
+    _defaultConfig({
+      path, method = "get", params = {}, data = undefined,
+    }) {
+      const config = {
+        url: `https://${this.$auth.domain}/api/${path}`,
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        },
+        method,
+        params,
+        data,
+      };
+      return config;
+    },
+  },
 };
