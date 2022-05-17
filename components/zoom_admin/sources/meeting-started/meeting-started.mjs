fb2d45a50773f8b4b93381940afa38bb03d1579f
vellames-turing <91911778+vellames-turing@users.noreply.github.com>
2022-05-17 13:14:47 -0300

    Zoom Bug - Meeting Started (#2864)
    * fix bug
    
    * CR
    
    * CR

--- components/zoom_admin/sources/meeting-started/meeting-started.mjs
+++ components/zoom_admin/sources/meeting-started/meeting-started.mjs
@@ -1,11 +1,12 @@
 import zoomAdmin from "../../zoom_admin.app.mjs";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   key: "zoom_admin-meeting-started",
   type: "source",
   name: "Meeting Started",
   description: "Emits an event each time a meeting starts in your Zoom account",
-  version: "0.0.3",
+  version: "0.0.4",
   dedupe: "unique", // Dedupe based on meeting ID
   props: {
     zoomAdmin,
@@ -19,11 +20,14 @@ export default {
   },
   async run(event) {
     const { payload } = event;
-    const { object } = payload;
     this.$emit(event, {
-      summary: `Meeting ${object.topic} started`,
-      id: object.uuid,
-      ts: +new Date(object.start_time),
+      summary: payload?.object?.topic
+        ? `Meeting ${payload?.object?.topic} started`
+        : "Meeting started",
+      id: payload?.object?.uuid ?? uuidv4(),
+      ts: payload?.object?.start_time
+        ? +new Date(payload.object.start_time)
+        : new Date(),
     });
   },
 };
