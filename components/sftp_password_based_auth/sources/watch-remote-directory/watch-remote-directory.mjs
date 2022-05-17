7cb5781307337c6edfc7fc8907f2d910eb2b23da
Alyson Freitas <alys.freitas@gmail.com>
2022-05-17 17:34:26 -0300

    sftp watch remote directory, fixing changes requested

--- components/sftp_password_based_auth/sources/watch-remote-directory/watch-remote-directory.mjs
+++ components/sftp_password_based_auth/sources/watch-remote-directory/watch-remote-directory.mjs
@@ -1,13 +1,13 @@
-/* eslint-disable pipedream/required-properties-name */
-/* eslint-disable pipedream/required-properties-description */
-/* eslint-disable pipedream/required-properties-type */
 import sftpApp from "../../sftp_password_based_auth.app.mjs";
 import base from "../../../sftp/sources/watch-remote-directory/watch-remote-directory.mjs";
 
 export default {
   ...base,
   key: "sftp_password_based_auth-watch-remote-directory",
+  name: "New Remote Directory Watcher",
+  description: "Emit new events when files get created, changed or deleted from a remote directory. [See the docs](https://github.com/theophilusx/ssh2-sftp-client#orgfac43d1)",
   version: "0.0.1",
+  type: "source",
   props: {
     ...base.props,
     sftpApp,
