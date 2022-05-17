ed2aa9dafe1960328b9ee3d5afda9064aeed2684
vellames-turing <91911778+vellames-turing@users.noreply.github.com>
2022-05-17 09:59:46 -0300

    Data Store new Actions (#2816)
    * list keys and has key
    
    * delete all records
    
    * delete single record
    
    * delete all records
    
    * list keys async options
    
    * list records
    
    * minors
    
    * minors

--- components/data_stores/actions/delete-single-record/delete-single-record.mjs
+++ components/data_stores/actions/delete-single-record/delete-single-record.mjs
@@ -1,10 +1,10 @@
 import app from "../../data_stores.app.mjs";
 
 export default {
-  key: "data_stores-delete-record",
+  key: "data_stores-delete-single-record",
   name: "Delete a single record",
   description: "Delete a single record in your [Pipedream Data Store](https://pipedream.com/data-stores/).",
-  version: "0.0.4",
+  version: "0.0.5",
   type: "action",
   props: {
     app,
@@ -15,8 +15,13 @@ export default {
       ],
     },
     key: {
-      label: "Key",
-      type: "string",
+      propDefinition: [
+        app,
+        "key",
+        ({ dataStore }) => ({
+          dataStore,
+        }),
+      ],
       description: "Key for the data you'd like to delete. Refer to your existing keys [here](https://pipedream.com/data-stores/).",
     },
   },
@@ -24,7 +29,7 @@ export default {
     const record = await this.dataStore.get(this.key);
 
     if (record) {
-      await this.dataStore.set(this.key, undefined);
+      await this.dataStore.delete(this.key);
       $.export("$summary", "Successfully deleted the record for key, `" + this.key + "`.");
     } else {
       $.export("$summary", "No record found for key, `" + this.key + "`. No data was deleted.");
