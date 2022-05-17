d221ea4bf0ae196026838e9b9f32c462fe325253
Alyson Freitas <alys.freitas@gmail.com>
2022-05-17 16:53:56 -0300

    sftp watch remote directory, fixing changes requested

--- components/sftp_password_based_auth/sources/watch-remote-directory/watch-remote-directory.mjs
+++ components/sftp_password_based_auth/sources/watch-remote-directory/watch-remote-directory.mjs
@@ -1,40 +1,21 @@
-import sftpPasswordBasedAuthApp from "../../sftp_password_based_auth.app.mjs";
-import base from "../../../sftp/sources/common/base.mjs";
+/* eslint-disable pipedream/required-properties-name */
+/* eslint-disable pipedream/required-properties-description */
+/* eslint-disable pipedream/required-properties-type */
+import sftpApp from "../../sftp_password_based_auth.app.mjs";
+import base from "../../../sftp/sources/watch-remote-directory/watch-remote-directory.mjs";
 
 export default {
+  ...base,
   key: "sftp_password_based_auth-watch-remote-directory",
-  name: "New Remote Directory Watcher",
-  description: "Emit new events when files get created, changed or deleted from a remote directory. [See the docs](https://github.com/theophilusx/ssh2-sftp-client#orgfac43d1)",
   version: "0.0.1",
-  type: "source",
-  dedupe: "unique",
   props: {
-    sftpPasswordBasedAuthApp,
     ...base.props,
+    sftpApp,
   },
   methods: {
     ...base.methods,
-  },
-  async run() {
-    this.validateRootDirectory(this.rootDirectory);
-
-    const sftp = await this.sftpPasswordBasedAuthApp.connect();
-
-    let directories = [
-      {
-        path: this.rootDirectory,
-      },
-    ];
-    directories = directories.concat(
-      await this.listDirectoriesDeep(sftp, this.rootDirectory, this.maxDepth, 0),
-    );
-
-    const currentFiles = await this.listAllFilesFromDirectories(sftp, directories);
-    const previousFiles = this.getFiles();
-    const filesChangesWithEvent = this.getChangesWithEvent(currentFiles, previousFiles);
-    this.emitEvents(filesChangesWithEvent);
-
-    this.setFiles(currentFiles);
-    await sftp.end();
+    async connect() {
+      return await this.sftpApp.connect();
+    },
   },
 };
