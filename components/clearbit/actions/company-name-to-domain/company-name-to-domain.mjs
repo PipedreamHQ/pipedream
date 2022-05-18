905574ef42d288570c8349af470fe9bd8615c5f5
Cassiano Vellames <cassiano.v@turing.com>
2022-05-18 15:46:20 -0300

    company name to domain

--- components/clearbit/actions/company-name-to-domain/company-name-to-domain.mjs
+++ components/clearbit/actions/company-name-to-domain/company-name-to-domain.mjs
@@ -1,29 +1,38 @@
-// legacy_hash_id: a_8Ki7wk
-import { axios } from "@pipedream/platform";
+import app from "../../clearbit.app.mjs";
 
 export default {
   key: "clearbit-company-name-to-domain",
   name: "Company Name to Domain",
-  description: "The Company Name to Domain action lets you convert the exact name of a company to a website domain, and a logo.",
-  version: "0.1.1",
+  description: "The Company Name to Domain action lets you convert the exact name of a company to a website domain, and a logo. [See the docs here](https://clearbit.com/docs/api/1.0/domains/find).",
+  version: "0.1.2",
   type: "action",
   props: {
-    clearbit: {
-      type: "app",
-      app: "clearbit",
+    app,
+    companyName: {
+      propDefinition: [
+        app,
+        "companyName",
+      ],
     },
-    company_name: {
-      type: "string",
-      description: "The name of the company.",
+    errorIfNoRecords: {
+      type: "boolean",
+      label: "Handle lack of records found as an error?",
+      description: "Defaults to `false`. If no records are found, the step will still be considered a success. If `false`, the step will return an error and workflow execution will stop.",
+      optional: true,
+      default: false,
     },
   },
   async run({ $ }) {
-    return await axios($, {
-      url: `https://company.clearbit.com/v1/domains/find?name=${this.company_name}`,
-      headers: {
-        Authorization: `Bearer ${this.clearbit.$auth.api_key}`,
-      },
-
-    });
+    try {
+      const res = await this.app.companyNameToDomain($, this.companyName);
+      $.export("$summary", "Successfully converted company name to domain.");
+      return res;
+    } catch (err) {
+      if (!this.errorIfNoRecords && err.response?.status == 404) {
+        $.export("$summary", "No domain found.");
+      } else {
+        throw err;
+      }
+    }
   },
 };
--- components/clearbit/clearbit.app.mjs
+++ components/clearbit/clearbit.app.mjs
@@ -1,11 +1,37 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "clearbit",
-  propDefinitions: {},
+  propDefinitions: {
+    companyName: {
+      label: "Company Name",
+      type: "string",
+      description: "The name of the company.",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://company.clearbit.com/v1";
+    },
+    _getHeaders() {
+      return {
+        Authorization: `Bearer ${this.$auth.secret_api_key}`,
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      const res = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return res;
+    },
+    companyNameToDomain(ctx = this, companyName) {
+      return axios(ctx, this._getAxiosParams({
+        method: "GET",
+        path: `/domains/find?name=${companyName}`,
+      }));
     },
   },
 };
