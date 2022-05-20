72f5cd6e0b7812c7dde759ac0336c9107848bc7c
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-05-20 17:26:18 +0100

    Send campaign key name updated

--- components/mailchimp/actions/send-campaign/send-campaign.mjs
+++ components/mailchimp/actions/send-campaign/send-campaign.mjs
@@ -4,7 +4,7 @@ import mailchimp from "../../mailchimp.app.mjs";
 export default {
   key: "mailchimp-send-campaign",
   name: "Send a Campaign",
-  description: "Sends a campaign draft to the audience signed up for the campaign.",
+  description: "Sends a campaign draft to the audience signed up for the campaign. [See docs here](https://mailchimp.com/developer/marketing/api/campaigns/send-campaign/)",
   version: "0.2.2",
   type: "action",
   props: {
