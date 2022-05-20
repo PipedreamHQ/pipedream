75d5010286eb2e540aeee947502c5bc8ee0bbce8
bush4u <68253993+bush4u@users.noreply.github.com>
2022-05-20 17:28:42 +0100

    Update components/mailchimp/mailchimp.app.mjs
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>

--- components/mailchimp/mailchimp.app.mjs
+++ components/mailchimp/mailchimp.app.mjs
@@ -43,7 +43,7 @@ export default {
           const campaignName = lsdIdx > 0
             ? campaign.long_archive_url.substring(lsdIdx + 1)
             : "";
-          const label = `Campaign ID/Name from URL (if any): ${campaign.id}/${campaignName}, List Id/Name: ${campaign.recipients.list_id}/${campaign.recipients.list_name}, Subject: ${campaign.settings.subject_line}`;
+          const label = `Campaign ID/Name from URL (if any): ${campaign.id}/${campaignName}, List ID/Name: ${campaign.recipients.list_id}/${campaign.recipients.list_name}, Subject: ${campaign.settings.subject_line}`;
           return {
             label,
             value: campaign.id,
