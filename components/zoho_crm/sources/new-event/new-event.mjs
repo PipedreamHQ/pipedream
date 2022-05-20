c678e8d85c8fee1f7291c3beef36588ddf85ff11
Jorge Cortes <jacortesmahmud@gmail.com>
2022-05-20 09:02:46 -0500

    Fix zoho crm merge with master and incremented versions (#2698)
    * Fix broken require references
    
    * Change zoho_crm references to zohoCrm
    
    * Change crudOps descriptions to be more readable (E.g. 'Account Updated' instead of 'Account Edition')
    
    * Increment version numbers and fix ESLint issues
    
    * Rename 'New Event' source for better readability.
    
    * Define getModuleName() in custom-module.js so module-entry sources work.
    
    * Define getModuleName() in custom-module.js so module-entry sources work.
    
    * Fix merge conflict
    
    Co-authored-by: Celeste Bancos <celestebancos@gmail.com>

--- components/zoho_crm/sources/new-event/new-event.mjs
+++ components/zoho_crm/sources/new-event/new-event.mjs
@@ -7,14 +7,14 @@ export default {
   key: "zoho_crm-new-event",
   name: "New Event (Instant)",
   description: "Emit new custom events from Zoho CRM",
-  version: "0.0.9",
+  version: "0.0.10",
   type: "source",
   props: {
     ...common.props,
     events: {
       type: "string[]",
       label: "Events",
-      description: "List of CRUD events that will trigger this event source",
+      description: "Choose one or more module events to trigger this event source.",
       async options({ page = 0 }) {
         if (page !== 0) {
           return [];
