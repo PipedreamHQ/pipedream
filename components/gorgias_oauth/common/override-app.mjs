a707874e0055b436a79d24bd5b3928d9271485bb
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-23 17:39:13 -0300

    add override app function comment

--- components/gorgias_oauth/common/override-app.mjs
+++ components/gorgias_oauth/common/override-app.mjs
@@ -1,6 +1,12 @@
 import app from "../gorgias_oauth.app.mjs";
 const appName = "gorgias";
 
+/**
+ * This function substitutes the app so that $auth and propDefinitions correspond to the correct app
+ * This is done to avoid duplicating code because all component props and methods are shared between
+ * gorgias_oauth and gorgias (api_key) apps
+ * @param {*} base app to inject - in this case, gorgias_oauth
+ */
 function overrideApp(base) {
   base.props[appName] = app;
 
