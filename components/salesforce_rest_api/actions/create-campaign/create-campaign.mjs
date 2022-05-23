493a5369d3f3d1ab914d9e9c53b372aa183e7476
bush4u <68253993+bush4u@users.noreply.github.com>
2022-05-23 23:05:34 +0100

    Update components/salesforce_rest_api/actions/create-campaign/create-campaign.mjs
    Co-authored-by: vellames-turing <91911778+vellames-turing@users.noreply.github.com>

--- components/salesforce_rest_api/actions/create-campaign/create-campaign.mjs
+++ components/salesforce_rest_api/actions/create-campaign/create-campaign.mjs
@@ -42,7 +42,7 @@ export default {
       $,
       data,
     });
-    $.export("$summary", `Created campaign "${this.name}"`);
+    $.export("$summary", `Successfully created campaign "${this.name}"`);
     return response;
   },
 };
