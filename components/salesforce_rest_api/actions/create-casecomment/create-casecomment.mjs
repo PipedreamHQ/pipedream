efa91e16c0854ba151a4266ba9723d4885846d7e
bush4u <68253993+bush4u@users.noreply.github.com>
2022-05-23 23:06:24 +0100

    Update components/salesforce_rest_api/actions/create-casecomment/create-casecomment.mjs
    Co-authored-by: vellames-turing <91911778+vellames-turing@users.noreply.github.com>

--- components/salesforce_rest_api/actions/create-casecomment/create-casecomment.mjs
+++ components/salesforce_rest_api/actions/create-casecomment/create-casecomment.mjs
@@ -42,7 +42,7 @@ export default {
       $,
       data,
     });
-    $.export("$summary", `Created case comment for ${this.ParentId}`);
+    $.export("$summary", `Successfully created case comment for ${this.ParentId}`);
     return response;
   },
 };
