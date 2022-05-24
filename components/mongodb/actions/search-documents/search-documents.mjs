0c42be7c0fa3f0d95a38bb3ecbf39743ef2f170c
michelle0927 <michellelbergero@hotmail.com>
2022-05-24 13:46:00 -0400

    parse filter in search-documents

--- components/mongodb/actions/search-documents/search-documents.mjs
+++ components/mongodb/actions/search-documents/search-documents.mjs
@@ -4,7 +4,7 @@ export default {
   key: "mongodb-search-documents",
   name: "Search Documents",
   description: "Search for specific documents or return all documents. [See the docs here](https://docs.mongodb.com/manual/reference/method/db.collection.find/)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     mongodbApp,
@@ -16,15 +16,16 @@ export default {
     },
     filter: {
       label: "Filter",
-      description: "Object to use as a filter",
+      description: "Object to use as a filter. The filter value(s) may be string, number, boolean, date, or valid JSON object.",
       type: "object",
       optional: true,
     },
   },
   async run({ $ }) {
+    const parsedFilter = this.mongodbApp.parseFilter(this.filter);
     const documents = await this.mongodbApp.searchDocuments(
       this.collection,
-      this.filter,
+      parsedFilter,
     );
 
     $.export("$summary", "Documents successfully fetched");
