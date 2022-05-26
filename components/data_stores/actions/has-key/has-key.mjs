0b462982aae3e77cface4c5c3a8cde37f75eb651
Andrew Chuang <andrewjschuang@gmail.com>
2022-05-26 16:26:31 -0300

    add record if not found to has key action

--- components/data_stores/actions/has-key/has-key.mjs
+++ components/data_stores/actions/has-key/has-key.mjs
@@ -4,7 +4,7 @@ export default {
   key: "data_stores-has-key",
   name: "Has Key",
   description: "Check if a key exists in your [Pipedream Data Store](https://pipedream.com/data-stores/).",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     app,
@@ -22,14 +22,34 @@ export default {
           dataStore,
         }),
       ],
-      description: "The key to check.",
+      description: "Check if the key exists.",
+    },
+    addRecordIfNotFound: {
+      propDefinition: [
+        app,
+        "addRecordIfNotFound",
+      ],
     },
   },
+  async additionalProps() {
+    if (this.app.shouldAddRecord(this.addRecordIfNotFound)) {
+      return this.app.valueProp();
+    }
+    return {};
+  },
   async run ({ $ }) {
-    const hasKey = await this.dataStore.has(this.key);
-    $.export("$summary", hasKey
-      ? `Key "${this.key}" exists.`
-      : `Key "${this.key}" does not exist.`);
-    return hasKey;
+    if (await this.dataStore.has(this.key)) {
+      $.export("$summary", `Key "${this.key}" exists.`);
+      return true;
+    }
+
+    if (this.app.shouldAddRecord(this.addRecordIfNotFound)) {
+      await this.dataStore.set(this.key, this.value);
+      $.export("$summary", `Key "${this.key}" was not found. Successfully added a new record.`);
+      return this.dataStore.get(this.key);
+    }
+
+    $.export("$summary", `Key "${this.key}" does not exist.`);
+    return false;
   },
 };
