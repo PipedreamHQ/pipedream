3561102f789f7497c9e8d446bf5eedcd427f16a1
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2022-05-30 13:32:21 -0300

    Fixing typos in IFTTT actions (#2991)

--- components/ifttt/actions/trigger-event-with-json/trigger-event-with-json.mjs
+++ components/ifttt/actions/trigger-event-with-json/trigger-event-with-json.mjs
@@ -2,9 +2,9 @@ import ifttt from "../../ifttt.app.mjs";
 
 export default {
   name: "Trigger Event with JSON",
-  description: "Trigger Event with an artibrary JSON payload. [See docs](https://help.ifttt.com/hc/en-us/articles/115010230347-Webhooks-service-FAQ)",
+  description: "Trigger Event with an arbitrary JSON payload. [See docs](https://help.ifttt.com/hc/en-us/articles/115010230347-Webhooks-service-FAQ)",
   key: "ifttt-trigger-event-with-json",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     ifttt,
