082c086bd0dc8ff2eb801b6154b97fb1c0ee3200
Dylan J. Sather <dylan.sather@gmail.com>
2022-05-30 16:05:00 -0700

    Modifications to app file

--- components/rss/app/rss.app.ts
+++ components/rss/app/rss.app.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import FeedParser from "feedparser";
 import { Item } from "feedparser";
 import hash from "object-hash";
-import { generateHTTPErrorClasses } from "@pipedream/helpers";
 import { defineApp } from "@pipedream/types";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default defineApp({
   type: "app",
@@ -33,18 +33,17 @@ export default defineApp({
       });
 
       // Handle status codes as error codes
-      const errors = generateHTTPErrorClasses();
-      if (res.status === 404) throw errors[404]("The URL does not exist. Please double-check the URL and try again.");
-      if (res.status === 429) throw errors[429](`${this.url} rate-limited the request. Please reach out to the site hosting the RSS feed to confirm or increase their rate limit.`);
-      if (res.status >= 500) throw errors[res.status](`${this.url} encountered an error. Please try again later or reach out to the site hosting the RSS feed if you continue to see this error.`);
+      if (res.status === 404) throw new ConfigurationError(`The URL ${url} does not exist. Please double-check the URL and try again.`);
+      if (res.status === 429) throw new ConfigurationError(`${url} rate-limited the request. Please reach out to the site hosting the RSS feed to confirm or increase their rate limit.`);
+      if (res.status >= 500) throw new ConfigurationError(`${url} is returning a server error. Please try again later or reach out to the site hosting the RSS feed if you continue to see this error.`);
       if (res.status >= 400) {
-        throw errors[res.status]("Error fetching URL. Please check the URL and try again.");
+        throw new ConfigurationError(`Error fetching URL ${url}. Please load the URL directly in your browser and try again.`);
       }
       return res.data;
     },
-    async parseFeed(data: any) {
+    async parseFeed(data: any): Promise<Item[]> {
       const feedparser = new FeedParser({
-        addmeta: false,
+        addmeta: true,
       });
       const items: Item[] = [];
       await new Promise((resolve, reject) => {
@@ -83,7 +82,7 @@ export default defineApp({
       return this.parseFeed(data);
     },
     validateFeedURL(url: string) {
-      if (!url) throw new Error("No feed URL provided");
+      if (!url) throw new ConfigurationError("No URL provided. Please enter an RSS URL to fetch");
     },
   },
 });
