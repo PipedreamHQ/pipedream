e98670feec82cdf23e268c44356ac61d5f69bb88
Cassiano Vellames <cassiano.v@turing.com>
2022-06-01 13:53:40 -0300

    refactor

--- components/airtable/actions/create-or-update-record/create-or-update-record.mjs
+++ components/airtable/actions/create-or-update-record/create-or-update-record.mjs
@@ -1,16 +1,12 @@
 import airtable from "../../airtable.app.mjs";
-import {
-  makeFieldProps,
-  makeRecord,
-} from "../../common/utils.mjs";
 import common from "../common.mjs";
-import { ConfigurationError } from "@pipedream/platform";
+import commonActions from "../../common/actions.mjs";
 
 export default {
   key: "airtable-create-single-record-or-update",
   name: "Create single record or update",
   description: "Updates a record if `recordId` is provided or adds a record to a table.",
-  version: "0.0.3",
+  version: "0.0.1",
   type: "action",
   props: {
     ...common.props,
@@ -19,89 +15,30 @@ export default {
       ...common.props.tableId,
       reloadProps: true,
     },
-    typecast: {
-      propDefinition: [
-        airtable,
-        "typecast",
-      ],
-    },
     recordId: {
       propDefinition: [
         airtable,
         "recordId",
       ],
       optional: true,
+      description: "Enter a [record ID](https://support.airtable.com/hc/en-us/articles/360051564873-Record-ID) if you want to update an existing record. Leave blank to create a new record.",
+    },
+    typecast: {
+      propDefinition: [
+        airtable,
+        "typecast",
+      ],
     },
   },
   async additionalProps() {
-    const baseId = this.baseId?.value ?? this.baseId;
-    const tableId = this.tableId?.value ?? this.tableId;
-    try {
-      const tableSchema = await this.airtable.table(baseId, tableId);
-      return makeFieldProps(tableSchema);
-    } catch (err) {
-      const hasManualTableInput = !this.tableId?.label;
-      // If manual input and .table throws error, return a record prop
-      // otherwise, throw ConfigurationError
-      if (hasManualTableInput) {
-        return {
-          // Use record propDefinition directly to workaround lack of support
-          // for propDefinition in additionalProps
-          record: airtable.propDefinitions.record,
-        };
-      }
-      throw new ConfigurationError("Could not find a table for the specified base ID and table ID. Please adjust the action configuration to continue.");
-    }
+    return commonActions.additionalProps(this);
   },
   async run({ $ }) {
-    const baseId = this.baseId?.value ?? this.baseId;
-    const tableId = this.tableId?.value ?? this.tableId;
     const recordId = this.recordId ?? undefined;
     if (!recordId) {
-      const table = this.airtable.base(baseId)(tableId);
-      const record = this.record ?? makeRecord(this);
-      this.airtable.validateRecord(record);
-
-      const data = [
-        {
-          fields: record,
-        },
-      ];
-
-      const params = {
-        typecast: this.typecast,
-      };
-
-      let response;
-      try {
-        [
-          response,
-        ] = await table.create(data, params);
-      } catch (err) {
-        this.airtable.throwFormattedError(err);
-      }
-
-      $.export("$summary", `Added 1 record to ${this.baseId?.label || baseId}: [${this.tableId?.label || tableId}](https://airtable.com/${baseId}/${tableId})`);
-      return response;
+      return commonActions.createRecord(this, $);
     } else {
-      this.airtable.validateRecordID(recordId);
-      const record = this.record ?? makeRecord(this);
-
-      const base = this.airtable.base(baseId);
-      let response;
-      try {
-        response = (await base(tableId).update([
-          {
-            id: recordId,
-            fields: record,
-          },
-        ]));
-      } catch (err) {
-        this.airtable.throwFormattedError(err);
-      }
-
-      $.export("$summary", `Updated record "${recordId}" in ${this.baseId?.label || baseId}: [${this.tableId?.label || tableId}](https://airtable.com/${baseId}/${tableId})`);
-      return response[0];
+      return commonActions.updateRecord(this, $);
     }
   },
 };
--- components/airtable/actions/get-record-or-create/get-record-or-create.mjs
+++ components/airtable/actions/get-record-or-create/get-record-or-create.mjs
@@ -1,24 +1,18 @@
-import {
-  makeFieldProps,
-  makeRecord,
-} from "../../common/utils.mjs";
 import airtable from "../../airtable.app.mjs";
 import common from "../common.mjs";
-import { ConfigurationError } from "@pipedream/platform";
+import commonActions from "../../common/actions.mjs";
 
 export default {
   key: "airtable-get-record-or-create",
   name: "Get Record or Create",
   description: "Get a record from a table by record ID or create a new register.",
-  version: "0.0.1",
+  version: "0.2.16",
   type: "action",
   props: {
     ...common.props,
+    // eslint-disable-next-line pipedream/props-label,pipedream/props-description
     tableId: {
-      propDefinition: [
-        airtable,
-        "tableId",
-      ],
+      ...common.props.tableId,
       reloadProps: true,
     },
     recordId: {
@@ -26,84 +20,33 @@ export default {
         airtable,
         "recordId",
       ],
+    },
+    typecast: {
+      propDefinition: [
+        airtable,
+        "typecast",
+      ],
       optional: true,
-      reloadProps: true,
     },
   },
   async additionalProps() {
-    const recordId = this.recordId ?? undefined;
-    if (!recordId) {
-      const baseId = this.baseId?.value ?? this.baseId;
-      const tableId = this.tableId?.value ?? this.tableId;
-      try {
-        const tableSchema = await this.airtable.table(baseId, tableId);
-        return {
-          typecast: {
-            type: "boolean",
-            label: "Typecast",
-            description: "The Airtable API will perform best-effort automatic data conversion from string values if the typecast parameter is `True`. Automatic conversion is disabled by default to ensure data integrity, but it may be helpful for integrating with 3rd party data sources.",
-          },
-          ...makeFieldProps(tableSchema),
-        };
-      } catch (err) {
-        const hasManualTableInput = !this.tableId?.label;
-        // If manual input and .table throws error, return a record prop
-        // otherwise, throw ConfigurationError
-        if (hasManualTableInput) {
-          return {
-            // Use record propDefinition directly to workaround lack of support
-            // for propDefinition in additionalProps
-            record: airtable.propDefinitions.record,
-          };
-        }
-        throw new ConfigurationError("Could not find a table for the specified base ID and table ID. Please adjust the action configuration to continue.");
-      }
-    }
-    return {};
+    return commonActions.additionalProps(this);
   },
   async run({ $ }) {
-    const baseId = this.baseId?.value ?? this.baseId;
-    const tableId = this.tableId?.value ?? this.tableId;
     const recordId = this.recordId ?? undefined;
 
     if (recordId) {
-      this.airtable.validateRecordID(recordId);
-      const base = this.airtable.base(baseId);
-      let response;
       try {
-        response = await base(tableId).find(recordId);
+        return await commonActions.getRecord(this, $, true);
       } catch (err) {
-        this.airtable.throwFormattedError(err);
+        if (err.statusCode === 404) {
+          return await commonActions.createRecord(this, $);
+        } else {
+          this.airtable.throwFormattedError(err);
+        }
       }
-      $.export("$summary", `Fetched record "${recordId}" from ${this.baseId?.label || baseId}: [${this.tableId?.label || tableId}](https://airtable.com/${baseId}/${tableId})`);
-      return response;
     } else {
-      const table = this.airtable.base(baseId)(tableId);
-      const record = this.record ?? makeRecord(this);
-
-      this.airtable.validateRecord(record);
-
-      const data = [
-        {
-          fields: record,
-        },
-      ];
-
-      const params = {
-        typecast: this.typecast,
-      };
-
-      let response;
-      try {
-        [
-          response,
-        ] = await table.create(data, params);
-      } catch (err) {
-        this.airtable.throwFormattedError(err);
-      }
-
-      $.export("$summary", `Added 1 record to ${this.baseId?.label || baseId}: [${this.tableId?.label || tableId}](https://airtable.com/${baseId}/${tableId})`);
-      return response;
+      return commonActions.createRecord(this, $);
     }
   },
 };
