62941447af1dcb299a0ba169b44da788630941a6
Luan Cazarine <luanhc@gmail.com>
2022-06-01 11:23:09 -0300

    Actions - Trigger Monkey

--- components/voice_monkey/voice_monkey.app.mjs
+++ components/voice_monkey/voice_monkey.app.mjs
@@ -1,5 +1,6 @@
 import { axios } from "@pipedream/platform";
 import common from "./common/constants.mjs";
+import { objectFilterNull } from "./utils/functions.mjs";
 
 export default {
   type: "app",
@@ -12,18 +13,64 @@ export default {
     },
     announcement: {
       type: "string",
-      label: "Announcement",
+      label: "Announcement text",
       description: "If you want your Alexa device to speak something out loud when you trigger your monkey then add the text to this parameter. You must open the Voice Monkey Skill as the last step of your routine.",
+    },
+    chime: {
+      type: "string",
+      label: "Chime",
+      description: "Add a notification/alarm sound to your routine.",
+      options: common.CHIMES,
+      optional: true,
+    },
+    voice: {
+      type: "string",
+      label: "Voice",
+      description: "Select from one of the many different voices available for your announcement. Alexa (default)",
+      options: common.VOICES,
+      optional: true,
+    },
+    audioFileUrl: {
+      type: "string",
+      label: "Audio File URL",
+      description: "Play an audio file on your Alexa device. Include the full URL including https://",
+      optional: true,
+    },
+    backgroundAudioFileUrl: {
+      type: "string",
+      label: "Background Audio File URL",
+      description: "Play an audio file in the background wishlist Alexa makes your announcement. Include the full URL including https://",
+      optional: true,
+    },
+    promptYesResponsePreset: {
+      type: "string",
+      label: "Prompt 'Yes' Response Preset Id",
+      description: "With prompts you can ask Alexa to ask you a yes/no question (that you include in the announcement of this request) e.g. \"Sall I turn on the lights?\". If you reply \"Yes\", the Preset Id you specify here will be triggered.",
+      optional: true,
+    },
+    promptNoResponsePreset: {
+      type: "string",
+      label: "Prompt 'No' Response Preset Id",
+      description: "With prompts you can ask Alexa to ask you a yes/no question (that you include in the announcement of this request) e.g. \"Sall I turn on the lights?\". If you reply \"No\", the Preset Id you specify here will be triggered.",
+      optional: true,
     },
     imageUrl: {
       type: "string",
       label: "Image URL (https://)",
       description: "Send an image hosted on a publicly accessible https URL to a screen enabled Alexa device such as an Echo Show. As with announcements, the Alexa routine must open the Voice Monkey Skill as its final action.",
+      optional: true,
     },
     videoUrl: {
       type: "string",
       label: "Video URL (https://)",
       description: "You can send a video to alexa devices with a screen including the Echo Show. The ‘announcement’ parameter must not be blank. You must open the Voice Monkey Skill as the last step of your routine.",
+      optional: true,
+    },
+    websiteUrl: {
+      type: "string",
+      label: "Website URL",
+      description: "Open a website on your screen enabled Alexa device. Include the full URL including https://",
+      optional: true,
     },
   },
   methods: {
@@ -31,7 +78,7 @@ export default {
       return {
         access_token: `${this.$auth.access_token}`,
         secret_token: `${this.$auth.secret_token}`,
-        ...params,
+        ...objectFilterNull(params),
       };
     },
     async _makeRequest({
