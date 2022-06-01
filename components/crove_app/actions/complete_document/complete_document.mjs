b2e0b22a12f361cea092a7fcca00dcb2873be1b2
unknown <atul.thakur1837@gmail.com>
2022-06-02 01:06:49 +0530

    moved document id to app file

--- components/crove_app/actions/complete_document/complete_document.mjs
+++ components/crove_app/actions/complete_document/complete_document.mjs
@@ -10,23 +10,10 @@ export default {
   props: {
     croveApp,
     document_id: {
-      type: "string",
-      label: "Document ID",
-      description: "Document ID of document.",
-      async options({ $ }) {
-        var resp = await axios($, {
-          url: "https://v2.api.crove.app/api/integrations/external/documents/?limit=50",
-          headers: {
-            "X-API-KEY": `${this.croveApp.$auth.api_key}`,
-          },
-          method: "GET",
-        });
-        resp = resp.results;
-        return resp.map((item) => ({
-          label: item.name,
-          value: item.id,
-        }));
-      },
+      propDefinition: [
+        croveApp,
+        "document_id",
+      ],
     },
   },
   async run({ $ }) {
--- components/crove_app/crove_app.app.mjs
+++ components/crove_app/crove_app.app.mjs
@@ -3,7 +3,27 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "crove_app",
-  propDefinitions: {},
+  propDefinitions: {
+    document_id: {
+      type: "string",
+      label: "Document ID",
+      description: "Document ID of document.",
+      async options({ $ }) {
+        var resp = await axios($, {
+          url: "https://v2.api.crove.app/api/integrations/external/documents/?limit=50",
+          headers: {
+            "X-API-KEY": `${this.$auth.api_key}`,
+          },
+          method: "GET",
+        });
+        resp = resp.results;
+        return resp.map((item) => ({
+          label: item.name,
+          value: item.id,
+        }));
+      },
+    }
+  },
   methods: {
     // this.$auth contains connected account data
     authKeys() {
