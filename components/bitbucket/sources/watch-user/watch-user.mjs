058b22bca0c454d4c6bca04fcdf21bf3e250ef15
bush4u <68253993+bush4u@users.noreply.github.com>
2022-06-02 20:47:48 +0100

    Source bitbucket watch user watch team (#2988)
    * Fixing eslint bug
    
    * Watch user error fixed
    
    * Watch user bug fix
    
    * Fixing bugs
    
    * New Event from any Repository done

---
+++ components/bitbucket/sources/watch-user/watch-user.mjs
@@ -0,0 +1,55 @@
+import common from "../common/common.mjs";
+const { bitbucket } = common.props;
+
+export default {
+  ...common,
+  type: "source",
+  name: "New Event from any Repository",
+  key: "bitbucket-new-event-from-any-repository",
+  description: "Emit new event when an event occurs from any repository belonging to the user. [See docs here](https://developer.atlassian.com/cloud/bitbucket/rest/api-group-repositories/#api-repositories-workspace-repo-slug-hooks-post)",
+  version: "0.0.1",
+  props: {
+    ...common.props,
+    repositoryIds: {
+      propDefinition: [
+        bitbucket,
+        "multiRepositories",
+        (c) => ({
+          workspaceId: c.workspaceId,
+        }),
+      ],
+    },
+    eventTypes: {
+      propDefinition: [
+        bitbucket,
+        "eventTypes",
+        () => ({
+          subjectType: "repository",
+        }),
+      ],
+    },
+  },
+  methods: {
+    ...common.methods,
+    getPath() {
+      return this.repositoryIds?.length > 1
+        ? this.repositoryIds.map((repositoryId) => `repositories/${this.workspaceId}/${repositoryId}/hooks`)
+        : `repositories/${this.workspaceId}/${this.repositoryIds[0]}/hooks`;
+    },
+    getWebhookEventTypes() {
+      return this.eventTypes;
+    },
+    proccessEvent(event) {
+      const {
+        headers,
+        body,
+      } = event;
+
+      this.$emit(body, {
+        id: headers["x-request-uuid"],
+        summary: `New repository event ${headers["x-event-key"]}`,
+        ts: Date.parse(headers["x-event-time"]),
+      });
+    },
+  },
+};
