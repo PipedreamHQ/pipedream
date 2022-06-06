9adaa9f56bfb61ddb05f2f37daca03306e588fbc
Andrew Chuang <andrewjschuang@gmail.com>
2022-06-06 09:47:53 -0300

    Snowflake Refactor and add Action (#3026)
    * update to esm
    
    * make table name a prop definition
    
    * add insert row action
    
    * remove unecessary context
    
    * update pnpm-lock.yaml
    
    * more informative values prop description

--- components/snowflake/snowflake.app.mjs
+++ components/snowflake/snowflake.app.mjs
@@ -1,9 +1,33 @@
-const { promisify } = require("util");
-const snowflake = require("snowflake-sdk");
+import snowflake from "snowflake-sdk";
+import { promisify } from "util";
 
-module.exports = {
+export default {
   app: "snowflake",
   type: "app",
+  propDefinitions: {
+    tableName: {
+      type: "string",
+      label: "Table Name",
+      async options() {
+        const options = await this.listTables();
+        return options.map((i) => i.name);
+      },
+    },
+    columns: {
+      type: "string[]",
+      label: "Columns",
+      description: "Select the columns to insert data",
+      async options({ tableName }) {
+        const fields = await this.listFieldsForTable(tableName);
+        return fields.map((field) => field.name);
+      },
+    },
+    values: {
+      type: "string[]",
+      label: "Values",
+      description: "Insert values for the selected columns respectively. For string values, wrap them in **single quotes**. Example: `'This is a string'`",
+    },
+  },
   methods: {
     async _getConnection() {
       if (this.connection) {
@@ -41,10 +65,23 @@ module.exports = {
     },
     async listTables() {
       const sqlText = "SHOW TABLES";
-      return this.collectRows({ sqlText });
+      return this.collectRows({
+        sqlText,
+      });
     },
     async listFieldsForTable(tableName) {
       const sqlText = "DESCRIBE TABLE IDENTIFIER(:1)";
+      const binds = [
+        tableName,
+      ];
+      const statement = {
+        sqlText,
+        binds,
+      };
+      return this.collectRows(statement);
+    },
+    async insertRow(tableName, columns, values) {
+      const sqlText = `INSERT INTO ${tableName} (${columns.join(",")}) VALUES (${values.join(",")});`;
       const binds = [
         tableName,
       ];
