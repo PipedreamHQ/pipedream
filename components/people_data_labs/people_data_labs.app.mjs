4e165b11612028cefcdd25170c64cce396f75710
Cassiano Vellames <cassiano.v@turing.com>
2022-06-06 13:04:47 -0300

    enrich person

--- components/people_data_labs/people_data_labs.app.mjs
+++ components/people_data_labs/people_data_labs.app.mjs
@@ -1,11 +1,51 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "people_data_labs",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://api.peopledatalabs.com/v5";
     },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "X-API-Key": this.$auth.api_key,
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      const res = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      delete res.path;
+      console.log(res);
+      return res;
+    },
+    async _makeRequest(opts = {}) {
+      try {
+        const axiosParams = this._getAxiosParams(opts);
+        return await axios(this, axiosParams);
+      } catch (err) {
+        if (err)
+        if (err?.response?.data?.error) {
+          if (err.response.data.error.status == 404) {
+            return null;
+          }
+          throw new Error(err.response.data.error.message)
+        }
+        throw err;
+      }
+      
+    },
+    async enrichPerson(params) {
+      return this._makeRequest({
+        method: "get",
+        path: "/person/enrich",
+        params
+      })
+    }
   },
 };
