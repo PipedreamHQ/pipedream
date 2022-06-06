3be13ae734adc3b17989df7af5ef30b38542a034
Luan Cazarine <luanhc@gmail.com>
2022-06-06 16:58:30 -0300

    2862 action bigcommerce (#3069)
    * Actions
    Custom Events | Triggers on any event from BigCommerce
    New Customer | Triggers when there is a new Customer
    New Order | Triggers when there is a new Order
    
    * removed unused props
    some adjusts
    
    * fix base_url constant
    
    * fix event response
    
    * Create action Get All Products
    
    * create action Get Product
    
    * action - cerate_product
    
    * action - create_product
    
    * action - delete_product
    
    * action - update_product
    
    * action - get-all-products-sort-order
    
    * action - get-product-variants
    
    * adjust
       - function changed to generator
       - some adjusted properties
    
    * adjusts
    
    * git sync
    
    * fix deploy
    
    * fix version
    
    Co-authored-by: Luan Cazarine <luan.cazarine@cobasi.com.br>

--- components/bigcommerce/actions/create-product/create-product.mjs
+++ components/bigcommerce/actions/create-product/create-product.mjs
@@ -6,7 +6,7 @@ export default {
   name: "Create Product",
   description:
     "Create a product. [See the docs here](https://developer.bigcommerce.com/api-reference/366928572e59e-create-a-product)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     bigcommerce,
