7680683f4d79640f1305a0c2bc633b54d8fe7aab
michelle0927 <michellelbergero@hotmail.com>
2022-06-07 13:10:03 -0400

    microsoft teams sources

--- components/microsoft_teams/microsoft_teams.app.mjs
+++ components/microsoft_teams/microsoft_teams.app.mjs
@@ -1,11 +1,123 @@
+import "isomorphic-fetch";
+import { Client } from "@microsoft/microsoft-graph-client";
+
 export default {
   type: "app",
   app: "microsoft_teams",
-  propDefinitions: {},
+  propDefinitions: {
+    team: {
+      type: "string",
+      label: "Team",
+      description: "Microsoft Team",
+      async options({ prevContext }) {
+        const response = prevContext.nextLink
+          ? await this.clientApiGetRequest(prevContext.nextLink)
+          : await this.listTeams();
+        const options = response.value.map((team) => ({
+          label: team.displayName,
+          value: team.id,
+        }));
+        return {
+          options,
+          context: {
+            nextLink: response["@odata.nextLink"],
+          },
+        };
+      },
+    },
+    channel: {
+      type: "string",
+      label: "Channel",
+      description: "Team Channel",
+      async options({
+        teamId, prevContext,
+      }) {
+        const response = prevContext.nextLink
+          ? await this.clientApiGetRequest(prevContext.nextLink)
+          : await this.listChannels({
+            teamId,
+          });
+        const options = response.value.map((channel) => ({
+          label: channel.displayName,
+          value: channel.id,
+        }));
+        return {
+          options,
+          context: {
+            nextLink: response["@odata.nextLink"],
+          },
+        };
+      },
+    },
+    chat: {
+      type: "string",
+      label: "Chat",
+      description: "Team Chat",
+      async options({ prevContext }) {
+        const response = prevContext.nextLink
+          ? await this.clientApiGetRequest(prevContext.nextLink)
+          : await this.listChats();
+        const options = response.value.map((chat) => ({
+          label: chat.topic ?? chat.id,
+          value: chat.id,
+        }));
+        return {
+          options,
+          context: {
+            nextLink: response["@odata.nextLink"],
+          },
+        };
+      },
+    },
+    max: {
+      type: "integer",
+      label: "Max",
+      description: "Maximum number of items to return",
+      optional: true,
+      default: 20,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    client() {
+      return new Client.initWithMiddleware({
+        authProvider: {
+          getAccessToken: () => Promise.resolve(this.$auth.oauth_access_token),
+        },
+      });
+    },
+    async authenticatedUserId() {
+      const { id } = await this.client()
+        .api("/me")
+        .get();
+      return id;
+    },
+    async clientApiGetRequest(endpoint) {
+      return this.client()
+        .api(endpoint)
+        .get();
+    },
+    async listTeams() {
+      const id = await this.authenticatedUserId();
+      return this.clientApiGetRequest(`/users/${id}/joinedTeams?orderby=createdDateTime%20desc`);
+    },
+    async listChannels({ teamId }) {
+      return this.clientApiGetRequest(`/teams/${teamId}/channels?orderby=createdDateTime%20desc`);
+    },
+    async listChannelMessages({
+      teamId, channelId, searchQuery,
+    }) {
+      return this.clientApiGetRequest(`/teams/${teamId}/channels/${channelId}/messages/delta?orderby=createdDateTime%20desc&${ searchQuery
+        ? "$search=" + searchQuery
+        : "" }`);
+    },
+    async listTeamMembers({ teamId }) {
+      return this.clientApiGetRequest(`/teams/${teamId}/members`);
+    },
+    async listChats() {
+      return this.clientApiGetRequest("/chats?orderby=createdDateTime%20desc");
+    },
+    async listChatMessages({ chatId }) {
+      return this.clientApiGetRequest(`/chats/${chatId}/messages?orderby=createdDateTime%20desc`);
     },
   },
 };
---
+++ components/microsoft_teams/sources/new-channel/new-channel.mjs
@@ -0,0 +1,46 @@
+import base from "../common/base.mjs";
+
+export default {
+  ...base,
+  key: "microsoft_teams-new-channel",
+  name: "New Channel",
+  description: "Emit new event when a new channel is created within a team",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    ...base.props,
+    team: {
+      propDefinition: [
+        base.props.microsoftTeams,
+        "team",
+      ],
+    },
+    max: {
+      propDefinition: [
+        base.props.microsoftTeams,
+        "max",
+      ],
+    },
+  },
+  methods: {
+    ...base.methods,
+    async getResources(lastCreated) {
+      return this.getNewPaginatedResources(
+        this.microsoftTeams.listChannels,
+        {
+          teamId: this.team,
+        },
+        this.max,
+        lastCreated,
+      );
+    },
+    generateMeta(channel) {
+      return {
+        id: channel.id,
+        summary: channel.displayName,
+        ts: Date.parse(channel.createdDateTime),
+      };
+    },
+  },
+};
