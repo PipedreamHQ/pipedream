ed38fbb2ccd8b56be5c55bb35f20d1915dd7447a
Lucas Caresia <lucascarezia@gmail.com>
2022-06-07 14:13:22 -0300

    Fixing versions

--- components/woocommerce/actions/update-product/update-product.mjs
+++ components/woocommerce/actions/update-product/update-product.mjs
@@ -4,7 +4,7 @@ export default {
   key: "woocommerce-update-product",
   name: "Update Product",
   description: "Updates a product. [See the docs](https://woocommerce.github.io/woocommerce-rest-api-docs/#update-a-product)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     woocommerce,
