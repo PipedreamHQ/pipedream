6d6301cdf68256b314e2e10e4443fb58b36caa8a
Andrew Chuang <andrewjschuang@gmail.com>
2022-06-09 10:03:59 -0300

    Mautic Legacy Refactor and New Triggers (#3073)
    * add app axios request method
    
    * add paginate method
    
    * refactor actions
    
    * extract variables from response
    
    * fix typos
    
    * change k to key
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * new sources
    
    * add webhook signature verification
    
    * retrieve historical events on deploy
    
    * rename event types to constants
    
    * update pnpm-lock.yaml
    
    * fix company prop
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>

--- components/mautic/actions/delete-company/delete-company.mjs
+++ components/mautic/actions/delete-company/delete-company.mjs
@@ -1,35 +1,26 @@
-// legacy_hash_id: a_RAiXXm
-import { axios } from "@pipedream/platform";
+import mautic from "../../mautic.app.mjs";
 
 export default {
   key: "mautic-delete-company",
   name: "Delete Company",
-  description: "Deletes a company.",
-  version: "0.1.1",
+  description: "Deletes a company. [See docs](https://developer.mautic.org/#delete-company)",
+  version: "0.2.0",
   type: "action",
   props: {
-    mautic: {
-      type: "app",
-      app: "mautic",
-    },
-    company_id: {
-      type: "string",
-      description: "ID of the company to get details.",
+    mautic,
+    companyId: {
+      propDefinition: [
+        mautic,
+        "companyId",
+      ],
     },
   },
   async run({ $ }) {
-  //See the API docs: https://developer.mautic.org/#delete-company
-
-    if (!this.company_id) {
-      throw new Error("Must provide company_id parameter.");
-    }
-
-    return await axios($, {
-      method: "delete",
-      url: `${this.mautic.$auth.mautic_url}/api/companies/${this.company_id}/delete`,
-      headers: {
-        Authorization: `Bearer ${this.mautic.$auth.oauth_access_token}`,
-      },
+    const response = await this.mautic.deleteCompany({
+      $,
+      companyId: this.companyId.value,
     });
+    $.export("$summary", "Successfully deleted company");
+    return response;
   },
 };
