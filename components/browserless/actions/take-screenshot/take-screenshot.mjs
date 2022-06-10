8ffd2f23f5741b9475cc76c315e2ba4aa0b1c72a
Dylan Pierce <me@dylanjpierce.com>
2022-06-10 11:56:32 -0400

    Adding viewport and screenshot options

--- components/browserless/actions/take-screenshot/take-screenshot.mjs
+++ components/browserless/actions/take-screenshot/take-screenshot.mjs
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer-core";
 export default {
   key: "browserless-take-screenshot",
   name: "Take a Screenshot",
-  version: "0.5.1",
+  version: "0.5.3",
   type: "action",
   props: {
     browserless: {
@@ -16,16 +16,29 @@ export default {
       label: "URL",
       description: "Enter the URL you'd like to take a screenshot of here",
     },
+    screenshotOptions: {
+      type: "object",
+      label: "Screenshot Options",
+      optional: true,
+      description: "Pass options directly to the `Page.screenshot()` method. [Full list available here](https://puppeteer.github.io/puppeteer/docs/9.1.1/puppeteer.page.screenshot/)."
+    },
+    viewportOptions: {
+      type: "object",
+      label: "Viewport Options",
+      optional: true,
+      description: "Set the browsers viewport options. Passed to the `page.setViewport()` method."
+    }
   },
   async run({ $ }) {
     const browser = await puppeteer.connect({
       browserWSEndpoint: `wss://chrome.browserless.io?token=${this.browserless.$auth.api_key}`,
     });
     const page = await browser.newPage();
+    page.setViewport(this.viewportOptions)
 
     const { url } = this;
     await page.goto(url);
-    const screenshot = await page.screenshot();
+    const screenshot = await page.screenshot(this.screenshotOptions ?? {});
 
     // export the base64-encoded screenshot for use in future steps,
     // along with the image type and filename
