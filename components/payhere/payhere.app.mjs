2774370be5cf13351e5c31e7a7ce3102f9a8be78
Lucas Caresia <lucascarezia@gmail.com>
2022-06-13 12:24:28 -0300

    Added events

--- components/payhere/payhere.app.mjs
+++ components/payhere/payhere.app.mjs
@@ -1,11 +1,44 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "payhere",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _environment() {
+      return this.$auth.environment;
+    },
+    _apiUrl() {
+      if (this._environment() === "sandbox") {
+        return "https://sandbox.payhere.co/api/v1";
+      }
+
+      return "https://api.payhere.co/api/v1";
+    },
+    async _makeRequest(path, options = {}, $ = undefined) {
+      return axios($ ?? this, {
+        url: `${this._apiUrl()}/${path}`,
+        headers: {
+          Authorization: `Bearer ${this._apiKey()}`,
+        },
+        ...options,
+      });
+    },
+    async createWebhook(data) {
+      const response = await this._makeRequest("hooks", {
+        method: "post",
+        data,
+      });
+
+      return response.data;
+    },
+    async removeWebhook(webhookId) {
+      return this._makeRequest(`hooks/${webhookId}`, {
+        method: "delete",
+      });
     },
   },
 };
