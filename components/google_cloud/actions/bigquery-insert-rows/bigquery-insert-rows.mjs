5e9e446f24c83cd8867e5668d8a5a2f16ae952fa
Jorge Cortes <jacortesmahmud@gmail.com>
2022-06-13 17:12:51 -0500

    Refactor and added new action bigquery-insert-rows (#3070)
    * Refactor and added new action bigquery-insert-rows
    
    * Removed GoogleAuth and modified BigQuery parameters with just credentials and projectId
    
    * Added try/catch block to bigquery action
    
    * Addressed comments in PR
    
    * Modified query in bigquery-new-row source to make it inclusive

---
+++ components/google_cloud/actions/bigquery-insert-rows/bigquery-insert-rows.mjs
@@ -0,0 +1,61 @@
+import googleCloud from "../../google_cloud.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  name: "Bigquery Insert Rows",
+  version: "0.0.1",
+  key: "google_cloud-bigquery-insert-rows",
+  type: "action",
+  description: "Inserts rows into a BigQuery table. [See the docs](https://github.com/googleapis/nodejs-bigquery) and for an example [here](https://github.com/googleapis/nodejs-bigquery/blob/main/samples/insertRowsAsStream.js).",
+  props: {
+    googleCloud,
+    datasetId: {
+      propDefinition: [
+        googleCloud,
+        "datasetId",
+      ],
+    },
+    tableId: {
+      propDefinition: [
+        googleCloud,
+        "tableId",
+        ({ datasetId }) => ({
+          datasetId,
+        }),
+      ],
+    },
+    rows: {
+      type: "string[]",
+      label: "Rows",
+      description: "The rows to insert into the table. Each row is a JSON object with column names as keys and rows as values. E.g. `{\"name\": \"John\", \"age\": 20}`",
+    },
+  },
+  async run({ $ }) {
+    const {
+      datasetId,
+      tableId,
+      rows,
+    } = this;
+
+    if (!Array.isArray(rows)) {
+      throw new Error("Rows must be an array");
+    }
+
+    const rowsParsed = rows.map(utils.parse);
+
+    try {
+      const response = await this.googleCloud
+        .getBigQueryClient()
+        .dataset(datasetId)
+        .table(tableId)
+        .insert(rowsParsed);
+
+      $.export("$summary", `Successfully inserted ${rows.length} rows into ${datasetId}.${tableId}`);
+
+      return response;
+    } catch (error) {
+      console.log(JSON.stringify(error.errors, null, 2));
+      throw error;
+    }
+  },
+};
