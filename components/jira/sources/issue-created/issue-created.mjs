304669bd7fb90291d2debbd585e731e686ceb7cf
feyzo <feyzullah.metu@gmail.com>
2022-06-13 18:56:49 +0300

    Jira 2 new actions, new sources, action refactors, bump action versions

---
+++ components/jira/sources/issue-created/issue-created.mjs
@@ -0,0 +1,22 @@
+import common from "../common/common.mjs";
+
+export default {
+  key: "jira-issue-created",
+  name: "New Issue Created Event",
+  description: "Emit new event when an issue is created. Note that Jira supports only one webhook, if more sources are needed please use `New Event` source and select multiple events.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  ...common,
+  props: {
+    ...common.props,
+  },
+  methods: {
+    ...common.methods,
+    getEvents() {
+      return [
+        "jira:issue_created",
+      ];
+    },
+  },
+};
