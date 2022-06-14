e739c44beb123edfac5d13cabcb530bf7be31789
guilherme.falcao <gtfalcao96@gmail.com>
2022-06-14 17:58:48 -0300

    [App:SurveyMonkey] Error and summary handling For all list actions

--- components/survey_monkey/actions/common/base-list-action.mjs
+++ components/survey_monkey/actions/common/base-list-action.mjs
@@ -1,24 +1,31 @@
 export default {
   methods: {
     getItemName() {
-      throw new Error("Item name not defined for this action");
+      throw new Error("Item name not defined for this list action");
     },
     async runRequest() {
-      throw new Error("Request method not defined for this action");
+      throw new Error("Request method not defined for this list action");
     },
   },
   async run({ $ }) {
     const response = await this.runRequest($);
 
     const amount = response.length;
-    $.export(
-      "$summary",
-      `Successfully fetched ${amount} ${this.getItemName()}${
-        amount === 1
-          ? ""
-          : "s"
-      }`,
-    );
+    if (typeof amount !== "number") {
+      throw new Error("Unknown return value for this list action");
+    }
+
+    const itemName = this.getItemName();
+    const summary =
+      amount === 0
+        ? `Successful request, but no ${itemName}s were found`
+        : `Successfully fetched ${amount} ${itemName}${
+          amount === 1
+            ? ""
+            : "s"
+        }`;
+
+    $.export("$summary", summary);
     return response;
   },
 };
