5e1fe2b3da0394355b5e6851b160998256169280
Lucas Caresia <lucascarezia@gmail.com>
2022-06-16 16:59:36 -0300

    package.json and one action

--- components/toggl/toggl.app.mjs
+++ components/toggl/toggl.app.mjs
@@ -1,11 +1,24 @@
+import TogglClient from "toggl-api";
+
 export default {
   type: "app",
   app: "toggl",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiToken() {
+      return this.$auth.api_token;
+    },
+    _client() {
+      return new TogglClient({
+        apiToken: this._apiToken(),
+      });
+    },
+    async getCurrentTimeEntry() {
+      const a = await this._client().getCurrentTimeEntry();
+
+      console.log(a);
+      return this._client().getCurrentTimeEntry()
+        .promise();
     },
   },
 };
