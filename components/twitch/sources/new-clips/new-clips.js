0a7853bfc8854c053d844450bfe9d214f7a44014
Birdie <Birdie0@users.noreply.github.com>
2022-06-17 08:59:19 +0000

    twitch: new clips - allow to filter by broadcaster

--- components/twitch/sources/new-clips/new-clips.js
+++ components/twitch/sources/new-clips/new-clips.js
@@ -6,11 +6,17 @@ module.exports = {
   name: "New Clips",
   key: "twitch-new-clips",
   description:
-    "Emits an event when there is a new clip for the specified game.",
-  version: "0.0.1",
+    "Emit new event when there is a new clip for the specified game and/or streamer.",
+  version: "0.0.2",
+  type: "source",
   props: {
     ...common.props,
-    game: { propDefinition: [twitch, "game"] },
+    game: { propDefinition: [twitch, "game"], optional: true },
+    broadcaster: {
+      propDefinition: [twitch, "broadcaster"],
+      type: "string",
+      optional: true,
+    },
     max: { propDefinition: [twitch, "max"] },
   },
   methods: {
@@ -25,17 +31,36 @@ module.exports = {
     },
   },
   async run() {
-    const { data: gameData } = await this.twitch.getGames([this.game]);
-    if (gameData.length == 0) {
-      console.log(`No game found with the name ${this.game}`);
+    if (!this.game && !this.broadcaster) {
+      console.log("Game or user have to be specified");
       return;
     }
 
-    // get and emit new clips of the specified game
-    const params = {
-      game_id: gameData[0].id,
-      started_at: this.getLastEvent(this.db.get("lastEvent")),
-    };
+    const params = {};
+
+    if (this.game) {
+      const { data: gameData } = await this.twitch.getGames([this.game]);
+      if (gameData.length == 0) {
+        console.log(`No game found with the name ${this.game}`);
+        return;
+      }
+
+      params.game_id = gameData[0].id;
+    }
+
+    if (this.broadcaster) {
+      const { data: broadcasterData } = await this.twitch.getUsers([this.broadcaster]);
+      if (broadcasterData.length == 0) {
+        console.log(`No user found with the name ${this.broadcaster}`);
+        return;
+      }
+
+      params.broadcaster_id = broadcasterData[0].id;
+    }
+
+    // get and emit new clips of the specified game/streamer
+    params.started_at = this.getLastEvent(this.db.get("lastEvent"));
+
     const clips = await this.paginate(
       this.twitch.getClips.bind(this),
       params,
