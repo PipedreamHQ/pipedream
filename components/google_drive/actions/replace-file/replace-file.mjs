8e4e3960ec3f9aa46d9f62395a022a3bfa84261c
vellames-turing <91911778+vellames-turing@users.noreply.github.com>
2022-06-20 13:00:18 -0300

    Bugfix - Replace File - Google Drive (#3155)
    * replace file
    
    * minors
    
    * fix media type

--- components/google_drive/actions/replace-file/replace-file.mjs
+++ components/google_drive/actions/replace-file/replace-file.mjs
@@ -1,14 +1,21 @@
 import path from "path";
 import googleDrive from "../../google_drive.app.mjs";
 import { omitEmptyStringValues } from "../../utils.mjs";
-
-import { getFileStream } from "../../utils.mjs";
+import {
+  getFileStream,
+  streamToBuffer,
+  byteToMB,
+} from "../../utils.mjs";
+import {
+  GOOGLE_DRIVE_UPLOAD_TYPE_MEDIA,
+  GOOGLE_DRIVE_UPLOAD_TYPE_RESUMABLE,
+} from "../../constants.mjs";
 
 export default {
   key: "google_drive-replace-file",
   name: "Replace File",
   description: "Upload a file that replaces an existing file. [See the docs](https://developers.google.com/drive/api/v3/reference/files/update) for more information",
-  version: "0.0.4",
+  version: "0.0.5",
   type: "action",
   props: {
     googleDrive,
@@ -57,6 +64,13 @@ export default {
       ],
       description: "The MIME type of the new file (e.g., `image/jpeg`)",
     },
+    uploadType: {
+      propDefinition: [
+        googleDrive,
+        "uploadType",
+      ],
+      optional: true,
+    },
   },
   async run({ $ }) {
     const {
@@ -66,6 +80,7 @@ export default {
       name,
       mimeType,
     } = this;
+    let { uploadType } = this;
     if (!fileUrl && !filePath) {
       throw new Error("One of File URL and File Path is required.");
     }
@@ -74,15 +89,44 @@ export default {
       fileUrl,
       filePath,
     });
+
+    if (!uploadType || uploadType === "") {
+      try {
+        // Its necessary to get the file stream again, after user streamToBuffer function and pass
+        // the same object to updateFileMedia function, the function will throw an error about
+        // circular json structure.
+        // Deep clone is very slow in this case, so its better get the stream again
+        const fileBuffer = await streamToBuffer(await getFileStream({
+          $,
+          fileUrl,
+          filePath,
+        }));
+        const bufferSize = byteToMB(Buffer.byteLength(fileBuffer));
+        uploadType = bufferSize > 5
+          ? GOOGLE_DRIVE_UPLOAD_TYPE_RESUMABLE
+          : undefined;
+      } catch (err) {
+        console.log(err);
+        uploadType = undefined;
+      }
+    }
+
+    if (uploadType === GOOGLE_DRIVE_UPLOAD_TYPE_MEDIA) {
+      uploadType = undefined;
+    }
+    console.log(`Upload type: ${uploadType}`);
     // Update file media separately from metadata to prevent multipart upload,
     // which `google-apis-nodejs-client` doesn't seem to support for
     // [files.update](https://bit.ly/3lP5sWn)
     await this.googleDrive.updateFileMedia(fileId, fileStream, omitEmptyStringValues({
       mimeType,
+      uploadType,
     }));
+
     const resp = await this.googleDrive.updateFile(fileId, omitEmptyStringValues({
       name: name || path.basename(fileUrl || filePath),
       mimeType,
+      uploadType,
     }));
     $.export("$summary", "Successfully replaced the file");
     return resp;
