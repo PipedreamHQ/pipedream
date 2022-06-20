8cc4b1ac37cded29be5fbb8b6502f8cf7a87db86
Alyson Freitas <103650302+alysonturing@users.noreply.github.com>
2022-06-20 15:58:35 -0300

    Update components/rss/sources/common/common.ts
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/rss/sources/common/common.ts
+++ components/rss/sources/common/common.ts
@@ -1,9 +1,23 @@
+import rss from "../../app/rss.app";
+
 export default {
-  generateMeta: ($, item) => {
-    return {
-      id: $.rss.itemKey(item),
-      summary: item.title,
-      ts: $.rss.itemTs(item),
-    };
+  props: {
+    rss,
+    timer: {
+      propDefinition: [
+        rss,
+        "timer",
+      ],
+    },
+  },
+  methods: {
+    generateMeta: (item) => {
+      return {
+        id: this.rss.itemKey(item),
+        summary: item.title,
+        ts: this.rss.itemTs(item),
+      };
+    }
   },
 };
+
