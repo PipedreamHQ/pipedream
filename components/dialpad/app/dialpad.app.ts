dde9d7bfdf75fc6fbceb02b0aa0a4366e8af773c
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-06-22 15:03:52 +0100

    App Setup completed

--- components/dialpad/app/dialpad.app.ts
+++ components/dialpad/app/dialpad.app.ts
@@ -1,13 +1,73 @@
 import { defineApp } from "@pipedream/types";
+import constants from "../common/constants.js";
+import { axios } from "@pipedream/platform";
 
 export default defineApp({
   type: "app",
   app: "dialpad",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    _getUrl(path) {
+      const {
+        BASE_URL,
+        HTTP_PROTOCOL,
+        VERSION_PATH,
+      } = constants;
+      return `${HTTP_PROTOCOL}${BASE_URL}${VERSION_PATH}${path}`;
+    },
+    async _makeRequest(args) {
+      const {
+        $,
+        method = "get",
+        path,
+        params,
+        data,
+      } = args;
+      const config = {
+        method,
+        url: this._getUrl(path),
+        headers: this._getHeaders(),
+        params,
+        data,
+      };
+      return axios($ ?? this, config);
+    },
+    async createWebhook({
+      path, hook_url, data,
+    }) {
+      // get webhook ID
+      const webhook = await this._makeRequest({
+        path: "/webhooks",
+        method: "post",
+        data: {
+          hook_url,
+        },
+      });
+
+      // Create webhook event with the webhook ID generated above
+      return this._makeRequest({
+        path,
+        method: "post",
+        data: {
+          webhook_id: webhook.id,
+          enabled: true,
+          ...data,
+        },
+      });
+    },
+    async removeWebhook({
+      path, webhookId,
+    }) {
+      return await this._makeRequest({
+        path: `${path}/${webhookId}`,
+        method: "delete",
+      });
     },
   },
 });
