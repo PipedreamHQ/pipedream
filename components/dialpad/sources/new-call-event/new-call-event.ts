3dcf8b684b7a6434ef2ea8c7641f4d9e2b943d2d
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-06-22 15:05:35 +0100

    New call event completed

---
+++ components/dialpad/sources/new-call-event/new-call-event.ts
@@ -0,0 +1,27 @@
+import { defineSource } from "@pipedream/types";
+import constants from "../../common/constants";
+import common from "../../common/common";
+
+export default defineSource({
+  ...common,
+  type: "source",
+  name: "New Call Event",
+  key: "dialpad-new-call-event",
+  description: `Emit new call event subscription. 
+  See [Event doc](https://developers.dialpad.com/reference/webhook_call_event_subscriptioncreate)
+  and [webhook doc](https://developers.dialpad.com/reference/webhookscreate)`,
+  version: "0.0.1",
+  methods: {
+    ...common.methods,
+    getPath() {
+      return constants.EVENT_TYPE.NEW_CALL.path;
+    },
+    processEvent(event) {
+      const { body } = event;
+      this.$emit(body, {
+        id: body.id,
+        summary: `New call event - ${body.id}`,
+      });
+    },
+  },
+});
