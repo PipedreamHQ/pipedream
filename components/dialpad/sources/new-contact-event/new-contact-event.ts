176410db4007028bc3c34a0766e07208e5f4180e
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-06-22 15:05:50 +0100

    New contact event completed

---
+++ components/dialpad/sources/new-contact-event/new-contact-event.ts
@@ -0,0 +1,42 @@
+import { defineSource } from "@pipedream/types";
+import dialpad from "../../app/dialpad.app";
+import constants from "../../common/constants";
+import common from "../../common/common";
+
+export default defineSource({
+  ...common,
+  type: "source",
+  name: "New Contact Event",
+  key: "dialpad-new-contact-event",
+  description: `Emit new contact event subscription. 
+  See [Event doc](https://developers.dialpad.com/reference/webhook_contact_event_subscriptioncreate)
+  and [webhook doc](https://developers.dialpad.com/reference/webhookscreate)`,
+  version: "0.0.1",
+  props: {
+    dialpad,
+    contactType: {
+      type: "string",
+      label: "Contact type",
+      description: "The contact type this event subscription subscribes to.",
+      options: constants.CONTACT_EVENT_TYPE,
+    },
+  },
+  methods: {
+    ...common.methods,
+    getPath() {
+      return constants.EVENT_TYPE.UPDATE_CONTACT.path;
+    },
+    getPayload() {
+      return {
+        contact_type: this.contactType,
+      };
+    },
+    processEvent(event) {
+      const { body } = event;
+      this.$emit(body, {
+        id: body.id,
+        summary: `New Contact event - ${body.id}`,
+      });
+    },
+  },
+});
