50ed97a3d8c7c1a8bd0bdecad7ab99cbb6a37e7c
bush4u <68253993+bush4u@users.noreply.github.com>
2022-06-22 18:44:30 +0100

    Update components/dialpad/sources/update-contact-event/update-contact-event.ts
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/dialpad/sources/update-contact-event/update-contact-event.ts
+++ components/dialpad/sources/update-contact-event/update-contact-event.ts
@@ -13,7 +13,7 @@ export default defineSource({
   and [webhook doc](https://developers.dialpad.com/reference/webhookscreate)`,
   version: "0.0.1",
   props: {
-    dialpad,
+    ...common.props,
     contactId: {
       type: "string",
       label: "Contact subscription ID",
