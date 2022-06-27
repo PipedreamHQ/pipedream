de9dd4586da0923df65eed451d58c4a99d4e0e29
Luan Cazarine <luanhc@gmail.com>
2022-06-27 16:46:59 -0300

    adjusts - remove article

--- components/figma/actions/delete-comment/delete-comment.mjs
+++ components/figma/actions/delete-comment/delete-comment.mjs
@@ -3,7 +3,7 @@ import figmaApp from "../../figma.app.mjs";
 export default {
   name: "Delete a Comment",
   description: "Delete a comment to a file. [See the docs here](https://www.figma.com/developers/api#delete-comments-endpoint)",
-  key: "figma-delete-a-comment",
+  key: "figma-delete-comment",
   version: "0.0.1",
   type: "action",
   props: {
