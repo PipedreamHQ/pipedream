4e8f97ad03a820f2a34d230facdd66b082cf4647
Dave Rogers <aca02djr@users.noreply.github.com>
2022-06-30 16:33:52 +0100

    Fix an error trying to assign to an undefined object when using the Reply To params

--- components/sendgrid/actions/send-email-single-recipient/send-email-single-recipient.mjs
+++ components/sendgrid/actions/send-email-single-recipient/send-email-single-recipient.mjs
@@ -251,7 +251,7 @@ export default {
     //specified, with `email` being required.
     let replyTo = undefined;
     if (this.replyToEmail) {
-      replyTo.email = this.replyToEmail;
+      replyTo = { email: this.replyToEmail };
       if (this.replyToName) {
         replyTo.name = this.replyToName;
       }
