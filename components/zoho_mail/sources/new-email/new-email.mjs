8a780c28b10235098de165d711bec94de9c7bf2d
michelle0927 <michellelbergero@hotmail.com>
2022-07-01 13:08:38 -0400

    new-email source

---
+++ components/zoho_mail/sources/new-email/new-email.mjs
@@ -0,0 +1,65 @@
+import zohoMail from "../../zoho_mail.app.mjs";
+
+export default {
+  key: "zoho_mail-new-email",
+  name: "New Email",
+  description: "Emit new event each time a new email is received",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    zohoMail,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      label: "Polling Interval",
+      description: "Pipedream will poll the Zoho API on this schedule",
+      default: {
+        intervalSeconds: 60 * 15, // 15 minutes
+      },
+    },
+    account: {
+      propDefinition: [
+        zohoMail,
+        "account",
+      ],
+    },
+  },
+  methods: {
+    _getLastReceivedTime() {
+      return this.db.get("lastReceivedTime");
+    },
+    _setLastReceivedTime(lastReceivedTime) {
+      this.db.set("lastReceivedTime", lastReceivedTime);
+    },
+    isLater(receivedTime, lastReceivedTime) {
+      return (!lastReceivedTime || parseInt(receivedTime) > parseInt(lastReceivedTime));
+    },
+    generateMeta(email) {
+      return {
+        id: email.messageId,
+        summary: email.subject,
+        ts: Date.parse(email.receivedTime),
+      };
+    },
+  },
+  async run() {
+    const lastReceivedTime = this._getLastReceivedTime();
+    let newLastReceivedTime = lastReceivedTime;
+    const results = await this.zohoMail.listEmails({
+      accountId: this.account,
+      sortorder: false, // descending
+    });
+    for (const email of results) {
+      if (this.isLater(email.receivedTime, lastReceivedTime)) {
+        if (this.isLater(email.receivedTime, newLastReceivedTime)) {
+          newLastReceivedTime = email.receivedTime;
+        }
+        const meta = this.generateMeta(email);
+        this.$emit(email, meta);
+      }
+    }
+
+    this._setLastReceivedTime(newLastReceivedTime);
+  },
+};
--- components/zoho_mail/zoho_mail.app.mjs
+++ components/zoho_mail/zoho_mail.app.mjs
@@ -1,11 +1,53 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "zoho_mail",
-  propDefinitions: {},
+  propDefinitions: {
+    account: {
+      type: "string",
+      label: "Account ID",
+      description: "The unique Zoho Account number associated with the particular account",
+      async options() {
+        const accounts = await this.listAccounts();
+        return accounts.map((account) => ({
+          label: account.displayName ?? account.accountId,
+          value: account.accountId,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    async makeRequest(args = {}) {
+      const {
+        method = "GET",
+        path,
+        $ = this,
+      } = args;
+      const config = {
+        method,
+        url: `http://mail.zoho.com/api/${path}`,
+        headers: {
+          "Authorization": `Zoho-oauthtoken ${this.$auth.oauth_access_token}`,
+          "User-Agent": "@PipedreamHQ/pipedream v0.1",
+        },
+        ...args,
+      };
+      return axios($, config);
+    },
+    async listAccounts({ $ } = {}) {
+      return (await this.makeRequest({
+        path: "accounts",
+        $,
+      })).data;
+    },
+    async listEmails({
+      $, accountId,
+    } = {}) {
+      return (await this.makeRequest({
+        path: `accounts/${accountId}/messages/view`,
+        $,
+      })).data;
     },
   },
 };
