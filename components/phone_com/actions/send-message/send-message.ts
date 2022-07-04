a7541099806e8ac943b5ee5d912eafa920e1ae44
guilherme.falcao <gtfalcao96@gmail.com>
2022-07-04 17:59:53 -0300

    [App:Phone_com] Adjusting action description

--- components/phone_com/actions/send-message/send-message.ts
+++ components/phone_com/actions/send-message/send-message.ts
@@ -3,7 +3,7 @@ import { defineAction } from "@pipedream/types";
 
 export default defineAction({
   name: "Send Message",
-  description: "Send a message via Phone.com [See docs](https://apidocs.phone.com/reference/post_v4-accounts-voip-id-messages)",
+  description: "Send a message via Phone.com [See docs here](https://apidocs.phone.com/reference/post_v4-accounts-voip-id-messages)",
   key: "phone_com-send-message",
   version: "0.0.1",
   type: "action",
