20b3bdf5511e74cffafee426912a69a89a72d77e
Lucas Caresia <lucascarezia@gmail.com>
2022-07-07 20:22:01 -0300

    Added create payment action

---
+++ components/quickbooks/actions/create-payment/create-payment.mjs
@@ -0,0 +1,60 @@
+import quickbooks from "../../quickbooks.app.mjs";
+
+export default {
+  key: "quickbooks-create-payment",
+  name: "Create Payment",
+  description: "Creates a payment.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    quickbooks,
+    totalAmount: {
+      label: "Total Amount",
+      description: "Indicates the total amount of the transaction. This includes the total of all the charges, allowances, and taxes. E.g. 25.0",
+      type: "string",
+    },
+    customerRefValue: {
+      label: "Customer Reference Value",
+      description: "Reference to a customer or job. Query the Customer name list resource to determine the appropriate Customer object for this reference. Use `Customer.Id` from that object for `CustomerRef.value`.",
+      type: "string",
+    },
+    customerRefName: {
+      label: "Customer Reference Name",
+      description: "Reference to a customer or job. Query the Customer name list resource to determine the appropriate Customer object for this reference. Use `Customer.DisplayName ` from that object for `CustomerRef.name`.",
+      type: "string",
+      optional: true,
+    },
+    currencyRefValue: {
+      label: "Currency Reference Value",
+      description: "A three letter string representing the ISO 4217 code for the currency. For example, `USD`, `AUD`, `EUR`, and so on. This must be defined if multicurrency is enabled for the company.\nMulticurrency is enabled for the company if `Preferences.MultiCurrencyEnabled` is set to `true`. Read more about multicurrency support [here](https://developer.intuit.com/docs?RedirectID=MultCurrencies). Required if multicurrency is enabled for the company.",
+      type: "string",
+      optional: true,
+    },
+    currencyRefName: {
+      label: "Currency Reference Name",
+      description: "The full name of the currency.",
+      type: "object",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.quickbooks.createPayment({
+      $,
+      data: {
+        TotalAmt: this.totalAmount,
+        CustomerRef: {
+          value: this.customerRefValue,
+          name: this.customerRefName,
+        },
+        CurrencyRef: {
+          value: this.currencyRefValue,
+          name: this.currencyRefName,
+        },
+      },
+    });
+
+    response && $.export("summary", "Successfully created payment");
+
+    return response;
+  },
+};
--- components/quickbooks/quickbooks.app.mjs
+++ components/quickbooks/quickbooks.app.mjs
@@ -1,11 +1,35 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "quickbooks",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _companyId() {
+      return this.$auth.company_id;
+    },
+    _accessToken() {
+      return this.$auth.oauth_access_token;
+    },
+    _apiUrl() {
+      return "https://quickbooks.api.intuit.com/v3";
+    },
+    async _makeRequest(path, options = {}, $ = undefined) {
+      return axios($ ?? this, {
+        url: `${this._apiUrl()}/${path}`,
+        headers: {
+          Authorization: `Bearer ${this._accessToken()}`,
+        },
+        ...options,
+      });
+    },
+    async createPayment({
+      $, data,
+    }) {
+      return this._makeRequest(`company/${this._companyId()}/payment`, {
+        method: "post",
+        data,
+      }, $);
     },
   },
 };
