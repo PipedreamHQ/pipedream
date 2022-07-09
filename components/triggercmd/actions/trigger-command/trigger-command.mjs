0e592fc2e9eb5471505874442f52ca9a674f6b21
Russell VanderMey <russell@vandermey.us>
2022-07-08 20:13:18 -0400

    Move trigger function to app, and use axios from @pipedream/platform

--- components/triggercmd/actions/trigger-command/trigger-command.mjs
+++ components/triggercmd/actions/trigger-command/trigger-command.mjs
@@ -1,11 +1,11 @@
 import triggercmd from "../../triggercmd.app.mjs";
-import axios from "axios";
 
 export default {
   name: "Trigger a command",
-  version: "0.0.12",
+  version: "0.0.26",
   key: "trigger-command",
   description: "Runs a command on a computer.  Refer to the [TRIGGERcmd Forum](https://triggercmd.com/forum) to learn more.",
+  type: "action",
   props: {
     token: {
       type: "string",
@@ -26,23 +26,9 @@ export default {
       optional: true
     }
   },
-  type: "action",
-  methods: {},
-  async run() {
-    // Make an HTTP POST request using axios
-    const resp = await axios({
-      method: "POST",
-      url: `https://www.triggercmd.com/api/run/triggerSave`,
-      data: {
-        token: this.token,
-        computer: this.computer,
-        trigger: this.trigger,
-        params: this.params
-      },
-    });
-
-    // Retrieve just the data from the response
-    const { data } = resp;
-    console.log(data)
+  async run({ $ }) {
+    const message = await triggercmd.methods.trigger(this.token, this.computer, this.trigger, this.params, $);
+    return message;
   },
+
 };
--- components/triggercmd/triggercmd.app.mjs
+++ components/triggercmd/triggercmd.app.mjs
@@ -1,11 +1,38 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "triggercmd",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://www.triggercmd.com";
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      const res = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return res;
+    },
+    async trigger(token, computer, trigger, params, ctx = this) {
+      const res = await axios(ctx, this._getAxiosParams({
+        method: "POST",
+        path: `/api/run/triggerSave`,
+        data: {
+          token: token,
+          computer: computer,
+          trigger: trigger,
+          params: params
+        },
+      }));
+      return res?.message || "Something went wrong.";
     },
   },
 };
