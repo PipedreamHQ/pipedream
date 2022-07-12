22164186dd1473fcd2ed498608ee8051d71ef4be
feyzullah <feyzullah.metu@gmail.com>
2022-07-12 17:31:40 +0300

    Update components/ghost_org_admin_api/actions/create-member/create-member.mjs

--- components/ghost_org_admin_api/actions/create-member/create-member.mjs
+++ components/ghost_org_admin_api/actions/create-member/create-member.mjs
@@ -3,7 +3,7 @@ import app from "../../ghost_org_admin_api.app.mjs";
 export default {
   key: "ghost_org_admin_api-create-member",
   name: "Create Member",
-  description: "Create a new member in Ghost. [See the docs here](https://ghost.org/docs/admin-api/#members)",
+  description: "Create a new member in Ghost. [See the docs here](https://ghost.org/docs/admin-api/#creating-a-member)",
   version: "0.0.1",
   type: "action",
   props: {
