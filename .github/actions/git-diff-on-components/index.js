70664c7724d5216c995f310c701a5de90baac70d
Jorge Cortes <jacortesmahmud@gmail.com>
2022-07-12 20:24:07 -0500

    added custom action to print variables

---
+++ .github/actions/git-diff-on-components/index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const baseCommit = core.getInput("base_commit");
+  const headCommit = core.getInput("head_commit");
+  const allFiles = core.getInput("all_files");
+
+  console.log("baseCommit", baseCommit);
+  console.log("headCommit", headCommit);
+  console.log("allFiles", allFiles);
+
+  core.setOutput("baseCommit output", baseCommit);
+  core.setOutput("headCommit output", headCommit);
+  core.setOutput("allFiles output", allFiles);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
