1fbebb4f727f4b05ec2a6f28479d2d9e3be4578f
Lucas Caresia <lucascarezia@gmail.com>
2022-07-13 10:44:15 -0300

    Revert "Added some actions"
    This reverts commit 201d8945572cf9f6e598ed425769fb53e11bec0c.

--- components/expensify/app/expensify.app.ts
+++ components/expensify/app/expensify.app.ts
@@ -1,63 +1,13 @@
 import { defineApp } from "@pipedream/types";
-import { axios } from "@pipedream/platform";
-import qs from "qs";
 
 export default defineApp({
   type: "app",
   app: "expensify",
   propDefinitions: {},
   methods: {
-    _partnerUserId() {
-      return this.$auth.partnerUserId;
-    },
-    _partnerUserSecret() {
-      return this.$auth.partnerUserSecret;
-    },
-    _apiUrl() {
-      return "https://integrations.expensify.com/Integration-Server/ExpensifyIntegrations";
-    },
-    async _makeRequest(options: any = {}, $ = this) {
-      return axios($, {
-        url: `${this._apiUrl()}`,
-        ...options,
-        data: qs.stringify({
-          requestJobDescription: JSON.stringify({
-            credentials: {
-              partnerUserID: this._partnerUserId(),
-              partnerUserSecret: this._partnerUserSecret(),
-            },
-            ...options?.data,
-          }),
-        }),
-      });
-    },
-    async createExpense({
-      $, data,
-    }) {
-      return this._makeRequest({
-        method: "post",
-        data: {
-          type: "create",
-          inputSettings: {
-            type: "expenses",
-            ...data,
-          },
-        },
-      }, $);
-    },
-    async exportReportToPDF({
-      $, data,
-    }) {
-      return this._makeRequest({
-        method: "post",
-        data: {
-          type: "create",
-          inputSettings: {
-            type: "expenses",
-            ...data,
-          },
-        },
-      }, $);
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
     },
   },
 });
