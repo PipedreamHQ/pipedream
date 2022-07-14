67be238c1face691cd034b248c6a30f9b1ab22ff
Andrew Chuang <andrewjschuang@gmail.com>
2022-07-14 09:38:31 -0300

    bump sources

--- components/gorgias_oauth/sources/ticket-updated/ticket-updated.mjs
+++ components/gorgias_oauth/sources/ticket-updated/ticket-updated.mjs
@@ -3,7 +3,6 @@ import overrideApp from "../../common/override-app.mjs";
 
 overrideApp(base);
 
-// same version as base
 // eslint-disable-next-line pipedream/required-properties-version
 export default {
   ...base,
