997cffaf8fd4fc4684f9d75c9c8219359a6899e0
Lucas Caresia <lucascarezia@gmail.com>
2022-07-14 09:15:50 -0300

    Updated versions

--- components/linear_app/sources/comment-created-instant/comment-created-instant.mjs
+++ components/linear_app/sources/comment-created-instant/comment-created-instant.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Created Comment (Instant)",
   description: "Emit new event when a new comment is created. See the docs [here](https://developers.linear.app/docs/graphql/webhooks)",
   type: "source",
-  version: "0.0.2",
+  version: "0.0.3",
   dedupe: "unique",
   methods: {
     ...common.methods,
@@ -29,7 +29,7 @@ export default {
     },
     async getLoadedProjectId(event) {
       return event?._project?.id
-        || (await this.linearApp.getIssue(event?._issue?.id))?._project?.id ;
+        || (await this.linearApp.getIssue(event?._issue?.id))?._project?.id;
     },
     getMetadata(resource) {
       const {
