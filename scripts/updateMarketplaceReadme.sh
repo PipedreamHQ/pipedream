08ae625b797fd501c19f749666498f8ffb087da0
Moshe Grunwald <moshe@pipedream.com>
2022-07-18 15:33:08 -0400

    refactor: fixed spelling

--- scripts/updateMarketplaceReadme.sh
+++ scripts/updateMarketplaceReadme.sh
@@ -1,18 +1,18 @@
 MARKDOWN_B64=$(base64 -w 0 $FILE_PATH)
 # To run on Mac, you may need to remove the -w 0 flag
 
-PATH_SEGMANT=$(awk -F/components '{print $(NF)}' <<< $FILE_PATH)
+PATH_SEGMENT=$(awk -F/components '{print $(NF)}' <<< $FILE_PATH)
 
-# If there are three segmants, the readme is for an app. 
+# If there are three segments, the readme is for an app. 
 # If there is five, it is for a component
-SEGMANT_COUNT=$(awk -F/ '{print NF}' <<< $PATH_SEGMANT)
+SEGMENT_COUNT=$(awk -F/ '{print NF}' <<< $PATH_SEGMENT)
 
-if [ $SEGMANT_COUNT -eq 3 ]; then
+if [ $SEGMENT_COUNT -eq 3 ]; then
     # This is an app
-    KEY=$(awk -F/ '{print $(NF-1)}' <<< $PATH_SEGMANT)
-elif [ $SEGMANT_COUNT -eq 5 ]; then
+    KEY=$(awk -F/ '{print $(NF-1)}' <<< $PATH_SEGMENT)
+elif [ $SEGMENT_COUNT -eq 5 ]; then
     # This is a component
-    KEY=$(awk -F/ '{print $2 "-" $4}' <<< $PATH_SEGMANT)
+    KEY=$(awk -F/ '{print $2 "-" $4}' <<< $PATH_SEGMENT)
 else
     echo Invalid file path found
     echo This readme will not be published to the Pipedream Marketplace
@@ -23,4 +23,4 @@ fi
 curl -vv --location  --request POST "https://api.pipedream.com/graphql" \
 --header "Authorization: Bearer ${PD_API_KEY}" \
 --header 'Content-Type: application/json' \
---data-raw '{"query":"mutation addNewMarketplaceEntry ($markdownB64: String!, $path: String!, $key: String!) {\n  marketplaceContentSet (key: $key, markdownB64: $markdownB64,\n  path: $path) {\n    marketplaceContent {\n      id\n }\n    errors\n  }    \n}","variables":{"key": "'${KEY}'", "markdownB64":"'${MARKDOWN_B64}'", "path": "'${PATH_SEGMANT}'"}}'
+--data-raw '{"query":"mutation addNewMarketplaceEntry ($markdownB64: String!, $path: String!, $key: String!) {\n  marketplaceContentSet (key: $key, markdownB64: $markdownB64,\n  path: $path) {\n    marketplaceContent {\n      id\n }\n    errors\n  }    \n}","variables":{"key": "'${KEY}'", "markdownB64":"'${MARKDOWN_B64}'", "path": "'${PATH_SEGMENT}'"}}'
