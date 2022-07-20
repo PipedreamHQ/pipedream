2b1560049dc6bcb1a4b6d3145cbba000d3d2054c
Lucas Caresia <lucascarezia@gmail.com>
2022-07-20 11:09:12 -0300

    Finished the rest of actions

---
+++ components/nectar_crm/actions/common/constants.ts
@@ -0,0 +1,60 @@
+export default {
+  CONTACT_TYPES: [
+    {
+      label: "Customer",
+      value: "0",
+    },
+    {
+      label: "Prospect",
+      value: "1",
+    },
+    {
+      label: "Suspect",
+      value: "2",
+    },
+    {
+      label: "Lead",
+      value: "3",
+    },
+    {
+      label: "Discarded",
+      value: "4",
+    },
+  ],
+  TASK_TYPES: [
+    {
+      label: "Common",
+      value: "0",
+    },
+    {
+      label: "Call",
+      value: "1",
+    },
+    {
+      label: "Notification",
+      value: "2",
+    },
+    {
+      label: "Email",
+      value: "3",
+    },
+    {
+      label: "Appointment",
+      value: "4",
+    },
+  ],
+  TASK_PRIORITIES: [
+    {
+      label: "Normal",
+      value: "0",
+    },
+    {
+      label: "Important",
+      value: "1",
+    },
+    {
+      label: "Urgent",
+      value: "2",
+    },
+  ],
+};
