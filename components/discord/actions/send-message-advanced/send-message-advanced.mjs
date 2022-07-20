4ba5762f147772ca8f3030f5867d9e87230eaa63
michelle0927 <michellelbergero@hotmail.com>
2022-07-20 16:08:32 -0400

    discord actions

---
+++ components/discord/actions/send-message-advanced/send-message-advanced.mjs
@@ -0,0 +1,58 @@
+import common from "../common/common.mjs";
+
+export default {
+  ...common,
+  key: "discord-send-message-advanced",
+  name: "Send Message (Advanced)",
+  description: "Send a simple or structured message (using embeds) to a Discord channel",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    message: {
+      propDefinition: [
+        common.props.discord,
+        "message",
+      ],
+      optional: true,
+    },
+    embeds: {
+      propDefinition: [
+        common.props.discord,
+        "embeds",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const {
+      message,
+      avatarURL,
+      threadID,
+      username,
+      includeSentViaPipedream,
+      embeds,
+    } = this;
+
+    if (!message && !embeds) {
+      throw new Error("This action requires at least 1 message OR embeds object. Please enter one or the other above.");
+    }
+
+    try {
+      const resp = await this.discord.createMessage(this.channel, {
+        avatarURL,
+        threadID,
+        username,
+        embeds,
+        content: includeSentViaPipedream
+          ? this.appendPipedreamText(message ?? "")
+          : message,
+      });
+      $.export("$summary", "Message sent successfully");
+      return resp;
+    } catch (err) {
+      const unsentMessage = this.getUserInputProps();
+      $.export("unsent", unsentMessage);
+      throw err;
+    }
+  },
+};
