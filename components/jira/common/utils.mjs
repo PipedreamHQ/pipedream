7e0d52f6ce9e8471387725b6b8f48628900dab19
Luan Cazarine <luanhc@gmail.com>
2022-07-20 17:00:30 -0300

    fixed parseObject utils

--- components/jira/common/utils.mjs
+++ components/jira/common/utils.mjs
@@ -9,9 +9,15 @@ export default {
     return parsed;
   },
   parseObject(obj) {
+    if (typeof obj === "string")
+      obj = JSON.parse(obj);
+
     let parsed = {};
     for (let key in obj) {
-      parsed[key] = this.parseOne(obj[key].replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, "\"$2\": "));
+      if (typeof obj[key] != "object" && typeof obj[key] != "number")
+        parsed[key] = this.parseOne(obj[key].replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, "\"$2\": "));
+      else
+        parsed[key] = obj[key];
     }
     return parsed;
   },
