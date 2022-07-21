09248b28a38ea00be3dc9d2d0a1173441d512a37
Oluwatosin Iyiola <olu.iyiola1@gmail.com>
2022-07-21 10:41:11 +0100

    Setup done

--- components/convertkit/convertkit.app.mjs
+++ components/convertkit/convertkit.app.mjs
@@ -1,11 +1,61 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "convertkit",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiSecretToken() {
+      return this.$auth.api_secret;
+    },
+    async _makeRequest(path, options = {}, $ = this) {
+      if (options.method.toLowerCase() === "get") {
+        options.params = {
+          ...options.params,
+          api_secret: this._apiSecretToken(),
+        };
+      } else {
+        options.data = {
+          ...options.data,
+          api_secret: this._apiSecretToken(),
+        };
+      }
+      return axios($, {
+        url: `${constants.API_HOST}${constants.API_VERSION}/${path}`,
+        ...options,
+      });
+    },
+
+    async *paginate({
+      $, fn, payload,
+    }, dataField = null) {
+      do {
+        const response = await fn({
+          $,
+          ...payload,
+        });
+
+        for (const d of (dataField
+          ? response[dataField]
+          : response)) {
+          yield d;
+        }
+        if ( response.total_pages > response.page) {
+          payload.page++;
+          return true;
+        }
+        break;
+      } while (true);
+    },
+    async listSubscribers({
+      $, ...params
+    }) {
+      const options = {
+        method: "get",
+        params,
+      };
+      return await this._makeRequest("subscribers", options, $);
     },
   },
 };
