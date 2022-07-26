bd22040f37de285715153c0cc17c9974ed98386e
michelle0927 <michelle0927@users.noreply.github.com>
2022-07-26 10:22:14 -0400

    Zoom - Add GET request to certain Instant Webhook-Based Sources (#3464)
    * add historical events
    
    * updated pnpm-lock.yaml
    
    * update pnpm -lock.yaml
    
    * bug fix

--- components/zoom/sources/webinar-started/webinar-started.mjs
+++ components/zoom/sources/webinar-started/webinar-started.mjs
@@ -1,14 +1,15 @@
-import zoom from "../../zoom.app.mjs";
+import common from "../common/common.mjs";
 
 export default {
+  ...common,
   key: "zoom-webinar-started",
-  name: "Webinar Started",
-  description:
-    "Emits an event each time a webinar starts where you're the host",
-  version: "0.0.3",
-  dedupe: "unique", // Dedupe based on meeting ID
+  name: "Webinar Started (Instant)",
+  description: "Emit new event each time a webinar starts where you're the host",
+  version: "0.0.4",
+  type: "source",
+  dedupe: "unique", // Dedupe based on webinar ID
   props: {
-    zoom,
+    ...common.props,
     zoomApphook: {
       type: "$.interface.apphook",
       appProp: "zoom",
@@ -17,19 +18,41 @@ export default {
       ],
     },
   },
-  async run(event) {
-    const { payload } = event;
-    const { object } = payload;
-    this.$emit(
-      {
+  hooks: {
+    async deploy() {
+      const { webinars } = await this.zoom.listWebinars({
+        page_size: 25,
+      });
+      if (!webinars || webinars.length === 0) {
+        return;
+      }
+      const objects = this.sortByDate(webinars, "start_time");
+      for (const object of objects) {
+        const startTime = Date.parse(object.start_time);
+        if (startTime < Date.now()) {
+          this.emitEvent({
+            object,
+            time_stamp: startTime,
+          }, object);
+        }
+      }
+    },
+  },
+  methods: {
+    ...common.methods,
+    emitEvent(payload, object) {
+      const meta = this.generateMeta(object);
+      this.$emit({
         event: "webinar.started",
         payload,
-      },
-      {
-        summary: object.topic,
+      }, meta);
+    },
+    generateMeta(object) {
+      return {
         id: object.uuid,
+        summary: object.topic,
         ts: +new Date(object.start_time),
-      },
-    );
+      };
+    },
   },
 };
