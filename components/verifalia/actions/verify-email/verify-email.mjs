cc4af53e0df309b651cd28475c8865cb007b3937
Efran Cobisi <efran.cobisi@cobisi.com>
2022-08-01 18:42:21 +0200

    Update components/verifalia/actions/verify-email/verify-email.mjs
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>

--- components/verifalia/actions/verify-email/verify-email.mjs
+++ components/verifalia/actions/verify-email/verify-email.mjs
@@ -10,8 +10,7 @@ import {
 export default {
   name: "Verify Email Address",
   description: "Verify an email address and check if it is properly formatted, really exists and can accept mails, " +
-        "flagging spam traps, disposable emails and much more. " +
-        "[See the docs](https://verifalia.com/developers#email-validations-creating) for more information",
+        "flagging spam traps, disposable emails and much more. [See the docs](https://verifalia.com/developers#email-validations-creating) for more information",
   key: "verifalia-verify-email",
   version: "1.0.3",
   type: "action",
