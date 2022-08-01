769df0fbf8888a14d037387faa1539b600310675
Lucas Caresia <lucascarezia@gmail.com>
2022-08-01 16:44:59 -0300

    Added actions and sources

---
+++ components/nocrm_io/actions/create-lead/create-lead.mjs
@@ -0,0 +1,66 @@
+import nocrm_io from "../../nocrm_io.app.mjs";
+
+export default {
+  name: "Create Lead",
+  version: "0.0.1",
+  key: "nocrm_io-create-lead",
+  description: "Creates a new lead. [See docs here](https://www.nocrm.io/api#leads)",
+  type: "action",
+  props: {
+    nocrm_io,
+    title: {
+      label: "Title",
+      description: "Lead's title. Usually it corresponds to the company name.",
+      type: "string",
+    },
+    description: {
+      label: "Description",
+      description: "Lead's description. Usually it contains the information of the contact in the company.",
+      type: "string",
+    },
+    userId: {
+      propDefinition: [
+        nocrm_io,
+        "userId",
+      ],
+      description: "User's ID to assign the lead to the user.",
+      optional: true,
+    },
+    tags: {
+      label: "Tags",
+      description: "An array of tags describing the lead.",
+      type: "string[]",
+      optional: true,
+    },
+    stepId: {
+      propDefinition: [
+        nocrm_io,
+        "stepId",
+      ],
+      description: "Step's id for the lead.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const parsedTags = typeof this.tags === "string"
+      ? JSON.parse(this.tags)
+      : this.tags;
+
+    const response = await this.nocrm_io.createLead({
+      $,
+      data: {
+        title: this.title,
+        description: this.description,
+        user_id: this.userId,
+        tags: parsedTags,
+        step: this.stepId,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully create lead with id ${response.id}`);
+    }
+
+    return response;
+  },
+};
---
+++ components/nocrm_io/actions/get-lead/get-lead.mjs
@@ -0,0 +1,30 @@
+import nocrm_io from "../../nocrm_io.app.mjs";
+
+export default {
+  name: "Get Lead",
+  version: "0.0.1",
+  key: "nocrm_io-get-lead",
+  description: "Get a lead. [See docs here](https://www.nocrm.io/api#leads)",
+  type: "action",
+  props: {
+    nocrm_io,
+    leadId: {
+      propDefinition: [
+        nocrm_io,
+        "leadId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.nocrm_io.getLead({
+      $,
+      leadId: this.leadId,
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully retrieved lead with id ${response.id}`);
+    }
+
+    return response;
+  },
+};
