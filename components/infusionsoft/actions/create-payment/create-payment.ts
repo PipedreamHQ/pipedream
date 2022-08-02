5895ed2bd021ee07a5455bf1655684c9e88b5fc1
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2022-08-02 12:56:02 -0300

    #983 - Infusionsoft Actions & Sources (#3870)
    * [App:Infusionsoft] Creating TS structure
    
    * [App:Infusionsoft] Creating 'get company'
    And company listing
    
    * [App:Infusionsoft] 'Get Company' action
    
    * [App:Infusionsoft] Creating 'Get Contact'
    
    * [App:Infusionsoft] 'Create Payment' action
    
    * [App:Infusionsoft] 'Create order item' + types
    
    * [App:Infusionsoft] Type adjustments
    
    * [App:Infusionsoft] Organizing type files
    
    * [App:Infusionsoft] Create Payment params
    
    * [App:Infusionsoft] Fixing types in async options listing
    
    * [App:Infusionsoft] Fixing http request
    
    * [App:Infusionsoft] Webhook initial implementation
    TS type correction pending
    
    * [App:Infusionsoft] Functional webhook system
    
    * [App:Infusionsoft] Creating generic hookObject type
    
    * pnpm-lock.yaml
    
    * Updating types package to 0.1.4
    
    * [App:Infusionsoft] Common webhook source
    
    * [App:Infusionsoft] Source structure additions
    
    * [App:Infusionsoft] New appointment and desc changes
    
    * [App:Infusionsoft] Order summary
    
    * [App:Infusionsoft] Simplifying types
    
    * [App:Infusionsoft] Webhook data and summaries
    
    * [App:Infusionsoft] Summary changes and doc links
    
    * [App:Infusionsoft] Simplifying types
    
    * pnpm-lock.yaml
    
    * [App:Infusionsoft] Simplifying types
    
    * [App:Infusionsoft] Removing status from http response
    
    * [App:Infusionsoft] ESLint + PD Axios
    
    * [App:Infusionsoft] Naming convention fixes
    
    * [App:Infusionsoft] Pull request suggested changes
    
    * [App:Infusionsoft] Using PD Axios + $ for all requests
    
    * [App:Infusionsoft] Request fix and quality improvements
    
    * ESLint
    
    * 'Create Payment' making some props mandatory
    
    * ESLint
    
    * tsconfig.json newline
    
    * Removing 'new appointment' source

---
+++ components/infusionsoft/actions/create-payment/create-payment.ts
@@ -0,0 +1,96 @@
+import infusionsoft from "../../app/infusionsoft.app";
+import { defineAction } from "@pipedream/types";
+import { CreatePaymentParams } from "../../types/requestParams";
+
+export default defineAction({
+  name: "Create Payment",
+  description:
+    "Create or add a payment record [See docs here](https://developer.infusionsoft.com/docs/rest/#operation/createPaymentOnOrderUsingPOST)",
+  key: "infusionsoft-create-payment",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    infusionsoft,
+    orderId: {
+      propDefinition: [
+        infusionsoft,
+        "orderId",
+      ],
+    },
+    paymentAmount: {
+      type: "string",
+      label: "Payment Amount",
+    },
+    paymentMethodType: {
+      type: "string",
+      label: "Payment Method",
+      options: [
+        {
+          label: "Credit Card",
+          value: "CREDIT_CARD",
+        },
+        {
+          label: "Cash",
+          value: "CASH",
+        },
+        {
+          label: "Check",
+          value: "CHECK",
+        },
+      ],
+    },
+    applyToCommissions: {
+      type: "boolean",
+      label: "Apply to Commissions",
+      optional: true,
+    },
+    chargeNow: {
+      type: "boolean",
+      label: "Charge Now",
+      optional: true,
+    },
+    creditCardId: {
+      type: "integer",
+      label: "Credit Card ID",
+      optional: true,
+    },
+    date: {
+      type: "string",
+      label: "Date",
+      description:
+        "Used when `Charge Now` is **false** or if inserting historical data. Must be a date-time string such as `2017-01-01T22:17:59.039Z`",
+      optional: true,
+    },
+    notes: {
+      type: "string",
+      label: "Notes",
+      optional: true,
+    },
+    paymentGatewayId: {
+      type: "string",
+      label: "Payment Gateway ID",
+      optional: true,
+    },
+  },
+  async run({ $ }): Promise<object> {
+    const params: CreatePaymentParams = {
+      $,
+      orderId: this.orderId,
+      data: {
+        apply_to_commissions: this.applyToCommissions,
+        charge_now: this.chargeNow,
+        credit_card_id: this.creditCardId,
+        date: this.date,
+        notes: this.notes,
+        payment_amount: this.paymentAmount,
+        payment_gateway_id: this.paymentGatewayId,
+        payment_method_type: this.paymentMethodType,
+      },
+    };
+    const data: object = await this.infusionsoft.createPayment(params);
+
+    $.export("$summary", "Created Payment successfully");
+
+    return data;
+  },
+});
