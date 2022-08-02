90957b6a41d3306a9c47647c647b89d4c766ddf3
michelle0927 <michellelbergero@hotmail.com>
2022-08-02 15:08:28 -0400

    typos

--- components/twitter/actions/simple-search-in-list/simple-search-in-list.mjs
+++ components/twitter/actions/simple-search-in-list/simple-search-in-list.mjs
@@ -2,9 +2,9 @@ import twitter from "../../twitter.app.mjs";
 
 export default {
   key: "twitter-simple-search-in-list",
-  name: "Simple Search In List",
-  description: "Return Tweets that matches your search criteria in a specific list. [See the docs here](https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/get-lists-statuses)",
-  version: "0.0.1",
+  name: "Simple Search in List",
+  description: "Return Tweets that match your search criteria in a specific list. [See the docs here](https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/get-lists-statuses)",
+  version: "0.0.2",
   type: "action",
   props: {
     twitter,
@@ -29,10 +29,7 @@ export default {
     includeEntities: {
       type: "boolean",
       label: "Entities",
-      description: `
-        Include the 'entities' node, which offers a variety of metadata about the
-        tweet in a discreet structure, including: user_mentions, urls, and hashtags
-      `,
+      description: "Include the 'entities' node, which offers a variety of metadata about the tweet in a discreet structure, including: user_mentions, urls, and hashtags",
       default: false,
     },
   },
@@ -43,7 +40,9 @@ export default {
       includeRetweets: this.includeRetweets !== "exclude",
     });
     tweets = tweets.filter((tweet) => tweet?.full_text?.includes(this.query));
-    $.export("$summary", `${tweets.length} search results has been found.`);
+    $.export("$summary", `${tweets.length} search ${tweets.length === 1
+      ? "result has"
+      : "results have"} been found.`);
     return tweets;
   },
 };
