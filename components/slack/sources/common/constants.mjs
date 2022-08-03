d0db2af4b4b4c18907231ed490fad422ba731c17
vellames-turing <91911778+vellames-turing@users.noreply.github.com>
2022-08-03 11:49:28 -0300

    [SOURCE] Slack - New Star Added (#3526)
    * new star added
    
    * fix bug

--- components/slack/sources/common/constants.mjs
+++ components/slack/sources/common/constants.mjs
@@ -5,6 +5,26 @@ const events = {
   channel: "Channel",
 };
 
+const eventsOptions = [
+  {
+    label: "User",
+    value: "im",
+  },
+  {
+    label: "Message",
+    value: "message",
+  },
+  {
+    label: "File",
+    value: "file",
+  },
+  {
+    label: "Channel",
+    value: "channel",
+  },
+];
+
 export {
   events,
+  eventsOptions,
 };
