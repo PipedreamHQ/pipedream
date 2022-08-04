aecb0fc95faa1a82fc58d5139882aa79d06c94b2
Cassiano Vellames <cassiano.v@turing.com>
2022-08-04 14:55:30 -0300

    new deployment

--- components/new_relic/new_relic.app.mjs
+++ components/new_relic/new_relic.app.mjs
@@ -1,11 +1,59 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "new_relic",
-  propDefinitions: {},
+  propDefinitions: {
+    application: {
+      type: "integer",
+      label: "Application ID",
+      description: "The ID of the application to create the deployment for",
+      async options({ page }) {
+        const apps = await this.listApps(page + 1);
+        return apps.map((app) => ({
+          label: app.name,
+          value: app.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://api.newrelic.com/v2";
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Api-Key": this.$auth.api_key,
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      const res = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return res;
+    },
+    async listApps(page) {
+      const res = await axios(this, this._getAxiosParams({
+        method: "GET",
+        path: "/applications.json",
+        params: {
+          page,
+        },
+      }));
+      return res.applications;
+    },
+    async newDeployment(appId, data) {
+      const res = await axios(this, this._getAxiosParams({
+        method: "POST",
+        path: `/applications/${appId}/deployments.json`,
+        data: {
+          deployment: data,
+        },
+      }));
+      return res.deployment;
     },
   },
 };
