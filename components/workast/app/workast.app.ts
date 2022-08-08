99121b0ba4a344f67066160a9d61bf51c35e809f
Lucas Caresia <lucascarezia@gmail.com>
2022-08-08 16:48:09 -0300

    Added sources and actions

--- components/workast/app/workast.app.ts
+++ components/workast/app/workast.app.ts
@@ -1,13 +1,90 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
 
 export default defineApp({
   type: "app",
   app: "workast",
-  propDefinitions: {},
+  propDefinitions: {
+    userId: {
+      label: "User ID",
+      description: "The user ID",
+      type: "string",
+      async options() {
+        const users = await this.getUsers();
+
+        return users.map((user) => ({
+          label: user.name,
+          value: user.id,
+        }));
+      },
+    },
+    listId: {
+      label: "List/Space ID",
+      description: "The list/space ID",
+      type: "string",
+      async options() {
+        const lists = await this.getLists();
+
+        return lists.map((list) => ({
+          label: list.name,
+          value: list.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiToken() {
+      return this.$auth.api_token;
+    },
+    _apiUrl() {
+      return "https://api.todobot.io";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this._apiToken()}`,
+        },
+        ...args,
+      });
+    },
+    async getUsers(args = {}) {
+      return this._makeRequest({
+        path: "/user",
+        ...args,
+      });
+    },
+    async getLists(args = {}) {
+      return this._makeRequest({
+        path: "/list",
+        ...args,
+      });
+    },
+    async getTasks({
+      listId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/list/${listId}/task`,
+        ...args,
+      });
+    },
+    async createTask({
+      listId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/list/${listId}/task`,
+        method: "post",
+        ...args,
+      });
+    },
+    async createSpace({ ...args }) {
+      return this._makeRequest({
+        path: "/list",
+        method: "post",
+        ...args,
+      });
     },
   },
 });
