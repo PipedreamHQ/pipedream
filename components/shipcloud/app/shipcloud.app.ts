85f63e1527e9736f35a6e4b56bca8b34b1a83405
guilherme.falcao <gtfalcao96@gmail.com>
2022-08-08 16:52:35 -0300

    Creating constants

--- components/shipcloud/app/shipcloud.app.ts
+++ components/shipcloud/app/shipcloud.app.ts
@@ -2,6 +2,7 @@ import { defineApp } from "@pipedream/types";
 import { axios } from "@pipedream/platform";
 import { GetShipmentParams, HttpRequestParams } from "../common/requestParams";
 import { Address, Shipment } from "../common/responseSchemas";
+import { CARRIER_OPTIONS, SERVICE_OPTIONS } from "../common/constants";
 
 export default defineApp({
   type: "app",
@@ -71,7 +72,9 @@ export default defineApp({
       });
     },
     getShipmentLabel({ packages, price, to }: Shipment) {
-      return `${packages.length} packages ($${price}) to ${this.getAddressLabel(to)}`;
+      return `${packages.length} packages ($${price}) to ${this.getAddressLabel(
+        to
+      )}`;
     },
     async listAddresses(): Promise<Address[]> {
       const response = await this._httpRequest({
@@ -131,51 +134,19 @@ export default defineApp({
       type: "string",
       label: "Carrier",
       description: "The carrier you want to use",
-      options: [
-        "angel_de",
-        "cargo_international",
-        "dhl",
-        "dhl_express",
-        "dpag",
-        "dpd",
-        "gls",
-        "go",
-        "hermes",
-        "iloxx",
-        "parcel_one",
-        "ups",
-      ],
-      service: {
-        type: "string",
-        label: "Service",
-        description: "The service that should be used for the shipment",
-        options: [
-          "standard",
-          "one_day",
-          "one_day_early",
-          "returns",
-          "asendia_epaq_standard_economy",
-          "asendia_epaq_standard_priority",
-          "cargo_international_express",
-          "dhl_europaket",
-          "dhl_prio",
-          "dhl_warenpost",
-          "dpag_warenpost",
-          "dpag_warenpost_signature",
-          "dpag_warenpost_untracked",
-          "gls_express_0800",
-          "gls_express_0900",
-          "gls_express_1000",
-          "gls_express_1200",
-          "ups_express_1200",
-        ],
-      },
-      package: {
-        type: "object",
-        label: "Package",
-        description:
-          "Object describing the package [(more info on the Shipcloud Docs)](https://developers.shipcloud.io/reference/#creating-a-shipment)",
-      },
+      options: CARRIER_OPTIONS,
+    },
+    service: {
+      type: "string",
+      label: "Service",
+      description: "The service that should be used for the shipment",
+      options: SERVICE_OPTIONS,
+    },
+    package: {
+      type: "object",
+      label: "Package",
+      description:
+        "Object describing the package [(more info on the Shipcloud Docs)](https://developers.shipcloud.io/reference/#creating-a-shipment)",
     },
   },
 });
---
+++ components/shipcloud/common/constants.ts
@@ -0,0 +1,37 @@
+const CARRIER_OPTIONS: string[] = [
+  "angel_de",
+  "cargo_international",
+  "dhl",
+  "dhl_express",
+  "dpag",
+  "dpd",
+  "gls",
+  "go",
+  "hermes",
+  "iloxx",
+  "parcel_one",
+  "ups",
+];
+
+const SERVICE_OPTIONS: string[] = [
+  "standard",
+  "one_day",
+  "one_day_early",
+  "returns",
+  "asendia_epaq_standard_economy",
+  "asendia_epaq_standard_priority",
+  "cargo_international_express",
+  "dhl_europaket",
+  "dhl_prio",
+  "dhl_warenpost",
+  "dpag_warenpost",
+  "dpag_warenpost_signature",
+  "dpag_warenpost_untracked",
+  "gls_express_0800",
+  "gls_express_0900",
+  "gls_express_1000",
+  "gls_express_1200",
+  "ups_express_1200",
+]
+
+export { CARRIER_OPTIONS, SERVICE_OPTIONS}
