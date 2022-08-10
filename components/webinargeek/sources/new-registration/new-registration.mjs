eeeaacbe1859265a1e4ebc937ec9791c3a2c3e70
Lucas Caresia <lucascarezia@gmail.com>
2022-08-10 15:03:42 -0300

    Added sources

---
+++ components/webinargeek/sources/new-registration/new-registration.mjs
@@ -0,0 +1,75 @@
+import webinargeek from "../../webinargeek.app.mjs";
+
+export default {
+  name: "New Registration",
+  version: "0.0.1",
+  key: "webinargeek-new-registration",
+  description: "Emit new event on each new registration.",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    webinargeek,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      static: {
+        intervalSeconds: 15 * 60, // 15 minutes
+      },
+    },
+  },
+  methods: {
+    emitEvent(body) {
+      const data = body?.entity ?? body;
+
+      this._setLastSubscriptionId(data.id);
+
+      this.$emit(data, {
+        id: data.id,
+        summary: `New registration with id ${data.id}`,
+        ts: Date.parse(data.created_at),
+      });
+    },
+    _setLastSubscriptionId(id) {
+      this.db.set("lastSubscriptionId", id);
+    },
+    _getLastSubscriptionId() {
+      this.db.get("lastSubscriptionId");
+    },
+  },
+  hooks: {
+    async deploy() {
+      const subscriptions = await this.webinargeek.getSubscriptions({
+        params: {
+          per_page: 10,
+        },
+      });
+
+      subscriptions.reverse().forEach(this.emitEvent);
+    },
+  },
+  async run() {
+    const lastSubscriptionId = this._getLastSubscriptionId();
+
+    let page = 1;
+
+    while (page >= 0) {
+      const subscriptions = await this.webinargeek.getSubscriptions({
+        params: {
+          page,
+          per_page: 100,
+        },
+      });
+
+      subscriptions.reverse().forEach(this.emitEvent);
+
+      if (
+        subscriptions.length < 100 ||
+        subscriptions.filter((subscription) => subscription.id === lastSubscriptionId)
+      ) {
+        return;
+      }
+
+      page++;
+    }
+  },
+};
---
+++ components/webinargeek/sources/new-replay-watched/new-replay-watched.mjs
@@ -0,0 +1,64 @@
+import webinargeek from "../../webinargeek.app.mjs";
+
+export default {
+  name: "New Replay Watched",
+  version: "0.0.1",
+  key: "webinargeek-new-replay-watched",
+  description: "Emit new event on each replay is watched.",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    webinargeek,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      static: {
+        intervalSeconds: 15 * 60, // 15 minutes
+      },
+    },
+  },
+  methods: {
+    emitEvent(body) {
+      const data = body?.entity ?? body;
+
+      this.$emit(data, {
+        id: data.id,
+        summary: `New replay watched for a subscription with id ${data.id}`,
+        ts: Date.parse(data.watch_start_replay),
+      });
+    },
+  },
+  hooks: {
+    async deploy() {
+      const subscriptions = await this.webinargeek.getSubscriptions({
+        params: {
+          per_page: 10,
+          watched_replay: true,
+        },
+      });
+
+      subscriptions.reverse().forEach(this.emitEvent);
+    },
+  },
+  async run() {
+    let page = 1;
+
+    while (page >= 0) {
+      const subscriptions = await this.webinargeek.getSubscriptions({
+        params: {
+          page,
+          per_page: 100,
+          watched_replay: true,
+        },
+      });
+
+      subscriptions.reverse().forEach(this.emitEvent);
+
+      if (subscriptions.length < 100) {
+        return;
+      }
+
+      page++;
+    }
+  },
+};
