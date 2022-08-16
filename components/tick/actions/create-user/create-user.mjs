eb28a6c93c98abd57c7a994f3ee63a1dcdded05c
Lucas Caresia <lucascarezia@gmail.com>
2022-08-16 11:35:21 -0300

    Done actions and sourcs

---
+++ components/tick/actions/create-user/create-user.mjs
@@ -0,0 +1,55 @@
+import tick from "../../tick.app.mjs";
+
+export default {
+  name: "Create User",
+  version: "0.0.1",
+  key: "tick-create-user",
+  description: "Creates a user. [See docs here](https://github.com/tick/tick-api/blob/master/sections/users.md#create-user)",
+  type: "action",
+  props: {
+    tick,
+    firstName: {
+      label: "First Name",
+      description: "The first name of the user",
+      type: "string",
+    },
+    lastName: {
+      label: "Last Name",
+      description: "The last name of the user",
+      type: "string",
+    },
+    email: {
+      label: "Email",
+      description: "The email of the user",
+      type: "string",
+    },
+    admin: {
+      label: "Is Admin",
+      description: "The user will be an admin",
+      type: "boolean",
+    },
+    billableRate: {
+      label: "Billable Rate",
+      description: "The billable rate of the user. E.g. `150.0`",
+      type: "string",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.tick.createUser({
+      $,
+      data: {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        admin: this.admin,
+        billable_rate: this.billableRate,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully created user with id ${response.id}`);
+    }
+
+    return response;
+  },
+};
