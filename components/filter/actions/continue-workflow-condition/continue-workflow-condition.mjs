c25c9035b829060a960402c59b5b32781076ddba
Hung Vu <vunguyenhung@outlook.com>
2022-08-17 16:32:54 +0700

    Update filter - continue workflow action without bump up

--- components/filter/actions/continue-workflow-condition/continue-workflow-condition.mjs
+++ components/filter/actions/continue-workflow-condition/continue-workflow-condition.mjs
@@ -16,7 +16,7 @@ export default {
         filter,
         "reason",
       ],
-      description: "The reason for ending the workflow...",
+      description: "The reason for ending the workflow without bump up",
       default: reasons.END,
       optional: true,
     },
