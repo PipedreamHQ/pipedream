4f383fa757ebb0d15e389264b7283980e3bfe5d1
Alyson Freitas <alys.freitas@gmail.com>
2022-08-17 21:21:01 -0300

    google slides create or copy presentation action

--- components/google_slides/google_slides.app.mjs
+++ components/google_slides/google_slides.app.mjs
@@ -1,11 +1,81 @@
+import slides from "@googleapis/slides";
+import googleDrive from "../google_drive/google_drive.app.mjs";
+
 export default {
+  ...googleDrive,
   type: "app",
   app: "google_slides",
-  propDefinitions: {},
+  propDefinitions: {
+    ...googleDrive.propDefinitions,
+    presentationId: {
+      type: "string",
+      label: "Presentation",
+      description: "The Presentation ID",
+      options({
+        prevContext,
+        driveId,
+      }) {
+        const { nextPageToken } = prevContext;
+        return this.listPresentationsOptions(driveId, nextPageToken);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    ...googleDrive.methods,
+    slides() {
+      const auth = new slides.auth.OAuth2();
+      auth.setCredentials({
+        access_token: this.$auth.oauth_access_token,
+      });
+      return slides.slides({
+        version: "v1",
+        auth,
+      });
+    },
+    async createPresentation(title) {
+      const slides = this.slides();
+      const presentation = await slides.presentations.create({
+        title,
+      });
+      return presentation.data;
+    },
+    async listPresentationsOptions(driveId, pageToken = null) {
+      const q = "mimeType='application/vnd.google-apps.presentation'";
+      let request = {
+        q,
+      };
+      if (driveId) {
+        request = {
+          ...request,
+          corpora: "drive",
+          driveId,
+          pageToken,
+          includeItemsFromAllDrives: true,
+          supportsAllDrives: true,
+        };
+      }
+      return this.listFilesOptions(pageToken, request);
+    },
+    async getPresentation(presentationId) {
+      const slides = this.slides();
+      const request = {
+        presentationId,
+      };
+      return (await slides.presentations.get(request)).data;
+    },
+    async copyPresentation(fileId, name) {
+      const drive = this.drive();
+      const resource = {
+        name,
+      };
+      return (
+        await drive.files.copy({
+          fileId,
+          fields: "*",
+          supportsAllDrives: true,
+          resource,
+        })
+      ).data;
     },
   },
 };
