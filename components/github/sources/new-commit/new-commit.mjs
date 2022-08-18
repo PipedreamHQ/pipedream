cafcbd022a6a2940db1bf5597165bdb6e1003123
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-18 14:30:54 -0300

    bump patch version

--- components/github/sources/new-commit/new-commit.mjs
+++ components/github/sources/new-commit/new-commit.mjs
@@ -6,7 +6,7 @@ export default {
   key: "github-new-commit",
   name: "New Commit (Instant)",
   description: "Emit new events on new commits to a repo or branch",
-  version: "0.1.0",
+  version: "0.1.1",
   type: "source",
   dedupe: "unique",
   props: {
