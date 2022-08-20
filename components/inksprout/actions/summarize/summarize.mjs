8767c3bd571a68350dfe4d9d6669839e55de941e
Alyson Freitas <alys.freitas@gmail.com>
2022-08-20 10:35:59 -0300

    summarize action

---
+++ components/inksprout/actions/summarize/summarize.mjs
@@ -0,0 +1,38 @@
+import app from "../../inksprout.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+import lodash from "lodash";
+
+export default {
+  key: "inksprout-summarize",
+  name: "Summarize",
+  description: "Summarize by either url or raw text. [See the docs](https://inksprout.co/docs/index.html).",
+  version: "0.0.3",
+  type: "action",
+  props: {
+    app,
+    url: {
+      type: "string",
+      label: "URL",
+      description: "Website link to summarize. Optional if text is passed.",
+      optional: true,
+    },
+    text: {
+      type: "string",
+      label: "Text",
+      description: "Text to summarize. Optional if url is passed.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const param = lodash.pick(this, [
+      "url",
+      "text",
+    ]);
+    if (!param.url && !param.text) {
+      throw new ConfigurationError("Either url or text is required.");
+    }
+    const result = await this.app.summarize($, param);
+    $.export("$summary", `Summarized text with status: ${result.status}`);
+    return result;
+  },
+};
