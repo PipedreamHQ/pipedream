4d5031f8697bdb91d6cd2248a8316b26418ccac2
guilherme.falcao <gtfalcao96@gmail.com>
2022-08-22 00:44:44 -0300

    Tookan request params

---
+++ components/tookan/actions/create-appointment-task/create-appointment-task.ts
@@ -0,0 +1,28 @@
+import tookan from "../../app/tookan.app";
+import { defineAction } from "@pipedream/types";
+import { CreateAppointmentTaskParams } from "../../common/requestParams";
+
+export default defineAction({
+  name: "Create Appointment Task",
+  description:
+    "Create an appointment task [See docs here](https://tookanapi.docs.apiary.io/#reference/task/create-task/create-an-appointment-task)",
+  key: "tookan-create-appointment-task",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    tookan,
+  },
+  async run({ $ }): Promise<any> {
+    const params: CreateAppointmentTaskParams = {
+      $,
+      // data: {
+        
+      // },
+    };
+    const data = await this.tookan.createAppointmentTask(params);
+
+    $.export("$summary", "Created appointment task successfully");
+
+    return data;
+  },
+});
--- components/tookan/common/requestParams.ts
+++ components/tookan/common/requestParams.ts
@@ -10,6 +10,75 @@ interface HttpRequestParams extends ActionRequestParams {
   method?: string;
 }
 
+interface CreateAppointmentTaskParams extends ActionRequestParams {
+  data?: {
+    additionalOptions: object;
+    customer_address: string;
+    job_delivery_datetime: string;
+    job_pickup_datetime: string;
+    timezone: string;
+    has_pickup: 0;
+    has_delivery: 0;
+    layout_type: 1;
+  };
+}
+
+interface CreateDeliveryTask extends ActionRequestParams {
+  data?: {
+    additionalOptions: object;
+    customer_address: string;
+    job_delivery_datetime: string;
+    timezone: string;
+    has_pickup: 0;
+    has_delivery: 1;
+    layout_type: 0;
+  };
+}
+
+interface CreateFieldWorkforceTask extends ActionRequestParams {
+  data?: {
+    additionalOptions: object;
+    customer_address: string;
+    job_delivery_datetime: string;
+    job_pickup_datetime: string;
+    timezone: string;
+    has_pickup: 0;
+    has_delivery: 0;
+    layout_type: 2;
+  };
+}
+
+interface CreatePickupTask extends ActionRequestParams {
+  data?: {
+    additionalOptions: object;
+    job_pickup_address: string;
+    job_pickup_datetime: string;
+    timezone: string;
+    has_pickup: 1;
+    has_delivery: 0;
+    layout_type: 0;
+  };
+}
+
+interface CreatePickupAndDeliveryTask extends ActionRequestParams {
+  data?: {
+    additionalOptions: object;
+    customer_address: string;
+    job_delivery_datetime: string;
+    job_pickup_address: string;
+    job_pickup_datetime: string;
+    timezone: string;
+    has_pickup: 1;
+    has_delivery: 1;
+    layout_type: 0;
+  };
+}
+
 export {
   HttpRequestParams,
+  CreateAppointmentTaskParams,
+  CreateDeliveryTask,
+  CreateFieldWorkforceTask,
+  CreatePickupTask,
+  CreatePickupAndDeliveryTask,
 };
