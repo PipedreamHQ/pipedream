df2f009c13e76d90e3da6be21d8940cb782b3ed4
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-22 15:26:46 -0300

    add actions

---
+++ components/you_need_a_budget/actions/create-transaction/create-transaction.mjs
@@ -0,0 +1,97 @@
+import app from "../../you_need_a_budget.app.mjs";
+
+export default {
+  key: "you_need_a_budget-create-transaction",
+  name: "Create Transaction",
+  description: "Creates a single transaction. [See the docs](https://api.youneedabudget.com/v1#/Transactions/createTransaction)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    budgetId: {
+      propDefinition: [
+        app,
+        "budgetId",
+      ],
+    },
+    accountId: {
+      propDefinition: [
+        app,
+        "accountId",
+        (c) => ({
+          budgetId: c.budgetId,
+        }),
+      ],
+    },
+    categoryId: {
+      propDefinition: [
+        app,
+        "categoryId",
+        (c) => ({
+          budgetId: c.budgetId,
+        }),
+      ],
+    },
+    date: {
+      propDefinition: [
+        app,
+        "date",
+      ],
+    },
+    payee: {
+      propDefinition: [
+        app,
+        "payee",
+        (c) => ({
+          budgetId: c.budgetId,
+        }),
+      ],
+    },
+    amount: {
+      propDefinition: [
+        app,
+        "amount",
+      ],
+    },
+    memo: {
+      type: "string",
+      label: "Memo",
+      description: "A short description",
+      optional: true,
+    },
+    cleared: {
+      type: "boolean",
+      label: "Cleared",
+      description: "Marking a transaction as cleared means it has posted to your bank account",
+      optional: true,
+    },
+    approved: {
+      type: "boolean",
+      label: "Approved",
+      description: "Approve it directly?",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    try {
+      const response = await this.app.createTransaction({
+        accountId: this.accountId,
+        date: this.date,
+        payee: this.payee,
+        budgetId: this.budgetId,
+        categoryId: this.categoryId.value,
+        amount: this.amount,
+        memo: this.memo,
+        cleared: this.cleared,
+        approved: this.approved,
+      });
+      $.export("$summary", `New transaction created in ${this.categoryId.label}`);
+      return response;
+    } catch (error) {
+      if (error.error) {
+        this.app.throwFormattedError(error.error);
+      }
+      this.app.throwFormattedError(error);
+    }
+  },
+};
--- components/you_need_a_budget/you_need_a_budget.app.mjs
+++ components/you_need_a_budget/you_need_a_budget.app.mjs
@@ -1,11 +1,176 @@
+import ynab from "ynab";
+
 export default {
   type: "app",
   app: "you_need_a_budget",
-  propDefinitions: {},
+  propDefinitions: {
+    // eslint-disable-next-line pipedream/props-description
+    budgetId: {
+      type: "string",
+      label: "Budget ID",
+      async options() {
+        const { budgets } = await this.getBudgets();
+        return budgets.map((budget) => ({
+          label: budget.name,
+          value: budget.id,
+        }));
+      },
+    },
+    accountId: {
+      type: "string",
+      label: "Account ID",
+      description: "Account",
+      async options({ budgetId }) {
+        const { accounts } = await this.getAccounts({
+          budgetId,
+        });
+        return accounts.map((account) => ({
+          label: account.name,
+          value: account.id,
+        }));
+      },
+    },
+    categoryId: {
+      type: "string",
+      label: "Category ID",
+      description: "The category for a budget",
+      withLabel: true,
+      async options({ budgetId }) {
+        const { category_groups: groups } = await this.getCategories({
+          budgetId,
+        });
+        return groups.map((group) => group.categories.map((category) => ({
+          label: `${group.name}: ${category.name}`,
+          value: category.id,
+        }))).flat();
+      },
+    },
+    month: {
+      type: "string",
+      label: "Month",
+      description: "The month for a budget",
+      default: "current",
+      async options({ budgetId }) {
+        const { months } = await this.getMonths({
+          budgetId,
+        });
+        const options = months
+          .filter((month) => !month.deleted)
+          .map(({ month }) => month);
+        return [
+          "current",
+        ].concat(options);
+      },
+    },
+    payee: {
+      type: "string",
+      label: "Payee",
+      description: "Select an ID or type in a new name and a payee will be created",
+      async options({ budgetId }) {
+        const { payees } = await this.getPayees({
+          budgetId,
+        });
+        return payees.map((payee) => ({
+          label: payee.name,
+          value: payee.id,
+        }));
+      },
+    },
+    date: {
+      type: "string",
+      label: "Date",
+      description: "Date - e.g. `2021-11-29`",
+    },
+    amount: {
+      type: "string",
+      label: "Amount",
+      description: "Amount - e.g. `290.99`",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _isUUID(value) {
+      const hyphen = "-";
+      return value.length === 36
+        && value.charAt(8) === hyphen
+        && value.charAt(13) === hyphen
+        && value.charAt(18) === hyphen
+        && value.charAt(23) === hyphen;
+    },
+    _client() {
+      return new ynab.API(this.$auth.oauth_access_token);
+    },
+    throwFormattedError(error) {
+      const message = JSON.stringify(error, null, 2);
+      console.error(message);
+      throw new Error(message);
+    },
+    /**
+     * YNAB uses a special format called milliunit as described in link below:
+     * https://api.youneedabudget.com/#formats
+     */
+    _convertToMilliunit(value) {
+      if (value.includes(",")) {
+        value = value.replace(",", ".");
+      }
+      return parseFloat(value).toFixed(2) * 1000;
+    },
+    async getBudgets() {
+      const response = await this._client().budgets.getBudgets();
+      return response.data;
+    },
+    async getAccounts({ budgetId: budget_id }) {
+      const response = await this._client().accounts.getAccounts(budget_id);
+      return response.data;
+    },
+    async getMonths({ budgetId: budget_id }) {
+      const response = await this._client().months.getBudgetMonths(budget_id);
+      return response.data;
+    },
+    async getCategories({ budgetId: budget_id }) {
+      const response = await this._client().categories.getCategories(budget_id);
+      return response.data;
+    },
+    async getPayees({ budgetId: budget_id }) {
+      const response = await this._client().payees.getPayees(budget_id);
+      return response.data;
+    },
+    async createTransaction({
+      budgetId, accountId, categoryId, payee, cleared, amount, ...data
+    }) {
+      const transaction = {
+        account_id: accountId,
+        category_id: categoryId,
+        amount: this._convertToMilliunit(amount),
+        ...data,
+      };
+      if (cleared) transaction.cleared = "cleared";
+      if (this._isUUID(payee)) {
+        transaction.payee_id = payee;
+      } else {
+        transaction.payee_name = payee;
+      }
+      const response = await this._client().transactions.createTransaction(
+        budgetId,
+        {
+          transaction,
+        },
+      );
+      return response.data;
+    },
+    async updateCategoryBudget({
+      budgetId, categoryId, month, budget,
+    }) {
+      const response = await this._client().categories.updateMonthCategory(
+        budgetId,
+        month,
+        categoryId,
+        {
+          category: {
+            budgeted: this._convertToMilliunit(budget),
+          },
+        },
+      );
+      return response.data;
     },
   },
 };
