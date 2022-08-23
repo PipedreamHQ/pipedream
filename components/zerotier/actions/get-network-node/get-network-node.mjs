690b122ac5f20896bb24f7a8ed5fe0e8c1cdb31f
Zalman Lew <35234925+zalmanlew@users.noreply.github.com>
2022-08-23 11:50:18 -0400

    update zerotier api (#4062)

--- components/zerotier/actions/get-network-node/get-network-node.mjs
+++ components/zerotier/actions/get-network-node/get-network-node.mjs
@@ -3,7 +3,7 @@ import zerotier from "../../zerotier.app.mjs";
 export default {
   key: "zerotier-get-network-node",
   type: "action",
-  version: "0.0.3",
+  version: "0.0.4",
   name: "Get Network Node",
   description: "Get a specific node of a specific network. Returns an individual member (node) on a network. [See docs here](https://docs.zerotier.com/central/v1/#operation/getNetworkMember)",
   props: {
