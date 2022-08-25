3e263c49d3b5204d41317e6977c700a3d53a332f
Luan Cazarine <luanhc@gmail.com>
2022-08-25 17:59:46 -0300

    3864 lemlist (#4002)
    * Actions:
      - Mark Lead From All Campaigns As Interested
      - Mark Lead From One Campaigns As Interested
    
    * Action: Add Lead To A Campaign
    
    * Actions:
      - Mark Lead From All Campaigns As Not Interested
      - Mark Lead From One Campaigns As Not Interested
    
    * Action:
      - Pause Lead From All Campaigns
    
    * Action:
      - Remove Lead From Campaign
    
    * Actions:
      - Delete Lead From Campaign
      - Unsubscribe Lead From Campaign
    
    * Action:
      - Resume Lead From All Campaigns
    
    * Action:
      - Add Email In Unsubscribes
    
    * Actions:
      - Update Lead In Campaign
    
    * Sources:
      - New Activity (Instant)
      - New Unsubscribed Recipient (Instant)
    
    * Actions:
      - Add Email In Unsubscribes
      - Delete An Email From Unsubscribes
      - Get Lead
    
    * create campaignEmail prop
    
    * fixes actions that use campaignEmail
    
    * fix variable name
    
    * adjusts
    
    * git fix
    
    * fix lint
    
    * fix params

---
+++ components/lemlist/actions/delete-lead-from-a-campaign/delete-lead-from-a-campaign.mjs
@@ -0,0 +1,42 @@
+import lemlist from "../../lemlist.app.mjs";
+
+export default {
+  key: "lemlist-delete-lead-from-a-campaign",
+  name: "Delete Lead From Campaign",
+  description: "This action deletes a lead from a specific campaign. All information, including statistics, will be deleted. [See the docs here](https://developer.lemlist.com/#delete-a-lead-from-a-campaign)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    lemlist,
+    campaignId: {
+      propDefinition: [
+        lemlist,
+        "campaignId",
+      ],
+      withLabel: true,
+    },
+    campaignEmail: {
+      propDefinition: [
+        lemlist,
+        "campaignEmail",
+        (c) => ({
+          campaignId: c.campaignId.value,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.lemlist.removeLeadFromACampaign({
+      $,
+      email: this.campaignEmail,
+      campaignId: this.campaignId.value,
+      params: {
+        action: "remove",
+      },
+    });
+
+    $.export("$summary", `Successfully removed ${this.campaignEmail} lead from ${this.campaignId.label} campaign!`);
+    return response;
+  },
+};
+
