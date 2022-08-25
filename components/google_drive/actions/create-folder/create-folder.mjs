e023ad927b7341732294f5f64d5e702467a6e805
Byron Sommardahl <byron@sommardahl.com>
2022-08-25 09:21:13 -0500

    Adding option and logic to optionally skip folder creation, fixes #4087

--- components/google_drive/actions/create-folder/create-folder.mjs
+++ components/google_drive/actions/create-folder/create-folder.mjs
@@ -1,4 +1,7 @@
 import googleDrive from "../../google_drive.app.mjs";
+import { getListFilesOpts } from "../../utils.mjs";
+
+import { GOOGLE_DRIVE_FOLDER_MIME_TYPE } from "../../constants.mjs";
 
 export default {
   key: "google_drive-create-folder",
@@ -36,12 +39,33 @@ export default {
       description: "The name of the new folder",
       optional: true,
     },
+    createIfExists: {
+      type: "boolean",
+      label: "Create If Exists?",
+      description: toSingleLineString(`
+        If the folder already exists and is not in the trash, should we create it? This option defaults to 'true' for
+        backwards compatability and to be consistent with default Google Drive behavior. 
+      `),
+      optional: true,
+      default: true,
+    },
   },
   async run({ $ }) {
     const {
       parentId,
       name,
     } = this;
+    
+    if(!this.createIfExists){
+      const folders = (await this.googleDrive.listFilesInPage(null, getListFilesOpts(this.drive, {
+        q: `mimeType = '${GOOGLE_DRIVE_FOLDER_MIME_TYPE}' and name contains '${this.nameSearchTerm}' and trashed=false`.trim()
+      }))).files;
+      if(folders.length>0){
+        $.export("$summary", `Found existing folder, therefore not creating folder. Returning found folder.`);
+        return folders[0];
+      }
+    }
+    
     const driveId = this.googleDrive.getDriveId(this.drive);
     const resp = await this.googleDrive.createFolder({
       name,
