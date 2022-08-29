1766e04720a5d37124b1f83f1966e8b4d0c52430
michelle0927 <michelle0927@users.noreply.github.com>
2022-08-29 13:20:47 -0400

    Esputnik - new sources and actions (#4105)
    * new sources
    
    * create-contact action
    
    * new actions
    
    * send-event action
    
    * package.json
    
    * pnpm-lock.yaml
    
    * add doc links
    
    * bug fix

---
+++ components/esputnik/actions/subscribe-contact/subscribe-contact.mjs
@@ -0,0 +1,80 @@
+import esputnik from "../../esputnik.app.mjs";
+
+export default {
+  key: "esputnik-subscribe-contact",
+  name: "Subscribe Contact",
+  description: "Create a new unverified contact in eSputnik. For use with double opt-in implementation. User will need to verify the email to confirm their subscription. [See the docs here](https://esputnik.com/api/methods.html#/v1/contact/subscribe-POST)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    esputnik,
+    firstName: {
+      propDefinition: [
+        esputnik,
+        "firstName",
+      ],
+    },
+    lastName: {
+      propDefinition: [
+        esputnik,
+        "lastName",
+      ],
+    },
+    email: {
+      propDefinition: [
+        esputnik,
+        "email",
+      ],
+    },
+    address: {
+      propDefinition: [
+        esputnik,
+        "address",
+      ],
+    },
+    town: {
+      propDefinition: [
+        esputnik,
+        "town",
+      ],
+    },
+    region: {
+      propDefinition: [
+        esputnik,
+        "region",
+      ],
+    },
+    postcode: {
+      propDefinition: [
+        esputnik,
+        "postcode",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const contact = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      address: {
+        address: this.address,
+        town: this.town,
+        region: this.region,
+        postcode: this.postcode,
+      },
+      channels: {
+        type: "email",
+        value: this.email,
+      },
+    };
+    const resp = await this.esputnik.subscribeContact({
+      data: {
+        contact,
+      },
+    });
+    const summary = resp.emailStatus === "ALREADY_SUBSCRIBED"
+      ? `Contact with ID ${resp.id} already subscribed`
+      : `Unverified contact added with ID ${resp.id}`;
+    $.export("$summary", summary);
+    return resp;
+  },
+};
