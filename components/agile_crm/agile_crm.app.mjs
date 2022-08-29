a56ecbdb46420a62b6f9b63b34ef4a26dc01904a
michelle0927 <michellelbergero@hotmail.com>
2022-08-29 15:06:10 -0400

    new agile_crm sources

--- components/agile_crm/agile_crm.app.mjs
+++ components/agile_crm/agile_crm.app.mjs
@@ -1,11 +1,82 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "agile_crm",
-  propDefinitions: {},
+  propDefinitions: {
+    filter: {
+      type: "string",
+      label: "Filter",
+      description: "Type of ticket filter",
+      async options() {
+        const filters = await this.listFilters();
+        if (!filters || filters?.length === 0) {
+          return [];
+        }
+        return filters.map((filter) => ({
+          label: filter.name,
+          value: filter.id,
+        }));
+      },
+    },
+    maxRequests: {
+      type: "integer",
+      min: 1,
+      max: 100,
+      label: "Max API Requests per Execution",
+      description: "The maximum number of API requests to make per execution (e.g., multiple requests are required to retrieve paginated results).",
+      optional: true,
+      default: 1,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return `https://${this.$auth.domain}.agilecrm.com/dev/api/`;
+    },
+    _getAuth() {
+      return {
+        username: this.$auth.username,
+        password: this.$auth.api_key,
+      };
+    },
+    async _makeRequest(args = {}) {
+      const {
+        method = "GET",
+        path,
+        $ = this,
+        ...otherArgs
+      } = args;
+      const config = {
+        method,
+        url: `${this._getBaseUrl()}${path}`,
+        auth: this._getAuth(),
+        ...otherArgs,
+      };
+      return axios($, config);
+    },
+    async listContacts(args = {}) {
+      return this._makeRequest({
+        path: "contacts",
+        ...args,
+      });
+    },
+    async listTasks(args = {}) {
+      return this._makeRequest({
+        path: "tasks",
+        ...args,
+      });
+    },
+    async listTickets(args = {}) {
+      return this._makeRequest({
+        path: "tickets/filter",
+        ...args,
+      });
+    },
+    async listFilters(args = {}) {
+      return this._makeRequest({
+        path: "tickets/filters",
+        ...args,
+      });
     },
   },
 };
