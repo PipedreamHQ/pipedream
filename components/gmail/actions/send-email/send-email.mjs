9b719fc997e2f89bb05a13595cd476cb39a9a68f
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-29 14:10:31 -0300

    [Gmail] New Components (#4100)
    * change to js
    
    * add label to email action
    
    * add find email action
    
    * add constants
    
    * add polling new email trigger
    
    * add polling new attachment trigger
    
    * refactor polling sources
    
    * add send email action
    
    * Update components/gmail/gmail.app.mjs
    
    typo
    
    * change attachments prop to an object
    
    * change query prop description
    
    * fix messages with attachments filtering
    
    * add delegate prop
    
    * fix send bcc emails
    
    * Prop description tweaks
    
    * add default value to prop description
    
    * requested changes
    
    * Improve Find Email - Label prop description
    
    * Fix version
    
    Co-authored-by: feyzullah <feyzullah.metu@gmail.com>
    Co-authored-by: Danny Roosevelt <danny@pipedream.com>
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/gmail/actions/send-email/send-email.mjs
@@ -0,0 +1,115 @@
+/* eslint-disable pipedream/props-description */
+import gmail from "../../gmail.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "gmail-send-email",
+  name: "Send Email",
+  description: "Send an email from your Google Workspace email account",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gmail,
+    to: {
+      type: "string[]",
+      label: "To",
+    },
+    cc: {
+      type: "string[]",
+      label: "Cc",
+      optional: true,
+    },
+    bcc: {
+      type: "string[]",
+      label: "Bcc",
+      optional: true,
+    },
+    fromName: {
+      type: "string",
+      label: "From Name",
+      description: "A name that will be displayed in the `From` section of the email",
+      optional: true,
+    },
+    delegate: {
+      propDefinition: [
+        gmail,
+        "delegate",
+      ],
+    },
+    replyTo: {
+      type: "string",
+      label: "Reply To",
+      description: "An email address that will appear on the `Reply To` field",
+      optional: true,
+    },
+    subject: {
+      type: "string",
+      label: "Subject",
+      description: "The subject of the email",
+    },
+    body: {
+      type: "any",
+      label: "Email Body",
+      description: "The email body plain text or html",
+    },
+    bodyType: {
+      type: "string",
+      label: "Body Type",
+      description: "Plain Text or HTML. Defaults to `plaintext`.",
+      optional: true,
+      default: "plaintext",
+      options: Object.values(constants.BODY_TYPES),
+    },
+    signature: {
+      propDefinition: [
+        gmail,
+        "signature",
+      ],
+    },
+    attachments: {
+      type: "object",
+      label: "Attachments",
+      description: "Add any attachments you'd like to include as objects. The `key` should be the **filename** and the `value` should be the **url** for the attachment, respectively. The **filename** must contain the file extension (i.e. `.jpeg`, `.txt`) and the **url** is the download link for the file.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    let from = await this.gmail.myEmailAddress();
+    if (this.fromName) from = `${this.fromName} <${from}>`;
+
+    const opts = {
+      from,
+      to: this.to,
+      cc: this.cc,
+      bcc: this.bcc,
+      replyTo: this.replyTo,
+      subject: this.subject,
+    };
+
+    if (this.attachments) {
+      opts.attachments = Object.entries(this.attachments)
+        .map(([
+          filename,
+          path,
+        ]) => ({
+          filename,
+          path,
+        }));
+    }
+
+    if (this.signature) {
+      this.body += this.signature;
+      this.bodyType = constants.BODY_TYPES.HTML;
+    }
+
+    if (this.bodyType === constants.BODY_TYPES.HTML) {
+      opts.html = this.body;
+    } else {
+      opts.text = this.body;
+    }
+
+    const response = await this.gmail.sendEmail(opts);
+    $.export("$summary", `Successfully sent email to ${this.to}`);
+    return response;
+  },
+};
