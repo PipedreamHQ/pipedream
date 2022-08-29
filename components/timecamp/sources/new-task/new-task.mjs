c7f74b8471e78b6e9af753adba89edf59e3167cf
Lucas Caresia <lucascarezia@gmail.com>
2022-08-29 17:55:02 -0300

    TimeCamp #3812 (#4084)
    * Added actions and sourcs
    
    * pnpm
    
    * Update components/timecamp/actions/create-task/create-task.mjs
    
    0.0.4 => 0.0.1
    
    * Fixed create-task action
    
    * Fixed create-time-entry action
    
    * Fixed new-time-entry source
    
    * Update create-task.mjs
    
    Co-authored-by: feyzullah <feyzullah.metu@gmail.com>

---
+++ components/timecamp/sources/new-task/new-task.mjs
@@ -0,0 +1,42 @@
+import timecamp from "../../timecamp.app.mjs";
+
+export default {
+  name: "New Task",
+  version: "0.0.1",
+  key: "timecamp-new-task",
+  description: "Emit new event on each created task.",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    timecamp,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      static: {
+        intervalSeconds: 15 * 60, // 15 minutes
+      },
+    },
+  },
+  methods: {
+    emitEvent(data) {
+      this.$emit(data, {
+        id: data.task_id,
+        summary: `New task with id ${data.task_id}`,
+        ts: Date.parse(data.add_date),
+      });
+    },
+    async emitAllTasks() {
+      const tasks = await this.timecamp.getTasks({});
+
+      tasks.reverse().forEach(this.emitEvent);
+    },
+  },
+  hooks: {
+    async deploy() {
+      await this.emitAllTasks();
+    },
+  },
+  async run() {
+    await this.emitAllTasks();
+  },
+};
