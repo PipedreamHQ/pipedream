94ede97bb4f8973f3ac4250f09d3b2409d44a375
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-29 14:46:27 -0300

    add default false value

--- components/gmail/actions/find-email/find-email.mjs
+++ components/gmail/actions/find-email/find-email.mjs
@@ -4,7 +4,7 @@ export default {
   key: "gmail-find-email",
   name: "Find Email",
   description: "Find an email using Google's Search Engine. [See the docs](https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     gmail,
@@ -29,6 +29,7 @@ export default {
       label: "Include Spam and Trash?",
       description: "Include messages from `SPAM` and `TRASH` in the results. Defaults to `false`.",
       optional: true,
+      default: false,
     },
   },
   async run({ $ }) {
