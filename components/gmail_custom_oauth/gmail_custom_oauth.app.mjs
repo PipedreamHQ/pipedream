97c7439049ea706a96380baf99737afed228833b
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-30 14:36:34 -0300

    add overriden components

---
+++ components/gmail_custom_oauth/gmail_custom_oauth.app.mjs
@@ -0,0 +1,20 @@
+import base from "../gmail/gmail.app.mjs";
+import gmail from "@googleapis/gmail";
+
+export default {
+  ...base,
+  type: "app",
+  app: "gmail_custom_oauth",
+  methods: {
+    _client() {
+      const auth = new gmail.auth.OAuth2();
+      auth.setCredentials({
+        access_token: this.$auth.oauth_access_token,
+      });
+      return gmail.gmail({
+        version: "v1",
+        auth,
+      });
+    },
+  },
+};
