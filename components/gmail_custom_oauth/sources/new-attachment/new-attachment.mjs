a52657ca38f18f4167fd2f652f2a9297357373be
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-30 15:59:07 -0300

    [Gmail] Custom OAuth Components (#4125)
    * update packages
    
    * add overriden components
    
    * Update gmail_custom_oauth.app.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>

---
+++ components/gmail_custom_oauth/sources/new-attachment/new-attachment.mjs
@@ -0,0 +1,13 @@
+/* eslint-disable pipedream/required-properties-name */
+/* eslint-disable pipedream/required-properties-description */
+/* eslint-disable pipedream/required-properties-version */
+/* eslint-disable pipedream/required-properties-type */
+import base from "../../../gmail/sources/new-attachment/new-attachment.mjs";
+import overrideApp from "../../common/override-app.mjs";
+
+overrideApp(base);
+
+export default {
+  ...base,
+  key: "gmail_custom_oauth-new-attachment",
+};
---
+++ components/gmail_custom_oauth/sources/new-email/new-email.mjs
@@ -0,0 +1,13 @@
+/* eslint-disable pipedream/required-properties-name */
+/* eslint-disable pipedream/required-properties-description */
+/* eslint-disable pipedream/required-properties-version */
+/* eslint-disable pipedream/required-properties-type */
+import base from "../../../gmail/sources/new-email/new-email.mjs";
+import overrideApp from "../../common/override-app.mjs";
+
+overrideApp(base);
+
+export default {
+  ...base,
+  key: "gmail_custom_oauth-new-email",
+};
