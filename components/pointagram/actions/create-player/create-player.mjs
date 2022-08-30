e78d954d37443488faf9be5a7a237c77b476c831
Cassiano Vellames <cassiano.v@turing.com>
2022-08-30 10:26:26 -0300

    create player

---
+++ components/pointagram/actions/create-player/create-player.mjs
@@ -0,0 +1,46 @@
+import app from "../../pointagram.app.mjs";
+
+export default {
+  type: "action",
+  key: "pointagram_create-player",
+  name: "Create Player",
+  description: "Create a new player",
+  version: "0.0.1",
+  props: {
+    app,
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    externalId: {
+      propDefinition: [
+        app,
+        "externalId",
+      ],
+    },
+    offline: {
+      propDefinition: [
+        app,
+        "offline",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const res = await this.app.createPlayer({
+      player_name: this.name,
+      player_email: this.email,
+      player_external_id: this.externalId,
+      offline: this.offline,
+    }, $);
+    $.export("$summary", "Player successfully created");
+    return res;
+  },
+};
--- components/pointagram/pointagram.app.mjs
+++ components/pointagram/pointagram.app.mjs
@@ -1,11 +1,59 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "pointagram",
-  propDefinitions: {},
+  propDefinitions: {
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the player in Pointagram",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email address of the player. Used for sending invitation",
+    },
+    externalId: {
+      type: "string",
+      label: "External ID",
+      description: "Optional id you can provide as an unique identifier for the player.",
+      optional: true,
+    },
+    offline: {
+      type: "boolean",
+      label: "Offline",
+      description: "Set `true` for offline or `false` for online player. An online player will receive an invitation to log on to Pointagram. Note, you can create players as offline players and convert them later in Pointagram.",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getAxiosParams(opts) {
+      const params =  {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      console.log(params);
+      return params;
+    },
+    _getBaseUrl() {
+      return "https://app.pointagram.com/server";
+    },
+    _getHeaders() {
+      return {
+        "content-type": "application/json",
+        "api_key": this.$auth.api_key,
+        "api_user": this.$auth.api_user,
+      };
+    },
+    async createPlayer(data, $ = this) {
+      const opts = {
+        path: "/externalapi.php/create_player",
+        method: "POST",
+        data,
+      };
+      return axios($, this._getAxiosParams(opts));
     },
   },
 };
