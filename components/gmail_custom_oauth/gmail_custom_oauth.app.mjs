a5dfe2be384c24360807f18a3d0ffae53907c4f5
Lucas Caresia <lucascarezia@gmail.com>
2022-08-30 15:00:45 -0300

    Update gmail_custom_oauth.app.mjs

--- components/gmail_custom_oauth/gmail_custom_oauth.app.mjs
+++ components/gmail_custom_oauth/gmail_custom_oauth.app.mjs
@@ -6,10 +6,13 @@ export default {
   type: "app",
   app: "gmail_custom_oauth",
   methods: {
+    _accessToken(){
+      return this.$auth.oauth_access_token;
+    },
     _client() {
       const auth = new gmail.auth.OAuth2();
       auth.setCredentials({
-        access_token: this.$auth.oauth_access_token,
+        access_token: this._accessToken(),
       });
       return gmail.gmail({
         version: "v1",
