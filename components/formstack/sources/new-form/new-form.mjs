d01ead7d3793f5aefdbc6344ed785ee1a1a102b6
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-31 17:57:59 -0300

    bump versions

--- components/formstack/sources/new-form/new-form.mjs
+++ components/formstack/sources/new-form/new-form.mjs
@@ -4,7 +4,7 @@ export default {
   key: "formstack-new-form",
   name: "New Form",
   description: "Emit new event for each new form added. [See docs here](https://formstack.readme.io/docs/form-id-webhook-post)",
-  version: "0.0.2",
+  version: "0.0.3",
   dedupe: "unique",
   type: "source",
   props: {
