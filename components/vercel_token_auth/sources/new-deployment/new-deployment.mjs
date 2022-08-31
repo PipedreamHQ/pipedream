68920cff64e1c152c7de9da480103c05bd48a17a
Andrew Chuang <andrewjschuang@gmail.com>
2022-08-31 10:38:45 -0300

    [Vercel Token Auth] Components (#4118)
    * add vercel app base
    
    * add vercel token auth components
    
    * bump versions
    
    * add versions

---
+++ components/vercel_token_auth/sources/new-deployment/new-deployment.mjs
@@ -0,0 +1,14 @@
+/* eslint-disable pipedream/required-properties-name */
+/* eslint-disable pipedream/required-properties-description */
+/* eslint-disable pipedream/required-properties-version */
+/* eslint-disable pipedream/required-properties-type */
+import base from "../../../vercel/sources/new-deployment/new-deployment.mjs";
+import overrideApp from "../../common/override-app.mjs";
+
+overrideApp(base);
+
+export default {
+  ...base,
+  key: "vercel_token_auth-new-deployment",
+  version: "0.0.2",
+};
