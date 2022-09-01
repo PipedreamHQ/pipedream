8a69037a8aead30a009cf29cbef96546803e8625
vellames <91911778+vellames@users.noreply.github.com>
2022-09-01 16:47:51 -0300

    [Actions] Pointagram (#4131)
    * signature
    
    * new folder, new list, updated task
    
    * fix bug
    
    * fix new folder empty summary bug
    
    * actions versions
    
    * create player
    
    * pnpm
    
    * add points to player
    
    * list players
    
    * fix keys name
    
    * CR

--- components/pointagram/pointagram.app.mjs
+++ components/pointagram/pointagram.app.mjs
@@ -1,11 +1,111 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "pointagram",
-  propDefinitions: {},
+  propDefinitions: {
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the player in Pointagram",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email address of the player. Used for sending invitation",
+    },
+    externalId: {
+      type: "string",
+      label: "External ID",
+      description: "Optional id you can provide as an unique identifier for the player.",
+      optional: true,
+    },
+    offline: {
+      type: "boolean",
+      label: "Offline",
+      description: "Set `true` for offline or `false` for online player. An online player will receive an invitation to log on to Pointagram. Note, you can create players as offline players and convert them later in Pointagram.",
+      optional: true,
+    },
+    playerId: {
+      type: "string",
+      label: "Player ID",
+      description: "The ID of the player in Pointagram",
+      async options() {
+        const players = await this.listPlayers();
+        return players.map((player) => ({
+          label: player.Name,
+          value: player.id,
+        }));
+      },
+    },
+    scoreSeriesId: {
+      type: "string",
+      label: "Score Series ID",
+      description: "The ID of the score series in Pointagram",
+      async options() {
+        const scoreSeries = await this.listScoreSeries();
+        return scoreSeries.map((scoreSeries) => ({
+          label: scoreSeries.name,
+          value: scoreSeries.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getAxiosParams(opts) {
+      const params =  {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return params;
+    },
+    _getBaseUrl() {
+      return "https://app.pointagram.com/server";
+    },
+    _getApiKey() {
+      return this.$auth.api_key;
+    },
+    _getApiUser() {
+      return this.$auth.api_user;
+    },
+    _getHeaders() {
+      return {
+        "content-type": "application/json",
+        "api_key": this._getApiKey(),
+        "api_user": this._getApiUser(),
+      };
+    },
+    async createPlayer(data, $ = this) {
+      const opts = {
+        path: "/externalapi.php/create_player",
+        method: "POST",
+        data,
+      };
+      return axios($, this._getAxiosParams(opts));
+    },
+    async listPlayers(params = {}, $ = this) {
+      const opts = {
+        path: "/externalapi.php/list_players",
+        method: "GET",
+        params,
+      };
+      return axios($, this._getAxiosParams(opts));
+    },
+    async addPointsToPlayer(data, $ = this) {
+      const opts = {
+        path: "/externalapi.php/add_score",
+        method: "POST",
+        data,
+      };
+      return axios($, this._getAxiosParams(opts));
+    },
+    async listScoreSeries($ = this) {
+      const opts = {
+        path: "/externalapi.php/list_score_series",
+        method: "GET",
+      };
+      return axios($, this._getAxiosParams(opts));
     },
   },
 };
