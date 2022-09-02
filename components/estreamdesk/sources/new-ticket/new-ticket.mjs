9c1148072ce9bd7cb73807bf865703eda2aa336d
Luan Cazarine <luanhc@gmail.com>
2022-09-02 17:44:09 -0300

    fix source version

--- components/estreamdesk/sources/new-ticket/new-ticket.mjs
+++ components/estreamdesk/sources/new-ticket/new-ticket.mjs
@@ -5,7 +5,7 @@ export default {
   key: "estreamdesk-new-ticket",
   name: "New Ticket",
   description: "Emit new event when a ticket is created",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   props: {
