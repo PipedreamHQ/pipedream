1cd22d6e42798d05eb5dbd6077b374779178bd0d
michelle0927 <michellelbergero@hotmail.com>
2022-09-07 16:24:05 -0400

    new sources

--- components/zoho_inventory/zoho_inventory.app.mjs
+++ components/zoho_inventory/zoho_inventory.app.mjs
@@ -1,11 +1,89 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "zoho_inventory",
-  propDefinitions: {},
+  propDefinitions: {
+    organization: {
+      type: "string",
+      label: "Organization",
+      description: "Select the organization",
+      async options({ page }) {
+        const params = {
+          per_page: constants.DEFAULT_PAGE_SIZE,
+          page: page + 1,
+        };
+        const { organizations } = await this.listOrganizations({
+          params,
+        });
+        return organizations.map((org) => ({
+          label: org.name,
+          value: org.organization_id,
+        }));
+      },
+    },
+    maxResults: {
+      type: "integer",
+      label: "Maximum Results",
+      description: "The maximum number of results to return at one time.",
+      optional: true,
+      default: 25,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://inventory.zoho.com/api/v1/";
+    },
+    _getHeaders() {
+      return {
+        Authorization: `Zoho-oauthtoken ${this.$auth.oauth_access_token}`,
+      };
+    },
+    async _makeRequest(args = {}) {
+      const {
+        method = "GET",
+        path,
+        $ = this,
+        ...otherArgs
+      } = args;
+      const config = {
+        method,
+        url: `${this._getBaseUrl()}${path}`,
+        headers: this._getHeaders(),
+        ...otherArgs,
+      };
+      return axios($, config);
+    },
+    async listOrganizations(args = {}) {
+      return this._makeRequest({
+        path: "organizations",
+        ...args,
+      });
+    },
+    async listContacts(args = {}) {
+      return this._makeRequest({
+        path: "contacts",
+        ...args,
+      });
+    },
+    async listItems(args = {}) {
+      return this._makeRequest({
+        path: "items",
+        ...args,
+      });
+    },
+    async listInvoices(args = {}) {
+      return this._makeRequest({
+        path: "invoices",
+        ...args,
+      });
+    },
+    async listSalesOrders(args = {}) {
+      return this._makeRequest({
+        path: "salesorders",
+        ...args,
+      });
     },
   },
 };
