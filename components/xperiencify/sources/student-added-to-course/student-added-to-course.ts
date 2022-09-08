77e5bacbdc3024f13dadc34b31ae9fb4c8341424
Andrew Chuang <andrewjschuang@gmail.com>
2022-09-08 14:51:23 -0300

    [Xperiencify] New Components (#4159)
    * update packages
    
    * add student to course action
    
    * student added to course trigger
    
    * remove student actions
    
    * student tags actions
    
    * final touches
    
    * requested changes
    
    * fix options undefined
    
    * bump package.json

---
+++ components/xperiencify/sources/student-added-to-course/student-added-to-course.ts
@@ -0,0 +1,47 @@
+import { defineSource } from "@pipedream/types";
+import xperiencify from "../../app/xperiencify.app";
+
+export default defineSource({
+  name: "Student Added to Course",
+  description: "Emit new event when a student enrolls into a course.",
+  key: "xperiencify-student-added-to-course",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    xperiencify,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: 60 * 15, // 15 minutes
+      },
+    },
+    courseId: {
+      propDefinition: [
+        xperiencify,
+        "courseId",
+      ],
+    },
+  },
+  methods: {
+    getMeta({
+      email, firstName,
+    }) {
+      return {
+        id: email,
+        summary: `New student ${firstName}: ${email}`,
+        ts: new Date().getTime(),
+      };
+    },
+  },
+  async run() {
+    const all = await this.xperiencify.getStudentsForCourse({
+      courseId: this.courseId,
+    });
+
+    for (const student of all) {
+      this.$emit(student, this.getMeta(student));
+    }
+  },
+});
