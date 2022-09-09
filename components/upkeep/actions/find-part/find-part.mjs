4ef77a37dd13c7df751ae06a7e78fa0658067141
feyzo <feyzullah.metu@gmail.com>
2022-09-09 23:14:51 +0300

    increased versions of components cannot be published

--- components/upkeep/actions/find-part/find-part.mjs
+++ components/upkeep/actions/find-part/find-part.mjs
@@ -4,7 +4,7 @@ import utils from "../../common/utils.mjs";
 export default {
   type: "action",
   key: "upkeep-find-part",
-  version: "0.0.1",
+  version: "0.0.2",
   name: "Find Part",
   description: "Finds parts according to props configured, if no prop configured returns all parts, [See the docs](https://developers.onupkeep.com/#get-all-parts)",
   props: {
--- components/upkeep/sources/work-order-status-change/work-order-status-change.mjs
+++ components/upkeep/sources/work-order-status-change/work-order-status-change.mjs
@@ -4,7 +4,7 @@ export default {
   key: "upkeep-work-order-status-change",
   name: "New Work Order Status Change Event",
   description: "Emit new event when a work order status is changed.",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   ...common,
