b4fb3706d1b6725ef4373d4ab381128cdfae7989
js07 <19861096+js07@users.noreply.github.com>
2022-09-14 19:30:34 -0400

    Increment modified components package versions
    Addresses failed "Check git diff for version changes" PR check

--- components/harvest/actions/stop-timer/stop-timer.mjs
+++ components/harvest/actions/stop-timer/stop-timer.mjs
@@ -4,7 +4,7 @@ export default {
   key: "harvest-stop-timer",
   name: "Stop Time Entry",
   description: "Stop a timer entry. [See docs here](https://help.getharvest.com/api-v2/timesheets-api/timesheets/time-entries/#stop-a-running-time-entry)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     harvest,
