e6305fae918c279cecb0e1e8f3d044976b048fd2
feyzo <feyzullah.metu@gmail.com>
2022-09-14 23:30:22 +0300

    alpaca actions

--- components/alpaca/alpaca.app.mjs
+++ components/alpaca/alpaca.app.mjs
@@ -1,11 +1,145 @@
+import { axios } from "@pipedream/platform";
+//Did not use Alpaca JS SDK because it does not work with OAuth
 export default {
   type: "app",
   app: "alpaca",
-  propDefinitions: {},
+  propDefinitions: {
+    isPaperAPI: {
+      type: "boolean",
+      label: "Paper API",
+      description: "Set to `true` if Paper API is used. Default is `false`.",
+      optional: true,
+    },
+    cancelOrders: {
+      type: "boolean",
+      label: "Cancel Orders",
+      description: "If `true` is specified, cancel all open orders before liquidating all positions.",
+      optional: true,
+    },
+    side: {
+      type: "string",
+      label: "Side",
+      description: "`buy` or `sell`",
+      options: [
+        "buy",
+        "sell",
+      ],
+    },
+    orderId: {
+      type: "string",
+      label: "Order Id",
+      description: "Order Id",
+      async options({ isPaperAPI }) {
+        const orders = await this.getOrders({
+          isPaperAPI,
+          params: {
+            limit: 500, //max allowed limit, no pagination
+          },
+        });
+        return orders.map((order) => ({
+          label: `${order.symbol} ${order.type} ${order.side} - ${order.qty ?? order.notional}`,
+          value: order.id,
+        }));
+      },
+    },
+    positionSymbol: {
+      type: "string",
+      label: "Position Id",
+      description: "Position Id",
+      async options({ isPaperAPI }) {
+        const positions = await this.getPositions({
+          isPaperAPI,
+        });
+        return positions.map((position) => ({
+          label: `${position.symbol} ${position.side} - ${position.qty}`,
+          value: position.symbol,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getUrl(path, isPaperAPI) {
+      // eslint-disable-next-line multiline-ternary
+      return `https://${isPaperAPI ? "paper-" : ""}api.alpaca.markets/v2${path}`;
+    },
+    _accessToken() {
+      return this.$auth.oauth_access_token;
+    },
+    _getHeaders(headers = {}) {
+      return {
+        "Authorization": `Bearer ${this._accessToken()}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "User-Agent": "@PipedreamHQ/pipedream v0.1",
+        ...headers,
+      };
+    },
+    async _makeRequest({
+      $ = this, isPaperAPI = false, path, headers, ...otherConfig
+    } = {}) {
+      const config = {
+        url: this._getUrl(path, isPaperAPI),
+        headers: this._getHeaders(headers),
+        ...otherConfig,
+      };
+      return axios($, config);
+    },
+    async getOrders({ ...args } = {}) {
+      return this._makeRequest({
+        path: "/orders",
+        ...args,
+      });
+    },
+    async getPositions({ ...args } = {}) {
+      return this._makeRequest({
+        path: "/positions",
+        ...args,
+      });
+    },
+    async cancelOrder({
+      orderId, ...args
+    } = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/orders/${orderId}`,
+        ...args,
+      });
+    },
+    async cancelAllOrders({ ...args } = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: "/orders",
+        ...args,
+      });
+    },
+    async closePosition({
+      symbol, ...args
+    } = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/positions/${symbol}`,
+        ...args,
+      });
+    },
+    async closeAllPositions({ ...args } = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: "/positions",
+        ...args,
+      });
+    },
+    async getAccountInfo({ ...args } = {}) {
+      return this._makeRequest({
+        path: "/account",
+        ...args,
+      });
+    },
+    async placeOrder({ ...args } = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/orders",
+        ...args,
+      });
     },
   },
 };
