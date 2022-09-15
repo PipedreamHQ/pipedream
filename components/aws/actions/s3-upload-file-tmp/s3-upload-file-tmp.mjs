bd90f52a01c8ee250cf4e02f8d147eb03877d2ce
Andrew Chuang <andrewjschuang@gmail.com>
2022-09-15 11:55:20 -0300

    [AWS] add upload file action variants (#4216)
    * small refactor of helper function
    
    * update pnpm-lock.yaml
    
    * add s3 upload file variants
    
    * [TEMP] @pipedream/helper_functions": "^0.3.5 version
    
    * update helper functions version

--- components/aws/actions/s3-upload-file-tmp/s3-upload-file-tmp.mjs
+++ components/aws/actions/s3-upload-file-tmp/s3-upload-file-tmp.mjs
@@ -1,32 +1,36 @@
 import common from "../../common/common-s3.mjs";
+import fs from "fs";
 import { toSingleLineString } from "../../common/utils.mjs";
 
 export default {
   ...common,
-  key: "aws-s3-upload-file",
-  name: "S3 - Upload File - Base64",
+  key: "aws-s3-upload-file-tmp",
+  name: "S3 - Upload File - /tmp",
   description: toSingleLineString(`
-    Accepts a base64-encoded string and a filename, then uploads as a file to S3.
+    Accepts a file path starting from /tmp, then uploads as a file to S3.
     [See the docs](https://docs.aws.amazon.com/AmazonS3/latest/userguide/upload-objects.html)
   `),
-  version: "0.2.1",
+  version: "0.0.1",
   type: "action",
   props: {
     aws: common.props.aws,
     region: common.props.region,
     bucket: common.props.bucket,
     filename: common.props.key,
-    data: {
+    path: {
       type: "string",
-      label: "Base64-encoded Data",
-      description: "A string of base64-encoded data, or a variable reference to that string",
+      label: "File Path",
+      description: "A path starting from `/tmp`, i.e. `/tmp/some_text_file.txt`",
     },
   },
   async run({ $ }) {
+    const file = fs.readFileSync(this.path, {
+      encoding: "base64",
+    });
     const response = await this.uploadFile({
       Bucket: this.bucket,
       Key: this.filename,
-      Body: Buffer.from(this.data, "base64"),
+      Body: Buffer.from(file, "base64"),
     });
     $.export("$summary", `Uploaded file ${this.filename} to S3`);
     return response;
