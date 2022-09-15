98104bc4a8ae117bdd889030c511b565620b9d5d
Danny Roosevelt <danny@pipedream.com>
2022-09-15 14:59:33 -0700

    Bumping gmail action versions

--- components/gmail_custom_oauth/actions/find-email/find-email.mjs
+++ components/gmail_custom_oauth/actions/find-email/find-email.mjs
@@ -4,7 +4,7 @@ export default {
   key: "gmail_custom_oauth-find-email",
   name: "Find Email",
   description: "Find an email using Google's Search Engine. [See the docs](https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list)",
-  version: "0.0.1",
+  version: "0.0.4",
   type: "action",
   props: {
     gmail,
