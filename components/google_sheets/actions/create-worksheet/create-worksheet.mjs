18b77772f221b751819f6b4db4e3388a36b225e3
js07 <19861096+js07@users.noreply.github.com>
2022-09-15 17:30:21 -0400

    Test duplicate component key PR check

--- components/google_sheets/actions/create-worksheet/create-worksheet.mjs
+++ components/google_sheets/actions/create-worksheet/create-worksheet.mjs
@@ -1,7 +1,7 @@
 import googleSheets from "../../google_sheets.app.mjs";
 
 export default {
-  key: "google_sheets-create-worksheet",
+  key: "google_sheets-new-worksheet",
   name: "Create Worksheet",
   description: "Create a blank worksheet with a title",
   version: "0.0.4",
