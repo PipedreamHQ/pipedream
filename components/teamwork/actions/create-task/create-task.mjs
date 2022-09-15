c7cda85601b9736412dc6c9dd013eb80a0e99215
Cassiano Vellames <cassiano.v@turing.com>
2022-09-15 10:20:59 -0300

    create task

--- components/teamwork/actions/create-task/create-task.mjs
+++ components/teamwork/actions/create-task/create-task.mjs
@@ -3,9 +3,9 @@ import app from "../../teamwork.app.mjs";
 export default {
   type: "action",
   key: "teamwork-create-task",
-  version: "0.0.6",
+  version: "0.0.13",
   name: "Create Task",
-  description: "Create a new task",
+  description: "Create a new task in the backlog. [See the docs here](https://apidocs.teamwork.com/docs/teamwork/cd8948166b1b1-create-a-task)",
   props: {
     app,
     tasklistId: {
@@ -14,8 +14,80 @@ export default {
         "tasklistId",
       ],
     },
+    content: {
+      type: "string",
+      label: "Content",
+      description: "The content of the task",
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "The description of the task",
+      optional: true,
+    },
+    priority: {
+      type: "string",
+      label: "Priority",
+      description: "The priority of the task",
+      optional: true,
+      options: [
+        "Low",
+        "Medium",
+        "High",
+      ],
+    },
+    creatorId: {
+      propDefinition: [
+        app,
+        "peopleId",
+      ],
+      label: "Creator Id",
+      description: "The id of the person who created the task",
+      optional: true,
+    },
+    responsiblePartyId: {
+      propDefinition: [
+        app,
+        "peopleId",
+      ],
+      label: "Responsible Party Id",
+      description: "The id of the person who is responsible for the task",
+      optional: true,
+    },
+    startDate: {
+      type: "string",
+      label: "Start Date",
+      description: "The date the task should start. `yyyymmdd`",
+      optional: true,
+    },
+    dueDate: {
+      type: "string",
+      label: "Due Date",
+      description: "The date the task is due `yyyymmdd`",
+      optional: true,
+    },
+    useDefaults: {
+      type: "boolean",
+      label: "Use Defaults",
+      description: "Use the default values for the task",
+      optional: true,
+    },
   },
-  async run () {
-    console.log(await this.app.listTasklists());
+  async run ({ $ }) {
+    const data = {
+      "content": this.content,
+      "description": this.description,
+      "creator-id": this.creatorId,
+      "responsible-party-id": this.responsiblePartyId,
+      "start-date": this.startDate,
+      "due-date": this.dueDate,
+      "use-defaults": this.useDefaults,
+    };
+    await this.app.createTask(
+      this.tasklistId,
+      data,
+      $,
+    );
+    $.export("$summary", "Task successfully created");
   },
 };
--- components/teamwork/teamwork.app.mjs
+++ components/teamwork/teamwork.app.mjs
@@ -16,6 +16,18 @@ export default {
         }));
       },
     },
+    peopleId: {
+      type: "string",
+      label: "People Id",
+      description: "Id of the people to list tasks from",
+      async options({ page }) {
+        const people = await this.listPeople(page + 1);
+        return people.map((item) => ({
+          label: `${item["first-name"]} ${item["last-name"]}`,
+          value: item.id,
+        }));
+      },
+    },
   },
   methods: {
     _getBaseUrl() {
@@ -33,21 +45,37 @@ export default {
         url: this._getBaseUrl() + opts.path,
         headers: this._getHeaders(),
       };
+      console.log(res);
       return res;
     },
     async listTasklists(page, ctx = this) {
       const res = await axios(ctx, this._getAxiosParams({
         method: "GET",
-        path: "/tasklists.json",
+        path: "tasklists.json",
         params: {
           page,
         },
       }));
       return res?.tasklists || [];
     },
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log((this.$auth));
+    async listPeople(page, ctx = this) {
+      const res = await axios(ctx, this._getAxiosParams({
+        method: "GET",
+        path: "people.json",
+        params: {
+          page,
+        },
+      }));
+      return res?.people || [];
+    },
+    async createTask(taskListId, data, ctx = this) {
+      return axios(ctx, this._getAxiosParams({
+        method: "POST",
+        path: `tasklists/${taskListId}/tasks.json`,
+        data: {
+          "todo-item": data,
+        },
+      }));
     },
   },
 };
