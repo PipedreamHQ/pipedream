285735b41db13923b780b14806f11815e05280ba
Cassiano Vellames <cassiano.v@turing.com>
2022-09-16 10:52:03 -0300

    fix version

--- components/teamwork/actions/update-task/update-task.mjs
+++ components/teamwork/actions/update-task/update-task.mjs
@@ -5,7 +5,7 @@ export default {
   key: "teamwork-update-task",
   name: "Update Task",
   description: "Update a task. [See the docs here](https://apidocs.teamwork.com/docs/teamwork/6e3da2c04d779-update-a-task)",
-  version: "0.0.4",
+  version: "0.0.1",
   props: {
     app,
     projectId: {
