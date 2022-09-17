48ce50f20335b262d3da2287c66c9a679a683e3a
Alyson Freitas <alys.freitas@gmail.com>
2022-09-17 16:46:17 -0300

    beehiiv, new actions: create subscriber, list all publications

---
+++ components/beehiiv/actions/list-all-publications/list-all-publications.ts
@@ -0,0 +1,18 @@
+import app from "../../app/beehiiv.app";
+import { defineAction } from "@pipedream/types";
+
+export default defineAction({
+  name: "List All Publications",
+  description: "Get a list of all your publications. [See docs](https://www.beehiiv.com/developers/docs)",
+  key: "beehiiv-list-all-publications",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  async run({ $ }) {
+    const { publications } = await this.app.listPublications($);
+    $.export("$summary", `Successfully listed ${publications.length} publications`);
+    return publications;
+  },
+});
--- components/beehiiv/app/beehiiv.app.ts
+++ components/beehiiv/app/beehiiv.app.ts
@@ -1,13 +1,100 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
 
 export default defineApp({
   type: "app",
   app: "beehiiv",
-  propDefinitions: {},
+  propDefinitions: {
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email address of the subscriber.",
+    },
+    publicationId: {
+      type: "string",
+      label: "Publication Id",
+      description: "The ID of the publication you want to subscribe to.",
+      async options() {
+        return this.getPublicationOpts();
+      },
+    },
+    reactivateExisting: {
+      type: "boolean",
+      label: "Reactivate Existing",
+      description: "Whether or not to reactivate the subscriber if they have already unsubscribed. **This option should be used only if the subscriber is knowingly resubscribing**. default: `false`.",
+      optional: true,
+    },
+    sendWelcomeEmail: {
+      type: "boolean",
+      label: "Send Welcome Email",
+      description: "Whether or not to send a welcome email to the subscriber. default: `false`.",
+      optional: true,
+    },
+    utmSource: {
+      type: "string",
+      label: "UTM Source",
+      description: "The source of the subscriber.",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getUrl(path) {
+      return `https://api.beehiiv.com/v1${path}`;
+    },
+    _getHeaders(headers = {}) {
+      return {
+        "X-ApiKey": this.$auth.api_key,
+        "Content-Type": "application/json",
+        ...headers,
+      };
+    },
+    _getRequestParams(opts: any) {
+      return {
+        ...opts,
+        url: this._getUrl(opts.path),
+        headers: this._getHeaders(),
+      };
+    },
+    filterEmptyValues(obj) {
+      if (!obj) {
+        return obj;
+      }
+      return Object.entries(obj)
+        .reduce((reduction,
+          [
+            key,
+            value,
+          ]) => {
+          if (value === undefined || value === null) {
+            return reduction;
+          }
+          return {
+            ...reduction,
+            [key]: value,
+          };
+        }, {});
+    },
+    async createSubscriber($ = this, param) {
+      const response = await axios($, this._getRequestParams({
+        method: "POST",
+        path: "/subscribers",
+        data: this.filterEmptyValues(param),
+      }));
+      return response;
+    },
+    async listPublications($ = this) {
+      const response = await axios($, this._getRequestParams({
+        method: "GET",
+        path: "/publications",
+      }));
+      return response;
+    },
+    async getPublicationOpts() {
+      const { publications } = await this.listPublications(this);
+      return publications.map((publication) => ({
+        label: publication.name,
+        value: publication.id,
+      }));
     },
   },
 });
