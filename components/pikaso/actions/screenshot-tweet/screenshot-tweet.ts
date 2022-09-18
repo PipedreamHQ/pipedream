f4b11558a30396140a46974a19ebb4aee4d4e98a
Alyson Freitas <alys.freitas@gmail.com>
2022-09-18 20:58:53 -0300

    pikaso, new action: screenshot-tweet

---
+++ components/pikaso/actions/screenshot-tweet/screenshot-tweet.ts
@@ -0,0 +1,50 @@
+import app from "../../app/pikaso.app";
+import { defineAction } from "@pipedream/types";
+
+export default defineAction({
+  name: "Screenshot Tweet",
+  description: "With the Pikaso API you can automate creating screenshots from tweets. [See docs](https://pikaso.me/api)",
+  key: "pikaso-screenshot-tweet",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    tweetId: {
+      propDefinition: [
+        app,
+        "tweetId",
+      ],
+    },
+    layout: {
+      propDefinition: [
+        app,
+        "layout",
+      ],
+    },
+    theme: {
+      propDefinition: [
+        app,
+        "theme",
+      ],
+    },
+    size: {
+      propDefinition: [
+        app,
+        "size",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const tweetId = this.tweetId?.split("/").pop();
+    const params = {
+      tweet_id: tweetId,
+      layout: this.layout,
+      theme: this.theme,
+      size: this.size,
+    };
+    const response = await this.app.screenshotTweet($, params);
+    $.export("$summary", `Successfully took a screenshot from the tweet with id: ${tweetId}`);
+    const encodedResponse = Buffer.from(response, "binary").toString("base64");
+    return `data:image/png;base64,${encodedResponse}`;
+  },
+});
--- components/pikaso/app/pikaso.app.ts
+++ components/pikaso/app/pikaso.app.ts
@@ -1,13 +1,87 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
+import constants from "../actions/common/constants";
 
 export default defineApp({
   type: "app",
   app: "pikaso",
-  propDefinitions: {},
+  propDefinitions: {
+    tweetId: {
+      type: "string",
+      label: "Tweet Id",
+      description: "The id of the tweet to be saved, eg: `1526568680661786625`.\n\nAccepts the full url of the tweet, eg: `https://twitter.com/pipedream/status/1526568680661786625`",
+    },
+    layout: {
+      type: "string",
+      label: "Layout",
+      description: "Layout of the generated image.",
+      options: constants.LAYOUT_OPTS,
+      optional: true,
+    },
+    theme: {
+      type: "string",
+      label: "Theme",
+      description: "Theme of the generated image.",
+      options: constants.THEME_OPTS,
+      optional: true,
+    },
+    size: {
+      type: "string",
+      label: "Size",
+      description: "Size of the generated image.",
+      options: constants.SIZE_OPTS,
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://pikaso.me/api/v1";
+    },
+    _getAuth() {
+      const {
+        token,
+        token_secret,
+      } = this.$auth;
+      const encodedToken = Buffer.from(`${token}:${token_secret}`).toString("base64");
+      return `Basic ${encodedToken}`;
+    },
+    _getHeaders() {
+      return {
+        "content-type": "application/json",
+        "Authorization": this._getAuth(),
+      };
+    },
+    _getRequestParams(opts: any) {
+      return {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+    },
+    filterEmptyValues(obj) {
+      return Object.entries(obj)
+        .reduce((reduction,
+          [
+            key,
+            value,
+          ]) => {
+          if (value === undefined || value === null) {
+            return reduction;
+          }
+          return {
+            ...reduction,
+            [key]: value,
+          };
+        }, {});
+    },
+    async screenshotTweet($ = this, params) {
+      const linkInfo = await axios($, this._getRequestParams({
+        method: "GET",
+        path: "/tweet",
+        params: this.filterEmptyValues(params),
+        responseType: "arraybuffer",
+      }));
+      return linkInfo;
     },
   },
 });
