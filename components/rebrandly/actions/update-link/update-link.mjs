1d38576e0b6469d4ef9a6bfb594e10ad01e408e4
Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>
2022-09-19 14:55:53 -0300

    Added actions and triggers

---
+++ components/rebrandly/actions/update-link/update-link.mjs
@@ -0,0 +1,50 @@
+import rebrandly from "../../rebrandly.app.mjs";
+
+export default {
+  name: "Update Link",
+  version: "0.0.1",
+  key: "rebrandly-update-link",
+  description: "Updates a link. [See docs here](https://developers.rebrandly.com/docs/update-a-link)",
+  type: "action",
+  props: {
+    rebrandly,
+    linkId: {
+      propDefinition: [
+        rebrandly,
+        "linkId",
+      ],
+    },
+    destination: {
+      label: "Destination",
+      description: "New destination URL you want to assign to a branded short link. E.g. `https://pipedream.com`",
+      type: "string",
+    },
+    title: {
+      label: "Title",
+      description: "New title you want to assign to a branded short link",
+      type: "string",
+    },
+    favourite: {
+      label: "Favourite",
+      description: "Whether a link should be marked as favourite (loved) or not",
+      type: "boolean",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.rebrandly.updateLink({
+      $,
+      linkId: this.linkId,
+      data: {
+        destination: this.destination,
+        title: this.title,
+        favourite: this.favourite,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully updated link with id ${response.id}`);
+    }
+
+    return response;
+  },
+};
--- components/rebrandly/rebrandly.app.mjs
+++ components/rebrandly/rebrandly.app.mjs
@@ -1,11 +1,101 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "rebrandly",
-  propDefinitions: {},
+  propDefinitions: {
+    domain: {
+      label: "Domain",
+      description: "The domain to be used",
+      type: "string",
+      async options({ prevContext }) {
+        const domains = await this.getDomains({
+          params: {
+            last: prevContext.lastId,
+            orderBy: "createdAt",
+            orderDir: "asc",
+          },
+        });
+
+        return {
+          options: domains.map((domain) => ({
+            label: domain.fullName,
+            value: domain.id,
+          })),
+          context: {
+            lastId: domains[domains.length].id,
+          },
+        };
+      },
+    },
+    linkId: {
+      label: "Link Id",
+      description: "The ID of the link",
+      type: "string",
+      async options({ prevContext }) {
+        const links = await this.getLinks({
+          params: {
+            last: prevContext.lastId,
+          },
+        });
+
+        return {
+          options: links.map((link) => ({
+            label: link.title ?? link.destination,
+            value: link.id,
+          })),
+          context: {
+            lastId: links[0].id,
+          },
+        };
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _accessToken() {
+      return this.$auth.oauth_access_token;
+    },
+    _apiUrl() {
+      return "https://api.rebrandly.com/v1";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this._accessToken()}`,
+        },
+        ...args,
+      });
+    },
+    async createLink(args = {}) {
+      return this._makeRequest({
+        path: "/links",
+        method: "post",
+        ...args,
+      });
+    },
+    async updateLink({
+      linkId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/links/${linkId}`,
+        method: "post",
+        ...args,
+      });
+    },
+    async getDomains(args = {}) {
+      return this._makeRequest({
+        path: "/domains",
+        ...args,
+      });
+    },
+    async getLinks(args = {}) {
+      return this._makeRequest({
+        path: "/links",
+        ...args,
+      });
     },
   },
 };
