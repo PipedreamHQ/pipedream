3afed0bd661f933ba44d6e50bf452ffcefe450be
Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>
2022-09-20 13:53:41 -0300

    Added actions

---
+++ components/rise/actions/common/constants.ts
@@ -0,0 +1,15 @@
+export default {
+    USER_ROLES: [{
+        label: "Learner",
+        value: "learner"
+    }, {
+        label: "Author",
+        value: "author"
+    }, {
+        label: "Reporter",
+        value: "reporter"
+    }, {
+        label: "Admin",
+        value: "admin"
+    }]
+}
--- components/rise/app/rise.app.ts
+++ components/rise/app/rise.app.ts
@@ -1,13 +1,59 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from '@pipedream/platform'
 
 export default defineApp({
   type: "app",
   app: "rise",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _apiUrl() {
+      return "https://api.rise.com";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this._apiKey()}`,
+        },
+        ...args,
+      });
+    },
+    async createWebhook({ ...args }) {
+      return this._makeRequest({
+        path: "/webhooks",
+        method: "post",
+        ...args,
+      });
+    },
+    async removeWebhook(webhookId) {
+      return this._makeRequest({
+        path: `/webhooks/${webhookId}`,
+        method: "delete",
+      });
+    },
+    async getInvitations(args = {}) {
+      return this._makeRequest({
+        path: "/invitations",
+        ...args,
+      });
+    },
+    async getUsers(args = {}) {
+      return this._makeRequest({
+        path: "/users",
+        ...args,
+      });
+    },
+    async createInvitation(args = {}) {
+      return this._makeRequest({
+        path: "/invitations",
+        method: "post",
+        ...args,
+      });
     },
   },
 });
