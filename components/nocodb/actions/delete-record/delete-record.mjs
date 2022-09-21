046873816c8cd0953e0ab351b2ce50a87f10cc21
Luan Cazarine <luanhc@gmail.com>
2022-09-21 09:33:13 -0300

    version fix

--- components/nocodb/actions/delete-record/delete-record.mjs
+++ components/nocodb/actions/delete-record/delete-record.mjs
@@ -5,7 +5,7 @@ export default {
   key: "nocodb-delete-record",
   name: "Delete Record",
   description: "This action deletes a row in a table. [See the docs here](https://all-apis.nocodb.com/#tag/DB-table-row/operation/db-table-row-delete)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     ...common.props,
