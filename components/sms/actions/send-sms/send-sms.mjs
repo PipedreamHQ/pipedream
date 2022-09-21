5f5e757aff8e2d429100e1b18a6f688d55752742
Pravin <psavkar@yahoo.com>
2022-09-20 19:07:22 -0700

    Update send-sms.mjs

--- components/sms/actions/send-sms/send-sms.mjs
+++ components/sms/actions/send-sms/send-sms.mjs
@@ -4,7 +4,7 @@ export default {
   key: "sms-send-sms",
   name: "Send SMS",
   description: "Send an SMS to the [verified phone number for your account](https://pipedream.com/settings). While this feature is in alpha, you may send up to 10 messages per day.",
-  version: "0.0.2",
+  version: "0.0.1",
   type: "action",
   props: {
     sms,
