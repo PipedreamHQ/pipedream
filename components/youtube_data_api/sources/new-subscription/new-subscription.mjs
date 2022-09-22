a516649575cf707cb994aee1410b9e27c2b43933
Luan Cazarine <luanhc@gmail.com>
2022-09-22 14:52:10 -0300

    adjusts

--- components/youtube_data_api/sources/new-subscription/new-subscription.mjs
+++ components/youtube_data_api/sources/new-subscription/new-subscription.mjs
@@ -17,39 +17,46 @@ export default {
     _setLastExecutionDate(lastExecutionDate) {
       this.db.set("lastExecutionDate", lastExecutionDate);
     },
-    async _emitLastSubscriptions() {
-      const lastExecutionDate = this._getLastExecutionDate();
-      let nextPageToken = null;
+    async _emitLastSubscriptions(maxResults = 50) {
+      let nextPageToken;
       do {
         const res = await this.youtubeDataApi.getSubscriptions({
           part: "id,snippet",
           mine: true,
-          maxResults: 50,
+          maxResults,
           pageToken: nextPageToken,
         });
+        const items = res.data.items.reverse();
+        let lastExecutionDate = this._getLastExecutionDate();
 
-        for (const item of res.data.items) {
-          if (new Date(item.snippet.publishedAt).getTime() < lastExecutionDate) {
-            return;
+        for (const item of items) {
+          const newLastTime = new Date(item.snippet.publishedAt).getTime();
+
+          if (lastExecutionDate > newLastTime) {
+            continue;
           }
+
           this.$emit(item, {
             id: item.id,
             summary: item.snippet.title,
             ts: new Date(item.snippet.publishedAt),
           });
-        }
 
+          if (!lastExecutionDate || (newLastTime > lastExecutionDate)) {
+            this._setLastExecutionDate(newLastTime);
+            lastExecutionDate = newLastTime;
+          }
+        }
         nextPageToken = res.data.nextPageToken;
       } while (nextPageToken);
     },
   },
   hooks: {
-    activate() {
-      this._setLastExecutionDate(new Date().getTime());
+    async activate() {
+      await this._emitLastSubscriptions(25);
     },
   },
   async run() {
     await this._emitLastSubscriptions();
-    this._setLastExecutionDate(new Date().getTime());
   },
 };
