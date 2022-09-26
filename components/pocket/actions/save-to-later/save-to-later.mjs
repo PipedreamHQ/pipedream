d7ba1476e998634a32a3db028ea9e6edf2540d4d
Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>
2022-09-26 10:23:42 -0300

    Added actions and sources

---
+++ components/pocket/actions/save-to-later/save-to-later.mjs
@@ -0,0 +1,38 @@
+import pocket from "../../pocket.app.mjs";
+
+export default {
+  name: "Save To Later",
+  version: "0.0.4",
+  key: "pocket-save-to-later",
+  description: "Save articles, videos, images and URLs to your Pocket list. [See docs here](https://getpocket.com/developer/docs/v3/add)",
+  type: "action",
+  props: {
+    pocket,
+    url: {
+      label: "URL",
+      description: "The URL of the item you want to save",
+      type: "string",
+    },
+    title: {
+      label: "Title",
+      description: "This can be included for cases where an item does not have a title, which is typical for image or PDF URLs",
+      type: "string",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.pocket.saveToLater({
+      $,
+      data: {
+        url: this.url,
+        title: this.title,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully saved item with id ${response.item.item_id}`);
+    }
+
+    return response;
+  },
+};
---
+++ components/pocket/sources/new-archived-item/new-archived-item.mjs
@@ -0,0 +1,26 @@
+import common from "../common/common.mjs";
+
+export default {
+  ...common,
+  name: "New Archived item",
+  version: "0.0.1",
+  key: "pocket-new-archived-item",
+  description: "Emit new event for each archived item.",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    additionalParams() {
+      return {
+        state: "archive",
+      };
+    },
+    emitEvent(data) {
+      this.$emit(data, {
+        id: data.item_id,
+        summary: `New item favorited with id ${data.item_id}`,
+        ts: Date.parse(data.time_favorited),
+      });
+    },
+  },
+};
