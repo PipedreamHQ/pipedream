ea306c6b6422658853b694279fd1f0865f78ea03
Luan Cazarine <luanhc@gmail.com>
2022-09-28 10:13:13 -0300

    fix version

--- components/hubspot/actions/enroll-contact-into-workflow/enroll-contact-into-workflow.mjs
+++ components/hubspot/actions/enroll-contact-into-workflow/enroll-contact-into-workflow.mjs
@@ -4,7 +4,7 @@ export default {
   key: "hubspot-enroll-contact-into-workflow",
   name: "Enroll Contact Into Workflow",
   description: "Add a contact to a workflow. Note: The Workflows API currently only supports contact-based workflows. [See the docs here](https://legacydocs.hubspot.com/docs/methods/workflows/add_contact)",
-  version: "0.0.3",
+  version: "0.0.2",
   type: "action",
   props: {
     hubspot,
