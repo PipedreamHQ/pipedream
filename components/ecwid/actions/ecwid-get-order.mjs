1ac74353b89d6f61447eaccd67e2602f813395b8
Trendoscope <sudh@trendoscope.au>
2022-09-28 22:52:59 +1000

    feat: Ecwid get order and update order status actions added

---
+++ components/ecwid/actions/ecwid-get-order.mjs
@@ -0,0 +1,24 @@
+import ecwid from "../../ecwid.app.mjs";
+
+export default {
+  name: "Ecwid Get Order",
+  version: "0.0.1",
+  key: "ecwid-get-order",
+  description: "",
+  props: {
+    orderId: {
+      label: "Order ID",
+      description: "Order ID for which fulfilment status need to be updated",
+      type: "string",
+      optional: false,
+    },
+    ecwid,
+  },
+  type: "action",
+  methods: {},
+  async run({ $ }) {
+    let order = await this.ecwid.getOrder(this.orderId);
+    $.export("$summary", `Retrieved Order \`${this.orderId}\``);
+    return order;
+  },
+};
---
+++ components/ecwid/actions/ecwid-update-order-status.mjs
@@ -0,0 +1,57 @@
+import { FULFILMENT_STATUS_LIST } from "../../commons/commons.mjs";
+import ecwid from "../../ecwid.app.mjs";
+export default {
+  name: "Ecwid Update Order Status",
+  version: "0.0.1",
+  key: "ecwid-update-order-status",
+  description: "",
+  props: {
+    condition: {
+      label: "Condition to execute Update",
+      description: "Apply status change conditionally based on input",
+      type: "boolean",
+      default: true,
+      optional: true,
+    },
+    orderId: {
+      label: "Order ID",
+      description: "Order ID for which fulfilment status need to be updated",
+      type: "string",
+      optional: false,
+    },
+    fulfilmentStatus: {
+      label: "Fulfilment Status",
+      description: "New Fulfilment Status to be updated in the order",
+      type: "string",
+      options: FULFILMENT_STATUS_LIST,
+      optional: false,
+    },
+    ecwid,
+  },
+  type: "action",
+  methods: {},
+  async run({ $ }) {
+    let update = "IGNORED";
+    if (this.condition) {
+      const updateStatus = await
+      this.ecwid.updateFulfilmentStatus(this.orderId, this.fulfilmentStatus);
+      if (updateStatus.updateCount === 1) {
+        console.log("Updated Order Status of " + this.orderId + " to " + this.fulfilmentStatus);
+        $.export("$summary", "Updated Order Status of " + this.orderId + " to " + this.fulfilmentStatus);
+        update = "UPDATED";
+      }
+      else {
+        console.error("Error Updating Order Status of " + this.orderId + " to " + this.fulfilmentStatus);
+        update = "ERROR";
+        $.export("$summary", "Error Updating Order Status of " + this.orderId + " to " + this.fulfilmentStatus);
+        return $.flow.exit("Error Updating Order Status of " + this.orderId + " to " + this.fulfilmentStatus);
+      }
+    } else {
+      $.export("$summary", "No updates done to order " + this.orderId);
+    }
+    $.export("$summary", "Order Update Successful " + this.orderId);
+    return {
+      "UpdateStatus": update,
+    };
+  },
+};
