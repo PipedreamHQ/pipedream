4559f94d88f0879b1a7d34388aeca54a8b615355
guilherme.falcao <gtfalcao96@gmail.com>
2022-09-28 19:59:53 -0300

    Testing rollback version

--- components/brex_staging/actions/list-transactions-for-primary-card-account/list-transactions-for-primary-card-account.mjs
+++ components/brex_staging/actions/list-transactions-for-primary-card-account/list-transactions-for-primary-card-account.mjs
@@ -6,7 +6,7 @@ export default {
   name: "List Transactions for Primary Card Account",
   description: "Lists all settled transactions for the primary card account. [See the docs here](https://developer.brex.com/openapi/transactions_api/#operation/listPrimaryCardTransactions).",
   key: "brex_staging-list-transactions-for-primary-card-account",
-  version: "0.0.2",
+  version: "0.0.1",
   type: "action",
   props: {
     brexApp,
