50a306a4b41a7a0a3476970b63f35afd855fabc3
Jorge Cortes <jacortesmahmud@gmail.com>
2022-09-28 19:11:57 -0500

    Fixed version and merge

--- components/asana/sources/tag-added-to-task/tag-added-to-task.mjs
+++ components/asana/sources/tag-added-to-task/tag-added-to-task.mjs
@@ -7,7 +7,7 @@ export default {
   type: "source",
   name: "New Tag Added To Task (Instant)",
   description: "Emit new event for each new tag added to a task.",
-  version: "0.1.2",
+  version: "0.1.1",
   dedupe: "unique",
   props: {
     ...common.props,
