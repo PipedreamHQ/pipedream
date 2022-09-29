f1ea4b6dcf756ab4b42e152d1683d076e5be5387
SH-Vahn <99463664+SH-Vahn@users.noreply.github.com>
2022-09-29 13:56:31 +0900

    Edit: supports sorting for database query

--- components/notion/actions/query-database/query-database.mjs
+++ components/notion/actions/query-database/query-database.mjs
@@ -20,12 +20,19 @@ export default {
       description: "The filter to query. [See how filters work here](https://developers.notion.com/reference/post-database-query-filter). E.g. { \"property\": \"Email\", \"rich_text\": { \"contains\": \"gmail.com\" } }",
       type: "string",
     },
+    sorts: {
+      label: "Sorts",
+      description: "The array of sorting option objects. [See how sort work here](https://developers.notion.com/reference/post-database-query-sort). E.g. [ { \"property\": \"Food group\", \"direction\": \"descending\" }, { \"property\": \"Name\", \"direction\": \"ascending\" } ]",
+      type: "string",
+      optional: true,
+    },
   },
   async run({ $ }) {
-    const { filter } = this;
+    const { filter, sorts } = this;
 
     const response = await this.notion.queryDatabase(this.databaseId, {
       filter: utils.parseStringToJSON(filter),
+      sorts: utils.parseStringToJSON(sorts),
     });
 
     $.export("$summary", "Retrieved database query result");
