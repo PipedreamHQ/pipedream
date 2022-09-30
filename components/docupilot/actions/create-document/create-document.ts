1cd47064778208785f0ac8e0c4f28e2a571b289b
guilherme.falcao <gtfalcao96@gmail.com>
2022-09-30 04:17:15 -0300

    JSON parsing for object values

--- components/docupilot/actions/create-document/create-document.ts
+++ components/docupilot/actions/create-document/create-document.ts
@@ -7,7 +7,7 @@ export default defineAction({
   description:
     "Create a document [See docs here](https://help.docupilot.app/create-document/api-and-webhook-integration#api-integration)",
   key: "docupilot-create-document",
-  version: "0.0.19",
+  version: "0.0.21",
   type: "action",
   props: {
     docupilot,
@@ -23,15 +23,46 @@ export default defineAction({
       label: "Template Tokens",
       description: `The tokens used in this template (as object keys) and their values.
        \\
-       Objects and arrays should be in JSON-stringified format.`,
+       Objects and arrays should be in JSON-stringified format.
+       \\
+       If you need to include characters such as \`{}[]\` in a value, and it should not be parsed as an object or array, prefix the key with \`$\`.`,
     },
   },
   async run({ $ }): Promise<any> {
     const url: string = this.templateUrl.trim();
     const baseUrl: string = this.docupilot._createDocumentBaseUrl();
-    if (!url.startsWith(baseUrl)) throw new ConfigurationError("Invalid `Template URL`. Check the prop and make sure you copied the URL properly.");
+    if (!url.startsWith(baseUrl))
+      throw new ConfigurationError(
+        "Invalid `Template URL`. Check the prop and make sure you copied the URL properly.",
+      );
 
-    const data = this.tokens;
+    type tokensObject = {
+      [key: string]: string;
+    };
+
+    const data = {};
+    const tokens: tokensObject = this.tokens;
+
+    if (tokens) {
+      Object.entries(tokens).forEach(([
+        key,
+        value,
+      ]) => {
+        try {
+          if (!key.startsWith("$") && value.match(/({[^}]*})|(\[[^\]]*\])/)) {
+            value = JSON.parse(value);
+          }
+        } catch (err) {
+          throw new ConfigurationError(`Error parsing JSON value for key \`${key}\`: *"${err}"*
+            \\
+            If this is not a JSON string for an object or array, prefix the key with \`$\` as in \`$${key}\``);
+        }
+
+        data[key.startsWith("$")
+          ? key.slice(1)
+          : key] = value;
+      });
+    }
 
     const params = {
       $,
