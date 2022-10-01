3dc912cfd9c364afc73b69a52b89a34a121865d8
guilherme.falcao <gtfalcao96@gmail.com>
2022-10-01 02:53:57 -0300

    NeverBounce TS structure

---
+++ components/neverbounce/actions/verify-email-address/verify-email-address.ts
@@ -0,0 +1,35 @@
+import quickemailverification from "../../app/neverbounce.app";
+import { defineAction } from "@pipedream/types";
+import {
+  VerifyEmailParams, VerifyEmailResponse,
+} from "../../common/types";
+
+export default defineAction({
+  name: "Verify Email Address",
+  description:
+    "Verify an email address [See docs here](http://docs.quickemailverification.com/email-verification-api/verify-an-email-address)",
+  key: "quickemailverification-verify-email-address",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    quickemailverification,
+    email: {
+      label: "Email Address",
+      description: "An email address to be verified.",
+      type: "string",
+    },
+  },
+  async run({ $ }): Promise<VerifyEmailResponse> {
+    const params: VerifyEmailParams = {
+      $,
+      params: {
+        email: this.email,
+      },
+    };
+    const data: VerifyEmailResponse = await this.quickemailverification.verifyEmailAddress(params);
+
+    $.export("$summary", `Verified email ${data.email} (${data.result})`);
+
+    return data;
+  },
+});
