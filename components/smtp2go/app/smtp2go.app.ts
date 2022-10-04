4233b0f3a4417407fc7c4d64fb3caf837dec10d5
michelle0927 <michellelbergero@hotmail.com>
2022-10-04 14:21:17 -0400

    updates/fixes

--- components/smtp2go/app/smtp2go.app.ts
+++ components/smtp2go/app/smtp2go.app.ts
@@ -3,6 +3,29 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "smtp2go",
+  propDefinitions: {
+    templateId: {
+      type: "string",
+      label: "Template ID",
+      description: "The ID of the pre-defined template in SMTP2GO.",
+      async options({ prevContext }) {
+        const { token } = prevContext;
+        const {
+          continue_token: continueToken, templates,
+        } = await this.listTemplates(token);
+        const options = templates.map((template) => ({
+          label: template.name,
+          value: template.id,
+        }));
+        return {
+          options,
+          context: {
+            continueToken,
+          },
+        };
+      },
+    },
+  },
   methods: {
     _apikey() {
       return this.$auth.api_key;
@@ -20,12 +43,14 @@ export default {
       } = params;
 
       return axios($, {
-        url: `https://api.smtp2go.com/v3/${endpoint}`,
+        url: endpoint.includes("http")
+          ? endpoint
+          : `https://api.smtp2go.com/v3/${endpoint}`,
         method,
         headers: this.getHeaders(),
         data: {
           ...data,
-          api_key: this._apikey,
+          api_key: this._apikey(),
         },
       });
     },
@@ -40,16 +65,30 @@ export default {
       if (result.data.failed > 0) {
         throw new Error(`Mail sender responded with the following error(s): ${result.data.failures.join(",")}`);
       }
+      return result;
     },
     async sendSingleEmail($, data: { sender: string; to: string[]; cc: string[]; bcc: string[]; subject: string; text_body: string; html_body: string; attachments: any[]; custom_headers: any[]; }, ignoreFailures: boolean) {
-      const result = this.sharedActionRequest($, "email/send", data);
+      const result = await this.sharedActionRequest($, "email/send", data);
       if (ignoreFailures) return result;
       return this.verifiedSent(result);
     },
     async sendSingleEmailWithTemplate($, data: { sender: string; to: string[]; cc: string[]; bcc: string[]; subject: string; template_id: string; template_data: any; attachments: any[]; custom_headers: any[]; }, ignoreFailures: boolean) {
-      const result = this.sharedActionRequest($, "email/send", data);
+      const result = await this.sharedActionRequest($, "email/send", data);
       if (ignoreFailures) return result;
       return this.verifiedSent(result);
+    },
+    async getTemplate(id: string, $ = this) {
+      const { data } = await this.sharedActionRequest($, "template/view", {
+        id,
+      });
+      return data;
+    },
+    async listTemplates(token: string, $ = this) {
+      if (token) {
+        return this.sharedActionRequest($, token);
+      }
+      const { data } = await this.sharedActionRequest($, "template/search");
+      return data;
     },
   },
 };
