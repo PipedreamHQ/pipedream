85a0ebd1ce824157bbe0855f8c4bcbcf515ba5b6
Cassiano Vellames <cassiano.v@turing.com>
2022-10-04 10:20:59 -0300

    google calendar

--- components/google_calendar/actions/query-free-busy-calendars/query-free-busy-calendars.mjs
+++ components/google_calendar/actions/query-free-busy-calendars/query-free-busy-calendars.mjs
@@ -2,8 +2,8 @@ import googleCalendar from "../../google_calendar.app.mjs";
 
 export default {
   key: "google_calendar-query-free-busy-calendars",
-  name: "Retreive Free/Busy Calendar Details",
-  description: "Retreive Free/Busy Calendar Details from the user account. [See the docs here](https://googleapis.dev/nodejs/googleapis/latest/calendar/classes/Resource$Freebusy.html#query)",
+  name: "Retrieve Free/Busy Calendar Details",
+  description: "Retrieve Free/Busy Calendar Details from the user account. [See the docs here](https://googleapis.dev/nodejs/googleapis/latest/calendar/classes/Resource$Freebusy.html#query)",
   version: "0.1.0",
   type: "action",
   props: {
@@ -27,7 +27,7 @@ export default {
       },
     });
 
-    $.export("$summary", "Successfully retreived free/busy calendar info");
+    $.export("$summary", "Successfully retrieved free/busy calendar info");
 
     return response;
   },
