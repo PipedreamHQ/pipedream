af8fe419cdbb797a763ab349a4c95d36d96da1f7
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-04 13:47:28 -0300

    make list prop optional on tasks props

--- components/clickup/actions/common/task-props.mjs
+++ components/clickup/actions/common/task-props.mjs
@@ -3,6 +3,18 @@ import common from "./list-props.mjs";
 export default {
   props: {
     ...common.props,
+    listId: {
+      propDefinition: [
+        common.props.clickup,
+        "lists",
+        (c) => ({
+          workspaceId: c.workspaceId,
+          spaceId: c.spaceId,
+          folderId: c.folderId,
+        }),
+      ],
+      optional: true,
+    },
     taskId: {
       propDefinition: [
         common.props.clickup,
@@ -11,6 +23,7 @@ export default {
           listId: c.listId,
         }),
       ],
+      description: "To show options please select a **List** first",
     },
   },
 };
