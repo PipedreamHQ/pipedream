beeb8034231b5765160633c264f85e65dcabef54
Luan Cazarine <luanhc@gmail.com>
2022-10-04 10:45:33 -0300

    VoilaNorbert #3831 Actions:   - Start Contact Search   - Verify Email   - Find Contact

---
+++ components/voilanorbert/actions/verifiy-email/verifiy-email.mjs
@@ -0,0 +1,33 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "voilanorbert-verifiy-email",
+  name: "Verify Email",
+  description: `Verifies the given list of emails.
+  In case your account does not have a sufficient Verify API balance the service will try to auto refill the balance by charging using the billing details of the account. If it fails to charge, an HTTP status code of 402 will be returned.
+  [See the docs here](https://api.voilanorbert.com/2018-01-08/#verify-endpoint-post)`,
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    emails: {
+      propDefinition: [
+        common.props.voilanorbert,
+        "emails",
+      ],
+    },
+  },
+  methods: {
+    async processEvent({ $ }) {
+      const { resume_url } = $.flow.suspend();
+      return this.voilanorbert.verifyEmails({
+        emails: this.emails,
+        webhook: resume_url,
+      });
+    },
+    getSummary() {
+      return "Emails Successfully sent for verification!";
+    },
+  },
+};
--- components/voilanorbert/voilanorbert.app.mjs
+++ components/voilanorbert/voilanorbert.app.mjs
@@ -1,11 +1,102 @@
+import { axios } from "@pipedream/platform";
+import { objectToString } from "./utils.mjs";
+
 export default {
   type: "app",
   app: "voilanorbert",
-  propDefinitions: {},
+  propDefinitions: {
+    contactId: {
+      type: "integer",
+      label: "Contact Id",
+      description: "The contact's id.",
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "Full name of the person to search.",
+    },
+    domain: {
+      type: "string",
+      label: "Domain",
+      description: "The domain of the company. (Either `domain` or `company` is required).",
+    },
+    company: {
+      type: "string",
+      label: "Company",
+      description: "The name of the company. (Either `domain` or `company` is required).",
+    },
+    emails: {
+      type: "string",
+      label: "Emails",
+      description: "Comma separated emails",
+    },
+    listId: {
+      type: "integer",
+      label: "List ID",
+      description: "A List ID the contact will affected to.",
+      async options() {
+        const { result } = await this.getLists();
+
+        return result.map(({
+          name: label, id: value,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _accessToken() {
+      return this.$auth.api_key;
+    },
+    _auth() {
+      return {
+        username: "",
+        password: this._accessToken(),
+      };
+    },
+    _apiUrl() {
+      return "https://api.voilanorbert.com/2018-01-08";
+    },
+    async _makeRequest({
+      $ = this, path, ...options
+    }) {
+      const config = {
+        url: `${this._apiUrl()}/${path}`,
+        ...options,
+        auth: this._auth(),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+      return axios($, config);
+    },
+    async getContact({ contactId }) {
+      return await this._makeRequest({
+        path: `contacts/${contactId}`,
+      });
+    },
+    async getLists() {
+      return await this._makeRequest({
+        path: "lists/",
+      });
+    },
+    async startContactSearch(data) {
+      const bodyFormData = objectToString(data);
+      return await this._makeRequest({
+        method: "POST",
+        path: "search/name",
+        data: bodyFormData,
+      });
+    },
+    async verifyEmails(data) {
+      const bodyFormData = objectToString(data);
+      return await this._makeRequest({
+        method: "POST",
+        path: "verifier/upload",
+        data: bodyFormData,
+      });
     },
   },
 };
