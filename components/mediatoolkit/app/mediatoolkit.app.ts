0a77adfc507821499cd8eb9eeeab7ef3bb39015f
Lucas Caresia <lucascarezia@gmail.com>
2022-10-05 16:25:11 -0300

    Mediatoolkit #3685 (#4351)
    * Added actions
    
    * pnpm
    
    * package.json fix
    
    * Fixed error 400 on request
    
    Co-authored-by: Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>

--- components/mediatoolkit/app/mediatoolkit.app.ts
+++ components/mediatoolkit/app/mediatoolkit.app.ts
@@ -1,13 +1,81 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from '@pipedream/platform'
 
 export default defineApp({
   type: "app",
   app: "mediatoolkit",
-  propDefinitions: {},
-  methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+  propDefinitions: {
+    organizationId: {
+      label: "Organization ID",
+      description: "The organization ID",
+      type: "string",
+      async options() {
+        const organizations = await this.getOrganizations()
+
+        return organizations.map(organization => ({
+          label: organization.name,
+          value: organization.id
+        }))
+      }
     },
+    groupId: {
+      label: "Group ID",
+      description: "The group ID",
+      type: "string",
+      async options({ organizationId }) {
+        const groups = await this.getGroups({
+          organizationId
+        })
+
+        return groups.map(group => ({
+          label: group.name,
+          value: group.id
+        }))
+      }
+    }
+  },
+  methods: {
+    _accessToken() {
+      return this.$auth.access_token;
+    },
+    _apiUrl() {
+      return "https://api.mediatoolkit.com";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        ...args,
+        params: {
+          ...args.params,
+          access_token: this._accessToken(),
+        },
+      });
+    },
+    async getOrganizations(args = {}) {
+      const response = await this._makeRequest({
+        path: "/me/organizations",
+        ...args
+      })
+
+      return response?.data?.organizations ?? []
+    },
+    async getGroups({ organizationId, ...args }) {
+      const response = await this._makeRequest({
+        path: `/organizations/${organizationId}/groups`,
+        ...args
+      })
+
+      return response?.data?.groups ?? []
+    },
+    async getMentions({ organizationId, groupId, ...args }) {
+      const response = await this._makeRequest({
+        path: `/organizations/${organizationId}/groups/${groupId}/mentions`,
+        ...args
+      })
+
+      return response?.data?.response ?? []
+    }
   },
 });
