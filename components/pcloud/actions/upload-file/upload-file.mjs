3773c7ddb38965d9e334a655b70de83f13dd4326
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-06 08:37:21 -0300

    bump versions

--- components/pcloud/actions/upload-file/upload-file.mjs
+++ components/pcloud/actions/upload-file/upload-file.mjs
@@ -8,7 +8,7 @@ export default {
   name: "Upload File",
   description:
     "Upload a file to the specified folder. [See the docs here](https://docs.pcloud.com/methods/file/uploadfile.html)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     ...common.props,
