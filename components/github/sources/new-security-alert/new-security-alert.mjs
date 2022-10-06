70711363bb1b2a1da7adc691c28d9fba224e7b5b
Danny Roosevelt <danny@pipedream.com>
2022-10-06 15:10:16 -0700

    Fixing typo in name

--- components/github/sources/new-security-alert/new-security-alert.mjs
+++ components/github/sources/new-security-alert/new-security-alert.mjs
@@ -3,9 +3,9 @@ import common from "../common/common-polling.mjs";
 export default {
   ...common,
   key: "github-new-security-alert",
-  name: "New Secutory Alert",
+  name: "New Security Alert",
   description: "Emit new events when GitHub discovers a security vulnerability in one of your repositories",
-  version: "0.1.2",
+  version: "0.1.3",
   type: "source",
   dedupe: "unique",
   async run() {
