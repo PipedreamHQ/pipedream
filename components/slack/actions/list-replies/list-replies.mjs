e59fe42c1d808de6c49d0a27a510b501ff99c3b4
Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>
2022-10-06 10:48:48 -0300

    Increasing sources and actions versions

--- components/slack/actions/list-replies/list-replies.mjs
+++ components/slack/actions/list-replies/list-replies.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack-list-replies",
   name: "List Replies",
   description: "Retrieve a thread of messages posted to a conversation. [See docs here](https://api.slack.com/methods/conversations.replies)",
-  version: "0.0.5",
+  version: "0.0.6",
   type: "action",
   props: {
     slack,
--- components/slack/actions/send-direct-message/send-direct-message.mjs
+++ components/slack/actions/send-direct-message/send-direct-message.mjs
@@ -5,7 +5,7 @@ export default {
   key: "slack-send-direct-message",
   name: "Send a Direct Message",
   description: "Send a direct message to a single user. See [postMessage](https://api.slack.com/methods/chat.postMessage) or [scheduleMessage](https://api.slack.com/methods/chat.scheduleMessage) docs here",
-  version: "0.2.7",
+  version: "0.2.8",
   type: "action",
   props: {
     ...common.props,
