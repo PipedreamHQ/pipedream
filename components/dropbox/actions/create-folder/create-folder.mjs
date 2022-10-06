6a15d721b63f4f07199cfa5ffdd9ff33dfbce1de
michelle0927 <michellelbergero@hotmail.com>
2022-10-06 10:44:16 -0400

    bump versions

--- components/dropbox/actions/create-folder/create-folder.mjs
+++ components/dropbox/actions/create-folder/create-folder.mjs
@@ -6,7 +6,7 @@ export default {
   name: "Create folder",
   description: "Create a folder. [See docs here](https://dropbox.github.io/dropbox-sdk-js/Dropbox.html#filesCreateFolderV2__anchor)",
   key: "dropbox-create-folder",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "action",
   props: {
     dropbox,
