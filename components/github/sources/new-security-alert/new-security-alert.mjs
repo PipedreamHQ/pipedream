1b0727f470ef2513d09375d3b0f029f9d52fa192
Danny Roosevelt <danny@pipedream.com>
2022-10-06 15:09:54 -0700

    Fixing typo in key

--- components/github/sources/new-security-alert/new-security-alert.mjs
+++ components/github/sources/new-security-alert/new-security-alert.mjs
@@ -2,10 +2,10 @@ import common from "../common/common-polling.mjs";
 
 export default {
   ...common,
-  key: "github-new-secutiry-alert",
+  key: "github-new-security-alert",
   name: "New Secutory Alert",
   description: "Emit new events when GitHub discovers a security vulnerability in one of your repositories",
-  version: "0.1.1",
+  version: "0.1.2",
   type: "source",
   dedupe: "unique",
   async run() {
