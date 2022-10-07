3fd2a7a3a60a3744d47ecc0a602478193c4bf250
michelle0927 <michellelbergero@hotmail.com>
2022-10-07 14:42:01 -0400

    bump versions

--- components/notion/sources/new-database/new-database.mjs
+++ components/notion/sources/new-database/new-database.mjs
@@ -6,7 +6,7 @@ export default {
   key: "notion-new-database",
   name: "New Database",
   description: "Emit new event when a database is created. Note: Databases must be shared with your Pipedream Integtration to trigger event.",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "source",
   async run() {
     const databases = [];
