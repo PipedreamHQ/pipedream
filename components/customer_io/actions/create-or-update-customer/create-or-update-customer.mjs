520a62f686537f251a1a498cf6efa43fb9a57181
Cassiano Vellames <cassiano.v@turing.com>
2022-10-10 11:49:51 -0300

    refactor create or update customer

--- components/customer_io/actions/create-or-update-customer/create-or-update-customer.mjs
+++ components/customer_io/actions/create-or-update-customer/create-or-update-customer.mjs
@@ -1,60 +1,48 @@
-// legacy_hash_id: a_WYi4wz
-import { axios } from "@pipedream/platform";
+import app from "../../customer_io.app.mjs";
 
 export default {
   key: "customer_io-create-or-update-customer",
   name: "Create or Update Customer",
-  description: "Creates or update a customer.",
-  version: "0.1.4",
+  description: "Creates or update a customer. [See the docs here](https://customer.io/docs/api/#apitrackcustomerscustomers_update)",
+  version: "0.2.2",
   type: "action",
   props: {
-    customer_io: {
-      type: "app",
-      app: "customer_io",
-    },
-    customer_id: {
-      type: "string",
-      description: "The unique identifier for the customer.",
+    app,
+    customerId: {
+      propDefinition: [
+        app,
+        "customerId",
+      ],
     },
     email: {
+      label: "Email",
       type: "string",
       description: "The unique identifier for the customer.",
     },
     created_at: {
       type: "string",
+      label: "Created At",
       description: "The UNIX timestamp from when the user was created in your system.",
       optional: true,
     },
     attributes: {
       type: "object",
+      label: "Attributes",
       description: "Custom attributes to define the customer.",
       optional: true,
     },
   },
   async run({ $ }) {
-  // See the API docs: https://customer.io/docs/api/#apitrackcustomerscustomers_update
-
-    if (!this.customer_id) {
-      throw new Error("Must provide customer_id parameter.");
+    if (!this.customerId) {
+      throw new Error("Must provide customerId parameter.");
     }
 
-    const basicauthUserPwd = `${this.customer_io.$auth.site_id}:${this.customer_io.$auth.api_key}`;
-    const buff = Buffer.from(basicauthUserPwd);
-    const base64BasicauthUserPwd = buff.toString("base64");
-
-    const config = {
-      method: "put",
-      url: `https://track.customer.io/api/v1/customers/${this.customer_id}`,
-      headers: {
-        Authorization: `Basic ${base64BasicauthUserPwd}`,
-      },
-      data: {
-        email: this.email,
-        created_at: this.created_at,
-        ...this.attributes,
-      },
-    };
-
-    return axios($, config);
+    const res = await this.app.createOrUpdateCustomer(this.customerId, {
+      email: this.email,
+      created_at: this.created_at,
+      ...this.attributes,
+    }, $);
+    $.export("$summary", "Successfully created/updated customer");
+    return res;
   },
 };
--- components/customer_io/customer_io.app.mjs
+++ components/customer_io/customer_io.app.mjs
@@ -1,11 +1,43 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "customer_io",
-  propDefinitions: {},
+  propDefinitions: {
+    customerId: {
+      type: "string",
+      label: "Customer ID",
+      description: "The unique identifier for the customer.",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getBaseUrl() {
+      return "https://track.customer.io/api/v1";
+    },
+    _getHeaders() {
+      const basicauthUserPwd = `${this.$auth.site_id}:${this.$auth.api_key}`;
+      const buff = Buffer.from(basicauthUserPwd);
+      const base64BasicauthUserPwd = buff.toString("base64");
+
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Basic ${base64BasicauthUserPwd}`,
+      };
+    },
+    _getAxiosParams(opts = {}) {
+      const res = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return res;
+    },
+    createOrUpdateCustomer(customerId, data, $ = this) {
+      return axios($, this._getAxiosParams({
+        method: "PUT",
+        path: `/customers/${customerId}`,
+        data,
+      }));
     },
   },
 };
