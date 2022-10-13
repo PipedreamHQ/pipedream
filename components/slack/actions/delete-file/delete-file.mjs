6a0b48f213aa2f47d693c20b025d329dda60fb0e
Lucas Caresia <lucascarezia@gmail.com>
2022-10-13 09:31:19 -0300

    [BUG] Slack Upload File action is confusing #4380 (#4435)
    * Refactored upload_file action to a better UI view
    
    * Increasing sources and actions versions
    
    * Fixing file upload
    
    * typo
    
    * Increasing versions
    
    * Fixing version
    
    * Fixed TIMEOUT bug on file upload
    
    * pnpm
    
    * Fixed pnpm
    
    Co-authored-by: Lucas <lucas@LAPTOP-93ERSJBJ.localdomain>

--- components/slack/actions/delete-file/delete-file.mjs
+++ components/slack/actions/delete-file/delete-file.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack-delete-file",
   name: "Delete File",
   description: "Delete a file. [See docs here](https://api.slack.com/methods/files.delete)",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "action",
   props: {
     slack,
--- components/slack/actions/list-users/list-users.mjs
+++ components/slack/actions/list-users/list-users.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack-list-users",
   name: "List Users",
   description: "Return a list of all users in a workspace. [See docs here](https://api.slack.com/methods/users.list)",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "action",
   props: {
     slack,
