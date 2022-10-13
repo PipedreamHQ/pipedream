3061326c2f68ff9d04c21a4e8f10f9d17d0862f4
Lucas Caresia <lucascarezia@gmail.com>
2022-10-13 10:26:04 -0300

    Increasing slack-reply-to-a-message-thread version (#4490)
    * Increase slack-reply-to-a-message-thread version
    
    * Increasing package.json version
    
    * Fixed folders name to fit with key
    
    * Empty space in the end of package.json

--- components/slack/actions/reply-to-a-message/reply-to-a-message.mjs
+++ components/slack/actions/reply-to-a-message/reply-to-a-message.mjs
@@ -6,7 +6,7 @@ export default {
   key: "slack-reply-to-a-message",
   name: "Reply to a Message Thread",
   description: "Send a message as a threaded reply. See [postMessage](https://api.slack.com/methods/chat.postMessage) or [scheduleMessage](https://api.slack.com/methods/chat.scheduleMessage) docs here",
-  version: "0.1.8",
+  version: "0.1.10",
   type: "action",
   props: {
     ...common.props,
