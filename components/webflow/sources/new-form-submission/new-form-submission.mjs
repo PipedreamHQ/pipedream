51cd9019661ae709042754a73e39907ec0ca2bd3
guilherme.falcao <gtfalcao96@gmail.com>
2022-10-17 13:18:34 -0300

    Incrementing webflow component versions

--- components/webflow/sources/new-form-submission/new-form-submission.mjs
+++ components/webflow/sources/new-form-submission/new-form-submission.mjs
@@ -5,7 +5,7 @@ export default {
   key: "webflow-new-form-submission",
   name: "New Form Submission",
   description: "Emit new event when a new form is submitted. [See the docs here](https://developers.webflow.com/#trigger-types)",
-  version: "0.2.0",
+  version: "0.2.1",
   ...common,
   methods: {
     ...common.methods,
