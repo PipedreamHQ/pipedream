4185b485c64dc0442f9dc46896af33fdf2870e23
Trendoscope <sudh@trendoscope.au>
2022-10-17 22:14:09 +1100

    fix: Fix Lint issue with shopify partner

--- components/shopify_partner/actions/verify-webhook/verify-webhook.mjs
+++ components/shopify_partner/actions/verify-webhook/verify-webhook.mjs
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 export default {
   name: "Shopify Partner Verify Webhook",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "shopify_partner-verify-webhook",
   description:
     "Verify an incoming webhook from Shopify. Exits the workflow if the signature is not valid, otherwise returns `true`",
