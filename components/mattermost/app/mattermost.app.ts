7abf0333686894eebe6f632be57700e63461f0df
guilherme.falcao <gtfalcao96@gmail.com>
2022-10-19 00:16:28 -0300

    Auth method

--- components/mattermost/app/mattermost.app.ts
+++ components/mattermost/app/mattermost.app.ts
@@ -9,16 +9,26 @@ export default defineApp({
   type: "app",
   app: "mattermost",
   methods: {
-    _baseUrl(): string {
-      return "https://api.mattermost.com/v1";
+    _baseUrl(domain: string): string {
+      return `https://${domain}/api/v4`;
     },
+    _getAuth(): object {
+      return {
+        domain: this.$auth.domain,
+        token: this.$auth.oauth_access_token,
+      }
+    }
     async _httpRequest({
       $ = this,
       endpoint,
       params,
     }: HttpRequestParams): Promise<object> {
+      const { domain, token } = this._getAuth();
       return axios($, {
-        url: this._baseUrl() + endpoint,
+        url: this._baseUrl(domain) + endpoint,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
         params: {
           ...params,
           apikey: this.$auth.api_key,
