d1afd3dcea1abb72cde6b85ca3be80160b2a4753
Lucas Caresia <lucascarezia@gmail.com>
2022-10-19 16:48:46 -0300

    Nectar CRM [APP] #3072 (#3484)
    * Finished the rest of actions
    
    * pnpm
    
    * Fixing ampty line in package.json
    
    * pnpm
    
    * Updating acess token from params to headers
    
    * Name is required to create a contact
    
    * pnpm
    
    * pnpm
    
    * Increasing versions
    
    * pnpm

---
+++ components/nectar_crm/actions/create-appointment/create-appointment.ts
@@ -0,0 +1,58 @@
+import { defineAction } from "@pipedream/types";
+import nectar_crm from "../../app/nectar_crm.app";
+
+export default defineAction({
+  key: "nectar_crm-create-appointment",
+  name: "Create Appointment",
+  description: "Created a new appointment. [See docs here](https://nectarcrm.docs.apiary.io/#reference/0/compromissos/criar)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    nectar_crm,
+    contactId: {
+      propDefinition: [
+        nectar_crm,
+        "contactId",
+      ],
+    },
+    subject: {
+      label: "Subject",
+      description: "The subject of the appointment",
+      type: "string",
+    },
+    beginDate: {
+      label: "Begin Date",
+      description: "The begin date of the appointment. E.g. `2022-07-20T00:00:00-03:00`",
+      type: "string",
+    },
+    finishDate: {
+      label: "Finish Date",
+      description: "The finish date of the appointment. E.g. `2022-07-20T00:00:00-03:00`",
+      type: "string",
+    },
+    description: {
+      label: "Description",
+      description: "The description of the appointment",
+      type: "string",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.nectar_crm.createAppointment({
+      $,
+      data: {
+        assunto: this.subject,
+        cliente: {
+          id: this.contactId,
+        },
+        dataInicio: this.beginDate,
+        dataFim: this.finishDate,
+        descricao: this.description,
+      },
+    });
+
+    $.export("$summary", `Successfully created appointment with id ${response.id}`);
+
+    return response;
+  },
+});
