0053229240f8a444383ce9b8eeb8ad51c39e417b
michelle0927 <michellelbergero@hotmail.com>
2022-10-19 15:37:23 -0400

    new components

---
+++ components/google_analytics/actions/run-report/run-report.mjs
@@ -0,0 +1,65 @@
+import analytics from "../../google_analytics.app.mjs";
+
+export default {
+  key: "google_analytics-run-report",
+  version: "0.0.1",
+  name: "Run Report",
+  description: "Return report metrics based on a start and end date. [See the docs here](https://developers.google.com/analytics/devguides/reporting/core/v4/rest?hl=en)",
+  type: "action",
+  props: {
+    analytics,
+    viewId: {
+      propDefinition: [
+        analytics,
+        "viewId",
+      ],
+    },
+    startDate: {
+      type: "string",
+      label: "Start Date",
+      description: "Start date in YYYY-MM-DD format",
+    },
+    endDate: {
+      type: "string",
+      label: "End Date",
+      description: "End date in YYYY-MM-DD format",
+    },
+    metrics: {
+      type: "string[]",
+      label: "Metrics",
+      description: "Dimension attributes for your data. Explore the availabe metrics and dimensions [here](https://ga-dev-tools.web.app/dimensions-metrics-explorer/)",
+    },
+    dimensions: {
+      type: "string[]",
+      label: "Dimensions",
+      description: "Dimension attributes for your data. Explore the availabe metrics and dimensions [here](https://ga-dev-tools.web.app/dimensions-metrics-explorer/)",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      resource: {
+        reportRequests: [
+          {
+            viewId: this.viewId,
+            dateRanges: [
+              {
+                startDate: this.startDate,
+                endDate: this.endDate,
+              },
+            ],
+            dimensions: this.dimensions.map((dimension) => ({
+              name: dimension,
+            })),
+            metrics: this.metrics.map((metric) => ({
+              expression: metric,
+            })),
+          },
+        ],
+      },
+    };
+    const { data: report } = await this.analytics.queryReports(data);
+    $.export("$summary", `Successfully retrieved report for view with ID ${this.viewId}`);
+    return report;
+  },
+};
