dc538c63cd472b06c4ee9e847fe7de00caa536c9
Lucas Caresia <lucascarezia@gmail.com>
2022-10-19 11:54:25 -0300

    Adding sendblue.app.mjs

---
+++ components/sendblue/actions/common/constants.mjs
@@ -0,0 +1,56 @@
+export default {
+  SEND_STYLES: [
+    {
+      label: "Celebration",
+      value: "celebration",
+    },
+    {
+      label: "Shooting Star",
+      value: "shooting_star",
+    },
+    {
+      label: "Fireworks",
+      value: "fireworks",
+    },
+    {
+      label: "Lasers",
+      value: "lasers",
+    },
+    {
+      label: "Love",
+      value: "love",
+    },
+    {
+      label: "Confetti",
+      value: "confetti",
+    },
+    {
+      label: "Balloons",
+      value: "balloons",
+    },
+    {
+      label: "Spotlight",
+      value: "spotlight",
+    },
+    {
+      label: "Echo",
+      value: "echo",
+    },
+    {
+      label: "Invisible",
+      value: "invisible",
+    },
+    {
+      label: "Gentle",
+      value: "gentle",
+    },
+    {
+      label: "Loud",
+      value: "loud",
+    },
+    {
+      label: "Slam",
+      value: "slam",
+    },
+  ],
+};
