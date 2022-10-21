3d563106440292b6d312c07f34b31fc153bfcf41
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2022-10-21 12:56:02 -0300

    #3999 - Webflow bugfix (#4515)
    * Changing domain ids to domain names
    
    * Incrementing webflow component versions

--- components/webflow/actions/refund-order/refund-order.mjs
+++ components/webflow/actions/refund-order/refund-order.mjs
@@ -4,7 +4,7 @@ export default {
   key: "webflow-refund-order",
   name: "Refund Order",
   description: "Refund an order. [See the docs here](https://developers.webflow.com/#refund-order)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     webflow,
