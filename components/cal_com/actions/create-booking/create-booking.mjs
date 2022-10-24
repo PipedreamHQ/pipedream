feacec5d57fc9fcde43aa22d5459efac5ff26feb
michelle0927 <michellelbergero@hotmail.com>
2022-10-24 17:28:25 -0400

    new actions

---
+++ components/cal_com/actions/create-booking/create-booking.mjs
@@ -0,0 +1,84 @@
+import calCom from "../../cal_com.app.mjs";
+
+export default {
+  key: "cal_com-create-booking",
+  name: "Create Booking",
+  description: "Create a new booking. [See the docs here](https://developer.cal.com/api/api-reference/bookings#create-a-new-booking)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    calCom,
+    eventType: {
+      propDefinition: [
+        calCom,
+        "eventType",
+      ],
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "Name of the person the booking is with",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email of the person the booking is with",
+    },
+    title: {
+      type: "string",
+      label: "Title",
+      description: "Title of the new booking",
+      optional: true,
+    },
+    startTime: {
+      type: "string",
+      label: "Start Time",
+      description: "The start time of the new booking in **ISO 8601** format",
+    },
+    endTime: {
+      type: "string",
+      label: "End Time",
+      description: "The end time of the new booking in **ISO 8601** format",
+    },
+    recurringCount: {
+      type: "integer",
+      label: "Recurring Count",
+      description: "Number of times the booking should repeat",
+      optional: true,
+    },
+    language: {
+      propDefinition: [
+        calCom,
+        "language",
+      ],
+    },
+    timeZone: {
+      propDefinition: [
+        calCom,
+        "timeZone",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      eventTypeId: this.eventType,
+      name: this.name,
+      email: this.email,
+      title: this.title,
+      start: this.startTime,
+      end: this.endTime,
+      recurringCount: this.recurringCount,
+      language: this.language,
+      timeZone: this.timeZone,
+      location: "",
+      customInputs: [],
+      metadata: {},
+    };
+    const response = await this.calCom.createBooking({
+      data,
+      $,
+    });
+    $.export("$summary", "Successfully created booking with ID");
+    return response;
+  },
+};
--- components/cal_com/cal_com.app.mjs
+++ components/cal_com/cal_com.app.mjs
@@ -1,15 +1,65 @@
-import { axios } from "@pipedream/platform";
+import {
+  axios, ConfigurationError,
+} from "@pipedream/platform";
+import retry from "async-retry";
+import languages from "./common/languages.mjs";
+import timeZones from "./common/time-zones.mjs";
 
 export default {
   type: "app",
   app: "cal_com",
-  propDefinitions: {},
+  propDefinitions: {
+    booking: {
+      type: "string",
+      label: "Booking",
+      description: "The booking to retrieve",
+      async options({ filterCancelled = false }) {
+        const { bookings = [] } = await this.listBookings();
+        const filteredBookings = filterCancelled
+          ? bookings.filter((booking) => booking.status !== "CANCELLED")
+          : bookings;
+        return filteredBookings.map((booking) => ({
+          label: booking.title,
+          value: booking.id,
+        }));
+      },
+    },
+    eventType: {
+      type: "string",
+      label: "Event Type",
+      description: "Event type of the new booking",
+      async options() {
+        const { event_types: eventTypes } = await this.listEventTypes();
+        return eventTypes.map((type) => ({
+          label: type.title,
+          value: type.id,
+        }));
+      },
+    },
+    language: {
+      type: "string",
+      label: "Language",
+      description: "The language for the new booking",
+      options: languages.LANGUAGE_OPTIONS,
+    },
+    timeZone: {
+      type: "string",
+      label: "Time Zone",
+      description: "The time-zone of the new booking",
+      options: timeZones.TIME_ZONES,
+    },
+  },
   methods: {
     _baseUrl() {
       return `https://${this.$auth.domain}/v1/`;
     },
     _apiKey() {
       return this.$auth.api_key;
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+      };
     },
     async _makeRequest(args = {}) {
       const {
@@ -22,13 +72,45 @@ export default {
       const config = {
         method,
         url: `${this._baseUrl()}${path}`,
+        headers: this._getHeaders(),
         params: {
           ...params,
           apiKey: this._apiKey(),
         },
         ...otherArgs,
       };
-      return axios($, config);
+      return this._withRetries(() => {
+        return axios($, config);
+      });
+    },
+    _isRetriableStatusCode(statusCode) {
+      return [
+        408,
+        429,
+        500,
+      ].includes(statusCode);
+    },
+    async _withRetries(apiCall) {
+      const retryOpts = {
+        retries: 3,
+        factor: 2,
+      };
+      return retry(async (bail) => {
+        try {
+          const data = await apiCall();
+
+          return data;
+        } catch (err) {
+          const { status = 500 } = err;
+          if (!this._isRetriableStatusCode(status)) {
+            bail(`
+              Unexpected error (status code: ${status}):
+              ${JSON.stringify(err.response)}
+            `);
+          }
+          throw new ConfigurationError("Could not get data");
+        }
+      }, retryOpts);
     },
     async createWebhook(data) {
       return this._makeRequest({
@@ -43,10 +125,36 @@ export default {
         path: `hooks/${hookId}`,
       });
     },
-    async listBookings() {
+    async getBooking(bookingId, $) {
       return this._makeRequest({
-        method: "GET",
+        path: `bookings/${bookingId}`,
+        $,
+      });
+    },
+    async listBookings(args = {}) {
+      return this._makeRequest({
         path: "bookings",
+        ...args,
+      });
+    },
+    async createBooking(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "bookings",
+        ...args,
+      });
+    },
+    async deleteBooking(bookingId, $) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `bookings/${bookingId}`,
+        $,
+      });
+    },
+    async listEventTypes(args = {}) {
+      return this._makeRequest({
+        path: "event-types",
+        ...args,
       });
     },
   },
