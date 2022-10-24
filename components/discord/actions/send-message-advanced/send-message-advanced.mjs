a40043fd230cb6df59fb01c609b15e2c982d6649
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-24 14:54:06 -0300

    bump versions

--- components/discord/actions/send-message-advanced/send-message-advanced.mjs
+++ components/discord/actions/send-message-advanced/send-message-advanced.mjs
@@ -5,7 +5,7 @@ export default {
   key: "discord-send-message-advanced",
   name: "Send Message (Advanced)",
   description: "Send a simple or structured message (using embeds) to a Discord channel",
-  version: "1.0.0",
+  version: "1.0.1",
   type: "action",
   props: {
     ...common.props,
--- components/discord/sources/new-message/new-message.mjs
+++ components/discord/sources/new-message/new-message.mjs
@@ -5,7 +5,7 @@ export default {
   key: "discord-new-message",
   name: "New Message (Instant)",
   description: "Emit new event for each message posted to one or more channels in a Discord server",
-  version: "1.0.1",
+  version: "1.0.2",
   dedupe: "unique",
   props: {
     discord,
