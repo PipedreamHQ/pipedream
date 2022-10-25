9996d354f2d102a4b6547dbdc6d922b52a8e4235
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-25 14:54:11 -0300

    fix bigcommerce components

--- components/bigcommerce/sources/custom-events/custom-events.mjs
+++ components/bigcommerce/sources/custom-events/custom-events.mjs
@@ -1,12 +1,12 @@
 import bigcommerce from "../../bigcommerce.app.mjs";
 import constants from "../../common/constants.mjs";
-import { filterObj } from "../../utils.mjs";
+import { filterObj } from "../../common/utils.mjs";
 import common from "../common/base.mjs";
 
 export default {
   ...common,
   name: "New Custom Events",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   key: "bigcommerce-custom-events",
   description: "Emit new custom webhook event",
