0099b6305a79263ad9a4a90833464180e14a7dc4
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-25 13:48:18 -0300

    fix stack_exchange components

--- components/stack_exchange/sources/new-question-for-specific-keywords/new-question-for-specific-keywords.js
+++ components/stack_exchange/sources/new-question-for-specific-keywords/new-question-for-specific-keywords.js
@@ -1,19 +1,30 @@
-const stack_exchange = require('../../stack_exchange.app');
+const stack_exchange = require("../../stack_exchange.app");
 
 module.exports = {
-  key: 'stack_exchange-new-question-for-specific-keywords',
-  name: 'New Question for Specific Keywords',
+  key: "stack_exchange-new-question-for-specific-keywords",
+  name: "New Question for Specific Keywords",
   description:
-    'Emits an event when a new question is posted and related to a set of specific keywords',
-  version: '0.0.2',
-  dedupe: 'unique',
+    "Emits an event when a new question is posted and related to a set of specific keywords",
+  version: "0.0.3",
+  type: "source",
+  dedupe: "unique",
   props: {
     stack_exchange,
-    db: '$.service.db',
-    siteId: {propDefinition: [stack_exchange, 'siteId']},
-    keywords: {propDefinition: [stack_exchange, 'keywords']},
+    db: "$.service.db",
+    siteId: {
+      propDefinition: [
+        stack_exchange,
+        "siteId",
+      ],
+    },
+    keywords: {
+      propDefinition: [
+        stack_exchange,
+        "keywords",
+      ],
+    },
     timer: {
-      type: '$.interface.timer',
+      type: "$.interface.timer",
       default: {
         intervalSeconds: 60 * 15, // 15 minutes
       },
@@ -22,7 +33,7 @@ module.exports = {
   hooks: {
     async activate() {
       const fromDate = this._getCurrentEpoch();
-      this.db.set('fromDate', fromDate);
+      this.db.set("fromDate", fromDate);
     },
   },
   methods: {
@@ -31,7 +42,11 @@ module.exports = {
       return Math.floor(Date.now() / 1000);
     },
     generateMeta(data) {
-      const {question_id: id, creation_date: ts, title} = data;
+      const {
+        question_id: id,
+        creation_date: ts,
+        title,
+      } = data;
       const summary = `New question: ${title}`;
       return {
         id,
@@ -41,14 +56,14 @@ module.exports = {
     },
   },
   async run() {
-    const fromDate = this.db.get('fromDate');
+    const fromDate = this.db.get("fromDate");
     const toDate = this._getCurrentEpoch();
-    const keywordsQuery = this.keywords.join(',');
+    const keywordsQuery = this.keywords.join(",");
     const searchParams = {
       fromDate,
       toDate,
-      sort: 'creation',
-      order: 'asc',
+      sort: "creation",
+      order: "asc",
       closed: false,
       site: this.siteId,
       q: keywordsQuery,
@@ -60,6 +75,6 @@ module.exports = {
       this.$emit(item, meta);
     }
 
-    this.db.set('fromDate', toDate);
+    this.db.set("fromDate", toDate);
   },
 };
