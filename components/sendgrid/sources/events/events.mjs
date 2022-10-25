5c40b3e1f6c3e0a3fc0f502113454d4637da9570
Andrew Chuang <andrewjschuang@gmail.com>
2022-10-25 13:44:08 -0300

    fix sendgrid components

--- components/sendgrid/sources/events/events.mjs
+++ components/sendgrid/sources/events/events.mjs
@@ -1,13 +1,13 @@
 import common from "../common/http-based.mjs";
-import deliveryEventTypes from "./delivery-event-types.mjs";
-import engagementEventTypes from "./engagement-event-types.mjs";
+import deliveryEventTypes from "../common/delivery-event-types.mjs";
+import engagementEventTypes from "../common/engagement-event-types.mjs";
 
 export default {
   ...common,
   key: "sendgrid-events",
   name: "New Events (Instant)",
   description: "Emit new event when any of the specified SendGrid events is received",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "source",
   dedupe: "unique",
   props: {
