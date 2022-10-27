7d9883bd8851f09ba235a30f9010cc7998a49553
Cassiano Vellames <cassiano.v@turing.com>
2022-10-27 11:41:33 -0300

    action and source

---
+++ components/transistor_fm/sources/new-event/new-event.mjs
@@ -0,0 +1,64 @@
+import app from "../../transistor_fm.app.mjs";
+import options from "../../common/options.mjs";
+
+export default {
+  name: "New Event",
+  description: "Emit new event when the desired event happens",
+  key: "transistor_fm-new-event",
+  version: "0.0.1",
+  type: "source",
+  props: {
+    app,
+    http: "$.interface.http",
+    db: "$.service.db",
+    showId: {
+      propDefinition: [
+        app,
+        "showId",
+      ],
+    },
+    eventName: {
+      type: "string",
+      label: "Event Name",
+      description: "The name of the event you want to subscribe to.",
+      options: options.EVENT_NAME_OPTIONS,
+    },
+  },
+  hooks: {
+    async activate() {
+      const { data } = await this.app.registerHook({
+        show_id: this.showId,
+        event_name: this.eventName,
+        url: this.http.endpoint,
+      });
+      this._setHookId(data.id);
+      console.log("Hook successfully registered");
+    },
+    async deactivate() {
+      await this.app.unregisterHook(this._getHookId());
+      this._setHookId(null);
+      console.log("Hook successfully unregistered");
+    },
+  },
+  methods: {
+    _setHookId(hookId) {
+      this.db.set("hookId", hookId);
+    },
+    _getHookId() {
+      return this.db.get("hookId");
+    },
+    _emit(data) {
+      this.$emit(data, {
+        id: data.id,
+        summary: `${this.eventName} - ${data.attributes.email}`,
+        ts: data.attributes.created_at || data.attributes.last_notified_at || new Date(),
+      });
+    },
+  },
+  async run(event) {
+    console.log("Event received");
+    console.log("WARN: Transistor.fm does not assign webhook events. Be awared that, because of this, we can not check the reliability of this event.");
+    this._emit(event.body.data);
+    console.log("Event emitted");
+  },
+};
