6d6e550bccfec137cd44516748292b1e9bcb32fe
Jorge Cortes <jacortesmahmud@gmail.com>
2022-10-28 17:00:16 -0500

    Changes reverted. Old way of getting twilios client instance

--- components/twilio/actions/list-message-media/list-message-media.mjs
+++ components/twilio/actions/list-message-media/list-message-media.mjs
@@ -5,7 +5,7 @@ export default {
   key: "twilio-list-message-media",
   name: "List Message Media",
   description: "Return a list of media associated with your message. [See the docs](https://www.twilio.com/docs/sms/api/media-resource#read-multiple-media-resources) for more information",
-  version: "0.1.0",
+  version: "0.1.1",
   type: "action",
   props: {
     twilio,
