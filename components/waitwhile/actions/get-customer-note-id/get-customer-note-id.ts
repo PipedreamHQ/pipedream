5da9bd20818f71bb7af4914f9f17d7707941b6da
Leo Vu <vunguyenhung@outlook.com>
2022-10-28 21:43:06 +0700

    4057 waitwhile actions (#4592)

---
+++ components/waitwhile/actions/get-customer-note-id/get-customer-note-id.ts
@@ -0,0 +1,39 @@
+import waitwhile from "../../app/waitwhile.app";
+import { defineAction } from "@pipedream/types";
+
+export default defineAction({
+  name: "Get Customer Note Id",
+  version: "0.0.1",
+  key: "waitwhile-get-customer-note-id",
+  description: "Get customer note by ID. [See the doc here](https://developers.waitwhile.com/reference/getcustomerscustomeridnotescustomernoteid)",
+  props: {
+    waitwhile,
+    customerId: {
+      propDefinition: [
+        waitwhile,
+        "customerId",
+      ],
+    },
+    customerNoteId: {
+      propDefinition: [
+        waitwhile,
+        "customerNoteId",
+        (c) => ({
+          customerId: c.customerId,
+        }),
+      ],
+    },
+  },
+  type: "action",
+  methods: {},
+  async run({ $ }) {
+    const params = {
+      customerId: this.customerId,
+      customerNoteId: this.customerNoteId,
+    };
+
+    const data = await this.waitwhile.getCustomerNoteById(params);
+    $.export("summary", "Successfully retrieved customer note");
+    return data;
+  },
+});
---
+++ components/waitwhile/actions/search-customers/search-customers.ts
@@ -0,0 +1,91 @@
+import waitwhile from "../../app/waitwhile.app";
+import { defineAction } from "@pipedream/types";
+
+export default defineAction({
+  name: "Search Customers",
+  version: "0.0.1",
+  key: "waitwhile-search-customers",
+  description: "Search for customers. [See the doc here](https://developers.waitwhile.com/reference/searchcustomers)",
+  props: {
+    waitwhile,
+    locationId: {
+      propDefinition: [
+        waitwhile,
+        "locationId",
+      ],
+    },
+    state: {
+      label: "State",
+      type: "string",
+      optional: true,
+      description: "State of visit",
+    },
+    tag: {
+      propDefinition: [
+        waitwhile,
+        "tag",
+      ],
+    },
+    q: {
+      label: "Query",
+      type: "string",
+      optional: true,
+      description: "Search query, prefix match on name, phone, email or customer identifier",
+    },
+    limit: {
+      propDefinition: [
+        waitwhile,
+        "limit",
+      ],
+    },
+    page: {
+      label: "Page",
+      type: "integer",
+      optional: true,
+      description: "Page in results",
+    },
+    fromDate: {
+      propDefinition: [
+        waitwhile,
+        "fromDate",
+      ],
+    },
+    toDate: {
+      propDefinition: [
+        waitwhile,
+        "toDate",
+      ],
+    },
+    fromTime: {
+      propDefinition: [
+        waitwhile,
+        "fromTime",
+      ],
+    },
+    toTime: {
+      propDefinition: [
+        waitwhile,
+        "toTime",
+      ],
+    },
+  },
+  type: "action",
+  methods: {},
+  async run({ $ }) {
+    const params = {
+      locationId: this.locationId,
+      state: this.state,
+      tag: this.tag,
+      q: this.q,
+      limit: this.limit,
+      page: this.page,
+      fromDate: this.fromDate,
+      toDate: this.toDate,
+      fromTime: this.fromTime,
+      toTime: this.toTime,
+    };
+    const data = await this.waitwhile.searchCustomers(params);
+    $.export("summary", "Successfully searched for customers");
+    return data;
+  },
+});
