7c8abd6b779a30c92dce057eff7afc249737ab0f
Lucas Caresia <lucascarezia@gmail.com>
2022-10-31 18:34:43 -0300

    Update components/expofp/actions/get-booth/get-booth.mjs
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

--- components/expofp/actions/get-booth/get-booth.mjs
+++ components/expofp/actions/get-booth/get-booth.mjs
@@ -2,7 +2,7 @@ import expofp from "../../expofp.app.mjs";
 
 export default {
   name: "Get Booth",
-  version: "0.0.3",
+  version: "0.0.1",
   key: "expofp-get-booth",
   description: "Get details of a booth. [See docs here](https://expofp.docs.apiary.io/#reference/0/get-booth-details/get-booth-details)",
   type: "action",
