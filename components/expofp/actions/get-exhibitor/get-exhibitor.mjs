37a5bac53d2964294662d3211ed9a62071a769e1
Lucas Caresia <lucascarezia@gmail.com>
2022-10-31 12:09:32 -0300

    Added actions

---
+++ components/expofp/actions/get-exhibitor/get-exhibitor.mjs
@@ -0,0 +1,41 @@
+import expofp from "../../expofp.app.mjs";
+
+export default {
+  name: "Get Exhibitor",
+  version: "0.0.1",
+  key: "expofp-get-exhibitor",
+  description: "Get an exhibitor. [See docs here](https://expofp.docs.apiary.io/#reference/0/get-exhibitor-details/get-exhibitor-details)",
+  type: "action",
+  props: {
+    expofp,
+    eventId: {
+      propDefinition: [
+        expofp,
+        "eventId",
+      ],
+    },
+    exhibitorId: {
+      propDefinition: [
+        expofp,
+        "exhibitorId",
+        (c) => ({
+          eventId: c.eventId,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.expofp.getExhibitor({
+      $,
+      data: {
+        id: this.exhibitorId,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully retrieved exhibitor with ID ${this.exhibitorId}`);
+    }
+
+    return response;
+  },
+};
---
+++ components/expofp/actions/update-booth/update-booth.mjs
@@ -0,0 +1,41 @@
+import expofp from "../../expofp.app.mjs";
+
+export default {
+  name: "Update Booth",
+  version: "0.0.7",
+  key: "expofp-update-booth",
+  description: "Updates a booth. [See docs here](https://expofp.docs.apiary.io/#reference/0/update-booth/update-booth)",
+  type: "action",
+  props: {
+    expofp,
+    eventId: {
+      propDefinition: [
+        expofp,
+        "eventId",
+      ],
+    },
+    boothId: {
+      label: "Booth ID/Name",
+      type: "string",
+      description: "The booth ID or name. E.g. `101` or `A21`",
+    },
+    adminNotes: {
+      label: "Admin Notes",
+      description: "Admin notes for the booth. E.g. `Hidden administrative notes for booth`",
+      type: "string",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.expofp.updateBooth({
+      $,
+      data: {
+        eventId: this.eventId,
+        name: this.boothId,
+        description: this.description,
+      },
+    });
+
+    return response;
+  },
+};
