8085f8e9b5baf5fce4c476687086ca21502aa2cb
Alyson Freitas <alys.freitas@gmail.com>
2022-11-01 00:12:52 -0300

    Fix for aws s3 stream, fixing the streaming of files that don't provide explicit content-length

--- components/aws/actions/s3-stream-file/s3-stream-file.mjs
+++ components/aws/actions/s3-stream-file/s3-stream-file.mjs
@@ -1,3 +1,4 @@
+import fs from "fs";
 import common from "../../common/common-s3.mjs";
 import { toSingleLineString } from "../../common/utils.mjs";
 
@@ -9,7 +10,7 @@ export default {
     Accepts a file URL, and streams the file to the provided S3 bucket/key.
     [See the docs](https://docs.aws.amazon.com/AmazonS3/latest/userguide/upload-objects.html)
   `),
-  version: "0.3.0",
+  version: "0.3.1",
   type: "action",
   props: {
     aws: common.props.aws,
@@ -18,14 +19,42 @@ export default {
     fileUrl: common.props.fileUrl,
     filename: common.props.key,
   },
+  methods: {
+    ...common.methods,
+    async getStreamAndContentLength(fileResponse, absoluteFilePath) {
+      if (fileResponse.headers["content-length"]) {
+        return {
+          stream: fileResponse.data,
+          contentLength: fileResponse.headers["content-length"],
+        };
+      }
+      const fileName = absoluteFilePath.split("/").pop();
+      const tempLocalPath = `/tmp/${fileName}`;
+      await (new Promise((resolve) => {
+        const writeStream = fs.createWriteStream(tempLocalPath);
+        fileResponse.data.pipe(writeStream);
+        writeStream.on("finish", resolve);
+      }));
+      var stats = fs.statSync(tempLocalPath);
+      return {
+        stream: fs.createReadStream(tempLocalPath),
+        contentLength: stats.size,
+      };
+    },
+  },
   async run({ $ }) {
     const fileResponse = await this.streamFile(this.fileUrl);
+    const filePath = this.filename.replace(/^\/+/, "");
+    const {
+      stream,
+      contentLength,
+    } = await this.getStreamAndContentLength(fileResponse, filePath);
     const response = await this.uploadFile({
       Bucket: this.bucket,
-      Key: this.filename.replace(/^\/+/, ""),
-      Body: fileResponse.data,
+      Key: filePath,
+      Body: stream,
       ContentType: fileResponse.headers["content-type"],
-      ContentLength: fileResponse.headers["content-length"],
+      ContentLength: contentLength,
     });
     $.export("$summary", `Streaming file ${this.filename} to ${this.bucket}`);
     return response;
