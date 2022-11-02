4a16f9f6f7dd25dc89285551e0bf70a9f525373a
Andrew Breckenridge <asbreckenridge@me.com>
2022-11-02 21:58:37 +0000

    components/linear: add createAsUser to createIssue

--- components/linear/linear.app.mjs
+++ components/linear/linear.app.mjs
@@ -3,6 +3,15 @@ import linearApp from "../linear_app/linear_app.app.mjs";
 export default {
   ...linearApp,
   app: "linear",
+  propDefitions: {
+    ...linearApp.propDefitions,
+    createAsUser: {
+      propDefinition: [
+        linearApp,
+        "createAsUser",
+      ],
+    },
+  },
   methods: {
     ...linearApp.methods,
     getClientOptions(options = {}) {
