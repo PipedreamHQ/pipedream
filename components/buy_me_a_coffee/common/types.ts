0a94547ba8eec6ed566c8b29a92bf89ab1fd9292
feyzo <feyzullah.metu@gmail.com>
2022-11-03 18:45:23 +0300

    Buy Me A Coffee Sources

---
+++ components/buy_me_a_coffee/common/types.ts
@@ -0,0 +1,16 @@
+import { Pipedream } from "@pipedream/types";
+
+export interface MakeRequestParams {
+  $?: Pipedream;
+  path: string;
+  headers?: object;
+  otherConfig?: object;
+};
+
+export interface ResourceFn {
+  (params:object): Promise<any>;
+};
+
+export interface ResourceGeneratorParams {
+  resourceFn: ResourceFn;
+};
---
+++ components/buy_me_a_coffee/sources/new-member-added/new-member-added.ts
@@ -0,0 +1,30 @@
+import { defineSource } from "@pipedream/types";
+import common from "../common/common";
+
+export default defineSource({
+  ...common,
+  key: "buy_me_a_coffee-new-member-added",
+  name: "New Member Added",
+  description: "Emit new events when a new member was added.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getResourceFn() {
+      return this.app.getMembers;
+    },
+    compareFn(item: any): boolean {
+      return this.getLastCreatedTime() < new Date(item.subscription_created_on).getTime();
+    },
+    getSummary(item: any): string {
+      return `New member ${item.payer_name} duration(${item.subscription_duration_type})`;
+    },
+    getTimeKey(): string {
+      return "subscription_created_on";
+    },
+    getIdKey(): string {
+      return "subscription_id";
+    },
+  },
+});
