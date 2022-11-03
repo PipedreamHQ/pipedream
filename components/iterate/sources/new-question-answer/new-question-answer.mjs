6f76fcb1a0a05ced9887d012da72ead0b30b5c96
Lucas Caresia <lucascarezia@gmail.com>
2022-11-03 18:43:16 -0300

    Iterate #3663 (#4635)
    * Created component and added sources
    
    * pnpm
    
    * Adde common file

---
+++ components/iterate/sources/new-question-answer/new-question-answer.mjs
@@ -0,0 +1,24 @@
+import common from "../common/common.mjs";
+
+export default {
+  ...common,
+  name: "New Question Answer",
+  version: "0.0.1",
+  key: "iterate-new-question-answer",
+  description: "Emit new event when a question is answered.",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    emitEvent(data) {
+      this.$emit(data, {
+        id: data.id,
+        summary: `New question answer with id ${data.id}`,
+        ts: Date.parse(data.updated_at[0]),
+      });
+    },
+    getResources(args = {}) {
+      return this.iterate.getAnswers(args);
+    },
+  },
+};
