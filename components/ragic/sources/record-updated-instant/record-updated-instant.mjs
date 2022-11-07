7ad778fd77aef219a02ebf850b17eec16fc058e9
Andrew Chuang <andrewjschuang@gmail.com>
2022-11-07 12:04:56 -0300

    [Ragic] New Components (#4657)
    * add package.json
    
    * add files
    
    * add webhook sources
    
    * add actions
    
    * add docs
    
    * add sheet prop
    
    * rename props

---
+++ components/ragic/sources/record-updated-instant/record-updated-instant.mjs
@@ -0,0 +1,21 @@
+import base from "../common/webhooks.mjs";
+
+const docLink = "https://www.ragic.com/intl/en/doc-api/33/Webhook-on-Ragic";
+
+export default {
+  ...base,
+  key: "ragic-record-updated-instant",
+  name: "New Updated Record (Instant)",
+  description: `Emit new event when a record is updated. [Instructions on creating webhooks here](${docLink}).`,
+  version: "0.0.1",
+  type: "source",
+  methods: {
+    ...base.methods,
+    isRelevant(recordId) {
+      if (this.isUpdate(recordId)) {
+        return true;
+      }
+      this.addRecord(recordId);
+    },
+  },
+};
