7da0e5f9fb6836c75d277825b917c8bb44740336
Andrew Chuang <andrewjschuang@gmail.com>
2022-11-09 13:44:42 -0300

    add form response submitted source

--- components/transform/transform.app.mjs
+++ components/transform/transform.app.mjs
@@ -1,11 +1,63 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "transform",
-  propDefinitions: {},
+  propDefinitions: {
+    formId: {
+      type: "string",
+      label: "Form ID",
+      description: "The form",
+      async options() {
+        const { result } = await this.getFormIds();
+        return result.map((form) => ({
+          label: form.formname,
+          value: form.formid,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _version() {
+      return "transformAPIVersion1.a5svc";
+    },
+    _baseUrl() {
+      return `https://transform.alphasoftware.com/${this._version()}`;
+    },
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    async _makeRequest({
+      $ = this, path = "", ...opts
+    }) {
+      const response = await axios($, {
+        ...opts,
+        url: `${this._baseUrl()}/${path}`,
+        headers: {
+          ...opts?.headers,
+          apikey: this._apiKey(),
+        },
+      });
+      if (response.error) {
+        throw new Error(response.errorText);
+      }
+      return response;
+    },
+    async getFormIds(opts = {}) {
+      const path = "/GetListOfFormDefinitionsForAccount";
+      return this._makeRequest({
+        ...opts,
+        path,
+      });
+    },
+    async getFormResponses({
+      formId, ...opts
+    }) {
+      const path = `/GetFormDataArrayForFormId/${formId}`;
+      return this._makeRequest({
+        ...opts,
+        path,
+      });
     },
   },
 };
