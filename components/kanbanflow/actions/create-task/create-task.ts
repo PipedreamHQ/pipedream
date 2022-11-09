39b29058593abebbfb9d56b29edee4d306def7af
GTFalcao <gtfalcao96@gmail.com>
2022-11-09 00:46:40 -0300

    KanbanFlow App structure

---
+++ components/kanbanflow/actions/create-task/create-task.ts
@@ -0,0 +1,35 @@
+import quickemailverification from "../../app/quickemailverification.app";
+import { defineAction } from "@pipedream/types";
+import {
+  VerifyEmailParams, VerifyEmailResponse,
+} from "../../common/types";
+
+export default defineAction({
+  name: "Create Task",
+  description:
+    "Verify an email address [See docs here](http://docs.quickemailverification.com/email-verification-api/verify-an-email-address)",
+  key: "kanbanflow-create-task",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    quickemailverification,
+    email: {
+      label: "Email Address",
+      description: "An email address to be verified.",
+      type: "string",
+    },
+  },
+  async run({ $ }): Promise<VerifyEmailResponse> {
+    const params: VerifyEmailParams = {
+      $,
+      params: {
+        email: this.email,
+      },
+    };
+    const data: VerifyEmailResponse = await this.quickemailverification.verifyEmailAddress(params);
+
+    $.export("$summary", `Verified email ${data.email} (${data.result})`);
+
+    return data;
+  },
+});
---
+++ components/kanbanflow/app/kanbanflow.app.ts
@@ -0,0 +1,39 @@
+import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
+import {
+  HttpRequestParams,
+  VerifyEmailParams, VerifyEmailResponse,
+} from "../common/types";
+
+export default defineApp({
+  type: "app",
+  app: "kanbanflow",
+  methods: {
+    _baseUrl(): string {
+      return "https://api.quickemailverification.com/v1";
+    },
+    async _httpRequest({
+      $ = this,
+      endpoint,
+      params,
+    }: HttpRequestParams): Promise<object> {
+      return axios($, {
+        url: this._baseUrl() + endpoint,
+        params: {
+          ...params,
+          apikey: this.$auth.api_key,
+        },
+      });
+    },
+    async verifyEmailAddress(args: VerifyEmailParams): Promise<VerifyEmailResponse> {
+      const response: VerifyEmailResponse = await this._httpRequest({
+        endpoint: "/verify",
+        ...args,
+      });
+
+      if (response.success !== "true") throw new Error(`QuickEmailVerification response: ${response.reason}`);
+
+      return response;
+    },
+  },
+});
