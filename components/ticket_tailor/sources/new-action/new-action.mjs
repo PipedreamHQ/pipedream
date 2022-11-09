25df17cba6b308d40c20fdf5ef9b7f3e06c52c89
Cassiano Vellames <cassiano.v@turing.com>
2022-11-09 10:31:20 -0300

    New Action Source

--- components/ticket_tailor/sources/new-action/new-action.mjs
+++ components/ticket_tailor/sources/new-action/new-action.mjs
@@ -1,4 +1,5 @@
 import app from "../../ticket_tailor.app.mjs";
+import crypto from "crypto";
 
 export default {
   type: "source",
@@ -13,7 +14,7 @@ export default {
     sharedSecret: {
       type: "string",
       label: "Shared Secret",
-      description: "The shared secret you configured in the webhook. We highly recommend you to use a secret key to prevent unauthorized access to your webhook.",
+      description: "The shared secret you configured in the webhook. We highly recommend you to use a secret key to prevent unauthorized access to your webhook. You can find your signing secret [here](https://app.tickettailor.com/box-office/webhooks)",
       optional: true,
     },
   },
@@ -25,16 +26,49 @@ export default {
         ts: event.created_at || Date.now(),
       });
     },
-    checkHmac() {
+    getSignature(event) {
+      const calendlySignature = event.headers["tickettailor-webhook-signature"];
+      return calendlySignature.split(",").reduce((acc, currentValue) => {
+        const [
+          key,
+          value,
+        ] = currentValue.split("=");
+        acc[key] = value;
+        return acc;
+      }, {});
+    },
+    checkHmac(bodyRaw, timestamp, hmac) {
       if (!this.sharedSecret) {
         console.log("No shared secret configured. Skipping HMAC check.");
         return;
       }
+
+      const data = timestamp + bodyRaw;
+      const expectedSignature = crypto.createHmac("sha256", this.sharedSecret)
+        .update(data, "utf8")
+        .digest("hex");
+
+      if (hmac !== expectedSignature) {
+        throw new Error("Invalid HMAC Signature, connection aborted.");
+      }
+    },
+    checkTolerance(timestamp) {
+      const fiveMinutes = 300000;
+      const tolerance = fiveMinutes;
+      const timestampMilliseconds = Number(timestamp) * 1000;
+
+      if (timestampMilliseconds < Date.now() - tolerance) {
+        throw new Error("Invalid Timestamp Signature. The signature's timestamp is outside of the tolerance zone.");
+      }
     },
   },
   async run(event) {
-    console.log(event);
-    this.checkHmac(event);
+    const {
+      t,
+      v1,
+    } = this.getSignature(event);
+    this.checkHmac(event.bodyRaw, t, v1);
+    this.checkTolerance(t);
     this.emit(event.body);
   },
 };
