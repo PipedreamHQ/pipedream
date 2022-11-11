7ea7194c748127e95d3c64da82654f509a03863d
Andrew Chuang <andrewjschuang@gmail.com>
2022-11-11 11:12:53 -0300

    task scheduler separate logic into methods

--- components/pipedream/sources/new-scheduled-tasks/task-scheduler.mjs
+++ components/pipedream/sources/new-scheduled-tasks/task-scheduler.mjs
@@ -7,7 +7,7 @@ export default {
   type: "source",
   description:
     "Exposes an HTTP API for scheduling messages to be emitted at a future time",
-  version: "0.1.0",
+  version: "0.2.0",
   dedupe: "unique", // Dedupe on a UUID generated for every scheduled task
   props: {
     pipedream,
@@ -29,84 +29,106 @@ export default {
   },
   methods: {
     // To schedule future emits, we emit to the selfChannel of the component
-    selfChannel() { return "self"; },
+    selfChannel() {
+      return "self";
+    },
     // Queue for future emits that haven't yet been delivered
-    queuedEventsChannel() { return "$in"; },
-  },
-  async run(event) {
-    const {
-      body,
-      headers,
-      path,
-      $channel,
-      $id,
-    } = event;
-
-    const componentId = process.env.PD_COMPONENT;
-    const selfChannel = this.selfChannel();
-    const inChannel = this.queuedEventsChannel();
-
-    // Subscribe the component to itself. We do this here because even in
-    // the activate hook, the component isn't available to take subscriptions.
-    // Scheduled tasks are sent to the self channel, which emits the message at
-    // the specified delivery_ts to this component.
-    const isSubscribedToSelf = this.db.get("isSubscribedToSelf");
-    if (!isSubscribedToSelf) {
-      console.log(`Subscribing to ${selfChannel} channel for event source`);
-      console.log(
-        await this.pipedream.subscribe(componentId, componentId, selfChannel),
-      );
-      this.db.set("isSubscribedToSelf", true);
-    }
-
-    // SCHEDULE NEW TASK
-    if (path === "/schedule") {
-      const {
-        timestamp,
-        message,
-      } = body;
+    queuedEventsChannel() {
+      return "$in";
+    },
+    httpRespond({
+      status, body,
+    }) {
+      this.http.respond({
+        headers: {
+          "content-type": "application/json",
+        },
+        status,
+        body,
+      });
+    },
+    async selfSubscribe() {
+      // Subscribe the component to itself. We do this here because even in
+      // the activate hook, the component isn't available to take subscriptions.
+      // Scheduled tasks are sent to the self channel, which emits the message at
+      // the specified delivery_ts to this component.
+      const isSubscribedToSelf = this.db.get("isSubscribedToSelf");
+      if (!isSubscribedToSelf) {
+        const componentId = process.env.PD_COMPONENT;
+        const selfChannel = this.selfChannel();
+        console.log(`Subscribing to ${selfChannel} channel for event source`);
+        console.log(
+          await this.pipedream.subscribe(componentId, componentId, selfChannel),
+        );
+        this.db.set("isSubscribedToSelf", true);
+      }
+    },
+    validateEventBody(event, operation) {
       const errors = [];
-
-      // timestamp should be an ISO 8601 string. Parse and check
-      // for validity below.
-      const epoch = Date.parse(timestamp);
 
       // Secrets are optional, so we first check if the user configured
       // a secret, then check its value against the prop (validation below)
-      if (this.secret && headers["x-pd-secret"] !== this.secret) {
+      if (this.secret && event.headers["x-pd-secret"] !== this.secret) {
         errors.push(
           "Secret on incoming request doesn't match the configured secret",
         );
       }
-      if (!timestamp) {
-        errors.push(
-          "No timestamp included in payload. Please provide an ISO8601 timestamp in the 'timestamp' field",
-        );
-      }
-      if (timestamp && !epoch) {
-        errors.push("Timestamp isn't a valid ISO 8601 string");
-      }
-      if (!message) {
-        errors.push("No message passed in payload");
-      }
-      if (errors.length) {
-        console.log(errors);
-        this.http.respond({
-          status: 400,
-          body: {
-            errors,
-          },
-          headers: {
-            "content-type": "application/json",
-          },
-        });
-        return;
+
+      if (operation === "schedule") {
+        const {
+          timestamp,
+          message,
+        } = event.body;
+        // timestamp should be an ISO 8601 string. Parse and check for validity below.
+        const epoch = Date.parse(timestamp);
+
+        if (!timestamp) {
+          errors.push(
+            "No timestamp included in payload. Please provide an ISO8601 timestamp in the 'timestamp' field",
+          );
+        }
+        if (timestamp && !epoch) {
+          errors.push("Timestamp isn't a valid ISO 8601 string");
+        }
+        if (!message) {
+          errors.push("No message passed in payload");
+        }
       }
 
+      return errors;
+    },
+    scheduleTask(event) {
+      const errors = this.validateEventBody(event, "schedule");
+      let status, body;
+
+      if (errors.length) {
+        console.log(errors);
+        status = 400;
+        body = {
+          errors,
+        };
+      } else {
+        const id = this.emitScheduleEvent(event);
+        status = 200;
+        body = {
+          msg: "Successfully scheduled task",
+          id,
+        };
+      }
+
+      this.httpRespond({
+        status,
+        body,
+      });
+    },
+    emitScheduleEvent(event) {
+      const selfChannel = this.selfChannel();
+      const epoch = Date.parse(event.body.timestamp);
       const $id = uuid();
+
       this.$emit(
         {
-          ...body,
+          ...event.body,
           $channel: selfChannel,
           $id,
         },
@@ -117,108 +139,100 @@ export default {
         },
       );
 
-      this.http.respond({
-        status: 200,
-        body: {
-          msg: "Successfully scheduled task",
-          id: $id,
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      return $id;
+    },
+    async cancelTask(event) {
+      const errors = this.validateEventBody(event, "cancel");
+      let status, msg;
 
-      return;
-    }
-
-    // CANCEL SCHEDULED TASK
-    // The user must pass a scheduled event UUID they'd like to cancel
-    // We lookup the event by ID and delete it
-    if (path === "/cancel") {
-      const { id } = body;
-
-      if (this.secret && headers["x-pd-secret"] !== this.secret) {
-        this.http.respond({
-          status: 400,
-          body: {
-            msg: "Secret on incoming request doesn't match the configured secret",
-          },
-          headers: {
-            "content-type": "application/json",
-          },
-        });
-        return;
-      }
-
-      let msg, status;
-      try {
-        // List events in the $in channel - the queue of
-        // scheduled events, to be emitted in the future
-        const events = await this.pipedream.listEvents(
-          componentId,
-          inChannel,
-        );
-        console.log("Events: ", events);
-
-        // Find the event in the list by id
-        const eventToCancel = events.data.find((e) => {
-          const { metadata } = e;
-          return metadata.id === id;
-        });
-
-        console.log("Event to cancel: ", eventToCancel);
-
-        if (!eventToCancel) {
-          console.log(`No event with ${id} found`);
-          this.http.respond({
-            status: 404,
-            body: {
-              msg: `No event with ${id} found`,
-            },
-          });
-          return;
+      if (errors.length) {
+        console.log(errors);
+        status = 400;
+        msg = "Secret on incoming request doesn't match the configured secret";
+      } else {
+        try {
+          const id = event.body.id;
+          const isCanceled = await this.deleteEvent(event);
+          if (isCanceled) {
+            status = 200;
+            msg = `Cancelled scheduled task for event ${id}`;
+          } else {
+            status = 404;
+            msg = `No event with ${id} found`;
+          }
+        } catch (error) {
+          console.log(error);
+          status = 500;
+          msg = "Failed to schedule task. Please see the logs";
         }
-
-        // Delete the event
-        console.log(
-          await this.pipedream.deleteEvent(
-            componentId,
-            eventToCancel.id,
-            inChannel,
-          ),
-        );
-        status = 200;
-        msg = `Cancelled scheduled task for event ${id}`;
-      } catch (err) {
-        status = 500;
-        msg = "Failed to schedule task. Please see the logs";
-        console.log(err);
       }
 
-      this.http.respond({
+      this.httpRespond({
         status,
         body: {
           msg,
         },
-        headers: {
-          "content-type": "application/json",
-        },
+      });
+    },
+    async deleteEvent(event) {
+      const componentId = process.env.PD_COMPONENT;
+      const inChannel = this.queuedEventsChannel();
+
+      // The user must pass a scheduled event UUID they'd like to cancel
+      // We lookup the event by ID and delete it
+      const { id } = event.body;
+
+      // List events in the $in channel - the queue of scheduled events, to be emitted in the future
+      const events = await this.pipedream.listEvents(
+        componentId,
+        inChannel,
+      );
+      console.log("Events: ", events);
+
+      // Find the event in the list by id
+      const eventToCancel = events.data.find((e) => {
+        const { metadata } = e;
+        return metadata.id === id;
       });
 
-      return;
-    }
+      console.log("Event to cancel: ", eventToCancel);
 
-    // INCOMING SCHEDULED EMIT
-    if ($channel === selfChannel) {
-      // Delete the channel name and id from the incoming event,
-      // which were used only as metadata
+      if (!eventToCancel) {
+        console.log(`No event with ${id} found`);
+        return false;
+      }
+
+      // Delete the event
+      await this.pipedream.deleteEvent(
+        componentId,
+        eventToCancel.id,
+        inChannel,
+      );
+      return true;
+    },
+    emitEvent(event, summary) {
+      // Delete the channel name and id from the incoming event, which were used only as metadata
+      const id = event.$id;
       delete event.$channel;
       delete event.$id;
+
       this.$emit(event, {
-        summary: JSON.stringify(event),
-        id: $id,
+        summary: summary ?? JSON.stringify(event),
+        id,
         ts: +new Date(),
       });
+    },
+  },
+  async run(event) {
+    await this.selfSubscribe();
+
+    const { path } = event;
+    if (path === "/schedule") {
+      this.scheduleTask(event);
+    } else if (path === "/cancel") {
+      await this.cancelTask(event);
+    } else if (event.$channel === this.selfChannel()) {
+      this.emitEvent(event);
     }
   },
 };
