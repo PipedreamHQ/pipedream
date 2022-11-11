8c6df22a335cc1fd3b91813dd23482769371bf4c
Lucas Caresia <lucascarezia@gmail.com>
2022-11-11 12:04:40 -0300

    Airbrake #3546 (#4706)
    * Added sources
    
    * pnpm
    
    * Added docs link
    
    * Fix event message
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/airbrake/sources/new-error-occurred/new-error-occurred.mjs
@@ -0,0 +1,78 @@
+import airbrake from "../../airbrake.app.mjs";
+
+export default {
+  name: "New Error Occurred",
+  version: "0.0.1",
+  key: "airbrake-new-error-occurred",
+  description: "Emit new event for each error occurred. [See docs here](https://docs.airbrake.io/docs/devops-tools/api/#list-notices-v4)",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    airbrake,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      static: {
+        intervalSeconds: 15 * 60, // 15 minutes
+      },
+    },
+    projectId: {
+      propDefinition: [
+        airbrake,
+        "projectId",
+      ],
+    },
+  },
+  methods: {
+    emitEvent(data) {
+      this._setLastNoticeId(data.id);
+
+      this.$emit(data, {
+        id: data.id,
+        summary: `New error with id ${data.id}`,
+        ts: Date.parse(data.created_at),
+      });
+    },
+    _setLastNoticeId(id) {
+      this.db.set("lastNoticeId", id);
+    },
+    _getLastNoticeId() {
+      return this.db.get("lastNoticeId");
+    },
+  },
+  hooks: {
+    async deploy() {
+      const { notices } = await this.airbrake.getProjectNotices({
+        projectId: this.projectId,
+        params: {
+          page: 1,
+        },
+      });
+
+      notices.slice(10).reverse()
+        .forEach(this.emitEvent);
+    },
+  },
+  async run() {
+    const lastNoticeId = this._getLastNoticeId();
+
+    let page = 1;
+
+    while (page >= 0) {
+      const { notices } = await this.airbrake.getProjectNotices({
+        projectId: this.projectId,
+        params: {
+          page,
+        },
+      });
+
+      notices.reverse().forEach(this.emitEvent);
+
+      if (notices.filter((notice) => notice.id === lastNoticeId)) {
+        return;
+      }
+
+      page++;
+    }
+  },
+};
