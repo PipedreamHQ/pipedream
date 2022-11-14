3747b1ec9813e69661276ba7b8473396c17ab9aa
Lucas Caresia <lucascarezia@gmail.com>
2022-11-14 11:33:08 -0300

    Firsts components

---
+++ components/dev_to/sources/new-reading-list-item/new-reading-list-item.mjs
@@ -0,0 +1,32 @@
+import devTo from "../../dev_to.app.mjs";
+import moment from "moment";
+import common from "../common.mjs";
+
+export default {
+  name: "New reading list item",
+  key: "dev_to-new-reading-list-item",
+  description:
+    "Emit new event for each new reading list item on your Dev.to account",
+  type: "source",
+  version: "0.0.2",
+  props: {
+    ...common.props,
+    devTo,
+  },
+  dedupe: "greatest",
+  async run({ $ }) {
+    const data = await this.devTo.getReadingList($, {
+      params: {
+        per_page: 100,
+      },
+    });
+
+    data.forEach((event) => {
+      this.$emit(event, {
+        id: event.id,
+        ts: moment(event.created_at).valueOf(),
+        summary: event.article.title,
+      });
+    });
+  },
+};
