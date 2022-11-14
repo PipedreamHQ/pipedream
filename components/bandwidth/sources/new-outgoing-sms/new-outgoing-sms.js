f0830f92982cebe3776ad1da4e9fb1f7d8fbe1f3
Lucas Caresia <lucascarezia@gmail.com>
2022-11-14 20:15:56 -0300

    Add some components sources to redeploy - 6

--- components/bandwidth/sources/new-outgoing-sms/new-outgoing-sms.js
+++ components/bandwidth/sources/new-outgoing-sms/new-outgoing-sms.js
@@ -1,15 +1,16 @@
-const bandwidth = require('../../bandwidth.app');
+const bandwidth = require("../../bandwidth.app");
 
 module.exports = {
-  name: 'New Outgoing SMS',
+  name: "New Outgoing SMS",
   description:
-    'Emits an event each time an outbound message status event is received at the source url',
-  key: 'bandwidth-new-ourgoing-sms',
-  version: '1.1.1',
+    "Emits an event each time an outbound message status event is received at the source url",
+  key: "bandwidth-new-outgoing-sms",
+  version: "1.1.1",
+  type: "source",
   props: {
     bandwidth,
     http: {
-      type: '$.interface.http',
+      type: "$.interface.http",
       customResponse: true,
     },
   },
@@ -20,7 +21,7 @@ module.exports = {
       status: 204,
     });
 
-    if (messageBody.message.direction == 'out') {
+    if (messageBody.message.direction == "out") {
       this.$emit(messageBody, {
         summary: messageBody.type,
         id: messageBody.message.id,
