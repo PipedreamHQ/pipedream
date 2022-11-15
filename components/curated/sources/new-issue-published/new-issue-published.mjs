309997ac81507181b548344ec6324bb9cebfaf95
Lucas Caresia <lucascarezia@gmail.com>
2022-11-15 19:24:06 -0300

    Added actions and sources

---
+++ components/curated/sources/new-issue-published/new-issue-published.mjs
@@ -0,0 +1,47 @@
+import common from "../common/common.mjs";
+
+export default {
+  ...common,
+  name: "New Issue Published",
+  version: "0.0.1",
+  key: "curated-new-issue-published",
+  description: "Emit new event when a issue is published.",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    publicationId: {
+      propDefinition: [
+        common.props.curated,
+        "publicationId",
+      ],
+    },
+  },
+  methods: {
+    ...common.methods,
+    emitEvent(data) {
+      if (!data.published_at) {
+        return;
+      }
+
+      this.$emit(data, {
+        id: data.id,
+        summary: `New issue published with id ${data.id}`,
+        ts: Date.parse(data.published_at),
+      });
+    },
+    async getResources(args = {}) {
+      const {
+        page, data,
+      } = await this.curated.getIssues({
+        ...args,
+        publicationId: this.publicationId,
+      });
+
+      return {
+        currentPage: page,
+        resources: data.filter((issue) => !!issue.published),
+      };
+    },
+  },
+};
