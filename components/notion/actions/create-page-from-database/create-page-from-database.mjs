eac83ec447e4361f7535199a29ae58f8bdc3779f
michelle0927 <michelle0927@users.noreply.github.com>
2022-11-16 15:23:49 -0500

    Notion: Duplicate Page action & update Append Block to handle all block types (#4737)
    * duplicate-page & update append-block
    
    * createGetFormattedBlocks method
    
    * update summary
    
    * duplicate title, cover & icon
    
    * package.json version
    
    * add url to summary

--- components/notion/actions/create-page-from-database/create-page-from-database.mjs
+++ components/notion/actions/create-page-from-database/create-page-from-database.mjs
@@ -7,7 +7,7 @@ export default {
   key: "notion-create-page-from-database",
   name: "Create Page from Database",
   description: "Creates a page from a database. [See the docs](https://developers.notion.com/reference/post-page)",
-  version: "0.1.2",
+  version: "0.1.3",
   type: "action",
   props: {
     notion,
