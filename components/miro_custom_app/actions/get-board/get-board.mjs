2950483d8ee7a89c4741e0f9ec36aa5cf2cfa797
William Bishop <william.b@miro.com>
2022-11-16 17:40:04 +0100

    Create get-board.mjs

---
+++ components/miro_custom_app/actions/get-board/get-board.mjs
@@ -0,0 +1,33 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  name: "Get Board",
+  version: "0.0.1",
+  key: "get-board",
+  description: "Returns a Miro board",
+  type: "action",
+  props: {
+    miro_custom_app: {
+      type: "app",
+      app: "miro_custom_app"
+    },
+    boardId: {
+      type: "string",
+      description: "Board ID",
+      optional: false,
+    }
+  },
+  async run({ $ }) {
+    //See the API docs developers.miro.com
+      const config = {
+        method: "get",
+        url: `https://api.miro.com/v2/boards/${this.boardId}`,
+        headers: {
+          "Authorization": `Bearer ${this.miro_custom_app.$auth.access_token}`,
+          "Content-Type": "application/json",
+        },
+      };
+      //console.log(this.miro_custom_app.$auth)
+      return await axios($, config);
+    },
+  };
