af692237db85eea4f49db001becaff4feb6c598e
GTFalcao <gtfalcao96@gmail.com>
2022-11-20 14:43:48 -0300

    Adjusting version numbers

--- components/linear/actions/create-issue/create-issue.mjs
+++ components/linear/actions/create-issue/create-issue.mjs
@@ -7,14 +7,13 @@ const { linearApp } = appProps;
 
 /* eslint-disable pipedream/required-properties-type */
 /* eslint-disable pipedream/required-properties-name */
-/* eslint-disable pipedream/required-properties-version */
 
 export default {
   ...createIssue,
   key: "linear-create-issue",
   description:
     "Create an issue (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/working-with-the-graphql-api#creating-and-editing-issues)",
-  version: "0.3.11",
+  version: "0.4.0",
   props: {
     ...appProps,
     useOwnUser: {
--- components/linear/actions/update-issue/update-issue.mjs
+++ components/linear/actions/update-issue/update-issue.mjs
@@ -7,14 +7,13 @@ const { linearApp } = appProps;
 
 /* eslint-disable pipedream/required-properties-type */
 /* eslint-disable pipedream/required-properties-name */
-/* eslint-disable pipedream/required-properties-version */
 
 export default {
   ...updateIssue,
   key: "linear-update-issue",
   description:
     "Update an issue (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/working-with-the-graphql-api#creating-and-editing-issues)",
-  version: "0.0.6",
+  version: "0.1.0",
   props: {
     ...appProps,
     useOwnUser: {
