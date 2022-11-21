c3fb11cf850427903786ec2f4d18e15f23ac6f65
Jorge Cortes <jacortesmahmud@gmail.com>
2022-11-21 12:53:30 -0500

    [ACTION] Added new WooCommerce action create-customer

---
+++ components/woocommerce/actions/create-customer/create-customer.mjs
@@ -0,0 +1,78 @@
+import woocommerce from "../../woocommerce.app.mjs";
+
+export default {
+  key: "woocommerce-create-customer",
+  name: "Create Customer",
+  description: "Creates a new customer. [See the docs](https://woocommerce.github.io/woocommerce-rest-api-docs/?javascript#create-a-customer)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    woocommerce,
+    email: {
+      propDefinition: [
+        woocommerce,
+        "email",
+      ],
+      description: "The email address for the customer.",
+      optional: false,
+    },
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "Customer first name.",
+      optional: true,
+    },
+    lastName: {
+      type: "string",
+      label: "Last Name",
+      description: "Customer last name.",
+      optional: true,
+    },
+    username: {
+      type: "string",
+      label: "Username",
+      description: "Customer login name.",
+      optional: true,
+    },
+    password: {
+      type: "string",
+      label: "Password",
+      description: "Customer password.",
+      optional: true,
+    },
+    isPayingCustomer: {
+      type: "string",
+      label: "Is Paying Customer",
+      description: "Is the customer a paying customer?",
+      optional: true,
+    },
+  },
+  methods: {
+    createCustomer(data) {
+      return this.woocommerce.postResource("customers", data);
+    },
+  },
+  async run({ $ }) {
+    const {
+      email,
+      firstName,
+      lastName,
+      username,
+      password,
+      isPayingCustomer,
+    } = this;
+
+    const res = await this.createCustomer({
+      email,
+      first_name: firstName,
+      last_name: lastName,
+      username,
+      password,
+      is_paying_customer: isPayingCustomer,
+    });
+
+    $.export("$summary", `Successfully created customer ID: ${res.id}`);
+
+    return res;
+  },
+};
