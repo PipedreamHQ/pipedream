4d2eb06f87c9e570c8d87b192422a4bf42a3cf79
Andrew Chuang <andrewjschuang@gmail.com>
2022-11-22 17:37:44 -0300

    add booking with state source

---
+++ components/timekit/sources/booking-with-state/booking-with-state.mjs
@@ -0,0 +1,95 @@
+import timekit from "../../timekit.app.mjs";
+import crypto from "crypto";
+
+export default {
+  key: "timekit-booking-with-state",
+  name: "New Booking with State",
+  description: "Emit new event when a booking has a specific state",
+  version: "0.0.1",
+  type: "source",
+  props: {
+    timekit,
+    db: "$.service.db",
+    http: "$.interface.http",
+    graph: {
+      propDefinition: [
+        timekit,
+        "graph",
+      ],
+    },
+    state: {
+      propDefinition: [
+        timekit,
+        "state",
+        (c) => ({
+          graph: c.graph,
+        }),
+      ],
+    },
+    secret: {
+      type: "string",
+      label: "Secret",
+      description: "Configured secret to verify webhook events. If left blank, no verification will be done.",
+      secret: true,
+      optional: true,
+    },
+  },
+  hooks: {
+    async activate() {
+      console.log("Creating webhook...");
+      const response = await this.timekit.createWebhook({
+        data: {
+          url: this.http.endpoint,
+          graph: this.graph,
+          state: this.state,
+          method: "post",
+        },
+      });
+      const id = response.meta.message.split("id ")[1];
+      this._setWebhookId(id);
+    },
+    async deactivate() {
+      const id = this._getWebhookId();
+      await this.timekit.deleteWebhook({
+        id,
+      });
+      console.log(`Webhook ${id} was deleted successfully`);
+    },
+  },
+  methods: {
+    _getWebhookId() {
+      return this.db.get("webhookId");
+    },
+    _setWebhookId(id) {
+      this.db.set("webhookId", id);
+    },
+    verifySecret(payload, header) {
+      if (this.secret) {
+        const hash = crypto
+          .createHmac("sha256", this.secret)
+          .update(payload)
+          .digest("hex");
+        return hash === header;
+      }
+      return true;
+    },
+  },
+  async run(event) {
+    if (!this.verifySecret(event.bodyRaw, event.headers["x-timekit-signature"])) {
+      console.log("Secret verification has failed for the incoming event. Skipping...");
+      return;
+    }
+
+    const {
+      id,
+      description,
+      updated_at: ts,
+    } = event.body;
+
+    this.$emit(event.body, {
+      id,
+      summary: `New ${this.graph} booking ${this.state}: ${description}`,
+      ts,
+    });
+  },
+};
--- components/timekit/timekit.app.mjs
+++ components/timekit/timekit.app.mjs
@@ -1,9 +1,34 @@
-import { axios } from "@pipedream/platform";
+import {
+  axios,
+  ConfigurationError,
+} from "@pipedream/platform";
+import constants from "./sources/common/constants.mjs";
 
 export default {
   type: "app",
   app: "timekit",
-  propDefinitions: {},
+  propDefinitions: {
+    graph: {
+      type: "string",
+      label: "Graph Type",
+      description: "Graph type according to [this link](https://help.timekit.io/en/articles/1389944-introduction-to-booking-graphs)",
+      options: constants.graphs,
+    },
+    state: {
+      type: "string",
+      label: "Booking State",
+      async options({ graph }) {
+        if (graph === "instant") return constants.states.INSTANT;
+        if (graph === "instant_payment") return constants.states.INSTANT_PAYMENT;
+        if (graph === "confirm_decline") return constants.states.CONFIRM_DECLINE;
+        if (graph === "group_owner") return constants.states.GROUP_OWNER;
+        if (graph === "group_customer") return constants.states.GROUP_CUSTOMER;
+        if (graph === "group_customer_payment") return constants.states.GROUP_CUSTOMER_PAYMENT;
+        if (graph === "reservation") return constants.states.RESERVATION;
+        throw new ConfigurationError(`Graph type **${graph}** is not supported`);
+      },
+    },
+  },
   methods: {
     _auth() {
       return {
@@ -53,9 +78,39 @@ export default {
         }
       }
     },
+    async createWebhook(opts = {}) {
+      return this.makeRequestOrPaginate({
+        path: "/webhooks",
+        method: "post",
+        ...opts,
+      });
+    },
+    async deleteWebhook({
+      id, ...opts
+    }) {
+      return this.makeRequestOrPaginate({
+        path: `/webhooks/${id}`,
+        method: "delete",
+        ...opts,
+      });
+    },
     async getCurrentApp(opts = {}) {
       return this.makeRequestOrPaginate({
         path: "/app",
+        ...opts,
+      });
+    },
+    async getProject({
+      id, ...opts
+    }) {
+      return this.makeRequestOrPaginate({
+        path: `/projects/${id}`,
+        ...opts,
+      });
+    },
+    async listProjects(opts = {}) {
+      return this.makeRequestOrPaginate({
+        path: "/projects",
         ...opts,
       });
     },
