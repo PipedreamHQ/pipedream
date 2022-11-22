41bebc3ddae79e9ea270f508e8cd8aec96564b96
Andrew Chuang <andrewjschuang@gmail.com>
2022-11-22 10:14:44 -0300

    init

--- components/timekit/timekit.app.mjs
+++ components/timekit/timekit.app.mjs
@@ -1,11 +1,31 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "timekit",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _auth() {
+      return {
+        username: "",
+        password: this.$auth.api_key,
+      };
+    },
+    _baseUrl() {
+      return "https://api.timekit.io/v2";
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        ...opts,
+        url: `${this._baseUrl()}` + path,
+        auth: this._auth(),
+        headers: {
+          ...opts.headers,
+          "Content-Type": "application/json",
+        },
+      });
     },
   },
 };
