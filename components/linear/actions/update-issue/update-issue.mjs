ae320df6bd45eb1c3a22dfd6d12118b69121057c
GTFalcao <gtfalcao96@gmail.com>
2022-11-23 18:58:43 -0300

    Reverting changes to 'update issue' action

--- components/linear/actions/update-issue/update-issue.mjs
+++ components/linear/actions/update-issue/update-issue.mjs
@@ -1,71 +1,14 @@
 import updateIssue from "../../../linear_app/actions/update-issue/update-issue.mjs";
 import utils from "../../common/utils.mjs";
-import common from "../../common/createOrUpdateIssue.mjs";
-
-const appProps = utils.getAppProps(updateIssue).props;
-const { linearApp } = appProps;
 
 /* eslint-disable pipedream/required-properties-type */
 /* eslint-disable pipedream/required-properties-name */
 
 export default {
   ...updateIssue,
-  ...common,
+  ...utils.getAppProps(updateIssue),
   key: "linear-update-issue",
-  description:
-    "Update an issue (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/working-with-the-graphql-api#creating-and-editing-issues)",
-  version: "0.1.0",
-  props: {
-    ...appProps,
-    useOwnUser: {
-      propDefinition: [
-        linearApp,
-        "useOwnUser",
-      ],
-    },
-  },
-  async run({ $ }) {
-    const {
-      issueId,
-      title,
-      description,
-      teamId,
-      assigneeId,
-      useOwnUser,
-      createAsUser,
-      displayIconUrl,
-    } = this;
-
-    const params = {
-      issueId,
-      input: {
-        teamId,
-        title,
-        description,
-        assigneeId,
-        createAsUser,
-        displayIconUrl,
-      },
-    };
-
-    if (useOwnUser) {
-      const {
-        avatarUrl, displayName,
-      } = await this.linearApp.getOwnUserInfo();
-      params.createAsUser = displayName;
-      if (avatarUrl) params.displayIconUrl = avatarUrl;
-    }
-    try {
-      const response = await this.linearApp.updateIssue(params);
-
-      const summary = response.success
-        ? `Updated issue ${response._issue.id}`
-        : "Failed to update issue";
-      $.export("$summary", summary);
-
-      return response;
-    } catch (err) {
-      this.checkOutdatedAuthError(err);
-    }
-  },
+  description: "Update an issue (OAuth). See the docs [here](https://developers.linear.app/docs/graphql/working-with-the-graphql-api#creating-and-editing-issues)",
+  version: "0.0.6",
 };
+
