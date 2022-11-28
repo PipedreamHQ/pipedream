f9fe3534a8ee1bfa7e0359dcb884067b30054c38
michelle0927 <michelle.bergeron@gmail.com>
2022-11-28 17:21:30 -0500

    refactoring legacy actions

--- components/smugmug/smugmug.app.mjs
+++ components/smugmug/smugmug.app.mjs
@@ -1,11 +1,84 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "smugmug",
-  propDefinitions: {},
+  propDefinitions: {
+    album: {
+      type: "string",
+      label: "Album Key",
+      description: "Album Key of the album to retrieve",
+      async options({ page }) {
+        const user = await this.getAuthenticatedUser();
+        const uri = user.Response.User.Uri;
+        const count = 10;
+        const params = {
+          Scope: uri,
+          count,
+          start: (page - 1) * count,
+        };
+        const response = await this.searchAlbums({
+          params,
+        });
+        const albums = response?.Response?.Album;
+        return albums?.length > 0
+          ? albums.map((album) => ({
+            label: album.Name,
+            value: album.AlbumKey,
+          }))
+          : [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://www.smugmug.com/api/v2";
+    },
+    _headers() {
+      return {
+        Accept: "application/json",
+      };
+    },
+    _signature() {
+      return {
+        token: {
+          key: this.$auth.oauth_access_token,
+          secret: this.$auth.oauth_refresh_token,
+        },
+        oauthSignerUri: this.$auth.oauth_signer_uri,
+      };
+    },
+    async _makeRequest(args = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherArgs
+      } = args;
+      const config = {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...otherArgs,
+      };
+      const signature = this._signature(); console.log(config);
+      return axios($, config, signature);
+    },
+    async getAuthenticatedUser(args = {}) {
+      return this._makeRequest({
+        path: "!authuser",
+        ...args,
+      });
+    },
+    async getAlbum(albumKey, args = {}) {
+      return this._makeRequest({
+        path: `/album/${albumKey}`,
+        ...args,
+      });
+    },
+    async searchAlbums(args = {}) {
+      return this._makeRequest({
+        path: "/album!search",
+        ...args,
+      });
     },
   },
 };
