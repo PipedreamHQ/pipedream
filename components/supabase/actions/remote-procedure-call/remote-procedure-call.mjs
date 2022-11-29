860b559cc11414bbdf6791f7c8e87f4dce8e5487
michelle0927 <michelle0927@users.noreply.github.com>
2022-11-29 11:17:31 -0500

    Supabase - new action components (#4838)
    * wip
    
    * wip
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * Undo changes to create-or-update-file-contents
    
    * Undo changes to github.app.mjs

---
+++ components/supabase/actions/remote-procedure-call/remote-procedure-call.mjs
@@ -0,0 +1,30 @@
+import supabase from "../../supabase.app.mjs";
+
+export default {
+  key: "supabase-remote-procedure-call",
+  name: "Remote Procedure Call",
+  type: "action",
+  version: "0.0.1",
+  description: "Call a Postgres function in a database. [See the docs here](https://supabase.com/docs/reference/javascript/rpc)",
+  props: {
+    supabase,
+    functionName: {
+      type: "string",
+      label: "Function Name",
+      description: "The function name to call",
+    },
+    args: {
+      type: "object",
+      label: "Arguments",
+      description: "The arguments to pass to the function call",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.supabase.remoteProcedureCall(this.functionName, this.args);
+    if (response) {
+      $.export("$summary", `Successfully executed remote procedure call ${this.functionName}`);
+    }
+    return response;
+  },
+};
---
+++ components/supabase/actions/upsert-row/upsert-row.mjs
@@ -0,0 +1,32 @@
+import supabase from "../../supabase.app.mjs";
+
+export default {
+  key: "supabase-upsert-row",
+  name: "Upsert Row",
+  type: "action",
+  version: "0.0.1",
+  description: "Updates a row in a database or inserts new row if not found. [See the docs here](https://supabase.com/docs/reference/javascript/upsert)",
+  props: {
+    supabase,
+    table: {
+      propDefinition: [
+        supabase,
+        "table",
+      ],
+      description: "Name of the table to upsert row",
+    },
+    data: {
+      propDefinition: [
+        supabase,
+        "data",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.supabase.upsertRow(this.table, this.data);
+    if (response) {
+      $.export("$summary", `Successfully upserted row into table ${this.table}`);
+    }
+    return response;
+  },
+};
