9c93718e5fa30ff4e98287753e7f234c1195078f
Alyson Freitas <alys.freitas@gmail.com>
2022-11-29 17:29:58 -0300

    envoy new action: create-an-invite

--- components/envoy/envoy.app.mjs
+++ components/envoy/envoy.app.mjs
@@ -3,17 +3,63 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "envoy",
-  propDefinitions: {},
-  methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-      console.log(this.$auth);
+  propDefinitions: {
+    expectedArrivalAt: {
+      type: "string",
+      label: "Arrival date",
+      description: "A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the date-time format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar.",
     },
+    locationId: {
+      type: "string",
+      label: "Location",
+      description: "Unique identifier of a location.",
+      async options() {
+        return this.listEnabledLocationsOpts();
+      },
+    },
+    visitorName: {
+      type: "string",
+      label: "Name",
+      description: "Visitor full name.",
+    },
+    visitorEmail: {
+      type: "string",
+      label: "Email",
+      description: "Visitor email address.",
+    },
+    expectedDepartureAt: {
+      type: "string",
+      label: "Departure date",
+      description: "A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the date-time format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar.",
+      optional: true,
+    },
+    employeeId: {
+      type: "string",
+      label: "Host",
+      description: "Unique identifier of an employee.",
+      async options() {
+        return this.listEmployeesOps();
+      },
+      optional: true,
+    },
+    notes: {
+      type: "string",
+      label: "Private notes",
+      description: "Enter your private notes.",
+      optional: true,
+    },
+    sendEmailToInvitee: {
+      type: "boolean",
+      label: "Send email to invitee",
+      description: "Either it should or not send email to invitee",
+      optional: true,
+    },
+  },
+  methods: {
     _getBaseUrl() {
       return "https://api.envoy.com/v1";
     },
     _getHeaders() {
-      this.authKeys();
       return {
         "content-type": "application/json",
         "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
@@ -26,11 +72,42 @@ export default {
         headers: this._getHeaders(),
       };
     },
+    async listEnabledLocationsOpts(ctx = this) {
+      const result = await axios(ctx, this._getRequestParams({
+        method: "GET",
+        path: "/locations",
+        params: {
+          enabled: true,
+        },
+      }));
+      return result.data.map((location) => ({
+        label: location.name,
+        value: location.id,
+      }));
+    },
+    async listEmployeesOps(ctx = this) {
+      const result = await axios(ctx, this._getRequestParams({
+        method: "GET",
+        path: "/employees",
+      }));
+      return result.data.map((location) => ({
+        label: location.name,
+        value: location.id,
+      }));
+    },
     async listInvites(ctx = this, params) {
       const result = await axios(ctx, this._getRequestParams({
         method: "GET",
         path: "/invites",
-        params: params,
+        params,
+      }));
+      return result;
+    },
+    async createInvite(ctx = this, data) {
+      const result = await axios(ctx, this._getRequestParams({
+        method: "POST",
+        path: "/invites",
+        data,
       }));
       return result;
     },
@@ -51,7 +128,7 @@ export default {
       const result = await axios(ctx, this._getRequestParams({
         method: "GET",
         path: "/entries",
-        params: params,
+        params,
       }));
       return result;
     },
