a8a9ca25ee8eafcb48024425af3e3e2e24a00e73
michelle0927 <michelle.bergeron@gmail.com>
2022-12-06 14:15:23 -0500

    new get-assignment action

--- components/google_classroom/google_classroom.app.mjs
+++ components/google_classroom/google_classroom.app.mjs
@@ -1,11 +1,103 @@
+import { google } from "googleapis";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "google_classroom",
-  propDefinitions: {},
+  propDefinitions: {
+    course: {
+      type: "string",
+      label: "Course",
+      description: "Identifier of the course",
+      async options({
+        prevContext, courseStates = [],
+      }) {
+        const { nextPageToken: pageToken } = prevContext;
+        const {
+          courses, nextPageToken,
+        } = await this.listCourses({
+          courseStates,
+          pageToken,
+        });
+        const options = courses?.map((course) => ({
+          label: course.name,
+          value: course.id,
+        })) || [];
+        return {
+          options,
+          context: {
+            nextPageToken,
+          },
+        };
+      },
+    },
+    coursework: {
+      type: "string",
+      label: "Assignment",
+      description: "Identifier of the course work/assignment",
+      async options({
+        prevContext, courseId, courseWorkStates = [],
+      }) {
+        const { nextPageToken: pageToken } = prevContext;
+        const {
+          courseWork, nextPageToken,
+        } = await this.listCoursework({
+          courseId,
+          courseWorkStates,
+          pageToken,
+        });
+        const options = courseWork?.map((assignment) => ({
+          label: assignment.title,
+          value: assignment.id,
+        })) || [];
+        return {
+          options,
+          context: {
+            nextPageToken,
+          },
+        };
+      },
+    },
+    courseStates: {
+      type: "string[]",
+      label: "Course States",
+      description: "Restricts returned courses to those in one of the specified states",
+      options: constants.COURSE_STATE_OPTIONS,
+      optional: true,
+    },
+    courseworkStates: {
+      type: "string[]",
+      label: "Assignment States",
+      description: "Restriction on the work status to return",
+      options: constants.COURSEWORK_STATE_OPTIONS,
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _client() {
+      const auth = new google.auth.OAuth2();
+      auth.setCredentials({
+        access_token: this.$auth.oauth_access_token,
+      });
+      return google.classroom({
+        version: "v1",
+        auth,
+      });
+    },
+    async listCourses(params = {}) {
+      const classroom = this._client();
+      const { data } = await classroom.courses.list(params);
+      return data;
+    },
+    async listCoursework(params = {}) {
+      const classroom = this._client();
+      const { data } = await classroom.courses.courseWork.list(params);
+      return data;
+    },
+    async getCoursework(params = {}) {
+      const classroom = this._client();
+      const { data } = await classroom.courses.courseWork.get(params);
+      return data;
     },
   },
 };
