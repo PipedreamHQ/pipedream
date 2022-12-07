a153388d9eb17ad23777efb93099831e742cdadc
Jorge Cortes <jacortesmahmud@gmail.com>
2022-12-07 14:28:01 -0500

    Added custom properties to create-contact action

--- components/agile_crm/actions/create-contact/create-contact.mjs
+++ components/agile_crm/actions/create-contact/create-contact.mjs
@@ -1,10 +1,11 @@
 import agileCrm from "../../agile_crm.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   key: "agile_crm-create-contact",
   name: "Create Contact",
   description: "Create a new contact in Agile CRM. [See the docs here](https://github.com/agilecrm/rest-api#13-creating-a-contact)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     agileCrm,
@@ -39,34 +40,79 @@ export default {
         "phone",
       ],
     },
+    customFields: {
+      type: "string[]",
+      label: "Custom Fields",
+      description: "Please provide a JSON structure like this per row: `{\"name\": \"Field Name\", \"value\": \"Field Value\"}`",
+      optional: true,
+    },
+  },
+  methods: {
+    getAdditionalProperties(customFields) {
+      if (!customFields) {
+        return [];
+      }
+
+      if (!Array.isArray(customFields)) {
+        throw new ConfigurationError("Custom Fields property is not an array");
+      }
+
+      try {
+        return customFields.map(JSON.parse)
+          .map(({
+            name, value = "",
+          }, idx) => {
+            if (!name) {
+              throw `No "name" property specified in row ${idx + 1}`;
+            }
+            return {
+              type: "CUSTOM",
+              name,
+              value,
+            };
+          });
+      } catch (error) {
+        throw new ConfigurationError(`Custom Field row not set with the right JSON structure: ${error}`);
+      }
+    },
   },
   async run({ $ }) {
+    const {
+      firstName,
+      lastName,
+      company,
+      email,
+      phone,
+      customFields,
+    } = this;
+
     const data = {
       properties: [
         {
           type: "SYSTEM",
           name: "first_name",
-          value: this.firstName,
+          value: firstName,
         },
         {
           type: "SYSTEM",
           name: "last_name",
-          value: this.lastName || "",
+          value: lastName || "",
         },
         {
           type: "SYSTEM",
           name: "company",
-          value: this.company || "",
+          value: company || "",
         },
         {
           type: "SYSTEM",
           name: "email",
-          value: this.email || "",
+          value: email || "",
         },
         {
           name: "phone",
-          value: this.phone || "",
+          value: phone || "",
         },
+        ...this.getAdditionalProperties(customFields),
       ],
     };
 
