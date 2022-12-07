3380a495dbd957d0791942022ad1e8488d507264
Luan Cazarine <luanhc@gmail.com>
2022-12-07 14:22:36 -0300

    Unisender
    Triggers
      - New Planned Campaign
      - New Subscriber
      - New Unsubscriber
      - New Completed Campaign
    
    Actions
      - Subscribe Contact
      - Unsubscribe Contact

---
+++ components/unisender/sources/new-completed-campaign/new-completed-campaign.mjs
@@ -0,0 +1,36 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  name: "New Completed Campaign (Instant)",
+  key: "unisender-new-completed-campaign",
+  description: "Emit new event when all messages have been sent and analysis of the results is completed.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEvent() {
+      return "campaign_status";
+    },
+    getMeta(item) {
+      const {
+        event_name, auth,
+      } = item;
+
+      const eventTime = new Date().getTime();
+
+      return {
+        id: auth + eventTime,
+        summary: `New completed campaign: ${event_name}`,
+        ts: eventTime,
+      };
+    },
+    processEvent(body) {
+      if (body.status == "completed") {
+        const meta = this.getMeta(body);
+        this.$emit(body, meta);
+      }
+    },
+  },
+};
