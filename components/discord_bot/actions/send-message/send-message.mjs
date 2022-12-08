7770b89d88747c14be7ea9a8255cd6b7cdcbc54c
Lucas Caresia <lucascarezia@gmail.com>
2022-12-08 15:03:36 -0300

    Removed unnecessary common file

--- components/discord_bot/actions/send-message/send-message.mjs
+++ components/discord_bot/actions/send-message/send-message.mjs
@@ -1,5 +1,4 @@
 import common from "../../common.mjs";
-import messageCommon from "../send-message-common.mjs";
 import utils from "../../common/utils.mjs";
 
 const { discord } = common.props;
@@ -10,11 +9,46 @@ export default {
   description: "Sends a message to an user. [See the docs here](https://discord.com/developers/docs/resources/user#create-dm) and [here](https://discord.com/developers/docs/resources/channel#create-message)",
   version: "0.0.1",
   type: "action",
-  ...messageCommon,
   props: {
     discord,
     ...common.props,
-    ...messageCommon.props,
+    message: {
+      propDefinition: [
+        discord,
+        "message",
+      ],
+    },
+    embeds: {
+      propDefinition: [
+        discord,
+        "embeds",
+      ],
+    },
+    threadID: {
+      propDefinition: [
+        discord,
+        "threadID",
+      ],
+    },
+    username: {
+      propDefinition: [
+        discord,
+        "username",
+      ],
+    },
+    avatarURL: {
+      propDefinition: [
+        discord,
+        "avatarURL",
+      ],
+    },
+    includeSentViaPipedream: {
+      type: "boolean",
+      optional: true,
+      default: true,
+      label: "Include link to workflow",
+      description: "Defaults to `true`, includes a link to this workflow at the end of your Discord message.",
+    },
     userId: {
       propDefinition: [
         discord,
@@ -23,6 +57,35 @@ export default {
           guildId,
         }),
       ],
+    },
+  },
+  method: {
+    ...common.methods,
+    getUserInputProps(omit = [
+      "discord",
+    ]) {
+      return Object.keys(this)
+        .filter((key) => typeof this[key] !== "function" && !omit.includes(key))
+        .reduce((props, key) => {
+          props[key] = this[key];
+          return props;
+        }, {});
+    },
+    appendPipedreamText(message) {
+      let content = message;
+      if (typeof content !== "string") {
+        content = JSON.stringify(content);
+      }
+      content += `\n\n${this.getSentViaPipedreamText()}`;
+      return content;
+    },
+    getSentViaPipedreamText() {
+      const workflowId = process.env.PIPEDREAM_WORKFLOW_ID;
+      // The link text is a URL without a protocol for consistency with the "Send via link" text in
+      // Slack messages
+      const linkText = `pipedream.com/@/${workflowId}?o=a&a=discord_webhook`;
+      const link = `https://${linkText}`;
+      return `Sent via [${linkText}](<${link}>)`;
     },
   },
   async run({ $ }) {
