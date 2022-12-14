731d69c0327d7d83460b4bc1a4617680905a3f70
michelle0927 <michelle.bergeron@gmail.com>
2022-12-14 16:29:35 -0500

    improvements

--- components/sendgrid/actions/search-contacts/search-contacts.mjs
+++ components/sendgrid/actions/search-contacts/search-contacts.mjs
@@ -1,18 +1,18 @@
-import common from "../common.mjs";
+import common from "../common/common.mjs";
 
 export default {
   ...common,
   key: "sendgrid-search-contacts",
   name: "Search Contacts",
   description: "Searches contacts with a SGQL query. [See the docs here](https://docs.sendgrid.com/api-reference/contacts/search-contacts)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     ...common.props,
     query: {
       type: "string",
       label: "Query",
-      description: "The query field accepts valid SGQL for searching for a contact. Only the first 50 contacts will be returned. If the query takes longer than 20 seconds a 408 Request Timeout will be returned.",
+      description: "The query field accepts valid SGQL for searching for a contact (.e.g `email LIKE 'hung.v%'` ). Only the first 50 contacts will be returned. [For more information about SGQL](https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language)",
     },
   },
   async run({ $ }) {
