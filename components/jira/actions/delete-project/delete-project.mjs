374f19d0e2f39d4883f6ce5d91adcc2462cacdda
Lucas Caresia <lucascarezia@gmail.com>
2022-12-14 13:52:09 -0300

    [FEATURE] Add list of authorized Jira cloud IDs as an async option for each Jira trigger / action #4858 (#4883)
    * Addec asyncoptions for cloud ID
    
    * Fixing some async options requests
    
    * Fixing a bug on pass cloudId param
    
    * Update optional field
    
    * Update optional field
    
    * Fixed bug on update-issue
    
    * Fixed some bugs in others actions
    
    * Fixed bug to pass cloudId
    
    * Fixed bug to pass cloudId
    
    * Fixed bug on data request
    
    * Fixed bug on data request
    
    * Fix transition prop always required
    
    Co-authored-by: vunguyenhung <vunguyenhung@outlook.com>

--- components/jira/actions/delete-project/delete-project.mjs
+++ components/jira/actions/delete-project/delete-project.mjs
@@ -4,26 +4,35 @@ export default {
   key: "jira-delete-project",
   name: "Delete Project",
   description: "Deletes a project, [See the docs](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-delete)",
-  version: "0.1.4",
+  version: "0.1.5",
   type: "action",
   props: {
     jira,
+    cloudId: {
+      propDefinition: [
+        jira,
+        "cloudId",
+      ],
+    },
     projectID: {
       propDefinition: [
         jira,
         "projectID",
+        (c) => ({
+          cloudId: c.cloudId,
+        }),
       ],
     },
     enableUndo: {
       type: "boolean",
       label: "Enable undo",
       description: "Whether this project is placed in the Jira recycle bin where it will be available for restoration.",
-      optional: true,
     },
   },
   async run({ $ }) {
     await this.jira.deleteProject({
       $,
+      cloudId: this.cloudId,
       projectIdOrKey: this.projectID,
       params: {
         enableUndo: this.enableUndo,
--- components/jira/actions/get-transitions/get-transitions.mjs
+++ components/jira/actions/get-transitions/get-transitions.mjs
@@ -4,14 +4,23 @@ export default {
   key: "jira-get-transitions",
   name: "Get Transitions",
   description: "Gets either all transitions or a transition that can be performed by the user on an issue, based on the issue's status, [See the docs](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-issueidorkey-transitions-get)",
-  version: "0.1.4",
+  version: "0.1.5",
   type: "action",
   props: {
     jira,
+    cloudId: {
+      propDefinition: [
+        jira,
+        "cloudId"
+      ]
+    },
     issueIdOrKey: {
       propDefinition: [
         jira,
         "issueIdOrKey",
+        (c) => ({
+          cloudId: c.cloudId
+        })
       ],
     },
     transitionId: {
@@ -49,6 +58,7 @@ export default {
   async run({ $ }) {
     const response = await this.jira.getTransitions({
       $,
+      cloudId: this.cloudId,
       issueIdOrKey: this.issueIdOrKey,
       params: {
         transitionId: this.transitionId,
