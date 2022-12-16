4f2b419ad04e456f4495ec0b84994a0ccd1e0dca
michelle0927 <michelle.bergeron@gmail.com>
2022-12-16 13:03:52 -0500

    search-contacts improvement

--- components/sendgrid/actions/search-contacts/search-contacts.mjs
+++ components/sendgrid/actions/search-contacts/search-contacts.mjs
@@ -1,4 +1,6 @@
 import common from "../common/common.mjs";
+import constants from "../common/constants.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   ...common,
@@ -13,10 +15,58 @@ export default {
       type: "string",
       label: "Query",
       description: "The query field accepts valid SGQL for searching for a contact (.e.g `email LIKE 'hung.v%'` ). Only the first 50 contacts will be returned. [For more information about SGQL](https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language)",
+      optional: true,
+    },
+    queryField: {
+      type: "string",
+      label: "Query Field",
+      description: "Select the field to search",
+      options: constants.CONTACT_FIELDS,
+      reloadProps: true,
+      optional: true,
     },
   },
+  async additionalProps() {
+    const props = {};
+    if (this.queryField) {
+      props.queryValue = {
+        type: "string",
+        label: "Query Value",
+        description: `The ${this.queryField} value to search for`,
+      };
+      props.matchCase = {
+        type: "string",
+        label: "Query Match Case",
+        description: "`Exact` will return only exact matches. `Fuzzy` is case insensitive and will return matches that include the search term.",
+        options: constants.QUERY_MATCH_CASE_OPTIONS,
+        default: "Fuzzy",
+      };
+    }
+    return props;
+  },
   async run({ $ }) {
-    const resp = await this.sendgrid.searchContacts(this.query);
+    const {
+      query,
+      queryField,
+      queryValue,
+      matchCase,
+    } = this;
+
+    if ((query && queryField) || (!query && !queryField)) {
+      throw new ConfigurationError("Please enter only one of `query` or `queryField`");
+    }
+
+    let q;
+    if (query) {
+      q = query;
+    } else {
+      q = matchCase === "Fuzzy"
+        ? `lower(${queryField}) LIKE '%${queryValue}%'`
+        : `${queryField} = '${queryValue}'`;
+    }
+
+    console.log(q);
+    const resp = await this.sendgrid.searchContacts(q);
     $.export("$summary", "Successfully completed search");
     return resp;
   },
