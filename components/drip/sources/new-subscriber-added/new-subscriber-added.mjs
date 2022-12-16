7bbbc4d3caf94f2c73bb373c9ca2f9ee98c5d3b9
Luan Cazarine <luanhc@gmail.com>
2022-12-16 12:51:51 -0300

    changes new-subscriber to new-subscriber-added

--- components/drip/sources/new-subscriber-added/new-subscriber-added.mjs
+++ components/drip/sources/new-subscriber-added/new-subscriber-added.mjs
@@ -2,7 +2,7 @@ import base from "../common/base.mjs";
 
 export default {
   ...base,
-  key: "drip-new-subscriber",
+  key: "drip-new-subscriber-added",
   name: "New Subscriber (Instant)",
   description: "Emit new event when a new subscriber is created",
   version: "0.0.1",
