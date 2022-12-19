323fcf312e366fc9e7883e2868e4817b6cd19369
Andrew Chuang <andrewjschuang@gmail.com>
2022-12-19 12:19:58 -0300

    bump versions

--- components/gmail_custom_oauth/actions/update-primary-signature/update-primary-signature.mjs
+++ components/gmail_custom_oauth/actions/update-primary-signature/update-primary-signature.mjs
@@ -4,7 +4,7 @@ export default {
   key: "gmail_custom_oauth-update-primary-signature",
   name: "Update Signature for Primary Email Address",
   description: "Update the signature for the primary email address. [See docs here](https://developers.google.com/gmail/api/reference/rest/v1/users.settings.sendAs/update)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     gmail,
