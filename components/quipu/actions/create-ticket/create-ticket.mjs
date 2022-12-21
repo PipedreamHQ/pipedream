d448c5f8f50bea800e36c7551804bf8e2b231f54
Alyson Freitas <alys.freitas@gmail.com>
2022-12-21 12:56:32 -0300

    quipu: create invoice, create ticket, added package.json, added async options, added constants, improved create contact descriptions

---
+++ components/quipu/actions/create-ticket/create-ticket.mjs
@@ -0,0 +1,99 @@
+import app from "../../quipu.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "quipu-create-ticket",
+  name: "Creating a ticket",
+  description: "Creates a new ticket. [See the docs](http://quipuapp.github.io/api-v1-docs/#creating-a-ticket).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    recipientName: {
+      type: "string",
+      label: "Recipient Name",
+      description: "The recipient name for the ticket.",
+    },
+    number: {
+      propDefinition: [
+        app,
+        "number",
+      ],
+    },
+    issued: {
+      propDefinition: [
+        app,
+        "issued",
+      ],
+    },
+    paidAt: {
+      propDefinition: [
+        app,
+        "paidAt",
+      ],
+    },
+    paymentMethod: {
+      propDefinition: [
+        app,
+        "paymentMethod",
+      ],
+    },
+    items: {
+      propDefinition: [
+        app,
+        "items",
+      ],
+      optional: true,
+    },
+    tags: {
+      propDefinition: [
+        app,
+        "tags",
+      ],
+      optional: true,
+    },
+    accountingCategory: {
+      propDefinition: [
+        app,
+        "accountingCategory",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const attributes = {
+      "kind": "income",
+      "recipient_name": this.recipientName,
+      "number": this.number,
+      "issue_date": this.issued,
+      "paid_at": this.paidAt,
+      "payment_method": this.paymentMethod,
+      "tags": (this.tags && this.tags.join(", ")) || undefined,
+    };
+    const relationships = {};
+    if (this.accountingCategory) {
+      relationships.accounting_category = {
+        "data": {
+          "id": this.accountingCategory,
+          "type": "accounting_categories",
+        },
+      };
+    }
+    if (this.items) {
+      let items = this.items;
+      if (typeof (this.items) === "string") {
+        items = JSON.parse(items);
+      }
+      if (!Array.isArray(items)) {
+        console.log(items);
+        throw new ConfigurationError("Items must to be an array.");
+      }
+      relationships.items = {
+        "data": items,
+      };
+    }
+    const ticket = await this.app.createTicket($, attributes, relationships);
+    $.export("$summary", `Successfully created ticket "${ticket.id}"`);
+    return ticket;
+  },
+};
--- components/quipu/quipu.app.mjs
+++ components/quipu/quipu.app.mjs
@@ -1,9 +1,58 @@
 import { axios } from "@pipedream/platform";
+import constants from "./actions/common/constants.mjs";
 
 export default {
   type: "app",
   app: "quipu",
-  propDefinitions: {},
+  propDefinitions: {
+    client: {
+      type: "string",
+      label: "Client",
+      description: "Select a client.",
+      async options() {
+        return this.getContactsOpts();
+      },
+    },
+    accountingCategory: {
+      type: "string",
+      label: "Accounting Category",
+      description: "Select an accounting category.",
+      async options() {
+        return this.getAccountingCategoriesOpts("income");
+      },
+    },
+    number: {
+      type: "string",
+      label: "Number",
+      description: "The invoice number.",
+    },
+    issued: {
+      type: "string",
+      label: "Issued",
+      description: "The issue date of the invoice, ISO 8601 format. Example: `2019-07-26`.",
+    },
+    paidAt: {
+      type: "string",
+      label: "Paid At",
+      description: "Date of payment, ISO 8601 format. Example: `2019-07-26`.",
+    },
+    paymentMethod: {
+      type: "string",
+      label: "Payment Method",
+      description: "The method of payment.",
+      options: constants.PAYMENT_METHOD_OPTS,
+    },
+    items: {
+      type: "string",
+      label: "Items",
+      description: "An array of objects as the following example:\n `[{\"type\": \"book_entry_items\", \"attributes\": {\"concept\": \"Screws\", \"unitary_amount\": \"0.50\", \"quantity\":30, \"vat_percent\":21, \"retention_percent\":0}}, {\"type\": \"book_entry_items\", \"attributes\": {\"concept\": \"Nuts\", \"unitary_amount\": \"0.35\", \"quantity\":30, \"vat_percent\":21, \"retention_percent\":0}}]`\n [see docs here](http://quipuapp.github.io/api-v1-docs/#items).",
+    },
+    tags: {
+      type: "string[]",
+      label: "Tags",
+      description: "Add tags to the invoice.",
+    },
+  },
   methods: {
     _getBaseUrl() {
       return "https://getquipu.com";
@@ -16,30 +65,82 @@ export default {
       };
     },
     _getRequestParams(opts = {}) {
-      return {
-        ...opts,
-        url: this._getBaseUrl() + opts.path,
-        headers: this._getHeaders(),
-      };
-    },
-    async createContact(ctx = this, attributes) {
       try {
-        const data = {
-          data: {
-            type: "contacts",
-            attributes,
-          },
+        return {
+          ...opts,
+          url: this._getBaseUrl() + opts.path,
+          headers: this._getHeaders(),
         };
-        const response = await axios(ctx, this._getRequestParams({
-          method: "POST",
-          path: "/contacts",
-          data,
-        }));
-        return response.data;
       } catch (ex) {
         console.log(ex?.response?.data?.errors);
         throw ex;
       }
+    },
+    async getContactsOpts() {
+      const response = await axios(this, this._getRequestParams({
+        method: "GET",
+        path: "/contacts",
+      }));
+      return response.data.map((client) => ({
+        label: client.attributes.name,
+        value: client.id,
+      }));
+    },
+    async getAccountingCategoriesOpts(kind) {
+      const response = await axios(this, this._getRequestParams({
+        method: "GET",
+        path: "/accounting_categories",
+        params: {
+          "filter[kind]": kind,
+        },
+      }));
+      return response.data.map((client) => ({
+        label: client.attributes.name,
+        value: client.id,
+      }));
+    },
+    async createContact(ctx = this, attributes) {
+      const data = {
+        data: {
+          type: "contacts",
+          attributes,
+        },
+      };
+      const response = await axios(ctx, this._getRequestParams({
+        method: "POST",
+        path: "/contacts",
+        data,
+      }));
+      return response.data;
+    },
+    async createTicket(ctx = this, attributes) {
+      const data = {
+        data: {
+          type: "tickets",
+          attributes,
+        },
+      };
+      const response = await axios(ctx, this._getRequestParams({
+        method: "POST",
+        path: "/tickets",
+        data,
+      }));
+      return response.data;
+    },
+    async createInvoice(ctx = this, attributes, relationships) {
+      const data = {
+        data: {
+          type: "invoices",
+          attributes,
+          relationships,
+        },
+      };
+      const response = await axios(ctx, this._getRequestParams({
+        method: "POST",
+        path: "/invoices",
+        data,
+      }));
+      return response.data;
     },
   },
 };
