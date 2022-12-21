bbe1cc54a945afdf8dd53ac478a9ec33a52e983e
Andrew Chuang <andrewjschuang@gmail.com>
2022-12-21 09:09:58 -0300

    [DigitalOcean Spaces] New components (#4999)
    * init
    
    * reuse aws s3 components architecture
    
    * add list files action
    
    * add delete files action
    
    * add sources base
    
    * add file uploaded source
    
    * add file deleted source
    
    * add pagination
    
    * bump package.json

---
+++ components/digitalocean_spaces/sources/file-deleted/file-deleted.mjs
@@ -0,0 +1,34 @@
+import base from "../common/base.mjs";
+
+export default {
+  ...base,
+  key: "digitalocean_spaces-file-deleted",
+  name: "File Deleted",
+  description: "Emit new event when a file is deleted from a DigitalOcean Spaces bucket",
+  version: "0.0.1",
+  type: "source",
+  hooks: {
+    async deploy() {
+      const files = await this.aws.listFiles({
+        Bucket: this.bucket,
+        Prefix: this.prefix,
+      });
+      this.setFileList(files);
+    },
+  },
+  methods: {
+    ...base.methods,
+    updateFileList(listedFiles) {
+      const currentList = this.getFileList();
+      this.setFileList(listedFiles);
+      return currentList.filter(({ Key }) => !listedFiles.map(this.getKey).includes(Key));
+    },
+    emitEvents(files) {
+      files.forEach((file) => this.$emit(file, {
+        id: file.Key,
+        summary: `File deleted: ${file.Key}`,
+        ts: file.LastModified,
+      }));
+    },
+  },
+};
