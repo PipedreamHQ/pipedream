5ede566895dc2619d4b0a8406fcd3cc13440fc59
Alyson Freitas <alys.freitas@gmail.com>
2022-12-23 10:36:29 -0300

    Adding a warning under the app account selection, to advise users that personal account is not supported.

--- components/microsoft_teams/sources/new-team-member/new-team-member.mjs
+++ components/microsoft_teams/sources/new-team-member/new-team-member.mjs
@@ -5,7 +5,7 @@ export default {
   key: "microsoft_teams-new-team-member",
   name: "New Team Member",
   description: "Emit new event when a new member is added to a team",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "source",
   dedupe: "unique",
   props: {
