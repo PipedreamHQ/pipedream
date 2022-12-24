7347f5a0163c309589e0dcb02332f223a9a3d906
GTFalcao <gtfalcao96@gmail.com>
2022-12-24 17:46:01 -0300

    Creating user and project listing + types

--- components/practitest/app/practitest.app.ts
+++ components/practitest/app/practitest.app.ts
@@ -1,7 +1,12 @@
 import { defineApp } from "@pipedream/types";
 import { axios } from "@pipedream/platform";
 import {
-  HttpRequestParams
+  CreateRequirementParams,
+  GetProjectsResponse,
+  GetUsersResponse,
+  HttpRequestParams,
+  Project,
+  User,
 } from "../common/types";
 
 export default defineApp({
@@ -9,23 +14,78 @@ export default defineApp({
   app: "practitest",
   methods: {
     _apiKey(): string {
-      return this.$auth.api_token
+      return this.$auth.api_token;
     },
     _baseUrl(): string {
       return "https://api.practitest.com/api/v2";
     },
     async _httpRequest({
       $ = this,
+      params,
       ...args
     }: HttpRequestParams): Promise<object> {
       return axios($, {
         baseURL: this._baseUrl(),
-        auth: {
-          username: 'any',
-          password: this._apiKey()
+        params: {
+          ...params,
+          api_token: this._apiKey(),
         },
         ...args,
       });
+    },
+    async createRequirement({ projectId, ...args }: CreateRequirementParams) {
+      return this._httpRequest({
+        method: "POST",
+        url: `projects/${projectId}/requirements.json`,
+        data: {
+          data: {
+            type: "requirements",
+            ...args,
+          },
+        },
+      });
+    },
+    async getProjects(): Promise<Project[]> {
+      const { data }: GetProjectsResponse = await this._httpRequest({
+        url: "/projects.json",
+      });
+      return data;
+    },
+    async getUsers(): Promise<User[]> {
+      const { data }: GetUsersResponse = await this._httpRequest({
+        url: "/users.json",
+      });
+      return data;
+    },
+  },
+  propDefinitions: {
+    project: {
+      type: "string",
+      label: "Project",
+      description:
+        "Choose a **Project** from the list, or provide a custom *Project ID*.",
+      async options() {
+        const projects: Project[] = await this.getProjects();
+        return projects.map(({ attributes: { name: label }, id: value }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    user: {
+      type: "string",
+      label: "User",
+      description:
+        "Choose an **User** from the list, or provide a custom *User ID*.",
+      async options() {
+        const user: User[] = await this.getUsers();
+        return user.map(
+          ({ attributes: { "display-name": label }, id: value }) => ({
+            label,
+            value,
+          })
+        );
+      },
     },
   },
 });
--- components/practitest/common/types.ts
+++ components/practitest/common/types.ts
@@ -1,4 +1,4 @@
-import { Pipedream } from "@pipedream/types";
+import { JSONValue, Pipedream } from "@pipedream/types";
 
 interface PdAxiosRequest {
   $: Pipedream;
@@ -9,4 +9,49 @@ export interface HttpRequestParams extends PdAxiosRequest {
   method?: string;
   params?: object;
   data?: object;
+}
+
+export interface CreateRequirementParams extends PdAxiosRequest {
+  projectId: string;
+  attributes: {
+    name: string;
+    "author-id": User["id"];
+    "assigned-to-id"?: User["id"];
+    description?: string;
+    version?: string;
+    priority?: string;
+    "custom-fields"?: Record<string, string>;
+    "parent-id": PractiTestEntity["id"];
+    tags?: string[];
+  };
+  traceability?: {
+    "test-ids": number[];
+  };
+}
+
+interface PractiTestResponse {
+  data: object | object[];
+}
+
+export interface GetProjectsResponse extends PractiTestResponse {
+  data: Project[];
+}
+export interface GetUsersResponse extends PractiTestResponse {
+  data: User[];
+}
+
+interface PractiTestEntity {
+  id: string;
+  attributes: Record<string, string>;
+}
+
+export interface Project extends PractiTestEntity {
+  attributes: {
+    name: string;
+  };
+}
+export interface User extends PractiTestEntity {
+  attributes: {
+    "display-name": string;
+  };
 }
