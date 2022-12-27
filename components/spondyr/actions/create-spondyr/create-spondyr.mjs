b380c53c575d42e9ceb48d9d71d362c0e74ad0bb
Luan Cazarine <luanhc@gmail.com>
2022-12-27 14:55:27 -0300

    Spondyr
    Triggers
      - Spondyr Processed
    
    Actions
      - Create Spondyr

---
+++ components/spondyr/actions/create-spondyr/create-spondyr.mjs
@@ -0,0 +1,55 @@
+import spondyr from "../../spondyr.app.mjs";
+
+export default {
+  key: "spondyr-create-spondyr",
+  name: "Create Spondyr",
+  version: "0.0.3",
+  description: "Generate and optionally deliver correspondence. [See the docs here](https://client.spondyr.io/#/Public/Public/Documentation?Section=send-api)",
+  type: "action",
+  props: {
+    spondyr,
+    transactionType: {
+      propDefinition: [
+        spondyr,
+        "transactionType",
+      ],
+    },
+    eventType: {
+      propDefinition: [
+        spondyr,
+        "eventType",
+        (c) => ({
+          transactionType: c.transactionType,
+        }),
+      ],
+    },
+    isGenerateOnly: {
+      type: "boolean",
+      label: "Is Generate Only",
+      description: "Set this flag to true to pause/defer delivery until a followup call is made via the Deliver API. This is useful for scenarios where you wish to view/preview the generated correspondence and validate things before allowing the correspondence to be delivered.",
+      optional: true,
+    },
+    data: {
+      type: "object",
+      label: "Data",
+      description: "It should contain your application's specific data, which should be in the same format as you defined for the `Transaction Type`",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      eventType,
+      isGenerateOnly,
+      data,
+    } = this;
+
+    const response = await this.spondyr.createSpondyr({
+      $,
+      EventType: eventType,
+      IsGenerateOnly: isGenerateOnly,
+      Data: data,
+    });
+    $.export("$summary", `Spondyr Successfully created with id ${response.ReferenceID}!`);
+    return response;
+  },
+};
