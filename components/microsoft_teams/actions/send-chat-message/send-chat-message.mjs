c9a2887057c5e2d11914bbe80d79ad640a379907
Alyson Freitas <103650302+alysonturing@users.noreply.github.com>
2022-12-29 13:53:41 -0300

    microsoft personal accounts message upgrade (#5062)
    Co-authored-by: Alyson Freitas <alys.freitas@gmail.com>

--- components/microsoft_teams/actions/send-chat-message/send-chat-message.mjs
+++ components/microsoft_teams/actions/send-chat-message/send-chat-message.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Send Chat Message",
   description: "Send a message to a team&#39;s chat. [See the docs here](https://docs.microsoft.com/en-us/graph/api/chat-post-messages?view=graph-rest-1.0&tabs=http)",
   type: "action",
-  version: "0.0.2",
+  version: "0.0.3",
   props: {
     microsoftTeams,
     chatId: {
--- components/microsoft_teams/sources/new-channel/new-channel.mjs
+++ components/microsoft_teams/sources/new-channel/new-channel.mjs
@@ -5,7 +5,7 @@ export default {
   key: "microsoft_teams-new-channel",
   name: "New Channel",
   description: "Emit new event when a new channel is created within a team",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "source",
   dedupe: "unique",
   props: {
