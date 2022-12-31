91bcb725609b73164276f918e7da7101a2d2d280
GTFalcao <gtfalcao96@gmail.com>
2022-12-31 03:21:51 -0300

    ESLint

--- components/practitest/common/constants.ts
+++ components/practitest/common/constants.ts
@@ -2,5 +2,5 @@ export const DOCS = {
   createRequirement: "https://www.practitest.com/api-v2/#create-a-requirement",
   createRun: "https://www.practitest.com/api-v2/#create-a-run-automated-test",
   issueCreated: "https://www.practitest.com/api-v2/#get-all-issues-in-your-project",
-  testCreated: "https://www.practitest.com/api-v2/#get-all-tests-in-your-project"
+  testCreated: "https://www.practitest.com/api-v2/#get-all-tests-in-your-project",
 };
