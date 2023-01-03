7369ef121bfb8f7e8b76d387b67b6a16773239b2
Andrew Chuang <andrewjschuang@gmail.com>
2023-01-03 14:19:49 -0300

    [Square] New Components (#5040)
    * add webhooks base
    
    * add triggers
    
    * add custom webhook events trigger
    
    * add pagination
    
    * emit historical events
    
    * add actions
    
    * add pagination to async options
    
    * Add example on Line Items
    
    * fix undefined issue
    
    * fix sources
    
    * Fix new invoice created source
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/square/sources/common/base.mjs
@@ -0,0 +1,100 @@
+import square from "../../square.app.mjs";
+import crypto from "crypto";
+
+export default {
+  props: {
+    square,
+    db: "$.service.db",
+    http: {
+      type: "$.interface.http",
+    },
+  },
+  hooks: {
+    async activate() {
+      const eventTypes = this.getEventTypes();
+
+      let name = "Pipedream Custom Webhook";
+      if (eventTypes.length === 1) {
+        name = `Pipedream Webhook for: ${eventTypes}`;
+      }
+
+      const response = await this.square.createWebhook({
+        name,
+        eventTypes: eventTypes,
+        url: this.http.endpoint,
+      });
+      const {
+        subscription: {
+          id,
+          signature_key: signatureKey,
+        },
+      } = response;
+      this.setWebhookId(id);
+      this.setSignatureKey(signatureKey);
+    },
+    async deactivate() {
+      const id = this.getWebhookId();
+      await this.square.deleteWebhook({
+        id,
+      });
+    },
+  },
+  methods: {
+    getEventTypes() {
+      throw new Error("getEventTypes not implemented");
+    },
+    getSummary() {
+      throw new Error("getSummary not implemented");
+    },
+    getTimestamp() {
+      throw new Error("getTimestamp not implemented");
+    },
+    isRelevant() {
+      return true;
+    },
+    getWebhookId() {
+      return this.db.get("webhookId");
+    },
+    setWebhookId(id) {
+      this.db.set("webhookId", id);
+    },
+    getSignatureKey() {
+      return this.db.get("signatureKey");
+    },
+    setSignatureKey(signatureKey) {
+      this.db.set("signatureKey", signatureKey);
+    },
+    validateSignature(body, signature) {
+      const hmac = crypto.createHmac("sha256", this.getSignatureKey());
+      hmac.update(this.http.endpoint + body);
+      const hash = hmac.digest("base64");
+      return hash === signature;
+    },
+  },
+  async run(event) {
+    const {
+      headers,
+      body,
+      bodyRaw,
+    } = event;
+
+    if (!this.validateSignature(bodyRaw, headers["x-square-hmacsha256-signature"])) {
+      console.log("Signature validation failed. Skipping event...");
+      return;
+    }
+
+    if (!this.isRelevant(body)) {
+      console.log("Event is not relevant. Skipping...");
+      return;
+    }
+
+    const id = body.event_id;
+    console.log(`Emitting event ${id}`);
+
+    this.$emit(body, {
+      id,
+      summary: this.getSummary(body),
+      ts: this.getTimestamp(body),
+    });
+  },
+};
