631f7aa1b3481c3d5111d45d777da117513610cf
Alyson Freitas <alys.freitas@gmail.com>
2023-01-03 17:17:24 -0300

    new action batch-create-or-update-contact for hubspot

--- components/hubspot/sources/new-contact/new-contact.mjs
+++ components/hubspot/sources/new-contact/new-contact.mjs
@@ -5,7 +5,7 @@ export default {
   key: "hubspot-new-contact",
   name: "New Contacts",
   description: "Emit new event for each new contact added.",
-  version: "0.0.9",
+  version: "0.0.10",
   dedupe: "unique",
   type: "source",
   methods: {
--- components/hubspot/sources/new-ticket/new-ticket.mjs
+++ components/hubspot/sources/new-ticket/new-ticket.mjs
@@ -5,7 +5,7 @@ export default {
   key: "hubspot-new-ticket",
   name: "New Tickets",
   description: "Emit new event for each new ticket created.",
-  version: "0.0.6",
+  version: "0.0.7",
   dedupe: "unique",
   type: "source",
   hooks: {},
