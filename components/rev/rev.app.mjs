c136b5b4581234e6d66b319a457cdf99f734f13c
Andrew Chuang <andrewjschuang@gmail.com>
2023-01-05 11:23:17 -0300

    init

--- components/rev/rev.app.mjs
+++ components/rev/rev.app.mjs
@@ -1,11 +1,51 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "rev",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.rev.com/api/v1";
+    },
+    _auth() {
+      return {
+        Authorization: `Rev ${this.$auth.client_api_key}:${this.$auth.user_api_key}`,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        ...opts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...opts.headers,
+          ...this._auth(),
+        },
+      });
+    },
+    async paginate({
+      fn, dataType, ...opts
+    }) {
+      const data = [];
+      opts.params = {
+        ...opts.params,
+        pageSize: 100,
+        page: 0,
+      };
+
+      while (true) {
+        const response = await fn.call(this, opts);
+        data.push(...response[dataType]);
+        opts.params.page++;
+
+        if (data.length >= response.total_count) {
+          return {
+            data,
+          };
+        }
+      }
     },
   },
 };
