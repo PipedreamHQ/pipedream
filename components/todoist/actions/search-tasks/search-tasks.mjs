84051aa7e1ffce39db5cb034985f75c5cef3bf88
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2023-01-05 13:52:35 +0100

    Solution to error when filtering by label

--- components/todoist/actions/search-tasks/search-tasks.mjs
+++ components/todoist/actions/search-tasks/search-tasks.mjs
@@ -11,7 +11,7 @@ export default {
     name: {
       type: "string",
       label: "Name Match",
-      description: "Returns tasks that contain the specified string in their name.",
+      description: "Returns tasks that contain the specified string in their name",
       optional: true,
     },
     label: {
@@ -43,15 +43,16 @@ export default {
       project,
       section,
     } = this;
+    const label_name = label ? (await this.todoist.getLabels({id: label})).name : "";
     const tasks = await this.todoist.getActiveTasks({
       $,
       params: {
-        label: label,
+        label: label_name,
         project_id: project,
         section_id: section,
       },
     });
-    let result = name ? tasks.filter((task) => (task.content.includes(name))) : tasks;
+    let result = name ? tasks.filter((task) => task.content.includes(name)) : tasks;
     let summary = `${result.length} task${result.length == 1
       ? ""
       : "s"} found`;
