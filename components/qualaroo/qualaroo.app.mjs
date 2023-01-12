ff2fde85eba4381129a2dd71adabfe45c067deef
Andrew Chuang <andrewjschuang@gmail.com>
2023-01-12 15:53:34 -0300

    init

--- components/qualaroo/qualaroo.app.mjs
+++ components/qualaroo/qualaroo.app.mjs
@@ -2,10 +2,5 @@ export default {
   type: "app",
   app: "qualaroo",
   propDefinitions: {},
-  methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
-  },
+  methods: {},
 };
