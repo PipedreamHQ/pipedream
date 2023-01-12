0c2e3a41f8ee76664678135cd515354ed4947d5b
Cassiano Vellames <cassiano.v@turing.com>
2023-01-12 11:38:01 -0300

    versions pipeline

--- components/gmail/actions/send-email/send-email.mjs
+++ components/gmail/actions/send-email/send-email.mjs
@@ -5,7 +5,7 @@ export default {
   key: "gmail-send-email",
   name: "Send Email",
   description: "Send an email from your Google Workspace email account",
-  version: "0.0.8",
+  version: "0.0.9",
   type: "action",
   props: {
     gmail,
