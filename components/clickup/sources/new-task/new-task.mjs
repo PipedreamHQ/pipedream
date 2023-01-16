663c886bc9661e5485006719799e18d8960a494d
Alyson Freitas <alys.freitas@gmail.com>
2023-01-16 09:35:56 -0300

    new tasks filter events

--- components/clickup/sources/new-task/new-task.mjs
+++ components/clickup/sources/new-task/new-task.mjs
@@ -5,9 +5,46 @@ export default {
   key: "clickup-new-task",
   name: "New Task (Instant)",
   description: "Emit new event when a new task is created",
-  version: "0.0.3",
+  version: "0.0.4",
   dedupe: "unique",
   type: "source",
+  props: {
+    ...common.props,
+    spaceId: {
+      propDefinition: [
+        common.props.app,
+        "spaces",
+        (c) => ({
+          workspaceId: c.workspaceId,
+        }),
+      ],
+      description: "If selected, the **Lists** will be filtered by this Space ID",
+      optional: true,
+    },
+    folderId: {
+      propDefinition: [
+        common.props.app,
+        "folders",
+        (c) => ({
+          spaceId: c.spaceId,
+        }),
+      ],
+      description: "If selected, the **Lists** will be filtered by this Folder ID",
+      optional: true,
+    },
+    listId: {
+      propDefinition: [
+        common.props.app,
+        "lists",
+        (c) => ({
+          workspaceId: c.workspaceId,
+          spaceId: c.spaceId,
+          folderId: c.folderId,
+        }),
+      ],
+      optional: true,
+    },
+  },
   methods: {
     ...common.methods,
     _getMeta({ task_id: taskId }) {
@@ -22,10 +59,34 @@ export default {
         "taskCreated",
       ];
     },
+    getTask({ task_id: taskId }) {
+      return this.app.getTask({
+        taskId,
+      });
+    },
+    eventFilter(task) {
+      let filter = true;
+      if (this.listId && task.list.id !== this.listId) {
+        filter = false;
+      }
+      if (this.folderId && task.folder.id !== this.folderId) {
+        filter = false;
+      }
+      if (this.spaceId && task.space.id !== this.spaceId) {
+        filter = false;
+      }
+      return filter;
+    },
   },
   async run(httpRequest) {
     console.log("Event received");
     this.checkSignature(httpRequest);
-    this.$emit(httpRequest.body, this._getMeta(httpRequest.body));
+    const task = await this.getTask(httpRequest.body);
+    if (this.eventFilter(task)) {
+      this.$emit({
+        ...httpRequest.body,
+        task,
+      }, this._getMeta(httpRequest.body));
+    }
   },
 };
