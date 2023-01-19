0d07289bf3af0a425d62864dd17ac895816a920d
michelle0927 <michelle0927@users.noreply.github.com>
2023-01-19 13:36:57 -0500

    Bingx trade_batch_cancel_orders bug (#5229)
    * update to add orderIds
    
    * pnpm-lock.yaml
    
    * remove console.log
    
    * updates
    
    * updates
    
    * minor fixes

--- components/bingx/actions/market-get-latest-price/market-get-latest-price.mjs
+++ components/bingx/actions/market-get-latest-price/market-get-latest-price.mjs
@@ -1,8 +1,8 @@
 import bingx from "../../bingx.app.mjs";
 
 export default {
-  name: "BingX Market GetLatestPrice",
-  version: "0.0.2",
+  name: "BingX Market Get Latest Price",
+  version: "0.0.3",
   key: "bingx-market-get-latest-price",
   description: "Get Latest Price of a Trading Pair [reference](https://bingx-api.github.io/docs/swap/market-api.html#_2-get-latest-price-of-a-trading-pair).",
   props: {
@@ -15,14 +15,13 @@ export default {
     },
   },
   type: "action",
-  methods: {},
   async run({ $ }) {
     const API_METHOD = "GET";
     const API_PATH = "/api/v1/market/getLatestPrice";
     const parameters = {
       "symbol": this.symbol,
     };
-    let returnValue = await this.bingx.makeRequest(API_METHOD, API_PATH, parameters);
+    const returnValue = await this.bingx.makeRequest(API_METHOD, API_PATH, parameters);
     $.export("$summary", `Latest Price of Trading Pair ${this.symbol}`);
     return returnValue;
   },
