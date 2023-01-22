38a68e62c537880b1693d25bc69a7b4a8674c825
GTFalcao <gtfalcao96@gmail.com>
2023-01-22 02:43:38 -0300

    Output functions for date formats

--- components/formatter/common/dateFormats.ts
+++ components/formatter/common/dateFormats.ts
@@ -2,135 +2,210 @@ interface DateFormat {
   label: string;
   value: string;
   inputFn?: (s: string) => Date;
-  outputFn?: (a: ReturnType<typeof getDateAttributes>) => string;
+  outputFn: (d: Date) => string | number;
 }
 
 const DEFAULT_INPUT_FUNCTION: DateFormat["inputFn"] = (str) => new Date(str);
 
-function getDateAttributes(dateObj: Date) {
-  const year = dateObj.getUTCFullYear();
-  const month = dateObj.getUTCMonth();
-  const dayOfMonth = dateObj.getUTCDate();
-  const dayOfWeek = dateObj.getUTCDay();
-  const hours = dateObj.getUTCHours();
-  const minutes = dateObj.getUTCMinutes();
-  const seconds = dateObj.getUTCSeconds();
-  const isoString = dateObj.toISOString();
-
-  return {
-    dateObj,
-    year,
-    month,
-    dayOfMonth,
-    dayOfWeek,
-    hours,
-    minutes,
-    seconds,
-    isoString,
-  };
-}
+// https://tc39.es/ecma402/#table-datetimeformat-components
 
 const DATE_FORMATS: DateFormat[] = [
   {
     label: "Sun Jan 22 23:04:05 -0000 2006",
     value: "ddd MMM DD HH:mm:ss Z YYYY",
+    outputFn(dateObj) {
+      const date = dateObj
+        .toLocaleDateString("en-US", {
+          weekday: "short",
+          day: "2-digit",
+          month: "short",
+        })
+        .replace(/,/g, "");
+
+      const time = dateObj.toLocaleTimeString("en-GB");
+
+      return `${date} ${time} -0000 ${dateObj.getFullYear()}`;
+    },
   },
   {
     label: "January 22 2006 23:04:05",
     value: "MMMM DD YYYY HH:mm:ss",
+    outputFn(dateObj) {
+      const date = dateObj
+        .toLocaleString("en-US", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        })
+        .replace(/,/g, "");
+
+      const time = dateObj.toLocaleTimeString("en-GB");
+
+      return `${date} ${time}`;
+    },
   },
   {
     label: "January 22 2006",
     value: "MMMM DD YYYY",
+    outputFn(dateObj) {
+      return dateObj
+        .toLocaleString("en-US", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        })
+        .replace(/,/g, "");
+    },
   },
   {
     label: "Jan 22 2006",
     value: "MMM DD YYYY",
+    outputFn(dateObj) {
+      return dateObj
+        .toLocaleString("en-US", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })
+        .replace(/,/g, "");
+    },
   },
   {
     label: "2006-01-22T23:04:05-0000",
     value: "YYYY-MM-DDTHH:mm:ssZ",
-    outputFn({ isoString }) {
-      return isoString.replace(/T|(\.[0-9]{3})/g, "");
+    outputFn(dateObj) {
+      return dateObj
+        .toISOString()
+        .replace(/\.[0-9]{3}/g, "")
+        .replace(/Z/, "-0000");
     },
   },
   {
     label: "2006-01-22 23:04:05 -0000",
     value: "YYYY-MM-DD HH:mm:ss Z",
-    outputFn({ isoString }) {
-      return isoString.replace(/\.[0-9]{3}/g, " ");
+    outputFn(dateObj) {
+      return dateObj
+        .toISOString()
+        .replace(/T|(\.[0-9]{3})/g, " ")
+        .replace(/Z/, "-0000");
     },
   },
   {
     label: "2006-01-22",
     value: "YYYY-MM-DD",
-    outputFn({ dayOfMonth, month, year }) {
-      return `${year}-${month}-${dayOfMonth}`;
+    outputFn(dateObj) {
+      return dateObj
+        .toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })
+        .split("/")
+        .reverse()
+        .join("-");
     },
   },
   {
     label: "01-22-2006",
     value: "MM-DD-YYYY",
-    outputFn({ dayOfMonth, month, year }) {
-      return `${month}-${dayOfMonth}-${year}`;
+    outputFn(dateObj) {
+      return dateObj
+        .toLocaleDateString("en-US", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })
+        .replace(/\//g, "-");
     },
   },
   {
     label: "01/22/2006",
     value: "MM/DD/YYYY",
-    outputFn({ dayOfMonth, month, year }) {
-      return `${month}/${dayOfMonth}/${year}`;
+    outputFn(dateObj) {
+      return dateObj.toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      });
     },
   },
   {
     label: "01/22/06",
     value: "MM/DD/YY",
-    outputFn({ dayOfMonth, month, year }) {
-      return `${month}-${dayOfMonth}-${year.toString().slice(-2)}`;
+    outputFn(dateObj) {
+      return dateObj.toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      });
     },
   },
   {
     label: "22-01-2006",
     value: "DD-MM-YYYY",
-    inputFn: (str) => {
+    inputFn(str) {
       const [day, month, year] = str.split("-");
       const date = new Date(
         Date.UTC(Number(year), Number(month) - 1, Number(day))
       );
       return date;
     },
+
+    outputFn(dateObj) {
+      return dateObj.toLocaleDateString("en-GB").replace(/\//g, "-");
+    },
   },
   {
     label: "22/01/2006",
     value: "DD/MM/YYYY",
-    inputFn: (str) => {
+    inputFn(str) {
       const [day, month, year] = str.split("/");
       const date = new Date(
         Date.UTC(Number(year), Number(month) - 1, Number(day))
       );
       return date;
     },
+    outputFn(dateObj) {
+      return dateObj.toLocaleDateString("en-GB");
+    },
   },
   {
     label: "22/01/06",
     value: "DD/MM/YY",
-    inputFn: (str) => {
+    inputFn(str) {
       const [day, month, year] = str.split("/");
       const date = new Date(
         Date.UTC(Number(year), Number(month) - 1, Number(day) + 2000)
       );
       return date;
     },
+    outputFn(dateObj) {
+      return dateObj.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      });
+    },
   },
   {
     label: "1137971045 (Unix time in seconds)",
     value: "S",
-    inputFn: (str) => new Date(Number(str) * 1000),
+    inputFn(str) {
+      return new Date(Number(str) * 1000);
+    },
+    outputFn(dateObj) {
+      return dateObj.valueOf() / 1000;
+    },
   },
   {
     label: "1137971045000 (Unix time in milliseconds)",
     value: "MS",
-    inputFn: (str) => new Date(Number(str)),
+    inputFn(str) {
+      return new Date(Number(str));
+    },
+    outputFn(dateObj) {
+      return dateObj.valueOf();
+    },
   },
 ];
 
