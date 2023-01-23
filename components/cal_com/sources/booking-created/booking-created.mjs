d53d1e824cddd6911f02919d62addbca0f3be1f1
Lucas Caresia <lucascarezia@gmail.com>
2023-01-23 10:24:03 -0300

    Removed MIT license from package.json

--- components/cal_com/sources/booking-created/booking-created.mjs
+++ components/cal_com/sources/booking-created/booking-created.mjs
@@ -5,7 +5,7 @@ export default {
   key: "cal_com-booking-created",
   name: "New Booking Created",
   description: "Emit new event when a new booking is created.",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   methods: {
