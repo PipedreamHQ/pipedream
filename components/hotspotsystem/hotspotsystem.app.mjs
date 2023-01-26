1294a323e503d66f3faba5ae709ab06c9093624c
Jorge Cortes <jacortesmahmud@gmail.com>
2023-01-26 16:14:02 -0500

    Added sources to app

--- components/hotspotsystem/hotspotsystem.app.mjs
+++ components/hotspotsystem/hotspotsystem.app.mjs
@@ -5,10 +5,35 @@ export default {
   type: "app",
   app: "hotspotsystem",
   propDefinitions: {
-    commonProperty: {
+    locationId: {
       type: "string",
-      label: "Common property",
-      description: "[See the docs here](https://example.com)",
+      label: "Location ID",
+      description: "The ID of the location to get customers from.",
+      async options({ prevContext }) {
+        const { offset = 0 } = prevContext;
+        const { items } = await this.listLocations({
+          params: {
+            limit: constants.DEFAULT_LIMIT,
+            offset,
+            fields: `${constants.FIELDS.ID},${constants.FIELDS.NAME}`,
+          },
+        });
+
+        const options =
+          items.map(({
+            name: label, id: value,
+          }) => ({
+            label,
+            value,
+          }));
+
+        return {
+          options,
+          context: {
+            offset: offset + constants.DEFAULT_LIMIT,
+          },
+        };
+      },
     },
   },
   methods: {
@@ -21,14 +46,13 @@ export default {
     getHeaders(headers) {
       return {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${this.$auth.api_key}`,
+        "sn-apikey": this.$auth.api_key,
         ...headers,
       };
     },
     async makeRequest({
       step = this, path, headers, ...args
     } = {}) {
-
       const config = {
         headers: this.getHeaders(headers),
         url: this.getUrl(path),
@@ -36,6 +60,55 @@ export default {
       };
 
       return axios(step, config);
+    },
+    listLocations(args = {}) {
+      return this.makeRequest({
+        path: "/locations",
+        ...args,
+      });
+    },
+    async *getResourcesStream({
+      resourcesFn,
+      resourcesFnArgs,
+      dateField,
+      lastDate,
+      max = constants.MAX_RESOURCES,
+    }) {
+      let offset = 0;
+      let resourcesCount = 0;
+
+      while (true) {
+        const response =
+          await resourcesFn({
+            ...resourcesFnArgs,
+            params: {
+              ...resourcesFnArgs?.params,
+              offset,
+            },
+          });
+
+        const { items: nextResources } = response;
+
+        if (!nextResources?.length) {
+          console.log("No more resources");
+          return;
+        }
+
+        for (const resource of nextResources) {
+          if (lastDate && Date.parse(resource[dateField]) < Date.parse(lastDate)) {
+            return;
+          }
+
+          yield resource;
+          resourcesCount += 1;
+
+          if (resourcesCount >= max) {
+            return;
+          }
+        }
+
+        offset += constants.DEFAULT_LIMIT;
+      }
     },
   },
 };
