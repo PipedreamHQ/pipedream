c1d5b37a1be65f91c916b5fc59a9783755e8b715
GTFalcao <gtfalcao96@gmail.com>
2023-01-29 16:41:56 -0300

    "Format Number" action

--- components/formatter/actions/format-number/format-number.ts
+++ components/formatter/actions/format-number/format-number.ts
@@ -1,4 +1,5 @@
 import { defineAction } from "@pipedream/types";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default defineAction({
   name: "[Numbers] Format Number",
@@ -16,31 +17,73 @@ export default defineAction({
     inputDecimalMark: {
       label: "Input Decimal Mark",
       description:
-        "The character the input uses to denote the decimal/fractional portion of the number.",
+        "The character the input uses to denote the decimal/fractional portion of the number. Defaults to period `.`",
       type: "string",
       options: [
-        "comma",
-        "period",
+        {
+          label: "comma",
+          value: ",",
+        },
+        {
+          label: "period",
+          value: ".",
+        },
       ],
+      optional: true,
     },
     toFormat: {
       label: "To Format",
       description: "The format the number will be converted to.",
       type: "string",
       options: [
-        "Comma for grouping & period for decimal",
-        "Period for grouping & comma for decimal",
-        "Space for grouping & period for decimal",
-        "Space for grouping & comma for decimal",
+        {
+          label: "Comma for grouping & period for decimal",
+          value: ",.",
+        },
+        {
+          label: "Period for grouping & comma for decimal",
+          value: ".,",
+        },
+        {
+          label: "Space for grouping & period for decimal",
+          value: " .",
+        },
+        {
+          label: "Space for grouping & comma for decimal",
+          value: " ,",
+        },
       ],
     },
   },
   async run({ $ }): Promise<string> {
-    // const {
-    //   input, currency, currencyFormat
-    // } = this;
+    const {
+      input, inputDecimalMark, toFormat,
+    }: Record<string, string> = this;
 
-    const result = "";
+    const decimalMark = inputDecimalMark ?? ".";
+    const splitInput = input.split(decimalMark);
+    if (splitInput.length > 1) {
+      throw new ConfigurationError(
+        `Input has more than one decimal mark \`${decimalMark}\`. Check if the \`Input Decimal Mark\` prop is set correctly.`,
+      );
+    }
+
+    const [
+      integer,
+      fractional,
+    ] = splitInput;
+    const [
+      groupChar,
+      decimalChar,
+    ] = toFormat.split("");
+    let result = "";
+
+    for (let i = 0; i < integer.length; i += 3) {
+      result += integer.slice(i, i + 3) + groupChar;
+    }
+    if (fractional) {
+      result += decimalChar + fractional;
+    }
 
     $.export("$summary", "Successfully formatted number");
     return result;
