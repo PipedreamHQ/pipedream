a5c548ce9a3dc443acfd5489d96c7e6271109a83
Alyson Freitas <alys.freitas@gmail.com>
2023-01-30 15:05:52 -0300

    adding source new task advanced, upgrading versions

--- components/clickup/sources/new-task/new-task.mjs
+++ components/clickup/sources/new-task/new-task.mjs
@@ -8,43 +8,6 @@ export default {
   version: "0.0.4",
   dedupe: "unique",
   type: "source",
-  props: {
-    ...common.props,
-    spaceId: {
-      propDefinition: [
-        common.props.app,
-        "spaces",
-        (c) => ({
-          workspaceId: c.workspaceId,
-        }),
-      ],
-      description: "If selected, the **Lists** will be filtered by this Space ID",
-      optional: true,
-    },
-    folderId: {
-      propDefinition: [
-        common.props.app,
-        "folders",
-        (c) => ({
-          spaceId: c.spaceId,
-        }),
-      ],
-      description: "If selected, the **Lists** will be filtered by this Folder ID",
-      optional: true,
-    },
-    listId: {
-      propDefinition: [
-        common.props.app,
-        "lists",
-        (c) => ({
-          workspaceId: c.workspaceId,
-          spaceId: c.spaceId,
-          folderId: c.folderId,
-        }),
-      ],
-      optional: true,
-    },
-  },
   methods: {
     ...common.methods,
     _getMeta({ task_id: taskId }) {
@@ -59,34 +22,10 @@ export default {
         "taskCreated",
       ];
     },
-    getTask({ task_id: taskId }) {
-      return this.app.getTask({
-        taskId,
-      });
-    },
-    eventFilter(task) {
-      let filter = true;
-      if (this.listId && task.list.id !== this.listId) {
-        filter = false;
-      }
-      if (this.folderId && task.folder.id !== this.folderId) {
-        filter = false;
-      }
-      if (this.spaceId && task.space.id !== this.spaceId) {
-        filter = false;
-      }
-      return filter;
-    },
   },
   async run(httpRequest) {
     console.log("Event received");
     this.checkSignature(httpRequest);
-    const task = await this.getTask(httpRequest.body);
-    if (this.eventFilter(task)) {
-      this.$emit({
-        ...httpRequest.body,
-        task,
-      }, this._getMeta(httpRequest.body));
-    }
+    this.$emit(httpRequest.body, this._getMeta(httpRequest.body));
   },
 };
