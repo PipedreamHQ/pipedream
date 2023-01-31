7df40d5d8c08fd7d3a05b4d1057475b4c7e5efe6
michelle0927 <michelle0927@users.noreply.github.com>
2023-01-31 14:39:51 -0500

    Twitter Upload Media Action bug fix (#5308)
    * upload-media fix
    
    * added mediaCategory prop, increased chunk size, & added check status step for videos
    
    * test
    
    * fix version

--- components/twitter/actions/upload-media/upload-media.mjs
+++ components/twitter/actions/upload-media/upload-media.mjs
@@ -6,7 +6,7 @@ export default {
   key: "twitter-upload-media",
   name: "Upload Media",
   description: "Upload images or other media types to Twitter. Returns a `media_id_string` to be used with the Create Tweet action. [See the docs here](https://developer.twitter.com/en/docs/tutorials/uploading-media)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     twitter,
@@ -23,6 +23,17 @@ export default {
         return Object.values(mime._types);
       },
     },
+    mediaCategory: {
+      type: "string",
+      label: "Media Category",
+      description: "The category that represents how the media will be used",
+      options: [
+        "amplify_video",
+        "tweet_gif",
+        "tweet_image",
+        "tweet_video",
+      ],
+    },
   },
   async run({ $ }) {
     // INIT
@@ -30,18 +41,22 @@ export default {
       encoding: "base64",
     });
     const fileSize = (fs.statSync(this.file)).size;
+    const params = {
+      command: "INIT",
+      total_bytes: fileSize,
+      media_type: this.mediaType,
+      additional_owners: this.twitter.$auth.oauth_uid,
+    };
+    if (this.mediaCategory) {
+      params.media_category = this.mediaCategory;
+    }
     const { media_id_string: mediaId } = await this.twitter.uploadMedia({
-      params: {
-        command: "INIT",
-        total_bytes: fileSize,
-        media_type: this.mediaType,
-        additional_owners: this.twitter.$auth.oauth_uid,
-      },
+      params,
       $,
     });
 
     // APPEND
-    const size = 5012;
+    const size = 9000;
     const numSegments = fileContent.length / size;
     let segment = 0;
     let startIndex = 0;
@@ -64,13 +79,35 @@ export default {
     }
 
     // FINALIZE
-    const response = await this.twitter.uploadMedia({
+    let response;
+    response = await this.twitter.uploadMedia({
       params: {
         command: "FINALIZE",
         media_id: mediaId,
       },
       $,
     });
+
+    // STATUS
+    while (this.mediaCategory?.includes("video")) {
+      response = await this.twitter.uploadMedia({
+        params: {
+          command: "STATUS",
+          media_id: mediaId,
+        },
+        method: "GET",
+        $,
+      });
+      const { processing_info: info } = response;
+      if (info?.error) {
+        throw new Error(JSON.stringify(info.error));
+      }
+      if (info?.state === "succeeded") {
+        break;
+      }
+      setTimeout(() => {}, info.check_after_secs * 1000);
+    }
+
     response && $.export("$summary", `Successfully uploaded media with Media ID ${mediaId}`);
     return response;
   },
