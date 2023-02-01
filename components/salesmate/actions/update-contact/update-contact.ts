11493daf6db39a57636ba0a4ea864093aaeb7d0e
Luan Cazarine <luanhc@gmail.com>
2023-02-01 08:54:39 -0300

    [Components] Salesmate #5321 Triggers   - New Contact   - New Opportunity
    Actions
      - Add Contact
      - Update Contact
      - Add Opportunity

---
+++ components/salesmate/actions/update-contact/update-contact.ts
@@ -0,0 +1,202 @@
+import { defineAction } from "@pipedream/types";
+import salesmate from "../../app/salesmate.app";
+
+export default defineAction({
+  name: "Update Contact",
+  version: "0.0.1",
+  key: "salesmate-update-contact",
+  description: "This api is used to update a contact details. [See docs here](https://apidocs.salesmate.io/#72ca69e0-e70d-4fc5-837b-a758963300b3)",
+  type: "action",
+  props: {
+    salesmate,
+    contactId: {
+      propDefinition: [
+        salesmate,
+        "contactId",
+      ],
+    },
+    firstName: {
+      propDefinition: [
+        salesmate,
+        "firstName",
+      ],
+      optional: true,
+    },
+    lastName: {
+      propDefinition: [
+        salesmate,
+        "lastName",
+      ],
+    },
+    owner: {
+      propDefinition: [
+        salesmate,
+        "owner",
+      ],
+    },
+    mobile: {
+      propDefinition: [
+        salesmate,
+        "mobile",
+      ],
+      optional: true,
+    },
+    company: {
+      propDefinition: [
+        salesmate,
+        "company",
+      ],
+      optional: true,
+    },
+    email: {
+      propDefinition: [
+        salesmate,
+        "email",
+      ],
+      optional: true,
+    },
+    website: {
+      propDefinition: [
+        salesmate,
+        "website",
+      ],
+      optional: true,
+    },
+    googlePlusHandle: {
+      propDefinition: [
+        salesmate,
+        "googlePlusHandle",
+      ],
+      optional: true,
+    },
+    linkedInHandle: {
+      propDefinition: [
+        salesmate,
+        "linkedInHandle",
+      ],
+      optional: true,
+    },
+    phone: {
+      propDefinition: [
+        salesmate,
+        "phone",
+      ],
+      optional: true,
+    },
+    otherPhone: {
+      propDefinition: [
+        salesmate,
+        "otherPhone",
+      ],
+      optional: true,
+    },
+    skypeId: {
+      propDefinition: [
+        salesmate,
+        "skypeId",
+      ],
+      optional: true,
+    },
+    facebookHandle: {
+      propDefinition: [
+        salesmate,
+        "facebookHandle",
+      ],
+      optional: true,
+    },
+    twitterHandle: {
+      propDefinition: [
+        salesmate,
+        "twitterHandle",
+      ],
+      optional: true,
+    },
+    currency: {
+      propDefinition: [
+        salesmate,
+        "currency",
+      ],
+      optional: true,
+    },
+    designation: {
+      propDefinition: [
+        salesmate,
+        "designation",
+      ],
+      optional: true,
+    },
+    billingAddressLine1: {
+      propDefinition: [
+        salesmate,
+        "billingAddressLine1",
+      ],
+      optional: true,
+    },
+    billingAddressLine2: {
+      propDefinition: [
+        salesmate,
+        "billingAddressLine2",
+      ],
+      optional: true,
+    },
+    billingCity: {
+      propDefinition: [
+        salesmate,
+        "billingCity",
+      ],
+      optional: true,
+    },
+    billingZipCode: {
+      propDefinition: [
+        salesmate,
+        "billingZipCode",
+      ],
+      optional: true,
+    },
+    billingState: {
+      propDefinition: [
+        salesmate,
+        "billingState",
+      ],
+      optional: true,
+    },
+    billingCountry: {
+      propDefinition: [
+        salesmate,
+        "billingCountry",
+      ],
+      optional: true,
+    },
+    description: {
+      propDefinition: [
+        salesmate,
+        "contactDescription",
+      ],
+      optional: true,
+    },
+    tags: {
+      propDefinition: [
+        salesmate,
+        "tags",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      salesmate,
+      ...data
+    } = this;
+    data.tags = data.tags.toString();
+
+    const response = await this.salesmate.updateContact({
+      $,
+      ...data,
+    });
+
+    $.export("$summary", `Contact with id: ${response.Data.id} successfuly update!`);
+
+    return response;
+  },
+});
--- components/salesmate/app/salesmate.app.ts
+++ components/salesmate/app/salesmate.app.ts
@@ -1,13 +1,457 @@
+import { axios } from "@pipedream/platform";
 import { defineApp } from "@pipedream/types";
+import queries from "../common/queries";
+import {
+  clearObj, getUrl
+} from "../common/utils";
 
 export default defineApp({
   type: "app",
   app: "salesmate",
-  propDefinitions: {},
+  propDefinitions: {
+    billingAddressLine1: {
+      label: "Billing Address Line 1",
+      description: "Line one of the billing address for contact.",
+      type: "string",
+    },
+    billingAddressLine2: {
+      label: "Billing Address Line 2",
+      description: "Line two of the billing address for contact.",
+      type: "string",
+    },
+    billingCity: {
+      label: "Billing City",
+      description: "City name of contact's address for billing purpose.",
+      type: "string",
+    },
+    billingCountry: {
+      label: "Billing Country",
+      description: "Country name of contact's address for billing purpose.",
+      type: "string",
+    },
+    billingState: {
+      label: "Billing State",
+      description: "State name of contact's address for billing purpose.",
+      type: "string",
+    },
+    billingZipCode: {
+      label: "Billing Zip Code",
+      description: "Zip code of the billing City.",
+      type: "string",
+    },
+    company: {
+      label: "Company",
+      description: "User can add company for the contact from existing companies. If contact's company is not defined, then user can quick add the company.",
+      type: "integer",
+      async options({ page }) {
+        const rows = 250;
+        const { Data: { data } } = await this.listCompanies({
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    contactId: {
+      label: "Contact Id",
+      description: "The unique identifier of the contact.",
+      type: "integer",
+      async options({ page }) {
+        const rows = 250;
+        const { Data: { data } } = await this.listContacts({
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    currency: {
+      label: "Currency",
+      description: "It contains three-letter ISO currency code, in uppercase.",
+      type: "string",
+      async options() {
+        const { Data } = await this.listCurrency();
+
+        return Data.map(({
+          code: value, name,
+        }) => ({
+          label: `(${value}) - ${name}`,
+          value,
+        }));
+      },
+    },
+    contactDescription: {
+      label: "Description",
+      description: "Description about the contact. It contains an arbitrary string attached to the contact object.",
+      type: "string",
+    },
+    designation: {
+      label: "Designation",
+      description: "Contact's designation.",
+      type: "string",
+    },
+    email: {
+      label: "Email",
+      description: "Email of the contact.",
+      type: "string",
+    },
+    facebookHandle: {
+      label: "Facebook Handle",
+      description: "Contact's facebook link.",
+      type: "string",
+    },
+    firstName: {
+      label: "First Name",
+      description: "First name of the contact.",
+      type: "string",
+    },
+    googlePlusHandle: {
+      label: "Google Plus Handle",
+      description: "Contact's google plus profile link.",
+      type: "string",
+    },
+    lastName: {
+      label: "Last Name",
+      description: "Last name of the contact.",
+      type: "string",
+    },
+    linkedInHandle: {
+      label: "LinkedIn Handle",
+      description: "Contact's linkedin profile link.",
+      type: "string",
+    },
+    mobile: {
+      label: "Mobile",
+      description: "Mobile number of the contact.",
+      type: "string",
+    },
+    otherPhone: {
+      label: "Other Phone",
+      description: "Phone number 2 of the contact.",
+      type: "string",
+    },
+    owner: {
+      label: "Owner",
+      description: "Owner of the contact.",
+      type: "integer",
+      async options({ page }) {
+        const rows = 250;
+        const { Data } = await this.listUsers({
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return Data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    phone: {
+      label: "Phone",
+      description: "Phone number 1 of the contact.",
+      type: "string",
+    },
+    pipelineId: {
+      label: "Pipeline",
+      description: "User can select any one pipeline from pre-defined pipelines.",
+      type: "string",
+      async options({ page }) {
+        const rows = 250;
+        const { Data } = await this.listPipelines({
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return Data.map(({ pipeline }) => (pipeline));
+      },
+    },
+    priority: {
+      label: "Priority",
+      description: "User can set the priority for the deal like high, low or medium.",
+      type: "string",
+      async options() {
+
+        const { fieldOptions } = await this.listVisibleFields({
+          module: "deal",
+          field: "priority",
+        });
+        return JSON.parse(fieldOptions).values;
+      },
+    },
+    skypeId: {
+      label: "Skype Id",
+      description: "Contact's skype id.",
+      type: "string",
+    },
+    source: {
+      label: "Source",
+      description: "User can add source from where contact came to know about user's company or deal's information like Ads, internet etc.",
+      type: "string",
+      async options() {
+
+        const { fieldOptions } = await this.listVisibleFields({
+          module: "deal",
+          field: "source",
+        });
+        return JSON.parse(fieldOptions).values;
+      },
+    },
+    stage: {
+      label: "Stage",
+      description: "User can select a stage for a deal. like new, contacted, qualified, proposal presented or in negotiation.",
+      type: "string",
+      async options({
+        page, pipeline,
+      }) {
+        const rows = 250;
+
+        const { Data } = await this.listStages({
+          pipeline,
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return Object.keys(Data).map((key) => key)
+          .slice(0, -3);
+      },
+    },
+    status: {
+      label: "Status",
+      description: "User can set the deal's status like Open, Won or Lost. By default it is Open.",
+      type: "string",
+      async options() {
+
+        const { fieldOptions } = await this.listVisibleFields({
+          module: "deal",
+          field: "status",
+        });
+        return JSON.parse(fieldOptions).values;
+      },
+    },
+    tags: {
+      label: "Tags",
+      description: "This field contains tags associated with contact.",
+      type: "string[]",
+      async options({ page }) {
+        const rows = 250;
+        const { Data } = await this.listTags({
+          params: {
+            rows,
+            from: rows * page,
+          },
+        });
+
+        return Data.map(({ tag }) => (tag));
+      },
+    },
+    twitterHandle: {
+      label: "Twitter Handle",
+      description: "Contact's twitter link.",
+      type: "string",
+    },
+    website: {
+      label: "Website",
+      description: "Website of the contact.",
+      type: "string",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiUrl(attr) {
+      return getUrl({
+        domain: this.$auth.domain,
+        ...attr,
+      });
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "accessToken": this.$auth.session_key,
+        "x-linkname": this.$auth.linked_name,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, action, version, ...opts
+    }) {
+      const config = {
+        url: `${this._apiUrl({
+          path,
+          action,
+          version,
+        })}`,
+        headers: this._getHeaders(),
+        ...opts,
+      };
+
+      return axios($, clearObj(config));
+    },
+    addContact(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: "contact",
+      });
+    },
+    addDeal(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: "deal",
+      });
+    },
+    listUsers({
+      params, ...args
+    }) {
+      return this._makeRequest({
+        ...args,
+        method: "GET",
+        path: "core",
+        action: "/users",
+        params: {
+          ...params,
+          status: "active",
+        },
+      });
+    },
+    listCompanies(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: "company",
+        action: "/search",
+        data: queries.listCompanies,
+      });
+    },
+    listContacts(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: "contact",
+        action: "/search",
+        data: queries.listContacts,
+      });
+    },
+    listCurrency(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "GET",
+        path: "lookups/active/currency",
+        version: "v3",
+      });
+    },
+    listDeals(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: "deal",
+        action: "/search",
+        data: queries.listDeals,
+      });
+    },
+    listPipelines(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "GET",
+        path: "apps/dealPipeline",
+        version: "v3",
+      });
+    },
+    async listStages({
+      pipeline, ...args
+    }) {
+      return this._makeRequest({
+        ...args,
+        method: "POST",
+        path: `board-view/${pipeline}/stage-wise-summary`,
+        data: queries.listStages(pipeline),
+        version: "v1deal",
+      });
+    },
+    listTags(args = {}) {
+      return this._makeRequest({
+        ...args,
+        method: "GET",
+        path: "tags",
+        version: "v1",
+      });
+    },
+    async listVisibleFields({
+      $, module, field,
+    }) {
+      const { Data } = await this._makeRequest({
+        $,
+        method: "GET",
+        path: "fields/getAllVisibleFields",
+        version: "v3",
+      });
+      const items = Data[module];
+      return items.filter((item) => (item.fieldName === field))[0];
+    },
+    updateContact({
+      $, contactId, ...data
+    }) {
+      return this._makeRequest({
+        $,
+        method: "PUT",
+        path: "contact",
+        action: `/${contactId}`,
+        data,
+      });
+    },
+    async *paginate({
+      fn, params = {
+        rows: 0,
+        from: 0,
+      }, maxResults = null,
+    }) {
+      let rows = 0;
+      let count = 0;
+      let page = 0;
+      const limit = 250;
+
+      do {
+        params.rows = limit;
+        params.from = ++page * limit;
+        const {
+          Data: {
+            data,
+            totalRows,
+          },
+        } = await fn(params);
+        for (const d of data) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        rows = totalRows;
+      } while (rows);
     },
   },
 });
