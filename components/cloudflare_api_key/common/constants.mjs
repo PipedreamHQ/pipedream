9a946799aa73d3e0215d3cc4512f29063f412e37
michelle0927 <michelle0927@users.noreply.github.com>
2023-02-09 13:37:07 -0500

    Cloudflare - new action Create IP Access Rule (#5434)
    * new action create-ip-access-rule
    
    * update versions

--- components/cloudflare_api_key/common/constants.mjs
+++ components/cloudflare_api_key/common/constants.mjs
@@ -79,6 +79,37 @@ const CERTIFICATE_REQUEST_TYPE_OPTIONS = [
   "keyless-certificate",
 ];
 
+const IP_ACCESS_RULE_MODE_OPTIONS = [
+  "block",
+  "challenge",
+  "whitelist",
+  "js_challenge",
+  "managed_challenge",
+];
+
+const IP_ACCESS_RULE_TARGET_OPTIONS = [
+  {
+    label: "IP (e.g. \"198.51.100.4\")",
+    value: "ip",
+  },
+  {
+    label: "IP6  (e.g. \"2001:DB8:100::CF\")",
+    value: "ip6",
+  },
+  {
+    label: "IP Range (e.g. \"198.51.100.4/16\")",
+    value: "ip_range",
+  },
+  {
+    label: "ASN (e.g. \"AS12345\")",
+    value: "asn",
+  },
+  {
+    label: "Country (e.g. \"US\")",
+    value: "country",
+  },
+];
+
 export default {
   ZONE_SECURITY_LEVEL_OPTIONS,
   DNS_RECORD_MATCH_OPTIONS,
@@ -86,4 +117,6 @@ export default {
   DNS_RECORD_TYPE_OPTIONS,
   ZONE_TYPE_OPTIONS,
   CERTIFICATE_REQUEST_TYPE_OPTIONS,
+  IP_ACCESS_RULE_MODE_OPTIONS,
+  IP_ACCESS_RULE_TARGET_OPTIONS,
 };
