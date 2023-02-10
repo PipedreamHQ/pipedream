9d7b3828f6d6128ae940e87b3f3024803b62cacd
michelle0927 <michelle0927@users.noreply.github.com>
2023-02-10 10:04:08 -0500

    Hubspot - Bug fix to New Deal In Stage (#5447)
    * update new-deal-in-stage
    
    * update versions

--- components/hubspot/sources/new-email-event/new-email-event.mjs
+++ components/hubspot/sources/new-email-event/new-email-event.mjs
@@ -5,7 +5,7 @@ export default {
   key: "hubspot-new-email-event",
   name: "New Email Event",
   description: "Emit new event for each new Hubspot email event.",
-  version: "0.0.11",
+  version: "0.0.12",
   dedupe: "unique",
   type: "source",
   hooks: {},
--- components/hubspot/sources/new-line-item/new-line-item.mjs
+++ components/hubspot/sources/new-line-item/new-line-item.mjs
@@ -5,7 +5,7 @@ export default {
   key: "hubspot-new-line-item",
   name: "New Line Item",
   description: "Emit new event for each new line item added.",
-  version: "0.0.8",
+  version: "0.0.9",
   dedupe: "unique",
   type: "source",
   hooks: {},
