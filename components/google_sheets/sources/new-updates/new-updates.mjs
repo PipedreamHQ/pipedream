9d9da34fb300cc9f25a212bfbe7f592ef39cc02d
Cassiano Vellames <cassiano.v@turing.com>
2023-02-15 10:54:00 -0300

    version

--- components/google_sheets/sources/new-updates/new-updates.mjs
+++ components/google_sheets/sources/new-updates/new-updates.mjs
@@ -8,7 +8,7 @@ export default {
   type: "source",
   name: "New Updates (Instant)",
   description: "Emit new event each time a row or cell is updated in a spreadsheet. To use this source with a spreadsheet in a [Shared Drive](https://support.google.com/a/users/answer/9310351), use the **New Updates (Shared Drive, Instant)** source instead.",
-  version: "0.0.27",
+  version: "0.0.28",
   dedupe: "unique",
   props: {
     ...httpBase.props,
