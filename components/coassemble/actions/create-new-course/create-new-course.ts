7c5eea147fe3f80001549dadb17fbc532a0006a6
Luan Cazarine <luanhc@gmail.com>
2023-02-16 15:47:20 -0300

    [Components] Coassemble #5382 (#5482)
    * [Components] Coassemble #5382
    Triggers
      - New Course Enrollment
      - New User Registration
    
    Actions
      - Create New Course
      - Create New User
      - Send Course Invitation
    
    * fix lint
    
    * Update components/coassemble/actions/create-new-user/create-new-user.ts
    
    Co-authored-by: feyzullah <feyzullah.metu@gmail.com>
    
    * Fix send course invitation action
    
    ---------
    
    Co-authored-by: feyzullah <feyzullah.metu@gmail.com>
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/coassemble/actions/create-new-course/create-new-course.ts
@@ -0,0 +1,48 @@
+import coassemble from "../../app/coassemble.app";
+
+export default {
+  key: "coassemble-create-new-course",
+  name: "Create New Course",
+  version: "0.0.1",
+  description: "Use this endpoint to create new courses in your workspace. [See the docs here](https://developers.coassemble.com/api/courses#create-a-new-course)",
+  type: "action",
+  props: {
+    coassemble,
+    title: {
+      type: "string",
+      label: "Title",
+      description: "Title for the newly-created course.",
+    },
+    category: {
+      propDefinition: [
+        coassemble,
+        "category",
+      ],
+    },
+    code: {
+      type: "string",
+      label: "Code",
+      description: "Course code (optional, will be displayed prior to course title).",
+      optional: true,
+    },
+    overview: {
+      type: "string",
+      label: "Overview",
+      description: "Short description of the course's content. May contain HTML formatting.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      coassemble,
+      ...data
+    } = this;
+    const response = await coassemble.createCourse({
+      $,
+      data,
+    });
+
+    $.export("$summary", `A new Course with id: ${response.id} was successfully created!`);
+    return response;
+  },
+};
