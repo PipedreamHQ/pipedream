5ac520a3629f5c33790c47bf179f4b7036951057
Alyson Freitas <alys.freitas@gmail.com>
2023-02-16 18:55:55 -0300

    pandadoc new components

--- components/pandadoc/pandadoc.app.mjs
+++ components/pandadoc/pandadoc.app.mjs
@@ -1,11 +1,39 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "pandadoc",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `https://api.pandadoc.com/public/v1${path}`;
+    },
+    getHeaders(headers = {}) {
+      return {
+        authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        ...headers,
+      };
+    },
+    makeRequest(customConfig) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherConfig
+      } = customConfig;
+
+      const config = {
+        headers: this.getHeaders(headers),
+        url: this.getUrl(path),
+        ...otherConfig,
+      };
+      return axios($, config);
+    },
+    listDocuments(args = {}) {
+      return this.makeRequest({
+        path: "/documents",
+        ...args,
+      });
     },
   },
 };
