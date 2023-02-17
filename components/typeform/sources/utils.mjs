87b21815736c3624dd778440249f15b3c1fd2437
Pedro Pedruzzi <pedro@vendah.com.br>
2023-02-17 13:48:51 -0300

    Fix the format

--- components/typeform/sources/utils.mjs
+++ components/typeform/sources/utils.mjs
@@ -5,7 +5,7 @@ export default {
     const dt = DateTime.fromISO(isoDate);
     return {
       isoDate,
-      date_time: dt.toFormat("yyyy-mm-dd hh:MM:ss a"),
+      date_time: dt.toFormat("yyyy-MM-dd hh:mm:ss a"),
       date: dt.toFormat("yyyy-MM-dd"),
       time: dt.toFormat("hh:mm:ss a"),
       timezone: dt.zoneName,
