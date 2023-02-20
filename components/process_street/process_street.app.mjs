a4a86f5d8b50bfdad5101435111fe499141e5f10
Andrew Chuang <andrewjschuang@gmail.com>
2023-02-20 10:57:29 -0300

    add start workflow run action

--- components/process_street/process_street.app.mjs
+++ components/process_street/process_street.app.mjs
@@ -3,13 +3,39 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "process_street",
-  propDefinitions: {},
+  propDefinitions: {
+    workflowId: {
+      type: "string",
+      label: "Workflow ID",
+      description: "The ID of the Workflow",
+      async options() {
+        const { workflows } = await this.listWorkflows();
+        return workflows.map((workflow) => ({
+          label: workflow.name,
+          value: workflow.id,
+        }));
+      },
+    },
+  },
   methods: {
     _baseUrl() {
       return "https://public-api.process.st/api/v1.1";
     },
     _auth() {
       return this.$auth.api_key;
+    },
+    async runWorkflow(opts) {
+      return this._makeRequest({
+        ...opts,
+        path: "/workflow-runs",
+        method: "post",
+      });
+    },
+    async listWorkflows(opts = {}) {
+      return this._makeRequest({
+        ...opts,
+        path: "/workflows",
+      });
     },
     async _makeRequest({
       $ = this, path, ...opts
