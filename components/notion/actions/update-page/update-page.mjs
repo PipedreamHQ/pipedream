fae41c7c7a146262b387ab936c52460d31b5e5a6
Andrew Chuang <andrewjschuang@gmail.com>
2023-02-22 11:57:23 -0300

    temporarily remove usage of additional props in update page

--- components/notion/actions/update-page/update-page.mjs
+++ components/notion/actions/update-page/update-page.mjs
@@ -1,13 +1,21 @@
 import notion from "../../notion.app.mjs";
 import base from "../common/base-page-builder.mjs";
 import { pick } from "lodash-es";
+import { ConfigurationError } from "@pipedream/platform";
+
+/**
+ * Currently, additionalProps cannot evaluate custom expressions - {{steps.trigger.event.id}}.
+ * This is a workaround to allow the user to set the property values without async options.
+ * When the issue is resolved, additionalProps and the indicated commented parts can be used again.
+ * See https://github.com/PipedreamHQ/pipedream/issues/3255.
+ */
 
 export default {
   ...base,
   key: "notion-update-page",
   name: "Update Page",
   description: "Updates page property values for the specified page. Properties that are not set will remain unchanged. To append page content, use the *append block* action. [See the docs](https://developers.notion.com/reference/patch-page)",
-  version: "0.2.5",
+  version: "0.3.0",
   type: "action",
   props: {
     notion,
@@ -16,7 +24,8 @@ export default {
         notion,
         "pageId",
       ],
-      reloadProps: true,
+      // TODO: change to `true` when additionalProps is enabled
+      reloadProps: false,
     },
     archived: {
       propDefinition: [
@@ -29,6 +38,8 @@ export default {
         notion,
         "metaTypes",
       ],
+      // TODO: remove this line when additionalProps is enabled
+      reloadProps: false,
     },
     propertyTypes: {
       propDefinition: [
@@ -39,6 +50,15 @@ export default {
           parentType: "page",
         }),
       ],
+      // TODO: remove this line when additionalProps is enabled
+      reloadProps: false,
+    },
+    // TODO: remove this prop when additionalProps is enabled
+    propertyTypesValues: {
+      type: "string[]",
+      label: "Property Types Values",
+      description: "The values for the selected page properties",
+      optional: true,
     },
   },
   async additionalProps() {
@@ -52,6 +72,25 @@ export default {
   },
   methods: {
     ...base.methods,
+    // TODO: remove this method when additionalProps is enabled
+    parseArray(array) {
+      return Array.isArray(array)
+        ? array
+        : JSON.parse(array || "[]");
+    },
+    // TODO: remove this method when additionalProps is enabled
+    configurePageValues() {
+      const keys = this.parseArray(this.propertyTypes);
+      const values = this.parseArray(this.propertyTypesValues);
+
+      if (keys.length !== values.length) {
+        throw new ConfigurationError("The number of property keys and values must be equal");
+      }
+
+      for (let i = 0; i < keys.length || 0; i++) {
+        this[keys[i]] = values[i];
+      }
+    },
     /**
      * Builds a page for a update operation
      * @param page - the parent page
@@ -67,6 +106,8 @@ export default {
     },
   },
   async run({ $ }) {
+    // TODO: remove this line when additionalProps is enabled
+    this.configurePageValues();
     const currentPage = await this.notion.retrievePage(this.pageId);
     const page = this.buildPage(currentPage);
     const response = await this.notion.updatePage(this.pageId, page);
