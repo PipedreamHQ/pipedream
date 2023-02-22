af3f18a426540eafa89eb3f75175a03811bc12f7
Andrew Chuang <andrewjschuang@gmail.com>
2023-02-22 13:55:39 -0300

    bump versions

--- components/typeform/sources/new-submission/new-submission.mjs
+++ components/typeform/sources/new-submission/new-submission.mjs
@@ -11,7 +11,7 @@ export default {
   ...common,
   key: "typeform-new-submission",
   name: "New Submission",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "source",
   description: "Emit new submission",
   props: {
