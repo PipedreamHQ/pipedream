3acd58d72707211cc49207232bbde5efb340c334
michelle0927 <michellelbergero@hotmail.com>
2023-02-22 15:15:08 -0500

    new sources

--- components/pivotal_tracker/pivotal_tracker.app.mjs
+++ components/pivotal_tracker/pivotal_tracker.app.mjs
@@ -1,11 +1,61 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "pivotal_tracker",
-  propDefinitions: {},
+  propDefinitions: {
+    projectId: {
+      type: "string",
+      label: "Project ID",
+      description: "The project identifier",
+      async options() {
+        const projects = await this.listProjects();
+        return projects?.map((project) => ({
+          label: project.name,
+          value: project.id,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://www.pivotaltracker.com/services/v5";
+    },
+    _headers() {
+      return {
+        "X-TrackerToken": this.$auth.api_token,
+      };
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      const config = {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      };
+      return axios($, config);
+    },
+    createWebhook(projectId, args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/projects/${projectId}/webhooks`,
+        ...args,
+      });
+    },
+    deleteWebhook(projectId, webhookId) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/projects/${projectId}/webhooks/${webhookId}`,
+      });
+    },
+    listProjects(args = {}) {
+      return this._makeRequest({
+        path: "/projects",
+        ...args,
+      });
     },
   },
 };
