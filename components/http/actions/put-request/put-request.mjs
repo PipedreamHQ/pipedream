d1aa02bea21e784d2954032ece71b6571c8d1c53
Lucas Caresia <lucascarezia@gmail.com>
2023-02-24 15:32:17 -0300

    Added

--- components/http/actions/put-request/put-request.mjs
+++ components/http/actions/put-request/put-request.mjs
@@ -6,7 +6,7 @@ export default {
   name: "Send PUT Request",
   description: "Send an HTTP PUT request to any URL. Optionally configure query string parameters, headers and basic auth.",
   type: "action",
-  version: "1.1.0",
+  version: "1.1.1",
   props: {
     ...customRequest.props,
     /* eslint-disable-next-line pipedream/props-label,pipedream/props-description */
