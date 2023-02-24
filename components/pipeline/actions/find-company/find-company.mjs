24b9066f84f90936f9882f05b56ae8f7a03d7dad
michelle0927 <michellelbergero@hotmail.com>
2023-02-24 12:22:34 -0500

    new actions

---
+++ components/pipeline/actions/find-company/find-company.mjs
@@ -0,0 +1,68 @@
+import pipeline from "../../pipeline.app.mjs";
+
+export default {
+  name: "Find Company",
+  key: "pipeline-find-company",
+  description: "Find an existing company in your Pipeline account. [See the docs here](https://app.pipelinecrm.com/api/docs#tag/Companies/paths/~1companies/get)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    pipeline,
+    companyId: {
+      propDefinition: [
+        pipeline,
+        "companyId",
+      ],
+    },
+    name: {
+      propDefinition: [
+        pipeline,
+        "name",
+      ],
+      description: "Return companies that have a name that exactly matches or starts with the provided string",
+      optional: true,
+    },
+    email: {
+      propDefinition: [
+        pipeline,
+        "email",
+      ],
+      description: "Return companies that have an email that exactly matches or starts with the provided string",
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      conditions: {
+        company_id: this.companyId,
+        company_name: this.name,
+        company_email: this.email,
+      },
+    };
+
+    const companies = [];
+    let page = 1;
+    while (true) {
+      const params = {
+        page,
+      };
+      const {
+        entries, pagination,
+      } = await this.pipeline.listCompanies({
+        data,
+        params,
+        $,
+      });
+      companies.push(...entries);
+      page++;
+      if (page > pagination.pages) {
+        break;
+      }
+    }
+
+    $.export("$summary", `Found ${companies.length} matching compan${companies.length === 1
+      ? "y"
+      : "ies"}.`);
+
+    return companies;
+  },
+};
