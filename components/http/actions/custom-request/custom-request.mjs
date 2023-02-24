9700a03365563e5e7d447815a7fdaa5350d3be63
Lucas Caresia <lucascarezia@gmail.com>
2023-02-24 12:33:50 -0300

    [FEATURE] Include response headers in HTTP actions #5512 (#5545)
    * Added headers to return
    
    * Increasing package.json

--- components/http/actions/custom-request/custom-request.mjs
+++ components/http/actions/custom-request/custom-request.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Send any HTTP Request",
   description: "Send an HTTP request using any method and URL. Optionally configure query string parameters, headers, and basic auth.",
   type: "action",
-  version: "1.0.1",
+  version: "1.1.0",
   props: {
     http,
     httpRequest: {
@@ -16,6 +16,11 @@ export default {
     },
   },
   async run() {
-    return await this.httpRequest.execute();
+    const response = await this.httpRequest.execute();
+
+    return {
+      headers: response.headers,
+      data: response,
+    };
   },
 };
--- components/http/actions/put-request/put-request.mjs
+++ components/http/actions/put-request/put-request.mjs
@@ -6,7 +6,7 @@ export default {
   name: "Send PUT Request",
   description: "Send an HTTP PUT request to any URL. Optionally configure query string parameters, headers and basic auth.",
   type: "action",
-  version: "1.0.1",
+  version: "1.1.0",
   props: {
     ...customRequest.props,
     /* eslint-disable-next-line pipedream/props-label,pipedream/props-description */
