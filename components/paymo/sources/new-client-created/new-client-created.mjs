3761b0d1e918013efb60e1ec91968fca95fcf2d3
Alyson Freitas <103650302+alysonturing@users.noreply.github.com>
2023-02-27 14:41:34 -0300

    Update new-client-created.mjs
    changing version to 0.0.1

--- components/paymo/sources/new-client-created/new-client-created.mjs
+++ components/paymo/sources/new-client-created/new-client-created.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Client Created",
   description: "Emit new event when a new client is created. [See the docs](https://github.com/paymoapp/api/blob/master/sections/webhooks.md#events).",
   type: "source",
-  version: "0.0.4",
+  version: "0.0.1",
   dedupe: "unique",
   methods: {
     ...common.methods,
