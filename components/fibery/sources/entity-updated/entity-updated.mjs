2cabab3695d6be08fd33e1e6fab7f9c6f0a44ae9
Andrew Chuang <andrewjschuang@gmail.com>
2023-02-27 11:33:40 -0300

    filter out unnecessary events

--- components/fibery/sources/entity-updated/entity-updated.mjs
+++ components/fibery/sources/entity-updated/entity-updated.mjs
@@ -7,10 +7,17 @@ export default {
   description: "Emit new event for every updated entity of a certain type",
   type: "source",
   version: "0.0.1",
+  methods: {
+    ...base.methods,
+    hasUpdatedValue(valuesBefore) {
+      return Object.keys(valuesBefore).length > 1;
+    },
+  },
   async run(event) {
     console.log(`Received new event with ${event.body.effects.length} sequence(s)`);
     event.body.effects
       .filter(({ effect }) => effect === "fibery.entity/update")
+      .filter(({ valuesBefore }) => this.hasUpdatedValue(valuesBefore))
       .forEach((effect) => {
         this.$emit(effect, {
           summary: `Updated entity: ${effect.id}`,
