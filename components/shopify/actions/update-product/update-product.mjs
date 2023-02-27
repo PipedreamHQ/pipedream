c7dea9f8ad68a1e88050c682a48e4142f0a6dd4c
michelle0927 <michellelbergero@hotmail.com>
2023-02-27 13:11:24 -0500

    update metafields props

--- components/shopify/actions/update-product/update-product.mjs
+++ components/shopify/actions/update-product/update-product.mjs
@@ -72,10 +72,10 @@ export default {
       ],
     },
     metafields: {
-      type: "string[]",
-      label: "Metafields",
-      description: "An array of objects, each one representing a metafield. If adding a new metafield, the object should contain `key`, `value`, `type`, and `namespace`. Example: `{{ [{ \"key\": \"new\", \"value\": \"newvalue\", \"type\": \"single_line_text_field\", \"namespace\": \"global\" }] }}`. To update an existing metafield, use the `id` and `value`. Example: `{{ [{ \"id\": \"28408051400984\", \"value\": \"updatedvalue\" }] }}`",
-      optional: true,
+      propDefinition: [
+        shopify,
+        "metafields",
+      ],
     },
     seoTitle: {
       type: "string",
