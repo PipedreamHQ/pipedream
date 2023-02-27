3dfde32f59bc5c071e38d28b669508e01e3e614d
michelle0927 <michellelbergero@hotmail.com>
2023-02-27 16:21:52 -0500

    new-task-created

--- components/totango/sources/new-user/new-user.mjs
+++ components/totango/sources/new-user/new-user.mjs
@@ -3,9 +3,9 @@ import common from "../common/common.mjs";
 export default {
   ...common,
   name: "New User",
-  version: "0.0.2",
+  version: "0.0.3",
   key: "totango-new-user",
-  description: "Emit new event for each created user",
+  description: "Emit new event for each created user. [See the docs](https://support.totango.com/hc/en-us/articles/204174135-Search-API-accounts-and-users-)",
   type: "source",
   dedupe: "unique",
   methods: {
@@ -14,11 +14,17 @@ export default {
       this.$emit(data, {
         id: data.id,
         summary: `New user with id ${data.id}`,
-        ts: new Date(),
+        ts: data[this.getTimestampKey()],
       });
     },
     getResourceMethod() {
       return this.totango.searchUsers;
+    },
+    getTerms(lastCreated = 0) {
+      return `{ "type":"date", "term" : "first_activity_time", "gte" : ${lastCreated}}`;
+    },
+    getTimestampKey() {
+      return "first_activity_time";
     },
   },
 };
