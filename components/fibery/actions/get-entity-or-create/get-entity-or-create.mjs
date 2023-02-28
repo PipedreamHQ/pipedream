ed9ba2a604ba82efe4741d75a7a778b1fae10fc4
Andrew Chuang <andrewjschuang@gmail.com>
2023-02-28 15:04:47 -0300

    add get entity or create action

---
+++ components/fibery/actions/get-entity-or-create/get-entity-or-create.mjs
@@ -0,0 +1,78 @@
+import fibery from "../../fibery.app.mjs";
+import {
+  createMutation,
+  findQuery,
+} from "../../common/queries.mjs";
+
+export default {
+  key: "fibery-get-entity-or-create",
+  name: "Get Entity or Create",
+  description: "Get an entity or create one if it doesn't exist. [See the docs here](https://api.fibery.io/graphql.html#create)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    fibery,
+    space: {
+      propDefinition: [
+        fibery,
+        "space",
+      ],
+    },
+    entityType: {
+      propDefinition: [
+        fibery,
+        "entityType",
+        (c) => ({
+          space: c.space,
+        }),
+      ],
+    },
+    listingType: {
+      propDefinition: [
+        fibery,
+        "listingType",
+        (c) => ({
+          space: c.space,
+        }),
+      ],
+    },
+    filter: {
+      propDefinition: [
+        fibery,
+        "filter",
+      ],
+    },
+    attributes: {
+      propDefinition: [
+        fibery,
+        "attributes",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { data } = await this.fibery.makeGraphQLRequest({
+      $,
+      space: this.space,
+      listingType: this.listingType,
+      query: findQuery(this.listingType, this.filter),
+    });
+
+    const searchResults = data[this.listingType];
+
+    if (searchResults.length) {
+      const suffix = searchResults.length === 1
+        ? "y"
+        : "ies";
+      $.export("$summary", `Found ${searchResults.length} existing entit${suffix}`);
+      return searchResults;
+    }
+
+    const response = await this.fibery.makeGraphQLRequest({
+      $,
+      space: this.space,
+      query: createMutation(this.entityType, this.attributes),
+    });
+    $.export("$summary", "Succesfully created a new entity");
+    return response;
+  },
+};
