a496b3cb68f37fec6938af13991b89124b5f9b49
Stan Tsouvallas <stan.tsouvallas@irl.com>
2023-03-02 08:59:49 +0000

    Add memix random

---
+++ components/memix/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+With the memix.com API you can generate a Memix share URL with a random template using the supplied caption
---
+++ components/memix/actions/random/random.mjs
@@ -0,0 +1,29 @@
+import { axios } from "@pipedream/platform";
+export default {
+  name: "Caption to Memix URL",
+  description:
+    "Generate a Memix share URL with a random template using the supplied caption",
+  key: "memix_random",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    caption: {
+      type: "string",
+      label: "Caption",
+    },
+  },
+  async run() {
+    const data = await axios(this, {
+      method: "get",
+      headers: {
+        "X-API-Partner": "pipedream",
+      },
+      url: "https://partner-api.memix.com/templates",
+    });
+    var template = data[Math.floor(Math.random() * data.length)];
+    const memixUrl = `https://media.memix.com/memix-${
+      template.id
+    }.gif?text=${encodeURIComponent(this.caption)}`;
+    return memixUrl;
+  },
+};
