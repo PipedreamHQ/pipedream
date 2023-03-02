41cf19daf19abd998697d96806a210505deb406f
michelle0927 <michellelbergero@hotmail.com>
2023-03-02 11:54:11 -0500

    new sources

--- components/overloop/overloop.app.mjs
+++ components/overloop/overloop.app.mjs
@@ -1,11 +1,83 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "overloop",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.overloop.com/public/v1";
+    },
+    _headers() {
+      return {
+        Authorization: `${this.$auth.api_key}`,
+      };
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      const config = {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      };
+      return axios($, config);
+    },
+    listContacts(args = {}) {
+      return this._makeRequest({
+        path: "/contacts",
+        ...args,
+      });
+    },
+    listDeals(args = {}) {
+      return this._makeRequest({
+        path: "/deals",
+        ...args,
+      });
+    },
+    listLists(args = {}) {
+      return this._makeRequest({
+        path: "/lists",
+        ...args,
+      });
+    },
+    listPipelines(args = {}) {
+      return this._makeRequest({
+        path: "/pipelines",
+        ...args,
+      });
+    },
+    listStages(args = {}) {
+      return this._makeRequest({
+        path: "/stages",
+        ...args,
+      });
+    },
+    listUsers(args = {}) {
+      return this._makeRequest({
+        path: "/users",
+        ...args,
+      });
+    },
+    listOrganizations(args = {}) {
+      return this._makeRequest({
+        path: "/organizations",
+        ...args,
+      });
+    },
+    listAutomations(args = {}) {
+      return this._makeRequest({
+        path: "/automations",
+        ...args,
+      });
+    },
+    listExclusingListItems(args = {}) {
+      return this._makeRequest({
+        path: "/exclusion_list_items",
+        ...args,
+      });
     },
   },
 };
