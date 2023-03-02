6dfd0ea6262daa3c64dd4754684afdc15faffd87
Jorge Cortes <jacortesmahmud@gmail.com>
2023-03-02 08:38:38 -0500

    [ACTIONS/SOURCES] Paymo new actions and sources (#5561)
    * WIP scafolding
    
    * Added new actions and sources
    
    * Added pnpm-lock.yaml
    
    * Update new-client-created.mjs
    
    changing version to 0.0.1
    
    * Fix find task action
    
    ---------
    
    Co-authored-by: Alyson Freitas <103650302+alysonturing@users.noreply.github.com>
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/paymo/actions/create-project/create-project.mjs
@@ -0,0 +1,58 @@
+import app from "../../paymo.app.mjs";
+
+export default {
+  key: "paymo-create-project",
+  name: "Create Project",
+  description: "Creates a project. [See the docs](https://github.com/paymoapp/api/blob/master/sections/projects.md#create).",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the project.",
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "The description of the project.",
+      optional: true,
+    },
+    clientId: {
+      optional: true,
+      propDefinition: [
+        app,
+        "clientId",
+      ],
+    },
+  },
+  methods: {
+    createProject(args = {}) {
+      return this.app.create({
+        path: "/projects",
+        ...args,
+      });
+    },
+  },
+  async run({ $: step }) {
+    const {
+      name,
+      description,
+      clientId,
+    } = this;
+
+    const response = await this.createProject({
+      step,
+      data: {
+        name,
+        description,
+        client_id: clientId,
+      },
+    });
+
+    step.export("$summary", `Successfully created project with ID ${response.projects[0].id}`);
+
+    return response;
+  },
+};
