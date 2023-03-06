c30ae50b19d5f09fdff470d179da453c022ac7d9
michelle0927 <michellelbergero@hotmail.com>
2023-03-06 14:26:13 -0500

    update version

--- components/shopify/actions/update-product-variant/update-product-variant.mjs
+++ components/shopify/actions/update-product-variant/update-product-variant.mjs
@@ -7,7 +7,7 @@ export default {
   key: "shopify-update-product-variant",
   name: "Update Product Variant",
   description: "Update an existing product variant. [See the docs](https://shopify.dev/api/admin-rest/2022-01/resources/product-variant#[put]/admin/api/2022-01/variants/{variant_id}.json)",
-  version: "0.0.9",
+  version: "0.0.10",
   type: "action",
   props: {
     shopify,
