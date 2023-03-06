bfd1186f21f70b549380bfacb501a1bca619a522
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-06 18:52:01 -0300

    fix source name

--- components/eventbrite/sources/new-event-updated/new-event-updated.mjs
+++ components/eventbrite/sources/new-event-updated/new-event-updated.mjs
@@ -3,7 +3,7 @@ import common from "../common/event.mjs";
 export default {
   ...common,
   key: "eventbrite-new-event-updated",
-  name: "New Event Updated(Instant)",
+  name: "New Event Updated (Instant)",
   description: "Emit new event when an event has been updated",
   version: "0.0.1",
   type: "source",
