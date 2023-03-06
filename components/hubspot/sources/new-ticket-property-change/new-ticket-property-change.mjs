a3c6d486c7cb4fc9684220eb05ec28eefb38c007
michelle0927 <michelle0927@users.noreply.github.com>
2023-03-06 11:16:39 -0500

    Hubspot - New Sources and Updates (#5605)
    * add pipeline and stage props to new-deal and deal-updated
    
    * new property change sources
    
    * updates per review

---
+++ components/hubspot/sources/new-ticket-property-change/new-ticket-property-change.mjs
@@ -0,0 +1,77 @@
+import common from "../common.mjs";
+
+export default {
+  ...common,
+  key: "hubspot-new-ticket-property-change",
+  name: "New Ticket Property Change",
+  description: "Emit new event when a specified property is provided or updated on a ticket. [See the docs here](https://developers.hubspot.com/docs/api/crm/tickets)",
+  version: "0.0.1",
+  dedupe: "unique",
+  type: "source",
+  props: {
+    ...common.props,
+    property: {
+      type: "string",
+      label: "Property",
+      description: "The ticket property to watch for changes",
+      async options() {
+        const { results: properties } = await this.hubspot.getProperties("tickets");
+        return properties.map((property) => property.name);
+      },
+    },
+  },
+  hooks: {},
+  methods: {
+    ...common.methods,
+    getTs(ticket) {
+      const history = ticket.propertiesWithHistory[this.property];
+      if (!history || !(history.length > 0)) {
+        return;
+      }
+      return Date.parse(history[0].timestamp);
+    },
+    generateMeta(ticket) {
+      const {
+        id,
+        properties,
+      } = ticket;
+      const ts = this.getTs(ticket);
+      return {
+        id: `${id}${ts}`,
+        summary: properties.subject,
+        ts,
+      };
+    },
+    isRelevant(ticket, updatedAfter) {
+      return !updatedAfter || this.getTs(ticket) > updatedAfter;
+    },
+    getParams() {
+      return {
+        limit: 50,
+        sorts: [
+          {
+            propertyName: "hs_lastmodifieddate",
+            direction: "DESCENDING",
+          },
+        ],
+        propertiesWithHistory: this.property,
+      };
+    },
+    async processResults(after, params) {
+      const { results } = await this.hubspot.listObjectsInPage("tickets", null, params);
+
+      let maxTs = after;
+      for (const result of results) {
+        if (this.isRelevant(result, after)) {
+          this.emitEvent(result);
+          const ts = this.getTs(result);
+          if (ts > maxTs) {
+            maxTs = ts;
+          }
+        }
+      }
+
+      this._setAfter(maxTs);
+    },
+  },
+};
