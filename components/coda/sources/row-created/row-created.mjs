b9556627a553bdea77e75d3117b2f9ae7f3b546e
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-06 07:18:17 -0300

    move parts into methods

--- components/coda/sources/row-created/row-created.mjs
+++ components/coda/sources/row-created/row-created.mjs
@@ -51,55 +51,60 @@ export default {
     _setNextPageToken(nextPageToken) {
       nextPageToken && this.db.set("nextPageToken", nextPageToken);
     },
+    async fetchRows() {
+      const rows = [];
+      let nextPageToken = this._getNextPageToken();
+      const params = {
+        pageToken: nextPageToken,
+      };
+
+      while (true) {
+        const {
+          items = [],
+          nextPageToken,
+        } = await this.coda.findRow(
+          null,
+          this.docId,
+          this.tableId,
+          params,
+        );
+
+        rows.push(...items);
+        params.pageToken = nextPageToken;
+        this._setNextPageToken(nextPageToken);
+
+        if (!nextPageToken) {
+          return rows;
+        }
+      }
+    },
     rowSummary(row) {
       const name = row.name && ` - ${row.name}` || "";
       return `Row index: ${row.index}` + name;
     },
+    emitEvents(events) {
+      const emittedRows = this._getEmittedRows();
+
+      for (const row of events) {
+        const id = this.includeUpdates
+          ? `${row.id}-${row.updatedAt}`
+          : row.id;
+
+        if (!emittedRows.includes(id)) {
+          emittedRows.unshift(id);
+          this.$emit(row, {
+            id,
+            summary: this.rowSummary(row),
+            ts: row.updatedAt,
+          });
+        }
+      }
+
+      this._setEmittedRows(emittedRows);
+    },
   },
   async run() {
-    const rows = [];
-    const emittedRows = this._getEmittedRows();
-    let nextPageToken = this._getNextPageToken();
-    const params = {
-      pageToken: nextPageToken,
-    };
-
-    while (true) {
-      const {
-        items = [],
-        nextPageToken,
-      } = await this.coda.findRow(
-        null,
-        this.docId,
-        this.tableId,
-        params,
-      );
-
-      rows.push(...items);
-      params.pageToken = nextPageToken;
-      this._setNextPageToken(nextPageToken);
-
-      if (!nextPageToken) {
-        break;
-      }
-    }
-
-    for (const row of rows.reverse()) {
-      const id = this.includeUpdates
-        ? `${row.id}-${row.updatedAt}`
-        : row.id;
-
-      if (!emittedRows.includes(id)) {
-        this.$emit(row, {
-          id,
-          summary: this.rowSummary(row),
-          ts: row.updatedAt,
-        });
-
-        emittedRows.unshift(id);
-      }
-    }
-
-    this._setEmittedRows(emittedRows);
+    const rows = await this.fetchRows();
+    this.emitEvents(rows.reverse());
   },
 };
