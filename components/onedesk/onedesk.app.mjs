926ccd506a8cceaaaa15e926d1477688558aacc8
michelle0927 <michellelbergero@hotmail.com>
2023-03-06 16:42:56 -0500

    new sources

--- components/onedesk/onedesk.app.mjs
+++ components/onedesk/onedesk.app.mjs
@@ -1,11 +1,38 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "onedesk",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://app.onedesk.com/rest/2.0";
+    },
+    _authToken() {
+      return this.$auth.oauth_access_token;
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      data,
+      ...args
+    }) {
+      const config = {
+        url: `${this._baseUrl()}${path}`,
+        method: "POST",
+        data: {
+          ...data,
+          authenticationToken: this._authToken(),
+        },
+        ...args,
+      };
+      return axios($, config);
+    },
+    getItemUpdates(args = {}) {
+      return this._makeRequest({
+        path: "/history/getItemUpdates",
+        ...args,
+      });
     },
   },
 };
