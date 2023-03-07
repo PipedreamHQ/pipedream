002054b6de4181660f73e742728d40a1bb57dcd7
vellames <91911778+vellames@users.noreply.github.com>
2023-03-07 00:24:11 -0300

    [Actions] Orbit (#5601)
    * Create Acitivity
    
    * pnpm
    
    * Create Member
    
    * Create Member Note
    
    * Get Member by Identity
    
    * Initial Webhook structure
    
    * pnpm
    
    * CR

---
+++ components/orbit/actions/create-member-note/create-member-note.mjs
@@ -0,0 +1,44 @@
+import app from "../../orbit.app.mjs";
+
+export default {
+  name: "Create Member Note",
+  description: "Create a new member note. [See the docs here](https://api.orbit.love/reference/post_workspace-slug-members-member-slug-notes)",
+  key: "orbit-create-member-note",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    workspaceSlug: {
+      propDefinition: [
+        app,
+        "workspaceSlug",
+      ],
+    },
+    workspaceMemberSlug: {
+      propDefinition: [
+        app,
+        "workspaceMemberSlug",
+        ({ workspaceSlug }) => ({
+          workspaceSlug,
+        }),
+      ],
+    },
+    note: {
+      type: "string",
+      label: "Note",
+      description: "The note to add to the member.",
+    },
+  },
+  async run({ $ }) {
+    const note = {
+      body: this.note,
+    };
+    const res = await this.app.createNote(
+      this.workspaceSlug,
+      this.workspaceMemberSlug,
+      note,
+    );
+    $.export("$summary", `Note successfully created with id "${res.data.id}"`);
+    return res;
+  },
+};
