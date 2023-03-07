5e27352dfbed494c164b08fa976322c7679b19d8
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-07 11:14:29 -0300

    add list apis method

---
+++ components/swaggerhub/common/constants.mjs
@@ -0,0 +1,3 @@
+export default {
+  MAX_LIMIT: 100,
+};
--- components/swaggerhub/swaggerhub.app.mjs
+++ components/swaggerhub/swaggerhub.app.mjs
@@ -1,6 +1,65 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "swaggerhub",
   propDefinitions: {},
-  methods: {},
+  methods: {
+    _auth() {
+      return this.$auth.api_key;
+    },
+    _baseUrl() {
+      return "https://api.swaggerhub.com";
+    },
+    async _makeRequest({
+      $ = this, path = "", ...opts
+    }) {
+      const response = await axios($, {
+        ...opts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...opts.headers,
+          Authorization: this._auth(),
+        },
+      });
+      return response;
+    },
+    async listApis({
+      paginate = false, owner, ...opts
+    }) {
+      const path = `/apis/${owner}`;
+
+      if (paginate) {
+        const results = [];
+        let page = -1;
+
+        while (true) {
+          const { apis } = await this._makeRequest({
+            ...opts,
+            path,
+            params: {
+              ...opts.params,
+              page: ++page,
+              limit: constants.MAX_LIMIT,
+            },
+          });
+
+          results.push(...apis);
+
+          if (apis.length < constants.MAX_LIMIT) {
+            return {
+              apis: results,
+              page,
+            };
+          }
+        }
+      }
+
+      return this._makeRequest({
+        ...opts,
+        path,
+      });
+    },
+  },
 };
