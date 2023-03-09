4c51ae99d768eec4d80ada07d4c6c878a156ec4f
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-09 14:52:14 -0300

    init

--- components/nethunt_crm/nethunt_crm.app.mjs
+++ components/nethunt_crm/nethunt_crm.app.mjs
@@ -1,11 +1,27 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "nethunt_crm",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://nethunt.com/api/v1";
+    },
+    _auth() {
+      return {
+        username: this.$auth.email_address,
+        password: this.$auth.api_key,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        ...opts,
+        url: this._baseUrl() + path,
+        auth: this._auth(),
+      });
     },
   },
 };
