856625edc023733892caf0565e6f092bd12b0cc9
michelle0927 <michellelbergero@hotmail.com>
2023-03-10 12:02:54 -0500

    crove version

--- components/crove_app/actions/submit-document/submit-document.mjs
+++ components/crove_app/actions/submit-document/submit-document.mjs
@@ -4,7 +4,7 @@ export default {
   key: "crove_app-submit-document",
   name: "Submit Document",
   description: "Submit the document like you do it via Crove form.",
-  version: "1.0.1",
+  version: "1.0.2",
   type: "action",
   props: {
     croveApp,
