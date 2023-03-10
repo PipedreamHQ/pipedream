d3d0074edc41a3ffe30eff1df64b0e22dff04185
Cassiano Vellames <cassiano.v@turing.com>
2023-03-10 09:24:08 -0300

    Create Affiliate

--- components/refersion/refersion.app.mjs
+++ components/refersion/refersion.app.mjs
@@ -1,11 +1,44 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "refersion",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
+    _getBaseUrl() {
+      return "https://api.refersion.com/v2";
+    },
+    _getPublicKey() {
+      return this.$auth.public_key;
+    },
+    _getSecretKey() {
+      return this.$auth.secret_key;
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Refersion-Public-Key": this._getPublicKey(),
+        "Refersion-Secret-Key": this._getSecretKey(),
+      };
+    },
+    async _makeHttpRequest(opts = {}, ctx = this) {
+      const axiosOpts = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return axios(ctx, axiosOpts);
+    },
+    async createAffiliate(data, ctx = this) {
+      return this._makeHttpRequest({
+        path: "/affiliate/new",
+        method: "POST",
+        data,
+      }, ctx);
+    },
     authKeys() {
-      console.log(Object.keys(this.$auth));
+      console.log((this.$auth));
     },
   },
 };
