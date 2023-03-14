2298dda9dbac622d6ec16d548e264557a7011c5b
Lucas Caresia <lucascarezia@gmail.com>
2023-03-14 12:56:28 -0300

    Added filter to fields

--- components/clickup/sources/updated-task/updated-task.mjs
+++ components/clickup/sources/updated-task/updated-task.mjs
@@ -1,12 +1,13 @@
 import common from "../common/common.mjs";
 import app from "../../clickup.app.mjs";
+import constants from "../common/constants.mjs";
 
 export default {
   ...common,
   key: "clickup-updated-task",
   name: "New Updated Task (Instant)",
   description: "Emit new event when a new task is updated",
-  version: "0.0.5",
+  version: "0.0.6",
   dedupe: "unique",
   type: "source",
   props: {
@@ -21,6 +22,23 @@ export default {
       ],
       description: "If a list is selected, only tasks updated in this list will emit an event",
       optional: true,
+    },
+    customFieldIds: {
+      type: "string[]",
+      description: "Select a custom field to filter",
+      propDefinition: [
+        app,
+        "customFields",
+        (c) => ({
+          listId: c.listId,
+        }),
+      ],
+    },
+    fields: {
+      type: "string[]",
+      label: "Fields",
+      description: "Select a field to filter",
+      options: constants.TASK_FIELDS,
     },
   },
   methods: {
@@ -53,6 +71,24 @@ export default {
       if (id !== this.listId) return;
     }
 
-    this.$emit(httpRequest.body, this._getMeta(httpRequest.body.history_items));
+    let isValidated = false;
+
+    for (const item of body.history_items) {
+      if (this.fields.length && this.fields.includes(item.field)) {
+        isValidated = true;
+        break;
+      }
+
+      if (this.customFieldIds.length && item.custom_field &&
+        this.customFieldIds.includes(item.custom_field.id)) {
+        isValidated = true;
+        break;
+      }
+    }
+
+    if (isValidated) {
+      this.$emit(body, this._getMeta(body.history_items));
+    }
+
   },
 };
