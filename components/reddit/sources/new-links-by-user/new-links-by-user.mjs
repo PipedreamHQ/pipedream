278b62ab8dfa6efcbeb1505b1618a63dce6677f5
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-14 11:28:54 -0300

    reddit

--- components/reddit/sources/new-links-by-user/new-links-by-user.mjs
+++ components/reddit/sources/new-links-by-user/new-links-by-user.mjs
@@ -7,7 +7,7 @@ export default {
   key: "reddit-new-links-by-user",
   name: "New Links by User",
   description: "Emit new event each time a user posts a new link.",
-  version: "0.0.8",
+  version: "0.1.0",
   dedupe: "unique",
   props: {
     ...common.props,
