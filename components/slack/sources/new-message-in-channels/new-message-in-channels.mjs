47ec3e9217115b967160920e05c335c4e23014dd
michelle0927 <michelle0927@users.noreply.github.com>
2023-03-16 15:04:34 -0700

    typo fix (#5763)

--- components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
+++ components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
@@ -5,7 +5,7 @@ export default {
   ...common,
   key: "slack-new-message-in-channels",
   name: "New Message In Channels (Instant)",
-  version: "1.0.7",
+  version: "1.0.8",
   description: "Emit new event when a new message is posted to one or more channels",
   type: "source",
   dedupe: "unique",
