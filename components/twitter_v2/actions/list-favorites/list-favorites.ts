1a26405f721a6b7e024f2dbbe8af9f0faf6687c4
GTFalcao <gtfalcao96@gmail.com>
2023-03-18 19:57:37 -0300

    creating source: New Tweet Liked By User

--- components/twitter_v2/actions/list-favorites/list-favorites.ts
+++ components/twitter_v2/actions/list-favorites/list-favorites.ts
@@ -6,11 +6,11 @@ import {
 import { tweetFieldProps } from "../../common/propGroups";
 import { GetLikedTweetParams } from "../../common/types/requestParams";
 
-const DOCS_LINK =
+export const DOCS_LINK =
   "https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-users-id-liked_tweets";
 const MIN_RESULTS = 10;
 const DEFAULT_RESULTS = 100;
-const MAX_RESULTS_PER_PAGE = 100;
+export const MAX_RESULTS_PER_PAGE = 100;
 
 export default defineAction({
   key: "twitter_v2-list-favorites",
---
+++ components/twitter_v2/sources/new-tweet-liked-by-user/new-tweet-liked-by-user.ts
@@ -0,0 +1,55 @@
+import app from "../../app/twitter_v2.app";
+import { defineSource } from "@pipedream/types";
+import common from "../common/base";
+import { getTweetSummary as getItemSummary } from "../common/getItemSummary";
+import { tweetFieldProps } from "../../common/propGroups";
+import {
+  getUserId, getTweetFields,
+} from "../../common/methods";
+import { GetLikedTweetParams } from "../../common/types/requestParams";
+import {
+  DOCS_LINK, MAX_RESULTS_PER_PAGE,
+} from "../../actions/list-favorites/list-favorites";
+import { Tweet } from "../../common/types/responseSchemas";
+
+export default defineSource({
+  ...common,
+  key: "twitter_v2-new-tweet-liked-by-user",
+  name: "New Tweet Liked By User",
+  description: `Emit new event when the specified user follows another user [See docs here](${DOCS_LINK})`,
+  version: "0.0.1",
+  type: "source",
+  props: {
+    ...common.props,
+    userNameOrId: {
+      propDefinition: [
+        app,
+        "userNameOrId",
+      ],
+    },
+    ...tweetFieldProps,
+  },
+  methods: {
+    ...common.methods,
+    getUserId,
+    getTweetFields,
+    getItemSummary,
+    getEntityName() {
+      return "Tweet Liked";
+    },
+    async getResources(customize: boolean): Promise<Tweet[]> {
+      const params: Partial<GetLikedTweetParams> = {
+        $: this,
+        maxPerPage: MAX_RESULTS_PER_PAGE,
+        maxResults: MAX_RESULTS_PER_PAGE,
+      };
+
+      if (customize) {
+        params.userId = this.getUserId();
+        params.params = this.getTweetFields();
+      }
+
+      return this.app.getLikedTweets(params);
+    },
+  },
+});
