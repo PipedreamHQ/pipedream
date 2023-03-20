5df82ca3282b7407049f244aca99550393c6882f
Cassiano Vellames <cassiano.v@turing.com>
2023-03-20 10:47:05 -0300

    Refersion

---
+++ components/refersion/common/options.mjs
@@ -0,0 +1,372 @@
+export default {
+  COUNTRIES: [
+    {
+      "value": "US",
+      "label": "United States",
+    },
+    {
+      "value": "AE",
+      "label": "United Arab Emirates",
+    },
+    {
+      "value": "AR",
+      "label": "Argentina",
+    },
+    {
+      "value": "AS",
+      "label": "American Samoa",
+    },
+    {
+      "value": "AT",
+      "label": "Austria",
+    },
+    {
+      "value": "AU",
+      "label": "Australia",
+    },
+    {
+      "value": "BA",
+      "label": "Bosnia and Herzegovina",
+    },
+    {
+      "value": "BD",
+      "label": "Bangladesh",
+    },
+    {
+      "value": "BE",
+      "label": "Belgium",
+    },
+    {
+      "value": "BG",
+      "label": "Bulgaria",
+    },
+    {
+      "value": "BR",
+      "label": "Brazil",
+    },
+    {
+      "value": "CA",
+      "label": "Canada",
+    },
+    {
+      "value": "CH",
+      "label": "Switzerland",
+    },
+    {
+      "value": "CL",
+      "label": "Chile",
+    },
+    {
+      "value": "CN",
+      "label": "China",
+    },
+    {
+      "value": "CO",
+      "label": "Colombia",
+    },
+    {
+      "value": "CR",
+      "label": "Costa Rica",
+    },
+    {
+      "value": "CZ",
+      "label": "Czech Republic",
+    },
+    {
+      "value": "DE",
+      "label": "Germany",
+    },
+    {
+      "value": "DK",
+      "label": "Denmark",
+    },
+    {
+      "value": "DZ",
+      "label": "Algeria",
+    },
+    {
+      "value": "EE",
+      "label": "Estonia",
+    },
+    {
+      "value": "ES",
+      "label": "Spain",
+    },
+    {
+      "value": "FI",
+      "label": "Finland",
+    },
+    {
+      "value": "FM",
+      "label": "Micronesia, Federated States of",
+    },
+    {
+      "value": "FO",
+      "label": "Faroe Islands",
+    },
+    {
+      "value": "FR",
+      "label": "France",
+    },
+    {
+      "value": "GB",
+      "label": "United Kingdom",
+    },
+    {
+      "value": "GH",
+      "label": "Ghana",
+    },
+    {
+      "value": "GR",
+      "label": "Greece",
+    },
+    {
+      "value": "GU",
+      "label": "Guam",
+    },
+    {
+      "value": "HK",
+      "label": "Hong Kong",
+    },
+    {
+      "value": "HN",
+      "label": "Honduras",
+    },
+    {
+      "value": "HR",
+      "label": "Croatia",
+    },
+    {
+      "value": "HU",
+      "label": "Hungary",
+    },
+    {
+      "value": "ID",
+      "label": "Indonesia",
+    },
+    {
+      "value": "IE",
+      "label": "Ireland",
+    },
+    {
+      "value": "IL",
+      "label": "Israel",
+    },
+    {
+      "value": "IM",
+      "label": "Isle of Man",
+    },
+    {
+      "value": "IN",
+      "label": "India",
+    },
+    {
+      "value": "IS",
+      "label": "Iceland",
+    },
+    {
+      "value": "IT",
+      "label": "Italy",
+    },
+    {
+      "value": "JM",
+      "label": "Jamaica",
+    },
+    {
+      "value": "JP",
+      "label": "Japan",
+    },
+    {
+      "value": "KR",
+      "label": "South Korea",
+    },
+    {
+      "value": "KW",
+      "label": "Kuwait",
+    },
+    {
+      "value": "LT",
+      "label": "Lithuania",
+    },
+    {
+      "value": "LU",
+      "label": "Luxembourg",
+    },
+    {
+      "value": "LV",
+      "label": "Latvia",
+    },
+    {
+      "value": "MA",
+      "label": "Morocco",
+    },
+    {
+      "value": "MH",
+      "label": "Marshall Islands",
+    },
+    {
+      "value": "MK",
+      "label": "North Macedonia",
+    },
+    {
+      "value": "MP",
+      "label": "Northern Mariana Islands",
+    },
+    {
+      "value": "MT",
+      "label": "Malta",
+    },
+    {
+      "value": "MX",
+      "label": "Mexico",
+    },
+    {
+      "value": "MY",
+      "label": "Malaysia",
+    },
+    {
+      "value": "NG",
+      "label": "Nigeria",
+    },
+    {
+      "value": "NL",
+      "label": "Netherlands",
+    },
+    {
+      "value": "NO",
+      "label": "Norway",
+    },
+    {
+      "value": "NZ",
+      "label": "New Zealand",
+    },
+    {
+      "value": "OM",
+      "label": "Oman",
+    },
+    {
+      "value": "PE",
+      "label": "Peru",
+    },
+    {
+      "value": "PH",
+      "label": "Philippines",
+    },
+    {
+      "value": "PK",
+      "label": "Pakistan",
+    },
+    {
+      "value": "PL",
+      "label": "Poland",
+    },
+    {
+      "value": "PR",
+      "label": "Puerto Rico",
+    },
+    {
+      "value": "PT",
+      "label": "Portugal",
+    },
+    {
+      "value": "PW",
+      "label": "Palau",
+    },
+    {
+      "value": "QA",
+      "label": "Qatar",
+    },
+    {
+      "value": "RO",
+      "label": "Romania",
+    },
+    {
+      "value": "RS",
+      "label": "Serbia",
+    },
+    {
+      "value": "RU",
+      "label": "Russia",
+    },
+    {
+      "value": "SA",
+      "label": "Saudi Arabia",
+    },
+    {
+      "value": "SE",
+      "label": "Sweden",
+    },
+    {
+      "value": "SG",
+      "label": "Singapore",
+    },
+    {
+      "value": "SI",
+      "label": "Slovenia",
+    },
+    {
+      "value": "SK",
+      "label": "Slovakia",
+    },
+    {
+      "value": "SV",
+      "label": "El Salvador",
+    },
+    {
+      "value": "TH",
+      "label": "Thailand",
+    },
+    {
+      "value": "TN",
+      "label": "Tunisia",
+    },
+    {
+      "value": "TR",
+      "label": "Turkey",
+    },
+    {
+      "value": "TW",
+      "label": "Taiwan",
+    },
+    {
+      "value": "TZ",
+      "label": "Tanzania",
+    },
+    {
+      "value": "UA",
+      "label": "Ukraine",
+    },
+    {
+      "value": "UY",
+      "label": "Uruguay",
+    },
+    {
+      "value": "VG",
+      "label": "British Virgin Islands",
+    },
+    {
+      "value": "VI",
+      "label": "U.S. Virgin Islands",
+    },
+    {
+      "value": "VN",
+      "label": "Vietnam",
+    },
+    {
+      "value": "WS",
+      "label": "Samoa",
+    },
+    {
+      "value": "ZA",
+      "label": "South Africa",
+    },
+  ],
+  AFFILIATE_STATUS: [
+    "PENDING",
+    "ACTIVE",
+  ],
+  MANUAL_COMMISSION_CREDIT_STATUS: [
+    "PENDING",
+    "APPROVED",
+  ],
+};
