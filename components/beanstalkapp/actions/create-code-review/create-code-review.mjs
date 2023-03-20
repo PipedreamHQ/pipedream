d6d09c68d2e32889ff2fd6c96970e8ccf9074adb
Jorge Cortes <jacortesmahmud@gmail.com>
2023-03-20 12:24:54 -0500

    [ACTIONS/SOURCES] Created new actions and sources (#5768)
    * Created new actions and sources
    
    * Increased package.json
    
    * new line in the EOF

---
+++ components/beanstalkapp/actions/create-code-review/create-code-review.mjs
@@ -0,0 +1,100 @@
+import app from "../../beanstalkapp.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  key: "beanstalkapp-create-code-review",
+  name: "Create Code Review",
+  description: "Creates a new code review. This action is essentially the same as clicking the “Request review” button in the app. [See the docs](https://api.beanstalkapp.com/code_review.html).",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    repositoryId: {
+      propDefinition: [
+        app,
+        "repositoryId",
+      ],
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "Description of the **Target Branch**.",
+      optional: true,
+    },
+    targetBranch: {
+      label: "Target Branch",
+      description: "Name of the branch that’s being reviewed.",
+      propDefinition: [
+        app,
+        "branch",
+        ({ repositoryId }) => ({
+          repositoryId,
+        }),
+      ],
+    },
+    sourceBranch: {
+      label: "Source Branch",
+      description: "Name of the branch to which **Target Branch** should be compared to.",
+      propDefinition: [
+        app,
+        "branch",
+        ({ repositoryId }) => ({
+          repositoryId,
+        }),
+      ],
+    },
+    merge: {
+      type: "boolean",
+      label: "Merge",
+      description: "Merge branches when review gets approved. `False` by default.",
+      optional: true,
+    },
+    assigneeIds: {
+      type: "string[]",
+      label: "Assignee IDs",
+      description: "IDs of the users who will be assigned to the code review.",
+      propDefinition: [
+        app,
+        "userId",
+      ],
+    },
+  },
+  methods: {
+    createCodeReview({
+      repositoryId, ...args
+    } = {}) {
+      return this.app.create({
+        path: `/${repositoryId}/code_reviews`,
+        ...args,
+      });
+    },
+  },
+  async run({ $: step }) {
+    const {
+      repositoryId,
+      description,
+      targetBranch,
+      sourceBranch,
+      merge,
+      assigneeIds,
+    } = this;
+
+    const response = await this.createCodeReview({
+      step,
+      repositoryId,
+      data: {
+        code_review: {
+          description,
+          target_branch: targetBranch,
+          source_branch: sourceBranch,
+          merge,
+          assignee_ids: utils.parseArray(assigneeIds),
+        },
+      },
+    });
+
+    step.export("$summary", `Successfully created code review with ID ${response.id}`);
+
+    return response;
+  },
+};
---
+++ components/beanstalkapp/sources/webhook-integration-triggered/webhook-integration-triggered.mjs
@@ -0,0 +1,95 @@
+import common from "../common/webhook.mjs";
+
+export default {
+  ...common,
+  key: "beanstalkapp-webhook-integration-triggered",
+  name: "New Webhook Integration Triggered (Instant)",
+  description: "Emit new event when selected flags trigger the webhook. [See the docs](https://api.beanstalkapp.com/integration.html#webhooks).",
+  type: "source",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    commit:	{
+      type: "boolean",
+      label: "Commit",
+      description: "On each commit (Subversion only).",
+      optional: true,
+    },
+    push:	{
+      type: "boolean",
+      label: "Push",
+      description: "On each push (Git only).",
+      optional: true,
+    },
+    deploy:	{
+      type: "boolean",
+      label: "Deploy",
+      description: "Every time a deployment is finished.",
+      optional: true,
+    },
+    comment:	{
+      type: "boolean",
+      label: "Comment",
+      description: "Any comment is posted on a changeset, a file or a code review.",
+      optional: true,
+    },
+    create_branch:	{
+      type: "boolean",
+      label: "Create Branch",
+      description: "When a branch is created.",
+      optional: true,
+    },
+    delete_branch:	{
+      type: "boolean",
+      label: "Delete Branch",
+      description: "When a branch is deleted.",
+      optional: true,
+    },
+    create_tag:	{
+      type: "boolean",
+      label: "Create Tag",
+      description: "When a tag is created.",
+      optional: true,
+    },
+    delete_tag: {
+      type: "boolean",
+      label: "Delete Tag",
+      description: "When a tag is deleted.",
+      optional: true,
+    },
+  },
+  methods: {
+    ...common.methods,
+    getEventName() {
+      const {
+        commit,
+        push,
+        deploy,
+        comment,
+        create_branch,
+        delete_branch,
+        create_tag,
+        delete_tag,
+      } = this;
+      return {
+        commit,
+        push,
+        deploy,
+        comment,
+        create_branch,
+        delete_branch,
+        create_tag,
+        delete_tag,
+      };
+    },
+    generateMeta() {
+      const ts = Date.now();
+      return {
+        id: ts,
+        summary: "New Event Triggered",
+        ts,
+      };
+    },
+  },
+};
