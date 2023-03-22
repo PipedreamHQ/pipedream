c25e5a335561a8447b82970bebd990a20077d8f5
Cassiano Vellames <cassiano.v@turing.com>
2023-03-22 10:47:16 -0300

    Recreating commit

---
+++ components/savvycal/common/options.mjs
@@ -0,0 +1,12 @@
+export default {
+  WEBHOOK_EVENT_TYPES: [
+    "event.created",
+    "event.requested",
+    "event.approved",
+    "event.declined",
+    "event.rescheduled",
+    "event.canceled",
+    "poll.response.created",
+    "workflow.action.triggered",
+  ],
+};
