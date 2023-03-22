94af02017b8b47b962a05da773d608c044317493
michelle0927 <michelle0927@users.noreply.github.com>
2023-03-22 07:48:16 -0700

    update new-deal-in-stage (#5818)

--- components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
+++ components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
@@ -5,7 +5,7 @@ export default {
   key: "hubspot-new-deal-in-stage",
   name: "New Deal In Stage",
   description: "Emit new event for each new deal in a stage.",
-  version: "0.0.12",
+  version: "0.0.13",
   dedupe: "unique",
   type: "source",
   hooks: {},
@@ -29,29 +29,28 @@ export default {
   },
   methods: {
     ...common.methods,
-    getTs(deal) {
-      return Date.parse(deal.updatedAt);
-    },
-    generateMeta(deal) {
-      const {
-        id,
-        properties,
-      } = deal;
-      const ts = this.getTs(deal);
-      return {
-        id: `${id}${properties.dealstage}`,
-        summary: `${properties.dealname}`,
-        ts,
-      };
-    },
-    async isRelevant(deal, updatedAfter) {
+    async getTs(deal) {
       const { properties } = await this.hubspot.getDeal({
         dealId: deal.id,
         params: {
           includePropertyVersions: true,
         },
       });
-      return properties.dealstage?.versions[0].timestamp > updatedAfter;
+      return properties.dealstage?.versions[0].timestamp;
+    },
+    emitEvent(deal, ts) {
+      const {
+        id,
+        properties,
+      } = deal;
+      this.$emit(deal, {
+        id: `${id}${properties.dealstage}`,
+        summary: `${properties.dealname}`,
+        ts,
+      });
+    },
+    isRelevant(ts, updatedAfter) {
+      return ts > updatedAfter;
     },
     getParams() {
       return null;
@@ -81,10 +80,38 @@ export default {
         object: "deals",
       };
     },
+    async processDeals(params, after) {
+      let maxTs = after || 0;
+      const limiter = this._limiter();
+
+      do {
+        const results = await this._requestWithLimiter(
+          limiter,
+          this.hubspot.searchCRM.bind(this),
+          params,
+        );
+        if (results.paging) {
+          params.after = results.paging.next.after;
+        } else {
+          delete params.after;
+        }
+
+        for (const deal of results.results) {
+          const ts = await this.getTs(deal);
+          if (this.isRelevant(ts, after)) {
+            this.emitEvent(deal, ts);
+            if (ts > maxTs) {
+              maxTs = ts;
+              this._setAfter(ts);
+            }
+          }
+        }
+      } while (params.after);
+    },
     async processResults(after) {
-      for (let stage of this.stages) {
+      for (const stage of this.stages) {
         const params = this.getStageParams(stage);
-        await this.searchCRM(params, after);
+        await this.processDeals(params, after);
       }
     },
   },
