a091e4894c114b2bb095a891248d5b0e8d2b88a8
Lucas Caresia <lucascarezia@gmail.com>
2023-03-23 09:21:31 -0300

    Update components/mysql/mysql.app.mjs
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

--- components/mysql/mysql.app.mjs
+++ components/mysql/mysql.app.mjs
@@ -115,7 +115,7 @@ export default {
         password,
         database,
         ssl: {
-          rejectUnauthorized: rejectUnauthorized ?? false,
+          rejectUnauthorized,
         },
       });
     },
