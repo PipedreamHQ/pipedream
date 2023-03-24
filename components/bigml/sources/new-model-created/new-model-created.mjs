b435810a12124d3da0c27fbabf223c8899c13b8c
Andrew Chuang <andrewjschuang@gmail.com>
2023-03-24 09:37:33 -0300

    [BigML] New Components (#5831)
    * init
    
    * add dataset id prop
    
    * add source and model id props
    
    * pagination return object with same structure
    
    * add new model created source
    
    * add new prediction made source
    
    * refactor sources to use base
    
    * add create model action
    
    * add create batch prediction action
    
    * add create source remote url action
    
    * bump package.json

---
+++ components/bigml/sources/new-model-created/new-model-created.mjs
@@ -0,0 +1,24 @@
+import base from "../common/base.mjs";
+
+export default {
+  ...base,
+  key: "bigml-new-model-created",
+  name: "New Model Created",
+  description: "Emit new event for every created model. [See docs here.](https://bigml.com/api/models?id=listing-models)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...base.methods,
+    listingFunction() {
+      return this.bigml.listModels;
+    },
+    emitEvent(model) {
+      this.$emit(model, {
+        id: model.resource,
+        summary: `New model created: ${model.name}`,
+        ts: model.created,
+      });
+    },
+  },
+};
