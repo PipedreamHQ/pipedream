b55c2781c9c6648ff0f080b25e24e032e5c26089
Dylan J. Sather <dylan.sather@gmail.com>
2023-03-26 15:36:58 -0700

    Another rejectUnauthorized fix

--- components/mysql/actions/execute-stored-procedure/execute-stored-procedure.mjs
+++ components/mysql/actions/execute-stored-procedure/execute-stored-procedure.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Execute Stored Procedure",
   description: "Execute Stored Procedure. [See the docs here](https://dev.mysql.com/doc/refman/8.0/en/stored-programs-defining.html)",
   type: "action",
-  version: "0.0.3",
+  version: "0.0.4",
   props: {
     mysql,
     rejectUnauthorized: {
