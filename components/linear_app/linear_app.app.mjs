de6f4058a0e9097b21e0749ead53c1143ae322e2
Jorge Cortes <jacortesmahmud@gmail.com>
2023-03-28 18:25:21 -0500

    Added stateId prop to linear and linear_app

--- components/linear_app/linear_app.app.mjs
+++ components/linear_app/linear_app.app.mjs
@@ -6,23 +6,6 @@ export default {
   type: "app",
   app: "linear_app",
   propDefinitions: {
-    issueId: {
-      type: "string",
-      label: "Issue ID",
-      description: "The issue ID to update",
-      async options({ prevContext }) {
-        return this.listResourcesOptions({
-          prevContext,
-          resourcesFn: this.listIssues,
-          resouceMapper: ({
-            id, title,
-          }) => ({
-            label: title,
-            value: id,
-          }),
-        });
-      },
-    },
     teamId: {
       type: "string",
       label: "Team ID",
@@ -35,6 +18,34 @@ export default {
             id, name,
           }) => ({
             label: name,
+            value: id,
+          }),
+        });
+      },
+    },
+    issueId: {
+      type: "string",
+      label: "Issue ID",
+      description: "The issue ID to update",
+      async options({
+        teamId, prevContext,
+      }) {
+        return this.listResourcesOptions({
+          prevContext,
+          resourcesFn: this.listIssues,
+          resourcesArgs: teamId && {
+            filter: {
+              team: {
+                id: {
+                  eq: teamId,
+                },
+              },
+            },
+          },
+          resouceMapper: ({
+            id, title,
+          }) => ({
+            label: title,
             value: id,
           }),
         });
@@ -77,6 +88,35 @@ export default {
           }) => ({
             label: name,
             value: id,
+          }),
+        });
+      },
+    },
+    stateId: {
+      type: "string",
+      label: "State ID",
+      description: "The identifier of the state to move the issue to",
+      optional: true,
+      async options({
+        teamId, prevContext,
+      }) {
+        return this.listResourcesOptions({
+          prevContext,
+          resourcesFn: this.listStates,
+          resourcesArgs: teamId && {
+            filter: {
+              team: {
+                id: {
+                  eq: teamId,
+                },
+              },
+            },
+          },
+          resouceMapper: ({
+            id: value, name: label,
+          }) => ({
+            label,
+            value,
           }),
         });
       },
@@ -184,6 +224,9 @@ export default {
     async listUsers(variables = {}) {
       return this.client().users(variables);
     },
+    async listStates(variables = {}) {
+      return this.client().workflowStates(variables);
+    },
     async listIssues(variables = {}) {
       return this.client().issues(variables);
     },
@@ -194,8 +237,8 @@ export default {
       return this.client().comments(variables);
     },
     async listResourcesOptions({
-      prevContext, resourcesFn, resouceMapper,
-    }) {
+      prevContext, resourcesFn, resourcesArgs, resouceMapper,
+    } = {}) {
       const {
         after,
         hasNextPage,
@@ -212,6 +255,7 @@ export default {
         await resourcesFn({
           after,
           first: constants.DEFAULT_LIMIT,
+          ...resourcesArgs,
         });
 
       return {
