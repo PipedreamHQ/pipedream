9227e9602558f2ad261daf62d70da7ae8d694a29
Lucas Caresia <lucascarezia@gmail.com>
2023-03-28 12:54:18 -0300

    Actions and sources

---
+++ components/maintainx/actions/update-work-order/update-work-order.mjs
@@ -0,0 +1,52 @@
+import maintainx from "../../maintainx.app.mjs";
+import constants from "../common/constants.mjs";
+
+export default {
+  name: "Update Work Order",
+  version: "0.0.1",
+  key: "maintainx-update-work-order",
+  description: "Updates a client. [See docs here](https://api.getmaintainx.com/v1/docs#tag/Work-Orders/paths/~1workorders~1{id}/patch)",
+  type: "action",
+  props: {
+    maintainx,
+    workorderId: {
+      propDefinition: [
+        maintainx,
+        "workorderId",
+      ],
+    },
+    title: {
+      type: "string",
+      label: "Title",
+      description: "The title of the work order",
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "The description of the work order",
+    },
+    priority: {
+      type: "string",
+      label: "Priority",
+      description: "The priority of the work order. E.g. `LOW`",
+      options: constants.PRIORITIES,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.maintainx.updateWorkOrder({
+      $,
+      workorderId: this.workorderId,
+      data: {
+        title: this.title,
+        description: this.description,
+        priority: this.priority,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully updated work order with ID ${response.workOrder.id}`);
+    }
+
+    return response;
+  },
+};
---
+++ components/maintainx/sources/new-work-order-completed/new-work-order-completed.mjs
@@ -0,0 +1,76 @@
+import maintainx from "../../maintainx.app.mjs";
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+
+export default {
+  name: "New Payment",
+  version: "0.0.1",
+  key: "maintainx-new-work-order-completed",
+  description: "Emit new event on each new work order is completed.",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    maintainx,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      static: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  methods: {
+    emitEvent(data) {
+      this._setLastWorkOrderId(data.id);
+
+      this.$emit(data, {
+        id: data.id,
+        summary: `New work order with ID ${data.id}`,
+        ts: Date.parse(data.createdAt),
+      });
+    },
+    _setLastWorkOrderId(id) {
+      this.db.set("lastWorkOrderId", id);
+    },
+    _getLastWorkOrderId() {
+      return this.db.get("lastWorkOrderId");
+    },
+  },
+  hooks: {
+    async deploy() {
+      const { workOrders } = await this.maintainx.getWorkOrders({
+        params: {
+          limit: 10,
+        },
+      });
+
+      workOrders.reverse().forEach(this.emitEvent);
+    },
+  },
+  async run() {
+    const lastWorkOrderId = this._getLastWorkOrderId();
+
+    let cursor;
+
+    do {
+      const {
+        nextCursor, workOrders,
+      } = await this.maintainx.getWorkOrders({
+        params: {
+          cursor,
+          limit: 100,
+        },
+      });
+
+      workOrders.reverse().forEach(this.emitEvent);
+
+      if (
+        workOrders.length < 100 ||
+        workOrders.filter((workOrder) => workOrder.id === lastWorkOrderId)
+      ) {
+        return;
+      }
+
+      cursor = nextCursor;
+    } while (cursor);
+  },
+};
