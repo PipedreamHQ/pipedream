6eb438db9a5e09a91865bbd5be37ccfcd0465bbf
feyzullah <feyzullah.metu@gmail.com>
2023-03-29 15:43:41 +0200

    Kanban Tool Components (#5868)
    * Kanban Tool Components
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/kanban_tool/actions/find-task/find-task.mjs
@@ -0,0 +1,54 @@
+import app from "../../kanban_tool.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  key: "kanban_tool-find-task",
+  version: "0.0.1",
+  type: "action",
+  name: "Find Task",
+  description: "Finds task with given parameters [See the docs here](https://kanbantool.com/developer/api-v3#searching-tasks)",
+  props: {
+    app,
+    searchQuery: {
+      propDefinition: [
+        app,
+        "searchQuery",
+      ],
+    },
+    boardId: {
+      propDefinition: [
+        app,
+        "boardId",
+      ],
+      optional: true,
+    },
+    isArchived: {
+      propDefinition: [
+        app,
+        "isArchived",
+      ],
+      optional: true,
+    },
+  },
+  async run ({ $ }) {
+    const resourcesStream = utils.getResourcesStream({
+      resourceFn: this.app.searchTasks,
+      resourceKey: "results",
+      resourceFnArgs: {
+        $,
+        params: {
+          board_id: this.boardId,
+          // eslint-disable-next-line multiline-ternary
+          archived: this.isArchived ? 1 : 0,
+          q: this.searchQuery ?? "",
+        },
+      },
+    });
+    const items = [];
+    for await (const item of resourcesStream)
+      items.push(item);
+    // eslint-disable-next-line multiline-ternary
+    $.export("$summary", `${items.length} task${items.length != 1 ? "s" : ""} has been found.`);
+    return items;
+  },
+};
