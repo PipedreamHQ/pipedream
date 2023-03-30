456c8339c219609f41d7249bc25b3edfc1a4b8ed
Luan Cazarine <luanhc@gmail.com>
2023-03-30 14:06:17 -0300

    fix props

--- components/automatic_data_extraction/actions/extract-data-from-url/extract-data-from-url.mjs
+++ components/automatic_data_extraction/actions/extract-data-from-url/extract-data-from-url.mjs
@@ -25,21 +25,25 @@ export default {
       type: "string",
       label: "Meta",
       description: "User UTF-8 string, which will be passed through the extraction pipeline and returned in the query result. Max size 4 Kb.",
+      optional: true,
     },
     articleBodyRaw: {
       type: "boolean",
       label: "Article Body Raw",
       description: "Whether or not to include article HTML in article extractions. True by default. Setting this to false can reduce response size significantly if HTML is not required.",
+      optional: true,
     },
     fullHtml: {
       type: "boolean",
       label: "Full HTML",
       description: "Include the full, raw HTML of the target web page in the query result. This is a premium feature that is disabled by default. [Open a support ticket](https://support.zyte.com/support/tickets/new) if you wish to have it enabled for your account.",
+      optional: true,
     },
     customHtml: {
       type: "string",
       label: "Custom HTML",
       description: "HTML source to be scraped. Extraction will be done from the provided HTML with additional resources (images, CSS, etc.) downloaded from the provided url. JavaScript processing will be disabled. The String should be UTF-8 encoded. The maximum length is 2,000,000 characters, longer requests will be rejected.",
+      optional: true,
     },
   },
   async run({ $ }) {
