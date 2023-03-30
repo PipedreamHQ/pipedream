6f71355f378c154f0c85566d613cbc5f9c9e1c7a
Luan Cazarine <luanhc@gmail.com>
2023-03-30 12:08:04 -0300

    5824 trigger gmail new labeled email and new email matching search more broadly (#5872)
    * [TRIGGER] Gmail new labeled email (and new email matching search more broadly #5824
    Source - New Labeled Email
    
    * pnpm update
    
    * version update
    
    * Update components/gmail_custom_oauth/sources/new-labeled-email/new-labeled-email.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/gmail_custom_oauth/sources/new-labeled-email/new-labeled-email.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/gmail_custom_oauth/sources/new-labeled-email/new-labeled-email.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/gmail_custom_oauth/sources/new-labeled-email/new-labeled-email.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Some adjusts
    
    * version update
    
    * fix version update
    
    * update
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

--- components/gmail_custom_oauth/actions/send-email/send-email.mjs
+++ components/gmail_custom_oauth/actions/send-email/send-email.mjs
@@ -10,7 +10,7 @@ overrideApp(base);
 export default {
   ...base,
   key: "gmail_custom_oauth-send-email",
-  version: "0.0.10",
+  version: "0.0.11",
   props: {
     ...base.props,
     inReplyTo: {
--- components/gmail_custom_oauth/gmail_custom_oauth.app.mjs
+++ components/gmail_custom_oauth/gmail_custom_oauth.app.mjs
@@ -1,6 +1,7 @@
-import base from "../gmail/gmail.app.mjs";
-import { google } from "googleapis";
 import gmail from "@googleapis/gmail";
+import { axios } from "@pipedream/platform";
+import { google } from "googleapis";
+import base from "../gmail/gmail.app.mjs";
 
 export default {
   ...base,
@@ -8,6 +9,25 @@ export default {
   app: "gmail_custom_oauth",
   methods: {
     ...base.methods,
+    _apiUrl() {
+      return "https://www.googleapis.com/gmail/v1/users/me";
+    },
+    _getHeaders() {
+      return {
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      const config = {
+        url: `${this._apiUrl()}/${path}`,
+        headers: this._getHeaders(),
+        ...opts,
+      };
+
+      return axios($, config);
+    },
     _accessToken() {
       return this.$auth.oauth_access_token;
     },
