b37f265b501eb434a55c844d2ac3ee6f34e8756e
GTFalcao <gtfalcao96@gmail.com>
2023-04-03 19:56:50 -0300

    'Update Event' and fixes

--- components/team_up/actions/update-event/update-event.ts
+++ components/team_up/actions/update-event/update-event.ts
@@ -1,19 +1,19 @@
 import app from "../../app/team_up.app";
 import { defineAction } from "@pipedream/types";
 import {
-  CreateEventParams,
+  UpdateEventParams,
 } from "../../common/requestParams";
 import { Event } from "../../common/responseSchemas";
 import {
   EVENT_PROPS, getEventProps,
 } from "../../common/eventProps";
 
-const DOCS_LINK = "https://apidocs.teamup.com/docs/api/3269d0159ae9f-create-an-event";
+const DOCS_LINK = "https://apidocs.teamup.com/docs/api/8b5d0d1556103-update-an-event";
 
 export default defineAction({
-  name: "Create Event",
-  description: `Create an event [See docs here](${DOCS_LINK})`,
-  key: "team_up-create-event",
+  name: "Update Event",
+  description: `Update an event [See docs here](${DOCS_LINK})`,
+  key: "team_up-update-event",
   version: "0.0.1",
   type: "action",
   props: {
@@ -23,6 +23,15 @@ export default defineAction({
         app,
         "calendarKey",
       ],
+    },
+    eventId: {
+      propDefinition: [
+        app,
+        "eventId",
+        ({ calendarKey }) => ({
+          calendarKey,
+        }),
+      ]
     },
     subCalendarIds: {
       propDefinition: [
@@ -37,26 +46,27 @@ export default defineAction({
   },
   async run({ $ }): Promise<Event> {
     const {
-      calendarKey, subCalendarIds,
+      calendarKey, eventId, subCalendarIds,
     } = this;
 
-    const params: CreateEventParams = {
+    const params: UpdateEventParams = {
       $,
       calendarKey,
+      eventId,
       data: {
         subcalendar_ids: subCalendarIds,
         ...getEventProps(),
       },
     };
 
-    const data = await this.app.createEvent(params);
+    const data = await this.app.updateEvent(params);
     const {
-      id, title,
+      title,
     } = data;
 
-    $.export("$summary", `Successfully created event ${title
+    $.export("$summary", `Successfully updated event ${title
       ? `"${title}"`
-      : id}`);
+      : eventId}`);
 
     return data;
   },
