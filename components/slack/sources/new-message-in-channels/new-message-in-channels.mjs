c79088d50c468b1ca708748c5406656a7f140653
Andrew Chuang <andrewjschuang@gmail.com>
2023-04-04 09:33:39 -0300

    bump versions

--- components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
+++ components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
@@ -5,7 +5,7 @@ export default {
   ...common,
   key: "slack-new-message-in-channels",
   name: "New Message In Channels (Instant)",
-  version: "1.0.9",
+  version: "1.0.10",
   description: "Emit new event when a new message is posted to one or more channels",
   type: "source",
   dedupe: "unique",
