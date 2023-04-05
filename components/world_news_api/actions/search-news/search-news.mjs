51ba0aef019dc15338dbd8283f578ca9b7946846
Cassiano Vellames <cassiano.v@turing.com>
2023-04-05 09:58:13 -0300

    Source

--- components/world_news_api/actions/search-news/search-news.mjs
+++ components/world_news_api/actions/search-news/search-news.mjs
@@ -1,7 +1,4 @@
 import app from "../../world_news_api.app.mjs";
-import countryCodes from "../../common/country_codes.mjs";
-import languages from "../../common/country_codes.mjs";
-import options from "../../common/options.mjs";
 
 export default {
   name: "Search News",
@@ -12,102 +9,94 @@ export default {
   props: {
     app,
     text: {
-      type: "string",
-      label: "Text",
-      description: "The text to match in the news content.",
-      optional: true,
+      propDefinition: [
+        app,
+        "text",
+      ],
     },
     sourceCountries: {
-      type: "string[]",
-      label: "Source Countries",
-      description: "List of [ISO 3166 country codes](https://worldnewsapi.com/docs/#Country-Codes) from which the news should originate.",
-      optional: true,
-      options: countryCodes,
+      propDefinition: [
+        app,
+        "sourceCountries",
+      ],
     },
     language: {
-      type: "string",
-      label: "Language",
-      description: "The [ISO 6391](https://worldnewsapi.com/docs/#Language-Codes) language code of the news.",
-      optional: true,
-      options: languages,
+      propDefinition: [
+        app,
+        "language",
+      ],
     },
     minSentiment: {
-      type: "string",
-      label: "Minimum Sentiment",
-      description: "The minimum sentiment of the news.",
-      optional: true,
+      propDefinition: [
+        app,
+        "minSentiment",
+      ],
     },
     maxSentiment: {
-      type: "string",
-      label: "Maximum Sentiment",
-      description: "The maximum sentiment of the news.",
-      optional: true,
+      propDefinition: [
+        app,
+        "maxSentiment",
+      ],
     },
     earliestPublishedDate: {
-      type: "string",
-      label: "Earliest Published Date",
-      description: "The news must have been published after this date. `YYYY-MM-DD HH:MM:SS` format",
-      optional: true,
+      propDefinition: [
+        app,
+        "earliestPublishedDate",
+      ],
     },
     latestPublishedDate: {
-      type: "string",
-      label: "Latest Published Date",
-      description: "The news must have been published before this date. `YYYY-MM-DD HH:MM:SS` format",
-      optional: true,
+      propDefinition: [
+        app,
+        "latestPublishedDate",
+      ],
     },
     newsSources: {
-      type: "string[]",
-      label: "News Sources",
-      description: "A list of news sources from which the news should originate.",
-      optional: true,
+      propDefinition: [
+        app,
+        "newsSources",
+      ],
     },
     authors: {
-      type: "string[]",
-      label: "Authors",
-      description: "A list of authors of the news.",
-      optional: true,
+      propDefinition: [
+        app,
+        "authors",
+      ],
     },
     entities: {
-      type: "string[]",
-      label: "Entities",
-      description: "Filter news by entities [(see semantic types)](https://worldnewsapi.com/docs/#Semantic-Types).",
-      optional: true,
+      propDefinition: [
+        app,
+        "entities",
+      ],
     },
     locationFilter: {
-      type: "string",
-      label: "Location Filter",
-      description: "Filter news by radius around a certain location. Format is `latitude,longitude,radius in kilometers`",
-      optional: true,
+      propDefinition: [
+        app,
+        "locationFilter",
+      ],
     },
     sort: {
-      type: "string",
-      label: "Sort",
-      description: "Sort the news by a certain field.",
-      optional: true,
-      options: options.SORT_BY,
+      propDefinition: [
+        app,
+        "sort",
+      ],
     },
     sortDirection: {
-      type: "string",
-      label: "Sort Direction",
-      description: "Sort the news in ascending or descending order.",
-      optional: true,
-      options: options.SORT_DIRECTION,
+      propDefinition: [
+        app,
+        "sortDirection",
+      ],
     },
     offset: {
-      type: "integer",
-      label: "Offset",
-      description: "The number of news to skip in range. `(0-1000)`",
-      optional: true,
-      min: 0,
-      max: 1000,
+      propDefinition: [
+        app,
+        "offset",
+      ],
     },
     number: {
-      type: "integer",
-      label: "Number",
-      description: "The number of news to return in range. `(0-100)`",
-      optional: true,
-      min: 0,
-      max: 100,
+      propDefinition: [
+        app,
+        "number",
+      ],
     },
   },
   async run({ $ }) {
@@ -121,7 +110,7 @@ export default {
       "latest-published-date": this.latestPublishedDate,
       "news-sources": this.newsSources?.join(","), // comma-separated list
       "authors": this.authors?.join(","), // comma-separated list
-      "entities": this.entities,
+      "entities": this.entities?.join(","), // comma-separated list
       "location-filter": this.locationFilter,
       "sort": this.sort,
       "sort-direction": this.sortDirection,
---
+++ components/world_news_api/sources/new-news/new-news.mjs
@@ -0,0 +1,165 @@
+import app from "../../world_news_api.app.mjs";
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+
+export default {
+  name: "New News",
+  description: "Emit new event when a new news is fetched. Calling this endpoint requires 1 point per page, up to 1000 news.",
+  key: "world_news_api-new-news",
+  version: "0.0.1",
+  type: "source",
+  props: {
+    app,
+    timer: {
+      label: "Polling Interval",
+      description: "Pipedream will poll the Docusign API on this schedule",
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+    db: "$.service.db",
+    text: {
+      propDefinition: [
+        app,
+        "text",
+      ],
+    },
+    sourceCountries: {
+      propDefinition: [
+        app,
+        "sourceCountries",
+      ],
+    },
+    language: {
+      propDefinition: [
+        app,
+        "language",
+      ],
+    },
+    minSentiment: {
+      propDefinition: [
+        app,
+        "minSentiment",
+      ],
+    },
+    maxSentiment: {
+      propDefinition: [
+        app,
+        "maxSentiment",
+      ],
+    },
+    earliestPublishedDate: {
+      propDefinition: [
+        app,
+        "earliestPublishedDate",
+      ],
+    },
+    latestPublishedDate: {
+      propDefinition: [
+        app,
+        "latestPublishedDate",
+      ],
+    },
+    newsSources: {
+      propDefinition: [
+        app,
+        "newsSources",
+      ],
+    },
+    authors: {
+      propDefinition: [
+        app,
+        "authors",
+      ],
+    },
+    entities: {
+      propDefinition: [
+        app,
+        "entities",
+      ],
+    },
+    locationFilter: {
+      propDefinition: [
+        app,
+        "locationFilter",
+      ],
+    },
+  },
+  methods: {
+    emit(data) {
+      this.$emit(data, {
+        id: data.id,
+        summary: data.title,
+        ts: Date.now(),
+      });
+    },
+    setLastId(id) {
+      this.db.set("lastId", id);
+    },
+    getLastId() {
+      return this.db.get("lastId");
+    },
+  },
+  async run({ $ }) {
+    const ITEMS_PER_PAGE = 100;
+    const MAX_CALLS_PER_EXECUTION = 10;
+    const MAX_OFFSET = MAX_CALLS_PER_EXECUTION * ITEMS_PER_PAGE;
+    let offset = 0;
+
+    let available = 0;
+    let currentCallLastId;
+    const beforeCallLastId = this.getLastId();
+    const newsArr = [];
+    loop1:
+    do {
+      const params = {
+        "text": this.text || undefined,
+        "source-countries": this.sourceCountries?.length > 0
+          ? this.sourceCountries.join(",")
+          : undefined,
+        "language": this.language ?? undefined,
+        "min-sentiment": this.minSentiment || undefined,
+        "max-sentiment": this.maxSentiment || undefined,
+        "earliest-published-date": this.earliestPublishedDate || undefined,
+        "latest-published-date": this.latestPublishedDate || undefined,
+        "news-sources": this.newsSource?.length > 0
+          ? this.newsSource
+          : undefined,
+        "authors": this.authors?.length > 0
+          ? this.authors.join(",")
+          : undefined,
+        "entities": this.entities?.length > 0
+          ? this.entities.join(",")
+          : undefined,
+        "location-filter": this.locationFilter || undefined,
+        "offset": offset,
+        "number": ITEMS_PER_PAGE,
+        "sort": "publish-time",
+        "sort-direction": "DESC",
+      };
+      const res = await this.app.searchNews(params, $);
+      console.log(res);
+      available = res.available;
+
+      if (res.news.length === 0) {
+        break loop1;
+      }
+
+      for (const news of res.news) {
+        if (!currentCallLastId) {
+          currentCallLastId = news.id;
+        }
+        if (news.id === beforeCallLastId) {
+          break loop1;
+        }
+        newsArr.unshift(news);
+      }
+      offset += ITEMS_PER_PAGE;
+    } while (offset <= available && offset <= MAX_OFFSET);
+    this.setLastId(currentCallLastId);
+
+    for (const news of newsArr.reverse()) {
+      this.emit(news);
+    }
+  },
+};
