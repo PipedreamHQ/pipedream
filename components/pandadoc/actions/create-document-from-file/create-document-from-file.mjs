ffd36661531f1760cb8c156e8885d3edc3bf4db3
GTFalcao <gtfalcao96@gmail.com>
2023-04-07 18:51:00 -0300

    action: Create Document from File + adjustments

---
+++ components/pandadoc/actions/create-document-from-file/create-document-from-file.mjs
@@ -0,0 +1,73 @@
+import app from "../../pandadoc.app.mjs";
+import createDocumentAttachment from "../create-document-attachment/create-document-attachment.mjs";
+
+export default {
+  key: "pandadoc-create-document-from-file",
+  name: "Create Document From File",
+  description: "Create a document from a file or public file URL. [See the docs here](https://developers.pandadoc.com/reference/create-document-from-pdf)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    recipients: {
+      propDefinition: [
+        app,
+        "recipients",
+      ],
+    },
+    file: {
+      propDefinition: [
+        app,
+        "file",
+      ],
+      optional: true,
+    },
+    fileUrl: {
+      type: "string",
+      label: "File URL",
+      description: "A public file URL to use instead of a local file.",
+      optional: true,
+    },
+  },
+  methods: createDocumentAttachment.methods,
+  async run({ $ }) {
+    const {
+      name,
+      recipients,
+      file,
+      fileUrl,
+    } = this;
+
+    let data, contentType, json = {
+      name,
+      recipients,
+    };
+
+    if (fileUrl) {
+      data = json;
+      contentType = "application/json";
+      data.url = fileUrl;
+    } else {
+      data = this.getFormData(file);
+      contentType = `multipart/form-data; boundary=${data._boundary}`;
+      data.append("data", json);
+    }
+
+    const response = await this.app.createDocument({
+      $,
+      data,
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
+
+    $.export("$summary", `Successfully created document attachment with ID: ${response.uuid}`);
+    return response;
+  },
+};
