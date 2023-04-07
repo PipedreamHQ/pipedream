57d4a6fb57473a56dba2f3e98f5d8fc064dc1c00
michelle0927 <michellelbergero@hotmail.com>
2023-04-07 10:26:35 -0400

    new components

---
+++ components/memberstack/actions/delete-member/delete-member.mjs
@@ -0,0 +1,27 @@
+import memberstack from "../../memberstack.app.mjs";
+
+export default {
+  key: "memberstack-delete-member",
+  name: "Delete Member",
+  description: "Deletes a member by ID. [See the docs](https://memberstack.notion.site/Admin-API-5b9233507d734091bd6ed604fb893bb8)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    memberstack,
+    memberId: {
+      propDefinition: [
+        memberstack,
+        "memberId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { data } = await this.memberstack.deleteMember({
+      id: this.memberId,
+    });
+
+    data && $.export("$summary", `Successfully deleted member with ID ${data.id}.`);
+
+    return data;
+  },
+};
