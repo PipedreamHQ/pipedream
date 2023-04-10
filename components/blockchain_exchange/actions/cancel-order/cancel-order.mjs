1454d5386375089f18e6a6e3d602b1273e2f773e
Jorge Cortes <jacortesmahmud@gmail.com>
2023-04-10 11:45:26 -0500

    Added new actions

--- components/blockchain_exchange/actions/cancel-order/cancel-order.mjs
+++ components/blockchain_exchange/actions/cancel-order/cancel-order.mjs
@@ -1,4 +1,6 @@
 import app from "../../blockchain_exchange.app.mjs";
+import constants from "../../common/constants.mjs";
+import utils from "../../common/utils.mjs";
 
 export default {
   key: "blockchain_exchange-cancel-order",
@@ -8,6 +10,34 @@ export default {
   version: "0.0.1",
   props: {
     app,
+    orderId: {
+      type: "string",
+      label: "Order ID",
+      description: "The unique identifier of the order.",
+    },
   },
-  async run() {},
+  methods: {
+    async cancelOrder(args = {}) {
+      const response = await this.app.sendMessage({
+        action: constants.TRADING_ACTION.CANCEL_ORDER_REQUEST,
+        channel: constants.CHANNEL.TRADING,
+        ...args,
+      });
+
+      if (utils.isRejected(response)) {
+        throw new Error(JSON.stringify(response));
+      }
+
+      return response;
+    },
+  },
+  async run({ $: step }) {
+    const response = await this.cancelOrder({
+      orderId: this.orderId,
+    });
+
+    step.export("$summary", "Successfully cancelled order");
+
+    return response;
+  },
 };
