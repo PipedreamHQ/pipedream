9e02e2f0d35d3c22c06e0e343cc9f84d0b4d699b
michelle0927 <michellelbergero@hotmail.com>
2023-04-10 16:38:52 -0400

    create-attachment action

---
+++ components/meistertask/actions/create-attachment/create-attachment.mjs
@@ -0,0 +1,73 @@
+import meistertask from "../../meistertask.app.mjs";
+import FormData from "form-data";
+import fs from "fs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "meister-create-attachment",
+  name: "Create Attachment",
+  description: "Create a new attachment. [See the docs](https://developers.meistertask.com/reference/post-attachment)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    meistertask,
+    taskId: {
+      propDefinition: [
+        meistertask,
+        "taskId",
+      ],
+    },
+    filepath: {
+      type: "string",
+      label: "File Path",
+      description: "Path of the file in /tmp folder to add as an attachment. To upload a file to /tmp folder, please follow the [doc here](https://pipedream.com/docs/code/nodejs/working-with-files/#writing-a-file-to-tmp)",
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the attachment",
+      optional: true,
+    },
+  },
+  methods: {
+    checkTmp(filename) {
+      if (filename.indexOf("/tmp") === -1) {
+        return `/tmp/${filename}`;
+      }
+      return filename;
+    },
+  },
+  async run({ $ }) {
+    const {
+      taskId,
+      filepath,
+      name,
+    } = this;
+
+    const data = new FormData();
+    const path = this.checkTmp(filepath);
+
+    if (!fs.existsSync(path)) {
+      throw new ConfigurationError("File does not exist");
+    }
+
+    const file = fs.createReadStream(path);
+    const stats = fs.statSync(path);
+    data.append("file", file, {
+      knownLength: stats.size,
+    });
+    if (name) {
+      data.append("name", file);
+    }
+    const headers = {
+      "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+    };
+
+    const response = await this.meistertask.createAttachment(taskId, {
+      data,
+      headers,
+    });
+    $.export("$summary", `Successfully created attachment with ID ${response.id}.`);
+    return response;
+  },
+};
--- components/meistertask/meistertask.app.mjs
+++ components/meistertask/meistertask.app.mjs
@@ -1,11 +1,62 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "meistertask",
-  propDefinitions: {},
+  propDefinitions: {
+    taskId: {
+      type: "string",
+      label: "Task",
+      description: "Identifier of a task",
+      async options({ page }) {
+        const tasks = await this.listTasks({
+          params: {
+            page: page + 1,
+          },
+        }); console.log(tasks);
+        return tasks?.map(({
+          id, name,
+        }) => ({
+          label: name,
+          value: id,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://www.meistertask.com/api";
+    },
+    _headers(headers = {}) {
+      return {
+        ...headers,
+        Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      headers,
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(headers),
+        ...args,
+      });
+    },
+    listTasks(args = {}) {
+      return this._makeRequest({
+        path: "/tasks",
+        ...args,
+      });
+    },
+    createAttachment(taskId, args = {}) {
+      return this._makeRequest({
+        path: `/tasks/${taskId}/attachments`,
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
