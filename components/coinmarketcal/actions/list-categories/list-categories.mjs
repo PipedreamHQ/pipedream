0147b6a515aed3d4f42948ebaccb9b2198c10ffa
Luan Cazarine <luanhc@gmail.com>
2023-04-12 18:19:29 -0300

    [Components] CoinMarketCal #5988 Triggers:  - Event Added
    Actions:
     - Search Events
     - Get Coins
     - Get Categories

---
+++ components/coinmarketcal/actions/list-categories/list-categories.mjs
@@ -0,0 +1,20 @@
+import coinmarketcal from "../../coinmarketcal.app.mjs";
+
+export default {
+  key: "coinmarketcal-list-categories",
+  name: "List Categories",
+  version: "0.0.1",
+  description: "Obtain a list of all available event categories in CoinMarketCal. [See the docs here](https://coinmarketcal.com/en/doc/redoc#/paths/~1categories/get)",
+  type: "action",
+  props: {
+    coinmarketcal,
+  },
+  async run({ $ }) {
+    const response = await this.coinmarketcal.listCategories({
+      $,
+    });
+
+    $.export("$summary", "Categories were successfully fetched!");
+    return response;
+  },
+};
---
+++ components/coinmarketcal/actions/list-coins/list-coins.mjs
@@ -0,0 +1,20 @@
+import coinmarketcal from "../../coinmarketcal.app.mjs";
+
+export default {
+  key: "coinmarketcal-list-coins",
+  name: "List Coins",
+  version: "0.0.1",
+  description: "Obtain a list of all available coins in CoinMarketCal. [See the docs here](https://coinmarketcal.com/en/doc/redoc#/paths/~1categories/get)",
+  type: "action",
+  props: {
+    coinmarketcal,
+  },
+  async run({ $ }) {
+    const response = await this.coinmarketcal.listCoins({
+      $,
+    });
+
+    $.export("$summary", "Coins were successfully fetched!");
+    return response;
+  },
+};
