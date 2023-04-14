40c455a79b7116f98cac737a167a6ac0aa899968
michelle0927 <michelle0927@users.noreply.github.com>
2023-04-14 10:21:56 -0700

    MeisterTask - New Components (#5999)
    * create-attachment action
    
    * new actions
    
    * new sources
    
    * updates
    
    * pnpm-lock.yaml
    
    * fix keys
    
    * pnpm-lock.yaml

---
+++ components/meistertask/actions/get-label/get-label.mjs
@@ -0,0 +1,59 @@
+import meistertask from "../../meistertask.app.mjs";
+
+export default {
+  key: "meistertask-get-label",
+  name: "Get Label",
+  description: "Retrieves information about a label. [See the docs](https://developers.meistertask.com/reference/get-label)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    meistertask,
+    projectId: {
+      propDefinition: [
+        meistertask,
+        "projectId",
+      ],
+    },
+    sectionId: {
+      propDefinition: [
+        meistertask,
+        "sectionId",
+        (c) => ({
+          projectId: c.projectId,
+        }),
+      ],
+      optional: true,
+    },
+    taskId: {
+      propDefinition: [
+        meistertask,
+        "taskId",
+        (c) => ({
+          projectId: c.projectId,
+          sectionId: c.sectionId,
+        }),
+      ],
+      optional: true,
+    },
+    labelId: {
+      propDefinition: [
+        meistertask,
+        "labelId",
+        (c) => ({
+          projectId: c.projectId,
+          taskId: c.taskId,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.meistertask.getLabel({
+      labelId: this.labelId,
+      $,
+    });
+    if (response) {
+      $.export("$summary", `Successfully retrieved label with ID ${this.labelId}`);
+    }
+    return response;
+  },
+};
