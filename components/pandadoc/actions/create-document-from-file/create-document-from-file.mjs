8ecfd0a96ccf0d9a11a3e795f49929847d1a8592
feyzullah <feyzullah.metu@gmail.com>
2023-04-18 00:16:39 +0200

    only component and package versions (#6059)

--- components/pandadoc/actions/create-document-from-file/create-document-from-file.mjs
+++ components/pandadoc/actions/create-document-from-file/create-document-from-file.mjs
@@ -7,7 +7,7 @@ export default {
   name: "Create Document From File",
   description: "Create a document from a file or public file URL. [See the docs here](https://developers.pandadoc.com/reference/create-document-from-pdf)",
   type: "action",
-  version: "0.0.2",
+  version: "0.0.3",
   props: {
     app,
     name: {
