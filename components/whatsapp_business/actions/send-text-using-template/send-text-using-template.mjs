88c06f7851453d84c9f60228338a4ead165f4608
michelle0927 <michelle0927@users.noreply.github.com>
2023-04-19 06:54:47 -0700

    Whatsapp - Send Text Using Template with support for dynamic URL in buttons (#6071)
    * wip
    
    * updates
    
    * pnpm-lock.yaml

--- components/whatsapp_business/actions/send-text-using-template/send-text-using-template.mjs
+++ components/whatsapp_business/actions/send-text-using-template/send-text-using-template.mjs
@@ -7,7 +7,7 @@ export default {
   key: "whatsapp_business-send-text-using-template",
   name: "Send Text Using Template",
   description: `Send a text message using a pre-defined template. Variables can be sent only as text. [See the docs.](${docLink})`,
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     whatsapp,
@@ -49,6 +49,20 @@ export default {
         continue;
       }
 
+      if (component.type === "BUTTONS") {
+        for (const button of component.buttons) {
+          if (button.type === "URL" && button.url.match(regex)) {
+            // only 1 dynamic url button possible
+            props["button_{{1}}"] = {
+              type: "string",
+              label: button.text,
+              description: `Dynamic URL: **${button.url}** Enter the dynamic text only.`,
+            };
+            continue;
+          }
+        }
+      }
+
       const matches = component.text?.match(regex);
       for (const match of matches ?? []) {
         props[match] = {
@@ -79,6 +93,22 @@ export default {
       components.push({
         type: "header",
         parameters: headerParameters,
+      });
+    }
+
+    const buttonParameters = Object.keys(this)
+      .filter((key) => key.includes("button_"))
+      .map((key) => ({
+        type: "text",
+        text: this[key],
+      }));
+
+    if (buttonParameters.length) {
+      components.push({
+        type: "button",
+        sub_type: "url",
+        index: 0,
+        parameters: buttonParameters,
       });
     }
 
