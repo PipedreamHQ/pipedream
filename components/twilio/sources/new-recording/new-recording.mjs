2bc1e0ebf2c697ed8b41f781a72834f6f53da658
vellames <91911778+vellames@users.noreply.github.com>
2023-04-19 12:29:49 -0300

    Actions - Twilio (#6051)
    * Send SMS Verification
    
    * Check Verification Token
    
    * pnpm
    
    * Update versions + English corrections
    
    * Versions + PNPM
    
    * PNPM
    
    * pnpm

--- components/twilio/sources/new-recording/new-recording.mjs
+++ components/twilio/sources/new-recording/new-recording.mjs
@@ -5,7 +5,7 @@ export default {
   key: "twilio-new-recording",
   name: "New Recording",
   description: "Emit new event when a new call recording is created",
-  version: "0.1.3",
+  version: "0.1.4",
   type: "source",
   dedupe: "unique",
   methods: {
