6ac85d2f0ecf570690f34cc8f330d88a7740e805
Luan Cazarine <luanhc@gmail.com>
2023-04-19 13:59:46 -0300

    [Components] EODHD APIs #6058 Triggers:   - Stock Price Updated: Triggered when historical stock prices for a specific symbol are updated.   - Financial Report Released: Triggered when a company releases a new financial report.
    Actions:
      - Retrieve Stock Prices: Fetch historical stock prices for a given symbol and time period.
      - Get Company Financials: Obtain financial statements for a specific company.
      - Search Stock Symbol: Find stock symbols using company names or partial symbols.

---
+++ components/eodhd_apis/common/constants.mjs
@@ -0,0 +1,24 @@
+export const FMTS = [
+  "csv",
+  "json",
+];
+
+export const ORDERS = [
+  "a",
+  "d",
+];
+
+export const PERIODS = [
+  "d",
+  "w",
+  "m",
+];
+
+export const TYPES = [
+  "stock",
+  "ETF",
+  "fund",
+  "bond",
+  "index",
+  "crypto",
+];
