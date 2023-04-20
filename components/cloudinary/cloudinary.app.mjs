b9e24d78dbb3e139bb3568640f355625f9a85311
michelle0927 <michellelbergero@hotmail.com>
2023-04-20 11:58:33 -0400

    get-resources

--- components/cloudinary/cloudinary.app.mjs
+++ components/cloudinary/cloudinary.app.mjs
@@ -1,11 +1,39 @@
+import { v2 } from "cloudinary";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "cloudinary",
-  propDefinitions: {},
+  propDefinitions: {
+    resourceType: {
+      type: "string",
+      label: "Resource Type",
+      description: "The type of asset. Defaults to `image` if left blank",
+      options: constants.RESOURCE_TYPE_OPTIONS,
+      default: "image",
+      optional: true,
+    },
+    deliveryType: {
+      type: "string",
+      label: "Type",
+      description: "The delivery type. Defaults to `upload` if left blank",
+      options: constants.DELIVERY_TYPE_OPTIONS,
+      default: "upload",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _client() {
+      const cloudinary = v2;
+      cloudinary.config({
+        cloud_name: this.$auth.cloud_name,
+        api_key: this.$auth.api_key,
+        api_secret: this.$auth.api_secret,
+      });
+      return cloudinary;
+    },
+    async getResources(options) {
+      return this._client().api.resources(options);
     },
   },
 };
---
+++ components/cloudinary/common/constants.mjs
@@ -0,0 +1,27 @@
+const RESOURCE_TYPE_OPTIONS = [
+  "image",
+  "raw",
+  "video",
+];
+
+const DELIVERY_TYPE_OPTIONS = [
+  "upload",
+  "private",
+  "authenticated",
+  "fetch",
+  "facebook",
+  "twitter",
+  "gravatar",
+  "youtube",
+  "hulu",
+  "vimeo",
+  "animoto",
+  "worldstarhiphop",
+  "dailymotion",
+  "list",
+];
+
+export default {
+  RESOURCE_TYPE_OPTIONS,
+  DELIVERY_TYPE_OPTIONS,
+};
