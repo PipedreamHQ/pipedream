67390b8d8ba334940c6263271fcb1e38adafe101
Danny Roosevelt <danny@pipedream.com>
2023-04-19 19:24:32 -0700

    version rev

--- components/danny_test_app/actions/hello-world/hello-world.mjs
+++ components/danny_test_app/actions/hello-world/hello-world.mjs
@@ -2,7 +2,7 @@ import app from "../../app/danny_test_app.app.mjs";
 
 export default {
   name: "hello world",
-  version: "0.0.3",
+  version: "0.0.4",
   key: "danny_test_app-hello-world",
   description: "Hello World test",
   type: "action",
