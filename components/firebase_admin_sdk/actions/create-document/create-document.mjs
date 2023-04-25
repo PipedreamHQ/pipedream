efbb347acd5e9435096da227039f435b0da3b145
Lucas Caresia <lucascarezia@gmail.com>
2023-04-25 18:45:09 -0300

    [FEATURE] Add prop to set Custom Document ID for Firebase - Create Document action #6105 (#6107)
    * Added customId prop to create a document
    
    * Update components/firebase_admin_sdk/actions/create-document/create-document.mjs
    
    Co-authored-by: Dylan J. Sather <dylan.sather@gmail.com>
    
    * Update components/firebase_admin_sdk/actions/replicate-event-firestore/replicate-event-firestore.mjs
    
    Co-authored-by: Dylan J. Sather <dylan.sather@gmail.com>
    
    * package.json
    
    * Removed segments from response
    
    ---------
    
    Co-authored-by: Dylan J. Sather <dylan.sather@gmail.com>

--- components/firebase_admin_sdk/actions/create-document/create-document.mjs
+++ components/firebase_admin_sdk/actions/create-document/create-document.mjs
@@ -5,7 +5,7 @@ export default {
   key: "firebase_admin_sdk-create-document",
   name: "Create Document",
   description: "Creates a New Document. [See the docs here](https://googleapis.dev/nodejs/firestore/latest/CollectionReference.html#add)",
-  version: "0.0.2",
+  version: "0.0.6",
   type: "action",
   props: {
     ...common.props,
@@ -24,14 +24,20 @@ export default {
         "data",
       ],
     },
+    customId: {
+      label: "Custom ID",
+      description: "Set this custom ID for the created document",
+      type: "string",
+      optional: true,
+    },
   },
   methods: {
     ...common.methods,
     async getResponse() {
-      return this.firebase.createDocument(this.collection, this.data);
+      return this.firebase.createDocument(this.collection, this.data, this.customId);
     },
     emitSummary($, response) {
-      $.export("$summary", `Successfully added document ${response._path.segments[1]}`);
+      $.export("$summary", `Successfully added document ${response?._path?.segments[1] ?? ""}`);
     },
   },
 };
