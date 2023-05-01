ef91a62dbdf5f3d6a3b6afc78f3757870ff44638
michelle0927 <michellelbergero@hotmail.com>
2023-05-01 15:17:50 -0400

    new components

---
+++ components/leap/actions/upload-image-samples/upload-image-samples.mjs
@@ -0,0 +1,49 @@
+import leap from "../../leap.app.mjs";
+
+export default {
+  key: "leap-upload-image-samples",
+  name: "Upload Image Samples",
+  description: "Uploads image samples to a custom model. [See the documentation](https://docs.tryleap.ai/reference/samplescontroller_createurl)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    leap,
+    model: {
+      propDefinition: [
+        leap,
+        "model",
+      ],
+    },
+    images: {
+      type: "string[]",
+      label: "Image URLs",
+      description: "An array of strings containing the URLs of the images to upload",
+    },
+    returnInObject: {
+      type: "boolean",
+      label: "Return in Object",
+      description: "Whether to return the sample in the response as an object. Will return array if false.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.leap.uploadImageSamples({
+      modelId: this.model,
+      params: {
+        returnInObject: this.returnInObject,
+      },
+      data: {
+        images: this.images,
+      },
+      $,
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully uploaded ${response.length} image${response.length === 1
+        ? ""
+        : "s"}`);
+    }
+
+    return response;
+  },
+};
