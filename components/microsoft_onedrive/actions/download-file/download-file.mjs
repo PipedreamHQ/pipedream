7eecf01c4721a657ec0eb5c40c9dbddb47258073
GTFalcao <gtfalcao96@gmail.com>
2023-05-01 16:39:26 -0300

    File download request, file name options

--- components/microsoft_onedrive/actions/download-file/download-file.mjs
+++ components/microsoft_onedrive/actions/download-file/download-file.mjs
@@ -1,22 +1,45 @@
+import { axios } from "@pipedream/platform";
+import fs from "fs";
 import onedrive from "../../microsoft_onedrive.app.mjs";
 
 export default {
-  name: "Get Table",
-  description: "Download a file stored in OneDrive",
+  name: "Download File",
+  description: "Download a file stored in OneDrive [See docs here](https://learn.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_get_content?view=odsp-graph-online)",
   key: "microsoft_onedrive-download-file",
   version: "0.0.1",
   type: "action",
   props: {
     onedrive,
-    folder: {
-      propDefinition: [
-        onedrive,
-        "folder",
-      ],
-      description: "The folder where the file is located",
+    filePath: {
+      type: "string",
+      label: "File Path",
+      description: "The path to the file from the root folder, e.g. `Documents/My Subfolder/File 1.docx`",
+    },
+    newFileName: {
+      type: "string",
+      label: "New File Name",
+      description: "The file name to save the downloaded content as, under the `/tmp` folder. If not provided, the file will be saved with the same name",
+      optional: true,
     },
   },
-  async run(/*{ $ }*/) {
-    return this.folder;
+  async run({ $ }) {
+    const {
+      filePath, newFileName,
+    } = this;
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root:/${encodeURI(filePath)}:/content`;
+
+    const response = await axios($, {
+      url,
+      headers: {
+        Authorization: `Bearer ${this.onedrive.$auth.oauth_access_token}`,
+      },
+    });
+
+    const fileName = (newFileName ?? filePath).split("/").pop();
+
+    fs.writeFileSync(`/tmp/${fileName}`, response);
+
+    $.export("$summary", `Returned file contents and saved to "/tmp/${fileName}"`);
+    return response;
   },
 };
