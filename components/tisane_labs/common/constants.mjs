bf154e9e7c10131a07e7737021e9c54fb569a12b
michelle0927 <michellelbergero@hotmail.com>
2023-05-02 15:18:22 -0400

    new components

---
+++ components/tisane_labs/common/constants.mjs
@@ -0,0 +1,38 @@
+const FORMAT_OPTIONS = [
+  "review",
+  "dialog",
+  "shortpost",
+  "longform",
+  "proofread",
+  "alias",
+  "search",
+];
+
+const FEATURE_STANDARD_OPTIONS = [
+  "ud",
+  "penn",
+  "native",
+  "description",
+];
+
+const TOPIC_STANDARD_OPTIONS = [
+  "iptc_code",
+  "iptc_description",
+  "iab_code",
+  "iab_description",
+  "native",
+];
+
+const SENTIMENT_ANALYSIS_TYPE_OPTIONS = [
+  "products_and_services",
+  "entity",
+  "creative_content_review",
+  "political_essay",
+];
+
+export default {
+  FORMAT_OPTIONS,
+  FEATURE_STANDARD_OPTIONS,
+  TOPIC_STANDARD_OPTIONS,
+  SENTIMENT_ANALYSIS_TYPE_OPTIONS,
+};
