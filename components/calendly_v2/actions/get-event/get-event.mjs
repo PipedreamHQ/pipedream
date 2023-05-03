2f13db4bd3249e0adbe71fdec0eacd9799729b44
Michael Lim <malexanderlim@gmail.com>
2023-05-03 15:07:41 -0700

    Fixed version number and moved function inside run method.

--- components/calendly_v2/actions/get-event/get-event.mjs
+++ components/calendly_v2/actions/get-event/get-event.mjs
@@ -2,20 +2,11 @@ import { axios } from "@pipedream/platform";
 import { ConfigurationError } from "@pipedream/platform";
 import { URL } from "url";
 
-function getEventUuidFromUrl(event_url) {
-  if (!event_url) {
-    return null;
-  }
-
-  const url = new URL(event_url);
-  return url.pathname.split("/").pop();
-}
-
 export default {
   key: "calendly_v2-get-event",
   name: "Get Event",
   description: "Gets information about an Event associated with a URI. [See docs here](https://developer.calendly.com/api-docs/e2f95ebd44914-get-event).",
-  version: "0.1.3",
+  version: "0.1.2",
   type: "action",
   props: {
     calendly_v2: {
@@ -38,6 +29,15 @@ export default {
   },
 
   async run({ $ }) {
+    function getEventUuidFromUrl(event_url) {
+      if (!event_url) {
+        return null;
+      }
+
+      const url = new URL(event_url);
+      return url.pathname.split("/").pop();
+    }
+
     if (!this.event_uuid && !this.event_url) {
       throw new ConfigurationError(
         "Please provide either the Event UUID or Event URL, then try again.",
