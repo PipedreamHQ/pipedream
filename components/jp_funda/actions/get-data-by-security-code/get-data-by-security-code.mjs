a47c16e11940fc23ef54cf3e31957d470f53a35f
Luan Cazarine <luanhc@gmail.com>
2023-05-03 11:06:19 -0300

    fix lint

--- components/jp_funda/actions/get-data-by-security-code/get-data-by-security-code.mjs
+++ components/jp_funda/actions/get-data-by-security-code/get-data-by-security-code.mjs
@@ -1,7 +1,7 @@
 import jpFunda from "../../jp_funda.app.mjs";
 
 export default {
-  key: "jp_funda-search-get-data-by-security-code",
+  key: "jp_funda-get-data-by-security-code",
   name: "Get Data By Security Code",
   version: "0.0.1",
   description: "Returns the latest and past years' securities report data. [See the docs here](https://www.jp-funda.com/docs/#%E8%A8%BC%E5%88%B8%E3%82%B3%E3%83%BC%E3%83%89%E3%81%A7%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%96%E5%BE%97)",
