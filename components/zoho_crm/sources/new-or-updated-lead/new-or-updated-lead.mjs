c21f922caa3f8fa08a98802871c0ba5b93f35298
Andrew Chuang <andrewjschuang@gmail.com>
2023-05-04 08:30:05 -0300

    bump sources

--- components/zoho_crm/sources/new-or-updated-lead/new-or-updated-lead.mjs
+++ components/zoho_crm/sources/new-or-updated-lead/new-or-updated-lead.mjs
@@ -6,7 +6,7 @@ export default {
   key: "zoho_crm-new-or-updated-lead",
   name: "New or Updated Lead (Instant)",
   description: "Emits an event each time a new lead is created or updated in Zoho CRM",
-  version: "0.0.8",
+  version: "0.0.9",
   type: "source",
   methods: {
     ...common.methods,
