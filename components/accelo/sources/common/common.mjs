47c5e947c30a2df6a8a795bd6c9bc121dd933012
Lucas Caresia <lucascarezia@gmail.com>
2023-05-05 10:32:09 -0300

    Testing

--- components/accelo/sources/common/common.mjs
+++ components/accelo/sources/common/common.mjs
@@ -17,7 +17,7 @@ export default {
       throw new Error("getWebhookEventType is not implemented");
     },
     emitEvent(event) {
-      throw new Error("emitEvent is not implemented dsadsadasdsadsasassadasdsadsadsasd", event);
+      throw new Error("emitEvent is not implemented dsadsadasdsadsasassadasdsadsadsasddasdsadsa", event);
     },
   },
   hooks: {
