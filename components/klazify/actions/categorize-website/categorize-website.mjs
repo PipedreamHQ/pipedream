44c9cffa53da708331074e904ebda30b5bd000ca
Jorge Cortes <jacortesmahmud@gmail.com>
2023-05-04 19:14:31 -0500

    [ACTIONS] Klazify: Added new actions (#6223)
    * Added new actions
    
    * Fix apostrophes pointed out by oncall

---
+++ components/klazify/actions/categorize-website/categorize-website.mjs
@@ -0,0 +1,38 @@
+import app from "../../klazify.app.mjs";
+
+export default {
+  key: "klazify-categorize-website",
+  name: "Categorize Website",
+  description: "Submit a website URL for categorization by the API. [See the documentation](https://www.klazify.com/category#docs).",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    url: {
+      propDefinition: [
+        app,
+        "url",
+      ],
+    },
+  },
+  methods: {
+    categorizeWebsite(args = {}) {
+      return this.app.post({
+        path: "/categorize",
+        ...args,
+      });
+    },
+  },
+  async run({ $: step }) {
+    const response = await this.categorizeWebsite({
+      step,
+      data: {
+        url: this.url,
+      },
+    });
+
+    step.export("$summary", `Successfully categorized ${this.url}`);
+
+    return response;
+  },
+};
