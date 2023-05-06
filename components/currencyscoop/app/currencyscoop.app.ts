31a72bf4bf5a0feae95470ea8ac2db7d9cf3b2ea
GTFalcao <gtfalcao96@gmail.com>
2023-05-06 16:27:25 -0300

    Basic currencyscoop request

--- components/currencyscoop/app/currencyscoop.app.ts
+++ components/currencyscoop/app/currencyscoop.app.ts
@@ -1,13 +1,25 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
+import { HttpRequestParams } from "../common/types";
 
 export default defineApp({
   type: "app",
   app: "currencyscoop",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    async _httpRequest<ResponseType extends object>({
+      $ = this,
+      params,
+      ...args
+    }: HttpRequestParams): Promise<ResponseType> {
+      const { api_key } = this.$auth;
+      return axios($, {
+        baseURL: "https://api.currencybeacon.com/v1",
+        params: {
+          ...params,
+          api_key,
+        },
+        ...args,
+      });
     },
   },
 });
