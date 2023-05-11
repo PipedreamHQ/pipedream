d5142dc1e8f006a928e4efbe333465d09251d222
michelle0927 <michellelbergero@hotmail.com>
2023-05-11 17:41:34 -0400

    new source ticket-added-to-view

--- components/zendesk/actions/update-ticket/update-ticket.mjs
+++ components/zendesk/actions/update-ticket/update-ticket.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Update Ticket",
   description: "Updates a ticket. [See the docs](https://developer.zendesk.com/api-reference/ticketing/tickets/tickets/#update-ticket).",
   type: "action",
-  version: "0.1.0",
+  version: "0.1.1",
   props: {
     app,
     ticketId: {
