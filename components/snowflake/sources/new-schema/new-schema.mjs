57856c3a86033356912d7cdb2d3d91bbc3a7fcf8
Cassiano Vellames <cassiano.v@turing.com>
2023-05-11 09:14:04 -0300

    versions

--- components/snowflake/sources/new-schema/new-schema.mjs
+++ components/snowflake/sources/new-schema/new-schema.mjs
@@ -7,7 +7,7 @@ export default {
   key: "snowflake-new-schema",
   name: "New Schema",
   description: "Emit new event when a schema is created",
-  version: "0.0.1",
+  version: "0.0.2",
   methods: {
     ...common.methods,
     alwaysRunInSingleProcessMode() {
