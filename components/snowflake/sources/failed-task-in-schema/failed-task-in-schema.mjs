c01d657ab299f7a265bcd885667bb40fe0c6d628
Cassiano Vellames <cassiano.v@turing.com>
2023-05-11 14:20:09 -0300

    Versions

--- components/snowflake/sources/failed-task-in-schema/failed-task-in-schema.mjs
+++ components/snowflake/sources/failed-task-in-schema/failed-task-in-schema.mjs
@@ -32,7 +32,7 @@ export default {
   // eslint-disable-next-line
   name: "Failed Task in Schema",
   description: "Emit new events when a task fails in a database schema",
-  version: "0.0.4",
+  version: "0.1.0",
   async run() {
     await this.emitFailedTasks({
       database: this.database,
