78e2895cc5d95ae88492facab3e01f62213e9269
Cassiano Vellames <cassiano.v@turing.com>
2023-05-15 10:49:38 -0300

    delete role source

--- components/snowflake/sources/delete-role/delete-role.mjs
+++ components/snowflake/sources/delete-role/delete-role.mjs
@@ -1,5 +1,6 @@
 import snowflake from "../../snowflake.app.mjs";
 import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+import { v4 as uuid } from "uuid";
 
 export default {
   type: "source",
@@ -18,35 +19,65 @@ export default {
       },
     },
   },
-  hooks: {
-    async activate() {
+  methods: {
+    async addValueToObject(key, value) {
       const db = this.db.get("values");
       if (!db) {
         this.db.set("values", {});
       }
-    },
-  },
-  methods: {
-    async addValueToObject(key, value) {
-      const db = this.db.get("values");
+
       db[key] = value;
       this.db.set("values", db);
     },
-    getMeta() {
+    async fetchData() {
+      return this.snowflake.getRows({
+        sqlText: "show roles",
+      });
+    },
+    async getArrayFromStream(stream) {
+      const array = [];
+      for await (const item of stream) {
+        array.push(item);
+      }
+      return array;
+    },
+    async updateRoles() {
+      const rows = await this.fetchData();
+      const roles = await this.getArrayFromStream(rows);
+      for (const role of roles) {
+        this.addValueToObject(role.name, role);
+      }
+      return roles;
+    },
+    async checkMissingRoles(currentRoles) {
+      const db = this.db.get("values");
+      const dbKeys = Object.keys(db);
 
+      // Check keys in dbKeys that are not in currentRowsStream
+      const missingKeys = dbKeys.filter((key) => {
+        return !currentRoles.includes(key);
+      });
+
+      // Delete missing keys from db
+      for (const key of missingKeys) {
+        this.emit(db[key]);
+        delete db[key];
+      }
+
+      this.db.set("values", db);
+    },
+    emit(event) {
+      this.$emit(event, {
+        summary: event.name,
+        id: uuid(),
+        ts: Date.now(),
+      });
     },
   },
   async run() {
-    const rowStream = await this.snowflake.getRows({
-      sqlText: "show roles",
-    });
-    const missingRoles = [];
-    const db = this.db.get("values");
-    for await (const row of rowStream) {
-      if (db[row.name]) {
-        missingRoles.push(row);
-      }
-    }
+    const roles = await this.updateRoles();
+    const roleNames = roles.map((role) => role.name);
+    await this.checkMissingRoles(roleNames);
   },
 };
 
