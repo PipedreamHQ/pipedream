a40248e9a0997df78c4e919b6ef214bd959ef1f8
michelle0927 <michellelbergero@hotmail.com>
2023-05-16 13:28:51 -0400

    bug fix

--- components/bingx/actions/trade-oneclick-close-all-positions/trade-oneclick-close-all-positions.mjs
+++ components/bingx/actions/trade-oneclick-close-all-positions/trade-oneclick-close-all-positions.mjs
@@ -2,7 +2,7 @@ import bingx from "../../bingx.app.mjs";
 
 export default {
   name: "BingX Trade One Click Close All Positions",
-  version: "0.0.3",
+  version: "0.0.2",
   key: "bingx-trade-oneclick-close-all-positions",
   description: "One-Click Close All Positions [reference](https://bingx-api.github.io/docs/swap/trade-api.html#_3-one-click-close-all-positions).",
   props: {
@@ -11,7 +11,7 @@ export default {
   type: "action",
   async run({ $ }) {
     const API_METHOD = "POST";
-    const API_PATH = "/api/v1/user/oneClickClosePosition";
+    const API_PATH = "/api/v1/user/oneClickCloseAllPositions";
     const parameters = {};
     const returnValue = await this.bingx.makeRequest(API_METHOD, API_PATH, parameters);
     $.export("$summary", "Oneclick close all positions");
