c3a2884e7847dcc6ac04ca629042eb1330d05ecb
feyzullah <feyzullah.metu@gmail.com>
2023-05-19 17:34:21 +0200

    Google Photos Components (#6533)
    * Google Photos Components
    
    * Fix empty album and items cause error
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/google_photos/actions/list-library-contents/list-library-contents.mjs
@@ -0,0 +1,29 @@
+import app from "../../google_photos.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  key: "google_photos-list-library-contents",
+  version: "0.0.1",
+  type: "action",
+  name: "List Library Contents",
+  description: "Retrieves library contents [See the documentation](https://developers.google.com/photos/library/guides/list#listing-library-contents)",
+  props: {
+    app,
+  },
+  async run ({ $ }) {
+    const resourcesStream = utils.getResourcesStream({
+      resourceFn: this.app.listMediaItems,
+      resourceKey: "mediaItems",
+      resourceFnArgs: {
+        $,
+      },
+    });
+    const items = [];
+    for await (const item of resourcesStream) {
+      items.push(item);
+    }
+    // eslint-disable-next-line multiline-ternary
+    $.export("$summary", `${items.length} album${items.length == 1 ? "" : "s"} found.`);
+    return items;
+  },
+};
