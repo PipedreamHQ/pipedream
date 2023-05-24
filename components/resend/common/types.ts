d734dab4fbf86e10a1fd3f7f29ea49637e38c946
GTFalcao <gtfalcao96@gmail.com>
2023-05-23 21:29:15 -0300

    Basic request method

---
+++ components/resend/common/types.ts
@@ -0,0 +1,12 @@
+import { Pipedream } from "@pipedream/types";
+
+interface PdAxiosRequest {
+  $: Pipedream;
+}
+
+export interface HttpRequestParams extends PdAxiosRequest {
+  url: string;
+  method?: string;
+  data?: object;
+  params?: object;
+}
