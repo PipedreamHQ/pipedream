80bfe1f324f8c3a400ecd20718659510dd5a843f
michelle0927 <michellelbergero@hotmail.com>
2023-05-29 15:24:11 -0400

    fix key

--- components/f15five/sources/new-high-five-received/new-high-five-received.mjs
+++ components/f15five/sources/new-high-five-received/new-high-five-received.mjs
@@ -2,7 +2,7 @@ import common from "../common/common.mjs";
 
 export default {
   ...common,
-  key: "f15five-high-five-received",
+  key: "f15five-new-high-five-received",
   name: "New High Five Received",
   description: "Emit new event for each new high five received. [See the documentation](https://my.15five.com/api/public/#tag/High-Five/paths/~1api~1public~1high-five~1/get)",
   version: "0.0.1",
