80660535d0608da642a3d8fc76dcc64b72698089
michelle0927 <michellelbergero@hotmail.com>
2023-05-31 11:31:25 -0400

    getter/setter methods

--- components/snowflake/sources/common.mjs
+++ components/snowflake/sources/common.mjs
@@ -15,6 +15,12 @@ export default {
     },
   },
   methods: {
+    _getLastMaxTimestamp() {
+      return this.db.get("lastMaxTimestamp");
+    },
+    _setLastMaxTimestamp(lastMaxTimestamp) {
+      this.db.set("lastMaxTimestamp", lastMaxTimestamp);
+    },
     async processCollection(statement, timestamp) {
       const rowStream = await this.snowflake.getRows(statement);
       this.$emit({
@@ -101,7 +107,7 @@ export default {
     },
     async watchObjectsAndEmitChanges(objectType, objectsToEmit, queryTypes) {
       // Get the timestamp of the last run, if available. Else set the start time to 1 day ago
-      const lastRun = this.db.get("lastMaxTimestamp") ?? +Date.now() - (1000 * 60 * 60 * 24);
+      const lastRun = this._getLastMaxTimestamp() ?? +Date.now() - (1000 * 60 * 60 * 24);
       console.log(`Max ts of last run: ${lastRun}`);
 
       const newMaxTs = await this.snowflake.maxQueryHistoryTimestamp();
@@ -114,13 +120,13 @@ export default {
       );
       console.log(`Raw results: ${JSON.stringify(results, null, 2)}`);
       this.filterAndEmitChanges(results, objectType, objectsToEmit, queryTypes);
-      await this.db.set("lastMaxTimestamp", newMaxTs);
+      this._setLastMaxTimestamp(newMaxTs);
     },
     async emitFailedTasks({
       database, schema,
     }) {
       // Get the timestamp of the last run, if available. Else set the start time to 1 day ago
-      const lastRun = this.db.get("lastMaxTimestamp") ?? +Date.now() - (1000 * 60 * 60 * 24);
+      const lastRun = this._getLastMaxTimestamp() ?? +Date.now() - (1000 * 60 * 60 * 24);
       console.log(`Max ts of last run: ${lastRun}`);
 
       let results;
@@ -181,7 +187,7 @@ export default {
         );
       }
 
-      await this.db.set("lastMaxTimestamp", Date.now());
+      this._setLastMaxTimestamp(Date.now());
     },
     getStatement() {
       throw new Error("getStatement is not implemented");
