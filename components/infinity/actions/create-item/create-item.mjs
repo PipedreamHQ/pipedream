5aacb4c76b44d7147c53686a1068dcc1689a5149
Luan Cazarine <luanhc@gmail.com>
2023-06-08 17:44:53 -0300

    [Components] Infinity #6771 Triggers:  - Item Created  - Item Updated  - Item Finalized
    Actions:
     - Create Item
     - Update Item
     - Delete Item

---
+++ components/infinity/actions/create-item/create-item.mjs
@@ -0,0 +1,141 @@
+import infinity from "../../infinity.app.mjs";
+
+export default {
+  key: "infinity-create-item",
+  name: "Create Item",
+  version: "0.0.1",
+  description: "Create a new item. [See the documentation](https://s3.amazonaws.com/devdocs.startinfinity.com/index.html#items-POSTapi-v2-workspaces--workspace--boards--board_id--items)",
+  type: "action",
+  props: {
+    infinity,
+    workspaceId: {
+      propDefinition: [
+        infinity,
+        "workspaceId",
+      ],
+    },
+    boardId: {
+      propDefinition: [
+        infinity,
+        "boardId",
+        ({ workspaceId }) => ({
+          workspaceId,
+        }),
+      ],
+    },
+    folderId: {
+      propDefinition: [
+        infinity,
+        "folderId",
+        ({
+          workspaceId, boardId,
+        }) => ({
+          workspaceId,
+          boardId,
+        }),
+      ],
+    },
+    valuesNumber: {
+      type: "integer",
+      label: "Values Quantity",
+      description: "The number of values you want to add.",
+      reloadProps: true,
+    },
+    parentId: {
+      propDefinition: [
+        infinity,
+        "itemId",
+        ({
+          workspaceId, boardId,
+        }) => ({
+          workspaceId,
+          boardId,
+        }),
+      ],
+      description: "The Id of the parent item.",
+      optional: true,
+    },
+  },
+  async additionalProps() {
+    if (this.workspaceId && this.boardId) {
+      return Array.from({
+        length: this.valuesNumber,
+      }).reduce((acc, _, index) => {
+        const pos = index + 1;
+
+        return {
+          ...acc,
+          [`attributeId-${pos}`]: {
+            type: "string",
+            label: `Attribute Id ${pos}`,
+            description: "The id of the atribute.",
+            options: async ({ prevContext }) => {
+              let workspaceId = this.workspaceId;
+              let boardId = this.boardId;
+
+              const {
+                data, after,
+              } = await this.infinity.listAttributes({
+                workspaceId,
+                boardId,
+                params: {
+                  after: prevContext?.after,
+                  limit: 100,
+                },
+              });
+              return {
+                options: Object.entries(data).map(({
+                  id: value, name: label,
+                }) => ({
+                  label,
+                  value,
+                })),
+                context: {
+                  after,
+                },
+              };
+            },
+          },
+          [`data-${pos}`]: {
+            type: "string",
+            label: `Data ${pos}`,
+            description: `The data of the atribute ${pos}.`,
+          },
+        };
+      }, {});
+    }
+  },
+  methods: {
+    getValue(index) {
+      const pos = index + 1;
+      const {
+        [`attributeId-${pos}`]: attribute_id,
+        [`data-${pos}`]: data,
+      } = this;
+      return {
+        attribute_id,
+        data,
+      };
+    },
+    getValues() {
+      return Array.from({
+        length: this.valuesNumber,
+      }).map((_, index) => this.getValue(index));
+    },
+  },
+  async run({ $ }) {
+    const response = await this.infinity.createItem({
+      $,
+      workspaceId: this.workspaceId,
+      boardId: this.boardId,
+      data: {
+        folder_id: this.folderId,
+        parent_id: this.parentId,
+        values: this.getValues(),
+      },
+    });
+
+    $.export("$summary", `A new item with Id: ${response.id} was successfully created!`);
+    return response;
+  },
+};
