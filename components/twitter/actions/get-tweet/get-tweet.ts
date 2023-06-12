5ce8ae096793b3e41daf70f13a881e41aafd0b9f
GTFalcao <gtfalcao96@gmail.com>
2023-06-12 16:25:19 -0300

    version bumps

--- components/twitter/actions/get-tweet/get-tweet.ts
+++ components/twitter/actions/get-tweet/get-tweet.ts
@@ -14,7 +14,7 @@ export default defineAction({
   key: "twitter-get-tweet",
   name: "Get Tweet",
   description: `Return a single tweet specified by ID. [See the documentation](${DOCS_LINK})`,
-  version: "2.0.2",
+  version: "2.0.3",
   type: "action",
   props: {
     app,
