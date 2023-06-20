9a1b9c8589df570746e62b2cbc9ad7a8950b103e
GTFalcao <gtfalcao96@gmail.com>
2023-06-20 00:41:42 -0300

    Creating base request method

--- components/facebook_conversions/app/facebook_conversions.app.ts
+++ components/facebook_conversions/app/facebook_conversions.app.ts
@@ -1,8 +1,25 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
 
 export default defineApp({
   type: "app",
   app: "facebook_conversions",
   propDefinitions: {},
-  methods: {},
+  methods: {
+    _getApiVersion() {
+      return "";
+    },
+    _getPixelId() {
+      return "";
+    },
+    async _httpRequest({
+      $ = this,
+      ...args
+    }/*: HttpRequestParams*/): Promise<object> {
+      return axios($, {
+        baseURL: `https://graph.facebook.com/${this._getApiVersion()}/${this._getPixelId()}`,
+        ...args,
+      });
+    },
+  },
 });
