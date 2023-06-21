17ecb88c27aa38ea792888ca780120630aa854d4
Colin Wu <colin@pipedream.com>
2023-06-21 09:35:53 -0400

    cleanup

--- scripts/findBadKeys.js
+++ scripts/findBadKeys.js
@@ -18,8 +18,8 @@ const isCommonFile = ( subname ) => {
   return regex.test(subname);
 };
 
-isTestEventFile = ( subname ) =>
-  subname.split("/").pop() === "test-event.mjs"
+const isTestEventFile = ( subname ) =>
+  subname.split("/").pop() === "test-event.mjs";
 
 const getComponentKey = ( p )  => {
   const data = fs.readFileSync(p, "utf8");
