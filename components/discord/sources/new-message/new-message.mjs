537aa891b6fcb7fda0bc276442cb20e4178eec57
Danny Roosevelt <danny@pipedream.com>
2023-06-21 15:30:14 -0700

    Created via Pipedream - Add Sample Test Event to Component Source, Create PR (p_8rC8exz)

--- components/discord/sources/new-message/new-message.mjs
+++ components/discord/sources/new-message/new-message.mjs
@@ -1,11 +1,13 @@
 import discord from "../../discord.app.mjs";
+import sampleEmit from "./test-event.mjs";
 
 export default {
   type: "source",
   key: "discord-new-message",
   name: "New Message (Instant)",
   description: "Emit new event for each message posted to one or more channels in a Discord server",
-  version: "1.0.1",
+  version: "1.0.2",
+
   dedupe: "unique",
   props: {
     discord,
@@ -32,4 +34,5 @@ export default {
       id: event.id,
     });
   },
+  sampleEmit,
 };
