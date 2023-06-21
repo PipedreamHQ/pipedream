37bae5195a86a9d66a4546fa975e9bf9d20d71f7
Danny Roosevelt <danny@pipedream.com>
2023-06-21 08:59:30 -0700

    dropbox new file test event

---
+++ components/dropbox/sources/new-file/test-event.mjs
@@ -0,0 +1,13 @@
+export default {
+    ".tag": "file",
+    "name": "pi-small.png",
+    "path_lower": "/doggos/pi-small.png",
+    "path_display": "/Doggos/pi-small.png",
+    "id": "id:a7pFeAtkXNAAAAAAAAAFOO",
+    "client_modified": "2023-06-09T19:45:03Z",
+    "server_modified": "2023-06-09T19:45:03Z",
+    "rev": "015fdb79ae9db1e000000011dc2c0e8",
+    "size": 181521,
+    "is_downloadable": true,
+    "content_hash": "fe24f0ac5d4996582e9ada3a0f3d5381a3fbbada712ce714df3545d2d6e123e5"   
+}
