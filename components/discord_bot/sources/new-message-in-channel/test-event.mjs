909f34b0a9554525407ff33aba64a0190fbd569a
Danny Roosevelt <danny@pipedream.com>
2023-06-27 08:54:13 -0700

    Adding test event json file

---
+++ components/discord_bot/sources/new-message-in-channel/test-event.mjs
@@ -0,0 +1,26 @@
+export default  {
+  "id": "8923472987582391239",
+  "type": 0,
+  "content": "",
+  "channel_id": "893425532980943933",
+  "author": {
+   "id": "283062505550879865",
+   "username": "htgk3",
+   "global_name": null,
+   "avatar": "c2qj9d324dqf68ez62aeb097ccab593z",
+   "discriminator": "3728",
+   "public_flags": 0,
+   "avatar_decoration": null
+  },
+  "attachments": [],
+  "embeds": [],
+  "mentions": [],
+  "mention_roles": [],
+  "pinned": false,
+  "mention_everyone": false,
+  "tts": false,
+  "timestamp": "2024-07-23T16:49:01.783000+00:00",
+  "edited_timestamp": null,
+  "flags": 0,
+  "components": []
+ }
