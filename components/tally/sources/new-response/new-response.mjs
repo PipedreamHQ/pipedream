205d660ab720ee87d70c868846a2be5f54fdf265
Danny Roosevelt <danny@pipedream.com>
2023-06-28 10:51:59 -0700

    Update new-response.mjs

--- components/tally/sources/new-response/new-response.mjs
+++ components/tally/sources/new-response/new-response.mjs
@@ -5,7 +5,6 @@ export default {
   ...common,
   name: "New Response",
   version: "0.0.2",
-
   key: "tally-new-response",
   description: "Emit new event on each form message. [See docs here](https://tallyso.notion.site/Tally-OAuth-2-reference-d0442c679a464664823628f675f43454)",
   type: "source",
