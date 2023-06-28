c402906e8c2df8a6fdb4f32deabba2e495bf5e9b
michelle0927 <michellelbergero@hotmail.com>
2023-06-28 14:20:19 -0400

    new actions

---
+++ components/google_directory/actions/create-user/create-user.mjs
@@ -0,0 +1,76 @@
+import googleDirectory from "../../google_directory.app.mjs";
+
+export default {
+  key: "google_directory-create-user",
+  name: "Create User",
+  description: "Creates a new user. [See the documentation](https://developers.google.com/admin-sdk/directory/reference/rest/v1/users/insert)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    googleDirectory,
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The user's primary email address",
+    },
+    password: {
+      type: "string",
+      label: "Password",
+      description: "The password for the user account",
+    },
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "First name of the contact",
+    },
+    lastName: {
+      type: "string",
+      label: "Last Name",
+      description: "Last name of the contact",
+    },
+    phone: {
+      type: "string",
+      label: "Phone",
+      description: "Phone number of the contact",
+      optional: true,
+    },
+    notes: {
+      type: "string",
+      label: "Notes",
+      description: "Notes for the user",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      primaryEmail: this.email,
+      password: this.password,
+      name: {
+        givenName: this.firstName,
+        familyName: this.lastName,
+      },
+    };
+
+    if (this.phone) {
+      data.phone = {
+        value: this.phone,
+      };
+    }
+
+    if (this.notes) {
+      data.notes = {
+        contentType: "text_plain",
+        value: this.notes,
+      };
+    }
+
+    const response = await this.googleDirectory.createUser({
+      data,
+      $,
+    });
+
+    $.export("$summary", `Successfully created user with ID ${response.id}.`);
+
+    return response;
+  },
+};
---
+++ components/google_directory/google_directory.app.mjs
@@ -0,0 +1,153 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  type: "app",
+  app: "google_directory",
+  propDefinitions: {
+    user: {
+      type: "string",
+      label: "User",
+      description: "Identifier of a user",
+      async options({ prevContext }) {
+        const { nextPageToken: pageToken } = prevContext;
+        const params = {
+          customer: "my_customer",
+          pageToken,
+        };
+        const {
+          users, nextPageToken,
+        } = await this.listUsers({
+          params,
+        });
+        if (!users) {
+          return;
+        }
+        const options = users.map(({
+          id, name, primaryEmail,
+        }) => ({
+          label: name.fullName || primaryEmail,
+          value: id,
+        }));
+        return {
+          options,
+          context: {
+            nextPageToken,
+          },
+        };
+      },
+    },
+    group: {
+      type: "string",
+      label: "Group",
+      description: "Identifier of a group",
+      async options({ prevContext }) {
+        const { nextPageToken: pageToken } = prevContext;
+        const params = {
+          customer: "my_customer",
+          pageToken,
+        };
+        const {
+          groups, nextPageToken,
+        } = await this.listGroups({
+          params,
+        });
+        if (!groups) {
+          return;
+        }
+        const options = groups.map(({
+          id, name,
+        }) => ({
+          label: name,
+          value: id,
+        }));
+        return {
+          options,
+          context: {
+            nextPageToken,
+          },
+        };
+      },
+    },
+  },
+  methods: {
+    _baseUrl() {
+      return "https://admin.googleapis.com/admin/directory/v1";
+    },
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      });
+    },
+    getGroup({
+      groupId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/groups/${groupId}`,
+        ...args,
+      });
+    },
+    getUser({
+      userId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/users/${userId}`,
+        ...args,
+      });
+    },
+    listGroups(args = {}) {
+      return this._makeRequest({
+        path: "/groups",
+        ...args,
+      });
+    },
+    listUsers(args = {}) {
+      return this._makeRequest({
+        path: "/users",
+        ...args,
+      });
+    },
+    createGroup(args = {}) {
+      return this._makeRequest({
+        path: "/groups",
+        method: "POST",
+        ...args,
+      });
+    },
+    createUser(args = {}) {
+      return this._makeRequest({
+        path: "/users",
+        method: "POST",
+        ...args,
+      });
+    },
+    async *paginate({
+      fn, params = {}, itemType, $,
+    }) {
+      do {
+        const response = await fn({
+          params,
+          $,
+        });
+        const items = response[itemType];
+        if (!items?.length) {
+          return;
+        }
+        for (const d of items) {
+          yield d;
+        }
+        params.pageToken = response?.nextPageToken;
+      } while (params.pageToken);
+    },
+  },
+};
