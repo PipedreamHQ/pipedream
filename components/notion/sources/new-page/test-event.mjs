8e10717c449af1e385406115e827b7408e522538
Danny Roosevelt <danny@pipedream.com>
2023-06-29 00:00:43 -0700

    Adding test event json file

---
+++ components/notion/sources/new-page/test-event.mjs
@@ -0,0 +1,72 @@
+export default  {
+  "object": "page",
+  "id": "a2ccghf0-327g-5h45-vv12-7p45k4f78l99",
+  "created_time": "2030-04-15T13:42:00.000Z",
+  "last_edited_time": "2030-04-15T13:42:00.000Z",
+  "created_by": {
+   "object": "user",
+   "id": "f562936h-c461-758f-3i6b-cd35w321987k"
+  },
+  "last_edited_by": {
+   "object": "user",
+   "id": "f562936h-c461-758f-3i6b-cd35w321987k"
+  },
+  "cover": null,
+  "icon": null,
+  "parent": {
+   "type": "database_id",
+   "database_id": "5bd2g5cf-89cd-5599-b798-350bc9f8ccde"
+  },
+  "archived": false,
+  "properties": {
+   "Property C": {
+    "id": "%5Drrg",
+    "type": "rich_text",
+    "rich_text": []
+   },
+   "Tags": {
+    "id": "Lw%2PJ",
+    "type": "multi_select",
+    "multi_select": []
+   },
+   "Status": {
+    "id": "fclk",
+    "type": "status",
+    "status": {
+     "id": "fg718403-jg31-8i90-df32-49p8945720ld",
+     "name": "In progress",
+     "color": "default"
+    }
+   },
+   "Property D": {
+    "id": "pkL%5D",
+    "type": "rich_text",
+    "rich_text": []
+   },
+   "Name": {
+    "id": "title",
+    "type": "title",
+    "title": [
+     {
+      "type": "text",
+      "text": {
+       "content": "New Project 3019 2",
+       "link": null
+      },
+      "annotations": {
+       "bold": false,
+       "italic": false,
+       "strikethrough": false,
+       "underline": false,
+       "code": false,
+       "color": "default"
+      },
+      "plain_text": "New Project 3019 2",
+      "href": null
+     }
+    ]
+   }
+  },
+  "url": "https://www.notion.so/New-Project-3019-2-a2ccghf0327g5h45vv127p45k4f78l99",
+  "public_url": null
+ }
