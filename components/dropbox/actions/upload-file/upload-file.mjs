9a62a9291bac1721d5e26f2038e2a1b31d649ad6
Alyson Freitas <alyson.f@turing.com>
2023-06-30 13:03:17 -0300

    update versions from dropbox components

--- components/dropbox/actions/upload-file/upload-file.mjs
+++ components/dropbox/actions/upload-file/upload-file.mjs
@@ -9,7 +9,7 @@ export default {
   name: "Upload a File",
   description: "Uploads a file to a selected folder. [See docs here](https://dropbox.github.io/dropbox-sdk-js/Dropbox.html#filesUpload__anchor)",
   key: "dropbox-upload-file",
-  version: "0.0.7",
+  version: "0.0.8",
   type: "action",
   props: {
     dropbox,
