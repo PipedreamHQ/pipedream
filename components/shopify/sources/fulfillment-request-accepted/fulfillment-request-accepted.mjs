d5d386a67130b17ba3ca05503ed0f9ae5eacc138
Colin Mollenhour <colin@mollenhour.com>
2023-07-03 13:22:45 -0400

    Shopify: Add sources for accepting and rejecting a fulfillment request.

---
+++ components/shopify/sources/fulfillment-request-accepted/fulfillment-request-accepted.mjs
@@ -0,0 +1,25 @@
+import common from "../common/webhook.mjs";
+import constants from "../common/constants.mjs";
+
+export default {
+  ...common,
+  key: "shopify-fulfillment-request-accepted",
+  name: "Fulfillment Request Accepted",
+  type: "source",
+  description: "Occurs when a fulfillment service accepts a fulfillment request that was sent by a merchant.",
+  version: "0.0.7",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getTopic() {
+      return constants.EVENT_TOPIC.FULFILLMENT_ORDERS_FULFILLMENT_REQUEST_ACCEPTED;
+    },
+    generateMeta(resource) {
+      return {
+        id: fulfillment_order.id,
+        summary: `Fulfillment request accepted for fulfillment order.`,
+        ts: Date.now(),
+      };
+    },
+  },
+};
