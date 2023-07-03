0d697ebcec290e2edc7a29591d2f7264c8cf3164
Cassiano Vellames <cassiano.v@turing.com>
2023-07-03 18:29:24 -0300

    Find Person

---
+++ components/agendor/agendor.app.mjs
@@ -0,0 +1,58 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  type: "app",
+  app: "agendor",
+  propDefinitions: {
+    personId: {
+      type: "string",
+      label: "Person ID",
+      description: "The ID of the person to retrieve.",
+      async options(prevContext) {
+        const persons = await this.listPersons(prevContext.page + 1);
+        return persons.data.map((person) => ({
+          label: person.name,
+          value: person.id,
+        }));
+      },
+    },
+  },
+  methods: {
+    _getApiToken() {
+      return this.$auth.api_token;
+    },
+    _getBaseUrl() {
+      return "https://api.agendor.com.br/v3";
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Token ${this._getApiToken()}`,
+      };
+    },
+    async _makeHttpRequest(opts = {}, ctx = this) {
+      const axiosOpts = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return axios(ctx, axiosOpts);
+    },
+    async getPerson(personId) {
+      return this._makeHttpRequest({
+        method: "GET",
+        path: `/people/${personId}`,
+      });
+    },
+    async listPersons(page) {
+      return this._makeHttpRequest({
+        method: "GET",
+        path: "/people",
+        params: {
+          page,
+          per_page: 100,
+        },
+      });
+    },
+  },
+};
