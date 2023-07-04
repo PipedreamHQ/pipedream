5c8c5cdfaa0b8842374fa027e566cecf2570c5ad
GTFalcao <gtfalcao96@gmail.com>
2023-07-04 18:06:18 -0300

    Adding server event params

--- components/facebook_conversions/actions/send-event-data/send-event-data.ts
+++ components/facebook_conversions/actions/send-event-data/send-event-data.ts
@@ -1,15 +1,94 @@
-import { Pipedream, defineAction } from "@pipedream/types";
+import { defineAction } from "@pipedream/types";
 import app from "../../app/facebook_conversions.app";
+import { ACTION_SOURCE_OPTIONS } from "../../common/constants";
+
+const URL_DOCS_PARAMS = "https://developers.facebook.com/docs/marketing-api/conversions-api/parameters";
+const URL_DOCS_SERVER_EVENT = `[See more on the documentation](${URL_DOCS_PARAMS}/server-event).`;
 
 export default defineAction({
-  name: "[Date/Time] Compare Dates",
+  name: "Send Event Data",
   description:
-    "Get the duration between two dates in days, hours, minutes, and seconds along with checking if they are the same.",
-  key: "formatting-compare-dates",
-  version: "0.0.2",
+    "Send an event to the Conversions API. [See the documentation](https://developers.facebook.com/docs/marketing-api/conversions-api/using-the-api)",
+  key: "facebook_conversions-send-event-data",
+  version: "0.0.1",
   type: "action",
   props: {
     app,
+    eventName: {
+      type: "string",
+      label: "Event Name",
+      description: `A standard event or custom event name. ${URL_DOCS_SERVER_EVENT}`
+    },
+    eventTime: {
+      type: "integer",
+      label: "Event Time",
+      description: `A Unix timestamp in seconds indicating when the actual event occurred. ${URL_DOCS_SERVER_EVENT}`
+    },
+    userData: {
+      type: "object",
+      label: "User Data",
+      description: `A map that contains customer information data. ${URL_DOCS_SERVER_EVENT}`
+    },
+    actionSource: {
+      type: "string",
+      label: "Action Source",
+      description: `This field allows you to specify where your conversions occurred. ${URL_DOCS_SERVER_EVENT}`,
+      options: ACTION_SOURCE_OPTIONS
+    },
+    customData: {
+      type: "object",
+      label: "Custom Data",
+      description: `A map that includes additional business data about the event. [See more on the documentation](https://developers.facebook.com/docs/marketing-api/conversions-api/parameters/custom-data).`,
+      optional: true,
+    },
+    eventSourceUrl: {
+      type: "string",
+      label: "Event Source URL",
+      description: `The browser URL where the event happened. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true
+    },
+    optOut: {
+      type: "boolean",
+      label: "Opt Out",
+      description: "A flag that indicates the event should not be used for ads delivery optimization. If set to true, the event is only used for attribution.",
+      optional: true,
+    },
+    eventId: {
+      type: "string",
+      label: "Event ID",
+      description: `This ID can be any *unique* string chosen by the advertiser. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    },
+    dataProcessingOptions: {
+      type: "string[]",
+      label: "Data Processing Options",
+      description: `Processing options you would like to enable for a specific event. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    },
+    dataProcessingOptionsCountry: {
+      type: "integer",
+      label: "Data Processing Options: Country",
+      description: `A country that you want to associate to this data processing option. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    },
+    dataProcessingOptionsState: {
+      type: "integer",
+      label: "Data Processing Options: State",
+      description: `A state that you want to associate to this data processing option. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    },
+    appData: {
+      type: "object",
+      label: "App Data",
+      description: `Parameters for sharing app data and device information with the Conversions API. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    },
+    extendedInfo: {
+      type: "object",
+      label: "Extended Info",
+      description: `Extended device information, such as screen width and height. ${URL_DOCS_SERVER_EVENT}`,
+      optional: true,
+    }
   },
   async run({ $ }): Promise<object> {
     const params = { $ };
---
+++ components/facebook_conversions/common/constants.ts
@@ -0,0 +1,24 @@
+export const ACTION_SOURCE_OPTIONS = [
+  {
+    label: "Conversion happened over email.",
+    value: "email",
+  },
+  { label: "Conversion was made on your website.", value: "website" },
+  { label: "Conversion was made on your mobile app.", value: "app" },
+  { label: "Conversion was made over the phone.", value: "phone_call" },
+  {
+    label:
+      "Conversion was made via a messaging app, SMS, or online messaging feature.",
+    value: "chat",
+  },
+  {
+    label: "Conversion was made in person at your physical store.",
+    value: "physical_store",
+  },
+  {
+    label:
+      "Conversion happened automatically, for example, a subscription renewal that's set to auto-pay each month.",
+    value: "system_generated",
+  },
+  { label: "Conversion happened in a way that is not listed.", value: "other" },
+];
