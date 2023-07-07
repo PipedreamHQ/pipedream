c6b28e4da111395a83a88453dd4cf8bfc397fe49
michelle0927 <michelle0927@users.noreply.github.com>
2023-07-07 08:49:54 -0700

    Eventbrite - Get My Orders Action & New Order Placed By Me Trigger (#7177)
    * new user order components
    
    * fix version
    
    ---------
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>

--- components/eventbrite/sources/new-event/new-event.mjs
+++ components/eventbrite/sources/new-event/new-event.mjs
@@ -5,7 +5,7 @@ export default {
   key: "eventbrite-new-event",
   name: "New Event (Instant)",
   description: "Emit new event when an event has been created",
-  version: "0.0.4",
+  version: "0.0.5",
   dedupe: "unique",
   type: "source",
   methods: {
