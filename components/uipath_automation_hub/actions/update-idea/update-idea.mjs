85e76fddd74d156b7c54281c9c1a76dc680c71b0
Luan Cazarine <luanhc@gmail.com>
2023-07-11 15:39:29 -0300

    6988 components uipath automation hub (#7102)
    * Action: Create Idea
    
    * Triggers:
      - Process Published.
    
    Actions:
     - Create Idea
     - Update Idea
     - Update Category Idea
    
    * some adjusts
    
    * Update components/uipath_automation_hub/sources/automation-published/automation-published.mjs
    
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>
    
    ---------
    
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

---
+++ components/uipath_automation_hub/actions/update-idea/update-idea.mjs
@@ -0,0 +1,72 @@
+import { ConfigurationError } from "@pipedream/platform";
+import app from "../../uipath_automation_hub.app.mjs";
+
+export default {
+  key: "uipath_automation_hub-update-idea",
+  name: "Update Idea",
+  version: "0.0.1",
+  description: "Modify an existing automation idea's details. [See the documentation](https://automation-hub.uipath.com/api/v1/api-doc/#/Automation%20Ideas/SubmitIdeaEmployeeDrivenwithSubmitter)",
+  type: "action",
+  props: {
+    app,
+    automationId: {
+      propDefinition: [
+        app,
+        "automationId",
+      ],
+    },
+    processName: {
+      propDefinition: [
+        app,
+        "processName",
+      ],
+      optional: true,
+    },
+    processDescription: {
+      propDefinition: [
+        app,
+        "processDescription",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      automationId,
+      processName,
+      processDescription,
+    } = this;
+
+    const dataObj = {};
+    if (processName) {
+      dataObj["OVR-OVERVIEW_NAME"] = {
+        "value": processName,
+      };
+    }
+    if (processDescription) {
+      dataObj["OVR-OVERVIEW_DESCRIPTION"] = {
+        "value": processDescription,
+      };
+    }
+
+    if (!Object.entries(dataObj).length) {
+      throw new ConfigurationError("You must fill in at least one of the fields.");
+    }
+
+    const response = await app.updateIdea({
+      $,
+      automationId,
+      data: {
+        "user_inputs": {
+          "OVR": {
+            "ah-section-ovr-0-0": dataObj,
+          },
+        },
+      },
+    });
+
+    $.export("$summary", `The idea with Id: ${automationId} was successfully updated!`);
+    return response;
+  },
+};
