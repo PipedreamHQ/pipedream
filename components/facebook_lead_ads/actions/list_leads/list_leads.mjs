75a1943a8d4f40a49ca708184bdd900de7ee20a9
Cassiano Vellames <cassiano.v@turing.com>
2023-07-11 12:04:51 -0300

    List Leads Improvements

--- components/facebook_lead_ads/actions/list_leads/list_leads.mjs
+++ components/facebook_lead_ads/actions/list_leads/list_leads.mjs
@@ -5,13 +5,24 @@ export default {
   key: "facebook_lead_ads-list_leads",
   name: "List Leads",
   description: "List Leads using Ad or Form ID. [See the documentation](https://developers.facebook.com/docs/marketing-api/guides/lead-ads/retrieving#reading-store-locator-question-value)",
-  version: "0.0.4",
+  version: "0.0.14",
   props: {
     app,
-    id: {
-      type: "string",
-      label: "Ad or Form ID",
-      description: "The ID of the Ad or Form to retrieve leads from.",
+    pageId: {
+      propDefinition: [
+        app,
+        "pageId",
+      ],
+    },
+    formId: {
+      propDefinition: [
+        app,
+        "formId",
+        (c) => ({
+          pageId: c.pageId,
+        }),
+      ],
+      optional: true,
     },
     additionalParams: {
       type: "object",
@@ -30,9 +41,9 @@ export default {
         ...this.additionalParams,
         after,
       };
-      res = await this.app.listLeadsByAdOrFormId(this.id, params);
+      res = await this.app.listLeadsByAdOrFormId(this.formId, params);
       data.push(...res.data);
-      after = res.paging.cursors?.after;
+      after = res.paging?.cursors?.after;
     } while (res.paging.next);
 
     if (data.length === 0) {
--- components/facebook_lead_ads/facebook_lead_ads.app.mjs
+++ components/facebook_lead_ads/facebook_lead_ads.app.mjs
@@ -3,10 +3,63 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "facebook_lead_ads",
-  propDefinitions: {},
+  propDefinitions: {
+    pageId: {
+      type: "string",
+      label: "Page ID",
+      description: "The ID of the page to retrieve forms from.",
+      async options({ prevContext }) {
+        const params = {
+          after: prevContext?.nextPage,
+        };
+        const res = await this.listPages(params);
+        return {
+          options: res.data.map((form) => ({
+            label: form.name,
+            value: form.id,
+          })),
+          context: {
+            nextPage: res.paging?.cursors?.after,
+          },
+        };
+      },
+    },
+    formId: {
+      type: "string",
+      label: "Form ID",
+      description: "The ID of the form to retrieve leads from.",
+      async options({
+        prevContext,
+        pageId,
+      }) {
+        if (!pageId) {
+          return [];
+        }
+        const params = {
+          after: prevContext?.nextPage,
+        };
+
+        const res = await this.listForms(
+          pageId,
+          params,
+          this._getPageAccessToken(pageId),
+        );
+        return {
+          options: res.data.map((form) => ({
+            label: form.name,
+            value: form.id,
+          })),
+          context: {
+            nextPage: res.paging?.cursors?.after,
+          },
+        };
+      },
+    },
+  },
   methods: {
     _getAccessToken() {
-      return this.$auth.oauth_access_token;
+      // return this.$auth.oauth_access_token;
+      return "EAAXkfGDLF9UBALCGNvVEEDbShU7jsjEzZASZCLZAYe0PUtt3PeZCDsOXbeFe28fq7ZA4z0R0nZBAViJoKIZA3eOnYRut1ADZBtljul0L1UhUAzT71bJq2msQNTKAFaZAVNIhV6lq7r4f8s10bhnuIBINAmZBeYUxIj2KQXda35EmpW3tUaUvJjudZBcuTpyNXRJv2HumZAI2wMy2ZCTsnb077T9z4";
     },
     _getApiVersion() {
       return "v17.0";
@@ -26,10 +79,35 @@ export default {
         headers: this._getHeaders(),
         params: {
           ...opts.params,
-          access_token: this._getAccessToken(),
+          access_token: opts.overrideAccessToken ?? this._getAccessToken(),
         },
       };
       return axios(ctx, axiosOpts);
+    },
+    async _getAllPages() {
+      const data = [];
+      let res;
+      let after = undefined;
+      do {
+        const params = {
+          after,
+        };
+        res = await this.listPages(params);
+        data.push(...res.data);
+        after = res.paging?.cursors?.after;
+      } while (res.paging.next);
+
+      return data;
+    },
+    // Form Id must be fetched with page id. Since we cant store more than one value in
+    // an assync option, we need to fetch the page id from the previous step.
+    async _getPageAccessToken(pageId) {
+      const allPages = await this._getAllPages();
+      const page = allPages.find((page) => page.id === pageId);
+      if (!page) {
+        throw new Error(`Page with id ${pageId} not found`);
+      }
+      return page.access_token;
     },
     async listLeadsByAdOrFormId(id, params) {
       return this._makeHttpRequest({
@@ -42,6 +120,21 @@ export default {
       return this._makeHttpRequest({
         method: "GET",
         path: `/${id}`,
+      });
+    },
+    async listForms(pageId, params, overrideAccessToken) {
+      return this._makeHttpRequest({
+        method: "GET",
+        path: `/${pageId}/leadgen_forms`,
+        params,
+        overrideAccessToken,
+      });
+    },
+    async listPages(params) {
+      return this._makeHttpRequest({
+        method: "GET",
+        path: "/me/accounts",
+        params,
       });
     },
   },
