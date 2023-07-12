043d7d63f954e28d48af0f652c17e9502de8d9d0
Luan Cazarine <luanhc@gmail.com>
2023-07-12 12:08:35 -0300

    6822 components clientify (#7211)
    * Actions:
      - Create Client
      - Update Client
      - Create Task
    
    * Triggers:
      - Client Added
      - Task Completed
    
    * fix lint
    
    * fix action slug
    
    * Fix task completed source
    
    * Improve start datetime and end datetime desc
    
    * Improve Due Date desc
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/clientify/sources/task-completed/task-completed.mjs
@@ -0,0 +1,26 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "clientify-task-completed",
+  name: "New Task Completed",
+  version: "0.0.1",
+  description: "Emit new event when a task is completed.",
+  type: "source",
+  methods: {
+    ...common.methods,
+    getFunction() {
+      return this.clientify.listTasks;
+    },
+    getSummary({ id }) {
+      return `The task with id: "${id}" was completed!`;
+    },
+    getParams(lastDate) {
+      return {
+        "orderBy": "created",
+        "created[gte]": lastDate,
+        "status_id": 5,
+      };
+    },
+  },
+};
