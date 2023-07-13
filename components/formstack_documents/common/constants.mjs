8782e00c931d329fa7ec4225307e6f7883480d5d
michelle0927 <michellelbergero@hotmail.com>
2023-07-13 16:57:45 -0400

    new components

---
+++ components/formstack_documents/common/constants.mjs
@@ -0,0 +1,20 @@
+const DOCUMENT_TYPES = [
+  "html",
+  "pdf",
+  "docx",
+  "xlsx",
+  "pptx",
+];
+
+const DOCUMENT_OUTPUT_TYPES = [
+  "pdf",
+  "docx",
+  "xlsx",
+  "pptx",
+  "email",
+];
+
+export default {
+  DOCUMENT_TYPES,
+  DOCUMENT_OUTPUT_TYPES,
+};
