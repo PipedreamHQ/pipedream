33f4be04dec0ad9705b4fdcec2e990f95459d7d5
michelle0927 <michelle0927@users.noreply.github.com>
2023-07-17 07:52:38 -0700

    Loops.so - New Components (#7269)
    * new components
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

---
+++ components/loops_so/actions/send-transactional-email/send-transactional-email.mjs
@@ -0,0 +1,42 @@
+import loops from "../../loops_so.app.mjs";
+
+export default {
+  key: "loops_so-send-transactional-email",
+  name: "Send Transactional Email",
+  description: "Send a transactional email. [See the Documentation](https://loops.so/docs/transactional/guide#send-your-email)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    loops,
+    transactionalId: {
+      type: "string",
+      label: "Transactional ID",
+      description: "The transactional ID",
+    },
+    email: {
+      propDefinition: [
+        loops,
+        "email",
+      ],
+    },
+    dataVariables: {
+      type: "object",
+      label: "Data Variables",
+      description: "Key/value pairs specifying the data variables for this transactional email",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.loops.sendTransactionalEmail({
+      data: {
+        transactionalId: this.transactionalId,
+        email: this.email,
+        dataVariables: this.dataVariables,
+      },
+      $,
+    });
+
+    $.export("$summary", `Successfully sent transactional email to  ${this.email}.`);
+
+    return response;
+  },
+};
