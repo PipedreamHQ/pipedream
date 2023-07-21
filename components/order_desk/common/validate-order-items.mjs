f7ce861fcfc40a6e904f15b3900eebea00d944a0
Cassiano Vellames <cassiano.v@turing.com>
2023-07-21 10:06:33 -0300

    Validate array

--- components/order_desk/common/validate-order-items.mjs
+++ components/order_desk/common/validate-order-items.mjs
@@ -1,11 +1,19 @@
+import { ConfigurationError } from "@pipedream/platform";
+
 export const getParsedOrderItems = (orderItems) => {
   const orderItemsParsed = [];
   for (let i = 0; i < orderItems.length; i++) {
+    let item;
     try {
-      orderItemsParsed.push(JSON.parse(orderItems[i]));
+      item = JSON.parse(orderItems[i]);
     } catch (err) {
       throw new Error(`Invalid JSON string for ordem_item at index ${i}: ${orderItems[i]}`);
     }
+
+    if (Array.isArray(item)) {
+      throw new ConfigurationError("You cant set an array as an order_item. If you want to send multiple items, please create a new child.");
+    }
+    orderItemsParsed.push(item);
   }
   return orderItemsParsed;
 };
