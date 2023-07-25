fdeaa5f23db7f78320f82fab6b813f440244b452
Pipedream Component Development <106282845+pipedream-component-development@users.noreply.github.com>
2023-07-25 01:31:00 -0700

    Adding test event json file

---
+++ components/gmail_custom_oauth/sources/new-email-received/test-event.mjs
@@ -0,0 +1,264 @@
+export default  {
+  "id": "1886a95d77",
+  "threadId": "1886a95d77",
+  "labelIds": [
+   "UNREAD",
+   "CATEGORY_P",
+   "INBOX"
+  ],
+  "snippet": "Hello from",
+  "payload": {
+   "partId": null,
+   "mimeType": "multipart/",
+   "filename": null,
+   "headers": [
+    {
+     "name": "Delivered-",
+     "value": "hung.v@tur"
+    },
+    {
+     "name": "Received",
+     "value": "by 2002:ac"
+    },
+    {
+     "name": "X-Google-S",
+     "value": "ACHHUZ4m3K"
+    },
+    {
+     "name": "X-Received",
+     "value": "by 2002:a1"
+    },
+    {
+     "name": "ARC-Seal",
+     "value": "i=2; a=rsa"
+    },
+    {
+     "name": "ARC-Messag",
+     "value": "i=2; a=rsa"
+    },
+    {
+     "name": "ARC-Authen",
+     "value": "i=2; mx.go"
+    },
+    {
+     "name": "Return-Pat",
+     "value": "<vunguyenh"
+    },
+    {
+     "name": "Received",
+     "value": "from JPN01"
+    },
+    {
+     "name": "Received-S",
+     "value": "pass (goog"
+    },
+    {
+     "name": "Authentica",
+     "value": "mx.google."
+    },
+    {
+     "name": "ARC-Seal",
+     "value": "i=1; a=rsa"
+    },
+    {
+     "name": "ARC-Messag",
+     "value": "i=1; a=rsa"
+    },
+    {
+     "name": "ARC-Authen",
+     "value": "i=1; mx.mi"
+    },
+    {
+     "name": "DKIM-Signa",
+     "value": "v=1; a=rsa"
+    },
+    {
+     "name": "Received",
+     "value": "from OSYP2"
+    },
+    {
+     "name": "Received",
+     "value": "from OSYP2"
+    },
+    {
+     "name": "From",
+     "value": "Vu Hung <v"
+    },
+    {
+     "name": "To",
+     "value": "Hung Leo V"
+    },
+    {
+     "name": "Subject",
+     "value": "Hello!"
+    },
+    {
+     "name": "Thread-Top",
+     "value": "Hello!"
+    },
+    {
+     "name": "Thread-Ind",
+     "value": "AQHZkqJQ2p"
+    },
+    {
+     "name": "Date",
+     "value": "Tue, 30 Ma"
+    },
+    {
+     "name": "Message-ID",
+     "value": "<OSYP286MB"
+    },
+    {
+     "name": "Accept-Lan",
+     "value": "vi-VN, en-"
+    },
+    {
+     "name": "Content-La",
+     "value": "vi-VN"
+    },
+    {
+     "name": "X-MS-Has-A",
+     "value": null
+    },
+    {
+     "name": "X-MS-TNEF-",
+     "value": null
+    },
+    {
+     "name": "msip_label",
+     "value": null
+    },
+    {
+     "name": "x-ms-excha",
+     "value": "1"
+    },
+    {
+     "name": "x-tmn",
+     "value": "[5FjRiZ8DJ"
+    },
+    {
+     "name": "x-ms-publi",
+     "value": "Email"
+    },
+    {
+     "name": "x-ms-traff",
+     "value": "OSYP286MB0"
+    },
+    {
+     "name": "x-ms-offic",
+     "value": "425567b9-7"
+    },
+    {
+     "name": "x-microsof",
+     "value": "BCL:0;"
+    },
+    {
+     "name": "x-microsof",
+     "value": "RfAIfjBKsG"
+    },
+    {
+     "name": "x-ms-excha",
+     "value": "1"
+    },
+    {
+     "name": "x-ms-excha",
+     "value": "rkKce6PJeP"
+    },
+    {
+     "name": "Content-Ty",
+     "value": "multipart/"
+    },
+    {
+     "name": "MIME-Versi",
+     "value": "1.0"
+    },
+    {
+     "name": "X-Originat",
+     "value": "outlook.co"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "Internal"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "OSYP286MB0"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "00000000-0"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "425567b9-7"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "30 May 202"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "Hosted"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "84df9e7f-e"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "00000000-0"
+    },
+    {
+     "name": "X-MS-Excha",
+     "value": "TYCP286MB3"
+    }
+   ],
+   "body": {
+    "size": null
+   },
+   "parts": [
+    {
+     "partId": "0",
+     "mimeType": "text/plain",
+     "filename": null,
+     "headers": [
+      {
+       "name": "Content-Ty",
+       "value": "text/plain"
+      },
+      {
+       "name": "Content-Tr",
+       "value": "quoted-pri"
+      }
+     ],
+     "body": {
+      "size": 37,
+      "data": "SGVsbG8gZn"
+     }
+    },
+    {
+     "partId": "1",
+     "mimeType": "text/html",
+     "filename": null,
+     "headers": [
+      {
+       "name": "Content-Ty",
+       "value": "text/html;"
+      },
+      {
+       "name": "Content-Tr",
+       "value": "quoted-pri"
+      }
+     ],
+     "body": {
+      "size": 672,
+      "data": "PGh0bWw-DQ"
+     }
+    }
+   ]
+  },
+  "sizeEstimate": 10443,
+  "historyId": "430402",
+  "internalDate": "1685415381",
+  "decodedContent": "Hello from"
+ }
