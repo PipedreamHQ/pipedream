ba7e675552ba5a9e3fcca0ae4a5f2a50a38254a7
Andrew Chuang <andrewjschuang@gmail.com>
2023-07-27 09:54:35 -0300

    print to sdout

--- packages/component_code_gen/run.py
+++ packages/component_code_gen/run.py
@@ -8,17 +8,9 @@ load_dotenv()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--app', '-a', help='the app_name_slug', required=True)
-    parser.add_argument('--no-print', dest='noprint', help='do not print output to terminal', required=False, default=False, action='store_true')
     parser.add_argument('prompt', help='the prompt to send to gpt-4, in between quotes')
     args = parser.parse_args()
 
     code = main(args.app, args.prompt, verbose=True)
     result = transform(code)
-
-    if (args.noprint is False):
-        print(result)
-
-    out = 'output.mjs'
-    with open(out, 'w') as f:
-        f.write(result)
-        print("wrote output to `{out}` file")
+    print(result)
