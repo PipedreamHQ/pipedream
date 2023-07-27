f75ad2b03a13f2662d7d72badb400c03a953cb1b
Danny Roosevelt <danny@pipedream.com>
2023-07-26 22:07:32 -0700

    Merge branch 'master' of https://github.com/PipedreamHQ/pipedream

--- components/zoho_cliq/app/zoho_cliq.app.ts
+++ components/zoho_cliq/app/zoho_cliq.app.ts
@@ -2,7 +2,7 @@ import { defineApp } from "@pipedream/types";
 
 export default defineApp({
   type: "app",
-  app: "axonaut",
+  app: "zoho_cliq",
   propDefinitions: {},
   methods: {
     // this.$auth contains connected account data
