c7cc17766e69be7295fd045b872e895f2b4caf37
Lucas Caresia <lucascarezia@gmail.com>
2023-08-01 09:26:15 -0300

    Added actions

---
+++ components/zoho_meeting/actions/get-meetings/get-meetings.mjs
@@ -0,0 +1,42 @@
+import app from "../../zoho_meeting.app.mjs";
+
+export default {
+  name: "Get Meetings",
+  version: "0.0.1",
+  key: "zoho_meeting-get-meetings",
+  description: "Get a list of meetings. [See docs here](https://www.zoho.com/meeting/api-integration/meeting-api/list-of-meeting-api.html)",
+  type: "action",
+  props: {
+    app,
+    index: {
+      label: "Index",
+      description: "The index or position of first meeting to list",
+      type: "integer",
+      optional: true,
+      default: 0,
+    },
+    count: {
+      label: "Count",
+      description: "Quantity of meetings to list",
+      type: "integer",
+      optional: true,
+      default: 100,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getMeetings({
+      $,
+      params: {
+        listtype: "all",
+        index: this.index,
+        count: this.count,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully retrieved ${response.session.length} meetings`);
+    }
+
+    return response;
+  },
+};
