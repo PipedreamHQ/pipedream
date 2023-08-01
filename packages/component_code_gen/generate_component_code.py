6fda90f12f2914b37186475e69522ecfd0731a0a
Andrew Chuang <andrewjschuang@gmail.com>
2023-08-01 12:55:07 -0300

    modify logic for main

--- packages/component_code_gen/generate_component_code.py
+++ packages/component_code_gen/generate_component_code.py
@@ -1,18 +1,12 @@
-import os
 import argparse
-import templates.webhook_samples
+import generate_action
+import generate_webhook_sample
+import generate_webhook_source
 
 
-def main(app, prompt, verbose=False):
+def main(action, app, prompt, verbose=False):
     validate_inputs(app, prompt)
-
-    if verbose:
-        os.environ['DEBUG'] = '1'
-
-    # this is here so that the DEBUG environment variable is set before the imports
-    from code_gen.generate_component_code import main
-
-    result = main(app, prompt, templates.webhook_samples)
+    result = actions[action](app, prompt, verbose)
     return result
 
 
@@ -24,11 +18,19 @@ def validate_inputs(app, prompt):
         raise Exception('app and prompt should be strings')
 
 
+actions = {
+    'generate_action': generate_action.main,
+    'generate_webhook_sample': generate_webhook_sample.main,
+    'generate_webhook_source': generate_webhook_source.main,
+}
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--app', '-a', help='the app_name_slug', required=True)
+    parser.add_argument('--action', help='which kind of code you want to generate?', choices=actions.keys(), required=True)
+    parser.add_argument('--app', help='the app_name_slug', required=True)
     parser.add_argument('prompt', help='the prompt to send to gpt-4, in between quotes')
     parser.add_argument('--verbose', dest='verbose', help='set the logging to debug', required=False, default=False, action='store_true')
     args = parser.parse_args()
-    result = main(args.app, args.prompt, args.verbose)
+    result = main(args.action, args.app, args.prompt, args.verbose)
     print(result)
--- packages/component_code_gen/generate_webhook_source.py
+++ packages/component_code_gen/generate_webhook_source.py
@@ -3,22 +3,32 @@ import argparse
 import templates.generate_webhook_sources
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--app', '-a', help='the app_name_slug', required=True)
-    parser.add_argument('prompt', help='the prompt to send to gpt-4, in between quotes')
-    parser.add_argument('--verbose', dest='verbose', help='set the logging to debug', required=False, default=False, action='store_true')
-    args = parser.parse_args()
+def main(app, prompt, verbose=False):
+    validate_inputs(app, prompt)
 
-    if args.verbose:
+    if verbose:
         os.environ['DEBUG'] = '1'
 
     # this is here so that the DEBUG environment variable is set before the imports
     from code_gen.generate_component_code import main
 
-    result = main(args.app, args.prompt, templates.generate_webhook_sources)
-    print(result)
+    result = main(app, prompt, templates.generate_webhook_sources)
+    return result
+
+
+def validate_inputs(app, prompt):
+    if not (bool(app) and bool(prompt)):
+        raise Exception('app and prompt are required')
+
+    if type(app) != str and type(prompt) != str:
+        raise Exception('app and prompt should be strings')
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--app', '-a', help='the app_name_slug', required=True)
+    parser.add_argument('prompt', help='the prompt to send to gpt-4, in between quotes')
+    parser.add_argument('--verbose', dest='verbose', help='set the logging to debug', required=False, default=False, action='store_true')
+    args = parser.parse_args()
+    result = main(args.app, args.prompt, args.verbose)
+    print(result)
