fa888a2b8ccff60a8e5019dab22c687c9b850ab0
michelle0927 <michellelbergero@hotmail.com>
2023-08-02 13:23:19 -0400

    list-shifts action

--- components/microsoft_teams/actions/list-shifts/list-shifts.mjs
+++ components/microsoft_teams/actions/list-shifts/list-shifts.mjs
@@ -3,7 +3,7 @@ import microsoftTeams from "../../microsoft_teams.app.mjs";
 export default {
   key: "microsoft_teams-list-shifts",
   name: "List Shifts",
-  description: "Get the list of shift instances in a schedule. [See the documentation](https://learn.microsoft.com/en-us/graph/api/schedule-list-shifts?view=graph-rest-1.0&tabs=http)",
+  description: "Get the list of shift instances for a team. [See the documentation](https://learn.microsoft.com/en-us/graph/api/schedule-list-shifts?view=graph-rest-1.0&tabs=http)",
   type: "action",
   version: "0.0.1",
   props: {
@@ -15,15 +15,15 @@ export default {
       ],
     },
   },
-  methods: {
-    listShifts() {
-      return this.microsoftTeams.makeRequest({
-        path: "",
-      });
-    },
-  },
   async run({ $ }) {
+    const paginator = this.microsoftTeams.paginate(this.microsoftTeams.listShifts, {
+      teamId: this.teamId,
+    });
+
     const shifts = [];
+    for await (const shift of paginator) {
+      shifts.push(shift);
+    }
 
     $.export("$summary", `Successfully fetched ${shifts?.length} shift${shifts?.length === 1
       ? ""
