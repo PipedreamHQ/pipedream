e4267c9bee427ad5853312878c343cdada7f1731
Alyson Freitas <alyson.f@turing.com>
2023-08-02 10:33:57 -0300

    zoho calendar - smart add component

--- components/zoho_calendar/actions/list-events/list-events.mjs
+++ components/zoho_calendar/actions/list-events/list-events.mjs
@@ -5,7 +5,7 @@ export default {
   key: "zoho_calendar-list-events",
   name: "List Events",
   description: "Returns a list of all the events in a particular calendar of the user. [See the documentation](https://www.zoho.com/calendar/help/api/get-events-list.html)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     app,
