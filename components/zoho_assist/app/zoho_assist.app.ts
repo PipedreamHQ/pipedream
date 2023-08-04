7351c80fb664a4ec3cd3ae701eb6e0c2a26f9e7a
GTFalcao <gtfalcao96@gmail.com>
2023-08-03 22:36:56 -0300

    Basic request info

--- components/zoho_assist/app/zoho_assist.app.ts
+++ components/zoho_assist/app/zoho_assist.app.ts
@@ -1,13 +1,23 @@
 import { defineApp } from "@pipedream/types";
+import { axios } from "@pipedream/platform";
+import { HttpRequestParams } from "../common/types";
 
 export default defineApp({
   type: "app",
   app: "zoho_assist",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    async _httpRequest({
+      $ = this,
+      ...args
+    }: HttpRequestParams): Promise<object> {
+      return axios($, {
+        baseURL: `https://assist.${this.$auth.base_api_url}/api/v2`,
+        headers: {
+          "Authorization": `Zoho-oauthtoken ${this.$auth.oauth_access_token}`,
+        },
+        ...args,
+      });
     },
   },
 });
