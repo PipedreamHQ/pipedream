443b4d5fe9b018d9a6b4414553e46b08422019c5
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2023-08-07 14:10:52 -0300

    #6979 Google My Business components (#7475)
    * Google My Business basic app structure
    
    * pnpm lock
    
    * pnpm lock
    
    * List Posts action sketch
    
    * Pagination for listing posts
    
    * list Posts (temporary)
    
    * pnpm
    
    * Partial advancement for location listing
    
    * Adding on query for filtering  locations
    
    * pnpm lock
    
    * Listing accounts + locations improvements
    
    * 'Create Post' action and adjustments
    
    * Props and logic for Create Post
    
    * Create/Update Reply action + improvements
    
    * Initial source structure
    
    * Finalizing source structure
    
    * Abstracting source structure
    
    * Creating 'new post created' source
    
    * Adding auth headers
    
    * Adding Google My Business to tsconfig
    
    * Adjustments to async options
    
    * pnpm and tsconfig
    
    * adding check for empty array
    
    * Typescript adjustments
    
    * update package
    
    * Including JSON parsing for objects

---
+++ components/google_my_business/sources/new-post-created/new-post-created.ts
@@ -0,0 +1,36 @@
+import { defineSource } from "@pipedream/types";
+import { ListPostsParams } from "../../common/requestParams";
+import { LocalPost } from "../../common/responseSchemas";
+import common from "../common";
+
+const DOCS_LINK = "https://developers.google.com/my-business/reference/rest/v4/accounts.locations.localPosts/list";
+
+export default defineSource({
+  ...common,
+  key: "google_my_business-new-post-created",
+  name: "New Post Created",
+  description: `Emit new event for each new local post on a location [See the documentation](${DOCS_LINK})`,
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    async getItems(): Promise<LocalPost[]> {
+      const {
+        account, location,
+      } = this;
+
+      const params: ListPostsParams = {
+        account,
+        location,
+      };
+
+      return this.app.listPosts(params, false);
+    },
+    getSummary({ summary }: LocalPost) {
+      return `New Post: "${summary.length > 50
+        ? summary.slice(0, 45) + "[...]"
+        : summary}"`;
+    },
+  },
+});
