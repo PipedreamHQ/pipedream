408a3e8a710081bf6df6072a278a8f3c566d0548
Luan Cazarine <luanhc@gmail.com>
2023-08-07 19:37:06 -0300

    [Components] Zoho BugTracker #7409 Triggers:   - New Bug Reported
    Actions:
      - Create Bug
      - Update Bug
      - Get bug details

---
+++ components/zoho_bugtracker/sources/new-bug-reported/test-event.mjs
@@ -0,0 +1,65 @@
+export default {
+  "updated_time_long": 1691446793000,
+  "comment_count": "0",
+  "updated_time": "08-07-2023",
+  "assignee_zpuid": 1231231312312312,
+  "flag": "Internal",
+  "updated_time_format": "08-07-2023 07:19:53 PM",
+  "link": {
+    "timesheet": {
+      "url": "https://bugtracker.zoho.com/restapi/portal/123123/projects/123123123123123/bugs/123123123123123/logs/"
+    },
+    "web": {
+      "url": "https://bugtracker.zoho.com/portal/turingdotcom#buginfo/123123123123123/123123123123123"
+    },
+    "self": {
+      "url": "https://bugtracker.zoho.com/restapi/portal/123123/projects/123123123123123/bugs/123123123123123/"
+    }
+  },
+  "title": "asd asdas d 30",
+  "assignee_name": "Unassigned",
+  "reporter_id": "123123123",
+  "id": 123123123123123123,
+  "has_log_hours": "false",
+  "escalation_level": "0",
+  "key": "PT1-I47",
+  "created_time_long": 1691446793000,
+  "severity": {
+    "id": "2156734000000047151",
+    "type": "None"
+  },
+  "created_time": "08-07-2023",
+  "created_time_format": "08-07-2023 07:19:53 PM",
+  "reproducible": {
+    "id": "123123123123123",
+    "type": "None"
+  },
+  "module": {
+    "name": "None",
+    "id": "123123123123123123"
+  },
+  "classification": {
+    "id": "123123123123123123",
+    "type": "None"
+  },
+  "GROUP_NAME": {
+    "ASSOCIATED_TEAMS": {
+      "AnyTeam": "Not Associated"
+    },
+    "ASSOCIATED_TEAMS_COUNT": 0,
+    "IS_TEAM_UNASSIGNED": true
+  },
+  "bug_number": "47",
+  "reporter_non_zuser": "false",
+  "reported_person": "Jon Doe",
+  "reporter_email": "jondoe@mail.com",
+  "id_string": "123123123123123123",
+  "closed": false,
+  "bug_prefix": "PT1",
+  "attachment_count": "0",
+  "status": {
+    "colorcode": "#08aeea",
+    "id": "2156734000000047083",
+    "type": "Open"
+  }
+}
