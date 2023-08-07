b229210d8e4a721ec1b8c78df7b80171c9adb368
Lucas Caresia <lucascarezia@gmail.com>
2023-08-07 13:19:40 -0300

    [ACTION] Asana — Get the current task list for the user #7274 (#7468)
    * Added action to retreive tasks
    
    * Bump versions
    
    * Update components/asana/actions/get-tasks-from-task-list/get-tasks-from-task-list.mjs
    
    Co-authored-by: Alyson Freitas <alyson.f@turing.com>
    
    * Update components/asana/package.json
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alyson Freitas <alyson.f@turing.com>
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

--- components/asana/sources/new-completed-task/new-completed-task.mjs
+++ components/asana/sources/new-completed-task/new-completed-task.mjs
@@ -7,7 +7,7 @@ export default {
   type: "source",
   name: "New Completed Task (Instant)",
   description: "Emit new event for each task completed in a project.",
-  version: "0.1.2",
+  version: "0.1.3",
   dedupe: "unique",
   props: {
     ...common.props,
