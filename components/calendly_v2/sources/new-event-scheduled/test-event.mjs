a48e7cc476cd89e8fe942d436f1703750d213b25
Pipedream Component Development <106282845+pipedream-component-development@users.noreply.github.com>
2023-08-10 01:32:44 -0700

    Adding Sample Test Event for calendly_v2-new-event-scheduled (#7532)

--- components/calendly_v2/sources/new-event-scheduled/test-event.mjs
+++ components/calendly_v2/sources/new-event-scheduled/test-event.mjs
@@ -1,18 +1,18 @@
 export default  {
   "calendar_event": {
-   "external_id": "2jkv6uldterkuf7v2p0vlr1mmx",
-   "kind": "outlook"
+   "external_id": "7kjv8lxgwirpz6k9c7bwtl4jji",
+   "kind": "yahoo"
   },
-  "created_at": "2022-04-20T10:04:01.015483Z",
-  "end_time": "2022-04-25T01:30:00.000000Z",
+  "created_at": "2022-04-18T10:35:19.045918Z",
+  "end_time": "2022-04-20T04:15:00.000000Z",
   "event_guests": [],
   "event_memberships": [
    {
-    "user": "https://api.calendly.com/users/bf4623d7-875e-26f6-78ac-5bda8c12a123",
-    "user_email": "john.s@data.com"
+    "user": "https://api.calendly.com/users/dj1739b6-1745-56e1-88ed-3dca9c94b456",
+    "user_email": "luke.m@outlook.com"
    }
   ],
-  "event_type": "https://api.calendly.com/event_types/50bcf512-5d97-5e6a-5b20-652c2abc8309",
+  "event_type": "https://api.calendly.com/event_types/69cdj835-6a78-9d6a-8b91-653d3cwt6718",
   "invitees_counter": {
    "active": 2,
    "limit": 2,
@@ -22,9 +22,9 @@ export default  {
    "location": null,
    "type": "custom"
   },
-  "name": "Dreamstream2605",
-  "start_time": "2022-04-25T01:00:00.000000Z",
-  "status": "active",
-  "updated_at": "2022-04-20T10:04:02.959246Z",
-  "uri": "https://api.calendly.com/scheduled_events/2165e984-74d2-45f2-aef9-d49935fbf23b"
+  "name": "DreamPipe2604",
+  "start_time": "2022-04-20T04:00:02.000000Z",
+  "status": "ongoing",
+  "updated_at": "2022-04-18T11:35:20.849258Z",
+  "uri": "https://api.calendly.com/scheduled_events/3146f192-84d3-51e3-bce9-f59922bd215b"
  }
