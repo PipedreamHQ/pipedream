6a7f520fb290e78cfcb05bd50a07aa4db1c3cba1
Luan Cazarine <luanhc@gmail.com>
2023-08-14 17:56:21 -0300

    [Components] GoToWebinar #6786 Triggers:   - Registrant joined   - Registrant added
    Actions:
      - Create Webinar
      - Create registrant

---
+++ components/gotowebinar/sources/registrant-added/registrant-added.mjs
@@ -0,0 +1,26 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "gotowebinar-registrant-added",
+  name: "New Registrant Added (Instant)",
+  description: "Emit new event when a registrant is added.",
+  version: "0.0.1",
+  dedupe: "unique",
+  type: "source",
+  methods: {
+    ...common.methods,
+    getEventName() {
+      return "registrant.added";
+    },
+    generateMeta({
+      eventKey, registrantKey, registrationDate,
+    }) {
+      return {
+        id: eventKey,
+        summary: `A new registrant with key ${registrantKey} has been successfully added!`,
+        ts: registrationDate,
+      };
+    },
+  },
+};
---
+++ components/gotowebinar/sources/registrant-joined/registrant-joined.mjs
@@ -0,0 +1,26 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "gotowebinar-registrant-joined",
+  name: "New Registrant Joined (Instant)",
+  description: "Emit new event when a registrant joins a weginar.",
+  version: "0.0.1",
+  dedupe: "unique",
+  type: "source",
+  methods: {
+    ...common.methods,
+    getEventName() {
+      return "registrant.joined";
+    },
+    generateMeta({
+      eventKey, registrantKey, registrationDate,
+    }) {
+      return {
+        id: eventKey,
+        summary: `A new registrant with key ${registrantKey} has been successfully joined!`,
+        ts: registrationDate,
+      };
+    },
+  },
+};
