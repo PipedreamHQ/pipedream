0c0a9b0f8b394cdbff35f0b56485463724734d5a
Pipedream Component Development <106282845+pipedream-component-development@users.noreply.github.com>
2023-08-14 01:34:16 -0700

    Adding test event json file

---
+++ components/airtable_oauth/sources/new-or-modified-records/test-event.mjs
@@ -0,0 +1,27 @@
+export default  {
+  "id": "recslymHzCjXDw",
+  "createdTime": "2023-07-31T04:29:26.000Z",
+  "fields": {
+   "Status": "In progress",
+   "ID": 27,
+   "Concat Calculation": "In progress-Medium",
+   "Record ID": "recslymHzCjXDw",
+   "Last Modified By": {
+    "id": "usrBLNP11LGUDJNTJ",
+    "email": "test@pipedream.com",
+    "name": "Pipedream Test"
+   },
+   "Created By": {
+    "id": "usrBLNP11LGUDJNTJ",
+    "email": "test@pipedream.com",
+    "name": "Pipedream Test"
+   },
+   "Name": "Pipedream 1129 Kanban",
+   "Priority": "Medium"
+  },
+  "type": "record_modified",
+  "metadata": {
+   "baseId": "appeSZQBBLVSAD2cz",
+   "tableId": "tblPay0bFq9h0M5s9"
+  }
+ }
