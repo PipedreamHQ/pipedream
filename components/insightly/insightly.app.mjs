827ddf8800e46e2ef7fe77e800bd0aee3e566eba
michelle0927 <michellelbergero@hotmail.com>
2023-08-15 14:49:30 -0400

    new actions

--- components/insightly/insightly.app.mjs
+++ components/insightly/insightly.app.mjs
@@ -1,11 +1,73 @@
+import { axios } from "@pipedream/platform";
+const DEFAULT_LIMIT = 20;
+
 export default {
   type: "app",
   app: "insightly",
-  propDefinitions: {},
+  propDefinitions: {
+    categoryId: {
+      type: "string",
+      label: "Category",
+      description: "Identifier of a task category",
+      optional: true,
+      async options({ page }) {
+        const top = DEFAULT_LIMIT;
+        const params = {
+          top,
+          skip: page * top,
+        };
+        const categories = await this.listTaskCategories({
+          params,
+        });
+        return categories?.map(({
+          CATEGORY_ID: value, CATEGORY_NAME: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://api.${this.$auth.pod}.insightly.com/v3.1`;
+    },
+    _auth() {
+      return {
+        username: `${this.$auth.api_key}`,
+        password: "",
+      };
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        auth: this._auth(),
+        ...args,
+      });
+    },
+    listTaskCategories(args = {}) {
+      return this._makeRequest({
+        path: "/TaskCategories",
+        ...args,
+      });
+    },
+    createContact(args = {}) {
+      return this._makeRequest({
+        path: "/Contacts",
+        method: "POST",
+        ...args,
+      });
+    },
+    createTask(args = {}) {
+      return this._makeRequest({
+        path: "/Tasks",
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
