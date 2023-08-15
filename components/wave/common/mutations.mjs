e46ebcae5a602a1c2046497658d38fb488bcc6c5
Cassiano Vellames <cassiano.v@turing.com>
2023-08-15 15:19:43 -0300

    create invoice

--- components/wave/common/mutations.mjs
+++ components/wave/common/mutations.mjs
@@ -34,3 +34,120 @@ export const CREATE_CUSTOMER_MUTATION = `
     }
   }
 `;
+
+export const CREATE_INVOICE_MUTATION = `
+  mutation ($input: InvoiceCreateInput!) {
+    invoiceCreate(input: $input) {
+      didSucceed
+      inputErrors {
+        message
+        code
+        path
+      }
+      invoice {
+        id
+        createdAt
+        modifiedAt
+        pdfUrl
+        viewUrl
+        status
+        title
+        subhead
+        invoiceNumber
+        invoiceDate
+        poNumber
+        customer {
+          id
+          name
+          # Can add additional customer fields here
+        }
+        currency {
+          code
+        }
+        dueDate
+        amountDue {
+          value
+          currency {
+            symbol
+          }
+        }
+        amountPaid {
+          value
+          currency {
+            symbol
+          }
+        }
+        taxTotal {
+          value
+          currency {
+            symbol
+          }
+        }
+        total {
+          value
+          currency {
+            symbol
+          }
+        }
+        exchangeRate
+        footer
+        memo
+        disableCreditCardPayments
+        disableBankPayments
+        itemTitle
+        unitTitle
+        priceTitle
+        amountTitle
+        hideName
+        hideDescription
+        hideUnit
+        hidePrice
+        hideAmount
+        items {
+          product {
+            id
+            name
+            # Can add additional product fields here
+          }
+          description
+          quantity
+          price
+          subtotal {
+            value
+            currency {
+              symbol
+            }
+          }
+          total {
+            value
+            currency {
+              symbol
+            }
+          }
+          account {
+            id
+            name
+            subtype {
+              name
+              value
+            }
+            # Can add additional account fields here
+          }
+          taxes {
+            amount {
+              value
+            }
+            salesTax {
+              id
+              name
+              # Can add additional sales tax fields here
+            }
+          }
+        }
+        lastSentAt
+        lastSentVia
+        lastViewedAt
+      }
+    }
+  }
+`;
--- components/wave/common/queries.mjs
+++ components/wave/common/queries.mjs
@@ -17,3 +17,59 @@ export const LIST_BUSINESS_QUERY = `
     }
   }
 `;
+
+export const LIST_CUSTOMERS_BY_BUSINESS_QUERY = `
+  query GetBusinessAndCustomers($businessId: ID!, $page: Int!, $pageSize: Int!, $sort: [CustomerSort!]) {
+    business(id: $businessId) {
+      id
+      customers(page: $page, pageSize: $pageSize, sort: $sort) {
+        pageInfo {
+          currentPage
+          totalPages
+          totalCount
+        }
+        edges {
+          node {
+            id
+            name
+            email
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const LIST_PRODUCTS_BY_BUSINESS_QUERY = `
+  query ($businessId: ID!, $page: Int!, $pageSize: Int!) {
+    business(id: $businessId) {
+      id
+      products(page: $page, pageSize: $pageSize) {
+        pageInfo {
+          currentPage
+          totalPages
+          totalCount
+        }
+        edges {
+          node {
+            id
+            name
+            description
+            unitPrice
+            defaultSalesTaxes {
+              id
+              name
+              abbreviation
+              rate
+            }
+            isSold
+            isBought
+            isArchived
+            createdAt
+            modifiedAt
+          }
+        }
+      }
+    }
+  }
+`;
