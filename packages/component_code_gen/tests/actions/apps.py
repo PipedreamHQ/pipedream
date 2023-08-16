088076e02587e73e350c43a1065162d2066094e5
Andrew Chuang <andrewjschuang@gmail.com>
2023-08-16 17:05:42 -0300

    alphabetical reorder tests

--- packages/component_code_gen/tests/actions/apps.py
+++ packages/component_code_gen/tests/actions/apps.py
@@ -1,23 +1,28 @@
 apps = [
     {
+        'app': 'accelo',
+        'instructions': 'how to create a contact',
+        'key': 'accelo-create-contact'
+    },
+    {
+        'app': 'asana',
+        'instructions': 'how to update a task',
+        'key': 'asana-update-task'
+    },
+    {
+        'app': 'brex',
+        'instructions': 'how to set a limit for an user',
+        'key': 'brex-set-limit-for-user'
+    },
+    {
+        'app': 'fibery',
+        'instructions': "how to get an entity or create one if it doesn't exist",
+        'key': 'fibery-get-entity-or-create'
+    },
+    {
         'app': 'github',
         'instructions': 'how to get a specific repository',
         'key': 'github-get-repository'
-    },
-    {
-        'app': 'stripe',
-        'instructions': 'how to cancel a payment intent',
-        'key': 'stripe-cancel-payment-intent'
-    },
-    {
-        'app': 'twilio',
-        'instructions': 'how to get a message',
-        'key': 'twilio-get-message'
-    },
-    {
-        'app': 'woocommerce',
-        'instructions': 'how to search for customers',
-        'key': 'woocommerce-search-customers'
     },
     {
         'app': 'postmark',
@@ -30,29 +35,9 @@ apps = [
         'key': 'process_street-start-workflow-run'
     },
     {
-        'app': 'zenkit',
-        'instructions': 'how to add a comment to an entry/item within a list/collection',
-        'key': 'zenkit-add-entry-comment'
-    },
-    {
-        'app': 'fibery',
-        'instructions': "how to get an entity or create one if it doesn't exist",
-        'key': 'fibery-get-entity-or-create'
-    },
-    {
-        'app': 'tally',
-        'instructions': 'how to get a list of responses',
-        'key': 'tally-get-responses'
-    },
-    {
-        'app': 'asana',
-        'instructions': 'how to update a task',
-        'key': 'asana-update-task'
-    },
-    {
-        'app': 'accelo',
-        'instructions': 'how to create a contact',
-        'key': 'accelo-create-contact'
+        'app': 'quaderno',
+        'instructions': 'how to create an invoice',
+        'key': 'quaderno-create-invoice'
     },
     {
         'app': 'shipcloud',
@@ -60,13 +45,28 @@ apps = [
         'key': 'shipcloud-get-shipment-info'
     },
     {
-        'app': 'quaderno',
-        'instructions': 'how to create an invoice',
-        'key': 'quaderno-create-invoice'
+        'app': 'stripe',
+        'instructions': 'how to cancel a payment intent',
+        'key': 'stripe-cancel-payment-intent'
     },
     {
-        'app': 'brex',
-        'instructions': 'how to set a limit for an user',
-        'key': 'brex-set-limit-for-user'
+        'app': 'tally',
+        'instructions': 'how to get a list of responses',
+        'key': 'tally-get-responses'
+    },
+    {
+        'app': 'twilio',
+        'instructions': 'how to get a message',
+        'key': 'twilio-get-message'
+    },
+    {
+        'app': 'woocommerce',
+        'instructions': 'how to search for customers',
+        'key': 'woocommerce-search-customers'
+    },
+    {
+        'app': 'zenkit',
+        'instructions': 'how to add a comment to an entry/item within a list/collection',
+        'key': 'zenkit-add-entry-comment'
     },
 ]
--- packages/component_code_gen/tests/webhook_sources/apps.py
+++ packages/component_code_gen/tests/webhook_sources/apps.py
@@ -1,23 +1,28 @@
 apps = [
     {
+        'app': 'accelo',
+        'instructions': 'how to get webhooks for every new assigned task',
+        'key': 'accelo-new-task-assigned'
+    },
+    {
+        'app': 'asana',
+        'instructions': 'how to get webhooks for every new project',
+        'key': 'asana-new-project'
+    },
+    {
+        'app': 'brex',
+        'instructions': 'how to get webhooks for every new transfer event',
+        'key': 'brex-new-transfer-event'
+    },
+    {
+        'app': 'fibery',
+        'instructions': 'how to get webhooks for every new created entity',
+        'key': 'fibery-entity-created'
+    },
+    {
         'app': 'github',
         'instructions': 'how to get webhooks for every new commit',
         'key': 'github-new-commit'
-    },
-    {
-        'app': 'stripe',
-        'instructions': 'how to get webhooks for every new payment',
-        'key': 'stripe-new-payment'
-    },
-    {
-        'app': 'twilio',
-        'instructions': 'how to get webhooks for every new call',
-        'key': 'twilio-new-call'
-    },
-    {
-        'app': 'woocommerce',
-        'instructions': 'how to get webhooks for every new order event',
-        'key': 'woocommerce-new-order-event'
     },
     {
         'app': 'postmark',
@@ -30,29 +35,9 @@ apps = [
         'key': 'process_street-workflow-run-completed'
     },
     {
-        'app': 'zenkit',
-        'instructions': 'how to get webhooks for every new notification',
-        'key': 'zenkit-new-notification'
-    },
-    {
-        'app': 'fibery',
-        'instructions': 'how to get webhooks for every new created entity',
-        'key': 'fibery-entity-created'
-    },
-    {
-        'app': 'tally',
-        'instructions': 'how to get webhooks for every new response',
-        'key': 'tally-new-response'
-    },
-    {
-        'app': 'asana',
-        'instructions': 'how to get webhooks for every new project',
-        'key': 'asana-new-project'
-    },
-    {
-        'app': 'accelo',
-        'instructions': 'how to get webhooks for every new assigned task',
-        'key': 'accelo-new-task-assigned'
+        'app': 'quaderno',
+        'instructions': 'how to get webhooks for every new received payment',
+        'key': 'quaderno-payment-received'
     },
     {
         'app': 'shipcloud',
@@ -60,13 +45,28 @@ apps = [
         'key': 'shipcloud-new-shipment-status'
     },
     {
-        'app': 'quaderno',
-        'instructions': 'how to get webhooks for every new received payment',
-        'key': 'quaderno-payment-received'
+        'app': 'stripe',
+        'instructions': 'how to get webhooks for every new payment',
+        'key': 'stripe-new-payment'
     },
     {
-        'app': 'brex',
-        'instructions': 'how to get webhooks for every new transfer event',
-        'key': 'brex-new-transfer-event'
+        'app': 'tally',
+        'instructions': 'how to get webhooks for every new response',
+        'key': 'tally-new-response'
+    },
+    {
+        'app': 'twilio',
+        'instructions': 'how to get webhooks for every new call',
+        'key': 'twilio-new-call'
+    },
+    {
+        'app': 'woocommerce',
+        'instructions': 'how to get webhooks for every new order event',
+        'key': 'woocommerce-new-order-event'
+    },
+    {
+        'app': 'zenkit',
+        'instructions': 'how to get webhooks for every new notification',
+        'key': 'zenkit-new-notification'
     },
 ]
