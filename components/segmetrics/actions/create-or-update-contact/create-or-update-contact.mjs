d262d3b5cc99267e019a5023cd40e47c60c1632e
Lucas Caresia <lucascarezia@gmail.com>
2023-08-16 10:17:54 -0300

    Added actions

---
+++ components/segmetrics/actions/create-or-update-contact/create-or-update-contact.mjs
@@ -0,0 +1,49 @@
+import app from "../../segmetrics.app.mjs";
+
+export default {
+  name: "Create Or Update Contact",
+  version: "0.0.4",
+  key: "app-create-or-update-contact",
+  description: "Create or update a contact. [See documentation here](https://developers.segmetrics.io/#contacts)",
+  type: "action",
+  props: {
+    app,
+    integrationId: {
+      label: "Integration ID",
+      type: "string",
+      description: "The ID of the integration from your Account Settings Page",
+    },
+    email: {
+      type: "string",
+      label: "Contact's Email",
+      description: "Email address of the contact",
+    },
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "First name of the contact",
+    },
+    lastName: {
+      type: "string",
+      label: "Last Name",
+      description: "Last name of the contact",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createOrUpdateContact({
+      $,
+      integrationId: this.integrationId,
+      data: {
+        email: this.email,
+        first_name: this.firstName,
+        last_name: this.lastName,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully created or updated contact with ID ${response.contact.contact_id}`);
+    }
+
+    return response;
+  },
+};
