752b915d41842d17178ec5b1267603db18eaca02
Jorge Cortes <jacortesmahmud@gmail.com>
2023-08-16 15:46:12 -0500

    Components tested

--- components/apollo_io/apollo_io.app.mjs
+++ components/apollo_io/apollo_io.app.mjs
@@ -1,11 +1,98 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "apollo_io",
-  propDefinitions: {},
+  propDefinitions: {
+    commonProperty: {
+      type: "string",
+      label: "Common property",
+      description: "[See the docs here](https://example.com)",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getBaseUrl() {
+      return `${constants.BASE_URL}${constants.VERSION_PATH}`;
+    },
+    getUrl(path) {
+      return `${this.getBaseUrl()}${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+        ...headers,
+      };
+    },
+    getParams(params) {
+      return {
+        api_key: this.$auth.api_key,
+        ...params,
+      };
+    },
+    makeRequest({
+      step = this, path, headers, params, ...args
+    } = {}) {
+
+      const config = {
+        headers: this.getHeaders(headers),
+        url: this.getUrl(path),
+        params: this.getParams(params),
+        ...args,
+      };
+
+      return axios(step, config);
+    },
+    post(args = {}) {
+      return this.makeRequest({
+        method: "post",
+        ...args,
+      });
+    },
+    searchContacts(args = {}) {
+      return this.makeRequest({
+        path: "/contacts/search",
+        ...args,
+      });
+    },
+    async *getResourcesStream({
+      resourceFn,
+      resourceFnArgs,
+      resourceName,
+      max = constants.DEFAULT_MAX,
+    }) {
+      let page = 1;
+      let resourcesCount = 0;
+
+      while (true) {
+        const response =
+          await resourceFn({
+            ...resourceFnArgs,
+            params: {
+              ...resourceFnArgs.params,
+              page,
+            },
+          });
+
+        const nextResources = resourceName && response[resourceName] || response;
+
+        if (!nextResources?.length) {
+          console.log("No more resources found");
+          return;
+        }
+
+        for (const resource of nextResources) {
+          yield resource;
+          resourcesCount += 1;
+
+          if (resourcesCount >= max) {
+            return;
+          }
+        }
+
+        page += 1;
+      }
     },
   },
 };
