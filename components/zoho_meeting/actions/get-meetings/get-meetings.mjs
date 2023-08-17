c92b2b0e9e5eca86db195809be59a6fcbb79c813
Lucas Caresia <lucascarezia@gmail.com>
2023-08-17 10:16:19 -0300

    [Components] Zoho Meeting #7423 (#7453)
    * Added actions
    
    * Fixed versions and descriptions
    
    * updates per previous CR
    
    * pnpm-lock.yaml
    
    * Added new prop to list meetings
    
    * Pagination
    
    * increment index
    
    * pnpm-lock.yaml
    
    * Change to work Vercel script
    
    * Bump versions
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/zoho_meeting/actions/get-meetings/get-meetings.mjs
@@ -0,0 +1,60 @@
+import app from "../../zoho_meeting.app.mjs";
+import constants from "../common/constants.mjs";
+
+export default {
+  name: "Get Meetings",
+  version: "0.0.1",
+  key: "zoho_meeting-get-meetings",
+  description: "Get a list of meetings. [See the documentation](https://www.zoho.com/meeting/api-integration/meeting-api/list-of-meeting-api.html)",
+  type: "action",
+  props: {
+    app,
+    count: {
+      label: "Count",
+      description: "Quantity of meetings to list",
+      type: "integer",
+      optional: true,
+      default: 100,
+    },
+    listType: {
+      label: "List Type",
+      description: "List type of the meeting. Default `all`",
+      type: "string",
+      optional: true,
+      default: constants.LIST_TYPES[0].value,
+      options: constants.LIST_TYPES,
+    },
+  },
+  async run({ $ }) {
+    let meetings = [];
+    const params = {
+      listtype: this.listType,
+      count: 100,
+      index: 1,
+    };
+
+    while (meetings.length < this.count) {
+      const {
+        count, session: sessions,
+      } = await this.app.getMeetings({
+        $,
+        params,
+      });
+
+      meetings = meetings.concat(sessions);
+
+      if (count < 100) break;
+      params.index += params.count;
+    }
+
+    if (meetings?.length > this.count) {
+      meetings.length = this.count;
+    }
+
+    if (meetings.length) {
+      $.export("$summary", `Successfully retrieved ${meetings.length} meetings`);
+    }
+
+    return meetings;
+  },
+};
