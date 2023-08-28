7291e6c225b372d429b0d57b56834466be4e8c19
Lucas Caresia <lucascarezia@gmail.com>
2023-08-28 11:17:08 -0300

    [Components] Neon (API Keys) #7616 (#7716)
    * Added actions
    
    * Update components/neon_api_keys/actions/create-branch/create-branch.mjs
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>
    
    * Update components/neon_api_keys/actions/create-database/create-database.mjs
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>
    
    * Update components/neon_api_keys/actions/create-database/create-database.mjs
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>
    
    * Update components/neon_api_keys/actions/create-project/create-project.mjs
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>
    
    * Update components/neon_api_keys/package.json
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>
    
    * Update create-database.mjs
    
    ---------
    
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>

---
+++ components/neon_api_keys/actions/common/constants.mjs
@@ -0,0 +1,24 @@
+export default {
+  REGIONS_ID: [
+    {
+      label: "US East (N. Virginia)",
+      value: "aws-us-east-1",
+    },
+    {
+      label: "US East (Ohio)",
+      value: "aws-us-east-2",
+    },
+    {
+      label: "US West (Oregon)",
+      value: "aws-us-west-2",
+    },
+    {
+      label: "Europe (Frankfurt)",
+      value: "aws-eu-central-1",
+    },
+    {
+      label: "Asia Pacific (Singapore)",
+      value: "aws-ap-southeast-1",
+    },
+  ],
+};
