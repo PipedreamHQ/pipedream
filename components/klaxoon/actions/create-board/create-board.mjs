b975dcde8c37162ed0377c0ea4840abad0d33512
Lucas Caresia <lucascarezia@gmail.com>
2023-08-29 10:22:36 -0300

    Added actions

---
+++ components/klaxoon/actions/create-board/create-board.mjs
@@ -0,0 +1,37 @@
+import app from "../../klaxoon.app.mjs";
+
+export default {
+  name: "Create Board",
+  version: "0.0.1",
+  key: "klaxoon-create-client",
+  description: "Creates a client. [See the documentation](https://developers.klaxoon.com/reference/v1boardpost)",
+  type: "action",
+  props: {
+    app,
+    title: {
+      type: "string",
+      label: "Title",
+      description: "The title of the board",
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "The description of the board",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createBoard({
+      $,
+      data: {
+        title: this.title,
+        description: this.description,
+      },
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully created board with ID \`${response.id}\``);
+    }
+
+    return response;
+  },
+};
