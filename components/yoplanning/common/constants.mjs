7f046fc3ae63241d752ae6641131387e565468da
Jorge Cortes <jacortesmahmud@gmail.com>
2023-08-29 16:53:41 -0500

    Action and source components

---
+++ components/yoplanning/common/constants.mjs
@@ -0,0 +1,13 @@
+const BASE_URL = "https://yoplanning.pro";
+const VERSION_PATH = "/api/v3.1";
+const DEFAULT_LIMIT = 100;
+const DEFAULT_MAX = 600;
+const LAST_CREATION_DATE = "lastCreationDate";
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  DEFAULT_MAX,
+  DEFAULT_LIMIT,
+  LAST_CREATION_DATE,
+};
