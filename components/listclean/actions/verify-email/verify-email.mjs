6cf044d509ef2d8028a6251202a9b4b80363b6b5
Lucas Caresia <lucascarezia@gmail.com>
2023-08-29 09:14:08 -0300

    Update components/listclean/actions/verify-email/verify-email.mjs
    Co-authored-by: Alyson Freitas <alyson.f@turing.com>

--- components/listclean/actions/verify-email/verify-email.mjs
+++ components/listclean/actions/verify-email/verify-email.mjs
@@ -4,7 +4,7 @@ export default {
   name: "Verify Email",
   version: "0.0.1",
   key: "listclean-verify-email",
-  description: "Verify an email address. [See documentation here](https://api.listclean.xyz/#tag/verifications)",
+  description: "Verify an email address. [See the documentation](https://api.listclean.xyz/#tag/verifications)",
   type: "action",
   props: {
     app,
