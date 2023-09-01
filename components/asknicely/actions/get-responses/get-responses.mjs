4782f987836ba68a37abf653b3941da88fbe27fe
Luan Cazarine <luanhc@gmail.com>
2023-09-01 13:14:14 -0300

    [Components] AskNicely #7812 Triggers:   - Survey Response Received
    Actions:
      - Get contact
      - Get responses
      - Add contact
      - Send survey

---
+++ components/asknicely/actions/get-responses/get-responses.mjs
@@ -0,0 +1,45 @@
+import asknicely from "../../asknicely.app.mjs";
+
+export default {
+  key: "asknicely-get-responses",
+  name: "Get Responses",
+  version: "0.0.1",
+  description: "Get the details of a particular contact. [See the documentation](https://demo.asknice.ly/help/apidocs/getcontact)",
+  type: "action",
+  props: {
+    asknicely,
+    filter: {
+      type: "string",
+      label: "Filter",
+      description: "Leave blank or **answered** for all survey responses **raw** for all surveys sent, this includes surveys sent and have not been responded to. **published** for only customer testimonials.",
+      options: [
+        "answered",
+        "published",
+        "raw",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      asknicely,
+      ...args
+    } = this;
+
+    const response = asknicely.paginate({
+      fn: asknicely.getResponses,
+      ...args,
+    });
+
+    const responseArray = [];
+    for await (const item of response) {
+      responseArray.push(item);
+    }
+
+    $.export("$summary", `${responseArray.length} response${responseArray.length > 1
+      ? "s were"
+      : " was"} successfully fetched!`);
+
+    return responseArray;
+  },
+};
