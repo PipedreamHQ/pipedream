5cd846b59358432a1f66ef3feddc6975596d8b62
Jorge Cortes <jacortesmahmud@gmail.com>
2023-09-04 17:19:08 -0500

    Added new actinos

---
+++ components/textcortex/actions/create-social-media-post/create-social-media-post.mjs
@@ -0,0 +1,106 @@
+import app from "../../textcortex.app.mjs";
+
+export default {
+  key: "textcortex-create-social-media-post",
+  name: "Create Social Media Post",
+  description: "Create a social media post. [See the documentation](https://docs.textcortex.com/api/paths/texts-social-media-posts/post)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    context: {
+      type: "string",
+      label: "Context",
+      description: "The context of the social media post.",
+    },
+    keywords: {
+      type: "string[]",
+      label: "Keywords",
+      description: "Keywords to be included in the post.",
+      optional: true,
+    },
+    maxTokens: {
+      propDefinition: [
+        app,
+        "maxTokens",
+      ],
+    },
+    mode: {
+      propDefinition: [
+        app,
+        "mode",
+      ],
+    },
+    model: {
+      propDefinition: [
+        app,
+        "model",
+      ],
+    },
+    n: {
+      propDefinition: [
+        app,
+        "n",
+      ],
+    },
+    sourceLang: {
+      propDefinition: [
+        app,
+        "sourceLang",
+      ],
+    },
+    targetLang: {
+      propDefinition: [
+        app,
+        "targetLang",
+      ],
+    },
+    temperature: {
+      propDefinition: [
+        app,
+        "temperature",
+      ],
+    },
+  },
+  methods: {
+    createSocialMediaPost(args = {}) {
+      return this.app.post({
+        path: "/texts/social-media-posts",
+        ...args,
+      });
+    },
+  },
+  async run({ $: step }) {
+    const {
+      createSocialMediaPost,
+      context,
+      keywords,
+      maxTokens,
+      mode,
+      model,
+      n,
+      sourceLang,
+      targetLang,
+      temperature,
+    } = this;
+
+    const response = await createSocialMediaPost({
+      step,
+      data: {
+        context,
+        keywords,
+        max_tokens: maxTokens,
+        mode,
+        model,
+        n,
+        source_lang: sourceLang,
+        target_lang: targetLang,
+        temperature,
+      },
+    });
+
+    step.export("$summary", `Successfully created social media post with status \`${response.status}\``);
+
+    return response;
+  },
+};
---
+++ components/textcortex/common/constants.mjs
@@ -0,0 +1,22 @@
+const BASE_URL = "https://api.textcortex.com";
+const VERSION_PATH = "/v1";
+
+const MODE = {
+  TWITTER: "twitter",
+  LINKEDIN: "linkedin",
+};
+
+const MODEL = {
+  VELOX1: "velox-1",
+  AECUS1: "aecus-1",
+  ALTA1: "alta-1",
+  SOPHOS1: "sophos-1",
+  CHATSOPHOS1: "chat-sophos-1",
+};
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  MODE,
+  MODEL,
+};
