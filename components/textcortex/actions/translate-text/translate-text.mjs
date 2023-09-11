b385aa107c6dcccc911cd0b532618fd958587d05
Jorge Cortes <jacortesmahmud@gmail.com>
2023-09-11 16:23:02 -0500

    [ACTIONS] Textcortex new actions (#7876)
    * Added new actinos
    
    * pnpm-lock.yaml
    
    * Fixed mode in summarize-text action
    
    * pnpm-lock.yaml
    
    * Reset versions

---
+++ components/textcortex/actions/translate-text/translate-text.mjs
@@ -0,0 +1,62 @@
+import app from "../../textcortex.app.mjs";
+
+export default {
+  key: "textcortex-translate-text",
+  name: "Translate Text",
+  description: "Translate given text into another language. [See the documentation](https://docs.textcortex.com/api/paths/texts-translations/post)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    text: {
+      propDefinition: [
+        app,
+        "text",
+      ],
+    },
+    sourceLang: {
+      description: "The language of the text to translate.",
+      propDefinition: [
+        app,
+        "sourceLang",
+      ],
+    },
+    targetLang: {
+      description: "The language to translate to.",
+      optional: false,
+      propDefinition: [
+        app,
+        "targetLang",
+      ],
+    },
+  },
+  methods: {
+    translateText(args = {}) {
+      return this.app.post({
+        path: "/texts/translations",
+        ...args,
+      });
+    },
+  },
+  async run({ $: step }) {
+    const {
+      translateText,
+      text,
+      sourceLang,
+      targetLang,
+    } = this;
+
+    const response = await translateText({
+      step,
+      data: {
+        text,
+        source_lang: sourceLang,
+        target_lang: targetLang,
+      },
+    });
+
+    step.export("$summary", `Successfully translated text from ${sourceLang} to ${targetLang}.`);
+
+    return response;
+  },
+};
