8cab8b492664e1da5eccd24fbc15fba441b3d670
Jorge Cortes <jacortesmahmud@gmail.com>
2023-09-11 18:02:45 -0500

    Added tested actions

---
+++ components/project_broadcast/actions/add-keyword-to-contact/add-keyword-to-contact.mjs
@@ -0,0 +1,50 @@
+import app from "../../project_broadcast.app.mjs";
+
+export default {
+  key: "project_broadcast-add-keyword-to-contact",
+  name: "Add Keyword To Contact",
+  description: "Add a keyword to a contact. [See the documentation](https://www.projectbroadcast.com/apidoc/#api-Contacts-Apply_Keyword)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    contacts: {
+      propDefinition: [
+        app,
+        "contactId",
+      ],
+    },
+    keywordId: {
+      propDefinition: [
+        app,
+        "keywordId",
+      ],
+    },
+  },
+  methods: {
+    addKeywordToContact({
+      contactId, keywordId, ...args
+    } = {}) {
+      return this.app.put({
+        path: `/contacts/${contactId}/keywords/${keywordId}/apply`,
+        ...args,
+      });
+    },
+  },
+  run({ $: step }) {
+    const {
+      // eslint-disable-next-line no-unused-vars
+      app,
+      addKeywordToContact,
+      contactId,
+      keywordId,
+    } = this;
+
+    return addKeywordToContact({
+      step,
+      contactId,
+      keywordId,
+      summary: (response) => `Successfully added keyword with ID \`${response._id}\`.`,
+    });
+  },
+};
