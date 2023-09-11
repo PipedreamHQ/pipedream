5d0f9e6dbb9d2538775f13ab6698cdaefba796a5
GTFalcao <gtfalcao96@gmail.com>
2023-09-11 01:10:56 -0300

    App file with AI + manual adjustments

--- components/nudgify/nudgify.app.mjs
+++ components/nudgify/nudgify.app.mjs
@@ -1,6 +1,34 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "nudgify",
-  propDefinitions: {},
-  methods: {},
+  methods: {
+    async _makeRequest({
+      $, path, headers, ...otherOpts
+    }) {
+      return axios($, {
+        ...otherOpts,
+        url: `https://app.nudgify.com/api${path}`,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.api_key}`,
+        },
+      });
+    },
+    async createPurchaseNudge(args) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/purchase-nudges",
+        ...args,
+      });
+    },
+    async createSignUpNudge(args) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/sign-ups",
+        ...args,
+      });
+    },
+  },
 };
