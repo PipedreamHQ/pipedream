be6d49e2bb905dd48f465174d514674c4215f021
GTFalcao <gtfalcao96@gmail.com>
2023-09-12 19:41:28 -0300

    Create Purchase Nudge improvements

--- components/nudgify/actions/create-purchase-nudge/create-purchase-nudge.mjs
+++ components/nudgify/actions/create-purchase-nudge/create-purchase-nudge.mjs
@@ -1,4 +1,4 @@
-import { axios } from "@pipedream/platform";
+import { ConfigurationError } from "@pipedream/platform";
 import app from "../../nudgify.app.mjs";
 
 export default {
@@ -9,43 +9,97 @@ export default {
   type: "action",
   props: {
     app,
-    siteKey: {
-      type: "string",
-      label: "Site Key",
-      description: "Your site key",
+    orderId: {
+      type: "integer",
+      label: "Order ID",
+      description: "The order-id used to identify which items are part of the same order",
     },
-    conversion: {
-      type: "object",
-      label: "Conversion",
-      description: "The conversion details to create the nudge",
+    items: {
+      type: "string[]",
+      label: "Items",
+      description: "One or more items as part of the purchase. Each item should be a JSON string and can contain the parameters `item_id`, `item_variation_id`, `item_name`, `item_link` and `image_url`. [See the documentation](https://www.nudgify.com/docs/knowledge-base/api-purchase-nudges/#parameters) for more information.",
+      optional: true,
     },
-  },
-  methods: {
-    async createPurchaseNudge({
-      $, siteKey, conversion,
-    }) {
-      return axios($, {
-        method: "POST",
-        url: "https://app.nudgify.com/api/conversions",
-        headers: {
-          "Authorization": `Bearer ${this.nudgify.$auth.api_key}`,
-          "content-type": "application/json",
-          "accept": "application/json",
-        },
-        data: {
-          site_key: siteKey,
-          conversions: [
-            conversion,
-          ],
-        },
-      });
+    date: {
+      propDefinition: [
+        app,
+        "date",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    firstName: {
+      propDefinition: [
+        app,
+        "firstName",
+      ],
+    },
+    lastName: {
+      propDefinition: [
+        app,
+        "lastName",
+      ],
+    },
+    ip: {
+      propDefinition: [
+        app,
+        "ip",
+      ],
+    },
+    city: {
+      propDefinition: [
+        app,
+        "city",
+      ],
+    },
+    state: {
+      propDefinition: [
+        app,
+        "state",
+      ],
+    },
+    country: {
+      propDefinition: [
+        app,
+        "country",
+      ],
     },
   },
   async run({ $ }) {
-    const response = await this.createPurchaseNudge({
+    const {
+      date, orderId, items, email, firstName, lastName, ip, city, state, country,
+    } = this;
+    const data = {
+      orders: [
+        {
+          order_id: orderId,
+          date,
+          email,
+          first_name: firstName,
+          last_name: lastName,
+          ip,
+          city,
+          state,
+          country,
+        },
+      ],
+    };
+
+    try {
+      if (items) {
+        data.orders[0].line_items = items.map((s) => JSON.parse(s));
+      }
+    } catch (err) {
+      throw new ConfigurationError("Error parsing items as JSON. Check that each entry in the array is a valid JSON string.");
+    }
+
+    const response = await this.app.createPurchaseNudge({
       $,
-      siteKey: this.siteKey,
-      conversion: this.conversion,
+      data,
     });
     $.export("$summary", "Successfully created purchase nudge");
     return response;
