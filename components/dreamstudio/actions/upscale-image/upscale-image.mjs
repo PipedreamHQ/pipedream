eee517169a76ecba36133da4cff48f260cec8adb
Luan Cazarine <luanhc@gmail.com>
2023-09-13 10:50:11 -0300

    7780 components dreamstudio stable diffusion (#7991)
    * some adjusts
    
    * pnpm update
    
    * Fix text prompts decs
    
    * Add image tmpPaths to generate image action
    
    * Add image tmpPaths
    
    * some adjusts
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

--- components/dreamstudio/actions/upscale-image/upscale-image.mjs
+++ components/dreamstudio/actions/upscale-image/upscale-image.mjs
@@ -1,12 +1,15 @@
 import FormData from "form-data";
 import fs from "node:fs";
+import {
+  getImagePath, writeImg,
+} from "../../common/utils.mjs";
 import common from "../common/images.mjs";
 
 export default {
   ...common,
   key: "dreamstudio-upscale-image",
   name: "Upscale Image",
-  version: "0.0.1",
+  version: "0.0.2",
   description: "Create a higher resolution version of an input image. [See the documentation](https://platform.stability.ai/docs/api-reference#tag/v1generation/operation/upscaleImage)",
   type: "action",
   props: {
@@ -14,7 +17,7 @@ export default {
     image: {
       type: "string",
       label: "Image",
-      description: "Image used to upscale. It must be the path to the image file saved to the `/tmp` directory (e.g. `/tmp/image.png`). [see docs here](https://pipedream.com/docs/workflows/steps/code/nodejs/working-with-files/#the-tmp-directory).",
+      description: "Image used to upscale. It can be an URL to the image or a path to the image file saved to the `/tmp` directory (e.g. `/tmp/image.png`). [see docs here](https://pipedream.com/docs/workflows/steps/code/nodejs/working-with-files/#the-tmp-directory).",
     },
     height: {
       propDefinition: [
@@ -42,7 +45,8 @@ export default {
 
     const formData = new FormData();
 
-    formData.append("image", fs.readFileSync(`/tmp/${image}`));
+    const imagePath = await getImagePath(image);
+    formData.append("image", fs.readFileSync(imagePath));
     for (const [
       label,
       value,
@@ -60,9 +64,12 @@ export default {
       data: formData,
     });
 
-    const paths = await this.writeImg(response.artifacts);
+    const paths = await writeImg(response.artifacts);
 
-    $.export("$summary", `The image was successfully uposcaled and sent to ${paths.toString()}!`);
-    return response;
+    $.export("$summary", `The image was successfully upscaled and sent to ${paths.toString()}!`);
+    return {
+      ...response,
+      tmpPaths: paths,
+    };
   },
 };
