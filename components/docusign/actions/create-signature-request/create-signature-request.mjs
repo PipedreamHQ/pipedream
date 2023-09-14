a390cfb451904e82ed756c0cd08c3b28711073da
Michael Ney <ney.quentin.michael@gmail.com>
2023-09-13 17:54:11 -0700

    Increment versions to get ready for PR publish from draft

--- components/docusign/actions/create-signature-request/create-signature-request.mjs
+++ components/docusign/actions/create-signature-request/create-signature-request.mjs
@@ -4,7 +4,7 @@ import common from "../common/common.mjs";
 export default {
   ...common,
   key: "docusign-create-signature-request",
-  version: "0.2.0",
+  version: "0.1.2",
   name: "Create Signature Request",
   description: "Creates a signature request from a template [See the docs here](https://developers.docusign.com/docs/esign-rest-api/reference/envelopes/envelopes/create)",
   type: "action",
