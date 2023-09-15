a6842b5852fe4a4d4ad515afa1bcf413bfcda1a4
James Ainslie <42301770+jamesainslie@users.noreply.github.com>
2023-09-15 10:13:05 -0400

    Update create-transcription.mjs

--- components/openai/actions/create-transcription/create-transcription.mjs
+++ components/openai/actions/create-transcription/create-transcription.mjs
@@ -22,6 +22,7 @@ const pipelineAsync = promisify(stream.pipeline);
 
 export default {
   name: "Create Transcription",
+  // incrementing based on new logic
   version: "0.0.10",
   key: "openai-create-transcription",
   description: "Transcribes audio into the input language. [See docs here](https://platform.openai.com/docs/api-reference/audio/create).",
