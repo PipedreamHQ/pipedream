99436ab6d3893f7ec68d47fbb6d9a8a7707ffdd1
Salmin Skenderovic <salmin89@hotmail.com>
2023-09-19 04:12:14 -0400

    [ACTIONS] Github - Get Repository Content - Support for Custom Media Type param (#8125)

--- components/github/sources/new-security-alert/new-security-alert.mjs
+++ components/github/sources/new-security-alert/new-security-alert.mjs
@@ -5,7 +5,7 @@ export default {
   key: "github-new-security-alert",
   name: "New Security Alert",
   description: "Emit new events when GitHub discovers a security vulnerability in one of your repositories",
-  version: "0.1.10",
+  version: "0.1.11",
   type: "source",
   dedupe: "unique",
   async run() {
