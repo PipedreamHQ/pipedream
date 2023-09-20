113eaf0850f3423bb16bbbbb42d7ff9aa33481fc
michelle0927 <michellelbergero@hotmail.com>
2023-09-20 12:54:40 -0400

    new component

---
+++ components/weglot/weglot.app.mjs
@@ -0,0 +1,58 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  type: "app",
+  app: "weglot",
+  propDefinitions: {
+    language: {
+      type: "string",
+      label: "Language",
+      description: "ISO 639-1 code of the language",
+      async options() {
+        const languages = await this.listLanguages();
+        return languages.map(({
+          code: value, english_name: label,
+        }) => ({
+          value,
+          label,
+        }));
+      },
+    },
+  },
+  methods: {
+    _baseUrl() {
+      return "https://api.weglot.com";
+    },
+    _authParams(params) {
+      return {
+        ...params,
+        api_key: `${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      params = {},
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        params: this._authParams(params),
+        ...args,
+      });
+    },
+    listLanguages(args = {}) {
+      return this._makeRequest({
+        path: "/public/languages",
+        ...args,
+      });
+    },
+    translateText(args = {}) {
+      return this._makeRequest({
+        path: "/translate",
+        method: "POST",
+        ...args,
+      });
+    },
+  },
+};
