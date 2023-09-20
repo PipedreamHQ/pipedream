6bdbb0f5381dbb326faab92c07425ebe2199272f
Luan Cazarine <luanhc@gmail.com>
2023-09-20 13:34:25 -0300

    [Components] TeamGantt #8103 Triggers:   - New Comment Added
    Actions:
      - Create Task
      - Update Task

---
+++ components/teamgantt/common/constants.mjs
@@ -0,0 +1,23 @@
+export const COLORS =  [
+  "blue1",
+  "blue2",
+  "blue3",
+  "brown1",
+  "green1",
+  "green2",
+  "green3",
+  "grey1",
+  "magenta1",
+  "orange1",
+  "orange2",
+  "pink1",
+  "purple1",
+  "purple2",
+  "red1",
+  "yellow1",
+];
+
+export const TYPES = [
+  "task",
+  "milestone",
+];
