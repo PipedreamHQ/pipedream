aa58d1698e86b6f5972058d66ebb30d365088380
GTFalcao <gtfalcao96@gmail.com>
2023-09-22 01:56:16 -0300

    Search Google action (AI generated + manually improved)

---
+++ components/serply/actions/search-google/search-google.mjs
@@ -0,0 +1,28 @@
+import serply from "../../serply.app.mjs";
+
+export default {
+  key: "serply-search-google",
+  name: "Search Google",
+  description: "Performs a Google search using the Serply API. [See the documentation](https://serply.io/docs/operations/v1/search)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    serply,
+    query: {
+      type: "string",
+      label: "Query",
+      description: "The search query. [See the documentation here.](https://moz.com/learn/seo/search-operators",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.serply.searchGoogle({
+      $,
+      params: {
+        q: encodeURIComponent(this.query),
+      },
+    });
+
+    $.export("$summary", `Received ${response?.results?.length} results for Google search`);
+    return response;
+  },
+};
