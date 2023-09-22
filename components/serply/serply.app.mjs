c1e199de764585343f6cad564f179d2ed7597d04
GTFalcao <gtfalcao96@gmail.com>
2023-09-22 00:55:41 -0300

    Serply (AI generated app file)

--- components/serply/serply.app.mjs
+++ components/serply/serply.app.mjs
@@ -1,6 +1,85 @@
-// import { axios } from "@pipedream/platform";
+import { axios } from "@pipedream/platform";
 
 export default {
   type: "app",
   app: "serply",
+  propDefinitions: {
+    query: {
+      type: "string",
+      label: "Query",
+      description: "The search query",
+    },
+    engine: {
+      type: "string",
+      label: "Search Engine",
+      description: "The search engine to use",
+      options: [
+        "google",
+        "bing",
+        "serp",
+      ],
+    },
+    website: {
+      type: "string",
+      label: "Website",
+      description: "The website to search in SERP",
+      optional: true,
+    },
+  },
+  methods: {
+    _baseUrl() {
+      return "https://api.serply.io/v1";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "get",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "X-Api-Key": `${this.$auth.api_token}`,
+        },
+      });
+    },
+    async searchGoogle({
+      query, ...opts
+    }) {
+      return this._makeRequest({
+        ...opts,
+        path: "/search",
+        params: {
+          q: query,
+        },
+      });
+    },
+    async searchBing({
+      query, ...opts
+    }) {
+      return this._makeRequest({
+        ...opts,
+        path: "/b/search",
+        params: {
+          q: query,
+        },
+      });
+    },
+    async searchSerp({
+      website, ...opts
+    }) {
+      return this._makeRequest({
+        ...opts,
+        path: "/serp",
+        params: {
+          website,
+        },
+      });
+    },
+  },
 };
