173a3c5693fd59339a2efc3e551f62bc5109f9ec
michelle0927 <michellelbergero@hotmail.com>
2023-09-25 09:35:40 -0400

    pandadoc fix

--- components/pandadoc/actions/send-document/send-document.mjs
+++ components/pandadoc/actions/send-document/send-document.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Send Document",
   description: "Move a document to sent status and send an optional email. [See the documentation](https://developers.pandadoc.com/reference/send-document)",
   type: "action",
-  version: "0.0.4",
+  version: "0.0.5",
   props: {
     app,
     documentId: {
