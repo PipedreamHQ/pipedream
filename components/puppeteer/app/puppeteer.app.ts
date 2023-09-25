46d52b7db2c8012f46358fc22d653da54da69836
Dylan Pierce <me@dylanjpierce.com>
2023-09-25 11:25:42 -0400

    Adding puppeeter support (#8214)

--- components/puppeteer/app/puppeteer.app.ts
+++ components/puppeteer/app/puppeteer.app.ts
@@ -1,13 +1,60 @@
 import { defineApp } from "@pipedream/types";
+import puppeteer from "puppeteer-core@19.8.0"
+import chromium from "@sparticuz/chromium@112"
 
 export default defineApp({
   type: "app",
   app: "puppeteer",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    /**
+     * Launch a new Puppeteer Headless Browser
+     * 
+     * After launching the browser, you can start new pages and perform browser actions
+     * 
+     * @param opts = {}
+     * @returns browser
+     */
+    async launch(opts = {}) {
+      const browser = await puppeteer.launch({
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+        defaultViewport: chromium.defaultViewport,
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        ...opts
+      });
+
+      return browser
     },
+    /**
+     * New Page
+     * 
+     * Creates a new web brower page.
+     * 
+     * This returns both the page and the browser instance so the browser instance can be closed.
+     * 
+     * @returns { page, browser }
+     */
+    async newPage() {
+      const browser = this._launch();
+      const page = await browser.newPage();
+
+      return {
+        page, browser
+      }
+    },
+    /**
+     * Goto URL
+     * 
+     * Shorthand method to go directly to a page
+     * 
+     * @returns { page, browser }
+     */
+    async goto() {
+      const { page, browser } = this.newPage();
+
+      return { page, browser }
+    }
   },
 });
