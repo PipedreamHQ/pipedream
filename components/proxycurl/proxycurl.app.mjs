a28f7b24b82680c408c130ebfa2c2b9586e83c3d
Cassiano Vellames <cassiano.v@turing.com>
2023-09-27 14:44:50 -0300

    retrieve company metadata from linkedin

--- components/proxycurl/proxycurl.app.mjs
+++ components/proxycurl/proxycurl.app.mjs
@@ -1,11 +1,36 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "proxycurl",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getApiKey() {
+      return this.$auth.api_key;
+    },
+    _getBaseUrl() {
+      return "https://nubela.co/proxycurl/api";
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this._getApiKey()}`,
+      };
+    },
+    async _makeHttpRequest(opts = {}, ctx = this) {
+      const axiosOpts = {
+        ...opts,
+        url: this._getBaseUrl() + opts.path,
+        headers: this._getHeaders(),
+      };
+      return axios(ctx, axiosOpts);
+    },
+    async retrieveCompanyMetadataFromLinkedin(params) {
+      return this._makeHttpRequest({
+        method: "GET",
+        path: "/linkedin/company",
+        params,
+      });
     },
   },
 };
