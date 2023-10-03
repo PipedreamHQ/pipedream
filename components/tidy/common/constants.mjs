f25eade8d0ef9f7eb53d739476ca21b16fdbc57a
michelle0927 <michelle0927@users.noreply.github.com>
2023-10-03 10:02:58 -0700

    Tidy New Components (#8221)
    * wip
    
    * new components
    
    * pnpm-lock.yaml
    
    * remove toDoListId
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * version
    
    * pnpm-lock.yaml
    
    * package version

---
+++ components/tidy/common/constants.mjs
@@ -0,0 +1,26 @@
+const PARKING_SPOT = [
+  "myspot",
+  "meter",
+  "street",
+  "guest",
+  "paidlot",
+];
+
+const PARKING_PAY_WITH = [
+  "card",
+  "cash",
+];
+
+const JOB_STATUS = [
+  "scheduled",
+  "in_progress",
+  "completed",
+  "cancelled",
+  "failed",
+];
+
+export default {
+  PARKING_SPOT,
+  PARKING_PAY_WITH,
+  JOB_STATUS,
+};
