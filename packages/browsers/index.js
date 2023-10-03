9326833c0750de21c6cb14b86e1be9ebe6901d99
Dylan Pierce <me@dylanjpierce.com>
2023-10-03 11:41:06 -0400

    adding package browsers

---
+++ packages/browsers/index.js
@@ -0,0 +1,9 @@
+export {
+  default as puppeteer,
+} from "puppeteer-core";
+export {
+  default as chromium,
+} from "@sparticuz/chromium";
+export {
+  chromium as playwright,
+} from "playwright-core@1.32.3";
