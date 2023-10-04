1b82a77736d076931b102e1f799f26e3dc320f74
Jorge Cortes <jacortesmahmud@gmail.com>
2023-10-04 11:04:17 -0500

    Fixed by enabling rejectUnauthorized apps prop

--- components/mysql_ssl/mysql_ssl.app.mjs
+++ components/mysql_ssl/mysql_ssl.app.mjs
@@ -15,6 +15,7 @@ export default {
         ca,
         key,
         cert,
+        rejectUnauthorized,
       } = this.$auth;
       return {
         host,
@@ -23,7 +24,7 @@ export default {
         password,
         database,
         ssl: {
-          rejectUnauthorized: true,
+          rejectUnauthorized: !!rejectUnauthorized,
           ca,
           key,
           cert,
