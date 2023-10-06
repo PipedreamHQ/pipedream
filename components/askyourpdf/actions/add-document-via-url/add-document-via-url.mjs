faaa9f9d68963c5b71866ddd9e93794b9717ddb5
Jorge Cortes <jacortesmahmud@gmail.com>
2023-10-06 08:00:55 -0500

    [ACTIONS] Askyoupdf new actions (#8091)
    * Added tested components
    
    * pnpm-lock.yaml
    
    * Removed uneccessary propDefinition
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

---
+++ components/askyourpdf/actions/add-document-via-url/add-document-via-url.mjs
@@ -0,0 +1,39 @@
+import app from "../../askyourpdf.app.mjs";
+
+export default {
+  key: "askyourpdf-add-document-via-url",
+  name: "Add Document Via URL",
+  description: "Add a document via URL. [See the documentation](https://docs.askyourpdf.com/askyourpdf-docs/#1.-adding-document-via-url)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    url: {
+      type: "string",
+      label: "Document URL",
+      description: "The URL of the document to add.",
+    },
+  },
+  methods: {
+    addDocumentViaURL(args = {}) {
+      return this.app.makeRequest({
+        path: "/api/download_pdf",
+        ...args,
+      });
+    },
+  },
+  run({ $: step }) {
+    const {
+      // eslint-disable-next-line no-unused-vars
+      app,
+      addDocumentViaURL,
+      ...params
+    } = this;
+
+    return addDocumentViaURL({
+      step,
+      params,
+      summary: (response) => `Successfully added document via URL with Doc ID: \`${response.docId}\``,
+    });
+  },
+};
