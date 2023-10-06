1744b0cf7c352baf071c03f6fb30bd5f19ab6489
Danny Roosevelt <danny@pipedream.com>
2023-10-06 11:26:28 -0700

    Updates

--- components/filter/actions/continue-based-on-condition/continue-based-on-condition.mjs
+++ components/filter/actions/continue-based-on-condition/continue-based-on-condition.mjs
@@ -3,10 +3,17 @@ import common from "../common/common.mjs";
 export default {
   ...common,
   name: "Continue execution if a condition Is met",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "filter-continue-based-on-condition",
   description: "Continue workflow execution only if a condition is met",
   type: "action",
+  props: {
+    ...common.props,
+    initialValue: {
+      ...common.props.initialValue,
+      label: "Only continue if...",
+    },
+  },
   methods: {
     consolidateResult($, result) {
       !result && $.flow.exit(this.messageOnEnd);
--- components/filter/actions/end-based-on-condition/end-based-on-condition.mjs
+++ components/filter/actions/end-based-on-condition/end-based-on-condition.mjs
@@ -3,10 +3,17 @@ import common from "../common/common.mjs";
 export default {
   ...common,
   name: "End execution if a condition is met",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "filter-end-based-on-condition",
   description: "End workflow execution if a condition is met",
   type: "action",
+  props: {
+    ...common.props,
+    initialValue: {
+      ...common.props.initialValue,
+      label: "End execution if...",
+    },
+  },
   methods: {
     consolidateResult($, result) {
       result && $.flow.exit(this.messageOnEnd);
