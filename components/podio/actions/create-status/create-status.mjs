a3662a5fe39cb228f01769afb3aa8b5b64aa4a4d
michelle0927 <michellelbergero@hotmail.com>
2023-10-09 13:45:28 -0400

    allow appId custom expressions in common-item

--- components/podio/actions/create-status/create-status.mjs
+++ components/podio/actions/create-status/create-status.mjs
@@ -4,9 +4,9 @@ import utils from "../../common/utils.mjs";
 export default {
   type: "action",
   key: "podio-create-status",
-  version: "0.0.1",
+  version: "0.0.2",
   name: "Create Status",
-  description: "Creates a status to the given workspace. [See the docs](https://developers.podio.com/doc/status/add-new-status-message-22336)",
+  description: "Creates a status to the given workspace. [See the documentation](https://developers.podio.com/doc/status/add-new-status-message-22336)",
   props: {
     app,
     orgId: {
