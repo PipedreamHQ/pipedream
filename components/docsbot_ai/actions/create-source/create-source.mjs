a49fe6c622e50ac3030103c923d04ffe4f1a61c2
Andrew Chuang <andrewjschuang@gmail.com>
2023-10-10 16:37:22 -0300

    docsbot_ai init

---
+++ components/docsbot_ai/actions/create-source/create-source.mjs
@@ -0,0 +1,77 @@
+import docsbot_ai from "../../docsbot_ai.app.mjs";
+
+export default {
+  key: "docsbot_ai-create-source",
+  name: "Create Source",
+  description: "Creates a new source for a bot. [See the documentation](https://docsbot.ai/documentation/developer/source-api)",
+  version: "0.0.{{ts}}",
+  type: "action",
+  props: {
+    docsbot_ai,
+    teamId: {
+      propDefinition: [
+        docsbot_ai,
+        "teamId",
+      ],
+    },
+    botId: {
+      propDefinition: [
+        docsbot_ai,
+        "botId",
+        (c) => ({
+          teamId: c.teamId,
+        }),
+      ],
+    },
+    type: {
+      propDefinition: [
+        docsbot_ai,
+        "type",
+      ],
+    },
+    title: {
+      propDefinition: [
+        docsbot_ai,
+        "title",
+      ],
+    },
+    url: {
+      propDefinition: [
+        docsbot_ai,
+        "url",
+      ],
+    },
+    file: {
+      propDefinition: [
+        docsbot_ai,
+        "file",
+      ],
+    },
+    faqs: {
+      propDefinition: [
+        docsbot_ai,
+        "faqs",
+      ],
+    },
+    scheduleInterval: {
+      propDefinition: [
+        docsbot_ai,
+        "scheduleInterval",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.docsbot_ai.createSource({
+      teamId: this.teamId,
+      botId: this.botId,
+      type: this.type,
+      title: this.title,
+      url: this.url,
+      file: this.file,
+      faqs: this.faqs,
+      scheduleInterval: this.scheduleInterval,
+    });
+    $.export("$summary", `Successfully created source with ID: ${response.id}`);
+    return response;
+  },
+};
--- components/docsbot_ai/docsbot_ai.app.mjs
+++ components/docsbot_ai/docsbot_ai.app.mjs
@@ -1,11 +1,162 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "docsbot_ai",
-  propDefinitions: {},
+  propDefinitions: {
+    teamId: {
+      type: "string",
+      label: "Team ID",
+      description: "The ID of the team",
+      async options() {
+        const teams = await this.listTeams();
+        return teams.map((team) => ({
+          value: team.id,
+          label: team.name,
+        }));
+      },
+    },
+    botId: {
+      type: "string",
+      label: "Bot ID",
+      description: "The ID of the bot",
+      async options({ teamId }) {
+        const bots = await this.listBots({
+          teamId,
+        });
+        return bots.map((bot) => ({
+          value: bot.id,
+          label: bot.name,
+        }));
+      },
+    },
+    question: {
+      type: "string",
+      label: "Question",
+      description: "The question to ask the bot",
+    },
+    full_source: {
+      type: "boolean",
+      label: "Full Source",
+      description: "Whether the full source content should be returned",
+      optional: true,
+    },
+    format: {
+      type: "string",
+      label: "Format",
+      description: "How to format the answer. Can be markdown or text",
+      optional: true,
+    },
+    history: {
+      type: "string[]",
+      label: "Chat History",
+      description: "The chat history array",
+      optional: true,
+    },
+    metadata: {
+      type: "object",
+      label: "Metadata",
+      description: "A user identification object with arbitrary metadata about the user",
+      optional: true,
+    },
+    type: {
+      type: "string",
+      label: "Source Type",
+      description: "The source type. Can be url, rss, sitemap, urls, csv, document, qa, or wp",
+    },
+    title: {
+      type: "string",
+      label: "Source Title",
+      description: "The source title",
+      optional: true,
+    },
+    url: {
+      type: "string",
+      label: "Source URL",
+      description: "The source URL",
+      optional: true,
+    },
+    file: {
+      type: "string",
+      label: "Source File Path",
+      description: "The source file path",
+      optional: true,
+    },
+    faqs: {
+      type: "string[]",
+      label: "FAQs",
+      description: "Required if type is qa. An array of objects like [{'question':'Question text', 'answer':'The answer.'}]",
+      optional: true,
+    },
+    scheduleInterval: {
+      type: "string",
+      label: "Schedule Interval",
+      description: "The source refresh scheduled interval. Can be daily, weekly, monthly, or none depending on your plan",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.docsbot.ai";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.api_key}`,
+        },
+      });
+    },
+    async listTeams() {
+      return this._makeRequest({
+        path: "/teams",
+      });
+    },
+    async listBots({ teamId }) {
+      return this._makeRequest({
+        path: `/teams/${teamId}/bots`,
+      });
+    },
+    async askQuestion({
+      teamId, botId, question, full_source, format, history, metadata,
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/teams/${teamId}/bots/${botId}/chat`,
+        data: {
+          question,
+          full_source,
+          format,
+          history,
+          metadata,
+        },
+      });
+    },
+    async createSource({
+      teamId, botId, type, title, url, file, faqs, scheduleInterval,
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/teams/${teamId}/bots/${botId}/sources`,
+        data: {
+          type,
+          title,
+          url,
+          file,
+          faqs,
+          scheduleInterval,
+        },
+      });
     },
   },
 };
