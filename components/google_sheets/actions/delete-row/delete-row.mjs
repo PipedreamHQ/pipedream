7ba3c1661577660fd00a887f0496af78770e2ebc
vellames <91911778+vellames@users.noreply.github.com>
2023-10-13 14:36:40 -0300

    [FEATURE] Adding reset row style as new prop to Add multiple rows (#8448)
    * Adding reset rows
    
    * pnpm
    
    * versions
    
    * versions
    
    * Remove wrong code
    
    * versions

--- components/google_sheets/actions/delete-row/delete-row.mjs
+++ components/google_sheets/actions/delete-row/delete-row.mjs
@@ -4,7 +4,7 @@ export default {
   key: "google_sheets-delete-row",
   name: "Delete Row",
   description: "Deletes a specific row in a spreadsheet",
-  version: "0.1.1",
+  version: "0.1.2",
   type: "action",
   props: {
     googleSheets,
