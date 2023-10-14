15a328bcc816aad7b278830e18c741aeb760cc2b
GTFalcao <gtfalcao96@gmail.com>
2023-10-14 14:43:04 -0300

    'Create Product' basic props

--- components/google-merchant-center/actions/create-product/create-product.mjs
+++ components/google-merchant-center/actions/create-product/create-product.mjs
@@ -14,17 +14,53 @@ export default {
         "merchantId",
       ],
     },
-    product: {
-      propDefinition: [
-        googleMerchant,
-        "product",
-      ],
+    title: {
+      type: "string",
+      label: "Title",
+      description: "Your product's name. Max 150 characters",
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "Your product's description. Max 5000 characters",
+    },
+    link: {
+      type: "string",
+      label: "Link",
+      description: "Your product's landing page",
+    },
+    imageLink: {
+      type: "string",
+      label: "Image Link",
+      description: "The URL of your product's main image",
+    },
+    additionalOptions: {
+      type: "string",
+      label: "Additional Options",
+      description: "Additional options for the product. [See the documentation here](https://support.google.com/merchants/answer/7052112?visit_id=638329007290220641-1391271113)",
     },
   },
   async run({ $ }) {
+    const additionalOptions = this.additionalOptions ?? {};
+    Object.entries(additionalOptions).forEach(([
+      key,
+      value,
+    ]) => {
+      try {
+        additionalOptions[key] = JSON.parse(value);
+      } catch (e) {
+        // ignore non-JSON values
+      }
+    });
     const response = await this.googleMerchant.createProduct({
       merchantId: this.merchantId,
-      product: this.product,
+      data: {
+        title: this.title,
+        description: this.description,
+        link: this.link,
+        image_link: this.imageLink,
+        ...additionalOptions,
+      },
     });
     $.export("$summary", `Product ${response.id} created successfully`);
     return response;
