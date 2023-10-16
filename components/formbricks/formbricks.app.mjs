71ef50a305dc746cfec50662113bab8eaea8c5a3
GTFalcao <gtfalcao96@gmail.com>
2023-10-16 20:03:05 -0300

    Adjustments and webhook creation/deletion hooks

--- components/formbricks/formbricks.app.mjs
+++ components/formbricks/formbricks.app.mjs
@@ -7,16 +7,15 @@ export default {
     surveyIds: {
       type: "string[]",
       label: "Survey IDs",
-      description: "The IDs of the surveys to trigger the webhook. Leave empty for all surveys.",
+      description: "The survey(s) to watch for new responses. If not provided, events will be triggered for all surveys.",
       async options() {
         const surveys = await this.listSurveys();
-        return surveys.map((survey) => ({
-          label: survey.name,
-          value: survey.id,
-        })).concat({
-          value: "[]",
-          label: "All Surveys",
-        });
+        return surveys.map(({
+          id, name,
+        }) => ({
+          label: name,
+          value: id,
+        }));
       },
     },
   },
@@ -36,8 +35,26 @@ export default {
       });
     },
     async listSurveys() {
-      return this._makeRequest({
+      const { data } = await this._makeRequest({
         url: "/management/surveys",
+      });
+      return data;
+    },
+    async createWebhook(args) {
+      const { data } = await this._makeRequest({
+        method: "POST",
+        url: "/webhooks",
+        ...args,
+      });
+      return data;
+    },
+    async deleteWebhook({
+      id, ...args
+    }) {
+      return this._makeRequest({
+        method: "DELETE",
+        url: `/webhooks/${id}`,
+        ...args,
       });
     },
   },
--- components/formbricks/sources/response-created/response-created.mjs
+++ components/formbricks/sources/response-created/response-created.mjs
@@ -3,25 +3,49 @@ import formbricks from "../../formbricks.app.mjs";
 export default {
   key: "formbricks-response-created",
   name: "Response Created",
-  description: "Emit new event when a new response is created for a survey. [See the documentation](https://formbricks.com/docs/api/management/webhooks)",
+  description: "Emit new event when a response is created for a survey. [See the documentation](https://formbricks.com/docs/api/management/webhooks)",
   version: "0.0.1",
   type: "source",
   dedupe: "unique",
   props: {
-    formbricks: {
-      type: "app",
-      app: "formbricks",
-    },
-    http: {
-      type: "$.interface.http",
-      customResponse: false,
-    },
+    formbricks,
+    http: "$.interface.http",
     db: "$.service.db",
     surveyIds: {
       propDefinition: [
         formbricks,
         "surveyIds",
       ],
+    },
+  },
+  methods: {
+    getTriggers() {
+      return [
+        "responseCreated",
+      ];
+    },
+    _getWebhookId() {
+      return this.db.get("webhookId");
+    },
+    _setWebhookId(value) {
+      this.db.set("webhookId", value);
+    },
+  },
+  hooks: {
+    async activate() {
+      const data = {
+        triggers: this.getTriggers(),
+        url: this.http.endpoint,
+      };
+
+      const { id } = await this.formbricks.createWebhook(data);
+      this._setWebhookId(id);
+    },
+    async deactivate() {
+      const id = this._getWebhookId();
+      await this.formbricks.deleteWebhook({
+        id,
+      });
     },
   },
   async run(event) {
