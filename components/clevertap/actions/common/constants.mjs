9849c92af4ccf13b6d39da62b9810645d5b29f5a
Lucas Caresia <lucascarezia@gmail.com>
2023-10-16 08:31:37 -0300

    Added actions

---
+++ components/clevertap/actions/common/constants.mjs
@@ -0,0 +1,12 @@
+export default {
+  EVENT_TYPES: [
+    "App Launched",
+    "App Installed",
+    "App Uninstalled",
+    "Charged",
+    "Notification Viewed",
+    "Notification Sent",
+    "Product Viewed",
+    "UTM Visited",
+  ],
+};
