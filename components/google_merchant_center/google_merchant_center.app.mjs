34d14ec2a6708ee8d619e805db5e67fa5e56c234
GTFalcao <gtfalcao96@gmail.com>
2023-10-16 16:57:21 -0300

    Google Merchant Center components

--- components/google_merchant_center/google_merchant_center.app.mjs
+++ components/google_merchant_center/google_merchant_center.app.mjs
@@ -1,11 +1,74 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "google_merchant_center",
-  propDefinitions: {},
+  propDefinitions: {
+    productId: {
+      type: "string",
+      label: "Product ID",
+      description: "The ID of the product",
+      async options() {
+        const products = await this.listProducts();
+        return products?.map?.((product) => ({
+          label: product.title,
+          value: product.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://shoppingcontent.googleapis.com/content/v2.1";
+    },
+    _merchantId() {
+      return this.$auth.merchant_id;
+    },
+    async _makeRequest({
+      $ = this,
+      path,
+      headers,
+      ...otherOpts
+    }) {
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async listProducts() {
+      const { resources } = await this._makeRequest({
+        path: `/${this._merchantId()}/products`,
+        params: {
+          maxResults: 250,
+        },
+      });
+      return resources;
+    },
+    async updateProduct({
+      productId, updateMask, ...args
+    }) {
+      return this._makeRequest({
+        method: "PATCH",
+        path: `/${this._merchantId()}/products/${productId}`,
+        params: {
+          updateMask,
+        },
+        ...args,
+      });
+    },
+    async createProduct(args) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/${this._merchantId()}/products`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        ...args,
+      });
     },
   },
 };
