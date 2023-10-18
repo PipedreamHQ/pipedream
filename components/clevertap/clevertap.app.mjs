ceb8b75ba86af578bf96c31f4cb7db5347498ad7
Lucas Caresia <lucascarezia@gmail.com>
2023-10-18 14:05:26 -0300

    [Components] CleverTap #8362 (#8488)
    * Added actions
    
    * Removed some events from constants
    
    * Make Event Name prop free text
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

--- components/clevertap/clevertap.app.mjs
+++ components/clevertap/clevertap.app.mjs
@@ -1,11 +1,41 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "clevertap",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _projectId() {
+      return this.$auth.project_id;
+    },
+    _passCode() {
+      return this.$auth.pass_code;
+    },
+    _region() {
+      return this.$auth.region;
+    },
+    _apiUrl() {
+      return `https://${this._region()}.clevertap.com/1`;
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        ...args,
+        headers: {
+          ...args.headers,
+          "X-CleverTap-Account-Id": this._projectId(),
+          "X-CleverTap-Passcode": this._passCode(),
+        },
+      });
+    },
+    async uploadEvent(args = {}) {
+      return this._makeRequest({
+        path: "/upload",
+        method: "post",
+        ...args,
+      });
     },
   },
 };
