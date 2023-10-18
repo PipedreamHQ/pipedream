b52dcc5440bbaeb4ca94020208e6b711fd6a1bb0
Luan Cazarine <luanhc@gmail.com>
2023-10-18 16:46:13 -0300

    3853 zoho workdrive (#8493)
    * Zoho WorkDrive #3853
    Triggers
      - New File in Folder
      - New Folder
    
    Actions
      - Upload File
    
    * pnpm update
    
    * some adjusts

--- components/zoho_workdrive/zoho_workdrive.app.mjs
+++ components/zoho_workdrive/zoho_workdrive.app.mjs
@@ -1,11 +1,216 @@
+import axios from "axios";
+import qs from "qs";
+import { LIMIT } from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "zoho_workdrive",
-  propDefinitions: {},
+  propDefinitions: {
+    teamId: {
+      type: "string",
+      label: "Team Id",
+      description: "The unique ID that represents a team.",
+      async options() {
+        const { data: { id: userId } } = await this.getUser();
+        const { data } = await this.listTeams({
+          userId,
+        });
+
+        return data.map(({
+          id: value, attributes: { name: label },
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    parentId: {
+      type: "string",
+      label: "Parent Id",
+      description: "The unique ID of the folder where files are to be uploaded.",
+      async options({
+        page, teamId,
+      }) {
+        return await this.listRootFolders({
+          teamId,
+          limit: LIMIT,
+          offset: LIMIT * page,
+        });
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiUrl(path, params = "") {
+      return `https://workdrive.${this.$auth.base_api_uri}/api/v1/${path}${params
+        ? `?${params}`
+        : ""}`;
+    },
+    _getHeaders(headers = {}) {
+      return {
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        ...headers,
+      };
+    },
+    async _makeRequest({
+      path, params, headers, ...opts
+    }) {
+      const config = {
+        url: this._apiUrl(path, params),
+        headers: this._getHeaders(headers),
+        ...opts,
+      };
+
+      const { data } = await axios(config);
+      return data;
+    },
+    getPrivateSpace({
+      teamCurrentUserId, ...args
+    }) {
+      return this._makeRequest({
+        path: `users/${teamCurrentUserId}/privatespace`,
+        ...args,
+      });
+    },
+    getTeamCurrentUser({
+      teamId, ...args
+    }) {
+      return this._makeRequest({
+        path: `teams/${teamId}/currentuser`,
+        ...args,
+      });
+    },
+    getUser(args = {}) {
+      return this._makeRequest({
+        path: "users/me",
+        ...args,
+      });
+    },
+    listFiles({
+      folderId, params = "", filter = "folder", sort = "name", ...args
+    }) {
+      return this._makeRequest({
+        path: `files/${folderId}/files`,
+        params: `${params}&filter%5Btype%5D=${filter}&sort=${sort}`,
+        ...args,
+      });
+    },
+    async listRootFolders({
+      teamId, limit, offset, params = {}, ...args
+    }) {
+      const { data: { id: teamCurrentUserId } } = await this.getTeamCurrentUser({
+        teamId,
+      });
+
+      const {
+        data: [
+          { id: privateSpaceId },
+        ],
+      } = await this.getPrivateSpace({
+        teamCurrentUserId,
+      });
+      params["page[limit]"] = limit;
+      params["page[offset]"] = offset;
+      params["sort"] = "name";
+      const reponseArray = [];
+      const { data: rootFolders } = await this._makeRequest({
+        path: `privatespace/${privateSpaceId}/folders`,
+        params: qs.stringify(params),
+        ...args,
+      });
+
+      for (const {
+        id, attributes,
+      } of rootFolders) {
+        reponseArray.push({
+          value: id,
+          label: attributes.name,
+        });
+        const subFolders = await this.paginateFolders({
+          folderId: id,
+          prefix: "- ",
+        });
+        reponseArray.push(...subFolders);
+      }
+      return reponseArray;
+    },
+    listTeams({
+      userId, ...args
+    }) {
+      return this._makeRequest({
+        path: `users/${userId}/teams`,
+        ...args,
+      });
+    },
+    uploadFile(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "upload",
+        ...args,
+      });
+    },
+    async paginateFolders({
+      folderId, prefix = "", params = {},
+    }) {
+      let hasMore = false;
+      let page = 0;
+      const responseArray = [];
+
+      do {
+        params["page[limit]"] = LIMIT;
+        params["page[offset]"] = LIMIT * page;
+        page++;
+
+        const { data: subFolders } = await this.listFiles({
+          folderId,
+          params: qs.stringify(params),
+        });
+
+        for (const {
+          id, attributes: { name },
+        } of subFolders) {
+          responseArray.push({
+            value: id,
+            label: `${prefix}${name}`,
+          });
+          responseArray.push(...await this.paginateFolders({
+            folderId: id,
+            prefix: `${prefix}- `,
+          }));
+        }
+
+        hasMore = subFolders.length;
+      } while (hasMore);
+
+      return responseArray;
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null, ...args
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params["page[limit]"] = LIMIT;
+        params["page[offset]"] = LIMIT * page;
+        page++;
+
+        const { data } = await fn({
+          params: qs.stringify(params),
+          ...args,
+        });
+
+        for (const d of data) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data.length;
+
+      } while (hasMore);
     },
   },
 };
