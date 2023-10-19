3be74dd7abc119d11b10d836774822d3adf69354
michelle0927 <michellelbergero@hotmail.com>
2023-10-19 12:02:09 -0400

    new components

---
+++ components/listmonk/actions/create-subscriber/create-subscriber.mjs
@@ -0,0 +1,60 @@
+import listmonk from "../../listmonk.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "listmonk-create-subscriber",
+  name: "Create Subscriber",
+  description: "Create a new subscriber in Listmonk. [See the documentation](https://listmonk.app/docs/apis/subscribers/#post-apisubscribers)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    listmonk,
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email of the subscriber to create.",
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the subscriber to create.",
+    },
+    status: {
+      type: "string",
+      label: "Status",
+      description: "The status of the new subscriber.",
+      options: constants.STATUS_OPTIONS,
+    },
+    listIds: {
+      propDefinition: [
+        listmonk,
+        "listIds",
+      ],
+      optional: true,
+    },
+    preconfirmSubscription: {
+      type: "boolean",
+      label: "Preconfirm Subscription",
+      description: "If `true`, marks subscription as `confirmed` and no-optin e-mails are sent for double opt-in lists.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const { data } = await this.listmonk.createSubscriber({
+      data: {
+        email: this.email,
+        name: this.name,
+        status: this.status,
+        lists: this.listIds,
+        preconfirm_subscriptions: this.preconfirmSubscription,
+      },
+      $,
+    });
+
+    if (data?.id) {
+      $.export("$summary", `Successfully created subscriber with ID ${data.id}.`);
+    }
+
+    return data;
+  },
+};
