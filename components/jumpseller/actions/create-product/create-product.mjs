1ec061d5d5edbc75471059420d287ac85d1acc4b
michelle0927 <michelle0927@users.noreply.github.com>
2023-10-20 09:33:44 -0700

    Jumpseller - New Components (#8544)
    * new components
    
    * pnpm-lock.yaml

---
+++ components/jumpseller/actions/create-product/create-product.mjs
@@ -0,0 +1,69 @@
+import jumpseller from "../../jumpseller.app.mjs";
+
+export default {
+  key: "jumpseller-create-product",
+  name: "Create Product",
+  description: "Create a new product in Jumpseller. [See the documentation](https://jumpseller.com/support/api/#tag/Products/paths/~1products.json/post)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    jumpseller,
+    name: {
+      propDefinition: [
+        jumpseller,
+        "name",
+      ],
+    },
+    price: {
+      propDefinition: [
+        jumpseller,
+        "price",
+      ],
+    },
+    description: {
+      propDefinition: [
+        jumpseller,
+        "description",
+      ],
+    },
+    stock: {
+      propDefinition: [
+        jumpseller,
+        "stock",
+      ],
+    },
+    sku: {
+      propDefinition: [
+        jumpseller,
+        "sku",
+      ],
+    },
+    status: {
+      propDefinition: [
+        jumpseller,
+        "status",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { product } = await this.jumpseller.createProduct({
+      data: {
+        product: {
+          name: this.name,
+          price: +this.price,
+          description: this.description,
+          stock: this.stock,
+          sku: this.sku,
+          status: this.status,
+        },
+      },
+      $,
+    });
+
+    if (product?.id) {
+      $.export("$summary", `Successfully created product with ID ${product.id}.`);
+    }
+
+    return product;
+  },
+};
