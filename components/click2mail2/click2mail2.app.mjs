a8b1c08839e6dca3c0a00036bcf65ebda122ff91
Dylan J. Sather <dylan.sather@gmail.com>
2023-10-23 13:32:58 -0700

    Adding app scaffolding for click2mail2

---
+++ components/click2mail2/click2mail2.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "click2mail2",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
