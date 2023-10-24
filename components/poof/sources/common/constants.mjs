9e610c5dbd613c69765c4ae103975e141c179819
michelle0927 <michelle0927@users.noreply.github.com>
2023-10-24 07:10:46 -0700

    Poof - New Components (#8525)
    * new components
    
    * pnpm-lock.yaml

---
+++ components/poof/sources/common/constants.mjs
@@ -0,0 +1,13 @@
+const PAYMENT_TYPES = {
+  "yes": "Payment Complete",
+  "payout": "Crypto Payout Sent",
+  "paid_partial": "Crypto Payment Partially Paid",
+  "transfer_out": "Bank Transfer Sent",
+  "pending": "Payment Processing",
+  "transfer_in": "Bank Transfer/ Poof Balance Received",
+  "hold": "Third party payment method pending",
+};
+
+export default {
+  PAYMENT_TYPES,
+};
