e39415032b21e69fa7034c1104aab67f5602ea04
Jorge Cortes <jacortesmahmud@gmail.com>
2023-10-24 17:31:32 -0500

    WIP create-credential action tested

--- components/accredible/actions/create-credential/create-credential.mjs
+++ components/accredible/actions/create-credential/create-credential.mjs
@@ -4,14 +4,26 @@ export default {
   key: "accredible-create-credential",
   name: "Create Credential",
   description: "Issue a new credential to a given recipient. [See the documentation](https://accrediblecredentialapi.docs.apiary.io)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
     accredible,
-    recipientEmail: {
+    email: {
       propDefinition: [
         accredible,
         "recipientEmail",
+      ],
+    },
+    name: {
+      propDefinition: [
+        accredible,
+        "recipientName",
+      ],
+    },
+    groupId: {
+      propDefinition: [
+        accredible,
+        "groupId",
       ],
     },
     credentialData: {
@@ -22,9 +34,26 @@ export default {
     },
   },
   async run({ $ }) {
-    const response = await this.accredible.createCredential({
-      recipientEmail: this.recipientEmail,
-      credentialData: this.credentialData,
+    const {
+      accredible,
+      email,
+      name,
+      groupId,
+      credentialData,
+    } = this;
+
+    const response = await accredible.createCredential({
+      $,
+      data: {
+        credential: {
+          recipient: {
+            email,
+            name,
+          },
+          group_id: groupId,
+          ...credentialData,
+        },
+      },
     });
     $.export("$summary", `Successfully created credential for ${this.recipientEmail}`);
     return response;
