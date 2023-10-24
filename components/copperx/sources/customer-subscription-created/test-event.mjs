f990e848e14172c74dcaf9a01cdf028bce6b066f
Luan Cazarine <luanhc@gmail.com>
2023-10-24 10:26:29 -0300

    8497 components copperx (#8542)
    * [Components] Copperx #8497
    Triggers
      - Invoice Paid
      - Customer Subscription Created
    
    Actions
      - Create invoice
      - Create customer
    
    * pnpm update

---
+++ components/copperx/sources/customer-subscription-created/test-event.mjs
@@ -0,0 +1,19 @@
+export default {
+  id: 'fdc5e9fb-52f8-4604-a8ec-0575c6996fa4',
+  apiVersion: '2023-01-11',
+  created: 1697735629533,
+  object: 'webhookEndpoint',
+  type: 'webhook_endpoint_test',
+  data: {
+    object: {
+      id: '366ff7e6-32af-4e2b-a94e-c6e8cab37762',
+      description: 'Pipedream-customer-subscription-created',
+      url: 'https://8d40d4079384e2e1ddd64e7af5fbd4b4.m.pipedream.net',
+      secret: 'wh_k8rxkaWJ0YN9kQGsqqb2lMNVQGjXpeEcwVIRNnvDJ0PleZwMhCXUepV8sg5zVjN6',
+      createdAt: '2023-10-19T17:13:30.922Z',
+      updatedAt: '2023-10-19T17:13:30.922Z',
+      enabledEvents: [Object],
+      isDisabled: false
+    }
+  }
+}
