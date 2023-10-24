b1eac52b0d63cc3e6ab0cce2998bdaca6b1a8035
Alyson Freitas <alyson.f@turing.com>
2023-10-24 12:45:19 -0300

    OneDrive - allow creating a new folder inside of a shared folder

--- components/microsoft_onedrive/actions/create-folder/create-folder.mjs
+++ components/microsoft_onedrive/actions/create-folder/create-folder.mjs
@@ -1,5 +1,6 @@
 import onedrive from "../../microsoft_onedrive.app.mjs";
 import httpRequest from "../../common/httpRequest.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   name: "Create Folder",
@@ -18,6 +19,21 @@ export default {
       description: "The ID of the folder which the the new folder should be created.",
       optional: true,
     },
+    sharedFolderReference: {
+      type: "string",
+      label: "Shared Folder Reference",
+      description: "The reference of the shared folder which the the new folder should be created.\n\nE.g. `/drives/{driveId}/items/{folderId}/children`",
+      optional: true,
+      async options() {
+        const { value } = await this.httpRequest({
+          url: "/sharedWithMe",
+        });
+        return value.map((shared) => ({
+          label: shared.name,
+          value: `/drives/${shared.remoteItem.parentReference.driveId}/items/${shared.remoteItem.id}/children`,
+        }));
+      },
+    },
     folderName: {
       type: "string",
       label: "Folder Name",
@@ -27,14 +43,18 @@ export default {
   methods: {
     httpRequest,
     createFolder({
-      folderName, parentFolderId,
+      folderName, parentFolderId, sharedFolderReference,
     }) {
       let url = "/root/children";
       if (parentFolderId) {
         url = `/items/${parentFolderId}/children`;
       }
+      if (sharedFolderReference) {
+        url = sharedFolderReference;
+      }
       return this.httpRequest({
         url,
+        useSharedDrive: !!sharedFolderReference,
         headers: {
           "Content-Type": "application/json",
         },
@@ -49,12 +69,17 @@ export default {
   },
   async run({ $ }) {
     const {
-      folderName, parentFolderId,
+      folderName, parentFolderId, sharedFolderReference,
     } = this;
+
+    if (sharedFolderReference && parentFolderId) {
+      throw new ConfigurationError("You have to select either a `Parent Folder` or a `Shared Folder`.");
+    }
 
     const response = await this.createFolder({
       folderName,
       parentFolderId,
+      sharedFolderReference,
     });
 
     if (response?.id) {
--- components/microsoft_onedrive/actions/upload-file/upload-file.mjs
+++ components/microsoft_onedrive/actions/upload-file/upload-file.mjs
@@ -8,7 +8,7 @@ export default {
   name: "Upload File",
   description: "Upload a file to OneDrive. [See the documentation](https://learn.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_put_content?view=odsp-graph-online)",
   key: "microsoft_onedrive-upload-file",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     onedrive,
