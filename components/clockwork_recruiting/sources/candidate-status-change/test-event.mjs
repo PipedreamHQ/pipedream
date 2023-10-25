35239b88c8783006683410d6358863c8fa973fb7
Luan Cazarine <luanhc@gmail.com>
2023-10-25 12:56:44 -0300

    8275 components clockwork recruiting (#8348)
    * [Components] Clockwork Recruiting #8275
    Triggers
      - Candidate Status Change
    
    Actions:
     - Create Person
     - Add Person Address
     - Add Person Email
     - Add Person Linkedin
     - Add Person Phone Number
     - Add Person Position
     - Add Person Tag
    
    * pnpm update
    
    * fix source key
    
    * pnpm update
    
    * some adjusts
    
    * some ajusts
    
    * Fix street 2
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

---
+++ components/clockwork_recruiting/sources/candidate-status-change/test-event.mjs
@@ -0,0 +1,19 @@
+export default {
+  "candidacy":{
+    "assessment_note_id":null,
+    "color_category":null,
+    "created_at":"2023-10-02T15:52:27Z",
+    "created_user_id":"12345678-233d-4fd5-9618-12345678343",
+    "id":"12345678-233d-4fd5-9618-12345678343",
+    "next_steps_note_id":null,
+    "overview_note_id":null,
+    "person_id":"12345678-233d-4fd5-9618-12345678343",
+    "project_id":"12345678-233d-4fd5-9618-12345678343",
+    "rank":14,
+    "status_id":"12345678-233d-4fd5-9618-12345678343",
+    "stoplight_status":"red",
+    "updated_at":"2023-10-02T15:54:34Z",
+    "updated_at_display":"2023-10-02T15:54:32Z"
+  },
+  "event_name":"candidacy_updated"
+}
