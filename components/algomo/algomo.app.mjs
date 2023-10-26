764ba65652533b8ada6c5edf5e772a079f74f871
Jorge Cortes <jacortesmahmud@gmail.com>
2023-10-26 11:54:31 -0500

    [ACTION] Algomo Added action component (#8602)
    * Added tested component
    
    * package.json
    
    * new line at EOF in package.json
    
    * Fixed redundancy in props

--- components/algomo/algomo.app.mjs
+++ components/algomo/algomo.app.mjs
@@ -1,11 +1,65 @@
+import {
+  axios, ConfigurationError,
+} from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "algomo",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    exportSummary(step) {
+      if (!step?.export) {
+        throw new ConfigurationError("The summary method should be bind to the step object aka `$`");
+      }
+      return (msg = "") => step.export(constants.SUMMARY_LABEL, msg);
+    },
+    getUrl(path) {
+      return `${constants.BASE_URL}${constants.VERSION_PATH}${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": `Bearer ${this.$auth.api_token}`,
+      };
+    },
+    getData(data) {
+      return {
+        botId: this.$auth.chatbot_id,
+        ...data,
+      };
+    },
+    async makeRequest({
+      step = this, path, headers, summary, data, ...args
+    } = {}) {
+      const {
+        getUrl,
+        getHeaders,
+        exportSummary,
+        getData,
+      } = this;
+
+      const config = {
+        url: getUrl(path),
+        headers: getHeaders(headers),
+        data: getData(data),
+        ...args,
+      };
+
+      const response = await axios(step, config);
+
+      if (typeof summary === "function") {
+        exportSummary(step)(summary(response));
+      }
+
+      return response;
+    },
+    post(args = {}) {
+      return this.makeRequest({
+        method: "post",
+        ...args,
+      });
     },
   },
 };
