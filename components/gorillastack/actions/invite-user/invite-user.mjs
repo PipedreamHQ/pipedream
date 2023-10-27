2fa9b27e9a3abf677e79211435b7754553c263aa
Alyson Freitas <alyson.f@turing.com>
2023-10-27 13:38:15 -0300

    GorillaStack - components

---
+++ components/gorillastack/actions/invite-user/invite-user.mjs
@@ -0,0 +1,38 @@
+import app from "../../gorillastack.app.mjs";
+
+export default {
+  key: "gorillastack-invite-user",
+  name: "Invite User",
+  description: "Invite new user to GorillaStack. [See the documentation](https://docs.gorillastack.com/swagger/v2#/users/post_teams__teamId__users_invite)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email of the user to invite",
+    },
+    role: {
+      type: "string",
+      label: "Role",
+      description: "Role of the user to invite",
+      options: [
+        "guest",
+        "member",
+        "admin",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.inviteUser({
+      $,
+      data: {
+        email: this.email,
+        role: this.role,
+      },
+    });
+    $.export("$summary", `Successfully invited "${this.email}" to GorillaStack`);
+    return response;
+  },
+};
