e2b26da9c5b20b4bbae11131f1497a2e6acab190
Cassiano Vellames <cassiano.v@turing.com>
2023-10-30 11:05:07 -0300

    versions

--- components/github/actions/list-releases/list-releases.mjs
+++ components/github/actions/list-releases/list-releases.mjs
@@ -6,7 +6,7 @@ export default {
   key: "github-list-releases",
   name: "List Releases",
   description: "List releases for a repository [See the documentation](https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#list-releases)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     github,
