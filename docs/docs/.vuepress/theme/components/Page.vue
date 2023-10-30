28c2d66f2330473079d4e1f52395baebb49c6863
Andrew Chuang <andrewjschuang@gmail.com>
2023-10-30 09:08:37 -0300

    banner in all pages

--- docs/docs/.vuepress/theme/components/Page.vue
+++ docs/docs/.vuepress/theme/components/Page.vue
@@ -1,5 +1,13 @@
 <template>
   <main class="page">
+    <div class="page">
+      <div class="deprecated-banner">
+        ⚠️ This version of the docs is deprecated. Looking for the latest features and updates?
+        <a href="https://pipedream.com/docs">Explore our latest documentation.</a>.
+      </div>
+      <Content/>
+    </div>
+
     <slot name="top" />
 
     <Content class="theme-default-content" />
@@ -9,14 +17,16 @@
 
     <slot name="bottom" />
   </main>
+
 </template>
 
 <script>
+import { Page } from '@theme/components/Page'
 import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
 
 export default {
-  components: { PageEdit, PageNav },
+  components: { PageEdit, PageNav, Content: Page.components.Content },
   props: ['sidebarItems']
 }
 </script>
@@ -27,5 +37,19 @@ export default {
 .page
   padding-bottom 2rem
   display block
+
+.deprecated-banner {
+  background-color: #ffee58;  /* Bright yellow background */
+  color: #333;  /* Dark text color for contrast */
+  padding: 1rem;
+  text-align: center;
+  border-bottom: 1px solid #eaecef;
+  font-weight: bold; /* Make text bold for emphasis */
+}
+
+.deprecated-banner a {
+  color: #0066cc;  /* A blue color for the link */
+  text-decoration: underline;  /* Underline for clarity */
+}
 
 </style>
