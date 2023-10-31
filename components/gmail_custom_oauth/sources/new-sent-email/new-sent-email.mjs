fabfd3499d5745612b3eb12990302e9acd612386
michelle0927 <michellelbergero@hotmail.com>
2023-10-31 12:12:13 -0400

    fix version

--- components/gmail_custom_oauth/sources/new-sent-email/new-sent-email.mjs
+++ components/gmail_custom_oauth/sources/new-sent-email/new-sent-email.mjs
@@ -5,7 +5,7 @@ export default {
   key: "gmail_custom_oauth-new-sent-email",
   name: "New Sent Email",
   description: "Emit new event for each new email sent. (Maximum of 300 events emited per execution)",
-  version: "0.04",
+  version: "0.0.4",
   type: "source",
   props: {
     app,
