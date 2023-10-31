18b1889186ec1334b4fed5629747dae4df563131
Lucas Caresia <lucascarezia@gmail.com>
2023-10-31 10:18:59 -0300

    Added actions

---
+++ components/doppler_marketing_automation/actions/remove-subscriber/remove-subscriber.mjs
@@ -0,0 +1,37 @@
+import app from "../../doppler_marketing_automation.app.mjs";
+
+export default {
+  name: "Remove Subscriber",
+  version: "0.0.1",
+  key: "doppler_marketing_automation-remove-subscriber",
+  description: "Remove a subscriber. [See the documentation](https://restapi.fromdoppler.com/docs/resources#!/Subscribers/AccountsByAccountNameListsByListIdSubscribersByEmailDelete)",
+  type: "action",
+  props: {
+    app,
+    listId: {
+      propDefinition: [
+        app,
+        "listId",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "subscriberEmail",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.removeSubscriber({
+      $,
+      email: this.email,
+      listId: this.listId,
+    });
+
+    if (response) {
+      $.export("$summary", `Successfully removed subscriber with email ${this.email}`);
+    }
+
+    return response;
+  },
+};
