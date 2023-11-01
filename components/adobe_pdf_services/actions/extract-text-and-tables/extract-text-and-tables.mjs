19463e94b42e6e72dcaa22bf5840bf4d951bb38a
michelle0927 <michellelbergero@hotmail.com>
2023-11-01 15:36:51 -0400

    new components

---
+++ components/adobe_pdf_services/actions/extract-text-and-tables/extract-text-and-tables.mjs
@@ -0,0 +1,31 @@
+import adobe from "../../adobe_pdf_services.app.mjs";
+
+export default {
+  key: "adobe_pdf_services-extract-text-and-tables",
+  name: "Extract Text and Tables From PDF",
+  description: "Extracts text and table element information from a PDF document and returns a JSON file along with table data in XLSX format within a .zip file saved to the `/tmp` directory. [See the documentation](https://developer.adobe.com/document-services/docs/overview/pdf-extract-api/howtos/extract-api/#extract-text-and-tables)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    adobe,
+    filePath: {
+      propDefinition: [
+        adobe,
+        "filePath",
+      ],
+    },
+    filename: {
+      propDefinition: [
+        adobe,
+        "filename",
+      ],
+    },
+  },
+  async run({ $ }) {
+    await this.adobe.extractPDF(this.filePath, "tables", this.filename);
+    $.export("$summary", "Successfully extracted text and tables from pdf file.");
+    return `/tmp/${this.filename.includes(".zip")
+      ? this.filename
+      : this.filename + ".zip"}`;
+  },
+};
