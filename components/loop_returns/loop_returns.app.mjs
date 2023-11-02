010c19d5f56fde24fe804e32fbff0ac747e1f190
Dylan J. Sather <dylan.sather@gmail.com>
2023-11-02 11:32:59 -0700

    Adding app scaffolding for loop_returns

---
+++ components/loop_returns/loop_returns.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "loop_returns",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
