15e474391aef2cf05c6da65c54cf6708a8cd7a19
Jorge Cortes <jacortesmahmud@gmail.com>
2023-11-03 09:21:56 -0500

    New Components - google_cloud (#8669)
    * google_cloud init
    
    * Untested new action component because of deploy error
    
    * pnpm-lock.yaml
    
    * Fixed destinationDatasetId prop
    
    * Fixed text in schedule prop
    
    * Added serviceAccountName arg to client.createTransferConfig function
    
    * Minor change
    
    * Minor change
    
    * Minor changes
    
    * Fixed createTranferConfig args

---
+++ components/google_cloud/common/constants.mjs
@@ -0,0 +1,13 @@
+const WRITE_DISPOSITION = {
+  WRITE_TRUNCATE: "WRITE_TRUNCATE",
+  WRITE_APPEND: "WRITE_APPEND",
+};
+
+const DATA_SOURCE_ID = {
+  SCHEDULED_QUERY: "scheduled_query",
+};
+
+export default {
+  WRITE_DISPOSITION,
+  DATA_SOURCE_ID,
+};
