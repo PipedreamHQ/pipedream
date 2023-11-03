0243e92cf9a106a3d6aa72ee7399eb518c2530b9
Jorge Cortes <jacortesmahmud@gmail.com>
2023-11-03 10:11:50 -0500

    Improved parseElements utils function

--- components/abyssale/common/utils.mjs
+++ components/abyssale/common/utils.mjs
@@ -1,11 +1,38 @@
+import { ConfigurationError } from "@pipedream/platform";
+
+function emptyStrToUndefined(value) {
+  const trimmed = typeof(value) === "string" && value.trim();
+  return trimmed === ""
+    ? undefined
+    : value;
+}
+
+function parse(value) {
+  const valueToParse = emptyStrToUndefined(value);
+  if (typeof(valueToParse) === "object" || valueToParse === undefined) {
+    return valueToParse;
+  }
+  try {
+    return JSON.parse(valueToParse);
+  } catch (e) {
+    throw new ConfigurationError(`Make sure the custom expression contains a valid JSON object: \`${valueToParse}\``);
+  }
+}
+
 function parseElements(elements) {
+  const parsed = parse(elements);
+
+  if (!Object.keys(parsed).length) {
+    throw new ConfigurationError(`The field must contain at least one element. \`${elements}\``);
+  }
+
   return Object.fromEntries(
-    Object.entries(elements).map(([
+    Object.entries(parsed).map(([
       key,
       value,
     ]) => [
       key,
-      JSON.parse(value),
+      parse(value),
     ]),
   );
 }
