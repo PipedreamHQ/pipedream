57f36ce5b8b6fad885053a3dfe86e7489be371b7
Lucas Caresia <lucascarezia@gmail.com>
2023-11-06 07:56:37 -0300

    Added actions (#8812)

--- components/kickbox/kickbox.app.mjs
+++ components/kickbox/kickbox.app.mjs
@@ -1,11 +1,40 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "kickbox",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _apiUrl() {
+      return "https://api.kickbox.com/v2";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        ...args,
+        params: {
+          ...args.params,
+          "apikey": this._apiKey(),
+        },
+      });
+    },
+    async verifyEmail(args = {}) {
+      return this._makeRequest({
+        path: "/verify",
+        ...args,
+      });
+    },
+    async verifyEmailBatch(args = {}) {
+      return this._makeRequest({
+        path: "/verify-batch",
+        method: "put",
+        ...args,
+      });
     },
   },
 };
