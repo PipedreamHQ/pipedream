3ca8b007af419c5d7328266c1434affdad2db234
michelle0927 <michelle0927@users.noreply.github.com>
2023-11-07 06:50:31 -0800

    Bingx - V2 Perpetual Swap BingX Actions (#8701)
    * upgrade to V2 of API
    
    * remove sortKeys
    
    * refactor
    
    * pnpm-lock.yaml
    
    * remove console.log
    
    * remove console.log
    
    * updates

--- components/bingx/actions/generic-api-call/generic-api-call.mjs
+++ components/bingx/actions/generic-api-call/generic-api-call.mjs
@@ -1,10 +1,13 @@
 import bingx from "../../bingx.app.mjs";
+import {
+  VERSION_2_PATH, VERSION_3_PATH,
+} from "../../common.mjs";
 
 export default {
   name: "BingX Generic API Call",
-  version: "0.0.2",
+  version: "0.0.3",
   key: "bingx-generic-api-call",
-  description: "Make any API call for Bingx Futures as per the documentation. See docs [here](https://bingx-api.github.io/docs/swap/)",
+  description: "Make any API call for Bingx Futures as per the documentation. [See the documentation](https://bingx-api.github.io/docs/#/swapV2/account-api.html)",
   props: {
     bingx,
     httpMethod: {
@@ -17,12 +20,27 @@ export default {
         "POST",
         "PUT",
       ],
-      default: "POST",
+      default: "GET",
     },
     apiPath: {
       label: "API Path",
-      description: "Ex. /api/v1/market/getAllContracts",
+      description: "Ex. /quote/contracts",
       type: "string",
+    },
+    apiVersion: {
+      type: "string",
+      label: "API Version",
+      description: "The API version the path belongs to",
+      options: [
+        {
+          label: "Version 2",
+          value: VERSION_2_PATH,
+        },
+        {
+          label: "Version 3",
+          value: VERSION_3_PATH,
+        },
+      ],
     },
     params: {
       label: "Parameters",
@@ -36,11 +54,15 @@ export default {
     if (!this.apiPath.startsWith("/")) {
       throw new Error("Api Path property should always start with `/` as a path like string");
     }
-    const returnValue = await this.bingx.makeRequest(
-      this.httpMethod, this.apiPath, this.params,
-    );
-    if (returnValue["ret_code"]) {
-      throw new Error(returnValue["ret_msg"]);
+    const returnValue = await this.bingx.makeRequest({
+      path: this.apiPath,
+      version: this.apiVersion,
+      method: this.httpMethod,
+      params: this.params,
+      $,
+    });
+    if (returnValue.code) {
+      throw new Error(returnValue.msg);
     } else {
       $.export("$summary", "Bingx Futures custom action successful");
     }
--- components/bingx/actions/trade-set-margin-mode/trade-set-margin-mode.mjs
+++ components/bingx/actions/trade-set-margin-mode/trade-set-margin-mode.mjs
@@ -2,9 +2,9 @@ import bingx from "../../bingx.app.mjs";
 
 export default {
   name: "BingX Trade Set Margin Mode",
-  version: "0.0.3",
+  version: "0.0.4",
   key: "bingx-trade-set-margin-mode",
-  description: "Switch Margin Mode [reference](https://bingx-api.github.io/docs/swap/trade-api.html#_9-switch-margin-mode).",
+  description: "Switch Margin Mode [See the documentation](https://bingx-api.github.io/docs/#/swapV2/trade-api.html#Switch%20Margin%20Mode).",
   props: {
     bingx,
     symbol: {
@@ -22,14 +22,20 @@ export default {
   },
   type: "action",
   async run({ $ }) {
-    const API_METHOD = "POST";
-    const API_PATH = "/api/v1/user/setMarginMode";
-    const parameters = {
-      "symbol": this.symbol,
-      "marginMode": this.marginMode,
-    };
-    const returnValue = await this.bingx.makeRequest(API_METHOD, API_PATH, parameters);
-    $.export("$summary", `Set Margin Mode ${this.marginMode} for ${this.symbol}`);
+    const returnValue = await this.bingx.makeRequest({
+      path: "/trade/marginType",
+      method: "POST",
+      params: {
+        symbol: this.symbol,
+        marginType: this.marginMode,
+      },
+      $,
+    });
+    if (returnValue.code) {
+      throw new Error(returnValue.msg);
+    } else {
+      $.export("$summary", `Set Margin Mode ${this.marginMode} for ${this.symbol}`);
+    }
     return returnValue;
   },
 };
