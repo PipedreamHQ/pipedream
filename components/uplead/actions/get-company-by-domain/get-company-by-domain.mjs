9c777843d1fac2acf7bca997c10304553562ea9d
Lucas Caresia <lucascarezia@gmail.com>
2023-11-08 08:40:26 -0300

    Added actions

---
+++ components/uplead/actions/get-company-by-domain/get-company-by-domain.mjs
@@ -0,0 +1,31 @@
+import app from "../../uplead.app.mjs";
+
+export default {
+  name: "Get Company By Domain",
+  version: "0.0.1",
+  key: "uplead-get-get-company-by-domain",
+  description: "Get a company by domain. [See the documentation](https://docs.uplead.com/#company-api)",
+  type: "action",
+  props: {
+    app,
+    domain: {
+      type: "string",
+      label: "Domain",
+      description: "Domain of the company. E.g. `pipedream.com`",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getCompanyByDomain({
+      $,
+      data: {
+        domain: this.domain,
+      },
+    });
+
+    if (response?.data?.id) {
+      $.export("$summary", `Successfully retrieved company with ID \`${response.data.id}\``);
+    }
+
+    return response;
+  },
+};
