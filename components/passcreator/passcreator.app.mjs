dfda433373249107e5d6644d4fea527860a79540
michelle0927 <michellelbergero@hotmail.com>
2023-11-09 12:52:22 -0500

    passcreator init

--- components/passcreator/passcreator.app.mjs
+++ components/passcreator/passcreator.app.mjs
@@ -1,11 +1,61 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "passcreator",
-  propDefinitions: {},
+  propDefinitions: {
+    scanId: {
+      type: "string",
+      label: "Scan ID",
+      description: "The ID of the app scan",
+    },
+    voidPassId: {
+      type: "string",
+      label: "Voided Pass ID",
+      description: "The ID of the voided pass",
+    },
+    newPassId: {
+      type: "string",
+      label: "New Pass ID",
+      description: "The ID of the new wallet pass",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.passcreator.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Authorization": `Bearer ${this.$auth.access_token}`,
+        },
+      });
+    },
+    async getScan(scanId) {
+      return this._makeRequest({
+        path: `/space/api/23494978/${scanId}`,
+      });
+    },
+    async getVoidedPass(voidPassId) {
+      return this._makeRequest({
+        path: `/space/api/23331211/${voidPassId}`,
+      });
+    },
+    async getNewPass(newPassId) {
+      return this._makeRequest({
+        path: `/space/api/23331116/subscription+endpoint/${newPassId}`,
+      });
     },
   },
 };
