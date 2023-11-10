8c0b5610361dcc168eb679c38b912bf33b5cf43c
Anish Karandikar <anishkny@gmail.com>
2023-11-10 09:28:37 -0800

    Refactor cart abandonment action

---
+++ components/stripe/sources/abandoned-cart/abandoned-cart.mjs
@@ -0,0 +1,16 @@
+import common from "../common/webhook-base.mjs";
+
+export default {
+  ...common,
+  key: "stripe-abandoned-cart",
+  name: "New Abandoned Cart",
+  type: "source",
+  version: "0.0.1",
+  description: "Emit new event when a customer abandons their cart.",
+  methods: {
+    ...common.methods,
+    getEvents() {
+      return ["checkout.session.expired"];
+    },
+  },
+};
