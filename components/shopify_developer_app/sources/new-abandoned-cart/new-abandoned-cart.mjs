2de7c815a634e594e88e5393778f75d46a6bc4b8
michelle0927 <michelle0927@users.noreply.github.com>
2023-11-13 06:02:31 -0800

    bug fix (#8923)

--- components/shopify_developer_app/sources/new-abandoned-cart/new-abandoned-cart.mjs
+++ components/shopify_developer_app/sources/new-abandoned-cart/new-abandoned-cart.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Abandoned Cart",
   type: "source",
   description: "Emit new event each time a user abandons their cart.",
-  version: "0.0.2",
+  version: "0.0.3",
   dedupe: "unique",
   props: {
     shopify,
