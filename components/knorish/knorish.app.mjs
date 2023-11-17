9eea70403f618f1b4f845e15158e3cf620abe159
Luan Cazarine <luanhc@gmail.com>
2023-11-17 11:52:17 -0300

    [Components] knorish #8860 Sources   - New Signup   - Bundle Purchased
    Actions
      - Add User In Course
      - Create User

--- components/knorish/knorish.app.mjs
+++ components/knorish/knorish.app.mjs
@@ -1,122 +1,144 @@
 import { axios } from "@pipedream/platform";
+import { LIMIT } from "./common/constants.mjs";
 
 export default {
   type: "app",
   app: "knorish",
   propDefinitions: {
-    userId: {
+    courseId: {
       type: "string",
-      label: "User ID",
-      description: "The ID of the user",
-      async options({ prevContext }) {
-        const page = prevContext.page
-          ? prevContext.page
-          : 0;
-        const users = await this.getUsers({
+      label: "Course ID",
+      description: "The ID of the course",
+      async options({ page }) {
+        return this.asyncOptions({
           page,
+          fn: this.listCourses,
         });
-        return {
-          options: users.map((user) => ({
-            label: user.name,
-            value: user.id,
-          })),
-          context: {
-            page: page + 1,
-          },
-        };
       },
     },
     bundleId: {
       type: "string",
-      label: "Bundle ID",
-      description: "The ID of the bundle",
-      async options({ prevContext }) {
-        const page = prevContext.page
-          ? prevContext.page
-          : 0;
-        const bundles = await this.getBundles({
+      label: "Bundle Id",
+      description: "The id of the bundle.",
+      async options({ page }) {
+        return this.asyncOptions({
           page,
+          fn: this.listBundles,
         });
-        return {
-          options: bundles.map((bundle) => ({
-            label: bundle.title,
-            value: bundle.id,
-          })),
-          context: {
-            page: page + 1,
-          },
-        };
       },
     },
-    newUserDetails: {
-      type: "object",
-      label: "New User Details",
-      description: "The details of the new user to create",
-    },
-    addUserToBundleDetails: {
-      type: "object",
-      label: "Add User to Bundle Details",
-      description: "The details for adding a user to a specific bundle",
+    userId: {
+      type: "string",
+      label: "User ID",
+      description: "The ID of the user",
+      async options({ page }) {
+        return this.asyncOptions({
+          page,
+          fn: this.listUsers,
+        });
+      },
     },
   },
   methods: {
     _baseUrl() {
-      return "https://api.knorish.com";
+      return "https://api.knorish.com/publisher/v1.0";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path,
-        data,
-        params,
-        headers,
-        ...otherOpts
-      } = opts;
-
+    _getHeaders() {
+      return {
+        "x-appid": this.$auth.app_id,
+        "x-appkey": this.$auth.api_key,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "Content-Type": "application/json",
-          "X-App-Id": `${this.$auth.app_id}`,
-          "X-App-Key": `${this.$auth.app_key}`,
+        url: `${this._baseUrl()}${path}`,
+        headers: this._getHeaders(),
+        ...opts,
+      });
+    },
+    async asyncOptions({
+      page, fn,
+    }) {
+      const { data } = await fn({
+        params: {
+          start: LIMIT * page,
+          length: LIMIT,
         },
-        data,
-        params,
       });
+
+      return data.map(({
+        id: value, name: label,
+      }) => ({
+        label,
+        value,
+      }));
     },
-    async getUsers(opts = {}) {
+    listBundles(opts = {}) {
       return this._makeRequest({
-        path: "/v1/users",
+        path: "/bundle/GetBundles",
         ...opts,
       });
     },
-    async getBundles(opts = {}) {
+    listBundleUsers(opts = {}) {
       return this._makeRequest({
-        path: "/v1/bundles",
+        path: "/bundle/GetBundleUsers",
         ...opts,
       });
     },
-    async createUser(userDetails) {
+    listCourses(opts = {}) {
       return this._makeRequest({
-        method: "POST",
-        path: "/v1/user",
-        data: userDetails,
+        path: "/course/GetCourses",
+        ...opts,
       });
     },
-    async addUserToBundle(details) {
+    listUsers(opts = {}) {
       return this._makeRequest({
-        method: "POST",
-        path: "/v1/user/bundle",
-        data: details,
+        path: "/user/GetUsers",
+        ...opts,
       });
     },
-    // Additional methods to handle new signups and bundle purchases would be
-    // defined here. However, these would typically be implemented in a source
-    // component, using methods like `getUsers` and `getBundles` to check for new
-    // signups or purchases.
+    createUser( opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/user/CreateUser",
+        ...opts,
+      });
+    },
+    addUserToCourse(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/user/AddUserInCourse",
+        ...opts,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null,
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.start = LIMIT * page;
+        params.length = LIMIT;
+        page++;
+
+        const { data } = await fn({
+          params,
+        });
+        for (const d of data) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data.length;
+
+      } while (hasMore);
+    },
   },
 };
---
+++ components/knorish/sources/new-signup/test-event.mjs
@@ -0,0 +1,13 @@
+export default {
+  "id": "123123123-1231-1234-1235-123412342341",
+  "name": "Name",
+  "profilepicture": "https://knorish-asset-cdn.azureedge.net/knorish-static-assets/images/v1/profilepicture/image-1.png",
+  "email": "email@test.com",
+  "phonenumber": "+1234567890",
+  "lastseenon": "2020-10-12T20:59:21.16",
+  "timestamp": "2020-10-11T20:59:21.16",
+  "customfieldcol3": null,
+  "customfieldcol4": null,
+  "customfieldcol5": null,
+  "customfieldcol6": null
+}
