1bb3b038bf4cbd044675df4f854203b3974e7e49
GTFalcao <gtfalcao96@gmail.com>
2023-11-17 14:10:05 -0300

    Removing premium actions

--- components/sms_alert/sms_alert.app.mjs
+++ components/sms_alert/sms_alert.app.mjs
@@ -4,11 +4,6 @@ export default {
   type: "app",
   app: "sms_alert",
   propDefinitions: {
-    apiKey: {
-      type: "string",
-      label: "API Key",
-      description: "The API Key generated from your SMS Alert account.",
-    },
     groupName: {
       type: "string",
       label: "Group Name",
@@ -23,42 +18,10 @@ export default {
         return response?.description?.map?.(({ Group: { name } }) => name);
       },
     },
-    senderId: {
-      type: "string",
-      label: "Sender ID",
-      description: "The Sender ID assigned to your account. [See the documentation for more details](https://kb.smsalert.co.in/developers-api/#Get-Available-Sender-Id-List).",
-      optional: true,
-      async options({ prevContext }) {
-        const { page } = prevContext || {
-          page: 1,
-        };
-        const response = await this.listSenderIds({
-          page,
-        });
-        if (response.status === "error") throw new Error(response.description);
-        return response.description?.map?.(({
-          id, name,
-        }) => ({
-          label: id,
-          value: name,
-        }));
-      },
-    },
     mobileNumber: {
       type: "string[]",
       label: "Mobile Number(s)",
       description: "The mobile number(s) to which the SMS is to be sent, with or without country code.",
-    },
-    messageText: {
-      type: "string",
-      label: "Message Text",
-      description: "The message to be sent.",
-    },
-    scheduleTime: {
-      type: "string",
-      label: "Schedule Time",
-      description: "The date and time for the schedule (Format: `YYYY-MM-DD HH:MM:SS`).",
-      optional: true,
     },
     contactName: {
       type: "string",
@@ -98,13 +61,6 @@ export default {
       return this._makeRequest({
         method: "POST",
         path: "/createcontact.json",
-        ...args,
-      });
-    },
-    async sendSMS(args) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/push.json",
         ...args,
       });
     },
