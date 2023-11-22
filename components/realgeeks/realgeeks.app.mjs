29e56cb484f79b656e3d24ba7244e6a4da99a6e6
michelle0927 <michellelbergero@hotmail.com>
2023-11-22 10:00:42 -0500

    remove unused props

--- components/realgeeks/realgeeks.app.mjs
+++ components/realgeeks/realgeeks.app.mjs
@@ -3,36 +3,6 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "realgeeks",
-  propDefinitions: {
-    leadName: {
-      type: "string",
-      label: "Lead Name",
-      description: "The name of the lead to create",
-    },
-    leadEmail: {
-      type: "string",
-      label: "Lead Email",
-      description: "The email of the lead to create",
-    },
-    leadPhone: {
-      type: "string",
-      label: "Lead Phone",
-      description: "The phone number of the lead to create",
-      optional: true,
-    },
-    leadSource: {
-      type: "string",
-      label: "Lead Source",
-      description: "The source of the lead",
-      optional: true,
-    },
-    leadStatus: {
-      type: "string",
-      label: "Lead Status",
-      description: "The status of the lead",
-      optional: true,
-    },
-  },
   methods: {
     async _makeRequest({
       $ = this,
