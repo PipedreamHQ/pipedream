3415b417a8762a9352820e1da46cc9a3a13770e3
Andrew Chuang <andrewjschuang@gmail.com>
2023-11-22 17:15:12 -0300

    bump versions

--- components/constant_contact/actions/update-contact/update-contact.mjs
+++ components/constant_contact/actions/update-contact/update-contact.mjs
@@ -4,7 +4,7 @@ import constantContact from "../../constant_contact.app.mjs";
 export default {
   key: "constant_contact-update-contact",
   name: "Update Contact",
-  version: "0.0.1",
+  version: "0.0.2",
   description: "Update an existing contact. [See the documentation](https://developer.constantcontact.com/api_reference/index.html#!/Contacts/putContact)",
   type: "action",
   props: {
