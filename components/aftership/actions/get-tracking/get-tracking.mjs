f0eca5dfc59fb95e7e0713ab656de704492f6f17
Luan Cazarine <luanhc@gmail.com>
2023-11-22 09:27:00 -0300

    New Components - aftership (#9002)
    * aftership init
    
    * [Components] AfterShip #8737
    Actions
      - Create Tracking
      - Update Tracking
      - Get Tracking
    
    * pnpm update

---
+++ components/aftership/actions/get-tracking/get-tracking.mjs
@@ -0,0 +1,23 @@
+import aftership from "../../aftership.app.mjs";
+
+export default {
+  key: "aftership-get-tracking",
+  name: "Get Tracking",
+  description: "Obtains an existing tracking system's data by ID. [See the documentation](https://www.aftership.com/docs/api/4/trackings/get-trackings)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    aftership,
+    trackingId: {
+      propDefinition: [
+        aftership,
+        "trackingId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.aftership.getTrackingById(this.trackingId);
+    $.export("$summary", `Successfully retrieved tracking data for ID ${this.trackingId}`);
+    return response;
+  },
+};
