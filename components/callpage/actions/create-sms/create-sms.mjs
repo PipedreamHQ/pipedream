d04ce2bbcf8e17a9fa31f4bca8a189efcda8967e
Luan Cazarine <luanhc@gmail.com>
2023-11-23 12:04:58 -0300

    New Components - callpage (#9042)
    * callpage init
    
    * [Components] CallPage #8707
    Sources
      - New Call Event
    
    Actions
      - Create SMS
    
    * pnpm update
    
    * fix date

---
+++ components/callpage/actions/create-sms/create-sms.mjs
@@ -0,0 +1,49 @@
+import callpage from "../../callpage.app.mjs";
+import { MESSAGE_OPTIONS } from "../../common/constants.mjs";
+
+export default {
+  key: "callpage-create-sms",
+  name: "Create SMS",
+  description: "Creates a new SMS in CallPage. [See the documentation](https://callpage.github.io/documentation-rest/)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    callpage,
+    widgetId: {
+      propDefinition: [
+        callpage,
+        "widgetId",
+      ],
+    },
+    messageId: {
+      type: "string",
+      label: "Message Id",
+      description: "Select a message identifier.",
+      options: MESSAGE_OPTIONS,
+    },
+    enabled: {
+      type: "boolean",
+      label: "Enabled",
+      description: "Indicates whether the message should be enabled or not.",
+      optional: true,
+    },
+    text: {
+      type: "string",
+      label: "SMS Text",
+      description: "The text of the SMS to send.",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.callpage.createSMS({
+      data: {
+        widget_id: this.widgetId,
+        message_id: this.messageId,
+        enbled: this.enabled || true,
+        text: this.text,
+      },
+    });
+
+    $.export("$summary", `Successfully created SMS with id ${response.data?.id}`);
+    return response;
+  },
+};
--- components/callpage/callpage.app.mjs
+++ components/callpage/callpage.app.mjs
@@ -1,11 +1,95 @@
+import { axios } from "@pipedream/platform";
+import { LIMIT } from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "callpage",
-  propDefinitions: {},
+  propDefinitions: {
+    widgetId: {
+      type: "string",
+      label: "Widget",
+      description: "The widget associated with the call or SMS.",
+      async options({ page }) {
+        const { data } = await this.listWidgets({
+          params: {
+            offset: LIMIT * page,
+            limit: LIMIT,
+          },
+        });
+        return data.map(({
+          id: value, description: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl(version = "v1") {
+      return `https://core.callpage.io/api/${version}/external`;
+    },
+    _getHeaders() {
+      return {
+        "Authorization": `${this.$auth.api_key}`,
+        "Accept": "application/json",
+      };
+    },
+    async _makeRequest({
+      $ = this, path, version, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl(version) + path,
+        headers: this._getHeaders(),
+        ...opts,
+      });
+    },
+    async createSMS(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/sms/create",
+        ...opts,
+      });
+    },
+    async listEvents(opts = {}) {
+      return this._makeRequest({
+        version: "v3",
+        path: "/calls/history",
+        ...opts,
+      });
+    },
+    async listWidgets(opts = {}) {
+      return this._makeRequest({
+        path: "/widgets/all",
+        ...opts,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null,
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.limit = LIMIT;
+        params.offset = LIMIT * page;
+        page++;
+
+        const { data } = await fn({
+          params,
+        });
+        for (const d of data) {
+          yield d.data;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data.length;
+
+      } while (hasMore);
     },
   },
 };
