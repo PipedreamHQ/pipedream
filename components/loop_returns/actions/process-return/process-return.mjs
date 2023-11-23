385ed964c757ca2b0ee4295b88fec23d7d88de17
Jorge Cortes <jacortesmahmud@gmail.com>
2023-11-23 09:42:44 -0500

    New Components - loop_returns (#8981)
    * loop_returns init
    
    * Added actions and sources
    
    * pnpm-lock.yaml
    
    * Fixed auth token header
    
    * Addressed issues pointed out by QA
    
    * Addressed improvment pointed out by QA

---
+++ components/loop_returns/actions/process-return/process-return.mjs
@@ -0,0 +1,42 @@
+import app from "../../loop_returns.app.mjs";
+
+export default {
+  key: "loop_returns-process-return",
+  name: "Process Return",
+  description: "Starts the processing of a return inside Loop. Return ID is a required prop to initiate the process. [See the documentation](https://docs.loopreturns.com/reference/processreturn)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    returnId: {
+      propDefinition: [
+        app,
+        "returnId",
+      ],
+    },
+  },
+  methods: {
+    processReturn({
+      returnId, ...args
+    } = {}) {
+      return this.app.post({
+        path: `/warehouse/return/${returnId}/process`,
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      processReturn,
+      returnId,
+    } = this;
+
+    const response = await processReturn({
+      $,
+      returnId,
+    });
+
+    $.export("$summary", `Successfully processed return with ID \`${returnId}\``);
+    return response;
+  },
+};
