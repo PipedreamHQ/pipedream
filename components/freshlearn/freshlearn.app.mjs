e6b889553239e4b4e2a44baaba24d92778317b69
michelle0927 <michelle0927@users.noreply.github.com>
2023-11-27 06:35:12 -0800

    FreshLearn - New Components (#9029)
    * freshlearn init
    
    * new components
    
    * pnpm-lock.yaml
    
    * add phone
    
    * Update optional props for freshlearn update member
    
    * Revert "Update optional props for freshlearn update member"
    
    This reverts commit 50e28e98566b1183b46a098c8f1155d0145c8955.
    
    ---------
    
    Co-authored-by: Hung Vu <vunguyenhung@outlook.com>

--- components/freshlearn/freshlearn.app.mjs
+++ components/freshlearn/freshlearn.app.mjs
@@ -1,11 +1,87 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "freshlearn",
-  propDefinitions: {},
+  propDefinitions: {
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email address of the member",
+    },
+    fullName: {
+      type: "string",
+      label: "Full Name",
+      description: "The full name of the member",
+    },
+    source: {
+      type: "string",
+      label: "Source",
+      description: "Zapier/FreshLearn/etc",
+    },
+    courseId: {
+      type: "string",
+      label: "Course ID",
+      description: "The ID of the course",
+    },
+    phone: {
+      type: "string",
+      label: "Phone",
+      description: "The phone number of the member",
+    },
+    city: {
+      type: "string",
+      label: "City",
+      description: "The city of the member",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.freshlearn.com/v1/integration";
+    },
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          "api-key": `${this._apiKey()}`,
+        },
+      });
+    },
+    listMembers(args = {}) {
+      return this._makeRequest({
+        path: "/member",
+        ...args,
+      });
+    },
+    createMember(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/member",
+        ...args,
+      });
+    },
+    updateMember(args = {}) {
+      return this._makeRequest({
+        method: "PUT",
+        path: "/member/update",
+        ...args,
+      });
+    },
+    enrollMemberInCourse(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/member/enroll",
+        ...args,
+      });
     },
   },
 };
