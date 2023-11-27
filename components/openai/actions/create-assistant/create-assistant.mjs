b7d8a8b04a6384b49b16055e6ff17830a05c283d
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2023-11-27 11:04:15 -0300

    #8974 OpenAI Tools adjustment (#9087)
    * Modifying tools prop for JSON string array
    
    * Version bumps
    
    * Source version bumps
    
    * Installing modules used by openai

--- components/openai/actions/create-assistant/create-assistant.mjs
+++ components/openai/actions/create-assistant/create-assistant.mjs
@@ -1,10 +1,11 @@
+import { parseToolsArray } from "../../common/helpers.mjs";
 import openai from "../../openai.app.mjs";
 
 export default {
   key: "openai-create-assistant",
   name: "Create Assistant",
   description: "Creates an assistant with a model and instructions. [See the docs here](https://platform.openai.com/docs/api-reference/assistants/createAssistant)",
-  version: "0.0.1",
+  version: "0.1.0",
   type: "action",
   props: {
     openai,
@@ -58,9 +59,7 @@ export default {
       name: this.name,
       description: this.description,
       instructions: this.instructions,
-      tools: (this.tools || []).map((tool) => ({
-        type: tool,
-      })),
+      tools: parseToolsArray(this.tools),
       file_ids: this.file_ids,
       metadata: this.metadata,
     });
--- components/openai/actions/retrieve-run/retrieve-run.mjs
+++ components/openai/actions/retrieve-run/retrieve-run.mjs
@@ -4,7 +4,7 @@ export default {
   key: "openai-retrieve-run",
   name: "Retrieve Run",
   description: "Retrieves a specific run within a thread. [See the documentation](https://platform.openai.com/docs/api-reference)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     openai,
