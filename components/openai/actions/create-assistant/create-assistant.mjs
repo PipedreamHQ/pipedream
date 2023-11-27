212ff1e318831d640590cf5aa5bd6b29af507548
Luan Cazarine <luanhc@gmail.com>
2023-11-27 13:56:30 -0300

    bump versions

--- components/openai/actions/create-assistant/create-assistant.mjs
+++ components/openai/actions/create-assistant/create-assistant.mjs
@@ -5,7 +5,7 @@ export default {
   key: "openai-create-assistant",
   name: "Create Assistant",
   description: "Creates an assistant with a model and instructions. [See the docs here](https://platform.openai.com/docs/api-reference/assistants/createAssistant)",
-  version: "0.1.0",
+  version: "0.1.1",
   type: "action",
   props: {
     openai,
--- components/openai/actions/retrieve-run-step/retrieve-run-step.mjs
+++ components/openai/actions/retrieve-run-step/retrieve-run-step.mjs
@@ -4,7 +4,7 @@ export default {
   key: "openai-retrieve-run-step",
   name: "Retrieve Run Step",
   description: "Retrieve a specific run step in a thread. [See the documentation](https://platform.openai.com/docs/api-reference)",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "action",
   props: {
     openai,
