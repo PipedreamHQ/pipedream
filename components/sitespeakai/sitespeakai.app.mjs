1259f537a04d8d9629c008b8177ee9d9ce9438ef
michelle0927 <michellelbergero@hotmail.com>
2023-11-28 14:35:35 -0500

    new components

--- components/sitespeakai/sitespeakai.app.mjs
+++ components/sitespeakai/sitespeakai.app.mjs
@@ -1,11 +1,66 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "sitespeakai",
-  propDefinitions: {},
+  propDefinitions: {
+    chatbotId: {
+      type: "string",
+      label: "Chatbot",
+      description: "The ID of your chatbot. You can find the ID on the settings page for your chatbot.",
+      async options() {
+        const chatbots = await this.listChatbots();
+        return chatbots?.map(({
+          id: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.sitespeak.ai/v1";
+    },
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.api_token}`,
+      };
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      });
+    },
+    listChatbots(args = {}) {
+      return this._makeRequest({
+        path: "/me/chatbots",
+        ...args,
+      });
+    },
+    listLeads({
+      chatbotId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/${chatbotId}/leads`,
+        ...args,
+      });
+    },
+    sendQuery({
+      chatbotId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/${chatbotId}/query`,
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
