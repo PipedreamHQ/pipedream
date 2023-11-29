9e6714bf4a4fada74462db8d13b8d6955329a600
michelle0927 <michellelbergero@hotmail.com>
2023-11-29 12:41:59 -0500

    wip

---
+++ components/zoho_survey/sources/new-survey-response/new-survey-response.mjs
@@ -0,0 +1,20 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "zoho_survey-new-survey-response",
+  name: "New Survey Response (Instant)",
+  description: "Emit new event when a new survey response is received in Zoho Surveys.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEvent() {
+      return "response_completed";
+    },
+    generateMeta() {
+      return {};
+    },
+  },
+};
