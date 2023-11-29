0d08cc371f61ca1703a99bb5c200f1e1b6da5b47
Jorge Cortes <jacortesmahmud@gmail.com>
2023-11-29 08:45:53 -0500

    New Components - zest (#9130)
    * zest init
    
    * Added tested sources componets only
    
    * pnpm-lock.yaml

--- components/zest/zest.app.mjs
+++ components/zest/zest.app.mjs
@@ -1,11 +1,58 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+import utils from "./common/utils.mjs";
+
 export default {
   type: "app",
   app: "zest",
-  propDefinitions: {},
+  propDefinitions: {
+    campaignId: {
+      type: "string",
+      label: "Campaign ID",
+      description: "The identifier of the campaign to create the gift within",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getApiKey() {
+      return this.$auth.api_key;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "x-api-key": this.getApiKey(),
+      };
+    },
+    getUrl(path) {
+      return `${constants.BASE_URL}${constants.VERSION_PATH}${path}`;
+    },
+    _makeRequest({
+      $ = this, path, headers, data, ...args
+    }) {
+      const {
+        getHeaders,
+        getUrl,
+      } = this;
+
+      const config = {
+        ...args,
+        url: getUrl(path),
+        data: utils.filterProps(data),
+        headers: getHeaders(headers),
+      };
+
+      return axios($, config);
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "post",
+        ...args,
+      });
+    },
+    delete(args = {}) {
+      return this._makeRequest({
+        method: "delete",
+        ...args,
+      });
     },
   },
 };
