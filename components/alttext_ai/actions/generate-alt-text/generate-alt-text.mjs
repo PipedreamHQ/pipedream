b4fd4071b4877cf64840e638bc3befbac8a8e4da
GTFalcao <gtfalcao96@gmail.com>
2023-12-04 02:45:06 -0300

    Generate Alt Text props and adjustments

--- components/alttext_ai/actions/generate-alt-text/generate-alt-text.mjs
+++ components/alttext_ai/actions/generate-alt-text/generate-alt-text.mjs
@@ -1,33 +1,92 @@
-import alttext_ai from "../../alttext_ai.app.mjs";
+import alttextAi from "../../alttext_ai.app.mjs";
+import fs from "fs";
+import { LANGUAGE_OPTIONS } from "../../commons/constants.mjs";
 
 export default {
   key: "alttext_ai-generate-alt-text",
   name: "Generate Alt Text",
-  description: "Generates a descriptive alt text for a given image. Optionally specify the language for the alt text. [See the documentation](https://api.alttext.org/v1/docs)",
+  description:
+    "Generates a descriptive alt text for a given image. [See the documentation](https://alttext.ai/apidocs#tag/Images/operation/create-image)",
   version: "0.0.1",
   type: "action",
   props: {
-    alttext_ai,
+    alttextAi,
     imageData: {
-      propDefinition: [
-        alttext_ai,
-        "imageData",
-      ],
+      type: "string",
+      label: "Image Data",
+      description:
+        "The image data in base64 format. Only one of `Image Data`, `Image File Path` or `Image URL` should be specified.",
+      optional: true,
+    },
+    imageUrl: {
+      type: "string",
+      label: "Image URL",
+      description:
+        "The public URL to an image. Only one of `Image URL`, `Image Data` or `Image File Path` should be specified.",
+      optional: true,
+    },
+    imageFilePath: {
+      type: "string",
+      label: "Image File Path",
+      description:
+        "The path to an image file in the `/tmp` directory. [See the documentation on working with files](https://pipedream.com/docs/code/nodejs/working-with-files/#the-tmp-directory). Only one of `Image File Path`, `Image URL` or `Image Data` should be specified.",
+      optional: true,
+    },
+    keywords: {
+      type: "string[]",
+      label: "Keywords",
+      description:
+        "Keywords / phrases to be considered when generating the alt text. Keywords must be in English, even if requesting alt text in another language. Max 6 items.",
+      optional: true,
+    },
+    negativeKeywords: {
+      type: "string[]",
+      label: "Negative Keywords",
+      description:
+        "Negative keywords / phrases to be removed from any generated alt text. Negative keywords must be in English, even if requesting alt text in another language. Max 6 items.",
+      optional: true,
+    },
+    keywordSource: {
+      type: "string",
+      label: "Keyword Source",
+      description:
+        "Text to use as the source of keywords for the alt text. Will only be used if `Keywords` is not provided. This text might be a product description, or a paragraph that describes the context of the images, etc. Max 1024 characters.",
+      optional: true,
     },
     language: {
-      propDefinition: [
-        alttext_ai,
-        "language",
-      ],
+      type: "string",
+      label: "Language",
+      description: "The language for the generated alt text.",
+      options: LANGUAGE_OPTIONS,
+      default: "en",
     },
   },
   async run({ $ }) {
-    const altTextResult = await this.alttext_ai.generateAltText({
-      imageData: this.imageData,
-      language: this.language,
+    const response = await this.alttextAi.generateAltText({
+      $,
+      data: {
+        image: {
+          url: this.imageUrl,
+          raw:
+            this.imageData ??
+            (this.imageFilePath &&
+              fs.readFileSync(
+                this.imageFilePath.includes("tmp/")
+                  ? this.imageFilePath
+                  : `/tmp/${this.imageFilePath}`,
+                {
+                  encoding: "base64",
+                },
+              )),
+        },
+        keywords: this.keywords,
+        negative_keywords: this.negativeKeywords,
+        keyword_source: this.keywordSource,
+        lang: this.language,
+      },
     });
 
-    $.export("$summary", `Generated alt text successfully: ${altTextResult.alt_text}`);
-    return altTextResult;
+    $.export("$summary", "Generated alt text successfully");
+    return response;
   },
 };
--- components/alttext_ai/alttext_ai.app.mjs
+++ components/alttext_ai/alttext_ai.app.mjs
@@ -3,39 +3,10 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "alttext_ai",
-  propDefinitions: {
-    imageData: {
-      type: "string",
-      label: "Image Data",
-      description: "The image data in base64 encoding or as a URL to the image.",
-      optional: false,
-    },
-    language: {
-      type: "string",
-      label: "Language",
-      description: "The language to use for the alt text. Defaults to English (en).",
-      options: [
-        {
-          label: "English",
-          value: "en",
-        },
-        {
-          label: "Spanish",
-          value: "es",
-        },
-        {
-          label: "French",
-          value: "fr",
-        },
-        // Add more languages as needed
-      ],
-      optional: true,
-      default: "en",
-    },
-  },
+  propDefinitions: {},
   methods: {
     _baseUrl() {
-      return "https://api.alttext.org/v1";
+      return "https://alttext.ai/api/v1";
     },
     async _makeRequest({
       $ = this,
@@ -48,24 +19,16 @@ export default {
         url: this._baseUrl() + path,
         headers: {
           ...headers,
-          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
-          "Content-Type": "application/json",
+          "X-API-Key": `${this.$auth.api_key}`,
         },
       });
     },
-    async generateAltText({
-      imageData, language,
-    }) {
+    async generateAltText(args) {
       return this._makeRequest({
-        path: "/generate",
-        data: {
-          image: imageData,
-          language,
-        },
+        path: "/images",
+        method: "POST",
+        ...args,
       });
-    },
-    authKeys() {
-      console.log(Object.keys(this.$auth));
     },
   },
 };
