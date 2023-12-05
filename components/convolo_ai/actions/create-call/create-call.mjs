2007c4b4438b0ff0d1cc4ddfb3d077e64aab7622
Luan Cazarine <luanhc@gmail.com>
2023-12-05 13:38:21 -0300

    [Components] convoloai #9190 Actions   - Create Call

--- components/convolo_ai/actions/create-call/create-call.mjs
+++ components/convolo_ai/actions/create-call/create-call.mjs
@@ -1,34 +1,28 @@
-import convolo_ai from "../../convolo_ai.app.mjs";
-import { axios } from "@pipedream/platform";
+import convoloAi from "../../convolo_ai.app.mjs";
 
 export default {
   key: "convolo_ai-create-call",
   name: "Create Call",
   description: "Initiates a phone call to a lead and establishes the connection with the first available agent. [See the documentation](https://help.convolo.ai/)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
-    convolo_ai,
-    agentIdentifier: {
-      propDefinition: [
-        convolo_ai,
-        "agentIdentifier",
-      ],
-    },
-    leadPhoneNumber: {
-      propDefinition: [
-        convolo_ai,
-        "leadPhoneNumber",
-      ],
+    convoloAi,
+    lcNumber: {
+      type: "string",
+      label: "LC Number",
+      description: "The phone number to create the call.",
     },
   },
   async run({ $ }) {
-    const response = await this.convolo_ai.initiatePhoneCall({
-      agentIdentifier: this.agentIdentifier,
-      leadPhoneNumber: this.leadPhoneNumber,
+    const response = await this.convoloAi.createCall({
+      $,
+      data: {
+        lc_number: this.lcNumber,
+      },
     });
 
-    $.export("$summary", `Successfully initiated a call to lead with phone number ${this.leadPhoneNumber}`);
+    $.export("$summary", `Successfully created a call with ID: ${response.callId}`);
     return response;
   },
 };
--- components/convolo_ai/convolo_ai.app.mjs
+++ components/convolo_ai/convolo_ai.app.mjs
@@ -3,89 +3,34 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "convolo_ai",
-  propDefinitions: {
-    endOfCallTrigger: {
-      type: "boolean",
-      label: "End of Call Trigger",
-      description: "Trigger when a call ends and gets logged in Convolo dashboard",
-    },
-    agentIdentifier: {
-      type: "string",
-      label: "Agent Identifier",
-      description: "The identifier of the agent to initiate a call with",
-    },
-    leadPhoneNumber: {
-      type: "string",
-      label: "Lead Phone Number",
-      description: "The phone number of the lead to call",
-    },
-    contactDetails: {
-      type: "string[]",
-      label: "Contact Details",
-      description: "An array of objects containing the details of the contact to incorporate into a power dialer project",
-    },
-    powerDialerProjectIdentifier: {
-      type: "string",
-      label: "Power Dialer Project Identifier",
-      description: "The identifier of the power dialer project to add a new contact to",
-    },
-  },
   methods: {
     _baseUrl() {
-      return "https://api.convolo.ai";
+      return "https://app.convolo.ai/rest/v1/ext";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path,
-        headers,
-        ...otherOpts
-      } = opts;
+    _authData() {
+      return {
+        widget_key: `${this.$auth.widget_key}`,
+        api_key: `${this.$auth.api_key}`,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, data, ...opts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
         url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        data: {
+          ...data,
+          ...this._authData(),
         },
+        ...opts,
       });
     },
-    async logCallEnd({ endOfCallTrigger }) {
-      if (endOfCallTrigger) {
-        // Logic to log the end of the call
-        // Since the API documentation link is not available, the exact endpoint and method are not defined
-        // This is a placeholder for where the API request would be made
-      }
-    },
-    async initiatePhoneCall({
-      agentIdentifier, leadPhoneNumber,
-    }) {
+    async createCall(opts = {}) {
       return this._makeRequest({
         method: "POST",
-        path: "/initiate-call", // Placeholder path, replace with actual API endpoint
-        data: {
-          agentIdentifier,
-          leadPhoneNumber,
-        },
+        path: "/add_call_api", // Placeholder path, replace with actual API endpoint
+        ...opts,
       });
-    },
-    async addContactToPowerDialer({
-      contactDetails, powerDialerProjectIdentifier,
-    }) {
-      const parsedContactDetails = contactDetails.map(JSON.parse);
-      return this._makeRequest({
-        method: "POST",
-        path: "/add-contact", // Placeholder path, replace with actual API endpoint
-        data: {
-          contactDetails: parsedContactDetails,
-          powerDialerProjectIdentifier,
-        },
-      });
-    },
-    authKeys() {
-      console.log(Object.keys(this.$auth));
     },
   },
 };
