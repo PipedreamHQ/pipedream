dcb273b313e44919e55db5c958aed3fb0ec9aec4
Luan Cazarine <luanhc@gmail.com>
2023-12-07 09:44:14 -0300

    New Components - convolo_ai (#9195)
    * convolo_ai init
    
    * [Components] convoloai #9190
    Actions
      - Create Call
    
    * pnpm update

--- components/convolo_ai/convolo_ai.app.mjs
+++ components/convolo_ai/convolo_ai.app.mjs
@@ -1,11 +1,36 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "convolo_ai",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://app.convolo.ai/rest/v1/ext";
+    },
+    _authData() {
+      return {
+        widget_key: `${this.$auth.widget_key}`,
+        api_key: `${this.$auth.api_key}`,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, data, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        data: {
+          ...data,
+          ...this._authData(),
+        },
+        ...opts,
+      });
+    },
+    async createCall(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/add_call_api", // Placeholder path, replace with actual API endpoint
+        ...opts,
+      });
     },
   },
 };
