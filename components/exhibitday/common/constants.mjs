f89204c06b8fe26280f7cfdf3f6d2a22076362e8
michelle0927 <michelle0927@users.noreply.github.com>
2023-12-07 10:06:41 -0800

    ExhibitDay - New Components (#9192)
    * ai generated
    
    * new components
    
    * pnpm-lock.yaml
    
    * remove console.log
    
    * fix for is_completed prop
    
    * add ConfigurationError to taskSectionId prop
    
    * eslint update

---
+++ components/exhibitday/common/constants.mjs
@@ -0,0 +1,34 @@
+const EVENT_FORMATS = [
+  {
+    value: 1,
+    label: "In-Person",
+  },
+  {
+    value: 2,
+    label: "Virtual",
+  },
+  {
+    value: 3,
+    label: "Hybrid",
+  },
+];
+
+const PARTICIPATION_TYPES = [
+  {
+    value: 1,
+    label: "Committed",
+  },
+  {
+    value: 2,
+    label: "Considering",
+  },
+  {
+    value: 3,
+    label: "Not Going",
+  },
+];
+
+export default {
+  EVENT_FORMATS,
+  PARTICIPATION_TYPES,
+};
