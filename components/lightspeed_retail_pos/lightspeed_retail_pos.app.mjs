284fe9e521b49b8f8c2ddbcfee08f22733f55bee
michelle0927 <michellelbergero@hotmail.com>
2023-12-08 13:16:49 -0500

    new components

--- components/lightspeed_retail_pos/lightspeed_retail_pos.app.mjs
+++ components/lightspeed_retail_pos/lightspeed_retail_pos.app.mjs
@@ -1,4 +1,5 @@
 import { axios } from "@pipedream/platform";
+const DEFAULT_LIMIT = 100;
 
 export default {
   type: "app",
@@ -26,27 +27,48 @@ export default {
       async options({
         accountId, prevContext,
       }) {
-        const args = {
+        return await this.getPropOptions({
           accountId,
-        };
-        if (prevContext?.next) {
-          args.url = prevContext.next;
-        }
-        const response = await this.listCustomerTypes(args);
-        const attributes = response["@attributes"];
-        const types = response.CustomerType;
-        const options = types?.map(({
-          customerTypeID: value, name: label,
-        }) => ({
-          value,
-          label,
-        })) || [];
-        return {
-          options,
-          context: {
-            next: attributes?.next,
-          },
-        };
+          resourceFn: this.listCustomerTypes,
+          resourceKey: "CustomerType",
+          valueKey: "customerTypeID",
+          labelKey: "name",
+          next: prevContext?.next,
+        });
+      },
+    },
+    itemId: {
+      type: "string",
+      label: "Item",
+      description: "Identifier of an inventory item",
+      async options({
+        accountId, prevContext,
+      }) {
+        return await this.getPropOptions({
+          accountId,
+          resourceFn: this.listItems,
+          resourceKey: "Item",
+          valueKey: "itemID",
+          labelKey: "description",
+          next: prevContext?.next,
+        });
+      },
+    },
+    shopId: {
+      type: "string",
+      label: "Shop",
+      description: "Identifier of a shop",
+      async options({
+        accountId, prevContext,
+      }) {
+        return await this.getPropOptions({
+          accountId,
+          resourceFn: this.listShops,
+          resourceKey: "Shop",
+          valueKey: "shopID",
+          labelKey: "name",
+          next: prevContext?.next,
+        });
       },
     },
   },
@@ -70,9 +92,52 @@ export default {
         ...args,
       });
     },
+    async getPropOptions({
+      accountId, resourceFn, resourceKey, valueKey, labelKey, next,
+    }) {
+      const args = {
+        accountId,
+      };
+      if (next) {
+        args.url = next;
+      }
+      const response = await resourceFn(args);
+      const attributes = response["@attributes"];
+      const items = Array.isArray(response[resourceKey])
+        ? response[resourceKey]
+        : [
+          response[resourceKey],
+        ];
+      const options = items?.map((item) => ({
+        value: item[valueKey],
+        label: item[labelKey],
+      })) || [];
+      return {
+        options,
+        context: {
+          next: attributes?.next,
+        },
+      };
+    },
     getAccount(args = {}) {
       return this._makeRequest({
         path: "/Account.json",
+        ...args,
+      });
+    },
+    getItem({
+      accountId, itemId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Item/${itemId}.json`,
+        ...args,
+      });
+    },
+    getInventoryLog({
+      accountId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/InventoryLog.json`,
         ...args,
       });
     },
@@ -84,6 +149,30 @@ export default {
         ...args,
       });
     },
+    listItems({
+      accountId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Item.json`,
+        ...args,
+      });
+    },
+    listShops({
+      accountId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Shop.json`,
+        ...args,
+      });
+    },
+    listSales({
+      accountId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Sale.json`,
+        ...args,
+      });
+    },
     createCustomer({
       accountId, ...args
     }) {
@@ -92,6 +181,61 @@ export default {
         method: "POST",
         ...args,
       });
+    },
+    createEmployee({
+      accountId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Employee.json`,
+        method: "POST",
+        ...args,
+      });
+    },
+    updateItem({
+      accountId, itemId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/Account/${accountId}/Item/${itemId}.json`,
+        method: "PUT",
+        ...args,
+      });
+    },
+    async *paginate({
+      resourceFn,
+      args,
+      resourceKey,
+      limit,
+    }) {
+      args = {
+        ...args,
+        params: {
+          ...args.params,
+          limit: DEFAULT_LIMIT,
+        },
+      };
+      let total = 0;
+      let count = 0;
+      do {
+        const response = await resourceFn(args);
+        const attributes = response["@attributes"];
+        if (!response[resourceKey]) {
+          return;
+        }
+        const items = Array.isArray(response[resourceKey])
+          ? response[resourceKey]
+          : [
+            response[resourceKey],
+          ];
+        for (const item of items) {
+          yield item;
+          count++;
+          if (limit && count >= limit) {
+            return;
+          }
+        }
+        args.url = attributes?.next;
+        total = items?.length;
+      } while (total === args.params.limit);
     },
   },
 };
