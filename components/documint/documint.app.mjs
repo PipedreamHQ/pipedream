c39950465adf5bae594b6ceffc7ab0d0c323e00c
Lucas Caresia <lucascarezia@gmail.com>
2023-12-08 08:26:25 -0300

    [Components] documint #9160 (#9186)
    * Added actions
    
    * Bump package.json version
    
    * Update components/documint/documint.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * list-templates action, bug fix, eslint
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

--- components/documint/documint.app.mjs
+++ components/documint/documint.app.mjs
@@ -1,11 +1,62 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "documint",
-  propDefinitions: {},
+  propDefinitions: {
+    templateId: {
+      label: "Template ID",
+      description: "The template ID",
+      type: "string",
+      async options({ page }) {
+        const { data: templates } = await this.getTemplates({
+          params: {
+            page: page + 1,
+          },
+        });
+
+        return templates.map(({
+          id, name,
+        }) => ({
+          label: name,
+          value: id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _apiUrl() {
+      return "https://api.documint.me/1";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        ...args,
+        headers: {
+          ...args.headers,
+          "api_key": this._apiKey(),
+        },
+      });
+    },
+    async createDocument({
+      templateId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/templates/${templateId}/content`,
+        method: "post",
+        ...args,
+      });
+    },
+    async getTemplates(args = {}) {
+      return this._makeRequest({
+        path: "/templates",
+        ...args,
+      });
     },
   },
 };
