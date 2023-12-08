1fbf420b611d5bc1e4f4367fc5508c86b203c95f
Jorge Cortes <jacortesmahmud@gmail.com>
2023-12-08 18:13:12 -0500

    New Components - userflow (#9170)
    * userflow init
    
    * Added new tested action components and sources
    
    * Increased version in package.json
    
    * Addressed improvements suggested by QA
    
    * Fixed version and removed utils file no longer needed

--- components/userflow/userflow.app.mjs
+++ components/userflow/userflow.app.mjs
@@ -1,11 +1,120 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "userflow",
-  propDefinitions: {},
+  propDefinitions: {
+    userId: {
+      type: "string",
+      label: "User ID",
+      description: "The unique identifier for the user.",
+      async options({ prevContext }) {
+        const {
+          hasMore,
+          lastId,
+        } = prevContext;
+
+        if (hasMore === false) {
+          return [];
+        }
+
+        const {
+          data,
+          has_more: hasMoreData,
+        } = await this.listUsers({
+          params: {
+            starting_after: lastId,
+          },
+        });
+
+        const options = data.map(({
+          id: value, attributes: { name: label },
+        }) => ({
+          label,
+          value,
+        }));
+
+        return {
+          options,
+          context: {
+            hasMore: hasMoreData,
+            lastId: data[data.length - 1]?.id,
+          },
+        };
+      },
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The user's email address.",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `${constants.BASE_URL}${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Authorization": `Bearer ${this.$auth.api_key}`,
+        "Userflow-Version": constants.API_VERSION,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      const config = {
+        ...args,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      };
+      return axios($, config);
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "post",
+        ...args,
+      });
+    },
+    delete(args = {}) {
+      return this._makeRequest({
+        method: "delete",
+        ...args,
+      });
+    },
+    getUser({
+      userId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/users/${userId}`,
+        ...args,
+      });
+    },
+    listUsers(args = {}) {
+      return this._makeRequest({
+        path: "/users",
+        ...args,
+      });
+    },
+    listContent(args = {}) {
+      return this._makeRequest({
+        path: "/content",
+        ...args,
+      });
+    },
+    listAttributeDefinitions(args = {}) {
+      return this._makeRequest({
+        path: "/attribute_definitions",
+        ...args,
+      });
+    },
+    listGroups(args = {}) {
+      return this._makeRequest({
+        path: "/groups",
+        ...args,
+      });
     },
   },
 };
