f35e198d302ba309c707b591a90f2abe9a4ba822
GTFalcao <gtfalcao96@gmail.com>
2023-12-12 20:15:54 -0300

    File Upload adjustments

--- components/file_store/actions/upload-file/upload-file.mjs
+++ components/file_store/actions/upload-file/upload-file.mjs
@@ -1,84 +1,50 @@
-import fileStore from "../../file_store.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+import app from "../../file_store.app.mjs";
 
 export default {
   key: "file_store-upload-file",
   name: "Upload File",
-  description: "Uploads a file to the File Store. [See the documentation](https://pipedream.com/docs/projects/file-stores/reference/)",
+  description: "Uploads a file to the File Store. [See the documentation](https://pipedream.com/docs/projects/file-stores/reference/#file-fromfile-localfilepath-contenttype)",
   version: "0.0.1",
   type: "action",
   props: {
-    fileStore,
-    directory: {
+    app,
+    filePath: {
       propDefinition: [
-        fileStore,
-        "directory",
+        app,
+        "filePath",
       ],
-    },
-    file: {
-      propDefinition: [
-        fileStore,
-        "file",
-        (c) => ({
-          directory: c.directory,
-        }),
-      ],
+      description: "The destination file path to upload to.",
     },
     localFilePath: {
       type: "string",
       label: "Local File Path",
-      description: "The local path of the file to upload. Only `/tmp` is writable in workflow environments.",
-    },
-    contentType: {
-      type: "string",
-      label: "Content Type",
-      description: "The content type of the file (e.g., `image/png`).",
-      optional: true,
-    },
-    contentLength: {
-      type: "integer",
-      label: "Content Length",
-      description: "The content length of the file in bytes. Pass this if possible to avoid writing the entire file to `/tmp` before uploading.",
+      description: "The path to a file in the `/tmp` directory to upload to the File Store. You should specify either a Local File Path or a File URL.",
       optional: true,
     },
     url: {
       type: "string",
       label: "File URL",
-      description: "The URL to read the file from. Use this if you want to upload a file from a URL.",
+      description: "The URL to a file to upload to the File Store. You should specify either a File URL or a Local File Path.",
       optional: true,
     },
   },
   async run({ $ }) {
-    let fileUrl;
+    const file = await $.files.open(this.filePath);
 
-    // Check if a URL is provided and use the fromUrl method
+    let response;
+
     if (this.url) {
-      fileUrl = await this.fileStore.uploadFileFromUrl({
-        url: this.url,
-        fileName: this.file,
-      });
+      response = await file.fromUrl(this.url);
     } else if (this.localFilePath) {
-      // If localFilePath is provided, upload the file from the local /tmp directory
-      if (this.contentLength) {
-        // If contentLength is provided, create a write stream
-        const writeStream = await this.fileStore.createWriteStream({
-          fileName: this.file,
-          contentType: this.contentType,
-          contentLength: this.contentLength,
-        });
-        fileUrl = writeStream.url;
-      } else {
-        // Otherwise, use the fromFile method
-        fileUrl = await this.fileStore.uploadFileFromPath({
-          localFilePath: this.localFilePath,
-          fileName: this.file,
-          contentType: this.contentType,
-        });
-      }
+      response = await file.fromFile(this.localFilePath.includes("tmp/")
+        ? this.localFilePath
+        : `/tmp/${this.localFilePath}`);
     } else {
-      throw new Error("You must provide either a Local File Path or a File URL.");
+      throw new ConfigurationError("You must provide either a Local File Path or a File URL.");
     }
 
-    $.export("$summary", `Uploaded file successfully: ${fileUrl}`);
-    return fileUrl;
+    $.export("$summary", `Successfully uploaded file to "${this.filePath}"`);
+    return response;
   },
 };
