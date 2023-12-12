aeae6e7e4e2c8617c46104b992eade8aecf1e810
GTFalcao <gtfalcao96@gmail.com>
2023-12-12 20:17:04 -0300

    App and package adjustments

--- components/file_store/file_store.app.mjs
+++ components/file_store/file_store.app.mjs
@@ -1,5 +1,3 @@
-import { axios } from "@pipedream/platform";
-
 export default {
   type: "app",
   app: "file_store",
@@ -15,102 +13,6 @@ export default {
       label: "File Path",
       description: "The path to the file to create a URL for.",
     },
-    file: {
-      type: "string",
-      label: "File",
-      description: "The name of the file to perform operations on.",
-      async options({ directory }) {
-        const files = await this.listFiles({
-          path: directory || "",
-        });
-        return files
-          .filter((file) => file.isFile())
-          .map((file) => ({
-            label: file.name,
-            value: file.path,
-          }));
-      },
-    },
   },
-  methods: {
-    _baseUrl() {
-      return "https://api.pipedream.com/v1";
-    },
-    async _makeRequest({
-      $ = this,
-      path,
-      headers,
-      ...otherOpts
-    }) {
-      return axios($, {
-        ...otherOpts,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          Authorization: `Bearer ${this.$auth.api_token}`,
-        },
-      });
-    },
-    async listFiles({
-      path = "", page = 0,
-    }) {
-      const files = await this._makeRequest({
-        path: "/files/dir",
-        params: {
-          path,
-          page,
-        },
-      });
-      return files;
-    },
-    async uploadFileFromPath({
-      localFilePath, fileName, contentType,
-    }) {
-      const file = await this._makeRequest({
-        method: "POST",
-        path: `/files/open/${encodeURIComponent(fileName)}/fromFile`,
-        data: {
-          localFilePath,
-          contentType,
-        },
-      });
-      return file.url;
-    },
-    async createWriteStream({
-      fileName, contentType, contentLength,
-    }) {
-      const writeStream = await this._makeRequest({
-        method: "POST",
-        path: `/files/open/${encodeURIComponent(fileName)}/createWriteStream`,
-        data: {
-          contentType,
-          contentLength,
-        },
-      });
-      return writeStream;
-    },
-    async downloadFile({
-      fileName, localPath,
-    }) {
-      await this._makeRequest({
-        method: "GET",
-        path: `/files/open/${encodeURIComponent(fileName)}/toFile`,
-        params: {
-          path: localPath,
-        },
-      });
-    },
-    async uploadFileFromUrl({
-      url, fileName,
-    }) {
-      const file = await this._makeRequest({
-        method: "POST",
-        path: `/files/open/${encodeURIComponent(fileName)}/fromUrl`,
-        data: {
-          url,
-        },
-      });
-      return file.url;
-    },
-  },
+  methods: {},
 };
