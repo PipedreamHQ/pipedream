0ff8bd872e9b347a11436ba43e09b7bf41b59488
Luan Cazarine <luanhc@gmail.com>
2023-12-15 18:25:43 -0300

    [Components] zoho_forms #9284 Sources  - New Response Received (Instant)
    Actions
     - List Forms
     - Get Form Fields

--- components/zoho_forms/zoho_forms.app.mjs
+++ components/zoho_forms/zoho_forms.app.mjs
@@ -4,76 +4,86 @@ export default {
   type: "app",
   app: "zoho_forms",
   propDefinitions: {
-    base_url: {
+    formId: {
       type: "string",
-      label: "Base URL",
-      description: "The base URL of your Creator account. E.g., 'creator.zoho.com' for Zoho's US DC, 'creator.zoho.eu' for Zoho's EU DC.",
+      label: "Form Id",
+      description: "The id of the form.",
+      async options() {
+        const { forms } = await this.listForms();
+
+        return forms.map(({
+          form_id: value, display_name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
     },
-    account_owner_name: {
+    formLinkName: {
       type: "string",
-      label: "Account Owner Name",
-      description: "The username of the Creator account's owner.",
-    },
-    app_link_name: {
-      type: "string",
-      label: "Application Link Name",
-      description: "The link name of the application from which you want to fetch the form meta information.",
-    },
-    environment: {
-      type: "string",
-      label: "Environment",
-      description: "The environment stage (development/stage/production). By default, 'production' is considered.",
-      options: [
-        {
-          label: "Development",
-          value: "development",
-        },
-        {
-          label: "Stage",
-          value: "stage",
-        },
-        {
-          label: "Production",
-          value: "production",
-        },
-      ],
-      optional: true,
-      default: "production",
+      label: "Form Link Name",
+      description: "The link name of the form.",
+      async options() {
+        const { forms } = await this.listForms();
+
+        return forms.map(({
+          link_name: value, display_name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
     },
   },
   methods: {
     _baseUrl() {
-      return `https://${this.base_url}`;
+      return "https://forms.zoho.com/api";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this, method = "GET", path, headers, ...otherOpts
-      } = opts;
-      return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          Authorization: `Zoho-oauthtoken ${this.$auth.oauth_access_token}`,
-        },
-      });
+    _headers() {
+      return {
+        Authorization: `Zoho-oauthtoken ${this.$auth.oauth_access_token}`,
+      };
     },
-    async getForms({
-      account_owner_name, app_link_name, environment,
+    _params(params) {
+      return {
+        ...params,
+        "zf_service": "Pipedream",
+      };
+    },
+    _makeRequest({
+      $ = this, path, params, ...opts
     }) {
-      const path = `/api/v2.1/${account_owner_name}/${app_link_name}/forms`;
-      const headers = {};
-      if (environment && environment !== "production") {
-        headers["X-com-zoho-environment"] = environment;
-      }
-      return this._makeRequest({
-        path,
-        headers,
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        params: this._params(params),
+        ...opts,
       });
     },
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getFormFields(opts = {}) {
+      return this._makeRequest({
+        path: "/resthooks/fields",
+        ...opts,
+      });
+    },
+    listForms() {
+      return this._makeRequest({
+        path: "/zforms",
+      });
+    },
+    createHook(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/resthooks",
+        ...opts,
+      });
+    },
+    deleteHook(opts = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: "/resthooks",
+        ...opts,
+      });
     },
   },
 };
