2c3b86551ca376719eb62e496d0e39624c7c8753
Luan Cazarine <luanhc@gmail.com>
2023-12-18 12:20:16 -0300

    New Components - whop (#9301)
    * whop init
    
    * [Components] whop #9271
    Sources
     - New Membership Validated (Instant)
     - New Payment Succeeded (Instant)
     - New Payment Failed (Instant)
    
    Actions
     - Create Checkout Session
     - Create Promo Code
     - Terminate Membership
    
    * pnpm update
    
    * Fix event summary
    
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>
    
    * List all membership instead of only active ones and remove redundant params
    
    ---------
    
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

---
+++ components/whop/actions/terminate-membership/terminate-membership.mjs
@@ -0,0 +1,26 @@
+import whop from "../../whop.app.mjs";
+
+export default {
+  key: "whop-terminate-membership",
+  name: "Terminate Membership",
+  description: "Permanently invalidates a specified membership using its unique ID, effectively unfulfilling the user's product experiences. Termination is irreversible. [See the documentation](https://dev.whop.com/api-reference/v2/memberships/terminate-a-membership)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    whop,
+    membershipId: {
+      propDefinition: [
+        whop,
+        "membershipId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.whop.terminateMembership({
+      membershipId: this.membershipId,
+    });
+
+    $.export("$summary", `Successfully terminated membership with ID: ${this.membershipId}`);
+    return response;
+  },
+};
