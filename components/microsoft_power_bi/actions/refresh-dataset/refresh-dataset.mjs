959fc3ea1632ec93123e2021af4014343c416c0b
GTFalcao <gtfalcao96@gmail.com>
2023-12-18 19:35:34 -0300

    Description adjustments

--- components/microsoft_power_bi/actions/refresh-dataset/refresh-dataset.mjs
+++ components/microsoft_power_bi/actions/refresh-dataset/refresh-dataset.mjs
@@ -2,7 +2,7 @@ import powerBi from "../../microsoft_power_bi.app.mjs";
 
 export default {
   key: "microsoft_power_bi-refresh-dataset",
-  name: "Refresh Power BI Dataset",
+  name: "Refresh Dataset",
   description: "Triggers a refresh operation for a specified Power BI dataset. [See the documentation](https://learn.microsoft.com/en-us/rest/api/power-bi/datasets/refresh-dataset)",
   version: "0.0.1",
   type: "action",
