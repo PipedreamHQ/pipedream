a83be4385c81132a29decf0f56963679227aa25a
GTFalcao <gtfalcao96@gmail.com>
2023-12-19 00:55:16 -0300

    version bump

--- components/github/actions/get-repository/get-repository.mjs
+++ components/github/actions/get-repository/get-repository.mjs
@@ -4,7 +4,7 @@ export default {
   key: "github-get-repository",
   name: "Get Repository",
   description: "Get specific repository. [See docs here](https://docs.github.com/en/rest/repos/repos#get-a-repository)",
-  version: "0.0.13",
+  version: "0.0.14",
   type: "action",
   props: {
     github,
