a0e2ba4a341f83120ca84a8173f250b2c5674e3f
Lucas Caresia <lucascarezia@gmail.com>
2023-12-19 17:48:01 -0300

    [Components] urlbae #9263 (#9342)
    * Added actions
    
    * Update urlbae.app.mjs
    
    ---------
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

---
+++ components/urlbae/actions/common/constants.mjs
@@ -0,0 +1,33 @@
+export default {
+  DATA_TYPES: [
+    {
+      label: "Text",
+      value: "text",
+    },
+    {
+      label: "vCard",
+      value: "vcard",
+    },
+    {
+      label: "Link",
+      value: "link",
+    },
+    {
+      label: "Email",
+      value: "email",
+    },
+    {
+      label: "Phone",
+      value: "phone",
+    },
+    {
+      label: "SMS",
+      value: "sms",
+    },
+    {
+      label: "WiFi",
+      value: "wifi",
+    },
+  ],
+};
+
--- components/urlbae/urlbae.app.mjs
+++ components/urlbae/urlbae.app.mjs
@@ -1,11 +1,40 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "urlbae",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _apiUrl() {
+      return "https://urlbae.com/api";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this._apiKey()}`,
+        },
+        ...args,
+      });
+    },
+    async createQrcode(args = {}) {
+      return this._makeRequest({
+        path: "/qr/add",
+        method: "post",
+        ...args,
+      });
+    },
+    async shortenUrl(args = {}) {
+      return this._makeRequest({
+        path: "/url/add",
+        method: "post",
+        ...args,
+      });
     },
   },
 };
