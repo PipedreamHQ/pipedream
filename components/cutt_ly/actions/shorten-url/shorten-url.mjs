485f8f0ba46a85d713c34be9b775115e4a0462d2
michelle0927 <michelle0927@users.noreply.github.com>
2023-12-20 07:22:34 -0800

    Cutt_ly - New Components (#9376)
    * init
    
    * new-components
    
    * pnpm-lock.yaml

---
+++ components/cutt_ly/actions/shorten-url/shorten-url.mjs
@@ -0,0 +1,27 @@
+import cuttLy from "../../cutt_ly.app.mjs";
+
+export default {
+  key: "cutt_ly-shorten-url",
+  name: "Shorten URL",
+  description: "Returns a shortened URL when given a target URL as a prop. [See the documentation](https://cutt.ly/cuttly-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    cuttLy,
+    targetUrl: {
+      type: "string",
+      label: "Target URL",
+      description: "The URL you want to shorten",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.cuttLy.callApi({
+      params: {
+        short: this.targetUrl,
+      },
+      $,
+    });
+    $.export("$summary", `Successfully shortened URL to ${response.url.shortLink}.`);
+    return response;
+  },
+};
---
+++ components/cutt_ly/actions/update-source-url/update-source-url.mjs
@@ -0,0 +1,33 @@
+import cuttLy from "../../cutt_ly.app.mjs";
+
+export default {
+  key: "cutt_ly-update-source-url",
+  name: "Update Source URL",
+  description: "Changes the source URL of a previously shortened URL. Requires a Paid Subscription. [See the documentation](https://cutt.ly/cuttly-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    cuttLy,
+    shortenedUrl: {
+      type: "string",
+      label: "Shortened URL",
+      description: "The shortened URL to edit. Must already be registered.",
+    },
+    newSourceUrl: {
+      type: "string",
+      label: "New Source URL",
+      description: "The new source URL to change a previously shortened URL",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.cuttLy.callApi({
+      params: {
+        edit: this.shortenedUrl,
+        source: this.newSourceUrl,
+      },
+      $,
+    });
+    $.export("$summary", `Successfully updated source URL for ${this.shortenedUrl}.`);
+    return response;
+  },
+};
