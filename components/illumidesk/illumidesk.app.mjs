d2681a7ea6426f6d6b62711938b0ac7c5724b1a8
michelle0927 <michelle0927@users.noreply.github.com>
2023-12-21 06:52:13 -0800

    Illumidesk - New Components (#9291)
    * init
    
    * pnpm-lock.yaml
    
    * new components
    
    * pnpm-lock.yaml
    
    * create constants file
    
    * add permission prop
    
    * pnpm-lock.yaml

--- components/illumidesk/illumidesk.app.mjs
+++ components/illumidesk/illumidesk.app.mjs
@@ -1,11 +1,142 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "illumidesk",
-  propDefinitions: {},
+  propDefinitions: {
+    courseSlug: {
+      type: "string",
+      label: "Course ID",
+      description: "The slug for the course",
+      async options({
+        campusSlug, prevContext,
+      }) {
+        const args = {
+          campusSlug,
+        };
+        if (prevContext?.next) {
+          args.url = prevContext.next;
+        }
+        const {
+          results, next,
+        } = await this.listCoursesByCampus(args);
+        const options = results?.map(({
+          slug: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+        return {
+          options,
+          context: {
+            next,
+          },
+        };
+      },
+    },
+    campusSlug: {
+      type: "string",
+      label: "Campus Slug",
+      description: "The slug for the campus",
+      async options() {
+        const campuses = await this.listCampuses();
+        return campuses?.map(({
+          slug: value, title: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.illumidesk.com/api/v1";
+    },
+    _headers() {
+      return {
+        Authorization: `Token ${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...otherOpts,
+      });
+    },
+    listCampuses(args = {}) {
+      return this._makeRequest({
+        path: "/campuses/",
+        ...args,
+      });
+    },
+    listCourseActivities({
+      courseSlug, ...args
+    }) {
+      return this._makeRequest({
+        path: `/courses/${courseSlug}/activities/`,
+        ...args,
+      });
+    },
+    listCourseParticipants({
+      courseSlug, ...args
+    }) {
+      return this._makeRequest({
+        path: `/courses/${courseSlug}/participants/`,
+        ...args,
+      });
+    },
+    listCourseLessons({
+      courseSlug, ...args
+    }) {
+      return this._makeRequest({
+        path: `/courses/${courseSlug}/lessons/`,
+        ...args,
+      });
+    },
+    createCourse({
+      campusSlug, ...args
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/campuses/${campusSlug}/courses/`,
+        ...args,
+      });
+    },
+    inviteUserToCourse({
+      courseSlug, ...args
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/courses/${courseSlug}/invitations/`,
+        ...args,
+      });
+    },
+    listCoursesByCampus({
+      campusSlug, ...args
+    }) {
+      return this._makeRequest({
+        path: `/campuses/${campusSlug}/courses/`,
+        ...args,
+      });
+    },
+    async *paginate({
+      resourceFn, args,
+    }) {
+      do {
+        const response = await resourceFn(args);
+        const items = response?.results || response;
+        for (const item of items) {
+          yield item;
+        }
+        args.url = response?.next;
+      } while (args.url);
     },
   },
 };
