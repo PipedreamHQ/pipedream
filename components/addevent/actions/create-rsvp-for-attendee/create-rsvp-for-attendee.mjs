5bcefb4f94811dd0de25ec6a0e7e8b9d7804f96a
michelle0927 <michelle0927@users.noreply.github.com>
2023-12-22 08:12:23 -0800

    AddEvent - New Components (#9398)
    * init
    
    * new components
    
    * pnpm-lock.yaml
    
    * formatting
    
    * bump package version
    
    * revert package version
    
    * add timezone prop

---
+++ components/addevent/actions/create-rsvp-for-attendee/create-rsvp-for-attendee.mjs
@@ -0,0 +1,51 @@
+import addevent from "../../addevent.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "addevent-create-rsvp-for-attendee",
+  name: "Create RSVP For Attendee",
+  description: "Creates an RSVP for an attendee for a specific event. [See the documentation](https://docs.addevent.com/reference/create-rsvp-attendee)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    addevent,
+    eventId: {
+      propDefinition: [
+        addevent,
+        "eventId",
+      ],
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "Name of the attendee",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email address of the RSVP attendee. Update and reminder emails about the event will be sent to this email address. Email addresses must be unique for each attendee of a given event.",
+    },
+    attending: {
+      type: "string",
+      label: "Attending",
+      description: "The attendee's response to the event",
+      options: constants.ATTENDING_RESPONSES,
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.addevent.createRsvp({
+      eventId: this.eventId,
+      data: {
+        email: this.email,
+        attending: this.attending,
+        rsvp_form_data: {
+          name: this.name,
+        },
+      },
+      $,
+    });
+    $.export("$summary", `Successfully created RSVP for ${this.email}.`);
+    return response;
+  },
+};
