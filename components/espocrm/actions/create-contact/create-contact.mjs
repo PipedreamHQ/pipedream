f6365a792724494ded794a9f615498397f2fc06b
michelle0927 <michellelbergero@hotmail.com>
2024-01-01 16:52:36 -0500

    new components

---
+++ components/espocrm/actions/create-contact/create-contact.mjs
@@ -0,0 +1,88 @@
+import espoCrm from "../../espocrm.app.mjs";
+
+export default {
+  key: "espo-crm-create-contact",
+  name: "Create Contact",
+  description: "Creates a new contact in Espo CRM. [See the documentation](https://docs.espocrm.com/development/api/crud/#create)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    espoCrm,
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "Specify the contact's first name",
+    },
+    lastName: {
+      type: "string",
+      label: "Last Name",
+      description: "Specify the contact's last name",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Specify the contact's email",
+    },
+    phone: {
+      type: "string",
+      label: "Phone",
+      description: "Specify the contact's phone number",
+      optional: true,
+    },
+    street: {
+      type: "string",
+      label: "Street Address",
+      description: "Specify the contact's street address",
+      optional: true,
+    },
+    city: {
+      type: "string",
+      label: "City",
+      description: "Specify the contact's city",
+      optional: true,
+    },
+    state: {
+      type: "string",
+      label: "State",
+      description: "Specify the contact's state/region",
+      optional: true,
+    },
+    postalCode: {
+      type: "string",
+      label: "Postal Code",
+      description: "Specify the contact's postal code",
+      optional: true,
+    },
+    country: {
+      type: "string",
+      label: "Country",
+      description: "Specify the contact's country",
+      optional: true,
+    },
+    title: {
+      type: "string",
+      label: "Job Title",
+      description: "Specify the contact's job title",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.espoCrm.createContact({
+      data: {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        emailAddress: this.email,
+        phoneNumber: this.phone,
+        addressStreet: this.street,
+        addressCity: this.city,
+        addressState: this.state,
+        addressPostalCode: this.postalCode,
+        addressCountry: this.country,
+        title: this.title,
+      },
+      $,
+    });
+    $.export("$summary", `Successfully created contact ${this.firstName} ${this.lastName}.`);
+    return response;
+  },
+};
---
+++ components/espocrm/actions/create-task/create-task.mjs
@@ -0,0 +1,67 @@
+import espoCrm from "../../espocrm.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "espo-crm-update-task",
+  name: "Create Task",
+  description: "This component creates a new task in Espo CRM. [See the documentation](https://docs.espocrm.com/development/api/crud/#create)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    espoCrm,
+    name: {
+      type: "string",
+      label: "Name",
+      description: "Specify the task name",
+    },
+    assigneeId: {
+      propDefinition: [
+        espoCrm,
+        "assigneeId",
+      ],
+      optional: true,
+    },
+    dueDate: {
+      type: "string",
+      label: "Due Date",
+      description: "Specify the task due date in the format `YYYY-MM-DD HH:MM:SS`",
+      optional: true,
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "Specify the task description",
+      optional: true,
+    },
+    status: {
+      type: "string",
+      label: "Status",
+      description: "Specify the task status",
+      options: constants.TASK_STATUS,
+      optional: true,
+    },
+    priority: {
+      type: "string",
+      label: "Priority",
+      description: "Specify the task priority",
+      options: constants.TASK_PRIORITY,
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.espoCrm.createTask({
+      data: {
+        name: this.name,
+        assignedUserId: this.assigneeId,
+        dateEnd: this.dueDate,
+        description: this.description,
+        status: this.status,
+        priority: this.priority,
+      },
+      $,
+    });
+
+    $.export("$summary", `Successfully created task ${this.name}.`);
+    return response;
+  },
+};
