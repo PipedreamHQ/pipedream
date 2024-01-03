388c12550ad4d9f6b41dbe99a9ad2c674d308708
Luan Cazarine <luanhc@gmail.com>
2024-01-03 13:24:11 -0300

    New Components - veriphone (#9502)
    * veriphone init
    
    * [Components] Veriphone #9489
     Actions
     - Verify Phone Number
     - Get Phone Example
    
    * pnpm update
    
    * some adjusts

--- components/veriphone/veriphone.app.mjs
+++ components/veriphone/veriphone.app.mjs
@@ -1,11 +1,62 @@
+import { axios } from "@pipedream/platform";
+import {
+  COUNTRY_OPTIONS, PHONE_TYPE_OPTIONS,
+} from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "veriphone",
-  propDefinitions: {},
+  propDefinitions: {
+    phoneNumber: {
+      type: "string",
+      label: "Phone Number",
+      description: "The phone number you wish to verify.",
+    },
+    countryCode: {
+      type: "string",
+      label: "Country Code",
+      description: "The country code. The country will be infered from the IP address if this parameter is absent or invalid.",
+      options: COUNTRY_OPTIONS,
+    },
+    phoneType: {
+      type: "string",
+      label: "Phone Type",
+      description: "The type of phone number.",
+      options: PHONE_TYPE_OPTIONS,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.veriphone.io/v2";
+    },
+    _auth() {
+      return {
+        key: this.$auth.api_key,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, params, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        params: {
+          ...params,
+          ...this._auth(),
+        },
+        ...opts,
+      });
+    },
+    async verifyPhoneNumber(opts = {}) {
+      return this._makeRequest({
+        path: "/verify",
+        ...opts,
+      });
+    },
+    async getDummyPhoneNumber( opts = {}) {
+      return this._makeRequest({
+        path: "/example",
+        ...opts,
+      });
     },
   },
 };
