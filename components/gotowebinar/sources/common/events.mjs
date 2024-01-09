574b7c63ca14e804ac81e2dcaff2d54c49ab2758
Jorge Cortes <jacortesmahmud@gmail.com>
2024-01-09 09:07:44 -0500

    [TRIGGER] GoToMeeting New Triggers (#9497)
    * Added new webhook sources
    
    * pnpm-lock.yaml

---
+++ components/gotowebinar/sources/common/events.mjs
@@ -0,0 +1,6 @@
+export default {
+  REGISTRANT_ADDED: "registrant.added",
+  REGISTRANT_JOINED: "registrant.joined",
+  WEBINAR_CREATED: "webinar.created",
+  WEBINAR_CHANGED: "webinar.changed",
+};
