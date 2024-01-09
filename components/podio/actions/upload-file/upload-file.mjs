3bf12d4d9b5ca3e0235ac700a949bdf1a9b3cfb5
GTFalcao <gtfalcao96@gmail.com>
2024-01-08 23:43:42 -0300

    Upload File action

---
+++ components/podio/actions/upload-file/upload-file.mjs
@@ -0,0 +1,57 @@
+import app from "../../podio.app.mjs";
+import FormData from "form-data";
+import fs from "fs";
+
+export default {
+  key: "podio-upload-file",
+  name: "Upload File",
+  description: "Upload a file to Podio. [See the documentation](https://developers.podio.com/doc/files/upload-file-1004361)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    filePath: {
+      type: "string",
+      label: "File Path",
+      description: "The path to a file in the `/tmp` directory. [See the documentation on working with files](https://pipedream.com/docs/code/nodejs/working-with-files/#writing-a-file-to-tmp).",
+    },
+    fileName: {
+      type: "string",
+      label: "File Name",
+      description: "File name of the uploaded file. If not specified, the original file name will be used.",
+      optional: true,
+    },
+  },
+  methods: {
+    uploadFile(args) {
+      return this.app._makeRequest({
+        path: "/file/",
+        method: "POST",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const data = new FormData();
+
+    const {
+      filePath, fileName,
+    } = this;
+
+    const content = fs.createReadStream(filePath.includes("tmp/")
+      ? filePath
+      : `/tmp/${filePath}`);
+
+    data.append("source", content);
+    data.append("filename", fileName ?? this.filePath.split("/").pop());
+
+    const response = await this.uploadFile({
+      $,
+      headers: data.getHeaders(),
+      data,
+    });
+
+    $.export("$summary", "Successfully uploaded file");
+    return response;
+  },
+};
