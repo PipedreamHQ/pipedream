df1c09e8fc40a3d0dde726aa599c607a71777e7e
Lucas Caresia <lucascarezia@gmail.com>
2024-01-11 10:45:15 -0300

    Added actions

---
+++ components/dub/actions/update-link/update-link.mjs
@@ -0,0 +1,85 @@
+import app from "../../dub.app.mjs";
+
+export default {
+  name: "Update Link",
+  version: "0.0.7",
+  key: "dub-update-link",
+  description: "Updates a link. [See the documentation](https://dub.co/docs/api-reference/endpoint/edit-a-link)",
+  type: "action",
+  props: {
+    app,
+    projectSlug: {
+      type: "string",
+      label: "Project slug",
+      description: "The slug for the project to create links for",
+      propDefinition: [
+        app,
+        "projectSlug",
+      ],
+    },
+    linkId: {
+      type: "string",
+      label: "Project slug",
+      description: "The slug for the project to create links for",
+      propDefinition: [
+        app,
+        "linkId",
+        (c) => ({
+          projectSlug: c.projectSlug,
+        }),
+      ],
+    },
+    domain: {
+      propDefinition: [
+        app,
+        "domain",
+      ],
+    },
+    url: {
+      propDefinition: [
+        app,
+        "url",
+      ],
+    },
+    password: {
+      propDefinition: [
+        app,
+        "password",
+      ],
+      optional: true,
+    },
+    publicStats: {
+      propDefinition: [
+        app,
+        "publicStats",
+      ],
+    },
+    comments: {
+      propDefinition: [
+        app,
+        "comments",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.updateLink({
+      $,
+      linkId: this.linkId,
+      params: {
+        projectSlug: this.projectSlug,
+      },
+      data: {
+        domain: this.domain,
+        url: this.url,
+        password: this.password,
+        publicStats: this.publicStats,
+        comments: this.comments,
+      },
+    });
+
+    $.export("$summary", `Successfully updated link with ID ${this.linkId}`);
+
+    return response;
+  },
+};
--- components/dub/dub.app.mjs
+++ components/dub/dub.app.mjs
@@ -1,11 +1,131 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "dub",
-  propDefinitions: {},
+  propDefinitions: {
+    projectSlug: {
+      type: "string",
+      label: "Project Slug",
+      description: "The project slug",
+      async options() {
+        const resources = await this.getProjects();
+
+        return resources.map(({
+          slug, name,
+        }) => ({
+          label: name,
+          value: slug,
+        }));
+      },
+    },
+    linkId: {
+      type: "string",
+      label: "Link ID",
+      description: "The link id",
+      async options({ projectSlug }) {
+        const resources = await this.getLinks({
+          params: {
+            projectSlug,
+          },
+        });
+
+        return resources.map(({
+          id, domain,
+        }) => ({
+          label: domain,
+          value: id,
+        }));
+      },
+    },
+    domain: {
+      type: "string",
+      label: "Domain",
+      description: "The domain of the short link",
+    },
+    url: {
+      type: "string",
+      label: "URL",
+      description: "The destination URL of the short link",
+    },
+    password: {
+      type: "string",
+      label: "Password",
+      description: "The password required to access the destination URL of the short link",
+      optional: true,
+    },
+    publicStats: {
+      type: "boolean",
+      label: "Public Stats",
+      description: "Whether the short link's stats are publicly accessible",
+    },
+    comments: {
+      type: "string",
+      label: "Comments",
+      description: "The comments for the short link",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiKey() {
+      return this.$auth.api_key;
+    },
+    _apiUrl() {
+      return "https://api.dub.co";
+    },
+    async _makeRequest({
+      $ = this, path, ...args
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this._apiKey()}`,
+        },
+        ...args,
+      });
+    },
+    async createLink(args = {}) {
+      const response = await this._makeRequest({
+        path: "/links",
+        method: "post",
+        ...args,
+      });
+
+      return response.subscriptions;
+    },
+    async updateLink({
+      linkId, ...args
+    }) {
+      const response = await this._makeRequest({
+        path: `/links/${linkId}`,
+        method: "put",
+        ...args,
+      });
+
+      return response.subscriptions;
+    },
+    async deleteLink({
+      linkId, ...args
+    }) {
+      const response = await this._makeRequest({
+        path: `/links/${linkId}`,
+        method: "delete",
+        ...args,
+      });
+
+      return response.subscriptions;
+    },
+    async getProjects(args = {}) {
+      return this._makeRequest({
+        path: "/projects",
+        ...args,
+      });
+    },
+    async getLinks(args = {}) {
+      return this._makeRequest({
+        path: "/links",
+        ...args,
+      });
     },
   },
 };
