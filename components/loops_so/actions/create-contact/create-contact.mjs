f59fc4a84952eec725221ebb80557f0dcdc12961
GTFalcao <gtfalcao96@gmail.com>
2024-01-14 18:24:25 -0300

    Abstracting common props and reloadProps for create/update

--- components/loops_so/actions/create-contact/create-contact.mjs
+++ components/loops_so/actions/create-contact/create-contact.mjs
@@ -1,44 +1,12 @@
-import loops from "../../loops_so.app.mjs";
+import common from "../common/common-create-update.mjs";
 
 export default {
+  ...common,
   key: "loops_so-create-contact",
   name: "Create Contact",
   description: "Creates a new contact. [See the Documentation](https://loops.so/docs/add-users/api-reference#add)",
   version: "0.1.0",
   type: "action",
-  props: {
-    loops,
-    email: {
-      propDefinition: [
-        loops,
-        "email",
-      ],
-    },
-    firstName: {
-      propDefinition: [
-        loops,
-        "firstName",
-      ],
-    },
-    lastName: {
-      propDefinition: [
-        loops,
-        "lastName",
-      ],
-    },
-    userGroup: {
-      propDefinition: [
-        loops,
-        "userGroup",
-      ],
-    },
-    customFields: {
-      propDefinition: [
-        loops,
-        "customFields",
-      ],
-    },
-  },
   async run({ $ }) {
     const { // eslint-disable-next-line no-unused-vars
       loops, email, firstName, lastName, userGroup, customFields, ...data
--- components/loops_so/actions/update-contact/update-contact.mjs
+++ components/loops_so/actions/update-contact/update-contact.mjs
@@ -1,45 +1,13 @@
-import loops from "../../loops_so.app.mjs";
+import common from "../common/common-create-update.mjs";
 import pickBy from "lodash.pickby";
 
 export default {
+  ...common,
   key: "loops_so-update-contact",
   name: "Update Contact",
   description: "Updates an existing contact by email. If email not found, a new contact will be created. [See the Documentation](https://loops.so/docs/add-users/api-reference#update)",
   version: "0.1.0",
   type: "action",
-  props: {
-    loops,
-    email: {
-      propDefinition: [
-        loops,
-        "email",
-      ],
-    },
-    firstName: {
-      propDefinition: [
-        loops,
-        "firstName",
-      ],
-    },
-    lastName: {
-      propDefinition: [
-        loops,
-        "lastName",
-      ],
-    },
-    userGroup: {
-      propDefinition: [
-        loops,
-        "userGroup",
-      ],
-    },
-    customFields: {
-      propDefinition: [
-        loops,
-        "customFields",
-      ],
-    },
-  },
   async run({ $ }) {
     const { // eslint-disable-next-line no-unused-vars
       loops, email, firstName, lastName, userGroup, customFields, ...data
