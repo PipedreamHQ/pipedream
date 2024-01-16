698d9247617023a91c5876618f230f1dc100919c
Leo Vu <vunguyenhung@outlook.com>
2024-01-16 10:24:10 +0700

    Update components/easyly/sources/new-event-triggered-instant/new-event-triggered-instant.mjs

--- components/easyly/sources/new-event-triggered-instant/new-event-triggered-instant.mjs
+++ components/easyly/sources/new-event-triggered-instant/new-event-triggered-instant.mjs
@@ -4,7 +4,7 @@ import app from "../../easyly.app.mjs";
 export default {
   key: "easyly-new-event-triggered-instant",
   name: "New Event Triggered (Instant)",
-  description: "Emit new event when an action is triggered in network leads. You need to first deploy this source in order to copy the webhook URL to Easyly platform. [See the documentation](https://api.easyly.com/webhooks)",
+  description: "Emit new event when an action is triggered in network leads. After creating the source, you need to [register a Webhook on Easyly](https://app.easyly.com/settings/developers) using the source's HTTP Endpoint. [See the documentation](https://api.easyly.com/webhooks)",
   version: "0.0.1",
   type: "source",
   dedupe: "unique",
