7fcd25322d923ea1324cd5f31fbd9e623a1882ac
Andrew Chuang <andrewjschuang@gmail.com>
2024-01-17 15:35:37 -0300

    change attachments to dynamic listing

--- components/gmail/actions/send-email/send-email.mjs
+++ components/gmail/actions/send-email/send-email.mjs
@@ -5,7 +5,7 @@ export default {
   key: "gmail-send-email",
   name: "Send Email",
   description: "Send an email from your Google Workspace email account",
-  version: "0.0.11",
+  version: "0.0.12",
   type: "action",
   props: {
     gmail,
@@ -57,12 +57,6 @@ export default {
         "bodyType",
       ],
     },
-    attachments: {
-      propDefinition: [
-        gmail,
-        "attachments",
-      ],
-    },
     inReplyTo: {
       propDefinition: [
         gmail,
@@ -75,9 +69,67 @@ export default {
         "mimeType",
       ],
     },
+    attachmentName_0: {
+      type: "string",
+      label: "Attachment Name #0",
+      description: "The name of the attachment. Must contain the extension (i.e. `.jpeg`, `.txt`)",
+      optional: true,
+      reloadProps: true,
+    },
+  },
+  additionalProps() {
+    const props = {};
+    let count = 0;
+
+    while (true) {
+      if (this[`attachmentName_${count}`]) {
+        props[`attachmentUrl_${count}`] = {
+          type: "string",
+          label: `Attachment URL #${count}`,
+          description: "The download URL of the file",
+        };
+      } else {
+        break;
+      }
+
+      count++;
+
+      props[`attachmentName_${count}`] = {
+        type: "string",
+        label: `Attachment Name #${count}`,
+        description: "The name of the attachment. Must contain the extension (i.e. `.jpeg`, `.txt`)",
+        optional: true,
+        reloadProps: true,
+      };
+    }
+
+    return props;
+  },
+  methods: {
+    parseAttachments() {
+      const attachments = [];
+      let count = 0;
+
+      while (true) {
+        if (!this[`attachmentName_${count}`]) {
+          break;
+        }
+
+        attachments.push({
+          filename: this[`attachmentName_${count}`],
+          path: this[`attachmentUrl_${count}`],
+        });
+        count++;
+      }
+
+      if (attachments.length) {
+        return attachments;
+      }
+    },
   },
   async run({ $ }) {
     const opts = await this.gmail.getOptionsToSendEmail($, this);
+    opts.attachments = this.parseAttachments();
     const response = await this.gmail.sendEmail(opts);
     $.export("$summary", `Successfully sent email to ${this.to}`);
     return response;
