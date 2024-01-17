ef00ab2dbe3b17c593382461e1a69d4b507b0a7d
michelle0927 <michellelbergero@hotmail.com>
2024-01-17 13:29:21 -0500

    surveycto init

--- components/surveycto/surveycto.app.mjs
+++ components/surveycto/surveycto.app.mjs
@@ -1,11 +1,65 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "surveycto",
-  propDefinitions: {},
+  propDefinitions: {
+    formName: {
+      type: "string",
+      label: "Form Name",
+      description: "The name of the form",
+      required: true,
+    },
+    submissionId: {
+      type: "string",
+      label: "Submission ID",
+      description: "The ID of the form submission",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
+    _baseUrl() {
+      return "https://api.surveycto.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.api_token}`,
+        },
+      });
+    },
     authKeys() {
       console.log(Object.keys(this.$auth));
+    },
+    async getFormData(formName, opts = {}) {
+      return this._makeRequest({
+        path: `/v2/forms/data/${formName}`,
+        ...opts,
+      });
+    },
+    async getSubmissionData(formName, submissionId, opts = {}) {
+      return this._makeRequest({
+        path: `/v2/forms/data/${formName}/${submissionId}`,
+        ...opts,
+      });
+    },
+    async submitForm(formData) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/forms/submit",
+        data: formData,
+      });
     },
   },
 };
