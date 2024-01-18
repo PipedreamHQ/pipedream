f2fc126b2fd8ca22d72c0d95f4efc0c41953957f
michelle0927 <michellelbergero@hotmail.com>
2024-01-18 15:47:09 -0500

    add test events

--- components/waiverfile/sources/edit-event-instant/edit-event-instant.mjs
+++ components/waiverfile/sources/edit-event-instant/edit-event-instant.mjs
@@ -1,4 +1,5 @@
 import common from "../common/base-webhook.mjs";
+import sampleEmit from "./test-event.mjs";
 
 export default {
   ...common,
@@ -22,4 +23,5 @@ export default {
       };
     },
   },
+  sampleEmit,
 };
--- components/waiverfile/sources/new-event-instant/new-event-instant.mjs
+++ components/waiverfile/sources/new-event-instant/new-event-instant.mjs
@@ -1,4 +1,5 @@
 import common from "../common/base-webhook.mjs";
+import sampleEmit from "./test-event.mjs";
 
 export default {
   ...common,
@@ -21,4 +22,5 @@ export default {
       };
     },
   },
+  sampleEmit,
 };
