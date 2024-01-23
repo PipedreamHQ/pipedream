a055b3a9b1b60cc2bef878502633b2e3a5da54ba
michelle0927 <michelle0927@users.noreply.github.com>
2024-01-23 07:10:19 -0800

    Apollo.io - New Actions (#10000)
    * new actions
    
    * pnpm-lock.yaml

---
+++ components/apollo_io/actions/update-account-stage/update-account-stage.mjs
@@ -0,0 +1,42 @@
+import app from "../../apollo_io.app.mjs";
+
+export default {
+  key: "apollo_io-update-account-stage",
+  name: "Update Account Stage",
+  description: "Updates the stage of one or more accounts in Apollo.io. [See the documentation](https://apolloio.github.io/apollo-api-docs/?shell#update-account-stage)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    accountIds: {
+      propDefinition: [
+        app,
+        "accountId",
+      ],
+      type: "string[]",
+      label: "Account IDs",
+      description: "Identifiers of the accounts to update",
+    },
+    accountStageId: {
+      propDefinition: [
+        app,
+        "accountStageId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { accounts } = await this.app.updateAccountStage({
+      $,
+      data: {
+        account_ids: this.accountIds,
+        account_stage_id: this.accountStageId,
+      },
+    });
+
+    $.export("$summary", `Successfully updated ${accounts.length} account${accounts.length === 1
+      ? ""
+      : "s"}.`);
+
+    return accounts;
+  },
+};
---
+++ components/apollo_io/actions/update-account/update-account.mjs
@@ -0,0 +1,54 @@
+import app from "../../apollo_io.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  key: "apollo_io-update-account",
+  name: "Update Account",
+  description: "Updates an existing account in Apollo.io. [See the documentation](https://apolloio.github.io/apollo-api-docs/?shell#update-an-account)",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    accountId: {
+      propDefinition: [
+        app,
+        "accountId",
+      ],
+    },
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+      optional: true,
+    },
+    domain: {
+      propDefinition: [
+        app,
+        "domain",
+      ],
+    },
+    phone: {
+      propDefinition: [
+        app,
+        "phone",
+      ],
+      description: "The corporate phone for this account",
+    },
+  },
+  async run({ $ }) {
+    const { account } = await this.app.updateAccount({
+      $,
+      accountId: this.accountId,
+      data: utils.cleanObject({
+        name: this.name,
+        domain: this.domain,
+        phone_number: this.phone,
+      }),
+    });
+
+    $.export("$summary", `Successfully updated account with ID ${account.id}`);
+
+    return account;
+  },
+};
