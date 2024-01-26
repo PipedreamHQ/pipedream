c396e7e328e647232e51252293f4f37f42277831
Lucas Caresia <lucascarezia@gmail.com>
2024-01-26 16:07:21 -0300

    [Components] northflank #9455 (#9999)
    * northflank init
    
    * Added actions
    
    * Update components/northflank/actions/create-domain/create-domain.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/northflank/actions/get-domains/get-domains.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/northflank/actions/create-project/create-project.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/northflank/actions/get-projects/get-projects.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Fixed actions
    
    * Update components/northflank/actions/get-projects/get-projects.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Package.json empty line
    
    * Fixed bugs
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/northflank/actions/create-project/create-project.mjs
@@ -0,0 +1,50 @@
+import app from "../../northflank.app.mjs";
+
+export default {
+  key: "northflank-create-project",
+  name: "Create Project",
+  description: "Creates a new project on Northflank. [See the documentation](https://northflank.com/docs/v1/api/projects/create-project)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    description: {
+      propDefinition: [
+        app,
+        "description",
+      ],
+    },
+    color: {
+      propDefinition: [
+        app,
+        "color",
+      ],
+    },
+    region: {
+      propDefinition: [
+        app,
+        "region",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createProject({
+      $,
+      data: {
+        name: this.name,
+        description: this.description,
+        color: this.color,
+        region: this.region,
+      },
+    });
+
+    $.export("$summary", `Successfully created project ${this.name}`);
+    return response;
+  },
+};
