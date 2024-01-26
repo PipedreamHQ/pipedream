b375cc7b1267e69b7b81e59e76f819c2abdadce6
Lucas Caresia <lucascarezia@gmail.com>
2024-01-26 11:44:12 -0300

    Update components/findymail/actions/create-list/create-list.mjs
    Co-authored-by: Luan Cazarine <luanhc@gmail.com>

--- components/findymail/actions/create-list/create-list.mjs
+++ components/findymail/actions/create-list/create-list.mjs
@@ -4,7 +4,7 @@ export default {
   key: "findymail-create-list",
   name: "Create List",
   description: "Creates a new list of contacts in Findymail. [See the documentation](https://app.findymail.com/docs/#contacts-postapi-lists)",
-  version: "0.0.2",
+  version: "0.0.1",
   type: "action",
   props: {
     findymail,
