6a86b4f7d05d57a4ca818e7882ac13adbf14cd1c
Lucas Caresia <lucascarezia@gmail.com>
2024-01-26 09:28:06 -0300

    retailed init

---
+++ components/retailed/actions/get-usage/get-usage.mjs
@@ -0,0 +1,24 @@
+import retailed from "../../retailed.app.mjs";
+import { axios } from "@pipedream/platform";
+
+export default {
+  key: "retailed-get-usage",
+  name: "Get API Usage",
+  description: "Gets the API usage information for the Retailed API. [See the documentation](https://docs.retailed.io/v1/usage)",
+  version: "0.0.{{ts}}",
+  type: "action",
+  props: {
+    retailed,
+    apiKey: {
+      propDefinition: [
+        retailed,
+        "apiKey",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.retailed.getApiUsage();
+    $.export("$summary", `Retrieved API usage information: ${response.remaining} requests remaining under the ${response.plan} plan`);
+    return response;
+  },
+};
--- components/retailed/retailed.app.mjs
+++ components/retailed/retailed.app.mjs
@@ -1,11 +1,43 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "retailed",
-  propDefinitions: {},
+  propDefinitions: {
+    apiKey: {
+      type: "string",
+      label: "API Key",
+      description: "The API key for authenticating requests to the Retailed API.",
+      secret: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
+    _baseUrl() {
+      return "https://app.retailed.io/api/v1";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this, method = "GET", path, headers, ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+          "x-api-key": this.$auth.apiKey,
+        },
+      });
+    },
+    async getApiUsage() {
+      return this._makeRequest({
+        path: "/usage",
+      });
+    },
     authKeys() {
       console.log(Object.keys(this.$auth));
     },
   },
+  version: "0.0.{{ts}}",
 };
