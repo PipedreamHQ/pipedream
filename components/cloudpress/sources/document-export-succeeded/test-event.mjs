020c883ec27ddb2e150329c74499da6c860457b1
michelle0927 <michelle0927@users.noreply.github.com>
2024-01-30 06:44:57 -0800

    New Components - Cloudpress (#10121)
    * new components
    
    * pnpm-lock.yaml

---
+++ components/cloudpress/sources/document-export-succeeded/test-event.mjs
@@ -0,0 +1,21 @@
+export default {
+  "eventType": "document_export.succeeded",
+  "eventTime": "2024-01-26T21:28:14.9629505+00:00",
+  "accountId": 659,
+  "data": {
+    "sourceConnection": {
+      "id": 857,
+      "name": "Notion"
+    },
+    "sourceDocumentTitle": "Hawaii Vacation",
+    "sourceDocumentReference": "3c38146f-21cd-42de-9411-eeedd8253aaf",
+    "sourceDocumentVersion": "2022-05-31T15:01:00.0000000+00:00",
+    "sourceDocumentKind": "notion",
+    "destinationConnection": {
+      "id": 854,
+      "name": "Collection1s"
+    },
+    "destinationReference": "65b423edce8f6ae483f2fa8d",
+    "exportedAt": "2024-01-26T21:28:14.7564733+00:00"
+  }
+}
