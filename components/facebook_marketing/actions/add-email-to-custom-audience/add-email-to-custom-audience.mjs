bf9ca0c481609a4423b9b5b6a9a6ba174c6e565f
GTFalcao <gtfalcao96@gmail.com>
2024-02-01 19:05:34 -0300

    Adding email prop as array

--- components/facebook_marketing/actions/add-email-to-custom-audience/add-email-to-custom-audience.mjs
+++ components/facebook_marketing/actions/add-email-to-custom-audience/add-email-to-custom-audience.mjs
@@ -8,41 +8,42 @@ export default {
   type: "action",
   props: {
     app,
-    email: {
-      type: "string",
-      label: "Email Address",
-      description: "The email address to add to the custom audience segment.",
+    adAccountId: {
+      propDefinition: [
+        app,
+        "adAccountId",
+      ],
     },
     customAudienceId: {
       propDefinition: [
         app,
         "customAudienceId",
-        async ({ prevContext }) => {
-          const page = prevContext.page
-            ? prevContext.page
-            : 0;
-          const response = await app.listCustomAudiences({
-            params: {
-              page,
-            },
-          });
-          return {
-            options: response.data.map((audience) => ({
-              label: audience.name,
-              value: audience.id,
-            })),
-            context: {
-              page: page + 1,
-            },
-          };
-        },
+        ({ adAccountId }) => ({
+          adAccountId,
+        }),
       ],
+    },
+    emails: {
+      type: "string[]",
+      label: "Email Address(es)",
+      description: "The email address(es) to add to the custom audience segment.",
+    },
+  },
+  methods: {
+    hashEmail(email) {
+      return email.trim().toLowerCase();
     },
   },
   async run({ $ }) {
     const response = await this.app.addEmailToCustomAudience({
+      $,
       customAudienceId: this.customAudienceId,
-      email: this.email,
+      data: {
+        payload: {
+          schema: "EMAIL_SHA256",
+          data: this.emails.map(this.hashEmail),
+        },
+      },
     });
     $.export("$summary", `Successfully added email to custom audience ${this.customAudienceId}`);
     return response;
