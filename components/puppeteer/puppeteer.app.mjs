e537f2a5dbe1d98f9a6800f8252b77cb18b9a0d6
js07 <19861096+js07@users.noreply.github.com>
2024-02-02 14:03:23 -0500

    Node 20 Playwright and Puppeteer support
    Pin the playwright-core, puppeteer-core, and @sparticuz/chromium
    packages to the latest versions, which are compatible with Node 20.

--- components/puppeteer/puppeteer.app.mjs
+++ components/puppeteer/puppeteer.app.mjs
@@ -1,8 +1,8 @@
 // Table for Chromium <> Puppeteer version support here: https://pptr.dev/chromium-support
 // @note: this is locked to an old chromium version
 //  because there's an unfulfilled promise bug in later version of puppeteer-core
-import puppeteer from "puppeteer-core@19.8.0";
-import chromium from "@sparticuz/chromium@112";
+import puppeteer from "puppeteer-core@21.11.0";
+import chromium from "@sparticuz/chromium@121.0.0";
 
 export default {
   type: "app",
