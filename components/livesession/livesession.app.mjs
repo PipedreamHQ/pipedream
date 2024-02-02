0631159d613db5c8da5a09832b0ba1a39373da4d
michelle0927 <michellelbergero@hotmail.com>
2024-02-02 11:50:21 -0500

    livesession init

--- components/livesession/livesession.app.mjs
+++ components/livesession/livesession.app.mjs
@@ -1,9 +1,82 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "livesession",
-  propDefinitions: {},
+  propDefinitions: {
+    sessionId: {
+      type: "string",
+      label: "Session ID",
+      description: "The ID of the session",
+    },
+    userData: {
+      type: "string",
+      label: "User Data",
+      description: "Data related to the user",
+      optional: true,
+    },
+    sessionDuration: {
+      type: "integer",
+      label: "Session Duration",
+      description: "The duration of the session in seconds",
+      optional: true,
+    },
+    sessionSource: {
+      type: "string",
+      label: "Session Source",
+      description: "The source of the session",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
+    _baseUrl() {
+      return "https://api.livesession.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async getSession(opts = {}) {
+      return this._makeRequest({
+        ...opts,
+        path: `/sessions/${opts.sessionId}`,
+      });
+    },
+    async createSessionEvent(opts = {}) {
+      const {
+        sessionId,
+        userData,
+        sessionDuration,
+        sessionSource,
+      } = opts;
+
+      const data = {
+        session_id: sessionId,
+        user_data: userData,
+        session_duration: sessionDuration,
+        session_source: sessionSource,
+      };
+
+      return this._makeRequest({
+        method: "POST",
+        path: "/session-events",
+        data,
+      });
+    },
     authKeys() {
       console.log(Object.keys(this.$auth));
     },
