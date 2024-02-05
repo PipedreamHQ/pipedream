789949304415100d3a71d3a8b4fbe55cacbae631
michelle0927 <michelle0927@users.noreply.github.com>
2024-02-05 07:29:20 -0800

    New Components - livesession (#10208)
    * livesession init
    
    * new component
    
    * pnpm-lock.yaml
    
    * fix pagination

--- components/livesession/livesession.app.mjs
+++ components/livesession/livesession.app.mjs
@@ -1,11 +1,65 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "livesession",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.livesession.io/v1";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this.$auth.api_token}`,
+          "Content-Type": "application/json",
+        },
+      });
+    },
+    listSessions(opts = {}) {
+      return this._makeRequest({
+        ...opts,
+        path: "/sessions",
+      });
+    },
+    async *paginate({
+      resourceFn,
+      params,
+      resourceType,
+      max,
+    }) {
+      params = {
+        ...params,
+        page: 0,
+        size: constants.DEFAULT_LIMIT,
+      };
+      let count = 0;
+      let total = 0;
+      do {
+        const response = await resourceFn({
+          params,
+        });
+        const items = response[resourceType];
+        if (!items?.length) {
+          return;
+        }
+        for (const item of items) {
+          yield item;
+          count++;
+          if (max && count >= max) {
+            return;
+          }
+        }
+        params.page++;
+        total = items?.length;
+      } while (total === params.size);
     },
   },
 };
