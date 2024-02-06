828dcc0eb54c688f9329cbe91c5f6cce1dd62aa6
GTFalcao <gtfalcao96@gmail.com>
2024-02-06 00:33:00 -0300

    Version bumps

--- components/airtable_oauth/sources/new-records-in-view/new-records-in-view.mjs
+++ components/airtable_oauth/sources/new-records-in-view/new-records-in-view.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Records in View",
   description: "Emit new event for each new record in a view",
   key: "airtable_oauth-new-records-in-view",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "source",
   props: {
     ...base.props,
