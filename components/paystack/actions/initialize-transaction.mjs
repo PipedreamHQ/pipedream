6fcbb6a9895152b91613c34193303474347c359d
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2024-02-06 22:54:07 +0100

    add paystack app config and initialize transaction action

---
+++ components/paystack/actions/initialize-transaction.mjs
@@ -0,0 +1,46 @@
+import paystack from "../paystack.app.mjs";
+
+export default {
+  key: "paystack-initialize-transaction",
+  name: "Initialize Transaction",
+  description:
+    "Initializes a new transaction on Paystack. [See the documentation](https://paystack.com/docs/api/transaction/#initialize)",
+  version: "0.0.10",
+  type: "action",
+  props: {
+    paystack,
+    email: {
+      propDefinition: [paystack, "email"],
+      optional: false,
+    },
+    amount: {
+      propDefinition: [paystack, "amount"],
+      optional: false,
+    },
+    currency: {
+      propDefinition: [paystack, "currency"],
+      optional: true,
+    },
+    reference: {
+      propDefinition: [paystack, "reference"],
+      optional: true,
+    },
+    callback_url: {
+      propDefinition: [paystack, "callback_url"],
+      optional: true,
+    },
+    metadata: {
+      propDefinition: [paystack, "metadata"],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.paystack.initializeTransaction({
+      email: this.email,
+      amount: this.amount,
+    });
+
+    $.export("$summary", `Transaction initialized`);
+    return response;
+  },
+};
---
+++ components/paystack/app/paystack.app.mjs
@@ -0,0 +1,69 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  type: "app",
+  app: "paystack",
+  propDefinitions: {
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email address of the customer to charge",
+    },
+    amount: {
+      type: "string",
+      label: "Amount",
+      description: "Amount to charge",
+    },
+    currency: {
+      type: "string",
+      label: "Currency",
+      description:
+        "Currency to use for the charge. Defaults to your integration currency",
+    },
+    reference: {
+      type: "string",
+      label: "Reference",
+      description:
+        "Unique transaction reference. Only alphanumeric characters and `-`, `.`, `=` are allowed",
+    },
+    callback_url: {
+      type: "string",
+      label: "Callback URL",
+      description:
+        "URL to redirect customers to after a successful transaction. Setting this overrides the callback URL set on the dashboard",
+    },
+    metadata: {
+      type: "object",
+      label: "Metadata",
+      description:
+        "Stringified JSON object of custom data. Check the [Metadata docs](https://paystack.com/docs/payments/metadata/) for more information",
+    },
+  },
+  methods: {
+    _baseUrl() {
+      return "https://api.paystack.co";
+    },
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+    async _makeAPIRequest({ $ = this, ...opts }) {
+      if (!opts.headers) opts.headers = {};
+      opts.headers["Authorization"] = `Bearer ${this.$auth.api_key}`;
+      opts.headers["Content-Type"] = "application/json";
+      opts.headers["user-agent"] = "@PaystackOSS/paystack v0.1";
+      const { path } = opts;
+      delete opts.path;
+      opts.url = `${this._baseUrl()}${path[0] === "/" ? "" : "/"}${path}`;
+      return axios($, opts);
+    },
+    async initializeTransaction(args = {}) {
+      return await this._makeAPIRequest({
+        method: "POST",
+        path: "/transaction/initialize",
+        data: {
+          ...args,
+        },
+      });
+    },
+  },
+};
