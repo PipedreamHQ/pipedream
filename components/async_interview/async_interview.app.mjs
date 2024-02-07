907c0bad5609688149cf5dad44ff3bdfd58efc71
Luan Cazarine <luanhc@gmail.com>
2024-02-07 10:30:06 -0500

    New Components - async_interview (#10240)
    * async_interview init
    
    * [Components] async-interview #10239
    Sources
     - New Interview Response
    
    * pnpm update
    
    * Update components/async_interview/sources/new-interview-response/new-interview-response.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/async_interview/async_interview.app.mjs
+++ components/async_interview/async_interview.app.mjs
@@ -1,11 +1,32 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "async_interview",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://app.asyncinterview.ai/api";
+    },
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.api_token}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    listInterviewResponses(opts = {}) {
+      return this._makeRequest({
+        path: "/interview_responses",
+        ...opts,
+      });
     },
   },
 };
