3205fa1d11f0ae7ea3deb378964336f7a0eb9bbd
Luan Cazarine <luanhc@gmail.com>
2024-02-08 12:24:51 -0300

    Update components/claid_ai/claid_ai.app.mjs
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/claid_ai/claid_ai.app.mjs
+++ components/claid_ai/claid_ai.app.mjs
@@ -9,32 +9,6 @@ export default {
       label: "Image URL",
       description: "The image URL or the path to a file in the `/tmp` directory. [See the documentation on working with files](https://pipedream.com/docs/code/nodejs/working-with-files/#writing-a-file-to-tmp).",
     },
-    operations: {
-      type: "object",
-      label: "Operations",
-      description: "The operations to be applied to the image.",
-    },
-    hdr: {
-      type: "boolean",
-      label: "HDR",
-      description: "Automatically adjust the color and lighting of an image by applying HDR.",
-      optional: true,
-      default: false,
-    },
-    removeBackground: {
-      type: "boolean",
-      label: "Remove Background",
-      description: "Erase the image's background, isolating the main subject.",
-      optional: true,
-      default: false,
-    },
-    upscale: {
-      type: "boolean",
-      label: "Upscale Image",
-      description: "Enlarge the image to improve its resolution.",
-      optional: true,
-      default: false,
-    },
   },
   methods: {
     _baseUrl() {
