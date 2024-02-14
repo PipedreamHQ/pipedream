204d738eba040a1276205c0949e9fe99d09bad2d
GTFalcao <gtfalcao96@gmail.com>
2024-02-14 20:37:22 -0300

    Add Customer adjustments

--- components/reputation_lyncs/actions/add-customer/add-customer.mjs
+++ components/reputation_lyncs/actions/add-customer/add-customer.mjs
@@ -1,3 +1,4 @@
+import { ConfigurationError } from "@pipedream/platform";
 import reputationLyncs from "../../reputation_lyncs.app.mjs";
 
 export default {
@@ -5,7 +6,7 @@ export default {
   name: "Add Customer",
   description:
     "Adds a new customer to the Reputation Lyncs platform. Requires the customer's full name. Optionally, an email and a phone number can be provided.",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
     reputationLyncs,
@@ -45,13 +46,18 @@ export default {
       this.whatsappEnabled === undefined
         ? undefined
         : Number(this.whatsappEnabled);
+
+    if (!this.email && !this.phoneNumber) {
+      throw new ConfigurationError("You must provide at least one of **Email** or **Phone Number**");
+    }
+
     const response = await this.reputationLyncs.addCustomer({
       $,
       data: {
-        fullName: this.customerName,
-        email: this.email,
-        phone: this.phoneNumber,
-        whatsappEnabled,
+        customer_name: this.customerName,
+        email_id: this.email,
+        phone_number: this.phoneNumber,
+        whatsapp_enabled: whatsappEnabled,
         tags: this.tags,
       },
     });
