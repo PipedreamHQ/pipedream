f0d8747d801e97b84f47feb39d9ff573f328424e
GTFalcao <gtfalcao96@gmail.com>
2024-02-15 20:29:20 -0300

    Adjustment

--- components/hubspot/actions/create-ticket/create-ticket.mjs
+++ components/hubspot/actions/create-ticket/create-ticket.mjs
@@ -8,9 +8,6 @@ export default {
   description: "Create a ticket in Hubspot. [See the documentation](https://developers.hubspot.com/docs/api/crm/tickets)",
   version: "0.0.1",
   type: "action",
-  props: {
-    ...common.props,
-  },
   methods: {
     ...common.methods,
     getObjectType() {
