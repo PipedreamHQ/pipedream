7d85b805222729379ba516349c4335ae561a5be1
michelle0927 <michelle0927@users.noreply.github.com>
2024-02-19 08:27:02 -0800

    New Components - appointedd (#10280)
    * apppointedd init
    
    * new components
    
    * pnpm-lock.yaml
    
    * make customerId optional
    
    * pnpm-lock.yaml

--- components/appointedd/appointedd.app.mjs
+++ components/appointedd/appointedd.app.mjs
@@ -1,11 +1,93 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "appointedd",
-  propDefinitions: {},
+  propDefinitions: {
+    customerId: {
+      type: "string",
+      label: "Customer ID",
+      description: "Filters the returned bookings by customer ID, returning only those that have a customer ID that matches the customer ID here.",
+      optional: true,
+      async options({ prevContext }) {
+        const params = prevContext?.next
+          ? {
+            start: next,
+          }
+          : {};
+        const {
+          data, next,
+        } = await this.listCustomers({
+          params,
+        });
+        const options = data?.map(({
+          id: value, profile,
+        }) => ({
+          value,
+          label: `${profile.firstname} ${profile.lastname}`,
+        })) || [];
+        return {
+          options,
+          context: {
+            next,
+          },
+        };
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.appointedd.com/v1";
+    },
+    _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "X-API-KEY": `${this.$auth.api_key}`,
+        },
+      });
+    },
+    listCustomers(opts = {}) {
+      return this._makeRequest({
+        path: "/customers",
+        ...opts,
+      });
+    },
+    listBookings(opts = {}) {
+      return this._makeRequest({
+        path: "/bookings",
+        ...opts,
+      });
+    },
+    async *paginate({
+      resourceFn,
+      params,
+      max,
+    }) {
+      let count = 0;
+      let totalItems = 0;
+      do {
+        const {
+          data, total, next,
+        } = await resourceFn({
+          params,
+        });
+        for (const item of data) {
+          yield item;
+          count++;
+          if (max && count >= max) {
+            return;
+          }
+        }
+        params.start = next;
+        totalItems = total;
+      } while (count < totalItems);
     },
   },
 };
