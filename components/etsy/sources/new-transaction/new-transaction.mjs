6131c95afa717cd584616ae1ac74ca106605ac17
Andrew Chuang <andrewjschuang@gmail.com>
2024-02-19 18:08:08 -0300

    bump versions

--- components/etsy/sources/new-transaction/new-transaction.mjs
+++ components/etsy/sources/new-transaction/new-transaction.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Transaction",
   description: "Emit new event when a new transaction is created. [See the Documentation](https://developers.etsy.com/documentation/reference#operation/getShopReceiptTransactionsByShop)",
   type: "source",
-  version: "0.0.1",
+  version: "0.0.2",
   dedupe: "unique",
   methods: {
     ...common.methods,
