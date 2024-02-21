64a9d743b734b9f1b7a08fd574f70eb9c6891b8e
GTFalcao <gtfalcao96@gmail.com>
2024-02-21 19:47:28 -0300

    Adding props to new action

--- components/coinmarketcap/actions/get-cryptocurrency-metadata/get-cryptocurrency-metadata.mjs
+++ components/coinmarketcap/actions/get-cryptocurrency-metadata/get-cryptocurrency-metadata.mjs
@@ -9,11 +9,44 @@ export default {
   type: "action",
   props: {
     coinmarketcap,
+    ids: {
+      type: "string[]",
+      label: "Cryptocurrency IDs",
+      description: "One or more cryptocurrency CoinMarketCap IDs. You can use the **Get ID Map** action to list cryptocurrency IDs.",
+    },
+    skipInvalid: {
+      type: "boolean",
+      label: "Skip Invalid",
+      description: "When requesting records on multiple cryptocurrencies, an error is returned if any invalid cryptocurrencies are requested or a cryptocurrency does not have matching records in the requested timeframe. If set to `true`, invalid lookups will be skipped allowing valid cryptocurrencies to still be returned.",
+      optional: true,
+      default: false,
+    },
+    aux: {
+      propDefinition: [
+        coinmarketcap,
+        "aux",
+      ],
+      options: [
+        "urls",
+        "logo",
+        "description",
+        "tags",
+        "platform",
+        "date_added",
+        "notice",
+        "status",
+      ],
+    },
   },
   async run({ $ }) {
     const response = await this.coinmarketcap._makeRequest({
       $,
       url: "/v2/cryptocurrency/info",
+      params: {
+        id: this.ids.join(),
+        skip_invalid: this.skipInvalid,
+        aux: this.auw,
+      },
     });
     $.export("$summary", "Successfully retrieved metadata");
     return response;
