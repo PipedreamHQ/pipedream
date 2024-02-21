e5e3479ef676bbb2be715a5fadce0a0cc08fb342
Dylan J. Sather <dylan.sather@gmail.com>
2024-02-20 16:12:48 -0800

    Adding Vercel analytics, fixing concurrency and throttling docs

--- docs-v2/pages/_app.tsx
+++ docs-v2/pages/_app.tsx
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({
   Component, pageProps,
 }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>;
 }
