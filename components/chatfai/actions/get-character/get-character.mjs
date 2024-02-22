2ca3bdcceb74136902bf416f667d03d9be42c9fc
Lucas Caresia <lucascarezia@gmail.com>
2024-02-22 10:16:20 -0300

    Added actions

---
+++ components/chatfai/actions/get-character/get-character.mjs
@@ -0,0 +1,26 @@
+import app from "../../chatfai.app.mjs";
+
+export default {
+  key: "chatfai-get-character",
+  name: "Get Public Character by ID",
+  description: "Gets a public character by ID from ChatFAI. [See the documentation](https://chatfai.com/developers/docs#tag/characters/paths/~1characters~1{id}/get)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    characterId: {
+      propDefinition: [
+        app,
+        "characterId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getCharacter({
+      $,
+      id: this.characterId,
+    });
+    $.export("$summary", `Successfully retrieved the character with ID ${this.characterId}`);
+    return response;
+  },
+};
