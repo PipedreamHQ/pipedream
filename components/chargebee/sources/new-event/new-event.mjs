b0d2a6368bfbe6fb9fb1c02700ecd2430bae7a03
Luan Cazarine <luanhc@gmail.com>
2024-02-26 11:47:28 -0500

    [TRIGGER] Chargebee: New Triggers #10472 Sources  - New Event  - New Invoice Updated  - Payment Refunded  - Subscription Activated  - Subscription Renewed

---
+++ components/chargebee/sources/new-event/new-event.mjs
@@ -0,0 +1,42 @@
+import constants from "../../common/constants.mjs";
+import events from "../common/events.mjs";
+import common from "../common/webhook.mjs";
+
+export default {
+  ...common,
+  key: "chargebee-new-event",
+  name: "New Event",
+  description: "Emit new event when the selected event is triggered. [See the Documentation](https://apidocs.chargebee.com/docs/api/events). Please make sure once you deploy this source, you copy/paste the webhook URL to create it in your [Chargebee Webhook settings](https://www.chargebee.com/docs/2.0/webhook_settings.html#configure-webhooks).",
+  type: "source",
+  version: "0.0.1",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    eventType: {
+      type: "string[]",
+      label: "Event Type",
+      description: "Select the type of the event you want to receive.",
+      options: Object.keys(events).map((key) => events[key]),
+    },
+  },
+  methods: {
+    ...common.methods,
+    getEventTypes() {
+      return this.eventType;
+    },
+    async getResources() {
+      const { list } = await this.app.getInvoices({
+        "limit": constants.MAX_LIMIT,
+        "sort_by[desc]": "date",
+      });
+      return list;
+    },
+    generateMeta(event) {
+      return {
+        id: event.id,
+        summary: `New Event Triggered: ${event.id}`,
+        ts: event.occurred_at,
+      };
+    },
+  },
+};
---
+++ components/chargebee/sources/new-invoice-updated-instant/new-invoice-updated-instant.mjs
@@ -0,0 +1,36 @@
+import constants from "../../common/constants.mjs";
+import events from "../common/events.mjs";
+import common from "../common/webhook.mjs";
+
+export default {
+  ...common,
+  key: "chargebee-new-invoice-updated-instant",
+  name: "New Invoice Updated (Instant)",
+  description: "Emit new event when a new invoice is updated. [See the Documentation](https://apidocs.chargebee.com/docs/api/events#invoice_updated). Please make sure once you deploy this source, you copy/paste the webhook URL to create it in your [Chargebee Webhook settings](https://www.chargebee.com/docs/2.0/webhook_settings.html#configure-webhooks).",
+  type: "source",
+  version: "0.0.1",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEventTypes() {
+      return [
+        events.INVOICE_GENERATED,
+      ];
+    },
+    async getResources() {
+      const { list } = await this.app.getInvoices({
+        "limit": constants.MAX_LIMIT,
+        "sort_by[desc]": "date",
+      });
+      return list;
+    },
+    generateMeta(event) {
+      const { invoice } = event.content ?? event;
+      return {
+        id: invoice.id,
+        summary: `New Invoice Updated: ${invoice.id}`,
+        ts: invoice.updated_at,
+      };
+    },
+  },
+};
