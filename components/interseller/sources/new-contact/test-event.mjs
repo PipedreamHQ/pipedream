9c8eb5c1af45e5e8d65ec3ce8d7a024ff79f0f8c
Luan Cazarine <luanhc@gmail.com>
2024-02-27 10:13:38 -0500

    New Components - interseller (#10500)
    * interseller init
    
    * [Components] Interseller #3659
    Sources
     - New Contact Replied
     - New Contact
    
    Actions
     - Update Contact
     - Set Contact Replied
    
    * pnpm update
    
    * some adjusts
    
    * Update components/interseller/sources/common/base.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/interseller/sources/new-contact/test-event.mjs
@@ -0,0 +1,59 @@
+export default {
+  "links": [],
+  "view_count": 0,
+  "steps_customized": [],
+  "_id": "586847773fdaa1332c918379",
+  "updated_at": "2019-07-03T01:10:38.885Z",
+  "created_at": "2017-01-01T00:04:07.269Z",
+  "_campaign": "5868476217adb832a2008f2b",
+  "_user": "584973302aa16b00115c5614",
+  "name": "Steven Lu",
+  "company": "Perf Labs",
+  "email": "stevenlu@perf.sh",
+  "valid": {
+    "verdict": "GOOD",
+    "details": {
+      "mx_accepted_address": true,
+      "is_free_service": false,
+      "is_disposable": false,
+      "accepts_all_addresses": false,
+      "mx_reachable": true,
+      "valid_mx_records": true,
+      "dns_reachable": true,
+      "valid_format": true
+    },
+    "email": "stevenlu@perf.sh"
+  },
+  "last_messaged_at": "2017-01-09T14:04:09.534Z",
+  "step": 2,
+  "viewed_at": null,
+  "validated_at": "2017-06-27T16:00:33.816Z",
+  "email_domain": "perf.sh",
+  "indexed_at": "2017-12-28T00:20:45.508Z",
+  "last_action_at": "2019-06-04T18:25:00.794Z",
+  "touched_at": "2019-07-03T01:10:38.885Z",
+  "first_messaged_at": "2017-01-02T14:09:53.836Z",
+  "linkedin_positions": [],
+  "unsubscribed": false,
+  "handle": "stevenlu",
+  "state": "messaged",
+  "domain": "perf.sh",
+  "finished": false,
+  "validity": "GOOD",
+  "parsed_name": {
+    "first": "Steven",
+    "last": "Lu",
+    "full": "Steven Lu",
+    "original": "Steven Lu"
+  },
+  "delayed": false,
+  "next_step": 3,
+  "synced_to": [],
+  "is_free_email": false,
+  "parsed_location": {
+    "city": null,
+    "state": null,
+    "country": null
+  },
+  "id": "586847773fdaa1332c918379"
+}
