f6840a1e9ac56995682351e2f6bf6953bbaf8029
michelle0927 <michelle0927@users.noreply.github.com>
2024-02-28 10:03:06 -0500

    New Components - heedjy (#10611)
    * heedjy init
    
    * new source component
    
    * pnpm-lock.yaml

---
+++ components/heedjy/sources/new-app-published/test-event.mjs
@@ -0,0 +1,55 @@
+export default {
+  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "version": 0,
+  "version_agg_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "definition_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "client_id": "string",
+  "name": "string",
+  "type": "string",
+  "slug": "string",
+  "logo": "string",
+  "status": "DRAFT",
+  "company": {
+    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "logo": "string",
+    "name": "string",
+    "description": "string",
+    "marketplace_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "subscription": "PREMIUM",
+    "partner_since": "2023-01-20T14:50:23.000Z",
+    "badge_logo": "string",
+    "badge_label": "string",
+    "badge_name": "string"
+  },
+  "description": "string",
+  "creator_id": "string",
+  "created_on": "2023-01-20T14:50:23.000Z",
+  "updated_on": "2023-01-20T14:50:23.000Z",
+  "submitted_on": "2023-01-20T14:50:23.000Z",
+  "first_published_on": "2023-01-20T14:50:23.000Z",
+  "in_status_since": "2023-01-20T14:50:23.000Z",
+  "categories": [
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "string",
+      "label": "string"
+    }
+  ],
+  "tags": [
+    "CERTIFIED"
+  ],
+  "custom_properties": [
+    {
+      "id": "string",
+      "name": "string",
+      "value": {}
+    }
+  ],
+  "has_version": true,
+  "stats": {
+    "activation_count": 42,
+    "review_rating": 4.34,
+    "review_count": 42
+  },
+  "has_required_change": true
+}
