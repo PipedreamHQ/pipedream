a3edfdacda397bdfd02df063f481102acc182d6d
Jorge Cortes <jacortesmahmud@gmail.com>
2024-02-29 09:49:17 -0500

    New Components - 46elks (#10569)
    * 46elks init
    
    * Components tested

---
+++ components/_46elks/common/constants.mjs
@@ -0,0 +1,11 @@
+const BASE_URL = "https://api.46elks.com";
+const VERSION_PATH = "/a1";
+const LAST_CREATED_AT = "lastCreatedAt";
+const DEFAULT_MAX = 600;
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  DEFAULT_MAX,
+  LAST_CREATED_AT,
+};
