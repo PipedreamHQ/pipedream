6ca2e729c94aa3605e1b7d5c3727ed7fbe68e753
Lucas Caresia <lucascarezia@gmail.com>
2024-02-29 14:03:17 -0300

    New Components - keysender (#10620)
    * keysender init
    
    * new component
    
    * pnpm-lock.yaml
    
    * validate amount
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

--- components/keysender/keysender.app.mjs
+++ components/keysender/keysender.app.mjs
@@ -1,11 +1,59 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "keysender",
-  propDefinitions: {},
+  propDefinitions: {
+    databaseId: {
+      type: "integer",
+      label: "Database ID",
+      description: "The database ID from which codes will be taken.",
+      async options({ page }) {
+        const databases = await this.listDatabases({
+          params: {
+            page,
+          },
+        });
+        return databases?.map(({
+          id: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://panel.keysender.co.uk/api/v1.0";
+    },
+    _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+          "Content-Type": "application/json",
+        },
+      });
+    },
+    listDatabases(opts = {}) {
+      return this._makeRequest({
+        path: "/databases",
+        ...opts,
+      });
+    },
+    createTransaction(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/transaction/addcustom",
+        ...opts,
+      });
     },
   },
 };
