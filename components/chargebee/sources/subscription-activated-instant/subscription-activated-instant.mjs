bc2c11b924b5cfc7c85d2fff18bd3f9e91bab5a9
Luan Cazarine <luanhc@gmail.com>
2024-03-01 11:20:05 -0500

    [TRIGGER] Chargebee: New Triggers #10472 (#10600)
    * [TRIGGER] Chargebee: New Triggers #10472
    Sources
     - New Event
     - New Invoice Updated
     - Payment Refunded
     - Subscription Activated
     - Subscription Renewed
    
    * some adjusts
    
    * Automatically updating actions and sources versions
    
    * fix param
    
    ---------
    
    Co-authored-by: Pipedream [Bot] <pipedream@pipedream.com>

--- components/chargebee/sources/subscription-activated-instant/subscription-activated-instant.mjs
+++ components/chargebee/sources/subscription-activated-instant/subscription-activated-instant.mjs
@@ -1,11 +1,11 @@
-import common from "../common/webhook.mjs";
-import events from "../common/events.mjs";
 import constants from "../../common/constants.mjs";
+import events from "../common/events.mjs";
+import common from "../common/webhook.mjs";
 
 export default {
   ...common,
-  key: "chargebee-supscription-activated-instant",
-  name: "Supscription Activated (Instant)",
+  key: "chargebee-subscription-activated-instant",
+  name: "New Subscription Activated (Instant)",
   description: "Emit new event when a subscription is activated. [See the Documentation](https://apidocs.chargebee.com/docs/api/events#subscription_activated). Please make sure once you deploy this source, you copy/paste the webhook URL to create it in your [Chargebee Webhook settings](https://www.chargebee.com/docs/2.0/webhook_settings.html#configure-webhooks).",
   type: "source",
   version: "0.0.1",
