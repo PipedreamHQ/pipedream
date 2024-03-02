fcee90e3bc721d8c4bb1cb98be6e80d08f0f1f04
GTFalcao <gtfalcao96@gmail.com>
2024-03-01 23:22:47 -0300

    Get Account Info action

---
+++ components/commpeak/actions/get-account-info/get-account-info.mjs
@@ -0,0 +1,19 @@
+import commpeak from "../../commpeak.app.mjs";
+
+export default {
+  key: "commpeak-get-account-info",
+  name: "Get Account Info",
+  description: "Get account information such as balance and tariffs. [See the documentation](https://lookup.api-docs.commpeak.com/?_gl=1*50xs02*_gcl_au*MTMxMzgzMzA3Ny4xNjk3NTY0NDE3#84b40f09-cf1e-4729-a556-4da44c6f6ade)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    commpeak,
+  },
+  async run({ $ }) {
+    const response = await this.commpeak.getAccountInfo({
+      $,
+    });
+    $.export("$summary", "Successfully obtained account info");
+    return response;
+  },
+};
