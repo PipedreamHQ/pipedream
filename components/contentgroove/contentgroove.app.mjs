6a7bfddb2b3079a34d10f5213d537b82f3336a7f
Luan Cazarine <luanhc@gmail.com>
2024-03-04 13:56:35 -0500

    New Components - contentgroove (#10653)
    * contentgroove init
    
    * [Components] contentgroove #10571
    Sources
     - New Media Processing Finished (Instant)
    
    * pnpm update

--- components/contentgroove/contentgroove.app.mjs
+++ components/contentgroove/contentgroove.app.mjs
@@ -1,11 +1,45 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "contentgroove",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.contentgroove.com/api/v1";
+    },
+    _headers() {
+      return {
+        "Authorization": `${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    createWebhook(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/webhook_subscriptions",
+        ...opts,
+      });
+    },
+    deleteWebhook(webhookId) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/webhook_subscriptions/${webhookId}`,
+      });
+    },
+    listProcessedMedia(opts = {}) {
+      return this._makeRequest({
+        path: "/medias",
+        ...opts,
+      });
     },
   },
 };
