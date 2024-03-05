721f6f7bdae7f0ed1b5fb288176383f4a3f5d809
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2024-03-05 05:48:45 +0300

    New Paystack Actions (#10696)
    * add verify transaction action
    * add list transactions action
    * add fetch transaction action
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>
    Co-authored-by: Leo Vu <Leo_Vu@mckinsey.com>

---
+++ components/paystack/actions/fetch-transaction/fetch-transaction.mjs
@@ -0,0 +1,27 @@
+import paystack from "../../paystack.app.mjs";
+
+export default {
+  key: "paystack-fetch-transaction",
+  name: "Fetch Transaction",
+  description: "Fetch a single transaction. [See the documentation](https://paystack.com/docs/api/transaction/#fetch)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    paystack,
+    transactionID: {
+      propDefinition: [
+        paystack,
+        "transactionID",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.paystack.fetchTransaction({
+      $,
+      transactionID: this.transactionID,
+    });
+
+    $.export("$summary", `Successfully fetched transaction with ID ${response.data.id}`);
+    return response;
+  },
+};
