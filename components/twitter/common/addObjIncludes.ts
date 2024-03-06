c3a1f1a2cb2e8ee7d2e05b1b8ff451a581044066
GTFalcao <gtfalcao96@gmail.com>
2024-03-06 17:49:28 -0300

    Get Tweet Includes structure

--- components/twitter/common/addObjIncludes.ts
+++ components/twitter/common/addObjIncludes.ts
@@ -1,35 +1,58 @@
-import { ResponseIncludes, Tweet, TwitterEntity } from "./types/responseSchemas";
+import {
+  ResponseIncludes,
+  Tweet,
+  TwitterEntity,
+} from "./types/responseSchemas";
 
 // https://developer.twitter.com/en/docs/twitter-api/expansions
 
-export function getTweetIncludes(obj: Tweet, includes: ResponseIncludes) {
-  const userIds = [
-    obj.author_id,
-    obj.in_reply_to_user_id,
-  ];
+function getTweetIncludeIds(obj: Tweet) {
+  const userIds = [obj.author_id, obj.in_reply_to_user_id];
 
-  const userNames = [
-    obj.entities?.mentions?.map(({ username }) => username)
-  ]
+  const userNames = [obj.entities?.mentions?.map(({ username }) => username)];
 
   const tweetIds = [
     obj.edit_history_tweet_ids,
     obj.referenced_tweets?.map(({ id }) => id),
-  ]
+  ];
 
-  const pollIds = [
-    obj.attachments?.poll_ids,
-  ]
+  const pollIds = [obj.attachments?.poll_ids];
 
-  const placeIds = [
-    obj.geo?.place_id,
-  ]
+  const placeIds = [obj.geo?.place_id];
 
-  const mediaKeys = [
-    obj.attachments?.media_keys,
-  ]
-  
-  //.filter(e => e !== undefined).flat();
+  const mediaKeys = [obj.attachments?.media_keys];
+
+  return Object.fromEntries(
+    Object.entries({
+      userIds,
+      userNames,
+      tweetIds,
+      pollIds,
+      placeIds,
+      mediaKeys,
+    }).map(([key, value]) => [key, value.filter((e) => e !== undefined).flat()])
+  );
+}
+
+export function getTweetIncludes(obj: Tweet, includes: ResponseIncludes) {
+  const result: ResponseIncludes = {};
+
+  const { userIds, userNames, tweetIds, pollIds, placeIds, mediaKeys } =
+    getTweetIncludeIds(obj);
+
+  result.media = includes.media?.filter((item) =>
+    mediaKeys.includes(item.media_key)
+  );
+  result.users = includes.users?.filter(
+    (item) => userIds.includes(item.id) || userNames.includes(item.username)
+  );
+  result.places = includes.places?.filter((item) => placeIds.includes(item.id));
+  result.polls = includes.polls?.filter((item) => pollIds.includes(item.id));
+  result.tweets = includes.tweets?.filter((item) => tweetIds.includes(item.id));
+
+  return Object.fromEntries(
+    Object.entries(result).filter(([, value]) => value?.length > 0)
+  );
 }
 
 export function getObjIncludes(obj: TwitterEntity, includes: ResponseIncludes) {
