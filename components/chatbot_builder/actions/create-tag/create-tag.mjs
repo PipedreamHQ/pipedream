162f2ae6e2f1290172a955de6b52feb3954e1654
Leo Vu <vunguyenhung@outlook.com>
2024-03-07 11:36:12 +0700

    Fix version string

--- components/chatbot_builder/actions/create-tag/create-tag.mjs
+++ components/chatbot_builder/actions/create-tag/create-tag.mjs
@@ -5,7 +5,7 @@ export default {
   key: "chatbot_builder-create-tag",
   name: "Create Tag",
   description: "Creates a new tag in Chatbot Builder. [See the documentation](https://app.chatgptbuilder.io/api/swagger/#/accounts/createtag)",
-  version: `0.0.1`,
+  version: "0.0.1",
   type: "action",
   props: {
     app,
