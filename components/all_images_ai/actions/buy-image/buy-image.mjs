e4e2b37cd79d1cd57ce714b6cc96bc20a8a28db2
Luan Cazarine <luanhc@gmail.com>
2024-03-07 13:51:09 -0500

    New Components - all_images_ai (#10749)
    * all_images_ai init
    
    * [Components] all_images_ai #10705
    Sources
     - New Generation Update Instant
    
    Actions
     - Generate Image Advanced
     - Get Image Generation
     - Buy Image
    
    * pnpm update
    
    * add doc links
    
    * pnpm-lock.yaml
    
    * some adjusts
    
    * fix var name
    
    * pnpm update
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/all_images_ai/actions/buy-image/buy-image.mjs
@@ -0,0 +1,29 @@
+import allImagesAi from "../../all_images_ai.app.mjs";
+
+export default {
+  key: "all_images_ai-buy-image",
+  name: "Buy Image",
+  description: "Allows user to purchase an image and receive a direct public link. User must have sufficient credit balance. [See the documentation](https://developer.all-images.ai/all-images.ai-api/api-reference/images#buy-image-return-direct-url)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    allImagesAi,
+    imageId: {
+      propDefinition: [
+        allImagesAi,
+        "imageId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.allImagesAi.purchaseImage({
+      $,
+      data: {
+        id: this.imageId,
+      },
+    });
+
+    $.export("$summary", `Successfully purchased image. Public link: ${response.url}`);
+    return response;
+  },
+};
