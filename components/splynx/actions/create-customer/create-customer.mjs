c276128aa8daac63f17a5f9245c5026b880e7bbc
GTFalcao <gtfalcao96@gmail.com>
2024-03-12 00:09:00 -0300

    Create Customer + props rehaul

--- components/splynx/actions/create-customer/create-customer.mjs
+++ components/splynx/actions/create-customer/create-customer.mjs
@@ -3,20 +3,112 @@ import splynx from "../../splynx.app.mjs";
 export default {
   key: "splynx-create-customer",
   name: "Create Customer",
-  description: "Creates a new customer with provided details including personal information and contact details. Unique identification details and contact information are required.",
+  description:
+    "Creates a new customer with the provided details. [See the documentation](https://splynx.docs.apiary.io/#reference/customers/customers-collection/create-a-customer)",
   version: "0.0.1",
   type: "action",
   props: {
     splynx,
-    personalInformation: splynx.propDefinitions.personalInformation,
-    contactDetails: splynx.propDefinitions.contactDetails,
+    name: {
+      propDefinition: [
+        splynx,
+        "name",
+      ],
+    },
+    partnerId: {
+      propDefinition: [
+        splynx,
+        "partnerId",
+      ],
+    },
+    locationId: {
+      propDefinition: [
+        splynx,
+        "locationId",
+      ],
+    },
+    category: {
+      propDefinition: [
+        splynx,
+        "category",
+      ],
+    },
+    login: {
+      propDefinition: [
+        splynx,
+        "login",
+      ],
+    },
+    status: {
+      propDefinition: [
+        splynx,
+        "status",
+      ],
+    },
+    email: {
+      propDefinition: [
+        splynx,
+        "email",
+      ],
+    },
+    billingEmail: {
+      propDefinition: [
+        splynx,
+        "billingEmail",
+      ],
+    },
+    phone: {
+      propDefinition: [
+        splynx,
+        "phone",
+      ],
+    },
+    street: {
+      propDefinition: [
+        splynx,
+        "street",
+      ],
+    },
+    zipCode: {
+      propDefinition: [
+        splynx,
+        "zipCode",
+      ],
+    },
+    city: {
+      propDefinition: [
+        splynx,
+        "city",
+      ],
+    },
+    dateAdd: {
+      propDefinition: [
+        splynx,
+        "dateAdd",
+      ],
+    },
   },
   async run({ $ }) {
-    const response = await this.splynx.createCustomer({
-      personalInformation: this.personalInformation,
-      contactDetails: this.contactDetails,
+    const {
+      splynx, partnerId, locationId, billingEmail, street, zipCode, dateAdd, ...data
+    } = this;
+
+    const response = await splynx.createCustomer({
+      $,
+      data: {
+        ...data,
+        partner_id: partnerId,
+        location_id: locationId,
+        billing_email: billingEmail,
+        street_1: street,
+        zip_code: zipCode,
+        date_add: dateAdd,
+      },
     });
-    $.export("$summary", `Successfully created customer with ID: ${response.id}`);
+    $.export(
+      "$summary",
+      `Successfully created customer "${this.name}" (ID: ${response.id})`,
+    );
     return response;
   },
 };
