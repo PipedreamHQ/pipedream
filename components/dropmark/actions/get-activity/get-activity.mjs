bbc59bad3682218d37f964e921e78e3f671a4706
michelle0927 <michelle0927@users.noreply.github.com>
2024-03-12 10:21:01 -0400

    New Components - dropmark (#10859)
    * dropmark init
    
    * new components
    
    * pnpm-lock.yaml
    
    * fix

---
+++ components/dropmark/actions/get-activity/get-activity.mjs
@@ -0,0 +1,19 @@
+import dropmark from "../../dropmark.app.mjs";
+
+export default {
+  key: "dropmark-get-activity",
+  name: "Get Activity",
+  description: "Retrieves a blended feed of newly created collections, items, comments, and reactions. [See the documentation](https://support.dropmark.com/article/96-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    dropmark,
+  },
+  async run({ $ }) {
+    const response = await this.dropmark.getActivityFeed({
+      $,
+    });
+    $.export("$summary", "Successfully retrieved activity feed");
+    return response;
+  },
+};
--- components/dropmark/dropmark.app.mjs
+++ components/dropmark/dropmark.app.mjs
@@ -1,11 +1,47 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "dropmark",
-  propDefinitions: {},
+  propDefinitions: {
+    collectionId: {
+      type: "string",
+      label: "Collection ID",
+      description: "The ID of the collection you want to retrieve",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://${this.$auth.username}.dropmark.com`;
+    },
+    _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: `${this._baseUrl()}${path}`,
+        auth: {
+          username: this.$auth.username,
+          password: this.$auth.password,
+        },
+      });
+    },
+    getCollectionItems({
+      collectionId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/${collectionId}.json`,
+        ...opts,
+      });
+    },
+    getActivityFeed(opts = {}) {
+      return this._makeRequest({
+        path: "/activity.json",
+        ...opts,
+      });
     },
   },
 };
