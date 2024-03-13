6e895679ac5015f2a2c1b99da811a828d40d1192
Jorge Cortes <jacortesmahmud@gmail.com>
2024-03-13 12:32:40 -0500

    New Components - chatpdf (#10866)
    * chatpdf init
    
    * Tested components

--- components/chatpdf/chatpdf.app.mjs
+++ components/chatpdf/chatpdf.app.mjs
@@ -1,11 +1,34 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "chatpdf",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `https://api.chatpdf.com/v1${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Content-Type": "application/json",
+        "x-api-key": this.$auth.api_key,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      const config = {
+        ...args,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      };
+      return axios($, config);
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
