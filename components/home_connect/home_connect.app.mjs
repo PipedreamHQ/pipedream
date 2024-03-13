09fbb75d48f4af0176f349b466a712bc9119119f
Lucas Caresia <lucascarezia@gmail.com>
2024-03-13 12:23:17 -0300

    New Components - home_connect (#10761)
    * home_connect init
    
    * Added actions
    
    * Fixed changes

--- components/home_connect/home_connect.app.mjs
+++ components/home_connect/home_connect.app.mjs
@@ -1,11 +1,66 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "home_connect",
-  propDefinitions: {},
+  propDefinitions: {
+    haId: {
+      type: "string",
+      label: "Home Appliance ID",
+      description: "The unique identifier of the home appliance.",
+      async options() {
+        const { data: { homeappliances: resources } } = await this.getAppliances();
+
+        return resources.map(({
+          haId, name,
+        }) => ({
+          value: haId,
+          label: name,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://${this.$auth.host_environment}/api`;
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async getAppliances(args = {}) {
+      return this._makeRequest({
+        path: "/homeappliances",
+        ...args,
+      });
+    },
+    async getApplianceStatus({
+      haId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/homeappliances/${haId}/status`,
+        ...args,
+      });
+    },
+    async getAvailablePrograms({
+      haId, ...args
+    }) {
+      return this._makeRequest({
+        path: `/homeappliances/${haId}/programs/available`,
+        ...args,
+      });
     },
   },
 };
