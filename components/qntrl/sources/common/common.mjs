80ac42056a5ef5f06b9cb98494d35e3b4a9e1b28
GTFalcao <gtfalcao96@gmail.com>
2024-03-14 03:14:32 -0300

    Common code for sources

---
+++ components/qntrl/sources/common/common.mjs
@@ -0,0 +1,48 @@
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+import app from "../../qntrl.app.mjs";
+
+export default {
+  props: {
+    app,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  hooks: {
+    async deploy() {
+      await this.getAndProcessData(false);
+    },
+  },
+  methods: {
+    _getSavedIds() {
+      return this.db.get("savedIds") ?? [];
+    },
+    _setSavedIds(value) {
+      this.db.set("savedIds", value);
+    },
+    async getAndProcessData(emit = true) {
+      const savedIds = this.getSavedIds();
+      const items = await this.getItems();
+      const ts = Date.now();
+      items?.filter(({ id }) => !savedIds.includes(id)).forEach((item) => {
+        const { id } = item;
+        if (emit) {
+          this.$emit(item, {
+            id,
+            summary: this.getSummary(item),
+            ts,
+          });
+        }
+        savedIds.push(id);
+      });
+      this._setSavedIds(savedIds);
+    },
+  },
+  async run() {
+    await this.getAndProcessData();
+  },
+};
