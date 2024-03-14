f27507be79fb7e09ca55f8bdc681510fb5bb847a
GTFalcao <gtfalcao96@gmail.com>
2024-03-14 00:41:55 -0300

    Post Comment + adjustmnets

--- components/qntrl/actions/post-comment/post-comment.mjs
+++ components/qntrl/actions/post-comment/post-comment.mjs
@@ -3,35 +3,40 @@ import qntrl from "../../qntrl.app.mjs";
 export default {
   key: "qntrl-post-comment",
   name: "Post Comment",
-  description: "Posts a new comment under a specific job. The 'job id' and 'comment' props are required. Additional optional props are 'attachment' for any documents or files. [See the documentation](https://coreapi.qntrl.com/blueprint/api)",
+  description: "Posts a new comment under a specific job. [See the documentation](https://core.qntrl.com/apidoc.html?type=reference&module=jobs&action=PostComment)",
   version: "0.0.1",
   type: "action",
   props: {
     qntrl,
+    orgId: {
+      propDefinition: [
+        qntrl,
+        "orgId",
+      ],
+    },
     jobId: {
       propDefinition: [
         qntrl,
         "jobId",
       ],
     },
-    comment: {
-      propDefinition: [
-        qntrl,
-        "comment",
-      ],
-    },
-    attachment: {
-      propDefinition: [
-        qntrl,
-        "attachment",
-      ],
+    content: {
+      type: "string",
+      label: "Content",
+      description: "Comment to be posted.",
     },
   },
   async run({ $ }) {
-    const response = await this.qntrl.postComment({
-      jobId: this.jobId,
-      comment: this.comment,
-      attachment: this.attachment,
+    const {
+      qntrl, orgId, jobId, content,
+    } = this;
+    const response = await qntrl.postComment({
+      $,
+      orgId,
+      jobId,
+      data: {
+        content,
+      },
     });
 
     $.export("$summary", `Successfully posted a comment under job ID ${this.jobId}`);
--- components/qntrl/qntrl.app.mjs
+++ components/qntrl/qntrl.app.mjs
@@ -4,11 +4,6 @@ export default {
   type: "app",
   app: "qntrl",
   propDefinitions: {
-    jobId: {
-      type: "string",
-      label: "Job ID",
-      description: "The unique identifier for the job.",
-    },
     orgId: {
       type: "string",
       label: "Organization ID",
@@ -33,78 +28,17 @@ export default {
         }));
       },
     },
-    jobCreatorId: {
+    jobId: {
       type: "string",
-      label: "Job Creator ID",
-      description: "The unique identifier for the job creator.",
-    },
-    title: {
-      type: "string",
-      label: "Title",
-      description: "The title of the job.",
-    },
-    description: {
-      type: "string",
-      label: "Description",
-      description: "The description of the job.",
-    },
-    jobType: {
-      type: "string",
-      label: "Job Type",
-      description: "The type of the job.",
-      optional: true,
-    },
-    priority: {
-      type: "string",
-      label: "Priority",
-      description: "The priority of the job.",
-      optional: true,
-    },
-    username: {
-      type: "string",
-      label: "Username",
-      description: "The username of the new user.",
-    },
-    email: {
-      type: "string",
-      label: "Email",
-      description: "The email of the new user.",
-    },
-    firstName: {
-      type: "string",
-      label: "First Name",
-      description: "The first name of the new user.",
-      optional: true,
-    },
-    lastName: {
-      type: "string",
-      label: "Last Name",
-      description: "The last name of the new user.",
-      optional: true,
-    },
-    password: {
-      type: "string",
-      label: "Password",
-      description: "The password for the new user account.",
-      optional: true,
-      secret: true,
-    },
-    userRole: {
-      type: "string",
-      label: "User Role",
-      description: "The role of the new user.",
-      optional: true,
-    },
-    comment: {
-      type: "string",
-      label: "Comment",
-      description: "The comment to be posted under the job.",
-    },
-    attachment: {
-      type: "string",
-      label: "Attachment",
-      description: "The attachment to be included with the comment.",
-      optional: true,
+      label: "Job ID",
+      description: "Select a job (card), or provide a custom job ID.",
+      async options({ orgId }) {
+        const jobs = await this.listJobs(orgId);
+        return jobs?.map((job) => ({
+          label: job.title,
+          value: job.id,
+        }));
+      },
     },
   },
   methods: {
@@ -135,6 +69,11 @@ export default {
         url: `/${orgId}/layout`,
       });
     },
+    async listJobs(orgId) {
+      return this._makeRequest({
+        url: `/${orgId}/job`,
+      });
+    },
     async createJob({
       orgId, ...args
     }) {
@@ -152,11 +91,11 @@ export default {
       });
     },
     async postComment({
-      jobId, ...args
+      orgId, jobId, ...args
     }) {
       return this._makeRequest({
         method: "POST",
-        url: `/jobs/${jobId}/comments`,
+        url: `/${orgId}/job/${jobId}/comment`,
         ...args,
       });
     },
