dbf60e97e92beeaeb081d65ad559d2d6131217a5
casret <github@casret.com>
2024-03-14 20:00:37 -0700

    Update snowflake.app.mjs (#10985)
    * Update snowflake.app.mjs
    
    Correct this typo.
    
    * bump versions
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

--- components/snowflake/sources/new-database/new-database.mjs
+++ components/snowflake/sources/new-database/new-database.mjs
@@ -7,7 +7,7 @@ export default {
   key: "snowflake-new-database",
   name: "New Database",
   description: "Emit new event when a database is created",
-  version: "0.0.8",
+  version: "0.0.9",
   methods: {
     ...common.methods,
     alwaysRunInSingleProcessMode() {
