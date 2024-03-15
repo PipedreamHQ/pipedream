e6aeadc1f187dbe12bf9f11157064d12290910ab
Jorge Cortes <jacortesmahmud@gmail.com>
2024-03-15 12:39:14 -0500

    New Components - shorten_rest (#10884)
    * shorten_rest init
    
    * Tested components

---
+++ components/shorten_rest/actions/get-clicks/get-clicks.mjs
@@ -0,0 +1,29 @@
+import app from "../../shorten_rest.app.mjs";
+
+export default {
+  key: "shorten_rest-get-clicks",
+  name: "Get Clicks",
+  description: "Gets the click data. [See the documentation](https://docs.shorten.rest/#tag/Click/operation/GetClicks)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  methods: {
+    getClicks(args = {}) {
+      return this.app._makeRequest({
+        path: "/clicks",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const { getClicks } = this;
+
+    const response = await getClicks({
+      $,
+    });
+    $.export("$summary", `Successfully retrieved \`${response.clicks.length}\` click(s).`);
+    return response;
+  },
+};
