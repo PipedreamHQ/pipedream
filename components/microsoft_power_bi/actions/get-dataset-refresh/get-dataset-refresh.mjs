dac363be7cc2a8e9fcb06fc79fa1429f63da6ce4
Jorge Cortes <jacortesmahmud@gmail.com>
2024-03-15 15:57:59 -0500

    Added new source and action

---
+++ components/microsoft_power_bi/actions/get-dataset-refresh/get-dataset-refresh.mjs
@@ -0,0 +1,54 @@
+import { ConfigurationError } from "@pipedream/platform";
+import app from "../../microsoft_power_bi.app.mjs";
+
+export default {
+  key: "microsoft_power_bi-get-dataset-refresh",
+  name: "Get Dataset Refresh",
+  description: "Triggers a refresh operation for a specified Power BI dataset. [See the documentation](https://learn.microsoft.com/en-us/rest/api/power-bi/datasets/get-refresh-history)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    datasetId: {
+      propDefinition: [
+        app,
+        "datasetId",
+      ],
+      optional: true,
+    },
+    customDatasetId: {
+      propDefinition: [
+        app,
+        "customDatasetId",
+      ],
+    },
+    top: {
+      type: "integer",
+      label: "Top",
+      description: "The number of refresh history items to retrieve.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      datasetId,
+      customDatasetId,
+      top,
+    } = this;
+
+    if (!datasetId && !customDatasetId) {
+      throw new ConfigurationError("Must enter one of Dataset ID or custom Dataset ID");
+    }
+
+    const response = await this.app.getRefreshHistory({
+      $,
+      datasetId: datasetId || customDatasetId,
+      params: {
+        ["$top"]: top,
+      },
+    });
+
+    $.export("$summary", `Successfully retrieved refresh history for dataset (ID \`${datasetId || customDatasetId}\`)`);
+    return response;
+  },
+};
