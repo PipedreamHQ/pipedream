e3e135c9e0182cc75afa4c65671e54ce8242bf45
michelle0927 <michellelbergero@hotmail.com>
2024-03-18 17:04:43 -0400

    fix key

--- components/discord/sources/new-guild-member/new-guild-member.mjs
+++ components/discord/sources/new-guild-member/new-guild-member.mjs
@@ -1,7 +1,7 @@
 import discord from "../../discord.app.mjs";
 
 export default {
-  key: "discord-guild-member",
+  key: "discord-new-guild-member",
   name: "New Guild Member (Instant)",
   description: "Emit new event for each new member added to a guild",
   version: "0.0.3",
