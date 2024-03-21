cd967b37d5006bcc61552f65cf34121e76717415
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2024-03-21 13:44:55 -0300

    Vybit new component (#11089)
    * Package and app file
    
    * pnpm
    
    * Send Vybit action
    
    * add newline

--- components/vybit/vybit.app.mjs
+++ components/vybit/vybit.app.mjs
@@ -1,11 +1,52 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "vybit",
-  propDefinitions: {},
+  propDefinitions: {
+    vybitId: {
+      type: "string",
+      label: "Vybit ID",
+      description: "Select a Vybit, or provide a Vybit ID.",
+      async options() {
+        const item = await this.listVybits();
+        return item?.map(({
+          triggerKey, name,
+        }) => ({
+          label: name,
+          value: triggerKey,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    async _makeRequest({
+      $ = this, headers, ...otherOpts
+    }) {
+      return axios($, {
+        ...otherOpts,
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Accept-Charset": "utf-8",
+          "Authorization": `${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async listVybits() {
+      return this._makeRequest({
+        url: "https://app.vybit.net/rest/vybits",
+      });
+    },
+    async sendVybit({
+      vybitId, ...args
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        url: `https://vybit.net/trigger/${vybitId}`,
+        ...args,
+      });
     },
   },
 };
