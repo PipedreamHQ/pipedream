13fa565f3bbbaad4c1c5078a93d089bdf37d1fe1
Dylan Pierce <me@dylanjpierce.com>
2024-03-21 09:54:23 -0400

    Fixing asset path and updating description

--- components/mux/actions/get-asset/get-asset.mjs
+++ components/mux/actions/get-asset/get-asset.mjs
@@ -2,7 +2,7 @@ import mux from "../../mux.app.mjs";
 
 export default {
   key: "mux-get-asset",
-  name: "Get Asset or Livestream ID",
+  name: "Get Asset",
   description: "Retrieves an asset. [See the documentation](https://docs.mux.com/api-reference#video/operation/get-asset)",
   version: "0.0.1",
   type: "action",
@@ -18,7 +18,7 @@ export default {
   async run({ $ }) {
     const { data } = await this.mux.getAsset({
       $,
-      playbackId: this.assetId,
+      assetId: this.assetId,
     });
     if (data?.tracks) {
       $.export("$summary", `Successfully retrieved tracks for asset: ${data.tracks}`);
