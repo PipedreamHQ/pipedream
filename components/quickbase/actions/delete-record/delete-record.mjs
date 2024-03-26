e2b6aee167d2bb83c72c18360df975a683fda8ff
michelle0927 <michelle0927@users.noreply.github.com>
2024-03-26 10:00:45 -0400

    New Components - quickbase (#11126)
    * quickbase init
    
    * new components
    
    * pnpm-lock.yaml
    
    * add records.mjs common file
    
    * add error, fix prop

---
+++ components/quickbase/actions/delete-record/delete-record.mjs
@@ -0,0 +1,44 @@
+import common from "../common/record.mjs";
+
+export default {
+  ...common,
+  key: "quickbase-delete-record",
+  name: "Delete Record",
+  description: "Deletes a record in a Quick Base table. [See the documentation](https://developer.quickbase.com/operation/deleteRecords)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    tableId: {
+      propDefinition: [
+        common.props.quickbase,
+        "tableId",
+        (c) => ({
+          appId: c.appId,
+        }),
+      ],
+    },
+    recordId: {
+      propDefinition: [
+        common.props.quickbase,
+        "recordId",
+        (c) => ({
+          appId: c.appId,
+          tableId: c.tableId,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const keyFieldId = await this.getKeyFieldId(this.appId, this.tableId);
+    const response = await this.quickbase.deleteRecord({
+      $,
+      data: {
+        from: this.tableId,
+        where: `{${keyFieldId}.EX.${this.recordId}}`,
+      },
+    });
+    $.export("$summary", `Successfully deleted record ${this.recordId}`);
+    return response;
+  },
+};
