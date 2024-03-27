9a3fe9f8b5727c45701d53450b5579b1c8303761
Luan Cazarine <luanhc@gmail.com>
2024-03-27 10:05:17 -0300

    [ACTION] Coda — Get Formula(s) #10878 (#11156)
    Actions
     - List Formulas

---
+++ components/coda/actions/list-formulas/list-formulas.mjs
@@ -0,0 +1,45 @@
+import coda from "../../coda.app.mjs";
+
+export default {
+  key: "coda-list-formulas",
+  name: "List Formulas",
+  description: "Lists formulas in a doc. [See docs](https://coda.io/developers/apis/v1#tag/Formulas/operation/listFormulas)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    coda,
+    docId: {
+      propDefinition: [
+        coda,
+        "docId",
+      ],
+    },
+    sortBy: {
+      propDefinition: [
+        coda,
+        "sortBy",
+      ],
+    },
+  },
+  async run({ $ }) {
+    let params = {
+      sortBy: this.sortBy,
+    };
+
+    let items = [];
+    let response;
+    do {
+      response = await this.coda.listFormulas(
+        $,
+        this.docId,
+        params,
+      );
+
+      items.push(...response.items);
+      params.pageToken = response.nextPageToken;
+    } while (params.pageToken);
+
+    $.export("$summary", `Retrieved ${items.length} formula(s)`);
+    return items;
+  },
+};
