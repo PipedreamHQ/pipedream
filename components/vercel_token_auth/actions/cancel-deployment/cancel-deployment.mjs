67bd5bbcb3e05fb30e67d4f4c71fcf8023cc00c9
js07 <19861096+js07@users.noreply.github.com>
2024-03-29 16:14:21 -0400

    Remove archived Vercel (OAuth) app
    Refactor Vercel (token auth) app & components

--- components/vercel_token_auth/actions/cancel-deployment/cancel-deployment.mjs
+++ components/vercel_token_auth/actions/cancel-deployment/cancel-deployment.mjs
@@ -1,14 +1,35 @@
-/* eslint-disable pipedream/required-properties-name */
-/* eslint-disable pipedream/required-properties-description */
-/* eslint-disable pipedream/required-properties-version */
-/* eslint-disable pipedream/required-properties-type */
-import base from "../../../vercel/actions/cancel-deployment/cancel-deployment.mjs";
-import overrideApp from "../../common/override-app.mjs";
-
-overrideApp(base);
+import vercelTokenAuth from "../../vercel_token_auth.app.mjs";
 
 export default {
-  ...base,
   key: "vercel_token_auth-cancel-deployment",
-  version: "0.0.2",
+  name: "Cancel Deployment",
+  description: "Cancel a deployment which is currently building. [See the docs](https://vercel.com/docs/rest-api#endpoints/deployments/cancel-a-deployment)",
+  version: "0.0.3",
+  type: "action",
+  props: {
+    vercelTokenAuth,
+    deployment: {
+      propDefinition: [
+        vercelTokenAuth,
+        "deployment",
+        () => ({
+          state: "BUILDING",
+        }),
+      ],
+    },
+    team: {
+      propDefinition: [
+        vercelTokenAuth,
+        "team",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const params = {
+      teamId: this.team,
+    };
+    const res = await this.vercelTokenAuth.cancelDeployment(this.deployment, params, $);
+    $.export("$summary", `Successfully canceled deployment ${this.deployment}`);
+    return res;
+  },
 };
