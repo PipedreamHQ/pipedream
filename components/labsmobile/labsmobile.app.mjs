2c12a5d4029f7fe76f02569a67cad70416e2607e
Luan Cazarine <luanhc@gmail.com>
2024-04-01 12:47:59 -0300

    [Components] labsmobile #11159 Actions - Send SMS

--- components/labsmobile/labsmobile.app.mjs
+++ components/labsmobile/labsmobile.app.mjs
@@ -3,62 +3,33 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "labsmobile",
-  propDefinitions: {
-    to: {
-      type: "string",
-      label: "Recipient's Phone Number",
-      description: "The phone number of the recipient. Include the country code without '+' or '00'.",
-    },
-    message: {
-      type: "string",
-      label: "SMS Text",
-      description: "The text message to send. Maximum message length is 160 characters for standard GSM characters.",
-    },
-  },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
       return "https://api.labsmobile.com/json";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "POST",
-        path = "/send",
-        data,
-        headers,
-        ...otherOpts
-      } = opts;
-
-      const authHeader = `Basic ${Buffer.from(`${this.$auth.username}:${this.$auth.password}`).toString("base64")}`;
-
+    _auth() {
+      return {
+        username: this.$auth.email,
+        password: this.$auth.api_token,
+      };
+    },
+    _makeRequest({
+      $ = this, path = "", ...otherOpts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
         url: this._baseUrl() + path,
         headers: {
-          ...headers,
-          "Content-Type": "application/json",
-          "Authorization": authHeader,
           "Cache-Control": "no-cache",
         },
-        data,
+        auth: this._auth(),
+        ...otherOpts,
       });
     },
-    async sendSMS({
-      to, message,
-    }) {
+    sendSMS(opts = {}) {
       return this._makeRequest({
-        data: {
-          message,
-          recipient: [
-            {
-              msisdn: to,
-            },
-          ],
-        },
+        method: "POST",
+        path: "/send",
+        ...opts,
       });
     },
   },
