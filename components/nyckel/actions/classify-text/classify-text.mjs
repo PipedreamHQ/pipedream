310bde6c43905334d13e4c6377e510e4cb1a1c8a
GTFalcao <gtfalcao96@gmail.com>
2024-04-02 02:00:32 -0300

    Classify Text adjustments

--- components/nyckel/actions/classify-text/classify-text.mjs
+++ components/nyckel/actions/classify-text/classify-text.mjs
@@ -4,19 +4,59 @@ export default {
   key: "nyckel-classify-text",
   name: "Classify Text",
   description: "Classifies text data based on pre-trained classifiers in Nyckel.  [See the documentation](https://www.nyckel.com/docs#invoke-text)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
     nyckel,
-    functionId: nyckel.propDefinitions.functionId,
-    data: nyckel.propDefinitions.data,
-    classifications: nyckel.propDefinitions.classifications,
+    functionId: {
+      propDefinition: [
+        nyckel,
+        "functionId",
+      ],
+    },
+    textInput: {
+      type: "string",
+      label: "Text Input",
+      description: "The text input to classify",
+    },
+    queryCount: {
+      propDefinition: [
+        nyckel,
+        "queryCount",
+      ],
+    },
+    includeMetadata: {
+      propDefinition: [
+        nyckel,
+        "includeMetadata",
+      ],
+    },
+    capture: {
+      propDefinition: [
+        nyckel,
+        "capture",
+      ],
+    },
+    externalId: {
+      propDefinition: [
+        nyckel,
+        "externalId",
+      ],
+    },
   },
   async run({ $ }) {
-    const response = await this.nyckel.classifyTextData({
+    const response = await this.nyckel.invokeFunction({
+      $,
       functionId: this.functionId,
-      data: this.data,
-      classifications: this.classifications,
+      data: {
+        data: this.textInput,
+      },
+      params: {
+        queryCount: this.queryCount,
+        includeMetadata: this.includeMetadata,
+        capture: this.capture,
+        externalId: this.externalId,
+      },
     });
 
     $.export("$summary", "Successfully classified text data");
