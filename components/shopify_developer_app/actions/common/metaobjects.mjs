6f9db36b5beadabdf14706e57aa6b6bb652cd573
Michael Lim <42158621+malexanderlim@users.noreply.github.com>
2024-04-03 15:56:39 -0700

    Updating Shopify API version due to quarterly deprecation. (#11272)
    * Updating Shopify API version due to quarterly deprecation.
    
    * Version bump.
    
    * Version bump.
    
    * Another version bump.

--- components/shopify_developer_app/actions/common/metaobjects.mjs
+++ components/shopify_developer_app/actions/common/metaobjects.mjs
@@ -9,7 +9,7 @@ export default {
       $ = this, ...args
     }) {
       return axios($, {
-        url: `https://${this.shopify.getShopId()}.myshopify.com/admin/api/2023-04/graphql.json`,
+        url: `https://${this.shopify.getShopId()}.myshopify.com/admin/api/2024-04/graphql.json`,
         method: "POST",
         headers: {
           "X-Shopify-Access-Token": `${this.shopify.$auth.access_token}`,
