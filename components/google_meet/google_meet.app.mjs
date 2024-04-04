b0a18ed4156f69dfbc4b52f89473113463430843
michelle0927 <michellelbergero@hotmail.com>
2024-04-04 12:45:13 -0400

    new component

--- components/google_meet/google_meet.app.mjs
+++ components/google_meet/google_meet.app.mjs
@@ -1,122 +1,140 @@
-import { axios } from "@pipedream/platform";
+import calendar from "@googleapis/calendar";
+import timezones from "moment-timezone";
 
 export default {
   type: "app",
   app: "google_meet",
   propDefinitions: {
-    calendar: {
+    calendarId: {
+      label: "Calendar ID",
       type: "string",
-      label: "Calendar",
-      description: "The calendar to create the event in",
+      description: "Optionally select the calendar, defaults to the primary calendar for the logged-in user",
+      default: "primary",
+      optional: true,
+      async options({ prevContext }) {
+        const { nextPageToken } = prevContext;
+        if (nextPageToken === false) {
+          return [];
+        }
+        const response = await this.listCalendars({
+          pageToken: nextPageToken,
+        });
+        const options = response.items.map((item) => ({
+          label: item.summary,
+          value: item.id,
+        }));
+        return {
+          options,
+          context: {
+            nextPageToken: response.nextPageToken ?? false,
+          },
+        };
+      },
+    },
+    timeZone: {
+      type: "string",
+      label: "Time Zone",
+      description: "Time zone used in the response. Optional. The default is the time zone of the calendar.",
+      optional: true,
+      options() {
+        const timeZonesList = timezones.tz.names().map((timezone) => {
+          return {
+            label: timezone,
+            value: timezone,
+          };
+        });
+        return timeZonesList;
+      },
+    },
+    sendUpdates: {
+      label: "Send Updates",
+      type: "string",
+      description: "Configure whether to send notifications about the event",
+      optional: true,
+      options: [
+        "all",
+        "externalOnly",
+        "none",
+      ],
+    },
+    sendNotifications: {
+      label: "Send Notifications",
+      type: "boolean",
+      description: "Whether to send notifications about the event update",
+      optional: true,
+    },
+    colorId: {
+      label: "Color ID",
+      type: "string",
+      description: "The color of the event. This is an ID referring to an entry in the event section of the colors definition (see the colors endpoint).",
+      optional: true,
       async options() {
-        const { items } = await this.listCalendars();
-        return items.map((calendar) => ({
-          label: calendar.summary,
-          value: calendar.id,
+        const response = await this.listColors();
+        return Object.entries(response.event).map(([
+          key,
+          value,
+        ]) => ({
+          label: `Background ${value.background} | Foreground ${value.foreground}`,
+          value: key,
         }));
       },
     },
-    eventTitle: {
-      type: "string",
-      label: "Event Title",
-      description: "The title of the event",
-      optional: true,
-    },
-    eventDescription: {
-      type: "string",
-      label: "Event Description",
-      description: "The description of the event",
-      optional: true,
-    },
-    eventStartDate: {
-      type: "string",
-      label: "Event Start Date",
-      description: "The start date of the event",
-    },
-    eventEndDate: {
-      type: "string",
-      label: "Event End Date",
-      description: "The end date of the event",
-    },
-    attendees: {
-      type: "string[]",
-      label: "Attendees",
-      description: "The email addresses of the attendees",
-      optional: true,
-    },
-    createMeetRoom: {
-      type: "boolean",
-      label: "Create Meet Room",
-      description: "Whether to create a Google Meet room for the event",
-      default: true,
-      optional: true,
-    },
   },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
-    _baseUrl() {
-      return "https://www.googleapis.com/calendar/v3";
-    },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path,
-        headers,
-        ...otherOpts
-      } = opts;
-      return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
-        },
+    client() {
+      const auth = new calendar.auth.OAuth2();
+      auth.setCredentials({
+        access_token: this.$auth.oauth_access_token,
+      });
+      return calendar.calendar({
+        version: "v3",
+        auth,
       });
     },
-    async listCalendars() {
-      return this._makeRequest({
-        path: "/users/me/calendarList",
+    async requestHandler({
+      api, method, args = {},
+    }) {
+      const {
+        returnOnlyData = true,
+        ...otherArgs
+      } = args;
+      try {
+        const calendar = this.client();
+        const response = await calendar[api][method](otherArgs);
+        return returnOnlyData
+          ? response.data
+          : response;
+      } catch (error) {
+        console.error(JSON.stringify(error.response?.data, null, 2));
+        throw error.response?.data?.error?.message ?? error;
+      }
+    },
+    listCalendars(args = {}) {
+      return this.requestHandler({
+        api: "calendarList",
+        method: "list",
+        args,
       });
     },
-    async createEvent(opts = {}) {
-      const {
-        calendarId, ...otherOpts
-      } = opts;
-      return this._makeRequest({
-        ...otherOpts,
-        method: "POST",
-        path: `/calendars/${calendarId}/events`,
-        data: {
-          summary: opts.eventTitle,
-          description: opts.eventDescription,
-          start: {
-            dateTime: opts.eventStartDate,
-          },
-          end: {
-            dateTime: opts.eventEndDate,
-          },
-          attendees: opts.attendees.map((email) => ({
-            email,
-          })),
-          conferenceData: {
-            createRequest: {
-              requestId: Math.random().toString(36)
-                .substring(2),
-              conferenceSolutionKey: {
-                type: opts.createMeetRoom
-                  ? "hangoutsMeet"
-                  : "eventHangout",
-              },
-            },
-          },
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
+    listColors(args = {}) {
+      return this.requestHandler({
+        api: "colors",
+        method: "get",
+        args,
+      });
+    },
+    getSettings(args = {}) {
+      return this.requestHandler({
+        api: "settings",
+        method: "get",
+        args,
+      });
+    },
+    createEvent(args = {}) {
+      return this.requestHandler({
+        api: "events",
+        method: "insert",
+        args,
       });
     },
   },
