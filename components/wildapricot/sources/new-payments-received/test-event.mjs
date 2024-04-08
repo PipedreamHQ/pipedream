cbabf4a09108d830746f1debd6adec950b4b1b1d
michelle0927 <michelle0927@users.noreply.github.com>
2024-04-08 12:30:49 -0400

    New Components - wildapricot (#11253)
    * wildapricot init
    
    * new components
    
    * pnpm-lock.yaml
    
    * updates

---
+++ components/wildapricot/sources/new-payments-received/test-event.mjs
@@ -0,0 +1,25 @@
+export default {
+  "Id": 93346951,
+  "Url": "https://api.wildapricot.org/v2.2/accounts/467183/Payments/93346951",
+  "Tender": null,
+  "Comment": "",
+  "PublicComment": "",
+  "AllocatedValue": 0,
+  "RefundedAmount": 0,
+  "Type": "InvoicePayment",
+  "Donation": null,
+  "Value": 1,
+  "DocumentDate": "2024-04-03T17:08:35+00:00",
+  "Contact": {
+    "Id": 74400049,
+    "Url": "https://api.wildapricot.org/v2.2/accounts/467183/Contacts/74400049",
+    "Name": "Test User"
+  },
+  "CreatedDate": "2024-04-03T17:08:35",
+  "CreatedBy": {
+    "Id": 74400049,
+    "Url": "https://api.wildapricot.org/v2.2/accounts/467183/Contacts/74400049"
+  },
+  "UpdatedDate": null,
+  "UpdatedBy": null
+}
