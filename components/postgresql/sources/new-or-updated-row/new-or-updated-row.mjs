9fd3382b99b24343b05db6be451557df72aa5fe7
Jay Vercellone <jverce@users.noreply.github.com>
2024-04-09 10:52:04 -0700

    Fix calls to unbounded PostgreSQL app methods (#11374)

--- components/postgresql/sources/new-or-updated-row/new-or-updated-row.mjs
+++ components/postgresql/sources/new-or-updated-row/new-or-updated-row.mjs
@@ -5,7 +5,7 @@ export default {
   name: "New or Updated Row",
   key: "postgresql-new-or-updated-row",
   description: "Emit new event when a row is added or modified. [See Docs](https://node-postgres.com/features/queries)",
-  version: "2.0.1",
+  version: "2.0.2",
   type: "source",
   dedupe: "unique",
   props: {
