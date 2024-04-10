9b0dafa2bd76399bad742c2156b7dba1213846dc
Jorge Cortes <jacortesmahmud@gmail.com>
2024-04-10 14:33:23 -0500

    contact_enhance init

--- components/contact_enhance/contact_enhance.app.mjs
+++ components/contact_enhance/contact_enhance.app.mjs
@@ -1,11 +1,45 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "contact_enhance",
-  propDefinitions: {},
+  propDefinitions: {
+    recordId: {
+      type: "string",
+      label: "Record ID",
+      description: "The unique identifier for the record in the Contact Enhance database",
+      required: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
     authKeys() {
       console.log(Object.keys(this.$auth));
+    },
+    _baseUrl() {
+      return "https://api.contactenhance.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async getRecord({ recordId }) {
+      return this._makeRequest({
+        path: `/records/${recordId}`,
+      });
     },
   },
 };
