e210b33e814fe954fe769fb8f35fc9c0b64bea7a
michelle0927 <michellelbergero@hotmail.com>
2024-04-10 11:33:54 -0400

    onstrategy init

--- components/onstrategy/onstrategy.app.mjs
+++ components/onstrategy/onstrategy.app.mjs
@@ -1,11 +1,39 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "onstrategy",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
     authKeys() {
       console.log(Object.keys(this.$auth));
+    },
+    _baseUrl() {
+      return "https://api.onstrategyapp.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async emitNewGoalEvent() {
+      return this._makeRequest({
+        method: "POST",
+        path: "/events/new_goal",
+      });
     },
   },
 };
