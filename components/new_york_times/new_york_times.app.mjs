9357cbff03ea482daebd85b7c66800aec8bbf9a1
Luan Cazarine <luanhc@gmail.com>
2024-04-10 17:47:55 -0300

    [Components] new_york_times #10750 Sources  - New Article

--- components/new_york_times/new_york_times.app.mjs
+++ components/new_york_times/new_york_times.app.mjs
@@ -3,82 +3,55 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "new_york_times",
-  propDefinitions: {
-    facet: {
-      type: "string",
-      label: "Facet",
-      description: "A comma-separated list of fields to facet on. Facets are used to group results based on the values of a specific field.",
-      optional: true,
-    },
-    facetFields: {
-      type: "string[]",
-      label: "Facet Fields",
-      description: "A list of fields to facet on. This is similar to `facet` but allows for an array of fields.",
-      optional: true,
-    },
-    facetFilter: {
-      type: "boolean",
-      label: "Facet Filter",
-      description: "A boolean to toggle filtering on facet fields. When true, facet counts are based on the documents that match all other search criteria.",
-      optional: true,
-    },
-    fl: {
-      type: "string",
-      label: "Field List",
-      description: "A comma-separated list of fields to return in the result set.",
-      optional: true,
-    },
-    fq: {
-      type: "string",
-      label: "Filtered Query",
-      description: "A query to filter the search results. This parameter supports the same syntax as the `q` parameter.",
-      optional: true,
-    },
-    q: {
-      type: "string",
-      label: "Query",
-      description: "Search query term. Search is performed on the article body, headline, and byline.",
-      optional: true,
-    },
-  },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
       return "https://api.nytimes.com/svc";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this, method = "GET", path, params, headers, ...otherOpts
-      } = opts;
+    _params(params = {}) {
+      return {
+        ...params,
+        "api-key": this.$auth.api_key,
+      };
+    },
+    _makeRequest({
+      $ = this, path, params, ...otherOpts
+    }) {
       return axios($, {
         ...otherOpts,
-        method,
         url: `${this._baseUrl()}${path}`,
-        params: {
-          "api-key": this.$auth.api_key,
-          ...params,
-        },
-        headers: {
-          ...headers,
-        },
+        params: this._params(params),
       });
     },
-    async searchArticles({
-      facet, facetFields, facetFilter, fl, fq, q,
-    } = {}) {
+    searchArticles(opts = {}) {
       return this._makeRequest({
         path: "/search/v2/articlesearch.json",
-        params: {
-          facet,
-          "facet_fields": facetFields && facetFields.join(","),
-          "facet_filter": facetFilter,
-          fl,
-          fq,
-          q,
-        },
+        ...opts,
       });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null, ...opts
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.page = page++;
+        const { response: { docs } } = await fn({
+          params,
+          ...opts,
+        });
+        for (const d of docs) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = docs.length;
+
+      } while (hasMore);
     },
   },
 };
