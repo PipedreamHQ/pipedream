d2ee7f096f033815c2e4687b957b9f82165d2b74
Luan Cazarine <luanhc@gmail.com>
2024-04-10 08:30:03 -0400

    New Components - outreach (#11151)
    * outreach init
    
    * [Components] outreach #11105
    Sources
     - New Call Event (Instant)
     - New Email Event (Instant)
     - New Task Event (Instant)
    
    Actions
     - Create Account
     - Create Prospect
     - Add Prospect To Sequence
    
    * pnpm update

---
+++ components/outreach/actions/add-prospect-sequence/add-prospect-sequence.mjs
@@ -0,0 +1,68 @@
+import { ConfigurationError } from "@pipedream/platform";
+import outreach from "../../outreach.app.mjs";
+
+export default {
+  key: "outreach-add-prospect-sequence",
+  name: "Add Prospect to Sequence",
+  description: "Adds an existing prospect to a specific sequence in Outreach. [See the documentation](https://developers.outreach.io/api/reference/tag/prospect/)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    outreach,
+    prospectId: {
+      propDefinition: [
+        outreach,
+        "prospectId",
+      ],
+    },
+    sequenceId: {
+      propDefinition: [
+        outreach,
+        "sequenceId",
+      ],
+    },
+    mailboxId: {
+      propDefinition: [
+        outreach,
+        "mailboxId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    try {
+      const response = await this.outreach.addProspectToSequence({
+        $,
+        data: {
+          data: {
+            type: "sequenceState",
+            relationships: {
+              prospect: {
+                data: {
+                  type: "prospect",
+                  id: this.prospectId,
+                },
+              },
+              sequence: {
+                data: {
+                  type: "sequence",
+                  id: this.sequenceId,
+                },
+              },
+              mailbox: {
+                data: {
+                  type: "mailbox",
+                  id: this.mailboxId,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      $.export("$summary", `Successfully added prospect ${this.prospectId} to sequence ${this.sequenceId}`);
+      return response;
+    } catch ({ message }) {
+      throw new ConfigurationError(JSON.parse(message).errors[0].detail);
+    }
+  },
+};
