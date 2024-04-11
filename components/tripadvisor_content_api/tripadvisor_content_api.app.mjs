000dfbe55f6ff582602b59e532d1cd3e3aaa7988
Lucas Caresia <lucascarezia@gmail.com>
2024-04-11 11:33:23 -0300

    tripadvisor_content_api init

--- components/tripadvisor_content_api/tripadvisor_content_api.app.mjs
+++ components/tripadvisor_content_api/tripadvisor_content_api.app.mjs
@@ -1,11 +1,91 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "tripadvisor_content_api",
-  propDefinitions: {},
-  methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+  propDefinitions: {
+    locationId: {
+      type: "string",
+      label: "Location ID",
+      description: "The unique identifier for the location.",
+    },
+    query: {
+      type: "string",
+      label: "Search Query",
+      description: "The search query to find locations.",
+      optional: true,
+    },
+    latitude: {
+      type: "string",
+      label: "Latitude",
+      description: "The latitude of the location for a nearby search.",
+    },
+    longitude: {
+      type: "string",
+      label: "Longitude",
+      description: "The longitude of the location for a nearby search.",
     },
   },
+  methods: {
+    _baseUrl() {
+      return "https://api.tripadvisor.com/api/v1";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        params,
+        data,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "X-TripAdvisor-API-Key": `${this.$auth.api_key}`,
+        },
+        params,
+        data,
+      });
+    },
+    async getLocationDetails({ locationId }) {
+      return this._makeRequest({
+        path: `/locations/${locationId}`,
+      });
+    },
+    async getLocationPhotos({ locationId }) {
+      return this._makeRequest({
+        path: `/locations/${locationId}/photos`,
+      });
+    },
+    async getLocationReviews({ locationId }) {
+      return this._makeRequest({
+        path: `/locations/${locationId}/reviews`,
+      });
+    },
+    async searchForLocations({ query }) {
+      return this._makeRequest({
+        path: "/locations/search",
+        params: {
+          query,
+        },
+      });
+    },
+    async searchForNearbyLocations({
+      latitude, longitude,
+    }) {
+      return this._makeRequest({
+        path: "/locations/mapsearch",
+        params: {
+          latitude,
+          longitude,
+        },
+      });
+    },
+  },
+  version: "0.0.{{ts}}",
 };
