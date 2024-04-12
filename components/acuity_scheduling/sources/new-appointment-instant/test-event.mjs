513886c9efdb0a9460123e0df40e8c2aed069101
Luan Cazarine <luanhc@gmail.com>
2024-04-12 12:14:52 -0300

    some adjusts

--- components/acuity_scheduling/sources/new-appointment-instant/test-event.mjs
+++ components/acuity_scheduling/sources/new-appointment-instant/test-event.mjs
@@ -7,26 +7,26 @@ export default {
 	"date": "June 17, 2013",
 	"time": "10:15am",
 	"endTime": "11:15am",
-  "dateCreated": "July 2, 2013",
-  "datetime": "2013-06-17T10:15:00-0700",
+	"dateCreated": "July 2, 2013",
+	"datetime": "2013-06-17T10:15:00-0700",
 	"price": "10.00",
 	"paid": "no",
-  "amountPaid": "0.00",
+	"amountPaid": "0.00",
 	"type": "Regular Visit",
-  "appointmentTypeID": 1,
-  "classID": null,
+	"appointmentTypeID": 1,
+	"classID": null,
 	"duration": "60",
 	"calendar": "My Calendar",
 	"calendarID": 27238,
-  "canClientCancel": false,
-  "canClientReschedule": false,
-  "location": "",
-  "certificate": null,
+	"canClientCancel": false,
+	"canClientReschedule": false,
+	"location": "",
+	"certificate": null,
 	"confirmationPage": "https://acuityscheduling.com/schedule.php?owner=11145481&id[]=1220aa9f41091c50c0cc659385cfa1d0&action=appt",
 	"formsText": "...",
 	"notes": "Notes",
 	"timezone": "America/New_York",
-  "scheduledBy": null,
+	"scheduledBy": null,
 	"forms": [
 		{
 			"id": 1,
@@ -47,11 +47,11 @@ export default {
 			]
 		}
 	],
-  "labels": [
-        {
-            "id": 3,
-            "name": "Completed",
-            "color": "pink"
-        }
-    ],
+	"labels": [
+		{
+			"id": 3,
+			"name": "Completed",
+			"color": "pink"
+		}
+	],
 }
