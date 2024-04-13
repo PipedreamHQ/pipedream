33fb07641d741c3623152e5a0d65a7a0a4d869f7
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2024-04-13 03:04:50 -0300

    Expofp new action (#11359)
    * Add Exhibitor Booth action
    
    * Booth async options + improving existing actions
    
    * Adding async options to new action
    
    * Adjustments
    
    * pnpm
    
    * Version bump
    
    * Version bump
    
    * Version bumps
    
    * text adjustment

--- components/expofp/actions/add-exhibitor/add-exhibitor.mjs
+++ components/expofp/actions/add-exhibitor/add-exhibitor.mjs
@@ -3,10 +3,10 @@ import common from "../common/add-or-update-exhibitor.mjs";
 export default {
   ...common,
   name: "Add Exhibitor",
-  version: "0.0.1",
+  version: "0.0.2",
   key: "expofp-add-exhibitor",
   description:
-    "Adds an exhibitor. [See docs here](https://expofp.docs.apiary.io/#reference/0/add-exhibitor/add-exhibitor)",
+    "Adds an exhibitor. [See the documentation](https://expofp.docs.apiary.io/#reference/0/add-exhibitor/add-exhibitor)",
   type: "action",
   methods: {
     ...common.methods,
