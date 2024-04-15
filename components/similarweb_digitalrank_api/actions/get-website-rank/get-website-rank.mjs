7a64a22b6526673914a9c2e6745be4d9078d4b63
Lucas Caresia <lucascarezia@gmail.com>
2024-04-15 08:21:32 -0300

    New Components - similarweb_digitalrank_api (#11233)
    * similarweb_digitalrank_api init
    
    * Added actons

---
+++ components/similarweb_digitalrank_api/actions/get-website-rank/get-website-rank.mjs
@@ -0,0 +1,28 @@
+import app from "../../similarweb_digitalrank_api.app.mjs";
+
+export default {
+  key: "similarweb_digitalrank_api-get-website-rank",
+  name: "Get Website Rank",
+  description: "Retrieves the global rank of a specific website using Similarweb's Digital Rank API. [See the documentation](https://developers.similarweb.com/docs/digital-rank-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    domain: {
+      propDefinition: [
+        app,
+        "domain",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getWebsiteRank({
+      $,
+      domain: this.domain,
+    });
+
+    $.export("$summary", `Retrieved global rank for domain: ${this.domain}`);
+
+    return response;
+  },
+};
--- components/similarweb_digitalrank_api/similarweb_digitalrank_api.app.mjs
+++ components/similarweb_digitalrank_api/similarweb_digitalrank_api.app.mjs
@@ -1,11 +1,60 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "similarweb_digitalrank_api",
-  propDefinitions: {},
+  propDefinitions: {
+    limit: {
+      type: "integer",
+      label: "Limit",
+      description: "The number of desired results",
+      optional: true,
+    },
+    domain: {
+      type: "string",
+      label: "Domain",
+      description: "Enter the domain to retrieve its global rank, i.e. `google.com`",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.similarweb.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        params,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        url: this._baseUrl() + path,
+        params: {
+          ...params,
+          api_key: `${this.$auth.api_key}`,
+        },
+        ...otherOpts,
+      });
+    },
+    async listTopRankingWebsites(args = {}) {
+      return this._makeRequest({
+        path: "/v1/similar-rank/top-sites/",
+        ...args,
+      });
+    },
+    async getWebsiteRank({
+      domain, ...args
+    }) {
+      return this._makeRequest({
+        path: `/v1/similar-rank/${domain}/rank`,
+        ...args,
+      });
+    },
+    async getSubscriptionStatus(args = {}) {
+      return this._makeRequest({
+        path: "/user-capabilities",
+        ...args,
+      });
     },
   },
 };
