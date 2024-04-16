446e1b787e192c58b19e597e9bd5fdf1f2beb9e7
Luan Cazarine <luanhc@gmail.com>
2024-04-16 12:21:10 -0400

    New Components - sevdesk (#11301)
    * sevdesk init
    
    * [Components] sevdesk #10782
    Sources
     - New Contact
     - New Order
     - New Voucher
    
    Actions
     - Create Invoice
     - Cancel Invoice
     - Send Invoice Email
    
    * pnpm update
    
    * Update components/sevdesk/actions/create-invoice/create-invoice.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Update components/sevdesk/actions/create-invoice/create-invoice.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Update components/sevdesk/actions/create-invoice/create-invoice.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * some adjusts
    
    ---------
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

---
+++ components/sevdesk/sources/new-order/test-event.mjs
@@ -0,0 +1,62 @@
+export default {
+  "id": "0",
+  "objectName": "Order",
+  "create": "2019-08-24T14:15:22Z",
+  "update": "2019-08-24T14:15:22Z",
+  "orderNumber": "Offer-1000",
+  "contact": {
+    "id": "0",
+    "objectName": "Contact"
+  },
+  "orderDate": "01.01.2020",
+  "status": "100",
+  "header": "My Offer-1000",
+  "headText": "string",
+  "footText": "string",
+  "addressCountry": {
+    "id": "string",
+    "objectName": "string"
+  },
+  "createUser": {
+    "id": "0",
+    "objectName": "SevUser"
+  },
+  "sevClient": {
+    "id": "0",
+    "objectName": "SevClient"
+  },
+  "deliveryTerms": "string",
+  "paymentTerms": "string",
+  "origin": {
+    "id": "0",
+    "objectName": "Order"
+  },
+  "version": "0",
+  "smallSettlement": "0",
+  "contactPerson": {
+    "id": "0",
+    "objectName": "SevUser"
+  },
+  "taxRate": "19",
+  "taxSet": {
+    "id": "0",
+    "objectName": "TaxSet"
+  },
+  "taxText": "Umsatzsteuer 19%",
+  "taxType": "default",
+  "orderType": "AN",
+  "sendDate": "2019-08-24T14:15:22Z",
+  "address": "string",
+  "currency": "EUR",
+  "sumNet": "string",
+  "sumTax": "string",
+  "sumGross": "string",
+  "sumDiscounts": "string",
+  "sumNetForeignCurrency": "string",
+  "sumTaxForeignCurrency": "string",
+  "sumGrossForeignCurrency": "string",
+  "sumDiscountsForeignCurrency": "string",
+  "customerInternalNote": "string",
+  "showNet": 1,
+  "sendType": "VPR"
+}
