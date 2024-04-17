3440c6f1a94fe2705f7bfaa059db349cc84414e0
michelle0927 <michellelbergero@hotmail.com>
2024-04-17 11:32:54 -0400

    new components

---
+++ components/hansei/actions/get-bot-conversations/get-bot-conversations.mjs
@@ -0,0 +1,28 @@
+import hansei from "../../hansei.app.mjs";
+
+export default {
+  key: "hansei-get-bot-conversations",
+  name: "Get Bot Conversations",
+  description: "Retrieves a list of conversations with the specified Bot in Hansei. [See the documentation](hhttps://developers.hansei.app/operation/operation-getbotconversations)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    hansei,
+    botId: {
+      propDefinition: [
+        hansei,
+        "botId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.hansei.listConversations({
+      $,
+      botId: this.botId,
+    });
+    $.export("$summary", `Successfully retrieved ${response.length} Conversation${response.length === 1
+      ? ""
+      : "s"}`);
+    return response;
+  },
+};
--- components/hansei/hansei.app.mjs
+++ components/hansei/hansei.app.mjs
@@ -18,58 +18,6 @@ export default {
         })) || [];
       },
     },
-    collectionIds: {
-      type: "string[]",
-      label: "Collection IDs",
-      description: "The collections to which this source belongs",
-      async options() {
-        const collections = await this.listCollections();
-        return collections?.map(({
-          id: value, name: label,
-        }) => ({
-          value,
-          label,
-        })) || [];
-      },
-    },
-    conversationId: {
-      type: "string",
-      label: "Conversation ID",
-      description: "Conversation Id provides the bot with additional context of last few messages to answer the question. A new Conversation Id is created when it is not provided.",
-      optional: true,
-      async options({ botId }) {
-        const conversations = await this.listConversations({
-          botId,
-        });
-        return conversations?.map(({
-          id: value, name: label,
-        }) => ({
-          value,
-          label,
-        })) || [];
-      },
-    },
-    metadata: {
-      type: "object",
-      label: "Metadata",
-      description: "Additional information about the webpage",
-      optional: true,
-    },
-    question: {
-      type: "string",
-      label: "Question",
-      description: "The inquiry to get an answer for",
-    },
-    file: {
-      type: "any",
-      label: "File",
-      description: "The file you want to upload to the knowledge base",
-    },
-    filetype: {
-      type: "string",
-      label: "File Type",
-      description: "The type of the file (text, word, pdf)",
-    },
   },
   methods: {
     _baseUrl() {
@@ -105,27 +53,6 @@ export default {
     }) {
       return this._makeRequest({
         path: `/bots/${botId}/conversations`,
-        ...opts,
-      });
-    },
-    addWebpage(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/sources/webpage",
-        ...opts,
-      });
-    },
-    getAnswerToQuestion(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/messages",
-        ...opts,
-      });
-    },
-    uploadFile(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/sources/upload",
         ...opts,
       });
     },
