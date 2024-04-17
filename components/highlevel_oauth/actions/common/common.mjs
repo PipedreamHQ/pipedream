ee79727275dcfa87164c1c5c2961ab0a3517b2c5
GTFalcao <gtfalcao96@gmail.com>
2024-04-17 00:46:09 -0300

    Reusing common props for contacts

---
+++ components/highlevel_oauth/actions/common/common.mjs
@@ -0,0 +1,51 @@
+import { parseObjectEntries } from "../../common/utils.mjs";
+import app from "../../highlevel_oauth.app.mjs";
+
+export default {
+  props: {
+    app,
+    name: {
+      type: "string",
+      label: "First Name",
+      description: "Full name of the contact, e.g. `Rosan Deo`",
+      optional: true,
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email of the contact, e.g. `rosan@deos.com`",
+      optional: true,
+    },
+    phone: {
+      type: "string",
+      label: "Phone Number",
+      description: "Phone number of the contact, e.g. `+1 888-888-8888`",
+      optional: true,
+    },
+    gender: {
+      type: "string",
+      label: "Gender",
+      description: "Gender of the contact, e.g. `male`",
+      optional: true,
+    },
+    additionalOptions: {
+      type: "object",
+      label: "Additional Options",
+      description:
+        "Additional parameters to send in the request. [See the documentation](https://highlevel.stoplight.io/docs/integrations/4c8362223c17b-create-contact) for available parameters. Values will be parsed as JSON where applicable.",
+      optional: true,
+    },
+  },
+  methods: {
+    getData() {
+      const {
+        app, additionalOptions, ...data
+      } = this;
+      return {
+        app,
+        ...data,
+        ...(additionalOptions && parseObjectEntries(additionalOptions)),
+      };
+    },
+  },
+};
--- components/highlevel_oauth/actions/create-contact/create-contact.mjs
+++ components/highlevel_oauth/actions/create-contact/create-contact.mjs
@@ -1,7 +1,13 @@
-import { parseObjectEntries } from "../../common/utils.mjs";
-import app from "../../highlevel_oauth.app.mjs";
+import common from "../common/common.mjs";
+
+const {
+  props: {
+    app, ...props
+  },
+} = common;
 
 export default {
+  ...common,
   key: "highlevel_oauth-create-contact",
   name: "Create Contact",
   description: "Creates a new contact on HighLevel. [See the documentation](https://highlevel.stoplight.io/docs/integrations/4c8362223c17b-create-contact)",
@@ -15,47 +21,15 @@ export default {
         "locationId",
       ],
     },
-    name: {
-      type: "string",
-      label: "First Name",
-      description: "Full name of the contact, e.g. `Rosan Deo`",
-      optional: true,
-    },
-    email: {
-      type: "string",
-      label: "Email",
-      description: "Email of the contact, e.g. `rosan@deos.com`",
-      optional: true,
-    },
-    phone: {
-      type: "string",
-      label: "Phone Number",
-      description: "Phone number of the contact, e.g. `+1 888-888-8888`",
-      optional: true,
-    },
-    gender: {
-      type: "string",
-      label: "Gender",
-      description: "Gender of the contact, e.g. `male`",
-      optional: true,
-    },
-    additionalOptions: {
-      type: "object",
-      label: "Additional Options",
-      description: "Additional parameters to send in the request. [See the documentation](https://highlevel.stoplight.io/docs/integrations/4c8362223c17b-create-contact) for available parameters. Values will be parsed as JSON where applicable.",
-      optional: true,
-    },
+    ...props,
   },
   async run({ $ }) {
     const {
-      app, additionalOptions, ...data
-    } = this;
+      app, ...data
+    } = this.getData();
     const response = await app.createContact({
       $,
-      data: {
-        ...data,
-        ...(additionalOptions && parseObjectEntries(additionalOptions)),
-      },
+      data,
     });
 
     $.export("$summary", `Successfully created contact (ID: ${response?.contact?.id})`);
