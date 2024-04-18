ece663987faad67a634f08c6f23bf36cde2af1b4
Jay Vercellone <jay@pipedream.com>
2024-04-18 15:52:06 -0300

    SQL Proxy interface updates
    * Upgrade `@pipedream/platform`
    * Implement `executeQueryAdapter` for MySQL and PostgreSQL
    * Bump component and app package versions

--- components/postgresql/actions/upsert-row/upsert-row.mjs
+++ components/postgresql/actions/upsert-row/upsert-row.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Upsert Row",
   key: "postgresql-upsert-row",
   description: "Adds a new row or updates an existing row. [See Docs](https://node-postgres.com/features/queries)",
-  version: "2.0.2",
+  version: "2.0.3",
   type: "action",
   props: {
     postgresql,
