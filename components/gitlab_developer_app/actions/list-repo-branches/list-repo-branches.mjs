21fc2d7327d14c7828b665d2f0798135ecbd37be
GTFalcao <gtfalcao96@gmail.com>
2024-04-20 21:58:41 -0300

    Adjusting actions' propDefinitions

--- components/gitlab_developer_app/actions/list-repo-branches/list-repo-branches.mjs
+++ components/gitlab_developer_app/actions/list-repo-branches/list-repo-branches.mjs
@@ -1,13 +1,12 @@
 import app from "../../gitlab_developer_app.app.mjs";
 import common from "../../../gitlab/actions/list-repo-branches/list-repo-branches.mjs";
 
+import { adjustPropDefinitions } from "../../common/utils.mjs";
+
 const {
   name, description, type, ...others
 } = common;
-
-const { // eslint-disable-next-line no-unused-vars
-  gitlab, ...props
-} = others.props;
+const props = adjustPropDefinitions(others.props, app);
 
 export default {
   ...others,
--- components/gitlab_developer_app/actions/update-issue/update-issue.mjs
+++ components/gitlab_developer_app/actions/update-issue/update-issue.mjs
@@ -1,13 +1,12 @@
 import app from "../../gitlab_developer_app.app.mjs";
 import common from "../../../gitlab/actions/update-issue/update-issue.mjs";
 
+import { adjustPropDefinitions } from "../../common/utils.mjs";
+
 const {
   name, description, type, ...others
 } = common;
-
-const { // eslint-disable-next-line no-unused-vars
-  gitlab, ...props
-} = others.props;
+const props = adjustPropDefinitions(others.props, app);
 
 export default {
   ...others,
