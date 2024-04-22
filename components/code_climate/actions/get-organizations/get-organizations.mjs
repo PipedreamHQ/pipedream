cb74bba3ef5ef362c34fd4ee3e3ac994fbfee22a
Leo Vu <18277920+vunguyenhung@users.noreply.github.com>
2024-04-22 14:55:02 +0700

    Update components/code_climate/actions/get-organizations/get-organizations.mjs

--- components/code_climate/actions/get-organizations/get-organizations.mjs
+++ components/code_climate/actions/get-organizations/get-organizations.mjs
@@ -2,7 +2,7 @@ import app from "../../code_climate.app.mjs";
 
 export default {
   key: "code_climate-get-organizations",
-  name: "Create Variable",
+  name: "Get Organizations",
   description: "Returns collection of organizations for the current user. [See the documentation](https://developer.codeclimate.com/#get-organizations)",
   version: "0.0.1",
   type: "action",
