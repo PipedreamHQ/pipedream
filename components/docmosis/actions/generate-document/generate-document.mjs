9cea0ec3079a5b2b02515288bf7be67b74cb7715
Jorge Cortes <jacortesmahmud@gmail.com>
2024-04-22 13:33:00 -0500

    New Components - docmosis (#11451)
    * docmosis init
    
    * Tested components

---
+++ components/docmosis/actions/generate-document/generate-document.mjs
@@ -0,0 +1,76 @@
+import utils from "../../common/utils.mjs";
+import app from "../../docmosis.app.mjs";
+import { writeFileSync  } from "fs";
+
+export default {
+  key: "docmosis-generate-document",
+  name: "Generate Document",
+  description: "Generates a document by merging data with a Docmosis template. [See the documentation](https://resources.docmosis.com/Documentation/Cloud/DWS4/Cloud-Web-Services-Guide-DWS4.pdf)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    templateName: {
+      propDefinition: [
+        app,
+        "templateName",
+      ],
+    },
+    data: {
+      propDefinition: [
+        app,
+        "data",
+      ],
+    },
+    outputName: {
+      propDefinition: [
+        app,
+        "outputName",
+      ],
+    },
+    outputFormat: {
+      propDefinition: [
+        app,
+        "outputFormat",
+      ],
+    },
+  },
+  methods: {
+    render(args = {}) {
+      return this.app.post({
+        path: "/render",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      render,
+      templateName,
+      outputName,
+      data,
+      outputFormat,
+    } = this;
+    const downloadedFilepath = `/tmp/${outputName}`;
+
+    const response = await render({
+      $,
+      responseType: "arraybuffer",
+      data: {
+        templateName,
+        data: utils.parseProp(data),
+        outputName,
+        outputFormat,
+      },
+    });
+
+    writeFileSync(downloadedFilepath, response);
+
+    $.export("$summary", `Successfully generated document with name \`${outputName}\``);
+
+    return [
+      outputName,
+      downloadedFilepath,
+    ];
+  },
+};
