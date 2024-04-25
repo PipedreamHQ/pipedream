a715f220402e85f588cffe6b48ded82d3f18f189
Andrew Chuang <andrewjschuang@gmail.com>
2024-04-25 11:07:20 -0300

    bump versions

--- components/google_sheets/actions/add-multiple-rows/add-multiple-rows.mjs
+++ components/google_sheets/actions/add-multiple-rows/add-multiple-rows.mjs
@@ -4,7 +4,7 @@ export default {
   key: "google_sheets-add-multiple-rows",
   name: "Add Multiple Rows",
   description: "Add multiple rows of data to a Google Sheet",
-  version: "0.2.2",
+  version: "0.2.3",
   type: "action",
   props: {
     googleSheets,
