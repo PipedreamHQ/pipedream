136e7a2780f13701b2e9b7f57f8b2e276be3a8f3
michelle0927 <michelle0927@users.noreply.github.com>
2024-04-25 10:40:25 -0400

    New Components - forcemanager (#11611)
    * forcemanager init
    
    * new components
    
    * pnpm-lock.yaml

---
+++ components/forcemanager/sources/new-activity/test-event.mjs
@@ -0,0 +1,42 @@
+export default {
+  "accountId": {
+    "id": 1186,
+    "value": "Polaroid"
+  },
+  "checkin": false,
+  "checkinTypeId": 1,
+  "checkoutDate": null,
+  "comment": "Take samples",
+  "contactId": {
+    "id": 1056,
+    "value": "Roger Chase"
+  },
+  "date": "2015-07-09T10:59:00Z",
+  "dateCreated": "2015-07-09T11:00:06.137Z",
+  "dateDeleted": null,
+  "dateUpdated": "2018-06-12T09:20:40.96Z",
+  "deleted": false,
+  "extId": null,
+  "geocoded": true,
+  "geocodingAccuracy": 1,
+  "id": 799,
+  "latitude": 41.3910821,
+  "longitude": 2.1291351,
+  "opportunityId": {
+    "id": 823,
+    "value": "Opportunity 2015-7"
+  },
+  "permissionLevel": 2,
+  "readOnly": false,
+  "salesRepId": {
+    "id": 5548,
+    "value": "Luis Sánchez"
+  },
+  "salesRepIdCreated": 5548,
+  "salesRepIdDeleted": null,
+  "salesRepIdUpdated": 51,
+  "typeId": {
+    "id": 6417,
+    "value": "Video call"
+  }
+}
