5510534921ad3d39ad46a0c49607884956e67871
Luan Cazarine <luanhc@gmail.com>
2024-04-26 08:56:23 -0300

    [Components] vryno #11624 Actions  - Create Unique Lead

--- components/vryno/actions/create-unique-lead/create-unique-lead.mjs
+++ components/vryno/actions/create-unique-lead/create-unique-lead.mjs
@@ -1,42 +1,223 @@
+import { ConfigurationError } from "@pipedream/platform";
 import vryno from "../../vryno.app.mjs";
-import { axios } from "@pipedream/platform";
 
 export default {
   key: "vryno-create-unique-lead",
   name: "Create Unique Lead",
-  description: "Creates a unique lead in the Vryno system, ensuring no duplication of lead details. [See the documentation]()", // Placeholder for documentation link
+  description: "Creates a unique lead in the Vryno system, ensuring no duplication of lead details. [See the documentation](https://vrynotest.ti2.in/docs/api-documentation/how-to-create-a-record-in-any-module-in-vryno-crm/)",
   version: "0.0.1",
   type: "action",
   props: {
     vryno,
-    name: vryno.propDefinitions.name,
-    email: vryno.propDefinitions.email,
-    phoneNumber: vryno.propDefinitions.phoneNumber,
-    source: vryno.propDefinitions.source,
-    interest: vryno.propDefinitions.interest,
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "The lead's first name.",
+      optional: true,
+    },
+    name: {
+      type: "string",
+      label: "Last Name",
+      description: "The lead's last name.",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The lead's email.",
+      optional: true,
+    },
+    phoneNumber: {
+      type: "string",
+      label: "Phone Number",
+      description: "The lead's phone number.",
+      optional: true,
+    },
+    company: {
+      type: "string",
+      label: "Company",
+      description: "The company the lead works for.",
+      optional: true,
+    },
+    website: {
+      type: "string",
+      label: "Website",
+      description: "The lead's website.",
+      optional: true,
+    },
+    ownerId: {
+      type: "string",
+      label: "Owner Id",
+      description: "The user Id related to the lead.",
+    },
+    score: {
+      type: "integer",
+      label: "Score",
+      description: "The lead's score.",
+      optional: true,
+    },
+    expectedRevenue: {
+      type: "integer",
+      label: "Expected Revenue",
+      description: "Expected revenue for the lead.",
+      optional: true,
+    },
+    numberOfEmployees: {
+      type: "integer",
+      label: "Number Of Employees",
+      description: "Number of employees at the lead company.",
+      optional: true,
+    },
+
+    billingAddress: {
+      type: "string",
+      label: "Billing Address",
+      description: "The lead's billing address.",
+      optional: true,
+    },
+    billingCity: {
+      type: "string",
+      label: "Billing City",
+      description: "The lead's billing city.",
+      optional: true,
+    },
+    billingState: {
+      type: "string",
+      label: "Billing State",
+      description: "The lead's billing state.",
+      optional: true,
+    },
+    billingCountry: {
+      type: "string",
+      label: "Billing Country",
+      description: "The lead's billing country.",
+      optional: true,
+    },
+    billingZipcode: {
+      type: "string",
+      label: "Billing Zipcode",
+      description: "The lead's billing zipcode",
+      optional: true,
+    },
+    shippingAddress: {
+      type: "string",
+      label: "Shipping Address",
+      description: "The lead's shipping address.",
+      optional: true,
+    },
+    shippingCity: {
+      type: "string",
+      label: "Shipping City",
+      description: "The lead's shipping city.",
+      optional: true,
+    },
+    shippingState: {
+      type: "string",
+      label: "Shipping State",
+      description: "The lead's shipping state.",
+      optional: true,
+    },
+    shippingCountry: {
+      type: "string",
+      label: "Shipping Country",
+      description: "The lead's shipping country.",
+      optional: true,
+    },
+    shippingZipcode: {
+      type: "string",
+      label: "Shipping Zipcode",
+      description: "The lead's shipping zipcode",
+      optional: true,
+    },
+    description: {
+      type: "string",
+      label: "Description",
+      description: "A brief description about the lead.",
+      optional: true,
+    },
   },
   async run({ $ }) {
-    // Check for duplicate lead
-    const duplicateCheck = await this.vryno.checkLeadDuplicate({
-      email: this.email,
-      phoneNumber: this.phoneNumber,
+    if (!this.email && !this.phoneNumber) {
+      throw new ConfigurationError("You must provide at least either **Email** or **Phone Number**.");
+    }
+    const duplicateCheck = await this.vryno.post({
+      data: {
+        query: `query {
+          fetchLead(filters:[${this.email
+    ? `{name: "email", operator:"eq",value:["${this.email}"]},`
+    : ""}${this.phoneNumber
+  ? `{name: "phoneNumber", operator:"eq",value:["${this.phoneNumber}"]}`
+  : ""}]){
+                code
+                status
+                message
+                messageKey
+                count
+                data {
+                  id
+                } 
+            }
+        }`,
+      },
     });
 
-    if (duplicateCheck.data && duplicateCheck.data.exists) {
+    console.log("duplicateCheck: ", duplicateCheck);
+
+    if (duplicateCheck.data.fetchLead.data) {
       $.export("$summary", "A lead with the same email and phone number already exists.");
       return duplicateCheck.data;
     }
 
-    // Create the lead if no duplicate was found
-    const response = await this.vryno.createLead({
-      name: this.name,
-      email: this.email,
-      phoneNumber: this.phoneNumber,
-      source: this.source,
-      interest: this.interest,
+    const {
+      vryno,
+      ...data
+    } = this;
+
+    let query = `mutation {
+      createLead(input: {
+        `;
+
+    for (const [
+      field,
+      value,
+    ] of Object.entries(data)) {
+      query += `${field}:`;
+      if ([
+        "score",
+        "expectedRevenue",
+        "numberOfEmployees",
+      ].includes(field)) {
+        query += ` ${value}
+        `;
+      } else {
+        query += ` "${value}"
+        `;
+      }
+    }
+
+    query += `}) {
+        code
+        message
+        status
+        messageKey
+        data {
+          id
+        }
+        errors
+      }
+    }`;
+
+    const response = await vryno.post({
+      $,
+      data: {
+        query,
+      },
     });
 
-    $.export("$summary", `Successfully created unique lead with email ${this.email}`);
+    if (response.data.createLead.code != 200) {
+      throw new ConfigurationError(response.data.createLead.message);
+    }
+
+    $.export("$summary", `Successfully created new lead with Id: ${response.data.createLead.data.id}`);
     return response;
   },
 };
--- components/vryno/vryno.app.mjs
+++ components/vryno/vryno.app.mjs
@@ -3,84 +3,30 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "vryno",
-  propDefinitions: {
-    name: {
-      type: "string",
-      label: "Name",
-      description: "Name of the lead.",
-    },
-    email: {
-      type: "string",
-      label: "Email",
-      description: "Email address of the lead.",
-    },
-    phoneNumber: {
-      type: "string",
-      label: "Phone Number",
-      description: "Phone number of the lead.",
-    },
-    source: {
-      type: "string",
-      label: "Source",
-      description: "Source from which the lead was obtained.",
-    },
-    interest: {
-      type: "string",
-      label: "Interest",
-      description: "Specific product or service the lead is interested in.",
-      optional: true,
-    },
-  },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
-      return "https://api.vryno.com";
+      return  `https://${this.$auth.company_instance_name}.ms.vryno.com`;
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this, method = "GET", path = "/", headers, ...otherOpts
-      } = opts;
+    _headers() {
+      return {
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+        "Content-Type": "application/json",
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...otherOpts
+    }) {
       return axios($, {
         ...otherOpts,
-        method,
         url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
-          "Content-Type": "application/json",
-        },
+        headers: this._headers(),
       });
     },
-    async createLead({
-      name, email, phoneNumber, source, interest,
-    }) {
-      const input = {
-        name,
-        email,
-        "contact details": {
-          phone: phoneNumber,
-          source,
-        },
-      };
-      if (interest) {
-        input.interest = interest;
-      }
+    post(opts = {}) {
       return this._makeRequest({
         method: "POST",
-        path: "/createLead",
-        data: {
-          input,
-        },
-      });
-    },
-    async checkLeadDuplicate({
-      email, phoneNumber,
-    }) {
-      return this._makeRequest({
-        method: "GET",
-        path: `/checkDuplicate?email=${email}&phone=${phoneNumber}`,
+        path: "/api/graphql/crm",
+        ...opts,
       });
     },
   },
