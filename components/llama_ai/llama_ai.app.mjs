f31eeb18a6547484941072abb10c689735d6d8e2
Jorge Cortes <jacortesmahmud@gmail.com>
2024-04-26 22:24:24 +0000

    Tested action component create-chat

--- components/llama_ai/llama_ai.app.mjs
+++ components/llama_ai/llama_ai.app.mjs
@@ -1,11 +1,33 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "llama_ai",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `https://api.llama-api.com${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.api_token}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      });
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "post",
+        ...args,
+      });
     },
   },
 };
