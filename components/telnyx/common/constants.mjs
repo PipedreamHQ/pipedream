2a82c3750ddb28eb83d098b08223236ba991407e
michelle0927 <michellelbergero@hotmail.com>
2024-04-29 10:57:06 -0400

    constants.mjs

--- components/telnyx/common/constants.mjs
+++ components/telnyx/common/constants.mjs
@@ -1,30 +1,28 @@
-  
 const phoneNumberStatus = {
-    'PURCHASE PENDING': "purchase_pending",
-    'PURCHASE FAILED': "purchase_failed",
-    'PORT PENDING': "port_pending",
-    'ACTIVE': "active",
-    'DELETED': "deleted",
-    'PORT FAILED': "port_failed",
-    'EMERGENCY ONLY': "emergency_only",
-    'PORTED OUT': "ported_out",
-    'PORT OUT PENDING': "port_out_pending",
+  "PURCHASE PENDING": "purchase_pending",
+  "PURCHASE FAILED": "purchase_failed",
+  "PORT PENDING": "port_pending",
+  "ACTIVE": "active",
+  "DELETED": "deleted",
+  "PORT FAILED": "port_failed",
+  "EMERGENCY ONLY": "emergency_only",
+  "PORTED OUT": "ported_out",
+  "PORT OUT PENDING": "port_out_pending",
 };
 
 const paymentMethods = {
-    'PAY PER MINUTE': "pay-per-minute",
-    'CHANNEL': "channel",
+  "PAY PER MINUTE": "pay-per-minute",
+  "CHANNEL": "channel",
 };
 
 const sortPhoneNumbers = {
-    'PURCHASED AT': "purchased_at",
-    'PHONE NUMBER': "phone_number",
-    'CONNECTION NAME': "connection_name",
-    'USAGE PAYMENT METHOD': "usage_payment_method",
+  "PURCHASED AT": "purchased_at",
+  "PHONE NUMBER": "phone_number",
+  "CONNECTION NAME": "connection_name",
+  "USAGE PAYMENT METHOD": "usage_payment_method",
 };
 export default {
-    phoneNumberStatus,
-    paymentMethods,
-    sortPhoneNumbers,
+  phoneNumberStatus,
+  paymentMethods,
+  sortPhoneNumbers,
 };
-  
