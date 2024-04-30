f604a3e62bb3c3dbd1d52759c1cf9db857b0e85a
Lucas Caresia <lucascarezia@gmail.com>
2024-04-30 09:38:45 -0300

    Added actions

---
+++ components/encodian/actions/validate-url/validate-url.mjs
@@ -0,0 +1,31 @@
+import app from "../../encodian.app.mjs";
+
+export default {
+  key: "encodian-validate-url",
+  name: "Validate URL",
+  description: "Validate the availability of the URL. [See the documentation](https://api.apps-encodian.com/index.html)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    url: {
+      propDefinition: [
+        app,
+        "url",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.validateUrl({
+      $,
+      data: {
+        url: this.url,
+        validateURL: true,
+      },
+    });
+
+    $.export("$summary", `The validation of the URL availability returned: '${response.result}'`);
+
+    return response;
+  },
+};
