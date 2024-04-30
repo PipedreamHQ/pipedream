458466896a12b52ccf73118345459e149e1abce8
GTFalcao <gtfalcao96@gmail.com>
2024-04-30 03:37:43 -0300

    Replacing node-fetch with axios

--- components/gitlab/sources/new-audit-event/new-audit-event.mjs
+++ components/gitlab/sources/new-audit-event/new-audit-event.mjs
@@ -1,6 +1,5 @@
 import gitlab from "../../gitlab.app.mjs";
 import base from "../common/base.mjs";
-import fetch from "node-fetch";
 import {
   create_destination,
   list_destinations,
@@ -40,18 +39,16 @@ export default {
       const query = create_destination(this.http.endpoint, this.groupPath);
 
       try {
-        await fetch(`https://${this._getBaseApiUrl()}/api/graphql`, {
+        await this.gitlab._makeRequest({
+          url: `https://${this._getBaseApiUrl()}/api/graphql`,
           method: "POST",
           headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
             "Authorization": `Bearer ${this.gitlab.$auth.oauth_access_token}`,
           },
-          body: JSON.stringify({
-            query,
-          }),
-        })
-          .then((r) => r.json());
+          data: query,
+        });
 
       } catch (err) {
         console.log(`Error thrown during activation: ${JSON.stringify(err)}`);
