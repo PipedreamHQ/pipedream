fe967f0b60c11b538cdc79fc066f2094976305ac
Luan Cazarine <luanhc@gmail.com>
2024-05-02 11:30:35 -0400

    New Components - foursquare (#11703)
    * foursquare init
    
    * [Components] foursquare #11579
    Sources
     - New Check In
     - New Tip
    
    Actions
     - Create Check In
     - Create Tip
    
    * pnpm update

---
+++ components/foursquare/actions/create-check-in/create-check-in.mjs
@@ -0,0 +1,36 @@
+import foursquare from "../../foursquare.app.mjs";
+
+export default {
+  key: "foursquare-create-check-in",
+  name: "Create Check In",
+  description: "Allows the user to generate a new check-in at a specific location on Foursquare. [See the documentation](https://docs.foursquare.com/developer/reference/create-a-checkin)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    foursquare,
+    venueId: {
+      propDefinition: [
+        foursquare,
+        "venueId",
+      ],
+    },
+    shout: {
+      type: "string",
+      label: "Shout",
+      description: "A message about your check-in.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.foursquare.createCheckIn({
+      $,
+      params: {
+        venueId: this.venueId,
+        shout: this.shout,
+      },
+    });
+
+    $.export("$summary", `Successfully created check-in at venue ${this.venueId}`);
+    return response;
+  },
+};
