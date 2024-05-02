bea6c5c95bf61efdec89bbd74464f4adf939fc68
Lucas Caresia <lucascarezia@gmail.com>
2024-05-02 11:37:53 -0300

    Update components/encodian/actions/compare-text/compare-text.mjs
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/encodian/actions/compare-text/compare-text.mjs
+++ components/encodian/actions/compare-text/compare-text.mjs
@@ -3,7 +3,7 @@ import app from "../../encodian.app.mjs";
 export default {
   key: "encodian-compare-text",
   name: "Compare Text",
-  description: "Compares two texts answearing if they are equal or not. [See the documentation](https://api.apps-encodian.com/index.html)",
+  description: "Compares two texts answering if they are equal or not. [See the documentation](https://api.apps-encodian.com/index.html)",
   version: "0.0.1",
   type: "action",
   props: {
