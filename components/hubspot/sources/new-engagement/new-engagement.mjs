3ad7fb6cfef939bc6d3fdfd6ec45bfd4c597db2c
michelle0927 <michelle0927@users.noreply.github.com>
2024-05-06 10:00:22 -0400

    bug fix (#11812)

--- components/hubspot/sources/new-engagement/new-engagement.mjs
+++ components/hubspot/sources/new-engagement/new-engagement.mjs
@@ -5,21 +5,21 @@ export default {
   key: "hubspot-new-engagement",
   name: "New Engagement",
   description: "Emit new event for each new engagement created. This action returns a maximum of 5000 records at a time, make sure you set a correct time range so you don't miss any events",
-  version: "0.0.17",
+  version: "0.0.18",
   dedupe: "unique",
   type: "source",
   hooks: {},
   methods: {
     ...common.methods,
     getTs(engagement) {
-      return Date.parse(engagement.createdAt);
+      return engagement.engagement.createdAt;
     },
     generateMeta(engagement) {
       const {
         id,
         type,
       } = engagement.engagement;
-      const ts = this.getTs(engagement.engagement);
+      const ts = this.getTs(engagement);
       return {
         id,
         summary: type,
@@ -27,7 +27,7 @@ export default {
       };
     },
     isRelevant(engagement, createdAfter) {
-      return this.getTs(engagement.engagement) > createdAfter;
+      return this.getTs(engagement) > createdAfter;
     },
   },
   async run() {
