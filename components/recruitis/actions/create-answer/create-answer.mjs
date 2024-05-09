a47b3cebba4ed57b25a8fb1f1d571e9941e0f804
Lucas Caresia <lucascarezia@gmail.com>
2024-05-09 17:45:29 -0300

    Added actions

---
+++ components/recruitis/actions/create-answer/create-answer.mjs
@@ -0,0 +1,79 @@
+import app from "../../recruitis.app.mjs";
+
+export default {
+  key: "recruitis-create-answer",
+  name: "Create Answer",
+  description: "Create a new answer for a job. [See the documentation](https://docs.recruitis.io/api/#tag/Candidates/paths/~1answers/post)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    jobID: {
+      propDefinition: [
+        app,
+        "jobID",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    phone: {
+      propDefinition: [
+        app,
+        "phone",
+      ],
+    },
+    twitter: {
+      propDefinition: [
+        app,
+        "twitter",
+      ],
+    },
+    facebook: {
+      propDefinition: [
+        app,
+        "facebook",
+      ],
+    },
+    linkedin: {
+      propDefinition: [
+        app,
+        "linkedin",
+      ],
+    },
+    skype: {
+      propDefinition: [
+        app,
+        "skype",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createAnswer({
+      $,
+      data: {
+        job_id: this.jobID,
+        email: this.email,
+        name: this.name,
+        phone: this.phone,
+        twitter: this.twitter,
+        facebook: this.facebook,
+        linkedin: this.linkedin,
+        skype: this.skype,
+      },
+    });
+
+    $.export("$summary", `Successfully created an answer for the user with ID: '${response.payload.user_id}'`);
+
+    return response;
+  },
+};
---
+++ components/recruitis/actions/create-candidate/create-candidate.mjs
@@ -0,0 +1,80 @@
+import app from "../../recruitis.app.mjs";
+
+export default {
+  key: "recruitis-create-candidate",
+  name: "Create Candidate",
+  description: "Create a new candidate. [See the documentation](https://docs.recruitis.io/api/#tag/Candidates/paths/~1candidates/post)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    phone: {
+      propDefinition: [
+        app,
+        "phone",
+      ],
+    },
+    twitter: {
+      propDefinition: [
+        app,
+        "twitter",
+      ],
+    },
+    facebook: {
+      propDefinition: [
+        app,
+        "facebook",
+      ],
+    },
+    linkedin: {
+      propDefinition: [
+        app,
+        "linkedin",
+      ],
+    },
+    skype: {
+      propDefinition: [
+        app,
+        "skype",
+      ],
+    },
+    gdprAgreement: {
+      propDefinition: [
+        app,
+        "gdprAgreement",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createCandidate({
+      $,
+      data: {
+        job_id: this.jobID,
+        email: this.email,
+        name: this.name,
+        phone: this.phone,
+        twitter: this.twitter,
+        facebook: this.facebook,
+        linkedin: this.linkedin,
+        skype: this.skype,
+        gdpr_agreement: this.gdprAgreement,
+      },
+    });
+
+    $.export("$summary", `Successfully created user with ID: '${response.payload.id}'`);
+
+    return response;
+  },
+};
