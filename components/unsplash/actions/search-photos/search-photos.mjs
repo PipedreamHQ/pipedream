473f97dd0acded3cef4c5a813f0478046cd0773f
Jorge Cortes <jacortesmahmud@gmail.com>
2024-05-15 11:00:32 -0500

    [ACTION] Unsplash - Search Photos, Get Photo (#11939)
    * Tested components
    
    * pnpm-lock.yaml

---
+++ components/unsplash/actions/search-photos/search-photos.mjs
@@ -0,0 +1,74 @@
+import constants from "../../common/constants.mjs";
+import app from "../../unsplash.app.mjs";
+
+export default {
+  key: "unsplash-search-photos",
+  name: "Search Photos",
+  description: "Get a single page of photo results for a query. [See the documentation](https://unsplash.com/documentation#search-photos)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    query: {
+      type: "string",
+      label: "Query",
+      description: "Search terms.",
+    },
+    contentFilter: {
+      type: "string",
+      label: "Content Filter",
+      description: "Limit results by content safety. Valid values are `low` and `high`.",
+      optional: true,
+      options: constants.CONTENT_FILTERS,
+    },
+    color: {
+      type: "string",
+      label: "Color",
+      description: "Filter results by color. Valid values are: `black_and_white`, `black`, `white`, `yellow`, `orange`, `red`, `purple`, `magenta`, `green`, `teal`, and `blue`.",
+      optional: true,
+      options: constants.COLOR_OPTIONS,
+    },
+    orientation: {
+      type: "string",
+      label: "Orientation",
+      description: "Filter by photo orientation. Optional. (Valid values: `landscape`, `portrait`, `squarish`)",
+      optional: true,
+      options: constants.ORIENTATION_OPTIONS,
+    },
+  },
+  methods: {
+    searchPhotos(args = {}) {
+      return this.app._makeRequest({
+        path: "/search/photos",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      searchPhotos,
+      query,
+      contentFilter,
+      color,
+      orientation,
+    } = this;
+
+    const photos = await app.paginate({
+      resourcesFn: searchPhotos,
+      resourcesFnArgs: {
+        $,
+        params: {
+          query,
+          content_filter: contentFilter,
+          color,
+          orientation,
+        },
+      },
+      resourceName: "results",
+    });
+
+    $.export("$summary", `Successfully retrieved \`${photos.length}\` photo(s).`);
+    return photos;
+  },
+};
--- components/unsplash/unsplash.app.mjs
+++ components/unsplash/unsplash.app.mjs
@@ -1,11 +1,98 @@
+import { axios } from "@pipedream/platform";
+import utils from "./common/utils.mjs";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "unsplash",
-  propDefinitions: {},
+  propDefinitions: {
+    photoId: {
+      type: "string",
+      label: "Photo ID",
+      description: "The ID of the photo to retrieve.",
+      async options({ page }) {
+        const photos = await this.listPhotos({
+          params: {
+            page: page + 1,
+            per_page: constants.DEFAULT_LIMIT,
+          },
+        });
+        return photos.map(({
+          id: value, slug: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `${constants.BASE_URL}${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Accept-Version": constants.API_VERSION,
+        "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        debug: true,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      });
+    },
+    listPhotos(args = {}) {
+      return this._makeRequest({
+        path: "/photos",
+        ...args,
+      });
+    },
+    async *getIterations({
+      resourcesFn, resourcesFnArgs, resourceName,
+      max = constants.DEFAULT_MAX,
+    }) {
+      let page = 1;
+      let resourcesCount = 0;
+
+      while (true) {
+        const response =
+          await resourcesFn({
+            ...resourcesFnArgs,
+            params: {
+              ...resourcesFnArgs?.params,
+              page,
+              per_page: constants.DEFAULT_LIMIT,
+            },
+          });
+
+        const nextResources = resourceName && response[resourceName] || response;
+
+        if (!nextResources?.length) {
+          console.log("No more resources found");
+          return;
+        }
+
+        for (const resource of nextResources) {
+          yield resource;
+          resourcesCount += 1;
+
+          if (resourcesCount >= max) {
+            console.log("Reached max resources");
+            return;
+          }
+        }
+
+        page += 1;
+      }
+    },
+    paginate(args = {}) {
+      return utils.iterate(this.getIterations(args));
     },
   },
 };
