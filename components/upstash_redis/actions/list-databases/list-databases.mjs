9d5e3ce4d4f3bddda7843d3d88c1ac4cde8a6ee9
Jorge Cortes <jacortesmahmud@gmail.com>
2024-05-16 18:33:49 -0500

    Tested components

---
+++ components/upstash_redis/actions/list-databases/list-databases.mjs
@@ -0,0 +1,19 @@
+import app from "../../upstash_redis.app.mjs";
+
+export default {
+  key: "upstash_redis-list-databases",
+  name: "List Redis Databases",
+  description: "Lists all Redis databases. [See the documentation](https://upstash.com/docs/devops/developer-api/redis/list_databases)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  async run({ $ }) {
+    const response = await this.app.listDatabases({
+      $,
+    });
+    $.export("$summary", `Successfully listed \`${response.length}\` Redis database(s)`);
+    return response;
+  },
+};
