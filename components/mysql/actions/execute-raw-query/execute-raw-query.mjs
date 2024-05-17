4b068d4347a13fac54df6a7f04e3d1b8573fe6d9
Biz Melesse <biz@pipedream.com>
2024-05-17 12:47:47 -0500

    Change prop type to sql and add auth

--- components/mysql/actions/execute-raw-query/execute-raw-query.mjs
+++ components/mysql/actions/execute-raw-query/execute-raw-query.mjs
@@ -2,14 +2,17 @@ import mysql from "../../mysql.app.mjs";
 
 export default {
   key: "mysql-execute-raw-query",
-  name: "Execute Raw Query",
+  name: "Execute Raw Query - Biz",
   description: "Find row(s) via a custom raw query. [See the documentation](https://dev.mysql.com/doc/refman/8.0/en/select.html)",
   type: "action",
-  version: "0.0.1",
+  version: "0.0.2",
   props: {
     mysql,
     sql: {
-      type: "string",
+      type: "sql",
+      auth: {
+        app: "mysql"
+      },
       label: "Query",
       description: "The SQL query to execute",
     },
