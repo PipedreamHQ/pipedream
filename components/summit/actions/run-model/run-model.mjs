7e68faaa7464d0e11f2f14afbd7673c9f7a49848
michelle0927 <michellelbergero@hotmail.com>
2024-05-20 16:32:11 -0400

    new components

--- components/summit/actions/run-model/run-model.mjs
+++ components/summit/actions/run-model/run-model.mjs
@@ -1,32 +1,69 @@
 import summit from "../../summit.app.mjs";
-import { axios } from "@pipedream/platform";
 
 export default {
   key: "summit-run-model",
   name: "Run Model",
-  description: "Executes a model within Summit and captures a response field. [See the documentation](https://summit.readme.io/docs)",
-  version: "0.0.{{ts}}",
+  description: "Executes a model within Summit and captures the response fields. [See the documentation](https://summit.readme.io/reference/model-api)",
+  version: "0.0.1",
   type: "action",
   props: {
     summit,
-    modelName: {
-      type: "string",
-      label: "Model Name",
-      description: "The name of the model to be run",
+    model: {
+      propDefinition: [
+        summit,
+        "model",
+      ],
+      reloadProps: true,
     },
-    fieldName: {
-      type: "string",
-      label: "Field Name",
-      description: "The name of the response field to be captured",
+    experimentalFlatten: {
+      type: "integer",
+      label: "Experimental Flatten",
+      description: "This allows you to choose the index of the column you'd like to pull from the grid. -1 is the last column, which is useful for grabbing the last slice of a time series model, like a forecast.",
       optional: true,
     },
   },
-  async run({ $ }) {
-    const response = await this.summit.executeModel({
-      model_name: this.modelName,
-      field_name: this.fieldName,
+  async additionalProps() {
+    const props = {};
+    if (!this.model) {
+      return props;
+    }
+    const { parameters } = await this.summit.getModel({
+      model: this.model,
     });
-    $.export("$summary", `Successfully executed model ${this.modelName}`);
+    for (const parameter of parameters) {
+      props[parameter.name] = {
+        type: "string",
+        label: `${parameter.name}`,
+        default: `${parameter.default_value}`,
+      };
+      if (parameter.value_choices) {
+        props[parameter.name].options = parameter.value_choices;
+      }
+    }
+    return props;
+  },
+  async run({ $ }) {
+    const { parameters } = await this.summit.getModel({
+      model: this.model,
+    });
+    const parametersObj = {};
+    for (const parameter of parameters) {
+      parametersObj[parameter.name] = this[parameter.name];
+    }
+    const data = {
+      parameters: parametersObj,
+    };
+    if (this.experimentalFlatten !== undefined) {
+      data.output = {
+        "__experimental_flatten": this.experimentalFlatten,
+      };
+    }
+    const response = await this.summit.runModel({
+      $,
+      model: this.model,
+      data,
+    });
+    $.export("$summary", `Successfully executed model \`${(this.model).split("/").pop()}\``);
     return response;
   },
 };
--- components/summit/summit.app.mjs
+++ components/summit/summit.app.mjs
@@ -4,136 +4,95 @@ export default {
   type: "app",
   app: "summit",
   propDefinitions: {
-    email: {
+    model: {
       type: "string",
-      label: "Email",
-      description: "The email address of the visitor",
-      optional: true,
+      label: "Model",
+      description: "The identifier model to run in the format `:organization_slug/:external_id/:app_slug`",
+      async options({ prevContext }) {
+        const args = prevContext?.next
+          ? {
+            url: prevContext.next,
+          }
+          : {};
+        const {
+          apps, next,
+        } = await this.listModels(args);
+        return {
+          options: apps?.map(({
+            app_identifier: value, name: label,
+          }) => ({
+            value,
+            label,
+          })) || [],
+          context: {
+            next,
+          },
+        };
+      },
     },
-    session_id: {
-      type: "string",
-      label: "Session ID",
-      description: "The ID of the session",
-    },
-    session_start: {
-      type: "string",
-      label: "Session Start",
-      description: "The start time of the session",
-      optional: true,
-    },
-    session_end: {
-      type: "string",
-      label: "Session End",
-      description: "The end time of the session",
-      optional: true,
-    },
-    simulation_id: {
-      type: "string",
-      label: "Simulation ID",
-      description: "The ID of the simulation",
-    },
-    simulation_start: {
-      type: "string",
-      label: "Simulation Start",
-      description: "The start time of the simulation",
-      optional: true,
-    },
-    simulation_end: {
-      type: "string",
-      label: "Simulation End",
-      description: "The end time of the simulation",
-      optional: true,
-    },
-    model_name: {
-      type: "string",
-      label: "Model Name",
-      description: "The name of the model to be run",
-    },
-    field_name: {
-      type: "string",
-      label: "Field Name",
-      description: "The name of the response field to be captured",
-      optional: true,
-    },
-    first_name: {
-      type: "string",
-      label: "First Name",
-      description: "The first name of the contact",
-    },
-    last_name: {
-      type: "string",
-      label: "Last Name",
-      description: "The last name of the contact",
-    },
-    email: {
-      type: "string",
-      label: "Email",
-      description: "The email of the contact",
-      optional: true,
-    },
-    phone_number: {
-      type: "string",
-      label: "Phone Number",
-      description: "The phone number of the contact",
-      optional: true,
-    },
+
   },
   methods: {
     _baseUrl() {
-      return "https://api.summit.com";
+      return "https://api.usesummit.com/v1";
     },
-    async _makeRequest(opts = {}) {
+    _makeRequest(opts = {}) {
       const {
         $ = this,
-        method = "GET",
         path,
-        headers,
         ...otherOpts
       } = opts;
       return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
+        url: `${this._baseUrl()}${path}`,
         headers: {
-          ...headers,
-          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+          "x-api-key": `${this.$auth.api_key}`,
         },
+        ...otherOpts,
       });
     },
-    async emitVisitorEvent(opts = {}) {
+    listModels(opts = {}) {
       return this._makeRequest({
-        method: "POST",
-        path: "/visitorEvents",
-        data: opts,
+        path: "/apps",
+        ...opts,
       });
     },
-    async emitSessionEvent(opts = {}) {
+    getModel({
+      model, ...opts
+    }) {
       return this._makeRequest({
-        method: "POST",
-        path: "/sessionEvents",
-        data: opts,
+        path: `/${model}/`,
+        ...opts,
       });
     },
-    async emitSimulationEvent(opts = {}) {
+    runModel({
+      model, ...opts
+    }) {
       return this._makeRequest({
         method: "POST",
-        path: "/simulationEvents",
-        data: opts,
+        path: `/${model}/`,
+        ...opts,
       });
     },
-    async executeModel(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/executeModel",
-        data: opts,
-      });
-    },
-    async createContact(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/contacts",
-        data: opts,
-      });
+    async *paginate({
+      resourceFn,
+      resourceType,
+      args = {},
+      max,
+    }) {
+      let next, count = 0;
+      do {
+        const results = await resourceFn(args);
+        const items = results[resourceType];
+        for (const item of items) {
+          yield item;
+          count++;
+          if (max && count >= max) {
+            return;
+          }
+        }
+        next = results?.next;
+        args.url = next;
+      } while (next);
     },
   },
 };
