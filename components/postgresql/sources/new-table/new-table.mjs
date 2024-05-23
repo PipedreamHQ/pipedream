133a22320f1301ee81edfa68069c996c69d258eb
Jay Vercellone <jay@pipedream.com>
2024-05-23 16:25:48 -0300

    Refactor getSchema in PostgreSQL

--- components/postgresql/sources/new-table/new-table.mjs
+++ components/postgresql/sources/new-table/new-table.mjs
@@ -5,7 +5,7 @@ export default {
   name: "New Table",
   key: "postgresql-new-table",
   description: "Emit new event when a new table is added to the database. [See the documentation](https://node-postgres.com/features/queries)",
-  version: "2.0.5",
+  version: "2.0.6",
   type: "source",
   props: {
     ...common.props,
