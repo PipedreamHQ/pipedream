0247ea54eb3a17af818234a5b3454435380e946a
Jorge Cortes <jacortesmahmud@gmail.com>
2024-05-22 23:26:05 -0500

    Tested components (#12016)

---
+++ components/botpress/actions/list-conversations/list-conversations.mjs
@@ -0,0 +1,52 @@
+import app from "../../botpress.app.mjs";
+import utils from "../../common/utils.mjs";
+
+export default {
+  key: "botpress-list-conversations",
+  name: "List Conversations",
+  description: "List conversations. [See the documentation](https://botpress.com/docs/api-documentation/#list-conversations)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    participanIds: {
+      type: "string[]",
+      label: "Participant IDs",
+      description: "IDs of the participants.",
+      optional: true,
+      propDefinition: [
+        app,
+        "userId",
+      ],
+    },
+    integrationName: {
+      type: "string",
+      label: "Integration Name",
+      description: "Name of the integration. Eg. `webchat`.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      participantIds,
+      integrationName,
+    } = this;
+
+    const resources = await app.paginate({
+      resourcesFn: app.listConversations,
+      resourcesFnArgs: {
+        $,
+        params: {
+          participantIds: utils.parseArray(participantIds),
+          integrationName,
+        },
+      },
+      resourceName: "conversations",
+    });
+
+    $.export("$summary", `Successfully listed \`${resources.length}\` conversation(s).`);
+
+    return resources;
+  },
+};
