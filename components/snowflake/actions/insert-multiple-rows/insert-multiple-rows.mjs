4555427086afb2fe27cee8ddb82b5608db47e232
Jay Vercellone <jverce@users.noreply.github.com>
2024-05-24 07:34:08 -0700

    Implement/refactor `getSchema` for the SQL prop (#12112)
    * Refactor `getSchema` in MySQL
    * Refactor `getSchema` in PostgreSQL
    * Implement `getSchema` in Snowflake
    * Use latest `@pipedream/platform` version

--- components/snowflake/actions/insert-multiple-rows/insert-multiple-rows.mjs
+++ components/snowflake/actions/insert-multiple-rows/insert-multiple-rows.mjs
@@ -6,7 +6,7 @@ export default {
   key: "snowflake-insert-multiple-rows",
   name: "Insert Multiple Rows",
   description: "Insert multiple rows into a table",
-  version: "0.0.9",
+  version: "0.0.10",
   props: {
     snowflake,
     database: {
