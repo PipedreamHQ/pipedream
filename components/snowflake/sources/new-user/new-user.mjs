7a27a3d2c9d1c4f5ff7d3d0b702d0a52f47d975d
Jay Vercellone <jverce@users.noreply.github.com>
2024-05-27 06:41:10 -0700

    Snowflake minor fixes (#12132)
    * Supress `INFO` logs that cause some noise
    * Coalesce the `rowCount` field in `getSchema` to a numeric value

--- components/snowflake/sources/new-user/new-user.mjs
+++ components/snowflake/sources/new-user/new-user.mjs
@@ -6,7 +6,7 @@ export default {
   key: "snowflake-new-user",
   name: "New User",
   description: "Emit new event when a user is created",
-  version: "0.0.10",
+  version: "0.0.11",
   methods: {
     ...common.methods,
     alwaysRunInSingleProcessMode() {
