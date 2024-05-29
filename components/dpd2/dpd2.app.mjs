5c2ae88853459ad857f4b583e60bed36710b9d14
michelle0927 <michellelbergero@hotmail.com>
2024-05-29 15:24:39 -0400

    dpd2 init

--- components/dpd2/dpd2.app.mjs
+++ components/dpd2/dpd2.app.mjs
@@ -1,11 +1,60 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "dpd2",
-  propDefinitions: {},
+  propDefinitions: {
+    productId: {
+      type: "string",
+      label: "Product ID",
+      description: "The ID of the product being purchased",
+      async options() {
+        const { data } = await this.getProducts();
+        return data.map((product) => ({
+          label: product.name,
+          value: product.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
     authKeys() {
       console.log(Object.keys(this.$auth));
+    },
+    _baseUrl() {
+      return "https://api.dpd2.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+      });
+    },
+    async getProducts() {
+      return this._makeRequest({
+        path: "/products",
+      });
+    },
+    async emitPurchaseEvent(productId) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/purchases/${productId}`,
+        data: {
+          productId: productId,
+        },
+      });
     },
   },
 };
