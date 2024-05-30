d0a7e0f2c388039c11234b8990999d59deeba556
michelle0927 <michelle0927@users.noreply.github.com>
2024-05-30 10:43:32 -0400

    New Components - albus (#12154)
    * albus init
    
    * new component
    
    * pnpm-lock.yaml

--- components/albus/albus.app.mjs
+++ components/albus/albus.app.mjs
@@ -1,11 +1,45 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "albus",
-  propDefinitions: {},
+  propDefinitions: {
+    prompt: {
+      type: "string",
+      label: "Prompt",
+      description: "The question to ask",
+    },
+    waitForCompletion: {
+      type: "boolean",
+      label: "Wait for Completion",
+      description: "Set to `true` to poll the API in 3-second intervals until the answer is ready",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api-albus.springworks.in";
+    },
+    makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Authorization": `${this.$auth.api_key}`,
+        },
+        ...otherOpts,
+      });
+    },
+    chatCompletion(opts = {}) {
+      return this.makeRequest({
+        method: "POST",
+        path: "/chat/completions/custom",
+        ...opts,
+      });
     },
   },
 };
