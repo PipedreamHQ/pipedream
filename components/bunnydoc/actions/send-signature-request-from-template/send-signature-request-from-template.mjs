657f5e9385a7cb07c07492b8dc18a1f64757276f
Luan Cazarine <luanhc@gmail.com>
2024-05-31 10:10:03 -0400

    New Components - bunnydoc (#12148)
    * bunnydoc init
    
    * [Components] bunnydoc #12069
    Webhooks
     - New Viewed Request (Instant)
     - New Signed Request (Instant)
     - New Completed Request (Instant)
    
    Actions
     - Send Signature Requrest Form Template
    
    * pnpm update

---
+++ components/bunnydoc/actions/send-signature-request-from-template/send-signature-request-from-template.mjs
@@ -0,0 +1,63 @@
+import bunnydoc from "../../bunnydoc.app.mjs";
+import { parseObject } from "../../common/utils.mjs";
+
+export default {
+  key: "bunnydoc-send-signature-request-from-template",
+  name: "Send Signature Request from Template",
+  description: "Sends a signature request using a pre-designed bunnydoc template. [See the documentation](https://support.bunnydoc.com/doc/api/#create-signature-request)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    bunnydoc,
+    templateId: {
+      type: "string",
+      label: "Template ID",
+      description: "The ID of the bunnydoc template to be used for the signature request.",
+    },
+    title: {
+      type: "string",
+      label: "Title",
+      description: "The title of the signature resquest.",
+      optional: true,
+    },
+    emailMessage: {
+      type: "string",
+      label: "Email Message",
+      description: "The message of the signature resquest.",
+      optional: true,
+    },
+    signingOrder: {
+      type: "boolean",
+      label: "Signing Order",
+      description: "Set the signing order.",
+      default: false,
+    },
+    recipients: {
+      type: "string",
+      label: "Recipients",
+      description: "A stringified array of objects of recipients. E.g. [{\"role\" : \"role1\", \"name\" : \"Signer1\", \"email\" : \"signer1@example.com\", \"accessCode\" : \"\"}].",
+    },
+    fields: {
+      type: "string",
+      label: "Fields",
+      description: "A stringified array of objects of fields. E.g. [{ \"apiLabel\": \"textFieldPatientHistory\", \"value\": \"My test value\", \"readOnly\" : 1 }]",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.bunnydoc.createSignatureRequestFromTemplate({
+      $,
+      data: {
+        templateId: this.templateId,
+        title: this.title,
+        emailMessage: this.emailMessage,
+        signingOrder: this.signingOrder,
+        recipients: parseObject(this.recipients),
+        fields: parseObject(this.fields),
+      },
+    });
+
+    $.export("$summary", "Successfully sent signature request.");
+    return response;
+  },
+};
---
+++ components/bunnydoc/sources/new-viewed-request-instant/new-viewed-request-instant.mjs
@@ -0,0 +1,21 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "bunnydoc-new-viewed-request-instant",
+  name: "New Viewed Signature Request (Instant)",
+  description: "Emit new event when a signature request is viewed.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEvents() {
+      return [
+        "signatureRequestViewed",
+      ];
+    },
+  },
+  sampleEmit,
+};
