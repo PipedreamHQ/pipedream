b453392f36663f1b8f5e9e321cbd1cecdc771df2
michelle0927 <michelle0927@users.noreply.github.com>
2024-05-31 10:14:13 -0400

    New Components - pumble (#12156)
    * new components
    
    * pnpm-lock.yaml

---
+++ components/pumble/actions/list-messages/list-messages.mjs
@@ -0,0 +1,30 @@
+import pumble from "../../pumble.app.mjs";
+
+export default {
+  key: "pumble-list-messages",
+  name: "List Messages",
+  description: "List messages in a channel. [See the documentation](https://pumble.com/help/integrations/add-pumble-apps/api-keys-integration/#list-messages-in-a-channel)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    pumble,
+    channel: {
+      propDefinition: [
+        pumble,
+        "channel",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { messages } = await this.pumble.listMessages({
+      $,
+      params: {
+        channel: this.channel,
+      },
+    });
+    $.export("$summary", `Successfully retrieved ${messages.length} message${messages.length === 1
+      ? ""
+      : "s"}`);
+    return messages;
+  },
+};
