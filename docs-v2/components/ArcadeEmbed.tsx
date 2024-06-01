1ff1697ea3489bb27762282f98f0d2caa7daf196
Danny Roosevelt <danny@pipedream.com>
2024-06-01 10:22:20 -0700

    Update ArcadeEmbed.tsx

--- docs-v2/components/ArcadeEmbed.tsx
+++ docs-v2/components/ArcadeEmbed.tsx
@@ -9,26 +9,14 @@ const ArcadeEmbed: React.FC<ArcadeEmbedProps> = ({
   src, title,
 }) => {
   return (
-    <div style={{
-      position: "relative",
-      paddingBottom: "calc(56.25% + 41px)",
-      height: 0,
-      width: "100%",
-    }}>
+    <div className="relative pb-[calc(56.25%+41px)] h-0 w-full">
       <iframe
         src={src}
         title={title}
         loading="lazy"
         allowFullScreen
         allow="clipboard-write"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          colorScheme: "light",
-        }}
+        className="absolute top-0 left-0 w-full h-full"
       ></iframe>
     </div>
   );
