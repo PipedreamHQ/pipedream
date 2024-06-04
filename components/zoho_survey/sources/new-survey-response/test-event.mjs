279e97d583886c0d2bfed664f92abff7f709cf42
michelle0927 <michelle0927@users.noreply.github.com>
2024-06-04 10:26:47 -0400

    updates (#12219)

--- components/zoho_survey/sources/new-survey-response/test-event.mjs
+++ components/zoho_survey/sources/new-survey-response/test-event.mjs
@@ -1,11 +1,14 @@
 export default {
-  "COLLECTOR_NAME": "eCommerce",
-  "RESPONSE_STATUS": "COMPLETED",
-  "Q-A": "Yes",
-  "COLLECTOR_ID": "ShdJpz",
-  "RESPONSE_STARTTIME": "Nov 30, 2023 09:51:19",
-  "RESPONSE_ID": "x7DjTWhf",
-  "IP_ADDRESS": "47.225.32.185",
-  "RESPONSE_ENDTIME": "Nov 30, 2023 09:51:21",
-  "RESPONSE_TIMETAKEN": "2 secs "
+  "Collector Name": "Customer Satisfaction Survey",
+  "Response Status": "COMPLETED",
+  "What is your name?": "Socrates",
+  "Collector ID": "MUDHdQ",
+  "Response Start Time": "Jun 03, 2024 11:07:29",
+  "Response ID": "S4CllLuR",
+  "IP Address": "47.225.32.185",
+  "Response End Time": "Jun 03, 2024 11:10:57",
+  "Time Taken to Respond": "3 mins 27 secs ",
+  "Others": "",
+  "Email address": "",
+  "Response URL": ""
 }
