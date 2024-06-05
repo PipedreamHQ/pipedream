f859d49ada9368c05bdd16e1b6069a1b11b1de43
Danny Roosevelt <danny@pipedream.com>
2024-06-04 20:09:56 -0700

    adding snowflake sql query action

---
+++ components/snowflake/actions/execute-sql-query/execute-sql-query.mjs
@@ -0,0 +1,28 @@
+import snowflake from "../../snowflake.app.mjs";
+
+export default {
+  name: "Execute SQL Query",
+  version: "0.0.1",
+  key: "snowflake-execute-sql-query",
+  description: "Execute a custom MySQL query. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
+  type: "action",
+  props: {
+    snowflake,
+    // eslint-disable-next-line pipedream/props-description
+    sql: {
+      type: "sql",
+      auth: {
+        app: "snowflake",
+      },
+      label: "SQL Query",
+    },
+  },
+  async run() {
+    const data = await this.snowflake.collectRows({
+      sqlText: this.sql.query,
+      binds: this.sql.params,
+    });
+    console.table(data);
+    return data;
+  },
+};
