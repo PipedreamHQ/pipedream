ececfc2b2b3b0da83ee33108b4b788858124d8ad
michelle0927 <michelle0927@users.noreply.github.com>
2024-06-05 10:40:35 -0400

    New Components - flippingbook (#12218)
    * flippingbook init
    
    * new components
    
    * pnpm-lock.yaml
    
    * fix test-event import

---
+++ components/flippingbook/actions/find-flipbook-by-title/find-flipbook-by-title.mjs
@@ -0,0 +1,29 @@
+import flippingbook from "../../flippingbook.app.mjs";
+
+export default {
+  key: "flippingbook-find-flipbook-by-title",
+  name: "Find Flipbook by Title",
+  description: "Locates a specific flipbook using the provided title. [See the documentation](https://apidocs.flippingbook.com/#list-filtered-and-or-paged-publications-in-the-account)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    flippingbook,
+    title: {
+      type: "string",
+      label: "Title",
+      description: "Title of the flipbook. In order to match, the publication name must contain the exact value",
+    },
+  },
+  async run({ $ }) {
+    const { publications } = await this.flippingbook.listFlipbooks({
+      $,
+      params: {
+        query: this.title,
+      },
+    });
+    $.export("$summary", `Found ${publications.length} flipbook${publications.length === 1
+      ? ""
+      : "s"} mathcing the title ${this.title}`);
+    return publications;
+  },
+};
