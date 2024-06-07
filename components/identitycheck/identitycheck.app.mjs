38d8c57b87678ac91222c80d7bbad8bbdeb6c8a1
Lucas Caresia <lucascarezia@gmail.com>
2024-06-07 08:05:53 -0300

    Added actions (#12273)

--- components/identitycheck/identitycheck.app.mjs
+++ components/identitycheck/identitycheck.app.mjs
@@ -1,11 +1,51 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "identitycheck",
-  propDefinitions: {},
+  propDefinitions: {
+    firstName: {
+      type: "string",
+      label: "First Name",
+      description: "First Name for the identity check",
+    },
+    lastName: {
+      type: "string",
+      label: "Last Name",
+      description: "Last Name for the identity check",
+    },
+    email: {
+      type: "string",
+      label: "Email",
+      description: "Email address",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://identity.stackgo.io/api";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Authorization": `Basic ${this.$auth.api_key}`,
+        },
+      });
+    },
+    async createVerification(args = {}) {
+      return this._makeRequest({
+        method: "post",
+        path: "/direct-verification",
+        ...args,
+      });
     },
   },
 };
