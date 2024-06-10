f675115ff5717e27116225e06e997b54a9b75eef
michelle0927 <michellelbergero@hotmail.com>
2024-06-10 10:21:36 -0500

    update

--- components/height/height.app.mjs
+++ components/height/height.app.mjs
@@ -20,6 +20,7 @@ export default {
             ? list.filter(({ id }) => !listIds.includes(id))
             : list.filter(({ id }) => listIds.includes(id));
         }
+        list = list.filter(({ type }) => type == "list" || type === "projects");
         return list?.map(({
           id: value, name: label,
         }) => ({
