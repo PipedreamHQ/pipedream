4ffe6127e86ad824c5b8826accbb479adef50253
Luan Cazarine <luanhc@gmail.com>
2024-06-17 09:20:27 -0400

    New Components - samsara (#12329)
    * samsara init
    
    * [Components] samsara #12326
    Webhooks
     - New Document Submitted (Instant)
     - New Route Job Completion (Instant)
     - New Route Started (Instant)
    
    Actions
     - Create Contact
     - Create Address
     - Create Route
    
    * pnpm update
    
    * Update components/samsara/common/utils.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ components/samsara/sources/new-route-started-instant/test-event.mjs
@@ -0,0 +1,217 @@
+export default {
+    "eventId": "017db07f-6e95-470e-8cc0-a371f9deed2b",
+    "eventTime": "1970-01-20T06:39:05.683Z",
+    "eventType": "RouteStopDeparture",
+    "orgId": 20936,
+    "webhookId": "1411751028848270",
+    "data": {
+        "driver": {
+            "externalIds": {
+                "payrollId": "ABFS18600"
+            },
+            "id": "45646",
+            "name": "Driver Bob"
+        },
+        "operation": "stop arrived",
+        "route": {
+            "actualRouteEndTime": "2019-06-13T19:08:25Z",
+            "actualRouteStartTime": "2019-06-13T19:08:25Z",
+            "externalIds": {
+                "myRouteId": "abc"
+            },
+            "id": "342341",
+            "name": "Bid 123",
+            "notes": "These are my notes",
+            "scheduledRouteEndTime": "2019-06-13T19:08:25Z",
+            "scheduledRouteStartTime": "2019-06-13T19:08:25Z",
+            "settings": {
+                "routeCompletionCondition": "arriveLastStop",
+                "routeStartingCondition": "departFirstStop"
+            },
+            "stops": [
+                {
+                    "actualArrivalTime": "2006-01-02T15:04:05+07:00",
+                    "actualDepartureTime": "2006-01-02T15:04:05+07:00",
+                    "address": {
+                        "externalIds": {
+                            "siteId": "54"
+                        },
+                        "id": "494123",
+                        "name": "Company Office #1"
+                    },
+                    "documents": [
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        },
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        }
+                    ],
+                    "enRouteTime": "2006-01-02T15:04:05+07:00",
+                    "eta": "2006-01-02T15:04:05+07:00",
+                    "externalIds": {
+                        "siteId": "54"
+                    },
+                    "id": "324231",
+                    "liveSharingUrl": "https://cloud.samsara.com/fleet/viewer/job/fleet_viewer_token",
+                    "locationLiveSharingLinks": [
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        },
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        }
+                    ],
+                    "name": "Stop #1",
+                    "notes": "These are my notes",
+                    "scheduledArrivalTime": "2019-06-13T19:08:25Z",
+                    "scheduledDepartureTime": "2019-06-13T19:08:25Z",
+                    "singleUseLocation": {
+                        "address": "1234 Main St, San Jose, CA",
+                        "latitude": 123.456,
+                        "longitude": 37.459
+                    },
+                    "skippedTime": "2006-01-02T15:04:05+07:00",
+                    "state": "scheduled"
+                },
+                {
+                    "actualArrivalTime": "2006-01-02T15:04:05+07:00",
+                    "actualDepartureTime": "2006-01-02T15:04:05+07:00",
+                    "address": {
+                        "externalIds": {
+                            "siteId": "54"
+                        },
+                        "id": "494123",
+                        "name": "Company Office #1"
+                    },
+                    "documents": [
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        },
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        }
+                    ],
+                    "enRouteTime": "2006-01-02T15:04:05+07:00",
+                    "eta": "2006-01-02T15:04:05+07:00",
+                    "externalIds": {
+                        "siteId": "54"
+                    },
+                    "id": "324231",
+                    "liveSharingUrl": "https://cloud.samsara.com/fleet/viewer/job/fleet_viewer_token",
+                    "locationLiveSharingLinks": [
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        },
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        }
+                    ],
+                    "name": "Stop #1",
+                    "notes": "These are my notes",
+                    "scheduledArrivalTime": "2019-06-13T19:08:25Z",
+                    "scheduledDepartureTime": "2019-06-13T19:08:25Z",
+                    "singleUseLocation": {
+                        "address": "1234 Main St, San Jose, CA",
+                        "latitude": 123.456,
+                        "longitude": 37.459
+                    },
+                    "skippedTime": "2006-01-02T15:04:05+07:00",
+                    "state": "scheduled"
+                },
+                {
+                    "actualArrivalTime": "2006-01-02T15:04:05+07:00",
+                    "actualDepartureTime": "2006-01-02T15:04:05+07:00",
+                    "address": {
+                        "externalIds": {
+                            "siteId": "54"
+                        },
+                        "id": "494123",
+                        "name": "Company Office #1"
+                    },
+                    "documents": [
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        },
+                        {
+                            "id": "494123",
+                            "name": "Fuel Receipt Wichita"
+                        }
+                    ],
+                    "enRouteTime": "2006-01-02T15:04:05+07:00",
+                    "eta": "2006-01-02T15:04:05+07:00",
+                    "externalIds": {
+                        "siteId": "54"
+                    },
+                    "id": "324231",
+                    "liveSharingUrl": "https://cloud.samsara.com/fleet/viewer/job/fleet_viewer_token",
+                    "locationLiveSharingLinks": [
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        },
+                        {
+                            "expiresAtTime": "2020-01-27T07:06:25Z",
+                            "liveSharingUrl": "https://cloud.samsara.com/o/123456/fleet/viewer/address/gEAitEnnOwcv92cuPzcU",
+                            "name": "Name"
+                        }
+                    ],
+                    "name": "Stop #1",
+                    "notes": "These are my notes",
+                    "scheduledArrivalTime": "2019-06-13T19:08:25Z",
+                    "scheduledDepartureTime": "2019-06-13T19:08:25Z",
+                    "singleUseLocation": {
+                        "address": "1234 Main St, San Jose, CA",
+                        "latitude": 123.456,
+                        "longitude": 37.459
+                    },
+                    "skippedTime": "2006-01-02T15:04:05+07:00",
+                    "state": "scheduled"
+                }
+            ]
+        },
+        "routeStopDetails": {
+            "actualArrivalTime": "2006-01-02T15:04:05+07:00",
+            "actualDepartureTime": "2006-01-02T15:04:05+07:00",
+            "enRouteTime": "2006-01-02T15:04:05+07:00",
+            "eta": "2006-01-02T15:04:05+07:00",
+            "externalIds": {
+                "siteId": "54"
+            },
+            "id": "141414",
+            "liveSharingUrl": "https://cloud.samsara.com/fleet/viewer/job/fleet_viewer_token",
+            "skippedTime": "2006-01-02T15:04:05+07:00",
+            "state": "scheduled"
+        },
+        "time": "2020-01-27T07:06:25Z",
+        "type": "route tracking",
+        "vehicle": {
+            "assetType": "vehicle",
+            "externalIds": {
+                "maintenanceId": "250020"
+            },
+            "gateway": {
+                "model": "VG34",
+                "serial": "GFRV-43N-VGX"
+            },
+            "id": "494123",
+            "licensePlate": "6SAM123",
+            "name": "Fleet Truck #1",
+            "vin": "1GBJ6P1B2HV112765"
+        }
+    }
+}
