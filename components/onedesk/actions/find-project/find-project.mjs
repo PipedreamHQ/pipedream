5fc12699f9c0c82677f23ff8abaf62dbd5022266
Jorge Cortes <jacortesmahmud@gmail.com>
2024-06-18 09:41:46 -0500

    Migration is complete with tested components (#12429)

--- components/onedesk/actions/find-project/find-project.mjs
+++ components/onedesk/actions/find-project/find-project.mjs
@@ -1,86 +1,93 @@
-import onedesk from "../../onedesk.app.mjs";
-import { ConfigurationError } from "@pipedream/platform";
+import app from "../../onedesk.app.mjs";
+import constants from "../../common/constants.mjs";
 
 export default {
   key: "onedesk-find-project",
   name: "Find Project",
-  description: "Search for a project/space by name or ID. [See the docs](https://www.onedesk.com/developers/#_search_spaces)",
-  version: "0.0.1",
+  description: "Search for a project/space by name or ID. [See the documentation](https://www.onedesk.com/dev/).",
+  version: "0.0.2",
   type: "action",
   props: {
-    onedesk,
-    spaceId: {
+    app,
+    creationTimeValue: {
+      type: "string",
+      label: "Creation Time Value",
+      description: "Project creation date. Eg. `2024-01-28`",
+    },
+    creationTimeOperator: {
+      type: "string",
+      label: "Creation Time Operator",
+      description: "Project creation date operator",
+      optional: true,
+      options: Object.values(constants.DATE_OPERATOR),
+    },
+    invoiceType: {
+      label: "Invoice Type",
+      description: "Type of invoice. Eg. `flat fee`",
       propDefinition: [
-        onedesk,
-        "spaceId",
+        app,
+        "invoice",
       ],
     },
-    name: {
+    dueDateValue: {
       type: "string",
-      label: "Name",
-      description: "Name of the project/space",
+      label: "Due Date Value",
+      description: "Due date of project. Eg. `2024-01-28`",
       optional: true,
     },
-    exactMatch: {
-      type: "boolean",
-      label: "Exact Match",
-      description: "The project name search should be an exact match",
+    dueDateOperator: {
+      type: "string",
+      label: "Due Date Operator",
+      description: "Due date of project operator",
       optional: true,
-      default: false,
+      options: Object.values(constants.DATE_OPERATOR),
     },
   },
   async run({ $ }) {
     const {
-      spaceId,
-      name,
-      exactMatch,
+      app,
+      creationTimeValue,
+      creationTimeOperator,
+      invoiceType,
+      dueDateValue,
+      dueDateOperator,
     } = this;
 
-    if (!spaceId && !name) {
-      throw new ConfigurationError("Must enter `spaceId` or `name`.");
-    }
+    const properties = [
+      {
+        property: "creationTime",
+        operation: creationTimeOperator || constants.DATE_OPERATOR.LT.value,
+        value: creationTimeValue,
+      },
+      {
+        property: "invoiceType",
+        operation: "EQ",
+        value: invoiceType,
+      },
+      {
+        property: "dueDate",
+        operation: dueDateOperator || constants.DATE_OPERATOR.LT.value,
+        value: dueDateValue,
+      },
+    ];
 
-    const data = {};
-
-    if (spaceId) {
-      data.itemIds = [
-        spaceId,
-      ];
-    }
-
-    if (name && exactMatch) {
-      data.filters = [
-        {
-          propertyName: "name",
-          operation: "EQ",
-          value: name,
+    const results = await app.paginate({
+      resourcesFn: app.filterProjectDetails,
+      resourcesFnArgs: {
+        $,
+        data: {
+          properties: properties.filter(({ value }) => value),
+          isAsc: false,
         },
-      ];
-    }
-
-    const { data: { items: projects } } = await this.onedesk.searchSpaces({
-      data,
-      $,
+      },
+      resourceName: "data",
     });
 
-    let results = [];
-    for (const projectId of projects) {
-      const { data: projectWithData } = await this.onedesk.getSpace({
-        data: {
-          id: projectId,
-        },
-        $,
-      });
-      results.push(projectWithData);
+    if (!results.length) {
+      $.export("$summary", "No projects found.");
+    } else {
+      $.export("$summary", `Found \`${results.length}\` matching project(s).`);
     }
-
-    if (name && !exactMatch) {
-      const lowerCaseName = name.toLowerCase();
-      results = results.filter((project) =>
-        project.name.toLowerCase().includes(lowerCaseName));
-    }
-
-    $.export("$summary", `Found ${results.length} matching project(s).`);
 
     return results;
   },
