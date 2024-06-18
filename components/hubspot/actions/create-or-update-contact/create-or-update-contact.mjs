08747b1da91312edb96f8fe6f65453b7cc738855
michelle0927 <michellelbergero@hotmail.com>
2024-06-18 11:42:53 -0400

    combine create-contact & create-or-update-contact

--- components/hubspot/actions/create-or-update-contact/create-or-update-contact.mjs
+++ components/hubspot/actions/create-or-update-contact/create-or-update-contact.mjs
@@ -5,107 +5,21 @@ export default {
   ...common,
   key: "hubspot-create-or-update-contact",
   name: "Create or Update Contact",
-  description: "Creates a new contact or updates an existing contact based on email address. [See the documentation](https://developers.hubspot.com/docs/api/crm/contacts#endpoint?spec=POST-/crm/v3/objects/contacts)",
+  description: "Create or update a contact in Hubspot. [See the documentation](https://developers.hubspot.com/docs/api/crm/contacts#endpoint?spec=POST-/crm/v3/objects/contacts)",
   version: "0.0.9",
   type: "action",
-  async additionalProps() {
-    const schema = await this.hubspot.getSchema({
-      objectType: this.getObjectType(),
-    });
-    const { results: properties } = await this.hubspot.getProperties({
-      objectType: this.getObjectType(),
-    });
-    const props = properties
-      .filter(this.isRelevantProperty)
-      .map((property) => this.makePropDefinition(property, schema.requiredProperties))
-      .reduce((p, {
-        name, ...definition
-      }) => {
-        p[name] = definition;
-        return p;
-      }, {});
-    const {
-      email, ...otherProps
-    } = props;
-    return email
-      ? {
-        email,
-        ...otherProps,
-      }
-      : props;
+  props: {
+    ...common.props,
+    updateIfExists: {
+      label: "Update If Exists",
+      description: "When selected, if Hubspot returns an error upon creation the resource should be updated.",
+      type: "boolean",
+    },
   },
   methods: {
     ...common.methods,
     getObjectType() {
       return OBJECT_TYPE.CONTACT;
     },
-    async getContact(email, $) {
-      if (!email) {
-        return;
-      }
-      const data = {
-        filters: [
-          {
-            propertyName: "email",
-            operator: "EQ",
-            value: email,
-          },
-        ],
-      };
-      return this.hubspot.searchCRM({
-        object: "contacts",
-        data,
-        $,
-      });
-    },
-  },
-  async run({ $ }) {
-    const {
-      hubspot,
-      /* eslint-disable no-unused-vars */
-      propertyGroups,
-      $db,
-      ...properties
-    } = this;
-    const objectType = this.getObjectType();
-
-    // checkbox (string[]) props must be semicolon separated strings
-    Object.keys(properties)
-      .forEach((key) => {
-        let value = properties[key];
-        if (Array.isArray(value)) {
-          properties[key] = value.join(";");
-        }
-      });
-
-    // search for the contact with the email provided
-    const {
-      total, results,
-    } = await this.getContact(this.email, $) || {};
-
-    // if contact is found, update it. if not, create new contact.
-    const response = total > 0
-      ? await hubspot.updateObject({
-        $,
-        objectType,
-        objectId: results[0].id,
-        data: {
-          properties,
-        },
-      })
-      : await hubspot.createObject({
-        $,
-        objectType,
-        data: {
-          properties,
-        },
-      });
-
-    const objectName = hubspot.getObjectTypeName(objectType);
-    $.export("$summary", `Successfully ${total > 0
-      ? "updated"
-      : "created"} ${objectName}`);
-
-    return response;
   },
 };
