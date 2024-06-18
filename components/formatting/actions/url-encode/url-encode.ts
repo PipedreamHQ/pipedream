83e5c8591c7bcd88f06cca19428b6048d1bffb3b
Jorge Cortes <jacortesmahmud@gmail.com>
2024-06-18 09:34:03 -0500

    Migration to Sugarjs Date formats and parsing

--- components/formatting/actions/url-encode/url-encode.ts
+++ components/formatting/actions/url-encode/url-encode.ts
@@ -5,7 +5,7 @@ export default defineAction({
   name: "[Text] Encode URL",
   description: "Encode a string as a URL",
   key: "formatting-url-encode",
-  version: "0.0.4",
+  version: "0.0.5",
   type: "action",
   props: {
     app,
