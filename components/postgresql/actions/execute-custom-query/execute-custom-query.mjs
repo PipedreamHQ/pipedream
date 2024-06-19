67669236e23833a6e7b4813dc3b239678f30d2f1
Danny Roosevelt <danny@pipedream.com>
2024-06-19 15:30:21 -0700

    Updating actions and docs

--- components/postgresql/actions/execute-custom-query/execute-custom-query.mjs
+++ components/postgresql/actions/execute-custom-query/execute-custom-query.mjs
@@ -4,7 +4,7 @@ export default {
   name: "Execute SQL Query",
   key: "postgresql-execute-custom-query",
   description: "Execute a custom PostgreSQL query. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
-  version: "3.0.0",
+  version: "3.0.1",
   type: "action",
   props: {
     postgresql,
@@ -17,8 +17,12 @@ export default {
       label: "PostreSQL Query",
     },
   },
-  run() {
+  async run({ $ }) {
     const args = this.postgresql.executeQueryAdapter(this.sql);
-    return this.postgresql.executeQuery(args);
+    const data = await this.postgresql.executeQuery(args);
+    $.export("$summary", `Returned ${data.length} ${data.length === 1
+      ? "row"
+      : "rows"}`);
+    return data;
   },
 };
