48171a0336cc9e8e00eb507bc7ad7a3451adefb9
Luan Cazarine <luanhc@gmail.com>
2024-06-19 09:28:08 -0300

    New Components - okta (#12439)
    * okta init
    
    * [Components] okta #12420
    Polling Sources
    - Watch New Events
    
    Actions
     - Create User
     - Get User
     - Update User
    
    * pnpm update
    
    * Update components/okta/actions/create-user/create-user.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update components/okta/sources/watch-new-events/watch-new-events.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * remove utils.mjs
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/okta/actions/create-user/create-user.mjs
@@ -0,0 +1,93 @@
+import { ConfigurationError } from "@pipedream/platform";
+import okta from "../../okta.app.mjs";
+
+export default {
+  key: "okta-create-user",
+  name: "Create User",
+  description: "Creates a new user in the Okta system. [See the documentation](https://developer.okta.com/docs/api/openapi/okta-management/management/tag/User/#tag/User/operation/createUser)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    okta,
+    activate: {
+      type: "boolean",
+      label: "Activate",
+      description: "Executes activation lifecycle operation when creating the user. Defaults to true.",
+      optional: true,
+    },
+    firstName: {
+      propDefinition: [
+        okta,
+        "firstName",
+      ],
+    },
+    lastName: {
+      propDefinition: [
+        okta,
+        "lastName",
+      ],
+    },
+    email: {
+      propDefinition: [
+        okta,
+        "email",
+      ],
+    },
+    login: {
+      propDefinition: [
+        okta,
+        "login",
+      ],
+    },
+    mobilePhone: {
+      propDefinition: [
+        okta,
+        "mobilePhone",
+      ],
+      optional: true,
+    },
+    typeId: {
+      propDefinition: [
+        okta,
+        "typeId",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    try {
+      const {
+        okta,
+        activate,
+        typeId,
+        ...profile
+      } = this;
+
+      const data = {
+        profile,
+      };
+
+      if (typeId) {
+        data.type = {
+          id: this.typeId,
+        };
+      }
+
+      const response = await okta.createUser({
+        $,
+        data,
+        params: {
+          activate,
+        },
+      });
+
+      $.export("$summary", `Successfully created user with ID ${response.id}`);
+      return response;
+    } catch ({ message }) {
+      const messageError = JSON.parse(message);
+      throw new ConfigurationError(messageError.errorCauses.length
+        ? messageError.errorCauses[0].errorSummary
+        : messageError.errorSummary);
+    }
+  },
+};
