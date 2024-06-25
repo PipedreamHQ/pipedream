f9cfc85457a06717d74aeda19c14659512338f66
michelle0927 <michellelbergero@hotmail.com>
2024-06-25 17:34:54 -0400

    add default props for get-company, get-contact, & get-deal

--- components/hubspot/actions/common/common-get-object.mjs
+++ components/hubspot/actions/common/common-get-object.mjs
@@ -1,4 +1,12 @@
 import hubspot from "../../hubspot.app.mjs";
+import {
+  DEFAULT_CONTACT_PROPERTIES,
+  DEFAULT_COMPANY_PROPERTIES,
+  DEFAULT_DEAL_PROPERTIES,
+  DEFAULT_TICKET_PROPERTIES,
+  DEFAULT_PRODUCT_PROPERTIES,
+  DEFAULT_LINE_ITEM_PROPERTIES,
+} from "../../common/constants.mjs";
 
 export default {
   props: {
@@ -10,6 +18,13 @@ export default {
       async options(opts) {
         return this.hubspot.createOptions(this.getObjectType(), opts);
       },
+      reloadProps: true,
+    },
+    info: {
+      type: "alert",
+      alertType: "info",
+      content: "",
+      hidden: true,
     },
     // eslint-disable-next-line pipedream/props-description
     additionalProperties: {
@@ -23,25 +38,59 @@ export default {
         const { results: properties } = await this.hubspot.getProperties({
           objectType: this.getObjectType(),
         });
-        return properties.map((property) => ({
-          label: property.label,
-          value: property.name,
-        }));
+        const defaultProperties = this.getDefaultProperties(this.getObjectType());
+        return properties
+          .filter(({ name }) => !defaultProperties.includes(name))
+          .map((property) => ({
+            label: property.label,
+            value: property.name,
+          }));
       },
     },
+  },
+  async additionalProps(props) {
+    return {
+      info: {
+        ...props.info,
+        content: `Properties:\n\`${this.getDefaultProperties(this.getObjectType()).join(", ")}\``,
+        hidden: false,
+      },
+    };
   },
   methods: {
     getObjectType() {
       throw new Error("getObjectType is not implemented");
     },
+    getDefaultProperties(objectType) {
+      if (objectType === "contact") {
+        return DEFAULT_CONTACT_PROPERTIES;
+      } else if (objectType === "company") {
+        return DEFAULT_COMPANY_PROPERTIES;
+      } else if (objectType === "deal") {
+        return DEFAULT_DEAL_PROPERTIES;
+      } else if (objectType === "ticket") {
+        return DEFAULT_TICKET_PROPERTIES;
+      } else if (objectType === "product") {
+        return DEFAULT_PRODUCT_PROPERTIES;
+      } else if (objectType === "line_item") {
+        return DEFAULT_LINE_ITEM_PROPERTIES;
+      } else {
+        return [];
+      }
+    },
   },
   async run({ $ }) {
     const objectType = this.getObjectType();
+    const { additionalProperties = [] } = this;
+    const defaultProperties = this.getDefaultProperties(this.getObjectType());
 
     const object = await this.hubspot.getObject(
       objectType,
       this.objectId,
-      this.additionalProperties,
+      [
+        ...defaultProperties,
+        ...additionalProperties,
+      ],
       $,
     );
 
