be184d6efe6bf565a16c2d7748371586f099c4dc
Danny Roosevelt <danny@pipedream.com>
2024-06-26 16:20:40 -0700

    Create query-sql-database.mjs

---
+++ components/database/actions/query-sql-database/query-sql-database.mjs
@@ -0,0 +1,28 @@
+import postgresql from "../../../postgresql/postgresql.app.mjs";
+
+export default {
+  name: "Query SQL Database",
+  key: "database-query-sql-database",
+  description: "Execute a SQL Database. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
+  version: "0.0.{{ts}}",
+  type: "action",
+  props: {
+    postgresql,
+    // eslint-disable-next-line pipedream/props-description
+    sql: {
+      type: "sql",
+      auth: {
+        app: "postgresql",
+      },
+      label: "PostreSQL Query",
+    },
+  },
+  async run({ $ }) {
+    const args = this.postgresql.executeQueryAdapter(this.sql);
+    const data = await this.postgresql.executeQuery(args);
+    $.export("$summary", `Returned ${data.length} ${data.length === 1
+      ? "row"
+      : "rows"}`);
+    return data;
+  },
+};
