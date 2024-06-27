63e4036d5110eb31da9ca423a029afb8bb07cff2
Luan Cazarine <luanhc@gmail.com>
2024-06-27 12:25:40 -0300

    New Components - clickfunnels (#12577)
    * clickfunnels init
    
    * [Components] clickfunnels #12570
    Sources
     - New Contact Identified (Instant)
     - New One Time Order Paid (Instant)
     - New Subscription Invoice Paid (Instant)
    
    Actions
     - Apply Tag Contact
     - Remove Tag Contact
     - Update Create Contact
    
    * pnpm update
    
    * Fix event
    
    * Update components/clickfunnels/sources/new-subscription-invoice-paid-instant/new-subscription-invoice-paid-instant.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leo Vu <18277920+vunguyenhung@users.noreply.github.com>
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/clickfunnels/common/utils.mjs
@@ -0,0 +1,41 @@
+export const clearObj = (obj) => {
+  return Object.entries(obj)
+    .filter(([
+      ,
+      v,
+    ]) => (v != null && v != "" && JSON.stringify(v) != "{}"))
+    .reduce((acc, [
+      k,
+      v,
+    ]) => ({
+      ...acc,
+      [k]: (!Array.isArray(v) && v === Object(v))
+        ? clearObj(v)
+        : v,
+    }), {});
+};
+
+export const parseObject = (obj) => {
+  if (!obj) return undefined;
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => {
+      if (typeof item === "string") {
+        try {
+          return JSON.parse(item);
+        } catch (e) {
+          return item;
+        }
+      }
+      return item;
+    });
+  }
+  if (typeof obj === "string") {
+    try {
+      return JSON.parse(obj);
+    } catch (e) {
+      return obj;
+    }
+  }
+  return obj;
+};
