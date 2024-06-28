a9b2a29bff3779d0026ce436e103ee479c7caec5
michelle0927 <michellelbergero@hotmail.com>
2024-06-28 17:40:18 -0400

    new components

--- components/spiritme/spiritme.app.mjs
+++ components/spiritme/spiritme.app.mjs
@@ -1,94 +1,130 @@
 import { axios } from "@pipedream/platform";
+const DEFAULT_LIMIT = 50;
 
 export default {
   type: "app",
   app: "spiritme",
   propDefinitions: {
-    avatarId: {
-      type: "string",
-      label: "Avatar ID",
-      description: "The identifier of the avatar",
-      required: true,
-    },
-    systemStatusNotifications: {
-      type: "boolean",
-      label: "System Status Notifications",
-      description: "Enable notifications for success, failure, insufficient balance",
-      optional: true,
-    },
-    voice: {
-      type: "object",
-      label: "Voice",
-      description: "Characteristics of the voice to be used, such as pitch, speed, and accent",
-      required: true,
-    },
     avatar: {
       type: "string",
       label: "Avatar",
-      description: "The identifier for the avatar to be used",
+      description: "The identifier for the avatar to be used. One of avatar or media is required.",
       optional: true,
+      async options({ page }) {
+        const { results } = await this.listAvatars({
+          params: {
+            limit: DEFAULT_LIMIT,
+            offset: page * DEFAULT_LIMIT,
+          },
+        });
+        return results?.map(({
+          id: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
     },
-    videoLength: {
-      type: "integer",
-      label: "Video Length",
-      description: "The length of the video in seconds",
-      optional: true,
-    },
-    script: {
+    voice: {
       type: "string",
-      label: "Script",
-      description: "The script for the video",
+      label: "Voice",
+      description: "The identifier of the voice to be used",
       optional: true,
+      async options({ page }) {
+        const { results } = await this.listVoices({
+          params: {
+            limit: DEFAULT_LIMIT,
+            offset: page * DEFAULT_LIMIT,
+          },
+        });
+        return results?.map(({
+          id: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
     },
-    desiredAnimation: {
+    file: {
       type: "string",
-      label: "Desired Animation",
-      description: "Desired animation for the video",
+      label: "Voice",
+      description: "The identifier of the file to be used",
       optional: true,
+      async options({
+        page, type,
+      }) {
+        const { results } = await this.listFiles({
+          params: {
+            limit: DEFAULT_LIMIT,
+            offset: page * DEFAULT_LIMIT,
+            type,
+          },
+        });
+        return results?.map(({
+          id: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
     },
   },
   methods: {
     _baseUrl() {
       return "https://api.spiritme.tech/api";
     },
-    async _makeRequest(opts = {}) {
+    _makeRequest(opts = {}) {
       const {
         $ = this,
-        method = "GET",
         path,
-        data,
-        params,
-        headers,
         ...otherOpts
       } = opts;
       return axios($, {
         ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        data,
-        params,
+        url: `${this._baseUrl()}${path}`,
         headers: {
-          ...headers,
-          "Authorization": `Token ${this.$auth.api_key}`,
+          Authorization: `Token ${this.$auth.api_key}`,
+          Accept: "application/json",
         },
       });
     },
-    async getVideoStatus(videoId) {
+    listAvatars(opts = {}) {
       return this._makeRequest({
-        path: `/videos/${videoId}/`,
+        path: "/avatars/",
+        ...opts,
       });
     },
-    async generateVideo(voice, avatar, videoLength, script, desiredAnimation) {
+    listVoices(opts = {}) {
+      return this._makeRequest({
+        path: "/tts/voices/",
+        ...opts,
+      });
+    },
+    listFiles(opts = {}) {
+      return this._makeRequest({
+        path: "/files/",
+        ...opts,
+      });
+    },
+    listVideos(opts = {}) {
+      return this._makeRequest({
+        path: "/videos/",
+        ...opts,
+      });
+    },
+    getVideo({
+      videoId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/videos/${videoId}/`,
+        ...opts,
+      });
+    },
+    generateVideo(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/videos/",
-        data: {
-          voice,
-          avatar,
-          video_length: videoLength,
-          script,
-          desired_animation: desiredAnimation,
-        },
+        ...opts,
       });
     },
   },
