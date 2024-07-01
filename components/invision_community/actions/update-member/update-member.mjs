0355b13be0098cbb0241f62506d3a27877a63f5d
Luan Cazarine <luanhc@gmail.com>
2024-07-01 12:35:03 -0300

    New Components - invision_community (#12623)
    * invision_community init
    
    * [Components] invision_community #12563
    Sources
     - New Member (Instant)
     - New Forum Topic (Instant)
     - New Topic Post (Instant)
    
    Actions
     - Create Member
     - Update Member
     - Create Forum Topic
    
    * pnpm update

---
+++ components/invision_community/actions/update-member/update-member.mjs
@@ -0,0 +1,69 @@
+import invisionCommunity from "../../invision_community.app.mjs";
+
+export default {
+  key: "invision_community-update-member",
+  name: "Update Member",
+  description: "Updates an existing member's details. [See the documentation](https://invisioncommunity.com/developers/rest-api?endpoint=core/members/postitem)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    invisionCommunity,
+    memberId: {
+      propDefinition: [
+        invisionCommunity,
+        "memberId",
+      ],
+    },
+    name: {
+      propDefinition: [
+        invisionCommunity,
+        "name",
+      ],
+      optional: true,
+    },
+    email: {
+      propDefinition: [
+        invisionCommunity,
+        "email",
+      ],
+      optional: true,
+    },
+    password: {
+      propDefinition: [
+        invisionCommunity,
+        "password",
+      ],
+      optional: true,
+    },
+    groupId: {
+      propDefinition: [
+        invisionCommunity,
+        "groupId",
+      ],
+      optional: true,
+    },
+    validated: {
+      propDefinition: [
+        invisionCommunity,
+        "validated",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.invisionCommunity.updateMember({
+      $,
+      memberId: this.memberId,
+      params: {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        group: this.groupId,
+        registrationIpAddress: this.registrationIpAddress,
+        validated: this.validated,
+      },
+    });
+    $.export("$summary", `Successfully updated member with Id: ${this.memberId}`);
+    return response;
+  },
+};
