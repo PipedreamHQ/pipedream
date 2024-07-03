1ec55e15449d190b873107c8a106b379b5866538
michelle0927 <michellelbergero@hotmail.com>
2024-07-03 10:57:43 -0400

    azure_sql updates

---
+++ components/azure_sql/actions/execute-raw-query/execute-raw-query.mjs
@@ -0,0 +1,28 @@
+import app from "../../azure_sql.app.mjs";
+
+export default {
+  key: "azure_sql-execute-raw-query",
+  name: "Execute SQL Query",
+  description: "Execute a custom SQL query. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    // eslint-disable-next-line pipedream/props-description
+    sql: {
+      type: "sql",
+      auth: {
+        app: "axure_sql",
+      },
+      label: "SQL Query",
+    },
+  },
+  async run({ $ }) {
+    const args = this.app.executeQueryAdapter(this.sql);
+    return await this.app.executeQuery({
+      $,
+      summary: () => "Successfully executed query.",
+      ...args,
+    });
+  },
+};
--- components/azure_sql/azure_sql.app.mjs
+++ components/azure_sql/azure_sql.app.mjs
@@ -50,6 +50,66 @@ export default {
     getConnection() {
       return sql.connect(this.getConfig());
     },
+    /**
+     * A helper method to get the schema of the database. Used by other features
+     * (like the `sql` prop) to enrich the code editor and provide the user with
+     * auto-complete and fields suggestion.
+     *
+     * @returns {DbInfo} The schema of the database, which is a
+     * JSON-serializable object.
+     */
+    async getSchema() {
+      const sql = `
+        SELECT t.table_schema AS tableSchema,
+            t.table_name AS tableName,
+            CAST(t.table_rows AS UNSIGNED) AS rowCount,
+            c.column_name AS columnName,
+            c.data_type AS dataType,
+            c.is_nullable AS isNullable,
+            c.column_default AS columnDefault
+        FROM information_schema.tables AS t
+            JOIN information_schema.columns AS c ON t.table_name = c.table_name
+            AND t.table_schema = c.table_schema
+        WHERE t.table_schema = ?
+        ORDER BY t.table_name,
+            c.ordinal_position
+      `;
+      const rows = await this.executeQuery({
+        query: sql,
+      });
+      return rows.reduce((acc, row) => {
+        acc[row.tableName] ??= {
+          metadata: {
+            rowCount: row.rowCount,
+          },
+          schema: {},
+        };
+        acc[row.tableName].schema[row.columnName] = {
+          ...row,
+        };
+        return acc;
+      }, {});
+    },
+    /**
+     * A method that performs the inverse transformation of `proxyAdapter`.
+     *
+     * @param {object} proxyArgs - The output of `proxyAdapter`.
+     * @param {string} proxyArgs.query - The SQL query to be executed.
+     * @param {string[]} proxyArgs.params - The values to replace in the SQL
+     * query.
+     * @returns {object} - The adapted query and parameters, compatible with
+     * `executeQuery`.
+     */
+    executeQueryAdapter(proxyArgs = {}) {
+      const {
+        query = "",
+        params: inputs = [],
+      } = proxyArgs;
+      return {
+        query,
+        inputs,
+      };
+    },
     async executeQuery({
       $ = this, summary, query = "", inputs = {},
     } = {}) {
