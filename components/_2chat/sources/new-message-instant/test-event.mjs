7e004ef31fc551e373daf751b501618cf8c1178a
michelle0927 <michelle0927@users.noreply.github.com>
2024-07-09 10:44:54 -0400

    New Components - 2chat (#12743)
    * 2chat init
    
    * new components
    
    * pnpm-lock.yaml

---
+++ components/_2chat/sources/new-message-instant/test-event.mjs
@@ -0,0 +1,20 @@
+export default {
+  "id": "MSGcf20bbdb-0a8a-4eef-bca1-aa2ea22c5294",
+  "uuid": "MSGcf20bbdb-0a8a-4eef-bca1-aa2ea22c5294",
+  "created_at": "2023-04-18T22:17:50Z",
+  "session_key": "WW-WPN1045d068-6c42-4157-8325-41e3c5710dc6-5215533334444@c.us",
+  "message": {
+    "media": {
+      "url": "https://2chat-user-data.s3.amazonaws.com/ACC28526e8e-ac01-4b1f-ac11-939f2b7fca73/example.jpeg",
+      "type": "image",
+      "mime_type": "image/jpeg",
+    },
+  },
+  "remote_phone_number": "+5215533334444",
+  "sent_by": "user",
+  "contact": {
+    "first_name": "Johny",
+    "last_name": "Doe",
+    "profile_pic_url": "https://2chat-user-data.s3.amazonaws.com/example-profile-pic.jpeg",
+  },
+};
