6e529ef9ea8ba13edba145c165ed3822ef53f4b9
John Nelson <john.aaron.nelson@gmail.com>
2024-07-09 11:32:27 -0400

    refactor demo

--- packages/sdk/examples/next-app/app/page.tsx
+++ packages/sdk/examples/next-app/app/page.tsx
@@ -1,75 +1,114 @@
 "use client"
 
 import { createRef, useEffect, useState } from "react";
-import { serverConnectTokenCreate, getAppsData, getSpotifyData, getSlackData } from "./server"
+import { serverConnectTokenCreate, getAppsData } from "./server"
 import { createClient } from "../../../src/browser"
 
-const PIPEDREAM_PROJECT_PUBLIC_KEY = process.env.NEXT_PUBLIC_PIPEDREAM_PROJECT_PUBLIC_KEY
-const PIPEDREAM_TEST_APP_ID = process.env.NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID
-if (!PIPEDREAM_PROJECT_PUBLIC_KEY) {
-  throw new Error("Missing NEXT_PUBLIC_PIPEDREAM_PROJECT_PUBLIC_KEY env var")
-}
-if (!PIPEDREAM_TEST_APP_ID) {
-  throw new Error("Missing NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID env var")
-}
+const publicKey = process.env.NEXT_PUBLIC_PIPEDREAM_PROJECT_PUBLIC_KEY
+const frontendHost = process.env.NEXT_PUBLIC_PIPEDREAM_FRONTEND_HOST
+const oauthAppId = process.env.NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID
 
-const pd = createClient({
-  publicKey: PIPEDREAM_PROJECT_PUBLIC_KEY,
-  frontendHost: process.env.NEXT_PUBLIC_PIPEDREAM_FRONTEND_HOST,
-})
+if (!publicKey) throw new Error("Missing NEXT_PUBLIC_PIPEDREAM_PROJECT_PUBLIC_KEY env var")
+if (!oauthAppId) throw new Error("Missing NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID env var")
+
+const pd = createClient({ publicKey, frontendHost })
 
 export default function Home() {
-  const [clientUserId, setClientUserId] = useState<string | null>(null)
+  const [externalUserId, setExternalUserId] = useState<string | null>(null)
   const [githubData, setGithubData] = useState<{ login: string } | null>(null)
   const [token, setToken] = useState<string | null>(null)
-  const [app, setApp] = useState<string>("")
+  const [oa, setOauthAppId] = useState<string | null>(null)
+  const [apn, setAuthProvisionId] = useState<string | null>(null)
   const inputRef = createRef<HTMLInputElement>()
-  useEffect(() => {
-    if (!clientUserId) {
-      setGithubData(null)
-    } else {
-      getAppsData(clientUserId).then((d) => {
-        setGithubData(d.github)
-      })
-    }
-  }, [clientUserId])
 
-  useEffect(() => {
-    if (!clientUserId) return
-    serverConnectTokenCreate(clientUserId).then((t) => setToken(t))
-  }, [clientUserId])
-
-  const connectApp = (app: string) => {
-    if (!clientUserId || !token) return
+  const connectApp = (oauthAppId: string) => {
+    if (!externalUserId || !token) return
     pd.startConnect({
-      app,
+      app: oauthAppId,
       token,
-      onSuccess: () => {
-        console.log(`Connected ${app} account for ${clientUserId}`)
+      onSuccess: ({ id: authProvisionId }: any) => {
+        setOauthAppId(oauthAppId)
+        setAuthProvisionId(authProvisionId as string)
       }
     })
   }
 
   const connectGitHub = async () => {
-    if (!PIPEDREAM_TEST_APP_ID) {
-      console.log("Missing NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID env var")
-      return
+    if (oauthAppId) connectApp(oauthAppId)
+    else console.error("Missing NEXT_PUBLIC_PIPEDREAM_TEST_APP_ID env var")
+  }
+
+  const signIn = () => {
+    setExternalUserId(inputRef.current?.value || "")
+  }
+
+  const signOut = () => {
+    setExternalUserId(null)
+  }
+
+  // Reactive Effects
+  useEffect(() => {
+    if (!externalUserId) {
+      setToken(null)
+      setGithubData(null)
+      setOauthAppId(null)
+      setAuthProvisionId(null)
+    } else {
+      getAppsData(externalUserId).then((d) => {
+        console.log('Github App Data? :>> ', d);
+        setGithubData(d.github)
+      })
     }
-    connectApp(PIPEDREAM_TEST_APP_ID)
-  } 
+  }, [externalUserId])
+
+  useEffect(() => {
+    if (!externalUserId) return
+    serverConnectTokenCreate(externalUserId).then((t) => setToken(t))
+  }, [externalUserId])
+
+
   return (
     <main className="p-5 flex flex-col gap-2">
       <h1 className="text-2xl font-bold">Pipedream connect Next.js demo</h1>
-      {!clientUserId ?
-        <p><input className="border" ref={inputRef} placeholder="clientUserId" /> <button onClick={() => setClientUserId(inputRef.current?.value || "")} style={{all: "revert"}}>Sign in</button></p>
-        : <div>
-            <p>Signed in as: {clientUserId} <button onClick={() => setClientUserId(null)} style={{all: "revert"}}>Sign out</button></p>
+      {
+        externalUserId ?
+          <div>
+            <p>External User Id: {externalUserId} </p>
             <p>Token: {token}</p>
-            <p className="pt-2">Your GitHub username: {githubData
-              ? <span><b>{githubData.login}</b></span>
-              : <button style={{all: "revert"}} onClick={connectGitHub}>Connect your GitHub account</button>}
+
+            {apn ?
+              <div>
+                <p>
+                  Auth Provision Id: {apn}
+                </p>
+                <p>
+                  Oauth App Id: {oa}
+                </p>
+                <p>
+                  <button onClick={signOut} style={{ all: "revert" }}>Sign out</button>
+                </p>
+              </div>
+              : <button style={{ all: "revert" }} onClick={connectGitHub}>Connect your GitHub account</button>
+            }
+            {
+              githubData?.login &&
+              <p className="pt-2">Your GitHub username:
+                <span><b>{githubData.login}</b></span>
+              </p>
+            }
+            <p>
             </p>
           </div>
+          :
+          <div>
+            <p>
+              <input className="border" ref={inputRef} placeholder="External user id" />
+            </p>
+            <p>
+              <button onClick={signIn} style={{ all: "revert" }}>Sign in</button>
+            </p>
+          </div>
+
       }
     </main>
   );
