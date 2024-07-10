52b1e86ed0996129ea3617d6b556cdabddd10e29
GTFalcao <gtfalcao96@gmail.com>
2024-07-09 21:48:32 -0300

    Attachment props + common props w/ async options

---
+++ components/salesforce_rest_api/common/props.mjs
@@ -0,0 +1,28 @@
+export default {
+  PartnerNetworkConnectionId: {
+    type: "string",
+    label: "Partner Network Connection ID",
+    description: "The ID of a connection between Salesforce organizations.",
+    optional: true,
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("PartnerNetworkConnection");
+      return items?.map((item) => ({
+        label: item.ConnectionName,
+        value: item.Id,
+      }));
+    },
+  },
+  UserId: {
+    type: "string",
+    label: "User ID",
+    description: "The ID of a user in your organization.",
+    optional: true,
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("User");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+};
