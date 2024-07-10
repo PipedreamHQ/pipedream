6f80db10d7767c9df7f95d1055eb2a11d26ed09a
GTFalcao <gtfalcao96@gmail.com>
2024-07-09 23:20:00 -0300

    Case props and lots of async options props

--- components/salesforce_rest_api/common/props.mjs
+++ components/salesforce_rest_api/common/props.mjs
@@ -1,4 +1,64 @@
 export default {
+  AccountId: {
+    type: "string",
+    label: "Account ID",
+    description: "The ID of an Account.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("Account");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+  BusinessHoursId: {
+    type: "string",
+    label: "Business Hours ID",
+    description: "The ID of a Business Hours object.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("BusinessHours");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+  CaseId: {
+    type: "string",
+    label: "Case ID",
+    description: "The ID of a Case object.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("Case");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+  CommunityId: {
+    type: "string",
+    label: "Community ID",
+    description: "The ID of a Community (Zone) object.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("Community");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+  ContactId: {
+    type: "string",
+    label: "Account ID",
+    description: "The ID of a Contact.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("Contact");
+      return items?.map((item) => ({
+        label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
   PartnerNetworkConnectionId: {
     type: "string",
     label: "Partner Network Connection ID",
@@ -19,6 +79,30 @@ export default {
       const items = await this.salesforce.listSObjectTypeIds("User");
       return items?.map((item) => ({
         label: item.Name,
+        value: item.Id,
+      }));
+    },
+  },
+  QuestionId: {
+    type: "string",
+    label: "Question ID",
+    description: "The ID of a Question object.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("Question");
+      return items?.map((item) => ({
+        label: item.Title,
+        value: item.Id,
+      }));
+    },
+  },
+  RecordTypeId: {
+    type: "string",
+    label: "Record Type ID",
+    description: "ID of the record type assigned to this object.",
+    async options() {
+      const items = await this.salesforce.listSObjectTypeIds("RecordType");
+      return items?.map((item) => ({
+        label: item.Title,
         value: item.Id,
       }));
     },
--- components/salesforce_rest_api/common/sobjects/case.mjs
+++ components/salesforce_rest_api/common/sobjects/case.mjs
@@ -1,17 +1,192 @@
+import commonProps from "../props.mjs";
+
 export default {
-  AccountId: {
-    type: "string",
-    label: "Account ID",
-    description: "ID of the account associated with this case.",
+  createProps: {},
+  initialProps: {
+    Description: {
+      type: "string",
+      label: "Description",
+      description: "A text description of the case. Limit: 32 KB.",
+      optional: true,
+    },
+    Status: {
+      type: "string",
+      label: "Status",
+      description: "The status of the case.",
+      optional: true,
+      options: [
+        "New",
+        "Working",
+        "Escalated",
+        "Closed",
+      ],
+    },
+    Subject: {
+      type: "string",
+      label: "Subject",
+      description: "The subject of the case. Max 255 characters.",
+      optional: true,
+    },
   },
-  Description: {
-    type: "string",
-    label: "Description",
-    description: "A text description of the case. Limit: 32 KB.",
-  },
-  IsEscalated: {
-    type: "boolean",
-    label: "Is Escalated?",
-    description: "Indicates whether the case has been escalated (true) or not. A case's escalated state does not affect how you can use a case, or whether you can query, delete, or update it. You can set this flag via the API. Label is Escalated.",
+  extraProps: {
+    AccountId: {
+      ...commonProps.AccountId,
+      description: "ID of the Account associated with this case.",
+      optional: true,
+    },
+    BusinessHoursId: {
+      ...commonProps.BusinessHoursId,
+      description: "ID of the Business Hours associated with this case.",
+      optional: true,
+    },
+    CommunityId: {
+      ...commonProps.CommunityId,
+      description:
+        "ID of the [Community (Zone)](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_community.htm) associated with this case.",
+      optional: true,
+    },
+    ConnectionReceivedId: {
+      ...commonProps.PartnerNetworkConnectionId,
+      label: "Connection Received ID",
+      description:
+        "ID of the `PartnerNetworkConnection` that shared this record with your organization.",
+      optional: true,
+    },
+    ContactId: {
+      ...commonProps.ContactId,
+      description: "ID of the Contact associated with this case.",
+      optional: true,
+    },
+    FeedItemId: {
+      ...commonProps.FeedItemId,
+      description: "ID of the question in Chatter associated with the case.",
+      optional: true,
+    },
+    IsEscalated: {
+      type: "boolean",
+      label: "Escalated",
+      description:
+        "Indicates whether the case has been escalated. A case's escalated state does not affect how you can use a case, or whether you can query, delete, or update it.",
+      optional: true,
+    },
+    IsStopped: {
+      type: "boolean",
+      label: "Is Stopped",
+      description:
+        "Indicates whether an entitlement process on a case is stopped.",
+      optional: true,
+    },
+    Language: {
+      type: "string",
+      label: "Language",
+      description: "The language of the case.",
+      optional: true,
+    },
+    Origin: {
+      type: "string",
+      label: "Case Origin",
+      description: "The source of the case.",
+      optional: true,
+      options: [
+        "Phone",
+        "Email",
+        "Web",
+      ],
+    },
+    OwnerId: {
+      ...commonProps.ContactId,
+      description: "ID of the contact who owns the case.",
+      optional: true,
+    },
+    ParentId: {
+      ...commonProps.CaseId,
+      description: "The ID of the parent case in the hierarchy.",
+      optional: true,
+    },
+    Priority: {
+      type: "string",
+      label: "Priority",
+      description: "The importance or urgency of the case.",
+      optional: true,
+      options: [
+        "High",
+        "Medium",
+        "Low",
+      ],
+    },
+    QuestionId: {
+      ...commonProps.QuestionId,
+      description:
+        "The question in the answers community that is associated with the case.",
+      optional: true,
+    },
+    Reason: {
+      type: "string",
+      label: "Reason",
+      description: "The reason why the case was created.",
+      optional: true,
+      options: [
+        "Installation",
+        "Equipment Complexity",
+        "Performance",
+        "Breakdown",
+        "Equipment Design",
+        "Feedback",
+        "Other",
+      ],
+    },
+    RecordTypeId: {
+      ...commonProps.RecordTypeId,
+      optional: true,
+    },
+    SlaStartDate: {
+      type: "string",
+      label: "SLA Start Date",
+      description: "The time that the case entered an entitlement process.",
+      optional: true,
+    },
+    SourceId: {
+      type: "string",
+      label: "Source ID",
+      description: "The ID of the social post source.",
+      optional: true,
+    },
+    SuppliedCompany: {
+      type: "string",
+      label: "Company",
+      description: "The company name that was entered when the case was created.",
+      optional: true,
+    },
+    SuppliedEmail: {
+      type: "string",
+      label: "Email",
+      description: "The email address that was entered when the case was created.",
+      optional: true,
+    },
+    SuppliedName: {
+      type: "string",
+      label: "Name",
+      description: "The name that was entered when the case was created.",
+      optional: true,
+    },
+    SuppliedPhone: {
+      type: "string",
+      label: "Phone",
+      description: "The phone number that was entered when the case was created.",
+      optional: true,
+    },
+    Type: {
+      type: "string",
+      label: "Type",
+      description: "The type of case.",
+      optional: true,
+      options: [
+        "Mechanical",
+        "Electrical",
+        "Electronic",
+        "Structural",
+        "Other",
+      ],
+    },
   },
 };
