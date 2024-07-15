c5a8f34705fdcce08cda16a25591496d72ffe356
Luan Cazarine <luanhc@gmail.com>
2024-07-15 11:35:44 -0300

    New Components - adobe_photoshop (#12810)
    * adobe_photoshop init
    
    * [Components] adobe_photoshop #12571
    Actions
     - Remove Background From Image
    
    * pnpm update
    
    * some adjusts
    
    * Fix API Doc url
    
    ---------
    
    Co-authored-by: Leo Vu <18277920+vunguyenhung@users.noreply.github.com>

---
+++ components/adobe_photoshop/common/constants.mjs
@@ -0,0 +1,14 @@
+export const STORAGE_OPTIONS = [
+  "external",
+  "azure",
+  "dropbox",
+  "adobe",
+];
+export const MASK_FORMAT_OPTIONS = [
+  "soft",
+  "binary",
+];
+export const OPTIMIZE_OPTIONS = [
+  "performance",
+  "batch",
+];
