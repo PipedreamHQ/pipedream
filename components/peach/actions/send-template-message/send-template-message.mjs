5353d622fe55a5005a0f7455f480df823ae0ddbd
Luan Cazarine <luanhc@gmail.com>
2024-07-16 17:55:19 -0300

    [Components] peach #12735 Actions  - Send Template Message

--- components/peach/actions/send-template-message/send-template-message.mjs
+++ components/peach/actions/send-template-message/send-template-message.mjs
@@ -1,56 +1,60 @@
+import { clearObj } from "../../common/utils.mjs";
 import peach from "../../peach.app.mjs";
-import { axios } from "@pipedream/platform";
 
 export default {
   key: "peach-send-template-message",
   name: "Send Template Message",
-  description: "Send a predefined message to a contact within the Peach app. [See the documentation](https://peach.apidocumentation.com/reference)",
-  version: "0.0.{{ts}}",
+  description: "Send a predefined message to a contact within the Peach app. [See the documentation](https://peach.apidocumentation.com/reference#tag/messaging/post/transactional_messages)",
+  version: "0.0.1",
   type: "action",
   props: {
     peach,
     phoneNumber: {
-      propDefinition: [
-        peach,
-        "phoneNumber",
-      ],
+      type: "string",
+      label: "Phone Number",
+      description: "The phone number of the contact to send the message to",
     },
     templateName: {
-      propDefinition: [
-        peach,
-        "templateName",
-      ],
-      optional: true,
+      type: "string",
+      label: "Template Name",
+      description: "WhatsApp approved utility template name",
     },
     contactName: {
-      propDefinition: [
-        peach,
-        "contactName",
-      ],
+      type: "string",
+      label: "Contact Name",
+      description: "The name of the contact",
       optional: true,
     },
     contactEmail: {
-      propDefinition: [
-        peach,
-        "contactEmail",
-      ],
+      type: "string",
+      label: "Contact Email",
+      description: "The email of the contact",
       optional: true,
     },
     arguments: {
-      propDefinition: [
-        peach,
-        "arguments",
-      ],
+      type: "object",
+      label: "Arguments",
+      description: "Arguments for the template",
       optional: true,
     },
   },
   async run({ $ }) {
     const response = await this.peach.sendTransactionalMessage({
-      phoneNumber: this.phoneNumber,
-      templateName: this.templateName,
-      contactName: this.contactName,
-      contactEmail: this.contactEmail,
-      arguments: this.arguments,
+      $,
+      data: clearObj({
+        template_name: this.templateName,
+        to: {
+          name: this.contactName,
+          email: this.contactEmail,
+          phone_number: this.phoneNumber,
+        },
+        arguments: this.arguments
+          ? Object.keys(this.arguments).map((key) => ({
+            key,
+            value: this.arguments[key],
+          }))
+          : {},
+      }),
     });
 
     $.export("$summary", `Message sent successfully to ${this.phoneNumber}`);
--- components/peach/peach.app.mjs
+++ components/peach/peach.app.mjs
@@ -3,93 +3,30 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "peach",
-  propDefinitions: {
-    phoneNumber: {
-      type: "string",
-      label: "Phone Number",
-      description: "The phone number of the contact to send the message to",
-    },
-    templateName: {
-      type: "string",
-      label: "Template Name",
-      description: "WhatsApp approved utility template name",
-      optional: true,
-    },
-    contactName: {
-      type: "string",
-      label: "Contact Name",
-      description: "The name of the contact",
-      optional: true,
-    },
-    contactEmail: {
-      type: "string",
-      label: "Contact Email",
-      description: "The email of the contact",
-      optional: true,
-    },
-    arguments: {
-      type: "object",
-      label: "Arguments",
-      description: "Arguments for the template",
-      optional: true,
-    },
-  },
   methods: {
     _baseUrl() {
       return "https://app.trypeach.io/api/v1";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path = "/",
-        headers,
-        ...otherOpts
-      } = opts;
+    _headers() {
+      return {
+        "Authorization": `${this.$auth.api_token}`,
+        "Content-Type": "application/json",
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
         url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "Authorization": `Bearer ${this.$auth.api_key}`,
-          "Content-Type": "application/json",
-        },
+        headers: this._headers(),
+        ...opts,
       });
     },
-    async sendTransactionalMessage(opts = {}) {
-      const {
-        phoneNumber,
-        templateName,
-        contactName,
-        contactEmail,
-        arguments: args,
-        ...otherOpts
-      } = opts;
-
-      const data = {
-        template_name: templateName,
-        to: {
-          name: contactName,
-          email: contactEmail,
-          phone_number: phoneNumber,
-        },
-        arguments: args,
-      };
-
+    sendTransactionalMessage(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/transactional_messages",
-        data,
-        ...otherOpts,
-      });
-    },
-    async emitNewAppResponseCreated() {
-      // Logic to emit new event when a new app response is created on Peach
-      // This is a placeholder function. Actual implementation would depend on the specific event system and API endpoints provided by Peach.
-      return this._makeRequest({
-        method: "GET",
-        path: "/new_event_endpoint", // Replace with actual endpoint
+        ...opts,
       });
     },
   },
