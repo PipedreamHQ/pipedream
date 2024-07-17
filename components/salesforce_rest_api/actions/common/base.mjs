a4fcd311e9bf1cabace085a6385921c7be4061ba
GTFalcao <gtfalcao96@gmail.com>
2024-07-17 18:43:19 -0300

    Create Campaign + date prop parsing

--- components/salesforce_rest_api/actions/common/base.mjs
+++ components/salesforce_rest_api/actions/common/base.mjs
@@ -1,3 +1,4 @@
+import { ConfigurationError } from "@pipedream/platform";
 import salesforce from "../../salesforce_rest_api.app.mjs";
 
 export function getProps({
@@ -65,6 +66,25 @@ export default {
           }
         }),
       );
+    },
+    formatDateTimeProps(props = {}) {
+      // https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/intro_valid_date_formats.htm
+      return Object.fromEntries(Object.entries(props).map(([
+        key,
+        value,
+      ]) => {
+        const numValue = Number(value);
+        const date = new Date(isNaN(numValue)
+          ? value
+          : numValue);
+        if (isNaN(date.valueOf())) {
+          throw new ConfigurationError(`Invalid date format for prop \`${key}\`. Please provide a [valid date string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#date_time_string_format).`);
+        }
+        return [
+          key,
+          date.toISOString(),
+        ];
+      }));
     },
   },
   additionalProps() {
--- components/salesforce_rest_api/actions/create-campaign/create-campaign.mjs
+++ components/salesforce_rest_api/actions/create-campaign/create-campaign.mjs
@@ -1,52 +1,41 @@
-import common from "../common/base.mjs";
+import common, { getProps } from "../common/base.mjs";
 import campaign from "../../common/sobjects/campaign.mjs";
-import {
-  pickBy, pick,
-} from "lodash-es";
-import { toSingleLineString } from "../../common/utils.mjs";
 
-const { salesforce } = common.props;
+const docsLink = "https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_campaign.htm";
 
 export default {
   ...common,
   key: "salesforce_rest_api-create-campaign",
   name: "Create Campaign",
-  description: toSingleLineString(`
-    Creates a marketing campaign, such as a direct mail promotion, webinar, or trade show.
-    See [Campaign SObject](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_campaign.htm)
-    and [Create Record](https://developer.salesforce.com/docs/atlas.en-us.228.0.api_rest.meta/api_rest/dome_sobject_create.htm)
-  `),
-  version: "0.3.0",
+  description: `Creates a marketing campaign. [See the documentation](${docsLink})`,
+  version: "0.3.{{ts}}",
   type: "action",
-  props: {
-    salesforce,
-    Name: {
-      type: "string",
-      label: "Name",
-      description: "Required. Name of the campaign. Limit: is 80 characters.",
-    },
-    selector: {
-      propDefinition: [
-        salesforce,
-        "fieldSelector",
-      ],
-      description: `${salesforce.propDefinitions.fieldSelector.description} Campaign`,
-      options: () => Object.keys(campaign),
-      reloadProps: true,
-      optional: true,
+  methods: {
+    ...common.methods,
+    getAdvancedProps() {
+      return campaign.extraProps;
     },
   },
-  additionalProps() {
-    return this.additionalProps(this.selector, campaign);
-  },
+  props: getProps({
+    objType: campaign,
+    docsLink,
+  }),
   async run({ $ }) {
-    const data = pickBy(pick(this, [
-      "Name",
-      ...this.selector,
-    ]));
-    const response = await this.salesforce.createCampaign({
+    /* eslint-disable no-unused-vars */
+    const {
+      salesforce, useAdvancedProps, docsInfo, additionalFields, StartDate, EndDate, ...data
+    } = this;
+    /* eslint-enable no-unused-vars */
+    const response = await salesforce.createCampaign({
       $,
-      data,
+      data: {
+        ...data,
+        ...this.formatDateTimeProps({
+          StartDate,
+          EndDate,
+        }),
+        ...this.getAdditionalFields(),
+      },
     });
     $.export("$summary", `Successfully created campaign "${this.Name}"`);
     return response;
