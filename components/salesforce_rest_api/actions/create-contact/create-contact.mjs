a0fd476c8511d298b994b3000dea0bf7022d80ca
GTFalcao <gtfalcao96@gmail.com>
2024-07-17 19:41:49 -0300

    Create Contact

--- components/salesforce_rest_api/actions/create-contact/create-contact.mjs
+++ components/salesforce_rest_api/actions/create-contact/create-contact.mjs
@@ -1,51 +1,43 @@
-import common from "../common/base.mjs";
+import common, { getProps } from "../common/base.mjs";
 import contact from "../../common/sobjects/contact.mjs";
-import {
-  pickBy, pick,
-} from "lodash-es";
-import { toSingleLineString } from "../../common/utils.mjs";
 
-const { salesforce } = common.props;
+const docsLink = "https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_contact.htm";
 
 export default {
   ...common,
   key: "salesforce_rest_api-create-contact",
   name: "Create Contact",
-  description: toSingleLineString(`
-    Creates a Contact, which is a person associated with an account.
-    See [Contact SObject](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_contact.htm)
-    and [Create Record](https://developer.salesforce.com/docs/atlas.en-us.228.0.api_rest.meta/api_rest/dome_sobject_create.htm)
-  `),
-  version: "0.3.0",
+  description: `Creates a contact. [See the documentation](${docsLink})`,
+  version: "0.3.{{ts}}",
   type: "action",
-  props: {
-    salesforce,
-    LastName: {
-      type: "string",
-      label: "Last name",
-      description: "Last name of the contact up to 80 characters.",
-    },
-    selector: {
-      propDefinition: [
-        salesforce,
-        "fieldSelector",
-      ],
-      description: `${salesforce.propDefinitions.fieldSelector.description} Contact`,
-      options: () => Object.keys(contact),
-      reloadProps: true,
+  methods: {
+    ...common.methods,
+    getAdvancedProps() {
+      return contact.extraProps;
     },
   },
+  props: getProps({
+    objType: contact,
+    docsLink,
+  }),
   additionalProps() {
     return this.additionalProps(this.selector, contact);
   },
   async run({ $ }) {
-    const data = pickBy(pick(this, [
-      "LastName",
-      ...this.selector,
-    ]));
-    const response = await this.salesforce.createContact({
+    /* eslint-disable no-unused-vars */
+    const {
+      salesforce, useAdvancedProps, docsInfo, additionalFields, Birthdate, ...data
+    } = this;
+    /* eslint-enable no-unused-vars */
+    const response = await salesforce.createContact({
       $,
-      data,
+      data: {
+        ...data,
+        ...this.formatDateTimeProps({
+          Birthdate,
+        }),
+        ...this.getAdditionalFields(),
+      },
     });
     $.export("$summary", `Successfully created contact "${this.LastName}"`);
     return response;
