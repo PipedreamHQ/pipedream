47f122232a948ce998f7cf22feee7b1ed0980b0c
Lucas Caresia <lucascarezia@gmail.com>
2024-07-17 10:53:41 -0300

    Added actions (#12841)

---
+++ components/_4dem/actions/confirm-email/confirm-email.mjs
@@ -0,0 +1,37 @@
+import app from "../../_4dem.app.mjs";
+
+export default {
+  key: "_4dem-confirm-email",
+  name: "Confirm Email",
+  description: "Confirm sender email address. [See the documentation](https://api.4dem.it/#/operations/senders.email.confirm)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    senderId: {
+      propDefinition: [
+        app,
+        "senderId",
+      ],
+    },
+    authCode: {
+      propDefinition: [
+        app,
+        "authCode",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.confirmEmail({
+      $,
+      senderId: this.senderId,
+      data: {
+        auth_code: this.authCode,
+      },
+    });
+
+    $.export("$summary", "Successfully confirmed the sender email");
+
+    return response;
+  },
+};
