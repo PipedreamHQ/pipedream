e73fa08ffb84f1341b66ec757118f9291009bbef
Dan Hsiung <dan@pipedream.com>
2024-07-18 16:07:19 -0700

    using interfaces for DedupedSource and NonDedupedSource instead of Modify

--- types/src/types.test.ts
+++ types/src/types.test.ts
@@ -6,24 +6,20 @@ const num: Pipedream.JSONValue = 3;
 const bool: Pipedream.JSONValue = true;
 const n: Pipedream.JSONValue = null;
 
-const arr: Pipedream.JSONValue[] = [
-  "hello",
-  "world",
-];
+const arr: Pipedream.JSONValue[] = ["hello", "world"];
 const arraysAreJSONToo: Pipedream.JSONValue = arr;
 
 const soAreObject: Pipedream.JSONValue = {
   foo: "bar",
-  blah: [
-    "hello",
-    "world",
-  ],
+  blah: ["hello", "world"],
   null: null,
   bool: false,
 };
 
 // @ts-expect-error $ExpectError - can't serialize functions to JSON
-const func: Pipedream.JSONValue = () => { console.log("foo"); };
+const func: Pipedream.JSONValue = () => {
+  console.log("foo");
+};
 
 const httpConfig: Pipedream.SendConfigHTTP = {
   method: "POST",
@@ -86,19 +82,37 @@ const sseConfig: Pipedream.SendConfigSSE = {
 };
 
 const dollarSend: Pipedream.SendFunctionsWrapper = {
-  http: (config: Pipedream.SendConfigHTTP) => { console.log("foo"); },
-  email: (config: Pipedream.SendConfigEmail) => { console.log("foo"); },
-  emit: (config: Pipedream.SendConfigEmit) => { console.log("foo");},
-  s3: (config: Pipedream.SendConfigS3) => { console.log("foo"); },
-  sse: (config: Pipedream.SendConfigSSE) => { console.log("foo"); },
+  http: (config: Pipedream.SendConfigHTTP) => {
+    console.log("foo");
+  },
+  email: (config: Pipedream.SendConfigEmail) => {
+    console.log("foo");
+  },
+  emit: (config: Pipedream.SendConfigEmit) => {
+    console.log("foo");
+  },
+  s3: (config: Pipedream.SendConfigS3) => {
+    console.log("foo");
+  },
+  sse: (config: Pipedream.SendConfigSSE) => {
+    console.log("foo");
+  },
 };
 
 // @ts-expect-error $ExpectError - Missing s3
 const badDollarSend: Pipedream.SendFunctionsWrapper = {
-  http: (config: Pipedream.SendConfigHTTP) => { console.log("foo"); },
-  email: (config: Pipedream.SendConfigEmail) => { console.log("foo"); },
-  emit: (config: Pipedream.SendConfigEmit) => { console.log("foo"); },
-  sse: (config: Pipedream.SendConfigSSE) => { console.log("foo"); },
+  http: (config: Pipedream.SendConfigHTTP) => {
+    console.log("foo");
+  },
+  email: (config: Pipedream.SendConfigEmail) => {
+    console.log("foo");
+  },
+  emit: (config: Pipedream.SendConfigEmit) => {
+    console.log("foo");
+  },
+  sse: (config: Pipedream.SendConfigSSE) => {
+    console.log("foo");
+  },
 };
 
 const httpResponse: Pipedream.HTTPResponse = {
@@ -111,17 +125,17 @@ const httpResponse: Pipedream.HTTPResponse = {
 
 const methods: Pipedream.Methods = {
   getClient(foo: string, bar: number) {
-    return [
-      foo,
-      bar,
-    ];
+    return [foo, bar];
   },
   randomNum() {
     return Math.random();
   },
 };
 
-const googleSheets: Pipedream.App<Pipedream.Methods, Pipedream.AppPropDefinitions> = {
+const googleSheets: Pipedream.App<
+  Pipedream.Methods,
+  Pipedream.AppPropDefinitions
+> = {
   type: "app",
   app: "google_sheets",
   propDefinitions: {
@@ -130,10 +144,7 @@ const googleSheets: Pipedream.App<Pipedream.Methods, Pipedream.AppPropDefinition
       description: "hello, world",
       type: "integer",
       async options() {
-        return [
-          1,
-          2,
-        ];
+        return [1, 2];
       },
       optional: true,
       default: "foo",
@@ -149,17 +160,11 @@ const github: Pipedream.App<Pipedream.Methods, Pipedream.AppPropDefinitions> = {
   propDefinitions: {
     googleSheets,
     sheetId: {
-      propDefinition: [
-        googleSheets,
-        "sheetId",
-      ],
+      propDefinition: [googleSheets, "sheetId"],
     },
     url: {
       type: "string",
-      options: [
-        "foo",
-        "bar",
-      ],
+      options: ["foo", "bar"],
       secret: true,
     },
   },
@@ -216,12 +221,21 @@ const httpRequestProp: Pipedream.HttpRequestProp = {
 };
 
 const hooks: Pipedream.Hooks = {
-  deploy: async () => { return; },
-  activate: async () => { return; },
-  deactivate: async () => { return; },
+  deploy: async () => {
+    return;
+  },
+  activate: async () => {
+    return;
+  },
+  deactivate: async () => {
+    return;
+  },
 };
 
-const source: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const source: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "source",
   name: "Test Source",
   description: "hello, world",
@@ -233,74 +247,130 @@ const source: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinition
     googleSheets,
     github,
     url: {
-      propDefinition: [
-        github,
-        "url",
-      ],
+      propDefinition: [github, "url"],
     },
   },
   dedupe: "unique",
   async run() {
-    this.$emit({
-      foo: "bar ",
-    }, {
-      id: "foo",
-      name: "channel",
-      summary: "Summary",
-      ts: 123,
-    });
+    this.$emit(
+      {
+        foo: "bar ",
+      },
+      {
+        id: "foo",
+        name: "channel",
+        summary: "Summary",
+        ts: 123,
+      }
+    );
+  },
+};
+
+const nonDedupedSource: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
+  key: "source",
+  name: "Test Source",
+  description: "hello, world",
+  version: "0.0.1",
+  type: "source",
+  async run() {
+    this.$emit(
+      {
+        foo: "bar ",
+      },
+      {
+        name: "channel",
+        summary: "Summary",
+        ts: 123,
+      }
+    );
   },
 };
 
 // Bad sources
 
 // @ts-expect-error $ExpectError - Missing key
-const sourceMissingKey: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceMissingKey: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   version: "0.0.1",
   type: "source",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
 // @ts-expect-error $ExpectError - Missing version
-const sourceMissingVersion: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceMissingVersion: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "foo",
   type: "source",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
 // @ts-expect-error $ExpectError - Missing type
-const sourceMissingType: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceMissingType: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
-const sourceWrongType: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceWrongType: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
   // @ts-expect-error $ExpectError - bad type
   type: "action",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
-const sourceWrongDedupeType: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceWrongDedupeType: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
   type: "source",
   // @ts-expect-error $ExpectError - bad dedupe type
   dedupe: "badValue",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
-const sourceReturningDataShouldWarn: Pipedream.Source<Pipedream.Methods, Pipedream.SourcePropDefinitions> = {
+const sourceReturningDataShouldWarn: Pipedream.Source<
+  Pipedream.Methods,
+  Pipedream.SourcePropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
   type: "source",
   // @ts-expect-error $ExpectError - can't return data from source run method
-  run(event) { return "data"; },
+  run(event) {
+    return "data";
+  },
 };
 
-const action: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinitions> = {
+const action: Pipedream.Action<
+  Pipedream.Methods,
+  Pipedream.ActionPropDefinitions
+> = {
   key: "action",
   name: "Test Action",
   description: "hello, world",
@@ -311,10 +381,7 @@ const action: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinition
     googleSheets,
     github,
     url: {
-      propDefinition: [
-        github,
-        "url",
-      ],
+      propDefinition: [github, "url"],
     },
   },
   async run(event) {
@@ -325,32 +392,52 @@ const action: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinition
 // Bad actions
 
 // @ts-expect-error $ExpectError - Missing key
-const actionMissingKey: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinitions> = {
+const actionMissingKey: Pipedream.Action<
+  Pipedream.Methods,
+  Pipedream.ActionPropDefinitions
+> = {
   version: "0.0.1",
   type: "action",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
 // @ts-expect-error $ExpectError - Missing version
-const actionMissingVersion: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinitions> = {
+const actionMissingVersion: Pipedream.Action<
+  Pipedream.Methods,
+  Pipedream.ActionPropDefinitions
+> = {
   key: "foo",
   type: "action",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
 // @ts-expect-error $ExpectError - Missing type
-const actionMissingType: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinitions> = {
+const actionMissingType: Pipedream.Action<
+  Pipedream.Methods,
+  Pipedream.ActionPropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
-const actionWrongType: Pipedream.Action<Pipedream.Methods, Pipedream.ActionPropDefinitions> = {
+const actionWrongType: Pipedream.Action<
+  Pipedream.Methods,
+  Pipedream.ActionPropDefinitions
+> = {
   key: "foo",
   version: "0.0.1",
   // @ts-expect-error $ExpectError - bad type
   type: "source",
-  run() { console.log("foo"); },
+  run() {
+    console.log("foo");
+  },
 };
 
 Pipedream.defineSource({
@@ -362,7 +449,7 @@ Pipedream.defineSource({
     this.$emit(
       {},
       // @ts-expect-error $ExpectError - Missing id property in metadata object
-      {},
+      {}
     );
   },
 });
