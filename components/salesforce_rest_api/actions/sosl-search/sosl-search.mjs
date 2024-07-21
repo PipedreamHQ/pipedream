75b87be58ed4c7572db24d70aa68f6b305a99750
GTFalcao <gtfalcao96@gmail.com>
2024-07-21 01:45:04 -0300

    SOQL and SOSL search action updates

--- components/salesforce_rest_api/actions/sosl-search/sosl-search.mjs
+++ components/salesforce_rest_api/actions/sosl-search/sosl-search.mjs
@@ -1,21 +1,24 @@
 import salesforce from "../../salesforce_rest_api.app.mjs";
-import { toSingleLineString } from "../../common/utils.mjs";
+
+const docsLink = "https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_sosl_examples.htm";
 
 export default {
   key: "salesforce_rest_api-sosl-search",
-  name: "SOSL Search",
-  description: toSingleLineString(`
-    Executes the specified SOSL search.
-    See [docs](https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_sosl.htm)
-  `),
-  version: "0.3.0",
+  name: "SOSL Search (Object Search)",
+  description: `Executes a [Salesforce Object Search Language (SOSL)](${docsLink}) text-based search query.`,
+  version: "0.3.{{ts}}",
   type: "action",
   props: {
     salesforce,
+    exampleInfo: {
+      type: "alert",
+      alertType: "info",
+      content: "Example query: `FIND {Joe Smith} IN Name Fields RETURNING lead(name, phone)`",
+    },
     search: {
       type: "string",
       label: "SOSL Query",
-      description: "A SOSL search query",
+      description: `A SOSL search query. [See the documentation](${docsLink}) for examples and more information.`,
     },
   },
   async run({ $ }) {
@@ -23,7 +26,7 @@ export default {
       $,
       search: this.search,
     });
-    $.export("$summary", `Successfully returned ${response.length} results for SOSL search`);
+    $.export("$summary", `Successfully returned ${response.searchRecords} results for SOSL search`);
     return response;
   },
 };
