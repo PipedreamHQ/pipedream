18a730e6b8c892cc57b2fb824634fdf690c9d928
GTFalcao <gtfalcao96@gmail.com>
2024-07-21 02:58:56 -0300

    Delete Opportunity update

--- components/salesforce_rest_api/actions/delete-opportunity/delete-opportunity.mjs
+++ components/salesforce_rest_api/actions/delete-opportunity/delete-opportunity.mjs
@@ -1,30 +1,30 @@
+import propsAsyncOptions from "../../common/props-async-options.mjs";
 import salesforce from "../../salesforce_rest_api.app.mjs";
-import { toSingleLineString } from "../../common/utils.mjs";
 
 export default {
   key: "salesforce_rest_api-delete-opportunity",
   name: "Delete Opportunity",
-  description: toSingleLineString(`
-    Deletes an opportunity.
-    See [Opportunity SObject](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_opportunity.htm)
-    and [Delete Record](https://developer.salesforce.com/docs/atlas.en-us.228.0.api_rest.meta/api_rest/dome_delete_record.htm)
-  `),
-  version: "0.3.0",
+  description: "Deletes an opportunity. [See the documentation](https://developer.salesforce.com/docs/atlas.en-us.228.0.api_rest.meta/api_rest/dome_delete_record.htm)",
+  version: "0.3.{{ts}}",
   type: "action",
   props: {
     salesforce,
-    OpportunityId: {
-      type: "string",
-      label: "Opportunity ID",
-      description: "ID of the Opportunity to delete",
+    opportunityId: {
+      ...propsAsyncOptions.OpportunityId,
+      description: "ID of the opportunity to delete.",
+      async options() {
+        return this.salesforce.listRecordOptions({
+          objType: "Opportunity",
+        });
+      },
     },
   },
   async run({ $ }) {
     const response = await this.salesforce.deleteOpportunity({
       $,
-      id: this.OpportunityId,
+      id: this.opportunityId,
     });
-    $.export("$summary", `Successfully deleted opportunity (ID: ${this.OpportunityId})`);
+    $.export("$summary", `Successfully deleted opportunity (ID: ${this.opportunityId})`);
     return response;
   },
 };
