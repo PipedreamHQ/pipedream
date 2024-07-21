e0df96b11656e71bc767667dca9046fbe795f789
GTFalcao <gtfalcao96@gmail.com>
2024-07-20 22:14:10 -0300

    Add Contact to Campaign adjustments

--- components/salesforce_rest_api/actions/add-contact-to-campaign/add-contact-to-campaign.mjs
+++ components/salesforce_rest_api/actions/add-contact-to-campaign/add-contact-to-campaign.mjs
@@ -1,52 +1,43 @@
-import salesForceRestApi from "../../salesforce_rest_api.app.mjs";
-import { toSingleLineString } from "../../common/utils.mjs";
+import salesforce from "../../salesforce_rest_api.app.mjs";
 import constants from "../../common/constants.mjs";
+import commonProps from "../../common/props-async-options.mjs";
 
 export default {
   key: "salesforce_rest_api-add-contact-to-campaign",
   name: "Add Contact to Campaign",
-  description: toSingleLineString(`
-    Adds an existing contact to an existing campaign. [See the documentation](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_campaignmember.htm)
-  `),
-  version: "0.1.0",
+  description: "Adds an existing contact to an existing campaign. [See the documentation](https://developer.salesforce.com/docs/atlas.en-us.228.0.object_reference.meta/object_reference/sforce_api_objects_campaignmember.htm)",
+  version: "0.1.{{ts}}",
   type: "action",
   props: {
-    salesForceRestApi,
-    campaignId: {
-      propDefinition: [
-        salesForceRestApi,
-        "sobjectId",
-        () => ({
-          objectType: constants.OBJECT_TYPE.CAMPAIGN,
-        }),
-      ],
-      label: "Campaign ID",
+    salesforce,
+    CampaignId: {
+      ...commonProps.CampaignId,
       description: "The Campaign to add a Contact to.",
+      async options() {
+        return this.salesforce.listRecordOptions({
+          objType: "Campaign",
+        });
+      },
     },
-    contactId: {
-      propDefinition: [
-        salesForceRestApi,
-        "sobjectId",
-        () => ({
-          objectType: constants.OBJECT_TYPE.CONTACT,
-        }),
-      ],
-      label: "Contact ID",
+    ContactId: {
+      ...commonProps.ContactId,
       description: "The Contact to add to the selected Campaign.",
+      async options() {
+        return this.salesforce.listRecordOptions({
+          objType: "Contact",
+        });
+      },
     },
   },
   async run({ $ }) {
-    const data = {
-      CampaignId: this.campaignId,
-      ContactId: this.contactId,
-    };
-    const response = await this.salesForceRestApi.createObject(
-      {
-        $,
-        objectType: constants.OBJECT_TYPE.CAMPAIGN_MEMBER,
-        data,
-      },
-    );
+    const {
+      salesforce, ...data
+    } = this;
+    const response = await salesforce.createObject({
+      $,
+      objectType: constants.OBJECT_TYPE.CAMPAIGN_MEMBER,
+      data,
+    });
     $.export("$summary", "Successfully added contact to campaign");
     return response;
   },
