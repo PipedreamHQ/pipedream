64d24d358599223b278d29d67e5f23934e94ef61
Lucas Caresia <lucascarezia@gmail.com>
2024-07-22 09:34:21 -0300

    Added actions

---
+++ components/tavily/actions/send-query/send-query.mjs
@@ -0,0 +1,51 @@
+import app from "../../tavily.app.mjs";
+
+export default {
+  key: "tavily-send-query",
+  name: "Send Query",
+  description: "Search for data based on a query. [See the documentation](https://docs.tavily.com/docs/tavily-api/rest_api#post-search)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    query: {
+      propDefinition: [
+        app,
+        "query",
+      ],
+    },
+    searchDepth: {
+      propDefinition: [
+        app,
+        "searchDepth",
+      ],
+    },
+    includeImages: {
+      propDefinition: [
+        app,
+        "includeImages",
+      ],
+    },
+    includeAnswer: {
+      propDefinition: [
+        app,
+        "includeAnswer",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.sendQuery({
+      $,
+      data: {
+        query: this.query,
+        search_depth: this.searchDepth,
+        include_images: this.includeImages,
+        include_answer: this.includeAnswer,
+      },
+    });
+
+    $.export("$summary", `Successfully sent query and got the following answer: '${response.answer}'`);
+
+    return response;
+  },
+};
