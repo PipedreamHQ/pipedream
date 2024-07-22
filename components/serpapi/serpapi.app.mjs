e2d6e14599948b3425b71bd3737b20b64255021b
Lucas Caresia <lucascarezia@gmail.com>
2024-07-22 09:27:25 -0300

    Added actions

--- components/serpapi/serpapi.app.mjs
+++ components/serpapi/serpapi.app.mjs
@@ -1,11 +1,58 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "serpapi",
-  propDefinitions: {},
+  propDefinitions: {
+    engine: {
+      type: "string",
+      label: "Engine",
+      description: "Engine to send the query to",
+      options: constants.ENGINES,
+    },
+    q: {
+      type: "string",
+      label: "Query",
+      description: "The query or queries you want to search. When passing multiple queries you need to use a comma `,` to separate them",
+    },
+    device: {
+      type: "string",
+      label: "Device",
+      description: "efines the device to use to get the results",
+      options: constants.DEVICES,
+    },
+    noCache: {
+      type: "boolean",
+      label: "No Cache",
+      description: "Force SerpApi to fetch the Google results even if a cached version is already present",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://serpapi.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        params,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        params: {
+          ...params,
+          api_key: this.$auth.api_key,
+        },
+      });
+    },
+    async scrapeSearch(args = {}) {
+      return this._makeRequest({
+        path: "/search",
+        ...args,
+      });
     },
   },
 };
