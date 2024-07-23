0ae66dd7994197d62b89aa81970158251316af78
GTFalcao <gtfalcao96@gmail.com>
2024-07-23 03:11:50 -0300

    Reusing allSobjects mapping for common props

--- components/salesforce_rest_api/common/props-async-options.mjs
+++ components/salesforce_rest_api/common/props-async-options.mjs
@@ -2,101 +2,65 @@
 // whereas when using regular props, the standard method syntax is needed instead.
 // These props are more commonly used in additionalProps, so all are defined as such,
 // and the options method needs to be redefined if used in regular props.
+
+import allSobjects from "./all-sobjects.mjs";
+
+const findSobjectOptions = (objName) => {
+  return allSobjects.find(({ name }) => name === objName)?.getRecords;
+};
+
 export default {
   AccountId: {
     type: "string",
     label: "Account ID",
     description: "The ID of an Account.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Account",
-      });
-    },
+    options: findSobjectOptions("Account"),
   },
   BusinessHoursId: {
     type: "string",
     label: "Business Hours ID",
     description: "The ID of a Business Hours record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "BusinessHours",
-      });
-    },
+    options: findSobjectOptions("BusinessHours"),
   },
   CallCenterId: {
     type: "string",
     label: "Call Center ID",
     description: "The ID of a Call Center.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "CallCenter",
-      });
-    },
+    options: findSobjectOptions("CallCenter"),
   },
   CampaignId: {
     type: "string",
     label: "Campaign ID",
     description: "The ID of a Campaign.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Campaign",
-      });
-    },
+    options: findSobjectOptions("Campaign"),
   },
   CaseId: {
     type: "string",
     label: "Case ID",
     description: "The ID of a Case.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Case",
-        fields: [
-          "Id",
-          "CaseNumber",
-          "Subject",
-          "SuppliedName",
-        ],
-        getLabel: (item) => item.SuppliedName ?? item.Subject ?? item.CaseNumber,
-      });
-    },
+    options: findSobjectOptions("Case"),
   },
   CommunityId: {
     type: "string",
     label: "Community ID",
     description: "The ID of a Community (Zone) record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
+    options: () =>
+      this.salesforce.listRecordOptions({
         objType: "Community",
-      });
-    },
+        nameField: "Name",
+      }),
   },
   ContactId: {
     type: "string",
     label: "Contact ID",
     description: "The ID of a Contact.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Contact",
-      });
-    },
+    options: findSobjectOptions("Contact"),
   },
   ContractId: {
     type: "string",
     label: "Contract ID",
     description: "The ID of a Contract.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Contract",
-        fields: [
-          "Id",
-          "ContractNumber",
-          "Description",
-        ],
-        getLabel: (item) => item.ContractNumber + (item.Description
-          ? ` - ${item.Description}`
-          : ""),
-      });
-    },
+    options: findSobjectOptions("Contract"),
   },
   ContactOrLeadIds: {
     type: "string[]",
@@ -121,110 +85,70 @@ export default {
     type: "string",
     label: "Individual ID",
     description: "The ID of an Individual.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Individual",
-      });
-    },
+    options: findSobjectOptions("Individual"),
   },
   LeadId: {
     type: "string",
     label: "Lead ID",
     description: "The ID of a Lead.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Lead",
-      });
-    },
+    options: findSobjectOptions("Lead"),
   },
   OperatingHoursId: {
     type: "string",
     label: "Operating Hours ID",
     description: "The ID of an Operating Hours record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "OperatingHours",
-      });
-    },
+    options: findSobjectOptions("OperatingHours"),
   },
   OpportunityId: {
     type: "string",
     label: "Opportunity ID",
     description: "The ID of an Opportunity.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Opportunity",
-      });
-    },
+    options: findSobjectOptions("Opportunity"),
   },
   Pricebook2Id: {
     type: "string",
     label: "Pricebook2 ID",
     description: "The ID of a Pricebook2 record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Pricebook2",
-      });
-    },
+    options: findSobjectOptions("Pricebook2"),
   },
   ProfileId: {
     type: "string",
     label: "Profile ID",
     description: "The ID of a Profile.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "Profile",
-      });
-    },
+    options: findSobjectOptions("Profile"),
   },
   ServiceContractId: {
     type: "string",
     label: "ServiceContract ID",
     description: "The ID of a Service Contract record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "ServiceContract",
-      });
-    },
+    options: findSobjectOptions("ServiceContract"),
   },
   UserId: {
     type: "string",
     label: "User ID",
     description: "The ID of a User in your organization.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "User",
-      });
-    },
+    options: findSobjectOptions("User"),
   },
   UserRoleId: {
     type: "string",
     label: "User Role ID",
     description: "The ID of a User Role record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "UserRole",
-      });
-    },
+    options: findSobjectOptions("UserRole"),
   },
   QuestionId: {
     type: "string",
     label: "Question ID",
     description: "The ID of a Question.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
+    options: () =>
+      this.salesforce.listRecordOptions({
         objType: "Question",
-      });
-    },
+        nameField: "Title",
+      }),
   },
   RecordTypeId: {
     type: "string",
     label: "Record Type ID",
     description: "ID of the record type assigned to this record.",
-    options: async () => {
-      return this.salesforce.listRecordOptions({
-        objType: "RecordType",
-      });
-    },
+    options: findSobjectOptions("RecordType"),
   },
 };
