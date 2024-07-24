d50b2a91f7046cbcd10e9239ebf02e190f6954d7
Jorge Cortes <jacortesmahmud@gmail.com>
2024-07-24 14:42:29 -0500

    Created new actions and sources for ONLYOFFICE DocSpace

---
+++ components/onlyoffice_docspace/actions/create-room/create-room.mjs
@@ -0,0 +1,77 @@
+import app from "../../onlyoffice_docspace.app.mjs";
+
+export default {
+  key: "onlyoffice_docspace-create-room",
+  name: "Create Room",
+  description: "Creates a new room. [See the documentation](https://api.onlyoffice.com/docspace/method/files/post/api/2.0/files/rooms)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    title: {
+      type: "string",
+      label: "Title",
+      description: "The name of the room to be created.",
+    },
+    roomType: {
+      type: "integer",
+      label: "Room Type",
+      description: "The type of the room.",
+      options: [
+        {
+          label: "Editing Room",
+          value: 2,
+        },
+        {
+          label: "Custom Room",
+          value: 5,
+        },
+        {
+          label: "Public Room",
+          value: 6,
+        },
+      ],
+    },
+    notify: {
+      type: "boolean",
+      label: "Notify",
+      description: "Whether to notify the user about the room creation.",
+      optional: true,
+    },
+    sharingMessage: {
+      type: "string",
+      label: "Sharing Message",
+      description: "Message to send when notifying about the shared room",
+      optional: true,
+    },
+  },
+  methods: {
+    createRoom(args = {}) {
+      return this.app.post({
+        path: "/files/rooms",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      createRoom,
+      title,
+      roomType,
+      notify,
+      sharingMessage,
+    } = this;
+
+    const response = await createRoom({
+      $,
+      data: {
+        Title: title,
+        RoomType: roomType,
+        Notify: notify,
+        SharingMessage: sharingMessage,
+      },
+    });
+    $.export("$summary", `Successfully created room with ID \`${response.response.id}\`.`);
+    return response;
+  },
+};
