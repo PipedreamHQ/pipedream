28080f8f63aa1db45211e00f86ad7739d168f616
Luan Cazarine <luanhc@gmail.com>
2024-07-25 10:32:45 -0300

    [Components] otter_waiver #12539 Sources  - New Check-In (Instant)  - New Signee (Instant)

--- components/otter_waiver/otter_waiver.app.mjs
+++ components/otter_waiver/otter_waiver.app.mjs
@@ -3,87 +3,36 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "otter_waiver",
-  propDefinitions: {
-    eventName: {
-      type: "string",
-      label: "Event Name",
-      description: "The name of the event for which the participant is checking in",
-      async options() {
-        const events = await this.getEvents();
-        return events.map((event) => ({
-          label: event.name,
-          value: event.id,
-        }));
-      },
-    },
-    signeeDetails: {
-      type: "object",
-      label: "Signee Details",
-      description: "Details of the new signee",
-    },
-    checkInTime: {
-      type: "string",
-      label: "Check-In Time",
-      description: "The time the participant checked in",
-      optional: true,
-    },
-    signeePreferences: {
-      type: "object",
-      label: "Signee Preferences",
-      description: "Preferences of the new signee",
-      optional: true,
-    },
-    signeeSubscriptions: {
-      type: "object",
-      label: "Signee Subscriptions",
-      description: "Subscriptions of the new signee",
-      optional: true,
-    },
-  },
   methods: {
     _baseUrl() {
       return "https://api.otterwaiver.com";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path = "/",
-        headers,
-        ...otherOpts
-      } = opts;
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
         url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          Authorization: `Bearer ${this.$auth.api_key}`,
-        },
+        headers: this._headers(),
+        ...opts,
       });
     },
-    async subscribeToEvent({
-      trigger, webhook,
-    }) {
+    async createWebhook(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/webhook/subscribe",
-        data: {
-          trigger,
-          webhook,
-        },
+        ...opts,
       });
     },
-    async unsubscribeFromEvent({
-      trigger, webhook,
-    }) {
+    async deleteWebhook(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/webhook/unsubscribe",
-        data: {
-          trigger,
-          webhook,
-        },
+        ...opts,
       });
     },
     async getLatestCheckIns() {
@@ -96,11 +45,6 @@ export default {
       return this._makeRequest({
         method: "GET",
         path: "/participants/latest",
-      });
-    },
-    async getEvents() {
-      return this._makeRequest({
-        path: "/events",
       });
     },
   },
--- components/otter_waiver/sources/new-check-in-instant/new-check-in-instant.mjs
+++ components/otter_waiver/sources/new-check-in-instant/new-check-in-instant.mjs
@@ -1,96 +1,25 @@
-import otterWaiver from "../../otter_waiver.app.mjs";
-import crypto from "crypto";
-import { axios } from "@pipedream/platform";
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
 
 export default {
+  ...common,
   key: "otter_waiver-new-check-in-instant",
-  name: "New Check-In Instant",
-  description: "Emit new event when a participant checks into an event. [See the documentation](https://api.otterwaiver.com/docs/webhook)",
-  version: "0.0.{{ts}}",
+  name: "New Check-In (Instant)",
+  description: "Emit new event when a participant checks into an event.",
+  version: "0.0.1",
   type: "source",
   dedupe: "unique",
-  props: {
-    otterWaiver: {
-      type: "app",
-      app: "otter_waiver",
-    },
-    http: {
-      type: "$.interface.http",
-      customResponse: true,
-    },
-    db: "$.service.db",
-    eventName: {
-      propDefinition: [
-        otterWaiver,
-        "eventName",
-      ],
-    },
-    checkInTime: {
-      propDefinition: [
-        otterWaiver,
-        "checkInTime",
-      ],
-      optional: true,
-    },
-  },
   methods: {
-    _getWebhookId() {
-      return this.db.get("webhookId");
+    ...common.methods,
+    getEvent() {
+      return "checkin";
     },
-    _setWebhookId(id) {
-      this.db.set("webhookId", id);
+    getFunction() {
+      return this.otterWaiver.getLatestCheckIns;
     },
-  },
-  hooks: {
-    async deploy() {
-      const checkIns = await this.otterWaiver.getLatestCheckIns();
-      for (const checkIn of checkIns) {
-        this.$emit(checkIn, {
-          id: checkIn.id,
-          summary: `New check-in: ${checkIn.signee.firstName} ${checkIn.signee.lastName}`,
-          ts: Date.parse(checkIn.timeStamps.checkedIn),
-        });
-      }
-    },
-    async activate() {
-      const webhook = this.http.endpoint;
-      const { id } = await this.otterWaiver.subscribeToEvent({
-        trigger: "CheckIn",
-        webhook,
-      });
-      this._setWebhookId(id);
-    },
-    async deactivate() {
-      const webhookId = this._getWebhookId();
-      if (webhookId) {
-        await this.otterWaiver.unsubscribeFromEvent({
-          trigger: "CheckIn",
-          webhook: this.http.endpoint,
-        });
-      }
+    getSummary(checkIn) {
+      return `New check-in: ${checkIn.id}`;
     },
   },
-  async run(event) {
-    const computedSignature = crypto.createHmac("sha256", this.otterWaiver.$auth.api_key)
-      .update(event.raw_body)
-      .digest("base64");
-    if (computedSignature !== event.headers["x-otter-signature"]) {
-      this.http.respond({
-        status: 401,
-        body: "Unauthorized",
-      });
-      return;
-    }
-    this.http.respond({
-      status: 200,
-      body: "OK",
-    });
-
-    const checkIn = event.body;
-    this.$emit(checkIn, {
-      id: checkIn.id,
-      summary: `New check-in: ${checkIn.signee.firstName} ${checkIn.signee.lastName}`,
-      ts: Date.parse(checkIn.timeStamps.checkedIn),
-    });
-  },
+  sampleEmit,
 };
