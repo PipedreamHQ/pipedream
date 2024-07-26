2c51bb840ce798827148b64d20a19ace627ea4ab
GTFalcao <gtfalcao96@gmail.com>
2024-07-25 22:25:52 -0300

    Fixing 'create event'

--- components/salesforce_rest_api/actions/create-event/create-event.mjs
+++ components/salesforce_rest_api/actions/create-event/create-event.mjs
@@ -5,6 +5,7 @@ const docsLink =
   "https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_event.htm";
 
 export default {
+  ...common,
   key: "salesforce_rest_api-create-event",
   name: "Create Event",
   description: `Creates an event. [See the documentation](${docsLink})`,
