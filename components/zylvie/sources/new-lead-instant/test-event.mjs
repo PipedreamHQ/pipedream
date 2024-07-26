605441fc8179da53e0e793832dfda2dd6cca85ad
Jorge Cortes <jacortesmahmud@gmail.com>
2024-07-26 10:22:54 -0500

    Added new source components (#13008)

---
+++ components/zylvie/sources/new-lead-instant/test-event.mjs
@@ -0,0 +1,25 @@
+export default {
+  "event": "lead",
+  "data": {
+      "transaction_id": "free_1711078342965279",
+      "created_at": "2025-03-23T03:12:35Z",
+      "buyer": {
+          "name": "Stavros Flatley",
+          "email": "stavros@greek.com",
+          "permission_to_send_emails": true
+      },
+      "products": [
+          {
+              "title": "The Ultimate Business Book",
+              "price": 0.0,
+              "quantity": 1
+          }
+      ],
+      "custom_fields": [
+          {
+              "name": "Tax identification number",
+              "value": "TIN-5252445767"
+          }
+      ]
+  }
+};
