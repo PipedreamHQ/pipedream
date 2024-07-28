21e2b0d5d21a69d0b80bdf966a5758f9863b7a82
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2024-07-28 14:46:11 +0100

    add charge auth (not linted yet)

--- components/paystack/paystack.app.mjs
+++ components/paystack/paystack.app.mjs
@@ -9,6 +9,22 @@ export default {
       type: "string",
       label: "Email",
       description: "Email address of the customer to charge",
+      async options({ page, query }) {
+        const { data } = await this.listCustomers({
+          params: {
+            page: page + 1,
+            email: query,
+          },
+        });
+        console.log(data);
+        return (
+          data?.map(({ email: value, email: label }) => ({
+            label,
+            value,
+          })) || []
+        );
+      },
+      useQuery: true,
     },
     amount: {
       type: "string",
@@ -18,39 +34,46 @@ export default {
     currency: {
       type: "string",
       label: "Currency",
-      description: "Currency to use for the charge. Defaults to your integration currency",
+      description:
+        "Currency to use for the charge. Defaults to your integration currency",
       options: constants.CURRENCIES,
     },
     reference: {
       type: "string",
       label: "Reference",
-      description: "Unique transaction reference. Only alphanumeric characters and `-`, `.`, `=` are allowed",
+      description:
+        "Unique transaction reference. Only alphanumeric characters and `-`, `.`, `=` are allowed",
       async options({ page }) {
         const { data } = await this.listTransactions({
           params: {
             page: page + 1,
           },
         });
-        return data?.map(({ reference }) => ({
-          value: reference,
-          label: `${reference}`,
-        })) || [];
+        return (
+          data?.map(({ reference }) => ({
+            value: reference,
+            label: `${reference}`,
+          })) || []
+        );
       },
     },
     callbackUrl: {
       type: "string",
       label: "Callback URL",
-      description: "URL to redirect customers to after a successful transaction. Setting this overrides the callback URL set on the dashboard",
+      description:
+        "URL to redirect customers to after a successful transaction. Setting this overrides the callback URL set on the dashboard",
     },
     metadata: {
       type: "object",
       label: "Metadata",
-      description: "Stringified JSON object of custom data. Check the [Metadata docs](https://paystack.com/docs/payments/metadata/) for more information",
+      description:
+        "Stringified JSON object of custom data. Check the [Metadata docs](https://paystack.com/docs/payments/metadata/) for more information",
     },
     status: {
       type: "string",
       label: "Status",
-      description: "Status of a transaction. Possible values are success, failed, and abandoned.",
+      description:
+        "Status of a transaction. Possible values are success, failed, and abandoned.",
       options: constants.STATUS,
     },
     transactionID: {
@@ -63,10 +86,12 @@ export default {
             page: page + 1,
           },
         });
-        return data?.map(({ id }) => ({
-          value: id,
-          label: `${id}`,
-        })) || [];
+        return (
+          data?.map(({ id }) => ({
+            value: id,
+            label: `${id}`,
+          })) || []
+        );
       },
     },
     customerID: {
@@ -78,30 +103,49 @@ export default {
           params: {
             page: page + 1,
           },
-        }); console.log(data);
-        return data?.map(({
-          id: value, email: label,
-        }) => ({
-          label,
-          value,
-        })) || [];
+        });
+        console.log(data);
+        return (
+          data?.map(({ id: value, email: label }) => ({
+            label,
+            value,
+          })) || []
+        );
       },
     },
-    from: {
+    authorization_code: {
       type: "string",
-      label: "From",
-      description: "The start date for record retrieval, in ISO 8601 format (e.g., 2016-09-24T00:00:05.000Z or 2016-09-21).",
-    },
-    to: {
-      type: "string",
-      label: "To",
-      description: "The end date for record retrieval, in ISO 8601 format (e.g., 2016-09-24T00:00:05.000Z or 2016-09-21).",
-    },
-    maxResults: {
-      type: "integer",
-      label: "Max Results",
-      description: "The maximum number of results to return",
-      optional: true,
+      label: "Authorization Code",
+      description:
+        "Authorization code to charge. This is created whenever a customer makes a payment on your integration",
+      async options({ customer }) {
+        const { data } = await this.fetchCustomer({ customer });
+        const authorizations = data?.authorizations || [];
+        return authorizations
+          .filter(({ reusable }) => reusable)
+          .map((authorization) => {
+            const {
+              bank,
+              channel,
+              card_type,
+              last4,
+              exp_month,
+              exp_year,
+              authorization_code,
+            } = authorization;
+            if (channel === "card") {
+              return {
+                label: ` ${bank} ${card_type} card ending in ${last4} (expires ${exp_month}/${exp_year})`,
+                value: authorization_code,
+              };
+            } else if (channel === "direct_debit") {
+              return {
+                label: `${bank} account ending in (${last4})`,
+                value: authorization_code,
+              };
+            }
+          });
+      },
     },
   },
   methods: {
@@ -110,14 +154,12 @@ export default {
     },
     _headers() {
       return {
-        "Authorization": `Bearer ${this.$auth.api_key}`,
+        Authorization: `Bearer ${this.$auth.api_key}`,
         "Content-Type": "application/json",
         "user-agent": "@PaystackOSS/paystack v0.1",
       };
     },
-    _makeRequest({
-      $ = this, path = "/", ...opts
-    }) {
+    _makeRequest({ $ = this, path = "/", ...opts }) {
       return axios($, {
         url: `${this._baseUrl()}${path}`,
         headers: this._headers(),
@@ -131,9 +173,7 @@ export default {
         ...args,
       });
     },
-    verifyTransaction({
-      reference, ...args
-    }) {
+    verifyTransaction({ reference, ...args }) {
       return this._makeRequest({
         path: `/transaction/verify/${reference}`,
         ...args,
@@ -151,17 +191,26 @@ export default {
         ...args,
       });
     },
-    fetchTransaction({
-      transactionID, ...args
-    }) {
+    fetchTransaction({ transactionID, ...args }) {
       return this._makeRequest({
         path: `/transaction/${transactionID}`,
         ...args,
       });
     },
-    async *paginate({
-      resourceFn, args, max,
-    }) {
+    fetchCustomer({ customer, ...args }) {
+      return this._makeRequest({
+        path: `/customer/${customer}`,
+        args,
+      });
+    },
+    chargeAuthorization(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/transaction/charge_authorization`,
+        ...args,
+      });
+    },
+    async *paginate({ resourceFn, args, max }) {
       args = {
         ...args,
         params: {
@@ -170,7 +219,8 @@ export default {
           page: 1,
         },
       };
-      let total, count = 0;
+      let total,
+        count = 0;
       do {
         const { data } = await resourceFn(args);
         for (const item of data) {
