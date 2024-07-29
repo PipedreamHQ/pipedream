50f1d5c324267b99688dfd60d9b10dfa94067e66
michelle0927 <michelle0927@users.noreply.github.com>
2024-07-29 10:03:56 -0400

    [Usability Audit] Google Calendar (#12963)
    * updates to sources
    
    * updates to actions
    
    * updates
    
    * updates
    
    * pnpm-lock.yaml
    
    * fix typo
    
    * fix typos
    
    * fix repeatInterval description

--- components/google_calendar/actions/get-calendar/get-calendar.mjs
+++ components/google_calendar/actions/get-calendar/get-calendar.mjs
@@ -4,7 +4,7 @@ export default {
   key: "google_calendar-get-calendar",
   name: "Retrieve Calendar Details",
   description: "Retrieve calendar details of a Google Calendar. [See the documentation](https://googleapis.dev/nodejs/googleapis/latest/calendar/classes/Resource$Calendars.html#get)",
-  version: "0.1.5",
+  version: "0.1.6",
   type: "action",
   props: {
     googleCalendar,
