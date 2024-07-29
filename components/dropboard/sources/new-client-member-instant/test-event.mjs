d2b8c896b250cb15bde0b4f54b29f5947ff29e53
Luan Cazarine <luanhc@gmail.com>
2024-07-29 10:43:19 -0300

    New Components - dropboard (#12642)
    * dropboard init
    
    * [Components] dropboard #12560
    Sources
     - New Candidate (Instant)
     - New Client Member (Instant)
     - New Job (Instant)
    
    Actions
     - Create Job
     - Create Client
     - Find Or Create Client
    
    * pnpm update

---
+++ components/dropboard/sources/new-client-member-instant/test-event.mjs
@@ -0,0 +1,8 @@
+export default {
+  "id": "usr_GYUugigiy",
+  "clientId": "co_GYUugigiy",
+  "created": "2024-07-02T15:55:47.127Z",
+  "email": "member@email.com",
+  "first": "First Name",
+  "last": "Last Name"
+}
