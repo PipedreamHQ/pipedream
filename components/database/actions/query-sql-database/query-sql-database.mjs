a85b4b163f441a7ecf7a6e4b512e7262cde2be9b
Danny Roosevelt <danny@pipedream.com>
2024-07-31 11:36:53 -0700

    reving versions (#13055)

--- components/database/actions/query-sql-database/query-sql-database.mjs
+++ components/database/actions/query-sql-database/query-sql-database.mjs
@@ -13,7 +13,7 @@ export default {
   key: "database-query-sql-database",
   description:
     "Execute a SQL Query. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "action",
   props: {
     database: {
