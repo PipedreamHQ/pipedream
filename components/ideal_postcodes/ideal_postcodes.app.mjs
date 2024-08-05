5d45f2974b6e4c39acdd5259bf918edc3e7b6303
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-05 14:43:18 -0500

    Ideal Postcode app new action components

--- components/ideal_postcodes/ideal_postcodes.app.mjs
+++ components/ideal_postcodes/ideal_postcodes.app.mjs
@@ -1,11 +1,35 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "ideal_postcodes",
-  propDefinitions: {},
+  propDefinitions: {
+    tags: {
+      type: "string",
+      label: "Tags",
+      description: "A comma separated list of tags to query over. Useful if you want to specify the circumstances in which the request was made. If multiple tags are specified, the response will only comprise of requests for which all the tags are satisfied - i.e. searching `foo,bar` will only query requests which tagged both `foo` and `bar`.",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `https://api.ideal-postcodes.co.uk/v1${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        Authorization: `api_key="${this.$auth.api_key}"`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        debug: true,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      });
     },
   },
 };
