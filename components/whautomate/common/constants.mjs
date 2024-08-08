44871be08bbe3b47294bc9c65f95dbc8cd26443a
Luan Cazarine <luanhc@gmail.com>
2024-08-08 12:06:02 -0300

    New Components - whautomate (#13119)
    * whautomate init
    
    * [Components] whautomate #13095
    Sources
     - New Appointment Cancelled (Instant)
     - New Appointment Scheduled (Instant)
     - New Client Created (Instant)
    
    Actions
     - Assign Tags Contact
     - Create Contact
     - Send Whatsapp Template Message
    
    * pnpm update
    
    * Update components/whautomate/sources/common/base.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * some adjusts
    
    * fix tags in data request
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

---
+++ components/whautomate/common/constants.mjs
@@ -0,0 +1,10 @@
+export const STAGE_OPTIONS = [
+  "Subscriber",
+  "Lead",
+  "Marketing qualified lead (MQL)",
+  "Sales qualified lead (SQL)",
+  "Opportunity",
+  "Customer",
+  "Evangelist",
+  "Other",
+];
