b9048d3210b81e67aa8a9a4767c548e25162e3f6
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-09 15:33:37 -0500

    Re open PR with new action Phone Number Lookup

---
+++ components/twilio/actions/phone-number-lookup/phone-number-lookup.mjs
@@ -0,0 +1,41 @@
+import app from "../../twilio.app.mjs";
+
+export default {
+  key: "twilio-phone-number-lookup",
+  name: "Phone Number Lookup",
+  description: "Lookup information about a phone number. [See the documentation](https://www.twilio.com/docs/lookup/v2-api/line-type-intelligence) for more information",
+  type: "action",
+  version: "0.0.1",
+  props: {
+    app,
+    sid: {
+      type: "string",
+      label: "Phone Number",
+      description: "The phone number to lookup",
+    },
+  },
+  methods: {
+    lineTypeLookup(sid) {
+      const client = this.app.getClient();
+      return client.lookups.v2.phoneNumbers(sid).fetch({
+        fields: "line_type_intelligence",
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      lineTypeLookup,
+      sid,
+    } = this;
+
+    const response = await lineTypeLookup(sid);
+
+    if (response.validationErrors?.length) {
+      $.export("$summary", `Failed to fetch phone number lookup: \`${JSON.stringify(response.validationErrors, null, 2)}\`.`);
+    } else {
+      $.export("$summary", `Successfully fetched phone number lookup of \`${sid}\``);
+    }
+
+    return response;
+  },
+};
