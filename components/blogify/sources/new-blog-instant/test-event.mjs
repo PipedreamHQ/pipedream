2aa6fcec2dc138463c2eecef673744fdecfebb0c
Luan Cazarine <luanhc@gmail.com>
2024-08-12 11:01:22 -0300

    New Components - blogify (#13359)
    * blogify init
    
    * [Components] blogify #13045
    Sources
    - New Blog (Instant)
    
    * pnpm update

---
+++ components/blogify/sources/new-blog-instant/test-event.mjs
@@ -0,0 +1,13 @@
+export default {
+  "title": "Blog Title",
+  "content": "<h2>Blog content</h2>",
+  "blogKeywords": [
+    "PDF files",
+    "downloading PDFs",
+    "viewing PDFs",
+    "printing PDFs",
+    "Adobe Acrobat"
+  ],
+  "draft": true,
+  "image": ""
+}
