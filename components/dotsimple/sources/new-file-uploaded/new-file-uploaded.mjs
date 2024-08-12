8884f6a4a1af4516fee36309cf38b83804e5ae7b
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-12 13:09:25 -0500

    Dotsimple app new action and source components

---
+++ components/dotsimple/sources/new-file-uploaded/new-file-uploaded.mjs
@@ -0,0 +1,24 @@
+import common from "../common/polling.mjs";
+
+export default {
+  ...common,
+  key: "dotsimple-new-file-uploaded",
+  name: "New File Uploaded",
+  description: "Emit new event when a new file is uploaded. [See the documentation](https://help.dotsimple.io/en/articles/67-media-files).",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getResourcesFn() {
+      return this.app.listMediaFiles;
+    },
+    generateMeta(resource) {
+      return {
+        id: resource.uuid,
+        summary: `New File: ${resource.uuid}`,
+        ts: Date.parse(resource.created_at),
+      };
+    },
+  },
+};
