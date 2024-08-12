b35a6706e8a00c0c7c6c21deafbad2e230196111
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-12 10:26:40 -0500

    [Components] Paradym new components (#13487)
    * Paradym new components
    
    * Update components/paradym/sources/new-verification-response-instant/new-verification-response-instant.mjs
    
    * Remove debug param
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>

--- components/paradym/paradym.app.mjs
+++ components/paradym/paradym.app.mjs
@@ -1,11 +1,265 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "paradym",
-  propDefinitions: {},
+  propDefinitions: {
+    projectId: {
+      type: "string",
+      label: "Project ID",
+      description: "The ID of the project.",
+      async options({ prevContext: { pageBeforeId } }) {
+        if (pageBeforeId === null) {
+          return [];
+        }
+        const { data } = await this.listProjects({
+          params: {
+            "sort": "-updatedAt",
+            "page[size]": constants.DEFAULT_LIMIT,
+            "page[before]": pageBeforeId,
+          },
+        });
+        const options = data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+        return {
+          options,
+          context: {
+            pageBeforeId:
+              data.length
+                ? data[data.length - 1].id
+                : null,
+          },
+        };
+      },
+    },
+    sdJWTVCCredentialTemplateId: {
+      type: "string",
+      label: "SD-JWT-VC Credential Template ID",
+      description: "The ID of the SD-JWT-VC credential template.",
+      async options({
+        projectId,
+        prevContext: { pageBeforeId },
+      }) {
+        if (pageBeforeId === null) {
+          return [];
+        }
+        const { data } = await this.listSDJWTVCCredentialTemplates({
+          projectId,
+          params: {
+            "sort": "-updatedAt",
+            "page[size]": constants.DEFAULT_LIMIT,
+            "page[before]": pageBeforeId,
+          },
+        });
+        const options = data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+        return {
+          options,
+          context: {
+            pageBeforeId:
+              data.length
+                ? data[data.length - 1].id
+                : null,
+          },
+        };
+      },
+    },
+    anoncredsCredentialTemplateId: {
+      type: "string",
+      label: "Anoncreds Credential Template ID",
+      description: "The ID of the anoncreds credential template.",
+      async options({
+        projectId,
+        prevContext: { pageBeforeId },
+      }) {
+        if (pageBeforeId === null) {
+          return [];
+        }
+        const { data } = await this.listAnonCredsCredentialTemplates({
+          projectId,
+          params: {
+            "sort": "-updatedAt",
+            "page[size]": constants.DEFAULT_LIMIT,
+            "page[before]": pageBeforeId,
+          },
+        });
+        const options = data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+        return {
+          options,
+          context: {
+            pageBeforeId:
+              data.length
+                ? data[data.length - 1].id
+                : null,
+          },
+        };
+      },
+    },
+    presentationTemplateId: {
+      type: "string",
+      label: "Presentation Template ID",
+      description: "The ID of the presentation template.",
+      async options({
+        projectId,
+        prevContext: { pageBeforeId },
+      }) {
+        if (pageBeforeId === null) {
+          return [];
+        }
+        const { data } = await this.listPresentationTemplates({
+          projectId,
+          params: {
+            "sort": "-updatedAt",
+            "page[size]": constants.DEFAULT_LIMIT,
+            "page[before]": pageBeforeId,
+          },
+        });
+        const options = data.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+        return {
+          options,
+          context: {
+            pageBeforeId:
+              data.length
+                ? data[data.length - 1].id
+                : null,
+          },
+        };
+      },
+    },
+    openId4VcVerificationId: {
+      type: "string",
+      label: "OpenID4VC Verification ID",
+      description: "The ID of the OpenID4VC verification.",
+      async options({
+        projectId,
+        prevContext: { pageBeforeId },
+      }) {
+        if (pageBeforeId === null) {
+          return [];
+        }
+        const { data } = await this.listOpenId4VcVerifications({
+          projectId,
+          params: {
+            "sort": "-updatedAt",
+            "page[size]": constants.DEFAULT_LIMIT,
+            "page[before]": pageBeforeId,
+          },
+        });
+        return {
+          options: data.map(({ id }) => id),
+          context: {
+            pageBeforeId:
+              data.length
+                ? data[data.length - 1].id
+                : null,
+          },
+        };
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `${constants.BASE_URL}${constants.VERSION_PATH}${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "X-Access-Token": this.$auth.api_key,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      });
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        ...args,
+      });
+    },
+    delete(args = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        ...args,
+      });
+    },
+    listProjects(args = {}) {
+      return this._makeRequest({
+        path: "/projects",
+        ...args,
+      });
+    },
+    listSDJWTVCCredentialTemplates({
+      projectId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/templates/credentials/sd-jwt-vc`,
+        ...args,
+      });
+    },
+    listAnonCredsCredentialTemplates({
+      projectId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/templates/credentials/anoncreds`,
+        ...args,
+      });
+    },
+    getSDJWTVCCredentialTemplate({
+      projectId, credentialTemplateId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/templates/credentials/sd-jwt-vc/${credentialTemplateId}`,
+        ...args,
+      });
+    },
+    getAnonCredsCredentialTemplate({
+      projectId, credentialTemplateId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/templates/credentials/anoncreds/${credentialTemplateId}`,
+        ...args,
+      });
+    },
+    listPresentationTemplates({
+      projectId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/templates/presentations`,
+        ...args,
+      });
+    },
+    listOpenId4VcVerifications({
+      projectId, ...args
+    } = {}) {
+      return this._makeRequest({
+        path: `/projects/${projectId}/openid4vc/verification`,
+        ...args,
+      });
     },
   },
 };
