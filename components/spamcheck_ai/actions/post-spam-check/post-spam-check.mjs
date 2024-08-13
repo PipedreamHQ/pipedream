e677537fca3fa8ecbf0ad3f45a682629b5394865
Lucas Caresia <lucascarezia@gmail.com>
2024-08-13 14:38:54 -0300

    Added actions

---
+++ components/spamcheck_ai/actions/post-spam-check/post-spam-check.mjs
@@ -0,0 +1,35 @@
+import app from "../../spamcheck_ai.app.mjs";
+
+export default {
+  key: "spamcheck_ai-post-spam-check",
+  name: "Spam Check",
+  description: "Post a new spam check for an email or IP. [See the documentation](https://app.spamcheck.ai/api_docs/index.html)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    ip: {
+      propDefinition: [
+        app,
+        "ip",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.spamCheck({
+      $,
+      data: {
+        ip: this.ip,
+        email: this.email,
+      },
+    });
+    $.export("$summary", `Successfully posted spam check with ID '${response.id}'`);
+    return response;
+  },
+};
---
+++ components/spamcheck_ai/actions/post-spam-report/post-spam-report.mjs
@@ -0,0 +1,56 @@
+import app from "../../spamcheck_ai.app.mjs";
+
+export default {
+  key: "spamcheck_ai-post-spam-report",
+  name: "Spam Report",
+  description: "Create a new spam report. [See the documentation](https://app.spamcheck.ai/api_docs/index.html)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    ip: {
+      propDefinition: [
+        app,
+        "ip",
+      ],
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    desiredOutcome: {
+      propDefinition: [
+        app,
+        "desiredOutcome",
+      ],
+    },
+    result: {
+      propDefinition: [
+        app,
+        "result",
+      ],
+    },
+    notes: {
+      propDefinition: [
+        app,
+        "notes",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.spamReport({
+      $,
+      data: {
+        ip: this.ip,
+        email: this.email,
+        desired_outcome: this.desiredOutcome,
+        result: this.result,
+        notes: this.notes,
+      },
+    });
+    $.export("$summary", "Successfully created a new spam report");
+    return response;
+  },
+};
