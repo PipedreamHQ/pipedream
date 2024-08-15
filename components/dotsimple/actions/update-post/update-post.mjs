06d1b85b9f120d54cf1b7221b22ab61255edf8ee
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-15 09:54:24 -0500

    Dotsimple app new action and source components (#13498)

---
+++ components/dotsimple/actions/update-post/update-post.mjs
@@ -0,0 +1,81 @@
+import app from "../../dotsimple.app.mjs";
+
+export default {
+  key: "dotsimple-update-post",
+  name: "Update Post",
+  description: "Amend an existing post on your DotSimple site. [See the documentation](https://help.dotsimple.io/en/articles/68-posts)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    postId: {
+      propDefinition: [
+        app,
+        "postId",
+      ],
+    },
+    accountId: {
+      propDefinition: [
+        app,
+        "accountId",
+      ],
+    },
+    body: {
+      propDefinition: [
+        app,
+        "contentBody",
+      ],
+    },
+    url: {
+      propDefinition: [
+        app,
+        "contentUrl",
+      ],
+    },
+  },
+  methods: {
+    updatePost({
+      postId, ...args
+    } = {}) {
+      return this.app.put({
+        path: `/posts/${postId}`,
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      updatePost,
+      postId,
+      accountId,
+      body,
+      url,
+    } = this;
+
+    const response = await updatePost({
+      $,
+      postId,
+      data: {
+        accounts: [
+          accountId,
+        ],
+        versions: [
+          {
+            account_id: accountId,
+            is_original: true,
+            content: [
+              {
+                body,
+                url,
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    $.export("$summary", "Successfully updated post.");
+
+    return response;
+  },
+};
