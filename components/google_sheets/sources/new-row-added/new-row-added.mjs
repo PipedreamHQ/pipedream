cc475d6c68c038629f4ab6449a2a35c0e02e1bf6
michelle0927 <michellelbergero@hotmail.com>
2024-08-21 15:33:29 -0400

    add useQuery to sheetID

--- components/google_sheets/sources/new-row-added/new-row-added.mjs
+++ components/google_sheets/sources/new-row-added/new-row-added.mjs
@@ -8,7 +8,7 @@ export default {
   key: "google_sheets-new-row-added",
   name: "New Row Added (Instant)",
   description: "Emit new event each time a row or rows are added to the bottom of a spreadsheet.",
-  version: "0.1.6",
+  version: "0.1.7",
   dedupe: "unique",
   type: "source",
   props: {
