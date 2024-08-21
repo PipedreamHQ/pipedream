9cb64c676b6bd27d66d618112bb569f4801d67a0
michelle0927 <michellelbergero@hotmail.com>
2024-08-21 17:26:25 -0400

    update version

--- components/github/sources/webhook-events/webhook-events.mjs
+++ components/github/sources/webhook-events/webhook-events.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Webhook Event (Instant)",
   description: "Emit new event for each selected event type",
   type: "source",
-  version: "1.0.2",
+  version: "1.0.3",
   props: {
     ...common.props,
     events: {
