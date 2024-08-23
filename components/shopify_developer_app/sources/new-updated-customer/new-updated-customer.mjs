63a00e446f566c6e2f6da65f8832773b302d880f
Luan Cazarine <luanhc@gmail.com>
2024-08-23 13:27:03 -0300

    bump versions

--- components/shopify_developer_app/sources/new-updated-customer/new-updated-customer.mjs
+++ components/shopify_developer_app/sources/new-updated-customer/new-updated-customer.mjs
@@ -7,7 +7,7 @@ export default {
   name: "New Updated Customer (Instant)",
   type: "source",
   description: "Emit new event each time a customer's information is updated.",
-  version: "0.0.3",
+  version: "0.0.4",
   dedupe: "unique",
   methods: {
     ...common.methods,
