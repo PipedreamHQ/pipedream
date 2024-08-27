283dd1bce28cb5f00cba4dfdf45c55db5c05b359
GTFalcao <gtfalcao96@gmail.com>
2024-08-27 17:29:33 -0300

    Version bumps and text description improvements

--- components/pandadoc/actions/get-document-status/get-document-status.mjs
+++ components/pandadoc/actions/get-document-status/get-document-status.mjs
@@ -6,7 +6,7 @@ export default {
   description:
     "Get basic status info about a document. [See documentation here](https://developers.pandadoc.com/reference/document-status)",
   type: "action",
-  version: "0.0.2",
+  version: "0.0.3",
   props: {
     app,
     id: {
