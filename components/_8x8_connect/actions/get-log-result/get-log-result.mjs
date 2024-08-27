ad8bb04e80816ac6d236c6c2e59d8bd7358ce0ef
Lucas Caresia <lucascarezia@gmail.com>
2024-08-27 09:35:56 -0300

    Added actions (#13608)

---
+++ components/_8x8_connect/actions/get-log-result/get-log-result.mjs
@@ -0,0 +1,28 @@
+import app from "../../_8x8_connect.app.mjs";
+
+export default {
+  key: "_8x8_connect-get-log-result",
+  name: "Get Log Result",
+  description: "Check the status of an SMS Logs export job and to get a download link if its generation has succeeded. [See the documentation](https://developer.8x8.com/connect/reference/get-log-export-job-result)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    jobId: {
+      propDefinition: [
+        app,
+        "jobId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getLog({
+      $,
+      jobId: this.jobId,
+    });
+
+    $.export("$summary", `Successfully retrieved the log status: '${response.status}'`);
+
+    return response;
+  },
+};
