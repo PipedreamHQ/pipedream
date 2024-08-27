d412192137f00c032d220aa8a0fb98e833cc421a
GTFalcao <gtfalcao96@gmail.com>
2024-08-27 17:32:57 -0300

    Version bumps

--- components/pandadoc/sources/document-creation-failed/document-creation-failed.mjs
+++ components/pandadoc/sources/document-creation-failed/document-creation-failed.mjs
@@ -6,9 +6,9 @@ export default {
   ...common,
   name: "Document Creation Failed (Instant)",
   description:
-    `Emit new event when a document failed to be created [See docs here](${DOCS_LINK})`,
+    `Emit new event when a document failed to be created. [See the documentation here](${DOCS_LINK})`,
   key: "pandadoc-document-creation-failed",
-  version: "0.0.4",
+  version: "0.0.5",
   type: "source",
   methods: {
     ...common.methods,
