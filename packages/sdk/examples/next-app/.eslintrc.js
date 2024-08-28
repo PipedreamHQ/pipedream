4992973c13fce308fc1a5dedc4337f6ff7b56333
michelle0927 <michellelbergero@hotmail.com>
2024-08-28 11:20:54 -0400

    fix lint errors

--- packages/sdk/examples/next-app/.eslintrc.js
+++ packages/sdk/examples/next-app/.eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   extends: [
-  //  'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended",
   ],
   plugins: ['@typescript-eslint'],
-  rules: {
-    // Add your custom rules here
-  },
-  ignorePatterns: ['node_modules/', '.eslintrc.js'],
+  rules: {},
+  ignorePatterns: [
+    "node_modules/", 
+    ".eslintrc.js",
+  ],
 };
 
