cb5d95e2b53dd383523880fe0f07b1fbbb6cda3e
Jorge Cortes <jacortesmahmud@gmail.com>
2024-08-29 11:05:36 -0500

    SignNow: new action and source components (#13720)

---
+++ components/signnow/sources/common/base.mjs
@@ -0,0 +1,14 @@
+import { ConfigurationError } from "@pipedream/platform";
+import app from "../../signnow.app.mjs";
+
+export default {
+  props: {
+    app,
+    db: "$.service.db",
+  },
+  methods: {
+    generateMeta() {
+      throw new ConfigurationError("generateMeta is not implemented");
+    },
+  },
+};
