b7714b980e19fc88462eddc61b74dac4dd74f74c
Lucas Caresia <lucascarezia@gmail.com>
2024-08-29 08:24:23 -0300

    [Components] are_na #13033 (#13512)
    * Added actions
    
    * Added actions
    
    * Added actions
    
    * Fixed requested changes
    
    * Fixed requested changes

---
+++ components/are_na/actions/update-channel/update-channel.mjs
@@ -0,0 +1,42 @@
+import app from "../../are_na.app.mjs";
+
+export default {
+  key: "are_na-update-channel",
+  name: "Update Channel",
+  description: "Update a Channel with the specified slug. [See the documentation](https://dev.are.na/documentation/channels#Block45048)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    channelSlug: {
+      propDefinition: [
+        app,
+        "channelSlug",
+      ],
+    },
+    channelTitle: {
+      propDefinition: [
+        app,
+        "channelTitle",
+      ],
+    },
+    channelStatus: {
+      propDefinition: [
+        app,
+        "channelStatus",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.updateChannel({
+      $,
+      slug: this.channelSlug,
+      params: {
+        title: this.channelTitle,
+        status: this.channelStatus,
+      },
+    });
+    $.export("$summary", `Successfully updated channel with slug: '${this.channelSlug}'`);
+    return response;
+  },
+};
