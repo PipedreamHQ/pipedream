c73c3415d29d4fcad8fb91c6159688c62981c703
Luan Cazarine <luanhc@gmail.com>
2024-08-30 10:54:50 -0300

    New Components - adrapid (#13708)
    * adrapid init
    
    * [Components] adrapid #13636
    Sources
     - New Banner Ready
    
    Actions
     - Create Banner
     - Get Banner
    
    * pnpm update

---
+++ components/adrapid/sources/new-banner-ready/test-event.mjs
@@ -0,0 +1,44 @@
+export default {
+  "id": "12345678-1234-1234-1234-123456789012",
+  "name": "Multi design - 1 Product demo",
+  "ownerId": "12345678-1234-1234-1234-123456789012",
+  "files": [
+    {
+      "url": "https://banners.adrapid.com/12345678-1234-1234-1234-123456789012/banners/12345678-1234-1234-1234-123456789012/base/base.png",
+      "name": "Multi design - 1 Product demo_base.png",
+      "size": 125751,
+      "type": "png",
+      "files": [
+        {
+          "path": "12345678-1234-1234-1234-123456789012/banners/12345678-1234-1234-1234-123456789012/base/base.png",
+          "size": 125751,
+          "mimeType": "image/png"
+        }
+      ],
+      "index": "12345678-1234-1234-1234-123456789012/banners/12345678-1234-1234-1234-123456789012/base/base.png",
+      "width": 300,
+      "height": 600,
+      "status": "ready",
+      "sizeName": "base"
+    }
+  ],
+  "size": null,
+  "status": "ready",
+  "failedReason": null,
+  "templateName": null,
+  "dataSource": null,
+  "createdAt": "2024-08-27T15:06:30.434Z",
+  "updatedAt": "2024-08-27T15:06:30.434Z",
+  "deletedAt": null,
+  "templateId": null,
+  "parentTemplateId": "12345678-1234-1234-1234-123456789012",
+  "filesCount": 1,
+  "permissions": [
+    "get",
+    "put",
+    "del"
+  ],
+  "bannerId": "12345678-1234-1234-1234-123456789012",
+  "groupId": 302,
+  "updatedat": "2024-08-27T15:06:31.160Z"
+}
