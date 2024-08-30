5f8b0c0a506d149f6f67d772592621976ecf17ef
Lucas Caresia <lucascarezia@gmail.com>
2024-08-30 13:22:56 -0300

    Requested changes done

--- components/remote_retrieval/actions/get-pending-orders/get-pending-orders.mjs
+++ components/remote_retrieval/actions/get-pending-orders/get-pending-orders.mjs
@@ -6,7 +6,7 @@ export default {
   name: "Get Pending Orders",
   description: "Retrieve a list of the orders for which the payment process has not been completed.[See the documentation](https://www.remoteretrieval.com/api-documentation/#pending-orders)",
   type: "action",
-  version: "0.0.1",
+  version: "0.0.2",
   props: {
     app,
   },
