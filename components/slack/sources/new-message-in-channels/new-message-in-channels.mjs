5067fd3eb4192a542becbb2ee7039deaf274c97f
Jorge Cortes <jacortesmahmud@gmail.com>
2024-09-02 13:56:53 -0500

    Slack/Slack Bot: Message Timestime prop w/out async options

--- components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
+++ components/slack/sources/new-message-in-channels/new-message-in-channels.mjs
@@ -6,7 +6,7 @@ export default {
   ...common,
   key: "slack-new-message-in-channels",
   name: "New Message In Channels (Instant)",
-  version: "1.0.20",
+  version: "1.0.21",
   description: "Emit new event when a new message is posted to one or more channels",
   type: "source",
   dedupe: "unique",
