eae5818457c8bdac7f268c9e85e75f01e908c227
Jorge Cortes <jacortesmahmud@gmail.com>
2024-09-02 11:55:38 -0500

    ActiTime: New action and source components

---
+++ components/actitime/sources/new-customer-instant/test-event.mjs
@@ -0,0 +1,9 @@
+export default {
+  "event.name": "customer.create",
+  "id": 12,
+  "name": "Test 11",
+  "archived": false,
+  "created": 1725295489000,
+  "url": "https://online.actitime.com/pipedream1/tasks/tasklist.do?customerId=12",
+  "description": "Test 11 Desc"
+};
---
+++ components/actitime/sources/updated-time-track-instant/test-event.mjs
@@ -0,0 +1,11 @@
+export default {
+  "event.name": "timetrack.update",
+  "userId": 1,
+  "taskId": 136,
+  "taskName": "Meetings",
+  "time": 15,
+  "comment": "Test 1",
+  "date": "2024-09-02",
+  "projectId": 23,
+  "customerId": 9
+};
