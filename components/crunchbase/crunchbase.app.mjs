1d10fc4b54c516faa38e6ce47c87864886e70c75
Luan Cazarine <luanhc@gmail.com>
2024-09-04 09:21:28 -0300

    New Components - crunchbase (#13767)
    * crunchbase init
    
    * [Components] crunchbase #13188
    Actions
     - Get Organization
     - Search Organizations
    
    * pnpm update
    
    * some adjusts

--- components/crunchbase/crunchbase.app.mjs
+++ components/crunchbase/crunchbase.app.mjs
@@ -1,11 +1,71 @@
+import { axios } from "@pipedream/platform";
+import {
+  FIELDS_OPTIONS, LIMIT,
+} from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "crunchbase",
-  propDefinitions: {},
+  propDefinitions: {
+    entityId: {
+      type: "string",
+      label: "Entity ID",
+      description: "UUID or permalink of the desired entity",
+      async options({ prevContext }) {
+        const { entities } = await this.searchOrganizations({
+          data: {
+            field_ids: FIELDS_OPTIONS,
+            limit: LIMIT,
+            after_id: prevContext.lastId,
+          },
+        });
+
+        return {
+          options: entities.map(({
+            uuid, properties: { name },
+          }) => ({
+            label: name,
+            value: uuid,
+          })),
+          context: {
+            lastId: entities[entities.length - 1].uuid,
+          },
+        };
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.crunchbase.com/v4/data";
+    },
+    _headers() {
+      return {
+        "X-cb-user-key": this.$auth.user_key,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    getOrganizationDetails({
+      entityId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/entities/organizations/${entityId}`,
+        ...opts,
+      });
+    },
+    searchOrganizations(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/searches/organizations",
+        ...opts,
+      });
     },
   },
 };
