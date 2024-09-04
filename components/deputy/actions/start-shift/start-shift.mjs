6a2ffe1711bcde1ddc191fec49815b8f5f61dd6a
michelle0927 <michelle0927@users.noreply.github.com>
2024-09-04 10:35:40 -0400

    New Components - deputy (#13803)
    * deputy init
    
    * new components
    
    * pnpm-lock.yaml

---
+++ components/deputy/actions/start-shift/start-shift.mjs
@@ -0,0 +1,36 @@
+import deputy from "../../deputy.app.mjs";
+
+export default {
+  key: "deputy-start-shift",
+  name: "Start Shift",
+  description: "Starts a work shift for a specified employee in Deputy. [See the documentation](https://developer.deputy.com/deputy-docs/reference/startanemployeestimesheetclockon)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    deputy,
+    locationId: {
+      propDefinition: [
+        deputy,
+        "locationId",
+      ],
+    },
+    employeeId: {
+      propDefinition: [
+        deputy,
+        "employeeId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.deputy.startTimesheet({
+      $,
+      data: {
+        intOpunitId: this.locationId,
+        intEmployeeId: this.employeeId,
+      },
+    });
+
+    $.export("$summary", `Successfully started work shift for employee ${this.employeeId}`);
+    return response;
+  },
+};
