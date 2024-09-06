97c8b75608131fb213b5a1057f8dae60506dd64f
Luan Cazarine <luanhc@gmail.com>
2024-09-06 12:58:47 -0300

    [Components] KrispCall #12729 Sources  - New Contact (Instant)  - New SMS or MMS (Instant)  - New Voicemail (Instant)
    Actions
     - Add Contact
     - Delete Contact
     - New SMS
     - New MMS

---
+++ components/krispcall/sources/new-sms-or-mms-instant/test-event.mjs
@@ -0,0 +1,7 @@
+export default {
+  "id": "LdpkiDcAdWcorm6Nm9a3DR",
+  "from_number": "+17345084292",
+  "to_number": "+12568889109",
+  "media_link": "https://api.medialink.com/8be6fd004ace587eca250cb17c5af4d",
+  "content": ""
+}
