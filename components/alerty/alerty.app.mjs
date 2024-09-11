8337c200149c421fcfb2b5a711497f8974040189
Luan Cazarine <luanhc@gmail.com>
2024-09-11 17:59:29 -0300

    [Components] alerty #13848 Actions  - Notify Devices

--- components/alerty/alerty.app.mjs
+++ components/alerty/alerty.app.mjs
@@ -3,106 +3,23 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "alerty",
-  propDefinitions: {
-    message: {
-      type: "string",
-      label: "Message",
-      description: "The message to be included in the push notification",
-    },
-    title: {
-      type: "string",
-      label: "Title",
-      description: "Title for your notification message",
-      optional: true,
-    },
-    image: {
-      type: "string",
-      label: "Image URL",
-      description: "URL of an image to be displayed in the notification",
-      optional: true,
-    },
-    icon: {
-      type: "string",
-      label: "Icon URL",
-      description: "URL of an image to be used as an icon by the notification",
-      optional: true,
-    },
-    deviceId: {
-      type: "string[]",
-      label: "Device ID",
-      description: "Specific device IDs to send the notification to",
-      optional: true,
-    },
-    urgency: {
-      type: "string",
-      label: "Urgency",
-      description: "Urgency of the notification",
-      options: [
-        "very-low",
-        "low",
-        "normal",
-        "high",
-      ],
-      default: "very-low",
-      optional: true,
-    },
-    actions: {
-      type: "string[]",
-      label: "Actions",
-      description: "Actions for the notification, each item should be a JSON string",
-      optional: true,
-    },
-  },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
-      return "https://alerty.dev/api";
+      return `${this.$auth.notification_url}`;
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "POST",
-        path = "/notify",
-        headers,
-        ...otherOpts
-      } = opts;
-      return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          Authorization: `Bearer ${this.$auth.api_key}`,
-        },
-      });
-    },
-    async sendNotification(opts = {}) {
-      const {
-        message,
-        title,
-        image,
-        icon,
-        deviceId,
-        urgency,
-        actions,
-        ...otherOpts
-      } = opts;
-      const data = {
-        message,
-        title,
-        image,
-        icon,
-        device_id: deviceId,
-        urgency,
-        actions: actions
-          ? actions.map(JSON.parse)
-          : undefined,
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.api_key}`,
       };
-      return this._makeRequest({
-        data,
-        ...otherOpts,
+    },
+    makeRequest({
+      $ = this, ...opts
+    }) {
+      return axios($, {
+        method: "POST",
+        url: this._baseUrl(),
+        headers: this._headers(),
+        ...opts,
       });
     },
   },
