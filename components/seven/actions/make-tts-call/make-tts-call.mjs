69bac91016509fd32bd91b3af6663b29c4e30cd1
michelle0927 <michellelbergero@hotmail.com>
2024-09-12 15:37:17 -0400

    new components

---
+++ components/seven/actions/make-tts-call/make-tts-call.mjs
@@ -0,0 +1,34 @@
+import seven from "../../seven.app.mjs";
+
+export default {
+  key: "seven-make-tts-call",
+  name: "Make TTS Call",
+  description: "Make a text-to-speech call via Seven. [See the documentation](https://docs.seven.io/en/rest-api/endpoints/voice#send-voice-call)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    seven,
+    to: {
+      propDefinition: [
+        seven,
+        "to",
+      ],
+    },
+    text: {
+      type: "string",
+      label: "Text",
+      description: "Text message to be read out",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.seven.sendTtsCall({
+      $,
+      data: {
+        to: this.to,
+        text: this.text,
+      },
+    });
+    $.export("$summary", `Successfully sent TTS call to number: ${this.to}`);
+    return response;
+  },
+};
