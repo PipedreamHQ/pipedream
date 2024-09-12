c86bee2afefc7beaea30c3028275024bc1ed9182
Jorge Cortes <jacortesmahmud@gmail.com>
2024-09-12 10:18:15 -0500

    [BUG]: Fixed utils function parseArray

--- components/grist/common/utils.mjs
+++ components/grist/common/utils.mjs
@@ -1,10 +1,6 @@
 import { ConfigurationError } from "@pipedream/platform";
 
 function isJson(value) {
-  if (typeof(value) !== "string") {
-    return false;
-  }
-
   try {
     JSON.parse(value);
   } catch (e) {
@@ -15,9 +11,14 @@ function isJson(value) {
 }
 
 function valueToObject(value) {
+  if (typeof(value) === "object") {
+    return value;
+  }
+
   if (!isJson(value)) {
     throw new ConfigurationError(`Make sure the custom expression contains a valid JSON object: ${value}`);
   }
+
   return JSON.parse(value);
 }
 
