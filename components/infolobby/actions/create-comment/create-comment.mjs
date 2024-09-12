b8ebebf7612d4591f735830c3c7b260a1f5137fb
Lucas Caresia <lucascarezia@gmail.com>
2024-09-12 12:41:43 -0300

    Added actions

---
+++ components/infolobby/actions/create-comment/create-comment.mjs
@@ -0,0 +1,48 @@
+import app from "../../infolobby.app.mjs";
+
+export default {
+  key: "infolobby-create-comment",
+  name: "Create Comment",
+  description: "Create a new Comment for a record. [See the documentation](https://infolobby.com/site/apidocs/4/working-with-record-comments/)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    tableId: {
+      propDefinition: [
+        app,
+        "tableId",
+      ],
+    },
+    itemId: {
+      propDefinition: [
+        app,
+        "itemId",
+        (c) => ({
+          tableId: c.tableId,
+        }),
+      ],
+    },
+    comment: {
+      propDefinition: [
+        app,
+        "comment",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createComment({
+      $,
+      tableId: this.tableId,
+      itemId: this.itemId,
+      data: {
+        comment: this.comment,
+      },
+
+    });
+
+    $.export("$summary", `Successfully created Comment with ID '${response}'`);
+
+    return response;
+  },
+};
