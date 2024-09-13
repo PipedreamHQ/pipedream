b1393d0640c0ea55b5fdaf42d8b404b82412d37d
GTFalcao <gtfalcao96@gmail.com>
2024-09-13 17:38:42 -0300

    Patch version bumps

--- components/openai/actions/create-transcription/create-transcription.mjs
+++ components/openai/actions/create-transcription/create-transcription.mjs
@@ -24,7 +24,7 @@ const pipelineAsync = promisify(stream.pipeline);
 
 export default {
   name: "Create Transcription (Whisper)",
-  version: "0.1.11",
+  version: "0.1.12",
   key: "openai-create-transcription",
   description: "Transcribes audio into the input language. [See the documentation](https://platform.openai.com/docs/api-reference/audio/create).",
   type: "action",
