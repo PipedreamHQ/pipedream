ae486813913e2b1cd8636b569aabb1280fce628f
danhsiung <35384182+danhsiung@users.noreply.github.com>
2024-09-16 13:33:04 -0700

    Adding app scaffolding for pdf_app_net

---
+++ components/pdf_app_net/pdf_app_net.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "pdf_app_net",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
