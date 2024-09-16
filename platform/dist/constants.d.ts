27fad263e07b88f085531a3d91dd77507e9a7fdc
michelle0927 <michellelbergero@hotmail.com>
2024-09-16 10:37:23 -0400

    truncate client id

--- platform/dist/constants.d.ts
+++ platform/dist/constants.d.ts
@@ -1,2 +1,2 @@
 export declare const DEFAULT_POLLING_SOURCE_TIMER_INTERVAL: number;
-export declare const PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = "38931588176-fnd4m13k1mjb6djallp1m9kr7o8kslcu.apps.googleusercontent.com";
+export declare const PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = "fnd4m13k1mjb6djallp1m9kr7o8kslcu";
--- platform/dist/constants.js
+++ platform/dist/constants.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = exports.DEFAULT_POLLING_SOURCE_TIMER_INTERVAL = void 0;
 exports.DEFAULT_POLLING_SOURCE_TIMER_INTERVAL = 60 * 15;
-exports.PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = "38931588176-fnd4m13k1mjb6djallp1m9kr7o8kslcu.apps.googleusercontent.com";
+exports.PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = "fnd4m13k1mjb6djallp1m9kr7o8kslcu";
