81fa82a51d31d5d44acf10ff9d250fa963af2a2e
Luan Cazarine <luanhc@gmail.com>
2024-09-18 16:21:54 -0300

    [Components] fileforge #13975 Actions  - Generate PDF

--- components/fileforge/actions/generate-pdf/generate-pdf.mjs
+++ components/fileforge/actions/generate-pdf/generate-pdf.mjs
@@ -1,79 +1,85 @@
-import fileforge from "../../fileforge.app.mjs";
-import { axios } from "@pipedream/platform";
 import FormData from "form-data";
 import fs from "fs";
-import stream from "stream";
-import { promisify } from "util";
-
-const pipeline = promisify(stream.pipeline);
+import {
+  checkTmp,
+  parseObject,
+} from "../../common/utils.mjs";
+import fileforge from "../../fileforge.app.mjs";
 
 export default {
   key: "fileforge-generate-pdf",
   name: "Generate PDF",
   description: "Generate a PDF from provided HTML. [See the documentation](https://docs.fileforge.com/api-reference/api-reference/pdf/generate)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
     fileforge,
+    alert: {
+      type: "alert",
+      alertType: "warning",
+      content: `An **\`index.html\`** file is required, and will be used as the main document.
+      Other documents may also be attached, such as stylesheets or images.
+      The path in the **\`filename\`** part of the multipart attachement will be respected during generation.
+      **Important notice:** during generation, the **\`index.html\`** file will be processed to include the base URL of the document.
+      This is required for assets to be loaded correctly.
+      To link your assets from the HTML file, you should not use a leading slash in the URL.
+      For example, use **\`<img src="image.jpg" />\`** instead of **\`<img src="/image.jpg" />\`**.`,
+    },
     files: {
-      propDefinition: [
-        fileforge,
-        "files",
-      ],
+      type: "string[]",
+      label: "HTML Files",
+      description: "The HTML files to convert to PDF. Each file should be a valid path to an HTML file.",
     },
     test: {
-      propDefinition: [
-        fileforge,
-        "test",
-      ],
+      type: "boolean",
+      label: "Test",
+      description: "Generate a test document instead of a production document. The generated document will contain a watermark. Defaults to true.",
+      optional: true,
     },
-    host: {
-      propDefinition: [
-        fileforge,
-        "host",
-      ],
+    expiresAt: {
+      type: "string",
+      label: "Expires At",
+      description: "The expiration timestamp for the PDF in ISO 8601 format",
+      optional: true,
     },
-    expiresat: {
-      propDefinition: [
-        fileforge,
-        "expiresat",
-      ],
+    fileName: {
+      type: "string",
+      label: "Filename",
+      description: "The desired filename for the generated PDF.",
+      optional: true,
     },
-    filename: {
-      propDefinition: [
-        fileforge,
-        "filename",
-      ],
-    },
-    allowviewing: {
-      propDefinition: [
-        fileforge,
-        "allowviewing",
-      ],
+    allowViewing: {
+      type: "boolean",
+      label: "Allow Viewing",
+      description: "Specifies whether viewing is allowed.",
+      optional: true,
     },
   },
   async run({ $ }) {
-    const response = await this.fileforge.generatePDF({
-      files: this.files,
-      test: this.test,
-      host: this.host,
-      expiresat: this.expiresat,
-      filename: this.filename,
-      allowviewing: this.allowviewing,
+    const {
+      fileforge,
+      files,
+      ...data
+    } = this;
+
+    const formData = new FormData();
+    const parsedFiles = parseObject(files);
+
+    for (const file of parsedFiles) {
+      formData.append("files", fs.createReadStream(checkTmp(file)));
+    }
+
+    formData.append("options", JSON.stringify(data), {
+      contentType: "application/json",
     });
 
-    const pdfPath = "/tmp/generated.pdf";
-    const pdfStream = await axios($, {
-      method: "GET",
-      url: response.url,
-      responseType: "stream",
+    const response = await fileforge.generatePDF({
+      $,
+      data: formData,
+      headers: formData.getHeaders(),
     });
 
-    await pipeline(pdfStream, fs.createWriteStream(pdfPath));
-
-    $.export("$summary", `Successfully generated PDF at ${pdfPath}`);
-    return {
-      pdfPath,
-    };
+    $.export("$summary", "Successfully generated the PDF file.");
+    return response;
   },
 };
--- components/fileforge/fileforge.app.mjs
+++ components/fileforge/fileforge.app.mjs
@@ -3,102 +3,30 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "fileforge",
-  propDefinitions: {
-    files: {
-      type: "string[]",
-      label: "HTML Files",
-      description: "The HTML files to convert to PDF. Each file should be a valid URL or path to an HTML file.",
-    },
-    test: {
-      type: "boolean",
-      label: "Test",
-      description: "Enable test mode",
-      optional: true,
-    },
-    host: {
-      type: "string",
-      label: "Host",
-      description: "Specifies the host for the PDF generation.",
-      optional: true,
-    },
-    expiresat: {
-      type: "string",
-      label: "Expires At",
-      description: "The expiration timestamp for the PDF in ISO 8601 format",
-      optional: true,
-    },
-    filename: {
-      type: "string",
-      label: "Filename",
-      description: "The desired filename for the generated PDF.",
-      optional: true,
-    },
-    allowviewing: {
-      type: "boolean",
-      label: "Allow Viewing",
-      description: "Specifies whether viewing is allowed.",
-      optional: true,
-    },
-  },
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
       return "https://api.fileforge.com";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this, method = "POST", path, headers, ...otherOpts
-      } = opts;
+    _headers(headers = {}) {
+      return {
+        "X-API-Key": this.$auth.api_key,
+        ...headers,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...opts
+    }) {
       return axios($, {
-        ...otherOpts,
-        method,
         url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "X-API-Key": this.$auth.api_key,
-          "Content-Type": "multipart/form-data",
-        },
+        headers: this._headers(headers),
+        ...opts,
       });
     },
-    async generatePDF({
-      files, test, host, expiresat, filename, allowviewing,
-    }) {
-      const formData = new FormData();
-
-      // Append files
-      files.forEach((file, index) => {
-        formData.append("files", file, `file${index}.html`);
-      });
-
-      // Construct options object
-      const options = {
-        ...(test !== undefined && {
-          test,
-        }),
-        ...(host && {
-          host,
-        }),
-        ...(expiresat && {
-          expiresat,
-        }),
-        ...(filename && {
-          filename,
-        }),
-        ...(allowviewing !== undefined && {
-          allowviewing,
-        }),
-      };
-
-      formData.append("options", JSON.stringify(options));
-
+    generatePDF(opts = {}) {
       return this._makeRequest({
+        method: "POST",
         path: "/pdf/generate/",
-        data: formData,
-        headers: {
-          Accept: "application/pdf",
-        },
+        ...opts,
       });
     },
   },
