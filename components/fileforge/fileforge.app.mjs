d83f8eeeeafcf9c5cfce3bf84ba7a0fe8bdd5968
Luan Cazarine <luanhc@gmail.com>
2024-09-20 16:06:08 -0300

    New Components - fileforge (#13989)
    * fileforge init
    
    * [Components] fileforge #13975
    Actions
     - Generate PDF
    
    * pnpm update
    
    * some adjusts
    
    * add form-data
    
    * pnpm update

--- components/fileforge/fileforge.app.mjs
+++ components/fileforge/fileforge.app.mjs
@@ -1,11 +1,33 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "fileforge",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.fileforge.com";
+    },
+    _headers(headers = {}) {
+      return {
+        "X-API-Key": this.$auth.api_key,
+        ...headers,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(headers),
+        ...opts,
+      });
+    },
+    generatePDF(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/pdf/generate/",
+        ...opts,
+      });
     },
   },
 };
