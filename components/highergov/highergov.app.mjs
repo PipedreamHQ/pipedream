a9580b8739235c4d928a7d7d105c499452b55800
Luan Cazarine <luanhc@gmail.com>
2024-09-23 17:23:34 -0300

    highergov init

--- components/highergov/highergov.app.mjs
+++ components/highergov/highergov.app.mjs
@@ -1,11 +1,67 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "highergov",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
     authKeys() {
       console.log(Object.keys(this.$auth));
     },
+    _baseUrl() {
+      return "https://www.highergov.com/zapier";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        method = "GET",
+        path = "/",
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        method,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "X-API-KEY": this.$auth.api_key,
+        },
+      });
+    },
+    async getAuth(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/auth",
+        ...opts,
+      });
+    },
+    async subscribeWebhook(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/pipeline/subscribe/",
+        data: {
+          target_url: opts.targetUrl,
+        },
+        ...opts,
+      });
+    },
+    async unsubscribeWebhook(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/pipeline/unsubscribe/",
+        ...opts,
+      });
+    },
+    async performList(opts = {}) {
+      return this._makeRequest({
+        method: "GET",
+        path: "/perform/",
+        ...opts,
+      });
+    },
   },
+  version: "0.0.{{ts}}",
 };
