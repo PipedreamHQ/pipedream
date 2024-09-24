eca76986a7e00223268c900e39cefcdedfa807c6
Luan Cazarine <luanhc@gmail.com>
2024-09-24 11:59:38 -0300

    [Components] highergov #14050 Sources  - New Pusuit Added (Instant)

--- components/highergov/highergov.app.mjs
+++ components/highergov/highergov.app.mjs
@@ -3,65 +3,39 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "highergov",
-  propDefinitions: {},
   methods: {
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
       return "https://www.highergov.com/zapier";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this,
-        method = "GET",
-        path = "/",
-        headers,
-        ...otherOpts
-      } = opts;
-      return axios($, {
-        ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
-        headers: {
-          ...headers,
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "X-API-KEY": this.$auth.api_key,
-        },
-      });
+    _headers() {
+      return {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-API-KEY": this.$auth.api_key,
+      };
     },
-    async getAuth(opts = {}) {
-      return this._makeRequest({
-        method: "POST",
-        path: "/auth",
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
         ...opts,
       });
     },
-    async subscribeWebhook(opts = {}) {
+    subscribeWebhook(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/pipeline/subscribe/",
-        data: {
-          target_url: opts.targetUrl,
-        },
         ...opts,
       });
     },
-    async unsubscribeWebhook(opts = {}) {
+    unsubscribeWebhook(opts = {}) {
       return this._makeRequest({
         method: "POST",
         path: "/pipeline/unsubscribe/",
         ...opts,
       });
     },
-    async performList(opts = {}) {
-      return this._makeRequest({
-        method: "GET",
-        path: "/perform/",
-        ...opts,
-      });
-    },
   },
-  version: "0.0.{{ts}}",
 };
--- components/highergov/sources/new-pursuit-added-instant/new-pursuit-added-instant.mjs
+++ components/highergov/sources/new-pursuit-added-instant/new-pursuit-added-instant.mjs
@@ -1,19 +1,15 @@
 import highergov from "../../highergov.app.mjs";
-import { axios } from "@pipedream/platform";
-import crypto from "crypto";
+import sampleEmit from "./test-event.mjs";
 
 export default {
   key: "highergov-new-pursuit-added-instant",
-  name: "New Pursuit Added Instant",
-  description: "Emit new event when a pursuit is added to the pipeline. [See the documentation](https://docs.highergov.com/import-and-export/zapier-integration)",
-  version: "0.0.{{ts}}",
+  name: "New Pursuit Added (Instant)",
+  description: "Emit new event when a pursuit is added to the pipeline.",
+  version: "0.0.1",
   type: "source",
   dedupe: "unique",
   props: {
-    highergov: {
-      type: "app",
-      app: "highergov",
-    },
+    highergov,
     http: {
       type: "$.interface.http",
       customResponse: true,
@@ -21,62 +17,33 @@ export default {
     db: "$.service.db",
   },
   methods: {
-    _getWebhookId() {
-      return this.db.get("webhookId");
-    },
-    _setWebhookId(id) {
-      this.db.set("webhookId", id);
+    emitEvent(event) {
+      this.$emit(event, {
+        id: event.opp_key,
+        summary: `New pursuit added: ${event.title}`,
+        ts: Date.parse(event.current_datetime),
+      });
     },
   },
   hooks: {
-    async deploy() {
-      const events = await this.highergov.performList({
-        max: 50,
-      });
-      for (const event of events) {
-        this.$emit(event, {
-          id: event.opp_key,
-          summary: `New pursuit added: ${event.title}`,
-          ts: Date.parse(event.current_datetime),
-        });
-      }
-    },
     async activate() {
       const hookId = await this.highergov.subscribeWebhook({
-        targetUrl: this.http.endpoint,
+        data: {
+          target_url: this.http.endpoint,
+        },
       });
-      this._setWebhookId(hookId);
+      console.log("hookId: ", hookId);
     },
     async deactivate() {
-      const id = this._getWebhookId();
-      await this.highergov.unsubscribeWebhook({
-        id,
-      });
+      await this.highergov.unsubscribeWebhook();
     },
   },
-  async run(event) {
-    const signature = event.headers["x-highergov-signature"];
-    const computedSignature = crypto.createHmac("sha256", this.highergov.$auth.api_key)
-      .update(event.rawBody)
-      .digest("hex");
-
-    if (signature !== computedSignature) {
-      this.http.respond({
-        status: 401,
-        body: "Unauthorized",
-      });
-      return;
-    }
-
-    this.http.respond({
-      status: 200,
-    });
-
-    const pursuit = event.body;
-    this.$emit(pursuit, {
-      id: pursuit.opp_key,
-      summary: `New pursuit added: ${pursuit.title}`,
-      ts: Date.parse(pursuit.current_datetime),
+  async run({ body }) {
+    this.$emit(body, {
+      id: body.opp_key,
+      summary: `New pursuit added: ${body.title}`,
+      ts: Date.parse(body.current_datetime),
     });
   },
+  sampleEmit,
 };
