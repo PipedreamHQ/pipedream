950a2d1f57f27744372780d6a6ca6eb826501f5f
michelle0927 <michelle0927@users.noreply.github.com>
2024-09-25 10:32:37 -0400

    Google Calendar - New Created or Updated Event (Instant) - updates to prevent rate limiting (#14063)
    * updates to reduce rate limiting
    
    * versions

--- components/google_calendar/sources/new-calendar/new-calendar.mjs
+++ components/google_calendar/sources/new-calendar/new-calendar.mjs
@@ -5,7 +5,7 @@ export default {
   key: "google_calendar-new-calendar",
   name: "New Calendar Created",
   description: "Emit new event when a calendar is created.",
-  version: "0.1.9",
+  version: "0.1.10",
   type: "source",
   dedupe: "unique",
   props: {
