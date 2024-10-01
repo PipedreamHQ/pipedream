bfbec2ea737c756da6d2c5a498cf7bb00d540181
Luan Cazarine <luanhc@gmail.com>
2024-10-01 09:35:36 -0300

    New Components - l2s (#14115)
    * l2s init
    
    * [Components] l2s #14107
    Actions
     - Create Shortened URL
    
    * pnpm update

--- components/l2s/l2s.app.mjs
+++ components/l2s/l2s.app.mjs
@@ -1,11 +1,32 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "l2s",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.l2s.is";
+    },
+    _headers() {
+      return {
+        Authorization: `Bearer ${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    shortenUrl(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/url",
+        ...opts,
+      });
     },
   },
 };
