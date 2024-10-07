56a2b2e367af370fe8d219024ae94d6285e850f8
michelle0927 <michellelbergero@hotmail.com>
2024-10-07 14:25:54 -0400

    new component

--- components/burstyai/actions/run-workflow/run-workflow.mjs
+++ components/burstyai/actions/run-workflow/run-workflow.mjs
@@ -3,30 +3,55 @@ import burstyai from "../../burstyai.app.mjs";
 export default {
   key: "burstyai-run-workflow",
   name: "Run Workflow",
-  description: "Triggers an AI workflow on BurstyAI. The specific workflow to run and optional parameters for the workflow can be configured. [See the documentation](https://burstyai.readme.io/reference)",
+  description: "Triggers an AI workflow on BurstyAI. [See the documentation](https://burstyai.readme.io/reference)",
   version: "0.0.1",
   type: "action",
   props: {
     burstyai,
     workflow: {
-      propDefinition: [
-        burstyai,
-        "workflow",
-      ],
+      type: "string",
+      label: "Workflow ID",
+      description: "The ID of the specific workflow to run. When viewing the workflow in BurstyAI, the ID is the last part of the URL. Example: `65e1cd00141fdc000195cb88`",
     },
-    parameters: {
-      propDefinition: [
-        burstyai,
-        "parameters",
-      ],
+    params: {
+      type: "object",
+      label: "Parameters",
+      description: "Optional parameters for the workflow",
+      optional: true,
+    },
+    waitForCompletion: {
+      type: "boolean",
+      label: "Wait For Completion",
+      description: "Set to `true` to poll the API in 3-second intervals until the workflow is completed",
+      optional: true,
     },
   },
   async run({ $ }) {
-    const response = await this.burstyai.triggerWorkflow({
+    let response = await this.burstyai.triggerWorkflow({
+      $,
       workflow: this.workflow,
-      parameters: this.parameters,
+      data: {
+        params: this.params || {},
+      },
     });
-    $.export("$summary", `Successfully triggered workflow ${this.workflow}`);
+
+    const jobId = response;
+
+    if (this.waitForCompletion) {
+      const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+      while (response?.jobStatus !== "END" && response?.jobStatus !== "ERROR") {
+        response = await this.burstyai.getWorkflowExecutionResult({
+          $,
+          jobId,
+        });
+        await timer(3000);
+      }
+    }
+
+    if (response?.status === "END") {
+      $.export("$summary", `Successfully triggered workflow ${this.workflow}`);
+    }
+
     return response;
   },
 };
--- components/burstyai/burstyai.app.mjs
+++ components/burstyai/burstyai.app.mjs
@@ -3,67 +3,39 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "burstyai",
-  propDefinitions: {
-    workflow: {
-      type: "string",
-      label: "Workflow",
-      description: "The specific workflow to run",
-      required: true,
-      async options() {
-        const workflows = await this.getWorkflows();
-        return workflows.map((workflow) => ({
-          label: workflow.name,
-          value: workflow.id,
-        }));
-      },
-    },
-    parameters: {
-      type: "object",
-      label: "Parameters",
-      description: "Optional parameters for the workflow",
-      optional: true,
-    },
-  },
   methods: {
     _baseUrl() {
-      return "https://app.burstyai.com";
+      return "https://app.burstyai.com/burstyai";
     },
-    async _makeRequest(opts = {}) {
+    _makeRequest(opts = {}) {
       const {
         $ = this,
-        method = "GET",
         path,
-        headers,
         ...otherOpts
       } = opts;
       return axios($, {
         ...otherOpts,
-        method,
-        url: this._baseUrl() + path,
+        url: `${this._baseUrl()}${path}`,
         headers: {
-          ...headers,
-          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+          Authorization: `Bearer ${this.$auth.api_key}`,
         },
       });
     },
-    async getWorkflows() {
-      const res = await this._makeRequest({
-        path: "/burstyai/aiflows",
-      });
-      return res;
-    },
-    async triggerWorkflow({
-      workflow, parameters,
+    triggerWorkflow({
+      workflow, ...args
     }) {
       return this._makeRequest({
         method: "POST",
-        path: `/burstyai/aiflows/${workflow}/async_run`,
-        data: parameters,
+        path: `/aiflows/${workflow}/async_run`,
+        ...args,
       });
     },
-    async getWorkflowExecutionResult(jobId) {
+    getWorkflowExecutionResult({
+      jobId, ...args
+    }) {
       return this._makeRequest({
-        path: `/burstyai/aiflowjobs/${jobId}/result`,
+        path: `/aiflowjobs/${jobId}/result`,
+        ...args,
       });
     },
   },
