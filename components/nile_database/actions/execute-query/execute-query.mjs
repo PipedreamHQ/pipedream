e421de5a1fc4df9af3ed0dee210df1072645c71c
michelle0927 <michellelbergero@hotmail.com>
2024-10-09 11:24:06 -0400

    execute-query

--- components/nile_database/actions/execute-query/execute-query.mjs
+++ components/nile_database/actions/execute-query/execute-query.mjs
@@ -3,23 +3,51 @@ import nile from "../../nile_database.app.mjs";
 export default {
   name: "Execute Query",
   key: "nile_database-execute-query",
-  description: "Execute a custom PostgreSQL query. See [our docs](https://pipedream.com/docs/databases/working-with-sql) to learn more about working with SQL in Pipedream.",
+  description: "Execute a custom PostgreSQL query.",
   version: "0.0.1",
   type: "action",
   props: {
     nile,
-    // eslint-disable-next-line pipedream/props-description
-    sql: {
-      type: "sql",
-      auth: {
-        app: "nile",
-      },
-      label: "PostreSQL Query",
+    user: {
+      type: "string",
+      label: "Username",
+      description: "The username of the database user",
+    },
+    password: {
+      type: "string",
+      label: "Password",
+      description: "The password of the database user",
+    },
+    host: {
+      type: "string",
+      label: "Host",
+      description: "The host of the database",
+    },
+    port: {
+      type: "string",
+      label: "Port",
+      description: "The port to connect to the database. Example: `5432`",
+    },
+    database: {
+      type: "string",
+      label: "Database",
+      description: "The name of the database to connect to",
+    },
+    query: {
+      type: "string",
+      label: "Query",
+      description: "The PostgreSQL query to execute",
     },
   },
   async run({ $ }) {
-    const args = this.nile.executeQueryAdapter(this.sql);
-    const data = await this.nile.executeQuery(args);
+    const config = {
+      user: this.user,
+      password: this.password,
+      host: this.host,
+      port: this.port,
+      database: this.database,
+    };
+    const data = await this.nile.executeQuery(config, this.query);
     $.export("$summary", `Returned ${data.length} ${data.length === 1
       ? "row"
       : "rows"}`);
--- components/nile_database/nile_database.app.mjs
+++ components/nile_database/nile_database.app.mjs
@@ -4,25 +4,7 @@ export default {
   type: "app",
   app: "nile_database",
   methods: {
-    getClientConfiguration() {
-      const {
-        username,
-        password,
-        host,
-        port,
-        database,
-      } = this.$auth;
-
-      return {
-        user: username,
-        password,
-        host,
-        port,
-        database,
-      };
-    },
-    async _getClient() {
-      const config = this.getClientConfiguration();
+    async _getClient(config) {
       const pool = new pg.Pool(config);
       const client = await pool.connect();
       return client;
@@ -30,63 +12,14 @@ export default {
     async _endClient(client) {
       return client.release();
     },
-    async executeQuery(query) {
-      const client = await this._getClient();
-
+    async executeQuery(config, query) {
+      const client = await this._getClient(config);
       try {
         const { rows } = await client.query(query);
         return rows;
       } finally {
         await this._endClient(client);
       }
-    },
-    executeQueryAdapter(proxyArgs = {}) {
-      const {
-        query: text = "",
-        params: values = [],
-      } = proxyArgs;
-      return {
-        text,
-        values,
-      };
-    },
-    proxyAdapter(query) {
-      if (typeof query === "string") {
-        return {
-          query,
-        };
-      }
-
-      return {
-        query: query.text,
-        params: query.values,
-      };
-    },
-    async getSchema() {
-      const text = `
-        SELECT table_name AS "tableName",
-          column_name AS "columnName",
-          is_nullable AS "isNullable",
-          data_type AS "dataType",
-          column_default AS "columnDefault"
-        FROM information_schema.columns
-        WHERE table_schema NOT IN ('pg_catalog', 'information_schema', 'users', 'auth')
-        ORDER BY table_name,
-          ordinal_position
-      `;
-      const rows = await this.executeQuery({
-        text,
-      });
-      return rows.reduce((acc, row) => {
-        acc[row.tableName] ??= {
-          metadata: {},
-          schema: {},
-        };
-        acc[row.tableName].schema[row.columnName] = {
-          ...row,
-        };
-        return acc;
-      }, {});
     },
   },
 };
