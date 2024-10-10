ff5b869ae869fdb5eebd342523910cfd317d8e4d
Jorge Cortes <jacortesmahmud@gmail.com>
2024-10-10 16:34:42 -0500

    Ollama: new action components

---
+++ components/ollama/actions/create-model/create-model.mjs
@@ -0,0 +1,55 @@
+import app from "../../ollama.app.mjs";
+
+export default {
+  key: "ollama-create-model",
+  name: "Create Model",
+  description: "Create a model from a modelfile. [See the documentation](https://github.com/ollama/ollama/blob/main/docs/api.md#create-a-model).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the model.",
+    },
+    modelfile: {
+      type: "string",
+      label: "Model File",
+      description: "Contents of the Modelfile. Eg. `FROM llama3 SYSTEM You are mario from Super Mario Bros`",
+    },
+    stream: {
+      propDefinition: [
+        app,
+        "stream",
+      ],
+    },
+  },
+  methods: {
+    createModel(args = {}) {
+      return this.app.post({
+        path: "/create",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      createModel,
+      name,
+      modelfile,
+      stream,
+    } = this;
+
+    const response = await createModel({
+      $,
+      data: {
+        name,
+        modelfile,
+        stream,
+      },
+    });
+    $.export("$summary", "Successfully created model.");
+    return response;
+  },
+};
---
+++ components/ollama/actions/push-model/push-model.mjs
@@ -0,0 +1,59 @@
+import app from "../../ollama.app.mjs";
+
+export default {
+  key: "ollama-push-model",
+  name: "Push Model to Library",
+  description: "Upload a model to a model library. Requires registering for ollama.ai and adding a public key first. [See the documentation](https://github.com/ollama/ollama/blob/main/docs/api.md#push-a-model).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    name: {
+      description: "Name of the model to push in the form of `<namespace>/<model>:<tag>`. Please make sure you follow [the instructions in this issue](https://github.com/ollama/ollama/issues/1140#issuecomment-1814823949) in order to push a model to your own library in [ollama.com](https://ollama.com/).",
+      propDefinition: [
+        app,
+        "model",
+      ],
+    },
+    insecure: {
+      propDefinition: [
+        app,
+        "insecure",
+      ],
+    },
+    stream: {
+      propDefinition: [
+        app,
+        "stream",
+      ],
+    },
+  },
+  methods: {
+    pushModel(args = {}) {
+      return this.app.post({
+        path: "/push",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      pushModel,
+      name,
+      insecure,
+      stream,
+    } = this;
+
+    const response = await pushModel({
+      $,
+      data: {
+        name,
+        insecure,
+        stream,
+      },
+    });
+
+    $.export("$summary", "Successfully pushed model.");
+    return response;
+  },
+};
