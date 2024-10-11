42abe7a11ad783e70c5b8f84bf1dc1f5997f283d
Lucas Caresia <lucascarezia@gmail.com>
2024-10-11 14:42:13 -0300

    [Components] xata #13198 (#13853)
    * Added actions
    
    * Added actions
    
    * Update components/xata/actions/list-branches/list-branches.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Requested changes done
    
    * updates
    
    * pnpm-lock.yaml
    
    * format recordData
    
    * updates
    
    * update
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/xata/actions/create-record/create-record.mjs
@@ -0,0 +1,31 @@
+import common from "../common/common.mjs";
+
+export default {
+  ...common,
+  key: "xata-create-record",
+  name: "Create Record",
+  description: "Create a new Record in the specified database. [See the documentation](https://xata.io/docs/api-reference/db/db_branch_name/tables/table_name/data#insert-record)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    recordData: {
+      propDefinition: [
+        common.props.app,
+        "recordData",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createRecord({
+      $,
+      endpoint: this.endpoint,
+      database: this.database,
+      branch: this.branch,
+      table: this.table,
+      data: await this.formatRecordData(),
+    });
+    $.export("$summary", `Successfully created Record with ID: '${response.id}'`);
+    return response;
+  },
+};
