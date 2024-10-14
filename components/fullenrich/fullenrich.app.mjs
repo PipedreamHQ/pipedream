fb808e1be1ce3ece530a43bb359ace51607d9450
Jorge Cortes <jacortesmahmud@gmail.com>
2024-10-14 09:55:36 -0500

    FullEnrich: new action components (#14282)

--- components/fullenrich/fullenrich.app.mjs
+++ components/fullenrich/fullenrich.app.mjs
@@ -1,11 +1,33 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "fullenrich",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `https://app.fullenrich.com/api/v1${path}`;
+    },
+    getHeaders(headers) {
+      return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.api_key}`,
+        ...headers,
+      };
+    },
+    _makeRequest({
+      $ = this, path, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        url: this.getUrl(path),
+        headers: this.getHeaders(headers),
+      });
+    },
+    post(args) {
+      return this._makeRequest({
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
