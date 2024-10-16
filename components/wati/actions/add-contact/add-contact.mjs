dd0bef486f24f83ef0778d159a8b9ab0229a2a7a
Luan Cazarine <luanhc@gmail.com>
2024-10-16 16:44:51 -0300

    New Components - wati (#14276)
    * wati init
    
    * [Components] wati #13213
    Actions
     - Add Contact
     - Send Template Message
     - Update Contact Attribute
    
    Source
     - New Contact Created
     - New Incoming Message
    
    * pnpm update
    
    * Update components/wati/wati.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * some adjusts
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/wati/actions/add-contact/add-contact.mjs
@@ -0,0 +1,53 @@
+import { ConfigurationError } from "@pipedream/platform";
+import wati from "../../wati.app.mjs";
+
+export default {
+  key: "wati-add-contact",
+  name: "Add Contact",
+  description: "Adds a new contact on the WATI platform. [See the documentation](https://docs.wati.io/reference/post_api-v1-addcontact-whatsappnumber)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    wati,
+    whatsappNumber: {
+      propDefinition: [
+        wati,
+        "whatsappNumber",
+      ],
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The name of the contact",
+      optional: true,
+    },
+    customParams: {
+      propDefinition: [
+        wati,
+        "customParams",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.wati.addContact({
+      $,
+      whatsappNumber: this.whatsappNumber,
+      data: {
+        name: this.name,
+        customParams: this.customParams && Object.entries(this.customParams).map(([
+          key,
+          value,
+        ]) => ({
+          name: key,
+          value,
+        })),
+      },
+    });
+    if (!response.result) {
+      throw new ConfigurationError(response.info);
+    }
+    $.export("$summary", `Successfully added contact with phone number: ${this.whatsappNumber}`);
+    return response;
+  },
+};
