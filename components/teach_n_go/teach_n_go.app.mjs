7432e034cfb3d20e0097048dc68c603599a82b53
danhsiung <35384182+danhsiung@users.noreply.github.com>
2024-10-15 19:33:02 -0700

    Adding app scaffolding for teach_n_go

---
+++ components/teach_n_go/teach_n_go.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "teach_n_go",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
