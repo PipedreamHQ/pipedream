8b83f210edaa3c5c870c52ad48e73aa0f6819688
michelle0927 <michellelbergero@hotmail.com>
2024-10-22 17:18:47 -0400

    typo

--- components/sms_fusion/actions/get-balance/get-balance.mjs
+++ components/sms_fusion/actions/get-balance/get-balance.mjs
@@ -3,7 +3,7 @@ import smsFusion from "../../sms_fusion.app.mjs";
 export default {
   key: "sms_fusion-get-balance",
   name: "Get Balance",
-  description: "Get current account balance including credit limts in SMS Fusion. [See the documentation](https://docs.smsfusion.com.au/)",
+  description: "Get current account balance including credit limits in SMS Fusion. [See the documentation](https://docs.smsfusion.com.au/)",
   version: "0.0.1",
   type: "action",
   props: {
