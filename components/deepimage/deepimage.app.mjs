b1e79590e9cae7817686d89f9e538ca76b2aff65
Luan Cazarine <luanhc@gmail.com>
2024-10-23 10:43:13 -0300

    New Components - deepimage (#14352)
    * deepimage init
    
    * [Components] deepimage #14347
    Actions
     - Auto Enhance
     - Remove Background
     - Upscale
    
    * pnpm update

--- components/deepimage/deepimage.app.mjs
+++ components/deepimage/deepimage.app.mjs
@@ -1,11 +1,34 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "deepimage",
-  propDefinitions: {},
+  propDefinitions: {
+    image: {
+      type: "string",
+      label: "Image",
+      description: "The URL of the image or the path to the file saved to the `/tmp` directory  (e.g. `/tmp/example.jpg`)  to process. [See the documentation](https://pipedream.com/docs/workflows/steps/code/nodejs/working-with-files/#the-tmp-directory).",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://deep-image.ai/rest_api/process_result";
+    },
+    _headers() {
+      return {
+        "content-type": "application/json",
+        "x-api-key": `${this.$auth.api_key}`,
+      };
+    },
+    makeRequest({
+      $ = this, ...opts
+    }) {
+      return axios($, {
+        method: "POST",
+        url: this._baseUrl(),
+        headers: this._headers(),
+        ...opts,
+      });
     },
   },
 };
