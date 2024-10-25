512432994866561c41c0de1c3437198830d3a01f
michelle0927 <michellelbergero@hotmail.com>
2024-10-25 13:13:31 -0400

    vector store actions

--- components/openai/actions/retrieve-run/retrieve-run.mjs
+++ components/openai/actions/retrieve-run/retrieve-run.mjs
@@ -4,7 +4,7 @@ export default {
   key: "openai-retrieve-run",
   name: "Retrieve Run (Assistants)",
   description: "Retrieves a specific run within a thread. [See the documentation](https://platform.openai.com/docs/api-reference/runs/getRun)",
-  version: "0.0.10",
+  version: "0.0.11",
   type: "action",
   props: {
     openai,
