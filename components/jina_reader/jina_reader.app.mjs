5abc2075c079e27cc01380319f375e0f151ad87f
Jorge Cortes <jacortesmahmud@gmail.com>
2024-10-29 21:22:10 -0500

    [Components] jina_reader: new action component (#14454)

--- components/jina_reader/jina_reader.app.mjs
+++ components/jina_reader/jina_reader.app.mjs
@@ -1,11 +1,33 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "jina_reader",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl() {
+      return "https://r.jina.ai/";
+    },
+    getHeaders(headers) {
+      return {
+        ...headers,
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this, headers, ...args
+    } = {}) {
+      return axios($, {
+        ...args,
+        url: this.getUrl(),
+        headers: this.getHeaders(headers),
+      });
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
