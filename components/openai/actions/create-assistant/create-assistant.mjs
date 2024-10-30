9ed1acb4c930f23e4f91f970b9090b4603f18540
michelle0927 <michelle0927@users.noreply.github.com>
2024-10-30 11:00:41 -0400

    OpenAI - Vector Store and Vector Store Files actions (#14435)
    * vector store actions
    
    * fixes

--- components/openai/actions/create-assistant/create-assistant.mjs
+++ components/openai/actions/create-assistant/create-assistant.mjs
@@ -6,7 +6,7 @@ export default {
   key: "openai-create-assistant",
   name: "Create Assistant",
   description: "Creates an assistant with a model and instructions. [See the documentation](https://platform.openai.com/docs/api-reference/assistants/createAssistant)",
-  version: "0.1.7",
+  version: "0.1.8",
   type: "action",
   props: {
     openai,
