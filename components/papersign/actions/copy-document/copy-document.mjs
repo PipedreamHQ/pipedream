6979126f13d291be3160c4191cc51a353748a6db
Luan Cazarine <luanhc@gmail.com>
2024-10-31 11:48:52 -0300

    New Components - papersign (#14423)
    * papersign init
    
    * [Components] papersign #14412
    Sources
     - New Event (Instant)
     - New Document Completed (Instant)
     - New Signer Signed (Instant)
    
    Actions
     - Copy Document
     - Get Document
     - Send Document
    
    * pnpm update
    
    * fix method call
    
    * remove optional true from scope

---
+++ components/papersign/actions/copy-document/copy-document.mjs
@@ -0,0 +1,65 @@
+import papersign from "../../papersign.app.mjs";
+
+export default {
+  key: "papersign-copy-document",
+  name: "Copy Document",
+  description: "Duplicates a given document. [See the documentation](https://paperform.readme.io/reference/papersigncopydocument)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    papersign,
+    documentId: {
+      propDefinition: [
+        papersign,
+        "documentId",
+      ],
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The new name of the copied document",
+      optional: true,
+    },
+    spaceId: {
+      propDefinition: [
+        papersign,
+        "spaceId",
+      ],
+      optional: true,
+    },
+    path: {
+      type: "string",
+      label: "Path",
+      description: "The path to copy the document to. Maximum depth is 4 levels. Any missing folders will be created.",
+      optional: true,
+    },
+    folderId: {
+      propDefinition: [
+        papersign,
+        "folderId",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const data = {};
+    if (this.folderId) {
+      data.folder_id = this.folderId;
+    } else {
+      data.space_id = this.spaceId;
+      data.path = this.path;
+    }
+
+    const response = await this.papersign.duplicateDocument({
+      $,
+      documentId: this.documentId,
+      data: {
+        ...data,
+        name: this.name,
+      },
+    });
+
+    $.export("$summary", `Successfully copied document: ${response.results.document.name}`);
+    return response;
+  },
+};
