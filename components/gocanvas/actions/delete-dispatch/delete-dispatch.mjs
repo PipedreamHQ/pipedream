5023061600f21b89488ad4f37dcacf712fdc2f40
michelle0927 <michellelbergero@hotmail.com>
2024-11-04 16:22:57 -0500

    new components

--- components/gocanvas/actions/delete-dispatch/delete-dispatch.mjs
+++ components/gocanvas/actions/delete-dispatch/delete-dispatch.mjs
@@ -4,14 +4,23 @@ export default {
   key: "gocanvas-delete-dispatch",
   name: "Delete Dispatch",
   description: "Removes a specific dispatch from GoCanvas. [See the documentation](https://help.gocanvas.com/hc/en-us/article_attachments/26468076609559)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
     gocanvas,
+    form: {
+      propDefinition: [
+        gocanvas,
+        "form",
+      ],
+    },
     dispatchId: {
       propDefinition: [
         gocanvas,
         "dispatchId",
+        (c) => ({
+          form: c.form,
+        }),
       ],
     },
   },
@@ -25,15 +34,8 @@ export default {
       data: `
         <?xml version="1.0" encoding="utf-8"?>
           <List>
-            <DI FormName="Daily Report" Action="Delete" OriginalDescription="${description}">
-              <DIEntry />
-              <DIListItems>
-                <DIListItem>
-                  <DIEntries>
-                    <DIEntry Label="Date" Value="${Date.now()}"/>
-                  </DIEntries>
-                </DIListItem>
-              </DIListItems>
+            <DI FormName="${this.form}" Action="Delete" OriginalDescription="${description}">
+              <DIEntry Label="Date" Value="${Date.now()}"/>
             </DI>
           </List>
       `,
--- components/gocanvas/gocanvas.app.mjs
+++ components/gocanvas/gocanvas.app.mjs
@@ -5,12 +5,24 @@ export default {
   type: "app",
   app: "gocanvas",
   propDefinitions: {
+    form: {
+      type: "string",
+      label: "Form",
+      description: "The identifier of a form",
+      async options() {
+        const forms = await this.listForms();
+        return forms?.map((form) => form.Name[0]) || [];
+      },
+    },
     dispatchId: {
       type: "string",
       label: "Dispatch ID",
       description: "Identifier of a dispatch",
-      async options({ page }) {
+      async options({
+        page, form,
+      }) {
         const dispatches = await this.getActiveDispatches({
+          form,
           data: {
             page: page + 1,
           },
@@ -47,22 +59,46 @@ export default {
         },
       });
     },
-    async getActiveDispatches(opts = {}) {
+    async getActiveDispatches({
+      form, ...opts
+    }) {
       const response = await this._makeRequest({
         path: "/dispatch_export",
         ...opts,
       });
       const { CanvasResult: { Dispatches: dispatches } } = await new xml2js
         .Parser().parseStringPromise(response);
-      const dispatchList = [];
-      for (const dispatch of dispatches) {
-        if (!dispatch.Dispatch) {
-          continue;
-        }
-        const activeDispatches = dispatch.Dispatch.filter((d) => d.Status[0] !== "deleted");
-        dispatchList.push(...activeDispatches);
+      if (!dispatches?.length) {
+        return [];
       }
-      return dispatchList;
+      return dispatches
+        .flatMap((d) => d.Dispatch || [])
+        .filter((d) => d.Status[0] !== "deleted")
+        .filter((d) => !form || d.Form[0] === form);
+    },
+    async listForms(opts = {}) {
+      const response = await this._makeRequest({
+        path: "/forms",
+        ...opts,
+      });
+      const { CanvasResult: { Forms: forms } } = await new xml2js
+        .Parser().parseStringPromise(response);
+      if (!forms?.length) {
+        return [];
+      }
+      return forms.flatMap((form) => form.Form || []);
+    },
+    async listSubmissions(opts = {}) {
+      const response = await this._makeRequest({
+        path: "/submissions",
+        ...opts,
+      });
+      const { CanvasResult: { Submissions: submissions } } = await new xml2js
+        .Parser().parseStringPromise(response);
+      if (!submissions?.length) {
+        return [];
+      }
+      return submissions.flatMap((sub) => sub.Submission || []);
     },
     async getDispatchDescription({
       dispatchId, ...opts
@@ -79,6 +115,37 @@ export default {
         path: "/dispatch_items",
         ...opts,
       });
+    },
+    createUpdateReferenceData(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/reference_datas",
+        ...opts,
+      });
+    },
+    async *paginate({
+      fn,
+      params,
+      max,
+    }) {
+      params = {
+        ...params,
+        page: 1,
+      };
+      let total, count = 0;
+      do {
+        const results = await fn({
+          params,
+        });
+        for (const item of results) {
+          yield item;
+          if (max && ++count >= max) {
+            return;
+          }
+        }
+        total = results?.length;
+        params.page++;
+      } while (total);
     },
   },
 };
