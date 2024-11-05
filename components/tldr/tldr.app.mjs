129369a28ccbe3d375bce3cba32c0450c533b664
Luan Cazarine <luanhc@gmail.com>
2024-11-05 10:23:27 -0300

    New Components - tldr (#14499)
    * tldr init
    
    * [Components] tldr #14496
    Actions
     - Summarize Text
    
    * pnpm update

--- components/tldr/tldr.app.mjs
+++ components/tldr/tldr.app.mjs
@@ -1,11 +1,33 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "tldr",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _headers() {
+      return {
+        "Authorization": `Bearer ${this.$auth.api_key}`,
+        "Content-Type": "application/json",
+      };
+    },
+    _baseUrl() {
+      return "https://runtldr.com/apis/v1";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    summarize(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/summarize",
+        ...opts,
+      });
     },
   },
 };
