c58aadb10b29cc0528c6aa0e6b7809d793122fb8
Luan Cazarine <luanhc@gmail.com>
2024-11-05 14:57:39 -0300

    New Components - crowdin (#14447)
    * crowdin init
    
    * [Components] crowdin #13242
    Sources
     - New Comment Issue (Instant)
     - File Approved (Instant)
     - New Directory
    
    Actions
     - Create Project
     - Upload File
     - Translate Via Machine Translation
    
    * pnpm update
    
    * some adjusts
    
    * pnpm update

---
+++ components/crowdin/sources/new-comment-issue-instant/test-event.mjs
@@ -0,0 +1,91 @@
+export default {
+  "event": "stringComment.created",
+  "comment": {
+    "id": "4",
+    "text": "text 01",
+    "type": "issue",
+    "issueType": "translation_mistake",
+    "issueStatus": "unresolved",
+    "resolvedAt": null,
+    "createdAt": "2024-10-30T17:06:06+00:00",
+    "string": {
+      "id": "2",
+      "identifier": "5ad6c6099c2bc7211eb8151483826e80",
+      "key": "NFDBB2FA9",
+      "text": "Text file",
+      "type": "text",
+      "context": "NFDBB2FA9",
+      "maxLength": "0",
+      "isHidden": false,
+      "isDuplicate": false,
+      "masterStringId": null,
+      "revision": "1",
+      "hasPlurals": false,
+      "labelIds": [],
+      "url": "https://crowdin.com/editor/project-01/14/en-ptbr#2",
+      "createdAt": "2024-10-28T14:18:30+00:00",
+      "updatedAt": null,
+      "file": {
+        "id": "14",
+        "name": "file.docx",
+        "title": null,
+        "type": "docx13",
+        "path": "/Folder 01/dummy.docx",
+        "status": "active",
+        "revision": "1",
+        "branchId": null,
+        "directoryId": "2"
+      },
+      "project": {
+        "id": "123123",
+        "userId": "123123",
+        "sourceLanguageId": "en",
+        "targetLanguageIds": [
+          "ru",
+          "pt-BR",
+          "yi",
+          "am",
+          "es-PR",
+          "es-NI",
+          "ar-YE"
+        ],
+        "identifier": "project-01",
+        "name": "Project 01",
+        "createdAt": "2024-10-28T13:54:03+00:00",
+        "updatedAt": "2024-10-30T16:40:17+00:00",
+        "lastActivity": "2024-10-30T17:03:02+00:00",
+        "description": "",
+        "url": "https://crowdin.com/project/project-01",
+        "cname": null,
+        "languageAccessPolicy": "open",
+        "visibility": "private",
+        "publicDownloads": true
+      }
+    },
+    "targetLanguage": {
+      "id": "pt-BR",
+      "name": "Portuguese, Brazilian",
+      "editorCode": "ptbr",
+      "twoLettersCode": "pt",
+      "threeLettersCode": "por",
+      "locale": "pt-BR",
+      "androidCode": "pt-rBR",
+      "osxCode": "pt-BR.lproj",
+      "osxLocale": "pt_BR",
+      "textDirection": "ltr",
+      "dialectOf": null
+    },
+    "user": {
+      "id": "16645627",
+      "username": "username",
+      "fullName": "username",
+      "avatarUrl": "https://crowdin-static.downloads.crowdin.com/avatar/123123/small/f1fb36bf9de3ded05b455e88ac9c77cf_default.png"
+    },
+    "commentResolver": {
+      "id": null,
+      "username": null,
+      "fullName": null,
+      "avatarUrl": null
+    }
+  }
+}
