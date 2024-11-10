ce78f3258a30545e2cb083b07816b86b9bfd447b
STalukder20 <shammotalukder4@gmail.com>
2024-11-09 22:12:25 -0500

    Spotify action component to retrieve currently playing track for user.

---
+++ components/spotify/actions/get-currently-playing-track/get-currently-playing-track.mjs
@@ -0,0 +1,37 @@
+import { axios } from "@pipedream/platform";
+import spotify from "../../spotify.app.mjs";
+
+export default {
+    name: "Get currently playing track",
+    description: "Get the object currently being played on the user's Spotify account.",
+    key: "spotify-get-currently-playing-track",
+    version: "0.0.2",
+    type: "action",
+    props: {
+        spotify,
+        market: {
+            propDefinition: [
+              spotify,
+              "market",
+            ],
+            optional: true,
+        },
+    },
+    async run({ $ }) {
+        const {
+            market,
+        } = this;
+
+    const res = await axios($, this.spotify._getAxiosParams({
+        method: "GET",
+        path: `/me/player/currently-playing`,
+        params: {
+            market,
+        },
+    }));
+
+    $.export("$summary", "Successfully retrieved currently playing track for user");
+
+    return res;
+  },
+}
