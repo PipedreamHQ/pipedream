8a7445c53f6c5b44c5c6574dfdd385c6021a8e4e
michelle0927 <michellelbergero@hotmail.com>
2024-11-13 18:03:28 -0500

    fix prop description

--- components/zerobounce/actions/file-validation/file-validation.mjs
+++ components/zerobounce/actions/file-validation/file-validation.mjs
@@ -60,7 +60,7 @@ export default {
     callbackWithRerun: {
       type: "boolean",
       label: "Callback With Rerun",
-      description: "Use the `$.flow.rerun` Node.js helper to rerun the step when the validation is completed. Overrides the `rerunUrl` prop. This will increase execution time and credit usage as a result. [See the documentation(https://pipedream.com/docs/code/nodejs/rerun/#flow-rerun)",
+      description: "Use the `$.flow.rerun` Node.js helper to rerun the step when the validation is completed. Overrides the `rerunUrl` prop. This will increase execution time and credit usage as a result. [See the documentation](https://pipedream.com/docs/code/nodejs/rerun/#flow-rerun)",
       optional: true,
     },
   },
