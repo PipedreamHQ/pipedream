b63891c79fc1d5f958a55bf153246f2103af57f0
Luan Cazarine <luanhc@gmail.com>
2024-11-13 12:31:03 -0300

    New Components - ignisign (#14577)
    * ignisign init
    
    * [Components] ignisign #14570
    Sources
     - New Signature Proof (Instant)
    
    Actions
     - Create Signer
     - Create Signature Request
     - Get Signature Proof
    
    * pnpm update
    
    * fix check component app prop step
    
    * pnpm update
    
    * some adjusts
    
    * Update components/ignisign/sources/new-signature-proof-instant/new-signature-proof-instant.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * some adjusts
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ components/ignisign/actions/create-signer/create-signer.mjs
@@ -0,0 +1,95 @@
+import ignisign from "../../ignisign.app.mjs";
+
+export default {
+  key: "ignisign-create-signer",
+  name: "Create Signer",
+  description: "Creates a new signer entity in IgniSign. [See the documentation](https://ignisign.io/docs/ignisign-api/create-signer)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ignisign,
+    signerProfileId: {
+      propDefinition: [
+        ignisign,
+        "signerProfileId",
+      ],
+      optional: true,
+    },
+    externalId: {
+      propDefinition: [
+        ignisign,
+        "externalId",
+      ],
+      optional: true,
+    },
+    firstName: {
+      propDefinition: [
+        ignisign,
+        "firstName",
+      ],
+      optional: true,
+    },
+    lastName: {
+      propDefinition: [
+        ignisign,
+        "lastName",
+      ],
+      optional: true,
+    },
+    email: {
+      propDefinition: [
+        ignisign,
+        "email",
+      ],
+    },
+    phoneNumber: {
+      propDefinition: [
+        ignisign,
+        "phoneNumber",
+      ],
+      optional: true,
+    },
+    nationality: {
+      propDefinition: [
+        ignisign,
+        "nationality",
+      ],
+      optional: true,
+    },
+    birthDate: {
+      propDefinition: [
+        ignisign,
+        "birthDate",
+      ],
+      optional: true,
+    },
+    birthPlace: {
+      propDefinition: [
+        ignisign,
+        "birthPlace",
+      ],
+      optional: true,
+    },
+    birthCountry: {
+      propDefinition: [
+        ignisign,
+        "birthCountry",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      ignisign,
+      ...data
+    } = this;
+
+    const response = await ignisign.createSigner({
+      $,
+      data,
+    });
+
+    $.export("$summary", `Successfully created signer with ID: ${response.signerId}`);
+    return response;
+  },
+};
