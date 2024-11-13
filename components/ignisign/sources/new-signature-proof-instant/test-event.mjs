70aca9e650227580729bb320bef9fa96a9383311
Luan Cazarine <luanhc@gmail.com>
2024-11-13 12:31:03 -0300

    New Components - ignisign (#14577)
    * ignisign init
    
    * [Components] ignisign #14570
    Sources
     - New Signature Proof (Instant)
    
    Actions
     - Create Signer
     - Create Signature Request
     - Get Signature Proof
    
    * pnpm update
    
    * fix check component app prop step
    
    * pnpm update
    
    * some adjusts
    
    * Update components/ignisign/sources/new-signature-proof-instant/new-signature-proof-instant.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * some adjusts
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ components/ignisign/sources/new-signature-proof-instant/test-event.mjs
@@ -0,0 +1,23 @@
+export default {
+  "appId": "appId_e5d6bcfb-e9f4-4858-8027-b65e1a170bd7",
+  "appEnv": "DEVELOPMENT",
+  "topic": "SIGNATURE_PROOF",
+  "action": "GENERATED",
+  "msgNature": "SUCCESS",
+  "content": {
+    "appEnv": "DEVELOPMENT",
+    "appId": "appId_e5d6bcfb-e9f4-4858-8027-b65e1a170bd7",
+    "signatureRequestId": "672d206d10e44e00125eac63",
+    "documents": [
+      {
+        "documentId": "672d206d9902ba0012e9a7f0",
+        "documentExternalId": "123123123",
+        "token": "5wvyV2d0Q0SapHS0PcdgrZgUbTHbUEVVq3uaZd8CcLIAA",
+        "url": "https://sign.ignisign.io/signature-requests/672d206d10e44e00125eac63/proofs?t=5wvyV2d0Q0SapHS0PcdgrZgUbTHbUEVVq3uaZd8CcLIAA"
+      }
+    ],
+    "signatureProofToken": "9CdzHRTIQFZZlGSBciSUmnLXahOl4J0dZqV3wwl9XzYIAA",
+    "signatureProofUrl": "https://sign.ignisign.io/signature-requests/672d206d10e44e00125eac63/proofs?t=9CdzHRTIQFZZlGSBciSUmnLXahOl4J0dZqV3wwl9XzYIAA"
+  },
+  "verificationToken": "N3gVBJB4TZZuE3MZSBeCqKo25ijHHFU2SvdiIMxm8znMAA"
+}
