428b6ee3207ebaf0eb1a9e59a46252ddfb4a1f73
michelle0927 <michellelbergero@hotmail.com>
2024-11-15 11:06:29 -0500

    new actions

--- components/slybroadcast/actions/start-campaign-audio-file/start-campaign-audio-file.mjs
+++ components/slybroadcast/actions/start-campaign-audio-file/start-campaign-audio-file.mjs
@@ -3,15 +3,15 @@ import slybroadcast from "../../slybroadcast.app.mjs";
 export default {
   key: "slybroadcast-start-campaign-audio-file",
   name: "Start Campaign with Audio File",
-  description: "Start a new voicemail campaign using an audio file uploaded to your slybroadcast account.",
-  version: "0.0.{{ts}}",
+  description: "Start a new voicemail campaign using an audio file uploaded to your slybroadcast account. [See the documentation](https://www.slybroadcast.com/documentation.php)",
+  version: "0.0.1",
   type: "action",
   props: {
     slybroadcast,
-    audioFileId: {
+    audioFile: {
       propDefinition: [
         slybroadcast,
-        "audioFileId",
+        "audioFile",
       ],
     },
     recipients: {
@@ -20,13 +20,23 @@ export default {
         "recipients",
       ],
     },
+    date: {
+      propDefinition: [
+        slybroadcast,
+        "date",
+      ],
+    },
   },
   async run({ $ }) {
-    const response = await this.slybroadcast.startCampaignWithFileId({
-      audioFileId: this.audioFileId,
-      recipients: this.recipients,
+    const response = await this.slybroadcast.startCampaign({
+      $,
+      data: {
+        c_record_audio: this.audioFile,
+        c_phone: this.recipients.join(),
+        c_date: this.date,
+      },
     });
-    $.export("$summary", `Started campaign with audio file ID: ${this.audioFileId}`);
+    $.export("$summary", `Started campaign with audio file ID: ${this.audioFile}`);
     return response;
   },
 };
--- components/slybroadcast/slybroadcast.app.mjs
+++ components/slybroadcast/slybroadcast.app.mjs
@@ -4,67 +4,66 @@ export default {
   type: "app",
   app: "slybroadcast",
   propDefinitions: {
-    audioFileId: {
+    audioFile: {
       type: "string",
-      label: "Audio File ID",
-      description: "The ID of the audio file uploaded to your slybroadcast account",
+      label: "Audio File",
+      description: "The audio file uploaded to your slybroadcast account",
+      async options() {
+        const files = await this.listAudioFiles();
+        const filenames = [
+          ...files.matchAll(/"\|"(.*?)"\|"/g),
+        ].map((match) => match[1]);
+        return filenames;
+      },
     },
     recipients: {
       type: "string[]",
       label: "Recipients",
-      description: "List of recipients for the voicemail campaign",
+      description: "Array of recipients' phone numbers for the voicemail campaign",
     },
-    audioFileUrl: {
+    date: {
       type: "string",
-      label: "Audio File URL",
-      description: "The URL of the audio file for the voicemail campaign",
+      label: "Date",
+      description: "Date/Time of delivery in Eastern Time. YYYY-MM-DD HH:MM:SS *Must use 24-hour time format. Example: 5:00pm = 17:00:00",
+      default: "now",
+      optional: true,
     },
   },
   methods: {
     _baseUrl() {
-      return "https://www.mobile-sphere.com/gateway/vmb.php";
+      return "https://www.mobile-sphere.com/gateway/";
     },
-    async _makeRequest(opts = {}) {
+    _makeRequest(opts = {}) {
       const {
-        $ = this, method = "POST", path, headers, ...otherOpts
-      } = opts;
+        $ = this, method = "POST", path, data, ...otherOpts
+      } = opts; console.log(data);
       return axios($, {
         ...otherOpts,
         method,
-        url: this._baseUrl() + path,
+        url: `${this._baseUrl()}${path}`,
+        data: {
+          ...data,
+          c_uid: `${this.$auth.email}`,
+          c_password: `${this.$auth.password}`,
+        },
         headers: {
-          ...headers,
           "Content-Type": "application/x-www-form-urlencoded",
         },
       });
     },
-    async startCampaignWithFileId({
-      audioFileId, recipients,
-    }) {
+    listAudioFiles(opts = {}) {
       return this._makeRequest({
+        path: "/vmb.aflist.php",
         data: {
-          c_uid: this.$auth.c_uid,
-          c_password: this.$auth.c_password,
-          c_callerID: this.$auth.c_callerID,
-          c_phone: recipients.join(","),
-          c_date: "now",
-          c_record_audio: audioFileId,
+          c_method: "get_audio_list",
         },
+        ...opts,
       });
     },
-    async startCampaignWithFileUrl({
-      audioFileUrl, recipients,
-    }) {
+    startCampaign(opts = {}) {
       return this._makeRequest({
-        data: {
-          c_uid: this.$auth.c_uid,
-          c_password: this.$auth.c_password,
-          c_callerID: this.$auth.c_callerID,
-          c_phone: recipients.join(","),
-          c_date: "now",
-          c_url: audioFileUrl,
-          c_audio: audioFileUrl.split(".").pop(),
-        },
+        path: "/vmb.php",
+        ...opts,
       });
     },
   },
