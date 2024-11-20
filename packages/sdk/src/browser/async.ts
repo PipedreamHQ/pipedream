f5bcfc9cc670b1c8e257962aa1a3461a737013df
Moshe Grunwald <34072688+TheBestMoshe@users.noreply.github.com>
2024-11-20 17:58:22 -0500

    feat(sdk-package): call apis directly from client sdk (#14699)
    Currently, the Pipedream Connect SDK only supports calling API endpoints from the server. This means that all client side calls need to be proxied by a trusted server.
    
    This PR adds support for calling most API endpoints directly from the client.

---
+++ packages/sdk/src/browser/async.ts
@@ -0,0 +1,24 @@
+import { AsyncResponseManager } from "../shared/async";
+import type { AsyncResponseManagerOpts } from "../shared/async";
+
+export type BrowserAsyncResponseManagerOpts = {
+  apiHost: string;
+  getConnectToken: () => Promise<string>;
+};
+
+export class BrowserAsyncResponseManager extends AsyncResponseManager {
+  private browserOpts: BrowserAsyncResponseManagerOpts;
+
+  constructor(opts: BrowserAsyncResponseManagerOpts) {
+    super();
+    this.browserOpts = opts;
+  }
+
+  protected override async getOpts(): Promise<AsyncResponseManagerOpts> {
+    const token = await this.browserOpts.getConnectToken();
+    const url = `wss://${this.browserOpts.apiHost}/websocket?ctok=${token}`;
+    return {
+      url,
+    };
+  }
+}
