595f344783e19734584ab7a1ed32d5ffbe745276
Lucas Caresia <lucascarezia@gmail.com>
2024-11-20 15:33:29 -0300

    Added actions

---
+++ components/microsoft_azure_ai_translator/actions/detect-language/detect-language.mjs
@@ -0,0 +1,31 @@
+import app from "../../microsoft_azure_ai_translator.app.mjs";
+
+export default {
+  key: "microsoft_azure_ai_translator-detect-language",
+  name: "Detect Language",
+  description: "Identifies the language of a piece of text. [See the documentation](https://learn.microsoft.com/en-us/azure/ai-services/translator/reference/v3-0-detect)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    text: {
+      propDefinition: [
+        app,
+        "text",
+      ],
+    },
+  },
+
+  async run({ $ }) {
+    const response = await this.app.detectLanguage({
+      $,
+      data: [
+        {
+          text: this.text,
+        },
+      ],
+    });
+    $.export("$summary", `Successfully detected language of the provided text: '${response.language}'s`);
+    return response;
+  },
+};
