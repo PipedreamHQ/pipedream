62f732f2a3df90464326acc92e55370e9bca2a20
michelle0927 <michellelbergero@hotmail.com>
2024-11-20 14:37:17 -0500

    new sources

---
+++ components/attio/sources/list-entry-updated-instant/test-event.mjs
@@ -0,0 +1,15 @@
+export default {
+  "event_type": "list-entry.updated",
+  "id": {
+    "workspace_id": "21e6dc8b-aa9f-4970-bdce-fa207cf3a7d9",
+    "list_id": "dd41a385-c7cd-4832-ad57-c9a824b32ba4",
+    "entry_id": "d7b73c9c-5b91-45df-bf3f-677b9bd563eb",
+    "attribute_id": "82b07502-9c9d-4996-a46f-4a5b687015d5"
+  },
+  "parent_object_id": "6a3d5e81-0de0-4e96-a535-957b28ba8211",
+  "parent_record_id": "6a3cce50-9589-4f94-be16-8aa0a80c46e1",
+  "actor": {
+    "type": "workspace-member",
+    "id": "3ccf848d-58e5-4ccb-88d7-d85944eda037"
+  }
+}
