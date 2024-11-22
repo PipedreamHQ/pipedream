d65068e2c29a22d2d39096a21bd07d670e006906
Luan Cazarine <luanhc@gmail.com>
2024-11-22 10:40:20 -0300

    New Components - cats (#14656)
    * cats init
    
    * [Components] cats #14489
    Sources
     - New Candidate (Instant)
     - New Contact (Instant)
     - New Activity (Instant)
    
    Actions
     - Create Candidate
     - Add Candidate Pipeline
     - Create Contact
    
    * pnpm update

---
+++ components/cats/sources/new-contact-instant/new-contact-instant.mjs
@@ -0,0 +1,29 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "cats-new-contact-instant",
+  name: "New Contact Created (Instant)",
+  description: "Emit new event when a contact related to a cat is created.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEventType() {
+      return [
+        "contact.created",
+      ];
+    },
+    generateMeta(body) {
+      const contact = body._embedded.contact;
+      return {
+        id: body.contact_id,
+        summary: `New contact: ${contact.first_name} ${contact.last_name}`,
+        ts: Date.parse(body.date || new Date()),
+      };
+    },
+  },
+  sampleEmit,
+};
