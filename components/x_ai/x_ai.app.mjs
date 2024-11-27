e86f52d4ef470ec13680f53850fb77486ca0cee8
Lucas Caresia <lucascarezia@gmail.com>
2024-11-27 00:50:39 -0300

    Merging pull request #14735
    * Added actions
    
    * Added actions
    
    * Added actions
    
    * Added actions

--- components/x_ai/x_ai.app.mjs
+++ components/x_ai/x_ai.app.mjs
@@ -1,11 +1,79 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "x_ai",
-  propDefinitions: {},
+  propDefinitions: {
+    model: {
+      type: "string",
+      label: "Model",
+      description: "Specifies the model to be used for the request",
+      async options() {
+        const response = await this.listModels();
+        const modelsIds = response.data;
+        return modelsIds.map(({ id }) => ({
+          value: id,
+        }));
+      },
+    },
+    prompt: {
+      type: "string",
+      label: "Prompt",
+      description: "Prompt for the request",
+    },
+    message: {
+      type: "string",
+      label: "Message",
+      description: "Message for the chat completion",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.x.ai";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.api_key}`,
+        },
+      });
+    },
+    async postChatCompletion(args = {}) {
+      return this._makeRequest({
+        path: "/v1/chat/completions",
+        method: "post",
+        ...args,
+      });
+    },
+    async postCompletion(args = {}) {
+      return this._makeRequest({
+        path: "/v1/completions",
+        method: "post",
+        ...args,
+      });
+    },
+    async getModel({
+      model, ...args
+    }) {
+      return this._makeRequest({
+        path: `/v1/models/${model}`,
+        ...args,
+      });
+    },
+    async listModels(args = {}) {
+      return this._makeRequest({
+        path: "/v1/models",
+        ...args,
+      });
     },
   },
 };
