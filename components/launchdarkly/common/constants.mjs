f7669f19fa3c45a1977c6c5917fc9c265433526f
Jorge Cortes <jacortesmahmud@gmail.com>
2024-11-28 13:03:46 -0500

    [Components] launchdarkly - new components (#14742)

---
+++ components/launchdarkly/common/constants.mjs
@@ -0,0 +1,11 @@
+const BASE_URL = "https://app.launchdarkly.com";
+const VERSION_PATH = "/api/v2";
+const WEBHOOK_ID = "webhookId";
+const SECRET = "secret";
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  WEBHOOK_ID,
+  SECRET,
+};
