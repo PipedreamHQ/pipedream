c05d062c3cb90189ac893596fb0b88b78b41b232
GTFalcao <gtfalcao96@gmail.com>
2024-11-29 00:27:21 -0300

    New form submission

--- components/webflow_v2/sources/new-form-submission/new-form-submission.mjs
+++ components/webflow_v2/sources/new-form-submission/new-form-submission.mjs
@@ -5,7 +5,7 @@ export default {
   type: "source",
   key: "webflow_v2-new-form-submission",
   name: "New Form Submission",
-  description: "Emit new event when a new form is submitted. [See the docs here](https://developers.webflow.com/#trigger-types)",
+  description: "Emit new event when a form is submitted. [See the docs here](https://developers.webflow.com/#trigger-types)",
   version: "0.0.{{ts}}",
   ...common,
   methods: {
@@ -14,10 +14,11 @@ export default {
       return "form_submission";
     },
     generateMeta(data) {
+      const { name, id, submittedAt } = data;
       return {
-        id: data._id,
-        summary: `New form ${data._id} submission`,
-        ts: Date.parse(data.date),
+        id,
+        summary: `Form submitted: ${name ?? id}`,
+        ts: Date.parse(submittedAt),
       };
     },
   },
