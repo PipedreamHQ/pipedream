0255315490a34c2fa96835835763f5c92620a011
Jorge Cortes <jacortesmahmud@gmail.com>
2024-11-29 17:04:36 -0500

    [Components] attractwell - New components

---
+++ components/attractwell/actions/lesson-approval/lesson-approval.mjs
@@ -0,0 +1,81 @@
+import app from "../../attractwell.app.mjs";
+
+export default {
+  key: "attractwell-lesson-approval",
+  name: "Lesson Approval",
+  description: "Approves, rejects, or unapproves a lesson in the AttractWell system based on the selected status.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    approvalStatus: {
+      type: "string",
+      label: "Approval Status",
+      description: "The approval status to set for the lesson.",
+      options: [
+        "approved",
+        "rejected",
+        "unapproved",
+      ],
+    },
+    contactEmail: {
+      type: "string",
+      label: "Contact Email",
+      description: "The email address of the contact.",
+    },
+    vaultId: {
+      optional: false,
+      propDefinition: [
+        app,
+        "vaultId",
+      ],
+    },
+    onlineClassLessonId: {
+      label: "Online Class Lesson ID",
+      description: "The ID of the online class lesson.",
+      optional: false,
+      propDefinition: [
+        app,
+        "lessonId",
+      ],
+    },
+    instructorComment: {
+      type: "string",
+      label: "Instructor Comment",
+      description: "The comment from the instructor.",
+      optional: true,
+    },
+  },
+  methods: {
+    lessonApproval(args = {}) {
+      return this.app.post({
+        path: "/classes/lessons/approve",
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      lessonApproval,
+      approvalStatus,
+      contactEmail,
+      vaultId,
+      onlineClassLessonId,
+      instructorComment,
+    } = this;
+
+    const response = await lessonApproval({
+      $,
+      data: {
+        approval_status: approvalStatus,
+        contact_email: contactEmail,
+        vault_id: vaultId,
+        online_class_lesson_id: onlineClassLessonId,
+        instructor_comment: instructorComment,
+      },
+    });
+
+    $.export("$summary", "Successfully updated lesson approval status.");
+    return response;
+  },
+};
---
+++ components/attractwell/sources/new-lead-from-landing-page-instant/new-lead-from-landing-page-instant.mjs
@@ -0,0 +1,30 @@
+import common from "../common/webhook.mjs";
+import triggerNames from "../common/trigger-names.mjs";
+import triggerContexts from "../common/trigger-contexts.mjs";
+
+export default {
+  ...common,
+  key: "attractwell-new-lead-from-landing-page-instant",
+  name: "New Lead from Landing Page (Instant)",
+  description: "Emit new event when a lead is gained from a landing page.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getTriggerName() {
+      return triggerNames.NEW_LEAD_OR_SALE;
+    },
+    getTriggerContext() {
+      return triggerContexts.LANDING_PAGE;
+    },
+    generateMeta(resource) {
+      const ts = Date.now();
+      return {
+        id: ts,
+        summary: `New Lead: ${resource.lead_source_id}`,
+        ts,
+      };
+    },
+  },
+};
