b4b1df6353540d6399debc6cb596032b3f3d9bc5
Lucas Caresia <lucascarezia@gmail.com>
2024-11-29 15:10:02 -0300

    Added actions

---
+++ components/apiary/actions/publish-blueprint/publish-blueprint.mjs
@@ -0,0 +1,29 @@
+import app from "../../apiary.app.mjs";
+
+export default {
+  key: "apiary-publish-blueprint",
+  name: "Publish Blueprint",
+  description: "Publish an API Blueprint for a particular API. [See the documentation](https://apiary.docs.apiary.io/#reference/blueprint/publish-blueprint/publish-blueprint)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    apiSubdomain: {
+      propDefinition: [
+        app,
+        "apiSubdomain",
+      ],
+    },
+  },
+
+  async run({ $ }) {
+    const response = await this.app.publishBlueprint({
+      $,
+      apiSubdomain: this.apiSubdomain,
+    });
+
+    $.export("$summary", `Successfully published the blueprint with the subdomain '${this.apisSubdomain}'`);
+
+    return response;
+  },
+};
--- components/apiary/apiary.app.mjs
+++ components/apiary/apiary.app.mjs
@@ -1,11 +1,95 @@
+import { axios } from "@pipedream/platform";
+import contants from "./common/contants.mjs";
+
 export default {
   type: "app",
   app: "apiary",
-  propDefinitions: {},
+  propDefinitions: {
+    apiSubdomain: {
+      type: "string",
+      label: "API Subdomain",
+      description: "Subdomain of the API",
+      async options() {
+        const response = await this.listApis();
+        const apisSubdomains = response.apis;
+        return apisSubdomains.map(({
+          apiSubdomain, apiName,
+        }) => ({
+          value: apiSubdomain,
+          label: apiName,
+        }));
+      },
+    },
+    type: {
+      type: "string",
+      label: "Type of the API",
+      description: "Name of the organization",
+      options: contants.API_TYPES,
+    },
+    public: {
+      type: "boolean",
+      label: "Public",
+      description: "Defines if the API is public or private",
+    },
+    desiredName: {
+      type: "string",
+      label: "Desired Name",
+      description: "If the desiredName is already taken, a different domain will be generated for your API Project. It can be later changed in the settings",
+    },
+    code: {
+      type: "string",
+      label: "Code",
+      description: "`FORMAT: 1`",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.apiary.io";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${this.$auth.token}`,
+        },
+      });
+    },
+    async createApiProject(args = {}) {
+      return this._makeRequest({
+        path: "/blueprint/create",
+        method: "post",
+        ...args,
+      });
+    },
+    async fetchBlueprint({
+      apiSubdomain, ...args
+    }) {
+      return this._makeRequest({
+        path: `/blueprint/get/${apiSubdomain}`,
+        ...args,
+      });
+    },
+    async publishBlueprint({
+      apiSubdomain, ...args
+    }) {
+      return this._makeRequest({
+        path: `/blueprint/get/${apiSubdomain}`,
+        ...args,
+      });
+    },
+    async listApis(args = {}) {
+      return this._makeRequest({
+        path: "/me/apis",
+        ...args,
+      });
     },
   },
 };
