599ad536e402af2a36425cfbd7578bef012518bc
michelle0927 <michellelbergero@hotmail.com>
2024-12-05 14:15:48 -0500

    versions

--- components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
+++ components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
@@ -7,7 +7,7 @@ export default {
   key: "hubspot-new-deal-in-stage",
   name: "New Deal In Stage",
   description: "Emit new event for each new deal in a stage.",
-  version: "0.0.19",
+  version: "0.0.20",
   dedupe: "unique",
   type: "source",
   props: {
--- components/hubspot/sources/new-or-updated-blog-article/new-or-updated-blog-article.mjs
+++ components/hubspot/sources/new-or-updated-blog-article/new-or-updated-blog-article.mjs
@@ -7,7 +7,7 @@ export default {
   key: "hubspot-new-or-updated-blog-article",
   name: "New or Updated Blog Post",
   description: "Emit new event for each new or updated blog post in Hubspot.",
-  version: "0.0.2",
+  version: "0.0.3",
   dedupe: "unique",
   type: "source",
   props: {
