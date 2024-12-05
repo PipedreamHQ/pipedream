d110209f22bc959a73b7fa87edd59ea61f7661c9
Luan Cazarine <luanhc@gmail.com>
2024-12-05 15:54:35 -0300

    14645 components zoho books (#14764)
    * [Components] zoho-books #14645
    - Component refactoring
    
    Sources
     - New Customer
     - New Expense
     - New Sales Invoice
    
    Actions
     - Create Customer
     - Create Estimate
    
    * pnpm update
    
    * some adjusts
    
    * some adjusts
    
    * Improve description
    
    ---------
    
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>

--- components/zoho_books/actions/delete-contact/delete-contact.mjs
+++ components/zoho_books/actions/delete-contact/delete-contact.mjs
@@ -1,39 +1,29 @@
 // legacy_hash_id: a_Lgiern
-import { axios } from "@pipedream/platform";
+import zohoBooks from "../../zoho_books.app.mjs";
 
 export default {
   key: "zoho_books-delete-contact",
   name: "Delete Contact",
-  description: "Deletes an existing contact.",
-  version: "0.2.1",
+  description: "Deletes an existing contact. [See the documentation](https://www.zoho.com/books/api/v3/contacts/#delete-a-contact)",
+  version: "0.3.0",
   type: "action",
   props: {
-    zoho_books: {
-      type: "app",
-      app: "zoho_books",
-    },
-    organization_id: {
-      type: "string",
-      description: "In Zoho Books, your business is termed as an organization. If you have multiple businesses, you simply set each of those up as an individual organization. Each organization is an independent Zoho Books Organization with it's own organization ID, base currency, time zone, language, contacts, reports, etc.\n\nThe parameter `organization_id` should be sent in with every API request to identify the organization.\n\nThe `organization_id` can be obtained from the GET `/organizations` API's JSON response. Alternatively, it can be obtained from the **Manage Organizations** page in the admin console.",
-    },
-    contact_id: {
-      type: "string",
-      description: "ID of the contact to delete.",
+    zohoBooks,
+    customerId: {
+      propDefinition: [
+        zohoBooks,
+        "customerId",
+      ],
+      description: "The Id of the contact which will be deleted.",
     },
   },
   async run({ $ }) {
-  // See the API docs: https://www.zoho.com/books/api/v3/#Contacts_Delete_a_Contact
-
-    if (!this.organization_id || !this.contact_id) {
-      throw new Error("Must provide organization_id, and contact_id parameters.");
-    }
-
-    return await axios($, {
-      method: "delete",
-      url: `https://books.${this.zoho_books.$auth.base_api_uri}/api/v3/contacts/${this.contact_id}?organization_id=${this.organization_id}`,
-      headers: {
-        Authorization: `Zoho-oauthtoken ${this.zoho_books.$auth.oauth_access_token}`,
-      },
+    const response = await this.zohoBooks.deleteContact({
+      $,
+      customerId: this.customerId,
     });
+
+    $.export("$summary", `Contact successfully deleted with Id: ${this.customerId}`);
+    return response;
   },
 };
