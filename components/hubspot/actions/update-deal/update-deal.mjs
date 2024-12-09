c64e31020fe8dc76f2176e0b2bc3c65bf4e24404
michelle0927 <michellelbergero@hotmail.com>
2024-12-09 11:19:50 -0500

    update-deal

---
+++ components/hubspot/actions/update-deal/update-deal.mjs
@@ -0,0 +1,31 @@
+import { OBJECT_TYPE } from "../../common/constants.mjs";
+import common from "../common/common-update-object.mjs";
+import hubspot from "../../hubspot.app.mjs";
+
+export default {
+  ...common,
+  key: "hubspot-update-deal",
+  name: "Update Deal",
+  description: "Update a deal in Hubspot. [See the documentation](https://developers.hubspot.com/beta-docs/guides/api/crm/objects/deals#update-deals)",
+  version: "0.0.1",
+  type: "action",
+  methods: {
+    ...common.methods,
+    getObjectType() {
+      return OBJECT_TYPE.DEAL;
+    },
+  },
+  props: {
+    hubspot,
+    objectId: {
+      propDefinition: [
+        hubspot,
+        "objectId",
+        () => ({
+          objectType: "deal",
+        }),
+      ],
+    },
+    ...common.props,
+  },
+};
