e313fb68d3c10a5a74f20c8b0ee9f1c2776dcce5
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2024-12-11 13:30:39 -0300

    Webflow v2 migration 2024 12 11 (#14841)
    * Re-applying webflow migration
    
    * Version updates
    
    * pnpm-lock

--- components/webflow/actions/create-collection-item/create-collection-item.mjs
+++ components/webflow/actions/create-collection-item/create-collection-item.mjs
@@ -1,22 +1,22 @@
-import webflow from "../../webflow.app.mjs";
+import app from "../../webflow.app.mjs";
 
 export default {
   key: "webflow-create-collection-item",
   name: "Create Collection Item",
-  description: "Create new collection item. [See the docs here](https://developers.webflow.com/#create-new-collection-item)",
-  version: "1.0.1",
+  description: "Create new collection item. [See the documentation](https://developers.webflow.com/data/reference/cms/collection-items/staged-items/create-item)",
+  version: "2.0.0",
   type: "action",
   props: {
-    webflow,
+    app,
     siteId: {
       propDefinition: [
-        webflow,
+        app,
         "sites",
       ],
     },
     collectionId: {
       propDefinition: [
-        webflow,
+        app,
         "collections",
         (c) => ({
           siteId: c.siteId,
@@ -24,21 +24,15 @@ export default {
       ],
       reloadProps: true,
     },
-    live: {
-      label: "Live",
-      description: "Indicate if the item should be published to the live site",
-      type: "boolean",
-      default: false,
-    },
   },
   async additionalProps() {
     const props = {};
     if (!this.collectionId) {
       return props;
     }
-    const { fields } = await this.webflow.getCollection(this.collectionId);
+    const { fields } = await this.app.getCollection(this.collectionId);
     for (const field of fields) {
-      if (field.editable && field.slug !== "_archived" && field.slug !== "_draft") {
+      if (field.isEditable && field.slug !== "isArchived" && field.slug !== "isDraft") {
         props[field.slug] = {
           type: "string",
           label: field.name,
@@ -47,7 +41,7 @@ export default {
             : field.slug === "slug"
               ? "URL structure of the Item in your site."
               : "See the documentation for additional information about [Field Types & Item Values](https://developers.webflow.com/reference/field-types-item-values).",
-          optional: !field.required,
+          optional: !field.isRequired,
         };
       }
     }
@@ -55,29 +49,23 @@ export default {
   },
   async run({ $ }) {
     const {
-      webflow,
+      app,
       // eslint-disable-next-line no-unused-vars
       siteId,
-      // eslint-disable-next-line no-unused-vars
       collectionId,
-      live,
-      ...fields
+      ...fieldData
     } = this;
 
-    const webflowClient = webflow._createApiClient();
-
-    const response = await webflowClient.createItem({
-      collectionId: this.collectionId,
-      fields: {
-        ...fields,
-        _archived: false,
-        _draft: false,
+    const response = await app.createCollectionItem(
+      collectionId,
+      {
+        fieldData,
+        isArchived: false,
+        isDraft: false,
       },
-    }, {
-      live,
-    });
+    );
 
-    $.export("$summary", `Successfully created collection item ${fields.name}`);
+    $.export("$summary", `Successfully created collection item ${this.name ?? ""}`);
 
     return response;
   },
