c89498ab4a2431e04ed62840760745db57962111
michelle0927 <michellelbergero@hotmail.com>
2024-12-18 13:57:42 -0500

    new components

--- components/circleci/circleci.app.mjs
+++ components/circleci/circleci.app.mjs
@@ -3,179 +3,208 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "circleci",
-  version: "0.0.{{ts}}",
   propDefinitions: {
-    // Required for creating a new item
-    title: {
+    pipelineId: {
       type: "string",
-      label: "Title",
-      description: "The title of the item",
+      label: "Pipeline ID",
+      description: "The identifier of a pipeline",
+      async options({
+        projectSlug, prevContext,
+      }) {
+        const {
+          items, nextPageToken,
+        } = await this.listPipelines({
+          projectSlug,
+          params: prevContext?.next
+            ? {
+              "page-token": prevContext.next,
+            }
+            : {},
+        });
+        return {
+          options: items?.map(({ id }) => id) || [],
+          context: {
+            next: nextPageToken,
+          },
+        };
+      },
     },
-    content: {
+    workflowId: {
       type: "string",
-      label: "Content",
-      description: "The content of the item",
+      label: "Workflow ID",
+      description: "The identifier of a workflow",
+      async options({
+        pipelineId, prevContext,
+      }) {
+        const {
+          items, nextPageToken,
+        } = await this.listPipelineWorkflows({
+          pipelineId,
+          params: prevContext?.next
+            ? {
+              "page-token": prevContext.next,
+            }
+            : {},
+        });
+        return {
+          options: items?.map(({
+            id: value, name: label,
+          }) => ({
+            value,
+            label,
+          })) || [],
+          context: {
+            next: nextPageToken,
+          },
+        };
+      },
     },
-    // Required for updating an existing item
-    itemId: {
+    jobNumber: {
       type: "string",
-      label: "Item ID",
-      description: "The unique identifier of the item",
+      label: "Job Number",
+      description: "The job number of a job",
+      async options({
+        workflowId, prevContext,
+      }) {
+        const {
+          items, nextPageToken,
+        } = await this.listWorkflowJobs({
+          workflowId,
+          params: prevContext?.next
+            ? {
+              "page-token": prevContext.next,
+            }
+            : {},
+        });
+        return {
+          options: items?.map(({
+            job_number: value, name: label,
+          }) => ({
+            value,
+            label,
+          })) || [],
+          context: {
+            next: nextPageToken,
+          },
+        };
+      },
     },
-    // Required for adding a comment
-    commentContent: {
-      type: "string",
-      label: "Comment Content",
-      description: "The content of the comment",
-    },
-    // Optional for creating a new item
-    metadata: {
-      type: "string",
-      label: "Metadata",
-      description: "Optional metadata as JSON string",
+    jobIds: {
+      type: "string[]",
+      label: "Jobs",
+      description: "A list of job IDs to rerun",
       optional: true,
+      async options({
+        workflowId, prevContext,
+      }) {
+        const {
+          items, nextPageToken,
+        } = await this.listWorkflowJobs({
+          workflowId,
+          params: prevContext?.next
+            ? {
+              "page-token": prevContext.next,
+            }
+            : {},
+        });
+        return {
+          options: items?.map(({
+            id: value, name: label,
+          }) => ({
+            value,
+            label,
+          })) || [],
+          context: {
+            next: nextPageToken,
+          },
+        };
+      },
     },
-    // Optional for updating an existing item
-    updateFields: {
+    projectSlug: {
       type: "string",
-      label: "Fields to Update",
-      description: "Fields to update in the item as JSON string",
-      optional: true,
-    },
-    // Optional for adding a comment to an existing item
-    userDetails: {
-      type: "string",
-      label: "User Details",
-      description: "Optional user details as JSON string",
-      optional: true,
-    },
-    // Optional filters for new item creation events
-    newItemType: {
-      type: "string",
-      label: "Item Type",
-      description: "Filter by item type",
-      optional: true,
-    },
-    newItemStatus: {
-      type: "string",
-      label: "Status",
-      description: "Filter by status",
-      optional: true,
-    },
-    // Optional filters for item update events
-    updatedItemFields: {
-      type: "string",
-      label: "Updated Fields",
-      description: "Filter by updated fields",
-      optional: true,
-    },
-    updatedItemType: {
-      type: "string",
-      label: "Item Type",
-      description: "Filter by item type",
-      optional: true,
-    },
-    // Optional filters for comment addition events
-    commentItemType: {
-      type: "string",
-      label: "Item Type",
-      description: "Filter by item type",
-      optional: true,
-    },
-    commentUser: {
-      type: "string",
-      label: "User",
-      description: "Filter by user",
-      optional: true,
+      label: "Project Slug",
+      description: "Project slug in the form `vcs-slug/org-name/repo-name` (found in Project Settings)",
     },
   },
   methods: {
-    // This method logs the authentication keys
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
-    // Base URL for CircleCI API
     _baseUrl() {
       return "https://circleci.com/api/v2";
     },
-    // Common method to make API requests
     async _makeRequest(opts = {}) {
       const {
-        $ = this, method = "GET", path = "/", headers, ...otherOpts
+        $ = this,
+        path,
+        ...otherOpts
       } = opts;
       return axios($, {
-        method,
-        url: this._baseUrl() + path,
+        url: `${this._baseUrl()}${path}`,
         headers: {
-          ...headers,
-          Authorization: `Bearer ${this.$auth.api_key}`,
+          "Circle-Token": this.$auth.token,
         },
         ...otherOpts,
       });
     },
-    // Create a new item
-    async createItem(opts = {}) {
-      const {
-        title, content, metadata, ...otherOpts
-      } = opts;
-      const data = {
-        title,
-        content,
-      };
-      if (metadata) {
-        try {
-          data.metadata = JSON.parse(metadata);
-        } catch (error) {
-          throw new Error("Invalid JSON for metadata");
-        }
-      }
+    createWebhook(opts = {}) {
       return this._makeRequest({
         method: "POST",
-        path: "/items",
-        data,
-        ...otherOpts,
+        path: "/webhook",
+        ...opts,
       });
     },
-    // Update an existing item
-    async updateItem(opts = {}) {
-      const {
-        itemId, updateFields, ...otherOpts
-      } = opts;
-      const data = {};
-      if (updateFields) {
-        try {
-          Object.assign(data, JSON.parse(updateFields));
-        } catch (error) {
-          throw new Error("Invalid JSON for updateFields");
-        }
-      }
+    deleteWebhook(webhookId) {
       return this._makeRequest({
-        method: "PUT",
-        path: `/items/${itemId}`,
-        data,
-        ...otherOpts,
+        method: "DELETE",
+        path: `/webhook/${webhookId}`,
       });
     },
-    // Add a comment to an existing item
-    async addComment(opts = {}) {
-      const {
-        itemId, commentContent, userDetails, ...otherOpts
-      } = opts;
-      const data = {
-        content: commentContent,
-      };
-      if (userDetails) {
-        try {
-          data.user = JSON.parse(userDetails);
-        } catch (error) {
-          throw new Error("Invalid JSON for userDetails");
-        }
-      }
+    listPipelines({
+      projectSlug, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/project/${projectSlug}/pipeline/mine`,
+        ...opts,
+      });
+    },
+    listPipelineWorkflows({
+      pipelineId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/pipeline/${pipelineId}/workflow`,
+        ...opts,
+      });
+    },
+    listWorkflowJobs({
+      workflowId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/workflow/${workflowId}/job`,
+        ...opts,
+      });
+    },
+    triggerPipeline({
+      projectSlug, ...opts
+    }) {
       return this._makeRequest({
         method: "POST",
-        path: `/items/${itemId}/comments`,
-        data,
-        ...otherOpts,
+        path: `/project/${projectSlug}/pipeline/run`,
+        ...opts,
+      });
+    },
+    getJobArtifacts({
+      projectSlug, jobNumber, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/project/${projectSlug}/${jobNumber}/artifacts`,
+        ...opts,
+      });
+    },
+    rerunWorkflow({
+      workflowId, ...opts
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/workflow/${workflowId}/rerun`,
+        ...opts,
       });
     },
   },
