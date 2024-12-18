28249a07fe90b7ffc7dc84513b54911285fbcde6
Lucas Caresia <lucascarezia@gmail.com>
2024-12-18 11:00:32 -0300

    Added requested changes

---
+++ components/bigdatacorp/actions/get-address-data/get-address-data.mjs
@@ -0,0 +1,40 @@
+import app from "../../bigdatacorp.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "bigdatacorp-get-address-data",
+  name: "Get Address Data",
+  description: "Returns the available data for a CEP number according to the selected dataset. [See the documentation](https://docs.bigdatacorp.com.br/plataforma/reference/enderecos_legal_amazon)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    doc: {
+      propDefinition: [
+        app,
+        "doc",
+      ],
+    },
+    dataset: {
+      propDefinition: [
+        app,
+        "dataset",
+      ],
+      options: constants.ADDRESS_DATASETS,
+    },
+  },
+
+  async run({ $ }) {
+    const response = await this.app.getAddressData({
+      $,
+      data: {
+        Datasets: this.dataset,
+        q: `zipcode{${this.doc}}`,
+      },
+    });
+
+    $.export("$summary", `Successfully sent the request for the '${this.dataset}' dataset`);
+
+    return response;
+  },
+};
---
+++ components/bigdatacorp/actions/get-company-data/get-company-data.mjs
@@ -0,0 +1,40 @@
+import app from "../../bigdatacorp.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "bigdatacorp-get-company-data",
+  name: "Get Company Data",
+  description: "Returns the available data for a CNPJ number according to the selected dataset. [See the documentation](https://docs.bigdatacorp.com.br/plataforma/reference/empresas_emails_extended)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    doc: {
+      propDefinition: [
+        app,
+        "doc",
+      ],
+    },
+    dataset: {
+      propDefinition: [
+        app,
+        "dataset",
+      ],
+      options: constants.COMPANY_DATASETS,
+    },
+  },
+
+  async run({ $ }) {
+    const response = await this.app.getCompanyData({
+      $,
+      data: {
+        Datasets: this.dataset,
+        q: `doc{${this.doc}}`,
+      },
+    });
+
+    $.export("$summary", `Successfully sent the request for the '${this.dataset}' dataset`);
+
+    return response;
+  },
+};
