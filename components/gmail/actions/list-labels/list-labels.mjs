96cdc593a30b62eeab7f153995edfe9adeef1d8d
Danny Roosevelt <danny@pipedream.com>
2024-12-23 14:16:56 -0800

    Adding Gmail List Labels action

---
+++ components/gmail/actions/list-labels/list-labels.mjs
@@ -0,0 +1,17 @@
+import gmail from "../../gmail.app.mjs";
+
+export default {
+  key: "gmail-list-labels",
+  name: "List Labels",
+  description: "List all the existing labels in the connected account. [See the docs](https://developers.google.com/gmail/api/reference/rest/v1/users.labels/list)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gmail,
+  },
+  async run({ $ }) {
+    const resp = await this.gmail.listLabels();
+    $.export("$summary", `Successfully retrieved ${resp.labels.length} labels`);
+    return resp;
+  },
+};
