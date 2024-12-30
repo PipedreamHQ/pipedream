22be9ab5ec2c56d4d62140e2ed779ca8d1aa55d6
Luan Cazarine <luanhc@gmail.com>
2024-12-30 14:47:51 -0300

    New Components - boldsign (#15013)
    * boldsign init
    
    * [Components] boldsign #14976
    Sources
     - New Document Completed
     - New Document Sent
     - New Document Declined
    
    Actions
     - Send Document Template
    
    * pnpm update
    
    * fix import
    
    * some adjusts
    
    * some adjusts
    
    * pnpm update
    
    * pnpm update
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/boldsign/sources/new-document-completed/new-document-completed.mjs
@@ -0,0 +1,27 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "boldsign-new-document-completed",
+  name: "New Document Completed",
+  description: "Emit new event when a document is completed by all the signers.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getFunction() {
+      return this.boldsign.listDocuments;
+    },
+    getSummary(item) {
+      return `Document Completed: ${item.documentId}`;
+    },
+    getParams() {
+      return {
+        status: "Completed",
+      };
+    },
+  },
+  sampleEmit,
+};
