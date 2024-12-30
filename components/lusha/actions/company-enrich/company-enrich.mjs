30a9f2c77ee4854482f526c311ac443c7c079426
Luan Cazarine <luanhc@gmail.com>
2024-12-30 10:45:05 -0300

    New Components - lusha (#14994)
    * lusha init
    
    * [ACTION] Lusha: Prospecting APIs #14992
    Actions
     - Contact Search
     - Company Search
     - Contact Enrich
     - Company Enrich
    
    * pnpm update
    
    * new release
    
    * remove node modules
    
    * some adjusts

---
+++ components/lusha/actions/company-enrich/company-enrich.mjs
@@ -0,0 +1,37 @@
+import lusha from "../../lusha.app.mjs";
+
+export default {
+  key: "lusha-company-enrich",
+  name: "Enrich Companies",
+  description: "Enriches company information based on provided company IDs. [See the documentation](https://www.lusha.com/docs/#company-enrich)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    lusha,
+    requestId: {
+      propDefinition: [
+        lusha,
+        "requestId",
+      ],
+      label: "Company Request ID",
+      description: "The request ID generated from the company search response.",
+    },
+    companiesIds: {
+      propDefinition: [
+        lusha,
+        "companiesIds",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.lusha.enrichCompanies({
+      $,
+      data: {
+        requestId: this.requestId,
+        companiesIds: this.companiesIds,
+      },
+    });
+    $.export("$summary", `Successfully enriched ${this.companiesIds.length} companies`);
+    return response;
+  },
+};
