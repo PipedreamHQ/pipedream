2aca239e443b9a099426a8acdf4a126121db969b
michelle0927 <michellelbergero@hotmail.com>
2024-12-30 14:13:39 -0500

    new component

--- components/change_photos/change_photos.app.mjs
+++ components/change_photos/change_photos.app.mjs
@@ -3,89 +3,31 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "change_photos",
-  version: "0.0.20240427",
-  propDefinitions: {
-    imageUrl: {
-      type: "string",
-      label: "Image URL",
-      description: "URL of the image to transform",
-    },
-    effects: {
-      type: "string[]",
-      label: "Effects",
-      description: "Effects to apply to the image",
-      async options() {
-        const effects = await this.getEffects();
-        return effects.map((effect) => ({
-          label: effect.name,
-          value: effect.id,
-        }));
-      },
-    },
-    optimizations: {
-      type: "string[]",
-      label: "Optimizations",
-      description: "Optimizations to apply to the image",
-      async options() {
-        const optimizations = await this.getOptimizations();
-        return optimizations.map((opt) => ({
-          label: opt.name,
-          value: opt.id,
-        }));
-      },
-    },
-  },
+  propDefinitions: {},
   methods: {
-    // Existing method
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
     _baseUrl() {
       return "https://www.change.photos/api";
     },
-    async _makeRequest(opts = {}) {
-      const {
-        $ = this, method = "GET", path = "/", headers, ...otherOpts
-      } = opts;
+    _makeRequest({
+      $ = this,
+      path,
+      ...otherOpts
+    }) {
       return axios($, {
         ...otherOpts,
-        method,
         url: `${this._baseUrl()}${path}`,
         headers: {
-          ...headers,
-          "User-Agent": "@PipedreamHQ/pipedream v0.1",
-          "Authorization": `Bearer ${this.$auth.apiToken}`,
+          Authorization: `Bearer ${this.$auth.api_key}`,
         },
+        debug: true,
       });
     },
-    async getEffects(opts = {}) {
-      const response = await this._makeRequest({
-        method: "GET",
-        path: "/effects",
-        ...opts,
-      });
-      return response;
-    },
-    async getOptimizations(opts = {}) {
-      const response = await this._makeRequest({
-        method: "GET",
-        path: "/optimizations",
-        ...opts,
-      });
-      return response;
-    },
-    async transformImage(opts = {}) {
-      const response = await this._makeRequest({
+    transformImage(opts = {}) {
+      return this._makeRequest({
         method: "POST",
-        path: "/transform",
-        data: {
-          image_url: this.imageUrl,
-          effects: this.effects,
-          optimizations: this.optimizations,
-          ...opts,
-        },
+        path: "/change",
+        ...opts,
       });
-      return response;
     },
   },
 };
