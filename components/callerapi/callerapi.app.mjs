cec207b8f9e3fea3dd6be98ee1b6788ea973dbde
Luan Cazarine <luanhc@gmail.com>
2025-01-02 14:33:10 -0300

    New Components - callerapi (#15102)
    * callerapi init
    
    * [Components] callerapi #14969
    Actions
     - Get Phone Number Information
     - Get Phone Number Picture
    
    * pnpm update
    
    * pnpm update

--- components/callerapi/callerapi.app.mjs
+++ components/callerapi/callerapi.app.mjs
@@ -1,11 +1,49 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "callerapi",
-  propDefinitions: {},
+  propDefinitions: {
+    phoneNumber: {
+      type: "string",
+      label: "Phone Number",
+      description: "The phone number to retrieve information for (E.164 format, e.g., +18006927753)",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://callerapi.com/api/phone";
+    },
+    _headers() {
+      return {
+        "x-auth": this.$auth.api_key,
+      };
+    },
+    _makeRequest({
+      $ = this, method, path = "/", ...opts
+    }) {
+      return axios($, {
+        ...opts,
+        method,
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+      });
+    },
+    getPhoneInfo({
+      $, phoneNumber,
+    }) {
+      return this._makeRequest({
+        $,
+        path: `/info/${phoneNumber}`,
+      });
+    },
+    getPhonePicture({
+      $, phoneNumber,
+    }) {
+      return this._makeRequest({
+        $,
+        path: `/pic/${phoneNumber}`,
+      });
     },
   },
 };
