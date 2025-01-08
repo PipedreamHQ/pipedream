e62b984ceb5aecf60ea9cad55662a9747c5b7a6a
Luan Cazarine <luanhc@gmail.com>
2025-01-08 15:46:50 -0300

    New Components - richpanel (#15108)
    * richpanel init
    
    * [Components] richpanel #15105
    Sources
     - New Ticket
     - New Message
     - New Ticket Status Change
    
    Actions
     - Create Ticket
     - Add Ticket Message
     - Update Ticket Status
    
    * pnpm update
    
    * [Components] richpanel #15105
    Sources
     - New Ticket
     - New Message
    
    Actions
     - Create Ticket
     - Add Ticket Message
     - Update Ticket Status
    
    * pnpm update
    
    * fix import
    
    * Add some props

---
+++ components/richpanel/actions/update-ticket-status/update-ticket-status.mjs
@@ -0,0 +1,37 @@
+import richpanel from "../../richpanel.app.mjs";
+
+export default {
+  key: "richpanel-update-ticket-status",
+  name: "Update Ticket Status",
+  description: "Updates the status of an existing ticket in Richpanel. [See the documentation](https://developer.richpanel.com/reference/update-a-conversation).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    richpanel,
+    conversationId: {
+      propDefinition: [
+        richpanel,
+        "conversationId",
+      ],
+    },
+    status: {
+      propDefinition: [
+        richpanel,
+        "status",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.richpanel.updateTicket({
+      $,
+      conversationId: this.conversationId,
+      data: {
+        ticket: {
+          status: this.status,
+        },
+      },
+    });
+    $.export("$summary", `Updated ticket ${this.conversationId} to status ${this.status}`);
+    return response;
+  },
+};
