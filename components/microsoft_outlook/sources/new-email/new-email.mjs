8d8ef41d6570525d7f3403004e7c1bee5cb6632f
michelle0927 <michellelbergero@hotmail.com>
2025-01-10 18:04:18 -0500

    updates

--- components/microsoft_outlook/sources/new-email/new-email.mjs
+++ components/microsoft_outlook/sources/new-email/new-email.mjs
@@ -15,10 +15,32 @@ export default {
       label: "Folder IDs to Monitor",
       description: "Specify the folder IDs or names in Outlook that you want to monitor for new emails. Leave empty to monitor all folders.",
       optional: true,
+      async options() {
+        const { value: folders } = await this.listFolders();
+        return folders?.map(({
+          id: value, displayName: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
     },
   },
   hooks: {
     ...common.hooks,
+    async deploy() {
+      this.db.set("sentItemFolderId", await this.getSentItemFolderId());
+
+      const events = await this.getSampleEvents({
+        pageSize: 25,
+      }); console.log(events);
+      if (!events || events.length == 0) {
+        return;
+      }
+      for (const item of events) {
+        this.emitEvent(item);
+      }
+    },
     async activate() {
       await this.activate({
         changeType: "created",
@@ -31,6 +53,16 @@ export default {
   },
   methods: {
     ...common.methods,
+    listFolders() {
+      return this.microsoftOutlook._makeRequest({
+        path: "/me/mailFolders",
+      });
+    },
+    async getSentItemFolderId() {
+      const { value: folders } = await this.listFolders();
+      const { id } = folders.find(({ displayName }) => displayName === "Sent Items");
+      return id;
+    },
     async getSampleEvents({ pageSize }) {
       const folders = this.folderIds?.length
         ? this.folderIds.map((id) => `/me/mailFolders/${id}/messages`)
@@ -51,13 +83,23 @@ export default {
       }
       return results;
     },
+    isRelevant(item) {
+      if (this.folderIds?.length) {
+        return true;
+      }
+      // if no folderIds are specified, filter out items in Sent Items
+      const sentItemFolderId = this.db.get("sentItemFolderId");
+      return item.parentFolderId !== sentItemFolderId;
+    },
     emitEvent(item) {
-      this.$emit(
-        {
-          email: item,
-        },
-        this.generateMeta(item),
-      );
+      if (this.isRelevant(item)) {
+        this.$emit(
+          {
+            email: item,
+          },
+          this.generateMeta(item),
+        );
+      }
     },
     generateMeta(item) {
       return {
@@ -78,11 +120,15 @@ export default {
       await this.run({
         event,
         emitFn: async ({ resourceId } = {}) => {
-          const item = await this.microsoftOutlook.getMessage({
-            resource: folder,
-            messageId: resourceId,
-          });
-          this.emitEvent(item);
+          try {
+            const item = await this.microsoftOutlook.getMessage({
+              resource: folder,
+              messageId: resourceId,
+            });
+            this.emitEvent(item);
+          } catch {
+            console.log(`Could not fetch message with ID: ${resourceId}`);
+          }
         },
       });
     }
