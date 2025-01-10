f18e3f86dbb268db01f14620f94984a8496e7035
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-01-10 16:33:32 -0300

    Basecamp usability improvements (#15212)
    * pnpm
    
    * Description standardizing and improvements
    
    * pnpm
    
    * Reusing common props
    
    * Improvements
    
    * More description improvements and standardizing
    
    * Adjustments
    
    * Adjustments
    
    * API method standardization
    
    * Source description updates and standardization
    
    * Reusing common props for sources
    
    * Version bump and markdown syntax fixes
    
    * Summary fix
    
    * Adjusting action export
    
    * Adding card selection to 'create comment'
    
    * pnpm-lock

--- components/basecamp/actions/create-todo-item/create-todo-item.mjs
+++ components/basecamp/actions/create-todo-item/create-todo-item.mjs
@@ -1,28 +1,14 @@
 import app from "../../basecamp.app.mjs";
+import common from "../../common/common.mjs";
 
 export default {
   key: "basecamp-create-todo-item",
-  name: "Create Todo Item",
-  description: "Creates a todo in the project and message board selected. [See the docs here](https://github.com/basecamp/bc3-api/blob/master/sections/todos.md#create-a-to-do)",
+  name: "Create To-do Item",
+  description: "Creates a to-do item in a selected to-do list. [See the documentation](https://github.com/basecamp/bc3-api/blob/master/sections/todos.md#create-a-to-do)",
   type: "action",
-  version: "0.0.7",
+  version: "0.0.8",
   props: {
-    app,
-    accountId: {
-      propDefinition: [
-        app,
-        "accountId",
-      ],
-    },
-    projectId: {
-      propDefinition: [
-        app,
-        "projectId",
-        ({ accountId }) => ({
-          accountId,
-        }),
-      ],
-    },
+    ...common.props,
     todoSetId: {
       propDefinition: [
         app,
@@ -53,13 +39,13 @@ export default {
     },
     content: {
       type: "string",
-      label: "Content",
-      description: "The title of the todo item.",
+      label: "Title (Content)",
+      description: "The title of the to-do item.",
     },
     description: {
       type: "string",
       label: "Description",
-      description: "Containing information about the to-do.  See [Rich text guide](https://github.com/basecamp/bc3-api/blob/master/sections/rich_text.md) for what HTML tags are allowed.",
+      description: "Description of the item. [See the documentation](https://github.com/basecamp/bc3-api/blob/master/sections/todos.md#create-a-to-do) for information on using HTML tags.",
       optional: true,
     },
     assigneeIds: {
@@ -75,19 +61,19 @@ export default {
         }),
       ],
       label: "Assignee",
-      description: "An array of people that will be assigned to this to-do.",
+      description: "One or more people to be assigned to this item.",
       optional: true,
     },
     dueOn: {
       type: "string",
-      label: "Due on",
-      description: "A date when the to-do should be completed. format: `yyyy-mm-dd`.",
+      label: "Due On",
+      description: "The due date when the item should be completed, in `YYYY-MM-DD` format.`",
       optional: true,
     },
     startsOn: {
       type: "string",
-      label: "Starts on",
-      description: "Allows the to-do to run from this date to the due date. format: `yyyy-mm-dd`.",
+      label: "Starts On",
+      description: "The start date, in `YYYY-MM-DD` format. Allows the item to run from this date to the `Due Date`.",
       optional: true,
     },
   },
@@ -117,7 +103,7 @@ export default {
       },
     });
 
-    $.export("$summary", `Successfully created todo with ID ${todo.id}`);
+    $.export("$summary", `Successfully created to-do (ID: ${todo.id})`);
     return todo;
   },
 };
