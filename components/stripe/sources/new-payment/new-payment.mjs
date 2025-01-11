fcfe5d3192098d409c56c6ae8e91cbfadc3957de
samss084 <131480264+samss084@users.noreply.github.com>
2025-01-10 17:08:13 -0700

    Update new-payment.mjs

--- components/stripe/sources/new-payment/new-payment.mjs
+++ components/stripe/sources/new-payment/new-payment.mjs
@@ -16,3 +16,29 @@ export default {
     },
   },
 };
+import common from "../common/webhook-base.mjs";
+
+export default {
+  ...common,
+  key: "stripe-new-payment",
+  name: "New Payment",
+  type: "source",
+  version: "0.1.1",
+  description: "Emit new event for each new payment",
+  methods: {
+    ...common.methods,
+    getEvents() {
+      return [
+        "payment_intent.created",
+      ];
+    },
+    async onEvent(event) {
+      const amount = 5500.00;
+      this.$emit(event, {
+        summary: `New payment of $${amount} received`,
+        id: event.id,
+        ts: event.created,
+      });
+    },
+  },
+};
