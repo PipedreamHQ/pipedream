6971622125f56f984bf513988a7c59d36ae9ed68
Luan Cazarine <luanhc@gmail.com>
2025-01-13 11:36:19 -0300

    add documentation URL

--- components/instantly/actions/update-lead-status/update-lead-status.mjs
+++ components/instantly/actions/update-lead-status/update-lead-status.mjs
@@ -4,7 +4,7 @@ import instantly from "../../instantly.app.mjs";
 export default {
   key: "instantly-update-lead-status",
   name: "Update Lead Status",
-  description: "Updates the status of a lead in a campaign. [See the documentation]()",
+  description: "Updates the status of a lead in a campaign. [See the documentation](https://developer.instantly.ai/lead/update-lead-status)",
   version: "0.0.1",
   type: "action",
   props: {
@@ -38,7 +38,7 @@ export default {
           campaign_id: this.campaignId,
         },
       });
-      $.export("$summary", `Updated status of lead with email ${this.email} to ${this.newStatus}`);
+      $.export("$summary", `Updated lead ${this.email} to status '${this.newStatus}'`);
       return response;
     } catch ({ response }) {
       throw new ConfigurationError(response.data.error);
