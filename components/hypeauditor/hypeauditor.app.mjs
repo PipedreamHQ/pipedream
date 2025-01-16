0cc715b62d6fb4f3da5ecce1cf9a1d4e51663e78
Lucas Caresia <lucascarezia@gmail.com>
2025-01-15 22:53:08 -0300

    Merging pull request #15288
    * Added actions
    
    * Done requests changes
    
    * Update components/hypeauditor/actions/get-instagram-report/get-instagram-report.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/hypeauditor/hypeauditor.app.mjs
+++ components/hypeauditor/hypeauditor.app.mjs
@@ -1,11 +1,77 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "hypeauditor",
-  propDefinitions: {},
+  propDefinitions: {
+    username: {
+      type: "string",
+      label: "Username",
+      description: "Username to request the report",
+      optional: true,
+    },
+    userId: {
+      type: "string",
+      label: "User ID",
+      description: "User ID to request the report",
+      optional: true,
+    },
+    channelUsername: {
+      type: "string",
+      label: "Channel Username",
+      description: "Identify the user by their Channel Username",
+      optional: true,
+    },
+    channelId: {
+      type: "string",
+      label: "Channel ID",
+      description: "Identify the user by their Channel ID",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://hypeauditor.com/api/method";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "content-type": "application/x-www-form-urlencoded",
+          "x-auth-id": `${this.$auth.client_id}`,
+          "x-auth-token": `${this.$auth.api_token}`,
+          "user-agent": "pipedream/1",
+        },
+      });
+    },
+    async getInstagramReport(args = {}) {
+      return this._makeRequest({
+        path: "/auditor.report/",
+        method: "post",
+        ...args,
+      });
+    },
+    async getYoutubeReport(args = {}) {
+      return this._makeRequest({
+        path: "/auditor.youtube/",
+        method: "post",
+        ...args,
+      });
+    },
+    async getTiktokReport(args = {}) {
+      return this._makeRequest({
+        path: "/auditor.tiktok",
+        method: "post",
+        ...args,
+      });
     },
   },
 };
