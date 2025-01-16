597a6d58325343d97633ada6da861e4ec8b3e633
michelle0927 <michellelbergero@hotmail.com>
2025-01-16 11:19:27 -0500

    add lineItemsJson prop

--- components/clear_books/actions/create-purchase-document/create-purchase-document.mjs
+++ components/clear_books/actions/create-purchase-document/create-purchase-document.mjs
@@ -1,4 +1,5 @@
 import clearBooks from "../../clear_books.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   key: "clear_books-create-purchase-document",
@@ -34,8 +35,15 @@ export default {
     numLineItems: {
       type: "integer",
       label: "Number of Line Items",
-      description: "The number of line items to create",
+      description: "The number of line items. Use this to manually enter Unit Price, Quantity, and Description of each line item.",
+      optional: true,
       reloadProps: true,
+    },
+    lineItemsJson: {
+      type: "string",
+      label: "Line Items JSON",
+      description: "JSON value containing an array of Line Items. For example: `[{\"description\":\"Line Item 1 Description\",\"unitPrice\":1022,\"quantity\":1,\"accountCode\":\"2001001\"},{\"description\":\"Line Item 2 Description\",\"unitPrice\":1023,\"quantity\":2,\"accountCode\":\"2001001\"}]`. [See documentation](https://u.pcloud.link/publink/show?code=XZkThJ5Z4zKewgCL6VBpfxlPeHPDdXXj0Cc7)",
+      optional: true,
     },
   },
   additionalProps() {
@@ -59,14 +67,45 @@ export default {
     }
     return props;
   },
+  methods: {
+    buildLineItems() {
+      const lineItems = [];
+      for (let i = 1; i <= this.numLineItems; i++) {
+        lineItems.push({
+          unitPrice: this[`line_item_${i}_unit_price`],
+          quantity: this[`line_item_${i}_quantity`],
+          description: this[`line_item_${i}_description`],
+        });
+      }
+      return lineItems;
+    },
+    parseLineItemsJson() {
+      try {
+        return Array.isArray(this.lineItemsJson)
+          ? this.lineItemsJson.map((item) => typeof item === "string"
+            ? JSON.parse(item)
+            : item)
+          : typeof this.lineItemsJson === "string"
+            ? JSON.parse(this.lineItemsJson)
+            : this.lineItemsJson;
+      } catch {
+        throw new ConfigurationError("Could not parse Line Items JSON");
+      }
+    },
+  },
   async run({ $ }) {
+    if (!this.numLineItems && !this.lineItemsJson) {
+      throw new ConfigurationError("Please enter at least one line item");
+    }
+
     const lineItems = [];
-    for (let i = 1; i <= this.numLineItems; i++) {
-      lineItems.push({
-        unitPrice: this[`line_item_${i}_unit_price`],
-        quantity: this[`line_item_${i}_quantity`],
-        description: this[`line_item_${i}_description`],
-      });
+    if (this.numLineItems) {
+      const lineItemsManual = this.buildLineItems();
+      lineItems.push(...lineItemsManual);
+    }
+    if (this.lineItemsJson) {
+      const lineItemsJson = this.parseLineItemsJson();
+      lineItems.push(...lineItemsJson);
     }
 
     const response = await this.clearBooks.createPurchaseDocument({
