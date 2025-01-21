7dd5427617b081531a22b9e3a4309bf20c6b19ce
Luan Cazarine <luanhc@gmail.com>
2025-01-21 15:09:51 -0300

    New Components - ragie (#15322)
    * ragie init
    
    * [Components] ragie #15183
    Sources
     - New Document
     - New Connection
    
    Actions
     - Create Document
     - Update Document File
    
    * pnpm update
    
    * fix props reference names

---
+++ components/ragie/sources/new-connection/test-event.mjs
@@ -0,0 +1,11 @@
+export default {
+  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "created_at": "2025-01-16T18:44:13.901Z",
+  "updated_at": "2025-01-16T18:44:13.901Z",
+  "metadata": {},
+  "type": "string",
+  "name": "string",
+  "enabled": true,
+  "last_synced_at": "2025-01-16T18:44:13.901Z",
+  "syncing": true
+}
