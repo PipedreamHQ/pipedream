838b18980e2a435c0db3e597dd68c17f84d419f3
Biz Melesse <bizmelesse@gmail.com>
2025-01-24 17:00:55 -0600

    Ensure propsNeedConfiguring is updated after field registration

--- packages/connect-react/src/hooks/form-context.tsx
+++ packages/connect-react/src/hooks/form-context.tsx
@@ -365,6 +365,9 @@ export const FormContextProvider = <T extends ConfigurableProps>({
       }
     }
     // propsNeedConfiguring.splice(0, propsNeedConfiguring.length, ..._propsNeedConfiguring)
+    // For some reason registerField is called in recursive loop if an empty
+    // propsNeedConfiguring is set to an empty _propsNeedConfiguring
+    if (!propsNeedConfiguring.length && !_propsNeedConfiguring.length) return
     setPropsNeedConfiguring(_propsNeedConfiguring)
   }
 
@@ -373,6 +376,7 @@ export const FormContextProvider = <T extends ConfigurableProps>({
       fields[field.prop.name] = field
       return fields
     });
+    checkPropsNeedConfiguring()
   };
 
   // console.log("***", configurableProps, configuredProps)
