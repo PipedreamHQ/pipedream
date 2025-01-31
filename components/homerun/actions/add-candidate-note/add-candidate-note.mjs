053017f330a647b2cec0f258a8e47cbfe30944ce
Jorge Cortes <jacortesmahmud@gmail.com>
2025-01-31 11:30:51 -0500

    [Components] homerun - new components untested

---
+++ components/homerun/actions/add-candidate-note/add-candidate-note.mjs
@@ -0,0 +1,58 @@
+import app from "../../homerun.app.mjs";
+
+export default {
+  key: "homerun-add-candidate-note",
+  name: "Add Candidate Note",
+  description: "Adds a note to a candidate's profile in Homerun. [See the documentation](https://developers.homerun.co/#tag/Job-Application-Notes/operation/job-applications.job-application-id.notes.post).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    jobApplicationId: {
+      propDefinition: [
+        app,
+        "jobApplicationId",
+      ],
+    },
+    note: {
+      type: "string",
+      label: "Note Content",
+      description: "The content of the note to add.",
+    },
+    displayName: {
+      type: "string",
+      label: "Display Name",
+      description: "Name of the note's author.",
+    },
+  },
+  methods: {
+    addCandidateNote({
+      jobApplicationId, ...args
+    } = {}) {
+      return this.app.post({
+        path: `/job-applications/${jobApplicationId}/notes`,
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      addCandidateNote,
+      jobApplicationId,
+      note,
+      displayName,
+    } = this;
+
+    const response = await addCandidateNote({
+      $,
+      jobApplicationId,
+      data: {
+        note,
+        display_name: displayName,
+      },
+    });
+
+    $.export("$summary", "Successfully added a note.");
+    return response;
+  },
+};
