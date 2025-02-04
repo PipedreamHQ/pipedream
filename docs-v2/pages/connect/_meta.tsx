a118e0265d38c08140ceffca7931b5b837005a1b
Danny Roosevelt <danny@pipedream.com>
2025-02-03 16:28:52 -0800

    checking work in; need to add redirects for dir changes

--- docs-v2/pages/connect/_meta.tsx
+++ docs-v2/pages/connect/_meta.tsx
@@ -5,38 +5,23 @@ export default {
   "use-cases": {
     "title": "Use cases",
   },
-  "quickstart": {
-    "title": "Managed auth",
-  },
-  "workflows": {
-    "title": "Running workflows",
+  "managed-auth": {
+    "title": "Managed authentication",
   },
   "components": {
-    "title": "Embedding components",
-  },
-  "api": {
-    "title": "API & SDK reference",
+    "title": "Pre-built tools",
   },
   "api-proxy": {
     "title": "API proxy",
   },
-  "tokens": {
-    "title": "Connect tokens",
+  "workflows": {
+    "title": "Workflows",
   },
   "environments": {
     "title": "Environments",
   },
-  "oauth-clients": {
-    "title": "OAuth clients",
-  },
-  "webhooks": {
-    "title": "Webhooks",
-  },
-  "connect-link": {
-    "title": "Connect Link",
-  },
-  "customize-your-app": {
-    "title": "Customize your app",
+  "api": {
+    "title": "API & SDK reference",
   },
   "troubleshooting": {
     "title": "Troubleshooting",
---
+++ docs-v2/pages/connect/managed-auth/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+  "index": "Overview",
+  "quickstart": "Quickstart",
+  "tokens": "Connect Tokens",
+  "connect-link": "Connect Link",
+  "oauth-clients": "OAuth clients",
+  "webhooks": "Webhooks",
+  "customize-your-app": "Customize your app",
+} as const
