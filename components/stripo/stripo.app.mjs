0b62f171f7003a7eb11b78e11b091d15ac868475
michelle0927 <michellelbergero@hotmail.com>
2025-02-05 13:15:12 -0500

    wip

--- components/stripo/stripo.app.mjs
+++ components/stripo/stripo.app.mjs
@@ -1,11 +1,55 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "stripo",
-  propDefinitions: {},
+  propDefinitions: {
+    templateId: {
+      type: "string",
+      label: "Template ID",
+      description: "An ID of a template with the auto-generated area. Please note that this template should be yours (saved in the project you have access to), not a basic or a public one.",
+      async options({ page }) {
+        const { data } = await this.listTemplates({
+          params: page,
+        });
+        return data?.map(({
+          templateId: value, name: label,
+        }) => ({
+          value,
+          label,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://my.stripo.email/emailgeneration/v1";
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Stripo-Api-Auth": this.$auth.api_key,
+        },
+        ...opts,
+      });
+    },
+    listTemplates(opts = {}) {
+      return this._makeRequest({
+        path: "/templates",
+        ...opts,
+      });
+    },
+    createEmail(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/email",
+        ...opts,
+      });
     },
   },
 };
