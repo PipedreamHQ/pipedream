b31687060e47a04bacc5eefa9d3a4d4fa7d5f774
Danny Roosevelt <danny@pipedream.com>
2025-02-06 09:09:00 -0800

    More updates

--- docs-v2/pages/connect/managed-auth/_meta.tsx
+++ docs-v2/pages/connect/managed-auth/_meta.tsx
@@ -1,10 +1,9 @@
 export default {
-  "index": "Overview",
-  "users": "Users",
   "quickstart": "Quickstart",
-  "tokens": "Connect tokens",
-  "connect-link": "Connect link",
-  "oauth-clients": "OAuth clients",
+  "users": "Users",
+  "tokens": "Connect Tokens",
+  "connect-link": "Connect Link",
+  "oauth-clients": "OAuth Clients",
   "webhooks": "Webhooks",
-  "customization": "Customizing the auth flow",
+  "customization": "Customizing the Auth Flow",
 } as const
