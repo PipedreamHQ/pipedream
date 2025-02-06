33a14e312b242108a00b817b6f69bab499763ba3
Lucas Caresia <lucascarezia@gmail.com>
2025-02-06 15:01:20 -0300

    [Components] scrapingant #13316 (#15379)
    * Added actions
    
    * Done requests changes
    
    * Update general-extraction.mjs
    
    * Update components/scrapingant/scrapingant.app.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Update components/scrapingant/scrapingant.app.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Update components/scrapingant/scrapingant.app.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Update components/scrapingant/scrapingant.app.mjs
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    
    * Added actions
    
    * Done requests changes
    
    ---------
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

---
+++ components/scrapingant/common/constants.mjs
@@ -0,0 +1,126 @@
+export default {
+  PROXY_COUNTRIES: [
+    {
+      label: "World",
+      value: "",
+    },
+    {
+      label: "Brazil",
+      value: "BR",
+    },
+    {
+      label: "Canada",
+      value: "CA",
+    },
+    {
+      label: "China",
+      value: "CN",
+    },
+    {
+      label: "Czech Republic",
+      value: "CZ",
+    },
+    {
+      label: "France",
+      value: "FR",
+    },
+    {
+      label: "Germany",
+      value: "DE",
+    },
+    {
+      label: "Hong Kong",
+      value: "HK",
+    },
+    {
+      label: "India",
+      value: "IN",
+    },
+    {
+      label: "Indonesia",
+      value: "ID",
+    },
+    {
+      label: "Italy",
+      value: "IT",
+    },
+    {
+      label: "Israel",
+      value: "IL",
+    },
+    {
+      label: "Japan",
+      value: "JP",
+    },
+    {
+      label: "Netherlands",
+      value: "NL",
+    },
+    {
+      label: "Poland",
+      value: "PL",
+    },
+    {
+      label: "Russia",
+      value: "RU",
+    },
+    {
+      label: "Saudi Arabia",
+      value: "SA",
+    },
+    {
+      label: "Singapore",
+      value: "SG",
+    },
+    {
+      label: "South Korea",
+      value: "KR",
+    },
+    {
+      label: "Spain",
+      value: "ES",
+    },
+    {
+      label: "United Kingdom",
+      value: "GB",
+    },
+    {
+      label: "United Arab Emirates",
+      value: "AE",
+    },
+    {
+      label: "USA",
+      value: "US",
+    },
+    {
+      label: "Vietnam",
+      value: "VN",
+    },
+  ],
+  PROXY_TYPES: [
+    {
+      label: "Residential",
+      value: "residential",
+    },
+    {
+      label: "Datacenter",
+      value: "datacenter",
+    },
+  ],
+  RESOURCE_TYPES: [
+    "document",
+    "stylesheet",
+    "image",
+    "media",
+    "font",
+    "script",
+    "texttrack",
+    "xhr",
+    "fetch",
+    "eventsource",
+    "websocket",
+    "manifest",
+    "other",
+  ],
+
+};
