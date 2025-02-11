c0171c1e40f5df97a16323860587300e13352aa6
Luan Cazarine <luanhc@gmail.com>
2025-02-11 11:37:50 -0300

    [Components] google_maps_platform #15158 Actions  - Search Places  - Get Place Details

--- components/google_maps_platform/actions/get-place-details/get-place-details.mjs
+++ components/google_maps_platform/actions/get-place-details/get-place-details.mjs
@@ -1,32 +1,23 @@
-import google_maps_platform from "../../google_maps_platform.app.mjs";
-import { axios } from "@pipedream/platform";
+import app from "../../google_maps_platform.app.mjs";
 
 export default {
   key: "google_maps_platform-get-place-details",
   name: "Get Place Details",
   description: "Retrieves detailed information for a specific place using its Place ID. [See the documentation](https://developers.google.com/maps/documentation/places/web-service/place-details)",
-  version: "0.0.{{ts}}",
+  version: "0.0.1",
   type: "action",
   props: {
-    google_maps_platform,
+    app,
     placeId: {
-      propDefinition: [
-        google_maps_platform,
-        "placeId",
-      ],
-    },
-    fields: {
-      propDefinition: [
-        google_maps_platform,
-        "fields",
-      ],
-      optional: true,
+      type: "string",
+      label: "Place ID",
+      description: "A textual identifier that uniquely identifies a place, returned from Search Places Action.",
     },
   },
   async run({ $ }) {
-    const response = await this.google_maps_platform.getPlaceDetails({
+    const response = await this.app.getPlaceDetails({
+      $,
       placeId: this.placeId,
-      fields: this.fields,
     });
 
     $.export("$summary", `Retrieved details for Place ID ${this.placeId}`);
