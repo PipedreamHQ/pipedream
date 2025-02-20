787abff16f188f3e7fed28ea01ec1dcfb145d666
Luan Cazarine <luanhc@gmail.com>
2025-02-20 19:23:21 -0300

    New Components - polygon (#15558)
    * polygon init
    
    * [Components] polygon #15141
    Sources
     - New Market Trade
     - New Market News
     - New Stock Price Summary
    
    Actions
     - Get Stock Price
     - Get Historical Prices
     - Get Company
    
    * pnpm update
    
    * Update components/polygon/actions/get-historical-prices/get-historical-prices.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * some adjusts
    
    * fix source summary
    
    * pnpm update
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ components/polygon/actions/get-stock-price/get-stock-price.mjs
@@ -0,0 +1,51 @@
+import polygon from "../../polygon.app.mjs";
+
+export default {
+  key: "polygon-get-stock-price",
+  name: "Get Stock Price",
+  description: "Get the open, close and afterhours prices of a stock symbol on a certain date. [See the documentation](https://polygon.io/docs/stocks/get_v1_open-close__stocksticker___date)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    polygon,
+    stockTicker: {
+      propDefinition: [
+        polygon,
+        "stockTicker",
+      ],
+    },
+    date: {
+      type: "string",
+      label: "Date",
+      description: "The date of the requested open/close in the format YYYY-MM-DD.",
+    },
+    adjusted: {
+      propDefinition: [
+        polygon,
+        "adjusted",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    try {
+
+      const response = await this.polygon.getCurrentPrice({
+        $,
+        date: this.date,
+        stockTicker: this.stockTicker,
+        params: {
+          adjusted: this.adjusted,
+        },
+      });
+
+      $.export("$summary", `Successfully fetched the price of ${this.stockTicker}`);
+      return response;
+    } catch ({ response }) {
+      if (response.status === 404) {
+        $.export("$summary", `No data found for ${this.stockTicker}`);
+        return {};
+      }
+    }
+  },
+};
