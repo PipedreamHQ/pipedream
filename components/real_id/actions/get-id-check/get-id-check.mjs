70a8363a171b667d723ac7681f72b1c45ac0c69d
Jorge Cortes <jacortesmahmud@gmail.com>
2025-02-21 13:20:31 -0500

    [Components] real_id  - new action components (#15662)

---
+++ components/real_id/actions/get-id-check/get-id-check.mjs
@@ -0,0 +1,52 @@
+import app from "../../real_id.app.mjs";
+
+export default {
+  key: "real_id-get-id-check",
+  name: "Get ID Check",
+  description: "Retrieve an ID check. [See the documentation](https://getverdict.com/help/docs/api/checks#retrieve-an-id-check).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    checkId: {
+      propDefinition: [
+        app,
+        "checkId",
+      ],
+    },
+    withPhotos: {
+      type: "boolean",
+      label: "Include Photos",
+      description: "All photos will be provided as short lived URLs in the API response under the `photos` key.",
+      optional: true,
+    },
+  },
+  methods: {
+    retrieveIdCheck({
+      checkId, ...args
+    } = {}) {
+      return this.app._makeRequest({
+        path: `/checks/${checkId}`,
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const {
+      retrieveIdCheck,
+      checkId,
+      withPhotos,
+    } = this;
+
+    const response = await retrieveIdCheck({
+      $,
+      checkId,
+      params: {
+        withPhotos,
+      },
+    });
+
+    $.export("$summary", "Successfully retrieved ID check.");
+    return response;
+  },
+};
