142796934570ded95aad2a06b4e558ef5640cd82
Lucas Caresia <lucascarezia@gmail.com>
2025-02-25 03:01:57 -0300

    Merging pull request #14777
    * Added actions
    
    * Added actions
    
    * Added actions
    
    * Fixed requested changes
    
    * Update components/apiary/actions/publish-blueprint/publish-blueprint.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Fixed requested changes
    
    * fixes
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/apiary/actions/publish-blueprint/publish-blueprint.mjs
@@ -0,0 +1,37 @@
+import app from "../../apiary.app.mjs";
+
+export default {
+  key: "apiary-publish-blueprint",
+  name: "Publish Blueprint",
+  description: "Publish an API Blueprint for a particular API. [See the documentation](https://apiary.docs.apiary.io/#reference/blueprint/publish-blueprint/publish-blueprint)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    apiSubdomain: {
+      propDefinition: [
+        app,
+        "apiSubdomain",
+      ],
+    },
+    code: {
+      propDefinition: [
+        app,
+        "code",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.publishBlueprint({
+      $,
+      apiSubdomain: this.apiSubdomain,
+      data: {
+        code: this.code,
+      },
+    });
+
+    $.export("$summary", `Successfully published the blueprint with the subdomain '${this.apiSubdomain}'`);
+
+    return response;
+  },
+};
