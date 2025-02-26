db6687eea63ea3141369738ac3e84f687dcdcb5c
Luan Cazarine <luanhc@gmail.com>
2025-02-26 15:44:07 -0300

    New Components - benchmarkone (#15668)
    * benchmarkone init
    
    * [Components] benchmarkone #15631
    Sources
     - New Webhook Automation Event (Instant)
    
    Actions
     - Add Note
     - Add Tag
     - Create Contact
     - Update Contact
    
    * pnpm update
    
    * pnpm update
    
    * Update components/benchmarkone/sources/new-webhook-automation-event-instant/new-webhook-automation-event-instant.mjs
    
    * some adjusts
    
    ---------
    
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>

---
+++ components/benchmarkone/actions/add-tag/add-tag.mjs
@@ -0,0 +1,35 @@
+import benchmarkone from "../../benchmarkone.app.mjs";
+import { parseObject } from "../../common/utils.mjs";
+
+export default {
+  key: "benchmarkone-add-tag",
+  name: "Add Tag to Contact",
+  description: "Adds tags to a contact. If the contact does not exist, it will be created first. [See the documentation](https://sandbox.hatchbuck.com/api/dist/#/Tags).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    benchmarkone,
+    contactId: {
+      propDefinition: [
+        benchmarkone,
+        "contactId",
+      ],
+    },
+    tags: {
+      type: "string[]",
+      label: "Tags",
+      description: "A list of tags to add to the contact.",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.benchmarkone.addTagToContact({
+      contactId: this.contactId,
+      data: parseObject(this.tags)?.map((item) => ({
+        name: item,
+      })),
+    });
+
+    $.export("$summary", `Succcessfully added tags to contact ID ${this.contactId}`);
+    return response;
+  },
+};
