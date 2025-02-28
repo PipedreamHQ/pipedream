335ff74fa8194e9fa6345af949abd176aed1e86a
Luan Cazarine <luanhc@gmail.com>
2025-02-28 14:57:24 -0300

    New Components - elastic_email (#15692)
    * elastic_email init
    
    * [Components] elasticemail #15687
    Sources
     - New Email Open
     - New Email Click
     - New Contact Added
    
    Actions
     - Send Email
     - Add Contact
     - Unsubscribe Contact
    
    * pnpm update
    
    * some adjusts
    
    * some adjusts
    
    * fix template prop

---
+++ components/elastic_email/sources/new-contact-added/new-contact-added.mjs
@@ -0,0 +1,28 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "elastic_email-new-contact-added",
+  name: "New Contact Added",
+  description: "Emit new event when a new contact is added to a mailing list. [See the documentation](https://elasticemail.com/developers/api-documentation/rest-api#operation/contactsGet)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getFunction() {
+      return this.app.listContacts;
+    },
+    getDateField() {
+      return "DateAdded";
+    },
+    getIdField() {
+      return "Email";
+    },
+    getSummary(item) {
+      return `New contact added: ${item.Email}`;
+    },
+  },
+  sampleEmit,
+};
