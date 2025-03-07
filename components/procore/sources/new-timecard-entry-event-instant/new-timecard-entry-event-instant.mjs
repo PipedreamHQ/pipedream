a294ed86459baaa99a0e5a3787b7afa34d8ec25e
Jorge Cortes <jacortesmahmud@gmail.com>
2025-03-07 10:48:48 -0500

    [Components] procore - new components

---
+++ components/procore/sources/new-timecard-entry-event-instant/new-timecard-entry-event-instant.mjs
@@ -0,0 +1,46 @@
+import common from "../common/webhook.mjs";
+import resourceNames from "../../common/resource-names.mjs";
+
+export default {
+  ...common,
+  name: "New Timecard Entry Event (Instant)",
+  key: "procore-new-timecard-entry-event-instant",
+  description: "Emit new event when a new timecard entry is created. [See the documentation](https://developers.procore.com/reference/rest/hooks?version=latest).",
+  version: "0.0.1",
+  type: "source",
+  methods: {
+    ...common.methods,
+    getResourceName() {
+      return resourceNames.TIMECARD_ENTRIES;
+    },
+    async getDataToEmit(body) {
+      const {
+        app,
+        companyId,
+      } = this;
+      const {
+        resource_id: timecardEntryId,
+        project_id: projectId,
+      } = body;
+
+      const resource = await app.getTimecardEntry({
+        companyId,
+        timecardEntryId,
+        params: {
+          project_id: projectId,
+        },
+      });
+      return {
+        ...body,
+        resource,
+      };
+    },
+    generateMeta(body) {
+      return {
+        id: body.id,
+        summary: `New Timecard Entry Event: ${body.resource_id}`,
+        ts: new Date(body.timestamp).getTime(),
+      };
+    },
+  },
+};
