6d72280e458fe638098357ab769068b954ce44c1
michelle0927 <michellelbergero@hotmail.com>
2025-03-14 12:15:24 -0400

    updates

--- components/shopify_developer_app/actions/get-metafields/get-metafields.mjs
+++ components/shopify_developer_app/actions/get-metafields/get-metafields.mjs
@@ -1,5 +1,6 @@
 import metafieldActions from "../common/metafield-actions.mjs";
 import common from "@pipedream/shopify/actions/get-metafields/get-metafields.mjs";
+import shopify from "../../shopify_developer_app.app.mjs";
 
 export default {
   ...common,
@@ -9,6 +10,7 @@ export default {
   version: "0.0.6",
   type: "action",
   props: {
+    shopify,
     ...metafieldActions.props,
     ...common.props,
   },
--- components/shopify_developer_app/actions/update-metafield/update-metafield.mjs
+++ components/shopify_developer_app/actions/update-metafield/update-metafield.mjs
@@ -17,6 +17,7 @@ export default {
   description,
   type,
   props: {
+    shopify,
     ...props,
     ...common.props,
   },
