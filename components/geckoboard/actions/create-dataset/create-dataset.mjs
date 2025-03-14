0b5f3f4294c216b06feae18fe47ed146fdfd31a0
Lucas Caresia <lucascarezia@gmail.com>
2025-03-14 10:03:33 -0300

    [Components] geckoboard #13394 (#15908)
    * Added actions
    
    * Improve Fields label
    
    * Add official documentation
    
    * Improve prop description
    
    ---------
    
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>

---
+++ components/geckoboard/actions/create-dataset/create-dataset.mjs
@@ -0,0 +1,35 @@
+import app from "../../geckoboard.app.mjs";
+
+export default {
+  key: "geckoboard-create-dataset",
+  name: "Create Dataset",
+  description: "Create a new dataset. [See the documentation](https://developer.geckoboard.com/?#find-or-create-a-new-dataset)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    fields: {
+      propDefinition: [
+        app,
+        "fields",
+      ],
+    },
+    id: {
+      propDefinition: [
+        app,
+        "id",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createDataset({
+      $,
+      id: this.id,
+      data: {
+        fields: JSON.parse(this.fields),
+      },
+    });
+    $.export("$summary", "Successfully created dataset");
+    return response;
+  },
+};
