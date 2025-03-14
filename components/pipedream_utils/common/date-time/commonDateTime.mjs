c766b0466500d2b6eafe6876cef9a38b6c29ae71
Andrew Chuang <andrewjschuang@gmail.com>
2025-03-14 11:56:29 -0300

    fix component import

--- components/pipedream_utils/common/date-time/commonDateTime.mjs
+++ components/pipedream_utils/common/date-time/commonDateTime.mjs
@@ -1,21 +1,21 @@
-import app from "../../app/formatting.app.mjs";
+import pipedream_utils from "../../app/pipedream_utils.app.mjs";
 import { ConfigurationError } from "@pipedream/platform";
 import {
   DATE_FORMAT_PARSE_MAP, DEFAULT_INPUT_FUNCTION,
 } from "./dateFormats.mjs";
 export default {
   props: {
-    app,
+    pipedream_utils,
     inputDate: {
       description: "A valid date string, in the format selected in **Input Format**. If the format is not set, Pipedream will attempt to infer it using the parser from [Sugar Date library](https://sugarjs.com/dates/#/Parsing).",
       propDefinition: [
-        app,
+        pipedream_utils,
         "inputDate",
       ],
     },
     inputFormat: {
       propDefinition: [
-        app,
+        pipedream_utils,
         "inputFormat",
       ],
     },
