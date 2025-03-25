e3fcf9dfcdd719d79ffa0d1d2d25534ccf9d1637
Lucas Caresia <lucascarezia@gmail.com>
2025-03-24 23:55:57 -0300

    Merging pull request #16012
    * Added actions
    
    * Update components/autobound/actions/write-personalized-content/write-personalized-content.mjs
    
    * Update components/autobound/common/constants.mjs
    
    * Update components/autobound/common/constants.mjs
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/autobound/autobound.app.mjs
+++ components/autobound/autobound.app.mjs
@@ -1,11 +1,70 @@
+import { axios } from "@pipedream/platform";
+import constants from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "autobound",
-  propDefinitions: {},
+  propDefinitions: {
+    contactEmail: {
+      type: "string",
+      label: "Contact Email",
+      description: "The email address of the contact the user is reaching out to",
+    },
+    userEmail: {
+      type: "string",
+      label: "User Email",
+      description: "The email address of the user the content is written on behalf of",
+    },
+    contentType: {
+      type: "string",
+      label: "Content Type",
+      description: "Type of content to generate",
+      options: constants.CONTENT_TYPES,
+    },
+    writingStyle: {
+      type: "string",
+      label: "Writing Style",
+      description: "Writing style for content generation",
+      options: constants.WRITING_STYLES,
+    },
+    additionalContext: {
+      type: "string",
+      label: "Additional Context",
+      description: "Extra information for customizing generated content",
+    },
+    wordCount: {
+      type: "integer",
+      label: "Word Count",
+      description: "Approximate word count for output",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.autobound.ai/api/external";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          "X-API-KEY": `${this.$auth.api_key}`,
+          "Content-Type": "application/json",
+          ...headers,
+        },
+      });
+    },
+    async writePersonalizedContent(args = {}) {
+      return this._makeRequest({
+        path: "/generate-content/v3.5",
+        method: "post",
+        ...args,
+      });
     },
   },
 };
