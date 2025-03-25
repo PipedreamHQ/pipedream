ab13d4e407301b13bb34492785704bf77e659421
Luan Cazarine <luanhc@gmail.com>
2025-03-25 14:35:39 -0300

    New Components - neo4j_auradb (#15848)
    * neo4j_auradb init
    
    * [Components] neo4j_auradb #15828
    Sources
     - New Node
     - New Relationship
     - New Query Result
    
    Actions
     - Create Node
     - Create Relationship
     - Run Query
    
    * pnpm update
    
    * remove unnecessary fields on source base file
    
    * Update components/neo4j_auradb/actions/create-node/create-node.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * some adjusts
    
    * [ACTION] Calendly - List User Availability Schedules #15214
    Actions
     - List User Availability Schedules
    
    * some adjusts
    
    * remove console.log
    
    * some adjusts
    
    * some adjusts
    
    * error validation added
    
    * pnpm update
    
    * remove wrong file
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/neo4j_auradb/actions/create-node/create-node.mjs
@@ -0,0 +1,41 @@
+import { ConfigurationError } from "@pipedream/platform";
+import { parseObject } from "../../common/utils.mjs";
+import app from "../../neo4j_auradb.app.mjs";
+
+export default {
+  key: "neo4j_auradb-create-node",
+  name: "Create Node",
+  description: "Creates a new node in the Neo4j AuraDB instance. [See the documentation](https://neo4j.com/docs/query-api/current/query/)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    nodeLabel: {
+      type: "string",
+      label: "Node Label",
+      description: "The label of the node to filter events for new node creation.",
+    },
+    nodeProperties: {
+      type: "object",
+      label: "Create Node Properties",
+      description: "An object representing the properties of the node to create.",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createNode({
+      $,
+      label: this.nodeLabel,
+      properties: parseObject(this.nodeProperties),
+    });
+
+    if (response.errors) {
+      throw new ConfigurationError(response.errors[0].message);
+    }
+
+    const elementId = response.data?.values?.[0]?.[0]?.elementId;
+    $.export("$summary", elementId
+      ? `Created node with id ${elementId}`
+      : "Node created successfully");
+    return response;
+  },
+};
