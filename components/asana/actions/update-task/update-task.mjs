2da8581f3e283f4f8d80059d4e081920487cf045
Danny Roosevelt <danny@pipedream.com>
2025-03-25 21:13:10 -0700

    Bumping versions

--- components/asana/actions/update-task/update-task.mjs
+++ components/asana/actions/update-task/update-task.mjs
@@ -5,7 +5,7 @@ export default {
   key: "asana-update-task",
   name: "Update Task",
   description: "Updates a specific and existing task. [See the documentation](https://developers.asana.com/docs/update-a-task)",
-  version: "0.4.0",
+  version: "0.4.1",
   type: "action",
   props: {
     ...common.props,
