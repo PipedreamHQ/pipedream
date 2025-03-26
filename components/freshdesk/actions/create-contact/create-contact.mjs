14135572f9a4dd7b0f7c8ccc8a40554519482e77
GTFalcao <gtfalcao96@gmail.com>
2025-03-26 01:04:20 -0300

    Version bumps

--- components/freshdesk/actions/create-contact/create-contact.mjs
+++ components/freshdesk/actions/create-contact/create-contact.mjs
@@ -4,7 +4,7 @@ export default {
   key: "freshdesk-create-contact",
   name: "Create a Contact",
   description: "Create a contact. [See the documentation](https://developers.freshdesk.com/api/#create_contact)",
-  version: "0.1.0",
+  version: "0.0.2",
   type: "action",
   props: {
     freshdesk,
