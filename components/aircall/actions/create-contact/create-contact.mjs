ee00be3dcdb31bad8a7d40f3b4df4c0e726e16c2
Leo Vu <vunguyenhung@outlook.com>
2025-03-27 10:39:22 +0700

    [IMPROVEMENT] Aircall - Improve phone number & email handling (#16060)
    * [Enhancement] Update Create Contact action to refine phone number handling and improve descriptions
    
    * [Enhancement] Update phone number handling in Create Contact action
    
    * Add pnpm-lock.yaml
    
    * [Enhancement] Refine email handling in Create Contact action
    
    * Update package.json
    
    * [Enhancement] Handle empty case for phoneNumbers and emails
    
    ---------
    
    Co-authored-by: Danny Roosevelt <danny@pipedream.com>

--- components/aircall/actions/create-contact/create-contact.mjs
+++ components/aircall/actions/create-contact/create-contact.mjs
@@ -3,36 +3,64 @@ import common from "../common/common-create-update.mjs";
 export default {
   ...common,
   name: "Create Contact",
-  description: "Create a contact in Aircall. [See the documentation](https://developer.aircall.io/api-references/#create-a-contact)",
+  description:
+    "Create a contact in Aircall. [See the documentation](https://developer.aircall.io/api-references/#create-a-contact)",
   key: "aircall-create-contact",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     ...common.props,
     emails: {
       type: "string[]",
       label: "Emails",
-      description: "Array of email address objects (max 20). Each should contain `label` and `value`. If a string is provided, it will be used as both the label and value.",
+      description:
+        "Array of email address objects (max 20). Each should contain `label` and `value`. If a string is provided, it will be used as both the label and value. For example, `{{ [{\"label\": \"Work\", \"value\": \"john.doe@test.com\"}] }}`, or `{{ [\"john.doe@test.com\"] }}`",
       optional: true,
     },
     phoneNumbers: {
       type: "string[]",
       label: "Phone Numbers",
-      description: "Array of phone number objects (max 20). Each should contain `label` and `value`. If a string is provided, it will be used as both the label and value.",
+      description:
+        "Array of phone number objects (max 20). Each should contain `label` and `value`. If a string is provided, it will be used as both the label and value. For example, `{{ [{\"label\": \"Work\", \"value\": \"+1 812-641-5139\"}] }}`, or `{{ [\"+1 812-641-5139\"] }}`",
     },
   },
   async run({ $ }) {
+    const refinedPhoneNumbers = (this.phoneNumbers || []).map((item) => {
+      if (typeof item === "object" && item !== null) {
+        return item;
+      }
+
+      return {
+        label: item,
+        value: item,
+      };
+    });
+    const refinedEmails = (this.emails || []).map((item) => {
+      if (typeof item === "object" && item !== null) {
+        return item;
+      }
+
+      return {
+        label: item,
+        value: item,
+      };
+    });
+
     const data = {
       ...this.getCommonData(),
-      emails: this.emails,
-      phone_numbers: this.phoneNumbers,
+      emails: refinedEmails,
+      phone_numbers: refinedPhoneNumbers,
     };
+
     const response = await this.aircall.createContact({
       $,
       data,
     });
 
-    $.export("$summary", `Successfully created contact (ID: ${response?.contact?.id})`);
+    $.export(
+      "$summary",
+      `Successfully created contact (ID: ${response?.contact?.id})`,
+    );
 
     return response;
   },
