91ce94f306ab9c2855ac5357a89086ca0e74604c
danhsiung <35384182+danhsiung@users.noreply.github.com>
2025-03-27 18:33:03 -0700

    Adding app scaffolding for nutrient_workflow_automation

---
+++ components/nutrient_workflow_automation/nutrient_workflow_automation.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "nutrient_workflow_automation",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
