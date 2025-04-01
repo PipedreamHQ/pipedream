23d260634766826d26588f58be02fbd4d747671f
Jorge Cortes <jacortesmahmud@gmail.com>
2025-04-01 10:06:24 -0500

    [Components] jenkins - New Jenkins Job Status Notification (Instant) (#16096)

--- components/jenkins/jenkins.app.mjs
+++ components/jenkins/jenkins.app.mjs
@@ -1,11 +1,82 @@
+import {
+  XMLParser, XMLBuilder,
+} from "fast-xml-parser";
+import { axios } from "@pipedream/platform";
+
+const parser = new XMLParser({
+  ignoreAttributes: false,
+});
+
+const builder = new XMLBuilder({
+  ignoreAttributes: false,
+  suppressUnpairedNode: true,
+});
+
 export default {
   type: "app",
   app: "jenkins",
-  propDefinitions: {},
+  propDefinitions: {
+    jobName: {
+      type: "string",
+      label: "Job Name",
+      description: "The name of the Jenkins job to monitor for status updates.",
+      async options() {
+        const { jobs } = await this.getApiInfo();
+        return jobs.map(({ name }) => name);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getUrl(path) {
+      return `${this.$auth.api_url}${path}`;
+    },
+    getAuth() {
+      const {
+        api_token: password,
+        username,
+      } = this.$auth;
+      return {
+        username,
+        password,
+      };
+    },
+    async _makeRequest({
+      $ = this, path, data, headers, ...args
+    } = {}) {
+      const hasXmlHeader = headers?.["Content-Type"]?.includes("xml");
+      let response;
+
+      try {
+        response = await axios($, {
+          ...args,
+          headers,
+          url: this.getUrl(path),
+          auth: this.getAuth(),
+          data: hasXmlHeader
+            ? data && builder.build(data) || undefined
+            : data,
+        });
+
+      } catch (error) {
+        console.log("Error:", error);
+        throw error;
+      }
+
+      return hasXmlHeader
+        ? parser.parse(response)
+        : response;
+    },
+    post(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        ...args,
+      });
+    },
+    getApiInfo(args = {}) {
+      return this._makeRequest({
+        path: "/api/json",
+        ...args,
+      });
     },
   },
 };
