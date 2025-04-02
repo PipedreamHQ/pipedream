c60b18af5a1eaee8168fbe3360eee043cb8298e0
michelle0927 <michelle0927@users.noreply.github.com>
2025-04-02 10:23:06 -0400

    Telegram Bot - New Message Updates (Instant) - Add optional Chat ID prop (#16071)
    * add chatId prop to message-updates
    
    * pnpm-lock.yaml
    
    * updates
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

--- components/telegram_bot_api/sources/message-updates/message-updates.mjs
+++ components/telegram_bot_api/sources/message-updates/message-updates.mjs
@@ -6,11 +6,37 @@ export default {
   key: "telegram_bot_api-message-updates",
   name: "New Message Updates (Instant)",
   description: "Emit new event each time a Telegram message is created or updated.",
-  version: "0.1.5",
+  version: "0.1.6",
   type: "source",
   dedupe: "unique",
+  props: {
+    ...base.props,
+    chatId: {
+      propDefinition: [
+        base.props.telegramBotApi,
+        "chatId",
+      ],
+      optional: true,
+    },
+  },
+  hooks: {
+    ...base.hooks,
+    async deploy() {
+      if (this.chatId) {
+        const { id } = await this.telegramBotApi.sdk().getChat(this.chatId);
+        this._setChatId(id);
+      }
+      await base.hooks.deploy.call(this);
+    },
+  },
   methods: {
     ...base.methods,
+    _getChatId() {
+      return this.db.get("chatId");
+    },
+    _setChatId(chatId) {
+      this.db.set("chatId", chatId);
+    },
     getMeta(event, message) {
       return {
         id: event.update_id,
@@ -26,6 +52,12 @@ export default {
     },
     processEvent(event) {
       const message = event.edited_message ?? event.message;
+      if (this.chatId) {
+        const chatId = this._getChatId();
+        if (chatId && chatId != message?.chat?.id) {
+          return;
+        }
+      }
       this.$emit(event, this.getMeta(event, message));
     },
   },
