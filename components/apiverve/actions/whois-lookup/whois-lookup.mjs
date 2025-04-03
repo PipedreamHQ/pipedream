49f541425b61c2fb0c69c97d210c780ef083e419
michelle0927 <michellelbergero@hotmail.com>
2025-04-03 13:36:33 -0400

    new components

---
+++ components/apiverve/actions/whois-lookup/whois-lookup.mjs
@@ -0,0 +1,30 @@
+import apiverve from "../../apiverve.app.mjs";
+
+export default {
+  key: "apiverve-whois-lookup",
+  name: "Whois Lookup",
+  description: "Check the registration of a domain name. [See the documentation](https://docs.apiverve.com/api/whoislookup)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    apiverve,
+    domain: {
+      propDefinition: [
+        apiverve,
+        "domain",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.apiverve.whoisLookup({
+      $,
+      params: {
+        domain: this.domain,
+      },
+    });
+    if (response?.status === "ok") {
+      $.export("$summary", `Successfully retrieved WHOIS records for ${this.domain}`);
+    }
+    return response;
+  },
+};
