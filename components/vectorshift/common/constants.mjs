a5b2d91737208b856446bc959f7a95fa4371c7cb
Luan Cazarine <luanhc@gmail.com>
2025-04-03 15:09:42 -0300

    New Components - vectorshift (#16023)
    * vectorshift init
    
    * [Components] vectorshift #16018
    Sources
     - New Pipeline
     - New Knowledge Base
     - New Chatbot
    
    Actions
     - Create Pipeline
     - Run Pipeline
     - Add Data To Knowledge Base
    
    * pnpm update
    
    * pnpm update
    
    * Update components/vectorshift/common/constants.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update components/vectorshift/sources/common/base.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix import
    
    * [Components] vectorshift #16018
    Sources
     - New Pipeline
     - New Knowledge Base
     - New Chatbot
    
    Actions
     - Create Pipeline
     - Run Pipeline
     - Add Data To Knowledge Base
    
    * pnpm update
    
    * some adjusts
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ components/vectorshift/common/constants.mjs
@@ -0,0 +1,15 @@
+export const DATA_TYPE_OPTIONS = [
+  "url",
+  "wikipedia",
+  "youtube",
+  "arxiv",
+  "git",
+];
+
+export const RESCRAPE_FREQUENCY_OPTIONS = [
+  "Never",
+  "Hourly",
+  "Daily",
+  "Weekly",
+  "Monthly",
+];
