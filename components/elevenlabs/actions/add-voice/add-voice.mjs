4bb82a6d0b688a200d5a89cbe9a2840243d1afc7
michelle0927 <michellelbergero@hotmail.com>
2025-04-07 11:56:08 -0400

    new components

--- components/elevenlabs/actions/add-voice/add-voice.mjs
+++ components/elevenlabs/actions/add-voice/add-voice.mjs
@@ -5,7 +5,7 @@ import elevenlabs from "../../elevenlabs.app.mjs";
 export default {
   key: "elevenlabs-add-voice",
   name: "Add Voice",
-  version: "0.0.1",
+  version: "0.0.2",
   description: "Add a voice from one or more audio files. [See the documentation](https://elevenlabs.io/docs/api-reference/add-voice)",
   type: "action",
   props: {
