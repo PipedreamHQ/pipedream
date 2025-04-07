7de6b8782f4aeb81aae9abd698c502a6fddcaa17
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-04-07 20:40:07 -0300

    Notion markdown addition (#16194)
    * add notion-to-md package
    
    * Adding markdown page retrieval
    
    * Description and clarity updates

--- components/notion/actions/retrieve-block/retrieve-block.mjs
+++ components/notion/actions/retrieve-block/retrieve-block.mjs
@@ -2,9 +2,9 @@ import notion from "../../notion.app.mjs";
 
 export default {
   key: "notion-retrieve-block",
-  name: "Retrieve Block",
-  description: "Get details of a block, which can be text, lists, media, a page, among others. [See the documentation](https://developers.notion.com/reference/retrieve-a-block)",
-  version: "0.0.6",
+  name: "Retrieve Page Content",
+  description: "Get page content as block objects or markdown. Blocks can be text, lists, media, a page, among others. [See the documentation](https://developers.notion.com/reference/retrieve-a-block)",
+  version: "0.1.0",
   type: "action",
   props: {
     notion,
@@ -13,18 +13,28 @@ export default {
         notion,
         "pageId",
       ],
-      label: "Block ID",
-      description: "Select a block or provide a block ID",
     },
     retrieveChildren: {
       type: "boolean",
-      label: "Retrieve Children",
-      description: "Retrieve all the children (recursively) for the specified block. [See the documentation](https://developers.notion.com/reference/get-block-children) for more information",
+      label: "Retrieve Content (Child Blocks)",
+      description: "Retrieve all the children (recursively) for the specified page. [See the documentation](https://developers.notion.com/reference/get-block-children) for more information",
       optional: true,
       default: false,
     },
+    retrieveMarkdown: {
+      type: "boolean",
+      label: "Retrieve as Markdown",
+      description: "Return the page content as markdown instead of block objects.",
+      optional: true,
+    },
   },
   async run({ $ }) {
+    if (this.retrieveMarkdown) {
+      const response = await this.notion.getPageAsMarkdown(this.blockId);
+      $.export("$summary", "Successfully retrieved page as markdown");
+      return response;
+    }
+
     const block = await this.notion.retrieveBlock(this.blockId);
     if (this.retrieveChildren) {
       block.children = await this.notion.retrieveBlockChildren(block);
