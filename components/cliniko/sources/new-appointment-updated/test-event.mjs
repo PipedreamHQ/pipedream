7db2dc4193f9ebd662ea66333a34375a33a02ff3
Luan Cazarine <luanhc@gmail.com>
2025-04-07 22:50:57 -0300

    Merging pull request #16185
    * [TRIGGER] Cliniko #12462
    Sources
     - New Appointment Updated
    
    * pnpm update
    
    * pnpm update
    
    * pnpm update
    
    * Update components/cliniko/sources/new-appointment-updated/new-appointment-updated.mjs
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/cliniko/sources/new-appointment-updated/test-event.mjs
@@ -0,0 +1,78 @@
+export default {
+  "appointment_type": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/appointment_types/1"
+    }
+  },
+  "archived_at": "2019-08-24T14:15:22Z",
+  "attendees": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/individual_appointments/1/attendees"
+    }
+  },
+  "booking_ip_address": "10.0.0.43",
+  "business": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/businesses/1"
+    }
+  },
+  "cancellation_note": "string",
+  "cancellation_reason": 0,
+  "cancellation_reason_description": "Feeling Better",
+  "cancelled_at": "2019-08-24T14:15:22Z",
+  "conflicts": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/individual_appointments/1/conflicts"
+    }
+  },
+  "created_at": "2019-08-24T14:15:22Z",
+  "deleted_at": "2019-08-24T14:15:22Z",
+  "did_not_arrive": true,
+  "email_reminder_sent": true,
+  "ends_at": "2019-08-24T14:15:22Z",
+  "has_patient_appointment_notes": true,
+  "id": "string",
+  "invoice_status": 0,
+  "links": {
+    "self": "https://api.au1.cliniko.com/v1/individual_appointments/1"
+  },
+  "notes": "string",
+  "online_booking_policy_accepted": true,
+  "patient": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/patients/1"
+    }
+  },
+  "patient_arrived": true,
+  "patient_case": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/patient_cases/1"
+    }
+  },
+  "patient_name": "string",
+  "practitioner": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/practitioners/1"
+    }
+  },
+  "repeat_rule": {
+    "number_of_repeats": 0,
+    "repeat_type": "Daily",
+    "repeating_interval": 0
+  },
+  "repeated_from": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/individual_appointments/1"
+    }
+  },
+  "repeats": {
+    "links": {
+      "self": "https://api.au1.cliniko.com/v1/individual_appointments?q[]=repeated_from_id:=1"
+    }
+  },
+  "sms_reminder_sent": true,
+  "starts_at": "2019-08-24T14:15:22Z",
+  "telehealth_url": "http://example.com",
+  "treatment_note_status": 0,
+  "updated_at": "2019-08-24T14:15:22Z"
+}
