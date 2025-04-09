6c610daac0f7a8b6dd3cde508e805725b15530de
michelle0927 <michellelbergero@hotmail.com>
2025-04-09 14:22:02 -0400

    new components

---
+++ components/adyntel/actions/meta-ad-search/meta-ad-search.mjs
@@ -0,0 +1,37 @@
+import adyntel from "../../adyntel.app.mjs";
+
+export default {
+  key: "adyntel-meta-ad-search",
+  name: "Meta Ad Search",
+  description: "Searches the Meta ad library. [See the documentation](https://docs.adyntel.com/ad-libraries/meta-ad-search)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    adyntel,
+    keyword: {
+      propDefinition: [
+        adyntel,
+        "keyword",
+      ],
+    },
+    countryCode: {
+      propDefinition: [
+        adyntel,
+        "countryCode",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.adyntel.metaAdSearch({
+      $,
+      data: {
+        keyword: this.keyword,
+        country_code: this.countryCode,
+      },
+    });
+    $.export("$summary", `Successfully performed Meta ad search with keyword: \`${this.keyword}\`${this.countryCode
+      ? ` and country code: \`${this.countryCode}\``
+      : ""}`);
+    return response;
+  },
+};
