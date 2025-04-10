1de92735efc064e2510cfd546562d949b480fdae
Lucas Caresia <lucascarezia@gmail.com>
2025-04-10 08:44:25 -0300

    Added actions

---
+++ components/swell/actions/create-account/create-account.mjs
@@ -0,0 +1,56 @@
+import app from "../../swell.app.mjs";
+
+export default {
+  key: "swell-create-account",
+  name: "Create Account",
+  description: "Create a new customer account. [See the documentation](https://developers.swell.is/backend-api/accounts/create-an-account)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    firstName: {
+      propDefinition: [
+        app,
+        "firstName",
+      ],
+    },
+    lastName: {
+      propDefinition: [
+        app,
+        "lastName",
+      ],
+    },
+    phone: {
+      propDefinition: [
+        app,
+        "phone",
+      ],
+    },
+    notes: {
+      propDefinition: [
+        app,
+        "notes",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createAccount({
+      $,
+      data: {
+        email: this.email,
+        first_name: this.firstName,
+        last_name: this.lastName,
+        phone: this.phone,
+        notes: this.notes,
+      },
+    });
+    $.export("$summary", "Successfully created account with ID: " + response.id);
+    return response;
+  },
+};
