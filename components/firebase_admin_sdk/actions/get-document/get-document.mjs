d5a79f866dd86a445b1366b020392bc748e317aa
Jorge Cortes <jacortesmahmud@gmail.com>
2025-04-11 18:29:35 -0500

    [ACTION] Get Firebase Document

---
+++ components/firebase_admin_sdk/actions/get-document/get-document.mjs
@@ -0,0 +1,66 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "firebase_admin_sdk-get-document",
+  name: "Get Document",
+  description: "Retrieves a document from a Firestore collection. [See the documentation](https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    ...common.props,
+    collection: {
+      propDefinition: [
+        common.props.firebase,
+        "collection",
+        (c) => ({
+          region: c.databaseRegion,
+        }),
+      ],
+    },
+    document: {
+      propDefinition: [
+        common.props.firebase,
+        "document",
+        (c) => ({
+          collection: c.collection,
+          region: c.databaseRegion,
+        }),
+      ],
+    },
+  },
+  methods: {
+    ...common.methods,
+    async getResponse() {
+      const docRef = this.firebase.getDocument({
+        collection: this.collection,
+        document: this.document,
+      });
+      const snapshot = await docRef.get();
+
+      if (!snapshot.exists) {
+        return {
+          exists: false,
+          id: this.document,
+        };
+      }
+
+      return {
+        exists: true,
+        id: snapshot.id,
+        data: snapshot.data(),
+        createTime: snapshot.createTime?.toDate(),
+        updateTime: snapshot.updateTime?.toDate(),
+        readTime: snapshot.readTime?.toDate(),
+        ref: snapshot.ref.path,
+      };
+    },
+    emitSummary($, response) {
+      if (!response.exists) {
+        $.export("$summary", `Document ${this.document} not found in collection ${this.collection}`);
+        return;
+      }
+      $.export("$summary", `Successfully retrieved document ${this.document} from collection ${this.collection}`);
+    },
+  },
+};
