30f6165155e66c4252521321650a8c7d157e2bdd
michelle0927 <michelle0927@users.noreply.github.com>
2025-04-15 10:29:07 -0400

    Rumble - New Live Stream (#16285)
    * new-live-stream
    
    * pnpm-lock.yaml

---
+++ components/rumble/sources/new-live-stream/test-event.mjs
@@ -0,0 +1,25 @@
+export default {
+  "id": "6pw8hx",
+  "title": "Livestream",
+  "created_on": "2025-04-14T17:07:27+00:00",
+  "is_live": true,
+  "visibility": "unlisted",
+  "categories": {
+    "primary": {
+      "slug": "technology",
+      "title": "Technology"
+    },
+    "secondary": null
+  },
+  "server_url": "",
+  "stream_key": "",
+  "likes": 0,
+  "dislikes": 0,
+  "watching_now": 0,
+  "chat": {
+    "latest_message": null,
+    "recent_messages": [],
+    "latest_rant": null,
+    "recent_rants": []
+  }
+}
