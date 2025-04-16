e84d0d42c48b72e01b928d49b3062756b3bccb43
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-04-16 00:30:16 -0300

    New Gmail action: 'Archive Email' (#16276)
    * package update
    
    * Creating 'archive email' action
    
    ---------
    
    Co-authored-by: Danny Roosevelt <danny@pipedream.com>

---
+++ components/gmail/actions/archive-email/archive-email.mjs
@@ -0,0 +1,35 @@
+import gmail from "../../gmail.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "gmail-archive-email",
+  name: "Archive Email",
+  description: "Archive an email message. [See the documentation](https://developers.google.com/gmail/api/reference/rest/v1/users.messages/modify)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gmail,
+    message: {
+      propDefinition: [
+        gmail,
+        "message",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const {
+      gmail,
+      message,
+    } = this;
+
+    const response = await gmail.updateLabels({
+      message,
+      removeLabelIds: [
+        constants.INBOX_LABEL_ID,
+      ],
+    });
+
+    $.export("$summary", `Successfully archived email (ID: ${message})`);
+    return response;
+  },
+};
