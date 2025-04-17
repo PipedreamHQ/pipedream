572de72af34343c3cb608483995a0a348190d100
Lucas Caresia <lucascarezia@gmail.com>
2025-04-17 08:28:27 -0300

    Added actions

---
+++ components/goodbits/actions/create-link/create-link.mjs
@@ -0,0 +1,56 @@
+import app from "../../goodbits.app.mjs";
+
+export default {
+  key: "goodbits-create-link",
+  name: "Create Link",
+  description: "Create a new link. [See the documentation](https://support.goodbits.io/article/115-goodbit-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    url: {
+      propDefinition: [
+        app,
+        "url",
+      ],
+    },
+    title: {
+      propDefinition: [
+        app,
+        "title",
+      ],
+    },
+    description: {
+      propDefinition: [
+        app,
+        "description",
+      ],
+    },
+    fetchRemoteThumbnailUrl: {
+      propDefinition: [
+        app,
+        "fetchRemoteThumbnailUrl",
+      ],
+    },
+    imageCandidates: {
+      propDefinition: [
+        app,
+        "imageCandidates",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.createLink({
+      $,
+      data: {
+        url: this.url,
+        title: this.title,
+        description: this.description,
+        fetch_remote_thumbnail_url: this.fetchRemoteThumbnailUrl,
+        image_candidates: this.imageCandidates,
+      },
+    });
+    $.export("$summary", "Successfully created new link");
+    return response;
+  },
+};
---
+++ components/goodbits/actions/update-subscriber-status/update-subscriber-status.mjs
@@ -0,0 +1,35 @@
+import app from "../../goodbits.app.mjs";
+
+export default {
+  key: "goodbits-update-subscriber-status",
+  name: "Update Subscriber Status",
+  description: "Update the status of a subscriber. [See the documentation](https://support.goodbits.io/article/115-goodbit-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    status: {
+      propDefinition: [
+        app,
+        "status",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.unsubscribeSubscriber({
+      $,
+      email: this.email,
+      data: {
+        status: this.status,
+      },
+    });
+    $.export("$summary", "Successfully uptated subscriber status");
+    return response;
+  },
+};
