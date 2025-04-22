01bc71d0d584cb77cac9cd7caa3205372179645d
Luan Cazarine <luanhc@gmail.com>
2025-04-22 18:28:20 -0300

    16246 feature update pipedrive to use webhooks v2 (#16297)
    * [FEATURE] Update Pipedrive to use Webhooks v2 #16246
    Sources
     - New Deal (Instant)
     - New Person (Instant)
     - New Deal Updated (Instant)
     - New Person Updated (Instant)
    
    * pnpm update
    
    * change source name
    
    * pnpm update

--- components/pipedrive/sources/updated-deal-instant/updated-deal-instant.mjs
+++ components/pipedrive/sources/updated-deal-instant/updated-deal-instant.mjs
@@ -6,19 +6,19 @@ export default {
   key: "pipedrive-updated-deal-instant",
   name: "New Deal Update (Instant)",
   description: "Emit new event when a deal is updated.",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "source",
   dedupe: "unique",
   methods: {
     ...common.methods,
     getExtraData() {
       return {
-        event_action: "updated",
+        event_action: "change",
         event_object: "deal",
       };
     },
     getSummary(body) {
-      return `Deal successfully updated: ${body.current.id}`;
+      return `Deal successfully updated: ${body.data.id}`;
     },
   },
   sampleEmit,
