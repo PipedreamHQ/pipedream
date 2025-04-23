a528d04b389b1dc734069425773536d44f60e6ee
Jorge Cortes <jacortesmahmud@gmail.com>
2025-04-23 14:38:43 -0500

    [Components] attio - added new components

--- components/attio/common/utils.mjs
+++ components/attio/common/utils.mjs
@@ -1,3 +1,5 @@
+import constants from "./constants.mjs";
+
 async function streamIterator(stream) {
   let resources = [];
   for await (const resource of stream) {
@@ -33,30 +35,97 @@ async function *paginate({
   } while (total === args.params.limit);
 }
 
-function parseValues(attributes, values) {
-  for (const [
-    key,
-    value,
-  ] of Object.entries(values)) {
+function toPascalCase(str) {
+  return str.replace(/(\w)(\w*)/g, (_, group1, group2) =>
+    group1.toUpperCase() + group2.toLowerCase());
+}
+
+function getMetadataProp({
+  index, fieldName, prefix, label,
+} = {}) {
+  const fieldIdx = index + 1;
+  const key = `${fieldName}${fieldIdx}`;
+  return {
+    prefix: prefix
+      ? `${prefix}${key}${constants.SEP}`
+      : `${key}${constants.SEP}`,
+    label: label
+      ? `${label} - ${toPascalCase(fieldName)} ${fieldIdx}`
+      : `${toPascalCase(fieldName)} ${fieldIdx}`,
+  };
+}
+
+function getFieldProps({
+  index, fieldName, prefix,
+  propsMapper = function propsMapper(prefix) {
+    const { [`${prefix}name`]: name } = this;
+    return {
+      name,
+    };
+  },
+} = {}) {
+  const { prefix: metaPrefix } = getMetadataProp({
+    index,
+    fieldName,
+    prefix,
+  });
+  return propsMapper(metaPrefix);
+}
+
+function getFieldsProps({
+  numberOfFields, fieldName, propsMapper, prefix,
+} = {}) {
+  return Array.from({
+    length: numberOfFields,
+  }).map((_, index) => getFieldProps({
+    index,
+    fieldName,
+    prefix,
+    propsMapper,
+  }));
+}
+
+async function getAdditionalProps({
+  numberOfFields, fieldName, prefix, label,
+  getPropDefinitions = async ({
+    prefix, label,
+  }) => ({
+    [`${prefix}name`]: {
+      type: "string",
+      label,
+      description: "The name of the field.",
+      optional: true,
+    },
+  }),
+} = {}) {
+  return Array.from({
+    length: numberOfFields,
+  }).reduce(async (reduction, _, index) => {
     const {
-      type, is_multiselect: isMultiselect,
-    } = attributes.find(({ id }) => id.attribute_id === key);
-    if (type === "checkbox") {
-      values[key] = isMultiselect
-        ? value.map((v) => !(v === "false" || v === "0"))
-        : !(value === "false" || value === "0");
-    }
-    if (type === "number" || type === "rating") {
-      values[key] = isMultiselect
-        ? value.map((v) => +v)
-        : +value;
-    }
-  }
-  return values;
+      prefix: metaPrefix,
+      label: metaLabel,
+    } = getMetadataProp({
+      index,
+      fieldName,
+      prefix,
+      label,
+    });
+
+    return {
+      ...await reduction,
+      ...await getPropDefinitions({
+        prefix: metaPrefix,
+        label: metaLabel,
+      }),
+    };
+  }, {});
 }
 
 export default {
   streamIterator,
   paginate,
-  parseValues,
+  getAdditionalProps,
+  getFieldProps,
+  getFieldsProps,
+  getMetadataProp,
 };
