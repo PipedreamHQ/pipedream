763bfebfa5cadf40e774f19bbe7dea410887fceb
Dan Hsiung <dan@pipederam.com>
2025-04-24 10:50:11 -0700

    lint errors

--- packages/connect-react/src/hooks/form-context.tsx
+++ packages/connect-react/src/hooks/form-context.tsx
@@ -183,6 +183,7 @@ export const FormContextProvider = <T extends ConfigurableProps>({
         // Get current value or default
         const value = configuredProps[prop.name as keyof ConfiguredProps<T>] ||
                      ("default" in prop && prop.default != null ? prop.default : undefined);
+
         // Call setConfiguredProp to trigger reload
         setConfiguredProp(idx, value);
       }
