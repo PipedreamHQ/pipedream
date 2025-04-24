8ca823ae725f78cca340b604277023c4aefde0f9
Danny Roosevelt <danny@pipedream.com>
2025-04-24 10:28:11 -0700

    Improve UUID generation
    Replace custom UUI generation w/ crypto.randomUUID()

--- docs-v2/components/GlobalConnectProvider.jsx
+++ docs-v2/components/GlobalConnectProvider.jsx
@@ -20,13 +20,7 @@ import {
  * Generate a UUID v4 for use as external_user_id
  */
 function generateUUID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0;
-    const v = c === "x"
-      ? r
-      : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+  return crypto.randomUUID();
 }
 
 // Create the context
