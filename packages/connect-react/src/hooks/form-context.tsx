bf3452d7458234f25a448df2964eb1a342b60e2e
Dan Hsiung <dan@pipederam.com>
2025-04-24 11:52:54 -0700

    remove trailing whitespace

--- packages/connect-react/src/hooks/form-context.tsx
+++ packages/connect-react/src/hooks/form-context.tsx
@@ -182,8 +182,8 @@ export const FormContextProvider = <T extends ConfigurableProps>({
       if (prop.hidden && prop.reloadProps) {
         // Get current value or default
         const value = configuredProps[prop.name as keyof ConfiguredProps<T>] ||
-                      ("default" in prop && prop.default != null 
-                        ? prop.default 
+                      ("default" in prop && prop.default != null
+                        ? prop.default
                         : undefined);
         // Call setConfiguredProp to trigger reload
         setConfiguredProp(idx, value);
