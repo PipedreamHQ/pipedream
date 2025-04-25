4212f14f62a2f7796bee86cbf2a8fa4e86d33e6b
michelle0927 <michelle0927@users.noreply.github.com>
2025-04-25 10:42:08 -0400

    Notion API Key - migrate components from Notion (#16412)
    * migrated components
    
    * pnpm-lock.yaml
    
    * fix filename

---
+++ components/notion_api_key/actions/duplicate-page/duplicate-page.mjs
@@ -0,0 +1,22 @@
+import app from "../../notion_api_key.app.mjs";
+import common from "@pipedream/notion/actions/duplicate-page/duplicate-page.mjs";
+
+import { adjustPropDefinitions } from "../../common/utils.mjs";
+
+const {
+  name, description, type, ...others
+} = common;
+const props = adjustPropDefinitions(others.props, app);
+
+export default {
+  ...others,
+  key: "notion_api_key-duplicate-page",
+  version: "0.0.1",
+  name,
+  description,
+  type,
+  props: {
+    notion: app,
+    ...props,
+  },
+};
