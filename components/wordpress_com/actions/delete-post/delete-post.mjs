5f1b0808e8aef73acbc75b841291d0407242538e
SokolovskyiK <sokolovskyi.engineer@gmail.com>
2025-04-29 18:32:20 +0400

    [COMPONENTS] wordpress_com. Actions + sources (#16461)
    * Initialization
    
    * Add [COMPONENTS] Wordpress.com
    
    * CodeRabbit change
    
    * update bnpm-lock.yaml
    
    * CodeRabbit change 2
    
    * Lint: fix eslint errors in WordPress.com components
    
    * Fix keys
    
    * Minor fix
    
    * updates
    
    * doc link
    
    ---------
    
    Co-authored-by: michelle0927 <michellelbergero@hotmail.com>

---
+++ components/wordpress_com/actions/delete-post/delete-post.mjs
@@ -0,0 +1,54 @@
+import wordpress from "../../wordpress_com.app.mjs";
+
+export default {
+  key: "wordpress_com-delete-post",
+  name: "Delete Post",
+  description: "Deletes a post. [See the documentation](https://developer.wordpress.com/docs/api/1.1/post/sites/%24site/posts/%24post_ID/delete/)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    wordpress,
+    site: {
+      propDefinition: [
+        wordpress,
+        "siteId",
+      ],
+    },
+    postId: {
+      propDefinition: [
+        wordpress,
+        "postId",
+        (c) => ({
+          site: c.site,
+        }),
+      ],
+      description: "The ID of the post you want to delete",
+    },
+  },
+  async run({ $ }) {
+    const warnings = [];
+
+    const {
+      site,
+      wordpress,
+      postId,
+    } =  this;
+
+    warnings.push(...wordpress.checkDomainOrId(site));
+
+    let response;
+
+    try {
+      response = await wordpress.deleteWordpressPost({
+        $,
+        site,
+        postId,
+      });
+    } catch (error) {
+      wordpress.throwCustomError("Could not delete post", error, warnings);
+    };
+
+    $.export("$summary", `Post ID = ${response?.ID} successfully deleted.` + "\n- "  + warnings.join("\n- "));
+  },
+};
+
