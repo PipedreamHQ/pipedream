e4dca751123234b11a1bd764715c8dcb34da0a8e
Danny Roosevelt <danny@pipedream.com>
2025-04-30 10:55:54 -0700

    Update 404.tsx

--- docs-v2/pages/404.tsx
+++ docs-v2/pages/404.tsx
@@ -1,16 +1,23 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function Custom404() {
   const router = useRouter()
-  
+
   useEffect(() => {
     // 302 redirect (temporary) to home page
-    router.replace('/')
-  }, [router])
-  
+    router.replace("/")
+  }, [
+    router,
+  ])
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}>
       <div>
         <h1>Page not found</h1>
         <p>Redirecting to home page...</p>
