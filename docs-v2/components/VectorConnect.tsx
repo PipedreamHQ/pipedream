09173e3cedec6bdd472b42ca2876e867246f41b3
Danny Roosevelt <danny@pipedream.com>
2025-05-02 15:20:02 -0700

    Update VectorConnect.tsx

--- docs-v2/components/VectorConnect.tsx
+++ docs-v2/components/VectorConnect.tsx
@@ -5,8 +5,15 @@ export default function VectorConnect() {
     <Script strategy="lazyOnload">
       {
         `
-        !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
-        vector.load("d354cbca-d3d5-4395-9955-92b515283489");
+        // Script for loading Vector with ID d354cbca-d3d5-4395-9955-92b515283489
+        // If Vector is already initialized, just call load with our ID
+        if (typeof window !== "undefined" && window.vector) {
+          window.vector.load("d354cbca-d3d5-4395-9955-92b515283489");
+        } else {
+          // Otherwise initialize Vector as normal
+          !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
+          window.vector && window.vector.load("d354cbca-d3d5-4395-9955-92b515283489");
+        }
         `
       }
     </Script>
