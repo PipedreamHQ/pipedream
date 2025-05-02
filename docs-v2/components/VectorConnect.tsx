30290b9b4baa62596ece5b19a02e65ebec808358
Danny Roosevelt <danny@pipedream.com>
2025-05-02 15:48:20 -0700

    Cleaning up the scripts

--- docs-v2/components/VectorConnect.tsx
+++ docs-v2/components/VectorConnect.tsx
@@ -11,8 +11,9 @@ export default function VectorConnect() {
           window.vector.load("d354cbca-d3d5-4395-9955-92b515283489");
         } else {
           // Otherwise initialize Vector as normal
-          !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
-          window.vector && window.vector.load("d354cbca-d3d5-4395-9955-92b515283489");
+          !function(e,r){try{if(e.vector)return;var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){}}(window,document);
+          // Then load our ID
+          typeof window !== "undefined" && window.vector && window.vector.load("d354cbca-d3d5-4395-9955-92b515283489");
         }
         `
       }
