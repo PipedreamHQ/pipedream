7ce3fbe91b25325e750679a0c72c13602aee2334
michelle0927 <michelle0927@users.noreply.github.com>
2025-05-06 10:10:47 -0400

    New Components - oanda (#16542)
    * wip
    
    * new components
    
    * pnpm-lock.yaml
    
    * update

--- components/oanda/oanda.app.mjs
+++ components/oanda/oanda.app.mjs
@@ -1,11 +1,131 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "oanda",
-  propDefinitions: {},
+  propDefinitions: {
+    isDemo: {
+      type: "boolean",
+      label: "Is Demo",
+      description: "Set to `true` if using a demo/practice account",
+    },
+    accountId: {
+      type: "string",
+      label: "Account ID",
+      description: "The identifier of an account",
+      async options({ isDemo }) {
+        const { accounts } = await this.listAccounts({
+          isDemo,
+        });
+        return accounts?.map(({ id }) => id) || [];
+      },
+    },
+    tradeId: {
+      type: "string",
+      label: "Trade ID",
+      description: "The ID of the Trade to close when the price threshold is breached",
+      async options({
+        isDemo, accountId, prevContext,
+      }) {
+        const {
+          trades, lastTransactionID,
+        } = await this.listTrades({
+          isDemo,
+          accountId,
+          params: prevContext?.beforeID
+            ? {
+              beforeID: prevContext.beforeID,
+            }
+            : {},
+        });
+        return {
+          options: trades?.map(({ id }) => id),
+          context: {
+            beforeID: lastTransactionID,
+          },
+        };
+      },
+    },
+    instrument: {
+      type: "string",
+      label: "Instrument Name",
+      description: "The instrument to filter the requested Trades by. E.g. `AUD_USD`",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl(isDemo = false) {
+      return isDemo
+        ? "https://api-fxpractice.oanda.com/v3"
+        : "https://api-fxtrade.oanda.com/v3";
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      isDemo,
+      ...otherOpts
+    }) {
+      return axios($, {
+        ...otherOpts,
+        url: `${this._baseUrl(isDemo)}${path}`,
+        headers: {
+          Authorization: `Bearer ${this.$auth.personal_token}`,
+        },
+      });
+    },
+    listAccounts(opts = {}) {
+      return this._makeRequest({
+        path: "/accounts",
+        ...opts,
+      });
+    },
+    listTrades({
+      accountId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/accounts/${accountId}/trades`,
+        ...opts,
+      });
+    },
+    listOpenTrades({
+      accountId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/accounts/${accountId}/openTrades`,
+        ...opts,
+      });
+    },
+    listOpenPositions({
+      accountId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/accounts/${accountId}/openPositions`,
+        ...opts,
+      });
+    },
+    listTransactions({
+      accountId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/accounts/${accountId}/transactions/idrange`,
+        ...opts,
+      });
+    },
+    getHistoricalPrices({
+      accountId, instrument, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/accounts/${accountId}/instruments/${instrument}/candles`,
+        ...opts,
+      });
+    },
+    createOrder({
+      accountId, ...opts
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/accounts/${accountId}/orders`,
+        ...opts,
+      });
     },
   },
 };
