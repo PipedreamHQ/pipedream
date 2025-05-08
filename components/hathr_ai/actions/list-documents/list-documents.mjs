3b0ec5a538004f5c4dbc1fef6d3eb211943cbedd
michelle0927 <michellelbergero@hotmail.com>
2025-05-08 15:23:55 -0400

    new components

---
+++ components/hathr_ai/actions/list-documents/list-documents.mjs
@@ -0,0 +1,21 @@
+import hathrAi from "../../hathr_ai.app.mjs";
+
+export default {
+  key: "hathr_ai-list-documents",
+  name: "List Documents",
+  description: "Retrieves a list of all available documents. [See the documentation](https://drive.google.com/drive/folders/1jtoSXqzhe-iwf9kfUwTCVQBu4iXVJO2x?usp=sharing)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    hathrAi,
+  },
+  async run({ $ }) {
+    const { response: { documents } } = await this.hathrAi.listDocuments({
+      $,
+    });
+    $.export("$summary", `Successfully retrieved ${documents.length} document${documents.length === 1
+      ? ""
+      : "s"}`);
+    return documents;
+  },
+};
