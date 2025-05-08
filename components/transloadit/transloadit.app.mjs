77086a991f4f6d90ad9b83b76fa4508686040099
Luan Cazarine <luanhc@gmail.com>
2025-05-08 16:37:13 -0300

    Update components/transloadit/transloadit.app.mjs
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/transloadit/transloadit.app.mjs
+++ components/transloadit/transloadit.app.mjs
@@ -13,7 +13,7 @@ export default {
         const { items } = await this.listTemplates({
           params: {
             page: page * LIMIT,
-            pageSize: LIMIT,
+            pagesize: LIMIT,
           },
         });
 
