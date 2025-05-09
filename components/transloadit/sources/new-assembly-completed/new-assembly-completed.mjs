0c5ad79dc8c56626384decbdcb9509853e05e162
Luan Cazarine <luanhc@gmail.com>
2025-05-09 09:52:04 -0300

    New Components - transloadit (#16579)
    * transloadit init
    
    * [Components] transloadit #16556
    Sources
     - New Assembly Completed
     - New Assembly Error
    
    Actions
     - Create Assembly
     - Cancel Assembly
     - Get Assembly Status
    
    * pnpm update
    
    * some adjusts
    
    * Update components/transloadit/transloadit.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/transloadit/transloadit.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

---
+++ components/transloadit/sources/new-assembly-completed/new-assembly-completed.mjs
@@ -0,0 +1,22 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "transloadit-new-assembly-completed",
+  name: "New Assembly Completed",
+  description: "Emit new event when a Transloadit assembly finishes processing. [See the documentation](https://transloadit.com/docs/api/assemblies-get/)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getType() {
+      return "completed";
+    },
+    getSummary(item) {
+      return `New assembly completed with ID: ${item.id}`;
+    },
+  },
+  sampleEmit,
+};
