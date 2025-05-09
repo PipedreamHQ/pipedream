3dc9601d6a227151d16d4c3615ae44b036243ce7
michelle0927 <michelle0927@users.noreply.github.com>
2025-05-09 10:37:31 -0400

    New Components - hathr_ai (#16592)
    * new components
    
    * pnpm-lock.yaml

---
+++ components/hathr_ai/actions/chat/chat.mjs
@@ -0,0 +1,63 @@
+import hathrAi from "../../hathr_ai.app.mjs";
+
+export default {
+  key: "hathr_ai-chat",
+  name: "Send Chat Message",
+  description: "Sends a chat message using Hathr AI. [See the documentation](https://drive.google.com/drive/folders/1jtoSXqzhe-iwf9kfUwTCVQBu4iXVJO2x?usp=sharing)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    hathrAi,
+    message: {
+      type: "string",
+      label: "Message",
+      description: "The message to send in the chat request",
+    },
+    documents: {
+      propDefinition: [
+        hathrAi,
+        "documents",
+      ],
+    },
+    temperature: {
+      type: "string",
+      label: "Temperature",
+      description: "Controls randomness (Optional, default: 0.2, range: 0-2.0)",
+      optional: true,
+    },
+    topP: {
+      type: "string",
+      label: "Top P",
+      description: "Controls diversity (Optional, default: 1.0, range: 0-1.0)",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const opts = {
+      $,
+      data: {
+        messages: [
+          {
+            role: "user",
+            text: this.message,
+          },
+        ],
+        temperature: this.temperature,
+        topP: this.topP,
+      },
+    };
+    const { response } = this.documents
+      ? await this.hathrAi.chatWithDocuments({
+        ...opts,
+        data: {
+          ...opts.data,
+          documents: this.documents,
+        },
+      })
+      : await this.hathrAi.chat(opts);
+
+    $.export("$summary", `Chat request sent successfully with message: "${this.message}"`);
+
+    return response;
+  },
+};
