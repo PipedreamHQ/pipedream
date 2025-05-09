9fb5ebb7b5a331ef8d78adb1b5ef6599b52729b6
michelle0927 <michellelbergero@hotmail.com>
2025-05-09 12:04:19 -0400

    versions

--- components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
+++ components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
@@ -7,7 +7,7 @@ export default {
   key: "hubspot-new-deal-in-stage",
   name: "New Deal In Stage",
   description: "Emit new event for each new deal in a stage.",
-  version: "0.0.27",
+  version: "0.0.28",
   dedupe: "unique",
   type: "source",
   props: {
