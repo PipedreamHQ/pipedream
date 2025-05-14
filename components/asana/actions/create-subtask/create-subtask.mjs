7991b216eaf23ea2bf1875d96a56dad414de6203
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-05-14 14:30:25 -0400

    versions

--- components/asana/actions/create-subtask/create-subtask.mjs
+++ components/asana/actions/create-subtask/create-subtask.mjs
@@ -5,7 +5,7 @@ export default {
   key: "asana-create-subtask",
   name: "Create Subtask",
   description: "Creates a new subtask and adds it to the parent task. [See the documentation](https://developers.asana.com/docs/create-a-subtask)",
-  version: "0.4.2",
+  version: "0.4.3",
   type: "action",
   props: {
     ...common.props,
