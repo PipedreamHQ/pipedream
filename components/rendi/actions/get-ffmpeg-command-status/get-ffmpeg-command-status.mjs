efb4bdb6cb7a09529ebdf8bca1f6877e64e1dc1e
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-05-19 14:52:14 -0400

    new components

---
+++ components/rendi/actions/get-ffmpeg-command-status/get-ffmpeg-command-status.mjs
@@ -0,0 +1,25 @@
+import rendi from "../../rendi.app.mjs";
+
+export default {
+  key: "rendi-get-ffmpeg-command-status",
+  name: "Get FFmpeg Command Status",
+  description: "Get the status of a previously submitted FFmpeg command. [See the documentation](https://docs.rendi.dev/api-reference/endpoint/poll-command)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    rendi,
+    commandId: {
+      type: "string",
+      label: "Command ID",
+      description: "ID of the FFmpeg command to check",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.rendi.getFfmpegCommand({
+      $,
+      commandId: this.commandId,
+    });
+    $.export("$summary", `Successfully retrieved status of FFmpeg command with ID: ${this.commandId}`);
+    return response;
+  },
+};
