b70c366fcb187b01f9469f3a65c87e8142fb2851
Luan Cazarine <luanhc@gmail.com>
2025-05-20 10:20:55 -0300

    some adjusts

--- components/scrapeless/actions/submit-scrape-job/submit-scrape-job.mjs
+++ components/scrapeless/actions/submit-scrape-job/submit-scrape-job.mjs
@@ -1,3 +1,4 @@
+import { ConfigurationError } from "@pipedream/platform";
 import { ACTOR_OPTIONS } from "../../common/constants.mjs";
 import { parseObject } from "../../common/utils.mjs";
 import scrapeless from "../../scrapeless.app.mjs";
@@ -34,29 +35,42 @@ export default {
       description: "Additional input parameters if you need to pass a specific configuration based on the actor. [See the documentation](https://apidocs.scrapeless.com/) for further details.",
       optional: true,
     },
+    asyncMode: {
+      type: "boolean",
+      label: "Async Mode",
+      description: "Whether to run the scrape job in asynchronous mode. If set to true, the job will be processed in the background.",
+    },
   },
   async run({ $ }) {
-    const data = {
-      actor: this.actor,
-      input: parseObject(this.additionalInput),
-      async: true,
-    };
-
-    if (this.inputUrl) {
-      data.input.url = this.inputUrl;
-    }
-    if (this.proxyCountry) {
-      data.proxy = {
-        country: this.proxyCountry,
+    try {
+      const data = {
+        actor: this.actor,
+        input: parseObject(this.additionalInput),
       };
+
+      if (this.asyncMode) {
+        data.async = this.asyncMode;
+      }
+      if (this.inputUrl) {
+        data.input.url = this.inputUrl;
+      }
+      if (this.proxyCountry) {
+        data.proxy = {
+          country: this.proxyCountry,
+        };
+      }
+
+      const response = await this.scrapeless.submitScrapeJob({
+        $,
+        data,
+      });
+
+      $.export("$summary", this.asyncMode
+        ? `Successfully submitted scrape job with ID: ${response.taskId}`
+        : "Successfully scraped the target configuration.");
+      return response;
+    } catch ({ response }) {
+      throw new ConfigurationError(response.data.message);
     }
-
-    const response = await this.scrapeless.submitScrapeJob({
-      $,
-      data,
-    });
-
-    $.export("$summary", `Successfully submitted scrape job with ID: ${response.taskId}`);
-    return response;
   },
 };
