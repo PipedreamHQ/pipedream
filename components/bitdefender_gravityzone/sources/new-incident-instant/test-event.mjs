1aa35fee0e18482e09888694ae0177bd41b70058
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-05-20 18:18:14 -0400

    new components

---
+++ components/bitdefender_gravityzone/sources/new-incident-instant/test-event.mjs
@@ -0,0 +1,34 @@
+export default {
+    "module": "new-incident",
+    "created": "2020-07-20T09:36:23.485Z",
+    "endpointId": "5efb3a520075db7384dfa285",
+    "companyId": "6776d4015c0c96e36a07798a",
+    "computer_id": "5efb3a520075db7384dfa286",
+    "computer_fqdn": "desktop-jac14gs",
+    "computer_name": "DESKTOP-JAC14GS",
+    "detection_name": "ATC.Malicious",
+    "attack_types": [
+      "Other"
+    ],
+    "computer_ip": "10.17.23.30",
+    "severityScore": 90,
+    "incident_id": "5f1557cbe7b2584f3959ee19",
+    "attack_entry": 1688239188,
+    "parent_process_path": "c:\\windows\\system32\\cmd.exe",
+    "parent_process_pid": 9636,
+    "process_path": "c:\\users\\bdadmin\\desktop\\atcsim\\atcsim32.exe",
+    "process_pid": 10324,
+    "username": "DESKTOP-JAC14GS\\bdadmin",
+    "user_sid": "S-1-5-21-3349207704-443292085-2237656896-1003",
+    "process_command_line": "detect",
+    "file_hash_md5": "ccb1b07bdf330627f02b3c832663a489",
+    "file_hash_sha256": "d5adc6a65a57d30d3ae70d195983d155e7cd24f26eb1ebebde9b92655251ec55",
+    "att_ck_id": [
+      "T1036",
+      "T1059",
+      "T1002",
+      "T1012"
+    ],
+    "severity": "high",
+    "main_action": "no action",
+  }
