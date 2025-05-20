7ac38a8f45f8fa21bcc7fb95f0c07fc4d2bc8bad
Luan Cazarine <luanhc@gmail.com>
2025-05-20 14:53:57 -0300

    New Components - mailosaur (#16666)
    * mailosaur init
    
    * [Components] mailosaur #16655
    Sources
     - New Message
     - New Message Macthing Criteira
    
    Actions
     - Create Email
     - Search Message
     - Delete Message
    
    * pnpm update
    
    * pnpm update
    
    * some adjusts
    
    * some adjusts

---
+++ components/mailosaur/actions/delete-email/delete-email.mjs
@@ -0,0 +1,37 @@
+import mailosaur from "../../mailosaur.app.mjs";
+
+export default {
+  key: "mailosaur-delete-email",
+  name: "Delete Email",
+  description: "Deletes an email from a Mailosaur server using its email ID. [See the documentation](https://mailosaur.com/docs/api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    mailosaur,
+    serverId: {
+      propDefinition: [
+        mailosaur,
+        "serverId",
+      ],
+    },
+    emailId: {
+      propDefinition: [
+        mailosaur,
+        "emailId",
+        ({ serverId }) => ({
+          serverId,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    await this.mailosaur.deleteEmail({
+      $,
+      emailId: this.emailId,
+    });
+    $.export("$summary", `Successfully deleted email with ID ${this.emailId}`);
+    return {
+      emailId: this.emailId,
+    };
+  },
+};
