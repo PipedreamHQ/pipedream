3f7a02db0c5a6349209ba2806ca44c8675ec07a7
danhsiung <35384182+danhsiung@users.noreply.github.com>
2025-05-20 18:33:05 -0700

    Adding app scaffolding for lead_identity_check

---
+++ components/lead_identity_check/lead_identity_check.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "lead_identity_check",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
