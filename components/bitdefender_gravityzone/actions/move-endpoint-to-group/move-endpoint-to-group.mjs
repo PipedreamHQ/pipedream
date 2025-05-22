6ca6b1f8c2c1d416a906e8a26e5ec404c0987a69
michelle0927 <michelle0927@users.noreply.github.com>
2025-05-22 13:54:49 -0400

    New Components - bitdefender_gravityzone (#16730)
    * new components
    
    * pnpm-lock.yaml
    
    * update

---
+++ components/bitdefender_gravityzone/actions/move-endpoint-to-group/move-endpoint-to-group.mjs
@@ -0,0 +1,40 @@
+import bitdefender from "../../bitdefender_gravityzone.app.mjs";
+
+export default {
+  key: "bitdefender_gravityzone-move-endpoint-to-group",
+  name: "Move Endpoint to Group",
+  description: "Move an endpoint to a different group. [See the documentation](https://www.bitdefender.com/business/support/en/77209-128489-moveendpoints.html)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    bitdefender,
+    endpointId: {
+      propDefinition: [
+        bitdefender,
+        "endpointId",
+      ],
+    },
+    groupId: {
+      propDefinition: [
+        bitdefender,
+        "groupId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.bitdefender.moveEndpointToGroup({
+      $,
+      data: {
+        params: {
+          endpointIds: [
+            this.endpointId,
+          ],
+          groupId: this.groupId,
+        },
+      },
+    });
+
+    $.export("$summary", `Successfully moved endpoint ${this.endpointId} to group ${this.groupId}`);
+    return response;
+  },
+};
