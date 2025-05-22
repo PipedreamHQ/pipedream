e79e0ed8b6adc12f552897b30e16294df84a389a
GTFalcao <gtfalcao96@gmail.com>
2025-05-22 16:54:45 -0300

    app/package base

--- components/utopian_labs/utopian_labs.app.mjs
+++ components/utopian_labs/utopian_labs.app.mjs
@@ -1,11 +1,14 @@
+import { UtopianLabs } from "utopianlabs";
+
 export default {
   type: "app",
   app: "utopian_labs",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _getClient() {
+      return new UtopianLabs({
+        apiKey: this.$auth.api_key,
+      });
     },
   },
 };
