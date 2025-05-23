711e202f89392e00e461a9e3405b9c8102201fe8
Luan Cazarine <luanhc@gmail.com>
2025-05-23 11:00:06 -0300

    New Components - scrapeless (#16712)
    * scrapeless init
    
    * [Components] scrapeless #16673
    Actions
     - Submit Scrape Job
     - Get Scrape Result
    
    * pnpm update
    
    * some adjusts
    
    * some adjusts

--- components/scrapeless/scrapeless.app.mjs
+++ components/scrapeless/scrapeless.app.mjs
@@ -1,11 +1,37 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "scrapeless",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.scrapeless.com/api/v1";
+    },
+    _headers() {
+      return {
+        "x-api-token": `${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    submitScrapeJob(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/scraper/request",
+        ...opts,
+      });
+    },
+    getScrapeResult({ scrapeJobId }) {
+      return this._makeRequest({
+        path: `/scraper/result/${scrapeJobId}`,
+      });
     },
   },
 };
