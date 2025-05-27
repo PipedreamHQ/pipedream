3fc65860882f6bcfe331016424f82ac227dce025
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-05-27 13:35:26 -0400

    updates, fix paths

--- components/close/sources/lead-status-changed/lead-status-changed.mjs
+++ components/close/sources/lead-status-changed/lead-status-changed.mjs
@@ -4,8 +4,8 @@ export default {
   ...common,
   key: "close-lead-status-changed",
   name: "New Lead Status Change",
-  description: "Emit new event when a Lead status is changed",
-  version: "0.1.0",
+  description: "Emit new event when a Lead's status is changed",
+  version: "0.1.1",
   type: "source",
   dedupe: "unique",
   methods: {
