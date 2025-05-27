ed71f9beccd83e427118ff6dd89d204fa8d1dfb2
michelle0927 <michelle0927@users.noreply.github.com>
2025-05-27 17:01:06 -0400

    New Components - easyship (#16818)
    * new components
    
    * pnpm-lock.yaml
    
    * updates
    
    * updates

---
+++ components/easyship/sources/warehouse-state-updated/test-event.mjs
@@ -0,0 +1,10 @@
+export default {
+    "event_type": "shipment.warehouse.state.updated",
+    "resource_type": "shipment",
+    "resource_id": "ESTEST00000",
+    "data": {
+      "easyship_shipment_id": "ESTEST00000",
+      "platform_order_number": "#100001",
+      "warehouse_state": "none"
+    }
+  }
