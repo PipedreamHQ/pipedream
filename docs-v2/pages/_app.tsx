9d73ec60434f52c1eb36763aec6047537835c955
Danny Roosevelt <danny@pipedream.com>
2025-05-27 23:00:11 -0700

    Fixing <title> and Vercel analytics (#16848)
    * Fixing title tag and Vercel analytics
    
    * Update pnpm-lock.yaml

--- docs-v2/pages/_app.tsx
+++ docs-v2/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { useRouter } from "next/router";
 
 import CR from "@/components/CR";
