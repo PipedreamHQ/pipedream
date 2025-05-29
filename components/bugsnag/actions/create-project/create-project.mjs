cac65df5e19937a1846c8a2d3cbf17c790eab249
michelle0927 <michelle0927@users.noreply.github.com>
2025-05-29 14:59:21 -0400

    New Components - bugsnag (#16866)
    * new components
    
    * pnpm-lock.yaml
    
    * package.json
    
    * pnpm-lock.yaml
    
    * update prop description
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

---
+++ components/bugsnag/actions/create-project/create-project.mjs
@@ -0,0 +1,50 @@
+import bugsnag from "../../bugsnag.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "bugsnag-create-project",
+  name: "Create Project",
+  description: "Create a new project for a specific organization in Bugsnag. [See the documentation](https://bugsnagapiv2.docs.apiary.io/#reference/projects/projects/create-a-project-in-an-organization)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    bugsnag,
+    organizationId: {
+      propDefinition: [
+        bugsnag,
+        "organizationId",
+      ],
+    },
+    name: {
+      type: "string",
+      label: "Project Name",
+      description: "The name of the project",
+    },
+    type: {
+      type: "string",
+      label: "Project Type",
+      description: "The new project's framework type",
+      options: constants.PROJECT_TYPES,
+    },
+    ignoreOldBrowsers: {
+      type: "boolean",
+      label: "Ignore Old Browsers",
+      description: "For javascript projects this will filter errors from older browsers",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.bugsnag.createProject({
+      $,
+      organizationId: this.organizationId,
+      data: {
+        name: this.name,
+        type: this.type,
+        ignore_old_browsers: this.ignoreOldBrowsers,
+      },
+    });
+
+    $.export("$summary", `Created project: ${this.name}`);
+    return response;
+  },
+};
---
+++ components/bugsnag/sources/new-release-created/test-event.mjs
@@ -0,0 +1,30 @@
+export default   {
+    "id": "5a8f7294d1befa07f361fcde",
+    "project_id": "5a907f72d1befa043987dedf",
+    "release_time": "2018-02-21T21:30:16+00:00",
+    "release_source": "unknown_source",
+    "app_version": "1.2.3",
+    "app_version_code": "1234",
+    "app_bundle_version": "1.2.3",
+    "build_label": "1.2.3",
+    "builder_name": "Daniel Deployer",
+    "build_tool": "webpack-bugsnag-plugins",
+    "errors_introduced_count": 2,
+    "errors_seen_count": 10,
+    "sessions_count_in_last_24h": 6500,
+    "total_sessions_count": 3000,
+    "unhandled_sessions_count": 60,
+    "accumulative_daily_users_seen": 5124,
+    "accumulative_daily_users_with_unhandled": 11,
+    "metadata": {},
+    "release_stage": {
+      "name": "production"
+    },
+    "source_control": {
+      "service": "github",
+      "commit_url": "",
+      "revision": "",
+      "diff_url_to_previous": ""
+    },
+    "release_group_id": "5cf9a562d1befa0021e6a600"
+  }
