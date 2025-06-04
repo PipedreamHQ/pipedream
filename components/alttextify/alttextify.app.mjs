12a4d6c33aea4fdcfaa1ba591841ac448851c912
Pavankumar Harikar <hvpavan@gmail.com>
2025-06-04 22:41:07 +0530

    Add AltTextify app with alt text generation action

--- components/alttextify/alttextify.app.mjs
+++ components/alttextify/alttextify.app.mjs
@@ -1,11 +1,34 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "alttextify",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.alttextify.net/api/v1";
     },
+    async _makeRequest({
+      $ = this,
+      path,
+      headers,
+      ...otherOpts
+    }) {
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Authorization": `Bearer ${this.$auth.api_key}`
+        },
+      });
+    },
+    async generateAltText(args) {
+      return this._makeRequest({
+        path: "/image/url",
+        method: "POST",
+        ...args,
+      });
+    },    
   },
 };
