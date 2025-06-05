2d334963d6a8967e4006a1d4cf2132e5e65cf613
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-06-05 11:04:29 -0400

    new components

---
+++ components/changes_page/actions/get-pinned-post/get-pinned-post.mjs
@@ -0,0 +1,27 @@
+import changesPage from "../../changes_page.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "changes_page-get-pinned-post",
+  name: "Get Pinned Post",
+  description: "Get the pinned post from Changes Page. [See the documentation](https://docs.changes.page/docs/api/page#get-pinned-post)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    changesPage,
+  },
+  async run({ $ }) {
+    try {
+      const post = await this.changesPage.getPinnedPost({
+        $,
+      });
+      $.export("$summary", "Successfully retrieved pinned post");
+      return post;
+    } catch (error) {
+      if (error.response.status === 404) {
+        throw new ConfigurationError("No pinned post found");
+      }
+      throw error;
+    }
+  },
+};
