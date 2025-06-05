169a3d0933dd5b1910bd9957f011b13fceaab0d9
Luan Cazarine <luanhc@gmail.com>
2025-06-05 16:41:55 -0300

    [Components] prospeo #16975 Actions  - Find Email  - Find Mobile Number  - Extract Data  - Domain Search  - Verify Email

---
+++ components/prospeo/actions/find-mobile-number/find-mobile-number.mjs
@@ -0,0 +1,39 @@
+import { ConfigurationError } from "@pipedream/platform";
+import prospeo from "../../prospeo.app.mjs";
+
+export default {
+  name: "Find Mobile Number",
+  version: "0.0.1",
+  key: "prospeo-find-mobile-number",
+  description: "Discover mobile numbers associated with a LinkedIn profile URL. [See the documentation](https://prospeo.io/api/mobile-finder)",
+  type: "action",
+  props: {
+    prospeo,
+    linkedinUrl: {
+      type: "string",
+      label: "LinkedIn URL",
+      description: "The LinkedIn profile URL.This endpoint is only compatible with public LinkedIn URL, and will not work with special IDs starting in `ACw..`, or `ACo...`",
+    },
+  },
+  async run({ $ }) {
+    try {
+      const response = await this.prospeo.findMobile({
+        $,
+        data: {
+          url: this.linkedinUrl,
+        },
+      });
+
+      $.export("$summary", `Successfully found mobile number for **${this.linkedinUrl}**`);
+
+      return response;
+    } catch ({ response }) {
+      if (response.data.message === "NO_RESULT") {
+        $.export("$summary", `No results found for **${this.linkedinUrl}**`);
+        return {};
+      } else {
+        throw new ConfigurationError(response.data.message);
+      }
+    }
+  },
+};
