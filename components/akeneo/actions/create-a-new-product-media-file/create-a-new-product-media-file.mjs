54a0c8d68af1cc5c7c55376a707a60d56a3e07f4
GTFalcao <gtfalcao96@gmail.com>
2025-06-04 22:17:53 -0300

    Akeneo and LLMWhisperer apps

--- components/akeneo/actions/create-a-new-product-media-file/create-a-new-product-media-file.mjs
+++ components/akeneo/actions/create-a-new-product-media-file/create-a-new-product-media-file.mjs
@@ -1,13 +1,13 @@
 import app from "../../akeneo.app.mjs";
-import utils from "../../common/utils.mjs";
-import { ConfigurationError } from "@pipedream/platform";
+import {
+  ConfigurationError, getFileStream,
+} from "@pipedream/platform";
 import FormData from "form-data";
-import fs from "fs";
 
 export default {
   type: "action",
   key: "akeneo-create-a-new-product-media-file",
-  version: "0.0.1",
+  version: "0.1.0",
   name: "Create A New Product Media File",
   description: "Allows you to create a new media file and associate it to an attribute value of a given product or product model. [See the docs](https://api.akeneo.com/api-reference.html#post_media_files)",
   props: {
@@ -32,18 +32,15 @@ export default {
     },
     filename: {
       type: "string",
-      label: "File",
-      description: "The file to be uploaded, please provide a file from `/tmp`. To upload a file to `/tmp` folder, please follow the doc [here](https://pipedream.com/docs/code/nodejs/working-with-files/#writing-a-file-to-tmp)",
+      label: "File Path or URL",
+      description: "The file to be uploaded. Provide either the path to a file in the `/tmp` directory (e.g. `/tmp/myFile.ext`) or a file URL. [See the documentation on working with files](https://pipedream.com/docs/workflows/building-workflows/code/nodejs/working-with-files/)",
     },
   },
   async run ({ $ }) {
     if (!this.productId && !this.productModelCode) {
       throw new ConfigurationError("Either `Product Identifier` or `Product Model Code` should be set!");
     }
-    const path = utils.checkTmp(this.filename);
-    if (!fs.existsSync(path)) {
-      throw new ConfigurationError("File does not exist!");
-    }
+
     const payload = {
       attribute: this.mediaFileAttributeCode,
       scope: null,
@@ -57,9 +54,10 @@ export default {
       payload.code = this.productModelCode;
       data.append("product_model", JSON.stringify(payload));
     }
-    const file = fs.readFileSync(path);
-    const fileParts = path.split("/");
-    data.append("file", file, fileParts[fileParts.length - 1]);
+
+    const fileStream = await getFileStream(this.filename);
+    const fileParts = this.filename.split("/");
+    data.append("file", fileStream, fileParts[fileParts.length - 1]);
     const contentLength = data.getLengthSync();
     await this.app.createProductMediaFile({
       $,
