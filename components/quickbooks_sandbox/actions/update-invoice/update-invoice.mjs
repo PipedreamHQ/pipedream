98def91c61710e8a56f2ad6d986916dd4ff7d379
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-06-11 13:53:27 -0400

    new components

---
+++ components/quickbooks_sandbox/actions/update-invoice/update-invoice.mjs
@@ -0,0 +1,23 @@
+import app from "../../quickbooks_sandbox.app.mjs";
+//import common from "@pipedream/quickbooks/actions/update-invoice/update-invoice.mjs";
+import common from "../../../quickbooks/actions/update-invoice/update-invoice.mjs";
+
+import { adjustPropDefinitions } from "../../common/utils.mjs";
+
+const {
+  name, description, type, ...others
+} = common;
+const props = adjustPropDefinitions(others.props, app);
+
+export default {
+  ...others,
+  key: "quickbooks_sandbox-update-invoice",
+  version: "0.0.1",
+  name,
+  description,
+  type,
+  props: {
+    quickbooks: app,
+    ...props,
+  },
+};
