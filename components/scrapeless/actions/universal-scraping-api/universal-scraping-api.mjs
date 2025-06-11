76ba386ea933c49566b831a81656f55f837fb71f
sunorains <ydkdsun@gmail.com>
2025-06-11 16:21:07 +0800

    [Components] Scrapeless update
    - Introduced a new README.md for Scrapeless, outlining its features and getting started guide.
    - Implemented multiple actions: Crawler, Scraping API, and Universal Scraping API, enabling users to extract data from various sources.

---
+++ components/scrapeless/actions/universal-scraping-api/universal-scraping-api.mjs
@@ -0,0 +1,87 @@
+import scrapeless from "../../scrapeless.app.mjs";
+import { countryOptions } from "../../common/constants.mjs";
+
+export default {
+  key: "scrapeless-universal-scraping-api",
+  name: "Universal Scraping API",
+  description: "Access any website at scale and say goodbye to blocks.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    scrapeless,
+    apiServer: {
+      type: "string",
+      label: "Please select a API server",
+      default: 'webUnlocker',
+      options: [
+        {
+          label: "Web Unlocker",
+          value: 'webUnlocker'
+        }
+      ],
+      reloadProps: true,
+    },
+  },
+  async run({ $ }) {
+    const { apiServer, ...rest } = this;
+
+    if (apiServer === 'webUnlocker') {
+      const submitData = {
+        actor: 'unlocker.webunlocker',
+        input: {
+          url: rest.url,
+          jsRender: rest.jsRender,
+          headless: rest.headless,
+        },
+        proxy: {
+          country: rest.country,
+        }
+      }
+      const response = await this.scrapeless.universalScrapingApi({
+        $,
+        submitData,
+        ...rest,
+      });
+
+      $.export("$summary", `Successfully retrieved scraping results for Web Unlocker`);
+      return response;
+    }
+  },
+  async additionalProps() {
+    const { apiServer } = this;
+
+    const props = {};
+
+    if (apiServer === 'webUnlocker') {
+      props.url = {
+        type: "string",
+        label: "Target URL",
+        description: "Parameter defines the URL you want to scrape.",
+      }
+
+      props.jsRender = {
+        type: "boolean",
+        label: "Js Render",
+        default: true,
+      }
+
+      props.headless = {
+        type: "boolean",
+        label: "Headless",
+        default: true,
+      }
+
+      props.country = {
+        type: "string",
+        label: "Country",
+        default: "ANY",
+        options: countryOptions.map((country) => ({
+          label: country.label,
+          value: country.value,
+        }))
+      }
+    }
+
+    return props;
+  }
+}
