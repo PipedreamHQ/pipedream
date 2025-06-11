9b71f3c7f10328ad5277af02bba254ffaca465d4
Jorge Cortes <jacortesmahmud@gmail.com>
2025-06-10 19:02:16 -0500

    [FIX] Gmail Upgraded lib version
    fixed historyId

--- components/gmail/sources/new-labeled-email/new-labeled-email.mjs
+++ components/gmail/sources/new-labeled-email/new-labeled-email.mjs
@@ -8,7 +8,7 @@ export default {
   name: "New Labeled Email",
   description: "Emit new event when a new email is labeled.",
   type: "source",
-  version: "0.0.12",
+  version: "0.0.13",
   dedupe: "unique",
   props: {
     gmail,
