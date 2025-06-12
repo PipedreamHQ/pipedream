3b26bc2ed4fc47885b7661a699c379051e64546c
joy-chanboop <chenzhaoyu1010@gmail.com>
2025-06-12 18:51:31 +0800

    [components] Scrapeless - new actions
    - Introduced a new README.md for Scrapeless, outlining its features and getting started guide.
    - Implemented multiple Scrapeless actions inclues `Crawler`, `Scraping API`, and `Universal Scraping API`.

---
+++ components/scrapeless/actions/scraping-api/scraping-api.mjs
@@ -0,0 +1,76 @@
+import scrapeless from "../../scrapeless.app.mjs";
+
+export default {
+  key: "scrapeless-scraping-api",
+  name: "Scraping API",
+  description: "Endpoints for fresh, structured data from 100+ popular sites.",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    scrapeless,
+    apiServer: {
+      type: "string",
+      label: "Please select a API server",
+      default: 'googleSearch',
+      options: [
+        {
+          label: "Google Search",
+          value: 'googleSearch'
+        }
+      ],
+      reloadProps: true,
+    },
+  },
+  async run({ $ }) {
+    const { apiServer, ...rest } = this;
+
+    if (apiServer === 'googleSearch') {
+      const submitData = {
+        actor: 'scraper.google.search',
+        input: {
+          q: rest.q,
+          hl: rest.hl,
+          gl: rest.gl,
+        }
+      }
+      const response = await this.scrapeless.scrapingApi({
+        $,
+        submitData,
+        ...rest,
+      });
+
+      $.export("$summary", `Successfully retrieved scraping results for Google Search`);
+      return response;
+    }
+  },
+  async additionalProps() {
+    const { apiServer } = this;
+
+    const props = {};
+
+    if (apiServer === 'googleSearch') {
+      props.q = {
+        type: "string",
+        label: "Search Query",
+        description: "Parameter defines the query you want to search. You can use anything that you would use in a regular Google search. e.g. inurl:, site:, intitle:. We also support advanced search query parameters such as as_dt and as_eq.",
+        default: "coffee"
+      }
+
+      props.hl = {
+        type: "string",
+        label: "Language",
+        description: "Parameter defines the language to use for the Google search. It's a two-letter language code. (e.g., en for English, es for Spanish, or fr for French).",
+        default: "en"
+      }
+
+      props.gl = {
+        type: "string",
+        label: "Country",
+        description: "Parameter defines the country to use for the Google search. It's a two-letter country code. (e.g., us for the United States, uk for United Kingdom, or fr for France).",
+        default: "us"
+      }
+    }
+
+    return props;
+  }
+}
--- components/scrapeless/scrapeless.app.mjs
+++ components/scrapeless/scrapeless.app.mjs
@@ -1,4 +1,5 @@
 import { axios } from "@pipedream/platform";
+import { isObject, log, isNullOrUnDef } from "./common/utils.mjs";
 
 export default {
   type: "app",
@@ -33,5 +34,200 @@ export default {
         path: `/scraper/result/${scrapeJobId}`,
       });
     },
+    async scrapingApi({ submitData, }) {
+      const path = '/scraper/request'
+      const requestWithSync = {
+        ...submitData,
+        async: true
+      };
+      const res = await this._makeRequest({
+        method: "POST",
+        path,
+        data: requestWithSync,
+      });
+
+      if (res.data) {
+        return res.data;
+      }
+
+      if (res?.taskId) {
+        log('Waiting for scrape result...')
+
+        while (true) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const result = await this.getScrapeResult({
+            scrapeJobId: res.taskId,
+          });
+
+          if (isObject(result) && Object.keys(result).length > 0) {
+            log('Scrape result received')
+            return result;
+          }
+
+          if (isNullOrUnDef(result)) {
+            log('Scrape result is undefined')
+            return result;
+          }
+        }
+      }
+      return res;
+    },
+    async universalScrapingApi({ submitData, }) {
+      const path = '/unlocker/request'
+      const res = await this._makeRequest({
+        method: "POST",
+        path,
+        data: submitData,
+      });
+
+      if (res.data) {
+        return res.data
+      }
+
+      return res;
+    },
+    async crawlerCrawl({ submitData, }) {
+      const path = '/crawler/crawl'
+
+      const browserOptions = {
+        "proxy_country": "ANY",
+        "session_name": "Crawl",
+        "session_recording": true,
+        "session_ttl": 900,
+      }
+
+      const data = {
+        url: submitData.url,
+        limit: submitData.limit,
+        browserOptions: browserOptions
+      }
+
+      const res = await this._makeRequest({
+        method: "POST",
+        path,
+        data,
+      })
+
+      // get job id 
+      if (res.id) {
+        log('Crawl job started')
+        return this.monitorJobStatus(res.id)
+      }
+
+      return res;
+    },
+    /**
+     * Monitor the status of a crawl job.
+     * @param {string} jobId - The ID of the crawl job.
+     * @param {number} [pollInterval=2] - The interval in seconds to poll for job status.
+     * @returns {Promise<Object>} - The status response of the crawl job.
+     */
+    async monitorJobStatus(jobId, pollInterval = 2) {
+      try {
+        while (true) {
+          let statusResponse = await this._makeRequest({
+            method: "GET",
+            path: `/crawler/crawl/${jobId}`,
+          });
+          log('Crawl job status: ', statusResponse.status)
+          if (statusResponse.status === 'completed') {
+            if ('data' in statusResponse) {
+              let data = statusResponse.data;
+              while (typeof statusResponse === 'object' && 'next' in statusResponse) {
+                if (data.length === 0) break;
+                statusResponse = await this._makeRequest({
+                  method: "GET",
+                  path: statusResponse.next,
+                });
+                data = data.concat(statusResponse.data);
+              }
+              statusResponse.data = data;
+              return statusResponse;
+            } else {
+              throw new Error('Crawl job completed but no data was returned');
+            }
+          } else if (['active', 'paused', 'pending', 'queued', 'waiting', 'scraping'].includes(statusResponse.status)) {
+            pollInterval = Math.max(pollInterval, 2);
+            await new Promise(resolve => setTimeout(resolve, pollInterval * 1000));
+          } else {
+            throw new Error(`Crawl job failed or was stopped. Status: ${statusResponse.status}`);
+          }
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    async crawlerScrape({ submitData, }) {
+      const path = '/crawler/scrape'
+      const browserOptions = {
+        "proxy_country": "ANY",
+        "session_name": "Scrape",
+        "session_recording": true,
+        "session_ttl": 900,
+      }
+
+      const data = {
+        url: submitData.url,
+        browserOptions: browserOptions
+      }
+      const res = await this._makeRequest({
+        method: "POST",
+        path,
+        data,
+      })
+
+      try {
+        const response = await this._makeRequest({
+          method: "POST",
+          path,
+          data,
+        });
+
+        if (!response.id) {
+          throw new Error('Failed to start a scrape job');
+        }
+
+        log('Scrape job started')
+
+        let pollInterval = 2;
+
+        while (true) {
+          const statusResponse = await this.checkScrapeStatus(response.id)
+          log('Scrape job status: ', statusResponse.status)
+          if (statusResponse.status !== 'scraping') {
+            return statusResponse;
+          }
+
+          pollInterval = Math.max(pollInterval, 2);
+          await new Promise(resolve => setTimeout(resolve, pollInterval * 1000));
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+
+    /**
+     * Check the status of a crawl job.
+     * @param {string} id - The ID of the crawl job.
+     * @returns {Promise<Object>} - The status response of the crawl job.
+     */
+    async checkScrapeStatus(id) {
+      if (!id) {
+        throw new Error('No scrape ID provided');
+      }
+      const url = `/crawler/scrape/${id}`;
+      try {
+        const response = await this._makeRequest({
+          method: "GET",
+          path: url,
+        });
+        return response;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+
   },
+
+
 };
