b7b190bf3f8774cc5b02b6e8a905748223189563
GTFalcao <gtfalcao96@gmail.com>
2025-06-13 03:41:04 -0300

    add: raindrop, ramp, reform

--- components/raindrop/actions/parse-bookmark-file/parse-bookmark-file.mjs
+++ components/raindrop/actions/parse-bookmark-file/parse-bookmark-file.mjs
@@ -1,58 +1,31 @@
 import raindrop from "../../raindrop.app.mjs";
-import fs from "fs";
-import got from "got";
-import stream from "stream";
-import { promisify } from "util";
+import { getFileStreamAndMetadata } from "@pipedream/platform";
 import FormData from "form-data";
 
 export default {
   key: "raindrop-parse-bookmark-file",
   name: "Parse HTML Bookmark File",
-  description: "Convert HTML bookmark file to JSON. Support Nestcape, Pocket and Instapaper file formats. [See the docs here](https://developer.raindrop.io/v1/import#parse-html-import-file)",
-  version: "0.0.7",
+  description: "Convert an HTML bookmark file to JSON. Supports Nestcape, Pocket and Instapaper file formats. [See the documentation](https://developer.raindrop.io/v1/import#parse-html-import-file)",
+  version: "0.1.0",
   type: "action",
   props: {
     raindrop,
-    fileUrl: {
-      type: "string",
-      label: "File URL",
-      description: "The URL of the HTML bookmark file you want to convert. Must specify either File URL or File Path.",
-      optional: true,
-    },
     filePath: {
       type: "string",
-      label: "File Path",
-      description: "The path to the file, e.g. /tmp/bookmarks.html . Must specify either File URL or File Path.",
-      optional: true,
+      label: "File Path or URL",
+      description: "The file to parse. Provide either a file URL or a path to a file in the `/tmp` directory (for example, `/tmp/bookmarks.html`)",
     },
   },
   async run({ $ }) {
-    const {
-      fileUrl,
-      filePath,
-    } = this;
-
-    if (!fileUrl && !filePath) {
-      throw new Error("Must specify either File URL or File Path");
-    }
-
     const form = new FormData();
-    if (filePath) {
-      if (!fs.existsSync(filePath)) {
-        throw new Error(`${filePath} does not exists`);
-      }
-      const readStream = fs.createReadStream(filePath);
-      form.append("import", readStream);
-    } else if (fileUrl) {
-      const tempFilePath = "/tmp/temp_bookmarks.html";
-      const pipeline = promisify(stream.pipeline);
-      await pipeline(
-        got.stream(fileUrl),
-        fs.createWriteStream(tempFilePath),
-      );
-      const readStream = fs.createReadStream(tempFilePath);
-      form.append("import", readStream);
-    }
+    const {
+      stream, metadata,
+    } = getFileStreamAndMetadata(this.filePath);
+    form.append("import", stream, {
+      contentType: metadata.contentType,
+      knownLength: metadata.size,
+      filename: metadata.name,
+    });
 
     const response = await this.raindrop.importFile($, form);
     $.export("$summary", "Successfully parsed bookmark file");
