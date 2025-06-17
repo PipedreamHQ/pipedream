55ef300958076bc6b1ba2cae2199f30e7d36c00c
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-06-17 16:58:40 -0400

    versions

--- components/google_drive/actions/upload-file/upload-file.mjs
+++ components/google_drive/actions/upload-file/upload-file.mjs
@@ -12,7 +12,7 @@ export default {
   key: "google_drive-upload-file",
   name: "Upload File",
   description: "Upload a file to Google Drive. [See the documentation](https://developers.google.com/drive/api/v3/manage-uploads) for more information",
-  version: "2.0.0",
+  version: "2.0.1",
   type: "action",
   props: {
     googleDrive,
