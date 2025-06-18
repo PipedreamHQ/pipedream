86341da8354c7db7e9b9aaf8dbf57ef8c8b53f00
Luan Cazarine <luanhc@gmail.com>
2025-06-18 16:21:41 -0300

    [TRIGGER] Trengo - VOICE_CALL_RECORDED #17079 Sources  - Voice Call Recorded

--- components/trengo/sources/new-outbound-message/new-outbound-message.mjs
+++ components/trengo/sources/new-outbound-message/new-outbound-message.mjs
@@ -2,9 +2,9 @@ import common from "../common/common.mjs";
 
 export default {
   key: "trengo-new-outbound-message",
-  name: "New Outbound Message Event",
+  name: "New Outbound Message Event (Instant)",
   description: "Emit new events when an outbound message sent. [See the docs here](https://developers.trengo.com/docs/webhooks)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   ...common,
---
+++ components/trengo/sources/voice-call-recorded/voice-call-recorded.mjs
@@ -0,0 +1,24 @@
+import common from "../common/common.mjs";
+
+export default {
+  key: "trengo-voice-call-recorded",
+  name: "New Voice Call Recorded Event",
+  description: "Emit new events when a voice call is recorded. [See the docs here](https://developers.trengo.com/docs/webhooks)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  ...common,
+  methods: {
+    ...common.methods,
+    getMeta(event) {
+      return {
+        id: Date.now(),
+        ts: Date.now(),
+        summary: `Voice call recorded: ${event?.body?.from} => ${event?.body?.to} (${event?.body?.duration}s)`,
+      };
+    },
+    getEvent() {
+      return "VOICE_CALL_RECORDED";
+    },
+  },
+};
