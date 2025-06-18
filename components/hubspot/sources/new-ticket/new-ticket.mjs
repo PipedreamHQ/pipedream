81100568b4b12ef5591e6953dba15cd3995e960c
michelle0927 <michelle0927@users.noreply.github.com>
2025-06-18 10:30:15 -0400

    Hubspot - New or Updated Contact Source updates (#17143)
    * update to new-or-updated-contact
    
    * pnpm-lock.yaml
    
    * versions

--- components/hubspot/sources/new-ticket/new-ticket.mjs
+++ components/hubspot/sources/new-ticket/new-ticket.mjs
@@ -9,7 +9,7 @@ export default {
   key: "hubspot-new-ticket",
   name: "New Ticket",
   description: "Emit new event for each new ticket created.",
-  version: "0.0.22",
+  version: "0.0.23",
   dedupe: "unique",
   type: "source",
   props: {
