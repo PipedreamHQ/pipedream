3053e4e4b4b2964c5ad820bfa94f9d29271abca1
Luan Cazarine <luanhc@gmail.com>
2025-06-21 16:16:39 -0300

    Add custom fields retrieval methods and enhance data parsing in Pipedrive sources
    - Introduced `getDealCustomFields` and `getPersonCustomFields` methods in the Pipedrive app for fetching custom fields.
    - Implemented `parseCustomFields` utility to map custom fields to their respective names.
    - Updated `parseData` method in common utilities to handle custom fields for both deals and persons.
    - Enhanced `parseData` methods in updated deal and person sources to utilize the new parsing logic.
    - Bumped version for updated deal and person sources to 0.1.0.

--- components/pipedrive/sources/updated-person-instant/updated-person-instant.mjs
+++ components/pipedrive/sources/updated-person-instant/updated-person-instant.mjs
@@ -1,3 +1,4 @@
+import { parseData } from "../../common/utils.mjs";
 import common from "../common/base.mjs";
 import sampleEmit from "./test-event.mjs";
 
@@ -6,7 +7,7 @@ export default {
   key: "pipedrive-updated-person-instant",
   name: "PersonUpdated (Instant)",
   description: "Emit new event when a person is updated.",
-  version: "0.0.7",
+  version: "0.1.0",
   type: "source",
   dedupe: "unique",
   methods: {
@@ -19,6 +20,12 @@ export default {
     },
     getSummary(body) {
       return `Person successfully updated: ${body.data.id}`;
+    },
+    async parseData(body) {
+      return await parseData({
+        fn: this.pipedrive.getPersonCustomFields,
+        body,
+      });
     },
   },
   sampleEmit,
