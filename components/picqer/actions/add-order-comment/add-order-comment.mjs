e53a33928393efab1f4f7687a6ff19e655011e4f
Luan Cazarine <luanhc@gmail.com>
2025-06-23 15:15:46 -0300

    New Components - picqer (#17044)
    * picqer init
    
    * [ACTION] Picqer.com MCP server #16939
    Sources
     - New Event (Instant)
    
    Actions
     - Add Order Comment
     - Add Return Comment
     - Create Order
     - Get Customer
     - Get Order
     - Get Picklist
     - Get Status Per Order Line
     - Search Orders
     - Update Order
    
    * pnpm update
    
    * Update components/picqer/sources/common/base.mjs
    
    * fix products prop
    
    * Update create-order.mjs to correct product description format and adjust idorderfield handling
    
    * pnpm update
    
    * update pnpm-lock.yaml
    
    * Refactor HMAC signature verification in base.mjs to ensure proper handling of unauthorized requests
    
    * [ACTION] New Trigger action "New Lead Update (Instant)" in Pipedrive #17171
    Sources
     - Updated Lead (Instant)
    
    * pnpm update
    
    * Revert "pnpm update"
    
    This reverts commit 406c684fe8eb2e390e0f3e14f6edffebb128cb70.
    
    * Revert "[ACTION] New Trigger action "New Lead Update (Instant)" in Pipedrive #17171"
    
    This reverts commit 530eee896bff0cd0265c863604a2a466e22a869f.
    
    ---------
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

---
+++ components/picqer/actions/add-order-comment/add-order-comment.mjs
@@ -0,0 +1,50 @@
+import picqer from "../../picqer.app.mjs";
+
+export default {
+  key: "picqer-add-order-comment",
+  name: "Add Comment To Order",
+  description: "Add a comment to an order in Picqer. [See the documentation](https://picqer.com/en/api/comments#adding-comments-to-an-order)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    picqer,
+    orderId: {
+      propDefinition: [
+        picqer,
+        "orderId",
+      ],
+    },
+    body: {
+      propDefinition: [
+        picqer,
+        "commentBody",
+      ],
+    },
+    showAtRelated: {
+      propDefinition: [
+        picqer,
+        "showAtRelated",
+      ],
+    },
+    isVisibleFulfillment: {
+      propDefinition: [
+        picqer,
+        "isVisibleFulfillment",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.picqer.addOrderComment({
+      $,
+      orderId: this.orderId,
+      data: {
+        body: this.body,
+        show_at_related: this.showAtRelated,
+        is_visible_fulfilment: this.isVisibleFulfillment,
+      },
+    });
+
+    $.export("$summary", `Successfully added comment to order ${this.orderId}`);
+    return response;
+  },
+};
