884ec8027a9ab08af2d57ea259cbb5754eca761d
michelle0927 <michelle0927@users.noreply.github.com>
2025-06-24 10:28:27 -0400

    New Components - oxylabs (#17218)
    * wip
    
    * wip
    
    * new source
    
    * pnpm-lock.yaml
    
    * update package.json
    
    * pnpm-lock.yaml
    
    * update
    
    * update

---
+++ components/oxylabs/oxylabs.app.mjs
@@ -0,0 +1,82 @@
+import { axios } from "@pipedream/platform";
+import { HttpsProxyAgent } from "https-proxy-agent";
+
+export default {
+  type: "app",
+  app: "oxylabs",
+  propDefinitions: {
+    scheduleId: {
+      type: "string",
+      label: "Schedule ID",
+      description: "The ID of the schedule to watch",
+      async options() {
+        const { schedules } = await this.listSchedules();
+        return schedules || [];
+      },
+    },
+    geoLocation: {
+      type: "string",
+      label: "Geo Location",
+      description: "The geo location to scrape from. [See the guide](https://developers.oxylabs.io/scraping-solutions/web-scraper-api/features/localization/e-commerce-localization) for using this property.",
+      optional: true,
+    },
+  },
+  methods: {
+    _getBaseUrl() {
+      return `https://${this.$auth.api_name}.oxylabs.io/v1`;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._getBaseUrl()}${path}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        auth: {
+          username: `${this.$auth.username}`,
+          password: `${this.$auth.password}`,
+        },
+        ...opts,
+      });
+    },
+    scrape(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/queries",
+        ...opts,
+      });
+    },
+    listSchedules(opts = {}) {
+      return this._makeRequest({
+        path: "/schedules",
+        ...opts,
+      });
+    },
+    async createSession({
+      $ = this, proxyUrl, ...opts
+    }) {
+      const agent = new HttpsProxyAgent(proxyUrl);
+      return axios($, {
+        url: "https://ip.oxylabs.io/location",
+        httpsAgent: agent,
+        ...opts,
+      });
+    },
+    createSchedule(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/schedules",
+        ...opts,
+      });
+    },
+    getRunsInfo({
+      scheduleId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/schedules/${scheduleId}/runs`,
+        ...opts,
+      });
+    },
+  },
+};
---
+++ components/oxylabs/sources/new-scheduled-run-completed/test-event.mjs
@@ -0,0 +1,27 @@
+export default {
+  "run_id": 25037485,
+  "jobs": [
+    {
+      "id": 7300439540206948353,
+      "create_status_code": 202,
+      "result_status": "done",
+      "created_at": "2025-02-26 09:00:21",
+      "result_created_at": "2025-02-26 09:00:23",
+    },
+    {
+      "id": 7300439540169188353,
+      "create_status_code": 202,
+      "result_status": "done",
+      "created_at": "2025-02-26 09:00:21",
+      "result_created_at": "2025-02-26 09:00:22",
+    },
+    {
+      "id": 7300439540198551553,
+      "create_status_code": 202,
+      "result_status": "done",
+      "created_at": "2025-02-26 09:00:21",
+      "result_created_at": "2025-02-26 09:00:23",
+    },
+  ],
+  "success_rate": 1,
+};
