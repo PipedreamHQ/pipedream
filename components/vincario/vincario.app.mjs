74c612891df3c8a53c41bb7ed164fcc55215b765
danhsiung <35384182+danhsiung@users.noreply.github.com>
2025-06-25 14:32:59 -0700

    Adding app scaffolding for vincario

---
+++ components/vincario/vincario.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "vincario",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
