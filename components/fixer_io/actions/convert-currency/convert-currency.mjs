a17fa5202f83adeb5aef30566f1385712fcd2587
michelle0927 <michelle0927@users.noreply.github.com>
2025-06-25 10:27:05 -0400

    New Component - fixer_io (#17178)
    * new action convert-currency
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

---
+++ components/fixer_io/actions/convert-currency/convert-currency.mjs
@@ -0,0 +1,50 @@
+import fixerIo from "../../fixer_io.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "fixer_io-convert-currency",
+  name: "Convert Currency",
+  description: "Convert amount from one currency to another using real-time exchange rates. [See the documentation](https://fixer.io/documentation)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    fixerIo,
+    from: {
+      type: "string",
+      label: "From Currency",
+      description: "The three-letter currency code of the currency you would like to convert from",
+    },
+    to: {
+      type: "string",
+      label: "To Currency",
+      description: "The three-letter currency code of the currency you would like to convert to",
+    },
+    amount: {
+      type: "string",
+      label: "Amount",
+      description: "The amount to be converted",
+    },
+    date: {
+      type: "string",
+      label: "Date",
+      description: "Specify a date (format YYYY-MM-DD) to use historical rates for this conversion",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.fixerIo.convertCurrency({
+      $,
+      params: {
+        from: this.from,
+        to: this.to,
+        amount: this.amount,
+        date: this.date,
+      },
+    });
+    if (response.error) {
+      throw new ConfigurationError(response.error.info);
+    }
+    $.export("$summary", `Successfully converted ${this.amount} ${this.from} to ${this.to}`);
+    return response;
+  },
+};
