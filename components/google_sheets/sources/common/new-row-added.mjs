3d331bc9b48b000912e421e69eb70a27d3f6daba
Andrew Chuang <andrewjschuang@gmail.com>
2025-06-25 17:02:50 -0300

    use row hash string as id

--- components/google_sheets/sources/common/new-row-added.mjs
+++ components/google_sheets/sources/common/new-row-added.mjs
@@ -28,9 +28,9 @@ export default {
     _setRowHashes(rowHashes) {
       this.db.set("rowHashes", rowHashes);
     },
-    getMeta(worksheet, rowNumber) {
+    getMeta(worksheet, rowNumber, rowHashString) {
       const ts = Date.now();
-      const id = `${worksheet.properties.sheetId}${rowNumber}`;
+      const id = rowHashString;
       const summary = `New row #${rowNumber} in ${worksheet.properties.title}`;
       return {
         id,
@@ -178,7 +178,7 @@ export default {
               worksheet,
               rowNumber,
             },
-            this.getMeta(worksheet, rowNumber),
+            this.getMeta(worksheet, rowNumber, rowHashString),
           );
         }
         this._setRowHashes(rowHashes);
