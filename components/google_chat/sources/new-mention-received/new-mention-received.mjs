6c0de7d3b1fe22aebba76fd234f768a18d7d0c3f
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-06-30 11:14:21 -0400

    new sources

---
+++ components/google_chat/sources/new-mention-received/new-mention-received.mjs
@@ -0,0 +1,32 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "google_chat-new-mention-received",
+  name: "New Mention Received",
+  description: "Emit new event when a new mention is received in a space. [See the documentation](https://developers.google.com/workspace/chat/api/reference/rest/v1/spaces.messages/list)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    memberId: {
+      propDefinition: [
+        common.props.googleChat,
+        "memberId",
+        (c) => ({
+          spaceId: c.spaceId,
+        }),
+      ],
+    },
+  },
+  methods: {
+    ...common.methods,
+    isRelevant(message) {
+      return message.formattedText.includes(`<users/${this.memberId}>`);
+    },
+    getSummary(message) {
+      return `New Mention in Message: ${message.formattedText.slice(0, 50)}`;
+    },
+  },
+};
