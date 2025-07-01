89082ad483b0c6f8c63cfcb84d7aaf2878485f42
michelle0927 <michelle0927@users.noreply.github.com>
2025-07-01 16:00:56 -0400

    New Components - add_to_calendar_pro (#17332)
    * wip
    
    * wip
    
    * actions
    
    * sources
    
    * pnpm-lock.yaml
    
    * updates
    
    * updates
    
    * fix prop

---
+++ components/add_to_calendar_pro/actions/create-landing-page-template/create-landing-page-template.mjs
@@ -0,0 +1,120 @@
+import addToCalendarPro from "../../add_to_calendar_pro.app.mjs";
+
+export default {
+  key: "add_to_calendar_pro-create-landing-page-template",
+  name: "Create Landing Page Template",
+  description: "Create a landing page template. [See the documentation](https://docs.add-to-calendar-pro.com/api/landingpages#add-a-landing-page-template)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    addToCalendarPro,
+    name: {
+      propDefinition: [
+        addToCalendarPro,
+        "landingPageTemplateName",
+      ],
+    },
+    title: {
+      propDefinition: [
+        addToCalendarPro,
+        "title",
+      ],
+    },
+    intro: {
+      propDefinition: [
+        addToCalendarPro,
+        "intro",
+      ],
+    },
+    legal: {
+      propDefinition: [
+        addToCalendarPro,
+        "legal",
+      ],
+    },
+    highlightColor: {
+      propDefinition: [
+        addToCalendarPro,
+        "highlightColor",
+      ],
+    },
+    backgroundColor1: {
+      propDefinition: [
+        addToCalendarPro,
+        "backgroundColor1",
+      ],
+    },
+    backgroundColor2: {
+      propDefinition: [
+        addToCalendarPro,
+        "backgroundColor2",
+      ],
+    },
+    background: {
+      propDefinition: [
+        addToCalendarPro,
+        "background",
+      ],
+    },
+    gradientDirection: {
+      propDefinition: [
+        addToCalendarPro,
+        "gradientDirection",
+      ],
+    },
+    imageRepeat: {
+      propDefinition: [
+        addToCalendarPro,
+        "imageRepeat",
+      ],
+    },
+    metaTitleOverride: {
+      propDefinition: [
+        addToCalendarPro,
+        "metaTitleOverride",
+      ],
+    },
+    metaDescriptionOverride: {
+      propDefinition: [
+        addToCalendarPro,
+        "metaDescriptionOverride",
+      ],
+    },
+    metaRobotsOverride: {
+      propDefinition: [
+        addToCalendarPro,
+        "metaRobotsOverride",
+      ],
+    },
+    customDomainId: {
+      propDefinition: [
+        addToCalendarPro,
+        "customDomainId",
+      ],
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.addToCalendarPro.createLandingPageTemplate({
+      $,
+      data: {
+        name: this.name,
+        title: this.title,
+        intro: this.intro,
+        legal: this.legal,
+        highlight_color: this.highlightColor,
+        background_color_1: this.backgroundColor1,
+        background_color_2: this.backgroundColor2,
+        background: this.background,
+        gradient_direction: this.gradientDirection,
+        image_repeat: this.imageRepeat,
+        meta_title_override: this.metaTitleOverride,
+        meta_description_override: this.metaDescriptionOverride,
+        meta_robots_override: this.metaRobotsOverride,
+        custom_domain: this.customDomainId,
+      },
+    });
+    $.export("$summary", "Successfully created landing page template.");
+    return response;
+  },
+};
---
+++ components/add_to_calendar_pro/actions/delete-landing-page-template/delete-landing-page-template.mjs
@@ -0,0 +1,26 @@
+import addToCalendarPro from "../../add_to_calendar_pro.app.mjs";
+
+export default {
+  key: "add_to_calendar_pro-delete-landing-page-template",
+  name: "Delete Landing Page Template",
+  description: "Delete a landing page template. [See the documentation](https://docs.add-to-calendar-pro.com/api/landingpages#delete-a-landing-page-template)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    addToCalendarPro,
+    landingPageTemplateId: {
+      propDefinition: [
+        addToCalendarPro,
+        "landingPageTemplateId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.addToCalendarPro.deleteLandingPageTemplate({
+      $,
+      landingPageTemplateId: this.landingPageTemplateId,
+    });
+    $.export("$summary", "Successfully deleted landing page template.");
+    return response;
+  },
+};
