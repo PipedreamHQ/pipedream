804d02533efc31b39218d21dff1c407736468aae
Jorge Cortes <jacortesmahmud@gmail.com>
2025-07-01 07:25:26 -0500

    [Components] appsflyer - new components

---
+++ components/appsflyer/actions/get-event-types/get-event-types.mjs
@@ -0,0 +1,35 @@
+import app from "../../appsflyer.app.mjs";
+
+export default {
+  key: "appsflyer-get-event-types",
+  name: "Get Event Types",
+  description: "Returns a list of the available event types for the specified endpoint type. [See the documentation](https://dev.appsflyer.com/hc/reference/get_event-types-attributing-entity)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    attributingEntity: {
+      type: "string",
+      label: "Attributing Entity",
+      description: "The endpoint type.",
+      options: [
+        "appsflyer",
+        "skadnetwork",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      attributingEntity,
+    } = this;
+
+    const response = await app.getEventTypes({
+      $,
+      attributingEntity,
+    });
+
+    $.export("$summary", `Successfully retrieved event types with request ID \`${response.request_id}\`.`);
+    return response;
+  },
+};
---
+++ components/appsflyer/actions/get-message-fields/get-message-fields.mjs
@@ -0,0 +1,36 @@
+import app from "../../appsflyer.app.mjs";
+
+export default {
+  key: "appsflyer-get-message-fields",
+  name: "Get Message Fields",
+  description: "Returns a list of the available message fields for each platform. [See the documentation](https://dev.appsflyer.com/hc/reference/get_fields-platform)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    platform: {
+      type: "string",
+      label: "Platform",
+      description: "The platform to retrieve message fields for.",
+      options: [
+        "ios",
+        "android",
+        "windowsphone",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      platform,
+    } = this;
+
+    const response = await app.getMessageFields({
+      $,
+      platform,
+    });
+
+    $.export("$summary", `Successfully retrieved message fields with request ID \`${response.request_id}\`.`);
+    return response;
+  },
+};
