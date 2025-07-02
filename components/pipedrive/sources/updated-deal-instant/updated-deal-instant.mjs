9f30f8bda8f111025691d89e6881446788d4fa32
michelle0927 <michelle0927@users.noreply.github.com>
2025-07-02 10:55:02 -0400

    Pipedrive - New Actions (#17387)
    * new actions
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * versions
    
    * fix descriptions

--- components/pipedrive/sources/updated-deal-instant/updated-deal-instant.mjs
+++ components/pipedrive/sources/updated-deal-instant/updated-deal-instant.mjs
@@ -7,7 +7,7 @@ export default {
   key: "pipedrive-updated-deal-instant",
   name: "Deal Updated (Instant)",
   description: "Emit new event when a deal is updated.",
-  version: "0.1.1",
+  version: "0.1.2",
   type: "source",
   dedupe: "unique",
   methods: {
