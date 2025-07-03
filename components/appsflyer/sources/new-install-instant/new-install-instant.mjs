3a8b3210aecdc74248d9cddbc708eaa2b46e4dbc
Jorge Cortes <jacortesmahmud@gmail.com>
2025-07-03 09:29:02 -0500

    [Components] appsflyer - new components (#17386)

---
+++ components/appsflyer/sources/new-install-instant/new-install-instant.mjs
@@ -0,0 +1,27 @@
+import common from "../common/webhook.mjs";
+
+export default {
+  ...common,
+  key: "appsflyer-new-install-instant",
+  name: "New Install (Instant)",
+  description: "Emit new event when a user installs an app tracked by AppsFlyer. [See docs here](https://dev.appsflyer.com/hc/reference/push-api-v2)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getEventTypes() {
+      return [
+        "install",
+        "organic-install",
+      ];
+    },
+    generateMeta(event) {
+      return {
+        summary: `New Install ${event.app_id}`,
+        id: event.event_id,
+        ts: Date.parse(event.event_time),
+      };
+    },
+  },
+};
