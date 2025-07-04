6e90be3e20fb8c976f5da455e3f0a6131a8f6faa
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-07-04 14:23:30 -0300

    LinkedIn improvements and new action (#17401)
    * pnpm
    
    * LinkedIn action improvements
    
    * Adding 'get profile picture fields' action
    
    * Improvement
    
    * removing comment
    
    * pnpm

--- components/linkedin/actions/get-member-organization-access-control/get-member-organization-access-control.mjs
+++ components/linkedin/actions/get-member-organization-access-control/get-member-organization-access-control.mjs
@@ -3,8 +3,8 @@ import linkedin from "../../linkedin.app.mjs";
 export default {
   key: "linkedin-get-member-organization-access-control",
   name: "Get Member's Organization Access Control Information",
-  description: "Gets the organization access control information of the current authenticated member. [See the docs here](https://docs.microsoft.com/en-us/linkedin/marketing/integrations/community-management/organizations/organization-access-control?context=linkedin/compliance/context#find-a-members-organization-access-control-information)",
-  version: "0.1.6",
+  description: "Gets the organization access control information of the current authenticated member. [See the documentation](https://learn.microsoft.com/en-us/linkedin/marketing/community-management/organizations/organization-access-control-by-role?view=li-lms-2025-01&tabs=http#find-a-members-organization-access-control-information)",
+  version: "1.0.0",
   type: "action",
   props: {
     linkedin,
@@ -28,32 +28,21 @@ export default {
     },
   },
   async run({ $ }) {
-    const count = 50;
-    const results = [];
-
     const params = {
       q: "roleAssignee",
       role: this.role,
       state: this.state,
       start: 0,
-      count,
+      count: 1,
     };
 
-    let done = false;
-    do {
-      const { elements } = await this.linkedin.getAccessControl({
-        $,
-        params,
-      });
-      results.push(...elements);
-      params.start += count;
-      if (elements?.length < count) {
-        done = true;
-      }
-    } while (results.length < this.max && !done);
+    const { data: { elements } } = await this.linkedin.getAccessControl({
+      $,
+      params,
+    });
 
     $.export("$summary", "Successfully retrieved access control information");
 
-    return results;
+    return elements[0];
   },
 };
