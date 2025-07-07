dfb2d1c07a5befe1f85c7aa933ddf477bf054c7d
Luan Cazarine <luanhc@gmail.com>
2025-07-07 14:33:11 -0300

    [Components] joggai #17338 Actions  - Create AI Avatar Photo  - Crate Avatar Video  - Create Product From Product Info  - Create Product From URL  - Update Product Info
    Sources
     - Video Status Changed (Instant)

---
+++ components/joggai/sources/common/base.mjs
@@ -0,0 +1,32 @@
+import joggai from "../../joggai.app.mjs";
+
+export default {
+  props: {
+    joggai,
+    db: "$.service.db",
+    http: "$.interface.http",
+  },
+  hooks: {
+    async activate() {
+      const response = await this.joggai.createHook({
+        data: {
+          events: this.getEvents(),
+          url: this.http.endpoint,
+          status: "enabled",
+        },
+      });
+      this.db.set("webhookId", response.data.endpoint_id);
+    },
+    async deactivate() {
+      const webhookId = this.db.get("webhookId");
+      await this.joggai.deleteHook(webhookId);
+    },
+  },
+  async run({ body }) {
+    this.$emit(body, {
+      id: body.event_id,
+      summary: this.getSummary(body),
+      ts: body.timestamp,
+    });
+  },
+};
