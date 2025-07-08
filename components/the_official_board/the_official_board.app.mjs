8360a4f52f4a12a00e6e6fe5069d0cd17a835686
danhsiung <35384182+danhsiung@users.noreply.github.com>
2025-07-08 14:32:58 -0700

    Adding app scaffolding for the_official_board

---
+++ components/the_official_board/the_official_board.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "the_official_board",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
