f314f607a12c3b8b9f1599366059a370fde1ad4a
Lucas Caresia <lucascarezia@gmail.com>
2025-07-09 09:17:45 -0300

    Added actions

---
+++ components/mumble/actions/add-new-label/add-new-label.mjs
@@ -0,0 +1,28 @@
+import app from "../../mumble.app.mjs";
+
+export default {
+  key: "mumble-add-new-label",
+  name: "Add New Label",
+  description: "Adds a new label. [See the documentation](https://app.mumble.co.il/mumbleapi/docs)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    labelName: {
+      propDefinition: [
+        app,
+        "labelName",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.addNewLabel({
+      $,
+      data: {
+        label_name: this.labelName,
+      },
+    });
+    $.export("$summary", "Successfully sent the request to add a new label: " + response.message);
+    return response;
+  },
+};
