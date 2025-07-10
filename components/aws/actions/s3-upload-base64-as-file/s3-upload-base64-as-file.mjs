26b2da62aaf4365a53a9a0751da0cdf82bcbdb8c
Jorge Cortes <jacortesmahmud@gmail.com>
2025-07-10 10:20:19 -0500

    [Components] Amazon Redshift - new components (#17457)

--- components/aws/actions/s3-upload-base64-as-file/s3-upload-base64-as-file.mjs
+++ components/aws/actions/s3-upload-base64-as-file/s3-upload-base64-as-file.mjs
@@ -5,7 +5,7 @@ export default {
   key: "aws-s3-upload-base64-as-file",
   name: "S3 - Upload Base64 As File",
   description: "Accepts a base64-encoded string and a filename, then uploads as a file to S3. [See the documentation](https://docs.aws.amazon.com/AmazonS3/latest/userguide/upload-objects.html)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     aws: common.props.aws,
