06eda2491b11eb407d65591e1414c63445cb97f1
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-07-10 17:30:12 -0400

    add tabId prop

--- components/google_docs/actions/get-document/get-document.mjs
+++ components/google_docs/actions/get-document/get-document.mjs
@@ -1,10 +1,11 @@
 import googleDocs from "../../google_docs.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
 
 export default {
   key: "google_docs-get-document",
   name: "Get Document",
   description: "Get the contents of the latest version of a document. [See the documentation](https://developers.google.com/docs/api/reference/rest/v1/documents/get)",
-  version: "0.1.5",
+  version: "0.1.6",
   type: "action",
   props: {
     googleDocs,
@@ -21,9 +22,31 @@ export default {
       optional: true,
       default: false,
     },
+    tabIds: {
+      type: "string[]",
+      label: "Tab IDs",
+      description: "Only return content for the specified tabs",
+      optional: true,
+      async options() {
+        const { tabs } = await this.googleDocs.getDocument(this.docId, this.includeTabsContent);
+        if (!tabs?.length) return [];
+        return tabs.map((tab) => ({
+          label: tab.tabProperties.title,
+          value: tab.tabProperties.tabId,
+        }));
+      },
+    },
   },
   async run({ $ }) {
+    if (this.tabIds?.length && !this.includeTabsContent) {
+      throw new ConfigurationError("Include Tabs Content must be true if tabIds are provided");
+    }
+
     const response = await this.googleDocs.getDocument(this.docId, this.includeTabsContent);
+
+    if (this.tabIds?.length) {
+      response.tabs = response.tabs.filter((tab) => this.tabIds.includes(tab.tabProperties.tabId));
+    }
 
     $.export("$summary", `Successfully retrieved document with ID: ${this.docId}`);
 
