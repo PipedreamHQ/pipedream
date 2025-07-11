aa28fcb5513cba0ddd25d5c759045e7fe3e037c1
michelle0927 <michelle0927@users.noreply.github.com>
2025-07-11 03:38:06 -0400

    Merging pull request #17572
    * new components
    
    * pnpm-lock.yaml
    
    * update
    
    * update

---
+++ components/freshchat/actions/send-message-in-chat/send-message-in-chat.mjs
@@ -0,0 +1,64 @@
+import freshchat from "../../freshchat.app.mjs";
+
+export default {
+  key: "freshchat-send-message-in-chat",
+  name: "Send Message in Chat",
+  description: "Sends a message in a specific conversation. [See the documentation](https://developers.freshchat.com/api/#send_message_to_conversation)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    freshchat,
+    userId: {
+      propDefinition: [
+        freshchat,
+        "userId",
+      ],
+    },
+    conversationId: {
+      propDefinition: [
+        freshchat,
+        "conversationId",
+        (c) => ({
+          userId: c.userId,
+        }),
+      ],
+    },
+    message: {
+      type: "string",
+      label: "Message",
+      description: "The content of the message to send",
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      message_parts: [
+        {
+          text: {
+            content: this.message,
+          },
+        },
+      ],
+      actor_type: "user",
+      actor_id: this.userId,
+      user_id: this.userId,
+    };
+    try {
+      await this.freshchat.sendMessageInChat({
+        $,
+        conversationId: this.conversationId,
+        data,
+      });
+    } catch {
+      // Sends message, but always returns the error
+    /*   {
+        "code": 400,
+        "status": "INVALID_VALUE",
+        "message": "com.demach.konotor.model.fragment.TextFragment cannot be cast to
+          com.demach.konotor.model.fragment.EmailFragment"
+      }
+      */
+    }
+    $.export("$summary", `Sent message in conversation ${this.conversationId}`);
+    return data;
+  },
+};
---
+++ components/freshchat/actions/update-conversation-status/update-conversation-status.mjs
@@ -0,0 +1,63 @@
+import freshchat from "../../freshchat.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "freshchat-update-conversation-status",
+  name: "Update Conversation Status",
+  description: "Updates the status of a specific conversation. [See the documentation](https://developers.freshchat.com/api/#update_a_conversation)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    freshchat,
+    userId: {
+      propDefinition: [
+        freshchat,
+        "userId",
+      ],
+    },
+    conversationId: {
+      propDefinition: [
+        freshchat,
+        "conversationId",
+        (c) => ({
+          userId: c.userId,
+        }),
+      ],
+    },
+    status: {
+      type: "string",
+      label: "Status",
+      description: "Status of the conversation",
+      options: [
+        "new",
+        "assigned",
+        "resolved",
+        "reopened",
+      ],
+    },
+    agentId: {
+      propDefinition: [
+        freshchat,
+        "agentId",
+      ],
+      description: "The ID of an agent to assign the conversation to. Required if status is `assigned`",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    if (this.status === "assigned" && !this.agentId) {
+      throw new ConfigurationError("Agent ID is required when status is `assigned`");
+    }
+
+    const response = await this.freshchat.updateConversation({
+      $,
+      conversationId: this.conversationId,
+      data: {
+        status: this.status,
+        assigned_agent_id: this.agentId,
+      },
+    });
+    $.export("$summary", `Updated conversation status to ${this.status}`);
+    return response;
+  },
+};
