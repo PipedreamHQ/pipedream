8c00f4638e9d48c92b42de02a6fb59509000bf4c
Luan Cazarine <luanhc@gmail.com>
2025-07-14 16:50:43 -0300

    [Components] indiefunnels #17406 Sources  - New Booking Session (Instant)  - New Form Submission (Instant)  - New Order (Instant)  - Contact Updated (Instant)
    Actions
     - Create Contact
     - Update Contact
     - Create Member
     - Update Member

---
+++ components/indiefunnels/sources/common/base.mjs
@@ -0,0 +1,34 @@
+import crypto from "crypto";
+import indiefunnels from "../../indiefunnels.app.mjs";
+
+export default {
+  props: {
+    indiefunnels,
+    db: "$.service.db",
+    http: "$.interface.http",
+  },
+  hooks: {
+    async activate() {
+      const response = await this.indiefunnels.createHook({
+        data: {
+          target: this.http.endpoint,
+          secret: crypto.randomUUID(),
+          events: this.getEvent(),
+        },
+      });
+      this.db.set("webhookId", response.id);
+    },
+    async deactivate() {
+      const webhookId = this.db.get("webhookId");
+      await this.indiefunnels.deleteHook(webhookId);
+    },
+  },
+  async run({ body }) {
+    const ts = body.created || Date.now();
+    this.$emit(body, {
+      id: `${body.id}-${ts}`,
+      summary: this.getSummary(body),
+      ts,
+    });
+  },
+};
---
+++ components/indiefunnels/sources/new-form-submission-instant/new-form-submission-instant.mjs
@@ -0,0 +1,23 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "indiefunnels-new-form-submission-instant",
+  name: "New Form Submission (Instant)",
+  description: "Emit new event when a form is submitted.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    getEvent() {
+      return [
+        "form_submitted",
+      ];
+    },
+    getSummary({ website }) {
+      return `New form submission from website: ${website.systemDomain}`;
+    },
+  },
+  sampleEmit,
+};
