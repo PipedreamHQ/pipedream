9306b4f7f69d15db3e5ff780bd48a6aad1dd2a54
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-07-15 13:27:03 -0400

    new components

---
+++ components/channable/sources/new-stock-update-created/new-stock-update-created.mjs
@@ -0,0 +1,27 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "channable-new-stock-update-created",
+  name: "New Stock Update Created",
+  description: "Emit new event when a new stock update is created. [See the documentation](https://api.channable.com/v1/docs#tag/stock_updates/operation/get_stock_updates_companies__company_id__projects__project_id__offers_get)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getResourceFn() {
+      return this.channable.listStockUpdates;
+    },
+    getTsField() {
+      return "created";
+    },
+    generateMeta(item) {
+      return {
+        id: item.id,
+        summary: `New stock update created: ${item.id}`,
+        ts: Date.parse(item.created),
+      };
+    },
+  },
+};
