19c3b7c095c732a99870c0dbc2adf032f2177e20
Atharva Patil <124063987+atharva-patil-23@users.noreply.github.com>
2025-07-18 20:28:23 +0530

    Fix :Update add to calendar pro component props and actions (#17678)
    * Fix(add-to-calendar-pro): Update component props and actions
    
    * versions, formatting
    
    * Update components/add_to_calendar_pro/actions/get-ics-data/get-ics-data.mjs
    
    * eslint fix
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Michelle Bergeron <michelle.bergeron@gmail.com>
    Co-authored-by: Leo Vu <vunguyenhung@outlook.com>

--- components/add_to_calendar_pro/sources/new-rsvp-answer-instant/test-event.mjs
+++ components/add_to_calendar_pro/sources/new-rsvp-answer-instant/test-event.mjs
@@ -3,7 +3,7 @@ export default {
     "status": "confirmed",
     "date_created": "2025-06-26T19:04:30.237Z",
     "date_updated": "2025-06-26T19:05:00.137Z",
-    "email": "",
+    "email": "john.doe@email.com",
     "prokey": "c6510c06-a277-4e1c-9baf-deec9c5507c7",
     "amount": 1,
     "payload": {}
