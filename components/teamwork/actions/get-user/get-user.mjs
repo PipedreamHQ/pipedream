fb4fd35df2f3529f3ec9466d58e5405a6c0b8651
michelle0927 <michelle0927@users.noreply.github.com>
2025-07-21 23:53:20 -0400

    Merging pull request #17721
    * new components
    
    * pnpm-lock.yaml
    
    * versions
    
    * updates
    
    * pnpm-lock.yaml

---
+++ components/teamwork/actions/get-user/get-user.mjs
@@ -0,0 +1,25 @@
+import app from "../../teamwork.app.mjs";
+
+export default {
+  key: "teamwork-get-user",
+  name: "Get User",
+  description: "Get a user by ID. [See the documentation](https://apidocs.teamwork.com/docs/teamwork/v1/people/get-people-id-json)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    peopleId: {
+      propDefinition: [
+        app,
+        "peopleId",
+      ],
+      label: "User ID",
+      description: "The ID of the user/person to get",
+    },
+  },
+  async run({ $ }) {
+    const user = await this.app.getPerson(this.peopleId, $);
+    $.export("$summary", `Found user ${user["first-name"]} ${user["last-name"]}`);
+    return user;
+  },
+};
