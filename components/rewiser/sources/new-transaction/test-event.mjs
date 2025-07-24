6eab05d3456353689e9b86f4d15eb2120b70690a
Luan Cazarine <luanhc@gmail.com>
2025-07-24 13:22:33 -0300

    [Components] Rewiser #17699 Actions  - Create Transaction
    Sources
     - New Transaction

---
+++ components/rewiser/sources/new-transaction/test-event.mjs
@@ -0,0 +1,11 @@
+export default {
+  id: "transaction_uuid_here",
+  name: "Notion Subscription",
+  amount: 19.99,
+  type: "Expense",
+  created_at: "2025-06-11T19:44:44.109694+00:00",
+  folder_id: "folder_uuid_here",
+  is_paid: false,
+  planned_date: "2025-06-15T22:44:00.000Z",
+  note: "Monthly subscription",
+};
