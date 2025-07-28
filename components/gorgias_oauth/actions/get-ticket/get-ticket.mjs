10e798c50229e7def46acd00bc6c76c1e26dad21
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-07-28 16:19:36 -0400

    new components

---
+++ components/gorgias_oauth/actions/get-ticket/get-ticket.mjs
@@ -0,0 +1,27 @@
+import gorgias_oauth from "../../gorgias_oauth.app.mjs";
+
+export default {
+  key: "gorgias_oauth-get-ticket",
+  name: "Get Ticket",
+  description: "Get a ticket. [See the documentation](https://developers.gorgias.com/reference/get-ticket)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gorgias_oauth,
+    ticketId: {
+      propDefinition: [
+        gorgias_oauth,
+        "ticketId",
+      ],
+      description: "The ID of a ticket to get",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.gorgias_oauth.retrieveTicket({
+      $,
+      id: this.ticketId,
+    });
+    $.export("$summary", `Successfully retrieved ticket with ID: ${this.ticketId}`);
+    return response;
+  },
+};
