368cac2cb5d2adf6a7d48e6765f092c345990d41
Jorge Cortes <jacortesmahmud@gmail.com>
2025-07-28 17:25:57 -0500

    [APP] implement new tool for sevdesk - findInvoice, getInvoice

---
+++ components/sevdesk/actions/get-invoice/get-invoice.mjs
@@ -0,0 +1,33 @@
+import app from "../../sevdesk.app.mjs";
+
+export default {
+  key: "sevdesk-get-invoice",
+  name: "Get Invoice",
+  description: "Find and retrieve a single invoice by its ID. [See the documentation](https://api.sevdesk.de/#tag/Invoice/operation/getInvoiceById)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    invoiceId: {
+      propDefinition: [
+        app,
+        "invoiceId",
+      ],
+      description: "ID of the invoice to retrieve",
+    },
+  },
+  async run({ $ }) {
+    const {
+      app,
+      invoiceId,
+    } = this;
+
+    const response = await app.getInvoice({
+      $,
+      invoiceId,
+    });
+
+    $.export("$summary", `Successfully retrieved invoice with ID ${invoiceId}`);
+    return response;
+  },
+};
