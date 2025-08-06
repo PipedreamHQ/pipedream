7c74563671c865ddac7a2743594b3384b031c21a
michelle0927 <michelle0927@users.noreply.github.com>
2025-08-05 22:26:27 -0400

    Merging pull request #17953
    * delete-email action
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml

---
+++ components/gmail/actions/delete-email/delete-email.mjs
@@ -0,0 +1,36 @@
+import gmail from "../../gmail.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "gmail-delete-email",
+  name: "Delete Email",
+  description: "Moves the specified message to the trash. [See the documentation](https://developers.google.com/workspace/gmail/api/reference/rest/v1/users.messages/trash)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gmail,
+    messageId: {
+      propDefinition: [
+        gmail,
+        "message",
+      ],
+      label: "Message ID",
+      description: "The ID of the message to delete",
+    },
+  },
+  methods: {
+    deleteEmail(opts = {}) {
+      return this.gmail._client().users.messages.trash({
+        userId: constants.USER_ID,
+        ...opts,
+      });
+    },
+  },
+  async run({ $ }) {
+    const response = await this.deleteEmail({
+      id: this.messageId,
+    });
+    $.export("$summary", `Deleted email (ID: ${this.messageId})`);
+    return response;
+  },
+};
