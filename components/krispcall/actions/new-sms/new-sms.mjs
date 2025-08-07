1daaf6128b80ee3f05f00633a073953631e061cc
siddhartha10-krispcall <siddhartha@krispcallmail.com>
2025-08-07 06:27:08 +0545

    Merging pull request #17961
    * Update Baseurl for the production
    
    * versions
    
    ---------
    
    Co-authored-by: Michelle Bergeron <michelle.bergeron@gmail.com>

--- components/krispcall/actions/new-sms/new-sms.mjs
+++ components/krispcall/actions/new-sms/new-sms.mjs
@@ -4,7 +4,7 @@ export default {
   key: "krispcall-new-sms",
   name: "Send New SMS",
   description: "Send a new SMS to a number. [See the documentation](https://documenter.getpostman.com/view/38507826/2sB2xEA8V5#be2f5790-43b5-482e-9f4e-c9e8cb9fd633)",
-  version: "0.0.5",
+  version: "0.0.6",
   type: "action",
   props: {
     krispcall,
