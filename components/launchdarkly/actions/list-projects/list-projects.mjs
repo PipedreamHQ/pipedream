eb8d58ebddad4d7d266e1f1828d761b4c0f0abab
michelle0927 <michelle0927@users.noreply.github.com>
2025-08-07 20:22:40 -0400

    Merging pull request #17973
    * new components
    
    * pnpm-lock.yaml
    
    * pnpm-lock.yaml
    
    * versions
    
    * update

---
+++ components/launchdarkly/actions/list-projects/list-projects.mjs
@@ -0,0 +1,80 @@
+import app from "../../launchdarkly.app.mjs";
+import { ConfigurationError } from "@pipedream/platform";
+
+export default {
+  key: "launchdarkly-list-projects",
+  name: "List Projects",
+  description: "List all projects. [See the documentation](https://launchdarkly.com/docs/api/projects/get-projects).",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    filter: {
+      type: "string[]",
+      label: "Filter",
+      description: "A list of filters. Each filter is of the form `field:value`. Example: `query:abc`. [See the documentation](https://launchdarkly.com/docs/api/projects/get-projects#filtering-projects) for supported fields.",
+      optional: true,
+    },
+    expand: {
+      type: "string[]",
+      label: "Expand",
+      description: "A list of properties that can reveal additional information in the response",
+      options: [
+        "environments",
+      ],
+      optional: true,
+    },
+    sort: {
+      type: "string",
+      label: "Sort Field",
+      description: "The field to sort by. Fields prefixed by a dash ( - ) sort in descending order.",
+      options: [
+        "createdOn",
+        "-createdOn",
+        "name",
+        "-name",
+      ],
+      optional: true,
+    },
+    maxResults: {
+      propDefinition: [
+        app,
+        "maxResults",
+      ],
+    },
+  },
+  async run({ $ }) {
+    // validate array props
+    if (this.filter && !Array.isArray(this.filter)) {
+      throw new ConfigurationError("Filter must be an array");
+    }
+    if (this.expand && !Array.isArray(this.expand)) {
+      throw new ConfigurationError("Expand must be an array");
+    }
+
+    const params = {
+      filter: this.filter?.join(","),
+      expand: this.expand?.join(","),
+      sort: this.sort,
+    };
+
+    const projects = this.app.paginate({
+      fn: this.app.listProjects,
+      args: {
+        $,
+        params,
+      },
+      max: this.maxResults,
+    });
+
+    const results = [];
+    for await (const project of projects) {
+      results.push(project);
+    }
+
+    $.export("$summary", `Found ${results.length} project${results.length === 1
+      ? ""
+      : "s"}`);
+    return results;
+  },
+};
