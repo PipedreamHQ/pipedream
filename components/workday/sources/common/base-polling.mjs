90b8083e7015568dda7fb9ae5492037948ccf1cb
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-08-12 15:28:18 -0400

    new components

---
+++ components/workday/sources/common/base-polling.mjs
@@ -0,0 +1,15 @@
+import workday from "../../workday.app.mjs";
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+
+export default {
+  props: {
+    workday,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+};
