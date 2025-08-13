5d5fd711aeec5688a9ba2493b591e1cc1bea3414
michelle0927 <michelle0927@users.noreply.github.com>
2025-08-13 19:56:46 -0400

    Merging pull request #18022
    * wip
    
    * new components
    
    * pnpm-lock.yaml
    
    * updates

---
+++ components/workday/actions/list-organization-types/list-organization-types.mjs
@@ -0,0 +1,35 @@
+import workday from "../../workday.app.mjs";
+
+export default {
+  key: "workday-list-organization-types",
+  name: "List Organization Types",
+  description: "List organization types. [See the documentation](https://community.workday.com/sites/default/files/file-hosting/restapi/#common/v1/organizationTypes)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    workday,
+    maxResults: {
+      propDefinition: [
+        workday,
+        "maxResults",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const results = this.workday.paginate({
+      fn: this.workday.listOrganizationTypes,
+      args: {
+        $,
+      },
+      max: this.maxResults,
+    });
+
+    const data = [];
+    for await (const result of results) {
+      data.push(result);
+    }
+
+    $.export("$summary", `Successfully fetched ${data.length} organization types`);
+    return data;
+  },
+};
---
+++ components/workday/actions/list-worker-payslips/list-worker-payslips.mjs
@@ -0,0 +1,42 @@
+import workday from "../../workday.app.mjs";
+
+export default {
+  key: "workday-list-worker-payslips",
+  name: "List Worker Payslips",
+  description: "List the payslips for a worker. [See the documentation](https://community.workday.com/sites/default/files/file-hosting/restapi/#common/v1/get-/workers/-ID-/paySlips)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    workday,
+    workerId: {
+      propDefinition: [
+        workday,
+        "workerId",
+      ],
+    },
+    maxResults: {
+      propDefinition: [
+        workday,
+        "maxResults",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const results = this.workday.paginate({
+      fn: this.workday.listWorkerPayslips,
+      args: {
+        $,
+        workerId: this.workerId,
+      },
+      max: this.maxResults,
+    });
+
+    const data = [];
+    for await (const result of results) {
+      data.push(result);
+    }
+
+    $.export("$summary", `Successfully fetched ${data.length} payslips for worker ${this.workerId}`);
+    return data;
+  },
+};
