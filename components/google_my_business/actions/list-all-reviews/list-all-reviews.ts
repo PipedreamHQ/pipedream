067c384234caa00f1fa95ba32519647355089dba
GTFalcao <gtfalcao96@gmail.com>
2025-08-13 13:59:46 -0300

    Adding new google my business actions

---
+++ components/google_my_business/actions/list-all-reviews/list-all-reviews.ts
@@ -0,0 +1,48 @@
+import { defineAction } from "@pipedream/types";
+import app from "../../app/google_my_business.app";
+import { ListReviewsParams } from "../../common/requestParams";
+
+const DOCS_LINK = "https://developers.google.com/my-business/content/review-data#list_all_reviews";
+
+export default defineAction({
+  key: "google_my_business-list-all-reviews",
+  name: "List All Reviews",
+  description: `List all reviews of a location to audit reviews in bulk. [See the documentation](${DOCS_LINK})`,
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    account: {
+      propDefinition: [
+        app,
+        "account",
+      ],
+    },
+    location: {
+      propDefinition: [
+        app,
+        "location",
+        ({ account }: { account: string; }) => ({
+          account,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const {
+      account, location,
+    } = this;
+
+    const params: ListReviewsParams = {
+      $,
+      account,
+      location,
+    };
+
+    const response = await this.app.listReviews(params);
+
+    $.export("$summary", `Successfully listed ${response.length} reviews`);
+
+    return response;
+  },
+});
