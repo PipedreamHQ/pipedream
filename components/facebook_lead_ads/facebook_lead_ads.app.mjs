7d103533947a7ddd16e7afcbffbf2b01b53771d9
GTFalcao <gtfalcao96@gmail.com>
2025-08-19 19:20:59 -0300

    Component batch #2

---
+++ components/facebook_lead_ads/facebook_lead_ads.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "facebook_lead_ads",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
---
+++ components/forms_on_fire/forms_on_fire.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "forms_on_fire",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
