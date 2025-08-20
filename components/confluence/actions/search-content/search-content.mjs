8ab3de04367b7d9a149bb9ebfcd0b74d739bdf90
Andrew Chuang <andrewjschuang@gmail.com>
2025-08-20 14:46:06 -0400

    bump versions

--- components/confluence/actions/search-content/search-content.mjs
+++ components/confluence/actions/search-content/search-content.mjs
@@ -4,7 +4,7 @@ export default {
   key: "confluence-search-content",
   name: "Search Content",
   description: "Searches for content using the Confluence Query Language (CQL). [See the documentation](https://developer.atlassian.com/cloud/confluence/rest/v1/api-group-search#api-wiki-rest-api-search-get)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     confluence,
