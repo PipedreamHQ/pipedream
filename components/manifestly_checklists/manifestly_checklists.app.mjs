706cea0eb508ce3183a0ca527d2e651b6600c522
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-08-21 14:11:01 -0300

    Converting 3rd batch of apps ts -> mjs (#18133)

---
+++ components/manifestly_checklists/manifestly_checklists.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "manifestly_checklists",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
