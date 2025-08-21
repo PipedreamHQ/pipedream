d66263dee9cacd4caa2edc9e0e7eaf4acd1b0d4e
michelle0927 <michelle0927@users.noreply.github.com>
2025-08-20 21:43:17 -0400

    Merging pull request #18131
    * new components
    
    * pnpm-lock.yaml

---
+++ components/gmail/actions/get-send-as-alias/get-send-as-alias.mjs
@@ -0,0 +1,32 @@
+import gmail from "../../gmail.app.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  key: "gmail-get-send-as-alias",
+  name: "Get Send As Alias",
+  description: "Get a send as alias for the authenticated user. [See the documentation](https://developers.google.com/workspace/gmail/api/reference/rest/v1/users.settings.sendAs/get)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    gmail,
+    sendAsEmail: {
+      type: "string",
+      label: "Send As Email",
+      description: "The email address of the send as alias to get",
+      async options() {
+        const { sendAs } = await this.gmail.listSignatures();
+        return sendAs.map(({ sendAsEmail }) => sendAsEmail);
+      },
+    },
+  },
+  async run({ $ }) {
+    const { data } = await this.gmail._client().users.settings.sendAs.get({
+      userId: constants.USER_ID,
+      sendAsEmail: this.sendAsEmail,
+    });
+
+    $.export("$summary", "Successfully retrieved send as alias");
+
+    return data;
+  },
+};
