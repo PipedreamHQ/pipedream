bd527ae75e526b9ed0a2d3f57d7967ee0fe5da29
Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
2025-08-22 02:27:19 -0300

    Cleaning up TS sample files - part 4 (#18140)
    * Converting 4th batch of apps
    
    * Reverting serveravatar
    
    * Reverting tolstoy
    
    * Reverting xperiencify
    
    * reverting trustpilot
    
    * Cleaning up tsconfig references

---
+++ components/writesonic/writesonic.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "writesonic",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
