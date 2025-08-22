be50b874a651c929fd5b27abe0bc81d061c9f91a
Luan Cazarine <luanhc@gmail.com>
2025-08-22 15:37:41 -0300

    Increment version numbers for HubSpot components: updated batch-update-companies to 0.0.3 and new-email-event to 0.0.29 for improved compatibility.

--- components/hubspot/sources/new-email-event/new-email-event.mjs
+++ components/hubspot/sources/new-email-event/new-email-event.mjs
@@ -8,7 +8,7 @@ export default {
   key: "hubspot-new-email-event",
   name: "New Email Event",
   description: "Emit new event for each new Hubspot email event.",
-  version: "0.0.28",
+  version: "0.0.29",
   dedupe: "unique",
   type: "source",
   props: {
