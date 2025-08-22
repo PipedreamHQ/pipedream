13365febdef72ff700894debb0d6bacffefba2e0
DocuGenerate <support@docugenerate.com>
2025-08-22 11:21:00 +0300

    Add success message with $.export("$summary")

--- components/docugenerate/actions/list-templates.mjs
+++ components/docugenerate/actions/list-templates.mjs
@@ -4,14 +4,14 @@ export default {
   key: "docugenerate-list-templates",
   name: "List Templates",
   description: "Retrieves a list of all templates in your DocuGenerate account",
-  version: "0.0.1",
+  version: "0.0.3",
   type: "action",
   props: {
     app,
   },
   async run({ $ }) {
     const response = await this.app.listTemplates($);
-    console.log(response);
+    $.export("$summary", `Successfully retrieved ${response?.length || 0} templates`);
     return response;
   },
 };
