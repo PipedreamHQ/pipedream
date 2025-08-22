ee977d00dbefa9960e42cf82dc4662e95adf8e51
DocuGenerate <support@docugenerate.com>
2025-08-22 10:06:21 +0300

    Create the "List Templates" action.

---
+++ components/docugenerate/actions/list-templates.mjs
@@ -0,0 +1,18 @@
+import app from "../docugenerate.app.mjs";
+
+export default {
+  key: "docugenerate-list-templates",
+  name: "List Templates",
+  description: "Retrieves a list of all templates in your DocuGenerate account",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  async run({ $ }) {
+    const response = await this.app.listTemplates($);
+    
+    $.export("$summary", `Successfully retrieved ${response.data?.length || 0} templates`);
+    return response;
+  },
+};
--- components/docugenerate/docugenerate.app.mjs
+++ components/docugenerate/docugenerate.app.mjs
@@ -1,11 +1,51 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "docugenerate",
-  propDefinitions: {},
+  propDefinitions: {
+    templateId: {
+      type: "string",
+      label: "Template ID",
+      description: "The ID of the template you want to use",
+      async options() {
+        const response = await this.listTemplates();
+        return response.data.map(template => ({
+          label: template.name,
+          value: template.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getBaseUrl() {
+      return "https://api.docugenerate.com/v1";
+    },
+    getHeaders() {
+      return {
+        "Authorization": `${this.$auth.api_key}`,
+        "Content-Type": "application/json",
+      };
+    },
+    async makeRequest({
+      $ = this,
+      method = "GET",
+      path,
+      ...args
+    }) {
+      const config = {
+        method,
+        url: `${this.getBaseUrl()}${path}`,
+        headers: this.getHeaders(),
+        ...args,
+      };
+      return axios($, config);
+    },
+    async listTemplates($ = this) {
+      return this.makeRequest({
+        $,
+        path: "/templates",
+      });
     },
   },
 };
