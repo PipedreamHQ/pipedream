69f72dab15ed1a91db5810fa0015931ec3d2130f
Luan Cazarine <luanhc@gmail.com>
2025-09-02 12:39:23 -0300

    Add order and shipment management features to Paazl app
    - Introduced prop definitions for Order ID and Shipment ID.
    - Implemented methods for retrieving order shipments and shipment details.
    - Added methods for creating orders and shipments.
    - Enhanced request handling with improved headers and base URL configuration.

--- components/paazl/paazl.app.mjs
+++ components/paazl/paazl.app.mjs
@@ -1,11 +1,85 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "paazl",
-  propDefinitions: {},
+  propDefinitions: {
+    orderId: {
+      type: "string",
+      label: "Order ID",
+      description: "The unique identifier of the order",
+    },
+    shipmentId: {
+      type: "string",
+      label: "Shipment ID",
+      description: "The unique identifier of the shipment",
+      async options({
+        page, orderId,
+      }) {
+        const { shipments } = await this.getOrderShipments({
+          orderId,
+          params: {
+            page,
+          },
+        });
+
+        return shipments.map(({ trackingNumber }) => trackingNumber);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _headers() {
+      return {
+        "Authorization": `Bearer ${this.$auth.api_key}:${this.$auth.api_secret}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json;charset=utf-8",
+      };
+    },
+    _baseUrl() {
+      return `${this.$auth.environment}/v1`;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      const config = {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      };
+      console.log("config: ", config);
+      return axios($, config);
+    },
+    getOrderShipments({
+      orderId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/orders/${orderId}/shipments`,
+        ...opts,
+      });
+    },
+    getOrderShipmentDetails({
+      orderId, shipmentId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/orders/${orderId}/shipments/${shipmentId}`,
+        ...opts,
+      });
+    },
+    createOrder(opts = {}) {
+      return this._makeRequest({
+        path: "/order",
+        method: "POST",
+        ...opts,
+      });
+    },
+    createShipment({
+      orderId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/orders/${orderId}/shipments`,
+        method: "POST",
+        ...opts,
+      });
     },
   },
 };
