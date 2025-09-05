d6a5661d97006353d98cdefe6bbfc40a922f87fc
Luan Cazarine <luanhc@gmail.com>
2025-09-05 15:39:25 -0300

    Enhance Microsoft Outlook component with new features and updates
    - Added userId and sharedFolderId props for retrieving messages from shared folders.
    - Introduced search, filter, and orderBy props for more refined email searches.
    - Implemented listUsers and listSharedFolders methods for user and folder management.
    - Added new action to find emails in shared folders.
    - Updated existing actions and sources with version bumps and minor fixes.
    - Bumped component version to 1.6.0.

--- components/microsoft_outlook/sources/new-email/new-email.mjs
+++ components/microsoft_outlook/sources/new-email/new-email.mjs
@@ -1,5 +1,5 @@
-import common from "../common/common-new-email.mjs";
 import md5 from "md5";
+import common from "../common/common-new-email.mjs";
 import sampleEmit from "./test-event.mjs";
 
 export default {
@@ -7,7 +7,7 @@ export default {
   key: "microsoft_outlook-new-email",
   name: "New Email Event (Instant)",
   description: "Emit new event when an email is received in specified folders.",
-  version: "0.0.19",
+  version: "0.0.20",
   type: "source",
   dedupe: "unique",
   methods: {
