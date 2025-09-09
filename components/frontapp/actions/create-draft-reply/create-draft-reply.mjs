0151fa0498fd34604d825f7ff3e270c90fd229a5
Lucas Caresia <lucascarezia@gmail.com>
2025-09-09 11:04:09 -0300

    [ACTION] Front - Create Message #18076 (#18180)
    * Added actions
    
    * Added actions
    
    * Added actions

--- components/frontapp/actions/create-draft-reply/create-draft-reply.mjs
+++ components/frontapp/actions/create-draft-reply/create-draft-reply.mjs
@@ -5,7 +5,7 @@ export default {
   key: "frontapp-create-draft-reply",
   name: "Create Draft Reply",
   description: "Create a new draft as a reply to the last message in the conversation. [See the documentation](https://dev.frontapp.com/reference/create-draft-reply)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     frontApp,
