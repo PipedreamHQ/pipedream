d4a1acbfae996f2013e4b244b71ba2668620aa5b
Jorge Cortes <jacortesmahmud@gmail.com>
2025-09-10 03:54:15 -0500

    Merging pull request #18283

---
+++ components/billbee/common/constants.mjs
@@ -0,0 +1,11 @@
+const BASE_URL = "https://app.billbee.io";
+const VERSION_PATH = "/api/v1";
+const DEFAULT_MAX = 300;
+const DEFAULT_LIMIT = 100;
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  DEFAULT_MAX,
+  DEFAULT_LIMIT,
+};
