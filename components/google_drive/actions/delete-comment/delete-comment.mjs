4816bcb382a45f93370bf8d2e8144f3063e9939d
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-09-10 10:48:28 -0400

    versions

--- components/google_drive/actions/delete-comment/delete-comment.mjs
+++ components/google_drive/actions/delete-comment/delete-comment.mjs
@@ -4,7 +4,7 @@ export default {
   key: "google_drive-delete-comment",
   name: "Delete Comment",
   description: "Delete a specific comment (Requires ownership or permissions). [See the documentation](https://developers.google.com/workspace/drive/api/reference/rest/v3/comments/delete)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     googleDrive,
--- components/google_drive/actions/resolve-comment/resolve-comment.mjs
+++ components/google_drive/actions/resolve-comment/resolve-comment.mjs
@@ -4,7 +4,7 @@ export default {
   key: "google_drive-resolve-comment",
   name: "Resolve Comment",
   description: "Mark a comment as resolved. [See the documentation](https://developers.google.com/workspace/drive/api/reference/rest/v3/comments/update)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     googleDrive,
