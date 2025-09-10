16fa18185284fddda346766c8d5014a727389d8a
Lucas Caresia <lucascarezia@gmail.com>
2025-09-10 05:41:44 -0300

    Merging pull request #18325
    * Added actions
    
    * Added actions

---
+++ components/elorus/actions/get-tasks/get-tasks.mjs
@@ -0,0 +1,44 @@
+import app from "../../elorus.app.mjs";
+
+export default {
+  key: "elorus-get-tasks",
+  name: "Get Tasks",
+  description: "Get a list of tasks from Elorus. [See the documentation](https://developer.elorus.com/#operation/tasks_list)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    search: {
+      propDefinition: [
+        app,
+        "search",
+      ],
+    },
+    project: {
+      propDefinition: [
+        app,
+        "project",
+      ],
+    },
+    active: {
+      propDefinition: [
+        app,
+        "active",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.getTasks({
+      $,
+      params: {
+        search: this.search,
+        project: this.project,
+        active: this.active
+          ? 1
+          : 0,
+      },
+    });
+    $.export("$summary", "Successfully sent the request and retrieved " + response.results.length + " results");
+    return response;
+  },
+};
