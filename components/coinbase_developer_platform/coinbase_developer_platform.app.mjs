179733699ff5eabb3b343ae9d1aeb4a5324f03f2
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-09-11 11:21:04 -0400

    new component

--- components/coinbase_developer_platform/coinbase_developer_platform.app.mjs
+++ components/coinbase_developer_platform/coinbase_developer_platform.app.mjs
@@ -1,11 +1,48 @@
+import {
+  Coinbase, Wallet, Webhook,
+} from "@coinbase/coinbase-sdk";
+
 export default {
   type: "app",
   app: "coinbase_developer_platform",
-  propDefinitions: {},
+  propDefinitions: {
+    walletId: {
+      type: "string",
+      label: "Wallet ID",
+      description: "The ID of the wallet to use",
+      async options() {
+        this.configure();
+        const { data } = await this.listWallets();
+        return data?.map((wallet) => ({
+          label: `${wallet.model.network_id} - ${wallet.model.default_address.wallet_id}`,
+          value: wallet.model.default_address.wallet_id,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    configure() {
+      const apiKeyName = this.$auth.api_key_id;
+      const privateKey = this.$auth.secret_key;
+      Coinbase.configure({
+        apiKeyName,
+        privateKey,
+      });
+    },
+    getWallet(walletId) {
+      return Wallet.fetch(walletId);
+    },
+    listWallets() {
+      return Wallet.listWallets();
+    },
+    listWebhooks() {
+      return Webhook.list();
+    },
+    createWebhook(opts) {
+      return Webhook.create(opts);
+    },
+    deleteWebhook(webhook) {
+      return webhook.delete();
     },
   },
 };
