136d163b284336f1857638ae1458e3642b226fb4
Luan Cazarine <luanhc@gmail.com>
2025-09-12 02:12:52 -0300

    Merging pull request #18336
    * Enhance Espy component with new actions and props
    
    - Added new props for value and searchId in espy.app.mjs to facilitate lookups.
    - Implemented actions for email lookup, name lookup, and retrieving lookup results, each with corresponding methods for API interaction.
    - Updated package version to 0.1.0 and added dependency on @pipedream/platform.
    - Improved API request handling with a centralized method for making requests.
    
    * pnpm update

---
+++ components/espy/actions/email-lookup/email-lookup.mjs
@@ -0,0 +1,31 @@
+import app from "../../espy.app.mjs";
+
+export default {
+  key: "espy-email-lookup",
+  name: "Email Lookup",
+  description: "Request a lookup for the provided email. [See the documentation](https://api-docs.espysys.com/name-socialscan/headers-and-body-request-by-name-lookup)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    value: {
+      propDefinition: [
+        app,
+        "value",
+      ],
+    },
+  },
+
+  async run({ $ }) {
+    const response = await this.app.emailLookup({
+      $,
+      data: {
+        value: this.value,
+        lookupId: 67,
+      },
+    });
+    $.export("$summary", `Successfully sent request. Use the ID to get the results: '${response.id}'`);
+
+    return response;
+  },
+};
