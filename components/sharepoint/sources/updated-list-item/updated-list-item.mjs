445f5f9d8637289e9d78e94f1ac975b910304941
michelle0927 <michelle0927@users.noreply.github.com>
2025-09-14 10:40:42 -0400

    Merging pull request #18361
    * update siteId prop
    
    * pnpm-lock.yaml
    
    * package.json version

--- components/sharepoint/sources/updated-list-item/updated-list-item.mjs
+++ components/sharepoint/sources/updated-list-item/updated-list-item.mjs
@@ -5,7 +5,7 @@ export default {
   key: "sharepoint-updated-list-item",
   name: "Updated List Item",
   description: "Emit new event when a list is updated in Microsoft Sharepoint.",
-  version: "0.0.4",
+  version: "0.0.5",
   type: "source",
   dedupe: "unique",
   props: {
