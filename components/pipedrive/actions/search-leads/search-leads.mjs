b9f0c0e888a292a4f4b23667fad72dff64421945
Andrew Chuang <andrewjschuang@gmail.com>
2025-09-15 16:48:52 -0400

    Pipedrive - pipelineId integer (#18372)
    * pipelineId - integer
    
    * bump versions

--- components/pipedrive/actions/search-leads/search-leads.mjs
+++ components/pipedrive/actions/search-leads/search-leads.mjs
@@ -5,7 +5,7 @@ export default {
   key: "pipedrive-search-leads",
   name: "Search Leads",
   description: "Search for leads by name or email. [See the documentation](https://developers.pipedrive.com/docs/api/v1/Leads#searchLeads)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   props: {
     pipedriveApp,
