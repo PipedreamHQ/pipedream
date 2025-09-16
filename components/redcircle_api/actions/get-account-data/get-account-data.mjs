7e37cfcc0d1eb5c244b00959e2a399341431f077
Lucas Caresia <lucascarezia@gmail.com>
2025-09-16 10:56:52 -0300

    Added actions

---
+++ components/redcircle_api/actions/get-account-data/get-account-data.mjs
@@ -0,0 +1,19 @@
+import app from "../../redcircle_api.app.mjs";
+
+export default {
+  key: "redcircle_api-get-account-data",
+  name: "Get Account Data",
+  description: "Get your account details. [See the documentation](https://docs.trajectdata.com/redcircleapi/account-api)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  async run({ $ }) {
+    const response = await this.app.getAccountData({
+      $,
+    });
+    $.export("$summary", "Successfully retrieved the account data");
+    return response;
+  },
+};
