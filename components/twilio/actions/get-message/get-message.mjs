85bd98a6f5000dfda5be4e5413aa4b9de5ebc4b3
Luan Cazarine <luanhc@gmail.com>
2025-09-17 18:05:43 -0300

    18314 twilio (#18350)
    * Update Twilio component versions and dependencies
    
    - Update Twilio Send Message action adding detailed description for 'from' prop and refactoring phone number validation logic.
    - Incremented action versions for several Twilio actions.
    
    * pnpm update

--- components/twilio/actions/get-message/get-message.mjs
+++ components/twilio/actions/get-message/get-message.mjs
@@ -5,7 +5,7 @@ export default {
   key: "twilio-get-message",
   name: "Get Message",
   description: "Return details of a message. [See the documentation](https://www.twilio.com/docs/sms/api/message-resource#fetch-a-message-resource)",
-  version: "0.1.4",
+  version: "0.1.5",
   type: "action",
   props: {
     twilio,
