7cfa2ac108404d63949adb4de850cd324bbbe02b
Luan Cazarine <luanhc@gmail.com>
2025-09-17 17:47:21 -0300

    Enhance Lightspeed eCom C-Series integration with new actions and event sources
    - Added actions for finding customers, invoices, orders, products, and shipments.
    - Introduced event sources for customer creation, updates, invoice updates, order creation, updates, and shipping events.
    - Updated app properties to include customer and order identifiers for better data retrieval.
    - Version bump to 0.1.0 and added dependencies for improved functionality.

---
+++ components/lightspeed_ecom_c_series/sources/new-order-created/new-order-created.mjs
@@ -0,0 +1,29 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "indiefunnels-new-order-created",
+  name: "New Order Created (Instant)",
+  description: "Emit new event when a order is created.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getItemGroup() {
+      return "orders";
+    },
+    getItemAction() {
+      return "created";
+    },
+    generateMeta(body) {
+      return {
+        id: body.order.id,
+        summary: this.getSummary(`Order with ID ${body.order.id} created`),
+        ts: Date.parse(body.order.createdAt),
+      };
+    },
+  },
+  sampleEmit,
+};
---
+++ components/lightspeed_ecom_c_series/sources/order-shipped/order-shipped.mjs
@@ -0,0 +1,29 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "indiefunnels-order-shipped",
+  name: "Order Shipped (Instant)",
+  description: "Emit new event when a order is shipped.",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getItemGroup() {
+      return "orders";
+    },
+    getItemAction() {
+      return "shipped";
+    },
+    generateMeta(body) {
+      return {
+        id: body.order.id,
+        summary: this.getSummary(`Order with ID ${body.order.id} shipped`),
+        ts: Date.parse(body.order.updatedAt),
+      };
+    },
+  },
+  sampleEmit,
+};
