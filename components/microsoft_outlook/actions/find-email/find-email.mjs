0185460d261fdc4c04ebbba7b215a146d18af997
michelle0927 <michelle0927@users.noreply.github.com>
2025-09-20 22:45:29 -0400

    Merging pull request #18408
    * 403 error message
    
    * versions
    
    * update

--- components/microsoft_outlook/actions/find-email/find-email.mjs
+++ components/microsoft_outlook/actions/find-email/find-email.mjs
@@ -4,7 +4,7 @@ export default {
   key: "microsoft_outlook-find-email",
   name: "Find Email",
   description: "Search for an email in Microsoft Outlook. [See the documentation](https://learn.microsoft.com/en-us/graph/api/user-list-messages)",
-  version: "0.0.10",
+  version: "0.0.11",
   type: "action",
   props: {
     microsoftOutlook,
