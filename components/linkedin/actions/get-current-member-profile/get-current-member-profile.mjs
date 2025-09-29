34c7a1aa14664d0d23f1b0a2176857d7a9f1047a
michelle0927 <michelle0927@users.noreply.github.com>
2025-09-28 21:20:10 -0400

    Merging pull request #18479
    * updates
    
    * pnpm-lock.yaml
    
    * versions

--- components/linkedin/actions/get-current-member-profile/get-current-member-profile.mjs
+++ components/linkedin/actions/get-current-member-profile/get-current-member-profile.mjs
@@ -4,7 +4,7 @@ export default {
   key: "linkedin-get-current-member-profile",
   name: "Get Current Member Profile",
   description: "Gets the profile of the current authenticated member. [See the docs here](https://docs.microsoft.com/en-us/linkedin/shared/integrations/people/profile-api#retrieve-current-members-profile)",
-  version: "0.1.9",
+  version: "0.1.10",
   type: "action",
   props: {
     linkedin,
