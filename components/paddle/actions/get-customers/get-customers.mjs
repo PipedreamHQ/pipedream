79de956fff20d32a254c22b617feb984f63cff17
Lucas Caresia <lucascarezia@gmail.com>
2025-09-30 16:13:22 -0300

    Added actions

---
+++ components/paddle/actions/get-customers/get-customers.mjs
@@ -0,0 +1,19 @@
+import app from "../../paddle.app.mjs";
+
+export default {
+  key: "paddle-get-customers",
+  name: "Get Customers",
+  description: "Get a list of customers registered in Paddle. [See the documentation](https://developer.paddle.com/api-reference/customers/list-customers)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+  },
+  async run({ $ }) {
+    const response = await this.app.getCustomers({
+      $,
+    });
+    $.export("$summary", "Successfully retrieved " + response.data.length + " customers");
+    return response;
+  },
+};
---
+++ components/paddle/common/constants.mjs
@@ -0,0 +1,6 @@
+export default {
+  STATUS_OPTIONS: [
+    "active",
+    "archived",
+  ],
+};
