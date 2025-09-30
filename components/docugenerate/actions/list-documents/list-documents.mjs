e02dfcc3f0ddcbc8cdeb9ac99e4fcfc5c9352257
DocuGenerate <160992825+docugenerate@users.noreply.github.com>
2025-09-30 03:58:53 +0200

    Merging pull request #18164
    * Create the "List Templates" action.
    
    * Update "List Templates" action with the correct URL.
    
    * Update for the "templateId" prop.
    
    * Add success message with $.export("$summary")
    
    * Add the "Delete Template" action.
    
    * Create the "List Documents" action.
    
    * Add the "Get Document" action.
    
    * Update the "Get Document" action.
    
    * Add the "Update Document" action.
    
    * Add the "Delete Document" action.
    
    * Add "Generate Document" action.
    
    * Add "Generate Document" action.
    
    * Add optional fields.
    
    * Update output format options.
    
    * Update summary message.
    
    * Set version: "1.0.0" for all actions.
    
    * Update package.json
    
    * Update README file.
    
    * Fix step numbering in README.
    
    * Running eslint --fix
    
    * Moving actions to individual folders
    
    * Adding docs links to descriptions
    
    * Fixing package version
    
    ---------
    
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    Co-authored-by: GTFalcao <gtfalcao96@gmail.com>

---
+++ components/docugenerate/actions/list-documents/list-documents.mjs
@@ -0,0 +1,24 @@
+import app from "../../docugenerate.app.mjs";
+
+export default {
+  key: "docugenerate-list-documents",
+  name: "List Documents",
+  description: "Retrieves a list of documents generated from a template. [See the documentation](https://api.docugenerate.com/#/Document/queryDocuments)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    templateId: {
+      propDefinition: [
+        app,
+        "templateId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.listDocuments($, this.templateId);
+
+    $.export("$summary", `Successfully retrieved ${response?.length || 0} documents`);
+    return response;
+  },
+};
--- components/docugenerate/docugenerate.app.mjs
+++ components/docugenerate/docugenerate.app.mjs
@@ -1,11 +1,99 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "docugenerate",
-  propDefinitions: {},
+  propDefinitions: {
+    templateId: {
+      type: "string",
+      label: "Template",
+      description: "The selected template",
+      async options() {
+        const response = await this.listTemplates();
+        return response.map((template) => ({
+          label: template.name,
+          value: template.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    getBaseUrl() {
+      return "https://api.docugenerate.com/v1";
+    },
+    getHeaders() {
+      return {
+        "Authorization": `${this.$auth.api_key}`,
+        "Content-Type": "application/json",
+      };
+    },
+    async makeRequest({
+      $ = this,
+      method = "GET",
+      path,
+      ...args
+    }) {
+      const config = {
+        method,
+        url: `${this.getBaseUrl()}${path}`,
+        headers: this.getHeaders(),
+        ...args,
+      };
+      return axios($, config);
+    },
+    async listTemplates($ = this) {
+      return this.makeRequest({
+        $,
+        path: "/template",
+      });
+    },
+    async getTemplate($ = this, templateId) {
+      return this.makeRequest({
+        $,
+        path: `/template/${templateId}`,
+      });
+    },
+    async deleteTemplate($ = this, templateId) {
+      return this.makeRequest({
+        $,
+        method: "DELETE",
+        path: `/template/${templateId}`,
+      });
+    },
+    async listDocuments($ = this, templateId) {
+      return this.makeRequest({
+        $,
+        path: `/document?template_id=${templateId}`,
+      });
+    },
+    async getDocument($ = this, documentId) {
+      return this.makeRequest({
+        $,
+        path: `/document/${documentId}`,
+      });
+    },
+    async updateDocument($ = this, documentId, body) {
+      return this.makeRequest({
+        $,
+        method: "PUT",
+        path: `/document/${documentId}`,
+        data: body,
+      });
+    },
+    async deleteDocument($ = this, documentId) {
+      return this.makeRequest({
+        $,
+        method: "DELETE",
+        path: `/document/${documentId}`,
+      });
+    },
+    async generateDocument($ = this, body) {
+      return this.makeRequest({
+        $,
+        method: "POST",
+        path: "/document",
+        data: body,
+      });
     },
   },
 };
