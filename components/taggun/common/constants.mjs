c90e8e60841f980b20947a2179697e21be9acd40
Lucas Caresia <lucascarezia@gmail.com>
2025-09-30 15:57:51 -0300

    Added actions

---
+++ components/taggun/common/constants.mjs
@@ -0,0 +1,17 @@
+export default {
+  LANGUAGE_OPTIONS: [
+    "en",
+    "es",
+    "fr",
+    "jp",
+    "he",
+    "iw",
+    "et",
+    "lv",
+    "lt",
+    "fi",
+    "el",
+    "zh",
+    "th",
+  ],
+};
