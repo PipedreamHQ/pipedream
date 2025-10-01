ddf083556f4a99b396ae70023c3b648583619b03
michelle0927 <michelle0927@users.noreply.github.com>
2025-09-30 22:04:59 -0400

    Merging pull request #18495

--- components/zoho_workdrive/actions/download-file/download-file.mjs
+++ components/zoho_workdrive/actions/download-file/download-file.mjs
@@ -1,13 +1,16 @@
 import app from "../../zoho_workdrive.app.mjs";
 import { getFilePath } from "../../common/utils.mjs";
 import { LIMIT } from "../../common/constants.mjs";
+import {
+  additionalFolderProps, findMaxFolderId,
+} from "../../common/additionalFolderProps.mjs";
 import fs from "fs";
 
 export default {
   key: "zoho_workdrive-download-file",
   name: "Download File to Tmp Direcory",
   description: "Download a file to the /tmp directory. [See the documentation](https://workdrive.zoho.com/apidocs/v1/filesfolders/downloadserverfile)",
-  version: "0.0.5",
+  version: "0.0.6",
   type: "action",
   props: {
     app,
@@ -34,21 +37,37 @@ export default {
           folderType,
         }),
       ],
-      label: "Folder Id",
-      description: "The unique ID of the folder where file is located.",
+      label: "Folder ID",
+      description: "The unique ID of the folder where file is located. Select a folder to view its subfolders.",
+      reloadProps: true,
     },
-    fileId: {
+    syncDir: {
+      type: "dir",
+      accessMode: "write",
+      sync: true,
+    },
+  },
+  async additionalProps() {
+    const folderProps = await additionalFolderProps.call(this);
+    const props = {
+      ...folderProps,
+    };
+    props.fileId = {
       type: "string",
-      label: "File Id",
+      label: "File ID",
       description: "The unique ID of the file to download.",
       withLabel: true,
-      async options({ page }) {
+      options: async ({ page }) => {
+        const num = this.findMaxFolderId(this);
+        const limit = this.getLimit();
         const { data } = await this.app.listFiles({
-          folderId: this.folderId,
+          folderId: num > 0
+            ? this[`folderId${num}`]
+            : this.folderId,
           filter: "allfiles",
           params: new URLSearchParams({
-            "page[limit]": LIMIT,
-            "page[offset]": LIMIT * page,
+            "page[limit]": limit,
+            "page[offset]": limit * page,
           }).toString(),
         });
         return data.map(({
@@ -58,17 +77,19 @@ export default {
           label: attributes.name,
         }));
       },
-    },
-    fileName: {
+    };
+    props.fileName = {
       type: "string",
       label: "Filename",
       description: "What to name the new file saved to /tmp directory",
       optional: true,
-    },
-    syncDir: {
-      type: "dir",
-      accessMode: "write",
-      sync: true,
+    };
+    return props;
+  },
+  methods: {
+    findMaxFolderId,
+    getLimit() {
+      return LIMIT;
     },
   },
   async run({ $ }) {
