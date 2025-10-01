9b469a0621a56b9a7aced35ed7c94384b51ac2c7
Luan Cazarine <luanhc@gmail.com>
2025-10-01 00:44:04 -0300

    Merging pull request #18498
    * Update @pipedream/the_official_board to version 0.1.0, add new actions for exporting org charts as PDF and XLSX, and retrieving executive and org chart information.
    
    * pnpm update

--- components/the_official_board/the_official_board.app.mjs
+++ components/the_official_board/the_official_board.app.mjs
@@ -1,11 +1,79 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "the_official_board",
-  propDefinitions: {},
+  propDefinitions: {
+    companyId: {
+      type: "string",
+      label: "Company ID",
+      description: "The ID of the company",
+    },
+    executiveId: {
+      type: "string",
+      label: "Bio ID",
+      description: "The ID of the executive to get information for",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseApiUrl() {
+      return "https://rest.theofficialboard.com/rest";
+    },
+    _headers(headers = {}) {
+      return {
+        "token": this.$auth.api_token,
+        "Accept": "application/json",
+        ...headers,
+      };
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      ...args
+    } = {}) {
+      return axios($, {
+        url: `${this._baseApiUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      });
+    },
+    getExecutiveSearch(opts = {}) {
+      return this._makeRequest({
+        path: "/executive/search",
+        ...opts,
+      });
+    },
+    getCompanySearch(opts = {}) {
+      return this._makeRequest({
+        path: "/company/search",
+        ...opts,
+      });
+    },
+    getCompanyOrgchart(opts = {}) {
+      return this._makeRequest({
+        path: "/company/orgchart",
+        ...opts,
+      });
+    },
+    exportOrgchartExcel(opts = {}) {
+      return this._makeRequest({
+        path: "/export/orgchart-excel",
+        responseType: "arraybuffer",
+        ...opts,
+      });
+    },
+    exportOrgchartPdf(opts = {}) {
+      return this._makeRequest({
+        path: "/export/orgchart-pdf",
+        responseType: "arraybuffer",
+        ...opts,
+      });
+    },
+    getExecutiveBiography(opts = {}) {
+      return this._makeRequest({
+        path: "/executive/biography",
+        ...opts,
+      });
     },
   },
 };
