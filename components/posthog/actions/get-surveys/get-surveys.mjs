3ee8a052058b0a6878eb49c14556230268da7abd
GTFalcao <gtfalcao96@gmail.com>
2025-10-01 13:04:26 -0300

    Updating posthog component versions

--- components/posthog/actions/get-surveys/get-surveys.mjs
+++ components/posthog/actions/get-surveys/get-surveys.mjs
@@ -4,7 +4,7 @@ export default {
   key: "posthog-get-surveys",
   name: "Get Surveys",
   description: "Retrieve a list of surveys. [See the documentation](https://posthog.com/docs/api/surveys#get-api-projects-project_id-surveys)",
-  version: "0.0.2",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
