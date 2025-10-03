0afd8aca7b9391c8b2213a268731e5a1853eae45
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-03 17:59:10 -0400

    add optional watchDrive prop, updates

--- components/google_sheets/sources/common/http-based/sheet.mjs
+++ components/google_sheets/sources/common/http-based/sheet.mjs
@@ -12,14 +12,36 @@ import drive from "./drive.mjs";
  */
 export default {
   ...base,
+  props: {
+    ...base.props,
+    watchDrive: {
+      type: "boolean",
+      label: "Watch Drive",
+      description: "Set to `true` to watch the drive for changes. May reduce rate limiting.",
+      optional: true,
+    },
+  },
   methods: {
     ...drive.methods,
     ...base.methods,
+    _getChangeToken() {
+      return this.db.get("changeToken");
+    },
+    _setChangeToken(changeToken) {
+      this.db.set("changeToken", changeToken);
+    },
     async activateHook(channelID) {
-      return this.googleSheets.activateHook(
+      if (this.watchDrive) {
+        return this.googleSheets.activateHook(
+          channelID,
+          this.http.endpoint,
+          this.googleSheets.getDriveId(this.watchedDrive),
+        );
+      }
+      return this.googleSheets.activateFileHook(
         channelID,
         this.http.endpoint,
-        this.googleSheets.getDriveId(this.watchedDrive),
+        this.getSheetId(),
       );
     },
     async getAllWorksheetIds(sheetID) {
@@ -28,6 +50,19 @@ export default {
         .map(({ properties }) => properties)
         .filter(({ sheetType }) => sheetType === "GRID")
         .map(({ sheetId }) => (sheetId.toString()));
+    },
+    async isSheetRelevant() {
+      const pageToken = this._getChangeToken() || this._getPageToken();
+      const drive = this.googleSheets.drive();
+      const { data } = await drive.changes.list({
+        pageToken,
+        driveId: this.googleSheets.getDriveId(this.watchedDrive),
+      });
+      const {
+        changes, newStartPageToken,
+      } = data;
+      this._setChangeToken(newStartPageToken);
+      return changes.some((change) => change.file.id === this.getSheetId());
     },
     async renewSubscription() {
       // Assume subscription & channelID may all be undefined at
@@ -53,15 +88,51 @@ export default {
       });
       this._setChannelID(newChannelID);
     },
+    async renewDriveSubscription() {
+      const subscription = this._getSubscription();
+      const channelID = this._getChannelID() || uuid();
+
+      const {
+        expiration,
+        resourceId,
+        newChannelID,
+        newPageToken,
+      } = await this.googleSheets.renewSubscription(
+        this.watchedDrive,
+        subscription,
+        this.http.endpoint,
+        channelID,
+        this._getPageToken(),
+      );
+
+      this._setSubscription({
+        expiration,
+        resourceId,
+      });
+      this._setChannelID(newChannelID);
+      this._setPageToken(newPageToken);
+    },
   },
   async run(event) {
     if (event.timestamp) {
       // Component was invoked by timer
+      if (this.watchDrive) {
+        return this.renewDriveSubscription();
+      }
       return this.renewSubscription();
     }
 
     if (!this.isEventRelevant(event)) {
       console.log("Sync notification, exiting early");
+      return;
+    }
+
+    this.http.respond({
+      status: 200,
+    });
+
+    if (this.watchDrive && !(await this.isSheetRelevant(event))) {
+      console.log("Change to unwatched file, exiting early");
       return;
     }
 
