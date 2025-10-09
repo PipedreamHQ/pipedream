593fee1f1837b4991caf3ca8368439166513eb2f
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-09 15:38:39 -0400

    updates

--- components/microsoft_teams/sources/new-chat-message/new-chat-message.mjs
+++ components/microsoft_teams/sources/new-chat-message/new-chat-message.mjs
@@ -5,7 +5,7 @@ export default {
   key: "microsoft_teams-new-chat-message",
   name: "New Chat Message",
   description: "Emit new event when a new message is received in a chat",
-  version: "0.0.10",
+  version: "0.0.11",
   type: "source",
   dedupe: "unique",
   props: {
