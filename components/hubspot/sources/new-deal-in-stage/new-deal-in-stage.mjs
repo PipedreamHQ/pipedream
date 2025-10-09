02493fad6acb60bc84df5552c81df59498e9f6fa
Kacper Choła <69403782+choeqq@users.noreply.github.com>
2025-10-09 04:57:51 +0200

    Merging pull request #18664
    * fetch contact will all properties
    
    * bump versions

--- components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
+++ components/hubspot/sources/new-deal-in-stage/new-deal-in-stage.mjs
@@ -13,7 +13,7 @@ export default {
   key: "hubspot-new-deal-in-stage",
   name: "New Deal In Stage",
   description: "Emit new event for each new deal in a stage.",
-  version: "0.1.0",
+  version: "0.1.1",
   dedupe: "unique",
   type: "source",
   props: {
--- components/hubspot/sources/new-email-subscriptions-timeline/new-email-subscriptions-timeline.mjs
+++ components/hubspot/sources/new-email-subscriptions-timeline/new-email-subscriptions-timeline.mjs
@@ -6,7 +6,7 @@ export default {
   key: "hubspot-new-email-subscriptions-timeline",
   name: "New Email Subscriptions Timeline",
   description: "Emit new event when a new email timeline subscription is added for the portal.",
-  version: "0.0.34",
+  version: "0.0.35",
   dedupe: "unique",
   type: "source",
   methods: {
