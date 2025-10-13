beb984270553e9f7ae65da8a11cf80fe41ac69dd
michelle0927 <michelle0927@users.noreply.github.com>
2025-10-12 21:58:52 -0400

    Merging pull request #18712
    * updates
    
    * versions

--- components/microsoft_teams/sources/new-chat/new-chat.mjs
+++ components/microsoft_teams/sources/new-chat/new-chat.mjs
@@ -4,17 +4,18 @@ export default {
   ...base,
   key: "microsoft_teams-new-chat",
   name: "New Chat",
-  description: "Emit new event when a new chat is created",
-  version: "0.0.11",
+  description: "Emit new event when a new chat is created. [See the documentation](https://learn.microsoft.com/en-us/graph/api/chat-list?view=graph-rest-1.0&tabs=http)",
+  version: "0.0.12",
   type: "source",
   dedupe: "unique",
   methods: {
     ...base.methods,
-    async getResources(lastCreated) {
+    async getResources(lastCreated, tsField) {
       return this.getNewPaginatedResources(
         this.microsoftTeams.listChats,
         {},
         lastCreated,
+        tsField,
       );
     },
     generateMeta(chat) {
