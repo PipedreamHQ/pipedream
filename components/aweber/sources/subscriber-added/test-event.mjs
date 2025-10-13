5af1965493c032ccd7cd52a661d157df3994a189
Luan Cazarine <luanhc@gmail.com>
2025-10-13 12:56:21 -0300

    Enhance Aweber component with new actions and updates
    - Added `createBroadcast` action to facilitate broadcast creation.
    - Introduced `New Broadcast Event` source to emit events for new broadcasts.
    - Updated existing actions to version 0.0.5 and adjusted descriptions for clarity.
    - Added new properties for list and integration options in the Aweber app.
    - Updated package version to 0.4.0 and dependencies to ensure compatibility.

---
+++ components/aweber/sources/subscriber-added/test-event.mjs
@@ -0,0 +1,38 @@
+export default {
+  "ad_tracking": "ebook",
+  "area_code": 555,
+  "city": "Chalfont",
+  "country": "United States",
+  "custom_fields": {
+    "apple": "fuji",
+    "pear": "bosc"
+  },
+  "dma_code": 504,
+  "email": "user@example.com",
+  "id": 789,
+  "ip_address": "204.194.222.28",
+  "is_verified": true,
+  "last_followup_message_number_sent": 1001,
+  "last_followup_sent_at": "2015-12-17T14:06:26.861089-05:00",
+  "last_followup_sent_link": "https://api.aweber.com/1.0/accounts/123/lists/456/campaigns/f12345678",
+  "latitude": 37.751,
+  "longitude": -97.822,
+  "misc_notes": "ebook",
+  "name": "John Doe",
+  "postal_code": "99999-9999",
+  "region": "PA",
+  "resource_type_link": "https://api.aweber.com/1.0/#subscriber",
+  "self_link": "https://api.aweber.com/1.0/accounts/123/lists/456/subscribers/789",
+  "status": "subscribed",
+  "subscribed_at": "2015-10-13T16:19:20-04:00",
+  "subscription_method": "api",
+  "subscription_url": "https://forms.aweber.com/form/16/123456789.htm",
+  "tags": [
+    "slow",
+    "fast",
+    "lightspeed"
+  ],
+  "unsubscribe_method": "unsubscribe link",
+  "unsubscribed_at": "2017-10-13T10:41:43.300889-04:00",
+  "verified_at": "2015-10-13T16:19:53"
+}
