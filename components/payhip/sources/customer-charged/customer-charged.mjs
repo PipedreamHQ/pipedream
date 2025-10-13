95a106ff857ca105a8e45e7284ab2052cb93bf4e
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-13 14:34:35 -0400

    new sources

---
+++ components/payhip/sources/customer-charged/customer-charged.mjs
@@ -0,0 +1,26 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "payhip-customer-charged",
+  name: "Customer Charged (Instant)",
+  description: "Emit new event when a customer is charged. Webhook of type \"paid\" must be created in Payhip Developer settings. [See the documentation](https://help.payhip.com/article/115-webhooks)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    isRelevant(event) {
+      return event.type === "paid";
+    },
+    generateMeta(event) {
+      return {
+        id: event.id,
+        summary: `${event.customer_email} charged $${event.price}`,
+        ts: event.date,
+      };
+    },
+  },
+  sampleEmit,
+};
---
+++ components/payhip/sources/new-subscription-created/new-subscription-created.mjs
@@ -0,0 +1,26 @@
+import common from "../common/base.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "payhip-new-subscription-created",
+  name: "New Subscription Created (Instant)",
+  description: "Emit new event when a new subscription is created. Webhook of type \"subscription.created\"must be created in Payhip Developer settings. [See the documentation](https://help.payhip.com/article/115-webhooks)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    isRelevant(event) {
+      return event.type === "subscription.created";
+    },
+    generateMeta(event) {
+      return {
+        id: event.subscription_id,
+        summary: `New Subscription: ${event.subscription_id}`,
+        ts: event.date_subscription_started,
+      };
+    },
+  },
+  sampleEmit,
+};
