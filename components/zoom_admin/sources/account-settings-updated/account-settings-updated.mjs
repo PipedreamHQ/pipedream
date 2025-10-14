a43c92d10ad4c56dd1b08fb68d34ff3a3cb749ea
choeqq <kacper.chola01@gmail.com>
2025-10-14 15:03:49 +0200

    Add new prop, bump sources version

--- components/zoom_admin/sources/account-settings-updated/account-settings-updated.mjs
+++ components/zoom_admin/sources/account-settings-updated/account-settings-updated.mjs
@@ -5,7 +5,7 @@ export default {
   type: "source",
   name: "Account Settings Updated",
   description: "Emits an event each time your master account or sub-account settings are updated",
-  version: "0.1.6",
+  version: "0.1.7",
   props: {
     zoomAdmin,
     zoomApphook: {
