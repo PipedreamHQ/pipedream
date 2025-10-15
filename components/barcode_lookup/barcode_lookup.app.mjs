d36ab54c4ff2c8ac9115631c785dd917f75189df
Lucas Caresia <lucascarezia@gmail.com>
2025-10-15 11:23:43 -0300

    Added actions

--- components/barcode_lookup/barcode_lookup.app.mjs
+++ components/barcode_lookup/barcode_lookup.app.mjs
@@ -7,7 +7,7 @@ export default {
     barcode: {
       type: "string",
       label: "Barcode",
-      description: "Barcode to search, must be 7, 8, 10, 11, 12, 13 or 14 digits long, i.e.: `865694301167`",
+      description: "Barcode to search, must be 7, 8, 10, 11, 12, 13 or 14 digits long, e.g. `865694301167`",
       optional: true,
     },
     mpn: {
@@ -19,37 +19,37 @@ export default {
     asin: {
       type: "string",
       label: "ASIN",
-      description: "Amazon Standard Identification Number of the product, i.e.: `B079L4WR4T`",
+      description: "Amazon Standard Identification Number of the product, e.g. `B079L4WR4T`",
       optional: true,
     },
     title: {
       type: "string",
       label: "Title",
-      description: "Product title or name, i.e.: `Red Running Shoes`",
+      description: "Product title or name, e.g. `Red Running Shoes`",
       optional: true,
     },
     category: {
       type: "string",
       label: "Category",
-      description: "Category or type of the product, i.e.: `Home & Garden > Decor`",
+      description: "Category or type of the product, e.g. `Home & Garden > Decor`",
       optional: true,
     },
     manufacturer: {
       type: "string",
       label: "Manufacturer",
-      description: "Name of the product manufacturer, i.e.: `Samsung`",
+      description: "Name of the product manufacturer, e.g. `Samsung`",
       optional: true,
     },
     brand: {
       type: "string",
       label: "Brand",
-      description: "Brand associated with the product, i.e.: `Calvin Klein`",
+      description: "Brand associated with the product, e.g. `Calvin Klein`",
       optional: true,
     },
     search: {
       type: "string",
       label: "Search",
-      description: "General search term for products, i.e.: `Air Jordan Red Shoes Size 40`",
+      description: "General search term for products, e.g. `Air Jordan Red Shoes Size 40`",
       optional: true,
     },
   },
