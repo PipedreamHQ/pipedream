4fe663c8d80158ee322b373584d437837907aefd
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-15 16:50:49 -0400

    wip

--- components/ups/ups.app.mjs
+++ components/ups/ups.app.mjs
@@ -1,11 +1,44 @@
+import { axios } from "@pipedream/platform";
+import { v4 as uuidv4 } from "uuid";
+
 export default {
   type: "app",
   app: "ups",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      //return "https://onlinetools.ups.com/api";
+      return "https://wwwcie.ups.com/api";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+          transactionsrc: "@PipedreamHQ/pipedream v0.1",
+          transid: uuidv4(),
+        },
+        ...opts,
+      });
+    },
+    getTrackingInfo({
+      trackingNumber, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/track/v1/details/${trackingNumber}`,
+        ...opts,
+      });
+    },
+    createShipment({
+      version = "v2409", ...opts
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/shipments/${version}/ship`,
+        ...opts,
+      });
     },
   },
 };
