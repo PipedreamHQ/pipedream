4e2db32c2d2fc24ed46245fdecbf1ed186db572b
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-16 12:38:14 -0400

    update base url

--- components/workday/actions/get-worker/get-worker.mjs
+++ components/workday/actions/get-worker/get-worker.mjs
@@ -4,7 +4,7 @@ export default {
   key: "workday-get-worker",
   name: "Get Worker",
   description: "Get a worker. [See the documentation](https://community.workday.com/sites/default/files/file-hosting/restapi/#common/v1/get-/workers/-ID-)",
-  version: "0.0.2",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
