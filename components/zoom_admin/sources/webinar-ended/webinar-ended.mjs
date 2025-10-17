e59d63ecdfcba99a912c9ea3fa3f806924b15f92
choeqq <kacper.chola01@gmail.com>
2025-10-17 10:03:55 +0200

    bump versions

--- components/zoom_admin/sources/webinar-ended/webinar-ended.mjs
+++ components/zoom_admin/sources/webinar-ended/webinar-ended.mjs
@@ -5,7 +5,7 @@ export default {
   type: "source",
   name: "Webinar Ended",
   description: "Emits an event each time a webinar ends in your Zoom account",
-  version: "0.1.7",
+  version: "0.1.8",
   dedupe: "unique", // Dedupe based on webinar ID
   props: {
     zoomAdmin,
