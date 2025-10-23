3426e134af67d2c9ed8711f9cee7470ebac07629
Jorge Cortes <jacortesmahmud@gmail.com>
2025-10-23 11:54:34 -0500

    [Components] Topdesk - new components

---
+++ components/topdesk/actions/get-incident/get-incident.mjs
@@ -0,0 +1,39 @@
+import app from "../../topdesk.app.mjs";
+
+export default {
+  key: "topdesk-get-incident",
+  name: "Get Incident",
+  description: "Returns an incident by ID. [See the documentation](https://developers.topdesk.com/explorer/?page=incident#/incident/getIncidentById)",
+  version: "0.0.1",
+  type: "action",
+  props: {
+    app,
+    incidentId: {
+      propDefinition: [
+        app,
+        "incidentId",
+      ],
+    },
+  },
+  annotations: {
+    readOnlyHint: true,
+    destructiveHint: false,
+    openWorldHint: true,
+    idempotentHint: true,
+  },
+  async run({ $ }) {
+    const {
+      app,
+      incidentId,
+    } = this;
+
+    const response = await app.getIncident({
+      $,
+      incidentId,
+    });
+
+    $.export("$summary", `Successfully retrieved incident with ID \`${response.id}\``);
+
+    return response;
+  },
+};
