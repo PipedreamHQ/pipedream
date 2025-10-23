09b338be43bd4abdb8ab6766fb00025e95b08ea0
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-23 13:58:26 -0400

    updates

--- components/sharepoint/common/utils.mjs
+++ components/sharepoint/common/utils.mjs
@@ -7,44 +7,4 @@ export default {
     }
     return o;
   },
-  adjustPropDefinitions(props, app) {
-    return Object.fromEntries(
-      Object.entries(props).map(([
-        key,
-        prop,
-      ]) => {
-        if (typeof prop === "string") return [
-          key,
-          prop,
-        ];
-        const {
-          propDefinition, ...otherValues
-        } = prop;
-        if (propDefinition) {
-          const [
-            , ...otherDefs
-          ] = propDefinition;
-          return [
-            key,
-            {
-              propDefinition: [
-                app,
-                ...otherDefs,
-              ],
-              ...otherValues,
-            },
-          ];
-        }
-        return [
-          key,
-          otherValues.type === "app"
-            ? null
-            : otherValues,
-        ];
-      })
-        .filter(([
-          , value,
-        ]) => value),
-    );
-  },
 };
--- components/sharepoint/sources/updated-list-item/updated-list-item.mjs
+++ components/sharepoint/sources/updated-list-item/updated-list-item.mjs
@@ -5,7 +5,7 @@ export default {
   key: "sharepoint-updated-list-item",
   name: "Updated List Item",
   description: "Emit new event when a list item is updated in Microsoft Sharepoint.",
-  version: "0.0.5",
+  version: "0.0.6",
   type: "source",
   dedupe: "unique",
   props: {
