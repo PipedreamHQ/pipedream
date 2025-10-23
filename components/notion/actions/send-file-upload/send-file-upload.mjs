d956cd7d80aaf18948d71008668b4b420f19953c
michelle0927 <michelle0927@users.noreply.github.com>
2025-10-23 15:18:10 -0400

    Notion improvements - use notion-helper (#18797)
    * use notion-helper
    
    * pnpm-lock.yaml
    
    * versions

--- components/notion/actions/send-file-upload/send-file-upload.mjs
+++ components/notion/actions/send-file-upload/send-file-upload.mjs
@@ -8,7 +8,7 @@ export default {
   key: "notion-send-file-upload",
   name: "Send File Upload",
   description: "Send a file upload. [See the documentation](https://developers.notion.com/reference/send-a-file-upload)",
-  version: "0.0.6",
+  version: "0.0.7",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
