c710b2864211f29aa43c984a9b638d296a30b661
michelle0927 <michelle0927@users.noreply.github.com>
2025-10-27 22:37:52 -0400

    Merging pull request #18859
    * jira improvements
    
    * typo
    
    * updates

--- components/jira/actions/add-watcher-to-issue/add-watcher-to-issue.mjs
+++ components/jira/actions/add-watcher-to-issue/add-watcher-to-issue.mjs
@@ -3,13 +3,13 @@ import jira from "../../jira.app.mjs";
 export default {
   key: "jira-add-watcher-to-issue",
   name: "Add Watcher To Issue",
-  version: "0.0.12",
+  version: "0.0.13",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
     readOnlyHint: false,
   },
-  description: "Adds a user as a watcher of an issue by passing the account ID of the user, For example, `5b10ac8d82e05b22cc7d4ef5`, If no user is specified the calling user is added. [See the docs](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-watchers/#api-rest-api-3-issue-issueidorkey-watchers-post)",
+  description: "Adds a user as a watcher of an issue by passing the account ID of the user, For example, `5b10ac8d82e05b22cc7d4ef5`, If no user is specified the calling user is added. [See the documentation](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-watchers/#api-rest-api-3-issue-issueidorkey-watchers-post)",
   type: "action",
   props: {
     jira,
