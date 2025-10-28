f90c7b1fd9df6ad6f5040765349ffbd6dbde4daa
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-10-28 14:24:18 -0400

    create-campaign action

--- components/grabpenny/grabpenny.app.mjs
+++ components/grabpenny/grabpenny.app.mjs
@@ -1,11 +1,56 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "grabpenny",
-  propDefinitions: {},
+  propDefinitions: {
+    taskTypeId: {
+      type: "integer",
+      label: "Task Type ID",
+      description: "ID of the task type",
+      async options() {
+        const { tasks } = await this.listTaskTypes();
+        return tasks.map((task) => ({
+          label: task.name,
+          value: task.id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://grabpenny.com/api/v1";
+    },
+    async _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      try {
+        return await axios($, {
+          url: `${this._baseUrl()}${path}`,
+          headers: {
+            Authorization: `Bearer ${this.$auth.api_key}`,
+          },
+          ...opts,
+        });
+      } catch (e) {
+        if (e.response.status === 500) {
+          throw new Error("Error occurred. Please verify that your account balance is sufficient to perform this action.");
+        }
+        throw e;
+      }
+    },
+    listTaskTypes(args = {}) {
+      return this._makeRequest({
+        path: "/client/tasks/",
+        ...args,
+      });
+    },
+    createCampaign(args = {}) {
+      return this._makeRequest({
+        path: "/client/campaigns/create/",
+        method: "POST",
+        ...args,
+      });
     },
   },
 };
