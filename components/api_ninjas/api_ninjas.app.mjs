37b55f76d1179ef824bc0786c3cc6daebe738bfe
Lucas Caresia <lucascarezia@gmail.com>
2025-11-01 11:13:23 -0300

    Added actions

--- components/api_ninjas/api_ninjas.app.mjs
+++ components/api_ninjas/api_ninjas.app.mjs
@@ -1,11 +1,62 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "api_ninjas",
-  propDefinitions: {},
+  propDefinitions: {
+    domain: {
+      type: "string",
+      label: "Domain",
+      description: "Domain that will be checked",
+    },
+    length: {
+      type: "string",
+      label: "Length",
+      description: "Length of the password that will be generated",
+    },
+    address: {
+      type: "string",
+      label: "IP",
+      description: "IP address to lookup",
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.api-ninjas.com/v1";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          "X-Api-Key": `${this.$auth.api_key}`,
+          ...headers,
+        },
+      });
+    },
+    async checkDomain(args = {}) {
+      return this._makeRequest({
+        path: "/domain",
+        ...args,
+      });
+    },
+    async generatePassword(args = {}) {
+      return this._makeRequest({
+        path: "/passwordgenerator",
+        ...args,
+      });
+    },
+    async ipLookup(args = {}) {
+      return this._makeRequest({
+        path: "/iplookup",
+        ...args,
+      });
     },
   },
 };
