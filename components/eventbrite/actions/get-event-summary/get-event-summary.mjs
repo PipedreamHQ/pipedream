11aee604707243d7c88e2da022dd24f2924df4d7
choeqq <kacper.chola01@gmail.com>
2025-11-02 16:32:28 +0100

    bump versions

--- components/eventbrite/actions/get-event-summary/get-event-summary.mjs
+++ components/eventbrite/actions/get-event-summary/get-event-summary.mjs
@@ -4,7 +4,7 @@ export default {
   key: "eventbrite-get-event-summary",
   name: "Get Event Summary",
   description: "Get event summary for a specified event. [See the documentation](https://www.eventbrite.com/platform/api#/reference/event/retrieve/retrieve-an-event)",
-  version: "0.0.4",
+  version: "0.0.5",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
