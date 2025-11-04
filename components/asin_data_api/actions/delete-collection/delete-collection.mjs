be8a45e44cac9c2f614c93ae3e3cedb375e792c8
Lucas Caresia <lucascarezia@gmail.com>
2025-11-04 14:15:37 -0300

    Added actions

---
+++ components/asin_data_api/actions/delete-collection/delete-collection.mjs
@@ -0,0 +1,31 @@
+import app from "../../asin_data_api.app.mjs";
+
+export default {
+  key: "asin_data_api-delete-collection",
+  name: "Delete Collection",
+  description: "Delete a collection from Asin Data API. [See the documentation](https://docs.trajectdata.com/asindataapi/collections-api/collections/delete)",
+  version: "0.0.1",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  type: "action",
+  props: {
+    app,
+    collectionId: {
+      propDefinition: [
+        app,
+        "collectionId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.deleteCollection({
+      $,
+      collectionId: this.collectionId,
+    });
+    $.export("$summary", "Successfully deleted the collection with ID: " + this.collectionId);
+    return response;
+  },
+};
