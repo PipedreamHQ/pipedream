0cd6339e1b5c4bf80139c6596133fbdd9e581c45
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-05 11:11:41 -0500

    add userAssigned activity type to cardChanged trigger

--- components/planview_leankit/sources/card-updated-on-board/card-updated-on-board.mjs
+++ components/planview_leankit/sources/card-updated-on-board/card-updated-on-board.mjs
@@ -17,10 +17,10 @@ export default {
       return this.planviewLeankit.listActivity;
     },
     validate(d) {
-      return d.type === "cardChanged";
+      return d.type === "cardChanged" || d.type === "userAssigned";
     },
     getSummary(data) {
-      return `Card '${data.data.card.title}' was updated on the board '${this.boardId.label}'`;
+      return `Card '${data.data.card.title}' was updated on the board '${this.boardId?.label || this.boardId}'`;
     },
   },
 };
