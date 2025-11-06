a654435ea85d74d49d49250860c4c515cf73cf23
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-06 11:39:24 -0500

    updates

--- components/zendesk/sources/ticket-added-to-view/ticket-added-to-view.mjs
+++ components/zendesk/sources/ticket-added-to-view/ticket-added-to-view.mjs
@@ -32,10 +32,6 @@ export default {
             field: "update_type",
             value: "Change",
           },
-          {
-            field: "update_type",
-            value: "Create",
-          },
         ],
       };
     },
