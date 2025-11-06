f4d35048e9eb26df7e1e02039314d0da51081e25
Reinhard Söllradl <7169663+wrktbiz@users.noreply.github.com>
2025-11-06 21:04:30 +0100

    Update Form.taxi Pipedream source with latest version

--- components/form_taxi/form_taxi.app.mjs
+++ components/form_taxi/form_taxi.app.mjs
@@ -4,26 +4,10 @@ export default {
   type: "app",
   app: "form_taxi",
   description: "Receive submissions from Form.taxi",
-  // auth: {
-  //   type: "custom",
-  //   fields: {
-  //     form_code: {
-  //       type: "string",
-  //       label: "Form Code",
-  //     },
-  //     api_key: {
-  //       type: "string",
-  //       label: "API Key",
-  //       secret: true,
-  //     },
-  //   },
-  // },
   methods: {
-
     _baseUrl() {
       return `https://form.taxi/int/pipedream`;
     },
-
     _makeRequest({
       $ = this, path, headers, ...opts
     }) {
@@ -38,7 +22,6 @@ export default {
         ...opts,
       });
     },
-
     async createWebhook(hook_url) {
       return this._makeRequest({
         method: "POST",
@@ -46,7 +29,6 @@ export default {
         data: { hook_url },
       });
     },
-
     async deleteWebhook(id) {
       return this._makeRequest({
         method: "DELETE",
@@ -56,57 +38,11 @@ export default {
         }
       });
     },
-
     async getSampleData() {
       return this._makeRequest({
         method: "GET",
         path: "/api/"+this.$auth.form_code,
       });
     },
-
-    // _apiUrl(form_code) {
-    //   return `https://form.taxi/int/pipedream/api/${encodeURIComponent(form_code)}`;
-    // },
-    // _headers(headers, api_key) {
-    //   return {
-    //     ...headers,
-    //     "Api-Key": api_key,
-    //     "Accept": "application/json",
-    //     "Content-Type": "application/json",
-    //   };
-    // },
-    // _makeRequest({
-    //   $ = this, headers, form_code, api_key, ...opts
-    // }) {
-    //   return axios($, {
-    //     url: this._apiUrl(form_code),
-    //     headers: this._headers(headers, api_key),
-    //     ...opts,
-    //   });
-    // },
-    // registerWebhook(form_code, api_key, opts = {},) {
-    //   return this._makeRequest({
-    //     method: "POST",
-    //     form_code: form_code,
-    //     api_key: api_key,
-    //     ...opts,
-    //   })
-    // },
-    // removeWebhook(form_code, api_key, opts = {}) {
-    //   return this._makeRequest({
-    //     method: "DELETE",
-    //     form_code: form_code,
-    //     api_key: api_key,
-    //     ...opts,
-    //   })
-    // },
-    // loadSampleData(form_code, api_key, opts = {}) {
-    //   return this._makeRequest({
-    //     method: "GET",
-    //     form_code: form_code,
-    //     api_key: api_key,
-    //     ...opts,
-    //   })
-    // },
   },
 }
