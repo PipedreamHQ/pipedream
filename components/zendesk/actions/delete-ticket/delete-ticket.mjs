2910fb4dce08311e59d90bdff97cfeb1664bd906
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-06 11:41:03 -0500

    versions

--- components/zendesk/actions/delete-ticket/delete-ticket.mjs
+++ components/zendesk/actions/delete-ticket/delete-ticket.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Delete Ticket",
   description: "Deletes a ticket. [See the documentation](https://developer.zendesk.com/api-reference/ticketing/tickets/tickets/#delete-ticket).",
   type: "action",
-  version: "0.1.9",
+  version: "0.1.10",
   annotations: {
     destructiveHint: true,
     openWorldHint: true,
