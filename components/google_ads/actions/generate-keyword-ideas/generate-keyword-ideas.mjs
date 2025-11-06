dfabdb3e09861000e1761102b2c0784cd51cca9c
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-06 13:40:48 -0500

    generate-keyword-ideas

---
+++ components/google_ads/actions/generate-keyword-ideas/generate-keyword-ideas.mjs
@@ -0,0 +1,36 @@
+import common from "../common/common.mjs";
+import { getAdditionalFields } from "../common/props.mjs";
+import {
+  parseObject, parseStringObject,
+} from "../../common/utils.mjs";
+const docLink = "https://developers.google.com/google-ads/api/reference/rpc/v22/KeywordPlanIdeaService/GenerateKeywordIdeas?transport=rest";
+
+export default {
+  key: "google-ads-generate-keyword-ideas",
+  name: "Generate Keyword Ideas",
+  description: `Generate keyword ideas using the Google Ads API. [See the documentation](${docLink})`,
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    ...common.props,
+    additionalFields: getAdditionalFields(docLink),
+  },
+  async run({ $ }) {
+    const additionalFields = parseObject(parseStringObject(this.additionalFields));
+    const response = await this.googleAds.generateKeywordIdeas({
+      $,
+      accountId: this.accountId,
+      customerClientId: this.customerClientId,
+      data: {
+        ...additionalFields,
+      },
+    });
+    $.export("$summary", "Successfully generated keyword ideas.");
+    return response;
+  },
+};
