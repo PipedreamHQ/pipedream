9c745bb1e1ebfd8565d218d33e3cb65f687b7422
Lucas Caresia <lucascarezia@gmail.com>
2025-11-11 08:58:45 -0300

    Added actions

--- components/blocknative/blocknative.app.mjs
+++ components/blocknative/blocknative.app.mjs
@@ -1,11 +1,67 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "blocknative",
-  propDefinitions: {},
+  propDefinitions: {
+    confidenceLevels: {
+      type: "integer[]",
+      label: "confidenceLevels",
+      description: "Confidence levels to include in the gas price estimation",
+    },
+    chainid: {
+      type: "string",
+      label: "chainid",
+      description: "Blockchain network identifier to query gas prices for",
+      async options() {
+        const response = await this.getChains();
+        return response.map(({
+          label, chainId,
+        }) => ({
+          value: chainId,
+          label: label,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.blocknative.com";
+    },
+    async _makeRequest(opts = {}) {
+      const {
+        $ = this,
+        path,
+        headers,
+        ...otherOpts
+      } = opts;
+      return axios($, {
+        ...otherOpts,
+        url: this._baseUrl() + path,
+        headers: {
+          "Authorization": `${this.$auth.api_key}`,
+          ...headers,
+        },
+      });
+    },
+
+    async getBlockprices(args = {}) {
+      return this._makeRequest({
+        path: "/gasprices/blockprices",
+        ...args,
+      });
+    },
+    async getChains(args = {}) {
+      return this._makeRequest({
+        path: "/chains",
+        ...args,
+      });
+    },
+    async getOracles(args = {}) {
+      return this._makeRequest({
+        path: "/oracles",
+        ...args,
+      });
     },
   },
 };
