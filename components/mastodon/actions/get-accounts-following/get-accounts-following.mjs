f392728a46a92e31fdafda11fee80977bb6dd42e
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-11 16:50:58 -0500

    get-accounts-following

---
+++ components/mastodon/actions/get-accounts-following/get-accounts-following.mjs
@@ -0,0 +1,30 @@
+import mastodon from "../../mastodon.app.mjs";
+
+export default {
+  key: "mastodon-get-accounts-following",
+  name: "Get Accounts Following",
+  description: "Get the accounts that the given account is following. [See the documentation](https://docs.joinmastodon.org/methods/accounts/#following)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    mastodon,
+    max: {
+      propDefinition: [
+        mastodon,
+        "max",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const accounts = await this.mastodon.paginate(this.mastodon.getAccountsFollowing, {
+      $,
+    }, this.max);
+    $.export("$summary", `Successfully retrieved ${accounts.length} item(s) from accounts following`);
+    return accounts;
+  },
+};
