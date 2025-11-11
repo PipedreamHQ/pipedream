fab166b044f41ba2016d57b9b827b9c6a707e3eb
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-11-11 16:14:21 -0500

    new components

---
+++ components/trengo/actions/list-labels/list-labels.mjs
@@ -0,0 +1,44 @@
+import utils from "../../common/utils.mjs";
+import app from "../../trengo.app.mjs";
+
+export default {
+  key: "trengo-list-labels",
+  name: "List Labels",
+  description: "List all labels. [See the documentation](https://developers.trengo.com/reference/update-a-label)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    app,
+    maxResults: {
+      type: "integer",
+      label: "Max Results",
+      description: "Maximum number of labels to return (if not specified, all results will be returned)",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const labels = [];
+    const resourcesStream = utils.getResourcesStream({
+      resourceFn: this.app.listLabels,
+      resourceFnArgs: {
+        $,
+      },
+    });
+    for await (const item of resourcesStream) {
+      labels.push(item);
+      if (this.maxResults && labels.length >= this.maxResults) {
+        break;
+      }
+    }
+    const length = labels.length;
+    $.export("$summary", `Successfully retrieved ${length} label${length === 1
+      ? ""
+      : "s"}`);
+    return labels;
+  },
+};
