d092bb7dd45bf5fd44d31aacd599edfd46c30d6a
Luan Cazarine <luanhc@gmail.com>
2025-11-11 16:56:25 -0300

    Implement AEvent component with registrant listing and new registrant source
    - Added methods for API interaction including listing registrants and pagination.
    - Introduced a new source for emitting events when a new registrant is added.
    - Updated package version to 0.1.0 and added dependency on @pipedream/platform.

--- components/aevent/aevent.app.mjs
+++ components/aevent/aevent.app.mjs
@@ -1,11 +1,58 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "aevent",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiUrl() {
+      return "https://app.aevent.com/api";
+    },
+    _getHeaders() {
+      return {
+        "Authorization": `Bearer ${this.$auth.api_token}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}/${path}`,
+        headers: this._getHeaders(),
+        ...opts,
+      });
+    },
+    listRegistrants(args = {}) {
+      return this._makeRequest({
+        path: "registrants",
+        ...args,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null,
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.page = page++;
+        const { success: data } = await fn({
+          params,
+        });
+
+        for (const d of data) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data.length;
+
+      } while (hasMore);
     },
   },
 };
