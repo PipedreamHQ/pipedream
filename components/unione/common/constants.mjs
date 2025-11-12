5477b2d6589b41f83214accfa9a8a6b78aa87b43
Luan Cazarine <luanhc@gmail.com>
2025-11-12 13:56:09 -0300

    Update UniOne component: bump version to 0.1.0, add dependencies, implement email sending action, and introduce new constants and utility functions.

---
+++ components/unione/common/constants.mjs
@@ -0,0 +1,89 @@
+export const LIMIT = 100;
+
+export const SKIP_UNSUBSCRIBE_OPTIONS = [
+  {
+    label: "Skip",
+    value: "1",
+  },
+  {
+    label: "Append",
+    value: "0",
+  },
+];
+
+export const GLOBAL_LANGUAGE_OPTIONS = [
+  {
+    label: "Belarusian",
+    value: "be",
+  },
+  {
+    label: "German",
+    value: "de",
+  },
+  {
+    label: "English",
+    value: "en",
+  },
+  {
+    label: "Spanish",
+    value: "es",
+  },
+  {
+    label: "French",
+    value: "fr",
+  },
+  {
+    label: "Italian",
+    value: "it",
+  },
+  {
+    label: "Polish",
+    value: "pl",
+  },
+  {
+    label: "Portuguese",
+    value: "pt",
+  },
+  {
+    label: "Russian",
+    value: "ru",
+  },
+  {
+    label: "Ukrainian",
+    value: "ua",
+  },
+  {
+    label: "Kazakh",
+    value: "kz",
+  },
+];
+
+export const TEMPLATE_ENGINE_OPTIONS = [
+  {
+    label: "Simple",
+    value: "simple",
+  },
+  {
+    label: "Velocity",
+    value: "velocity",
+  },
+  {
+    label: "Liquid",
+    value: "liquid",
+  },
+  {
+    label: "None",
+    value: "none",
+  },
+];
+
+export const TRACK_OPTIONS = [
+  {
+    label: "On",
+    value: "1",
+  },
+  {
+    label: "Off",
+    value: "0",
+  },
+];
