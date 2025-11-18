b616f42ad66975f9c95f5780f6abf8c0e968139a
Jorge Cortes <jacortesmahmud@gmail.com>
2025-11-18 12:10:22 -0500

    [Deprecation] Xola - Deprecated order.* sources and introduced purchase.* sources (#19062)
    * [Deprecation] Xola - Deprecated order.* sources and introduced purchase.* sources
    
    * Update components/xola/sources/order-deleted-instant/order-deleted-instant.mjs
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/xola/sources/purchase-updated-instant/purchase-updated-instant.mjs
+++ components/xola/sources/purchase-updated-instant/purchase-updated-instant.mjs
@@ -3,23 +3,23 @@ import sampleEmit from "./test-event.mjs";
 
 export default {
   ...common,
-  key: "xola-order-deleted-instant",
-  name: "Order Deleted (Instant)",
-  description: "Emit new event when an order is deleted. [See the documentation](https://developers.xola.com/reference/webhook-introduction)",
-  version: "0.0.2",
+  key: "xola-purchase-updated-instant",
+  name: "Purchase Updated (Instant)",
+  description: "Emit new event when a purchase is updated. [See the documentation](https://developers.xola.com/reference/webhook-introduction)",
+  version: "0.0.1",
   type: "source",
   dedupe: "unique",
   methods: {
     ...common.methods,
     getEventName() {
-      return "order.delete";
+      return "purchase.update";
     },
     generateMeta(body) {
       const { data } = body;
-      const ts = Date.now();
+      const ts = Date.parse(data.updatedAt);
       return {
         id: `${data.id}-${ts}`,
-        summary: `Order Deleted ${data.id}`,
+        summary: `Purchase Updated ${data.id}`,
         ts,
       };
     },
