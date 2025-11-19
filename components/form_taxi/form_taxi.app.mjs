1ad795f50a87724ba9819efa6123dd62ec574571
Reinhard Söllradl <7169663+wrktbiz@users.noreply.github.com>
2025-11-19 14:33:08 +0100

    Added Component Form.taxi (#18198)
    * Added Form.taxi
    
    * Update components/form_taxi/package.json
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update components/form_taxi/sources/new-form-submission/new-form-submission.mjs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update package.json
    
    * Update new-form-submission.mjs
    
    Hook Correction
    
    * Apply suggestion from @lcaresia
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Apply suggestion from @lcaresia
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update Form.taxi component with latest code revisions
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update components/form_taxi/sources/new-form-submission/new-form-submission.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/form_taxi.app.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/sources/common/base.mjs
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update components/form_taxi/package.json
    
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi Pipedream source with latest version
    
    * Update Form.taxi component with latest code changes
    
    * update pnpm-lock
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>
    Co-authored-by: Lucas Caresia <lucascarezia@gmail.com>
    Co-authored-by: GTFalcao <gtfalcao96@gmail.com>

--- components/form_taxi/form_taxi.app.mjs
+++ components/form_taxi/form_taxi.app.mjs
@@ -1,11 +1,50 @@
+import { axios } from "@pipedream/platform";
+
 export default {
+  propDefinitions: {},
   type: "app",
   app: "form_taxi",
-  propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://form.taxi/int/pipedream/api/";
+    },
+    _makeRequest({
+      $ = this, path, headers, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: {
+          ...headers,
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Api-Key": this.$auth.api_key,
+        },
+        ...opts,
+      });
+    },
+    async createWebhook(hook_url) {
+      return this._makeRequest({
+        method: "POST",
+        path: this.$auth.form_code,
+        data: {
+          hook_url,
+        },
+      });
+    },
+    async deleteWebhook(id) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: this.$auth.form_code,
+        params: {
+          "hook_id": id,
+        },
+      });
+    },
+    async getSampleData() {
+      return this._makeRequest({
+        method: "GET",
+        path: this.$auth.form_code,
+      });
     },
   },
 };
