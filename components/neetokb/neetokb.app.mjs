93881989ceb90ebd949f878647400fc8c703038c
Luan Cazarine <luanhc@gmail.com>
2025-11-25 12:07:58 -0500

    Implement neetokb API integration with new methods for listing articles and pagination; add new source for detecting published articles; update package version to 0.1.0 and include dependencies.

--- components/neetokb/neetokb.app.mjs
+++ components/neetokb/neetokb.app.mjs
@@ -1,11 +1,65 @@
+import { axios } from "@pipedream/platform";
+import { LIMIT } from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "neetokb",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _apiUrl() {
+      return `https://${this.$auth.organization_name}.neetokb.com/api/v1`;
+    },
+    _getHeaders() {
+      return {
+        "Content-Type": "application/json",
+        "X-Api-Key": `${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._apiUrl()}/${path}`,
+        headers: this._getHeaders(),
+        ...opts,
+      });
+    },
+    listArticles(args = {}) {
+      return this._makeRequest({
+        path: "articles",
+        ...args,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null, ...opts
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.page_number = ++page;
+        params.page_size = LIMIT;
+        const {
+          articles,
+          pagination: {
+            total_pages: tPages, current_page_number: cPage,
+          },
+        } = await fn({
+          params,
+          ...opts,
+        });
+        for (const d of articles) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = cPage < tPages;
+
+      } while (hasMore);
     },
   },
 };
---
+++ components/neetokb/sources/new-published-article/new-published-article.mjs
@@ -0,0 +1,69 @@
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+import neetokb from "../../neetokb.app.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  key: "neetokb-new-action",
+  name: "New Published Article",
+  description: "Emit new event when a new article is published. [See the documentation](https://neetokb-apis.mintlify.app/api-reference/articles/list-articles).",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    neetokb,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  methods: {
+    _getPublishedIds() {
+      return this.db.get("publishedIds") || [];
+    },
+    _setPublishedIds(publishedIds) {
+      this.db.set("publishedIds", publishedIds);
+    },
+    async emitEvent(maxResults = false) {
+      const publishedIds = this._getPublishedIds();
+      const response = this.neetokb.paginate({
+        fn: this.neetokb.listArticles,
+        params: {
+          status: "published",
+        },
+      });
+
+      const responseArray = [];
+      for await (const item of response) {
+        responseArray.push(item);
+      }
+
+      let newArticles = responseArray.filter((article) => !publishedIds.includes(article.id));
+
+      if (maxResults && newArticles.length > maxResults) {
+        newArticles = newArticles.slice(0, maxResults);
+      }
+
+      for (const article of newArticles.reverse()) {
+        const ts = new Date();
+        this.$emit(article, {
+          id: `${article.id}-${ts}`,
+          summary: `New Published Article: ${article.title}`,
+          ts,
+        });
+      }
+      this._setPublishedIds(responseArray.map((article) => article.id));
+    },
+  },
+  hooks: {
+    async deploy() {
+      await this.emitEvent(25);
+    },
+  },
+  async run() {
+    await this.emitEvent();
+  },
+  sampleEmit,
+};
