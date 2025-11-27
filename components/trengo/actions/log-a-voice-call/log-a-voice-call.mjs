e78a7499d2719ebac3ef69f6b5ead505a113b027
Jorge Cortes <jacortesmahmud@gmail.com>
2025-11-27 14:06:55 -0500

    [ACTION] trengo - close ticket (#19278)

--- components/trengo/actions/log-a-voice-call/log-a-voice-call.mjs
+++ components/trengo/actions/log-a-voice-call/log-a-voice-call.mjs
@@ -3,7 +3,7 @@ import app from "../../trengo.app.mjs";
 export default {
   type: "action",
   key: "trengo-log-a-voice-call",
-  version: "0.0.7",
+  version: "0.0.8",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
