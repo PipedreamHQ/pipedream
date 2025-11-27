be57ab629fd15a46f00f39130de7f327bd554e03
Jorge Cortes <jacortesmahmud@gmail.com>
2025-11-27 11:30:58 -0500

    [ACTION] Google Sheets - Provide polling options as an alternative to current webhook based ones

---
+++ components/google_sheets/sources/new-updates-polling/test-event.mjs
@@ -0,0 +1,71 @@
+export default {
+  "worksheet": {
+    "properties": {
+      "sheetId": 358595775,
+      "title": "Test",
+      "index": 0,
+      "sheetType": "GRID",
+      "gridProperties": {
+        "rowCount": 1029,
+        "columnCount": 50
+      }
+    }
+  },
+  "currentValues": {
+    "values": [
+      [
+        "1",
+        "Leo 1 Updated"
+      ],
+      [
+        "2",
+        "Leo 2"
+      ],
+      [
+        "3",
+        "Leo 3"
+      ],
+      [
+        "4",
+        "Updated"
+      ],
+      [
+        "5",
+        "Updated 0857"
+      ],
+      [
+        "6",
+        "Update 0858"
+      ]
+    ],
+    "range": "Test!A1:AX1029",
+    "majorDimension": "ROWS"
+  },
+  "changes": [
+    {
+      "cell": "B:4",
+      "previous_value": "",
+      "new_value": "Updated"
+    },
+    {
+      "cell": "A:5",
+      "previous_value": "",
+      "new_value": "5"
+    },
+    {
+      "cell": "B:5",
+      "previous_value": "",
+      "new_value": "Updated 0857"
+    },
+    {
+      "cell": "A:6",
+      "previous_value": "",
+      "new_value": "6"
+    },
+    {
+      "cell": "B:6",
+      "previous_value": "",
+      "new_value": "Update 0858"
+    }
+  ]
+}
