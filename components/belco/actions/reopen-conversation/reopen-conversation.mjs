1f43a7d28731b3bf14f978cbdedc27081a2eab88
michelle0927 <michelle0927@users.noreply.github.com>
2025-11-27 11:15:35 -0500

    Belco - Add Tags to Conversation action (#19276)
    * add-tags-to-conversation
    
    * versions

--- components/belco/actions/reopen-conversation/reopen-conversation.mjs
+++ components/belco/actions/reopen-conversation/reopen-conversation.mjs
@@ -4,7 +4,7 @@ export default {
   key: "belco-reopen-conversation",
   name: "Reopen Conversation",
   description: "Reopen a conversation specified by ID. [See the documentation](https://developers.belco.io/reference/put_conversations-conversationid-open)",
-  version: "0.0.2",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
