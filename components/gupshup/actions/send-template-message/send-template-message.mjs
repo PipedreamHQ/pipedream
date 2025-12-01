2beee75bf4b0f94345b2b7e362f5a3b882381175
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-01 15:41:58 -0500

    new components

---
+++ components/gupshup/actions/send-template-message/send-template-message.mjs
@@ -0,0 +1,55 @@
+import gupshup from "../../gupshup.app.mjs";
+
+export default {
+  key: "gupshup-send-template-message",
+  name: "Send Template Message",
+  description: "Send a template message. Requires a paid Gupshup account. [See the documentation](https://docs.gupshup.io/reference/sending-text-template)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    gupshup,
+    source: {
+      type: "string",
+      label: "Source",
+      description: "Sender Whatsapp Number",
+    },
+    destination: {
+      type: "string",
+      label: "Destination",
+      description: "Receiver Whatsapp Number",
+    },
+    templateId: {
+      propDefinition: [
+        gupshup,
+        "templateId",
+      ],
+    },
+    params: {
+      type: "string[]",
+      label: "Parameters",
+      description: "List of template parameters",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.gupshup.sendTemplateMessage({
+      $,
+      data: {
+        source: this.source,
+        destination: this.destination,
+        template: {
+          id: this.templateId,
+          params: this.params,
+        },
+      },
+    });
+    if (response.status === "success") {
+      $.export("$summary", `Successfully sent template message to ${this.destination}`);
+    }
+    return response;
+  },
+};
--- components/gupshup/gupshup.app.mjs
+++ components/gupshup/gupshup.app.mjs
@@ -1,11 +1,62 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "gupshup",
-  propDefinitions: {},
+  propDefinitions: {
+    templateId: {
+      type: "string",
+      label: "Template ID",
+      description: "The ID of the template to use",
+      async options({ page }) {
+        const { data } = await this.listTemplates({
+          params: {
+            pageNo: page,
+          },
+        });
+        return data.map(({ id }) => id);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.gupshup.io";
+    },
+    _appName() {
+      return this.$auth.appname;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "apikey": `${this.$auth.apikey}`,
+        },
+        ...opts,
+      });
+    },
+    listTemplates(opts = {}) {
+      return this._makeRequest({
+        path: `/wa/app/${this._appName()}/template`,
+        ...opts,
+      });
+    },
+    sendTemplateMessage(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/wa/app/v1/template/msg",
+        ...opts,
+      });
+    },
+    updateSubscription({
+      subscriptionId, ...opts
+    }) {
+      return this._makeRequest({
+        method: "PUT",
+        path: `/wa/app/${this._appName()}/subscription/${subscriptionId}`,
+        ...opts,
+      });
     },
   },
 };
