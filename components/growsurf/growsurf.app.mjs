ab99c68ba10527645094fed4a4df2daf7e5fa0f6
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-03 13:18:18 -0500

    new components

--- components/growsurf/growsurf.app.mjs
+++ components/growsurf/growsurf.app.mjs
@@ -1,11 +1,80 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "growsurf",
-  propDefinitions: {},
+  propDefinitions: {
+    campaignId: {
+      type: "string",
+      label: "Campaign ID",
+      description: "The ID of the campaign to list",
+      async options() {
+        const { campaigns } = await this.listCampaigns();
+        return campaigns?.map((campaign) => ({
+          label: campaign.name,
+          value: campaign.id,
+        })) || [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.growsurf.com/v2";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this.$auth.api_key}`,
+        },
+        ...opts,
+      });
+    },
+    listCampaigns(opts = {}) {
+      return this._makeRequest({
+        path: "/campaigns",
+        ...opts,
+      });
+    },
+    listParticipants({
+      campaignId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/campaign/${campaignId}/participants`,
+        ...opts,
+      });
+    },
+    listReferrals({
+      campaignId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/campaign/${campaignId}/referrals`,
+        ...opts,
+      });
+    },
+    async *paginate({
+      fn, args, resourceKey, max,
+    }) {
+      args.params = args.params || {};
+      let hasMore = true;
+      let count = 0;
+      do {
+        const response = await fn(args);
+        const items = response[resourceKey];
+        if (!items?.length) {
+          return;
+        }
+        for (const item of items) {
+          yield item;
+          if (max && ++count >= max) {
+            return;
+          }
+        }
+        hasMore = response.nextId;
+        args.params.nextId = response.nextId;
+      } while (hasMore);
     },
   },
 };
