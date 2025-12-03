df21a4f82c473efc6f157ece44e6cdb93aa2a798
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-03 15:24:46 -0500

    wip

---
+++ components/wics/actions/delete-order-line/delete-order-line.mjs
@@ -0,0 +1,41 @@
+import wics from "../../wics.app.mjs";
+
+export default {
+  key: "wics-delete-order-line",
+  name: "Delete Order Line",
+  description: "Delete a line from an order. [See the documentation](https://docs.wics.nl/test-environment.html#orders-delete-order-line)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    wics,
+    orderReference: {
+      propDefinition: [
+        wics,
+        "orderReference",
+      ],
+    },
+    lineNumber: {
+      propDefinition: [
+        wics,
+        "lineNumber",
+        (c) => ({
+          orderReference: c.orderReference,
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    const { data } = await this.wics.deleteOrderLine({
+      $,
+      orderReference: this.orderReference,
+      lineNumber: this.lineNumber,
+    });
+    $.export("$summary", `Successfully deleted line ${this.lineNumber} from order ${this.orderReference}`);
+    return data;
+  },
+};
