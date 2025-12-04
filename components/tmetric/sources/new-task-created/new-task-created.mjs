06fc58432b8bbfa49b24c519739477e5abbe5663
Lucas Caresia <lucascarezia@gmail.com>
2025-12-04 16:30:54 -0300

    Added actions

--- components/tmetric/sources/new-task-created/new-task-created.mjs
+++ components/tmetric/sources/new-task-created/new-task-created.mjs
@@ -3,8 +3,8 @@ import sampleEmit from "./test-event.mjs";
 
 export default {
   ...common,
-  key: "tmetric-new-task",
-  name: "New Task",
+  key: "tmetric-new-task-created",
+  name: "New Task Created",
   description: "Emit new event when a task is created. [See the docs here](https://app.tmetric.com/api-docs/?_gl=1*n76hcy*_gcl_aw*R0NMLjE3NjQwOTU2MDguQ2owS0NRaUF4SlhKQmhEX0FSSXNBSF9KR2pncU8zZzgxcHp3VUhQWGdjazEyUWFTaThXeE00ZTBUZ1hvak5ma1AzeG10a1pGYWJuek8wOGFBbC1KRUFMd193Y0I.*_gcl_au*MjU2MzU4NTI3LjE3NjQwOTU2MDg.*_ga*ODE4ODUyMDE2LjE3NjQwOTU2MDM.*_ga_66EFKVJKC5*czE3NjQwOTU2MDMkbzEkZzEkdDE3NjQwOTYyODkkajQzJGwwJGgw#/Tasks/get-accounts-accountId-tasks)",
   type: "source",
   version: "0.0.1",
