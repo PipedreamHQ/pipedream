b94e94f9a5b836a01a16c4ed7c21e777908b9d74
Lucas Caresia <lucascarezia@gmail.com>
2025-12-04 10:33:07 -0300

    Add search action for spotify #18791 (#19358)
    * Added actions
    
    * Bump versions

--- components/spotify/actions/get-album-tracks/get-album-tracks.mjs
+++ components/spotify/actions/get-album-tracks/get-album-tracks.mjs
@@ -7,7 +7,7 @@ export default {
   name: "Get Album Tracks",
   description: "Get all tracks in an album. [See the docs here](https://developer.spotify.com/documentation/web-api/reference/get-an-albums-tracks)",
   key: "spotify-get-album-tracks",
-  version: "0.0.3",
+  version: "0.0.4",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
---
+++ components/spotify/actions/search/search.mjs
@@ -0,0 +1,88 @@
+import { ConfigurationError } from "@pipedream/platform";
+import spotify from "../../spotify.app.mjs";
+import {
+  ITEM_TYPES_LIST, ITEM_TYPES,
+} from "../../consts.mjs";
+
+export default {
+  key: "spotify-search",
+  name: "Search",
+  description: "Search for items on Spotify (tracks, albums, artists, playlists, shows, or episodes). [See the docs here](https://developer.spotify.com/documentation/web-api/reference/search)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    spotify,
+    query: {
+      type: "string",
+      label: "Search Query",
+      description: "Search query keywords and optional field filters. [See the Spotify docs for query syntax](https://developer.spotify.com/documentation/web-api/reference/search)",
+    },
+    type: {
+      type: "string[]",
+      label: "Search Type",
+      description: "Type(s) of items to search for",
+      options: ITEM_TYPES_LIST,
+      default: [
+        ITEM_TYPES.TRACK,
+      ],
+    },
+    market: {
+      propDefinition: [
+        spotify,
+        "market",
+      ],
+      optional: true,
+    },
+    limit: {
+      propDefinition: [
+        spotify,
+        "limit",
+      ],
+      description: "The maximum number of results to return per type.",
+      default: 20,
+      max: 50,
+    },
+    offset: {
+      propDefinition: [
+        spotify,
+        "offset",
+      ],
+    },
+    includeExternal: {
+      type: "string",
+      label: "Include External",
+      description: "If `audio` is specified, the response will include any relevant audio content that is hosted externally.",
+      optional: true,
+      options: [
+        "audio",
+      ],
+    },
+  },
+  async run({ $ }) {
+    if (!this.query || this.query.trim().length === 0) {
+      throw new ConfigurationError("Search `query` cannot be empty");
+    }
+
+    if (!this.type) {
+      throw new ConfigurationError("Select at least one search type");
+    }
+
+    const res = await this.spotify.search({
+      q: this.query,
+      type: this.type.join(","),
+      market: this.market,
+      limit: this.limit,
+      offset: this.offset,
+      include_external: this.includeExternal,
+    });
+
+    $.export("$summary", `Successfully retrieved results matching query: "${this.query}"`);
+
+    return res;
+  },
+};
