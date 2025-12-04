9f701205cc0a595907ba4053f9705a462336337f
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-04 16:38:25 -0500

    Update components/returnless/actions/list-return-statuses/list-return-statuses.mjs

--- components/returnless/actions/list-return-statuses/list-return-statuses.mjs
+++ components/returnless/actions/list-return-statuses/list-return-statuses.mjs
@@ -4,7 +4,7 @@ export default {
   key: "returnless-list-return-statuses",
   name: "List Return Statuses",
   description: "List all return statuses. [See the documentation](https://docs.returnless.com/docs/api-rest-reference/6129c8f41f66f-list-all-return-statuses)",
-  version: "0.0.2",
+  version: "0.0.1",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
