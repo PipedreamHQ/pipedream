e1a595240615fcc29acdcf103f83a76832e3b2ed
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-04 10:37:57 -0500

    Educateme - new components (#19357)
    * new components
    
    * pnpm-lock.yaml
    
    * updates

---
+++ components/educateme/sources/common/base.mjs
@@ -0,0 +1,31 @@
+import educateme from "../../educateme.app.mjs";
+import {
+  DEFAULT_POLLING_SOURCE_TIMER_INTERVAL, ConfigurationError,
+} from "@pipedream/platform";
+
+export default {
+  props: {
+    educateme,
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  methods: {
+    async getResources() {
+      throw new ConfigurationError("getResources method must be implemented");
+    },
+    generateMeta() {
+      throw new ConfigurationError("generateMeta method must be implemented");
+    },
+  },
+  async run() {
+    const resources = await this.getResources();
+    for (const resource of resources || []) {
+      const meta = this.generateMeta(resource);
+      this.$emit(resource, meta);
+    }
+  },
+};
