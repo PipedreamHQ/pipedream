d8bdb5a3cb441774df338a60de338213f47c42d5
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-04 10:39:25 -0500

    Hubspot - new components (#19230)
    * new components
    
    * versions
    
    * wip
    
    * updates
    
    * fix version
    
    * updates

--- components/hubspot/actions/create-meeting/create-meeting.mjs
+++ components/hubspot/actions/create-meeting/create-meeting.mjs
@@ -11,7 +11,7 @@ export default {
   name: "Create Meeting",
   description:
     "Creates a new meeting with optional associations to other objects. [See the documentation](https://developers.hubspot.com/docs/reference/api/crm/engagements/meetings#post-%2Fcrm%2Fv3%2Fobjects%2Fmeetings)",
-  version: "0.0.12",
+  version: "0.0.13",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
