4a8ce16f24ff74edf59623b8fc9e720c4836a145
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-05 15:04:20 -0500

    Slack - New Private Channel Created (#19376)
    * new-private-channel-created
    
    * pnpm-lock.yaml
    
    * move source to slack_v2
    
    * package.json
    
    * pnpm-lock.yaml
    
    * fix version
    
    * fix package version

---
+++ components/slack_v2/sources/common/base-polling.mjs
@@ -0,0 +1,50 @@
+import slack from "../../slack_v2.app.mjs";
+import {
+  DEFAULT_POLLING_SOURCE_TIMER_INTERVAL, ConfigurationError,
+} from "@pipedream/platform";
+
+export default {
+  props: {
+    slack,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  methods: {
+    _getLastTs() {
+      return this.db.get("lastTs") || 0;
+    },
+    _setLastTs(lastTs) {
+      this.db.set("lastTs", lastTs);
+    },
+    getResources() {
+      throw new ConfigurationError("getResources is not implemented");
+    },
+    generateMeta() {
+      throw new ConfigurationError("generateMeta is not implemented");
+    },
+    async processEvents(max) {
+      const results = await this.getResources(max);
+      if (!results.length) {
+        return;
+      }
+
+      results.forEach((result) => {
+        const meta = this.generateMeta(result);
+        this.$emit(result, meta);
+      });
+    },
+  },
+  hooks: {
+    async deploy() {
+      await this.processEvents(10);
+    },
+  },
+  async run() {
+    await this.processEvents();
+  },
+};
