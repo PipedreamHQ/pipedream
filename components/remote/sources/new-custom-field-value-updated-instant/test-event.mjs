dabd201cd3efe69a5a5dd68ad0ad0a67d67dc5d3
Luan Cazarine <luanhc@gmail.com>
2025-12-10 18:45:42 -0300

    Update test events for employment onboarding and custom field value updates
    - Change event types to reflect new actions: 'employment.onboarding.started' and 'custom_field.value_updated'.
    - Adjust employment IDs and custom field IDs in the respective test event files.

--- components/remote/sources/new-custom-field-value-updated-instant/test-event.mjs
+++ components/remote/sources/new-custom-field-value-updated-instant/test-event.mjs
@@ -1,5 +1,5 @@
 export default {
-  "employment_id": "2614f814-b08e-4c8e-8c4d-ddbcc4692d99",
-  "employment_request_id": "129d02bc-dd6a-11ed-ac99-cb057df06a33",
-  "event_type": "contract_amendment.done",
+  "custom_field_id": "2614f814-b08e-4c8e-8c4d-ddbcc4692d99",
+  "employment_id": "129d02bc-dd6a-11ed-ac99-cb057df06a33",
+  "event_type": "custom_field.value_updated",
 }
