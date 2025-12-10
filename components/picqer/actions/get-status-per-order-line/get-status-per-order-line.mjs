3f73239a8d6f9ced7cad9623c505bf8709fbbf22
Jorge Cortes <jacortesmahmud@gmail.com>
2025-12-10 09:40:56 -0500

    [ACTIONS] picqer - new components (#19383)

--- components/picqer/actions/get-status-per-order-line/get-status-per-order-line.mjs
+++ components/picqer/actions/get-status-per-order-line/get-status-per-order-line.mjs
@@ -4,7 +4,7 @@ export default {
   key: "picqer-get-status-per-order-line",
   name: "Get Status Per Order Line",
   description: "Get the status per order line in Picqer. [See the documentation](https://picqer.com/en/api/orders#get-status-per-order-line)",
-  version: "0.0.2",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
