96293e304ca49be0c35489d12a410f411f9f033c
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-11 10:23:15 -0500

    Habitify - new components (#19416)
    * fix file structure
    
    * new components
    
    * pnpm-lock.yaml
    
    * updates

---
+++ components/habitify/sources/habit-status-updated/test-event.mjs
@@ -0,0 +1,10 @@
+export default {
+  "status": "completed",
+  "progress": {
+    "current_value": 8,
+    "target_value": 8,
+    "unit_type": "rep",
+    "periodicity": "daily",
+    "reference_date": "2025-12-10T00:00:00.000Z"
+  }
+}
