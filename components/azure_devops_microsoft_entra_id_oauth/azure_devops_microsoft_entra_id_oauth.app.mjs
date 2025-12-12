74a74382f3299e37fe4013bd7938a06517c7bd8a
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-12 14:09:29 -0500

    migrate azure_devops source

--- components/azure_devops_microsoft_entra_id_oauth/azure_devops_microsoft_entra_id_oauth.app.mjs
+++ components/azure_devops_microsoft_entra_id_oauth/azure_devops_microsoft_entra_id_oauth.app.mjs
@@ -1,11 +1,59 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "azure_devops_microsoft_entra_id_oauth",
-  propDefinitions: {},
+  propDefinitions: {
+    eventType: {
+      type: "string",
+      label: "Event Type",
+      description: "Event type to receive events for",
+      async options() {
+        const types = await this.listEventTypes();
+        return types?.map((type) => type.id);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://dev.azure.com/${this.$auth.organization}/_apis`;
+    },
+    _makeRequest({
+      $ = this, path, params, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "Authorization": `Bearer ${this.$auth.oauth_access_token}`,
+          "content-type": "application/json",
+        },
+        params: {
+          ...params,
+          "api-version": "7.1",
+        },
+        ...opts,
+      });
+    },
+    async listEventTypes(opts = {}) {
+      const { value } = await this._makeRequest({
+        path: "/hooks/publishers/tfs/eventtypes",
+        ...opts,
+      });
+      return value;
+    },
+    createSubscription(opts = {}) {
+      return  this._makeRequest({
+        method: "POST",
+        path: "/hooks/subscriptions",
+        ...opts,
+      });
+    },
+    deleteSubscription(subscriptionId, opts = {}) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/hooks/subscriptions/${subscriptionId}`,
+        ...opts,
+      });
     },
   },
 };
