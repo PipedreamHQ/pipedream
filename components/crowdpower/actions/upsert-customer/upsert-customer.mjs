e4ed33b993ec51e9d687d8e1bf4e7d4bfe76266a
Lucas Caresia <lucascarezia@gmail.com>
2025-12-12 08:38:42 -0300

    Added action

---
+++ components/crowdpower/actions/upsert-customer/upsert-customer.mjs
@@ -0,0 +1,57 @@
+import app from "../../crowdpower.app.mjs";
+
+export default {
+  key: "crowdpower-upsert-customer",
+  name: "Upsert Customer",
+  description: "Create or update a customer. [See the documentation](https://documenter.getpostman.com/view/17896162/UV5TFKbh#9415bd14-59ab-47b1-b0ec-73826d9cb5db)",
+  version: "0.0.1",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  type: "action",
+  props: {
+    app,
+    userId: {
+      propDefinition: [
+        app,
+        "userId",
+      ],
+      description: "ID of the customer that will be created or updated",
+    },
+    email: {
+      propDefinition: [
+        app,
+        "email",
+      ],
+    },
+    name: {
+      propDefinition: [
+        app,
+        "name",
+      ],
+    },
+    customAttributes: {
+      propDefinition: [
+        app,
+        "customAttributes",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.app.upsertCustomer({
+      $,
+      data: {
+        user_id: this.userId,
+        email: this.email,
+        name: this.name,
+        custom_attributes: this.customAttributes,
+      },
+    });
+    $.export("$summary", response.success
+      ? `Request succeeded with code ${response.code}`
+      : `Request failed with code ${response.code}`);
+    return response;
+  },
+};
