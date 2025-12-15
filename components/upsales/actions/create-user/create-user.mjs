ba675740bcbc45e990c66fced36e5a7e94ed449a
GTFalcao <gtfalcao96@gmail.com>
2025-12-15 20:53:49 -0300

    Initial app file and Create User action

---
+++ components/upsales/actions/create-user/create-user.mjs
@@ -0,0 +1,125 @@
+import { ConfigurationError } from "@pipedream/platform";
+import app from "../../upsales.app.mjs";
+
+export default {
+  key: "upsales-create-user",
+  name: "Create User",
+  description: "Creates a new user in Upsales. [See the documentation](https://apidocs.upsales.com/v2/users/post-users)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    app,
+    email: {
+      type: "string",
+      label: "Email",
+      description: "The email address of the user",
+    },
+    clientId: {
+      type: "integer",
+      label: "Client ID",
+      description: "The client ID to associate with the user",
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "The full name of the user",
+    },
+    language: {
+      type: "string",
+      label: "Language",
+      description: "The language code for the user (e.g., `en-EN`, `sv-SE`)",
+      optional: true,
+    },
+    password: {
+      type: "string",
+      label: "Password",
+      description: "The password for the user account",
+      secret: true,
+    },
+    teamLeader: {
+      type: "boolean",
+      label: "Team Leader",
+      description: "Whether the user is a team leader",
+      optional: true,
+    },
+    active: {
+      type: "boolean",
+      label: "Active",
+      description: "Whether the user is active",
+      optional: true,
+      default: true,
+    },
+    ghost: {
+      type: "boolean",
+      label: "Ghost",
+      description: "Whether the user is a ghost user",
+      optional: true,
+      default: false,
+    },
+    export: {
+      type: "boolean",
+      label: "Export",
+      description: "Whether the user can export data",
+      optional: true,
+    },
+    administrator: {
+      type: "boolean",
+      label: "Administrator",
+      description: "Whether the user is an administrator",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const {
+      email,
+      clientId,
+      name,
+      language,
+      password,
+      teamLeader,
+      active,
+      ghost,
+      export: exportProp,
+      administrator,
+    } = this;
+
+    const response = await this.app.createUser({
+      $,
+      data: {
+        email,
+        clientId,
+        name,
+        language,
+        password,
+        teamLeader: teamLeader
+          ? 1
+          : 0,
+        active: active
+          ? 1
+          : 0,
+        ghost: ghost
+          ? 1
+          : 0,
+        export: exportProp
+          ? 1
+          : 0,
+        administrator: administrator
+          ? 1
+          : 0,
+      },
+    });
+
+    if (response.error) {
+      throw new ConfigurationError(`Failed to create user: ${JSON.stringify(response.error)}`);
+    }
+
+    $.export("$summary", `Successfully created user with ID: ${response.data.id}`);
+    return response.data;
+  },
+};
+
--- components/upsales/upsales.app.mjs
+++ components/upsales/upsales.app.mjs
@@ -1,11 +1,30 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "upsales",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://integration.upsales.com/api/v2/master";
+    },
+    async _makeRequest({
+      $ = this, ...args
+    }) {
+      return axios($, {
+        baseURL: this._baseUrl(),
+        params: {
+          token: this.$auth.api_key,
+        },
+        ...args,
+      });
+    },
+    async createUser(args = {}) {
+      return this._makeRequest({
+        method: "POST",
+        url: "/users",
+        ...args,
+      });
     },
   },
 };
