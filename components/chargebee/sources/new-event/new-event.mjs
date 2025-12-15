cc043d6a18cb5ebe33dd7621d297059f4cbceebc
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-15 10:51:28 -0500

    copy description to alert prop

--- components/chargebee/sources/new-event/new-event.mjs
+++ components/chargebee/sources/new-event/new-event.mjs
@@ -12,6 +12,11 @@ export default {
   dedupe: "unique",
   props: {
     ...common.props,
+    info: {
+      type: "alert",
+      alertType: "info",
+      content: "Emit new event when the selected event is triggered. [See the Documentation](https://apidocs.chargebee.com/docs/api/events). Please make sure once you deploy this source, you copy/paste the webhook URL to create it in your [Chargebee Webhook settings](https://www.chargebee.com/docs/2.0/webhook_settings.html#configure-webhooks).",
+    },
     eventType: {
       type: "string[]",
       label: "Event Type",
