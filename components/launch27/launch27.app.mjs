63355ea0a7a5c8fc1103fff6ed3a8b55464d0c5a
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-16 10:29:16 -0500

    wip

--- components/launch27/launch27.app.mjs
+++ components/launch27/launch27.app.mjs
@@ -1,11 +1,23 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "launch27",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://${this.$auth.domain}.launch27.com/v1`;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        heaaders: {
+          Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+        },
+        ...opts,
+      });
     },
   },
 };
