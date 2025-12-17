1f6f23457473c62b90c9780fd45ca91da6a7456b
Luan Cazarine <luanhc@gmail.com>
2025-12-17 19:18:27 -0300

    Add constants for signal options in Lusha component
    - Introduced SIGNALS_OPTIONS and COMPANY_SIGNALS_OPTIONS constants to standardize signal types and enhance component functionality.
    - Each constant includes labels and values for various signal categories, improving clarity and usability for users interacting with the Lusha component.

---
+++ components/lusha/common/constants.mjs
@@ -0,0 +1,33 @@
+export const SIGNALS_OPTIONS = [
+  {
+    label: "All Signals",
+    value: "allSignals",
+  },
+  {
+    label: "Promotion",
+    value: "promotion",
+  },
+  {
+    label: "Company Changes",
+    value: "companyChange",
+  },
+];
+
+export const COMPANY_SIGNALS_OPTIONS = [
+  {
+    label: "All Signals",
+    value: "allSignals",
+  },
+  {
+    label: "Headcount Growth",
+    value: "headcountGrowth",
+  },
+  {
+    label: "New Jobs Open",
+    value: "newJobsOpen",
+  },
+  {
+    label: "News Event",
+    value: "newsEvent",
+  },
+];
