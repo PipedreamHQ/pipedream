28126de525c352520e95fc11d269432a7976c3e3
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-17 16:15:42 -0500

    new components

---
+++ components/cloudbeds/sources/new-reservation-created/new-reservation-created.mjs
@@ -0,0 +1,29 @@
+import common from "../common/base-webhook.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "cloudbeds-new-reservation-created",
+  name: "New Reservation Created (Instant)",
+  description: "Emit new event when a new reservation is created in Cloudbeds. [See the documentation](https://developers.cloudbeds.com/reference/post_postwebhook-2)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getObject() {
+      return "reservation";
+    },
+    getAction() {
+      return "created";
+    },
+    generateMeta(body) {
+      return {
+        id: body.reservationID,
+        summary: `New reservation created with ID: ${body.reservationID}`,
+        ts: Math.floor(body.timestamp),
+      };
+    },
+  },
+  sampleEmit,
+};
---
+++ components/cloudbeds/sources/reservation-status-changed/reservation-status-changed.mjs
@@ -0,0 +1,50 @@
+import common from "../common/base-webhook.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  ...common,
+  key: "cloudbeds-reservation-status-changed",
+  name: "Reservation Status Changed (Instant)",
+  description: "Emit new event when a reservation status is changed in Cloudbeds. [See the documentation](https://developers.cloudbeds.com/reference/post_postwebhook-2)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    statuses: {
+      type: "string[]",
+      label: "Statuses",
+      description: "Watch for reservations that have been updated to the selected statuses",
+      options: [
+        "in_progress",
+        "confirmed",
+        "not_confirmed",
+        "canceled",
+        "checked_in",
+        "checked_out",
+        "no_show",
+      ],
+      optional: true,
+    },
+  },
+  methods: {
+    ...common.methods,
+    getObject() {
+      return "reservation";
+    },
+    getAction() {
+      return "status_changed";
+    },
+    isRelevant(body) {
+      return this.statuses.includes(body.status);
+    },
+    generateMeta(body) {
+      return {
+        id: body.timestamp,
+        summary: `Reservation status changed to ${body.status}`,
+        ts: Math.floor(body.timestamp),
+      };
+    },
+  },
+  sampleEmit,
+};
