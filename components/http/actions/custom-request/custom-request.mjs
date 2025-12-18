d74b45d439c79733411fcfb7a55ef82b8b0f7333
Michelle Bergeron <michelle.bergeron@gmail.com>
2025-12-18 16:47:34 -0500

    versions

--- components/http/actions/custom-request/custom-request.mjs
+++ components/http/actions/custom-request/custom-request.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Send any HTTP Request",
   description: "Send an HTTP request using any method and URL. Optionally configure query string parameters, headers, and basic auth.",
   type: "action",
-  version: "1.1.2",
+  version: "1.1.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
