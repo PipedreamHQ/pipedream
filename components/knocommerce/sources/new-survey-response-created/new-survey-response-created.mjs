c3271b4f2888d7bca3de6da702ac8b245a278eb1
michelle0927 <michelle0927@users.noreply.github.com>
2025-12-22 10:19:27 -0500

    Knocommerce - new component (#19550)
    * new-survey-response-created
    
    * pnpm-lock.yaml
    
    * update

---
+++ components/knocommerce/sources/new-survey-response-created/new-survey-response-created.mjs
@@ -0,0 +1,63 @@
+import knocommerce from "../../knocommerce.app.mjs";
+import sampleEmit from "./test-event.mjs";
+
+export default {
+  key: "knocommerce-new-survey-response-created",
+  name: "New Survey Response Created (Instant)",
+  description: "Emit new event when a new survey response is created. [See the documentation](https://developers.knocommerce.com/#645e16d0-be4f-4cd7-8d81-1a7cfee5f911)",
+  version: "0.0.1",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    knocommerce,
+    db: "$.service.db",
+    http: {
+      type: "$.interface.http",
+      customResponse: true,
+    },
+  },
+  methods: {
+    _getHookId() {
+      return this.db.get("hookId");
+    },
+    _setHookId(hookId) {
+      this.db.set("hookId", hookId);
+    },
+  },
+  hooks: {
+    async activate() {
+      const { data: { webhook: { id } } } = await this.knocommerce.createWebhook({
+        data: {
+          topic: "response/create",
+          url: this.http.endpoint,
+        },
+      });
+      this._setHookId(id);
+    },
+    async deactivate() {
+      const hookId = this._getHookId();
+      if (hookId) {
+        await this.knocommerce.deleteWebhook({
+          hookId,
+        });
+      }
+    },
+  },
+  async run(event) {
+    this.http.respond({
+      status: 200,
+    });
+
+    const { body } = event;
+    if (!body || !body.response) {
+      return;
+    }
+
+    this.$emit(body, {
+      id: body.response.id,
+      summary: `New survey response: ${body.response.id}`,
+      ts: Date.parse(body.response.created_at),
+    });
+  },
+  sampleEmit,
+};
