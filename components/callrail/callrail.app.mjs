b15bf976d949e7b05c6bef5f287575eee1de7d5f
Luan Cazarine <luanhc@gmail.com>
2025-12-23 16:14:41 -0300

    13349 components callrail (#19545)
    * Add CallRail components for SMS and phone call events
    
    - Implemented new sources for "New SMS Received (Instant)" and "Phone Call Completed (Instant)" events.
    - Created a common base for shared functionality across sources.
    - Added methods for managing webhooks and handling event data.
    - Included sample event data for testing purposes.
    
    * Enhance CallRail component with account and company ID support
    
    - Added prop definitions for accountId and companyId to facilitate user selection.
    - Implemented methods to list accounts and companies dynamically.
    - Updated existing methods for creating and updating webhooks to use integration IDs.
    - Introduced pagination support for API responses.
    - Bumped version to 0.1.0 and added dependencies for platform compatibility.
    
    * pnpm update
    
    * Update components/callrail/callrail.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Update components/callrail/callrail.app.mjs
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>
    
    * Fix typo in description of Phone Call Started (Instant) component
    
    ---------
    
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/callrail/callrail.app.mjs
+++ components/callrail/callrail.app.mjs
@@ -1,11 +1,142 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "callrail",
-  propDefinitions: {},
+  propDefinitions: {
+    accountId: {
+      type: "string",
+      label: "Account ID",
+      description: "The ID of the account you wish to use",
+      async options({ page }) {
+        const { accounts } = await this.listAccounts({
+          params: {
+            page: page + 1,
+          },
+        });
+        return accounts.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+    companyId: {
+      type: "string",
+      label: "Company ID",
+      description: "The ID of the company you wish to use",
+      async options({
+        accountId, page,
+      }) {
+        const { companies } = await this.listCompanies({
+          accountId,
+          params: {
+            page: page + 1,
+          },
+        });
+        return companies.map(({
+          id: value, name: label,
+        }) => ({
+          label,
+          value,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.callrail.com/v3/a";
+    },
+    _headers() {
+      return {
+        "Authorization": `Token token=${this.$auth.api_key}`,
+      };
+    },
+    _makeRequest({
+      $ = this,
+      path,
+      ...args
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...args,
+      });
+    },
+    listAccounts(args = {}) {
+      return this._makeRequest({
+        path: ".json",
+        ...args,
+      });
+    },
+    listCompanies({
+      accountId,
+      ...args
+    }) {
+      return this._makeRequest({
+        path: `/${accountId}/companies.json`,
+        ...args,
+      });
+    },
+    listIntegrations({
+      accountId,
+      ...args
+    }) {
+      return this._makeRequest({
+        path: `/${accountId}/integrations.json`,
+        ...args,
+      });
+    },
+    createHook({
+      accountId,
+      ...args
+    }) {
+      return this._makeRequest({
+        method: "POST",
+        path: `/${accountId}/integrations.json`,
+        ...args,
+      });
+    },
+    updateHook({
+      accountId,
+      integrationId,
+      ...args
+    }) {
+      return this._makeRequest({
+        method: "PUT",
+        path: `/${accountId}/integrations/${integrationId}.json`,
+        ...args,
+      });
+    },
+    async *paginate({
+      fn, params = {}, dataField, maxResults = null, ...opts
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.page = ++page;
+        const {
+          [dataField]: data,
+          page: aPage,
+          total_pages: tPages,
+        } = await fn({
+          params,
+          ...opts,
+        });
+        for (const d of data) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = aPage < tPages;
+
+      } while (hasMore);
     },
   },
 };
---
+++ components/callrail/sources/phone-call-completed-instant/test-event.mjs
@@ -0,0 +1,19 @@
+export default {
+  "answered": false,
+  "business_phone_number": null,
+  "customer_city": "New York City",
+  "customer_country": "US",
+  "customer_name": "Jimmy Pesto, Sr.",
+  "customer_phone_number": "+13036231131",
+  "customer_state": "NY",
+  "direction": "inbound",
+  "duration": 4,
+  "id": "CAL8154748ae6bd4e278a7cddd38a662f4f",
+  "recording": "https://api.callrail.com/v3/a/227799611/calls/111222333/recording.json",
+  "recording_duration": "27",
+  "recording_player": "https://app.callrail.com/calls/111222333/recording?access_key=3b91eb7f7cc08a4d01ed",
+  "start_time": "2017-01-24T11:27:48.119-05:00",
+  "tracking_phone_number": "+13038163491",
+  "voicemail": false,
+  "agent_email": "gil@televised.com"
+}
