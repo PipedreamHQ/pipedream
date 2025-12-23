8f61b28ba170a3e7d70f53fde87701a6a6beeb20
Luan Cazarine <luanhc@gmail.com>
2025-12-23 19:37:44 -0300

    Enhance Constant Contact component with new actions and updates
    - Added new actions: `createEmailCampaign`, `createOrUpdateContact`, and `addAccountEmailAddress`.
    - Introduced new sources for tracking new accounts and contacts.
    - Updated existing actions with new versions and improved functionality.
    - Bumped package version to 0.2.0 and updated dependencies for compatibility.
    - Added utility functions for parsing objects and defined address type options.

---
+++ components/constant_contact/common/constants.mjs
@@ -0,0 +1,14 @@
+export const ADDRESS_TYPE_OPTIONS = [
+  {
+    label: "Home",
+    value: "home",
+  },
+  {
+    label: "Work",
+    value: "work",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+];
