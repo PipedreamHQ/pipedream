3dcb665f6e989f213565ae3fd1c3cd87a600c00b
Luan Cazarine <luanhc@gmail.com>
2026-01-02 13:23:51 -0300

    Update Trengo component to version 0.9.0 and add 'Get Ticket' action … (#19567)
    * Update Trengo component to version 0.9.0 and add 'Get Ticket' action for retrieving specific tickets.
    
    * Update Trengo components to latest versions, including actions and sources, to improve functionality and maintain compatibility.

--- components/trengo/actions/get-block/get-block.mjs
+++ components/trengo/actions/get-block/get-block.mjs
@@ -4,7 +4,7 @@ export default {
   key: "trengo-get-block",
   name: "Get Block",
   description: "Get a specific block. [See the documentation](https://developers.trengo.com/reference/get-a-block)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   annotations: {
     destructiveHint: false,
