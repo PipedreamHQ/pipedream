dbd994186335656911c16c5cbd09a7dec0c9e3c4
Luan Cazarine <luanhc@gmail.com>
2026-01-06 16:04:40 -0300

    Refactor Robly component and add new actions and sources
    - Updated version in package.json to 0.1.0.
    - Introduced prop definitions for List ID and Contact Email.
    - Added actions for creating a contact, adding a contact to a list, and removing a contact from a list.
    - Implemented new sources for emitting events when new contacts and lists are created.
    - Added utility functions for object parsing and defined constants for pagination limits.
    - Enhanced the base polling source for better event handling.

--- components/robly/robly.app.mjs
+++ components/robly/robly.app.mjs
@@ -1,11 +1,139 @@
+import { axios } from "@pipedream/platform";
+import { LIMIT } from "./common/constants.mjs";
+
 export default {
   type: "app",
   app: "robly",
-  propDefinitions: {},
+  propDefinitions: {
+    listId: {
+      type: "string",
+      label: "List ID",
+      description: "The ID of the list",
+      async options() {
+        const lists = await this.getLists();
+        if (Array.isArray(lists)) {
+          return lists.map(({
+            sub_list: {
+              id: value,
+              name: label,
+            },
+          }) => ({
+            label,
+            value,
+          }));
+        }
+        return [];
+      },
+    },
+    contactEmail: {
+      type: "string",
+      label: "Contact Email",
+      description: "The email address of the contact",
+      async options({
+        page, listId,
+      }) {
+        const params = {
+          limit: LIMIT,
+          offset: page * LIMIT,
+          email_only: true,
+        };
+
+        if (listId) {
+          params.list_id = listId;
+        }
+
+        const contacts = await this.getContacts({
+          params,
+        });
+
+        if (Array.isArray(contacts)) {
+          return contacts.map(({ member: { email } }) => email);
+        }
+        return [];
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.robly.com/api/v1";
+    },
+    _getAuthParams(params = {}) {
+      return {
+        api_id: this.$auth.api_id,
+        api_key: this.$auth.api_key,
+        ...params,
+      };
+    },
+    _makeRequest({
+      $ = this, path, params, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        params: this._getAuthParams(params),
+        ...opts,
+      });
+    },
+    createContact(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/sign_up/generate",
+        ...opts,
+      });
+    },
+    addContactToList(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/contacts/add_sub_list",
+        ...opts,
+      });
+    },
+    removeContactFromList(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/contacts/remove_sub_list",
+        ...opts,
+      });
+    },
+    getLists(args = {}) {
+      return this._makeRequest({
+        path: "/sub_lists/show",
+        ...args,
+      });
+    },
+    getContacts(args = {}) {
+      return this._makeRequest({
+        path: "/contacts",
+        ...args,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null, ...opts
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.limit = LIMIT;
+        params.offset = LIMIT * page++;
+        const data = await fn({
+          params,
+          ...opts,
+        });
+
+        if (!Array.isArray(data)) return false;
+
+        for (const d of data) {
+          yield d.member;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data.length;
+
+      } while (hasMore);
     },
   },
 };
---
+++ components/robly/sources/common/base-polling.mjs
@@ -0,0 +1,67 @@
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+import robly from "../../robly.app.mjs";
+
+export default {
+  props: {
+    robly,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      label: "Polling interval",
+      description: "How often to poll the Robly API for new events",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+  },
+  methods: {
+    _getLastId() {
+      return this.db.get("lastId") || 0;
+    },
+    _setLastId(lastId) {
+      this.db.set("lastId", lastId);
+    },
+    getParams() {
+      return {};
+    },
+    async emitEvent(maxResults = false) {
+      const lastId = this._getLastId();;
+
+      const response = await this.robly.paginate({
+        fn: this.robly.getContacts,
+        maxResults,
+        params: this.getParams(),
+      });
+
+      let responseArray = [];
+      for await (const item of response) {
+        if (item.id <= lastId) break;
+        responseArray.push(item);
+      }
+
+      if (responseArray.length) {
+        if (maxResults && (responseArray.length > maxResults)) {
+          responseArray.length = maxResults;
+        }
+        this._setLastId(responseArray[0].id);
+      }
+
+      for (const item of responseArray.reverse()) {
+        this.$emit(item, {
+          id: item.id,
+          summary: this.getSummary(item),
+          ts: Date.parse(item.created_at),
+        });
+      }
+    },
+  },
+  hooks: {
+    async deploy() {
+      await this.emitEvent(25);
+    },
+  },
+  async run() {
+    await this.emitEvent();
+  },
+};
+
