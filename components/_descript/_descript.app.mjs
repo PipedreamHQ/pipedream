bd2249bb3599880074a28b4f134878613ef73ed6
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-01-08 16:11:00 -0500

    wip

--- components/_descript/_descript.app.mjs
+++ components/_descript/_descript.app.mjs
@@ -1,11 +1,36 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "_descript",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://descriptapi.com/v1";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this.$auth.api_token}`,
+        },
+        ...opts,
+      });
+    },
+    getPublishedProjectMetadata(opts = {}) {
+      return this._makeRequest({
+        path: `/published_projects/${this.$auth.published_project_slug}`,
+        ...opts,
+      });
+    },
+    createImportUrl(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/edit_in_descript/schema",
+        ...opts,
+      });
     },
   },
 };
