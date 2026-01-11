784d845e3685e474f571d0e490ac8fb44d9aaa30
michelle0927 <michelle0927@users.noreply.github.com>
2026-01-11 10:41:56 -0500

    Zooom - Delete Meeting (#19668)
    * delete-meeting
    
    * update doc link
    
    * versions
    
    * remove duplicate description

--- components/zoom/sources/meeting-updated/meeting-updated.mjs
+++ components/zoom/sources/meeting-updated/meeting-updated.mjs
@@ -6,7 +6,7 @@ export default {
   key: "zoom-meeting-updated",
   name: "Meeting Updated (Instant)",
   description: "Emit new event each time a meeting is updated where you're the host",
-  version: "0.1.4",
+  version: "0.1.5",
   type: "source",
   dedupe: "unique",
   props: {
