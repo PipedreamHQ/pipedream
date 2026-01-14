edb96dcc6f487ce6079fd8d79d7beb5382324b9b
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-01-14 11:29:05 -0500

    wip

---
+++ components/jira_data_center/actions/list-boards/list-boards.mjs
@@ -0,0 +1,71 @@
+import jiraDataCenter from "../../jira_data_center.app.mjs";
+
+export default {
+  name: "List Boards",
+  key: "jira_data_center-list-boards",
+  description: "Retrieves a list of boards. [See the documentation](https://developer.atlassian.com/server/jira/platform/rest/v10002/api-group-board/#api-agile-1-0-board-get)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    jiraDataCenter,
+    projectId: {
+      propDefinition: [
+        jiraDataCenter,
+        "projectId",
+      ],
+      optional: true,
+    },
+    name: {
+      type: "string",
+      label: "Name",
+      description: "Filter by name",
+      optional: true,
+    },
+    type: {
+      type: "string",
+      label: "Type",
+      description: "Filter by type",
+      optional: true,
+      options: [
+        "kanban",
+        "scrum",
+      ],
+    },
+    maxResults: {
+      propDefinition: [
+        jiraDataCenter,
+        "maxResults",
+      ],
+    },
+    startAt: {
+      type: "integer",
+      label: "Start At",
+      description: "The index of the first result to return",
+      default: 0,
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.jiraDataCenter.listBoards({
+      $,
+      params: {
+        projectKeyOrId: this.projectId,
+        name: this.name,
+        type: this.type,
+        maxResults: this.maxResults,
+        startAt: this.startAt,
+      },
+    });
+
+    $.export("$summary", `Successfully got ${response.values.length} board${response.values.length === 1
+      ? ""
+      : "s"}`);
+
+    return response;
+  },
+};
--- components/jira_data_center/jira_data_center.app.mjs
+++ components/jira_data_center/jira_data_center.app.mjs
@@ -1,11 +1,75 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "jira_data_center",
-  propDefinitions: {},
+  propDefinitions: {
+    projectId: {
+      type: "string",
+      label: "Project ID",
+      description: "The ID of the project",
+      async options() {
+        const projects = await this.listProjects();
+        return projects.map((project) => ({
+          label: project.name,
+          value: project.id,
+        }));
+      },
+    },
+    boardId: {
+      type: "string",
+      label: "Board ID",
+      description: "The ID of the board",
+      async options() {
+        const { values: boards } = await this.listBoards();
+        return boards.map((board) => ({
+          label: board.name,
+          value: board.id,
+        }));
+      },
+    },
+    maxResults: {
+      type: "integer",
+      label: "Max Results",
+      description: "The maximum number of results to return",
+      default: 25,
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `${this.$auth.api_url}/rest`;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          Authorization: `Bearer ${this.$auth.personal_access_token}`,
+        },
+        ...opts,
+      });
+    },
+    getBoard({
+      boardId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/agile/1.0/board/${boardId}`,
+        ...opts,
+      });
+    },
+    listProjects(opts = {}) {
+      return this._makeRequest({
+        path: "/api/2/project",
+        ...opts,
+      });
+    },
+    listBoards(opts = {}) {
+      return this._makeRequest({
+        path: "/agile/1.0/board",
+        ...opts,
+      });
     },
   },
 };
