d571a2e403830b961fb61b1661fab7ac65ca944b
Jorge Cortes <jacortesmahmud@gmail.com>
2026-01-20 17:43:58 -0500

    [ACTION] Add dedicated actions to update Salesforce notes and enhanced notes

---
+++ components/salesforce_rest_api/actions/update-content-note/update-content-note.mjs
@@ -0,0 +1,78 @@
+import common, { getProps } from "../common/base-create-update.mjs";
+import contentNote from "../../common/sobjects/content-note.mjs";
+
+const docsLink = "https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_contentnote.htm";
+
+const {
+  salesforce, ...props
+} = getProps({
+  createOrUpdate: "update",
+  objType: contentNote,
+  docsLink,
+});
+
+export default {
+  ...common,
+  key: "salesforce_rest_api-update-content-note",
+  name: "Update Content Note",
+  description: `Updates an enhanced Salesforce content note, which can include formatted text and is part of Salesforce Files. [See the documentation](${docsLink}) and [Set Up Notes](https://help.salesforce.com/s/articleView?id=sales.notes_admin_setup.htm&type=5).`,
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  methods: {
+    ...common.methods,
+    getObjectType() {
+      return "ContentNote";
+    },
+  },
+  props: {
+    salesforce,
+    contentNoteId: {
+      propDefinition: [
+        salesforce,
+        "recordId",
+        () => ({
+          objType: "ContentNote",
+          nameField: "Title",
+        }),
+      ],
+      label: "Content Note ID",
+      description: "The ID of the content note to update.",
+    },
+    ...props,
+  },
+  async run({ $ }) {
+    /* eslint-disable no-unused-vars */
+    const {
+      salesforce,
+      contentNoteId,
+      getAdvancedProps,
+      getObjectType,
+      getAdditionalFields,
+      formatDateTimeProps,
+      useAdvancedProps,
+      docsInfo,
+      dateInfo,
+      additionalFields,
+      ...data
+    } = this;
+    /* eslint-enable no-unused-vars */
+    await salesforce.updateRecord("ContentNote", {
+      $,
+      id: contentNoteId,
+      data: {
+        ...data,
+        ...getAdditionalFields(),
+      },
+    });
+
+    $.export("$summary", `Successfully updated content note (ID: ${contentNoteId})`);
+    return {
+      success: true,
+    };
+  },
+};
---
+++ components/salesforce_rest_api/actions/update-note/update-note.mjs
@@ -0,0 +1,79 @@
+import common, { getProps } from "../common/base-create-update.mjs";
+import note from "../../common/sobjects/note.mjs";
+
+const docsLink = "https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_note.htm";
+
+const {
+  salesforce, ...props
+} = getProps({
+  createOrUpdate: "update",
+  objType: note,
+  docsLink,
+});
+
+export default {
+  ...common,
+  key: "salesforce_rest_api-update-note",
+  name: "Update Note",
+  description: `Updates a classic Salesforce note, which can contain up to 32 KB of text and is associated with a parent record. [See the documentation](${docsLink})`,
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  methods: {
+    ...common.methods,
+    getObjectType() {
+      return "Note";
+    },
+  },
+  props: {
+    salesforce,
+    noteId: {
+      propDefinition: [
+        salesforce,
+        "recordId",
+        () => ({
+          objType: "Note",
+          nameField: "Title",
+        }),
+      ],
+      label: "Note ID",
+      description: "The ID of the note to update.",
+    },
+    ...props,
+  },
+  async run({ $ }) {
+    /* eslint-disable no-unused-vars */
+    const {
+      salesforce,
+      noteId,
+      getAdvancedProps,
+      getObjectType,
+      getAdditionalFields,
+      formatDateTimeProps,
+      useAdvancedProps,
+      docsInfo,
+      dateInfo,
+      additionalFields,
+      ...data
+    } = this;
+    /* eslint-enable no-unused-vars */
+
+    await salesforce.updateRecord("Note", {
+      $,
+      id: noteId,
+      data: {
+        ...data,
+        ...getAdditionalFields(),
+      },
+    });
+
+    $.export("$summary", `Successfully updated note (ID: ${noteId})`);
+    return {
+      success: true,
+    };
+  },
+};
