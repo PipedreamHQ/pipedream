35154a6c112c44ac8aafc5f3a484a4a955aba6b1
Luan Cazarine <luanhc@gmail.com>
2026-01-22 12:47:49 -0300

    Enhance Heartbeat component with new actions and properties
    - Added new actions: Add To Group, Create Event, Send Direct Message.
    - Introduced new properties for group and user management in the Heartbeat app.
    - Updated package version to 0.1.0 and added dependencies.
    - Implemented utility function for parsing objects.
    - Created new sources for instant events and user updates.

---
+++ components/heartbeat/actions/send-direct-message/send-direct-message.mjs
@@ -0,0 +1,56 @@
+import heartbeat from "../../heartbeat.app.mjs";
+
+export default {
+  key: "heartbeat-send-direct-message",
+  name: "Send Direct Message",
+  description: "Send a direct message in Heartbeat. [See the documentation](https://heartbeat.readme.io/reference/createdirectmessage)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    heartbeat,
+    text: {
+      type: "string",
+      label: "Text",
+      description: "See the [Rich Text page](https://heartbeat.readme.io/reference/rich-text) to learn more about formatting",
+    },
+    to: {
+      propDefinition: [
+        heartbeat,
+        "userId",
+      ],
+      type: "string",
+      label: "To",
+      description: "The user id of the receiver",
+    },
+    from: {
+      propDefinition: [
+        heartbeat,
+        "userId",
+        () => ({
+          onlyAdmin: true,
+        }),
+      ],
+      label: "From",
+      description: "The user ID of the sender. Only an admin user can be chosen. If this field is not provided, the user that created the API key will be used",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.heartbeat.sendDirectMessage({
+      $,
+      data: {
+        text: this.text,
+        to: this.to,
+        from: this.from,
+      },
+    });
+
+    $.export("$summary", `Direct message successfully sent to ${this.to}.`);
+    return response;
+  },
+};
--- components/heartbeat/heartbeat.app.mjs
+++ components/heartbeat/heartbeat.app.mjs
@@ -1,11 +1,126 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "heartbeat",
-  propDefinitions: {},
+  propDefinitions: {
+    groupID: {
+      type: "string",
+      label: "Group ID",
+      description: "The ID of the group",
+      async options() {
+        const groups = await this.getGroups();
+        return groups.map((group) => ({
+          label: group.name,
+          value: group.id,
+        }));
+      },
+    },
+    userId: {
+      type: "string",
+      label: "User ID",
+      description: "The ID of the user",
+      async options({ onlyAdmin = false }) {
+        const users = await this.getUsers();
+        return users
+          .filter(({ isAdmin }) => onlyAdmin
+            ? isAdmin
+            : true)
+          .map(({
+            email, name, id: value,
+          }) => ({
+            label: `${name} (${email})`,
+            value,
+          }));
+      },
+    },
+    emails: {
+      type: "string[]",
+      label: "Emails",
+      description: "The emails of the users that should be added to the group",
+      async options() {
+        const users = await this.getUsers();
+        return users.map((user) => user.email);
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.heartbeat.chat/v0";
+    },
+    _headers() {
+      return {
+        "Authorization": `Bearer ${this.$auth.api_key}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    getUsers() {
+      return this._makeRequest({
+        path: "/users",
+      });
+    },
+    getUser(userID) {
+      return this._makeRequest({
+        method: "GET",
+        path: `/users/${userID}`,
+      });
+    },
+    getGroups() {
+      return this._makeRequest({
+        path: "/groups",
+      });
+    },
+    getEvent(eventID) {
+      return this._makeRequest({
+        method: "GET",
+        path: `/events/${eventID}`,
+      });
+    },
+    addToGroup({
+      groupID, ...args
+    }) {
+      return this._makeRequest({
+        method: "PUT",
+        path: `/groups/${groupID}/memberships`,
+        ...args,
+      });
+    },
+    sendDirectMessage(args = {}) {
+      return this._makeRequest({
+        method: "PUT",
+        path: "/directMessages",
+        ...args,
+      });
+    },
+    createEvent(args = {}) {
+      return this._makeRequest({
+        method: "PUT",
+        path: "/events",
+        ...args,
+      });
+    },
+    createHook(args = {}) {
+      return this._makeRequest({
+        method: "PUT",
+        path: "/webhooks",
+        ...args,
+      });
+    },
+    deleteHook(webhookID) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/webhooks/${webhookID}`,
+      });
     },
   },
 };
