bc9b48f43541a936f0f3facc5f6a62812946ca5e
Luan Cazarine <luanhc@gmail.com>
2026-01-23 16:37:58 -0300

    13323 components heartbeat (#19812)
    * Enhance Heartbeat component with new actions and properties
    
    - Added new actions: Add To Group, Create Event, Send Direct Message.
    - Introduced new properties for group and user management in the Heartbeat app.
    - Updated package version to 0.1.0 and added dependencies.
    - Implemented utility function for parsing objects.
    - Created new sources for instant events and user updates.
    
    * pnpm update
    
    * Refactor Heartbeat component descriptions for clarity
    
    - Removed redundant wording in descriptions for new event and user sources.
    - Updated method calls in the Heartbeat app to streamline request handling.
    
    * Improve Heartbeat component functionality and descriptions
    
    - Updated the description for the 'To' field in the Send Direct Message action for better clarity.
    - Changed the timestamp generation method in the base source to use Date.parse for improved accuracy.
    - Modified the getDate method in the user-updated-instant source to return a Date object instead of a timestamp.

---
+++ components/heartbeat/sources/common/base.mjs
@@ -0,0 +1,45 @@
+import heartbeat from "../../heartbeat.app.mjs";
+
+export default {
+  props: {
+    heartbeat,
+    db: "$.service.db",
+    http: "$.interface.http",
+  },
+  methods: {
+    _setWebhookId(id) {
+      this.db.set("webhookId", id);
+    },
+    _getWebhookId() {
+      return this.db.get("webhookId");
+    },
+  },
+  hooks: {
+    async activate() {
+      const response = await this.heartbeat.createHook({
+        data: {
+          url: this.http.endpoint,
+          action: {
+            name: this.getEvent(),
+          },
+        },
+      });
+      this._setWebhookId(response.id);
+    },
+    async deactivate() {
+      const webhookId = this._getWebhookId();
+      await this.heartbeat.deleteHook(webhookId);
+    },
+  },
+  async run({ body }) {
+    const fn = this.getFunction();
+    const eventBody = await fn(body.id);
+    const ts = Date.parse(this.getDate(eventBody));
+
+    this.$emit(eventBody, {
+      id: `${body.id}-${ts}`,
+      summary: this.getSummary(body),
+      ts,
+    });
+  },
+};
---
+++ components/heartbeat/sources/user-updated-instant/test-event.mjs
@@ -0,0 +1,46 @@
+export default {
+  "id": "21c2a82a-c4b2-4e29-a031-bfc39a44b595",
+  "email": "email@example.com",
+  "name": "John Doe",
+  "firstName": "John",
+  "lastName": "Doe",
+  "bio": "bio example",
+  "createdAt": "2026-01-21T21:24:41.186Z",
+  "role": "User",
+  "isAdmin": false,
+  "groups": [
+    {
+      "name": "User",
+      "id": "6fffc3b3-879c-4a11-bfcc-b6b09fc5e675"
+    },
+    {
+      "name": "Group Name",
+      "id": "ec162713-4690-4707-9925-a4518985c1c8"
+    }
+  ],
+  "avatar": "https://dfle76rxbxaz7.cloudfront.net/assets/5a4e1a09-f961-4b70-b84c-4131cf972920-image.jpeg?Expires=2084629409&Key-Pair-Id=APKAIPAIYEJQ7WRNJNKQ&Signature=Q~wZl~u0S-j6n9f-SUWdcFxJz3aU4BzIMyYHFo~35ZaZmbwQS8ePnD9ulQ~wlYTsJvcxuAPwqU7MPLhs9EsxtJj2HiHAGukImsgWH8zcv8QCGCUO1~jTUGPZTMu3BO7jYNA3lR6PuOuHcfQ6HsnmxEQV6AyMoF1uKvmsjh6CZhUQNjGUYy~Vq4~fciXc8LjoM1ys1d3jJZjyI0gIsmFlWgkSzPAAncVuXOOsWyftSW1xO-nCFpOGSiNovSyNqr6-N15WpuWPck6pPL249wjVllfpjSaRKxQuGJeu1mHH9YEiyMhMgbcV-LjLOFWFMjCgCxP~70hqpNXTxOkeSWbChA__",
+  "linkedInSummary": "",
+  "linkedInData": null,
+  "onboardingResponses": [
+    {
+      "question": "Tell us about yourself",
+      "answer": "Answer to the question",
+      "isPrivate": false
+    },
+    {
+      "question": "What are you great at (and where can you help other people)?",
+      "answer": "Answer to the question",
+      "isPrivate": false
+    },
+    {
+      "question": "What's one big win you've had over the last couple weeks?",
+      "answer": "",
+      "isPrivate": false
+    },
+    {
+      "question": "What's one thing you're looking to get/learn from this community?",
+      "answer": "",
+      "isPrivate": false
+    }
+  ]
+}
