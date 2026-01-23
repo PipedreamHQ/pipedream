58923b250dc002dbd58ef223b70d0f9249220531
michelle0927 <michelle0927@users.noreply.github.com>
2026-01-23 09:56:56 -0500

    Pitchlane - new components (#19740)
    * wip
    
    * new components
    
    * pnpm-lock.yaml
    
    * updates

---
+++ components/pitchlane/sources/new-video-first-viewed/test-event.mjs
@@ -0,0 +1,69 @@
+export default {
+  "video": {
+    "_id": "eGW3A4OoTqpLMmYidtEC0",
+    "shortId": "2JtvheObJPXE",
+    "importId": "API import",
+    "campaignId": "O7kkxLnMYXUpAM1VzqHg4",
+    "meta": {},
+    "createdAt": "2026-01-19T20:51:50.462Z",
+    "storagePaths": {
+      "video": "u6FTur0CpNU89BrMeRXn4.mp4",
+      "video_mp4": "u6FTur0CpNU89BrMeRXn4.mp4",
+      "thumbnail": "I_sDsMlA0xhJZl4XPDixx.gif",
+      "thumbnail_png": "RBXXeah4fVdpxxwXib3dZ.png"
+    },
+    "userId": "google-oauth2|117665792695448708367",
+    "originalRow": {
+      "website": "https://example.com",
+      "first_name": "Test",
+      "last_name": "User",
+      "email": "test@example.com",
+      "company_name": "Pipedream"
+    },
+    "originalRowIndex": null,
+    "importSource": "API",
+    "title": "Video for Test",
+    "overrideRow": {
+      "website": "https://example.com"
+    },
+    "_backupVariables": {
+      "first_name": "Test",
+      "last_name": "User",
+      "email": "test@example.com",
+      "company_name": "Pipedream",
+      "website_0": "https://example.com"
+    },
+    "bullmqRootJobId": "864f4024-eede-4487-aefe-df05b4829dbe",
+    "queuedAt": "2026-01-19T20:58:10.055Z",
+    "renderingStatus": "RENDERED",
+    "startedProcessingAt": "2026-01-19T20:58:10.839Z",
+    "recordingOutputs": {
+      "pageResults": {
+        "0": {
+          "pageResult": {
+            "scrollableElementResult": {
+              "found": false
+            },
+            "hasMotionGraphics": false,
+            "usedGPU": false,
+            "receivedFrames": 1,
+            "html": null,
+            "languagePreference": "en-US",
+            "proxyServer": null
+          }
+        }
+      }
+    },
+    "backgroundRecordedAt": "2026-01-19T20:59:10.432Z",
+    "startedRenderingAt": "2026-01-19T20:59:11.914Z",
+    "renderedAt": "2026-01-19T21:00:16.533Z",
+    "triggeredWebhooks": [
+      "VIDEO_RENDERED"
+    ],
+    "campaignName": "My First Campaign"
+  },
+  "viewedAt": "2026-01-19T21:05:42.940Z",
+  "eventAt": "2026-01-19T21:05:42.940Z",
+  "viewerUrl": "https://playpitchlane.com/video/2JtvheObJPXE",
+  "event": "VIDEO_FIRST_VIEWED"
+}
