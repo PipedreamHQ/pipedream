e08e7e9af1300f1290996b325cc4ba85549ec99c
Luan Cazarine <luanhc@gmail.com>
2026-01-23 16:41:34 -0300

    Enhance BrowserStack component with new actions and sources (#19694)
    * Enhance BrowserStack component with new actions and sources
    
    - Added prop definitions for browser and projectId in browserstack.app.mjs.
    - Implemented new action: Take Screenshot for generating screenshots across multiple browsers and devices.
    - Introduced new source: New Automate Build to emit events when a new automate build is created.
    - Added utility functions and constants for handling options related to resolutions, quality, and wait times.
    - Updated version to 0.7.0 in package.json.
    
    * Fix timestamp handling in New Automate Build source
    
    - Updated the timestamp parsing to default to the current date if `started_at` is not available.
    - Modified the emitted event ID to include the parsed timestamp for better uniqueness.
    
    * Refactor BrowserStack component URL handling
    
    - Renamed parameter in _baseUrl method for clarity.
    - Updated _makeRequest method to accept baseUrl as an argument.
    - Corrected baseUrl in listBrowsers method to use the correct API endpoint.
    
    * Update pagination handling in BrowserStack component
    
    - Introduced a new variable `hasNext` to manage pagination logic.
    - Updated the loop condition to use `hasNext` instead of `nextPage` for improved clarity and functionality.
    
    * Remove unused New Automate Build source and related files
    
    - Deleted the New Automate Build source and its associated test event file.
    - Cleaned up the BrowserStack component by removing the projectId property and related methods to streamline functionality.

---
+++ components/browserstack/common/constants.mjs
@@ -0,0 +1,82 @@
+export const WINRES_OPTIONS = [
+  {
+    label: "1024x768",
+    value: "1024x768",
+  },
+  {
+    label: "1280x1024",
+    value: "1280x1024",
+  },
+];
+
+export const MACRES_OPTIONS = [
+  {
+    label: "1024x768",
+    value: "1024x768",
+  },
+  {
+    label: "1280x960",
+    value: "1280x960",
+  },
+  {
+    label: "1280x1024",
+    value: "1280x1024",
+  },
+  {
+    label: "1600x1200",
+    value: "1600x1200",
+  },
+  {
+    label: "1920x1080",
+    value: "1920x1080",
+  },
+];
+
+export const QUALITY_OPTIONS = [
+  {
+    label: "Original",
+    value: "original",
+  },
+  {
+    label: "Compressed",
+    value: "compressed",
+  },
+];
+
+export const WAITTIME_OPTIONS = [
+  {
+    label: "2",
+    value: 2,
+  },
+  {
+    label: "5",
+    value: 5,
+  },
+  {
+    label: "10",
+    value: 10,
+  },
+  {
+    label: "15",
+    value: 15,
+  },
+  {
+    label: "20",
+    value: 20,
+  },
+  {
+    label: "60",
+    value: 60,
+  },
+];
+
+export const ORIENTATION_OPTIONS = [
+  {
+    label: "Portrait",
+    value: "portrait",
+  },
+  {
+    label: "Landscape",
+    value: "landscape",
+  },
+];
