0c3aa7b4da5c5be451ce84bb1f84b7b23865c665
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-01-27 14:24:49 -0500

    versions

--- components/raindrop/actions/delete-bookmark/delete-bookmark.mjs
+++ components/raindrop/actions/delete-bookmark/delete-bookmark.mjs
@@ -4,7 +4,7 @@ export default {
   key: "raindrop-delete-bookmark",
   name: "Delete Bookmark",
   description: "Delete a bookmark. [See the docs here](https://developer.raindrop.io/v1/raindrops/single#remove-raindrop)",
-  version: "0.0.5",
+  version: "0.0.6",
   annotations: {
     destructiveHint: true,
     openWorldHint: true,
