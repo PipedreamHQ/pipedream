0d2bd51d9cc1505290f4f6a3dc67f77becad7f37
Luan Cazarine <luanhc@gmail.com>
2026-01-28 12:00:52 -0300

    Enhance Humanitix component with new actions and sources
    - Added prop definitions for eventId, overrideLocation, since, and maxResults in humanitix.app.mjs to improve user input options.
    - Implemented new actions: Get Events, Get Orders, and Get Tickets for retrieving event, order, and ticket details from Humanitix.
    - Introduced new sources: New Event Created, New Order Created, and New Ticket Created to emit events when new events, orders, and tickets are created.
    - Updated package version to 0.1.0 in package.json for the new features and improvements.
    - Expanded README.md to include detailed descriptions and examples for the new actions and sources.

--- components/humanitix/humanitix.app.mjs
+++ components/humanitix/humanitix.app.mjs
@@ -1,11 +1,110 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "humanitix",
-  propDefinitions: {},
+  propDefinitions: {
+    eventId: {
+      type: "string",
+      label: "Event ID",
+      description: "The unique identifier of the event",
+      async options({ page }) {
+        const { events } = await this.getEvents({
+          params: {
+            page: page + 1,
+          },
+        });
+        return events.map((event) => ({
+          label: event.name,
+          value: event._id,
+        }));
+      },
+    },
+    overrideLocation: {
+      type: "string",
+      label: "Override Location",
+      description: "By default, queries will be made with the user location. Use this parameter to override the user location for these requests. Format is that of ISO 3166-1 alpha-2 country codes",
+      optional: true,
+    },
+    since: {
+      type: "string",
+      label: "Since",
+      description: "Results since this date-time (ISO 8601). E.g. `2021-02-01T23:26:13.485Z`",
+      optional: true,
+    },
+    maxResults: {
+      type: "integer",
+      label: "Max Results",
+      description: "Maximum number of results to return",
+      optional: true,
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.humanitix.com/v1";
+    },
+    _headers() {
+      return {
+        "x-api-key": `${this.$auth.api_key}`,
+        "Content-Type": "application/json",
+      };
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: this._baseUrl() + path,
+        headers: this._headers(),
+        ...opts,
+      });
+    },
+    getEvents(opts = {}) {
+      return this._makeRequest({
+        path: "/events",
+        ...opts,
+      });
+    },
+    getOrders({
+      eventId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/events/${eventId}/orders`,
+        ...opts,
+      });
+    },
+    getTickets({
+      eventId, ...opts
+    }) {
+      return this._makeRequest({
+        path: `/events/${eventId}/tickets`,
+        ...opts,
+      });
+    },
+    async *paginate({
+      fn, params = {}, maxResults = null, dataField, ...opts
+    }) {
+      let hasMore = false;
+      let count = 0;
+      let page = 0;
+
+      do {
+        params.page = ++page;
+        const { [dataField]: data } = await fn({
+          params,
+          ...opts,
+        });
+
+        for (const d of data || []) {
+          yield d;
+
+          if (maxResults && ++count === maxResults) {
+            return count;
+          }
+        }
+
+        hasMore = data && data.length;
+
+      } while (hasMore);
     },
   },
 };
---
+++ components/humanitix/sources/new-event-created/test-event.mjs
@@ -0,0 +1,167 @@
+export default {
+  "_id": "5ac598ccd8fe7c0c0f212e2a",
+  "userId": "nEOqx8s9UueyRu48789C0sY9set1",
+  "organiserId": "5ac597aed8fe7c0c0f212e27",
+  "currency": "AUD",
+  "name": "Hobbit Dance Off",
+  "description": "Where hobbits from all across the shire come to show off their moves!",
+  "sharingDescription": "Calling all hobbits! Come and show everyone your moves",
+  "slug": "hobbit-dance-off",
+  "url": "https://events.humanitix.com/hobbit-dance-off",
+  "tagIds": [
+    "string"
+  ],
+  "category": "community",
+  "classification": {
+    "category": "autoBoatAndAir",
+    "subcategory": "air"
+  },
+  "artists": [
+    {
+      "origin": "spotify",
+      "name": "Gandalf",
+      "externalId": "4ZNG0WQPQ10ehIVkCnM5ku"
+    }
+  ],
+  "public": true,
+  "published": true,
+  "suspendSales": true,
+  "markedAsSoldOut": true,
+  "startDate": "2021-02-01T23:26:13.485Z",
+  "endDate": "2021-02-01T23:26:13.485Z",
+  "timezone": "Pacific/Auckland",
+  "totalCapacity": 1000,
+  "ticketTypes": [
+    {
+      "_id": "5da50970ec90824b5ca3608f",
+      "name": "Adult",
+      "price": 100,
+      "priceRange": {
+        "enabled": true,
+        "min": 10,
+        "max": 100
+      },
+      "priceOptions": {
+        "enabled": true,
+        "options": [
+          {
+            "value": "50"
+          }
+        ]
+      },
+      "quantity": 500,
+      "description": "Admits one hobbit.",
+      "disabled": true,
+      "deleted": true,
+      "isDonation": true
+    }
+  ],
+  "pricing": {
+    "minimumPrice": 0,
+    "maximumPrice": 123.45
+  },
+  "paymentOptions": {
+    "refundSettings": {
+      "refundPolicy": "Refunds are available 1 month prior to the event",
+      "customRefundPolicy": "In the event you are not able to attend the hobbit dance off due to unforeseen circumstances, The Shire Council needs to be advised in writing no later than 14 days prior to the event in order to receive a full refund.  If your cancellation is less than 14 days prior to the event you will be refunded the cost of the dance off less fireworks charges and a $25 administration fee. By booking you confirm that you are aware of our cancellation policy."
+    }
+  },
+  "publishedAt": "2021-02-01T23:26:13.485Z",
+  "additionalQuestions": [
+    {
+      "_id": "5ac5c5e85aec29000ff064f4",
+      "inputType": "text",
+      "question": "string",
+      "required": true,
+      "description": "string",
+      "perOrder": true,
+      "disabled": true,
+      "createdAt": "2021-02-01T23:26:13.485Z",
+      "updatedAt": "2021-02-01T23:26:13.485Z"
+    }
+  ],
+  "bannerImage": {
+    "url": "https://lh3.googleusercontent.com/proxy/IPr6keWrYy7JmdkUpFwyZB2Ev7OJg3oWLlqKlht7r5YGu2lPkEsiDUiHDkAbcrgLkh9zlPELDvQ-4Qa8F0hBYn2bjw3qlA5RvgwR-tW8ayfG1gfVrUxC6K3Oba0WR1g7cjh3D4eRQPkChlatag"
+  },
+  "featureImage": {
+    "url": "https://lh3.googleusercontent.com/proxy/IPr6keWrYy7JmdkUpFwyZB2Ev7OJg3oWLlqKlht7r5YGu2lPkEsiDUiHDkAbcrgLkh9zlPELDvQ-4Qa8F0hBYn2bjw3qlA5RvgwR-tW8ayfG1gfVrUxC6K3Oba0WR1g7cjh3D4eRQPkChlatag"
+  },
+  "socialImage": {
+    "url": "https://lh3.googleusercontent.com/proxy/IPr6keWrYy7JmdkUpFwyZB2Ev7OJg3oWLlqKlht7r5YGu2lPkEsiDUiHDkAbcrgLkh9zlPELDvQ-4Qa8F0hBYn2bjw3qlA5RvgwR-tW8ayfG1gfVrUxC6K3Oba0WR1g7cjh3D4eRQPkChlatag"
+  },
+  "eventLocation": {
+    "type": "address",
+    "venueName": "",
+    "address": "501 Buckland Road, Hinuera, Matamata 3472, New Zealand",
+    "latLng": [
+      -37.8691623,
+      175.6802895
+    ],
+    "instructions": "Take the guided tour departing from The Shires rest, 15 minutes from Matamata town centre by car.",
+    "placeId": "ChIJP0sTGs-uMioR7xB_WgdR9Bo",
+    "onlineUrl": "www.zoom.com/hobbit-dance-off",
+    "mapUrl": "https://cdn.filestackcontent.com/o5uJJsdJS8uH4PGcyBXx",
+    "city": "Sydney",
+    "region": "NSW",
+    "country": "AU"
+  },
+  "dates": [
+    {
+      "_id": "5c9c25e08965939104239aab",
+      "startDate": "2021-02-01T23:26:13.485Z",
+      "endDate": "2021-02-01T23:26:13.485Z",
+      "scheduleId": "5fb6ceea1b2dec000ab9d367",
+      "disabled": true,
+      "deleted": true
+    }
+  ],
+  "packagedTickets": [
+    {
+      "_id": "5da50970ec90824b5ca3608f",
+      "name": "Family ticket",
+      "price": 120,
+      "quantity": 125,
+      "description": "Includes 2x Adult ticket and 2x Child ticket",
+      "disabled": true,
+      "deleted": true,
+      "tickets": [
+        {
+          "ticketTypeId": "5da50970ec90824b5ca3608f",
+          "quantity": 2
+        }
+      ]
+    }
+  ],
+  "accessibility": {
+    "contactName": "Gandalf The Grey",
+    "contactNumber": "0412345678",
+    "travelInstructions": "The closest drop off point is The Shires Rest. The best public transport option is...",
+    "entryInstructions": "To enter the building there is...",
+    "afterEntryInstructions": "fter entering the building walk 10 meters forward than 3 meters right where you should introduce yourself to reception...",
+    "hazards": "NA",
+    "toiletLocation": "Disabled toilets are located on ground level of the building only.",
+    "disabledParking": "5 spaces available in the Wilsons car park at 151 Example Street.",
+    "features": {
+      "access": true,
+      "wheelchairAccessibility": true,
+      "audioDescription": true,
+      "telephoneTypewriter": true,
+      "volumeControlTelephone": true,
+      "assistiveListeningSystems": true,
+      "signLanguageInterpretation": true,
+      "accessiblePrint": true,
+      "closedCaptioning": true,
+      "openedCaptioning": true,
+      "brailleSymbol": true
+    }
+  },
+  "affiliateCode": {
+    "code": "string"
+  },
+  "keywords": [
+    "sydney"
+  ],
+  "location": "AU",
+  "createdAt": "2021-02-01T23:26:13.485Z",
+  "updatedAt": "2021-02-01T23:26:13.485Z"
+}
