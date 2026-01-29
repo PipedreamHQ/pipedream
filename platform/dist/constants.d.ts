aa1dfb2712fdf8118857f3183df5e9be08dd4656
GTFalcao <gtfalcao96@gmail.com>
2026-01-29 00:23:07 -0300

    Adjusting platform publishing workflow

--- platform/dist/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DEFAULT_POLLING_SOURCE_TIMER_INTERVAL: number;
-export declare const PD_OFFICIAL_GMAIL_OAUTH_CLIENT_ID = "fnd4m13k1mjb6djallp1m9kr7o8kslcu";
--- platform/dist/sql.d.ts
+++
@@ -1,1 +0,0 @@
-export type ExecuteQueryArgs = object | string;
