01e5c4bf50279731613c24c4a6d14bff784859c2
Tyler Malone <19tylermalone94@gmail.com>
2026-02-10 10:09:17 +0900

    feat(highlevel_oauth): add update-business action

---
+++ components/highlevel_oauth/actions/update-business/update-business.mjs
@@ -0,0 +1,46 @@
+import common from "../common/common-business.mjs";
+
+const {
+  props: {
+    app, ...props
+  },
+} = common;
+
+export default {
+  ...common,
+  key: "highlevel_oauth-update-business",
+  name: "Update Business",
+  description: "Updates an existing business in HighLevel. [See the documentation](https://marketplace.gohighlevel.com/docs/ghl/businesses/update-business)",
+  version: "0.0.1",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  type: "action",
+  props: {
+    app,
+    businessId: {
+      propDefinition: [
+        app,
+        "businessId",
+      ],
+    },
+    ...props,
+  },
+  async run({ $ }) {
+    const {
+      app: appRef,
+      businessId,
+      ...data
+    } = this.getData(false);
+    const response = await appRef.updateBusiness({
+      $,
+      businessId,
+      data,
+    });
+
+    $.export("$summary", `Successfully updated business (ID: ${businessId})`);
+    return response;
+  },
+};
