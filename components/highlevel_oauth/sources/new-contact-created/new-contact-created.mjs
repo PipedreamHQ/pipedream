65b962d3a1c6a8bf4d8a0a544160b68f49489c46
Tyler Malone <88516482+19tylermalone94@users.noreply.github.com>
2026-02-11 09:25:58 +0900

    chore(highlevel_oauth): bump patch version in new-contact-created.mjs
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

--- components/highlevel_oauth/sources/new-contact-created/new-contact-created.mjs
+++ components/highlevel_oauth/sources/new-contact-created/new-contact-created.mjs
@@ -6,7 +6,7 @@ export default {
   key: "highlevel_oauth-new-contact-created",
   name: "New Contact Created",
   description: "Emit new event when a new contact is created. [See the documentation](https://marketplace.gohighlevel.com/docs/ghl/contacts/get-contacts)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   methods: {
