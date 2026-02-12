e567fe769f6858d68bc4bb6633da7c2617aea4c7
Jorge Cortes <jacortesmahmud@gmail.com>
2026-02-12 15:04:03 -0500

    Fixed pnpm-lock.yaml (#20015)

--- components/microsoft_entra_id/actions/remove-member-from-group/remove-member-from-group.mjs
+++ components/microsoft_entra_id/actions/remove-member-from-group/remove-member-from-group.mjs
@@ -4,7 +4,7 @@ export default {
   key: "microsoft_entra_id-remove-member-from-group",
   name: "Remove Member From Group",
   description: "Removes a member from a group Microsoft Entra ID. [See the documentation](https://learn.microsoft.com/en-us/graph/api/group-delete-members?view=graph-rest-1.0&tabs=http)",
-  version: "0.0.4",
+  version: "0.0.5",
   annotations: {
     destructiveHint: true,
     openWorldHint: true,
