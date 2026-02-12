aed9e6ded6798c7bad34b4f4b62c55de4b13ef76
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-02-12 17:07:29 -0500

    wip

--- components/new_sloth/new_sloth.app.mjs
+++ components/new_sloth/new_sloth.app.mjs
@@ -1,11 +1,46 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "new_sloth",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://app.newsloth.com/api/v1";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        auth: {
+          username: `${this.$auth.api_key}`,
+          password: `${this.$auth.api_secret}`,
+        },
+        ...opts,
+      });
+    },
+    listSources(opts = {}) {
+      return this._makeRequest({
+        path: "/sources",
+        ...opts,
+      });
+    },
+    createSource(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/sources",
+        ...opts,
+      });
+    },
+    deleteSource({
+      sourceId, ...opts
+    }) {
+      return this._makeRequest({
+        method: "DELETE",
+        path: `/sources/${sourceId}`,
+        ...opts,
+      });
     },
   },
 };
