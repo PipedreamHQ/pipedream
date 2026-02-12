2881acb670b019c83c96e9ffdbef69177d1b2698
Luan Cazarine <luanhc@gmail.com>
2026-02-12 13:05:06 -0300

    13365 components float (#19975)
    * Enhance Float component with new actions and prop definitions
    
    - Added new actions for creating and updating projects and tasks.
    - Introduced prop definitions for client, tags, account, stage, project, phase, and task.
    - Implemented utility functions for object parsing and cleaning.
    - Added constants for budget types, priorities, and project statuses.
    - Updated package version to 0.1.0 and included dependency on @pipedream/platform.
    
    * pnpm update
    
    * pnpm update
    
    * Refactor Float component actions and sources for improved data handling
    
    - Updated pagination parameter from 'per_page' to 'per-page' in float.app.mjs.
    - Enhanced project creation and update actions to handle null values for non_billable, locked_task_list, active, and all_pms_schedule properties.
    - Modified task update action to handle null values for billable property.
    - Improved timestamp handling in base.mjs to default to current date if created date is not provided.
    - Updated summary messages in new-client and new-project sources to include client and project names alongside their IDs.
    
    * Update billable property handling in create-task action to support null values
    
    * update pnpm
    
    * update pnpm

---
+++ components/float/sources/new-project/test-event.mjs
@@ -0,0 +1,29 @@
+export default {
+  "project_id": 12345678,
+  "name": "Project Name",
+  "project_code": "1234",
+  "client_id": 12345678,
+  "color": "C2C2C2",
+  "notes": "Notes for the project",
+  "tags": [
+    "Tag 1",
+    "Tag 2"
+  ],
+  "budget_type": 1,
+  "budget_total": null,
+  "budget_per_phase": 0,
+  "budget_priority": 2,
+  "default_hourly_rate": "12.0000",
+  "non_billable": 0,
+  "tentative": 0,
+  "status": 0,
+  "stage_id": 12345678,
+  "locked_task_list": 1,
+  "active": 1,
+  "project_manager": 12345678,
+  "all_pms_schedule": 1,
+  "created": "2026-02-06 18:00:04",
+  "modified": "2026-02-06 18:17:52",
+  "start_date": "2026-03-03",
+  "end_date": "2026-04-04"
+}
