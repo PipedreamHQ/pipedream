9bd6b463468b2c41475d2fcb182a62b6e0bc490f
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-02-13 14:02:02 -0500

    new components

--- components/new_sloth/new_sloth.app.mjs
+++ components/new_sloth/new_sloth.app.mjs
@@ -3,22 +3,39 @@ import { axios } from "@pipedream/platform";
 export default {
   type: "app",
   app: "new_sloth",
-  propDefinitions: {},
+  propDefinitions: {
+    feedRssUrl: {
+      type: "string",
+      label: "Feed RSS URL",
+      description: "URL of the feed RSS to delete",
+      async options() {
+        const { data } = await this.listSources();
+        return data?.map(({ sourceUrl }) => sourceUrl) || [];
+      },
+    },
+  },
   methods: {
     _baseUrl() {
       return "https://app.newsloth.com/api/v1";
     },
-    _makeRequest({
+    async _makeRequest({
       $ = this, path, ...opts
     }) {
-      return axios($, {
-        url: `${this._baseUrl()}${path}`,
-        auth: {
-          username: `${this.$auth.api_key}`,
-          password: `${this.$auth.api_secret}`,
-        },
-        ...opts,
-      });
+      try {
+        return await axios($, {
+          url: `${this._baseUrl()}${path}`,
+          auth: {
+            username: `${this.$auth.api_key}`,
+            password: `${this.$auth.api_secret}`,
+          },
+          ...opts,
+        });
+      } catch (error) {
+        if (error.status === 400) {
+          throw new Error(`Please ensure your New Sloth plan supports full API access. https://newsloth.com/pricing \n\n${error.message}`);
+        }
+        throw new Error(error.message);
+      }
     },
     listSources(opts = {}) {
       return this._makeRequest({
@@ -33,12 +50,10 @@ export default {
         ...opts,
       });
     },
-    deleteSource({
-      sourceId, ...opts
-    }) {
+    deleteSource(opts = {}) {
       return this._makeRequest({
         method: "DELETE",
-        path: `/sources/${sourceId}`,
+        path: "/sources",
         ...opts,
       });
     },
