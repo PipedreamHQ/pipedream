fa205563a65f4a4be4c2b7617a54540b197f5dbf
Luan Cazarine <luanhc@gmail.com>
2026-02-13 18:29:04 -0300

    13372 components seven (#20018)
    * Update Seven component to version 0.2.0 and enhance functionality
    
    - Bumped package version to 0.2.0 and updated dependency on @pipedream/platform to ^3.2.5.
    - Added new actions for retrieving account analytics and sending voice calls.
    - Introduced subaccount prop definition for filtering data.
    - Updated README with new features and example use cases.
    - Incremented patch versions for existing actions to 0.0.3.
    - Added new sources for handling incoming SMS and voice call events.
    
    * pnpm update
    
    * Refactor Seven component: remove sendVoiceCall action, update makeTTSCall action with new props, and increment version to 0.1.0
    
    - Deleted the sendVoiceCall action as it is no longer needed.
    - Enhanced the makeTTSCall action by adding new optional properties: from, ringtime, and foreignId.
    - Updated the version of the makeTTSCall action to 0.1.0.
    
    * pnpm update

---
+++ components/seven/sources/new-incoming-call-instant/test-event.mjs
@@ -0,0 +1,10 @@
+export default {
+  "webhook_event": "voice_call",
+  "webhook_timestamp": "2024-08-02T07:28:59+02:00",
+  "data": {
+    "id": 0,
+    "caller": "4943160049851",
+    "time": 1722576539,
+    "system": "4915170517246"
+  }
+};
