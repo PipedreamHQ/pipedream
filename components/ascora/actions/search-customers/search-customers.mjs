b936b31487e15b64c68e7ed161db67a28e03d481
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-02-17 12:13:23 -0500

    wip

---
+++ components/ascora/actions/search-customers/search-customers.mjs
@@ -0,0 +1,52 @@
+import ascora from "../../ascora.app.mjs";
+
+export default {
+  key: "ascora-search-customers",
+  name: "Search Customers",
+  description: "Search for customers. [See the documentation](https://www.dropbox.com/scl/fi/75586ygs35arfb7d3cvxi/API-Endpoints-1.2.pdf)",
+  version: "0.0.1",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  type: "action",
+  props: {
+    ascora,
+    query: {
+      type: "string",
+      label: "Query",
+      description: "Search query",
+      optional: true,
+    },
+    page: {
+      type: "integer",
+      label: "Page",
+      description: "Page number",
+      optional: true,
+      default: 1,
+    },
+    pageSize: {
+      type: "integer",
+      label: "Page Size",
+      description: "Page size",
+      optional: true,
+      default: 10,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.ascora.searchCustomers({
+      $,
+      params: {
+        FilterText: this.query,
+        Page: this.page,
+        PageSize: this.pageSize,
+      },
+    });
+
+    $.export("$summary", `Successfully fetched ${response.results?.length ?? 0} customer${response.results?.length === 1
+      ? ""
+      : "s"}`);
+    return response;
+  },
+};
--- components/ascora/ascora.app.mjs
+++ components/ascora/ascora.app.mjs
@@ -1,11 +1,29 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "ascora",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return "https://api.ascora.com.au";
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          Auth: this.$auth.api_key,
+        },
+        ...opts,
+      });
+    },
+    searchCustomers(opts = {}) {
+      return this._makeRequest({
+        path: "/Customers/Customers",
+        ...opts,
+      });
     },
   },
 };
