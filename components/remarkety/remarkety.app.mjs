c62d467e8cbe5e34e8c17e55220763067b0c9453
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-02-18 17:07:19 -0500

    wip

--- components/remarkety/remarkety.app.mjs
+++ components/remarkety/remarkety.app.mjs
@@ -1,11 +1,42 @@
+import { axios } from "@pipedream/platform";
+
 export default {
   type: "app",
   app: "remarkety",
   propDefinitions: {},
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    _baseUrl() {
+      return `https://app.remarkety.com/api/v2/stores/${this.$auth.store_id}`;
+    },
+    _makeRequest({
+      $ = this, path, ...opts
+    }) {
+      return axios($, {
+        url: `${this._baseUrl()}${path}`,
+        headers: {
+          "X-API-Key": `${this.$auth.api_token}`,
+        },
+        ...opts,
+      });
+    },
+    listCustomers(opts = {}) {
+      return this._makeRequest({
+        path: "/customers",
+        ...opts,
+      });
+    },
+    listProducts(opts = {}) {
+      return this._makeRequest({
+        path: "/products",
+        ...opts,
+      });
+    },
+    createDiscount(opts = {}) {
+      return this._makeRequest({
+        method: "POST",
+        path: "/discounts",
+        ...opts,
+      });
     },
   },
 };
