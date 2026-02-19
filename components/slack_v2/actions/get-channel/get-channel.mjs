83314f8f833ad8a2c4bb6f60ff68ec4e2892546b
vetriviky <47851115+vetriviky@users.noreply.github.com>
2026-02-19 18:37:23 +0530

    changes with respect to review comment
    changes with respect to review comment

--- components/slack_v2/actions/get-channel/get-channel.mjs
+++ components/slack_v2/actions/get-channel/get-channel.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack_v2-get-channel",
   name: "Get Channel Details",
   description: "Retrieve details for a Slack channel by selecting it or providing an ID. [See the documentation](https://api.slack.com/methods/conversations.info)",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "action",
   annotations: {
     destructiveHint: false,
@@ -22,20 +22,34 @@ export default {
       ],
       description: "Select a channel or enter a channel ID manually.",
     },
+    includeLocale: {
+      type: "boolean",
+      label: "Include Locale",
+      description: "Set to `true` to receive the locale for this channel",
+      default: false,
+      optional: true,
+    },
+    includeNumberOfMembers: {
+      type: "boolean",
+      label: "Number of members",
+      description: "Set to `true` to receive the number of member of this channel",
+      default: false,
+      optional: true,
+    },
   },
 
   async run({ $ }) {
 
     const response = await this.slack.conversationsInfo({
       channel: this.channel,
-      include_locale: true,
-      include_num_members: true,
+      include_locale: this.includeLocale,
+      include_num_members: this.includeNumberOfMembers,
     });
 
-    const channel = response.channel;
+    const channelName = response.channel?.name || response.channel?.id || this.channel;
 
-    $.export("$summary", `Fetched details for channel "${channel.name || channel.id}"`);
+    $.export("$summary", `Fetched details for channel ${channelName}`);
 
-    return channel;
+    return response;
   },
 };
