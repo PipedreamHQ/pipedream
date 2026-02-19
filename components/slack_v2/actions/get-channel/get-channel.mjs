6a5f6b80db3f4a7e4fbde0bc3444f0c20c744544
vetriviky <47851115+vetriviky@users.noreply.github.com>
2026-02-19 17:39:27 +0530

    Update get-channel.mjs

--- components/slack_v2/actions/get-channel/get-channel.mjs
+++ components/slack_v2/actions/get-channel/get-channel.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack_v2-get-channel",
   name: "Get Channel Details",
   description: "Retrieve details for a Slack channel by selecting it or providing an ID. [See the documentation](https://api.slack.com/methods/conversations.info)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   annotations: {
     destructiveHint: false,
@@ -25,9 +25,6 @@ export default {
   },
 
   async run({ $ }) {
-    if (!this.channel) {
-      throw new Error("Please select a channel or provide a channel ID");
-    }
 
     const response = await this.slack.conversationsInfo({
       channel: this.channel,
