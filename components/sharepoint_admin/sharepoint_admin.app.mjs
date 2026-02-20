5c2cdbfc8518b42a3e9914fe360cc41694becff9
Luan Cazarine <luanhc@gmail.com>
2026-02-19 21:11:09 -0300

    Remove unused propDefinitions and authKeys method from sharepoint_admin app component

--- components/sharepoint_admin/sharepoint_admin.app.mjs
+++ components/sharepoint_admin/sharepoint_admin.app.mjs
@@ -3,11 +3,4 @@ import sharepoint from "../sharepoint/sharepoint.app.mjs";
 export default {
   ...sharepoint,
   app: "sharepoint_admin",
-  propDefinitions: {},
-  methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
-    },
-  },
 };
