d7df96e978718ed34c90b4bef7a633edab47f803
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-03-06 16:19:24 -0500

    fix key

--- components/ipgeolocation/actions/parse-bulk-user-agents/parse-bulk-user-agents.mjs
+++ components/ipgeolocation/actions/parse-bulk-user-agents/parse-bulk-user-agents.mjs
@@ -1,8 +1,8 @@
 import ipgeolocation from "../../ipgeolocation.app.mjs";
 
 export default {
-  key: "ipgeolocation-parse-bulk-user-agent",
-  name: "Parse Bulk User Agent",
+  key: "ipgeolocation-parse-bulk-user-agents",
+  name: "Parse Bulk User Agents",
   description:
     "Extract browser, device, operating system, and engine details from multiple user agent strings in a single request. Maximum 50,000 strings per request. Only available on paid plans. [See the documentation](https://ipgeolocation.io/documentation/user-agent-api.html#parse-bulk-user-agent-strings)",
   version: "0.0.1",
