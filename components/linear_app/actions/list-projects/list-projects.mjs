f5c2f5a39a7709e0037b11a485a18c7a12b3775b
Dylan J. Sather <dylan.sather@gmail.com>
2026-03-09 11:14:51 -0700

    Removing eval() from Linear components (#20212)

--- components/linear_app/actions/list-projects/list-projects.mjs
+++ components/linear_app/actions/list-projects/list-projects.mjs
@@ -6,7 +6,7 @@ export default {
   name: "List Projects",
   description: "List projects in Linear. [See the documentation](https://studio.apollographql.com/public/Linear-API/variant/current/schema/reference/objects/ProjectConnection?query=projects).",
   type: "action",
-  version: "0.0.2",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
