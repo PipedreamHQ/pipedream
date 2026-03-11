018c218f2a1a5f18b5f7e3c71b3529c182430e38
michelle0927 <michelle0927@users.noreply.github.com>
2026-03-11 15:52:38 -0400

    Linear App - new components (#20236)
    * new components
    
    * versions
    
    * updates

---
+++ components/linear_app/actions/create-comment/create-comment.mjs
@@ -0,0 +1,52 @@
+import linearApp from "../../linear_app.app.mjs";
+
+export default {
+  key: "linear_app-create-comment",
+  name: "Create Comment",
+  description: "Create a comment in Linear. [See the documentation](https://studio.apollographql.com/public/Linear-API/variant/current/schema/reference/objects/Mutation?query=comment)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    linearApp,
+    teamId: {
+      propDefinition: [
+        linearApp,
+        "teamId",
+      ],
+      description: "Filter issue selection by team",
+      optional: true,
+    },
+    issueId: {
+      propDefinition: [
+        linearApp,
+        "issueId",
+        (c) => ({
+          teamId: c.teamId,
+        }),
+      ],
+      description: "The issue to create the comment on",
+    },
+    body: {
+      type: "string",
+      label: "Body",
+      description: "The body of the comment",
+    },
+  },
+  async run({ $ }) {
+    const response = await this.linearApp.createComment({
+      issueId: this.issueId,
+      body: this.body,
+    });
+
+    if (response?._comment) {
+      $.export("$summary", `Successfully created comment with ID ${response._comment.id}`);
+    }
+
+    return response;
+  },
+};
---
+++ components/linear_app/actions/remove-label-from-issue/remove-label-from-issue.mjs
@@ -0,0 +1,54 @@
+import linearApp from "../../linear_app.app.mjs";
+
+export default {
+  key: "linear_app-remove-label-from-issue",
+  name: "Remove Label from Issue",
+  description: "Remove a label from an issue in Linear. [See the documentation](https://studio.apollographql.com/public/Linear-API/variant/current/schema/reference/objects/Mutation?query=issueremovelabel)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: true,
+    openWorldHint: true,
+    readOnlyHint: false,
+  },
+  props: {
+    linearApp,
+    teamId: {
+      propDefinition: [
+        linearApp,
+        "teamId",
+      ],
+      description: "Filter selected issues by team",
+      optional: true,
+    },
+    issueId: {
+      propDefinition: [
+        linearApp,
+        "issueId",
+        ({ teamId }) => ({
+          teamId,
+        }),
+      ],
+      description: "The ID of the issue to remove the label from",
+      optional: false,
+    },
+    labelId: {
+      propDefinition: [
+        linearApp,
+        "issueLabels",
+        () => ({
+          byId: true,
+        }),
+      ],
+      type: "string",
+      label: "Label",
+      description: "The ID of the label to remove from the issue",
+      optional: false,
+    },
+  },
+  async run({ $ }) {
+    const response = await this.linearApp.removeLabelFromIssue(this.issueId, this.labelId);
+    $.export("$summary", `Successfully removed label from issue ${this.issueId}`);
+    return response;
+  },
+};
