d60c1ec1f6bcb937541d6ec054531a318dc6ccfa
Desel72 <pedroluiscolmenares722@gmail.com>
2026-03-11 11:29:03 -0500

    Update components/jira_data_center/actions/get-server-info/get-server-info.mjs
    Co-authored-by: Jorge Cortes <jacortesmahmud@gmail.com>

--- components/jira_data_center/actions/get-server-info/get-server-info.mjs
+++ components/jira_data_center/actions/get-server-info/get-server-info.mjs
@@ -3,7 +3,7 @@ import jiraDataCenter from "../../jira_data_center.app.mjs";
 export default {
   key: "jira_data_center-get-server-info",
   name: "Get Server Info",
-  description: "Returns general information about the Jira Data Center instance, including version, base URL, and deployment type. [See the documentation](https://developer.atlassian.com/server/jira/platform/rest-apis/)",
+  description: "Returns general information about the Jira Data Center instance, including version, base URL, and deployment type. [See the documentation](https://developer.atlassian.com/server/jira/platform/rest/v11003/api-group-serverinfo/#api-api-2-serverinfo-get)",
   version: "0.0.1",
   type: "action",
   annotations: {
