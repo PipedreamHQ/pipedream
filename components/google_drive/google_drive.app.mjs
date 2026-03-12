5e74eb1abd77c4bd42e9545255a4e591237abcfc
Luan Cazarine <luanhc@gmail.com>
2026-03-12 14:00:44 -0300

    Enhance Google Drive component: Added 'Include Items From All Drives' option to list files action and updated version to 0.2.0. Bumped package version to 1.5.3.

--- components/google_drive/google_drive.app.mjs
+++ components/google_drive/google_drive.app.mjs
@@ -370,6 +370,12 @@ export default {
         };
       },
     },
+    includeItemsFromAllDrives: {
+      label: "Include Items From All Drives",
+      type: "boolean",
+      description: "If `true`, include items from all drives. If `false`, include items from the drive specified in the `drive` prop.",
+      default: false,
+    },
   },
   methods: {
     // Static methods
