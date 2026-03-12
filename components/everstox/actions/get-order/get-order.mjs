8f5d03f2ae17eb55e4d4113dc5d5741d0a41b610
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-03-12 16:11:14 -0400

    new components

---
+++ components/everstox/actions/get-order/get-order.mjs
@@ -0,0 +1,33 @@
+import everstox from "../../everstox.app.mjs";
+
+export default {
+  key: "everstox-get-order",
+  name: "Get Order",
+  description: "Get an order from Everstox. [See the documentation](https://api.everstox.com/api/v1/ui/#/Order/district_core.api.shops.orders.orders.Orders.get)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    everstox,
+    orderId: {
+      propDefinition: [
+        everstox,
+        "orderId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const response = await this.everstox.getOrder({
+      $,
+      orderId: this.orderId,
+    });
+
+    $.export("$summary", `Successfully retrieved order with ID \`${this.orderId}\`.`);
+
+    return response;
+  },
+};
