1d86d68950bc30576e3a1504247e055e2924c80e
michelle0927 <michelle0927@users.noreply.github.com>
2026-03-18 15:06:47 -0400

    Freshdesk - Add Forward Ticket, Create Thread Message (with subject), and Reply to Forward actions (#20288)
    * updates
    
    * versions
    
    * versions
    
    * version

--- components/freshdesk/actions/create-solution-article/create-solution-article.mjs
+++ components/freshdesk/actions/create-solution-article/create-solution-article.mjs
@@ -6,7 +6,7 @@ export default {
   key: "freshdesk-create-solution-article",
   name: "Create Solution Article",
   description: "Create a solution article in Freshdesk. [See the documentation](https://developers.freshdesk.com/api/#solution_article_attributes)",
-  version: "0.0.5",
+  version: "0.0.6",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
