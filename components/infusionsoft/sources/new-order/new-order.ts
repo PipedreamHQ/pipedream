469e6aaa4e7ad5f170b7aa49cb040fa5de68388a
Caleb Lehman <caleb.lehman@robosource.us>
2026-03-19 17:05:38 -0400

    update source versions

--- components/infusionsoft/sources/new-order/new-order.ts
+++ components/infusionsoft/sources/new-order/new-order.ts
@@ -8,7 +8,7 @@ export default defineSource({
   description:
     "Emit new event for each new **order** [See docs here](https://developer.infusionsoft.com/docs/rest/#operation/getOrderUsingGET)",
   key: "infusionsoft-new-order",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   methods: {
     ...common.methods,
--- components/infusionsoft/sources/new-payment/new-payment.ts
+++ components/infusionsoft/sources/new-payment/new-payment.ts
@@ -8,7 +8,7 @@ export default defineSource({
   description:
     "Emit new event for each new **payment** [See docs here](https://developer.infusionsoft.com/docs/rest/#operation/getTransactionUsingGET)",
   key: "infusionsoft-new-payment",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   methods: {
     ...common.methods,
