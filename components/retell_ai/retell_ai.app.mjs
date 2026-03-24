8284342bfa04b9000e1ac0fcd1ec95bcf7191a66
michelle0927 <michelle0927@users.noreply.github.com>
2026-03-24 14:36:28 -0400

    Retell AI - new source component (#20364)
    * new source
    
    * pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Michelle Bergeron <michelle.pipedream@gmail.com>

--- components/retell_ai/retell_ai.app.mjs
+++ components/retell_ai/retell_ai.app.mjs
@@ -1,11 +1,35 @@
+import Retell from "retell-sdk";
+
 export default {
   type: "app",
   app: "retell_ai",
-  propDefinitions: {},
+  propDefinitions: {
+    agentId: {
+      type: "string",
+      label: "Agent ID",
+      description: "The ID of the agent",
+      async options() {
+        const agents = await this.listAgents();
+        return agents.map((agent) => ({
+          label: agent.agent_name,
+          value: agent.agent_id,
+        }));
+      },
+    },
+  },
   methods: {
-    // this.$auth contains connected account data
-    authKeys() {
-      console.log(Object.keys(this.$auth));
+    client() {
+      return new Retell({
+        apiKey: this.$auth.api_key,
+      });
+    },
+    listAgents() {
+      return this.client().agent.list();
+    },
+    updateAgent({
+      agentId, data,
+    }) {
+      return this.client().agent.update(agentId, data);
     },
   },
 };
