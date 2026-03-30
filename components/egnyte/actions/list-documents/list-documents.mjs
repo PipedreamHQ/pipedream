a71189d5fe5f8e0ea1c8d34a990f28d62c3a4c91
Michelle Bergeron <michelle.pipedream@gmail.com>
2026-03-30 16:45:19 -0400

    new actions

---
+++ components/egnyte/actions/list-documents/list-documents.mjs
@@ -0,0 +1,35 @@
+import egnyte from "../../egnyte.app.mjs";
+
+export default {
+  key: "egnyte-list-documents",
+  name: "List Documents",
+  description: "List documents in your Egnyte workspace. [See the documentation](https://developers.egnyte.com/api-docs/read/file-system-management-api-documentation)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    egnyte,
+    folderPath: {
+      type: "string",
+      label: "Folder Path",
+      description: "List documents within this folder. Defaults to the root folder.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const { files } = await this.egnyte.getFolder({
+      $,
+      folderPath: this.folderPath
+        ? this.folderPath.replace(/^\//, "")
+        : "",
+    });
+    $.export("$summary", `Successfully retrieved ${files?.length || 0} document${files?.length != 1
+      ? "s"
+      : ""}.`);
+    return files || [];
+  },
+};
