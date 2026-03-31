19ebbfc09cdb2780be1a16bc022d8001b787bd32
Danny Roosevelt <danny@pipedream.com>
2026-03-31 08:23:33 -0700

    Versions

--- components/google_sheets/actions/find-rows/find-rows.mjs
+++ components/google_sheets/actions/find-rows/find-rows.mjs
@@ -13,7 +13,7 @@ export default {
     + " for subsequent **Update Rows** calls)."
     + " For simple reads without filtering, use **Read Rows**"
     + " instead.",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   annotations: {
     destructiveHint: false,
