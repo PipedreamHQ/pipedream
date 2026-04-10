f4dd95ea096089975daab48703368f754b2988dc
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-04-10 14:57:02 -0400

    new components

---
+++ components/liveagent/actions/add-tag-to-ticket/add-tag-to-ticket.mjs
@@ -0,0 +1,53 @@
+import liveagent from "../../liveagent.app.mjs";
+
+export default {
+  key: "add-tag-to-ticket",
+  name: "Add Tag to Ticket",
+  description: "Add a tag to a ticket. [See the documentation](https://support.liveagent.com/911737-API-v3)",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    liveagent,
+    ticketId: {
+      propDefinition: [
+        liveagent,
+        "ticketId",
+      ],
+    },
+    tagId: {
+      propDefinition: [
+        liveagent,
+        "tagId",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const ticket = await this.liveagent.getTicket({
+      $,
+      ticketId: this.ticketId,
+    });
+
+    ticket.tags = ticket.tags || [];
+    if (!ticket.tags.includes(this.tagId)) {
+      ticket.tags = [
+        ...ticket.tags,
+        this.tagId,
+      ];
+    }
+
+    const response = await this.liveagent.updateTicket({
+      $,
+      ticketId: this.ticketId,
+      data: {
+        ...ticket,
+      },
+    });
+    $.export("$summary", `Successfully added tag to ticket with ID: ${this.ticketId}`);
+    return response;
+  },
+};
