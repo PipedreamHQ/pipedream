7bf93070f9ae51efe480242bcd141e7126de162e
Luan Cazarine <luanhc@gmail.com>
2026-04-14 17:32:55 -0300

    Fix description typo in Get Return Order Emails action for clarity

--- components/returnista/actions/get-return-order-emails/get-return-order-emails.mjs
+++ components/returnista/actions/get-return-order-emails/get-return-order-emails.mjs
@@ -3,7 +3,7 @@ import returnista from "../../returnista.app.mjs";
 export default {
   key: "returnista-get-return-order-emails",
   name: "Get Return Order Emails",
-  description: "Returns emails related to return a order. [See the documentation](https://platform.returnista.com/reference/rest-api/#get-/account/-accountId/return-orders/emails)",
+  description: "Returns emails related to a return order. [See the documentation](https://platform.returnista.com/reference/rest-api/#get-/account/-accountId/return-orders/emails)",
   version: "0.0.1",
   annotations: {
     destructiveHint: false,
