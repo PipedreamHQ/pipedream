7a8f6f8bce335d8b2eeb7aa613573171a45b21f8
danhsiung <35384182+danhsiung@users.noreply.github.com>
2026-04-14 18:32:58 -0700

    Adding app scaffolding for pubrio

---
+++ components/pubrio/pubrio.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "pubrio",
+  propDefinitions: {},
+  methods: {
+    // this.$auth contains connected account data
+    authKeys() {
+      console.log(Object.keys(this.$auth));
+    },
+  },
+};
