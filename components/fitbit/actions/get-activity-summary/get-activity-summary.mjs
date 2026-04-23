62fffd6fc81f3117c54e93a82ed07398af106c88
Zhanhan Liu <belugazhan@gmail.com>
2026-04-23 13:36:53 -0400

    fix(fitbit): address versioning and lint issues

--- components/fitbit/actions/get-activity-summary/get-activity-summary.mjs
+++ components/fitbit/actions/get-activity-summary/get-activity-summary.mjs
@@ -21,7 +21,9 @@ export default {
     },
   },
   async run({ $ }) {
-    const date = this.date || new Date().toISOString().slice(0, 10);
+    const date = this.date || new Date()
+      .toISOString()
+      .slice(0, 10);
     const response = await this.fitbit.getActivitySummary({
       $,
       date,
