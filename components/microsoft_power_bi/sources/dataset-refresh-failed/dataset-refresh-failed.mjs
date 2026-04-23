669229241e403fb27fe7a2332420d16b45471867
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-04-23 11:39:23 -0400

    fix source descriptions

--- components/microsoft_power_bi/sources/dataset-refresh-failed/dataset-refresh-failed.mjs
+++ components/microsoft_power_bi/sources/dataset-refresh-failed/dataset-refresh-failed.mjs
@@ -3,7 +3,7 @@ import common from "../common.mjs";
 export default {
   key: "microsoft_power_bi-dataset-refresh-failed",
   name: "Dataset Refresh Failed",
-  description: "Emits an event when a dataset refresh operation has failed in Power BI. [See the documentation](https://learn.microsoft.com/en-us/rest/api/power-bi/datasets/get-refresh-history)",
+  description: "Emit new event when a dataset refresh operation has failed in Power BI. [See the documentation](https://learn.microsoft.com/en-us/rest/api/power-bi/datasets/get-refresh-history)",
   version: "0.0.6",
   type: "source",
   dedupe: "unique",
