44fd89fc0d154acecfffd4da9959475b08fec7e3
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-04-28 12:21:24 -0400

    add page & perPage props

--- components/statuspage/actions/fetch-active-incidents/fetch-active-incidents.mjs
+++ components/statuspage/actions/fetch-active-incidents/fetch-active-incidents.mjs
@@ -3,7 +3,7 @@ import statuspage from "../../statuspage.app.mjs";
 export default {
   key: "statuspage-fetch-active-incidents",
   name: "Fetch Active Incidents",
-  description: "Fetch first 100 unresolved (active) incidents for a Statuspage page — i.e. incidents not yet in the `resolved` or `postmortem` state. Returns an array (possibly empty) of incident objects. [See the documentation](https://developer.statuspage.io/#operation/getPagesPageIdIncidentsUnresolved)",
+  description: "Fetch unresolved (active) incidents for a Statuspage page — i.e. incidents not yet in the `resolved` or `postmortem` state. Returns an array (possibly empty) of incident objects. [See the documentation](https://developer.statuspage.io/#operation/getPagesPageIdIncidentsUnresolved)",
   version: "0.0.1",
   type: "action",
   annotations: {
@@ -19,11 +19,29 @@ export default {
         "pageId",
       ],
     },
+    page: {
+      type: "integer",
+      label: "Page",
+      description: "The page number to fetch",
+      optional: true,
+      default: 1,
+    },
+    perPage: {
+      type: "integer",
+      label: "Per Page",
+      description: "The number of incidents to fetch per page",
+      optional: true,
+      default: 100,
+    },
   },
   async run({ $ }) {
     const response = await this.statuspage.getUnresolvedIncidents({
       $,
       pageId: this.pageId,
+      params: {
+        page: this.page,
+        per_page: this.perPage,
+      },
     });
 
     if (response?.length) {
