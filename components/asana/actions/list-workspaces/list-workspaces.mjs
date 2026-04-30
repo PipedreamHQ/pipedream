4f4b48bbc7afd341d39f16033e6b98fb8f249f98
michelle0927 <michelle0927@users.noreply.github.com>
2026-04-30 17:57:20 -0400

    Update components/asana/actions/list-workspaces/list-workspaces.mjs
    Co-authored-by: Guilherme Falcão <48412907+GTFalcao@users.noreply.github.com>

--- components/asana/actions/list-workspaces/list-workspaces.mjs
+++ components/asana/actions/list-workspaces/list-workspaces.mjs
@@ -16,7 +16,7 @@ export default {
     optFields: {
       type: "string[]",
       label: "Opt Fields",
-      description: "This endpoint returns a resource which excludes some properties by default. To include those optional properties, set this query parameter to a list of the properties you wish to include (e.g. `[\"name\", \"email_domains\", \"is_organization\"]`).",
+      description: "This action returns a resource which excludes some properties by default. To include those optional properties, set this prop to a list of the properties you wish to include (e.g. `[\"name\", \"email_domains\", \"is_organization\"]`).",
       optional: true,
       options: [
         "resource_type",
