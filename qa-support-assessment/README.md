05257de3d8546a29be8d42e0beef11919f21cf21
Michelle Bergeron <michelle.bergeron@gmail.com>
2026-05-05 10:46:37 -0400

    qa-support-assessment

---
+++ qa-support-assessment/README.md
@@ -0,0 +1,12 @@
+
+### Mock Support Request:
+
+I'm trying to use the Zendesk actions in my workflow but I'm running into issues with both of them.
+
+### Issue 1: Create Ticket action fails
+
+I'm using the **Create Ticket** action to automatically create support tickets in our Zendesk instance when customers fill out a form on our website. When I test the action, I get back an error from the Zendesk API. I've double-checked my Zendesk connection and it seems to be authenticated correctly.
+
+### Issue 2: Search Tickets returns no results
+
+To troubleshoot, I tried using the **Search Tickets** action to see if maybe tickets were being created but with wrong data. I used the query `type:tickets` to search for all tickets, but the search returns an error even though I know we have hundreds of tickets in our Zendesk account.
---
+++ qa-support-assessment/search-tickets.mjs
@@ -0,0 +1,51 @@
+import { axios } from "@pipedream/platform";
+
+export default {
+  key: "zendesk-search-tickets",
+  name: "Search Tickets",
+  // intentional typo on next line
+  description: "Search for tickets using Zendesk's search API. [See the docuentation](https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#search-tickets).",
+  type: "action",
+  version: "0.0.1",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    zendesk: {
+      type: "app",
+      app: "zendesk",
+    },
+    // query gets error with "type:tickets" (plural)
+    query: {
+      type: "string",
+      label: "Query",
+      description: "The search query to find tickets. You can use Zendesk's search syntax. Example: `type:ticket status:open priority:high`. [See the documentation](https://developer.zendesk.com/documentation/ticketing/using-the-zendesk-api/searching-with-the-zendesk-api/)",
+    },
+  },
+  methods: {
+    searchTickets({
+      $, ...args
+    }) {
+      return axios($, {
+        baseURL: `https://${this.zendesk.$auth.subdomain}.zendesk.com`,
+        url: "/api/v2/search",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.zendesk.$auth.oauth_access_token}`,
+        },
+        ...args,
+      });
+    },
+  },
+  async run({ $ }) {
+    const response = await this.searchTickets({
+      $,
+      params: {
+        query: this.query,
+      },
+    });
+    return response;
+  },
+};
