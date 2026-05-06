126e5364ced019f2e37a87e12694ab329bb2af91
kesku <admin@kesku.me>
2026-05-06 23:37:58 +0100

    Exa - move payload test under common

--- components/exa/common/exa.test.mjs
+++ components/exa/common/exa.test.mjs
@@ -1,9 +1,9 @@
 import { jest } from "@jest/globals";
-import app from "./exa.app.mjs";
-import answerQuestion from "./actions/answer-question/answer-question.mjs";
-import findSimilarLinks from "./actions/find-similar-links/find-similar-links.mjs";
-import getContents from "./actions/get-contents/get-contents.mjs";
-import search from "./actions/search/search.mjs";
+import app from "../exa.app.mjs";
+import answerQuestion from "../actions/answer-question/answer-question.mjs";
+import findSimilarLinks from "../actions/find-similar-links/find-similar-links.mjs";
+import getContents from "../actions/get-contents/get-contents.mjs";
+import search from "../actions/search/search.mjs";
 
 describe("Exa component payloads", () => {
   it("adds the Exa integration tracking header", () => {
