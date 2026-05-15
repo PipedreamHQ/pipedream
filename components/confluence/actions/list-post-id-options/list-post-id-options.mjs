a8901829b7f2d1657a44ec9e498491f866c8b7db
michelle0927 <michelle0927@users.noreply.github.com>
2026-05-15 15:18:34 -0400

    new actions (#20884)
    Co-authored-by: Michelle Bergeron <michelle.bergeron@ahp7jvtgvfr.celonis.cloud>

---
+++ components/confluence/actions/list-post-id-options/list-post-id-options.mjs
@@ -0,0 +1,32 @@
+import confluence from "../../confluence.app.mjs";
+
+export default {
+  key: "confluence-list-post-id-options",
+  name: "List Post ID Options",
+  description: "Retrieves available options for the Post ID field.",
+  version: "0.0.1",
+  type: "action",
+  annotations: {
+    destructiveHint: false,
+    openWorldHint: true,
+    readOnlyHint: true,
+  },
+  props: {
+    confluence,
+    cursor: {
+      type: "string",
+      label: "Cursor",
+      description: "The cursor to start from. Use the cursor to paginate through the results.",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const options = await confluence.propDefinitions.postId.options.call(this.confluence, {
+      cursor: this.cursor,
+    });
+    $.export("$summary", `Successfully retrieved ${options.length} option${options.length === 1
+      ? ""
+      : "s"}`);
+    return options;
+  },
+};
