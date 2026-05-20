728a8f39f466130823d16bcad0ffa4ba8f00682f
Ashwin Srinivas <ashwin.srinivas@workday.com>
2026-05-20 11:22:42 +0530

    chore(spotify): update package versions

--- components/spotify/actions/add-items-to-playlist/add-items-to-playlist.mjs
+++ components/spotify/actions/add-items-to-playlist/add-items-to-playlist.mjs
@@ -4,7 +4,7 @@ export default {
   name: "Add Items to a Playlist",
   description: "Add one or more items to a user’s playlist. [See the docs here](https://developer.spotify.com/documentation/web-api/reference/#/operations/add-tracks-to-playlist).",
   key: "spotify-add-items-to-playlist",
-  version: "0.1.5",
+  version: "0.1.6",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
