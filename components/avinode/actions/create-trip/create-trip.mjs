b03fc32c1c005a7db2ab000c386be8279dfac6c8
e11man <115963151+e11man@users.noreply.github.com>
2026-05-27 10:55:20 -0400

    Update components/avinode/actions/create-trip/create-trip.mjs
    Co-authored-by: michelle0927 <michelle0927@users.noreply.github.com>

--- components/avinode/actions/create-trip/create-trip.mjs
+++ components/avinode/actions/create-trip/create-trip.mjs
@@ -5,7 +5,7 @@ export default {
   name: "Create Trip",
   description:
     "Create a trip in Avinode. [See the documentation](https://developer.avinodegroup.com/reference/createtrip)",
-  version: "0.0.2",
+  version: "0.0.1",
   type: "action",
   annotations: {
     destructiveHint: false,
