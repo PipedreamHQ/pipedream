71feba0cbd9637d14788ddfaba0426cd5d6bb7f0
Michelle Bergeron <michelle.bergeron@ahp7jvtgvfr.celonis.cloud>
2026-05-28 15:35:02 -0400

    versions

--- components/helpspot/actions/list-email-from-options/list-email-from-options.mjs
+++ components/helpspot/actions/list-email-from-options/list-email-from-options.mjs
@@ -4,7 +4,7 @@ export default {
   key: "helpspot-list-email-from-options",
   name: "List Send Email From Options",
   description: "Retrieves available options for the Send Email From field.",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   annotations: {
     destructiveHint: false,
