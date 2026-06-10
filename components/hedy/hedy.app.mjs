6452e460500ed1b7d1bcc4fed2f0a72c086f1b43
michelle0927 <michelle0927@users.noreply.github.com>
2026-06-10 11:57:38 -0400

    Update components/hedy/hedy.app.mjs

--- components/hedy/hedy.app.mjs
+++ components/hedy/hedy.app.mjs
@@ -64,7 +64,7 @@ export default {
   },
   methods: {
     _baseUrl() {
-      return this.$auth.base_url || "https://api.hedy.bot";
+      return "https://api.hedy.bot";
     },
     _headers() {
       return {
