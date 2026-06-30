e8ae07e9bc967e1752f409e6b723973ca55da623
GTFalcao <gtfalcao96@gmail.com>
2026-06-29 23:04:26 -0300

    Fixing readOnlyHint on several components

--- components/typeform/actions/get-form/get-form.mjs
+++ components/typeform/actions/get-form/get-form.mjs
@@ -5,11 +5,11 @@ export default {
   name: "Get a Form",
   description: "Select a specific form to get the data. [See the docs here](https://developer.typeform.com/create/reference/retrieve-form/)",
   type: "action",
-  version: "0.0.3",
+  version: "0.0.4",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
-    readOnlyHint: false,
+    readOnlyHint: true,
   },
   props: {
     typeform,
