510c357465368fd1e9eb7edf6c05b4c04683edbf
Tom Phillips <tom@tomphillips.uk>
2026-07-01 22:13:16 +0100

    Address CodeRabbit review: actions/get-task-status/get-task-status.mjs

--- components/loopquest/actions/get-task-status/get-task-status.mjs
+++ components/loopquest/actions/get-task-status/get-task-status.mjs
@@ -3,9 +3,14 @@ import app from "../../loopquest.app.mjs";
 export default {
   key: "loopquest-get-task-status",
   name: "Get Task Status",
-  description: "Check a LoopQuest task's status / verdict. [See the docs](https://loopquest.tomphillips.uk/docs).",
+  description: "Check a LoopQuest task's status / verdict. [See the documentation](https://loopquest.tomphillips.uk/docs).",
   version: "0.0.1",
   type: "action",
+  annotations: {
+    readOnlyHint: true,
+    destructiveHint: false,
+    openWorldHint: true,
+  },
   props: {
     loopquest: app,
     taskId: { propDefinition: [app, "taskId"] },
