c171e50a9f7821f284a7415736fac77b7844d7aa
Ashwin Srinivas <ashwin.srinivas@workday.com>
2026-07-02 22:16:37 +0530

    chore(sunshine): update package version

--- components/sunshine_conversations/sources/new-message-delivered-to-channel/new-message-delivered-to-channel.mjs
+++ components/sunshine_conversations/sources/new-message-delivered-to-channel/new-message-delivered-to-channel.mjs
@@ -5,7 +5,7 @@ export default {
   key: "sunshine_conversations-new-message-delivered-to-channel",
   name: "New Message Delivered to Channel (Instant)",
   description: "Emit new event when a new message is delivered to a channel. [See the documentation](https://developer.zendesk.com/api-reference/conversations/#tag/Webhooks/operation/CreateWebhook)",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "source",
   dedupe: "unique",
   methods: {
