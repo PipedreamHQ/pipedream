76b417332617c9b8a24a5986fdfbe79f93a0e2f4
Dhruv Maniya <dhruvmaniya1998@gmail.com>
2026-07-15 20:57:59 +0530

    Bump Gorgias component versions

--- components/gorgias_oauth/actions/create-ticket-message/create-ticket-message.mjs
+++ components/gorgias_oauth/actions/create-ticket-message/create-ticket-message.mjs
@@ -6,7 +6,7 @@ export default {
   key: "gorgias_oauth-create-ticket-message",
   name: "Create Ticket Message",
   description: "Create a message for a ticket in the Gorgias system. [See the documentation](https://developers.gorgias.com/reference/create-ticket-message)",
-  version: "0.1.2",
+  version: "0.1.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
