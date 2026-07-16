481e0b85d218cd9ffe83ad8c267b0dd047a12975
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2026-07-16 21:31:14 +0900

    fix(jira): clarify Cloud ID discovery

--- components/jira/actions/get-cloud-id/get-cloud-id.mjs
+++ components/jira/actions/get-cloud-id/get-cloud-id.mjs
@@ -3,8 +3,8 @@ import jira from "../../jira.app.mjs";
 export default {
   key: "jira-get-cloud-id",
   name: "Get Cloud ID",
-  description: "Gets the cloud ID and details of all accessible Jira Cloud sites. [See the documentation](https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/)",
-  version: "0.0.5",
+  description: "Lists all accessible Jira Cloud sites and their IDs. Run this before any Jira action that requires a Cloud ID, then pass the selected site's `id` to that action. [See the documentation](https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/#3-1-get-the-cloudid-for-your-site)",
+  version: "0.0.6",
   type: "action",
   annotations: {
     destructiveHint: false,
