b778a3de41df14923f64716083754ed90de7c846
Michelle Bergeron <michelle.bergeron@workday.com>
2026-07-17 11:56:47 -0400

    update create-ticket-message

--- components/gorgias_oauth/actions/update-customer/update-customer.mjs
+++ components/gorgias_oauth/actions/update-customer/update-customer.mjs
@@ -10,7 +10,7 @@ export default {
   key: "gorgias_oauth-update-customer",
   name: "Update Customer",
   description: "Update a customer. [See the docs](https://developers.gorgias.com/reference/put_api-customers-id-)",
-  version: "0.0.11",
+  version: "0.0.12",
   annotations: {
     destructiveHint: true,
     openWorldHint: true,
