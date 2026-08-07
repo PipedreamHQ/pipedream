ad1c0b91f9943fe3da68a47728be2b35b3ee4318
Ashwin Srinivas <ashwin.srinivas@workday.com>
2026-08-07 14:04:16 +0530

    fix(jira): Modify descriptions

--- components/jira/actions/get-cloud-id/get-cloud-id.mjs
+++ components/jira/actions/get-cloud-id/get-cloud-id.mjs
@@ -3,7 +3,7 @@ import jira from "../../jira.app.mjs";
 export default {
   key: "jira-get-cloud-id",
   name: "Get Cloud ID",
-  description: "Lists all accessible Jira Cloud sites and their IDs. Run this before any Jira action that requires a Cloud ID, then pass the selected site's `id` to that action. [See the documentation](https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/#3-1-get-the-cloudid-for-your-site)",
+  description: "Lists all accessible Jira Cloud sites and their IDs. [See the documentation](https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/#3-1-get-the-cloudid-for-your-site)",
   version: "0.0.6",
   type: "action",
   annotations: {
