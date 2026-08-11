233ed04af2f9593d4996fa1d3399c932ddbdbdd4
Priyadharshan <priyadharshan@pipedream.com>
2026-08-11 13:15:26 +0530

    Fix location name formatting in Google My Business components
    Use the app's clean name helper to correctly format account and location
    IDs when constructing request paths.

--- components/google_my_business/sources/new-review-created-multiple-locations/new-review-created-multiple-locations.ts
+++ components/google_my_business/sources/new-review-created-multiple-locations/new-review-created-multiple-locations.ts
@@ -13,7 +13,7 @@ export default defineSource({
   key: "google_my_business-new-review-created-multiple-locations",
   name: "New Review Created (Multiple Locations)",
   description: `Emit new event for each new review on any of the selected locations [See the documentation](${DOCS_LINK})`,
-  version: "0.0.4",
+  version: "0.0.5",
   type: "source",
   dedupe: "unique",
   props: {
@@ -43,10 +43,13 @@ export default defineSource({
         account, location,
       } = this;
 
+      const accountId = this.app.getCleanName(account);
+
       const params: BatchGetReviewsParams = {
-        account,
+        account: accountId,
         data: {
-          locationNames: location?.map((locationName: string) => `accounts/${account}/locations/${locationName}`),
+          locationNames: location?.map((locationName: string) =>
+            `accounts/${accountId}/locations/${this.app.getCleanName(locationName)}`),
         },
       };
 
