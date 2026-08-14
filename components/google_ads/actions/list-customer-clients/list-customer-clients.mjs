b4105f7e946a456840a3ebd50cf7532ac7e86cc9
Ashwin Srinivas <103921345+ashwins01@users.noreply.github.com>
2026-08-15 02:29:34 +0530

    [21604] feat(google_ads): migrate from google-ads v21 to v25 (#21631)
    * feat(google_ads): migrate from google-ads v21 to v25
    
    * fix(google_ads): address coderabbit review comments
    
    * fix(google_ads): modify descriptions, address coderabbit review comments
    
    * fix(google_ads): fix v21 to v25 migration gaps
    
    * fix(google_ads): support extra conversion-fields for v25

--- components/google_ads/actions/list-customer-clients/list-customer-clients.mjs
+++ components/google_ads/actions/list-customer-clients/list-customer-clients.mjs
@@ -3,8 +3,8 @@ import googleAds from "../../google_ads.app.mjs";
 export default {
   key: "google_ads-list-customer-clients",
   name: "List Customer Clients",
-  description: "List all customer clients for a given account. [See the documentation](https://developers.google.com/google-ads/api/reference/rpc/v21/GoogleAdsService/Search?transport=rest)",
-  version: "0.0.2",
+  description: "List all customer clients for a given account. [See the documentation](https://developers.google.com/google-ads/api/reference/rpc/v25/GoogleAdsService/Search?transport=rest)",
+  version: "0.0.3",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
