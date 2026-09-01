863364e8175e98b77f7cbb5a445cbd8904d5c97b
Priyadharshan <priyadharshan@pipedream.com>
2026-09-01 12:46:39 +0530

    version bumps

--- components/airtable_oauth/actions/create-or-update-record/create-or-update-record.mjs
+++ components/airtable_oauth/actions/create-or-update-record/create-or-update-record.mjs
@@ -7,7 +7,7 @@ export default {
   key: "airtable_oauth-create-or-update-record",
   name: "Create or Update Record",
   description: "Create a new record, or update an existing one if a Record ID is provided (an upsert). Leave `Record ID` blank to create a record from `record`; supply it to update that record's fields instead. Use **List Tables** to look up field names first, and **List Records** to find an existing record's ID. [See the create-record documentation](https://airtable.com/developers/web/api/create-records) and the [update-record documentation](https://airtable.com/developers/web/api/update-record)",
-  version: "0.2.0",
+  version: "1.0.0",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
--- components/airtable_oauth/actions/get-record-or-create/get-record-or-create.mjs
+++ components/airtable_oauth/actions/get-record-or-create/get-record-or-create.mjs
@@ -7,7 +7,7 @@ export default {
   key: "airtable_oauth-get-record-or-create",
   name: "Get Record Or Create",
   description: "Fetch a record by its Record ID. If the ID is blank, or doesn't match an existing record, create a new record instead using `record`. Use **List Tables** to look up field names first, and **List Records** to find an existing record's ID. [See the get-record documentation](https://airtable.com/developers/web/api/get-record) and the [create-record documentation](https://airtable.com/developers/web/api/create-records)",
-  version: "0.1.0",
+  version: "1.0.0",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
