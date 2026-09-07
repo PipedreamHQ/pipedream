323539778d4f8826af9cc03ab1dbf51bf0faaff7
Ashwin Srinivas <103921345+ashwins01@users.noreply.github.com>
2026-09-07 12:39:37 +0530

    fix(slack_v2): Remove slack web-api version pin (#21884)
    * fix(slack_v2): bump version to 8.1.1
    
    * chore(slack_v2): bump patch versions

--- components/slack_v2/actions/list-reminder-options/list-reminder-options.mjs
+++ components/slack_v2/actions/list-reminder-options/list-reminder-options.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack_v2-list-reminder-options",
   name: "List Reminder Options",
   description: "Retrieves available options for the Reminder field. [See the documentation](https://docs.slack.dev/reference/methods/reminders.list)",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "action",
   ai: "optimized",
   annotations: {
