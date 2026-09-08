c93b91d631523530fab46007e6e4d2e8601c2948
Moshe Grunwald <moshe.grunwald@workday.com>
2026-09-08 11:38:17 -0400

    fix(jira): bump versions above master after merge (check_version)

--- components/jira/actions/add-watcher-to-issue/add-watcher-to-issue.mjs
+++ components/jira/actions/add-watcher-to-issue/add-watcher-to-issue.mjs
@@ -3,7 +3,7 @@ import jira from "../../jira.app.mjs";
 export default {
   key: "jira-add-watcher-to-issue",
   name: "Add Watcher To Issue",
-  version: "0.0.24",
+  version: "0.0.25",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
