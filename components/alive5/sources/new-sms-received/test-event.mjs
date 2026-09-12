32503132fdb323ebce566b95c494f33b02ec31a8
RaghavOjha <jeh267@txstate.edu>
2026-09-12 15:20:48 -0500

    Add Alive5 SMS actions and managed webhook source

---
+++ components/alive5/sources/new-sms-received/test-event.mjs
@@ -0,0 +1,10 @@
+export default {
+  message: "Example inbound SMS",
+  from_phone: "+14155550123",
+  business_line: "+14155550100",
+  channel_id: "sample-channel",
+  thread_id: "sample-thread",
+  direction: "inbound",
+  received_at: "2026-09-12T12:00:00.000Z",
+  media_url: null,
+};
