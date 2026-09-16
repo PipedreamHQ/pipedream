da8a453f6b448ba413071b130eddef1b9edf357b
Ashwin Srinivas <ashwin.srinivas@workday.com>
2026-09-16 20:49:55 +0530

    chore(slack): bump patch version

--- components/slack_v2/actions/list-replies/list-replies.mjs
+++ components/slack_v2/actions/list-replies/list-replies.mjs
@@ -4,7 +4,7 @@ export default {
   key: "slack_v2-list-replies",
   name: "List Replies",
   description: "Retrieve a thread of messages posted to a conversation. [See the documentation](https://api.slack.com/methods/conversations.replies)",
-  version: "0.0.36",
+  version: "0.0.37",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
