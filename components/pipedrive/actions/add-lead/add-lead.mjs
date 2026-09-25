148ae68077a94f40a829034cd86e02ffe34c1c05
Priyadharshan <priyadharshan@pipedream.com>
2026-09-25 14:13:08 +0530

    Version Bumps

--- components/pipedrive/actions/add-lead/add-lead.mjs
+++ components/pipedrive/actions/add-lead/add-lead.mjs
@@ -6,7 +6,7 @@ export default {
   key: "pipedrive-add-lead",
   name: "Add Lead",
   description: "Create a new lead in Pipedrive. [See the documentation](https://developers.pipedrive.com/docs/api/v1/Leads#addLead)",
-  version: "0.0.20",
+  version: "0.0.21",
   annotations: {
     destructiveHint: false,
     openWorldHint: true,
--- components/pipedrive/actions/list-filters/list-filters.mjs
+++ components/pipedrive/actions/list-filters/list-filters.mjs
@@ -5,7 +5,7 @@ export default {
   key: "pipedrive-list-filters",
   name: "List Filters",
   description: "List filters in your Pipedrive account, optionally scoped to a single entity type. [See the documentation](https://developers.pipedrive.com/docs/api/v1/Filters#getFilters)",
-  version: "0.0.2",
+  version: "0.0.3",
   type: "action",
   ai: "optimized",
   annotations: {
